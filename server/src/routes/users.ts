5867b64f59f1e2b67008bc3f869ae1d5b34c7ea1
Steven Lucco <steveluc@microsoft.com>
2016-09-21 11:52:41 -0700

    add math agent to live server

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+var agent = require('./agent');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/math', function(req, res, next) {
+  var answer = false;
+  if (req.query.text && req.query.axiom && req.query.vname) {
+      var checker = agent.createChecker(req.query.axiom, req.query.vname);
+      answer = checker.check(req.query.text);
+  }
+  var msg = answer?('on the right track: ' + req.query.text + " leads to " + req.query.axiom):'keep trying'
+  res.send('agent says: '+msg);
 });
 
 module.exports = router;
