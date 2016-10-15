7209285a8b4001cef3ede55a3c1875ad17a344bf
kurtb <kurtberglund@hotmail.com>
2016-10-15 09:21:56 -0700

    Restyling - prep for reload of calendar

--- server/src/calendar/driver.ts
+++ server/src/calendar/driver.ts
@@ -1,26 +1,16 @@
 import * as $ from 'jquery';
 import * as _ from 'lodash';
-import { pnhost, IEchoService, EchoServiceName, ITableService, TableServiceName, ITableListener } from '../api/index';
+import { Promise } from 'es6-promise';
+import { pnhost, IEchoService, EchoServiceName, ITable, ITableService, TableServiceName, ITableListener } from '../api/index';
 var fullcalendar = require('fullcalendar');
 var qtip = require('qtip2');
 
-// TODO better wrap this into some kind of model structure
-var rowsModel: any[] = [];
-var pendingDeletes: any[] = [];
+class TableListener implements ITableListener {
+    constructor(private _viewModel: CalendarViewModel) {
+    }
 
-class TableListener implements ITableListener {    
     rowsChanged(rows: any[]): Promise<void> {
-        // compute the difference between the existing rowsModel and the updates
-        let deletedRows = _.filter(rowsModel, (rowModel) => _.find(rows, (row) => row.id === rowModel.id) === undefined);
-        $('#calendar').fullCalendar('removeEvents', (event) => {
-            return _.find(deletedRows, (deletedRow) => deletedRow.id === event.id);
-        });        
-
-        // Add the rows to delete to the pending list
-        pendingDeletes = pendingDeletes.concat(deletedRows);
-
-        // Update the model with the changed rows
-        rowsModel = rows;
+        this._viewModel.rowsChanged(rows);
 
         return Promise.resolve();
     }
@@ -30,113 +20,181 @@ class TableListener implements ITableListener {
     }
 }
 
-$(document).ready(() => {
-    let tableServiceP: Promise<ITableService>;
-    if (pnhost) {
-        tableServiceP = pnhost.listServices().then((services) => {
-            return _.includes(services, TableServiceName) ? pnhost.getService(TableServiceName) : Promise.resolve(null);
+class RemoteCalendar {
+    constructor() {
+    }
+
+    getCalendars(): Promise<Calendar[]> {
+        return new Promise((resolve, reject) => {
+            $.ajax("/calendars", {
+                cache: false,
+                dataType: "json",
+                success: (calData: Calendar[]) => resolve(calData),
+                error: (jqXHR: JQueryXHR, textStatus: string, errorThrown: string) => reject(jqXHR)
+            });
         });
     }
-    else {
-        tableServiceP = Promise.resolve(null);
+}
+
+class CalendarViewModel {
+    private _calendar = new RemoteCalendar();
+    private _tableServiceP: Promise<ITableService>;
+    private _cachedCalendars: Calendar[];
+    private _pendingDeletes: any[] = [];
+    private _tableP: Promise<ITable>;
+    private _tableListener: TableListener;
+    private _tableBoundRows: any[];
+
+    constructor() {
     }
 
-    $.ajax("/calendars", {
-        cache: false,
-        dataType: "json",
-        success: (calData: Calendar[]) => {
-            if (pnhost) {
-                $("#buttons").append($('<button id="load-table">Export to Table</button><button id="save">Save</button>'));
-                
-                // Save processes any pending calendar changes
-                $("#save").click(() => {
-                    // iterate over the deletedRows URLs and delete them
-                    for (let pendingDelete of pendingDeletes) {
-                        $.ajax(pendingDelete.self, { method: "DELETE" });
-                        console.log(`DELETE: ${pendingDelete.self}`);
-                    }
-                });
+    private initView() {
+        $("#buttons").append($('<div class="fc-right"><div class="fc-button-group"><button id="reload">Reload</button></div></div>'));
+        $("#reload").click(() => {
+        });
 
-                $("#load-table").click(() => {
-                    tableServiceP.then((tableService) => {
-                        if (!tableService) {
-                            return;
-                        }
+        if (pnhost) {
+            $("#buttons").append($('<div class="fc-left"><div class="fc-button-group"><button id="load-table">Export to Table</button></div></div>'));
+            $("#buttons .fc-right .fc-button-group").append('<button id="save">Save</button>');
 
-                        tableService.createTable().then((table) => {
-                            let columns = ['provider', 'title', 'location', 'start', 'end', 'responseStatus'];                            
-                            for (let calendar of calData) {
-                                for (let event of calendar.events) {
-                                    rowsModel.push({
-                                        id: event.id,
-                                        provider: calendar.sourceName,
-                                        title: event.title,
-                                        location: event.location,
-                                        start: event.start,
-                                        end: event.end,
-                                        responseStatus: event.responseStatus,
-                                        self: event.self
-                                    })
-                                }
-                            }
-
-                            table.loadData(columns, rowsModel);
-
-                            // Listen for updates
-                            table.addListener(new TableListener());
-                        });
-                    })
-                });
-            }
-
-            // page is now ready, initialize the calendar...
-            var fcOptions = <FullCalendar.Options>{
-                minTime: "07:00:00",
-                maxTime: "21:00:00",
-                weekends: false,
-                height: "auto",
-                eventRender: (event, element) => {
-                    var content = event.title;
-                    if (event.location && (event.location.length > 0)) {
-                        content += ("<br/>" + event.location);
-                    }
-                    if (event.responseStatus && (event.responseStatus.length > 0)) {
-                        content += ("<br/>" + event.responseStatus);
-                    }
-                    var qtipOptions: QTip2.QTipOptions = {
-                        content: content,
-                        position: {
-                            my: "left center",
-                            at: "center"
-                        }
-                    };
-                    element.qtip(qtipOptions);
+            // Save processes any pending calendar changes
+            $("#save").click(() => {
+                // iterate over the deletedRows URLs and delete them
+                for (let pendingDelete of this._pendingDeletes) {
+                    $.ajax(pendingDelete.self, { method: "DELETE" });
+                    console.log(`DELETE: ${pendingDelete.self}`);
                 }
-            };
+            });
 
-            var events: FullCalendar.EventObject[] = [];
-            for (var ncal = calData.length, ical = 0; ical < ncal; ical++) {
-                var cal = calData[ical];
-                var borderColor = (ical == 0) ? "black" : "darkblue";
-                var color = (ical == 0) ? "purple" : "green";
-
-                for (var nevent = cal.events.length, iev = 0; iev < nevent; iev++) {
-                    let ev = cal.events[iev];
-                    events.push({
-                        id: ev.id,
-                        title: ev.title,
-                        color: color,
-                        borderColor: borderColor,
-                        location: ev.location,
-                        responseStatus: ev.responseStatus,                        
-                        start: new Date(ev.start),
-                        end: new Date(ev.end),
-                    });
+            $("#load-table").click(() => {
+                if (!this._tableP) {
+                    this._tableP = this._tableServiceP.then((tableService) => tableService.createTable());
                 }
-            }
-            fcOptions.events = events;
-            $('#calendar').fullCalendar(fcOptions);
-            $('#calendar').fullCalendar('changeView', 'agendaWeek');
+
+                this._tableP.then((table) => {
+                    let columns = ['provider', 'title', 'location', 'start', 'end', 'responseStatus'];
+
+                    // Get and store the rows we will bind to the pnhost table
+                    this._tableBoundRows = [];
+                    for (let calendar of this._cachedCalendars) {
+                        for (let event of calendar.events) {
+                            this._tableBoundRows.push({
+                                id: event.id,
+                                provider: calendar.sourceName,
+                                title: event.title,
+                                location: event.location,
+                                start: event.start,
+                                end: event.end,
+                                responseStatus: event.responseStatus,
+                                self: event.self
+                            })
+                        }
+                    }
+
+                    // Load the rows into the hosted table
+                    table.loadData(columns, this._tableBoundRows);
+
+                    // Setup a table listener if it doesn't already exist 
+                    if (!this._tableListener) {
+                        this._tableListener = new TableListener(this);
+                        table.addListener(this._tableListener);
+                    }
+                })
+            });
         }
-    });
-});
+    }
+
+    private loadAndCacheCalendars(): Promise<Calendar[]> {
+        return this._calendar.getCalendars().then((calendars) => {
+            this._cachedCalendars = calendars;
+            return calendars;
+        })
+    }
+
+    rowsChanged(rows: any[]) {
+        // compute the difference between the received rows and the bound data
+        let deletedRows = _.filter(this._tableBoundRows, (rowModel) => _.find(rows, (row) => row.id === rowModel.id) === undefined);
+        $('#calendar').fullCalendar('removeEvents', (event) => {
+            return _.find(deletedRows, (deletedRow) => deletedRow.id === event.id);
+        });
+
+        // Add the rows to delete to the pending list
+        this._pendingDeletes = this._pendingDeletes.concat(deletedRows);
+
+        // Update the bound data values
+        this._tableBoundRows = rows;
+    }
+
+    init() {
+        if (pnhost) {
+            this._tableServiceP = pnhost.listServices().then((services) => {
+                return _.includes(services, TableServiceName) ? pnhost.getService(TableServiceName) : Promise.resolve(null);
+            });
+        }
+        else {
+            this._tableServiceP = Promise.resolve(null);
+        }
+
+        $(document).ready(() => {
+            let eventsP = this.loadAndCacheCalendars();
+            eventsP.then((cachedCalendars) => {
+                // Initialize the custom UI once we load the first batch of data
+                this.initView();
+
+                // page is now ready, initialize the calendar...
+                var fcOptions = <FullCalendar.Options>{
+                    minTime: "07:00:00",
+                    maxTime: "21:00:00",
+                    weekends: false,
+                    height: "auto",
+                    eventRender: (event, element) => {
+                        var content = event.title;
+                        if (event.location && (event.location.length > 0)) {
+                            content += ("<br/>" + event.location);
+                        }
+                        if (event.responseStatus && (event.responseStatus.length > 0)) {
+                            content += ("<br/>" + event.responseStatus);
+                        }
+                        var qtipOptions: QTip2.QTipOptions = {
+                            content: content,
+                            position: {
+                                my: "left center",
+                                at: "center"
+                            }
+                        };
+                        element.qtip(qtipOptions);
+                    }
+                };
+
+                var events: FullCalendar.EventObject[] = [];
+                for (var ncal = cachedCalendars.length, ical = 0; ical < ncal; ical++) {
+                    var cal = cachedCalendars[ical];
+                    var borderColor = (ical == 0) ? "black" : "darkblue";
+                    var color = (ical == 0) ? "purple" : "green";
+
+                    for (var nevent = cal.events.length, iev = 0; iev < nevent; iev++) {
+                        let ev = cal.events[iev];
+                        events.push({
+                            id: ev.id,
+                            title: ev.title,
+                            color: color,
+                            borderColor: borderColor,
+                            location: ev.location,
+                            responseStatus: ev.responseStatus,
+                            start: new Date(ev.start),
+                            end: new Date(ev.end),
+                        });
+                    }
+                }
+
+                fcOptions.events = events;
+                $('#calendar').fullCalendar(fcOptions);
+                $('#calendar').fullCalendar('changeView', 'agendaWeek');
+            });
+        });
+    }
+}
+
+// Create and initialize the view model that will bind to and run the UI
+let viewModel = new CalendarViewModel();
+viewModel.init();
