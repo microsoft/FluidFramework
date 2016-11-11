92d09dd9834ca472895b9f6702e2bb7435553cec
Kurt Berglund <kurtb@microsoft.com>
2016-11-10 22:42:10 -0800

    Support connect and disconnect messages

--- server/src/collab/collab.ts
+++ server/src/collab/collab.ts
@@ -1,3 +1,36 @@
 import * as io from 'socket.io-client';
+import * as Quill from 'quill';
+import * as $ from 'jquery';
 
 let socket = io();
+
+socket.on('user connect', (msg) => {
+    $("#console").append('<div>New user connected</div>');
+});
+
+socket.on('user disconnect', (msg) => {
+    $("#console").append('<div>User disconnected</div>');
+});
+
+// Implement and register module
+Quill.register('modules/counter', function(quill, options) {
+    var container = document.querySelector(options.container);
+    quill.on('text-change', function() {
+        var text = quill.getText();
+        // There are a couple issues with counting words
+        // this way but we'll fix these later
+        container.innerHTML = text.split(/\s+/).length;
+
+        socket.emit('chat message', text);
+    });
+});
+
+var editor = new Quill('#editor', {
+    modules: { 
+        toolbar: '#toolbar',
+        counter: {
+        container: '#counter'
+        } 
+    },
+    theme: 'snow'
+});
--- server/src/io.ts
+++ server/src/io.ts
@@ -3,8 +3,15 @@ import socketIo = require('socket.io');
 var io = socketIo();
 
 io.on('connection', (socket) => {
-    console.log('a user connected!');
-});
+    socket.on('chat message', (msg) => {
+        console.log(`message: ` + msg);
+    });
 
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user disconnect');        
+    })
+
+    socket.broadcast.emit('user connect', 'hi');
+});
 
 export default io;
