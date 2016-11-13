569dc6f7e5422a9ee534ce6668284acc3b11eb55
Kurt Berglund <kurtb@microsoft.com>
2016-11-13 08:58:20 -0800

    Use redis for socket.io coordination

--- server/src/io.ts
+++ server/src/io.ts
@@ -1,7 +1,31 @@
 import socketIo = require('socket.io');
+import * as redis from 'redis';
+var socketIoRedis = require('socket.io-redis');
 import * as deltasDB from './db/deltas';
+import * as nconf from 'nconf';
+import * as _ from 'lodash';
 
 var io = socketIo();
+
+let host = nconf.get("redis:host");
+let port = nconf.get("redis:port");
+let pass = nconf.get("redis:pass");
+
+// Setup redis options
+let options: any = { auth_pass: pass };
+if (nconf.get('redis:tls')) {
+    options.tls = {
+        servername: host
+    }
+}
+
+let pubOptions = _.clone(options);
+let subOptions = _.clone(options);
+subOptions.return_buffers = true;
+
+var pub = redis.createClient(port, host, pubOptions);
+var sub = redis.createClient(port, host, subOptions);
+io.adapter(socketIoRedis({ pubClient: pub, subClient: sub }));
 
 interface IAppendOperation {
     room: string;
@@ -23,10 +47,7 @@ io.on('connection', (socket) => {
             });        
     });
 
-    socket.on('append', (append: IAppendOperation) => {
-        console.log('Append received');
-        console.log(JSON.stringify(append, null, 2));
-
+    socket.on('append', (append: IAppendOperation) => {        
         deltasDB.append(append.room, append.ops);
         socket.to(append.room).broadcast.emit('append', append.ops);        
     });
