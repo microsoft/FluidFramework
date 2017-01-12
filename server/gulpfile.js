32026981582c2d01cd5026fe7f828184fe5454c2
Kurt Berglund <kurtb@Kurts-MacBook-Pro.local>
2017-01-11 20:51:08 -0800

    Simplify the gulpfile used to build

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -36,98 +36,33 @@ gulp.task("build", function () {
         })
 });
 
-// Site app
-gulp.task("browserify", function() {
-    return browserify({
-            basedir: '.',
-            debug: true,
-            entries: ['src/ng/main.ts'],
-            cache: {},
-            packageCache: {}
-        })        
-        .plugin(tsify)        
-        .bundle()        
-        .pipe(source('main.js'))
-        .pipe(gulp.dest("public/dist/ng"))
-});
+// Browserified client code definitions
+let controllers = [
+    { name: "browserify", src: "src/ng/main.ts", outFile: "main.js", folder: "public/dist/ng", standalone: "controller" },
+    { name: "api", src: "src/api/index.ts", outFile: "api.js", folder: "public/dist/api", standalone: "pronet" },
+    { name: "calendar", src: "src/calendar/driver.ts", outFile: "driver.js", folder: "public/dist/views/calendar", standalone: "calendar" },
+    { name: "collab", src: "src/collab/collab.ts", outFile: "collab.js", folder: "public/dist/collab", standalone: "collab" },
+    { name: "canvas", src: "src/canvas/canvas.ts", outFile: "canvas.js", folder: "public/dist/canvas", standalone: "canvas" },
+];
 
-// Client side Interactive Document API
-gulp.task("api", function() {
-    return browserify({
-            basedir: '.',
-            debug: true,
-            entries: ['src/api/index.ts'],
-            cache: {},
-            packageCache: {},
-            standalone: 'pronet'
-        })        
-        .plugin(tsify)        
-        .bundle()        
-        .pipe(source('api.js'))
-        .pipe(gulp.dest("public/dist/api"))
-});
+// Generate tasks for the browserified code
+for (let controller of controllers) {
+    gulp.task(controller.name, function() {
+        return browserify({
+                basedir: '.',
+                debug: true,
+                entries: controller.src,
+                cache: {},
+                packageCache: {},
+                standalone: controller.standalone,
+            })        
+            .plugin(tsify)        
+            .bundle()        
+            .pipe(source(controller.outFile))
+            .pipe(gulp.dest(controller.folder))
+    });
+}
 
-// Calendar app
-gulp.task("calendar", function() {
-    return browserify({
-            basedir: '.',
-            debug: true,
-            entries: ['src/calendar/driver.ts'],
-            cache: {},
-            packageCache: {}
-        })        
-        .plugin(tsify)        
-        .bundle()        
-        .pipe(source('driver.js'))
-        .pipe(gulp.dest("public/dist/views/calendar"))
-});
+let browserifyTasks = controllers.map(function(controller) { return controller.name });
 
-// Collab app
-gulp.task("collab", function() {
-    return browserify({
-            basedir: '.',
-            debug: true,
-            entries: ['src/collab/collab.ts'],
-            cache: {},
-            packageCache: {},
-            standalone: 'collab'
-        })        
-        .plugin(tsify)        
-        .bundle()        
-        .pipe(source('collab.js'))
-        .pipe(gulp.dest("public/dist/collab"))
-});
-
-// ShareDB app
-gulp.task("sharedb", function() {
-    return browserify({
-            basedir: '.',
-            debug: true,
-            entries: ['src/collab/sharedb.ts'],
-            cache: {},
-            packageCache: {},
-            standalone: 'collab'
-        })        
-        .plugin(tsify)        
-        .bundle()        
-        .pipe(source('sharedb.js'))
-        .pipe(gulp.dest("public/dist/collab"))
-});
-
-// Canvas controller
-gulp.task("canvas", function() {
-    return browserify({
-            basedir: '.',
-            debug: true,
-            entries: ['src/canvas/canvas.ts'],
-            cache: {},
-            packageCache: {},
-            standalone: 'canvas'
-        })        
-        .plugin(tsify)        
-        .bundle()        
-        .pipe(source('canvas.js'))
-        .pipe(gulp.dest("public/dist/canvas"))
-});
-
-gulp.task("default", ["build", "browserify", "api", "calendar", "collab", "sharedb", "canvas"]);
+gulp.task("default", ["build"].concat(browserifyTasks));
