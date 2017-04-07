9bdbaaf93dec8bbc14d3e77fe9c6e825648492ee
Kurt Berglund <kurtb@microsoft.com>
2017-04-07 14:54:44 -0700

    Serialize deltas to mongodb

--- routerlicious/src/deli/index.ts
+++ routerlicious/src/deli/index.ts
@@ -1,22 +1,11 @@
 import { Client, Sender } from "azure-event-hubs";
 import * as nconf from "nconf";
 import * as path from "path";
-import * as socketIoEmitter from "socket.io-emitter";
 import * as socketStorage from "../socket-storage";
 import * as utils from "../utils";
 
 // Setup the configuration system - pull arguments, then environment variables
 nconf.argv().env(<any> "__").file(path.join(__dirname, "../../config.json")).use("memory");
-
-// Initialize Socket.io and connect to the Redis adapter
-// TODO put in the extra stuff here
-let host = nconf.get("redis:host");
-let port = nconf.get("redis:port");
-
-let io = socketIoEmitter(({ host, port }));
-io.redis.on("error", (error) => {
-    console.error(error);
-});
 
 // Configure access to the event hub
 const rawDeltasConfig = nconf.get("eventHub:raw-deltas");
@@ -44,9 +33,7 @@ function listenForMessages(client: Client, sender: Sender, id: string) {
                 console.log(`Received message on partition ${id} with key ${message.partitionKey}`);
 
                 // TODO assign the sequence number here
-
-                // Route the updated message to connected clients
-                console.log(`Routing message to clients`);
+                console.log(`Assigning sequence number`);
                 const submitOpMessage = message.body as socketStorage.ISubmitOpMessage;
                 const routedMessage: socketStorage.IRoutedOpMessage = {
                     clientId: submitOpMessage.clientId,
@@ -54,8 +41,8 @@ function listenForMessages(client: Client, sender: Sender, id: string) {
                     op: submitOpMessage.op,
                     sequenceNumber: sequenceNumber++,
                 };
-                io.to(submitOpMessage.objectId).emit("op", routedMessage);
 
+                // Serialize the sequenced message to the event hub
                 console.log(`Serializing sequenced message to event hub`);
                 sender.send(routedMessage, routedMessage.objectId);
             });
--- routerlicious/src/scriptorium/index.ts
+++ routerlicious/src/scriptorium/index.ts
@@ -1,10 +1,43 @@
 import { Client } from "azure-event-hubs";
+import { Collection, MongoClient } from "mongodb";
 import * as nconf from "nconf";
 import * as path from "path";
+import * as socketIoEmitter from "socket.io-emitter";
+import * as socketStorage from "../socket-storage";
 import * as utils from "../utils";
 
 // Setup the configuration system - pull arguments, then environment variables
 nconf.argv().env(<any> "__").file(path.join(__dirname, "../../config.json")).use("memory");
+
+// Initialize Socket.io and connect to the Redis adapter
+// TODO put in the extra stuff here
+let host = nconf.get("redis:host");
+let port = nconf.get("redis:port");
+
+let io = socketIoEmitter(({ host, port }));
+io.redis.on("error", (error) => {
+    console.error(error);
+});
+
+// Connect to the database
+const mongoUrl = nconf.get("mongo:endpoint");
+const mongoClientP = MongoClient.connect(mongoUrl);
+const collectionP = mongoClientP.then(async (db) => {
+    const deltasCollectionName = nconf.get("mongo:collectionNames:deltas");
+    const collection = db.collection(deltasCollectionName);
+
+    // TODO remove once we've stabalized
+    await collection.drop();
+
+    const indexP = collection.createIndex({
+            objectId: 1,
+            sequenceNumber: 1,
+        },
+        { unique: true });
+
+    await indexP;
+    return collection;
+});
 
 // Configure access to the event hub where we'll send sequenced packets
 const deltasConfig = nconf.get("eventHub:deltas");
@@ -12,21 +45,35 @@ const deltasConnectionString = utils.getEventHubConnectionString(deltasConfig.en
 const client = Client.fromConnectionString(deltasConnectionString, deltasConfig.entityPath);
 const consumerGroup = nconf.get("scriptorium:consumerGroup");
 
-function listenForMessages(receiveClient: Client, id: string) {
+async function processMessage(message: socketStorage.IRoutedOpMessage, db: Collection): Promise<void> {
+    // Serialize the message to backing store
+    console.log(`Inserting to mongodb`);
+    db.insert(message).catch((error) => {
+        console.error(error);
+    });
+
+    // Route the message to clients
+    console.log(`Routing message to clients`);
+    io.to(message.objectId).emit("op", message);
+}
+
+async function listenForMessages(receiveClient: Client, id: string) {
+    const deltas = await collectionP;
+
     // TODO I'm limiting to messages after now - which we'll want to remove once we have proper checkpointing
     receiveClient.createReceiver(consumerGroup, id, { startAfterTime: Date.now() }).then((receiver) => {
-            console.log(`Receiver created for partition ${id}`);
-            receiver.on("errorReceived", (error) => {
-                console.log(error);
-            });
-
-            receiver.on("message", (message) => {
-                console.log(`${id}: Key ${message.partitionKey} Seq# ${message.body.sequenceNumber}`);
-
-                // Store to disk
-            });
-            console.log("Listening");
+        console.log(`Receiver created for partition ${id}`);
+        receiver.on("errorReceived", (error) => {
+            console.log(error);
         });
+
+        receiver.on("message", (message) => {
+            console.log(`${id}: Key ${message.partitionKey} Seq# ${message.body.sequenceNumber}`);
+            processMessage(message.body, deltas);
+        });
+
+        console.log("Listening");
+    });
 }
 
 client.open().then(() => {
