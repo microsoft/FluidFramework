a0759cb56cacef8aba4371bc43d579505fce8577
Kurt Berglund <kurtb@microsoft.com>
2017-04-19 15:07:10 -0700

    Protocol definition

---
+++ routerlicious/src/api/protocol.ts
@@ -0,0 +1,41 @@
+// Is there a doc comment genrator for TS?
+
+export interface IMessageBase {
+    clientId: string;
+
+    clientSequenceNumber: number;
+
+    referenceSequenceNumber: number;
+
+    minimumSequenceNumber: number;
+
+    // The collaborative object the operation is intended for
+    objectId: string;
+
+    op: IDelta;
+}
+
+export interface IMessage extends IMessageBase {
+}
+
+export interface ISequencedMessage extends IMessageBase {
+    sequenceNumber: number;
+}
+
+export interface IDelta {
+}
+
+export const enum SegTreeMsgType {
+    INSERT,
+    REMOVE
+}
+
+export interface SegTreeDeltaMsg extends IDelta {
+    /**
+     * Type of this change.
+     */
+    type: SegTreeMsgType;
+    pos1: number;
+    pos2?: number;
+    text?: string;
+}
