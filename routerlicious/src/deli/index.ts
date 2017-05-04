aa1361a0d4d8dd49efe2e2128dcc224ac2d0d1a9
Tanvir Aumi <mdaumi@microsoft.com>
2017-05-04 10:50:25 -0700

    manually checkpointing to kafka after ensuring that latest sequence number is stored in mongo.

--- routerlicious/src/deli/index.ts
+++ routerlicious/src/deli/index.ts
@@ -12,6 +12,7 @@ const zookeeperEndpoint = nconf.get("zookeeper:endpoint");
 const kafkaClientId = nconf.get("deli:kafkaClientId");
 const receiveTopic = nconf.get("deli:topics:receive");
 const sendTopic = nconf.get("deli:topics:send");
+const CheckpointBatchSize = nconf.get("deli:checkpointBatchSize");
 
 // Connection to stored document details
 const client = MongoClient.connect(mongoUrl);
@@ -44,6 +45,7 @@ producerReady.then(
     () => {
         const groupId = nconf.get("deli:groupId");
         const consumerGroup = new kafka.ConsumerGroup({
+                autoCommit: false,
                 fromOffset: "earliest",
                 groupId,
                 host: zookeeperEndpoint,
@@ -51,20 +53,32 @@ producerReady.then(
                 protocol: ["roundrobin"],
             },
             [receiveTopic]);
-
+        const consumerOffset = new kafka.Offset(kafkaClient);
         consumerGroup.on("message", async (message: any) => {
             const value = JSON.parse(message.value);
             const objectId = value.objectId;
 
             // Go grab the takeANumber machine for the objectId and mark it as dirty
-            const collection = await objectsCollectionP;
             if (!(objectId in dispensers)) {
+                const collection = await objectsCollectionP;
                 dispensers[objectId] = new TakeANumber(objectId, collection, producer, sendTopic);
             }
             const dispenser = dispensers[objectId];
 
             await dispenser.ticket(message);
-            await dispenser.checkpoint();
+            // Periodically checkpoints to mongo and checkpoints offset back to kafka.
+            if (message.offset % CheckpointBatchSize === 0) {
+                await dispenser.checkpoint();
+                consumerOffset.commit(groupId,
+                    [{ topic: message.topic, partition: message.partition, offset: message.offset }],
+                    (error, data) => {
+                            if (error) {
+                                console.error(`Error checkpointing kafka offset: ${error}`);
+                            } else {
+                                console.log(`Success checkpointing kafka offset: ${message.offset}`);
+                            }
+                    });
+            }
         });
     },
     (error) => {
