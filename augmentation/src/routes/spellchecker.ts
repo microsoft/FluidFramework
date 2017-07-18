cc4bd2db6e49e036147b3769298b9ac16a6aac8b
Tanvir Aumi <mdaumi@microsoft.com>
2017-07-17 22:39:55 -0700

    Updated to node 8 and moved whole service to one promise.

--- augmentation/src/routes/spellchecker.ts
+++ augmentation/src/routes/spellchecker.ts
@@ -5,23 +5,35 @@ import * as service from "../service";
 const router: Router = Router();
 
 /**
+ * Writes to input file and invokes spellchecker service.
+ */
+async function processReuqest(text: string): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+        service.writeFile("../../../../app/ParameterCollection.json", text).then(() => {
+            service.runCommand("../../../../app", "dotnet editorservicerelay.dll").then((data) => {
+                    // Converts the console output to JSON.
+                    resolve(helper.extractJSON(data));
+            }, (error) => {
+                reject(error)
+            });
+        }, (error) => {
+            reject(error);
+        });
+    });
+}
+
+/**
  * Returns spellchecker result for the given string.
  */
 router.post("/", async (request, response, next) => {
     const text = request.body.documents[0].text;
     console.info(`Text to check spelling: ${text}`);
-    
-    // Writes the text to the file that service reads as an input.
-    await service.writeFile("../../../../app/ParameterCollection.json", text).catch((error) => {
+    const resultP = processReuqest(text);
+    resultP.then((result) => {
+        response.status(200).json(result);
+    }, (error) => {
         response.status(400).json({ error });
     });
-    // Invokes the service.
-    const result = await service.runCommand("../../../../app", "dotnet editorservicerelay.dll").catch((error) => {
-        response.status(400).json({ error });
-    });
-    // Converts the console output to JSON.
-    const resultBody = helper.extractJSON(result);
-    response.status(200).json(resultBody);
 });
 
 export default router;
