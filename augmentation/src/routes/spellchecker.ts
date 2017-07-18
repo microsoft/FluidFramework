f80eed289d746553d3daaad4f9bb64f58c67fe16
Tanvir Aumi <mdaumi@microsoft.com>
2017-07-17 17:20:20 -0700

    Clean up an refactoring.

--- augmentation/src/routes/spellchecker.ts
+++ augmentation/src/routes/spellchecker.ts
@@ -1,41 +1,27 @@
 import { Router } from "express";
-import * as service from "../service"
+import * as helper from "../helper"
+import * as service from "../service";
 
 const router: Router = Router();
 
 /**
- * Retrieves deltas for the given document. With an optional from and to range (both exclusive) specified
+ * Returns spellchecker result for the given string.
  */
 router.post("/", async (request, response, next) => {
     const text = request.body.documents[0].text;
-    console.log(`Text to check: ${text}`);
-    await service.writeFile("../../../../app/ParameterCollection.json", text);
-    const result = await service.runCommand("../../../../app", "dotnet editorservicerelay.dll");
-    const resultBody = extractJSON(result);
+    console.info(`Text to check: ${text}`);
+    
+    // Writes the text to the file that service reads as an input.
+    await service.writeFile("../../../../app/ParameterCollection.json", text).catch((error) => {
+        response.status(400).json({ error });
+    });
+    // Invokes the service.
+    const result = await service.runCommand("../../../../app", "dotnet editorservicerelay.dll").catch((error) => {
+        response.status(400).json({ error });
+    });
+    // Converts the console output to JSON.
+    const resultBody = helper.extractJSON(result);
     response.status(200).json(resultBody);
 });
-
-function extractJSON(str) {
-    var firstOpen, firstClose, candidate;
-    firstOpen = str.indexOf('{', firstOpen + 1);
-    do {
-        firstClose = str.lastIndexOf('}');
-        if(firstClose <= firstOpen) {
-            return null;
-        }
-        do {
-            candidate = str.substring(firstOpen, firstClose + 1);
-            try {
-                var res = JSON.parse(candidate);
-                return res;
-            }
-            catch(e) {
-                console.log(`Failed parsing response JSON.`);
-            }
-            firstClose = str.substr(0, firstClose).lastIndexOf('}');
-        } while(firstClose > firstOpen);
-        firstOpen = str.indexOf('{', firstOpen + 1);
-    } while(firstOpen != -1);
-}
 
 export default router;
--- augmentation/src/service.ts
+++ augmentation/src/service.ts
@@ -1,8 +1,10 @@
 import { exec } from "child_process";
 import * as fs from "fs";
+import * as helper from "./helper";
 
-console.log("HELLO OUR TEST APP");
-
+/**
+ * Given a directory path and command, invokes the command into a shell.
+ */
 export async function runCommand(path: string, cmd: string): Promise<any> {
     return new Promise<any>((resolve, reject) => {
         exec(cmd, {cwd: path}, (err, stdout, stderr) => {
@@ -14,6 +16,9 @@ export async function runCommand(path: string, cmd: string): Promise<any> {
     });
 }
 
+/**
+ * Given full file path, returns the content of the file.
+ */
 export async function readFile(path: string): Promise<any> {
     return new Promise<any>((resolve, reject) => {
         fs.readFile(path, "utf8", (error, data) => {
@@ -26,8 +31,11 @@ export async function readFile(path: string): Promise<any> {
     });
 }
 
+/**
+ * Given full file path and content, writes to the file.
+ */
 export async function writeFile(path: string, text: string): Promise<any> {
-    const textToWrite = JSON.stringify(constructJson(text));
+    const textToWrite = JSON.stringify(helper.constructSpellcheckerInput(text));
     return new Promise<any>((resolve, reject) => {
         fs.writeFile(path, textToWrite, (error, data) => {
             if (error) {
@@ -38,72 +46,3 @@ export async function writeFile(path: string, text: string): Promise<any> {
         });
     });
 }
-
-function constructJson(text: string) {
-    return {
-        Parameters: [
-            {
-                Name: "AppId",
-                Present: "true",
-                Value: "TestApp",
-            },
-            {
-                Name: "AppVersion",
-                Present: "false",
-                Value: "1.0.0.0",
-            },
-            {
-                Name: "RequestId",
-                Present: "true",
-                Value: "{B025D6F9-1C19-4207-A830-264A8CBC8BB1}",
-            },
-            {
-                Name: "Text",
-                Present: "true",
-                Value: text,
-            },
-            {
-                Name: "Start",
-                Present: "false",
-                Value: "0",
-            },
-            {
-                Name: "Length",
-                Present: "false",
-                Value: text.length,
-            },
-            {
-                Name: "LanguageId",
-                Present: "true",
-                Value: "en-us",
-            },
-            {
-                Name: "LanguageUxId",
-                Present: "false",
-                Value: "en-us",
-            },
-            {
-                Name: "RunOnProfileId",
-                Present: "true",
-                Value: "{24BCFF65-03B5-40E9-90C8-59B75ABD453C}",
-            },
-            {
-                Name: "RunOnProfileGenerationId",
-                Present: "false",
-                Value: "0",
-            },
-        ],
-    };
-}
-
-/*
-setInterval(async () => {
-    fs.readFile("../../../../app/ParameterCollection.json", "utf8", (error, data) => {
-        if (error) {
-            console.log(`Error reading file: ${error}`);
-        }
-    });
-    await runCommand("../../../../app", "dotnet editorservicerelay.dll");
-
-    console.log(`alive`);
-}, 10000);*/
