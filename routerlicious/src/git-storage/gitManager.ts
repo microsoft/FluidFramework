c2a0d5c50823b5a1a79d5ce0d40c342aedbf65af
Kurt Berglund <kurtb@microsoft.com>
2017-07-20 23:12:51 -0700

    Update to include commit creation

--- routerlicious/src/git-storage/gitManager.ts
+++ routerlicious/src/git-storage/gitManager.ts
@@ -69,74 +69,121 @@ export class GitManager {
      * Writes to the object with the given ID
      */
     public async write(branch: string, files: IGitFile[], message: string): Promise<void> {
-        // Rip through all the files and create blobs for them and retrieve the hashes
-        // function createBlob(supertest: request.SuperTest<request.Test>, repoName: string, blob: ICreateBlobParams) {
-        //     return supertest
-        //         .post(`/repos/${repoName}/git/blobs`)
-        //         .set("Accept", "application/json")
-        //         .set("Content-Type", "application/json")
-        //         .send(blob)
-        //         .expect(201);
-        // }
-        // const testBlob: ICreateBlobParams = {
-        //     content: "Hello, World!",
-        //     encoding: "utf-8",
-        // };
+        // Iterate through all the files and create blobs for them and retrieve the hashes
+        const blobs = await Promise.all(files.map((file) => {
+            return new Promise<string>((resolve, reject) => {
+                request.post(
+                    {
+                        body: {
+                            content: file.data,
+                            encoding: "utf-8",
+                        },
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        json: true,
+                        url: `${this.apiBaseUrl}/repos/${this.repository}/git/blobs`,
+                    },
+                    (error, response, body) => {
+                        if (error) {
+                            return reject(error);
+                        } else if (response.statusCode !== 201) {
+                            return reject(response.statusCode);
+                        } else {
+                            return resolve(response.body.sha);
+                        }
+                    });
+            });
+        }));
 
         // Construct a new tree from the collection of hashes
-        // function createTree(supertest: request.SuperTest<request.Test>, repoName: string, tree: ICreateTreeParams) {
-        //     return supertest
-        //         .post(`/repos/${repoName}/git/trees`)
-        //         .set("Accept", "application/json")
-        //         .set("Content-Type", "application/json")
-        //         .send(tree)
-        //         .expect(201);
-        // }
-        // const testTree: ICreateTreeParams = {
-        //     tree: [
-        //         {
-        //             mode: "100644",
-        //             path: "file.txt",
-        //             sha: "b45ef6fec89518d314f546fd6c3025367b721684",
-        //             type: "blob",
-        //         }],
-        // };
+        const tree: any[] = [];
+        for (var i = 0; i < blobs.length; i++) {
+            tree.push({
+                mode: "100644",
+                path: files[i].path,
+                sha: blobs[i],
+                type: "blob",
+            });
+        }
+        const treeSha = await new Promise<string>((resolve, reject) => {
+            request.post(
+                {
+                    body: tree,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    json: true,
+                    url: `${this.apiBaseUrl}/repos/${this.repository}/git/trees`,
+                },
+                (error, response, body) => {
+                    if (error) {
+                        return reject(error);
+                    } else if (response.statusCode !== 201) {
+                        return reject(response.statusCode);
+                    } else {
+                        return resolve(response.body.sha);
+                    }
+                });
+        });
 
         // Construct a commit for the tree
-        // function createCommit(supertest: request.SuperTest<request.Test>,
-        //         repoName: string, commit: ICreateCommitParams) {
-        //     return supertest
-        //         .post(`/repos/${repoName}/git/commits`)
-        //         .set("Accept", "application/json")
-        //         .set("Content-Type", "application/json")
-        //         .send(commit)
-        //         .expect(201);
-        // }
-        // const testCommit: ICreateCommitParams = {
-        //     author: {
-        //         date: "Thu Jul 13 2017 20:17:40 GMT-0700 (PDT)",
-        //         email: "kurtb@microsoft.com",
-        //         name: "Kurt Berglund",
-        //     },
-        //     message: "first commit",
-        //     parents: [],
-        //     tree: "bf4db183cbd07f48546a5dde098b4510745d79a1",
-        // };
+        const commit = {
+            author: {
+                date: Date.now(),
+                email: "kurtb@microsoft.com",
+                name: "Kurt Berglund",
+            },
+            message: "Commit @{TODO seq #}",
+            parents: [],
+            tree: treeSha,
+        };
+        const commitSha = await new Promise<string>((resolve, reject) => {
+            request.post(
+                {
+                    body: commit,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    json: true,
+                    url: `${this.apiBaseUrl}/repos/${this.repository}/git/commits`,
+                },
+                (error, response, body) => {
+                    if (error) {
+                        return reject(error);
+                    } else if (response.statusCode !== 201) {
+                        return reject(response.statusCode);
+                    } else {
+                        return resolve(response.body.sha);
+                    }
+                });
+        });
 
         // Update (force) the ref to the new commit
-        // function createRef(supertest: request.SuperTest<request.Test>, repoName: string, ref: ICreateRefParams) {
-        //     return supertest
-        //         .post(`/repos/${repoName}/git/refs`)
-        //         .set("Accept", "application/json")
-        //         .set("Content-Type", "application/json")
-        //         .send(ref)
-        //         .expect(201);
-        // }
-        // const testRef: ICreateRefParams = {
-        //     ref: "refs/heads/master",
-        //     sha: "cf0b592907d683143b28edd64d274ca70f68998e",
-        // };
-        // Write all the objects - create a commit referencing them - update the branch header
+        const ref = {
+            force: true,
+            sha: commitSha,
+        };
+        await new Promise<string>((resolve, reject) => {
+            request.patch(
+                {
+                    body: ref,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    json: true,
+                    url: `${this.apiBaseUrl}/repos/${this.repository}/git/refs/heads/${branch}`,
+                },
+                (error, response, body) => {
+                    if (error) {
+                        return reject(error);
+                    } else if (response.statusCode !== 201) {
+                        return reject(response.statusCode);
+                    } else {
+                        return resolve(response.body.sha);
+                    }
+                });
+        });
     }
 }
 
@@ -157,6 +204,7 @@ function repositoryExists(apiBaseUrl: string, repository: string) {
 function createRepository(apiBaseUrl: string, repository: string): Promise<void> {
     return new Promise<void>((resolve, reject) => {
         request.post({
+                body: { repository },
                 headers: {
                     "Content-Type": "application/json",
                 },
