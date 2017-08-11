aec8662c4a3f755c8411df0d4f0dd8147d7d71ee
Kurt Berglund <kurtb@microsoft.com>
2017-08-10 18:17:52 -0700

    Switch delta connection to use RangeTracker from shared

--- routerlicious/src/api/deltaConnection.ts
+++ routerlicious/src/api/deltaConnection.ts
@@ -1,88 +1,7 @@
-import * as assert from "assert";
 import { EventEmitter } from "events";
+import { RangeTracker } from "../shared";
 import { Document, IDeltaConnection } from "./document";
 import { IObjectMessage, ISequencedObjectMessage } from "./protocol";
-
-interface IRange {
-    primary: number;
-    secondary: number;
-    length: number;
-}
-
-/**
- * Helper class that keeps track of the mapping between sequence number ranges.
- * The secondary range is assumed to increase monotonically between calls.
- *
- * I should have this thing hand out tickets as well
- */
-class RangeTracker {
-    private ranges: IRange[] = [];
-
-    constructor(primary: number, private secondary: number) {
-        this.ranges.push({
-            length: 1,
-            primary,
-            secondary,
-        });
-    }
-
-    public updateBase(primary: number) {
-        assert(primary > this.ranges[0].primary);
-
-        // Walk the ranges looking for the first one that is greater than the primary. Primary is then within the
-        // previous index by definition (since it's less than the current index's primary but greather than the
-        // previous index's primary) and we know primary must be greater than the base.
-        let index = 1;
-        for (; index < this.ranges.length; index++) {
-            if (primary < this.ranges[index].primary) {
-                break;
-            }
-        }
-        assert(primary >= this.ranges[index - 1].primary);
-
-        // Update the last range
-        const range = this.ranges[index - 1];
-        range.length = Math.min(primary - range.primary, 1);
-        range.primary = primary;
-
-        // And remove unnecessary ranges
-        this.ranges = this.ranges.slice(index - 1);
-    }
-
-    public ticket(primary: number): number {
-        // Pre-increment secondary to get the next sequence number
-        const secondary = ++this.secondary;
-        const tail = this.ranges[this.ranges.length - 1];
-
-        // See if we can simply extend the length of the tail
-        if (tail.primary + tail.length === primary) {
-            tail.length++;
-        } else {
-            this.ranges.push({ length: 1, primary, secondary });
-        }
-
-        return secondary;
-    }
-
-    public getClosest(primary: number) {
-        assert(primary >= this.ranges[0].primary);
-
-        // Find the first range where the starting position is greater than the primary. Our target range is
-        // the one before it.
-        let index = 1;
-        for (; index < this.ranges.length; index++) {
-            if (primary < this.ranges[index].primary) {
-                break;
-            }
-        }
-        assert(primary >= this.ranges[index - 1].primary);
-
-        // If the difference is within the stored range use it - otherwise add in the length - 1 as the highest
-        // stored secondary value to use.
-        const closestRange = this.ranges[index - 1];
-        return Math.min(primary - closestRange.primary, closestRange.length - 1) + closestRange.secondary;
-    }
-}
 
 export class DeltaConnection implements IDeltaConnection {
     protected events = new EventEmitter();
@@ -98,7 +17,7 @@ export class DeltaConnection implements IDeltaConnection {
     constructor(
         public objectId: string,
         private document: Document,
-        sequenceNumber,
+        private sequenceNumber,
         documentSequenceNumber: number) {
 
         this.minSequenceNumber = sequenceNumber;
@@ -116,8 +35,10 @@ export class DeltaConnection implements IDeltaConnection {
         documentMinimumSequenceNumber: number,
         documentSequenceNumber: number) {
 
-        this.minSequenceNumber = this.map.getClosest(documentMinimumSequenceNumber);
-        const sequenceNumber = this.map.ticket(documentSequenceNumber);
+        const sequenceNumber = ++this.sequenceNumber;
+        this.map.add(documentSequenceNumber, sequenceNumber);
+        this.minSequenceNumber = this.map.get(documentMinimumSequenceNumber);
+
         const sequencedObjectMessage: ISequencedObjectMessage = {
             clientId,
             clientSequenceNumber: message.clientSequenceNumber,
@@ -132,7 +53,7 @@ export class DeltaConnection implements IDeltaConnection {
     }
 
     public updateMinSequenceNumber(value: number) {
-        const newMinSequenceNumber = this.map.getClosest(value);
+        const newMinSequenceNumber = this.map.get(value);
 
         // Notify clients when then number changed
         if (newMinSequenceNumber !== this.minimumSequenceNumber) {
