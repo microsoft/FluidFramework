e43cf4ec0c93627cc566a606c5b780f824ab47ec
Steven Lucco <steveluc@microsoft.com>
2017-09-19 15:00:50 -0700

    hierarchy test

--- routerlicious/src/test/merge-tree/beastTest.ts
+++ routerlicious/src/test/merge-tree/beastTest.ts
@@ -1404,6 +1404,265 @@ function tst() {
     console.log(p4);
 }
 
+export class RandomPack {
+    mt: Random.MT19937;
+    constructor() {
+        this.mt = random.engines.mt19937();
+        this.mt.seedWithArray([0xdeadbeef, 0xfeedbed]);
+    }
+
+    randInteger(min: number, max: number) {
+        return random.integer(min, max)(this.mt);
+    }
+
+    randString(wordCount: number) {
+        let exampleWords = ["giraffe", "hut", "aardvark", "gold", "hover",
+            "yurt", "hot", "antelope", "gift", "banana", "book", "airplane",
+            "kitten", "moniker", "lemma", "doughnut", "orange", "tangerine"
+        ];
+        let buf = "";
+        for (let i = 0; i < wordCount; i++) {
+            let exampleWord = exampleWords[this.randInteger(0, exampleWords.length - 1)];
+            if (i > 0) {
+                buf += ' ';
+            }
+            buf += exampleWord;
+        }
+        return buf;
+    }
+}
+
+function docNodeToString(docNode: DocumentNode) {
+    if (typeof docNode === "string") {
+        return docNode;
+    } else {
+        return docNode.name;
+    }
+}
+
+export type DocumentNode = string | DocumentTree;
+/**
+ * Generate and model documents from the following tree grammar:
+ * Row -> row[Box*];
+ * Box -> box[Content]; 
+ * Content -> (Row|Paragraph)*; 
+ * Paragraph -> pgtile text; 
+ * Document-> Content
+ */
+export class DocumentTree {
+    pos = 0;
+    ids = { box: 0, row: 0 };
+    id: string;
+    static randPack = new RandomPack();
+
+    constructor(public name: string, public children: DocumentNode[]) {
+    }
+
+    addToMergeTree(client: MergeTree.Client, docNode: DocumentNode) {
+        if (typeof docNode === "string") {
+            let text = <string>docNode;
+            client.insertTextLocal(text, this.pos);
+            this.pos += text.length;
+        } else {
+            let id: number;
+            if (docNode.name === "pg") {
+                client.insertMarkerLocal(this.pos, ops.MarkerBehaviors.Tile,
+                    {
+                        [MergeTree.reservedMarkerLabelsKey]: [docNode.name],
+                    },
+                );
+                this.pos++;
+            } else {
+                let trid = docNode.name + this.ids[docNode.name].toString();
+                docNode.id = trid;
+                id = this.ids[docNode.name]++;
+                client.insertMarkerLocal(this.pos, ops.MarkerBehaviors.RangeBegin,
+                    {
+                        [MergeTree.reservedMarkerIdKey]: trid,
+                        [MergeTree.reservedMarkerLabelsKey]: [docNode.name],
+                    },
+                );
+                this.pos++;
+            }
+            for (let child of docNode.children) {
+                this.addToMergeTree(client, child);
+            }
+            if (docNode.name !== "pg") {
+                let etrid = "end-" + docNode.name + id.toString();
+                client.insertMarkerLocal(this.pos, ops.MarkerBehaviors.RangeEnd,
+                    {
+                        [MergeTree.reservedMarkerIdKey]: etrid,
+                        [MergeTree.reservedMarkerLabelsKey]: [docNode.name],
+                    },
+                );
+                this.pos++;
+            }
+        }
+    }
+
+    checkStacksAllPositions(client: MergeTree.Client) {
+        let errorCount = 0;
+        let pos = 0;
+        let verbose = false;
+        let stacks = {
+            box: new Collections.Stack<string>(),
+            row: new Collections.Stack<string>()
+        };
+
+        function printStack(stack: Collections.Stack<string>) {
+            for (let item in stack.items) {
+                console.log(item);
+            }
+        }
+
+        function printStacks() {
+            for (let name of ["box", "row"]) {
+                console.log(name + ":");
+                printStack(stacks[name]);
+            }
+        }
+
+        function checkTreeStackEmpty(treeStack: Collections.Stack<string>) {
+            if (!treeStack.empty()) {
+                if (verbose) {
+                    console.log("mismatch: client stack empty; tree stack not");
+                }
+                errorCount++;
+            }
+        }
+
+        let checkNodeStacks = (docNode: DocumentNode) => {
+            if (typeof docNode === "string") {
+                let text = <string>docNode;
+                let epos = pos + text.length;
+                if (verbose) {
+                    console.log(`stacks for [${pos}, ${epos}): ${text}`);
+                    printStacks();
+                }
+                let cliStacks = client.mergeTree.getStackContext(pos,
+                    client.getClientId(), ["box", "row"]);
+                for (let name of ["box", "row"]) {
+                    let cliStack = cliStacks[name];
+                    let treeStack = <Collections.Stack<string>>stacks[name];
+                    if (cliStack) {
+                        let len = cliStack.items.length;
+                        if (len > 0) {
+                            if (len !== treeStack.items.length) {
+                                if (verbose) {
+                                    console.log(`stack length mismatch cli ${len} tree ${treeStack.items.length}`);
+                                }
+                                errorCount++;
+                            }
+                            for (let i = 0; i < len; i++) {
+                                let cliMarkerId = cliStack.items[i].getId();
+                                let treeMarkerId = treeStack.items[i];
+                                if (cliMarkerId !== treeMarkerId) {
+                                    if (verbose) {
+                                        console.log(`mismatch index ${i}: ${cliMarkerId} !== ${treeMarkerId} pos ${pos} text ${text}`);
+                                        printStack(treeStack);
+                                        console.log(client.mergeTree.toString());
+                                    }
+                                    errorCount++;
+                                }
+                            }
+                        } else {
+                            checkTreeStackEmpty(treeStack);
+                        }
+                    } else {
+                        checkTreeStackEmpty(treeStack);
+                    }
+                }
+                pos = epos;
+            } else {
+                pos++;
+                if (docNode.name === "pg") {
+                    checkNodeStacks(docNode.children[0]);
+                } else {
+                    stacks[docNode.name].push(docNode.id);
+                    for (let child of docNode.children) {
+                        checkNodeStacks(child);
+                    }
+                    stacks[docNode.name].pop();
+                    pos++;
+                }
+            }
+        }
+
+        for (let rootChild of this.children) {
+            if (verbose) {
+                console.log(`next child ${pos} with name ${docNodeToString(rootChild)}`);
+            }
+            // printStacks();
+            checkNodeStacks(rootChild);
+        }
+        return errorCount;
+    }
+
+    private generateClient() {
+        let client = new MergeTree.Client("", { blockUpdateMarkers: true });
+        client.startCollaboration("Fred");
+        for (let child of this.children) {
+            this.addToMergeTree(client, child);
+        }
+        return client;
+    }
+
+    static test1() {
+        let doc = DocumentTree.generateDocument();
+        let client = doc.generateClient();
+        return doc.checkStacksAllPositions(client);
+    }
+
+    static generateDocument() {
+        let tree = new DocumentTree("Document", DocumentTree.generateContent(0.6));
+        return tree;
+    }
+
+    static generateContent(rowProbability: number) {
+        let items = <DocumentNode[]>[];
+        let docLen = DocumentTree.randPack.randInteger(7, 25);
+        for (let i = 0; i < docLen; i++) {
+            let rowThreshold = rowProbability * 1000;
+            let selector = DocumentTree.randPack.randInteger(1, 1000);
+            if (selector >= rowThreshold) {
+                let pg = DocumentTree.generateParagraph();
+                items.push(pg);
+            } else {
+                rowProbability /= 2;
+                if (rowProbability < 0.08) {
+                    rowProbability = 0;
+                }
+                let row = DocumentTree.generateRow(rowProbability);
+                items.push(row);
+            }
+
+        }
+        return items;
+    }
+
+    // model pg tile as tree with single child
+    static generateParagraph() {
+        let wordCount = DocumentTree.randPack.randInteger(1, 6);
+        let text = DocumentTree.randPack.randString(wordCount);
+        let pgTree = new DocumentTree("pg", [text]);
+        return pgTree;
+    }
+
+    static generateRow(rowProbability: number) {
+        let items = <DocumentNode[]>[];
+        let rowLen = DocumentTree.randPack.randInteger(1, 5);
+        for (let i = 0; i < rowLen; i++) {
+            let item = DocumentTree.generateBox(rowProbability);
+            items.push(item);
+        }
+        return new DocumentTree("row", items);
+    }
+
+    static generateBox(rowProbability: number) {
+        return new DocumentTree("box", DocumentTree.generateContent(rowProbability));
+    }
+}
+
 let testTST = false;
 if (testTST) {
     tst();
@@ -1415,6 +1674,10 @@ describe("Routerlicious", () => {
             const testPack = TestPack(false);
             testPack.firstTest();
         });
+
+        it("hierarchy", () => {
+            assert(DocumentTree.test1() == 0);
+        }).timeout(Number.MAX_VALUE);
 
         it("randolicious", () => {
             const testPack = TestPack(false);
