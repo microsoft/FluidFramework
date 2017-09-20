5770ae7dda92271eb3f55c7f6a32ff7d309a004b
kurtb <kurtberglund@hotmail.com>
2017-09-20 16:16:08 -0700

    Move Scriptorium to runner model (#246)
    * Fix Deli runner bug
    
    * Migrate scriptorium to runner pattern
    
    * Rev historian/gitrest versions

--- routerlicious/src/scriptorium/index.ts
+++ routerlicious/src/scriptorium/index.ts
@@ -1,43 +1,25 @@
-// Setup the configuration system - pull arguments, then environment variables - prior to loading other modules that
-// may depend on the config already being initialized
-import { queue } from "async";
 import * as nconf from "nconf";
 import * as path from "path";
 import * as redis from "redis";
 import * as socketIoEmitter from "socket.io-emitter";
 import * as util from "util";
 import * as winston from "winston";
-import * as core from "../core";
 import * as services from "../services";
-import * as shared from "../shared";
 import * as utils from "../utils";
+import { ScriptoriumRunner } from "./runner";
 
 const provider = nconf.argv().env(<any> "__").file(path.join(__dirname, "../../config/config.json")).use("memory");
 
 // Initialize Socket.io and connect to the Redis adapter
-let redisConfig = nconf.get("redis");
-const kafkaEndpoint = nconf.get("kafka:lib:endpoint");
-const kafkaLibrary = nconf.get("kafka:lib:name");
-const topic = nconf.get("scriptorium:topic");
-const groupId = nconf.get("scriptorium:groupId");
-const checkpointBatchSize = nconf.get("scriptorium:checkpointBatchSize");
-const checkpointTimeIntervalMsec = nconf.get("scriptorium:checkpointTimeIntervalMsec");
-const mongoUrl = nconf.get("mongo:endpoint") as string;
-const deltasCollectionName = nconf.get("mongo:collectionNames:deltas");
-
-let checkpointTimer: any;
-
-async function checkpoint(partitionManager: core.PartitionManager) {
-    partitionManager.checkPoint();
-    // Clear timer since we just checkpointed.
-    if (checkpointTimer) {
-        clearTimeout(checkpointTimer);
-    }
-    // Set up next cycle.
-    checkpointTimer = setTimeout(() => {
-        checkpoint(partitionManager);
-    }, checkpointTimeIntervalMsec);
-}
+let redisConfig = provider.get("redis");
+const kafkaEndpoint = provider.get("kafka:lib:endpoint");
+const kafkaLibrary = provider.get("kafka:lib:name");
+const topic = provider.get("scriptorium:topic");
+const groupId = provider.get("scriptorium:groupId");
+const checkpointBatchSize = provider.get("scriptorium:checkpointBatchSize");
+const checkpointTimeIntervalMsec = provider.get("scriptorium:checkpointTimeIntervalMsec");
+const mongoUrl = provider.get("mongo:endpoint") as string;
+const deltasCollectionName = provider.get("mongo:collectionNames:deltas");
 
 /**
  * Default logger setup
@@ -58,13 +40,8 @@ winston.configure({
 });
 
 async function run() {
-    const deferred = new shared.Deferred<void>();
-
     const redisClient = redis.createClient(redisConfig.port, redisConfig.host);
     let io = socketIoEmitter(redisClient);
-    io.redis.on("error", (error) => {
-        deferred.reject(error);
-    });
 
     const mongoFactory = new services.MongoDbFactory(mongoUrl);
     const mongoManager = new utils.MongoManager(mongoFactory, false);
@@ -77,105 +54,42 @@ async function run() {
         true);
 
     let consumer = utils.kafkaConsumer.create(kafkaLibrary, kafkaEndpoint, groupId, topic, false);
-    const partitionManager = new core.PartitionManager(
+
+    const runner = new ScriptoriumRunner(
+        consumer,
+        collection,
+        io,
         groupId,
         topic,
-        consumer,
         checkpointBatchSize,
-        checkpointTimeIntervalMsec,
-    );
-
-    consumer.on("data", (message) => {
-        q.push(message);
-    });
-
-    consumer.on("error", (err) => {
-        consumer.close();
-        deferred.reject(err);
-    });
-
-    const throughput = new utils.ThroughputCounter(winston.info);
-    // Mongo inserts don't order promises with respect to each other. To work around this we track the last
-    // Mongo insert we've made for each document. And then perform a then on this to maintain causal ordering
-    // for any dependent operations (i.e. socket.io writes)
-    const lastMongoInsertP: { [documentId: string]: Promise<any> } = {};
-
-    const ioBatchManager = new utils.BatchManager<core.ISequencedOperationMessage>((documentId, work) => {
-        // Initialize the last promise if it doesn't exist
-        if (!(documentId in lastMongoInsertP)) {
-            lastMongoInsertP[documentId] = Promise.resolve();
-        }
-
-        winston.verbose(`Inserting to mongodb ${documentId}@${work[0].operation.sequenceNumber}:${work.length}`);
-        const insertP = collection.insertMany(work, false)
-            .catch((error) => {
-                // Ignore duplicate key errors since a replay may cause us to attempt to insert a second time
-                if (error.name !== "MongoError" || error.code !== 11000) {
-                    return Promise.reject(error);
-                }
-            });
-        lastMongoInsertP[documentId] = lastMongoInsertP[documentId].then(() => insertP);
-
-        lastMongoInsertP[documentId].then(
-            () => {
-                // Route the message to clients
-                // tslint:disable-next-line:max-line-length
-                winston.verbose(`Routing message to clients ${documentId}@${work[0].operation.sequenceNumber}:${work.length}`);
-                io.to(documentId).emit("op", documentId, work.map((value) => value.operation));
-                throughput.acknolwedge(work.length);
-            },
-            (error) => {
-                deferred.reject(error);
-            });
-    });
-
-    const q = queue((message: any, callback) => {
-        // NOTE the processing of the below messages must make sure to notify clients of the messages in increasing
-        // order. Be aware of promise handling ordering possibly causing out of order messages to be delivered.
-
-        throughput.produce();
-        const baseMessage = JSON.parse(message.value.toString("utf8")) as core.IMessage;
-        if (baseMessage.type === core.SequencedOperationType) {
-            const value = baseMessage as core.ISequencedOperationMessage;
-
-            // Batch up work to more efficiently send to socket.io and mongodb
-            ioBatchManager.add(value.documentId, value);
-        }
-
-        // Update partition manager.
-        partitionManager.update(message.partition, message.offset);
-
-        // Checkpoint to kafka after completing all operations.
-        // We should experiment with 'CheckpointBatchSize' here.
-        if (message.offset % checkpointBatchSize === 0) {
-            // Finally call checkpointing.
-            checkpoint(partitionManager).catch((error) => {
-                winston.error(error);
-            });
-        }
-        callback();
-    }, 1);
-
+        checkpointTimeIntervalMsec);
     process.on("SIGTERM", () => {
+        runner.stop();
+    });
+
+    const runningP = runner.start();
+
+    // Clean up all resources when the runner finishes
+    const doneP = runningP.catch((error) => error);
+    const closedP = doneP.then(() => {
+        winston.info("Closing service connections");
         const consumerClosedP = consumer.close();
         const mongoClosedP = mongoManager.close();
         const redisP = util.promisify(((callback) => redisClient.quit(callback)) as Function)();
-
-        Promise.all([consumerClosedP, mongoClosedP, redisP]).then(
-            () => {
-                deferred.resolve();
-            },
-            (error) => {
-                deferred.reject(error);
-            });
+        Promise.all([consumerClosedP, mongoClosedP, redisP]);
     });
 
-    return deferred.promise;
+    return Promise.all([runningP, closedP]);
 }
 
 // Start up the scriptorium service
+winston.info("Starting");
 const runP = run();
-runP.catch((error) => {
-    winston.error(error);
-    process.exit(1);
-});
+runP.then(
+    () => {
+        winston.info("Exiting");
+    },
+    (error) => {
+        winston.error(error);
+        process.exit(1);
+    });
