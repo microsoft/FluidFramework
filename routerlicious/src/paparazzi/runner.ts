5225cb04f8d625d965c7acc831fdd68cea7da731
kurtb <kurtberglund@hotmail.com>
2017-09-20 18:26:16 -0700

    Update TMZ and Paparazzi to use Runners (#247)
    * Move paparazzi to runner model
    
    * Update TMZ to runner model

---
+++ routerlicious/src/paparazzi/runner.ts
@@ -0,0 +1,35 @@
+import * as shared from "../shared";
+import * as utils from "../utils";
+
+// TODO can likely consolidate the runner and the worker service
+
+export class PaparazziRunner implements utils.IRunner {
+    private workerService: shared.WorkerService;
+    private running = new shared.Deferred<void>();
+
+    constructor(
+        alfredUrl: string,
+        tmzUrl: string,
+        workerConfig: string,
+        historianUrl: string,
+        repo: string) {
+
+        this.workerService = new shared.WorkerService(
+            alfredUrl,
+            tmzUrl,
+            historianUrl,
+            repo,
+            workerConfig);
+    }
+
+    public start(): Promise<void> {
+        const workerRunningP = this.workerService.connect("Paparazzi");
+        workerRunningP.then(() => this.running.resolve(), (error) => this.running.reject(error));
+
+        return this.running.promise;
+    }
+    public stop(): Promise<void> {
+        this.workerService.close().then(() => this.running.resolve(), (error) => this.running.reject(error));
+        return this.running.promise;
+    }
+}
