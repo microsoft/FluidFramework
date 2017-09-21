7e69b7e3c784b1d32910581b9a4faaedf1c3860b
Kurt Berglund <kurtberglund@hotmail.com>
2017-09-21 08:53:15 -0700

    Move all services to factory runner pattern

--- routerlicious/src/paparazzi/index.ts
+++ routerlicious/src/paparazzi/index.ts
@@ -1,39 +1,4 @@
-import * as nconf from "nconf";
-import * as path from "path";
-import * as winston from "winston";
 import * as utils from "../utils";
-import { PaparazziRunner } from "./runner";
+import { PaparazziResourcesFactory, PaparazziRunnerFactory } from "./runnerFactory";
 
-const provider = nconf.argv().env(<any> "__").file(path.join(__dirname, "../../config/config.json")).use("memory");
-
-// Configure logging
-utils.configureLogging(provider.get("logger"));
-
-async function run(config: nconf.Provider) {
-    // Connect to alfred and tmz and subscribes for work.
-    const alfredUrl = provider.get("paparazzi:alfred");
-    const tmzUrl = provider.get("paparazzi:tmz");
-    const workerConfig = provider.get("worker");
-    const gitConfig = provider.get("git");
-
-    const runner = new PaparazziRunner(alfredUrl, tmzUrl, workerConfig, gitConfig.historian, gitConfig.repository);
-    const runningP = runner.start();
-
-    process.on("SIGTERM", () => {
-        runner.stop();
-    });
-
-    return runningP;
-}
-
-// Start up the paparazzi service
-winston.info("Starting");
-const runP = run(provider);
-runP.then(
-    () => {
-        winston.info("Exiting");
-    },
-    (error) => {
-        winston.error(error);
-        process.exit(1);
-    });
+utils.runService(new PaparazziResourcesFactory(), new PaparazziRunnerFactory());
---
+++ routerlicious/src/scriptorium/runnerFactory.ts
@@ -0,0 +1,82 @@
+import { Provider } from "nconf";
+import * as redis from "redis";
+import * as socketIoEmitter from "socket.io-emitter";
+import * as util from "util";
+import * as core from "../core";
+import * as services from "../services";
+import * as utils from "../utils";
+import { ScriptoriumRunner } from "./runner";
+
+export class ScriptoriumResources implements utils.IResources {
+    constructor(
+        public consumer: utils.kafkaConsumer.IConsumer,
+        public collection: core.ICollection<any>,
+        public mongoManager: utils.MongoManager,
+        public redisClient: redis.RedisClient,
+        public io,
+        public groupId: string,
+        public topic: string,
+        public checkpointBatchSize: number,
+        public checkpointTimeIntervalMsec: number) {
+    }
+
+    public async dispose(): Promise<void> {
+        const consumerClosedP = this.consumer.close();
+        const mongoClosedP = this.mongoManager.close();
+        const redisP = util.promisify(((callback) => this.redisClient.quit(callback)) as Function)();
+        await Promise.all([consumerClosedP, mongoClosedP, redisP]);
+    }
+}
+
+export class ScriptoriumResourcesFactory implements utils.IResourcesFactory<ScriptoriumResources> {
+    public async create(config: Provider): Promise<ScriptoriumResources> {
+        let redisConfig = config.get("redis");
+        const kafkaEndpoint = config.get("kafka:lib:endpoint");
+        const kafkaLibrary = config.get("kafka:lib:name");
+        const topic = config.get("scriptorium:topic");
+        const groupId = config.get("scriptorium:groupId");
+        const checkpointBatchSize = config.get("scriptorium:checkpointBatchSize");
+        const checkpointTimeIntervalMsec = config.get("scriptorium:checkpointTimeIntervalMsec");
+        const mongoUrl = config.get("mongo:endpoint") as string;
+        const deltasCollectionName = config.get("mongo:collectionNames:deltas");
+
+        const redisClient = redis.createClient(redisConfig.port, redisConfig.host);
+        let io = socketIoEmitter(redisClient);
+
+        const mongoFactory = new services.MongoDbFactory(mongoUrl);
+        const mongoManager = new utils.MongoManager(mongoFactory, false);
+        const db = await mongoManager.getDatabase();
+        const collection = db.collection(deltasCollectionName);
+        await collection.createIndex({
+                "documentId": 1,
+                "operation.sequenceNumber": 1,
+            },
+            true);
+
+        let consumer = utils.kafkaConsumer.create(kafkaLibrary, kafkaEndpoint, groupId, topic, false);
+
+        return new ScriptoriumResources(
+            consumer,
+            collection,
+            mongoManager,
+            redisClient,
+            io,
+            groupId,
+            topic,
+            checkpointBatchSize,
+            checkpointTimeIntervalMsec);
+    }
+}
+
+export class ScriptoriumRunnerFactory implements utils.IRunnerFactory<ScriptoriumResources> {
+    public async create(resources: ScriptoriumResources): Promise<utils.IRunner> {
+        return new ScriptoriumRunner(
+            resources.consumer,
+            resources.collection,
+            resources.io,
+            resources.groupId,
+            resources.topic,
+            resources.checkpointBatchSize,
+            resources.checkpointTimeIntervalMsec);
+    }
+}
