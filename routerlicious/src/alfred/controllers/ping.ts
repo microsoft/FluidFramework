c2c47b36145b0ce5a57d382f17d660687d4b4c85
Tanvir Aumi <mdaumi@microsoft.com>
2017-10-26 21:32:40 -0700

    Added ping client.

---
+++ routerlicious/src/alfred/controllers/ping.ts
@@ -0,0 +1,45 @@
+import { core, socketIoClient as io } from "../../client-api";
+
+// new average = old average + (next data - old average) / next count
+let messageCount = 0;
+let averageLatency = 0.0;
+
+const pingInterval = 50;
+
+const socket = io(document.location.origin, { transports: ["websocket"] });
+
+export function load() {
+    setInterval(() => {
+        const pingMessage: core.IPingMessage = {
+            acked: false,
+            traces: [{
+                action: "start",
+                service: "ping",
+                timestamp: Date.now(),
+            }],
+        };
+        socket.emit(
+            "pingObject",
+            pingMessage,
+            (error, response: core.IPingMessage) => {
+                if (!error && response.traces.length === 1) {
+                    // Calculate and show running average latency.
+                    ++messageCount;
+                    const ts = Date.now();
+                    const pingLatency = ts - response.traces[0].timestamp;
+                    averageLatency += (pingLatency - averageLatency) / messageCount;
+                    document.getElementById("avg-latency").innerText =
+                    `Average ping latency: ${(averageLatency).toFixed(2)} ms`;
+
+                    // Push back to alfred for tracking purpose.
+                    response.acked = true;
+                    response.traces.push({
+                        action: "end",
+                        service: "ping",
+                        timestamp: ts,
+                    });
+                    socket.emit("pingObject", response);
+                }
+            });
+    }, pingInterval);
+}
