9c935c88fb4cdeba121e14ee224e5d60483992b5
kurtb <kurtberglund@hotmail.com>
2017-11-01 10:15:44 -0700

    Kafka producer tests (#290)
    * Kafka producer tests
    
    * Add in some TODOs for future perf work

---
+++ routerlicious/src/test/utils/kafkaProducer.spec.ts
@@ -0,0 +1,54 @@
+import * as assert from "assert";
+import { kafkaProducer } from "../../utils";
+
+class TestProducer extends kafkaProducer.Producer {
+    public sentSizes: number[] = [];
+
+    public close(): Promise<void> {
+        return Promise.resolve();
+    }
+
+    protected sendCore(key: string, messages: kafkaProducer.IPendingMessage[]) {
+        this.sentSizes.push(messages.length);
+    }
+
+    protected canSend(): boolean {
+        return true;
+    }
+}
+
+describe("Routerlicious", () => {
+    describe("Utils", () => {
+        const key = "document";
+        let producer: TestProducer;
+
+        beforeEach(() => {
+            producer = new TestProducer();
+        });
+
+        describe("Producer", () => {
+            describe(".send()", async () => {
+                it("", async () => {
+                    const sendCount = 100;
+                    for (let i = 0; i < sendCount; i++) {
+                        producer.send("Hello", key);
+                    }
+
+                    return new Promise((resolve, reject) => {
+                        setImmediate(() => {
+                            assert.equal(producer.sentSizes.length, 1);
+                            assert.equal(producer.sentSizes[0], sendCount);
+                            resolve();
+                        });
+                    });
+                });
+            });
+
+            describe(".close()", () => {
+                it("Should close the producer", async () => {
+                    await producer.close();
+                });
+            });
+        });
+    });
+});
