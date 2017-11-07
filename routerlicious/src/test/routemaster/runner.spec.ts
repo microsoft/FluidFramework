8831ff85925c2a2986e925194e5f9071affabba2
Kurt Berglund <kurtberglund@hotmail.com>
2017-11-07 11:36:10 -0800

    Simple routemaster test

---
+++ routerlicious/src/test/routemaster/runner.spec.ts
@@ -0,0 +1,69 @@
+import * as assert from "assert";
+import { RouteMasterRunner } from "../../routemaster/runner";
+import { MessageFactory, TestCollection, TestKafka } from "../testUtils";
+
+describe("Routerlicious", () => {
+    describe("Routemaster", () => {
+        describe("Runner", () => {
+            const testId = "test";
+            const testClientId = "quiet-rat";
+            const testGroupId = "testGroup";
+            const testReceiveTopic = "testReceiveTopic";
+            const testCheckpointBatchSize = 10;
+            const testCheckpointTimeIntervalMsec = 10000;
+
+            let testCollection: TestCollection;
+            let receiveTopic: TestKafka;
+            let sendTopic: TestKafka;
+            let kafkaOffset: number;
+            let runner: RouteMasterRunner;
+            let messageFactory: MessageFactory;
+
+            beforeEach(() => {
+                const testData = [{ _id: testId }];
+                testCollection = new TestCollection(testData);
+                receiveTopic = new TestKafka();
+                sendTopic = new TestKafka();
+                kafkaOffset = 0;
+
+                const producer = sendTopic.createProducer();
+                const consumer = receiveTopic.createConsumer();
+                runner = new RouteMasterRunner(
+                    producer,
+                    consumer,
+                    testCollection,
+                    testGroupId,
+                    testReceiveTopic,
+                    testCheckpointBatchSize,
+                    testCheckpointTimeIntervalMsec);
+
+                messageFactory = new MessageFactory(testId, testClientId);
+            });
+
+            describe("#start()", () => {
+                it("Should be able to stop after starting", async () => {
+                    const startP = runner.start();
+                    const stopP = runner.stop();
+                    await Promise.all([startP, stopP]);
+                });
+
+                it("Should process incoming messages after starting", async () => {
+                    const TestMessages = 100;
+
+                    const started = runner.start();
+                    const testProducer = receiveTopic.createProducer();
+
+                    for (let i = 0; i < TestMessages; i++) {
+                        const message = messageFactory.create();
+                        testProducer.send(JSON.stringify(message), testId);
+                    }
+                    await runner.stop();
+
+                    assert.ok(true);
+
+                    return started;
+                });
+            });
+        });
+    });
+});
