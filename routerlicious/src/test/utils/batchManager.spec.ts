cf5df82d47fffac7cd93be95bfffaa2ba48d9acc
Tanvir Aumi <mdaumi@microsoft.com>
2017-11-08 22:53:09 -0800

    Added batching of websocket messages.

--- routerlicious/src/test/utils/batchManager.spec.ts
+++ routerlicious/src/test/utils/batchManager.spec.ts
@@ -1,5 +1,5 @@
 import * as assert from "assert";
-import { BatchManager } from "../../utils";
+import { BatchManager } from "../../core-utils";
 
 describe("Routerlicious", () => {
     describe("Utils", () => {
