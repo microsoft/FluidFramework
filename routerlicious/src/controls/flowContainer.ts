b83874233187d41c3fc97bfea83f141bb180432e
Kurt Berglund <kurtb@microsoft.com>
2017-11-10 19:51:40 -0800

    Toggle off branching

--- routerlicious/src/controls/flowContainer.ts
+++ routerlicious/src/controls/flowContainer.ts
@@ -108,13 +108,13 @@ export class FlowContainer extends ui.Component {
             this.overlayCanvas.enableInk(value);
         });
 
-        this.status.addButton("branch", "branch");
-        this.status.on("branch", () => {
-            console.log("Time to branch the document!!!");
-            collabDocument.branch().then((branchId) => {
-                window.open(`/sharedText/${branchId}`, "_blank");
-            });
-        });
+        // this.status.addButton("branch", "branch");
+        // this.status.on("branch", () => {
+        //     console.log("Time to branch the document!!!");
+        //     collabDocument.branch().then((branchId) => {
+        //         window.open(`/sharedText/${branchId}`, "_blank");
+        //     });
+        // });
 
         // Add children to the panel once we have both
         this.layerPanel.addChild(this.flowView);
