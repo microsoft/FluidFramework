5ca093cd09df0dc9c92da63688304de2d245c12a
steveluc <steveluc@microsoft.com>
2017-11-13 09:20:40 -0800

    start of graph example

--- experiments/graphs/src/index.ts
+++ experiments/graphs/src/index.ts
@@ -27,6 +27,17 @@ function getLatestVersion(id: string): Promise<any> {
     return versionP;
 }
 
+let nextGraphId = 0;
+
+class GraphProxy {
+    id: string;
+    
+}
+
+function createGraphProxy() {
+
+}
+
 async function run(id: string): Promise<void> {
     // Get the latest version of the document
     const version = await getLatestVersion(id);
@@ -36,40 +47,12 @@ async function run(id: string): Promise<void> {
     const collabDoc = await prague.api.load(id, { blockUpdateMarkers: true }, version);
 
     const rootView = await collabDoc.getRoot().getView();
-    console.log("Keys");
-    console.log(rootView.keys());
-
-    // Add in the text string if it doesn't yet exist
-    if (!rootView.has("text")) {
-        rootView.set("text", collabDoc.createString());
+    if (!rootView.has("graphs")) {
+        rootView.set("graphs", this.doc.createMap());
     }
-
-    // Load the text string and listen for updates
-    const text = rootView.get("text");
-
-    const textElement = document.getElementById("text");
-    textElement.innerText = text.client.getText();
-
-    // Update the text after being loaded as well as when receiving ops
-    text.loaded.then(() => {
-        textElement.innerText = text.client.getText();
-    });
-    text.on("op", (msg) => {
-        textElement.innerText = text.client.getText();
-    });
-
-    const insertElement = document.getElementById("insertForm") as HTMLFormElement;
-    insertElement.onsubmit = (event) => {
-        const insertText = insertElement.elements["insertText"].value;
-        const insertPosition = parseInt(insertElement.elements["insertLocation"].value);
-
-        text.insertText(insertText, insertPosition);
-
-        event.preventDefault();
-    };
 }
 
-const documentId = "test-document2";
+const documentId = "graph1";
 run(documentId).catch((error) => {
     console.error(error);
 })
