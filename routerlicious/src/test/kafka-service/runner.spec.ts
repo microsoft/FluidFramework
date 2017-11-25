3db36ed94a4be10a80167f2f182456727c6e00e6
Kurt Berglund <kurtb@microsoft.com>
2017-11-25 01:01:42 -0800

    kafka-service test starter files

---
+++ routerlicious/src/test/kafka-service/runner.spec.ts
@@ -0,0 +1,21 @@
+import * as assert from "assert";
+import { KafkaRunner } from "../../kafka-service/runner";
+import { TestKafka } from "../testUtils";
+
+describe("kafka-service", () => {
+    describe("Runner", () => {
+        let runner: KafkaRunner;
+        let kafka: TestKafka;
+
+        beforeEach(() => {
+            kafka = new TestKafka();
+            runner = new KafkaRunner(kafka.createConsumer(), 10, 100);
+        });
+
+        describe("#start()", () => {
+            it("Should be able to stop after starting", async () => {
+                assert.ok(true);
+            });
+        });
+    });
+});
