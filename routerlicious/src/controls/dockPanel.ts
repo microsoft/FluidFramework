c10a63dacce05bfcdca306b6a52eb20ee6e3295f
Tanvir Aumi <mdaumi@microsoft.com>
2017-11-26 17:14:22 -0800

    Added title bar and coloring based on document id.

--- routerlicious/src/controls/dockPanel.ts
+++ routerlicious/src/controls/dockPanel.ts
@@ -6,6 +6,7 @@ import * as ui from "../ui";
 export class DockPanel extends ui.Component {
     public bottom: ui.Component;
     public content: ui.Component;
+    public top: ui.Component;
 
     constructor(element: HTMLDivElement) {
         super(element);
@@ -21,17 +22,28 @@ export class DockPanel extends ui.Component {
         this.updateChildren();
     }
 
+    public addTop(top: ui.Component) {
+        this.top = top;
+        this.updateChildren();
+    }
+
     protected resizeCore(bounds: ui.Rectangle) {
         let bottomOffset = 0;
         if (this.bottom) {
             const result = this.bottom.measure(bounds.size);
             bottomOffset = result.height;
         }
+        let topOffset = 0;
+        if (this.top) {
+            const result = this.top.measure(bounds.size);
+            topOffset = result.height;
+        }
 
-        let split = bounds.nipVertBottom(bottomOffset);
+        let split = bounds.nipVertTopBottom(topOffset, bottomOffset);
 
-        this.updateChildBoundsIfExists(this.content, split[0]);
-        this.updateChildBoundsIfExists(this.bottom, split[1]);
+        this.updateChildBoundsIfExists(this.top, split[0]);
+        this.updateChildBoundsIfExists(this.content, split[1]);
+        this.updateChildBoundsIfExists(this.bottom, split[2]);
     }
 
     /**
