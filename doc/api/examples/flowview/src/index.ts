c92e24a8d366836494c8ba2e1766aa5a9ab1214e
Kurt Berglund <kurtberglund@hotmail.com>
2017-12-21 17:22:26 -0800

    Update flowview example to use es5 API code

--- doc/api/examples/flowview/src/index.ts
+++ doc/api/examples/flowview/src/index.ts
@@ -1,8 +1,8 @@
 // For local development
-// const routerlicious = "http://localhost:3000";
-// const historian = "http://localhost:3001";
-const routerlicious = "http://praguekube.westus2.cloudapp.azure.com";
-const historian = "http://prague-historian.westus2.cloudapp.azure.com";
+const routerlicious = "http://localhost:3000";
+const historian = "http://localhost:3001";
+// const routerlicious = "http://praguekube.westus2.cloudapp.azure.com";
+// const historian = "http://prague-historian.westus2.cloudapp.azure.com";
 const repository = "prague";
 
 const pragueUi = window["pragueUi"];
@@ -10,34 +10,11 @@ const pragueUi = window["pragueUi"];
 // Register endpoint connection
 prague.socketStorage.registerAsDefault(routerlicious, historian, repository);
 
-function getLatestVersion(id: string): Promise<any> {
-    const versionP = new Promise<any>((resolve, reject) => {
-        const versionsP = $.getJSON(`${historian}/repos/${repository}/commits?sha=${encodeURIComponent(id)}&count=1`);
-        versionsP
-            .done((version) => {
-                resolve(version[0]);
-            })
-            .fail((error) => {
-                if (error.status === 400) {
-                    resolve(null);
-                } else {
-                    reject(error.status);
-                }
-            });
-    });
-
-    return versionP;
-}
-
 async function run(id: string): Promise<void> {
     const host = new pragueUi.ui.BrowserContainerHost();
 
-    // Get the latest version of the document
-    const version = await getLatestVersion(id);
-    console.log(version);
-
     // Load in the latest and connect to the document
-    const collabDoc = await prague.api.load(id, { blockUpdateMarkers: true }, version);
+    const collabDoc = await prague.api.load(id, { blockUpdateMarkers: true });
 
     const rootView = await collabDoc.getRoot().getView();
     console.log("Keys");
