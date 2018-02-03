210472f03668537232ff4e166f471b7814f383c6
Tanvir Aumi <mdaumi@microsoft.com>
2018-02-02 19:55:42 -0800

    Links in homepage with auto id creation.

--- apps/src/routes/maps.ts
+++ apps/src/routes/maps.ts
@@ -1,22 +1,31 @@
 import { Router } from "express";
+import * as moniker from "moniker";
 import { defaultPartials } from "./partials";
+
+function renderView(request, response, id: string, config: any) {
+    response.render(
+        "maps",
+        {
+            endpoints: JSON.stringify(config.endpoints),
+            id,
+            partials: defaultPartials,
+            repository: config.repository,
+            title: id,
+            token: request.query.token,
+            workerConfig: JSON.stringify(config.worker),
+        },
+    );
+}
 
 export function create(config: any): Router {
     const router: Router = Router();
 
+    router.get("/", (request, response, next) => {
+        response.redirect(`/maps/${moniker.choose()}`);
+    });
+
     router.get("/:id", (request, response, next) => {
-        response.render(
-            "maps",
-            {
-                endpoints: JSON.stringify(config.endpoints),
-                id: request.params.id,
-                partials: defaultPartials,
-                repository: config.repository,
-                title: request.params.id,
-                token: request.query.token,
-                workerConfig: JSON.stringify(config.worker),
-            },
-        );
+        renderView(request, response, request.params.id, config);
     });
 
     return router;
