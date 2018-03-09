3c6795a2b656d9d991632f6c201bfdb6da57fb17
Tanvir Aumi <mdaumi@microsoft.com>
2018-03-08 17:38:01 -0800

    Embedded grafana dashboard.

--- admin/src/controllers/components/Analytics.tsx
+++ admin/src/controllers/components/Analytics.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
+import Iframe from "react-iframe"
 
 export class Analytics extends React.Component<{}, {}> {
     render() {
-        return <h4>Embed the grafana dashboard here!</h4>;
+        return (
+            <div>
+                <h2 className="analytics-header">Analytics Dashboard</h2>
+                <Iframe url="http://prague-grafana.westus2.cloudapp.azure.com/dashboard/db/latency-summary?orgId=1"
+                        position="absolute"
+                        width="100%"
+                        id="grafana"
+                        height="100%"
+                        allowFullScreen/>
+            </div>
+        );
     }
 }
