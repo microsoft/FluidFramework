2fcdda2f4d381bcdf9b0bf427773b0eff19e44d9
Kurt Berglund <kurtb@microsoft.com>
2018-04-16 15:02:55 -0700

    Unify forced and automatic snapshot logic.

--- routerlicious/src/agent/serializer.ts
+++ routerlicious/src/agent/serializer.ts
@@ -1,115 +1,128 @@
-import * as queue from "async/queue";
+import * as assert from "assert";
 import { SaveOperation } from "../api-core";
 import { api, core } from "../client-api";
 
+// Consider idle 5s of no activity. And snapshot if a minute has gone by with no snapshot.
+const IdleDetectionTime = 5000;
+const MaxTimeWithoutSnapshot = IdleDetectionTime * 12;
+const SnapshotRetryTime = 1000;
+
 /**
- * Checks a flag every 'checkInterval' ms to see if its been cleared.
+ * Wrapper interface holding snapshot details for a given op
  */
-function checkFlag(flag: boolean, checkInterval: number, resolve, reject) {
-    if (!flag) {
-        resolve();
-    } else {
-        setTimeout(() => checkFlag(flag, checkInterval, resolve, reject), checkInterval);
-    }
+interface IOpSnapshotDetails {
+    // Whether we should snapshot at the given op
+    shouldSnapshot: boolean;
+
+    // The message to include with the snapshot
+    message: string;
+
+    // Whether creating the snapshot at this op is required
+    required: boolean;
 }
 
 /**
- * Returns a promie that resolves once snapshotting flag is cleared.
+ * Mananges snapshot creation for a distributed document
  */
-function waitForFlagClear(flag: boolean, checkInterval: number): Promise<void> {
-    return new Promise<void>((resolve, reject) => checkFlag(flag, checkInterval, resolve, reject));
-}
-
-// Loads a document from DB.
 export class Serializer {
-
-    private currentMsn: number = -1;
-    private snapshotRequested = false;
-    private snapshotTimer: any = null;
-    private saveQueue: any;
-    private forceSaving: boolean = false;
-    private autoSaving: boolean = false;
+    private lastSnapshotTime: number = -1;
+    private idleTimer = null;
+    private lastOp: core.ISequencedDocumentMessage = null;
+    private lastSnapshotOp: core.ISequencedDocumentMessage = null;
+    private lastOpSnapshotDetails: IOpSnapshotDetails = null;
+    private snapshotting = false;
 
     constructor(private document: api.Document) {
-        // Snapshot queue to perform snapshots sequentially.
-        this.saveQueue = queue( async (message: string, callback) => {
-            this.forceSaving = true;
-            await waitForFlagClear(this.autoSaving, 5);
-            const snapshotP = this.snapshot(message);
-            snapshotP.then((result) => {
-                this.forceSaving = false;
-                callback();
-            }, (error) => {
-                this.forceSaving = false;
-                callback();
-            });
-        }, 1);
     }
 
     public run(op: core.ISequencedDocumentMessage) {
-        // Forced snapshot.
-        if (op.type === SaveOperation) {
-            const saveMessage = op.contents.message === null ? "" : `: ${op.contents.message}`;
-            const tagMessage = `;${op.clientId}${saveMessage}`;
-            this.saveQueue.push(tagMessage);
-            return;
-        }
-        // Exit early in the case that the minimum sequence number hasn't changed
-        if (this.currentMsn === op.minimumSequenceNumber) {
-            return;
-        }
+        assert(!this.snapshotting, "Op processing should be paused when a snapshot is happening");
 
-        // Otherwise update the MSN and request a snapshot
-        this.currentMsn = op.minimumSequenceNumber;
-        this.requestSnapshot();
+        // Stop any idle processing
+        this.clearIdleTimer();
+
+        // Get the snapshot details for the given op
+        this.lastOp = op;
+        this.lastOpSnapshotDetails = this.getOpSnapshotDetails(op);
+
+        if (this.lastOpSnapshotDetails.shouldSnapshot) {
+            // Snapshot immediately if requested
+            this.snapshot(this.lastOpSnapshotDetails.message, this.lastOpSnapshotDetails.required);
+        } else {
+            // Otherwise detect when we idle to trigger the snapshot
+            this.startIdleTimer();
+        }
     }
 
-    private requestSnapshot() {
-        // TODO we probably want to split this into a strategy pattern around when to fire the snapshot
-        // and/or create an intelligent agent that detects this and inserts a special value into the stream
-        this.snapshotRequested = true;
+    private snapshot(message: string, required: boolean) {
+        this.snapshotting = true;
 
-        if (!this.snapshotTimer) {
-            const snapshotRequestTime = Date.now();
-
-            this.snapshotTimer = setInterval(() => {
-                // capture the snapshot requested state and then clear it
-                const snapshotRequested = this.snapshotRequested;
-                this.snapshotRequested = false;
-
-                // We will snapshot if no snapshot was requested within the interval (i.e. we idled) or we have
-                // been waiting for a specified amount of time without being able to snapshot
-                const delta = Date.now() - snapshotRequestTime;
-                if ((!snapshotRequested || delta > 60000) && !this.forceSaving) {
-                    // Stop the timer but don't clear the field to avoid anyone else starting the timer
-                    clearInterval(this.snapshotTimer);
-                    this.autoSaving = true;
-                    this.snapshot().then(() => {
-                        this.snapshotTimer = null;
-                        this.autoSaving = false;
-                        if (this.snapshotRequested) {
-                            this.requestSnapshot();
-                        }
-                    }, (error) => {
-                        this.snapshotTimer = null;
-                        this.autoSaving = false;
-                    });
-                }
+        // Otherwise pause the processing of inbound ops and then resume once the snapshot is complete
+        console.log(`Snapshotting ${this.document.id}@${this.lastOp.sequenceNumber}`);
+        this.document.deltaManager.inbound.pause();
+        const snapshotP = this.document.snapshot(message).then(
+            () => {
+                // On succes note the last op and time of the snapshot. Skip on error to cause us to
+                // attempt the snapshot again.
+                this.lastSnapshotOp = this.lastOp;
+                this.lastSnapshotTime = Date.now();
+                return true;
             },
-            10000);
-        }
-    }
+            (error) => {
+                console.error(`Snapshotting error ${this.document.id}`, error);
+                return false;
+            });
 
-    /**
-     * Performs the actual snapshot of the collaborative document
-     */
-    private snapshot(tagMessage: string = undefined) {
-        console.log(`Snapshotting ${this.document.id}@${this.currentMsn}`);
-        return this.document.snapshot(tagMessage).catch((error) => {
-            // TODO we will just log errors for now. Will want a better strategy later on (replay, wait)
-            if (error) {
-                console.error(`Error snapshotting ${this.document.id}`, error);
+        // If we were able to snapshot - or we failed but the snapshot wasn't required - then resume the inbound
+        // message flow. Otherwise attempt the snapshot again
+        snapshotP.then((success) => {
+            if (!success && required) {
+                setTimeout(() => this.snapshot(message, required), SnapshotRetryTime);
+            } else {
+                this.document.deltaManager.inbound.resume();
+                this.snapshotting = false;
             }
         });
+    }
+
+    private getOpSnapshotDetails(op: core.ISequencedDocumentMessage): IOpSnapshotDetails {
+        if (op.type === SaveOperation) {
+            // Forced snapshot.
+            const saveMessage = op.contents.message === null ? "" : `: ${op.contents.message}`;
+            return {
+                message: `;${op.clientId}${saveMessage}`,
+                required: true,
+                shouldSnapshot: true,
+            };
+        } else {
+            // Snapshot if it has been above the max time between snapshots.
+            const timeSinceLastSnapshot = Date.now() - this.lastSnapshotTime;
+            return {
+                message: "",
+                required: false,
+                shouldSnapshot: timeSinceLastSnapshot > MaxTimeWithoutSnapshot,
+            };
+        }
+    }
+
+    private clearIdleTimer() {
+        if (!this.idleTimer) {
+            return;
+        }
+
+        clearTimeout(this.idleTimer);
+        this.idleTimer = null;
+    }
+
+    private startIdleTimer() {
+        assert(!this.idleTimer);
+        this.idleTimer = setTimeout(
+            () => {
+                if (!this.lastSnapshotOp || this.lastSnapshotOp.sequenceNumber !== this.lastOp.sequenceNumber) {
+                    console.log("Snapshotting due to being idle");
+                    this.snapshot(this.lastOpSnapshotDetails.message, this.lastOpSnapshotDetails.required);
+                }
+            },
+            IdleDetectionTime);
     }
 }
