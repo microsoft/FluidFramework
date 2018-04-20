6fe0ed0f24eef17c8c46d4c9a1fb2ebdf1e24037
Sam Broner <SamBroner@users.noreply.github.com>
2018-04-20 04:03:24 -0700

    Add a game clock (#571)

--- apps/src/controllers/components/snake/body.tsx
+++ apps/src/controllers/components/snake/body.tsx
@@ -1,26 +1,67 @@
+import { map, types } from "@prague/routerlicious/dist/client-api";
 import * as React from "react";
 
-export interface IMove {
-    x: number;
-    y: number;
-    move: string;
+export interface ISnakeState {
+    turn: number;
 }
 
 export interface ISnakeProps {
-    body: string;
     name: string;
+    playerMap: types.IMap;
+    gameCounter: map.Counter;
 }
 
-export class Body extends React.Component<ISnakeProps, {}> {
+export class Body extends React.Component<ISnakeProps, ISnakeState> {
+    private playerView: types.IMapView;
+    private start: number;
     constructor(props: ISnakeProps) {
         super(props);
-        console.log("body Construct");
+        this.props.playerMap.getView().
+            then((view) => {
+                this.playerView = view;
+
+                console.log("Building Body Name: " + this.props.name +
+                            " Last Move: " + this.props.playerMap.get("lastMove"));
+
+                this.props.playerMap.on("valueChanged", async (delta: types.IValueChanged) => {
+
+                    if (delta.key === "playerStart") {
+                        this.start = await this.props.playerMap.get<number>("playerStart");
+
+                    } else if (delta.key === "gameCounter") {
+                        if (this.start === undefined && this.playerView.has("playerStart")) {
+                            this.start = this.playerView.get("playerStart");
+                        }
+
+                        if (this.start !== undefined) {
+                            const turnTime = this.playerView.get("gameCounter") - this.start;
+                            console.log(turnTime);
+                            this.setState({
+                                turn: turnTime,
+                            });
+                        }
+                    } else if (delta.key === "lastMove") {
+                        console.log("ValueChanged " + this.props.name + "key: " +
+                            delta.key +  "value: " + this.playerView.get(delta.key));
+                    } else {
+                        console.log("ValueChanged " + this.props.name + "key: " +
+                            delta.key +  "value: " + this.playerView.get(delta.key));
+                    }
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 
     public render() {
-        console.log("bodyRender");
-        return (
-            <h1> {this.props.name}: {this.props.body} </h1>
-        );
+        if (this.playerView) {
+            return (
+                <h1> {this.props.name}: {this.playerView.get("lastMove")}: {this.state.turn} </h1>
+            );
+        } else {
+            return (<h1> {this.props.name}: Player Not Yet Created </h1>);
+        }
+
     }
 }
--- apps/src/controllers/snake.tsx
+++ apps/src/controllers/snake.tsx
@@ -1,4 +1,4 @@
-import { api, socketStorage, types } from "@prague/routerlicious/dist/client-api";
+import { api, map, socketStorage, types } from "@prague/routerlicious/dist/client-api";
 
 import * as React from "react";
 import * as ReactDOM from "react-dom";
@@ -20,6 +20,28 @@ function displayError(parentElement: JQuery, error: string) {
     parentElement.append(idElement);
 }
 
+async function setMaps(gameMap: types.IMap, doc: api.Document): Promise<void> {
+
+    const gameCounter: map.Counter = await gameMap.get("gameCounter");
+
+    const p1Map = await gameMap.set("player1", doc.createMap());
+    await p1Map.set("lastMove", "ArrowLeft");
+    const p2Map = await gameMap.set("player2", doc.createMap());
+    await p2Map.set("lastMove", "ArrowRight");
+    const p3Map = await gameMap.set("player3", doc.createMap());
+    await p3Map.set("lastMove", "ArrowDown");
+    const p4Map = await gameMap.set("player4", doc.createMap());
+    await p4Map.set("lastMove", "ArrowUp");
+
+    gameCounter.onIncrement = (val) => {
+        const count = gameCounter.value;
+        p1Map.set("gameCounter", count);
+        p2Map.set("gameCounter", count);
+        p3Map.set("gameCounter", count);
+        p4Map.set("gameCounter", count);
+    };
+}
+
 export async function load(id: string, repository: string,  owner: string, endPoints: any, token?: string) {
     $("document").ready(() => {
         socketStorage.registerAsDefault(endPoints.delta, endPoints.storage, owner, repository);
@@ -34,6 +56,7 @@ export async function load(id: string, repository: string,  owner: string, endPo
             let gameMap: types.IMap;
             let gameView: types.IMapView;
             let moves: types.IMap;
+            let gameCounter: map.Counter;
             let canJoin: boolean = true;
 
             /*
@@ -47,40 +70,50 @@ export async function load(id: string, repository: string,  owner: string, endPo
             if (rootView.has("game")) {
                 gameMap = rootView.get("game") as types.IMap;
                 gameView = await gameMap.getView();
+                gameCounter = gameView.get("gameCounter");
             } else {
                 rootView.set("game", doc.createMap());
                 gameMap = rootView.get("game") as types.IMap;
                 gameView = await gameMap.getView();
+                gameCounter = gameView.set<map.Counter>("gameCounter", undefined, map.CounterValueType.Name);
+                await setMaps(gameMap, doc);
+                setInterval(() => {
+                    gameCounter.increment(1);
+                }, 1000);
             }
 
-            if (! (await gameMap.has("player1"))) {
+            if (! (await gameMap.has("player1Id"))) {
                 playerId = 1;
                 await gameMap.set("player1Id", playerId);
-                moves = await gameMap.set("player1", doc.createMap());
-            } else if (! (await gameMap.has("player2"))) {
+                moves = gameView.get("player1");
+                await moves.set("playerStart", gameCounter.value);
+            } else if (! (await gameMap.has("player2Id"))) {
                 playerId = 2;
                 await gameMap.set("player2Id", playerId);
-                moves = await gameMap.set("player2", doc.createMap());
-            } else if (! (await gameMap.has("player3"))) {
+                moves = gameView.get("player2");
+                await moves.set("playerStart", gameCounter.value);
+            } else if (! (await gameMap.has("player3Id"))) {
                 playerId = 3;
                 await gameMap.set("player3Id", playerId);
-                moves = await gameMap.set("player3", doc.createMap());
-            } else if (! (await gameMap.has("player4"))) {
+                moves = gameView.get("player3");
+                await moves.set("playerStart", gameCounter.value);
+            } else if (! (await gameMap.has("player4Id"))) {
                 playerId = 4;
                 await gameMap.set("player4Id", playerId);
-                moves = await gameMap.set("player4", doc.createMap());
+                moves = gameView.get("player4");
+                await moves.set("playerStart", gameCounter.value);
             } else {
                 canJoin = false;
             }
-            console.log(moves);
 
             if (!canJoin) {
                 console.log(`${playerId} can't join the game!`);
                 displayError($("#snakeViews"), "No more players allowed");
             } else {
+                const movesView = await moves.getView();
                 console.log(`${playerId} can join the game!`);
                 ReactDOM.render(
-                    <GameBoard gameMap={gameMap} gameView={gameView} moves={moves} width={100} height={100}
+                    <GameBoard gameMap={gameMap} gameView={gameView} moves={movesView} width={100} height={100}
                                 playerId={playerId} playerName={playerName}/>,
                     document.getElementById("snakeViews"),
                 );
