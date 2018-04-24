63d8bb826ed6012da390ed51d442db330e6982f5
Sam Broner <SamBroner@users.noreply.github.com>
2018-04-24 16:09:31 -0700

    Clean and Async Kafka Node Test (#574)
    * Add deferred promises
    
    * Implement async/queue
    
    * Close the producer, end the process

--- routerlicious/src/tools/kafkaTest.ts
+++ routerlicious/src/tools/kafkaTest.ts
@@ -1,16 +1,18 @@
+import * as queue from "async/queue";
 import * as commander from "commander";
 import * as kafkaNode from "kafka-node";
+import { Deferred } from "../core-utils";
 
 let endpoint = "zookeeper:2181"; // Must be run internally to use this endpoint
 let clientId = "testClient";
 let topic = "testtopic"; // Has to be registered
-let curOffset = 0;
+let partition = 0;
 
 commander
     .version("0.0.1")
     .option("-i, --implementation [implementation]", "Choose your Implementation", "kafka-node")
     .option("-m, --messages [messages]", "number of messages to test with", parseFloat, 10 ** 6)
-    .option("-b, --batchSize [batchSize]", "how many messages to put in a batch", parseInt, 1)
+    .option("-b, --batchSize [batchSize]", "how many messages to put in a batch", parseFloat, 1)
     .option("-p, --progress [progress]", "show progress")
     .parse(process.argv);
 
@@ -19,90 +21,98 @@ commander
                 " BatchSize: " + commander.batchSize +
                 " Progress: " + commander.progress);
 
-export async function getKafkaNodeOffset() {
+async function getKafkaNodeOffset(): Promise<number> {
     let client = new kafkaNode.Client(endpoint, clientId);
     let offset = new kafkaNode.Offset(client);
+    let deferred: Deferred<number> = new Deferred();
 
-    await offset.fetchLatestOffsets(
+    offset.fetchLatestOffsets(
         [topic], (err, offsets) => {
-        curOffset = offsets[topic]["0"];
+        client.close();
+        deferred.resolve(offsets[topic]["0"]);
     });
+
+    return deferred.promise;
 }
 
-export async function runKafkaNodeTest() {
+async function runKafkaNodeTest(startOffset: number) {
     console.log("Kafka-Node");
     let client = new kafkaNode.Client(endpoint, clientId);
     let producer = new kafkaNode.Producer(client, {partionerType: 3});
 
-    let offset = new kafkaNode.Offset(client);
+    let deferred: Deferred<number> = new Deferred();
 
-    let messages: string[] = new Array<string>();
+    let messageBatches: string[][] = new Array<string[]>();
 
-    let start: number;
+    for (let i = 0; i < commander.messages; ) {
+        let messageBatch: string[] = [];
+        for (let j = 0; j < commander.batchSize && i < commander.messages; j++, i++) {
+            let m = "message" + i;
+            messageBatch.push(m);
+        }
+        messageBatches.push(messageBatch);
+    }
+
     producer.on("ready", () => {
-        // Build Messages
-        for (let i = 0; i < commander.messages; i++) {
-            messages.push("message" + i);
-        }
-        let counter = 0;
-        start = Date.now();
-        while (messages.length > 1) {
-            if (commander.progress && messages.length % (commander.messages / 10) === 0) {
-                console.log("Messages added: " + messages.length);
-            }
-            producer.send([{
-                attributes: 0,
-                messages: [messages.pop()],
-                partition: 0,
-                topic,
-                }],
-                (err, result) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    counter++;
-                    if (commander.progress && counter % (commander.messages / 10) === 0) {
-                        console.log("Counter: " + counter);
-                    }
+        let responseCtr = 0;
+        let start = Date.now();
+
+        let sendQueue = new queue((messageBatch: string[], cb) => {
+            setImmediate(() => {
+                producer.send([{
+                    attributes: 0,
+                    messages: messageBatch,
+                    partition,
+                    topic,
+                    }],
+                    (err, result) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            responseCtr++;
+
+                            if (responseCtr === commander.messages) {
+                                let totalTime = Date.now() - start;
+                                client.close();
+                                producer.close((error) => {
+                                    deferred.resolve(totalTime);
+                                });
+                            }
+                            if (commander.progress && responseCtr % (commander.messages / 10) === 0) {
+                                let offset = getOffsetFromSend(result) - startOffset + 1;
+                                console.log((offset * 100 / commander.messages) + "% Completed");
+                            }
+                        }
+
+                });
+                cb();
             });
+        }, 1);
+
+        for (let messageBatch of messageBatches) {
+            sendQueue.push(messageBatch, () => { return undefined; });
         }
-
-        producer.send([{
-            attributes: 0,
-            messages: [messages.pop()],
-            partition: 0,
-            topic,
-            }],
-            (err, result) => {
-                let totalTime = Date.now() - start;
-                counter++;
-                console.log("Total Time: " + totalTime);
-                console.log("Total Counter: " + counter);
-                console.log("Time (ms) Per Ack: " + (totalTime / counter));
-        });
     });
-
-    // gets latest offset for each partition
-    offset.fetchLatestOffsets(
-            [topic], (err, offsets) => {
-                let otherTotalTime = Date.now() - start;
-                let totalOffset = (offsets[topic]["0"] - curOffset);
-                console.log("Time at offset: " + otherTotalTime);
-                console.log("Total Acked Messages: " + totalOffset);
-                console.log("Time (ms) Per Ack (offset): " + (otherTotalTime / totalOffset));
-                process.exit();
-        });
+    return deferred.promise;
 }
 
-export async function kafkaNodeRunner() {
+function getOffsetFromSend(result): number {
+    let offset = result[topic][partition.toString()];
+    return offset;
+}
 
-    await getKafkaNodeOffset();
+async function kafkaNodeRunner() {
 
-    // Wait to confirm prior offset...
-    // TODO: could be done in the first pop
-    setTimeout(async () => {
-        await runKafkaNodeTest();
-    }, 5000);
+    let startOffset = await getKafkaNodeOffset();
+    let totalTime = await runKafkaNodeTest(startOffset);
+    let endOffset = await getKafkaNodeOffset();
+    const totalOffset = endOffset - startOffset;
+    let messagesPerSecond = (totalOffset / totalTime) * 1000;
+
+    console.log("Total Time: " + totalTime);
+    console.log("Total Acked Messages: " + totalOffset);
+    console.log("Time (ms) Per Ack (offset): " + (totalTime / totalOffset));
+    console.log("Messages/Second: " + messagesPerSecond);
 }
 
 kafkaNodeRunner();
