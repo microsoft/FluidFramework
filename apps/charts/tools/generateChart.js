7b68efcc35e3f0fa45322e3d4018356e41712c5f
Tanvir Aumi <mdaumi@microsoft.com>
2018-05-01 10:19:46 -0700

    New tenantid and key for apps

--- apps/charts/tools/generateChart.js
+++ apps/charts/tools/generateChart.js
@@ -42,8 +42,8 @@ service:
   host: www.wu2.prague.office-int.com
   sslCert: wu2-tls-certificate
 
-tenantId: Distracted Bose
-secretKey: 3e7ceb4d70b937a572afbdd8f170a24f
+tenantId: loving-jones
+secretKey: 1dc089ccce64800a8d24a1b671248b5b
 
 resources:
   limits:
