44ae05fd2f087e9a7a4b63459dfee8b79c431f15
Sam Broner <sabroner@microsoft.com>
2018-05-07 15:21:45 -0700

    Creates .map and .js, untested

--- routerlicious/webpack.config.js
+++ routerlicious/webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 module.exports = {
     entry: {
-      api: "./src/client-api/index.ts",
-      ui: "./src/client-ui/index.ts",
-      agent: "./src/agent/index.ts",
-      controllers: "./src/alfred/controllers/index.ts"
+      api: "./dist/client-api/index.js",
+      ui: "./dist/client-ui/index.js",
+      agent: "./dist/agent/index.js",
+      controllers: "./dist/alfred/controllers/index.js"
     },
     // entry: __dirname + '/src/client-api/index.ts',
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "public/scripts/dist"),
         filename: '[name].js'
     },
     devtool: "source-map",
@@ -17,12 +17,21 @@ module.exports = {
         extensions: [".ts", ".tsx", ".js", ".json"]
     },
     // module: {
-    //     loaders: [
-    //         {
-    //             test:/
-                
-    //         }
-    //     ]
+    //     loader:
+    //       // Compile .tsx?
+    //       {
+    //         test: /\.(ts|tsx)$/,
+    //         include: paths.appSrc,
+    //         use: [
+    //           {
+    //             loader: require.resolve('ts-loader'),
+    //             options: {
+    //               // disable type checker - we will use it in fork plugin
+    //               transpileOnly: true,
+    //             },
+    //           },
+    //         ],
+    //       }
     // },
     node: {
         dgram: 'empty',
