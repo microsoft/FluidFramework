78b3e5c46847e06c6cc296063a98f1364bc09bb7
Tanvir Aumi <tanvir@cs.washington.edu>
2018-05-31 19:17:37 -0700

    Revert back raven-js changes (#691)

--- routerlicious/src/api/errorTrackingService.ts
+++ routerlicious/src/api/errorTrackingService.ts
@@ -1,18 +1,18 @@
 // tslint:disable:ban-types
-// import * as raven from "raven-js";
+import * as raven from "raven-js";
 import { IErrorTrackingService } from "../core-utils";
 
-// const sentryDSN = "https://7235bc222189462ca65fa1a38591f96b@sentry.wu2.prague.office-int.com/2";
+const sentryDSN = "https://7235bc222189462ca65fa1a38591f96b@sentry.wu2.prague.office-int.com/2";
 
 export class BrowserErrorTrackingService implements IErrorTrackingService {
 
-    // private ravenContext: raven.RavenStatic;
+    private ravenContext: raven.RavenStatic;
 
     constructor() {
-        // this.ravenContext = raven.config(sentryDSN).install();
+        this.ravenContext = raven.config(sentryDSN).install();
     }
 
     public track(func: Function) {
-        // this.ravenContext.context(func);
+        this.ravenContext.context(func);
     }
 }
