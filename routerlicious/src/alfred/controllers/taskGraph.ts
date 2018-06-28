02b0d3bef987780db8131f76f3a24448d4301021
Tanvir Aumi <mdaumi@microsoft.com>
2018-06-28 14:06:48 -0700

    Dynamic addition/removal of nodes

--- routerlicious/src/alfred/controllers/taskGraph.ts
+++ routerlicious/src/alfred/controllers/taskGraph.ts
@@ -3,6 +3,7 @@ import * as resources from "gitresources";
 import { api, socketStorage, types } from "../../client-api";
 
 interface INode {
+    clientId: string;
     id: string;
     group: number;
     radius: number;
@@ -12,6 +13,7 @@ interface ILink {
     source: string;
     target: string;
     strength: number;
+    label: string;
 }
 
 interface IGraph {
@@ -19,6 +21,7 @@ interface IGraph {
     links: ILink[];
 }
 
+// svg setup
 const w = window.innerWidth;
 const h = window.innerHeight;
 
@@ -26,11 +29,9 @@ const svg = d3.select("body")
 .append("svg")
 .attr("width", w)
 .attr("height", h);
-
 const color = d3.scaleOrdinal(d3.schemeCategory10);
-
 const simulation: any = d3.forceSimulation()
-    .force("charge", d3.forceManyBody().strength(-20))
+    .force("charge", d3.forceManyBody().strength(-3000))
     .force("center", d3.forceCenter(w / 2, h / 2))
     .force("link", d3.forceLink()
         .id((link: any) => link.id)
@@ -55,91 +56,76 @@ const dragDrop = d3
         node.fy = null;
     });
 
-function getNeighbors(node, links) {
-    return links.reduce(
-        (neighbors, link) => {
-        if (link.target.id === node.id) {
-            neighbors.push(link.source.id);
-        } else if (link.source.id === node.id) {
-            neighbors.push(link.target.id);
-        }
-        return neighbors;
-        },
-        [node.id],
-    );
-}
+let linkElements;
+let nodeElements;
+let textElements;
 
-function isNeighborLink(node: any, link) {
-    return link.target.id === node.id || link.source.id === node.id;
-}
+// we use svg groups to logically group the elements together
+const linkGroup = svg.append("g").attr("class", "links");
+const nodeGroup = svg.append("g").attr("class", "nodes");
+const textGroup = svg.append("g").attr("class", "texts");
+const labelGroup = svg.append("g").attr("class", "labels");
 
-function getNodeColor(node, neighbors) {
-    if (neighbors.indexOf(node.id)) {
-      return node.group === 1 ? "blue" : "green";
-    }
-    return color(node.group);
-}
-function getTextColor(node, neighbors) {
-    return neighbors.indexOf(node.id) ? "green" : "black";
-}
-function getLinkColor(node, link) {
-    return isNeighborLink(node, link) ? "green" : "#E5E5E5";
-}
+function updateGraph(graph: any) {
+    // links
+    linkElements = linkGroup.selectAll("line").data(graph.links, (link: any) => link.target.id + link.source.id);
+    linkElements.exit().remove();
 
-function renderGraph(graph: any) {
-    const nodeElements = svg.append("g")
-        .selectAll("circle")
-        .data(graph.nodes)
-        .enter().append("circle")
+    const linkEnter = linkElements.enter()
+        .append("line")
+        .attr("stroke-width", 1)
+        .attr("stroke", "rgba(50, 50, 50, 0.2)");
+
+    linkElements = linkEnter.merge(linkElements);
+
+    // nodes
+    nodeElements = nodeGroup.selectAll("circle").data(graph.nodes, (node: any) => node.id);
+    nodeElements.exit().remove();
+
+    const nodeEnter = nodeElements
+        .enter()
+        .append("circle")
         .attr("r", (d: any) => d.radius)
         .attr("fill", (d: any) => color(d.group))
-        .call(dragDrop)
-        .on("click", selectNode);
+        .call(dragDrop);
 
-    const textElements = svg.append("g")
-        .selectAll("text")
-        .data(graph.nodes)
-        .enter().append("text")
-        .text((d: any) => d.id)
-        .attr("font-size", 10)
-        .attr("dx", 15)
-        .attr("dy", 4);
+    nodeElements = nodeEnter.merge(nodeElements);
 
-    const linkElements = svg.append("g")
-        .selectAll("line")
-        .data(graph.links)
-        .enter().append("line")
-          .attr("stroke-width", 2)
-          .attr("stroke", "#E5E5E5");
+    // texts
+    textElements = textGroup.selectAll("text").data(graph.nodes, (node: any) => node.id);
+    textElements.exit().remove();
 
-    function selectNode(selectedNode) {
-        const neighbors = getNeighbors(selectedNode, graph.links);
-        nodeElements.attr("fill", (node) => getNodeColor(node, neighbors));
-        textElements.attr("fill", (node) => getTextColor(node, neighbors));
-        linkElements.attr("stroke", (link) => getLinkColor(selectedNode, link));
-    }
+    const textEnter = textElements
+        .enter()
+            .append("text")
+                .text((node: any) => node.id)
+                .attr("font-size", (node: any) => node.group === 1 ? 18 : 14)
+                .attr("dx", 0)
+                .attr("dy", 0)
+                .style("text-anchor", "middle")
+                .style("font-weight", "bold");
+
+    textElements = textEnter.merge(textElements);
+}
+
+function updateSimulation(graph: any) {
+    updateGraph(graph);
 
     simulation.nodes(graph.nodes).on("tick", () => {
-        nodeElements
-            .attr("cx", (node: any) => node.x)
-            .attr("cy", (node: any) => node.y);
-        textElements
-            .attr("x", (node: any) => node.x)
-            .attr("y", (node: any) => node.y);
+        nodeElements.attr("cx", (node: any) => node.x).attr("cy", (node: any) => node.y);
+        textElements.attr("x", (node: any) => node.x).attr("y", (node: any) => node.y);
         linkElements
             .attr("x1", (link: any) => link.source.x)
             .attr("y1", (link: any) => link.source.y)
             .attr("x2", (link: any) => link.target.x)
             .attr("y2", (link: any) => link.target.y);
-        });
+    });
+
     simulation.force("link").links(graph.links);
+    simulation.restart();
 }
 
 export async function load(id: string, version: resources.ICommit, config: any, token?: string) {
-    console.log(id);
-    console.log(JSON.stringify(version));
-    console.log(config);
-    console.log(token);
     socketStorage.registerAsDefault(
         document.location.origin,
         config.blobStorageUrl,
@@ -148,25 +134,26 @@ export async function load(id: string, version: resources.ICommit, config: any, 
     const doc = await api.load(id, { client: { type: "robot" }, encrypted: false, token }, version);
     const taskMap = await getTaskMap(doc);
     const taskMapView = await taskMap.getView();
-    const graph = generateGraphData(id, taskMapView);
-    renderGraph(graph);
-    /*
+    let graph = generateGraphData(id, taskMapView);
+    updateSimulation(graph);
+
     taskMap.on("valueChanged", () => {
         graph = generateGraphData(id, taskMapView);
-        renderGraph(graph);
-    });*/
+        console.log(graph);
+        updateSimulation(graph);
+    });
 }
 
 function generateGraphData(docId: string, taskMapView: types.IMapView): IGraph {
     const nodes: INode[] = [];
     const links: ILink[] = [];
-    nodes.push({ id: docId, group: 1, radius: 20});
+    nodes.push({ clientId: undefined, id: docId, group: 1, radius: 100});
     let groupId = 1;
     for (const task of taskMapView.keys()) {
         const clientId = taskMapView.get(task);
         if (clientId) {
-            nodes.push({ id: task, group: ++groupId, radius: 10});
-            links.push({source: docId, target: task, strength: 0.000001});
+            nodes.push({ clientId, id: task, group: ++groupId, radius: 50 });
+            links.push({ label: clientId, source: docId, target: task, strength: 0.1});
         }
     }
     const graph: IGraph = {
