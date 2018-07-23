fb3a2bf2ed4214e13f597bf354fa2e1b3939bf73
Tanvir Aumi <mdaumi@microsoft.com>
2018-07-23 15:29:15 -0700

    Static todo page

--- hackathon/todo-ionic-prague/src/pages/home/home.ts
+++ hackathon/todo-ionic-prague/src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+ 
+  public items;
+ 
   constructor(public navCtrl: NavController) {
-
+ 
   }
-
+ 
+  ionViewDidLoad(){
+ 
+    this.items = [
+      {title: 'hi1', description: 'test1'},
+      {title: 'hi2', description: 'test2'},
+      {title: 'hi3', description: 'test3'}
+    ];
+ 
+  }
+ 
+  addItem(){
+ 
+  }
+ 
+  viewItem(){
+ 
+  }
+ 
 }
