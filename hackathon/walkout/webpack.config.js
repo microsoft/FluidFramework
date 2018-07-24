22f772e62237062871641c7272af81c923fccb92
Kurt Berglund <kurtberglund@hotmail.com>
2018-07-24 11:00:05 -0700

    Make use of optimization chunk to speed up webpack

--- hackathon/walkout/webpack.config.js
+++ hackathon/walkout/webpack.config.js
@@ -14,6 +14,25 @@ module.exports = {
     resolve: {
         extensions: [ '.tsx', '.ts', '.js' ]
     },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    chunks: "initial",
+                    minChunks: 2,
+                    maxInitialRequests: 5, // The default limit is too small to showcase the effect
+                    minSize: 0 // This is example is too small to create commons chunks
+                },
+                vendor: {
+                    test: /node_modules/,
+                    chunks: "initial",
+                    name: "vendor",
+                    priority: 10,
+                    enforce: true
+                }
+            }
+        }
+    },
     output: {
         filename: 'bundle.js',
         library: "controllers",
