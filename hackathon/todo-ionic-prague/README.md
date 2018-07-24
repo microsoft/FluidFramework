9d86c7a78cda64a0e6b7448529e60228bde5833f
Tanvir Aumi <mdaumi@microsoft.com>
2018-07-23 18:07:00 -0700

    Prague inside ionic runtime

--- hackathon/todo-ionic-prague/README.md
+++ hackathon/todo-ionic-prague/README.md
@@ -1,1 +1,5 @@
 IONIC todo app leveraging prague runtime
+
+
+1. Because of ionic compatibility issue, we are using an older typescript version, that might cause a bug.
+2. have to disabble dgram in webpack config file.
--- hackathon/todo-ionic-prague/src/pages/home/home.ts
+++ hackathon/todo-ionic-prague/src/pages/home/home.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { api as prague } from "@prague/routerlicious";
 import { ModalController, NavController } from 'ionic-angular';
+import * as jwt from "jsonwebtoken";
 import { AddItemPage } from '../add-item/add-item'
 import { ItemDetailPage } from '../item-detail/item-detail';
 import { Data } from '../../providers/data/data';
+
+const routerlicious = "https://alfred.wu2-ppe.prague.office-int.com";
+const historian = "https://historian.wu2-ppe.prague.office-int.com";
+const tenantId = "confident-turing";
+const secret = "24c1ebcf087132e31a7e3c25f56f6626";
  
 @Component({
   selector: 'page-home',
@@ -21,6 +28,17 @@ export class HomePage {
       }
  
     });
+
+    prague.socketStorage.registerAsDefault(routerlicious, historian, tenantId);
+    const documentId = "hack-todo-0001";
+
+    const token = this.makeToken(documentId);
+
+    this.loadDocument(documentId, token).then((doc) => {
+      console.log(`Doc client id: ${doc.clientId}`);
+    }, (err) => {
+      console.log(err);
+    })
  
   }
  
@@ -53,6 +71,30 @@ export class HomePage {
     this.navCtrl.push(ItemDetailPage, {
       item: item
     });
+  }
+
+  private async loadDocument(id: string, token?: string): Promise<prague.api.Document> {
+    console.log("Loading in root document...");
+    const document = await prague.api.load(id, { encrypted: false, token }).catch((err) => {
+        return Promise.reject(err);
+    });
+
+    console.log("Document loaded");
+    return document;
+  }
+
+  private makeToken(documentId: string) {
+    const token = jwt.sign(
+      {
+          documentId,
+          permission: "read:write", // use "read:write" for now
+          tenantId,
+          user: {
+              id: "test",
+          },
+      },
+      secret);
+      return token;
   }
  
 }
