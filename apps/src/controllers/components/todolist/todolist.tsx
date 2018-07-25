c74a48b4423b358fa3a866c7f77dc0fdf5645039
Tanvir Aumi <mdaumi@microsoft.com>
2018-07-25 14:14:09 -0700

    Added modal to add items

--- apps/src/controllers/components/todolist/todolist.tsx
+++ apps/src/controllers/components/todolist/todolist.tsx
@@ -1,12 +1,13 @@
 import { api as prague } from "@prague/routerlicious";
 import * as React from "react";
+import Modal from "react-responsive-modal";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 
 export interface IItem {
     title: string;
     description: string;
-  }
+}
 
 export interface IToDoListProps {
     map: prague.types.IMap;
@@ -15,6 +16,9 @@ export interface IToDoListProps {
 
 export interface IToDoListState {
     list: IItem[];
+    modalOpen: boolean;
+    title: string;
+    description: string;
 }
 
 export class TodoList extends React.Component<IToDoListProps, IToDoListState> {
@@ -23,10 +27,33 @@ export class TodoList extends React.Component<IToDoListProps, IToDoListState> {
         this.setInitialState();
         this.listenForUpdate();
     }
+    public onOpenModal = () => {
+        this.setState({ modalOpen: true });
+    }
+
+    public onCloseModal = () => {
+        this.setState({ modalOpen: false, title: "", description: "" });
+    }
+
+    public handleTitleChange = (e) => {
+        const { value } = e.target;
+        this.setState({ title: value });
+    }
+
+    public handleDescriptionChange = (e) => {
+        const { value } = e.target;
+        this.setState({ description: value });
+    }
+
+    public onSave = () => {
+        const title = this.state.title;
+        const description = this.state.description;
+        this.save(title, description);
+    }
 
     public render() {
-      const data = this.state.list;
-      const columns = [
+        const data = this.state.list;
+        const columns = [
         {
             Header: "Item",
             accessor: "title",
@@ -34,23 +61,45 @@ export class TodoList extends React.Component<IToDoListProps, IToDoListState> {
         {
             Header: "Description",
             accessor: "description",
-        },
-    ];
-      return (
-        <div>
-              <ReactTable
-                data={data}
-                columns={columns}
-                defaultPageSize={10}
-                className="-striped -highlight"
-              />
-        </div>
-      );
+        }];
+        const { modalOpen, title, description } = this.state;
+        return (
+            <div>
+                <ReactTable
+                    data={data}
+                    columns={columns}
+                    defaultPageSize={10}
+                    className="-striped -highlight"
+                />
+                <button onClick={this.onOpenModal}>Add Item</button>
+                <Modal open={modalOpen} onClose={this.onCloseModal} center>
+                    <h2>Add Item</h2>
+                    Title: <input type="text" name="title" value={title} onChange={this.handleTitleChange} />
+                    Description: <input
+                        type="text"
+                        name="description"
+                        value={description}
+                        onChange={this.handleDescriptionChange} />
+                    <button onClick={this.onSave}>Save</button>
+                </Modal>
+            </div>
+        );
+    }
+
+    private save(title: string, description: string) {
+        const keyArray = Array.from(this.props.view.keys());
+        const mapIndex = keyArray.length;
+        this.props.view.set(String(mapIndex), `${title}@${description}`);
+        console.log(`New item: Title: ${title}, Description: ${description}`);
+        this.setState({ modalOpen: false, title: "", description: "" });
     }
 
     private setInitialState() {
         this.state = {
+            description: "",
             list: this.getData(),
+            modalOpen: false,
+            title: "",
         };
     }
 
