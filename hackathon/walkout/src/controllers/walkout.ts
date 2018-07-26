59db28b7d3dde197e957e1f65007f7b14e5f882d
Kurt Berglund <kurtberglund@hotmail.com>
2018-07-26 03:41:12 -0700

    Overlay commit message on top of video

--- hackathon/walkout/src/controllers/walkout.ts
+++ hackathon/walkout/src/controllers/walkout.ts
@@ -1,6 +1,8 @@
 import { api as prague } from "@prague/routerlicious";
 import * as async from "async";
+import * as escape from "escape-html";
 import * as jwt from "jsonwebtoken";
+import SvgText from "svg-text";
 import { VideoDocument } from "../documents";
 import * as gh from "../github";
 
@@ -12,7 +14,7 @@ prague.socketStorage.registerAsDefault(routerlicious, historian, tenantId);
 
 class VideoPlayer {
     private player: any;
-    private playbackQueue: async.AsyncQueue<string>;
+    private playbackQueue: async.AsyncQueue<gh.IPushHook>;
     private videoDocMap = new Map<string, Promise<VideoDocument>>();
     private playDeferred: prague.utils.Deferred<void>;
 
@@ -40,7 +42,7 @@ class VideoPlayer {
                 width: "100%",
             });
 
-        this.playbackQueue = async.queue<string, any>(
+        this.playbackQueue = async.queue<gh.IPushHook, any>(
             (work, callback) => {
                 this.playVideo(work)
                     .catch(() => { return; })
@@ -50,11 +52,15 @@ class VideoPlayer {
         this.playbackQueue.pause();
     }
 
-    public addToPlaylist(id: string) {
-        this.playbackQueue.push(id);
+    public addToPlaylist(hook: gh.IPushHook) {
+        this.playbackQueue.push(hook);
     }
 
-    private async playVideo(id: string) {
+    private async playVideo(hook: gh.IPushHook) {
+        const commit = hook.commits[0];
+        const author = commit.author;
+
+        const id = author.username;
         if (!this.videoDocMap.has(id)) {
             const token = jwt.sign(
                 {
@@ -69,10 +75,95 @@ class VideoPlayer {
             const videoP = VideoDocument.Load(id, token);
             this.videoDocMap.set(id, videoP);
         }
-
         const video = await this.videoDocMap.get(id);
+
+        let rects = "";
+        let firstDraw = "";
+        let secondDraw = "";
+        let defs = "";
+        const height = 60;
+        const smallHeight = 30;
+        let y = height;
+        const x = 10;
+        let index = 1;
+        const delayDelta = 0.5;
+
+        // const measureOuter = document.getElementById("measure");
+        // measureOuter.innerHTML = `<svg id="measureSvg" width="60%" height="1000px"></svg>`;
+        const measureElement = document.getElementById("measureSvg");
+
+        const splitLines = commit.message.split("\n").map((text) => ({ text, class: "" }));
+        splitLines.push({ text: author.email, class: "small" });
+
+        const lines = new Array<{ text: string, class: string, lineHeight: number }>();
+        for (const line of splitLines) {
+            const lineHeight = line.class === "small" ? smallHeight : height;
+            const text = new SvgText({
+                element: measureElement,
+                maxWidth: window.innerWidth / 2,
+                style: { "font-family": "Arial, sans-serif", "font-size": `${lineHeight}px` },
+                text: line.text,
+            }).text as SVGTextElement;
+
+            for (const childNode of text.childNodes) {
+                lines.push({
+                    class: line.class,
+                    lineHeight,
+                    text: escape(childNode.textContent),
+                });
+            }
+        }
+
+        let firstDelay = 0;
+        let secondDelay = lines.length * delayDelta;
+        for (const line of lines) {
+            // We compute again to get accurate sizing fo the tspan
+            const bbox = new SvgText({
+                element: measureElement,
+                maxWidth: window.innerWidth / 2,
+                style: { "font-family": "Arial, sans-serif", "font-size": `${line.lineHeight}px` },
+                text: line.text,
+            }).text.getBBox();
+
+            // tslint:disable:max-line-length
+            // Super cool SVG animation from https://codepen.io/supah/pen/vXyBza?editors=1111 and part of
+            // https://speckyboy.com/css-javascript-text-animation-snippets/
+            const rect = `<rect x="${bbox.x + x}" y="${bbox.y + y}" width="${bbox.width}" height="${bbox.height}" style="fill:rgb(0,0,0);stroke-width:10;stroke:rgb(0,0,0);" />`;
+            const firstText = `<text text-anchor="start" x="${x}" y="${y}" class="text text-stroke ${line.class}" clip-path="url(#text${index})" style="-webkit-animation-delay: ${firstDelay}s; animation-delay: ${firstDelay}s;">${line.text}</text>`;
+            const secondText = `<text text-anchor="start" x="${x}" y="${y}" class="text text-stroke text-stroke-2 ${line.class}" clip-path="url(#text${index})" style="-webkit-animation-delay: ${secondDelay}s; animation-delay: ${secondDelay}s;">${line.text}</text>`;
+            const clipPath = `<clipPath id="text${index}"><text text-anchor="start" x="${x}" y="${y}" class="text ${line.class}">${line.text}</text></clipPath>`;
+            // tslint:enable:max-line-length
+
+            rects += rect;
+            firstDraw += firstText;
+            secondDraw += secondText;
+            defs += clipPath;
+            y += line.lineHeight;
+            index++;
+            firstDelay += delayDelta;
+            secondDelay += delayDelta;
+        }
+
+        const svgHTML = `
+            <svg class="intro go" width="100%" height="100%">
+                <g style="opacity:0.7">
+                    ${rects}
+                </g>
+                <g>
+                    ${firstDraw}
+                    ${secondDraw}
+                </g>
+                <defs>
+                    ${defs}
+                </defs>
+            </svg>
+        `;
+        const details = document.getElementById("details");
+        details.innerHTML = svgHTML;
+
         this.playDeferred = new prague.utils.Deferred<void>();
         this.player.loadVideoById({ videoId: video.id, startSeconds: video.start, endSeconds: video.end });
+        this.player.playVideo();
 
         await this.playDeferred.promise;
     }
@@ -80,7 +171,6 @@ class VideoPlayer {
 
 async function run(id: string, YT: any): Promise<void> {
     const player = new VideoPlayer(YT, "player");
-
     const token = jwt.sign(
         {
             documentId: id,
@@ -94,6 +184,10 @@ async function run(id: string, YT: any): Promise<void> {
 
     // Load in the latest and connect to the document
     const collabDoc = await prague.api.load(id, { blockUpdateMarkers: true, token });
+    await new Promise((resolve) => {
+        collabDoc.once("connected", () => resolve());
+    });
+
     const rootView = await collabDoc.getRoot().getView();
 
     // Add in the text string if it doesn't yet exist
@@ -113,7 +207,7 @@ async function run(id: string, YT: any): Promise<void> {
                 const rawPen = operation.stylusDown.pen as any;
                 if (rawPen.event === "push") {
                     const pushEvent = rawPen.hook as gh.IPushHook;
-                    player.addToPlaylist(pushEvent.commits[0].author.username);
+                    player.addToPlaylist(pushEvent);
                 }
             }
         }
