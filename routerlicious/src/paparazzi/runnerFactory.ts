747081c13acd5ca1bab0bfa8e98e3162ae477234
Tanvir Aumi <mdaumi@microsoft.com>
2018-07-30 14:40:16 -0700

    Paparazzi listens to agent uploader directly.

--- routerlicious/src/paparazzi/runnerFactory.ts
+++ routerlicious/src/paparazzi/runnerFactory.ts
@@ -1,12 +1,14 @@
 import { Provider } from "nconf";
-import { createUploader } from "../services";
+import * as core from "../core";
+import * as services from "../services";
 import * as utils from "../utils";
-import { createMessageReceiver } from "./messageReceiver";
-import { IMessageReceiver } from "./messages";
 import { PaparazziRunner } from "./runner";
 
 export class PaparazziResources implements utils.IResources {
-    constructor(public workerConfig: any, public messageReceiver: IMessageReceiver) {
+    constructor(
+        public workerConfig: any,
+        public messageReceiver: core.IMessageReceiver,
+        public agentUploader: core.IAgentUploader) {
     }
 
     public async dispose(): Promise<void> {
@@ -17,18 +19,24 @@ export class PaparazziResources implements utils.IResources {
 export class PaparazziResourcesFactory implements utils.IResourcesFactory<PaparazziResources> {
     public async create(config: Provider): Promise<PaparazziResources> {
         const tmzConfig = config.get("tmz");
-        const rabbitmqConfig = config.get("rabbitmq");
         const workerConfig = config.get("worker");
         const queueName = config.get("paparazzi:queue");
 
-        const messageReceiver = createMessageReceiver(rabbitmqConfig, tmzConfig, queueName);
+        const rabbitmqConfig = config.get("rabbitmq");
+        const minioConfig = config.get("minio");
 
-        return new PaparazziResources(workerConfig, messageReceiver);
+        const messageReceiver = services.createMessageReceiver(rabbitmqConfig, tmzConfig, queueName);
+        const agentUploader = services.createUploader("minio", minioConfig);
+
+        return new PaparazziResources(workerConfig, messageReceiver, agentUploader);
     }
 }
 
 export class PaparazziRunnerFactory implements utils.IRunnerFactory<PaparazziResources> {
     public async create(resources: PaparazziResources): Promise<utils.IRunner> {
-        return new PaparazziRunner(resources.workerConfig, resources.messageReceiver);
+        return new PaparazziRunner(
+            resources.workerConfig,
+            resources.messageReceiver,
+            resources.agentUploader);
     }
 }
