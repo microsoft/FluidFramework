1577924985e805f159cbdcb04f3dba2eeddfdb07
Kurt Berglund <kurtb@microsoft.com>
2018-08-15 15:15:03 -0700

    stdin and stdout for console client

--- experiments/runtime/packages/loader-client/src/index.ts
+++ experiments/runtime/packages/loader-client/src/index.ts
@@ -1,19 +1,44 @@
 import * as loader from "@prague/loader";
 import { IDocumentService, ITokenService } from "@prague/runtime-definitions";
 import * as driver from "@prague/socket-storage";
+import chalk from "chalk";
 import * as commander from "commander";
 import * as jwt from "jsonwebtoken";
+import * as ora from "ora";
 import * as process from "process";
+import * as readline from "readline";
 
 // tslint:disable-next-line:no-var-requires
 const packageDetails = require("../package.json");
+
+async function readlineAsync(input: readline.ReadLine, prompt: string): Promise<string> {
+    return new Promise<string>((resolve) => {
+        input.question(prompt, (answer) => resolve(answer));
+    });
+}
 
 async function run(
     token: string,
     options: any,
     documentServices: IDocumentService,
     tokenServices: ITokenService): Promise<void> {
-    await loader.load(token, null, documentServices, tokenServices);
+    const claims = tokenServices.extractClaims(token);
+
+    const loadP = loader.load(token, null, documentServices, tokenServices);
+    ora.promise(loadP, `Loading ${claims.tenantId}/${claims.documentId}`);
+    await loadP;
+
+    console.log("");
+    console.log("Begin entering proposals (ctrl+c to quit)");
+    console.log("");
+
+    const input = readline.createInterface(process.stdin, process.stdout);
+    while (true) {
+        const key = await readlineAsync(input, chalk.green("Key: "));
+        const value = await readlineAsync(input, chalk.green("Value: "));
+        console.log(`${key}: ${value}`);
+        console.log("");
+    }
 }
 
 // Process command line input
