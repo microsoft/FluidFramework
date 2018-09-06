37473ccd4e0c0467e0301c5e9884d1393962905a
Tanvir Aumi <tanvir@cs.washington.edu>
2018-09-06 14:54:07 -0700

    Initial view for quizzes (#942)

---
+++ apps/src/routes/quizzes.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import { ensureAuthenticated } from "./authCheker";
+import { defaultPartials } from "./partials";
+
+function renderView(viewName: string, request, response, docId: string, config: any) {
+    response.render(
+        viewName,
+        {
+            endpoints: JSON.stringify(config.tenantInfo.endpoints),
+            id: docId,
+            partials: defaultPartials,
+            tenantId: config.tenantInfo.id,
+            title: docId,
+            token: request.query.token,
+        },
+    );
+}
+
+export function create(config: any): Router {
+    const router: Router = Router();
+
+    router.get("/edit/:id", ensureAuthenticated(config.tenantInfo.id, config.tenantInfo.secretKey),
+              (request, response, next) => {
+        request.query.token = response.locals.token;
+        const docId = request.params.id;
+        renderView("editquiz", request, response, docId, config);
+    });
+
+    router.get("/view/:id", ensureAuthenticated(config.tenantInfo.id, config.tenantInfo.secretKey),
+              (request, response, next) => {
+        request.query.token = response.locals.token;
+        const docId = request.params.id;
+        renderView("viewquiz", request, response, docId, config);
+    });
+
+    return router;
+}
