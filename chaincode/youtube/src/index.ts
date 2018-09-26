c523e17dd1903ba894109650e8f6dd4bf72ac119
kurtb <kurtberglund@hotmail.com>
2018-09-26 09:10:15 -0700

    Fix up YT chaincode (#993)

--- chaincode/youtube/src/index.ts
+++ chaincode/youtube/src/index.ts
@@ -4,29 +4,39 @@ import { IChaincode, IPlatform } from "@prague/runtime-definitions";
 import { Chaincode } from "./chaincode";
 import { Document } from "./document";
 
+const template =
+`<div id="player-div">
+    <form id="text-form">
+        <div class="form-group">
+            <label for="video">Youtube Video Id</label>
+            <input type="text" class="form-control" id="videoId">
+        </div>
+        <button type="button" class="btn btn-default" id="switch" style="margin-bottom: 10px;">Switch</button>
+    </form>
+</div>`;
+
 class Runner {
     public async run(collabDoc: Document, platform: IPlatform) {
-        const hostContent: HTMLElement = platform ? platform.queryInterface<HTMLElement>("div") : null;
+        const hostContent: HTMLDivElement = platform ? platform.queryInterface<HTMLDivElement>("div") : null;
         if (!hostContent) {
             // If headless exist early
             return;
         }
 
-        const content = document.createElement("div");
-        hostContent.appendChild(content);
+        hostContent.innerHTML = template;
 
         const host = new ui.BrowserContainerHost();
-        
+
         const root = collabDoc.getRoot();
 
         // Create our distributed Map, called "youTubeVideo", on the root map
-        if (collabDoc.existing) {
+        if (!collabDoc.existing) {
             await root.set<IMap>("youTubeVideo", collabDoc.createMap());
         }
 
         const videoMap = await root.wait<IMap>("youTubeVideo");
-        
-        const canvas = new controls.YouTubeVideoCanvas(hostContent, videoMap);
+
+        const canvas = new controls.YouTubeVideoCanvas(hostContent.children[0] as HTMLDivElement, videoMap);
         host.attach(canvas);
     }
 }
