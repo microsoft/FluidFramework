d54bfc12d9624c9515f099ed7f01eefbac19f39c
Kurt Berglund <kurtb@microsoft.com>
2018-09-29 00:12:57 -0700

    Shared text as chaincode

---
+++ routerlicious/packages/fv/src/chaincode.ts
@@ -0,0 +1,79 @@
+import { Document } from "@prague/client-api";
+import * as map from "@prague/map";
+import { IChaincode, IPlatform, IRuntime } from "@prague/runtime-definitions";
+import {
+    CollaborativeStringExtension,
+    SharedIntervalCollectionValueType,
+    SharedStringIntervalCollectionValueType,
+} from "@prague/shared-string";
+import { StreamExtension } from "@prague/stream";
+import * as assert from "assert";
+import { EventEmitter } from "events";
+
+const rootMapId = "root";
+const insightsMapId = "insights";
+
+class LocalPlatform extends EventEmitter implements IPlatform {
+    public queryInterface<T>(id: string) {
+        return null;
+    }
+}
+
+/**
+ * A document is a collection of collaborative types.
+ */
+export class Chaincode extends EventEmitter implements IChaincode {
+    private modules = new Map<string, any>();
+
+    /**
+     * Constructs a new document from the provided details
+     */
+    constructor(private runner: { run: (document, platofrm) => Promise<IPlatform> }) {
+        super();
+
+        // Register default map value types
+        map.registerDefaultValueType(new map.DistributedSetValueType());
+        map.registerDefaultValueType(new map.CounterValueType());
+        map.registerDefaultValueType(new SharedStringIntervalCollectionValueType());
+        map.registerDefaultValueType(new SharedIntervalCollectionValueType());
+
+        this.modules.set(map.MapExtension.Type, new map.MapExtension());
+        this.modules.set(CollaborativeStringExtension.Type, new CollaborativeStringExtension());
+        this.modules.set(StreamExtension.Type, new StreamExtension());
+    }
+
+    public getModule(type: string): any {
+        assert(this.modules.has(type));
+        return this.modules.get(type);
+    }
+
+    /**
+     * Stops the instantiated chaincode from running
+     */
+    public close(): Promise<void> {
+        return Promise.resolve();
+    }
+
+    public async run(runtime: IRuntime, platform: IPlatform): Promise<IPlatform> {
+        this.startForReal(runtime, platform);
+        return new LocalPlatform();
+    }
+
+    private async startForReal(runtime: IRuntime, platform: IPlatform) {
+        let root: map.IMap;
+
+        if (!runtime.existing) {
+            root = runtime.createChannel(rootMapId, map.MapExtension.Type) as map.IMap;
+            root.attach();
+
+            const insights = runtime.createChannel(insightsMapId, map.MapExtension.Type);
+            root.set(insightsMapId, insights);
+        } else {
+            root = await runtime.getChannel("root") as map.IMap;
+        }
+
+        const document = new Document(runtime, root);
+
+        return this.runner.run(document, platform);
+    }
+}
---
+++ routerlicious/packages/fv/src/localServer.ts
@@ -0,0 +1,107 @@
+import * as pragueLoader from "@prague/loader";
+import { IChaincodeFactory, ICodeLoader, IPlatform, IPlatformFactory } from "@prague/runtime-definitions";
+import * as socketStorage from "@prague/socket-storage";
+import { EventEmitter } from "events";
+import * as jwt from "jsonwebtoken";
+import * as testFactory from "./index";
+
+export class WebPlatform extends EventEmitter implements IPlatform {
+    constructor(private div: HTMLElement) {
+        super();
+    }
+
+    public queryInterface<T>(id: string) {
+        switch (id) {
+            case "dom":
+                return document;
+            case "div":
+                return this.div;
+            default:
+                return null;
+        }
+    }
+
+    // Temporary measure to indicate the UI changed
+    public update() {
+        this.emit("update");
+    }
+}
+
+export class WebPlatformFactory implements IPlatformFactory {
+    constructor(private div: HTMLElement) {
+    }
+
+    public async create(): Promise<IPlatform> {
+        return new WebPlatform(this.div);
+    }
+}
+
+export class CodeLoader implements ICodeLoader {
+    constructor(private factory: IChaincodeFactory) {
+    }
+
+    public async load(source: string): Promise<IChaincodeFactory> {
+        return this.factory;
+    }
+}
+
+const routerlicious = "http://localhost:3000";
+const historian = "http://localhost:3001";
+const tenantId = "prague";
+const secret = "43cfc3fbf04a97c0921fd23ff10f9e4b";
+
+async function initializeChaincode(document: pragueLoader.Document, pkg: string): Promise<void> {
+    const quorum = document.getQuorum();
+
+    // Wait for connection so that proposals can be sent
+    if (!document.connected) {
+        await new Promise<void>((resolve) => document.on("connected", () => resolve()));
+    }
+
+    // And then make the proposal if a code proposal has not yet been made
+    if (!quorum.has("code")) {
+        await quorum.propose("code", pkg);
+    }
+
+    console.log(`Code is ${quorum.get("code")}`);
+}
+
+/**
+ * Loads a specific version (commit) of the collaborative object
+ */
+export async function start(id: string, factory: IChaincodeFactory): Promise<void> {
+
+    const service = socketStorage.createDocumentService(routerlicious, historian);
+
+    const classicPlatform = new WebPlatformFactory(document.getElementById("content"));
+    const tokenService = new socketStorage.TokenService();
+    const codeLoader = new CodeLoader(factory);
+
+    const token = jwt.sign(
+        {
+            documentId: id,
+            permission: "read:write", // use "read:write" for now
+            tenantId,
+            user: {
+                id: "test",
+            },
+        },
+        secret);
+
+    // Load the Prague document
+    const loaderDoc = await pragueLoader.load(
+        token,
+        { blockUpdateMarkers: true },
+        classicPlatform,
+        service,
+        codeLoader,
+        tokenService);
+
+    // If this is a new document we will go and instantiate the chaincode. For old documents we assume a legacy
+    // package.
+    if (!loaderDoc.existing) {
+        await initializeChaincode(loaderDoc, `@local/test`).catch((error) => console.log("chaincode error", error));
+    }
+}
+
+start("chart-61", testFactory).catch((error) => console.error(error));
