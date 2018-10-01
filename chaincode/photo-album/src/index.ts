ff0d3b48877fcf654d72f35f34b23c4d2a526138
Sam Broner <SamBroner@users.noreply.github.com>
2018-09-30 23:41:43 -0700

    Fix carousel (#1021)
    * Load multiple images and display
    
    * set up next buttons

--- chaincode/photo-album/src/index.ts
+++ chaincode/photo-album/src/index.ts
@@ -1,6 +1,6 @@
-import { controls, ui } from "@prague/client-ui";
+import { ui } from "@prague/client-ui";
 import { IMap } from "@prague/map";
-import { IChaincode, IGenericBlob, IPlatform } from "@prague/runtime-definitions";
+import { IChaincode, IGenericBlob, IPlatform, MessageType } from "@prague/runtime-definitions";
 import { gitHashFile } from "@prague/utils";
 import { Chaincode } from "./chaincode";
 import { Document } from "./document";
@@ -11,12 +11,19 @@ const template =
 `<div id="player-div">
     <h1> Photo-Album 2.0 </h1>
     <input id="file-uploader" type="file" type="image">
-    <div id="carousel"></div>
+    <div id="carousel">
+        <div id="focus" class="focus"></div>
+        <a id="prev-button" class="prev">&#10094;</a>
+        <a id="next-button" class="next">&#10095;</a>
+        <div id="thumbnails" class="row"></div>
+    </div>
 </div>`;
 
+class PhotoCarousel extends ui.Component {
+
+}
+
 class Runner {
-    // private root: IMap;
-    private images: IGenericBlob[];
 
     public async run(doc: Document, platform: IPlatform) {
         const hostContent: HTMLDivElement = platform ? platform.queryInterface<HTMLDivElement>("div") : null;
@@ -26,159 +33,136 @@ class Runner {
         }
 
         hostContent.innerHTML = template;
+        this.carouselBuilder(doc);
+
+        const nextButton = document.getElementById("next-button");
+        nextButton.onclick = () => {this.plusSlides(1); };
+
+        const prevButton = document.getElementById("prev-button");
+        prevButton.onclick = () => {this.plusSlides(-1); };
 
         const fileUploader = document.getElementById("file-uploader") as HTMLInputElement;
         fileUploader.onchange = (e) => {
-            const file = fileUploader.files[0];
-            console.log(file);
 
-            const arrayBufferReader = new FileReader();
+            for (const file of fileUploader.files) {
+                const arrayBufferReader = new FileReader();
 
-            arrayBufferReader.onloadend = () => {
-                const blobData = Buffer.from(arrayBufferReader.result as ArrayBuffer);
-                const incl = {
-                    content: blobData,
-                    fileName: file.name,
-                    sha: gitHashFile(blobData),
-                    size: blobData.byteLength,
-                    type: "generic",
-                    url: "",
-                } as IGenericBlob;
-                // this.images.push()
-                this.imageHandler(doc.uploadBlob(incl));
-            };
-            arrayBufferReader.readAsArrayBuffer(file);
+                arrayBufferReader.onloadend = () => {
+                    const blobData = Buffer.from(arrayBufferReader.result as ArrayBuffer);
+                    const incl = {
+                        content: blobData,
+                        fileName: file.name,
+                        sha: gitHashFile(blobData),
+                        size: blobData.byteLength,
+                        type: "generic",
+                        url: "",
+                    } as IGenericBlob;
+
+                    // Move this somewhere more reasonable.
+                    doc.uploadBlob(incl);
+                };
+                arrayBufferReader.readAsArrayBuffer(file);
+            }
         };
 
-        console.log(fileUploader);
+        doc.runtime.on(MessageType.BlobUploaded, (ev) => {
+            this.carouselBuilder(doc);
+        });
 
         const host = new ui.BrowserContainerHost();
 
         const root = doc.getRoot();
-        this.images = await doc.getBlobMetaData();
 
-        // Create our distributed Map, called "youTubeVideo", on the root map
-        if (!doc.existing) {
-            await root.set<IMap>("youTubeVideo", doc.createMap());
-        }
-
-        const videoMap = await root.wait<IMap>("youTubeVideo");
-
-        const canvas = new controls.YouTubeVideoCanvas(hostContent.children[0] as HTMLDivElement, videoMap);
+        const canvas = new PhotoCarousel(hostContent.children[0] as HTMLDivElement);
         host.attach(canvas);
     }
 
-    private async imageHandler(blobP: Promise<IGenericBlob>) {
-        const blob = await blobP;
-        this.images.push(blob);
-        // const img = document.createElement("img");
-        // img.src = blob.url;
-        // carousel.appendChild(img);
-        this.carouselBuilder();
+    private async carouselBuilder(doc: Document) {
+        const images = await doc.getBlobMetaData();
+
+        const focusImage = document.getElementById("focus") as HTMLDivElement;
+        focusImage.innerHTML = "";
+        focusImage.style.height = "100%";
+        if (images.length > 0 ) {
+            focusImage.appendChild(this.buildSlides(images[0].url));
+        }
+
+        const thumbnails = document.getElementById("thumbnails") as HTMLDivElement;
+        thumbnails.innerHTML = "";
+        for (const image of images) {
+            thumbnails.appendChild(this.buildThumbnails(image.url));
+        }
     }
 
-    private async carouselBuilder() {
-        const carousel = document.getElementById("carousel") as HTMLDivElement;
-        let i = 0;
+    private buildSlides(url: string): HTMLDivElement {
+        const slide = document.createElement("div");
+        slide.id = "main-slide";
 
-        const script = document.createElement("script");
-        script.innerHTML = `
-                    // Open the Modal
-            function openModal() {
-            document.getElementById('myModal').style.display = "block";
+        const label = document.createElement("div");
+        label.classList.add("numberText");
+
+        const img = document.createElement("img");
+        img.id = "focus-image";
+        img.src = url;
+        img.style.height = "100%";
+        img.style.maxHeight = "500px";
+
+        slide.appendChild(label);
+        slide.appendChild(img);
+        return slide;
+    }
+
+    private buildThumbnails(url: string): HTMLDivElement {
+
+        const thumbnailDiv = document.createElement("div");
+        thumbnailDiv.classList.add("column");
+
+        const img = document.createElement("img");
+        img.classList.add("thumbnail");
+        img.src = url;
+        img.onclick = () => { this.setCurrentSlide(url); };
+
+        thumbnailDiv.appendChild(img);
+        return thumbnailDiv;
+    }
+
+    private setCurrentSlide(url: string) {
+        const slide = (document.getElementById("focus")) as HTMLDivElement;
+        slide.innerHTML = "";
+        slide.appendChild(this.buildSlides(url));
+    }
+
+    private getCurrentSlideUrl(): string {
+        const slide = (document.getElementById("focus-image")) as HTMLImageElement;
+        return slide.src;
+    }
+
+    private plusSlides(moves: number) {
+        const thumbnails = document.getElementsByClassName("thumbnail") as HTMLCollectionOf<HTMLImageElement>;
+        const current = this.getCurrentSlideUrl();
+
+        let prior = "";
+        let next = "";
+
+        prior = thumbnails[(thumbnails.length - 1)].src;
+        console.log(prior);
+        let getNext = false;
+        for (const thumbnail of thumbnails) {
+            if (getNext) {
+                next = thumbnail.src;
+                break;
+            } else if (thumbnail.src === current) {
+                getNext = true;
+            } else {
+                prior = thumbnail.src;
             }
-
-            // Close the Modal
-            function closeModal() {
-            document.getElementById('myModal').style.display = "none";
-            }
-
-            var slideIndex = 1;
-            showSlides(slideIndex);
-
-            // Next/previous controls
-            function plusSlides(n) {
-            showSlides(slideIndex += n);
-            }
-
-            // Thumbnail image controls
-            function currentSlide(n) {
-            showSlides(slideIndex = n);
-            }
-
-            function showSlides(n) {
-                var i;
-                var slides = document.getElementsByClassName("mySlides");
-                var dots = document.getElementsByClassName("demo");
-                var captionText = document.getElementById("caption");
-                if (n > slides.length) {slideIndex = 1}
-                if (n < 1) {slideIndex = slides.length}
-                for (i = 0; i < slides.length; i++) {
-                    slides[i].style.display = "none";
-                }
-                for (i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" active", "");
-                }
-                slides[slideIndex-1].style.display = "block";
-                dots[slideIndex-1].className += " active";
-                captionText.innerHTML = dots[slideIndex-1].alt;
-            }`;
-
-        carousel.innerHTML = `
-            <div class="mySlides">
-                <div class="numbertext">1 / 4</div>
-                <img src="` + this.images[i++].url + `" style="width:100%">
-            </div> `;
-        if (this.images.length > i) {
-            carousel.innerHTML += `
-
-            <div class="mySlides">
-                <div class="numbertext">2 / 4</div>
-                <img src="` + this.images[i++].url + `" style="width:100%">
-            </div> `;
-        } else if (this.images.length > i) {
-            carousel.innerHTML += `
-
-            <div class="mySlides">
-                <div class="numbertext">3 / 4</div>
-                <img src="` + this.images[i++].url + `" style="width:100%">
-            </div> `;
-        } else if (this.images.length > i) {
-            carousel.innerHTML += `
-
-            <div class="mySlides">
-                <div class="numbertext">4 / 4</div>
-                <img src="` + this.images[i++].url + `" style="width:100%">
-            </div> `;
         }
-        i = 0;
-        carousel.innerHTML += `
-            <!-- Next/previous controls -->
-            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a>
 
-            <!-- Thumbnail image controls -->
-            <div class="column">
-                <img class="demo" src="` + this.images[i++].url + `" onclick="currentSlide(1)" alt="Nature">
-            </div>`;
-        if (this.images.length > i ) {
-            carousel.innerHTML += `
-            <div class="column">
-                <img class="demo" src="` + this.images[i++].url + `" onclick="currentSlide(2)" alt="Snow">
-            </div>`;
-        } else if (this.images.length > i ) {
-            carousel.innerHTML += `
-            <div class="column">
-                <img class="demo" src="` + this.images[i++].url + `" onclick="currentSlide(3)" alt="Mountains">
-            </div>`;
-        } else if (this.images.length > i ) {
-            carousel.innerHTML += `
-            <div class="column">
-                <img class="demo" src="` + this.images[i++].url + `" onclick="currentSlide(4)" alt="Lights">
-            </div>`;
+        if (moves === -1 ) {
+            this.setCurrentSlide(prior);
+        } else {
+            this.setCurrentSlide(next);
         }
-        document.head.appendChild(script);
-
     }
 }
 
