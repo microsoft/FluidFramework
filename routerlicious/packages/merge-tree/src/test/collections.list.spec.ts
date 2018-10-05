8fab3ad0d3ae2656d49d9169f0410b5ce34a80a1
Tony Murphy <anthony.murphy@microsoft.com>
2018-10-04 20:45:27 -0700

    Add initial tests for collections list (#1039)
    * add inital list tests
    
    * fix linting errors

---
+++ routerlicious/packages/merge-tree/src/test/collections.list.spec.ts
@@ -0,0 +1,28 @@
+import * as assert from "assert";
+import * as Collections from "../collections";
+
+describe("Test List", () => {
+    const listCount = 5;
+    const list: Collections.List<number> = Collections.ListMakeHead<number>();
+
+    for (let i = 0; i < listCount; i++) {
+        list.push(i);
+    }
+
+    it("count", () => assert.equal(list.count(), listCount, "The list count doesn't match the expected count."));
+
+    it("first", () => assert.equal(list.first(), listCount - 1, "first item not expected value"));
+
+    it("last", () => assert.equal(list.last(), 0, "last item not expected value"));
+
+    it("isHead", () => assert.equal(list.isHead, true, "expected head not head"));
+
+    it("walk", () => {
+        let i = listCount - 1;
+
+        list.walk((data) => {
+            assert.equal(data, i, "elemeted not expected value");
+            i--;
+        });
+    });
+});
