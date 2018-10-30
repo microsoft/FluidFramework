47647ffd1c49ccd0a5053cf306e969e12cdbc4c9
Kurt Berglund <kurtb@microsoft.com>
2018-10-29 19:29:50 -0700

    Rebalance callbacks to use with rdkafka

--- routerlicious/functions/deli/src/consumer.ts
+++ routerlicious/functions/deli/src/consumer.ts
@@ -1,26 +1,42 @@
-/*
- * node-rdkafka - Node.js wrapper for RdKafka C/C++ library
- *
- * Copyright (c) 2016 Blizzard Entertainment
- *
- * This software may be modified and distributed under the terms
- * of the MIT license.    See the LICENSE.txt file for details.
- */
-
 import * as Kafka from "node-rdkafka";
+
+// https://github.com/edenhill/librdkafka/wiki/How-to-decrease-message-latency
 
 const consumer = new Kafka.KafkaConsumer(
     {
-        // 'debug': 'all',
         "enable.auto.commit": false,
+        "event_cb": () => {
+            console.log("event_cb");
+        },
+        "fetch.error.backoff.ms": 20,
+        "fetch.wait.max.ms": 5,
         "group.id": "node-rdkafka-consumer-flow-example",
         "metadata.broker.list": "kafka:9092",
+        "rebalance_cb": (err, assignment) => {
+            if (err.code === Kafka.CODES.ERRORS.ERR__ASSIGN_PARTITIONS) {
+                console.log("NEW ASSIGNMENTS", assignment);
+                // Note: this can throw when you are disconnected. Take care and wrap it in
+                // a try catch if that matters to you
+                consumer.assign(assignment);
+            } else if (err.code === Kafka.CODES.ERRORS.ERR__REVOKE_PARTITIONS) {
+                console.log("REMOVE ASSIGNMENTS", assignment);
+                // Same as above
+                consumer.unassign();
+            } else {
+                // We had a real error
+                console.error(err);
+            }
+        },
+        "statistics.interval.ms": 10000,
+        "stats_cb": (stats) => {
+            console.log("I got stats!");
+        },
     },
     {
-        "auto.offset.reset": "earliest",
+        "auto.offset.reset": "latest",
     });
 
-const topicName = "testtopic";
+const topicName = "rawdeltas";
 
 // logging debug messages, if debug is enabled
 consumer.on("event.log", (log) => {
@@ -33,37 +49,35 @@ consumer.on("event.error", (err) => {
     console.error(err);
 });
 
-// counter to commit offsets every numMessages are received
-const numMessages = 100000;
-let counter = 0;
-let start: number;
+consumer.on("rebalance", (stuff) => {
+    console.log("rebalance");
+    // console.log(stuff);
+});
 
 consumer.on("ready", (arg) => {
-    console.log("consumer ready." + JSON.stringify(arg));
+    console.log("consumer ready", arg);
+
+    console.log("Assignments", consumer.assignments());
 
     consumer.subscribe([topicName]);
     // start consuming messages
     consumer.consume();
+});
 
-    start = Date.now();
+(consumer as any).on("event.stats", (stats) => {
+    // console.log("I GOT STATS!", stats);
+    // console.log("Assignments", consumer.assignments());
 });
 
 consumer.on("data", (m) => {
-    counter++;
-
-    // Update stopwatch periodically
-    if (counter % numMessages === 0) {
-        const now = Date.now();
-        const total = now - start;
-        console.log(`${(counter * 1000 / total).toFixed(4)} msg/s - ${counter} / ${total / 1000}`);
-        counter = 0;
-        start = now;
-        consumer.commit(m);
-    }
-
     // Output the actual message contents
-    // console.log(JSON.stringify(m));
+    console.log(m.offset);
     // console.log(m.value.toString());
+
+    if (m.offset % 1000 === 0) {
+        console.log("Commit trigger");
+        consumer.commit([{ topic: topicName, partition: m.partition, offset: m.offset }]);
+    }
 });
 
 consumer.on("disconnected", (arg) => {
@@ -72,8 +86,3 @@ consumer.on("disconnected", (arg) => {
 
 // starting the consumer
 consumer.connect();
-
-setTimeout(() => {
-    console.log("disconnect");
-    consumer.disconnect();
-}, 5000);
