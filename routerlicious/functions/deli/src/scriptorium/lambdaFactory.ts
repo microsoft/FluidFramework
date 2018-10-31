388760986cc8a9500ee50dc03717a88e37fd0580
Kurt Berglund <kurtb@microsoft.com>
2018-10-31 01:41:10 -0700

    Pull over scriptorium as well for quicker updating

---
+++ routerlicious/functions/deli/src/scriptorium/lambdaFactory.ts
@@ -0,0 +1,33 @@
+import * as core from "@prague/routerlicious/dist/core";
+import * as services from "@prague/routerlicious/dist/services";
+import * as utils from "@prague/routerlicious/dist/utils";
+import { EventEmitter } from "events";
+import { Provider } from "nconf";
+import { IContext, IPartitionLambda, IPartitionLambdaFactory } from "../kafka-service/lambdas";
+import { ScriptoriumLambda } from "./lambda";
+
+export class ScriptoriumLambdaFactory extends EventEmitter implements IPartitionLambdaFactory {
+    constructor(
+        private mongoManager: utils.MongoManager,
+        private collection: core.ICollection<any>,
+        private io: services.SocketIoRedisPublisher) {
+        super();
+
+        this.io.on("error", (error) => {
+            // After an IO error we need to recreate the lambda
+            this.emit("error", error);
+        });
+    }
+
+    public async create(config: Provider, context: IContext): Promise<IPartitionLambda> {
+        // Takes in the io as well as the collection. I can probably keep the same lambda but only ever give it stuff
+        // from a single document
+        return new ScriptoriumLambda(this.io, this.collection, context);
+    }
+
+    public async dispose(): Promise<void> {
+        const mongoClosedP = this.mongoManager.close();
+        const publisherP = this.io.close();
+        await Promise.all([mongoClosedP, publisherP]);
+    }
+}
