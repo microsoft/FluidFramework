c0ba0405d47e79eb9f99bca87aafe2c0c1ce4e33
Tanvir Aumi <mdaumi@microsoft.com>
2018-10-31 15:30:35 -0700

    Deleted conflicting merge state

--- routerlicious/packages/sharepoint-socket-storage/lib/registration.d.ts
+++
@@ -1,4 +0,0 @@
-import * as api from "@prague/client-api";
-import * as resources from "@prague/gitresources";
-import { ITokenProvider, IUser } from "@prague/runtime-definitions";
-export declare function load(snapshotUrl: string, deltaFeedUrl: string, webSocketUrl: string, id: string, tenantId: string, user: IUser, tokenProvider: ITokenProvider, options?: any, version?: resources.ICommit, connect?: boolean): Promise<api.Document>;
