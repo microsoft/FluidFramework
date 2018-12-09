9ce428ade99ae5df4bdee2121193f36f46a5c192
Kurt Berglund <kurtberglund@hotmail.com>
2018-12-09 15:32:27 -0800

    Fix up dockerfile after package split

--- routerlicious/packages/sharepoint-socket-storage/lib/token.d.ts
+++
@@ -1,7 +0,0 @@
-import { ITokenProvider } from "@prague/runtime-definitions";
-export declare class TokenProvider implements ITokenProvider {
-    storageToken: string;
-    deltaStorageToken: string;
-    deltaStreamToken: string;
-    constructor(deltaStorageToken: string, deltaStreamToken: string);
-}
