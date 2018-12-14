6d0b620015aec639e07fe25d224d982a6833aed7
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2018-12-13 17:23:00 -0800

    Change the correct package.

--- routerlicious/packages/socket-storage/src/index.ts
+++ routerlicious/packages/socket-storage/src/index.ts
@@ -3,4 +3,5 @@ export * from "./messages";
 export * from "./deltaStorageService";
 export * from "./documentService";
 export * from "./blobStorageService";
+export * from "./registration";
 export * from "./tokens";
--- routerlicious/packages/standard-socket-storage/src/index.ts
+++ routerlicious/packages/standard-socket-storage/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./deltaStorageService";
-export * from "./registration";
 export * from "./contracts";
 export * from "./documentService";
 export * from "./documentStorageService";
