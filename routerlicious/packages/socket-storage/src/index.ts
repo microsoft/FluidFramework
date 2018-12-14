1c3d4ed999efdaf0b1d0c5e610c574a1d35b606c
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2018-12-13 16:40:04 -0800

    Don't re-export registration

--- routerlicious/packages/socket-storage/src/index.ts
+++ routerlicious/packages/socket-storage/src/index.ts
@@ -3,5 +3,4 @@ export * from "./messages";
 export * from "./deltaStorageService";
 export * from "./documentService";
 export * from "./blobStorageService";
-export * from "./registration";
 export * from "./tokens";
