f5beac0cf02771be92bc6c39e5a02197b5d15eab
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2018-12-14 11:18:11 -0800

    Move DocumentService2 to another module

--- routerlicious/packages/socket-storage/src/registration.ts
+++ routerlicious/packages/socket-storage/src/registration.ts
@@ -1,5 +1,5 @@
 import { IDocumentService, IErrorTrackingService } from "@prague/runtime-definitions";
-import { DocumentService, DocumentService2 } from "./documentService";
+import { DocumentService } from "./documentService";
 import { DefaultErrorTracking } from "./errorTracking";
 
 export function createDocumentService(
@@ -12,26 +12,6 @@ export function createDocumentService(
 
     /* tslint:disable:no-unsafe-any */
     const service = new DocumentService(
-        deltaUrl,
-        gitUrl,
-        errorTracking,
-        disableCache,
-        historianApi,
-        credentials);
-
-    return service;
-}
-
-export function createDocumentService2(
-    deltaUrl: string,
-    gitUrl: string,
-    errorTracking: IErrorTrackingService = new DefaultErrorTracking(),
-    disableCache = false,
-    historianApi = true,
-    credentials?): IDocumentService {
-
-    /* tslint:disable:no-unsafe-any */
-    const service = new DocumentService2(
         deltaUrl,
         gitUrl,
         errorTracking,
