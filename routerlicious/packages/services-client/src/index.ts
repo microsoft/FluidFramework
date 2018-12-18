66b700fdbbc79818249bddc23cbca165e465c6b8
Skyler Jokiel <sky.jokiel@outlook.com>
2018-12-18 11:48:26 -0800

    Basic snapshot implementation for standard-socket-storage (#1220)

--- routerlicious/packages/services-client/src/index.ts
+++ routerlicious/packages/services-client/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./gitManager";
 export * from "./historian";
+export * from "./restWrapper";
 export * from "./utils";
--- routerlicious/packages/standard-socket-storage/src/index.ts
+++ routerlicious/packages/standard-socket-storage/src/index.ts
@@ -2,4 +2,5 @@ export * from "./deltaStorageService";
 export * from "./contracts";
 export * from "./documentService";
 export * from "./documentStorageService";
+export * from "./standardDocumentStorageManager";
 export * from "./token";
