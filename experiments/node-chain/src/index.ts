70840f64ea51dc7e42aac17b13d4d9669d08ccf5
Tanvir Aumi <mdaumi@microsoft.com>
2019-01-11 16:28:16 +0600

    Promisify waiting for new runtime.

--- experiments/node-chain/src/index.ts
+++ experiments/node-chain/src/index.ts
@@ -2,6 +2,7 @@ import * as loader from "@prague/loader";
 import { IMap } from "@prague/map";
 import {
     IDocumentService,
+    IRuntime,
     ITokenProvider,
     IUser,
 } from "@prague/runtime-definitions";
@@ -17,11 +18,18 @@ import { NodePlatformFactory } from "./nodePlatformFactory";
 const rootMapId = "root";
 
 // tslint:disable:no-unsafe-any
-
 async function readlineAsync(input: readline.ReadLine, prompt: string): Promise<string> {
     return new Promise<string>((resolve) => {
         // tslint:disable-next-line:no-unnecessary-callback-wrapper
         input.question(prompt, (answer) => resolve(answer));
+    });
+}
+
+async function waitForNewRuntime(document: loader.Document) {
+    return new Promise<IRuntime>((resolve, reject) => {
+        document.on("runtimeChanged", (newRuntime: IRuntime) => {
+            resolve(newRuntime);
+        });
     });
 }
 
@@ -44,31 +52,34 @@ async function run(
     ora.promise(documentP, `Loading ${tenantId}/${id}`);
     const document = await documentP;
 
-    document.on("runtimeChanged", async (newRuntime) => {
-        const root = await newRuntime.getChannel(rootMapId) as IMap;
-        const rootView = await root.getView();
+    const newRuntimeP = waitForNewRuntime(document);
+    ora.promise(newRuntimeP, `Waiting for new runtime\n`);
+    const newRuntime = await newRuntimeP;
 
-        console.log("");
-        console.log("Enter increment amount (ctrl+c to quit)");
-        console.log("");
+    const root = await newRuntime.getChannel(rootMapId) as IMap;
+    const rootView = await root.getView();
 
-        const input = readline.createInterface(process.stdin, process.stdout);
-        // tslint:disable-next-line:no-constant-condition
-        while (true) {
-            const value = await readlineAsync(input, "Value: ");
+    console.log("");
+    console.log("Enter increment amount (ctrl+c to quit)");
+    console.log("");
 
-            // Only accepts number as input.
-            let parsedValue: number;
-            try {
-                parsedValue = Number(value);
-                // tslint:disable no-backbone-get-set-outside-model
-                const clicks = Number(rootView.get("clicks"));
-                rootView.set("clicks", clicks + parsedValue);
-            } catch {
-                console.log(`Only integer value is allowed`);
-            }
+    const input = readline.createInterface(process.stdin, process.stdout);
+    // tslint:disable-next-line:no-constant-condition
+    while (true) {
+        const value = await readlineAsync(input, "Value: ");
+
+        // Only accepts number as input.
+        let parsedValue: number;
+        try {
+            parsedValue = Number(value);
+            // tslint:disable no-backbone-get-set-outside-model
+            const clicks = Number(rootView.get("clicks"));
+            rootView.set("clicks", clicks + parsedValue);
+            console.log(`New value: ${rootView.get("clicks")}`);
+        } catch {
+            console.log(`Only integer value is allowed`);
         }
-    });
+    }
 }
 
 // Process command line input
