b82a1951a4ce9b5d0ce591390ab7bc73fea7e44e
Sam Broner <sabroner@microsoft.com>
2019-01-16 21:11:52 -0800

    Consolidate URLs

--- routerlicious/packages/flow-app/src/index.tsx
+++ routerlicious/packages/flow-app/src/index.tsx
@@ -4,7 +4,7 @@ import { App } from "./app";
 
 export function start() {
     ReactDOM.render(
-        <App />,
+        <App alfredUrl="http://localhost:3000" mediaUrl="http://localhost:8080" verdaccioUrl="http://localhost:4873"/>,
         document.body
     );
 }
