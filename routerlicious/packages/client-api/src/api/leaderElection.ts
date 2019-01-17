8a0ab556d92e4685e695a20cdd43cdf76feef2a2
Tanvir Aumi <mdaumi@microsoft.com>
2019-01-17 20:47:26 +0600

    Enable leader election only for browser clients.

--- routerlicious/packages/client-api/src/api/leaderElection.ts
+++ routerlicious/packages/client-api/src/api/leaderElection.ts
@@ -6,49 +6,47 @@ export class LeaderElector extends EventEmitter {
     private leader: string;
     constructor(private quorum: IQuorum, private clientId: string) {
         super();
-        quorum.on("approveProposal", this.setLeader);
-        quorum.on("addProposal", this.handleLeadership);
-        quorum.on("removeMember", this.checkLeadership);
-        quorum.on("rejectProposal", this.handleRejection);
-        this.checkLeadership();
+        this.attachQuorumListeners();
+        this.quorum.propose("leader", this.clientId).then(() => {
+            // console.log(`Proposal accepted: ${this.clientId}!`);
+        }, (err) => {
+            // console.log(`Error proposing new leadership: ${err}`);
+        });
     }
 
     public getLeader() {
         return this.leader;
     }
 
-    private setLeader(sequenceNumber: number, key: string, value: any) {
-        if (key === "leader") {
-            this.leader = value as string;
-            this.emit("leader", value);
-            console.log(`New leader elected: ${value}`);
-        }
-    }
-
-    private handleLeadership(proposal: IPendingProposal) {
-        if (proposal.key === "leader") {
-            if (this.leader !== undefined) {
-                proposal.reject();
+    private attachQuorumListeners() {
+        this.quorum.on("approveProposal", (sequenceNumber: number, key: string, value: any) => {
+            if (key === "leader") {
+                this.leader = value as string;
+                this.emit("leader", this.leader);
+                // console.log(`New leader elected: ${value}`);
             }
-        }
-    }
+        });
+        this.quorum.on("addProposal", (proposal: IPendingProposal) => {
+            if (proposal.key === "leader") {
+                if (this.leader !== undefined) {
+                    proposal.reject();
+                }
+            }
+        });
 
-    private checkLeadership(removedClientId?: string) {
-        if (this.leader === undefined || removedClientId === this.leader) {
-            this.leader = undefined;
-            this.proposeLeadership().then(() => {
-                console.log(`Successfully propsed!`);
-            }, (err) => {
-                console.log(`Error proposing new leadership`);
-            });
-        }
-    }
+        this.quorum.on("removeMember", (removedClientId: string) => {
+            if (this.leader === undefined || removedClientId === this.leader) {
+                this.leader = undefined;
+                this.quorum.propose("leader", this.clientId).then(() => {
+                    // console.log(`Proposal accepted: ${this.clientId}!`);
+                }, (err) => {
+                    // console.log(`Error proposing new leadership: ${err}`);
+                });
+            }
+        });
 
-    private async proposeLeadership() {
-        return this.quorum.propose("leader", this.clientId);
-    }
-
-    private handleRejection(sequenceNumber: number, key: string, value: any) {
-        console.log(`Proposal rejected @${sequenceNumber}. ${key}:${value}`);
+        this.quorum.on("rejectProposal", (sequenceNumber: number, key: string, value: any) => {
+            // console.log(`Proposal rejected @${sequenceNumber}. ${key}:${value}`);
+        });
     }
 }
