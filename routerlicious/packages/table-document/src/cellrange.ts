dfffc73aa0ab3e400bfea15977a997cd6ccb3f9f
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2019-02-04 10:02:48 -0800

    Table-Slice: WIP

---
+++ routerlicious/packages/table-document/src/cellrange.ts
@@ -0,0 +1,37 @@
+import { LocalReference } from "@prague/merge-tree";
+import { SharedStringInterval } from "@prague/sequence";
+
+export class CellRange {
+    constructor (
+        private readonly interval: SharedStringInterval,
+        private readonly resolve: (localRef: LocalReference) => { row: number, col: number }) {}
+
+    public getPositions() {
+        const { start, end } = this.interval;
+        return { start: this.resolve(start), end: this.resolve(end) };
+    }
+
+    public forEachRowMajor(callback: (row: number, col: number) => boolean) {
+        const {start, end} = this.getPositions();
+        
+        for (let row = start.row; row < end.row; row++) {
+            for (let col = start.col; col < end.col; col++) {
+                if (!callback(row, col)) {
+                    return;
+                }
+            }
+        }
+    }
+
+    public forEachColMajor(callback: (row: number, col: number) => boolean) {
+        const {start, end} = this.getPositions();
+        
+        for (let col = start.col; col < end.col; col++) {
+            for (let row = start.row; row < end.row; row++) {
+                if (!callback(row, col)) {
+                    return;
+                }
+            }
+        }
+    }
+}
--- routerlicious/packages/table-document/src/index.ts
+++ routerlicious/packages/table-document/src/index.ts
@@ -18,8 +18,13 @@ import {
     MergeTree,
     UniversalSequenceNumber,
     ReferenceType,
-    Marker
+    Marker,
+    IntervalType,
+    LocalReference
 } from "@prague/merge-tree";
+
+import { CellRange } from "./cellRange";
+export { CellRange };
 
 const loadCellTextSym = Symbol("TableDocument.loadCellText");
 const storeCellTextSym = Symbol("TableDocument.storeCellText");
@@ -110,13 +115,19 @@ export class TableDocument extends Component {
 
             this.emit("op", op, local)
         });
-        
+
         this.maybeWorkbook = new WorkbookAdapter(this);
     }
 
-    private get length() { return this.mergeTree.getLength(UniversalSequenceNumber, this.clientId); }
-    public get numCols() { return Math.min(this.rootView.get("stride").value, this.length); }
-    public get numRows() { return Math.floor(this.length / this.numCols); }
+    private localRefToPosition(localRef: LocalReference) {
+        return localRef.toPosition(this.mergeTree, UniversalSequenceNumber, this.clientId);
+    }
+
+    private readonly localRefToRowCol = (localRef: LocalReference) => this.positionToRowCol(this.localRefToPosition(localRef));
+
+    private get length()     { return this.mergeTree.getLength(UniversalSequenceNumber, this.clientId); }
+    public  get numCols()    { return Math.min(this.rootView.get("stride").value, this.length); }
+    public  get numRows()    { return Math.floor(this.length / this.numCols); }
 
     private parseResult(result: EvaluationResult): string | number | boolean {
         switch (result.kind) {
@@ -149,6 +160,12 @@ export class TableDocument extends Component {
         return row * this.numCols + col;
     }
 
+    private positionToRowCol(position: number) {
+        const row = Math.floor(position / this.numCols);
+        const col = position - (row * this.numCols);
+        return {row, col};
+    }
+
     private [loadCellTextSym](row: number, col: number): string {
         const { segment } = this.mergeTree.getContainingSegment(this.rowColToPosition(row, col), UniversalSequenceNumber, this.clientId);
         return (segment as Marker).properties["value"];
@@ -169,11 +186,24 @@ export class TableDocument extends Component {
         this.sharedString.insertMarker(position, ReferenceType.Simple, { value: value.toString() });
     }
 
-    private get sharedString() { return this.maybeSharedString!; }
-    private get mergeTree() { return this.maybeMergeTree!; }
-    private get clientId() { return this.maybeClientId!; }
-    private get workbook() { return this.maybeWorkbook!; }
-    private get rootView() { return this.maybeRootView!; }
+    public createRange(label: string, minRow: number, minCol: number, maxRow: number, maxCol: number) {
+        const start = this.rowColToPosition(minRow, minCol);
+        const end = this.rowColToPosition(maxRow, maxCol);
+        const intervals = this.sharedString.getSharedIntervalCollection(label);
+        intervals.add(start, end, IntervalType.Simple);
+    }
+
+    public async getRange(label: string) {
+        const intervals = this.sharedString.getSharedIntervalCollection(label);
+        const interval = (await intervals.getView()).nextInterval(0);
+        return new CellRange(interval, this.localRefToRowCol);
+    }
+
+    private get sharedString()  { return this.maybeSharedString!; }
+    private get mergeTree()     { return this.maybeMergeTree!; }
+    private get clientId()      { return this.maybeClientId!; }
+    private get workbook()      { return this.maybeWorkbook!; }
+    private get rootView()      { return this.maybeRootView!; }
 
     public static readonly type = `${require("../package.json").name}@${require("../package.json").version}`;
 }
