4c793e44e52bf30a4eab7f7fa29f9995789009c5
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2019-02-05 11:38:37 -0800

    Components: Stop auto-rev'ing package version in dev builds

--- routerlicious/packages/flow-host/webpack.dev.js
+++ routerlicious/packages/flow-host/webpack.dev.js
@@ -1,7 +1,4 @@
 module.exports = {
     mode: "development",
     devtool: "inline-source-map",
-    devServer: {
-        contentBase: "./dist"
-    }
 };
--- routerlicious/packages/table-view/webpack.dev.js
+++ routerlicious/packages/table-view/webpack.dev.js
@@ -1,31 +1,4 @@
-const spawn = require("child_process").spawn;
-const webpack = require("webpack");
-
-const runCmd = (cmd, args) => new Promise(resolve => {
-    spawn(cmd, args, { stdio: [process.stdin, process.stdout, process.stderr] }).on('close', resolve);
-});
-
 module.exports = {
     mode: "development",
     devtool: "inline-source-map",
-    plugins: [
-        {
-            apply: (compiler) => {
-                compiler.hooks.watchRun.tapPromise("VersionChaincodePlugin", () => runCmd("npm", ["version", "patch"])),
-                compiler.hooks.afterEmit.tapPromise("PublishChaincodePlugin",
-                    (compilation) => {
-                        if (compilation.errors.length > 0) {
-                            console.warn(`Skipping @chaincode publication due to compilation errors.`);
-                            return Promise.resolve();
-                        }
-                        
-                        return runCmd("npm", ["run", "publish-local"]);
-                    }
-                );
-            }
-        },
-        // Ensure that automatically versioning package.json (above) does not cause '--watch' to build
-        // in an infinite loop.
-        new webpack.WatchIgnorePlugin([/\bpackage.json\b/])
-    ]
 };
