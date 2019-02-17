64566e07a0ffd752c64c882ef222f1cffa2818ff
Sam Broner <SamBroner@users.noreply.github.com>
2019-02-16 16:13:40 -0800

    Fix counter, trim chat (#1545)
    * fix counter
    
    * fix chat

--- chaincode/chat/src/index.tsx
+++ chaincode/chat/src/index.tsx
@@ -3,15 +3,9 @@ import { ChatApp } from "./chat-app";
 import { 
   IContainerContext, 
   IRuntime,
-  ITree,
-  IPlatform
 } from "@prague/container-definitions";
-import { ComponentHost } from "@prague/component";
 import {
-  IChaincode,
-  IChaincodeComponent,
-  IComponentDeltaHandler,
-  IComponentRuntime } from "@prague/runtime-definitions";
+  IChaincodeComponent } from "@prague/runtime-definitions";
 
 // Example chainloader bootstrap.
 export async function instantiateComponent(): Promise<IChaincodeComponent> {
@@ -24,43 +18,4 @@ export async function instantiateRuntime(context: IContainerContext): Promise<IR
   return Component.instantiateRuntime(context, "name", "@chaincode/chat", [
     ["@chaincode/chat", Promise.resolve({ instantiateComponent })]
   ]);
-}
-
-export class ChatAppComponent implements IChaincodeComponent {
-  private chat: ChatApp;
-  private chaincode: IChaincode;
-  private component: ComponentHost;
-
-  constructor() {
-      this.chat = new ChatApp();
-      this.chaincode = Component.instantiate(this.chat);
-  }
-
-  public getModule(type: string) {
-      return null;
-  }
-
-  public async close(): Promise<void> {
-      return;
-  }
-
-  public async run(runtime: IComponentRuntime): Promise<IComponentDeltaHandler> {
-      const chaincode = this.chaincode;
-
-      // All of the below would be hidden from a developer
-      // Is this an await or does it just go?
-      const component = await ComponentHost.LoadFromSnapshot(runtime, chaincode);
-      this.component = component;
-
-      return component;
-  }
-
-  public async attach(platform: IPlatform): Promise<IPlatform> {
-      return this.chat.attach(platform);
-  }
-
-  public snapshot(): ITree {
-      const entries = this.component.snapshotInternal();
-      return { entries };
-  }
 }
--- chaincode/counter/src/index.ts
+++ chaincode/counter/src/index.ts
@@ -1,42 +1,68 @@
-import { IChaincode } from "@prague/runtime-definitions";
-import { Component, Document } from "@prague/app-component";
+import { Component } from "@prague/app-component";
+import {
+  IContainerContext,
+  IPlatform,
+  IRuntime
+} from "@prague/container-definitions";
+import { IChaincodeComponent } from "@prague/runtime-definitions";
+import { Document } from "@prague/app-component";
 import { Counter, CounterValueType } from "@prague/map";
+import { Deferred } from "@prague/utils";
 
 export class Clicker extends Document {
-    // Initialize the document/component (only called when document is initially created).
-    protected async create() {
-        this.root.set<Counter>("clicks", 0, CounterValueType.Name);
+  private ready = new Deferred<void>();
+
+  // Initialize the document/component (only called when document is initially created).
+  protected async create() {
+    this.root.set<Counter>("clicks", 0, CounterValueType.Name);
+  }
+
+  // Once document/component is opened, finish any remaining initialization required before the
+  // document/component is returned to to the host.
+  public async opened() {
+    await this.ready.resolve();
+  }
+
+  // Once document/component is opened, finish any remaining initialization required before the
+  // document/component is returned to to the host.
+  public async attach(platform: IPlatform): Promise<IPlatform> {
+    await this.ready.promise;
+
+    // If the host provided a <div>, display a minimual UI.
+    const maybeDiv = await platform.queryInterface<HTMLElement>("div");
+    if (maybeDiv) {
+      const counter = await this.root.wait<Counter>("clicks");
+
+      // Create a <span> that displays the current value of 'clicks'.
+      const span = document.createElement("span");
+      const update = () => {
+        span.textContent = counter.value.toString();
+      };
+      this.root.on("valueChanged", update);
+      update();
+
+      // Create a button that increments the value of 'clicks' when pressed.
+      const btn = document.createElement("button");
+      btn.textContent = "+";
+      btn.addEventListener("click", () => {
+        counter.increment(1);
+      });
+
+      // Add both to the <div> provided by the host:
+      maybeDiv.appendChild(span);
+      maybeDiv.appendChild(btn);
+    } else {
+      return;
     }
-
-    // Once document/component is opened, finish any remaining initialization required before the
-    // document/component is returned to to the host.
-    public async opened() {
-        // If the host provided a <div>, display a minimual UI.
-        const maybeDiv = await this.platform.queryInterface<HTMLElement>("div");        
-        if (maybeDiv) {
-            const counter = await this.root.wait<Counter>("clicks");
-
-            // Create a <span> that displays the current value of 'clicks'.
-            const span = document.createElement("span");           
-            const update = () => { span.textContent = counter.value.toString(); }
-            this.root.on("valueChanged", update);
-            update();
-            
-            // Create a button that increments the value of 'clicks' when pressed.
-            const btn = document.createElement("button");
-            btn.textContent = "+";
-            btn.addEventListener("click", () => {
-                counter.increment(1);
-            });
-
-            // Add both to the <div> provided by the host:
-            maybeDiv.appendChild(span);
-            maybeDiv.appendChild(btn);
-        }
-    }
+  }
 }
 
-// Example chainloader bootstrap.
-export async function instantiate(): Promise<IChaincode> {
-    return Component.instantiate(new Clicker());
+export async function instantiateComponent(): Promise<IChaincodeComponent> {
+  return Component.instantiateComponent(Clicker);
+}
+
+export async function instantiateRuntime( context: IContainerContext ): Promise<IRuntime> {
+  return Component.instantiateRuntime(context, "name", "@chaincode/counter", [
+    ["@chaincode/counter", Promise.resolve({ instantiateComponent })]
+  ]);
 }
