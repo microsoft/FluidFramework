0c7cfa949a19d7c02ffaf92c4f5cb2fbc1178383
kurtb <kurtberglund@hotmail.com>
2019-02-19 19:28:41 -0800

    Update shared text package to pull in monaco, chart and pinpoint packages (#1573)

--- chaincode/shared-text/webpack.config.js
+++ chaincode/shared-text/webpack.config.js
@@ -11,7 +11,14 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: [{
+                    loader:'ts-loader',
+                    options: {
+                        compilerOptions: {
+                            module: "esnext"
+                        },
+                    }
+                }],
                 exclude: /node_modules/
             },
             {
