b7bcb3761d2f2b29d3e7c34abb506e08f5ee04b3
kurtb <kurtberglund@hotmail.com>
2019-02-22 10:20:06 -0800

    Update monaco and pinpoint-editor to also operate as containers (#1589)

---
+++ chaincode/containers/pinpoint-editor/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+// var Visualizer = require('webpack-visualizer-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'source-map',
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [{
+                    loader:'ts-loader',
+                    options: {
+                        compilerOptions: {
+                            module: "esnext"
+                        },
+                    }
+                }],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.js$/,
+                use: ["source-map-loader"],
+                enforce: "pre"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].async.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/dist/",
+        library: "[name]",
+        libraryTarget: "umd"
+    },
+    plugins: [
+        // new MonacoWebpackPlugin()
+        // new BundleAnalyzerPlugin()
+    ]
+};
