5aa856c17bb003db56cfe40ee63988755a5127bb
Tanvir Aumi <mdaumi@microsoft.com>
2019-02-21 19:32:39 -0800

    Use package for chaincode work.

--- routerlicious/packages/server/agent/src/agent/chaincodes/intelWork.ts
+++ routerlicious/packages/server/agent/src/agent/chaincodes/intelWork.ts
@@ -1,66 +1,26 @@
-import { ISharedObject } from "@prague/api-definitions";
 import {
     ICodeLoader,
     IDocumentService,
-    IPlatformFactory,
-    ISequencedDocumentMessage,
     ITokenProvider,
-    MessageType,
 } from "@prague/container-definitions";
-import { Container } from "@prague/container-loader";
-import { ISharedMap } from "@prague/map";
-import { textAnalytics } from "../../intelligence";
 import { IWork} from "../definitions";
 import { ChaincodeWork } from "./chaincodeWork";
-import { IntelligentServicesManager } from "./intelligence";
 
 export class IntelWork extends ChaincodeWork implements IWork {
-
-    private intelligenceManager: IntelligentServicesManager;
-
     constructor(
         docId: string,
         tenantId: string,
         tokenProvider: ITokenProvider,
         service: IDocumentService,
-        codeLoader: ICodeLoader,
-        platformFactory: IPlatformFactory,
-        task: string,
-        private config: any) {
-        super(docId, tenantId, tokenProvider, service, codeLoader, platformFactory, task);
+        codeLoader: ICodeLoader) {
+        super(docId, tenantId, tokenProvider, service, codeLoader);
     }
 
     public async start(): Promise<void> {
-        await this.loadChaincode(
-            { localMinSeq: 0, encrypted: undefined, client: { type: "intel" } });
-        const insightsMap = await this.document.runtime.getChannel("insights") as ISharedMap;
-        this.processIntelligenceWork(this.document, insightsMap);
+        return this.loadChaincode({ localMinSeq: 0, encrypted: undefined, client: { type: "intel" } });
     }
 
     public async stop(): Promise<void> {
-        if (this.intelligenceManager) {
-            await this.intelligenceManager.stop();
-        }
-        await super.stop();
-    }
-
-    public registerNewService(service: any) {
-        this.intelligenceManager.registerService(service.factory.create(this.config.intelligence.resume));
-    }
-
-    private processIntelligenceWork(doc: Container, insightsMap: ISharedMap) {
-        this.intelligenceManager = new IntelligentServicesManager(doc, insightsMap);
-        this.intelligenceManager.registerService(textAnalytics.factory.create(this.config.intelligence.textAnalytics));
-
-        this.document.on("op", (op: ISequencedDocumentMessage, object?: ISharedObject) => {
-            // TODO include object as part of op
-            if (object) {
-                if (op.type === MessageType.Operation) {
-                    this.intelligenceManager.process(object);
-                } else if (op.type === MessageType.Attach) {
-                    this.intelligenceManager.process(object);
-                }
-            }
-        });
+        return super.stop();
     }
 }
--- routerlicious/packages/server/agent/src/agent/chaincodes/spellcheckerWork.ts
+++ routerlicious/packages/server/agent/src/agent/chaincodes/spellcheckerWork.ts
@@ -1,80 +1,32 @@
-import * as core from "@prague/api-definitions";
 import {
     ICodeLoader,
     IDocumentService,
-    IPlatformFactory,
-    ISequencedDocumentMessage,
     ITokenProvider,
-    MessageType,
 } from "@prague/container-definitions";
-import * as MergeTree from "@prague/merge-tree";
-import * as Sequence from "@prague/sequence";
 import { IWork} from "../definitions";
-import { Spellcheker } from "../spellchecker";
 import { ChaincodeWork } from "./chaincodeWork";
 
 export class SpellcheckerWork extends ChaincodeWork implements IWork {
-
-    private dict = new MergeTree.TST<number>();
-    private spellchecker: Spellcheker;
-
     constructor(
         docId: string,
         tenantId: string,
         tokenProvider: ITokenProvider,
         service: IDocumentService,
-        codeLoader: ICodeLoader,
-        platformFactory: IPlatformFactory,
-        task: string,
-        dictionary: MergeTree.TST<number>) {
-
-        super(docId, tenantId, tokenProvider, service, codeLoader, platformFactory, task);
-        this.dict = dictionary;
+        codeLoader: ICodeLoader) {
+        super(docId, tenantId, tokenProvider, service, codeLoader);
     }
 
     public async start(): Promise<void> {
-        await this.loadChaincode(
+        return this.loadChaincode(
             {
                 blockUpdateMarkers: true,
                 client: { type: "spell"},
                 encrypted: undefined,
                 localMinSeq: 0,
             });
-        // TODO emit target as part of op event
-        const eventHandler = (op: ISequencedDocumentMessage, object?: core.ISharedObject) => {
-            if (object) {
-                if (op.type === MessageType.Operation || op.type === MessageType.Attach) {
-                    this.spellCheck(object);
-                }
-            }
-        };
-
-        // Temporary workaround: Currently annotations are not being added to the pending op list when the document
-        // is not connected. Making sure that the document is fully connected before starting the spellchecker.
-        if (this.document.connected) {
-            this.document.on("op", eventHandler);
-        } else {
-            console.log(`Waiting for the document to fully connected before running spellcheck!`);
-            this.document.on("connected", () => {
-                this.document.on("op", eventHandler);
-            });
-        }
     }
 
     public async stop(): Promise<void> {
-        if (this.spellchecker) {
-            this.spellchecker.stop();
-        }
-        await super.stop();
-    }
-
-    // Enable spell checking for the document
-    // TODO will want to configure this as a pluggable insight
-    private spellCheck(object: core.ISharedObject) {
-        if (object.type === Sequence.SharedStringExtension.Type && !this.spellchecker) {
-            const sharedString = object as Sequence.SharedString;
-            this.spellchecker = new Spellcheker(sharedString, this.dict);
-            this.spellchecker.run();
-        }
+        return super.stop();
     }
 }
