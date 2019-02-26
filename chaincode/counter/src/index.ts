6051410db544f846cd87a271d9d4ded6e8b4e5c3
Sam Broner <SamBroner@users.noreply.github.com>
2019-02-25 21:17:39 -0800

    Clean Counter for Yo

--- chaincode/counter/src/index.ts
+++ chaincode/counter/src/index.ts
@@ -1,68 +1,52 @@
-import { Component } from "@prague/app-component";
-import {
-  IContainerContext,
-  IPlatform,
-  IRuntime
-} from "@prague/container-definitions";
-import { IChaincodeComponent } from "@prague/runtime-definitions";
-import { Document } from "@prague/app-component";
+import { Component, Document } from "@prague/app-component";
+import { IContainerContext, IRuntime } from "@prague/container-definitions";
 import { Counter, CounterValueType } from "@prague/map";
-import { Deferred } from "@prague/utils";
 
 export class Clicker extends Document {
-  private ready = new Deferred<void>();
 
-  // Initialize the document/component (only called when document is initially created).
+  // Create the component's schema and perform other initialization tasks
+  // (only called when document is initially created).
   protected async create() {
-    this.root.set<Counter>("clicks", 0, CounterValueType.Name);
+    this.root.set("clicks", 0, CounterValueType.Name);
   }
 
-  // Once document/component is opened, finish any remaining initialization required before the
-  // document/component is returned to to the host.
+  protected async render(host: HTMLDivElement) {
+    const counter = await this.root.wait<Counter>("clicks");
+
+    // Create a <span> that displays the current value of 'clicks'.
+    const span = document.createElement("span");
+    const update = () => {
+      span.textContent = counter.value.toString();
+    };
+    this.root.on("valueChanged", update);
+    update();
+
+    // Create a button that increments the value of 'clicks' when pressed.
+    const btn = document.createElement("button");
+    btn.textContent = "+";
+    btn.addEventListener("click", () => {
+      counter.increment(1);
+    });
+
+    // Add both to the <div> provided by the host:
+    host.appendChild(span);
+    host.appendChild(btn);
+  }
+
+  // The component has been loaded. Attempt to get a div from the host. TODO explain this better.
   public async opened() {
-    await this.ready.resolve();
-  }
-
-  // Once document/component is opened, finish any remaining initialization required before the
-  // document/component is returned to to the host.
-  public async attach(platform: IPlatform): Promise<IPlatform> {
-    await this.ready.promise;
-
-    // If the host provided a <div>, display a minimual UI.
-    const maybeDiv = await platform.queryInterface<HTMLElement>("div");
+    // If the host provided a <div>, render the component into that Div
+    const maybeDiv = await this.platform.queryInterface<HTMLDivElement>("div");
     if (maybeDiv) {
-      const counter = await this.root.wait<Counter>("clicks");
-
-      // Create a <span> that displays the current value of 'clicks'.
-      const span = document.createElement("span");
-      const update = () => {
-        span.textContent = counter.value.toString();
-      };
-      this.root.on("valueChanged", update);
-      update();
-
-      // Create a button that increments the value of 'clicks' when pressed.
-      const btn = document.createElement("button");
-      btn.textContent = "+";
-      btn.addEventListener("click", () => {
-        counter.increment(1);
-      });
-
-      // Add both to the <div> provided by the host:
-      maybeDiv.appendChild(span);
-      maybeDiv.appendChild(btn);
+      this.render(maybeDiv);
     } else {
       return;
     }
   }
 }
 
-export async function instantiateComponent(): Promise<IChaincodeComponent> {
-  return Component.instantiateComponent(Clicker);
-}
-
 export async function instantiateRuntime( context: IContainerContext ): Promise<IRuntime> {
-  return Component.instantiateRuntime(context, "name", "@chaincode/counter", [
-    ["@chaincode/counter", Promise.resolve({ instantiateComponent })]
+  return Component.instantiateRuntime(context, "@chaincode/counter", [
+    ["@chaincode/counter", Clicker]
   ]);
 }
