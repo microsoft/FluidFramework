1c13f21bae3cd5091e54dfd57665b6d1dd350df6
Tanvir Aumi <mdaumi@microsoft.com>
2019-02-27 11:12:52 -0800

    Timed redis caching.

--- headless-agent/src/runnerFactory.ts
+++ headless-agent/src/runnerFactory.ts
@@ -4,10 +4,15 @@ import * as services from "@prague/services";
 import * as core from "@prague/services-core";
 import * as utils from "@prague/services-utils";
 import { Provider } from "nconf";
+import * as redis from "redis";
+import { ICache, RedisCache } from "./redisCache";
 import { HeadlessRunner } from "./runner";
 
 export class HeadlessResources implements utils.IResources {
-    constructor(public workerConfig: any, public messageReceiver: core.ITaskMessageReceiver) {
+    constructor(
+        public workerConfig: any,
+        public messageReceiver: core.ITaskMessageReceiver,
+        public cache: ICache) {
     }
 
     public async dispose(): Promise<void> {
@@ -18,12 +23,15 @@ export class HeadlessResources implements utils.IResources {
 export class HeadlessResourcesFactory implements utils.IResourcesFactory<HeadlessResources> {
     public async create(config: Provider): Promise<HeadlessResources> {
         const rabbitmqConfig = config.get("rabbitmq");
+        const redisConfig = config.get("redis");
         const workerConfig = config.get("worker");
         const queueName = config.get("headless-agent:queue");
 
+        const redisClient = redis.createClient(redisConfig.port, redisConfig.host);
+        const cache = new RedisCache(redisClient);
         const messageReceiver = services.createMessageReceiver(rabbitmqConfig, queueName);
 
-        return new HeadlessResources(workerConfig, messageReceiver);
+        return new HeadlessResources(workerConfig, messageReceiver, cache);
     }
 }
 
@@ -31,6 +39,7 @@ export class HeadlessRunnerFactory implements utils.IRunnerFactory<HeadlessResou
     public async create(resources: HeadlessResources): Promise<utils.IRunner> {
         return new HeadlessRunner(
             resources.workerConfig,
-            resources.messageReceiver);
+            resources.messageReceiver,
+            resources.cache);
     }
 }
