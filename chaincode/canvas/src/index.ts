90653cdf39fb739703c12dd82f508c0df7743a60
kurtb <kurtberglund@hotmail.com>
2019-02-28 13:41:01 -0800

    Migrate legacy canvas page to a component (#1661)

---
+++ chaincode/canvas/src/index.ts
@@ -0,0 +1,40 @@
+import { Component, Document } from "@prague/app-component";
+import * as api from "@prague/client-api";
+import { controls, ui } from "@prague/client-ui";
+import { IContainerContext, IRuntime } from "@prague/container-definitions";
+
+import "./style.less";
+
+export class canvas extends Document {
+  // Create the component's schema and perform other initialization tasks
+  // (only called when document is initially created).
+  protected async create() {
+    this.root.set("ink", this.createStream());
+  }
+
+  protected async render(host: HTMLDivElement) {
+    const browserHost = new ui.BrowserContainerHost();
+
+    await this.root.wait("ink");
+
+    const canvas = new controls.FlexView(host, new api.Document(this.runtime, this.root), this.root);
+    browserHost.attach(canvas);
+  }
+
+  // The component has been loaded. Attempt to get a div from the host. TODO explain this better.
+  public async opened() {
+    // If the host provided a <div>, render the component into that Div
+    const maybeDiv = await this.platform.queryInterface<HTMLDivElement>("div");
+    if (maybeDiv) {
+      this.render(maybeDiv);
+    } else {
+      return;
+    }
+  }
+}
+
+export async function instantiateRuntime(context: IContainerContext): Promise<IRuntime> {
+  return Component.instantiateRuntime(context, "@chaincode/canvas", [
+    ["@chaincode/canvas", canvas]
+  ]);
+}
---
+++ chaincode/canvas/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const merge = require("webpack-merge");
+
+module.exports = env => {
+    const isProduction = env === "production";
+
+    return merge({
+        entry: {
+            main: "./src/index.ts"
+        },
+        node: {
+            dgram: 'empty',
+            fs: 'empty',
+            net: 'empty',
+            tls: 'empty',
+            child_process: 'empty',
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+        },
+        module: {
+            rules: [{ 
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: 'style-loader' // creates style nodes from JS strings
+                }, {
+                    loader: 'css-loader' // translates CSS into CommonJS
+                }, {
+                    loader: 'less-loader' // compiles Less to CSS
+                }]
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000
+                }
+            }]
+        },
+        output: {
+            filename: "[name].bundle.js",
+            path: path.resolve(__dirname, "dist"),
+            library: "[name]",
+            // https://github.com/webpack/webpack/issues/5767
+            // https://github.com/webpack/webpack/issues/7939            
+            devtoolNamespace: "chaincode/canvas",
+            libraryTarget: "umd"
+        },
+        devServer: {
+            publicPath: '/dist'
+        }
+    }, isProduction
+        ? require("./webpack.prod")
+        : require("./webpack.dev"));
+};
