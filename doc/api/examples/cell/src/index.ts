5c283a64d5bb85c6f48e41136bef8ad802d9861d
Tanvir Aumi <tanvir@cs.washington.edu>
2019-03-06 17:08:41 -0800

    Added cell experiment. (#1722)

---
+++ doc/api/examples/cell/src/index.ts
@@ -0,0 +1,80 @@
+import { ICell } from "@prague/cell";
+import * as API from "@prague/client-api";
+import { ISharedMap, IValueChanged } from "@prague/map";
+import * as socketStorage from "@prague/routerlicious-socket-storage";
+import { InsecureUrlResolver } from "./urlResolver";
+
+// Using package verions published in 03-04-2019
+// For local development
+// const routerlicious = "http://localhost:3000";
+// const historian = "http://localhost:3001";
+// const tenantId = "prague";
+// const secret = "43cfc3fbf04a97c0921fd23ff10f9e4b";
+// const routerlicious = "https://alfred.wu2.prague.office-int.com";
+// const historian = "https://historian.wu2.prague.office-int.com";
+// const tenantId = "gallant-hugle";
+// const secret = "03302d4ebfb6f44b662d00313aff5a46";
+const routerlicious = "https://alfred.wu2-ppe.prague.office-int.com";
+// Use undefined for only subscribing to the ordering service.
+const historian = undefined;
+const tenantId = "stupefied-kilby";
+const secret = "4a9211594f7c3daebca3deb8d6115fe2";
+
+const userId = "test";
+
+const documentId = window.location.search.slice(1) || "cell-test-03062019-02";
+
+// Register endpoint connection
+const documentServices = socketStorage.createDocumentService(routerlicious, historian);
+API.registerDocumentService(documentServices);
+
+async function run(id: string): Promise<void> {
+    const resolver = new InsecureUrlResolver(
+        routerlicious,
+        historian,
+        userId,
+        secret);
+
+    const documentUrl = `prague://${new URL(routerlicious).host}` +
+        `/${encodeURIComponent(tenantId)}` +
+        `/${encodeURIComponent(documentId)}`;
+    const apiHost = { resolver };
+
+    const collabDoc = await API.load(
+        documentUrl,
+        apiHost,
+        { blockUpdateMarkers: true });
+
+    const rootMap = await collabDoc.getRoot();
+
+    if (!collabDoc.existing) {
+        rootMap.set( "c1", collabDoc.createCell());
+        rootMap.set( "m1", collabDoc.createMap());
+    } else {
+        await Promise.all([rootMap.wait("c1"), rootMap.wait("m1")]);
+    }
+
+    const cellEl: ICell = rootMap.get("c1");
+    const mapEl: ISharedMap = rootMap.get("m1");
+
+    if (!collabDoc.existing) {
+        setInterval(async () => {
+            // tslint:disable-next-line:insecure-random
+            await cellEl.set(Math.floor(Math.random() * 100000).toString());
+            // tslint:disable-next-line:insecure-random
+            mapEl.set("foo", Math.floor(Math.random() * 100000).toString());
+        }, 5000);
+    }
+
+    cellEl.on("valueChanged", (val) => {
+        console.log(`New cell value: ${val}`);
+    });
+
+    mapEl.on("valueChanged", (mapKey: IValueChanged) => {
+        console.log(`New map value: ${mapEl.get(mapKey.key)}`);
+    });
+}
+
+run(documentId).catch((error) => {
+    console.error(error);
+});
---
+++ doc/api/examples/cell/src/urlResolver.ts
@@ -0,0 +1,52 @@
+import {
+    IPragueResolvedUrl,
+    IRequest,
+    IResolvedUrl,
+    ITokenClaims,
+    IUrlResolver,
+} from "@prague/container-definitions";
+import * as jwt from "jsonwebtoken";
+
+export class InsecureUrlResolver implements IUrlResolver {
+
+    constructor(
+        private readonly ordererUrl: string,
+        private readonly storageUrl: string,
+        private readonly user: string,
+        private readonly key: string,
+    ) { }
+
+    public async resolve(request: IRequest): Promise<IResolvedUrl> {
+        console.log(`resolving url=${JSON.stringify(request)}`);
+
+        // tslint:disable-next-line:no-http-string - Replacing protocol so URL will parse.
+        const parsedUrl = new URL(request.url.replace(/^prague:\/\//, "http://"));
+        const [tenantId, documentId] = parsedUrl.pathname.substr(1).split("/");
+
+        const documentUrl = `prague://${new URL(this.ordererUrl).host}` +
+            `/${encodeURIComponent(tenantId)}` +
+            `/${encodeURIComponent(documentId)}`;
+
+        // tslint:disable-next-line:no-unnecessary-local-variable
+        const response: IPragueResolvedUrl = {
+            ordererUrl: this.ordererUrl,
+            storageUrl: this.storageUrl,
+            tokens: { jwt: this.auth(tenantId, documentId) },
+            type: "prague",
+            url: documentUrl,
+        };
+
+        return response;
+    }
+
+    private auth(tenantId: string, documentId: string) {
+        const claims: ITokenClaims = {
+            documentId,
+            permission: "read:write",
+            tenantId,
+            user: { id: this.user },
+        };
+
+        return jwt.sign(claims, this.key);
+    }
+}
