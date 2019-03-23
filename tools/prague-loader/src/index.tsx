ab6e04bc3f6b58147d44f19e788149660d167708
Sam Broner <SamBroner@users.noreply.github.com>
2019-03-23 12:39:33 -0700

    React loader package (#1861)
    * remove json
    
    * finish up prague loader
    
    * clean up loader

--- tools/prague-loader/src/index.tsx
+++ tools/prague-loader/src/index.tsx
@@ -1,19 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App, { IContainerLoaderProps } from './App';
+import { Component } from "react";
+import * as React from "react";
+import { DataStore } from "@prague/app-datastore";
+import { createDocumentService } from "@prague/routerlicious-socket-storage";
+import { WebLoader } from "@prague/loader-web";
+import * as UrlParse from "url-parse";
 
-const props: IContainerLoaderProps = {
-    containerId: "ChangeThisValue-155aa3036248361",
-    ordererUrl: "https://alfred.wu2-ppe.prague.office-int.com",
-    storageUrl: "https://historian.wu2-ppe.prague.office-int.com",
-    registryUrl: "https://pragueauspkn-3873244262.azureedge.net",
-    div: document.getElementById('root') as HTMLDivElement,
-};
+export interface ILoaderProps {
+  containerId: string; // ID of the container
+  ordererUrl: string; // "Alfred" "localhost:3000" "alfred.wu2..."
+  storageUrl: string; // "Historian" "localhost:3003" "historian.wu2..."
+  // (registryUrl is a different endpoint than verdaccio)
+  registryUrl: string; // "Auspkn" "localhost:3002" https://pragueauspkn-3873244262.azureedge.net"
+}
 
-ReactDOM.render(<App
-    containerId={props.containerId}
-    ordererUrl={props.ordererUrl}
-    storageUrl={props.storageUrl}
-    registryUrl={props.registryUrl}
-    div={props.div}
-    />, document.getElementById('root'));
+export interface ILoaderUrl {
+  url: string;
+}
+
+export function URLToLoaderProps(
+  urlString: string
+): ILoaderProps {
+  const url = UrlParse(urlString);
+  const pathParts = url.pathname.split("/");
+  const container = pathParts[3];
+  const propsWithoutDiv = {
+    containerId: container,
+    ordererUrl: "https://" + url.host.replace("www", "alfred"),
+    storageUrl: "https://" + url.host.replace("www", "historian"),
+    registryUrl: "https://pragueauspkn-3873244262.azureedge.net"
+  };
+  return propsWithoutDiv;
+}
+
+export class Loader extends Component<ILoaderUrl, any> {
+  dataStore: DataStore;
+  bag: ILoaderProps;
+  divRef: React.RefObject<HTMLDivElement>;
+  constructor(props: ILoaderUrl) {
+    super(props);
+    this.bag = URLToLoaderProps(this.props.url);
+    this.divRef = React.createRef();
+    const tenantSecret = "4a9211594f7c3daebca3deb8d6115fe2";
+    const tenantId = "stupefied-kilby";
+
+    this.dataStore = new DataStore(
+      this.bag.ordererUrl,
+      this.bag.storageUrl,
+      new WebLoader(this.bag.registryUrl),
+      createDocumentService(this.bag.ordererUrl, this.bag.storageUrl),
+      tenantSecret,
+      tenantId,
+      "anonymous-coward"
+    );
+  }
+
+  async componentDidMount() {
+    const services: ReadonlyArray<[string, Promise<any>]> = [
+      ["div", Promise.resolve(this.divRef.current)],
+      ["datastore", Promise.resolve(this.dataStore)]
+    ];
+
+    this.dataStore.open(this.bag.containerId, "", "", services);
+  }
+
+  render() {
+    if (!this.dataStore) {
+      return (
+        <>
+          <p>Error: Data Store Not Found</p>
+        </>
+      );
+    }
+    return (
+      <div>
+        <p>Rendering Container {this.bag.containerId} </p>
+        <div ref={this.divRef}/>
+      </div>
+    );
+  }
+}
