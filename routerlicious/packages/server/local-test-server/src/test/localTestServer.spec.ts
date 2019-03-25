7ea14e745b40d6681e61e968af138638147f4a50
vladsud <vladsud@microsoft.com>
2019-03-25 15:57:13 -0700

    Revert "A bunch of test code not to be submitted"
    This reverts commit a24cd877889ba995496ac693f1bf3d762fc3c081.

---
+++ routerlicious/packages/server/local-test-server/src/test/localTestServer.spec.ts
@@ -0,0 +1,106 @@
+import * as api from "@prague/client-api";
+import { MessageType } from "@prague/container-definitions";
+import { SharedString } from "@prague/sequence";
+import * as assert from "assert";
+import {
+  createTestDocumentService,
+  DocumentDeltaEventManager,
+  ITestDeltaConnectionServer,
+  TestDeltaConnectionServer,
+  TestResolver,
+} from "..";
+
+describe("LocalTestServer", () => {
+  const id = "prague://test.com/test/test";
+
+  let testDeltaConnectionServer: ITestDeltaConnectionServer;
+  let documentDeltaEventManager: DocumentDeltaEventManager;
+  let user1Document: api.Document;
+  let user2Document: api.Document;
+  let user1SharedString: SharedString;
+  let user2SharedString: SharedString;
+
+  beforeEach(async () => {
+    testDeltaConnectionServer = TestDeltaConnectionServer.Create();
+    documentDeltaEventManager = new DocumentDeltaEventManager(testDeltaConnectionServer);
+
+    const resolver = new TestResolver();
+    const documentService = createTestDocumentService(testDeltaConnectionServer);
+    user1Document = await api.load(
+      id, { resolver }, {}, documentService);
+    let root = user1Document.getRoot();
+    user1SharedString = user1Document.createString();
+    root.set("SharedString", user1SharedString);
+    documentDeltaEventManager.registerDocuments(user1Document);
+
+    user2Document = await api.load(
+      id, { resolver }, {}, documentService);
+    root = user2Document.getRoot();
+    user2SharedString = await root.wait("SharedString") as SharedString;
+    documentDeltaEventManager.registerDocuments(user2Document);
+  });
+
+  describe("Document.existing", () => {
+    it("Validate document is new for user1 1 and exists for client 2", () => {
+      assert.equal(user1Document.existing, false, "Document already exists");
+      assert.equal(user2Document.existing, true, "Document does not exist on the server");
+      assert.notEqual(user2SharedString, undefined, "Document does not contain a SharedString");
+    });
+  });
+
+  describe("Attach Op Handlers on Both Clients", () => {
+    it("Validate messaging", async () => {
+      let user1ReceivedMsgCount: number = 0;
+      let user2ReceivedMsgCount: number = 0;
+
+      user1SharedString.on("op", (msg, local) => {
+        if (!local) {
+          if (msg.type === MessageType.Operation) {
+            user1ReceivedMsgCount = user1ReceivedMsgCount + 1;
+          }
+        }
+      });
+
+      user2SharedString.on("op", (msg, local) => {
+        if (!local) {
+          if (msg.type === MessageType.Operation) {
+            user2ReceivedMsgCount = user2ReceivedMsgCount + 1;
+          }
+        }
+      });
+
+      documentDeltaEventManager.pauseProcessing();
+
+      user1SharedString.insertText("A", 0);
+      user2SharedString.insertText("C", 0);
+      assert.equal(user1ReceivedMsgCount, 0, "User1 received message count is incorrect");
+      assert.equal(user2ReceivedMsgCount, 0, "User2 received message count is incorrect");
+
+      await documentDeltaEventManager.processOutgoing(user1Document);
+      assert.equal(user1ReceivedMsgCount, 0, "User1 received message count is incorrect");
+      assert.equal(user2ReceivedMsgCount, 0, "User2 received message count is incorrect");
+
+      await documentDeltaEventManager.process(user2Document);
+      assert.equal(user1ReceivedMsgCount, 0, "User1 received message count is incorrect");
+      assert.equal(user2ReceivedMsgCount, 1, "User2 received message count is incorrect");
+
+      await documentDeltaEventManager.processIncoming(user1Document);
+      assert.equal(user1ReceivedMsgCount, 1, "User1 received message count is incorrect");
+      assert.equal(user2ReceivedMsgCount, 1, "User2 received message count is incorrect");
+
+      user1SharedString.insertText("B", 0);
+      await documentDeltaEventManager.process(user1Document, user2Document);
+
+      assert.equal(user1SharedString.getText(), user2SharedString.getText());
+      assert.equal(user1SharedString.getText().length, 3, user1SharedString.getText());
+      assert.equal(user1ReceivedMsgCount, 1, "User1 received message count is incorrect");
+      assert.equal(user2ReceivedMsgCount, 2, "User2 received message count is incorrect");
+    });
+  });
+
+  afterEach(async () => {
+    user1Document.close();
+    user2Document.close();
+    await testDeltaConnectionServer.webSocketServer.close();
+  });
+});
---
+++ routerlicious/packages/server/local-test-server/src/test/mapEndToEndTests.spec.ts
@@ -0,0 +1,236 @@
+/* tslint:disable:no-unsafe-any */
+/* tslint:disable:no-backbone-get-set-outside-model  */
+import * as api from "@prague/client-api";
+import { MessageType } from "@prague/container-definitions";
+import { ISharedMap } from "@prague/map";
+import * as assert from "assert";
+import {
+    createTestDocumentService,
+    DocumentDeltaEventManager,
+    ITestDeltaConnectionServer,
+    TestDeltaConnectionServer,
+    TestResolver,
+} from "..";
+
+describe("Map", () => {
+    const id = "prague://test.com/test/test";
+
+    let testDeltaConnectionServer: ITestDeltaConnectionServer;
+    let documentDeltaEventManager: DocumentDeltaEventManager;
+    let user1Document: api.Document;
+    let user2Document: api.Document;
+    let user3Document: api.Document;
+    let root1: ISharedMap;
+    let root2: ISharedMap;
+    let root3: ISharedMap;
+
+    beforeEach(async () => {
+        testDeltaConnectionServer = TestDeltaConnectionServer.Create();
+        documentDeltaEventManager = new DocumentDeltaEventManager(testDeltaConnectionServer);
+        const documentService = createTestDocumentService(testDeltaConnectionServer);
+        const resolver = new TestResolver();
+        user1Document = await api.load(
+            id, { resolver }, {}, documentService);
+        documentDeltaEventManager.registerDocuments(user1Document);
+
+        user2Document = await api.load(
+            id, { resolver }, {}, documentService);
+        documentDeltaEventManager.registerDocuments(user2Document);
+
+        user3Document = await api.load(
+            id, { resolver }, {}, documentService);
+        documentDeltaEventManager.registerDocuments(user3Document);
+        root1 = user1Document.getRoot();
+        root2 = user2Document.getRoot();
+        root3 = user3Document.getRoot();
+        documentDeltaEventManager.pauseProcessing();
+        root1.set("testKey1", "testValue");
+        await documentDeltaEventManager.process(user1Document, user2Document, user3Document);
+    });
+
+    function expectAllValues(msg, key, value1, value2, value3) {
+        const user1Value = root1.get(key) as string;
+        assert.equal(user1Value, value1, `Incorrect value for ${key} in document 1 ${msg}`);
+        const user2Value = root2.get(key) as string;
+        assert.equal(user2Value, value2, `Incorrect value for ${key} in document 2 ${msg}`);
+        const user3Value = root3.get(key) as string;
+        assert.equal(user3Value, value3, `Incorrect value for ${key} in document 3 ${msg}`);
+    }
+    function expectAllBeforeValues(key, value1, value2, value3) {
+        expectAllValues("before process", key, value1, value2, value3);
+    }
+    function expectAllAfterValues(key, value) {
+        expectAllValues("after process", key, value, value, value);
+    }
+
+    function expectAllSize(size) {
+        const keys1 = Array.from(root1.keys());
+        assert.equal(keys1.length, size, "Incorrect number of Keys in document1");
+        const keys2 = Array.from(root2.keys());
+        assert.equal(keys2.length, size, "Incorrect number of Keys in document2");
+        const keys3 = Array.from(root3.keys());
+        assert.equal(keys3.length, size, "Incorrect number of Keys in document3");
+
+        assert.equal(root1.size, size, "Incorrect map size in document1");
+        assert.equal(root2.size, size, "Incorrect map size in document2");
+        assert.equal(root3.size, size, "Incorrect map size in document3");
+    }
+
+    it("should set key value in three documents correctly", async () => {
+        expectAllAfterValues("testKey1", "testValue");
+    });
+
+    it("Should delete values in 3 documents correctly", async () => {
+        root2.delete("testKey1");
+        await documentDeltaEventManager.process(user1Document, user2Document, user3Document);
+
+        const hasKey1 = root1.has("testKey1");
+        assert.equal(hasKey1, false, "testKey1 not deleted in document 1");
+
+        const hasKey2 = root2.has("testKey1");
+        assert.equal(hasKey2, false, "testKey1 not deleted in document 1");
+
+        const hasKey3 = root3.has("testKey1");
+        assert.equal(hasKey3, false, "testKey1 not deleted in document 1");
+    });
+
+    it("Should check if three documents has same number of keys", async () => {
+        root3.set("testKey3", true);
+        await documentDeltaEventManager.process(user1Document, user2Document, user3Document);
+
+        // check the number of keys in the map (2 keys set  + insights key = 3)
+        expectAllSize(3);
+    });
+
+    it("Should update value and trigger onValueChanged on other two documents", async () => {
+        let user1ValueChangedCount: number = 0;
+        let user2ValueChangedCount: number = 0;
+        let user3ValueChangedCount: number = 0;
+        root1.on("valueChanged", (changed, local, msg) => {
+            if (!local) {
+                if (msg.type === MessageType.Operation) {
+                    assert.equal(changed.key, "testKey1", "Incorrect value for testKey1 in document 1");
+                    user1ValueChangedCount = user1ValueChangedCount + 1;
+                }
+            }
+        });
+        root2.on("valueChanged", (changed, local, msg) => {
+            if (!local) {
+                if (msg.type === MessageType.Operation) {
+                    assert.equal(changed.key, "testKey1", "Incorrect value for testKey1 in document 2");
+                    user2ValueChangedCount = user2ValueChangedCount + 1;
+                }
+            }
+        });
+        root3.on("valueChanged", (changed, local, msg) => {
+            if (!local) {
+                if (msg.type === MessageType.Operation) {
+                    assert.equal(changed.key, "testKey1", "Incorrect value for testKey1 in document 3");
+                    user3ValueChangedCount = user3ValueChangedCount + 1;
+                }
+            }
+        });
+
+        root1.set("testKey1", "updatedValue");
+
+        await documentDeltaEventManager.process(user1Document, user2Document, user3Document);
+
+        assert.equal(user1ValueChangedCount, 0, "Incorrect number of valueChanged op received in document 1");
+        assert.equal(user2ValueChangedCount, 1, "Incorrect number of valueChanged op received in document 2");
+        assert.equal(user3ValueChangedCount, 1, "Incorrect number of valueChanged op received in document 3");
+
+        expectAllAfterValues("testKey1", "updatedValue");
+    });
+
+    it("Simultaneous set should reach eventual consistency with the same value", async () => {
+        root1.set("testKey1", "value1");
+        root2.set("testKey1", "value2");
+        root3.set("testKey1", "value0");
+        root3.set("testKey1", "value3");
+
+        expectAllBeforeValues("testKey1", "value1", "value2", "value3");
+        await documentDeltaEventManager.process(user1Document, user2Document, user3Document);
+        expectAllAfterValues("testKey1", "value3");
+    });
+
+    it("Simultaneous delete/set should reach eventual consistency with the same value", async () => {
+        // set after delete
+        root1.set("testKey1", "value1.1");
+        root2.delete("testKey1");
+        root3.set("testKey1", "value1.3");
+
+        expectAllBeforeValues("testKey1", "value1.1", undefined, "value1.3");
+        await documentDeltaEventManager.process(user1Document, user2Document, user3Document);
+        expectAllAfterValues("testKey1", "value1.3");
+    });
+
+    it("Simultaneous delete/set on same map should reach eventual consistency with the same value", async () => {
+        // delete and then set on the same map
+        root1.set("testKey2", "value2.1");
+        root2.delete("testKey2");
+        root3.set("testKey2", "value2.3");
+        // drain the outgoing so that the next set will come after
+        await documentDeltaEventManager.processOutgoing(user1Document, user2Document, user3Document);
+        root2.set("testKey2", "value2.2");
+
+        expectAllBeforeValues("testKey2", "value2.1", "value2.2", "value2.3");
+        await documentDeltaEventManager.process(user1Document, user2Document, user3Document);
+        expectAllAfterValues("testKey2", "value2.2");
+
+    });
+
+    it("Simultaneous set/delete should reach eventual consistency with the same value", async () => {
+        // delete after set
+        root1.set("testKey3", "value3.1");
+        root2.set("testKey3", "value3.2");
+        root3.delete("testKey3");
+
+        expectAllBeforeValues("testKey3", "value3.1", "value3.2", undefined);
+        await documentDeltaEventManager.process(user1Document, user2Document, user3Document);
+        expectAllAfterValues("testKey3", undefined);
+    });
+
+    it("Simultaneous set/clear on a key should reach eventual consistency with the same value", async () => {
+        // clear after set
+        root1.set("testKey1", "value1.1");
+        root2.set("testKey1", "value1.2");
+        root3.clear();
+        expectAllBeforeValues("testKey1", "value1.1", "value1.2", undefined);
+        assert.equal(root3.size, 0, "Incorrect map size after clear");
+        await documentDeltaEventManager.process(user1Document, user2Document, user3Document);
+        expectAllAfterValues("testKey1", undefined);
+        expectAllSize(0);
+    });
+
+    it("Simultaneous clear/set on same map should reach eventual consistency with the same value", async () => {
+        // set after clear on the same map
+        root1.set("testKey2", "value2.1");
+        root2.clear();
+        root3.set("testKey2", "value2.3");
+        // drain the outgoing so that the next set will come after
+        await documentDeltaEventManager.processOutgoing(user1Document, user2Document, user3Document);
+        root2.set("testKey2", "value2.2");
+        expectAllBeforeValues("testKey2", "value2.1", "value2.2", "value2.3");
+        await documentDeltaEventManager.process(user1Document, user2Document, user3Document);
+        expectAllAfterValues("testKey2", "value2.2");
+        expectAllSize(1);
+    });
+
+    it("Simultaneous clear/set should reach eventual consistency and resolve to the same value", async () => {
+        // set after clear
+        root1.set("testKey3", "value3.1");
+        root2.clear();
+        root3.set("testKey3", "value3.3");
+        expectAllBeforeValues("testKey3", "value3.1", undefined, "value3.3");
+        await documentDeltaEventManager.process(user1Document, user2Document, user3Document);
+        expectAllAfterValues("testKey3", "value3.3");
+        expectAllSize(1);
+    });
+
+    afterEach(async () => {
+        user1Document.close();
+        user2Document.close();
+        user3Document.close();
+        await testDeltaConnectionServer.webSocketServer.close();
+    });
+});
