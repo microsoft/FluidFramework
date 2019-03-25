b0c6443aae2423866126b936d4d5f4c5dbd515da
Curtis Man <curtism@microsoft.com>
2019-03-25 14:26:47 -0700

    Fold MergeTree.Client related code in testUtils into TestClient (#1871)
    * Fold MergeTree.Client related code in testUtils into TestClient
    
    * Refactor and combine makeClientOpMessage and makeOpMessage into one

--- routerlicious/packages/runtime/merge-tree/src/test/testUtils.ts
+++ routerlicious/packages/runtime/merge-tree/src/test/testUtils.ts
@@ -1,6 +1,5 @@
-import { ISequencedDocumentMessage, MessageType } from "@prague/container-definitions";
 import * as fs from "fs";
-import { Client, IMergeTreeDeltaOpArgs, IMergeTreeOp, Marker, MergeTree, SubSequence, TextSegment   } from "..";
+import { IMergeTreeDeltaOpArgs, Marker, MergeTree, TextSegment  } from "..";
 import * as ops from "../ops";
 import * as Properties from "../properties";
 import { loadText } from "../text";
@@ -16,45 +15,6 @@ export function loadTextFromFileWithMarkers(filename: string, mergeTree: MergeTr
     const content = fs.readFileSync(filename, "utf8");
     return loadText(content, mergeTree, segLimit, true);
 }
-
-export function makeClientOpMessage(client: Client, op: IMergeTreeOp, seq: number) {
-    return makeOpMessage(op, seq, client.mergeTree.collabWindow.currentSeq, client.longClientId);
-}
-export function makeOpMessage(op: IMergeTreeOp, seq: number, refSeq: number, longClientId: string) {
-    const msg: ISequencedDocumentMessage = {
-        clientId: longClientId,
-        clientSequenceNumber: 1,
-        contents: op,
-        minimumSequenceNumber: undefined,
-        origin: null,
-        referenceSequenceNumber: refSeq,
-        sequenceNumber: seq,
-        timestamp: Date.now(),
-        traces: [],
-        type: MessageType.Operation,
-    };
-    return msg;
-}
-// tslint:disable:no-unsafe-any
-export function specToSegment(spec: any) {
-    const maybeText = TextSegment.fromJSONObject(spec);
-    if (maybeText) {
-        return maybeText;
-    }
-
-    const maybeMarker = Marker.fromJSONObject(spec);
-    if (maybeMarker) {
-        return maybeMarker;
-    }
-
-    const maybeSubSequence = SubSequence.fromJSONObject(spec);
-    if (maybeSubSequence) {
-        return maybeSubSequence;
-    }
-
-    throw new Error(`Unrecognized IJSONSegment type: '${JSON.stringify(spec)}'`);
-}
-// tslint:enable:no-unsafe-any
 
 export function insertMarker(
     mergeTree: MergeTree,
@@ -78,82 +38,4 @@ export function insertText(
     opArgs: IMergeTreeDeltaOpArgs,
 ) {
     mergeTree.insertSegment(pos, refSeq, clientId, seq, TextSegment.make(text, props, seq, clientId), opArgs);
-}
-
-export function insertTextLocal(
-    client: Client,
-    text: string,
-    pos: number,
-    props?: Properties.PropertySet,
-    opArgs?: IMergeTreeDeltaOpArgs,
-) {
-    const segment = new TextSegment(text);
-    if (props) {
-        segment.addProperties(props);
-    }
-    client.insertSegmentLocal(pos, segment, opArgs);
-}
-
-export function insertMarkerLocal(
-    client: Client,
-    pos: number,
-    behaviors: ops.ReferenceType,
-    props?: Properties.PropertySet,
-    opArgs?: IMergeTreeDeltaOpArgs,
-) {
-    const segment = new Marker(behaviors);
-    if (props) {
-        segment.addProperties(props);
-    }
-    client.insertSegmentLocal(pos, segment, opArgs);
-}
-
-export function insertItemsRemote(
-    client: Client,
-    items: ops.SequenceItem[],
-    pos: number,
-    props: Properties.PropertySet,
-    seq: number,
-    refSeq: number,
-    clientId: number,
-    opArgs?: IMergeTreeDeltaOpArgs,
-) {
-    const segment = new SubSequence(items);
-    if (props) {
-        segment.addProperties(props);
-    }
-    client.insertSegmentRemote(segment, pos, seq, refSeq, clientId, opArgs);
-}
-
-export function insertMarkerRemote(
-    client: Client, markerDef:
-    ops.IMarkerDef,
-    pos: number,
-    props: Properties.PropertySet,
-    seq: number,
-    refSeq: number,
-    clientId: number, opArgs?: IMergeTreeDeltaOpArgs,
-) {
-    const segment = new Marker(markerDef.refType);
-    if (props) {
-        segment.addProperties(props);
-    }
-    client.insertSegmentRemote(segment, pos, seq, refSeq, clientId, opArgs);
-}
-
-export function insertTextRemote(
-    client: Client,
-    text: string,
-    pos: number,
-    props: Properties.PropertySet,
-    seq: number,
-    refSeq: number,
-    clientId: number,
-    opArgs?: IMergeTreeDeltaOpArgs,
-) {
-    const segment = new TextSegment(text);
-    if (props) {
-        segment.addProperties(props);
-    }
-    client.insertSegmentRemote(segment, pos, seq, refSeq, clientId, opArgs);
 }
