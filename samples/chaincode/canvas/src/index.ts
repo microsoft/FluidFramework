eaae13df0a12c9fd0b975483ab07637057e15ab1
kurtb <kurtberglund@hotmail.com>
2019-04-23 17:22:56 -0700

    Specify canvas background image via query param (#2109)

--- samples/chaincode/canvas/src/index.ts
+++ samples/chaincode/canvas/src/index.ts
@@ -2,6 +2,7 @@ import { Component, Document } from "@prague/app-component";
 import * as api from "@prague/client-api";
 import { controls, ui } from "@prague/client-ui";
 import { IContainerContext, IRuntime } from "@prague/container-definitions";
+import * as querystring from "querystring";
 
 import "./style.less";
 
@@ -9,6 +10,11 @@ export class canvas extends Document {
   // Create the component's schema and perform other initialization tasks
   // (only called when document is initially created).
   protected async create() {
+    const params = querystring.parse(window.location.search.substr(1));
+    if (params.image) {
+      this.root.set("image", params.image);
+    }
+
     this.root.set("ink", this.createStream());
   }
 
@@ -17,10 +23,21 @@ export class canvas extends Document {
 
     await this.root.wait("ink");
 
+    let image: HTMLImageElement = null;
+    if (this.root.has("image")) {
+      image = new Image();
+      const readyP = new Promise((resolve) => {
+        image.onload = resolve;
+      });
+      image.src = this.root.get("image");
+      await readyP;
+    }
+
     const canvas = new controls.FlexView(
       host,
       new api.Document(this.runtime, null, this.root),
-      this.root);
+      this.root,
+      image);
     browserHost.attach(canvas);
   }
 
