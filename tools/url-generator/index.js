5ade1f500e4b7c731c392027d87344072fec8e38
Sam Broner <SamBroner@users.noreply.github.com>
2019-05-20 16:59:17 -0700

    Make url generator (#2276)
    * get rid of the "getURL" file everywhere
    
    * fix up the package path

---
+++ tools/url-generator/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+var program = require('commander');
+
+program
+    .version('0.0.1')
+    .option('-l --loader <value>', 'Whats the url of the loader you want to use? (e.g. https://www.wu2.prague.office-int.com/loader/', 'https://www.wu2.prague.office-int.com/loader/')
+    .option('-t --tenant <value>', 'What tenant do you want to use?', "prague")
+    .option('-c --chaincode [value]', 'Which chaincode? e.g. @chaincode/counter@0.2.3')
+    .option('-p --pathToPackage [value]', 'If you want to just reference your package.json, put the path here (we guess the path as default)', "../../../package.json")
+    .parse(process.argv);
+
+const pkg = require(program.pathToPackage);
+
+let chaincode;
+if (program.chaincode) {
+    chaincode = program.chaincode;
+} else if (program.pathToPackage) {
+    chaincode = pkg.name + "@" + pkg.version;
+}
+
+const date = new Date();
+const url = `${program.loader}${program.tenant}/ChangeThisValue-${date.getTime()}?chaincode=${chaincode}`;
+console.log("View your chaincode at:");
+console.log('\x1b[36m%s\x1b[0m', url);
