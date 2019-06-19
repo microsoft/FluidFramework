e229169a8dcdec984f793e427aa9929ed75bcb6d
Tanvir Aumi <mdaumi@microsoft.com>
2019-06-19 13:58:54 -0700

    Update node loader for key-value chaincode (#2493)

--- samples/experiments/node-key-value/src/index.ts
+++ samples/experiments/node-key-value/src/index.ts
@@ -3,9 +3,8 @@
  * Licensed under the MIT License.
  */
 
-import { IPlatform, IPragueResolvedUrl } from "@prague/container-definitions";
+import { IComponent, IPragueResolvedUrl } from "@prague/container-definitions";
 import { Container, Loader } from "@prague/container-loader";
-import { ISharedMap } from "@prague/map";
 import { ContainerUrlResolver } from "@prague/routerlicious-host";
 import { RouterliciousDocumentServiceFactory } from "@prague/routerlicious-socket-storage";
 import { NodeCodeLoader, NodePlatform } from "@prague/services";
@@ -15,9 +14,9 @@ import * as ora from "ora";
 import * as process from "process";
 import * as url from "url";
 
-interface ISharedMapWrapper {
-    root: ISharedMap;
-    attach(platform: IPlatform): Promise<IPlatform>;
+interface IKeyValue {
+    set(key: string, value: any): void;
+    get(key: string): any;
 }
 
 function registerAttach(loader: Loader, container: Container, uri: string, platform: NodePlatform) {
@@ -29,26 +28,51 @@ function registerAttach(loader: Loader, container: Container, uri: string, platf
 
 async function attach(loader: Loader, docUrl: string, platform: NodePlatform) {
     const response = await loader.request({ url: docUrl });
-    if (response.status !== 200) {
+    if (response.status !== 200 || response.mimeType !== "prague/component") {
         return;
     }
-    if (response.mimeType === "prague/component") {
-        const keyValueComponent = response.value as ISharedMapWrapper;
-        await keyValueComponent.attach(platform);
-        const rootMap = keyValueComponent.root as ISharedMap;
-        rootMap.set("something", "1");
-        rootMap.set("something2", "2");
-        rootMap.set("something3", "3");
-        console.log("Done");
+    // Query for desired interface.
+    const component = response.value as IComponent;
+    const keyValue = component.query<IKeyValue>("IKeyValue");
+    if (!keyValue) {
+        return;
     }
+
+    keyValue.set("key1", Math.floor(Math.random() * 100000).toString());
+    keyValue.set("key2", Math.floor(Math.random() * 100000).toString());
+
+    console.log(keyValue.get("key1"));
+    console.log(keyValue.get("key2"));
+    console.log("Done");
 }
 
-async function run(loader: Loader, docUrl: string): Promise<void> {
+async function run(loader: Loader, docUrl: string, codePackage: string): Promise<void> {
     const loaderP = loader.resolve({ url: docUrl });
     ora.promise(loaderP, `Resolving...`);
     const container = await loaderP;
     const platform = new NodePlatform();
     registerAttach(loader, container, docUrl, platform);
+
+    if (!container.existing) {
+        await initializeChaincode(container, codePackage)
+            .catch((error) => console.error("chaincode error", error));
+    }
+}
+
+async function initializeChaincode(container: Container, pkg: string): Promise<void> {
+    if (!pkg) {
+        return;
+    }
+    const quorum = container.getQuorum();
+    // Wait for connection so that proposals can be sent
+    if (!container.connected) {
+        await new Promise<void>((resolve) => container.on("connected", () => resolve()));
+    }
+    // And then make the proposal if a code proposal has not yet been made
+    if (!quorum.has("code2")) {
+        await quorum.propose("code2", pkg);
+    }
+    console.log(`Code is ${quorum.get("code2")}`);
 }
 
 // Process command line input
@@ -58,6 +82,7 @@ commander
     .option("-h, --storage [storage]", "Storage URL", "https://historian.wu2-ppe.prague.office-int.com")
     .option("-t, --tenant [tenant]", "Tenant", "prague")
     .option("-s, --secret [secret]", "Secret", "43cfc3fbf04a97c0921fd23ff10f9e4b")
+    .option("-p, --package [package]", "Package", "@chaincode/key-value@0.5.7917")
     .arguments("<documentId>")
     .action((documentId) => {
         action = true;
@@ -110,7 +135,7 @@ commander
             new NodeCodeLoader("https://packages.wu2.prague.office-int.com", "/tmp/chaincode", 60000),
             null);
 
-        run(loader, documentUrl)
+        run(loader, documentUrl, commander.package)
             .catch((error) => {
                 console.error(error);
                 process.exit(1);
