9b698e628f92fe29d0032e9a2665cde75a88f252
Vlad Sudzilouski <46826984+vladsud@users.noreply.github.com>
2019-06-25 11:13:54 -0700

    Fixing type info for Container.on() method (#2560)

--- packages/loader/container-loader/src/container.ts
+++ packages/loader/container-loader/src/container.ts
@@ -208,10 +208,11 @@ export class Container extends EventEmitter implements IContainer {
     }
 
     public on(event: "connected" | "contextChanged", listener: (clientId: string) => void): this;
-    public on(event: "disconnect", listener: () => void): this;
+    public on(event: "disconnected", listener: () => void): this;
     public on(event: "error", listener: (error: any) => void): this;
     public on(event: "op", listener: (message: ISequencedDocumentMessage) => void): this;
     public on(event: "pong" | "processTime", listener: (latency: number) => void): this;
+    public on(event: MessageType.BlobUploaded, listener: (contents: any) => void): this;
 
     /* tslint:disable:no-unnecessary-override */
     public on(event: string | symbol, listener: (...args: any[]) => void): this {
