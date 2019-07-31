fb1fd466559c89e0229b2a78cfe76460004ead06
Arin Taylor <artaylor@microsoft.com>
2019-07-31 13:14:59 -0700

    Add Type-Race to samples (#2902)
    * Initial commit of type race
    
    * Text generator example
    
    * Some stuff
    
    * Work
    
    * Add some formatting
    
    * wpm improvements
    
    * Add a few other text sources
    
    * start countdown
    
    * medals
    
    * removed console.log()
    
    * Small changes to countdown
    
    * Change other text view to red for incorrect text
    
    * Rename to type-race
    
    * changed countdown
    
    * Add copyright headers to type-race files

---
+++ samples/chaincode/type-race/src/index.ts
@@ -0,0 +1,29 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {
+  SimpleModuleInstantiationFactory
+} from "@prague/aqueduct";
+
+import { TyperaceInstantiationFactory as ComponentInstantiationFactory } from "./main";
+
+// tslint:disable-next-line: no-var-requires no-require-imports
+const pkg = require("../package.json");
+const chaincodeName = pkg.name as string;
+
+/**
+ * This does setup for the Container. The SimpleModuleInstantiationFactory also enables dynamic loading in the 
+ * EmbeddedComponentLoader.
+ * 
+ * There are two important things here:
+ * 1. Default Component name
+ * 2. Map of string to factory for all components
+ */
+export const fluidExport = new SimpleModuleInstantiationFactory(
+  chaincodeName,
+  new Map([
+      [chaincodeName, Promise.resolve(ComponentInstantiationFactory)],
+  ]),
+);
