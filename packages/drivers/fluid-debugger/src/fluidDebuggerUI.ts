7be38b51d440ed5e6c26b162f78403d56092c5f4
Vlad Sudzilouski <vladsud@microsoft.com>
2019-08-07 15:08:17 -0700

    Fluid debugger changes to support document recovery (#3026)

--- packages/drivers/fluid-debugger/src/fluidDebuggerUI.ts
+++ packages/drivers/fluid-debugger/src/fluidDebuggerUI.ts
@@ -3,24 +3,85 @@
  * Licensed under the MIT License.
  */
 
-import { ISequencedDocumentMessage } from "@prague/container-definitions";
+import { ISequencedDocumentMessage, IVersion } from "@prague/container-definitions";
 import * as assert from "assert";
 
 export interface IDebuggerUI {
-    addVersion(version: string): void;
-    versionSelected(VersionInfo): void;
+    /**
+     * Version information is provided.
+     * Expect updates (information about seq#, timestamp) through updateVerison() calls
+     */
+    addVersions(version: IVersion[]): void;
+
+    /**
+     * Call when new version is downloaded from storage
+     * Expect multiple callbacks.
+     */
+    updateVerison(index: number, version: IVersion, seqNumber: number, timeStamp: number): void;
+
+    /**
+     * Called in response to successful onVersionSelection() call
+     * and provides extra information about selection.
+     * It expected that UI layer would change its mode as result of this call, i.e. switch to
+     * displaying op playback controls (if this is supported)
+     * Note: There maybe no call to versionSelected() in response to onVersionSelection() call
+     * if file does not exist, has wrong name of wrong format.
+     * @param version - version, file name, or undefined if playing ops.
+     */
+    versionSelected(seqNumber: number, version?: IVersion | string): void;
+
+    /**
+     * Called by controller in response to new ops being downloaded
+     * Called with disable = true if there are no (currently) ops to play
+     */
     disableNextOpButton(disable: boolean): void;
+
+    /**
+     * Called by controller when new ops arrive (or we are done playing previous batch)
+     * Indicates next batch of ops that would be played when UI calls controller's onOpButtonClick()
+     * Called with ops=[] when there are no ops to play.
+     */
     updateNextOpText(ops: ISequencedDocumentMessage[]): void;
-    updateVersionText(text: string): void;
-    updateLastOpText(text: string): void;
+
+    /**
+     * Called periodically when new versions are downloaded from server
+     */
+    updateVersionText(versionsLeft: number): void;
+
+    /**
+     * Called periodically to notify about last known op
+     * @param lastKnownOp - seq number of last known op. -1 if can't play ops in this mode (load from file)
+     * @param stillLoading - true if we did not reach yet the end of the stream
+     */
+    updateLastOpText(lastKnownOp: number, stillLoading: boolean): void;
 }
 
 export interface IDebuggerController {
+    /**
+     * Initialization. UI layers calls into controller to connect the two.
+     * @param ui - UI layer
+     */
     connectToUi(ui: IDebuggerUI);
+
+    /**
+     * Called by UI layer when debugger window is closed by user
+     * If called before user makes selection of snapshot/file/no snapshot, original
+     * document service is returned to loader (instead of debugger service) and normal document load continues.
+     */
     onClose(): void;
-    onVersionSelection(index: number): void;
+
+    /**
+     * UI Layer notifies about selection of version to continue.
+     * On successful load, versionSelected() is called.
+     * @param version - Version, File or undefined (playing ops)
+     */
+    onVersionSelection(version: IVersion | File | undefined): void;
+
+    /**
+     * "next op" button is clicked in UI
+     * @param steps - number of ops to play.
+     */
     onOpButtonClick(steps: number): void;
-    onSnapshotFileSelection(file: File): void;
 }
 
 const debuggerWindowHtml =
@@ -93,6 +154,7 @@ export class DebuggerUI {
     protected text3?: HTMLDivElement;
     protected lastOpText?: HTMLDivElement;
     protected wasVersionSelected = false;
+    protected versions: IVersion[] = [];
 
     protected documentClosed = false;
 
@@ -117,28 +179,53 @@ export class DebuggerUI {
         this.versionText = doc.getElementById("versionText") as HTMLDivElement;
 
         buttonVers.onclick = () => {
-            controller.onVersionSelection(this.selector!.selectedIndex);
+            // Accounting for "no snapshots"
+            const index = this.selector!.selectedIndex;
+            controller.onVersionSelection(index === 0 ? undefined : this.versions[index - 1]);
         };
 
         fileSnapshot.addEventListener("change", async () => {
             const files = fileSnapshot.files;
             if (files) {
-                controller.onSnapshotFileSelection(files[0]);
+                controller.onVersionSelection(files[0]);
             }
         }, false);
+
+        this.versionText.textContent = "Fetching snapshots, please wait...";
 
         controller.connectToUi(this);
     }
 
-    public addVersion(version: string) {
+    public addVersions(versions: IVersion[]) {
         if (this.selector) {
-            const option = document.createElement("option");
-            option.text = version;
-            this.selector.add(option);
+            this.versions = versions;
+            for (const version of versions) {
+                const option = document.createElement("option");
+                option.text = `id = ${version.id}`;
+                this.selector.add(option);
+            }
         }
     }
 
-    public versionSelected(VersionInfo: string) {
+    public updateVerison(index: number, version: IVersion, seqNumber: number, timeStamp: number) {
+        if (this.selector) {
+            const option = document.createElement("option");
+            option.text = `id = ${version.id}   seq = ${seqNumber}`;
+            // Accounting for "no snapshots"
+            this.selector[index + 1] = option;
+        }
+    }
+
+    public versionSelected(seqNumber: number, version?: IVersion | string) {
+        let text: string;
+        if (version === undefined) {
+            text = "Playing from seq# 0";
+        } else if (typeof version === "string") {
+            text = `Playing ${version} file`;
+        } else {
+            text = `Playing from ${version.id}, seq# ${seqNumber}`;
+        }
+
         this.wasVersionSelected = true;
         this.selector = undefined;
 
@@ -160,7 +247,7 @@ export class DebuggerUI {
             this.controller.onOpButtonClick(Number(steps.value));
         };
 
-        this.versionText.textContent = VersionInfo;
+        this.versionText.textContent = text;
     }
 
     public disableNextOpButton(disable: boolean) {
@@ -183,13 +270,23 @@ export class DebuggerUI {
         }
     }
 
-    public updateVersionText(text: string) {
+    public updateVersionText(versionCount: number) {
         if (!this.wasVersionSelected) {
+            const text = versionCount === 0 ? "" : `Fetching information about ${versionCount} snapshots...`;
             this.versionText.textContent = text;
         }
     }
 
-    public updateLastOpText(text: string) {
+    public updateLastOpText(lastKnownOp: number, stillLoading: boolean) {
+        let text: string;
+        if (lastKnownOp < 0) {
+            text = `FluidDebugger can't play ops in this mode`;
+        }
+        if (stillLoading) {
+            text = `Last op (still loading): ${lastKnownOp}`;
+        } else {
+            text = `Document's last op seq#: ${lastKnownOp}`;
+        }
         this.lastOpText!.textContent = text;
     }
 }
