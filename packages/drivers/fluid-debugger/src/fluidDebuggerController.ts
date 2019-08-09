3862c8bdc787b148a6a1ac1f62b2b845d42f3e22
Vlad Sudzilouski <vladsud@microsoft.com>
2019-08-09 16:12:35 -0700

    Port fluid debugger refactoring (#3039)
    Cherry picking hot fixes for Fluid Debugger from 0.7:
    
    * Cherry pick commit: 5d0da309daa7dcbad9154280068b34fc8e0b0e15 [f43194a]
    Fluid debugger changes to support document recovery (#3026)
    * FluidDebugger: regression: snapshots no longer load (#3037)
    * Regression in FluidDebugger - we stop fetching ops at 2000 mark (#3069)
    * Build break

--- packages/drivers/fluid-debugger/src/fluidDebuggerController.ts
+++ packages/drivers/fluid-debugger/src/fluidDebuggerController.ts
@@ -61,11 +61,15 @@ export class DebugReplayController extends ReplayController implements IDebugger
     protected ui: IDebuggerUI = null as any as IDebuggerUI; // not to check on every line that it's not null
     protected stepsDeferred?: Deferred<number>;
     protected startSeqDeferred = new Deferred<number>();
-    protected doneFetchingOps = false;
+
+    // true will cause us ping server indefinitely waiting for new ops
+    protected retryFetchOpsOnEndOfFile = false;
+
     protected documentStorageService?: IDocumentStorageService;
     protected versions: IVersion[] = [];
     protected stepsToPlay: number = 0;
     protected lastOpReached = false;
+    protected versionCount = 0;
 
     protected storage?: ReadDocumentStorageServiceBase;
 
@@ -77,24 +81,22 @@ export class DebugReplayController extends ReplayController implements IDebugger
         this.startSeqDeferred.resolve(DebugReplayController.WindowClosedSeq);
     }
 
-    public async onVersionSelection(indexS: number) {
-        if (indexS === 0 || !this.documentStorageService) {
-            // no snapshot
-            this.resolveStorage("Playing from seq# 0", 0, new OpStorage());
+    public async onVersionSelection(version?: IVersion) {
+        if (version === undefined) {
+            this.resolveStorage(0, new OpStorage());
             return;
         }
-        let index = indexS - 1;
-        if (index < 0 || index >= this.versions.length) {
-            index = 0;
+
+        if (!this.documentStorageService) {
+            throw new Error("onVersionSelection: no storage");
         }
 
-        const version = this.versions[index];
         const tree = await this.documentStorageService.getSnapshotTree(version);
         const seq = await DebugReplayController.seqFromTree(this.documentStorageService, tree);
         this.resolveStorage(
-                `Playing from ${version.id}, seq# ${seq}`,
                 seq,
-                new SnapshotStorage(this.documentStorageService, tree));
+                new SnapshotStorage(this.documentStorageService, tree),
+                version);
     }
 
     public onOpButtonClick(steps: number) {
@@ -130,11 +132,12 @@ export class DebugReplayController extends ReplayController implements IDebugger
                     this.startSeqDeferred.resolve(seq);
                     */
                     // No ability to load ops, so just say - pick up from infinite op.
-                    this.doneFetchingOps = true;
+                    this.retryFetchOpsOnEndOfFile = false;
+                    this.lastOpReached = true;
                     this.resolveStorage(
-                        `Playing ${file.name} file`,
                         Number.MAX_SAFE_INTEGER,
-                        new FileSnapshotReader(json));
+                        new FileSnapshotReader(json),
+                        file.name);
                 } catch (error) {
                     alert(`Error parsing file: ${error}`);
                     return;
@@ -148,59 +151,85 @@ export class DebugReplayController extends ReplayController implements IDebugger
         return currentOp + MaxBatchDeltas;
     }
 
+    // returns true if version / file / ops selections is made.
+    public isSelectionMade() {
+        return this.storage !== undefined;
+    }
+
+    public async downloadVersionInfo(
+            documentStorageService: IDocumentStorageService,
+            prevRequest: Promise<void>,
+            index: number,
+            version: IVersion): Promise<void> {
+
+        if (this.isSelectionMade()) {
+            return;
+        }
+
+        await prevRequest;
+
+        const treeV = await documentStorageService.getSnapshotTree(version);
+        const seqV = await DebugReplayController.seqFromTree(documentStorageService, treeV);
+
+        if (!this.isSelectionMade()) {
+            this.versionCount--;
+            this.ui.updateVersionText(this.versionCount);
+            this.ui.updateVerison(index, version, seqV, -1);
+        }
+
+    }
+
     public async initStorage(documentStorageService: IDocumentStorageService): Promise<boolean> {
         assert(documentStorageService);
         assert(!this.documentStorageService);
         this.documentStorageService = documentStorageService;
 
         // User can chose "no snapshot" or "file" at any moment in time!
-        if (this.storage === undefined) {
-            this.ui.updateVersionText("Fetching snapshots, please wait...");
+        if (!this.isSelectionMade()) {
             this.versions = await documentStorageService.getVersions("", 50);
+            if (!this.isSelectionMade()) {
+                this.ui.addVersions(this.versions);
+                this.ui.updateVersionText(this.versionCount);
+            }
         }
 
         /* Short circuit for when we have no versions
         we do not want to use it to leave ability for user to close window and use real storage.
-        if (this.storage === undefined && this.versions.length === 0) {
+        if (!this.isSelectionMade() && this.versions.length === 0) {
             this.text1.textContent = "";
             this.resolveStorage(0, new OpStorage());
             return true;
         }
         */
 
-        let versionCount = this.versions.length;
+        this.versionCount = this.versions.length;
 
-        // Order versions, but also do not do too many parallel requests.
-        // Can be done better (faster) in the future.
-        let prevRequest = Promise.resolve();
-        this.versions.map((version) => {
-            if (this.storage === undefined) {
-                prevRequest = prevRequest.then(async () => {
-                    this.ui.updateVersionText(`Fetching information about ${versionCount} snapshots...`);
-                    versionCount--;
-
-                    const treeV = await documentStorageService.getSnapshotTree(version);
-                    const seqV = await DebugReplayController.seqFromTree(documentStorageService, treeV);
-
-                    this.ui.addVersion(`id = ${version.id}   seq = ${seqV}`);
-
-                });
+        // Download all versions - do 10 downloads in parallel to avoid being throttled
+        const buckets = 10;
+        // tslint:disable-next-line:prefer-array-literal
+        const work: Array<Promise<void>> = [];
+        for (let i = 0; i < buckets; i++) {
+            let prevRequest = Promise.resolve();
+            for (let index = i; index < this.versions.length; index += buckets) {
+                const version = this.versions[index];
+                prevRequest = this.downloadVersionInfo(documentStorageService, prevRequest, index, version);
             }
-        });
+            work.push(prevRequest);
+        }
 
         // Don't wait for stuff to populate.
         // tslint:disable-next-line:no-floating-promises
-        prevRequest.then(() => {
-            this.ui.updateVersionText("");
+        Promise.all(work).then(() => {
+            this.ui.updateVersionText(0);
         });
 
         const useController = await this.startSeqDeferred.promise !== DebugReplayController.WindowClosedSeq;
-        assert((this.storage !== undefined) === useController);
+        assert(this.isSelectionMade() === useController);
         return useController;
     }
 
     public async read(blobId: string): Promise<string> {
-        if (this.storage) {
+        if (this.storage !== undefined) {
             return this.storage.read(blobId);
         }
         throw new Error("Reading blob before storage is setup properly");
@@ -209,14 +238,14 @@ export class DebugReplayController extends ReplayController implements IDebugger
     public async getVersions(
             versionId: string,
             count: number): Promise<IVersion[]> {
-        if (this.storage) {
+        if (this.storage !== undefined) {
             return this.storage.getVersions(versionId, count);
         }
         throw new Error("initStorage() was not called!");
     }
 
     public async getSnapshotTree(versionRequested?: IVersion): Promise<ISnapshotTree | null> {
-        if (this.storage) {
+        if (this.storage !== undefined) {
             return this.storage.getSnapshotTree(versionRequested);
         }
         throw new Error("Reading snapshot tree before storage is setup properly");
@@ -228,18 +257,17 @@ export class DebugReplayController extends ReplayController implements IDebugger
 
     /**
      * Return true if we are done processing ops
-     * Returning false as we want to continue to ping server for any new ops.
      */
     public isDoneFetch(currentOp: number, lastTimeStamp?: number): boolean {
         if (lastTimeStamp === undefined) {
             this.lastOpReached = true;
             if (currentOp === Number.MAX_SAFE_INTEGER) {
-                this.ui.updateLastOpText(`FluidDebugger can't play ops in this mode`);
+                this.ui.updateLastOpText(-1, false);
             } else {
-                this.ui.updateLastOpText(`Document's last op seq#: ${currentOp}`);
+                this.ui.updateLastOpText(currentOp, false);
             }
         }
-        return this.doneFetchingOps;
+        return this.lastOpReached && !this.retryFetchOpsOnEndOfFile;
     }
 
     public async replay(
@@ -249,7 +277,7 @@ export class DebugReplayController extends ReplayController implements IDebugger
         if (!this.lastOpReached) {
             const op = fetchedOps[fetchedOps.length - 1];
             const lastSeq = op.sequenceNumber;
-            this.ui.updateLastOpText(`Last op (still loading): ${lastSeq}`);
+            this.ui.updateLastOpText(lastSeq, true);
         }
         while (true) {
             if (fetchedOps.length === 0) {
@@ -283,12 +311,16 @@ export class DebugReplayController extends ReplayController implements IDebugger
         }
     }
 
-    protected resolveStorage(versionInfo: string, seq: number, storage: ReadDocumentStorageServiceBase) {
-        assert(!this.storage);
+    protected resolveStorage(
+            seq: number,
+            storage: ReadDocumentStorageServiceBase,
+            version?: IVersion | string) {
+        assert(!this.isSelectionMade());
         assert(storage);
         this.storage = storage;
+        assert(this.isSelectionMade());
 
-        this.ui.versionSelected(versionInfo);
+        this.ui.versionSelected(seq, version);
         this.startSeqDeferred.resolve(seq);
     }
 }
