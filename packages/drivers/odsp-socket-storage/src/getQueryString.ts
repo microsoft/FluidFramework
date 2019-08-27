11a3549cacce8b7e88abb45316c901227c2409ad
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2019-08-27 16:58:52 -0400

    Use trees/latest in the ODSP driver (#3335) (#3337)
    * Use trees/latest
    
    * Don't read from the cache for return value, hardcode query string
    
    (cherry picked from commit 5d4a1f8df510c19023e0f10dd86505c361b59079)

--- packages/drivers/odsp-socket-storage/src/getQueryString.ts
+++ packages/drivers/odsp-socket-storage/src/getQueryString.ts
@@ -5,7 +5,7 @@
 
 /**
  * Generates query string from the given query parameters.
- * @param queryParams - Query parametres from which to create a query.
+ * @param queryParams - Query parameters from which to create a query.
  */
 export function getQueryString(queryParams: { [key: string]: string }): string {
     let queryString = "";
