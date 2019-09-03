44f257e2f7f61136e28fd7187086f25690e0b1b2
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2019-09-03 16:03:31 -0700

    Cherry pick #3377 into 0.9 (#3409)
    * Cherry pick
    
    * no message

--- packages/drivers/odsp-socket-storage/src/OdspDocumentService.ts
+++ packages/drivers/odsp-socket-storage/src/OdspDocumentService.ts
@@ -6,6 +6,7 @@
 import * as resources from "@prague/gitresources";
 import * as api from "@prague/protocol-definitions";
 import { DocumentDeltaConnection } from "@prague/socket-storage-shared";
+import { BaseTelemetryNullLogger } from "@prague/utils";
 import * as io from "socket.io-client";
 import { IOdspSnapshot, ISocketStorageDiscovery } from "./contracts";
 import { IFetchWrapper } from "./fetchWrapper";
@@ -91,6 +92,7 @@ export class OdspDocumentService implements api.IDocumentService {
 
                     return this.tokenProvider;
                 },
+                new BaseTelemetryNullLogger(),
             ),
         );
     }
