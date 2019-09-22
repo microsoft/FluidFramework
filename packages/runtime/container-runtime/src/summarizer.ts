e8ad0dc3fc79d1b3f230265170fc8eaea115bb15
Arin Taylor <artaylor@microsoft.com>
2019-09-22 15:07:10 -0700

    Wait for summary ack before summarizing

--- packages/runtime/container-runtime/src/summarizer.ts
+++ packages/runtime/container-runtime/src/summarizer.ts
@@ -6,6 +6,7 @@
 import { IComponentLoadable } from "@microsoft/fluid-component-core-interfaces";
 import { Deferred } from "@microsoft/fluid-core-utils";
 import { ISequencedDocumentMessage, ISummaryConfiguration, MessageType } from "@microsoft/fluid-protocol-definitions";
+import * as assert from "assert";
 import { ContainerRuntime } from "./containerRuntime";
 import { debug } from "./debug";
 
@@ -15,6 +16,9 @@ import { debug } from "./debug";
 interface IOpSummaryDetails {
     // Whether we should summarize at the given op
     shouldSummarize: boolean;
+
+    // Whether we can start idle timer
+    canStartIdleTimer: boolean;
 
     // The message to include with the summarize
     message: string;
@@ -45,6 +49,7 @@ export class Summarizer implements IComponentLoadable, ISummarizer {
     private lastSummaryTime: number = Date.now();
     private lastSummarySeqNumber: number = 0;
     private summarizing = false;
+    private summaryPending = false;
     private idleTimer: NodeJS.Timeout | null = null;
     private lastOp: ISequencedDocumentMessage | null = null;
     private lastOpSummaryDetails: IOpSummaryDetails | null = null;
@@ -75,12 +80,24 @@ export class Summarizer implements IComponentLoadable, ISummarizer {
         // start the timer after connecting to the document
         this.resetIdleTimer();
 
+        // listen for summary ops
+        this.runtime.deltaManager.inbound.on("op", (op) => this.handleSummaryOp(op as ISequencedDocumentMessage));
+
         this.runtime.on("batchEnd", (error: any, op: ISequencedDocumentMessage) => {
             if (error) {
                 return;
             }
 
             this.clearIdleTimer();
+
+            // clear pending if timeout waiting for server ack
+            if (this.summaryPending) {
+                const pendingTime = Date.now() - this.lastSummaryTime;
+                if (pendingTime > this.configuration.maxAckWaitTime) {
+                    this.runtime.logger.sendTelemetryEvent({ eventName: "SummaryAckWaitTimeout" });
+                    this.summaryPending = false;
+                }
+            }
 
             // Get the summary details for the given op
             this.lastOp = op;
@@ -90,7 +107,7 @@ export class Summarizer implements IComponentLoadable, ISummarizer {
                 // Summarize immediately if requested
                 // tslint:disable-next-line: no-floating-promises
                 this.summarize(this.lastOpSummaryDetails.message);
-            } else {
+            } else if (this.lastOpSummaryDetails.canStartIdleTimer) {
                 // Otherwise detect when we idle to trigger the snapshot
                 this.resetIdleTimer();
             }
@@ -99,8 +116,19 @@ export class Summarizer implements IComponentLoadable, ISummarizer {
         return this.runDeferred.promise;
     }
 
+    private handleSummaryOp(op: ISequencedDocumentMessage) {
+        if (op.type === MessageType.SummaryAck || op.type === MessageType.SummaryNack) {
+            if (this.summaryPending) {
+                this.summaryPending = false;
+            }
+        }
+    }
+
     private async summarize(message: string) {
         try {
+            // it shouldn't be possible to enter here if already summarizing or pending
+            assert(!this.summarizing && !this.summaryPending);
+
             // generateSummary could take some time
             // mark that we are currently summarizing to prevent concurrent summarizing
             this.summarizing = true;
@@ -108,6 +136,8 @@ export class Summarizer implements IComponentLoadable, ISummarizer {
             debug(`Summarizing: ${message}`);
 
             const summarySequenceNumber = this.lastOp ? this.lastOp.sequenceNumber : 1;
+
+            this.summaryPending = true;
 
             await this.generateSummary();
 
@@ -118,24 +148,26 @@ export class Summarizer implements IComponentLoadable, ISummarizer {
 
         } catch (ex) {
             debug(`Summarize error ${this.runtime.id}`, ex);
-
+            this.summaryPending = false;
         } finally {
             this.summarizing = false;
         }
     }
 
     private getOpSummaryDetails(op: ISequencedDocumentMessage): IOpSummaryDetails {
-        if (this.summarizing) {
+        if (this.summarizing || this.summaryPending) {
             // We are currently summarizing. Don't summarize again.
             return {
                 message: "",
                 shouldSummarize: false,
+                canStartIdleTimer: false,
             };
         } else if (op.type === MessageType.Save) {
             // Forced summary.
             return {
                 message: `;${op.clientId}: ${op.contents}`,
                 shouldSummarize: true,
+                canStartIdleTimer: true,
             };
         } else {
             // Summarize if it has been above the max time between summaries.
@@ -145,6 +177,7 @@ export class Summarizer implements IComponentLoadable, ISummarizer {
                 message: "",
                 shouldSummarize: (timeSinceLastSummary > this.configuration.maxTime) ||
                     (opCountSinceLastSummary > this.configuration.maxOps),
+                canStartIdleTimer: true,
             };
         }
     }
