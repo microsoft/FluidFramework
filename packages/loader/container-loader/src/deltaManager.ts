3d7a5db721cbf4be11e86dfcb058ca6cb1c89c46
vladsud <vladsud@microsoft.com>
2019-09-23 10:02:11 -0700

    Better comment / easier to read logic

--- packages/loader/container-loader/src/deltaManager.ts
+++ packages/loader/container-loader/src/deltaManager.ts
@@ -403,8 +403,13 @@ export class DeltaManager extends EventEmitter implements IDeltaManager<ISequenc
                 // If we have no upper bound and fetched less than the max deltas - meaning we got as many as exit -
                 // then we can resolve the promise. We also resolve if we fetched up to the expected to. Otherwise
                 // we will look to try again
-                if (to === undefined ? maxFetchTo > lastFetch + 1 : to <= lastFetch + 1) {
-                    telemetryEvent.end({lastFetch, totalDeltas: allDeltas.length, retries: retry});
+                // Note #1: we can get more ops than what we asked for - need to account for that!
+                // Note #2: from & to are exclusive! I.e. we actually expect [from + 1, to - 1] range of ops back!
+                // 1) to === undefined case: if last op  is below what we expect, then storage does not have
+                //    any more, thus it's time to leave
+                // 2) else case: if we got what we asked (to - 1) or more, then time to leave.
+                if (to === undefined ? lastFetch < maxFetchTo - 1 : to - 1 <= lastFetch) {
+                    telemetryEvent.end({ lastFetch, totalDeltas: allDeltas.length, retries: retry });
                     return allDeltas;
                 }
 
