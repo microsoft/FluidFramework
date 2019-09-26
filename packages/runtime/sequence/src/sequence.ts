80166fb62f0f4cfbe97bc80b24d1121024abeb74
Tony Murphy <anthony.murphy@microsoft.com>
2019-09-26 15:43:19 -0700

    add split range option to walkSegments (#173)
    * add split range option to walkSegments
    
    * fix comments for doc build

--- packages/runtime/sequence/src/sequence.ts
+++ packages/runtime/sequence/src/sequence.ts
@@ -323,11 +323,25 @@ export abstract class SharedSegmentSequence<T extends MergeTree.ISegment>
         return this.client.posFromRelativePos(relativePos);
     }
 
+    /**
+     * Walk the underlying segments of the sequence.
+     * The walked segments may extend beyond the range
+     * if the segments cross the ranges start or end boundaries.
+     * Set split range to true to esure only segments within the
+     * range are walked.
+     *
+     * @param handler - The function to handle each segment
+     * @param start - Optional. The start of range walk.
+     * @param end - Optional. The end of range walk
+     * @param accum - Optional. An object that will be passed to the handler for accumulation
+     * @param splitRange - Optional. Splits boundary segements on the range boundaries
+     */
     public walkSegments<TClientData>(
         handler: MergeTree.ISegmentAction<TClientData>,
-        start?: number, end?: number, accum?: TClientData) {
+        start?: number, end?: number, accum?: TClientData,
+        splitRange: boolean = false) {
 
-        return this.client.walkSegments<TClientData>(handler, start, end, accum);
+        return this.client.walkSegments<TClientData>(handler, start, end, accum, splitRange);
     }
 
     public getStackContext(startPos: number, rangeLabels: string[]) {
