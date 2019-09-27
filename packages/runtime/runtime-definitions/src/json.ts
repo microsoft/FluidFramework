f8c34bab23a037287f31a65bedc3c604201ee313
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2019-09-27 09:42:16 -0700

    Json tweak

--- packages/runtime/runtime-definitions/src/json.ts
+++ packages/runtime/runtime-definitions/src/json.ts
@@ -8,20 +8,20 @@
 // tslint:disable:interface-name
 
 export type JsonPrimitive = undefined | null | boolean | number | string;
-export interface JsonObject<T> extends Record<string, T | JsonArray<T> | JsonObject<T>> { }
-export interface JsonArray<T> extends Array<T | JsonObject<T> | JsonArray<T>> { }
+export interface JsonObject<T> extends Record<string, Json<T>> { }
+export interface JsonArray<T> extends Array<Json<T>> { }
 
 /**
  * Used to constrain a value to types that are serializable as JSON.  The `T` type parameter may be used to
  * customize the type of the leaves to support situations where a `replacer` is used to handle special values.
- * (e.g., Json&lt;JsonPrimitive | IComponentHandle&gt;)
+ * (e.g., `Json<JsonPrimitive | IComponentHandle>`)
  *
- * Note that the Json type does not protect against the following pitfalls when marshalling `undefined` and
+ * Note that the Json type does not protect against the following pitfalls when serializing `undefined` and
  * non-finite numbers:
  *
- *  - `undefined` properties on objects are omitted (i.e., properties become undefined instead equal to undefined).
- *  - `undefined` is coerced to `null` when marshalling an `undefined` root or array element.
- *  - Non-finite numbers (NaN, +/-Infinity) are coerced to `null`.
- *  - (null always marshalls as null)
+ *  - `undefined` properties on objects are omitted (i.e., properties become undefined instead of equal to undefined).
+ *  - When `undefined` appears as the root object or as an array element it is coerced to `null`.
+ *  - Non-finite numbers (`NaN`, `+/-Infinity`) are also coerced to `null`.
+ *  - (`null` always serializes as `null`.)
  */
 export type Json<T = JsonPrimitive> = T | JsonArray<T> | JsonObject<T>;
