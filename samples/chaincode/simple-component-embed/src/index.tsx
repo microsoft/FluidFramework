1bac7fd3a80e95488d88201b887daf35e416abe2
Wes Carlson <wesley.carlson@microsoft.com>
2019-09-27 16:45:33 -0700

    move getComponent into componentHasInitialized

--- samples/chaincode/simple-component-embed/src/index.tsx
+++ samples/chaincode/simple-component-embed/src/index.tsx
@@ -10,24 +10,33 @@ import { ClickerInstantiationFactory, Clicker } from "@fluid-example/clicker";
 export class SimpleComponentEmbed extends PrimedComponent implements IComponentHTMLVisual {
   public get IComponentHTMLVisual() { return this; }
 
+  private clicker: Clicker;
+
   /**
    * This is only run the first time a document is created
-   * Here we will create a new embedded component. This can happen at any time but in this scenario we only want it to be created once.
+   * Here we will create a new embedded component. This can happen at any time
+   * but in this scenario we only want it to be created once.
    */
   protected async componentInitializingFirstTime() {
     await this.createAndAttachComponent("myEmbeddedCounter", "@fluid-example/clicker");
   }
 
-  public async render(div: HTMLDivElement) {
+  /**
+   * Get Clicker component using ID from before
+   */
+  protected async componentHasInitialized() {
+    this.clicker = await this.getComponent<Clicker>("myEmbeddedCounter");
+  }
+
+  public render(div: HTMLDivElement) {
     // Create a div that we will use to embed the component
     // and attach that div to the page
     const componentDiv = document.createElement("div");
     componentDiv.id = 'componentDiv';
     div.appendChild(componentDiv);
 
-    // Get Clicker component using id from before, then render it in our div
-    const clickerP = this.getComponent<Clicker>("myEmbeddedCounter")
-    clickerP.then(clicker =>  clicker.render(componentDiv));
+    // Then render the clicker in our div
+    this.clicker.render(componentDiv);
   }
 }
 
