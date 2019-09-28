a0eb31df444f5a9bc88f6a2c61134a1e083a1d9b
Tyler Butler <tylerbu@microsoft.com>
2019-09-28 11:59:17 -0700

    init from sudoku

---
+++ samples/chaincode/grid-layout/src/fluidSudoku.tsx
@@ -0,0 +1,108 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { PrimedComponent, PrimedComponentFactory } from "@microsoft/fluid-aqueduct";
+import { IComponentReactViewable } from "@microsoft/fluid-aqueduct-react";
+import { IComponentHandle, IComponentHTMLVisual } from "@microsoft/fluid-component-core-interfaces";
+import { ISharedMap, SharedMap } from "@microsoft/fluid-map";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { loadPuzzle } from "./helpers/puzzles";
+import { SudokuCell } from "./helpers/sudokuCell";
+import { SudokuView } from "./react/sudokuView";
+
+import "./helpers/styles.css";
+
+export const FluidSudokuName = "FluidSudoku";
+
+/**
+ * A collaborative Sudoku component built on the Fluid Framework.
+ */
+export class FluidSudoku extends PrimedComponent implements IComponentHTMLVisual, IComponentReactViewable {
+    public get IComponentHTMLVisual() {
+        return this;
+    }
+
+    public get IComponentReactViewable() {
+        return this;
+    }
+
+    public static getFactory() {
+        return this.factory;
+    }
+
+    /**
+     * This is where you define all your Distributed Data Structures
+     */
+    private static readonly factory = new PrimedComponentFactory(FluidSudoku, [SharedMap.getFactory()]);
+
+
+    private puzzle: ISharedMap | undefined;
+
+    /**
+     * ComponentInitializingFirstTime is where you do setup for your component. This is only called once the first time
+     * your component is created. Anything that happens in componentInitializingFirstTime will happen before any other
+     * user will see the component.
+     */
+    protected async componentInitializingFirstTime() {
+        const puzzle = SharedMap.create(this.runtime);
+        loadPuzzle(0, puzzle);
+
+        this.root.set("puzzle", puzzle.handle);
+    }
+
+    /**
+     * This method will be called whenever the component has initialized, be it the first time or subsequent times.
+     */
+    protected async componentHasInitialized() {
+        // Shared objects that are stored within other Shared objects (e.g. a SharedMap within the root, which is a
+        // SharedDirectory) must be retrieved asynchronously. We do that here, in this async function, then store a
+        // local reference to the object so we can easily use it in synchronous code.
+        //
+        // Our "puzzle" SharedMap is stored as a handle on the "root" SharedDirectory. To get it we must make a
+        // synchronous call to get the IComponentHandle, then an asynchronous call to get the ISharedMap from the
+        // handle.
+        this.puzzle = await this.root
+            .get<IComponentHandle>("puzzle")
+            .get<ISharedMap>();
+    }
+
+    public createJSXElement(props?): JSX.Element {
+        if (this.puzzle) {
+            return (
+                <SudokuView puzzle={this.puzzle} />
+            );
+        } else {
+            return <div />
+        }
+    }
+
+    /**
+     * This method is called automatically by the Fluid runtime.
+     */
+    public render(div: HTMLElement) {
+        const rerender = () => {
+            console.log("rerender!");
+            if (this.puzzle) {
+                ReactDOM.render(this.createJSXElement(), div);
+            }
+        };
+
+        rerender();
+
+        if (this.puzzle) {
+            this.puzzle.on("valueChanged", (changed, local, op) => {
+                const prev = changed.previousValue as SudokuCell;
+                console.log(`local: ${local} | op: ${JSON.stringify(op)}`);
+                if (this.puzzle) {
+                    console.log(`${local ? "Local" : "Remote"} valueChanged: ${changed.key} ==> ${
+                        JSON.stringify(this.puzzle.get<SudokuCell>(changed.key))} :: was ${
+                        JSON.stringify(prev)})`);
+                    rerender();
+                }
+            });
+        }
+    }
+}
---
+++ samples/chaincode/grid-layout/src/index.ts
@@ -0,0 +1,22 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { SimpleModuleInstantiationFactory } from "@microsoft/fluid-aqueduct";
+import { FluidSudoku, FluidSudokuName } from "./FluidSudoku";
+
+/**
+ * This does setup for the Container. The SimpleModuleInstantiationFactory also enables dynamic loading in the
+ * EmbeddedComponentLoader.
+ *
+ * There are two important things here:
+ * 1. Default Component name
+ * 2. Map of string to factory for all components
+ */
+export const fluidExport = new SimpleModuleInstantiationFactory(
+    FluidSudokuName,
+    new Map([
+        [FluidSudokuName, Promise.resolve(FluidSudoku.getFactory())],
+    ]),
+);
