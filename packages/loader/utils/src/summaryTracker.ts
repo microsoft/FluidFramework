34f1f82f11f705c4eebe15918567f93f8d5f4718
Arin Taylor <artaylor@microsoft.com>
2019-09-30 23:47:08 -0700

    Use 3 states instead of 2 independent flags for SummaryTracker

--- packages/loader/utils/src/summaryTracker.ts
+++ packages/loader/utils/src/summaryTracker.ts
@@ -6,18 +6,30 @@
 import { ISnapshotTree } from "@microsoft/fluid-protocol-definitions";
 
 /**
+ * Initial - this is the initial state of the tracker, which means that
+ * no changes have come in, but the base summary has not been set yet.
+ * Invalid - when a change comes in, the baseId cannot be used.
+ * Valid - no changes have come in since the last reset, and the
+ * base summary has been set.
+ */
+export enum SummaryTrackerState {
+    Initial = 0,
+    Invalid = -1,
+    Valid = 1,
+}
+
+/**
  * Responsible for tracking changes related to base summary tree usage.
  * It basically tracks 2 things: whether there have been any changes since
  * the base summary, and whether the base summary has been refreshed.
  * Only if both things are true can the baseId be reused during summarization.
+ * This is represented by 3 states in the SummaryTrackerState enum.
  */
 export class SummaryTracker {
-    public get unchangedSinceBase() { return this._unchangedSinceBase; }
-    public get baseIsLatest() { return this._baseIsLatest; }
+    public get state() { return this._state; }
     public get baseTree() { return this._baseSnapshotTree; }
 
-    private _unchangedSinceBase = true;
-    private _baseIsLatest = false;
+    private _state: SummaryTrackerState = SummaryTrackerState.Initial;
     private _baseSnapshotTree: ISnapshotTree | null = null;
 
     /**
@@ -25,7 +37,7 @@ export class SummaryTracker {
      * returns null otherwise
      */
     public getBaseId(): string | null {
-        if (this._unchangedSinceBase && this._baseIsLatest && this._baseSnapshotTree) {
+        if (this._state === SummaryTrackerState.Valid && this._baseSnapshotTree) {
             return this._baseSnapshotTree.id;
         } else {
             return null;
@@ -36,25 +48,28 @@ export class SummaryTracker {
      * Indicate that a change has occurred since the base
      */
     public trackChange() {
-        if (this._unchangedSinceBase) {
-            this._unchangedSinceBase = false;
+        if (this._state !== SummaryTrackerState.Invalid) {
+            this._state = SummaryTrackerState.Invalid;
         }
     }
 
     /**
-     * Resets to initial state: base is not latest, but there are no changes
+     * Resets to initial state: there are no changes, but base not yet set
      */
     public resetChangeTracker() {
-        this._unchangedSinceBase = true;
-        this._baseIsLatest = false;
+        if (this._state !== SummaryTrackerState.Initial) {
+            this._state = SummaryTrackerState.Initial;
+        }
     }
 
     /**
-     * Sets the base summary and marks that it is latest
+     * Sets the base summary and sets state as valid if initial
      * @param snapshot - base summary to set
      */
     public refreshBaseSummary(snapshot: ISnapshotTree | null) {
         this._baseSnapshotTree = snapshot;
-        this._baseIsLatest = true;
+        if (this._state === SummaryTrackerState.Initial) {
+            this._state = SummaryTrackerState.Valid;
+        }
     }
 }
