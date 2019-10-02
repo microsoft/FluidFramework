ade6e7ef2abb575cba99c57fddd566f737875da6
Vlad Sudzilouski <vladsud@microsoft.com>
2019-10-01 20:40:48 -0700

    Chery pick 61de68 from release/10.0 (#227)
    Logging improvements (#177): 10.0 -> master port
    
    Changes in logging structure based on observing telemetry from dogfooders.
    
    1) The main reason for these changes - make it easier to have initial triage decisions based on looking at event counts. To support that, I chose not to add some information as properties, but as a suffix on event name itself. For example, all perf events will have _start, _end or _cancel suffixes, making it easier to count them and see discrepancies / failures right from initial triage screen.
    2) Added namespaces and clarified names in couple places.
    3) Added telemetry for using expired tokens (and thus needing extra roundtrip to get data, in addition to fetching new token).
    4) Include snapshot properties (count of trees, blobs, ops).
    5) Extra event on first being connected, to easier measure user experience
    6) cancel events for perf events can contain error information
    7) Handling of getLatest was slightly incorrect in case of using expired token - we would fall back to fetching trees one by one. Fixed.

--- packages/drivers/odsp-socket-storage/src/OdspDocumentService.ts
+++ packages/drivers/odsp-socket-storage/src/OdspDocumentService.ts
@@ -45,6 +45,8 @@ export class OdspDocumentService implements IDocumentService {
 
     private storageManager?: OdspDocumentStorageManager;
 
+    private readonly getStorageToken: (refresh: boolean) => Promise<string | null>;
+
     /**
      * @param appId - app id used for telemetry for network requests
      * @param hashedDocumentId - A unique identifer for the document. The "hashed" here implies that the contents of this string
@@ -69,13 +71,23 @@ export class OdspDocumentService implements IDocumentService {
         driveId: string,
         itemId: string,
         private readonly snapshotStorageUrl: string,
-        readonly getStorageToken: (siteUrl: string, refresh: boolean) => Promise<string | null>,
+        getStorageToken: (siteUrl: string, refresh: boolean) => Promise<string | null>,
         readonly getWebsocketToken: () => Promise<string | null>,
         private readonly logger: ITelemetryLogger,
         private readonly storageFetchWrapper: IFetchWrapper,
         private readonly deltasFetchWrapper: IFetchWrapper,
         private readonly socketIOClientP: Promise<SocketIOClientStatic>,
     ) {
+        this.getStorageToken = (refresh: boolean) => {
+            if (refresh) {
+                // Potential perf issue:
+                // Host should optimize and provide non-expired tokens on all critical paths.
+                // Exceptions: race conditions around expiration, revoked tokens, host that does not care (fluid-fetcher)
+                this.logger.sendTelemetryEvent({eventName: "StorageTokenRefresh"});
+            }
+            return getStorageToken(this.siteUrl, refresh);
+        };
+
         this.websocketEndpointRequestThrottler = new SinglePromise(() =>
             getSocketStorageDiscovery(
                 appId,
@@ -83,7 +95,7 @@ export class OdspDocumentService implements IDocumentService {
                 itemId,
                 siteUrl,
                 logger,
-                getStorageToken,
+                this.getStorageToken,
             ),
         );
     }
@@ -102,7 +114,7 @@ export class OdspDocumentService implements IDocumentService {
             this.snapshotStorageUrl,
             latestSha,
             this.storageFetchWrapper,
-            (refresh: boolean) => this.getStorageToken(this.siteUrl, refresh),
+            this.getStorageToken,
             this.logger,
             true,
         );
@@ -123,7 +135,7 @@ export class OdspDocumentService implements IDocumentService {
               // any other requests are result of catching up on missing ops and are coming after websocket is established (or reconnected),
               // and thus we already have fresh join session call.
               // That said, tools like Fluid-fetcher will hit it, so that's valid code path.
-              this.logger.sendErrorEvent({ eventName: "OdspOpStreamPerf" });
+              this.logger.sendErrorEvent({ eventName: "ExtraJoinSessionCall" });
 
               this.websocketEndpointP = this.websocketEndpointRequestThrottler.response;
             }
@@ -136,7 +148,7 @@ export class OdspDocumentService implements IDocumentService {
             urlProvider,
             this.deltasFetchWrapper,
             this.storageManager ? this.storageManager.ops : undefined,
-            (refresh: boolean) => this.getStorageToken(this.siteUrl, refresh),
+            this.getStorageToken,
         );
     }
 
