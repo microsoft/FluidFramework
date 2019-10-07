7fea35bdc8bad22ec5bdf0464568660e68eb79fc
Matt Rakow <marakow@microsoft.com>
2019-10-07 10:27:15 -0700

    Add utils documentation

--- packages/runtime/shared-object-common/src/utils.ts
+++ packages/runtime/shared-object-common/src/utils.ts
@@ -5,6 +5,15 @@
 
 import { IComponentHandle, IComponentHandleContext, IComponentSerializer } from "@microsoft/fluid-component-core-interfaces";
 
+/**
+ * Given a mostly-plain object that may have handle objects embedded within, will return a fully-plain object
+ * where the handle objects have been replaced with a serializable form.
+ * @param value - The mostly-plain object
+ * @param serializer - The serializer that knows how to convert handles into serializable format
+ * @param context - The handle context for the container
+ * @param bind - Bind any other handles we find in the object against this given handle.
+ * @returns The fully-plain object
+ */
 export function serializeHandles(
     value: any,
     serializer: IComponentSerializer,
@@ -19,6 +28,14 @@ export function serializeHandles(
         : value;
 }
 
+/**
+ * Given a fully-plain object that may have serializable-form handles within, will return the mostly-plain object
+ * with handle objects created instead.
+ * @param value - The fully-plain object
+ * @param serializer - The serializer that knows how to convert serializable-form handles into handle objects
+ * @param context - The handle context for the container
+ * @returns The mostly-plain object with handle objects within
+ */
 export function parseHandles(
     value: any,
     serializer: IComponentSerializer,
