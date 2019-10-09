55b312e4474ba470efda9033499017735de5ba70
marap <marap@microsoft.com>
2019-10-09 11:03:31 -0700

    Update IComponentClipboardData interface name

--- packages/framework/framework-definitions/src/clipboard/IComponentClipboardProvider.ts
+++ packages/framework/framework-definitions/src/clipboard/IComponentClipboardProvider.ts
@@ -3,17 +3,17 @@
  * Licensed under the MIT License.
  */
 declare module "@microsoft/fluid-component-core-interfaces" {
-  export interface IComponent extends Readonly<Partial<IProvideComponentClipboardProvider>> {}
+  export interface IComponent extends Readonly<Partial<IProvideComponentClipboardData>> {}
 }
 
-export interface IProvideComponentClipboardProvider {
-  readonly IComponentClipboardProvider: IComponentClipboardProvider;
+export interface IProvideComponentClipboardData {
+  readonly IComponentClipboardDataProvider: IComponentClipboardData;
 }
 
 /**
  * During copy, component hosts can use their “selection” or equivalent concept to identify any nested
  * components involved. If the selection includes nested components, the host component should use the
- * **IComponentClipboardProvider** interface on each of these nested component to acquire their contribution
+ * **IComponentClipboardData** interface on each of these nested component to acquire their contribution
  * to the copied content, and combine it with its own copied content. These nested components should do the
  * same with their own nested components. What content a component provides is entirely up to it.
  *
@@ -28,7 +28,7 @@ export interface IProvideComponentClipboardProvider {
 
 export const fluidUrlAttributeName = "fluidUrl";
 
-export interface IComponentClipboardProvider extends IProvideComponentClipboardProvider {
+export interface IComponentClipboardData extends IProvideComponentClipboardData {
   /**
    * Return a new/unattached HTMLElement representation of the entire component instance to be
    * serialized for the html clipboard slot.
