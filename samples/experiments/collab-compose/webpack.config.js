d92b8f922cf4fed61bdfdbab198ba480c1e0a58c
Kurt Berglund <kurtb@microsoft.com>
2019-10-09 21:55:12 -0700

    Two policy check fixes

--- samples/experiments/collab-compose/webpack.config.js
+++ samples/experiments/collab-compose/webpack.config.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 const devCerts = require("office-addin-dev-certs");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
@@ -64,7 +69,7 @@ module.exports = async (env, options) => {
         "Access-Control-Allow-Origin": "*"
       },      
       https: (options.https !== undefined) ? options.https : await devCerts.getHttpsServerOptions(),
-      port: process.env.npm_package_config_dev_server_port || 3000
+      port: 3030 || process.env.npm_package_config_dev_server_port
     }
   };
 
