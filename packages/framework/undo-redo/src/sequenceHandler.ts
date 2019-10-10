630f40830c05aec464796c64ee72db36513ef590
Tony <anthony.murphy@microsoft.com>
2019-10-10 14:48:31 -0700

    coalesce tracking groups before revert

--- packages/framework/undo-redo/src/sequenceHandler.ts
+++ packages/framework/undo-redo/src/sequenceHandler.ts
@@ -7,6 +7,7 @@ import {
     IJSONSegment,
     ISegment,
     matchProperties,
+    MergeTreeDeltaOperationType,
     MergeTreeDeltaType,
     PropertySet,
     ReferenceType,
@@ -21,8 +22,13 @@ import { IRevertable, UndoRedoStackManager } from "./undoRedoStackManager";
  */
 export class SharedSequenceUndoRedoHandler {
 
+    private readonly sequences =
+        new Map<SharedSegmentSequence<ISegment>, SharedSequenceRevertable | undefined>();
+
     constructor(private readonly stackManager: UndoRedoStackManager) {
-     }
+        this.stackManager.on("operationClosed", () => this.sequences.clear());
+        this.stackManager.on("changePushed", () => this.sequences.clear());
+    }
 
     public attachSequence<T extends ISegment>(sequence: SharedSegmentSequence<T>) {
         sequence.on("sequenceDelta", this.sequenceDeltaHandler);
@@ -34,79 +40,75 @@ export class SharedSequenceUndoRedoHandler {
 
     private readonly sequenceDeltaHandler = (event: SequenceDeltaEvent, target: SharedSegmentSequence<ISegment>) => {
         if (event.isLocal) {
-            this.stackManager.push(new SharedSequenceRevertable(event, target));
+            let revertable = this.sequences.get(target);
+            if (revertable === undefined) {
+                revertable = new SharedSequenceRevertable(target);
+                this.stackManager.push(revertable);
+                this.sequences.set(target, revertable);
+            }
+            revertable.add(event);
         }
     }
 }
 
+interface ITrackedSharedSequenceRevertable {
+    trackingGroup: TrackingGroup;
+    propertyDelta: PropertySet;
+    operation: MergeTreeDeltaOperationType;
+}
+
 /**
- * Tracks a change on a shared sequence and allows reverting it
+ * Tracks a changes on a shared sequence and allows reverting them
  */
 export class SharedSequenceRevertable implements IRevertable {
 
-    private readonly tracking: { trackingGroup: TrackingGroup, propertyDelta: PropertySet }[];
+    private readonly tracking: ITrackedSharedSequenceRevertable[];
 
     constructor(
-        public readonly event: SequenceDeltaEvent,
         public readonly sequence: SharedSegmentSequence<ISegment>,
     ) {
-        const tracking: { trackingGroup: TrackingGroup, propertyDelta: PropertySet }[] = [];
+        this.tracking = [];
+    }
+
+    public add(event: SequenceDeltaEvent) {
         if (event.ranges.length > 0) {
-            let current = { trackingGroup: new TrackingGroup(), propertyDelta: event.ranges[0].propertyDeltas };
-            tracking.push(current);
-            current.trackingGroup.link(event.ranges[0].segment);
-            for (let i = 1; i < event.ranges.length; i++) {
-                if (matchProperties(current.propertyDelta, event.ranges[i].propertyDeltas)) {
-                    current.trackingGroup.link(event.ranges[i].segment);
+            let current = this.tracking.length > 0 ? this.tracking[this.tracking.length - 1] : undefined;
+            for (const range of event.ranges) {
+                if (current !== undefined
+                    && current.operation === event.deltaOperation
+                    && matchProperties(current.propertyDelta, range.propertyDeltas)) {
+                    current.trackingGroup.link(range.segment);
                 } else {
                     const tg = new TrackingGroup();
-                    tg.link(event.ranges[i].segment);
-                    current = { trackingGroup: tg, propertyDelta: event.ranges[i].propertyDeltas };
-                    tracking.push(current);
+                    tg.link(range.segment);
+                    current = {
+                            trackingGroup: tg,
+                            propertyDelta: range.propertyDeltas,
+                            operation: event.deltaOperation as MergeTreeDeltaOperationType,
+                        };
+                    this.tracking.push(current);
                 }
             }
         }
-        this.tracking = tracking;
     }
 
     public revert() {
-        const sequence = this.sequence;
-
         this.tracking.forEach((tracked) => {
-            while (tracked.trackingGroup.size > 0) {
-                const sg = tracked.trackingGroup.segments[0];
-                sg.trackingCollection.unlink(tracked.trackingGroup);
-                switch (this.event.deltaOperation) {
-                    case MergeTreeDeltaType.INSERT:
-                        if (sg.removedSeq === undefined) {
-                            const start = sequence.getPosition(sg);
-                            this.sequence.removeRange(start, start + sg.cachedLength);
-                        }
-                        break;
+            switch (tracked.operation) {
+                case MergeTreeDeltaType.INSERT:
+                    this.revertInsert(tracked);
+                    break;
 
-                    case MergeTreeDeltaType.REMOVE:
-                        const insertSegment = this.sequence.segmentFromSpec(sg.toJSONObject() as IJSONSegment);
-                        this.sequence.insertAtReferencePosition(
-                                this.sequence.createPositionReference(sg, 0, ReferenceType.Transient),
-                                insertSegment);
-                        sg.trackingCollection.trackingGroups.forEach((tg) => {
-                            tg.link(insertSegment);
-                            tg.unlink(sg);
-                        });
-                        break;
+                case MergeTreeDeltaType.REMOVE:
+                    this.revertRemove(tracked);
+                    break;
 
-                    case MergeTreeDeltaType.ANNOTATE:
-                        if (sg.removedSeq === undefined) {
-                            const start = sequence.getPosition(sg);
-                            this.sequence.annotateRange(
-                                    start,
-                                    start + sg.cachedLength,
-                                    tracked.propertyDelta,
-                                    undefined);
-                        }
-                    default:
-                        throw new Error("operationt type not revertable");
-                }
+                case MergeTreeDeltaType.ANNOTATE:
+                    this.revertAnnotate(tracked);
+                    break;
+
+                default:
+                    throw new Error("operation type not revertable");
             }
         });
     }
@@ -114,5 +116,62 @@ export class SharedSequenceRevertable implements IRevertable {
     public disgard() {
         this.tracking.forEach((tracked) => tracked.trackingGroup.segments.forEach(
             (sg) => sg.trackingCollection.unlink(tracked.trackingGroup)));
+    }
+
+    private revertInsert(tracked: ITrackedSharedSequenceRevertable) {
+        this.coalesceTrackingGroupToRangesAndRevert(
+            tracked.trackingGroup,
+            (start, end) => this.sequence.removeRange(start, end));
+    }
+
+    private revertRemove(tracked: ITrackedSharedSequenceRevertable) {
+        tracked.trackingGroup.segments.forEach((sg) => {
+            sg.trackingCollection.unlink(tracked.trackingGroup);
+            const insertSegment = this.sequence.segmentFromSpec(sg.toJSONObject() as IJSONSegment);
+            this.sequence.insertAtReferencePosition(
+                this.sequence.createPositionReference(sg, 0, ReferenceType.Transient),
+                insertSegment);
+            sg.trackingCollection.trackingGroups.forEach((tg) => {
+                tg.link(insertSegment);
+                tg.unlink(sg);
+            });
+        });
+    }
+
+    private revertAnnotate(tracked: ITrackedSharedSequenceRevertable) {
+        this.coalesceTrackingGroupToRangesAndRevert(
+            tracked.trackingGroup,
+            (start, end) => this.sequence.annotateRange(
+                start,
+                end,
+                tracked.propertyDelta,
+                undefined));
+    }
+
+    private coalesceTrackingGroupToRangesAndRevert(
+        trackingGroup: TrackingGroup,
+        revertAction: (start: number, end: number) => void,
+    ) {
+        if (trackingGroup.size > 0) {
+            let start = this.sequence.getPosition(trackingGroup.segments[0]);
+            let end = start + trackingGroup.segments[0].cachedLength;
+            trackingGroup.unlink(trackingGroup.segments[0]);
+            while (trackingGroup.size > 0) {
+                const segment = trackingGroup.segments[0];
+                trackingGroup.unlink(segment);
+                if (segment.removedSeq === undefined) {
+                    const segStart = this.sequence.getPosition(segment);
+                    const segEnd = segStart + segment.cachedLength;
+                    if (end === segStart) {
+                        end = segEnd;
+                    } else {
+                        revertAction(start, end);
+                        start = segStart;
+                        end = segEnd;
+                    }
+                }
+            }
+            revertAction(start, end);
+        }
     }
 }
