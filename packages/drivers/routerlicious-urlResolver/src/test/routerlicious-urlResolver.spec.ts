51424c2c91c454e96055f1d1768a665203e4da27
Jatin Garg <48029724+jatgarg@users.noreply.github.com>
2019-10-11 11:27:33 -0700

    Adding urls resolvers in driver (#245)
    * Add odspurlresolver example for office.com urls
    
    * move IODsp url resolver to protocol definitions
    
    * add spo url resolver
    
    * move packages out
    
    * move iodspurlresolver to driver
    
    * Add tests
    
    * adding url resolver for fluid app
    
    * adding routerlicious url resolver
    
    * adding routerlicious url resolver
    
    * rename resolvers
    
    * rename
    
    * add in docvkerfile
    
    * change package names
    
    * modify fluit fetcher
    
    * change logic to refresh tokens
    
    * correct refresh token logic
    
    * taking pr suggestions
    
    * fixing bug
    
    * removing odsp hosted
    
    * fixing fluid fetcher
    
    * delete office url resolver
    
    * put dummy values

---
+++ packages/drivers/routerlicious-urlResolver/src/test/routerlicious-urlResolver.spec.ts
@@ -0,0 +1,35 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+// tslint:disable: max-line-length
+import { IFluidResolvedUrl } from "@microsoft/fluid-protocol-definitions";
+import * as assert from "assert";
+import { RouterliciousUrlResolver } from "../urlResolver";
+
+describe("Routerlicious Url Resolver", () => {
+
+    const token = "dummy";
+    it("Should resolve the Routerlicious urls correctly", async () => {
+        const urlResolver = new RouterliciousUrlResolver(undefined, token, []);
+        const url: string = "https://www.wu2.prague.office-int.com/loader/fluid/thinkable-list?chaincode=@fluid-example/shared-text@0.11.14146";
+        const resolved = (await urlResolver.resolve({ url })) as IFluidResolvedUrl;
+        assert.equal(resolved.tokens.jwt, token, "Token does not match");
+        assert.equal(resolved.endpoints.storageUrl, "https://historian.wu2.prague.office-int.com/repos/fluid", "Storage url does not match");
+        assert.equal(resolved.endpoints.deltaStorageUrl, "https://alfred.wu2.prague.office-int.com/deltas/fluid/thinkable-list", "Delta storage url does not match");
+        assert.equal(resolved.endpoints.ordererUrl, "https://alfred.wu2.prague.office-int.com", "Orderer url does not match");
+        assert.equal(resolved.url, "fluid://wu2.prague.office-int.com/fluid/thinkable-list?chaincode=@fluid-example/shared-text@0.11.14146", "FluidUrl does not match");
+    });
+
+    it("Should resolve the localhost urls correctly", async () => {
+        const urlResolver = new RouterliciousUrlResolver(undefined, token, []);
+        const url: string = "http://localhost:3000/loader/fluid/damp-competition?chaincode=@fluid-example/shared-text@^0.11.0";
+        const resolved = (await urlResolver.resolve({ url })) as IFluidResolvedUrl;
+        assert.equal(resolved.tokens.jwt, token, "Token does not match");
+        assert.equal(resolved.endpoints.storageUrl, "http://localhost:3001/repos/fluid", "Storage url does not match");
+        assert.equal(resolved.endpoints.deltaStorageUrl, "http://localhost:3003/deltas/fluid/damp-competition", "Delta storage url does not match");
+        assert.equal(resolved.endpoints.ordererUrl, "http://localhost:3003/", "Orderer url does not match");
+        assert.equal(resolved.url, "fluid://localhost:3003/fluid/damp-competition?chaincode=@fluid-example/shared-text@^0.11.0", "FluidUrl does not match");
+    });
+});
