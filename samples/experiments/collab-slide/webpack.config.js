ca59616c3d5c707d2f867f6a078a27909db93c82
Kurt Berglund <kurtb@microsoft.com>
2019-10-11 17:02:26 -0700

    Copyright headers

--- samples/experiments/collab-slide/webpack.config.js
+++ samples/experiments/collab-slide/webpack.config.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
 const devCerts = require("office-addin-dev-certs");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
