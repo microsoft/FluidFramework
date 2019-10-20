5c953174b039402b5da52e4e9b2bb7c4f8c1dab1
Tanvir Aumi <mdaumi@microsoft.com>
2019-10-19 21:44:18 -0700

    Worker loader implements IComponentRunnable

--- packages/loader/container-loader/src/workerLoader.ts
+++ packages/loader/container-loader/src/workerLoader.ts
@@ -9,7 +9,7 @@ import {
     IRequest,
     IResponse,
 } from "@microsoft/fluid-component-core-interfaces";
-import { ILoader } from "@microsoft/fluid-container-definitions";
+import { IContainer, ILoader } from "@microsoft/fluid-container-definitions";
 import { IFluidResolvedUrl } from "@microsoft/fluid-protocol-definitions";
 import * as Comlink from "comlink";
 
@@ -28,7 +28,7 @@ interface IProxyLoader extends ILoader, IComponentRunnable {
 }
 
 // Proxies request to web worker loader.
-export class WorkerLoader {
+export class WorkerLoader implements ILoader, IComponentRunnable, IComponentRouter {
     public static async load(
         id: string,
         version: string | null | undefined,
@@ -54,21 +54,16 @@ export class WorkerLoader {
     constructor(private readonly proxy: Comlink.Remote<IProxyLoader>) {
     }
 
+    public get IComponentRouter() { return this; }
+    public get IComponentRunnable() { return this; }
+
     public async request(request: IRequest): Promise<IResponse> {
         const response = await this.proxy.request(request);
         if (response.status !== 200 || response.mimeType !== "fluid/component") {
             return response;
         }
-        return { status: 200, mimeType: "fluid/component", value: new Runnable(this.proxy) };
+        return { status: 200, mimeType: "fluid/component", value: this };
     }
-}
-
-// Proxies request to IComponentRunnable.
-class Runnable implements IComponentRouter, IComponentRunnable {
-    constructor(private readonly proxy: Comlink.Remote<IProxyLoader>) {}
-
-    public get IComponentRouter() { return this; }
-    public get IComponentRunnable() { return this; }
 
     public async run(...args: any[]): Promise<void> {
         return this.proxy.run(...args);
@@ -78,11 +73,7 @@ class Runnable implements IComponentRouter, IComponentRunnable {
         return this.proxy.stop(reason);
     }
 
-    public async request(request: IRequest): Promise<IResponse> {
-        return {
-            mimeType: "fluid/component",
-            status: 200,
-            value: this,
-        };
+    public async resolve(request: IRequest): Promise<IContainer> {
+        return this.proxy.resolve(request);
     }
 }
