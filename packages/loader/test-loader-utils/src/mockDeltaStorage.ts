bb79cad33f8937e91c7bf0f8c1b5ea43390d9a4c
Arin Taylor <artaylor@microsoft.com>
2019-10-25 14:14:49 -0700

    Send no-op on quorum approval to expedite commit phase (#10)
    * Send immediate no-op on quorum approval to expedite commit event
    
    * Update other references to ProtocolOpHandler constructors
    
    * Change no-op decision to be in ProtocolOpHandler and Quorum, but handle it in DeltaManager
    
    * Minor changes for PR
    
    * Add a few unit tests to DeltaManager for update min seq num
    
    * Add test-loader-utils to Dockerfile
    
    * Update unit tests to use sinon for timeout mocking
    
    * Updated for merge conflicts - readonly mode and package renames
    
    * Update unit tests for merge changes
    
    * Refactor a little so that immediate noop for code proposals are only handled in quorum
    
    * I guess package-lock

---
+++ packages/loader/test-loader-utils/src/mockDeltaStorage.ts
@@ -0,0 +1,35 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {
+    IDocumentDeltaStorageService,
+    ISequencedDocumentMessage,
+} from "@microsoft/fluid-protocol-definitions";
+
+/**
+ * Mock Document Delta Storage Service for testing
+ */
+export class MockDocumentDeltaStorageService implements IDocumentDeltaStorageService {
+    constructor(private readonly messages: ISequencedDocumentMessage[]) {
+        this.messages = messages.sort((a, b) => b.sequenceNumber - a.sequenceNumber);
+    }
+
+    public async get(from?: number, to?: number): Promise<ISequencedDocumentMessage[]> {
+        const ret: ISequencedDocumentMessage[] = [];
+        let index: number = -1;
+
+        // find first
+        if (from) {
+            while (this.messages[++index].sequenceNumber <= from) {}
+        }
+
+        // start reading
+        while (++index < this.messages.length && (!to || this.messages[++index].sequenceNumber < to)) {
+            ret.push(this.messages[index]);
+        }
+
+        return ret;
+    }
+}
