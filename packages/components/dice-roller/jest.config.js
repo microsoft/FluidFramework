cbbaccd3c739bda45f6a666d5e83009cc0d0b6b8
Skyler <skjokiel@microsoft.com>
2019-10-30 11:25:03 -0700

    dice roller working

--- packages/components/dice-roller/jest.config.js
+++ packages/components/dice-roller/jest.config.js
@@ -6,6 +6,6 @@ module.exports = {
   testMatch: ["**/?(*.)+(spec|test).[t]s"],
   testPathIgnorePatterns: ['/node_modules/', 'dist'],
   transform: {
-		"^.+\\.ts?$": "ts-jest"
-	},
+    "^.+\\.ts?$": "ts-jest"
+  },
 };
