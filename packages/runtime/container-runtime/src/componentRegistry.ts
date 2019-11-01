fe9d71fb80b82dd78831404aa1fa5d99512c96b9
Tony <anthony.murphy@microsoft.com>
2019-11-01 10:38:59 -0700

    remove function support from component registry

--- packages/runtime/container-runtime/src/componentRegistry.ts
+++ packages/runtime/container-runtime/src/componentRegistry.ts
@@ -10,26 +10,19 @@ import {
 
 export class ComponentRegistry implements IComponentRegistry {
 
-    private readonly map: Map<string, Promise<ComponentRegistryEntry> | (() => Promise<ComponentRegistryEntry>)>;
+    private readonly map: Map<string, Promise<ComponentRegistryEntry> >;
 
     public get IComponentRegistry() { return this; }
 
     constructor(namedEntries: NamedComponentRegistryEntries) {
 
-        this.map =
-        new Map<string, Promise<ComponentRegistryEntry> | (() => Promise<ComponentRegistryEntry>)>(
-            namedEntries);
+        this.map = new Map(namedEntries);
     }
 
     public async get(name: string): Promise<ComponentRegistryEntry | undefined> {
 
         if (this.map.has(name)) {
-            const entry = this.map.get(name);
-            if (typeof entry === "function") {
-                return entry();
-            } else {
-                return entry;
-            }
+            return this.map.get(name);
         }
 
         return undefined;
