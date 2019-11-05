d03e60a6ff5c2c03444fb8285a60ea36e2c168f9
Jatin Garg <jatgarg@microsoft.com>
2019-11-04 16:59:01 -0800

    adding telemetry for ops on reconnect

--- packages/loader/container-loader/src/container.ts
+++ packages/loader/container-loader/src/container.ts
@@ -171,6 +171,7 @@ export class Container extends EventEmitterWithErrorHandling implements IContain
 
     private firstConnection = true;
     private readonly connectionTransitionTimes: number[] = [];
+    private messageCountAfterDisconnection: number | undefined;
 
     private _closed = false;
 
@@ -984,9 +985,17 @@ export class Container extends EventEmitterWithErrorHandling implements IContain
 
     private propagateConnectionState() {
         assert(this.loaded);
+        if (this._connectionState === ConnectionState.Connected && !this.firstConnection) {
+            this.messageCountAfterDisconnection = 0;
+        }
         this.context!.changeConnectionState(this._connectionState, this.clientId!, this._version!);
         this.protocolHandler!.quorum.changeConnectionState(this._connectionState, this.clientId!);
         raiseConnectedEvent(this, this._connectionState, this.clientId!);
+        if (this.messageCountAfterDisconnection) {
+            this.logger.sendTelemetryEvent(
+                { eventName: "OpsSentOnReconnect", count: this.messageCountAfterDisconnection });
+            this.messageCountAfterDisconnection = undefined;
+        }
     }
 
     private submitMessage(type: MessageType, contents: any, batch?: boolean, metadata?: any): number {
@@ -995,6 +1004,9 @@ export class Container extends EventEmitterWithErrorHandling implements IContain
             return -1;
         }
 
+        if (this.messageCountAfterDisconnection) {
+            this.messageCountAfterDisconnection += 1;
+        }
         return this._deltaManager!.submit(type, contents, batch, metadata);
     }
 
