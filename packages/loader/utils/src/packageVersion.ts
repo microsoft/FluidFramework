961c8ca7617a7ed43b48653f4beef95ff12c0028
Wes Carlson <wesley.carlson@microsoft.com>
2019-11-05 16:55:42 -0800

    update to 0.12

--- packages/loader/utils/src/packageVersion.ts
+++ packages/loader/utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@microsoft/fluid-core-utils";
-export const pkgVersion = "0.11.0";
+export const pkgVersion = "0.12.0";
--- packages/server/memory-orderer/src/packageVersion.ts
+++ packages/server/memory-orderer/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@microsoft/fluid-server-memory-orderer";
-export const pkgVersion = "0.11.0";
+export const pkgVersion = "0.12.0";
