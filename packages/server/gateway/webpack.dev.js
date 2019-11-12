2f719f9cf15f30901f2854d47095ad033f81eaf8
Curtis Man <curtism@microsoft.com>
2019-11-11 16:50:17 -0800

    Fluid-Build: Add webpack config to the set of file we will monitor changes for webpack tasks

--- packages/server/gateway/webpack.dev.js
+++ packages/server/gateway/webpack.dev.js
@@ -12,7 +12,7 @@ module.exports = {
         filename: "[name].js",
         library: "[name]",
         // https://github.com/webpack/webpack/issues/5767
-        // https://github.com/webpack/webpack/issues/7939            
+        // https://github.com/webpack/webpack/issues/7939
         devtoolNamespace: "routerlicious"
     },
 }
