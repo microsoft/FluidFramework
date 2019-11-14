2dde413e4ac75a2596b4fdb9faa77e723bc40d17
vladsud <vladsud@microsoft.com>
2019-11-14 11:54:05 -0800

    Initial implementation

---
+++ packages/test/end-to-end/src/test/primedComponent.spec.ts
@@ -0,0 +1,53 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { PrimedComponent, PrimedComponentFactory } from "@microsoft/fluid-aqueduct";
+import { IComponentHandle } from "@microsoft/fluid-component-core-interfaces";
+import { TestHost } from "@microsoft/fluid-local-test-server";
+import * as assert from "assert";
+
+const PrimiedType = "@microsoft/fluid-primsedComponent";
+
+/**
+ * My sample component
+ */
+class Component extends PrimedComponent {
+}
+
+describe("PrimedComponent", () => {
+
+    describe("Blob support", () => {
+        const componentId = "id";
+        let host: TestHost;
+        let component: Component;
+
+        beforeEach(async () => {
+            const factory = new PrimedComponentFactory(Component, []);
+            host = new TestHost([
+                [PrimiedType, Promise.resolve(factory)],
+            ]);
+            component = await host.createAndAttachComponent(componentId, PrimiedType);
+        });
+
+        afterEach(async () => { await host.close(); });
+
+        it("Blob support", async () => {
+            const handle = await component.writeBlob("aaaa");
+            assert(await handle.get<string>() === "aaaa");
+            component.root.set("key", handle);
+
+            const handle2 = component.root.get<IComponentHandle>("key");
+            const value2 = await handle2.get<string>();
+            assert(value2 === "aaaa");
+
+            const host2 = host.clone();
+            await TestHost.sync(host, host2);
+            const component2 = await host2.getComponent<Component>(componentId);
+            const value = await component2.root.get<IComponentHandle>("key").get<string>();
+            assert(value === "aaaa");
+            await host2.close();
+        });
+    });
+});
