bd85bc280fddfb240f021b9e445287b0380b6258
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2019-11-15 09:09:58 -0800

    MergeTree: Improve comment in test

--- packages/runtime/merge-tree/src/test/snapshot.spec.ts
+++ packages/runtime/merge-tree/src/test/snapshot.spec.ts
@@ -90,16 +90,18 @@ class TestString {
     }
 
     private queue(op: IMergeTreeOp, increaseMsn: boolean) {
+        const refSeq = this.seq;
+        const seq = ++this.seq;
+
         this.pending.push(
             this.client.makeOpMessage(
                 op,
-                /* seq: */ this.seq + 1,
-                /* refSeq: */ this.seq,
+                seq,
+                refSeq,
                 this.client.longClientId,
-                /* minSeq: */ this.minSeq = increaseMsn
-                    ? this.seq + 1
+                this.minSeq = increaseMsn
+                    ? seq
                     : this.minSeq));
-        this.seq++;
     }
 }
 
@@ -107,19 +109,25 @@ describe("snapshot", () => {
     let str: TestString;
 
     beforeEach(() => {
-        str = new TestString("0");
+        str = new TestString("fakeId");
     });
 
     afterEach(async () => {
+        // Paranoid check that ensures `str` roundtrips through snapshot/load.  This helps to catch
+        // bugs that might be missed if the test case forgets to call/await `str.expect()`.
         await str.checkSnapshot();
     });
 
     it("excludes un-acked segments", async () => {
         str.append("0", /* increaseMsn: */ false);
+
+        // Invoke `load/getSnapshot()` directly instead of `str.expect()` to avoid ACKing the
+        // pending insert op.
         const client2 = await loadSnapshot(str.getSnapshot());
 
-        // Original client has inserted text, but the one loaded from the snapshot does not.
-        // This is because the un-ACKed ops will be fetched after the snapshot has loaded.
+        // Original client has inserted text, but the one loaded from the snapshot should be empty.
+        // This is because un-ACKed ops are not included in snapshots.  Instead, these ops are
+        // retransmitted and applied after the snapshot has loaded.
         assert.equal(str.getText(), "0");
         assert.equal(client2.getText(), "");
     });
@@ -134,18 +142,43 @@ describe("snapshot", () => {
         await str.expect("0");
     });
 
-    it("includes removed segments above the MSN", async () => {
+    it("includes removals of segments above the MSN", async () => {
         str.append("0x", /* increaseMsn: */ false);
-        str.append("1", /* increaseMsn: */ false);
         str.removeRange(1, 2, /* increaseMsn: */ false);
+        await str.expect("0");
+    });
+
+    it("includes removals above the MSN of segments below the MSN", async () => {
+        str.append("0x", /* increaseMsn: */ true);
+        str.removeRange(1, 2, /* increaseMsn: */ false);
+        await str.expect("0");
+    });
+
+    it("can insert segments after loading removed segment", async () => {
+        str.append("0x", /* increaseMsn: */ true);
+        str.removeRange(1, 2, /* increaseMsn: */ false);
+        await str.expect("0");
+        str.append("1", /* increaseMsn: */ false);
         await str.expect("01");
     });
 
-    it("can insert segments relative to loaded removed segment", async () => {
+    it("can insert segments relative to removed segment", async () => {
         str.append("0x", /* increaseMsn: */ false);
         str.append("2", /* increaseMsn: */ false);
         str.removeRange(1, 2, /* increaseMsn: */ false);
+        str.insert(1, "1", /* increaseMsn: */ false);
+        str.append("3", /* increaseMsn: */ false);
+        await str.expect("0123");
+    });
+
+    it("can insert segments relative to removed segment loaded from snapshot", async () => {
+        str.append("0x", /* increaseMsn: */ false);
+        str.append("2", /* increaseMsn: */ false);
+        str.removeRange(1, 2, /* increaseMsn: */ false);
+
+        // Note that calling str.expect() switches the underlying client to the one loaded from the snapshot.
         await str.expect("02");
+
         str.insert(1, "1", /* increaseMsn: */ false);
         str.append("3", /* increaseMsn: */ false);
         await str.expect("0123");
