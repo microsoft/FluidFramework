484b247eb931e88804c0fe9161494044c0ff7e8e
Vlad Sudzilouski <vladsud@microsoft.com>
2019-11-20 14:50:33 -0800

    Map: Breaking snapshots into multiple blobs (#597)
    Break out large map & directory properties into its own blobs when generating snapshots.
    New code is enabled only for consuming snapshots in new format, generation of snapshots in new format will be enabled in the future.

--- packages/runtime/shared-object-common/src/sharedObject.ts
+++ packages/runtime/shared-object-common/src/sharedObject.ts
@@ -132,8 +132,6 @@ export abstract class SharedObject extends EventEmitterWithErrorHandling impleme
 
         this.services = services;
 
-        await this.getOwnerSnapshot(services.objectStorage);
-
         await this.loadCore(
             branchId,
             services.objectStorage);
@@ -214,15 +212,6 @@ export abstract class SharedObject extends EventEmitterWithErrorHandling impleme
      * @returns The owner of the object if it is an OwnedSharedObject, otherwise undefined
      */
     protected setOwner(): string | undefined {
-        return;
-    }
-
-    /**
-     * Reads and sets the owner from storage if this is an ownedSharedObject
-     *
-     * @param storage - The storage used by the shared object
-     */
-    protected async getOwnerSnapshot(storage: IObjectStorageService): Promise<void> {
         return;
     }
 
