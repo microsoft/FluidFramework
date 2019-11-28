fb96a71269e27611d1f54e90ef841aa5fba89760
Tyler Butler <tylerbu@microsoft.com>
2019-11-27 20:27:13 -0800

    @microsoft/eslint-config-fluid

--- packages/utils/eslint-config-fluid/.eslintrc.js
+++ packages/utils/eslint-config-fluid/.eslintrc.js
@@ -1,4 +1,9 @@
-{
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = {
     "env": {
         "browser": true,
         "es6": true,
