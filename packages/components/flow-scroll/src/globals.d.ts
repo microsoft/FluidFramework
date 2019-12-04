6954b3120bd224f80f96a651cd9b3d2e31736a91
Tyler Butler <tylerbu@microsoft.com>
2019-12-04 06:59:39 -0800

    Eslint configs for all projects (#686)
    This is the first in several changes to switch from TSLint to ESLint per #314. This change includes the following:
    
    * Fixes/changes for the shared Fluid eslint config
    * Adds `.eslintrc.json` and `.eslintignore` for all lerna-managed packages
    * Adds new `eslint` and `eslint:fix` npm tasks to all lerna-managed packages
    
    This doesn't make any changes to functional code.

--- packages/components/flow-scroll/src/globals.d.ts
+++ packages/components/flow-scroll/src/globals.d.ts
@@ -3,4 +3,5 @@
  * Licensed under the MIT License.
  */
 
-declare var __webpack_public_path__: string;
+// eslint-disable-next-line @typescript-eslint/camelcase
+declare let __webpack_public_path__: string;
--- packages/utils/eslint-config-fluid/index.js
+++ packages/utils/eslint-config-fluid/index.js
@@ -3,4 +3,4 @@
  * Licensed under the MIT License.
  */
 
-module.exports = require("./.eslintrc.json")
+module.exports = require("./.eslintrc.js")
