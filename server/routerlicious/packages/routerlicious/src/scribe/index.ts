441bab6b73132f2fb96272916a5adb1db9e6f8a4
Marcin Juraszek <mail@marcinjuraszek.com>
2019-12-05 12:48:35 -0800

    Update index.ts

--- server/routerlicious/packages/routerlicious/src/scribe/index.ts
+++ server/routerlicious/packages/routerlicious/src/scribe/index.ts
@@ -20,7 +20,8 @@ export async function scribeCreate(config: Provider): Promise<IPartitionLambdaFa
     const mongoUrl = config.get("mongo:endpoint") as string;
     const documentsCollectionName = config.get("mongo:collectionNames:documents");
     const messagesCollectionName = config.get("mongo:collectionNames:scribeDeltas");
-    const historianUrl = config.get("worker:internalBlobStorageUrl") as string || config.get("worker:blobStorageUrl") as string;
+    const internalHistorianUrl = config.get("worker:internalBlobStorageUrl") as string;
+    const historianUrl = internalHistorianUrl || config.get("worker:blobStorageUrl") as string;
     const kafkaEndpoint = config.get("kafka:lib:endpoint");
     const kafkaLibrary = config.get("kafka:lib:name");
     const maxMessageSize = bytes.parse(config.get("kafka:maxMessageSize"));
