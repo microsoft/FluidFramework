46b8eaea377c5410409c53450ae23429ef2d7e0e
Tyler Butler <tylerbu@microsoft.com>
2019-12-06 17:20:18 -0800

    @fluid-example/draft-js

--- samples/chaincode/draft-js/src/MemberList.tsx
+++ samples/chaincode/draft-js/src/MemberList.tsx
@@ -7,30 +7,31 @@ import { IQuorum, ISequencedClient, ISequencedDocumentMessage } from "@microsoft
 import { ISharedObject } from "@microsoft/fluid-shared-object-base";
 import * as React from "react";
 
+// eslint-disable-next-line import/no-internal-modules, import/no-unassigned-import
 import "./css/MemberList.css";
 
 interface IProps {
-  /**
+    /**
    * From the component, this.runtime.getQuorum()
    */
-  quorum: IQuorum;
-  /**
+    quorum: IQuorum;
+    /**
    * Any Distributed Data Structure that users will interact with. A user is not shown in the
    * member list until the have touched this DDS.
    */
-  dds: ISharedObject;
-  style?: React.CSSProperties;
+    dds: ISharedObject;
+    style?: React.CSSProperties;
 }
 
 interface MemberInfo {
-  color: string;
-  name: string;
-  initials: string;
-  id: string;
+    color: string;
+    name: string;
+    initials: string;
+    id: string;
 }
 
 interface IState {
-  members: MemberInfo[];
+    members: MemberInfo[];
 }
 
 export { IProps as IMemberListProps };
@@ -39,11 +40,11 @@ export { IProps as IMemberListProps };
  * Get a semi-random, but deterministic, color for a given client
  */
 export const getColorForMember = (sc: ISequencedClient) => {
-  // Support 20 unique colors
-  const colorNumber = sc.sequenceNumber % 20;
-  // Spread them through the hue range with an overflow so the first few authors are likely
-  // to have very different colors
-  return `hsl(${(colorNumber * 360 / 7) % 360}, 100%, 35%)`;
+    // Support 20 unique colors
+    const colorNumber = sc.sequenceNumber % 20;
+    // Spread them through the hue range with an overflow so the first few authors are likely
+    // To have very different colors
+    return `hsl(${(colorNumber * 360 / 7) % 360}, 100%, 35%)`;
 };
 
 /**
@@ -51,73 +52,74 @@ export const getColorForMember = (sc: ISequencedClient) => {
  * @see getColorForMember to use the same color in other UI for each author
  */
 export class MemberList extends React.Component<IProps, IState> {
-  private readonly knownHumanMemberIds = new Set<string>();
+    private readonly knownHumanMemberIds = new Set<string>();
 
-  public constructor(props: IProps) {
-    super(props);
-    this.state = {
-      members: [],
-    };
-  }
-
-  public componentWillMount() {
-    this.updateMemberList();
-    this.props.quorum.on("addMember", this.updateMemberList);
-    this.props.quorum.on("removeMember", this.updateMemberList);
-    this.props.dds.on("op", (op: ISequencedDocumentMessage, isLocal: boolean) => {
-      if (!isLocal && !this.knownHumanMemberIds.has(op.clientId)) {
-        this.knownHumanMemberIds.add(op.clientId);
-        this.updateMemberList();
-      }
-    });
-  }
-
-  private readonly updateMemberList = () => {
-    const members = Array.from(
-      this.props.quorum
-        .getMembers()
-        .entries(),
-    )
-      .filter(([id, sc]) => this.knownHumanMemberIds.has(id))
-      .map(([id, sc]) => {
-        let name: string = (sc.client.user as any).displayName || (sc.client.user as any).name;
-        let initials: string;
-        if (name) {
-          const nameWords = name.replace("_", " ").split(" ");
-          initials = nameWords[0][0].toUpperCase();
-          if (nameWords.length > 1) {
-            initials += nameWords[nameWords.length - 1][0].toUpperCase();
-          }
-        } else {
-          name = sc.client.user.id.substring(0, 5);
-          initials = name.substring(0, 2);
-        }
-
-        return {
-          name,
-          initials,
-          color: getColorForMember(sc),
-          id,
+    public constructor(props: IProps) {
+        super(props);
+        this.state = {
+            members: [],
         };
-      });
+    }
 
-    this.setState({ members });
-  };
+    // eslint-disable-next-line react/no-deprecated
+    public componentWillMount() {
+        this.updateMemberList();
+        this.props.quorum.on("addMember", this.updateMemberList);
+        this.props.quorum.on("removeMember", this.updateMemberList);
+        this.props.dds.on("op", (op: ISequencedDocumentMessage, isLocal: boolean) => {
+            if (!isLocal && !this.knownHumanMemberIds.has(op.clientId)) {
+                this.knownHumanMemberIds.add(op.clientId);
+                this.updateMemberList();
+            }
+        });
+    }
 
-  public render() {
-    return (
-      <div className="memberList" style={this.props.style}>
-        {this.state.members.map((memberInfo) => (
-          <div
-            className="memberIcon"
-            key={memberInfo.id}
-            style={{ backgroundColor: memberInfo.color }}
-            title={memberInfo.name}
-          >
-            {memberInfo.initials}
-          </div>
-        ))}
-      </div>
-    );
-  }
+    private readonly updateMemberList = () => {
+        const members = Array.from(
+            this.props.quorum
+                .getMembers()
+                .entries(),
+        )
+            .filter(([id, sc]) => this.knownHumanMemberIds.has(id))
+            .map(([id, sc]) => {
+                let name: string = (sc.client.user as any).displayName || (sc.client.user as any).name;
+                let initials: string;
+                if (name) {
+                    const nameWords = name.replace("_", " ").split(" ");
+                    initials = nameWords[0][0].toUpperCase();
+                    if (nameWords.length > 1) {
+                        initials += nameWords[nameWords.length - 1][0].toUpperCase();
+                    }
+                } else {
+                    name = sc.client.user.id.substring(0, 5);
+                    initials = name.substring(0, 2);
+                }
+
+                return {
+                    name,
+                    initials,
+                    color: getColorForMember(sc),
+                    id,
+                };
+            });
+
+        this.setState({ members });
+    };
+
+    public render() {
+        return (
+            <div className="memberList" style={this.props.style}>
+                {this.state.members.map((memberInfo) => (
+                    <div
+                        className="memberIcon"
+                        key={memberInfo.id}
+                        style={{ backgroundColor: memberInfo.color }}
+                        title={memberInfo.name}
+                    >
+                        {memberInfo.initials}
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
--- samples/chaincode/draft-js/src/index.tsx
+++ samples/chaincode/draft-js/src/index.tsx
@@ -4,11 +4,11 @@
  */
 
 import {
-  PrimedComponent,
-  PrimedComponentFactory,
-  SimpleModuleInstantiationFactory,
+    PrimedComponent,
+    PrimedComponentFactory,
+    SimpleModuleInstantiationFactory,
 } from "@microsoft/fluid-aqueduct";
-import { IComponentHTMLVisual } from "@microsoft/fluid-component-core-interfaces"
+import { IComponentHTMLVisual } from "@microsoft/fluid-component-core-interfaces";
 import { SharedMap } from "@microsoft/fluid-map";
 import { SharedString } from "@microsoft/fluid-sequence";
 
@@ -18,49 +18,50 @@ import { FluidEditor } from "./FluidEditor";
 import { insertBlockStart } from "./RichTextAdapter";
 import { MemberList } from "./MemberList";
 
-// tslint:disable-next-line: no-var-requires no-require-imports
+// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 const pkg = require("../package.json");
 export const DraftJsName = pkg.name as string;
 
 export class DraftJsExample extends PrimedComponent implements IComponentHTMLVisual {
-  public get IComponentHTMLVisual() { return this; }
+    public get IComponentHTMLVisual() { return this; }
 
-  /**
-   * Do setup work here
-   */
-  protected async componentInitializingFirstTime() {
-    const text = SharedString.create(this.runtime);
-    insertBlockStart(text, 0);
-    text.insertText(text.getLength(), "starting text");
-    this.root.set("text", text.handle);
+    /**
+     * Do setup work here
+     */
+    // eslint-disable-next-line @typescript-eslint/require-await
+    protected async componentInitializingFirstTime() {
+        const text = SharedString.create(this.runtime);
+        insertBlockStart(text, 0);
+        text.insertText(text.getLength(), "starting text");
+        this.root.set("text", text.handle);
 
-    const authors = SharedMap.create(this.runtime);
-    this.root.set("authors", authors.handle);
-  }
+        const authors = SharedMap.create(this.runtime);
+        this.root.set("authors", authors.handle);
+    }
 
-  /**
-   * Will return a new view
-   */
-  public async render(div: HTMLElement) {
-    const [text, authors] = await Promise.all([this.root.get("text").get(), this.root.get("authors").get()]);
-    ReactDOM.render(
-      <div style={{ margin: "20px auto", maxWidth: 800 }}>
-        <MemberList quorum={this.runtime.getQuorum()} dds={authors} style={{ textAlign: "right" }} />
-        <FluidEditor sharedString={text} authors={authors} runtime={this.runtime} />
-      </div>,
-      div,
-    );
-    return div;
-  }
+    /**
+     * Will return a new view
+     */
+    public async render(div: HTMLElement) {
+        const [text, authors] = await Promise.all([this.root.get("text").get(), this.root.get("authors").get()]);
+        ReactDOM.render(
+            <div style={{ margin: "20px auto", maxWidth: 800 }}>
+                <MemberList quorum={this.runtime.getQuorum()} dds={authors} style={{ textAlign: "right" }} />
+                <FluidEditor sharedString={text} authors={authors} runtime={this.runtime} />
+            </div>,
+            div,
+        );
+        return div;
+    }
 }
 
 // ----- COMPONENT SETUP STUFF -----
 export const DraftInstantiationFactory = new PrimedComponentFactory(
-  DraftJsExample,
-  [SharedMap.getFactory(), SharedString.getFactory()],
+    DraftJsExample,
+    [SharedMap.getFactory(), SharedString.getFactory()],
 );
 
 export const fluidExport = new SimpleModuleInstantiationFactory(
-  DraftJsName,
-  new Map([[DraftJsName, Promise.resolve(DraftInstantiationFactory)]]),
+    DraftJsName,
+    new Map([[DraftJsName, Promise.resolve(DraftInstantiationFactory)]]),
 );
