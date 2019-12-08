96da6c2c03bb7b63cf8ad0957a5b2338d1aa7e26
Arin Taylor <artaylor@microsoft.com>
2019-12-07 23:25:44 -0800

    Handle undefined summaryData; give error category (#730)

--- packages/runtime/container-runtime/src/summarizer.ts
+++ packages/runtime/container-runtime/src/summarizer.ts
@@ -387,7 +387,7 @@ export class RunningSummarizer implements IDisposable {
     private async summarize(reason: string) {
         // wait to generate and send summary
         const summaryData = await this.generateSummaryWithLogging(reason);
-        if (!summaryData.submitted) {
+        if (!summaryData || !summaryData.submitted) {
             // did not send the summary op
             return;
         }
@@ -448,7 +448,7 @@ export class RunningSummarizer implements IDisposable {
         try {
             summaryData = await this.generateSummary();
         } catch (error) {
-            summarizingEvent.cancel({}, error);
+            summarizingEvent.cancel({ category: "error" }, error);
             return;
         }
 
