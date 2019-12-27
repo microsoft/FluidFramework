651d24ddcaa515343da4dd1fb4d0a332a7d2e0a0
Tony Murphy <anthonm@microsoft.com>
2019-12-26 16:32:15 -0800

    remove es-lint overrides

--- server/routerlicious/packages/tinylicious/.eslintrc.js
+++ server/routerlicious/packages/tinylicious/.eslintrc.js
@@ -8,7 +8,6 @@
         "@microsoft/eslint-config-fluid/no-tslint",
     ],
     rules: {
-        "@typescript-eslint/no-use-before-define": "off",
         "@typescript-eslint/strict-boolean-expressions": "off",
         "no-case-declarations": "off",
         "no-null/no-null": "off"
