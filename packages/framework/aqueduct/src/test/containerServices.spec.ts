38b29da837e05b8756d7951ada9940e2565bdf9c
Skyler <skjokiel@microsoft.com>
2020-01-16 11:45:27 -0800

    fix some lint errors

--- packages/framework/aqueduct/src/test/containerServices.spec.ts
+++ packages/framework/aqueduct/src/test/containerServices.spec.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
+/* eslint-disable @typescript-eslint/consistent-type-assertions */
 
 import * as assert from "assert";
 
@@ -44,7 +45,7 @@ describe("Routerlicious", () => {
         describe("InstanceContainerServiceFactory", () => {
             describe("getService", () => {
                 it("Two gets should return different objects", async () => {
-                    const serviceFactory = new InstanceContainerServiceFactory("id1", ContainerServiceMock)
+                    const serviceFactory = new InstanceContainerServiceFactory("id1", ContainerServiceMock);
 
                     const component1 = serviceFactory.getService({} as IHostRuntime);
                     const component2 = serviceFactory.getService({} as IHostRuntime);
@@ -60,9 +61,9 @@ describe("Routerlicious", () => {
                 const requestParser = new RequestParser({url:`/${serviceRoutePathRoot}`});
 
                 const response = await requestHandler(requestParser, {} as IHostRuntime);
-                
+
                 assert(response, "Response returned");
-                assert(response?.status === 400, "Returned 400 Status Code")
+                assert(response?.status === 400, "Returned 400 Status Code");
             });
 
             it("Unknown service should return 404 with no services", async () => {
@@ -70,20 +71,20 @@ describe("Routerlicious", () => {
                 const requestParser = new RequestParser({url:`/${serviceRoutePathRoot}/id1`});
 
                 const response = await requestHandler(requestParser, {} as IHostRuntime);
-                
+
                 assert(response, "Response returned");
-                assert(response?.status === 404, "Returned 404 Status Code")
+                assert(response?.status === 404, "Returned 404 Status Code");
             });
 
             it("Unknown service should return 404 with services", async () => {
-                const serviceFactory1 = new SingletonContainerServiceFactory("id1", ContainerServiceMock)
+                const serviceFactory1 = new SingletonContainerServiceFactory("id1", ContainerServiceMock);
                 const requestHandler = generateContainerServicesRequestHandler([serviceFactory1]);
                 const requestParser = new RequestParser({url:`/${serviceRoutePathRoot}/id2`});
 
                 const response = await requestHandler(requestParser, {} as IHostRuntime);
-                
+
                 assert(response, "Response returned");
-                assert(response?.status === 404, "Returned 404 Status Code")
+                assert(response?.status === 404, "Returned 404 Status Code");
             });
 
             it("Correct service should be returned with single service", async () => {
@@ -94,9 +95,9 @@ describe("Routerlicious", () => {
                 const requestParser = new RequestParser({url:`/${serviceRoutePathRoot}/id1`});
 
                 const response = await requestHandler(requestParser, {} as IHostRuntime);
-                
+
                 assert(response, "Response returned");
-                assert(response?.status === 200, "Returned 200 Status Code")
+                assert(response?.status === 200, "Returned 200 Status Code");
                 assert(response?.value === service1, "Returned expected service");
             });
 
@@ -108,9 +109,9 @@ describe("Routerlicious", () => {
                 const requestParser = new RequestParser({url:`/${serviceRoutePathRoot}/id2`});
 
                 const response = await requestHandler(requestParser, {} as IHostRuntime);
-                
+
                 assert(response, "Response returned");
-                assert(response?.status === 200, "Returned 200 Status Code")
+                assert(response?.status === 200, "Returned 200 Status Code");
                 assert(response?.value === service2, "Returned expected service");
             });
 
@@ -122,9 +123,9 @@ describe("Routerlicious", () => {
                 const requestParser = new RequestParser({url:`/${serviceRoutePathRoot}/id1`});
 
                 const response = await requestHandler(requestParser, {} as IHostRuntime);
-                
+
                 assert(response, "Response returned");
-                assert(response?.status === 200, "Returned 200 Status Code")
+                assert(response?.status === 200, "Returned 200 Status Code");
                 assert(response?.value === service1, "Returned expected service");
             });
 
@@ -135,9 +136,9 @@ describe("Routerlicious", () => {
                 const requestParser = new RequestParser({url:`/${serviceRoutePathRoot}/id1/sub1`});
 
                 const response = await requestHandler(requestParser, {} as IHostRuntime);
-                
+
                 assert(response, "Response returned");
-                assert(response?.status === 200, "Returned 200 Status Code")
+                assert(response?.status === 200, "Returned 200 Status Code");
                 assert(response?.value === service1, "Returned expected service");
                 assert((response?.value as ContainerServiceMock).route === "sub1", "sub-route persisted");
             });
