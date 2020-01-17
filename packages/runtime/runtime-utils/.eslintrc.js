1939ff5463ad11fba5055d6a0a845354f3d1b105
Tyler Butler <tylerbu@microsoft.com>
2020-01-17 11:45:28 -0800

    fix up json to be js

--- packages/runtime/runtime-utils/.eslintrc.js
+++ packages/runtime/runtime-utils/.eslintrc.js
@@ -1,4 +1,9 @@
-{
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = {
     "extends": [
         "@microsoft/eslint-config-fluid"
     ],
