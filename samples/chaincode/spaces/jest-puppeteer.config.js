1512915e5c98ced552b2c806aee49064ec4cb049
Skyler <skjokiel@microsoft.com>
2020-01-21 10:31:35 -0800

    test fixes

--- samples/chaincode/spaces/jest-puppeteer.config.js
+++ samples/chaincode/spaces/jest-puppeteer.config.js
@@ -5,13 +5,13 @@
 
 module.exports = {
   server: {
-    command: "npm run start",
-    port: 8080
+    command: "npm run start -- --port 8084",
+    port: 8084
   },
   launch: {
-    // args: ['--no-sandbox', '--disable-setuid-sandbox'], // https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
+    args: ['--no-sandbox', '--disable-setuid-sandbox'], // https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
     dumpio: true, // output browser console to cmd line
-    slowMo: 500, // slows down process for easier viewing
-    headless: false, // run in the browser
+    // slowMo: 500, // slows down process for easier viewing
+    // headless: false, // run in the browser
   },
 };
--- samples/chaincode/spaces/tests/spaces.test.ts
+++ samples/chaincode/spaces/tests/spaces.test.ts
@@ -13,34 +13,5 @@ describe("spaces", () => {
 
     it("There's a button to be clicked", async () => {
         await expect(page).toClick("button", { text: "Edit: true" });
-    });
-
-    // it("Clicking the button updates both users", async () => {
-    //   const getValue = async (index: number) => {
-    //     return page.evaluate((i: number) => {
-    //         const clickerElements = document.getElementsByClassName("clicker-value-class");
-    //         const clicker = clickerElements[i] as HTMLDivElement;
-    //         if (clicker) {
-    //             return clicker.innerText;
-    //         }
-
-    //         return "";
-    //     }, index);
-    //   };
-
-    //   // Validate both users have 0 as their value
-    //   const preValue = await getValue(0);
-    //   expect(preValue).toEqual("0");
-    //   const preValue2 = await getValue(1);
-    //   expect(preValue2).toEqual("0");
-
-    //   // Click the button
-    //   await expect(page).toClick("button", { text: "+" });
-
-    //   // Validate both users have 1 as their value
-    //   const postValue = await getValue(0);
-    //   expect(postValue).toEqual("1");
-    //   const postValue2 = await getValue(1);
-    //   expect(postValue2).toEqual("1");
-    // });
+    }, 10000);
   });
