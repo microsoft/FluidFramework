81deb2e360a88a8e33f440d6614889a08c4b098b
Tyler Butler <tylerbu@microsoft.com>
2020-01-29 08:29:15 -0800

    pwa init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,6 +7,17 @@ module.exports = {
     title: "💧 Fluid Framework",
     description: "State that flows",
     evergreen: true,
+    head: [
+        ["link", { rel: "icon", href: "/logo.png" }],
+        ["link", { rel: "manifest", href: "/manifest.webmanifest" }],
+        ["meta", { name: "theme-color", content: "#3eaf7c" }],
+        ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+        ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
+        ["link", { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.png" }],
+        ["link", { rel: "mask-icon", href: "/icons/safari-pinned-tab.svg", color: "#3eaf7c" }],
+        ["meta", { name: "msapplication-TileImage", content: "/icons/msapplication-icon-144x144.png" }],
+        ["meta", { name: "msapplication-TileColor", content: "#000000" }]
+    ],
     plugins: [
         ["vuepress-plugin-check-md"],
         ["tabs"],
@@ -16,6 +27,13 @@ module.exports = {
         //         color: "#999",
         //     }
         // ],
+        [
+            "@vuepress/pwa",
+            {
+                serviceWorker: true,
+                updatePopup: true
+            }
+        ],
         [
             "vuepress-plugin-container",
             {
