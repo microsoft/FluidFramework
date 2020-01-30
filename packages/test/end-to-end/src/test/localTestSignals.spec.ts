2fa8b31c1ed74dab1bf1db21773fbc30088879ee
Xi Wang <xiwang3@microsoft.com>
2020-01-30 12:45:16 -0800

    Add unit tests

--- packages/test/end-to-end/src/test/localTestSignals.spec.ts
+++ packages/test/end-to-end/src/test/localTestSignals.spec.ts
@@ -37,7 +37,7 @@ describe("TestSignals", () => {
     });
 
     describe("Attach signal Handlers on Both Clients", () => {
-        it("Validate signals", async () => {
+        it("Validate component runtime signals", async () => {
             let user1SignalReceivedCount = 0;
             let user2SignalReceivedCount = 0;
 
@@ -62,6 +62,80 @@ describe("TestSignals", () => {
             assert.equal(user2SignalReceivedCount, 2, "client 2 did not received signal");
 
         });
+
+        it("Validate host runtime signals", async () => {
+            let user1SignalReceivedCount = 0;
+            let user2SignalReceivedCount = 0;
+            const user1HostRuntime = user1Document.context.hostRuntime;
+            const user2HostRuntime = user2Document.context.hostRuntime;
+
+            user1HostRuntime.on("signal", (message: IInboundSignalMessage, local: boolean) => {
+                if (message.type === "TestSignal") {
+                    user1SignalReceivedCount += 1;
+                }
+            });
+
+            user2HostRuntime.on("signal", (message: IInboundSignalMessage, local: boolean) => {
+                if (message.type === "TestSignal") {
+                    user2SignalReceivedCount += 1;
+                }
+            });
+
+            user1HostRuntime.submitSignal("TestSignal", true);
+            assert.equal(user1SignalReceivedCount, 1, "client 1 did not receive signal");
+            assert.equal(user2SignalReceivedCount, 1, "client 2 did not receive signal");
+
+            user2HostRuntime.submitSignal("TestSignal", true);
+            assert.equal(user1SignalReceivedCount, 2, "client 1 did not receive signal");
+            assert.equal(user2SignalReceivedCount, 2, "client 2 did not receive signal");
+        });
+    });
+
+    it("Validate signal events are raised on the correct runtime", async () => {
+        let user1HostSignalReceivedCount = 0;
+        let user2HostSignalReceivedCount = 0;
+        let user1CompSignalReceivedCount = 0;
+        let user2CompSignalReceivedCount = 0;
+        const user1HostRuntime = user1Document.context.hostRuntime;
+        const user2HostRuntime = user2Document.context.hostRuntime;
+        const user1ComponentRuntime = user1Document.runtime;
+        const user2ComponentRuntime = user2Document.runtime;
+
+        user1ComponentRuntime.on("signal", (message: IInboundSignalMessage, local: boolean) => {
+            if (message.type === "TestSignal") {
+                user1CompSignalReceivedCount += 1;
+            }
+        });
+
+        user2ComponentRuntime.on("signal", (message: IInboundSignalMessage, local: boolean) => {
+            if (message.type === "TestSignal") {
+                user2CompSignalReceivedCount += 1;
+            }
+        });
+
+        user1HostRuntime.on("signal", (message: IInboundSignalMessage, local: boolean) => {
+            if (message.type === "TestSignal") {
+                user1HostSignalReceivedCount += 1;
+            }
+        });
+
+        user2HostRuntime.on("signal", (message: IInboundSignalMessage, local: boolean) => {
+            if (message.type === "TestSignal") {
+                user2HostSignalReceivedCount += 1;
+            }
+        });
+
+        user1HostRuntime.submitSignal("TestSignal", true);
+        assert.equal(user1HostSignalReceivedCount, 1, "client 1 did not receive signal on host runtime");
+        assert.equal(user2HostSignalReceivedCount, 1, "client 2 did not receive signal on host runtime");
+        assert.equal(user1CompSignalReceivedCount, 0, "client 1 should not receive signal on component runtime");
+        assert.equal(user2CompSignalReceivedCount, 0, "client 2 should not receive signal on component runtime");
+
+        user2ComponentRuntime.submitSignal("TestSignal", true);
+        assert.equal(user1HostSignalReceivedCount, 1, "client 1 should not receive signal on host runtime");
+        assert.equal(user2HostSignalReceivedCount, 1, "client 2 should not receive signal on host runtime");
+        assert.equal(user1CompSignalReceivedCount, 1, "client 1 did not receive signal on component runtime");
+        assert.equal(user2CompSignalReceivedCount, 1, "client 2 did not receive signal on component runtime");
     });
 
     afterEach(async () => {
