1e74dd3e98b261c5b26a862d5fa0e3859a02da27
Jatin Garg <48029724+jatgarg@users.noreply.github.com>
2020-01-30 14:37:33 -0800

    Cherrypick experimental create new driver (#1087)
    * Add in memory driver. (#1025)
    
    * creation driver
    
    * pr ssuggestions
    
    * fixing package.json (#1033)
    
    * fixing package.json
    
    * fixing package.json
    
    * remove undefined (#961)
    
    * Revert "remove undefined (#961)"
    
    This reverts commit 55385d6d368cce97ca73305a26d12ac61ce1101b.
    
    * fix

---
+++ packages/drivers/create-new-driver/src/test/creationDeltaConnection.spec.ts
@@ -0,0 +1,63 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { IDocumentDeltaConnection, IDocumentService, IResolvedUrl } from "@microsoft/fluid-driver-definitions";
+import { IClient, IDocumentMessage, MessageType, ScopeType } from "@microsoft/fluid-protocol-definitions";
+import * as assert from "assert";
+import { CreationServerMessagesHandler } from "..";
+import { CreationDocumentServiceFactory } from "../creationDocumentServiceFactory";
+
+describe("Creation Driver", () => {
+
+    let service: IDocumentService;
+    let client: IClient;
+    let documentDeltaConnection1: IDocumentDeltaConnection;
+    let documentDeltaConnection2: IDocumentDeltaConnection;
+    beforeEach(async () => {
+        const factory = new CreationDocumentServiceFactory();
+        const resolved: IResolvedUrl = {endpoints: {}, type: "fluid", url: "", tokens: {}};
+        service = await factory.createDocumentService(resolved);
+        client = {
+            mode: "write",
+            details: {capabilities: {interactive: false}},
+            permission: ["write"],
+            scopes: [ScopeType.DocWrite],
+            user: {id: "user1"},
+        };
+    });
+
+    // tslint:disable-next-line: no-string-based-set-timeout
+    const delay = async (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    it("Initial driver connection details", async () => {
+        documentDeltaConnection1 = await service.connectToDeltaStream(client, "write");
+        documentDeltaConnection2 = await service.connectToDeltaStream(client, "write");
+        assert.equal(documentDeltaConnection1.mode, "write", "Connection mode should be write.");
+        assert.equal(documentDeltaConnection1.existing, false, "Document should not be existing.");
+        assert.equal(documentDeltaConnection2.existing, true, "Document should be existing for second connection.");
+        assert.equal(documentDeltaConnection1.initialMessages!.length, 1, "Join message should be fired.");
+        assert.equal(documentDeltaConnection2.initialMessages!.length, 1, "Join message should be fired.");
+    });
+
+    it("Server messages test", async () => {
+        const message: IDocumentMessage = {
+            clientSequenceNumber: 1,
+            contents: {},
+            referenceSequenceNumber: 0,
+            type: MessageType.Operation,
+        };
+        const creationServerMessagesHandler = CreationServerMessagesHandler.getInstance();
+        assert.equal(creationServerMessagesHandler.queuedMessages.length, 2,
+            "Total messages should be 2 at this time including join messages");
+        documentDeltaConnection1.submit([message]);
+        documentDeltaConnection2.submit([message]);
+        await delay(0);
+        const queueLength: number = creationServerMessagesHandler.queuedMessages.length;
+        assert.equal(queueLength, 4,
+            "Total messages should be 4 at this time including join message");
+        assert.equal(creationServerMessagesHandler.queuedMessages[queueLength - 1].sequenceNumber, 4,
+            "Sequence number should be 4");
+    });
+});
