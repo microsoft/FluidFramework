92130b0ff31c0b96d5731bbeb7471427a9a40f1d
Sam Broner <sabroner@microsoft.com>
2020-01-29 18:17:59 -0800

    remove gateway change (until next PR)

--- server/gateway/src/gatewayUrlResolver.ts
+++ server/gateway/src/gatewayUrlResolver.ts
@@ -48,14 +48,14 @@ export function resolveUrl(
             };
         }
 
-        const endPointConfig = {
-            config,
+        const endPointConfig: IConfig = {
+            blobStorageUrl: config.get("worker:blobStorageUrl"),
+            serverUrl: config.get("worker:serverUrl"),
             tenantId,
             documentId,
         };
-
         const resolverList = [new RouterliciousUrlResolver(endPointConfig, undefined, appTenants, scopes, user)];
-        const resolvedP = resolveFluidUrl({ url: request.originalUrl, hostname: request.hostname }, resolverList);
+        const resolvedP = resolveFluidUrl(request.originalUrl, resolverList);
         const fullTreeP = alfred.getFullTree(tenantId, documentId);
         return [resolvedP, fullTreeP];
     }
