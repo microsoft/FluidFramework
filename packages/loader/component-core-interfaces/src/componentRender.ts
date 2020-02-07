302c83cb2e9307c01badbd673b47a5fcb2d26b38
Matt Rakow <marakow@microsoft.com>
2020-02-07 00:57:38 -0800

    Tweak comments

--- packages/loader/component-core-interfaces/src/componentRender.ts
+++ packages/loader/component-core-interfaces/src/componentRender.ts
@@ -5,16 +5,6 @@
 
 import { IComponent } from "./components";
 
-// Following is what loosely-coupled hosts need to show a component
-
-/**
- * Render the component into an HTML element. In the case of Block display,
- * elm.getBoundingClientRect() defines the dimensions of the viewport in which
- * to render. Typically, this means that elm should already be placed into the DOM.
- * If elm has an empty client rect, then it is assumed that it will expand to hold the
- * rendered component.
- */
-
 export interface IComponentHTMLOptions {
     display?: "block" | "inline";
 }
@@ -23,7 +13,18 @@ export interface IProvideComponentHTMLView {
     readonly IComponentHTMLView: IComponentHTMLView;
 }
 
+/**
+ * An IComponentHTMLView is a renderable component, which may or may not also be its own model.
+ * If it is its own model, it is a "thick" view, otherwise it is a "thin" view.
+ */
 export interface IComponentHTMLView extends IProvideComponentHTMLView {
+    /**
+     * Render the component into an HTML element. In the case of Block display,
+     * elm.getBoundingClientRect() defines the dimensions of the viewport in which
+     * to render. Typically, this means that elm should already be placed into the DOM.
+     * If elm has an empty client rect, then it is assumed that it will expand to hold the
+     * rendered component.
+     */
     render(elm: HTMLElement, options?: IComponentHTMLOptions): void;
     remove?(): void;
 }
@@ -32,6 +33,10 @@ export interface IProvideComponentHTMLVisual {
     readonly IComponentHTMLVisual: IComponentHTMLVisual;
 }
 
+/**
+ * An IComponentHTMLVisual is a view factory.  Typically it will be a model, binding itself to the views
+ * it creates.
+ */
 export interface IComponentHTMLVisual extends IProvideComponentHTMLVisual {
     addView(scope?: IComponent): IComponentHTMLView;
 }
