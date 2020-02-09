fe5c9804955ed486524599ee53f93059ddf1c13d
Tanvir Aumi <mdaumi@microsoft.com>
2020-02-09 00:31:18 -0800

    Close db

--- server/tinylicious/src/services/levelDb.ts
+++ server/tinylicious/src/services/levelDb.ts
@@ -23,7 +23,7 @@ export class LevelDb extends EventEmitter implements IDb {
     }
 
     public async close(): Promise<void> {
-        return;
+        return this.db.close();
     }
 
     public collection<T>(name: string): ICollection<T> {
