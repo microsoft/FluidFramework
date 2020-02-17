719865bcaca8ba455bdd6e576673c2f383f8c657
Tyler Butler <tylerbu@microsoft.com>
2020-02-16 17:12:53 -0800

    es5 aqueduct output

--- common/lib/common-utils/src/rangeTracker.ts
+++ common/lib/common-utils/src/rangeTracker.ts
@@ -3,11 +3,10 @@
  * Licensed under the MIT License.
  */
 
-import * as assert from "assert";
+import assert from "assert";
 
-// eslint-disable-next-line max-len
-// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires, import/no-internal-modules
-const cloneDeep = require("lodash/cloneDeep");
+// eslint-disable-next-line import/no-internal-modules
+import cloneDeep from "lodash/cloneDeep";
 
 /**
  * A range in the RangeTracker
