71f3f18e16173cc9df2a8e634e14e2b9cbbc7715
Dany Fabian <daniel.fabian@microsoft.com>
2020-02-18 14:55:43 +0000

    Add missing copyright headers.

--- packages/runtime/matrix/src/split.ts
+++ packages/runtime/matrix/src/split.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { SetOperations, Pair } from "./bspSet";
 
 /** Represents a half-open interval [a, b) */
