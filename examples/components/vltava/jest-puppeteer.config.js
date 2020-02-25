8f8aa13487ad074a7118b9006f7fd89fd48937af
Curtis Man <curtism@microsoft.com>
2020-02-24 19:25:25 -0800

    Add launchTimeout and usedPortAction to puppeteer config

--- examples/components/vltava/jest-puppeteer.config.js
+++ examples/components/vltava/jest-puppeteer.config.js
@@ -6,7 +6,9 @@
 module.exports = {
   server: {
     command: "npm run start -- --port 8085",
-    port: 8085
+    port: 8085,
+    launchTimeout:10000,
+    usedPortAction: 'error'
   },
   launch: {
     args: ['--no-sandbox', '--disable-setuid-sandbox'], // https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
