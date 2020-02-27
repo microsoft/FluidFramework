29427b7738e0099b64c173b0d306227d13004dfb
Tyler Butler <tylerbu@microsoft.com>
2020-02-26 20:10:31 -0800

    Revert "what is happening?"
    This reverts commit 22ee00a144d8d7d7f418c6f4b123ac255a8cfee1.

--- tools/fluid-build/src/common/npmPackage.ts
+++ tools/fluid-build/src/common/npmPackage.ts
@@ -7,7 +7,7 @@ import { queue } from "async";
 import * as chalk from "chalk";
 import * as fs from "fs";
 import * as path from "path";
-import sortPackageJson from "sort-package-json";
+import { sortPackageJson } from "sort-package-json";
 import { logStatus, logVerbose } from "./logging";
 import {
     copyFileAsync,
