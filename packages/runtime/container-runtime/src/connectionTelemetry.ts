67f970d53e87f56174f6d8dbe450f50d22795067
vladsud <vladsud@microsoft.com>
2020-02-28 20:27:56 -0800

    Copyright header missing

--- packages/runtime/container-runtime/src/connectionTelemetry.ts
+++ packages/runtime/container-runtime/src/connectionTelemetry.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import * as assert from "assert";
 import { ITelemetryLogger } from "@microsoft/fluid-common-definitions";
 import { IDeltaManager } from "@microsoft/fluid-container-definitions";
