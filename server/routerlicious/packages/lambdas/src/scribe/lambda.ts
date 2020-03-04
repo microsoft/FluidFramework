72b1f562289574215d1e708744af7fa9a689c3bf
Tanvir Aumi <mdaumi@microsoft.com>
2020-03-04 14:16:23 -0800

    Generate service summary in scribe. (#1415)
    * Generate service summary in scribe
    
    * Local orderer fix
    
    * Fix comments

--- server/routerlicious/packages/lambdas/src/scribe/lambda.ts
+++ server/routerlicious/packages/lambdas/src/scribe/lambda.ts
@@ -54,6 +54,9 @@ export class ScribeLambda extends SequencedLambda {
     private sequenceNumber = 0;
     private minSequenceNumber = 0;
 
+    // Ref of the last client generated summary
+    private lastClientSummaryHead: string;
+
     constructor(
         context: IContext,
         private readonly documentCollection: ICollection<IDocument>,
@@ -64,8 +67,9 @@ export class ScribeLambda extends SequencedLambda {
         private readonly storage: IGitManager,
         private readonly producer: IProducer,
         private readonly protocolHandler: ProtocolOpHandler,
-        private readonly protocolHead: number,
+        private protocolHead: number,
         messages: ISequencedOperationMessage[],
+        private readonly generateServiceSummary: boolean,
         private readonly nackOnSummarizeException?: boolean,
     ) {
         super(context);
@@ -73,8 +77,9 @@ export class ScribeLambda extends SequencedLambda {
         this.lastOffset = scribe.logOffset;
         this.sequenceNumber = scribe.sequenceNumber;
         this.minSequenceNumber = scribe.minimumSequenceNumber;
+        this.lastClientSummaryHead = scribe.lastClientSummaryHead;
 
-        // Filter messages in case they were not deleted after the last checkpoint.
+        // Filter and keep messages up to protocol state.
         this.pendingMessages = new Deque<ISequencedDocumentMessage>(
             messages
                 .filter((message) => message.operation.sequenceNumber > scribe.protocolState.sequenceNumber)
@@ -128,6 +133,7 @@ export class ScribeLambda extends SequencedLambda {
                             this.protocolHandler.sequenceNumber,
                             this.protocolHandler.quorum.snapshot(),
                             summarySequenceNumber);
+                        this.protocolHead = summarySequenceNumber;
                     } catch (ex) {
                         if (this.nackOnSummarizeException) {
                             // SPO wants to nack when summarize fails
@@ -141,6 +147,15 @@ export class ScribeLambda extends SequencedLambda {
                             throw ex;
                         }
                     }
+                } else if (value.operation.type === MessageType.NoClient) {
+                    if (this.generateServiceSummary) {
+                        const summarySequenceNumber = value.operation.sequenceNumber;
+                        await this.createServiceSummary(summarySequenceNumber);
+                        this.protocolHead = summarySequenceNumber;
+                    }
+                } else if (value.operation.type === MessageType.SummaryAck) {
+                    const content = value.operation.contents as ISummaryAck;
+                    this.lastClientSummaryHead = content.handle;
                 }
             }
         }
@@ -169,6 +184,7 @@ export class ScribeLambda extends SequencedLambda {
         const protocolState = this.protocolHandler.getProtocolState();
 
         const checkpoint: IScribe = {
+            lastClientSummaryHead: this.lastClientSummaryHead,
             logOffset: queuedMessage.offset,
             minimumSequenceNumber: this.minSequenceNumber,
             protocolState,
@@ -251,10 +267,11 @@ export class ScribeLambda extends SequencedLambda {
             null);
 
         // And then delete messagse we no longer will reference
+        const removeSequenceNumber = Math.min(checkpoint.protocolState.sequenceNumber, this.protocolHead);
         await this.messageCollection
             .deleteMany({
                 "documentId": this.documentId,
-                "operation.sequenceNumber": { $lte: checkpoint.protocolState.sequenceNumber },
+                "operation.sequenceNumber": { $lte: removeSequenceNumber },
                 "tenantId": this.tenantId,
             });
     }
@@ -283,7 +300,11 @@ export class ScribeLambda extends SequencedLambda {
         const existingRef = await this.storage.getRef(encodeURIComponent(this.documentId));
 
         if (content.head) {
-            if (!existingRef || existingRef.object.sha !== content.head) {
+            // In usual case, client always refers to last summaryAck so lastClientSummaryHead should always match.
+            // However, the ack itself might be lost If scribe dies right after creating the summary. In that case,
+            // the client code just fetches the last summary which should be the same as existingRef sha.
+            if (!existingRef ||
+                (this.lastClientSummaryHead !== content.head && existingRef.object.sha !== content.head)) {
                 await this.sendSummaryNack(
                     summarySequenceNumber,
                     // eslint-disable-next-line max-len
@@ -402,6 +423,82 @@ export class ScribeLambda extends SequencedLambda {
         }
 
         await this.sendSummaryAck(commit.sha, summarySequenceNumber);
+    }
+
+    private async createServiceSummary(sequenceNumber: number): Promise<void> {
+        if (this.protocolHead >= sequenceNumber) {
+            return;
+        }
+
+        const existingRef = await this.storage.getRef(encodeURIComponent(this.documentId));
+
+        // Client assumes at least one app generated summary. To keep compatibility for now, service summary requires
+        // at least one prior client generated summary.
+        if (!existingRef) {
+            return;
+        }
+
+        // Fetch the logtail starting from the last protocol state
+        const query = {
+            "documentId": this.documentId,
+            "tenantId": this.tenantId,
+            "operation.sequenceNumber": {
+                $gt: this.protocolHead,
+                $lt: sequenceNumber + 1,
+            },
+        };
+        const logTail = this.messageCollection.find(query, { "operation.sequenceNumber": 1 });
+        const entries: ITreeEntry[] = [
+            {
+                mode: FileMode.File,
+                path: "logTail",
+                type: TreeEntry[TreeEntry.Blob],
+                value: {
+                    contents: JSON.stringify(logTail),
+                    encoding: "utf-8",
+                },
+            },
+        ];
+
+        // Fetch the last commit and summary tree. Create a new tree with logTail.
+        const lastCommit = await this.storage.getCommit(existingRef.object.sha);
+        const [logTailTree, lastSummaryTree] = await Promise.all([
+            this.storage.createTree({ entries, id: null }),
+            this.storage.getTree(lastCommit.tree.sha, false),
+        ]);
+
+        // Combine the last summary tree with .logTail tree
+        const newTreeEntries = lastSummaryTree.tree.map((value) => {
+            const createTreeEntry: ICreateTreeEntry = {
+                mode: value.mode,
+                path: value.path,
+                sha: value.sha,
+                type: value.type,
+            };
+            return createTreeEntry;
+        });
+        newTreeEntries.push({
+            mode: FileMode.Directory,
+            path: ".logTail",
+            sha: logTailTree.sha,
+            type: "tree",
+        });
+
+        const gitTree = await this.storage.createGitTree({ tree: newTreeEntries });
+        const commitParams: ICreateCommitParams = {
+            author: {
+                date: new Date().toISOString(),
+                email: "praguertdev@microsoft.com",
+                name: "Routerlicious Service",
+            },
+            message: "Service_Summary",
+            parents: lastCommit.parents.map((parent) => parent.sha),
+            tree: gitTree.sha,
+        };
+
+        // Finally commit the service summary and update the ref.
+        const commit = await this.storage.createCommit(commitParams);
+        await this.storage.upsertRef(this.documentId, commit.sha);
     }
 
     private async sendSummaryAck(handle: string, summarySequenceNumber: number) {
