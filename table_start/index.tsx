8120eb6151f7058e7681b6dfafe40dc54709550a
Micah Godbolt <mgodbolt@microsoft.com>
2020-03-05 09:06:18 -0800

    move table to src

--- table_start/index.tsx
+++
@@ -1,7 +0,0 @@
-import { withData } from "./utils/withData";
-import { App } from "./App";
-import { PrimedContext } from "./provider";
-
-export const initialState = { diceValue: 1, clicked: 1 };
-
-export const fluidExport = withData(App, initialState, PrimedContext);
