0de6e30f13d90c442c0a985499b1173bdd2f2eeb
Curtis Man <curtism@microsoft.com>
2020-03-05 16:40:04 -0800

    Fluid-Build: don't warn about symlink if node_modules is not installed for the monorepo

--- tools/fluid-build/src/fluidBuild/symlinkUtils.ts
+++ tools/fluid-build/src/fluidBuild/symlinkUtils.ts
@@ -127,6 +127,15 @@ export async function symlinkPackage(repo: FluidRepo, pkg: Package, buildPackage
     let changed = 0;
     const monoRepo = repo.getMonoRepo(pkg);
     const monoRepoNodeModulePath = repo.getMonoRepoNodeModulePath(monoRepo);
+
+    if (monoRepoNodeModulePath && !existsSync(monoRepoNodeModulePath)) {
+        // If the node_modules isn't install at all, just don't check
+        if (options.symlink) {
+            console.warn(`${pkg.nameColored}: node_modules not installed.  Can't fix symlink.`)
+        }
+        return 0;
+    }
+
     for (const { name: dep, version } of pkg.combinedDependencies) {
         const depBuildPackage = buildPackages.get(dep);
         // Check and fix link if it is a known package and version satisfy the version.
