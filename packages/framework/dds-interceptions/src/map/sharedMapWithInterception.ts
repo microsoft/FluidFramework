639a34bf9a3759c0cb2abd5b9d625166284a9b73
Navin Agarwal <naagar@microsoft.com>
2020-03-12 10:52:31 -0700

    Renamed interceptionCallback to setInterceptionCallback.

--- packages/framework/dds-interceptions/src/map/sharedMapWithInterception.ts
+++ packages/framework/dds-interceptions/src/map/sharedMapWithInterception.ts
@@ -8,26 +8,26 @@ import { IComponentContext } from "@microsoft/fluid-runtime-definitions";
 
 /**
  * - Create a new object from the passed SharedMap.
- * - Modify the set method to call the interceptionCallback before calling set on the underlying SharedMap.
- * - The interceptionCallback and the call to the underlying SharedMap are wrapped around an
+ * - Modify the set method to call the setInterceptionCallback before calling set on the underlying SharedMap.
+ * - The setInterceptionCallback and the call to the underlying SharedMap are wrapped around an
  *   orderSequentially call to batch any operations that might happen in the callback.
  *
  * @param sharedMap - The underlying SharedMap
  * @param context - The IComponentContext that will be used to call orderSequentially
- * @param interceptionCallback - The interception callback to be called
+ * @param setInterceptionCallback - The interception callback to be called
  *
- * @returns A new SharedMap that intercepts the set method and calls the interceptionCallback.
+ * @returns A new SharedMap that intercepts the set method and calls the setInterceptionCallback.
  */
 export function createSharedMapWithInterception(
     sharedMap: SharedMap,
     context: IComponentContext,
-    interceptionCallback: (sharedMap: SharedMap, key: string, value: any) => void): SharedMap {
+    setInterceptionCallback: (sharedMap: SharedMap, key: string, value: any) => void): SharedMap {
     const sharedMapWithInterception = Object.create(sharedMap);
 
     sharedMapWithInterception.set = (key: string, value: any) => {
         let map;
         context.hostRuntime.orderSequentially(() => {
-            interceptionCallback(sharedMap, key, value);
+            setInterceptionCallback(sharedMap, key, value);
             map = sharedMap.set(key, value);
         });
         return map;
