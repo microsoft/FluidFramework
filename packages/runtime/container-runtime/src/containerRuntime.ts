5453019e1ade3afba4001d3915ca0f240aeda741
Jatin Garg <jatgarg@microsoft.com>
2020-03-17 00:04:58 -0700

    fix

--- packages/runtime/container-runtime/src/containerRuntime.ts
+++ packages/runtime/container-runtime/src/containerRuntime.ts
@@ -572,7 +572,7 @@ export class ContainerRuntime extends EventEmitter implements IHostRuntime, IRun
         this.latestSummaryAck = {
             proposalHandle: undefined,
             ackHandle: expContainerContext.isExperimentalContainerContext && expContainerContext.getLoadedFromVersion
-                ? expContainerContext.getLoadedFromVersion().id : undefined };
+                && expContainerContext.getLoadedFromVersion() ? expContainerContext.getLoadedFromVersion().id : undefined };
         this.summaryTracker = new SummaryTracker(
             useContext,
             "", // fullPath - the root is unnamed
