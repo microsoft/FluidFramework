48ea27dd72dd907ace72ca3f4346064a1e206654
Tony Murphy <anthonm@microsoft.com>
2020-03-19 12:38:50 -0700

    add copyright header

--- packages/loader/loader-web/src/utils.ts
+++ packages/loader/loader-web/src/utils.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { IFluidPackage } from "@microsoft/fluid-container-definitions";
 
 export interface IPackageIdentifierDetails {
