9f42abe3eb9cfbe8d805c1c4bb1879146b020171
Trevor Harris <trharris@microsoft.com>
2020-03-19 09:02:58 -0700

    Updates based on PR feedback
    Updated uuid import and removed moniker from the routerlicious\packages\lambdas package.json

--- server/routerlicious/packages/lambdas/src/utils/clientIdGenerator.ts
+++ server/routerlicious/packages/lambdas/src/utils/clientIdGenerator.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-import * as uuid from "uuid";
+// eslint-disable-next-line import/no-internal-modules
+import * as uuid from "uuid/v4";
 
 export const generateClientId = (): string => uuid();
