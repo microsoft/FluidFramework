ef84c6cad762fdabc965c9fb702c82e7b17d6c18
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-03-25 23:46:21 -0700

    add comments and license

--- packages/server/local-test-utils/src/testUtils.ts
+++ packages/server/local-test-utils/src/testUtils.ts
@@ -1,10 +1,16 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { IComponent } from "@microsoft/fluid-component-core-interfaces";
 import { TestHost } from ".";
 
 /**
+ * TODO:
  * Keeping getComponent in testUtils even though it has been deprecated from sharedComponent
  * This is to allow us to fetch the _scheduler which is set at initializing
- * Add ticket number
+ * Issue #1628
 */
 export async function getComponent<T extends IComponent>(
     host: TestHost,
