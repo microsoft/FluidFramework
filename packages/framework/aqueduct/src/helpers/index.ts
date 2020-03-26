24807b21e354e8319790a0aa2d38666e3c4e1298
Matt Rakow <marakow@microsoft.com>
2020-03-26 15:56:28 -0700

    Move container runtimes into separate directory

--- packages/framework/aqueduct/src/helpers/index.ts
+++ packages/framework/aqueduct/src/helpers/index.ts
@@ -3,9 +3,5 @@
  * Licensed under the MIT License.
  */
 
-export * from "./baseContainerRuntimeFactory";
-export * from "./defaultComponentContainerRuntimeFactory";
 export * from "./primedComponentFactory";
-export * from "./simpleContainerRuntimeFactory";
-export * from "./simpleModuleInstantiationFactory";
 export * from "./sharedComponentFactory";
