433a4166e5cfac410c0a2211cccf2bc9cc829210
Curtis Man <curtism@microsoft.com>
2020-03-27 10:09:47 -0700

    add --no-live-reload flag for webpack-dev-server with jest test (#1652)

--- packages/components/dice-roller/jest-puppeteer.config.js
+++ packages/components/dice-roller/jest-puppeteer.config.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   server: {
-    command: "npm run start -- --port 8081",
+    command: "npm run start -- --no-live-reload --port 8081",
     port: 8081,
     launchTimeout:10000,
     usedPortAction: 'error'
--- packages/test/context-reload/jest-puppeteer.config.js
+++ packages/test/context-reload/jest-puppeteer.config.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   server: {
-    command: "npm run start -- --port 8088",
+    command: "npm run start -- --no-live-reload --port 8088",
     port: 8088,
     launchTimeout: 10000,
     usedPortAction: 'error',
