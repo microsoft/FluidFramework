ee2be2af3c152c8b0954fe425b38ea2f776fc63f
Matt Rakow <marakow@microsoft.com>
2020-03-31 14:34:16 -0700

    flow-scroll depends on the *.css.d.ts file

---
+++ examples/components/flow-scroll/src/host/index.css.d.ts
@@ -0,0 +1,15 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+declare const styles: {
+    readonly "host": string;
+    readonly "viewport": string;
+    readonly "search": string;
+    readonly "bold": string;
+    readonly "padding": string;
+    readonly "slot": string;
+};
+
+export = styles;
