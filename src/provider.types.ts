3918d03bd98c7b24bbbb0153910a2f27838c5aeb
Micah Godbolt <mgodbolt@microsoft.com>
2020-03-31 15:51:33 -0700

    fixed dropdown for real this time

--- src/provider.types.ts
+++ src/provider.types.ts
@@ -4,16 +4,16 @@ export interface IViewProps {
 }
 
 export enum AvailabilityType {
-  No = "no",
-  Maybe = "maybe",
-  Yes = "yes"
+  No = 0,
+  Maybe = 1,
+  Yes = 2
 }
 
 export interface IViewActions {
   setAvailability: (
     personKey: number,
     dateKey: number,
-    value: AvailabilityType
+    value: string | number
   ) => void;
   setName: (personKey: number, value: string) => void;
   setDate: (dateKey: number, date: Date) => void;
