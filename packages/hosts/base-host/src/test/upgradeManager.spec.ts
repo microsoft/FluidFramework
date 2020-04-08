a0766f34e0b33f5513a23fca79dcc3ad22a9ca01
Wes Carlson <wesley.carlson@microsoft.com>
2020-04-08 15:05:06 -0700

    policy check

--- packages/hosts/base-host/src/test/upgradeManager.spec.ts
+++ packages/hosts/base-host/src/test/upgradeManager.spec.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import * as assert from "assert";
 import { ProtocolOpHandler } from "@microsoft/fluid-protocol-base";
 import { ISequencedDocumentMessage } from "@microsoft/fluid-protocol-definitions";
