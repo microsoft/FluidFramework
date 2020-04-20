b92f31e2c7a10c99487145b0ff14e682393cf01b
Tony Murphy <anthonm@microsoft.com>
2020-04-20 09:56:22 -0700

    add descriptions to tests

--- packages/runtime/runtime-definitions/test-d/asJsonable.test-d.ts
+++ packages/runtime/runtime-definitions/test-d/asJsonable.test-d.ts
@@ -8,8 +8,27 @@ import { Jsonable, AsJsonable } from '../dist/jsonable';
 
 declare function foo<T>(a: AsJsonable<T>): void;
 
+// --- ideally wouldn't work but do as we don't know how to just exclude classes
+
+// test simple class.
+class Z {
+    public a = "a";
+}
+
+foo<Z>(new Z())
+
+//test class with getter
+class getter {
+    public get baz(): number {
+        return 0;
+    }
+}
+
+foo(new getter());
+
 // --- should work
 
+// test plain types
 foo(1);
 foo("");
 foo(undefined);
@@ -21,24 +40,25 @@ foo([""]);
 foo({});
 foo({ a:"a" });
 
-
+// test simple interface
 interface IA {
     a: "a";
 }
 declare const a: IA;
 foo(a)
 
-
+// test simple indexed interface
 interface IA2 {
     ["a"]: "a";
 }
 declare const a2: IA2;
 foo(a2);
 
-
+// text complex indexed interface
 declare const a3: { [key: string]: Jsonable<string> };
 foo(a3);
 
+// test interface with multiple properties
 interface A5 {
     a: "a",
     b: "b",
@@ -46,12 +66,14 @@ interface A5 {
 declare const a5: A5;
 foo(a5);
 
+// test interface with optional property
 interface A6 {
     a?: "a",
 };
 declare const a6: A6;
 foo(a6);
 
+// test simple type
 type A7 = {
     a: "a",
 };
@@ -59,12 +81,8 @@ type A7 = {
 declare const a7: A7;
 foo(a7);
 
-class Z {
-    public a = "a";
-}
 
-foo<Z>(new Z())
-
+// test nested interface
 interface IBN {
     b2: string;
 }
@@ -73,7 +91,6 @@ interface INested {
     a: string;
     b: IBN;
 }
-
 
 const nested: INested = {
     a: "a",
@@ -85,6 +102,7 @@ foo(nested);
 
 // --- should not work
 
+// test interface with method, and member
 interface IA11 {
     ["a"]: "a";
     foo: () => void;
@@ -93,27 +111,28 @@ declare const a11: IA11;
 expectError(foo(a11));
 
 
+// test interface with optional method
 interface A12 {
     foo?: () => void,
 };
 declare const a12: A12;
 expectError(foo(a12));
 
-
+// test type with method
 type A13 = {
     foo: () => void,
 };
 declare const a13: A13;
 expectError(foo(a13));
 
-
+// test type with primative and object with classes union
 interface IA14 {
     a: number | Date;
 }
 declare const a14: IA14;
 expectError(foo(a14));
 
-
+//test class with function
 class bar {
     public baz() {
     }
@@ -121,6 +140,8 @@ class bar {
 
 expectError(foo(new bar()));
 
+
+// test class with complex property
 interface MapProp{
     m: Map<string,string>
 }
@@ -129,6 +150,7 @@ const mt: MapProp = {
 }
 expectError(foo(mt));
 
+//test nested interface with complex property
 interface NestedMapProp{
     n: MapProp;
 }
@@ -137,6 +159,7 @@ const nmt: NestedMapProp = {
 };
 expectError(foo(nmt));
 
+// test class with symbol indexer for property
 const sym = Symbol.for("test");
 interface ISymbol{
     [sym]:string,
