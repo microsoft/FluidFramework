ba450c86a4d0a60641c37edeb3993fde919b08b6
Navin Agarwal <45832642+agarwal-navin@users.noreply.github.com>
2020-04-20 12:32:18 -0700

    Add CodeMirror and ProseMirror to spaces. (#1831)

--- examples/components/spaces/src/index.ts
+++ examples/components/spaces/src/index.ts
@@ -6,6 +6,8 @@
 import { ContainerRuntimeFactoryWithDefaultComponent } from "@microsoft/fluid-aqueduct";
 import { IComponent } from "@microsoft/fluid-component-core-interfaces";
 import { ClickerInstantiationFactory } from "@fluid-example/clicker";
+import { fluidExport as cmfe } from "@fluid-example/codemirror/dist/codemirror";
+import { fluidExport as pmfe } from "@fluid-example/prosemirror/dist/prosemirror";
 import {
     IProvideComponentFactory,
     NamedComponentRegistryEntries,
@@ -107,6 +109,20 @@ const generateFactory = () => {
             factory: Promise.resolve(TextBox.getFactory()),
             friendlyName: FriendlyTextBoxName,
             fabricIconName: "TextField",
+            capabilities: ["IComponentHTMLView"],
+        },
+        {
+            type: "codemirror",
+            factory: Promise.resolve(cmfe),
+            friendlyName: "Code Mirror",
+            fabricIconName: "Code",
+            capabilities: ["IComponentHTMLView"],
+        },
+        {
+            type: "prosemirror",
+            factory: Promise.resolve(pmfe),
+            friendlyName: "Prose Mirror",
+            fabricIconName: "Edit",
             capabilities: ["IComponentHTMLView"],
         },
     ];
