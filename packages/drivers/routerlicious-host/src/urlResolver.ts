7c8d4350b26c97c29e7a7b3159c8ee78dc19e172
markfields <markfields-git@outlook.com>
2020-04-20 15:34:40 -0700

    UrlResolver

--- packages/drivers/routerlicious-host/src/urlResolver.ts
+++ packages/drivers/routerlicious-host/src/urlResolver.ts
@@ -3,6 +3,7 @@
  * Licensed under the MIT License.
  */
 
+import { PromiseCache } from "@microsoft/fluid-common-utils";
 import {
     IRequest,
 } from "@microsoft/fluid-component-core-interfaces";
@@ -13,29 +14,26 @@ import {
 import Axios from "axios";
 
 export class ContainerUrlResolver implements IUrlResolver {
-    private readonly cache = new Map<string, Promise<IResolvedUrl>>();
+    private readonly cache = new PromiseCache<string, IResolvedUrl>();
 
     constructor(
         private readonly baseUrl: string,
         private readonly jwt: string,
         cache?: Map<string, IResolvedUrl>,
     ) {
-        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-        if (cache) {
+        if (cache !== undefined) {
             for (const [key, value] of cache) {
-                this.cache.set(key, Promise.resolve(value));
+                this.cache.addValue(key, value);
             }
         }
     }
 
-    // This function is handling caching of Promises, not awaiting them.
-    // eslint-disable-next-line @typescript-eslint/promise-function-async
-    public resolve(request: IRequest): Promise<IResolvedUrl> {
-        if (!this.cache.has(request.url)) {
+    public async resolve(request: IRequest): Promise<IResolvedUrl> {
+        const fetchResolvedUrl = async () => {
             const headers = {
                 Authorization: `Bearer ${this.jwt}`,
             };
-            const resolvedUrlP = Axios.post<IResolvedUrl>(
+            const resolvedUrl = await Axios.post<IResolvedUrl>(
                 `${this.baseUrl}/api/v1/load`,
                 {
                     url: request.url,
@@ -44,9 +42,9 @@ export class ContainerUrlResolver implements IUrlResolver {
                     headers,
                 });
 
-            this.cache.set(request.url, resolvedUrlP.then((response) => response.data));
-        }
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        return this.cache.get(request.url)!;
+            return resolvedUrl.data;
+        };
+
+        return this.cache.addOrGet(request.url, fetchResolvedUrl);
     }
 }
