56e28d4ca5557c5ce3af37ddfc0ec24b0d7c0012
markfields <markfields-git@outlook.com>
2020-04-23 19:44:54 -0700

    Revert unnecessary changes to debug.ts

--- packages/runtime/consensus-register-collection/src/debug.ts
+++ packages/runtime/consensus-register-collection/src/debug.ts
@@ -3,18 +3,8 @@
  * Licensed under the MIT License.
  */
 
-import * as assert from "assert";
 import * as registerDebug from "debug";
 import { pkgName, pkgVersion } from "./packageVersion";
 
 export const debug = registerDebug("fluid:consensus-register-collection");
 debug(`Package: ${pkgName} - Version: ${pkgVersion}`);
-
-export function strongAssert(value: unknown): asserts value {
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (!value) { assert(value); }
-}
-
-export function unreachableCase(value: never): never {
-    throw new Error(`Unreachable Case: Type of ${value} is never`);
-}
