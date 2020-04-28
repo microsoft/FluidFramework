edceab5e06aa5b36cab769bf5a4fe657a92f0508
sumedhb1995 <sumedhb1995@gmail.com>
2020-04-28 11:36:15 -0700

    Hotfix testhost to store components on its own root (#1959)
    Bohemia doesnt have a DDS to store the handles on. As such, it needs to have the test root handle storing and retrieving its components.
    
    ScriptorPad is using TestHost and they don't have a DDS of their own. This is a workaround for ScriptorPad still using ID while we have removed IDs in 0.16. Ultimately, we should migrate ScriptorPad off of using TestHost as they are treating it as a container but that is a bigger change and should be isolated from the version migration.
    
    fixes #1962 and #1628

--- packages/components/table-document/src/test/table.spec.ts
+++ packages/components/table-document/src/test/table.spec.ts
@@ -30,7 +30,7 @@ describe("TableDocument", () => {
         return id;
     }
 
-    const createTable = async () => host.createAndAttachComponent(TableDocumentType);
+    const createTable = async () => host.createAndAttachComponent(makeId(TableDocumentType), TableDocumentType);
 
     let table: TableDocument;
     beforeEach(async () => {
