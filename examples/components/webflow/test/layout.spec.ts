e452425dc451cee4e185a7120cb041b7384fe2da
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-04-28 11:08:50 -0700

    fix layout

--- examples/components/webflow/test/layout.spec.ts
+++ examples/components/webflow/test/layout.spec.ts
@@ -50,7 +50,7 @@ describe("Layout", () => {
             [flowDocumentFactory.type, Promise.resolve(flowDocumentFactory)],
         ]);
 
-        doc = await host.createAndAttachComponent("test-doc" flowDocumentFactory.type);
+        doc = await host.createAndAttachComponent("test-doc", flowDocumentFactory.type);
     });
 
     after(async () => {
