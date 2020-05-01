5530faa244a57b9550423d48bdaca5407890a8f4
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-05-01 14:54:48 -0700

    fix policy check

--- tools/bohemia-intercept/chrome-extension/background.js
+++ tools/bohemia-intercept/chrome-extension/background.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 function webPartsHandler(req){
   return { redirectUrl: 'http://localhost:3000/getclientsidewebparts' };
 }
