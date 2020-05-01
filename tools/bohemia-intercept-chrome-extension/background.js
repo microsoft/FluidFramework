4e670b4214e196d6273d8babfe887ac73ff74c72
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-05-01 12:45:22 -0700

    fluid icons!

--- tools/bohemia-intercept-chrome-extension/background.js
+++ tools/bohemia-intercept-chrome-extension/background.js
@@ -1,15 +1,22 @@
 function webPartsHandler(req){
   return { redirectUrl: 'http://localhost:3000/getclientsidewebparts' };
 }
-function packageHandler(req){
-  return { redirectUrl: 'http://localhost:3000/main.bundle.js' };
-}
+
 function setListeners() {
-  chrome.webRequest.onBeforeRequest.addListener(webPartsHandler,
-    {urls: ["https://*.microsoft.sharepoint-df.com/*/getclientsidewebparts", "https://microsofteur-my.sharepoint.com/*/getclientsidewebparts"]},
-    ['requestBody', 'blocking']);
+  chrome.webRequest.onBeforeRequest.addListener(
+    webPartsHandler,
+    {
+      urls: [
+        "https://*.microsoft.sharepoint-df.com/*/getclientsidewebparts",
+        "https://microsofteur-my.sharepoint.com/*/getclientsidewebparts"
+      ]
+    },
+    ['requestBody', 'blocking']
+  );
 }
+
 function removeListeners() {
-  chrome.webRequest.onBeforeRequest.removeListener(requestHandler);
+  chrome.webRequest.onBeforeRequest.removeListener(webPartsHandler);
 }
+
 setListeners();
--- tools/bohemia-intercept-chrome-extension/server/app.js
+++ tools/bohemia-intercept-chrome-extension/server/app.js
@@ -9,10 +9,6 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
