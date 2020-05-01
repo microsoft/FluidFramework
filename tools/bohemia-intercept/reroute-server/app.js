65fad20660bb527d6a521b2f0c5ab21985318d01
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-05-01 14:52:28 -0700

    fix policy check

--- tools/bohemia-intercept/reroute-server/app.js
+++ tools/bohemia-intercept/reroute-server/app.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 var createError = require('http-errors');
 var cors = require('cors');
 var express = require('express');
--- tools/bohemia-intercept/reroute-server/routes/index.js
+++ tools/bohemia-intercept/reroute-server/routes/index.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 var express = require('express');
 var fs = require('fs');
 var router = express.Router();
