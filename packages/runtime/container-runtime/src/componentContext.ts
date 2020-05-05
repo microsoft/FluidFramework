347b85e8bfe30fece752185f4b11d308aa2a0f5f
sumedhb1995 <sumedhb1995@gmail.com>
2020-05-04 17:40:49 -0700

    Restore hostRuntime with @deprecated tag for back compat (#2032)

--- packages/runtime/container-runtime/src/componentContext.ts
+++ packages/runtime/container-runtime/src/componentContext.ts
@@ -142,6 +142,14 @@ export abstract class ComponentContext extends EventEmitter implements IComponen
         return this._containerRuntime;
     }
 
+    /**
+     * @deprecated 0.17 Issue #1888 Rename IHostRuntime to IContainerRuntime and refactor usages
+     * Use containerRuntime instead of hostRuntime
+     */
+    public get hostRuntime(): IContainerRuntime {
+        return this._containerRuntime;
+    }
+
     public get baseSnapshot(): ISnapshotTree | undefined {
         return this._baseSnapshot;
     }
