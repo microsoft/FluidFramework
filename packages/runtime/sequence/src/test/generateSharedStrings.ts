cd84971dd9be40eaa9262953b8c6f384dd8ae0c1
Tony Murphy <anthony.murphy@microsoft.com>
2020-05-05 09:45:41 -0700

    Add Support for Body Blob Array in MergeTree Snapshot and clean V1 Contract (#1904)
    * add chunk v1
    
    * support chunk arrays
    
    * fixes
    
    * readd start index
    
    * rename snapshot files to legacy
    
    * revert some uncessary changes
    
    * add new format tests
    
    * get rid of intermediate version
    
    * more simplification
    
    * add copyright header
    
    * integrate v1 snapshot tests
    
    * lint fixes
    
    * delete old files
    
    Co-authored-by: Tony Murphy <anthonm@microsoft.com>
    
    This is an extension of the work @DLehenbauer did to support snapshotting blue segments.
    
    This change adds backwards and forwards support for Body Blob Arrays in the merge tree, in addition to backwards and forward support for a cleaned up contract in V1. We need the content blobs to be an array to support more advanced virtualization, and stable partitioning in the future. Adding the arrays now will ease in keeping those change backwards compatible, as older clients can ignore anything else added, and just serially load the blobs like they do now.
    
    Move all chunk related contracts and helper functions to snapshotChucks file
    Changed loader to always work with the latest contract regardless of contract used in snapshot
    Snapshot Legacy uses the old format, but also add necessary headerMetadata so once all clients have this code we can move to the new format.
    Snapshot V1 cleaned up the contract
    sequenceTestSnapshot*.json existing files now have header metadata, but are still backwards compatible with clients that don't know about arrays. The new files are in the new format.

--- packages/runtime/sequence/src/test/generateSharedStrings.ts
+++ packages/runtime/sequence/src/test/generateSharedStrings.ts
@@ -9,66 +9,84 @@ import * as mocks from "@microsoft/fluid-test-runtime-utils";
 import { SharedString } from "../sharedString";
 import { SharedStringFactory } from "../sequenceFactory";
 
-export const LocationBase: string = "src/test/snapshots/legacy/";
+export const LocationBase: string = "src/test/snapshots/";
+
+export const supportedVersions = new Map<string, any>([
+    ["legacy",{}],
+    ["v1",{ newMergeTreeSnapshotFormat: true }]]);
 
 export function* generateStrings(): Generator<[string, SharedString]> {
-    const documentId = "fakeId";
-    const runtime: mocks.MockRuntime = new mocks.MockRuntime();
-    const insertText = "text";
+    for (const [version, options] of supportedVersions) {
+        const documentId = "fakeId";
+        const runtime: mocks.MockRuntime = new mocks.MockRuntime();
+        for (const key of Object.keys(options)) {
+            runtime.options[key] = options[key];
+        }
+        const insertText = "text";
 
-    let sharedString = new SharedString(runtime, documentId, SharedStringFactory.Attributes);
-    sharedString.initializeLocal();
-    // Small enough so snapshot won't have body
-    for (let i = 0; i < (Snapshot.sizeOfFirstChunk / insertText.length) / 2; ++i) {
-        sharedString.insertText(0, `${insertText}${i}`);
+        let sharedString = new SharedString(runtime, documentId, SharedStringFactory.Attributes);
+        sharedString.initializeLocal();
+        // Small enough so snapshot won't have body
+        for (let i = 0; i < (Snapshot.sizeOfFirstChunk / insertText.length) / 2; ++i) {
+            sharedString.insertText(0, `${insertText}${i}`);
+        }
+
+        yield [`${version}/headerOnly`, sharedString];
+
+        sharedString = new SharedString(runtime, documentId, SharedStringFactory.Attributes);
+        sharedString.initializeLocal();
+        // Big enough that snapshot will have body
+        for (let i = 0; i < (Snapshot.sizeOfFirstChunk / insertText.length) * 2; ++i) {
+            sharedString.insertText(0, `${insertText}${i}`);
+        }
+
+        yield [`${version}/headerAndBody`, sharedString];
+
+        sharedString = new SharedString(runtime, documentId, SharedStringFactory.Attributes);
+        sharedString.initializeLocal();
+        // Very big sharedString
+        for (let i = 0; i < Snapshot.sizeOfFirstChunk; ++i) {
+            sharedString.insertText(0, `${insertText}-${i}`);
+        }
+
+        yield [`${version}/largeBody`, sharedString];
+
+        sharedString = new SharedString(runtime, documentId, SharedStringFactory.Attributes);
+        sharedString.initializeLocal();
+        // SharedString with markers
+        for (let i = 0; i < (Snapshot.sizeOfFirstChunk / insertText.length) * 2; ++i) {
+            sharedString.insertText(0, `${insertText}${i}`);
+        }
+        for (let i = 0; i < sharedString.getLength(); i += 70) {
+            sharedString.insertMarker(i, 1, {
+                ItemType: "Paragraph",
+                Properties: { Bold: false },
+                markerId: `marker${i}`,
+                referenceTileLabels: ["Eop"],
+            });
+        }
+
+        yield [`${version}/withMarkers`, sharedString];
+
+        sharedString = new SharedString(runtime, documentId, SharedStringFactory.Attributes);
+        sharedString.initializeLocal();
+        // SharedString with annotations
+        for (let i = 0; i < (Snapshot.sizeOfFirstChunk / insertText.length) * 2; ++i) {
+            sharedString.insertText(0, `${insertText}${i}`);
+        }
+        for (let i = 0; i < sharedString.getLength(); i += 70) {
+            sharedString.annotateRange(i, i + 10, { bold: true });
+        }
+
+        yield [`${version}/withAnnotations`, sharedString];
+
+        sharedString = new SharedString(runtime, documentId, SharedStringFactory.Attributes);
+        sharedString.initializeLocal();
+        // Very big sharedString
+        for (let i = 0; i < Snapshot.sizeOfFirstChunk; ++i) {
+            sharedString.insertText(0, `${insertText}-${i}`);
+        }
+
+        yield [`${version}/largeBody`, sharedString];
     }
-
-    yield ["headerOnly", sharedString];
-
-    sharedString = new SharedString(runtime, documentId, SharedStringFactory.Attributes);
-    sharedString.initializeLocal();
-    // Big enough that snapshot will have body
-    for (let i = 0; i < (Snapshot.sizeOfFirstChunk / insertText.length) * 2; ++i) {
-        sharedString.insertText(0, `${insertText}${i}`);
-    }
-
-    yield ["headerAndBody", sharedString];
-
-    sharedString = new SharedString(runtime, documentId, SharedStringFactory.Attributes);
-    sharedString.initializeLocal();
-    // Very big sharedString
-    for (let i = 0; i < Snapshot.sizeOfFirstChunk; ++i) {
-        sharedString.insertText(0, `${insertText}-${i}`);
-    }
-
-    yield ["largeBody", sharedString];
-
-    sharedString = new SharedString(runtime, documentId, SharedStringFactory.Attributes);
-    sharedString.initializeLocal();
-    // SharedString with markers
-    for (let i = 0; i < (Snapshot.sizeOfFirstChunk / insertText.length) * 2; ++i) {
-        sharedString.insertText(0, `${insertText}${i}`);
-    }
-    for (let i = 0; i < sharedString.getLength(); i += 70) {
-        sharedString.insertMarker(i, 1, {
-            ItemType: "Paragraph",
-            Properties: { Bold: false },
-            markerId: `marker${i}`,
-            referenceTileLabels: ["Eop"],
-        });
-    }
-
-    yield ["withMarkers", sharedString];
-
-    sharedString = new SharedString(runtime, documentId, SharedStringFactory.Attributes);
-    sharedString.initializeLocal();
-    // SharedString with annotations
-    for (let i = 0; i < (Snapshot.sizeOfFirstChunk / insertText.length) * 2; ++i) {
-        sharedString.insertText(0, `${insertText}${i}`);
-    }
-    for (let i = 0; i < sharedString.getLength(); i += 70) {
-        sharedString.annotateRange(i, i + 10, { bold: true });
-    }
-
-    yield ["withAnnotations", sharedString];
 }
