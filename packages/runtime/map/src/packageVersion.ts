ee3c20b0ec3adf649a2b72e0dbeb15ea6fdac991
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-05-05 19:26:49 -0700

    Bump development version for clients to 0.16.4
               @microsoft/fluid-build-common:     0.16.0 (unchanged)
              @microsoft/eslint-config-fluid:     0.16.1 (unchanged)
         @microsoft/fluid-common-definitions:     0.16.1 (unchanged)
               @microsoft/fluid-common-utils:     0.16.1 (unchanged)
                                      Server:   0.1004.3 (unchanged)
                                      Client:     0.16.3 -> 0.16.4
                  @microsoft/generator-fluid:     0.16.3 -> 0.16.4
                       @yo-fluid/dice-roller:     0.16.3 -> 0.16.4

--- packages/runtime/map/src/packageVersion.ts
+++ packages/runtime/map/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@microsoft/fluid-map";
-export const pkgVersion = "0.16.3";
+export const pkgVersion = "0.16.4";
--- packages/runtime/shared-summary-block/src/packageVersion.ts
+++ packages/runtime/shared-summary-block/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@microsoft/fluid-shared-summary-block";
-export const pkgVersion = "0.16.3";
+export const pkgVersion = "0.16.4";
