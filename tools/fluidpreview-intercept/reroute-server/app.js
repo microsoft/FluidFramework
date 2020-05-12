6d4ba50de60602d280df3b2c0a8d2910408398b9
Tyler Butler <tylerbu@microsoft.com>
2020-05-11 19:15:52 -0700

    FluidPreview Intercept: Add more URLs (#2118)
    Also fixes indentation to be our 4-space standard

--- tools/fluidpreview-intercept/reroute-server/app.js
+++ tools/fluidpreview-intercept/reroute-server/app.js
@@ -24,19 +24,19 @@ app.use(cors());
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
