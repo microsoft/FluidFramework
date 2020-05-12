dab5bf4d7ca4d7ad22ffb5eff5649393cd752b36
markfields <markfields-git@outlook.com>
2020-05-12 09:46:48 -0700

    Typos

--- packages/test/end-to-end/src/test/consensusRegisterCollectionEndToEndTests.spec.ts
+++ packages/test/end-to-end/src/test/consensusRegisterCollectionEndToEndTests.spec.ts
@@ -9,7 +9,7 @@ import { IFluidCodeDetails } from "@microsoft/fluid-container-definitions";
 import { Container } from "@microsoft/fluid-container-loader";
 import { ISharedMap, SharedMap } from "@microsoft/fluid-map";
 import {
-    IIncomingRegisterOperation,
+    ConsensusRegisterCollection,
     IConsensusRegisterCollection,
     ReadPolicy,
 } from "@microsoft/fluid-register-collection";
@@ -52,7 +52,7 @@ function generate(name: string, ctor: ISharedObjectConstructor<IConsensusRegiste
         async function createContainer(): Promise<Container> {
             const factory = new TestFluidComponentFactory([
                 [ mapId, SharedMap.getFactory() ],
-                [ undefinedIIIIncomingRegisterOperation.getFactory() ],
+                [ undefined, ConsensusRegisterCollection.getFactory() ],
             ]);
             const loader = createLocalLoader([[ codeDetails, factory ]], deltaConnectionServer);
             return initializeLocalContainer(id, loader, codeDetails);
@@ -174,8 +174,7 @@ function generate(name: string, ctor: ISharedObjectConstructor<IConsensusRegiste
             assert.strictEqual(versions6[0], "value10", "Happened after value did not overwrite");
         });
 
-        // Disabled temporarily due to n/n-1 issues (see https://github.com/microsoft/FluidFramework/issues/2130)
-        it.skip("Can store handles", async () => {
+        it("Can store handles", async () => {
             // Set up the collection with two handles and add it to the map so other containers can find it
             const collection1 = ctor.create(component1.runtime);
             sharedMap1.set("test", "sampleValue");
@@ -204,4 +203,4 @@ function generate(name: string, ctor: ISharedObjectConstructor<IConsensusRegiste
     });
 }
 
-generate("ConsensusRegisterCollectioIIIIIIncomingRegisterOperation);
+generate("ConsensusRegisterCollection", ConsensusRegisterCollection);
