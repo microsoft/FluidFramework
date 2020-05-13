c246984e43f533c93c1ba5e2dc6136115e0e7e8e
sumedhb1995 <sumedhb1995@gmail.com>
2020-05-13 15:21:20 -0700

    Merge/0.17.3 to release/0.17.x (#2172)
    * Remove pre-release dependencies for client
    
                                      Client -> ^0.17.3
    
    * Bump development version for clients to 0.17.4
    
               @microsoft/fluid-build-common:     0.16.0 (unchanged)
              @microsoft/eslint-config-fluid:     0.17.0 (unchanged)
         @microsoft/fluid-common-definitions:     0.17.0 (unchanged)
               @microsoft/fluid-common-utils:     0.17.1 (unchanged)
                                      Server:   0.1005.0 (unchanged)
                                      Client:     0.17.3 -> 0.17.4
                  @microsoft/generator-fluid:     0.17.3 -> 0.17.4
                       @yo-fluid/dice-roller:     0.17.3 -> 0.17.4
    
    Co-authored-by: Sumedh Bhattacharya <sumbhatt@microsoft.com>

--- packages/server/local-test-utils/src/packageVersion.ts
+++ packages/server/local-test-utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@microsoft/fluid-local-test-utils";
-export const pkgVersion = "0.17.3";
+export const pkgVersion = "0.17.4";
--- packages/test/functional/src/packageVersion.ts
+++ packages/test/functional/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-internal/functional-tests";
-export const pkgVersion = "0.17.3";
+export const pkgVersion = "0.17.4";
