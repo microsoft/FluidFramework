c9e56577b612476403e6a7234fee09db87efcc6f
Jatin Garg <48029724+jatgarg@users.noreply.github.com>
2020-05-18 10:46:22 -0700

    Correctly implement isAttached on handle and dont send attach op if the component is local (#2187)
    * correctly implement isAttached on handle and add tests and dont send attach op if the component is local
    
    * fix assert

--- packages/runtime/shared-object-common/src/types.ts
+++ packages/runtime/shared-object-common/src/types.ts
@@ -48,6 +48,13 @@ export interface ISharedObject<TEvent extends ISharedObjectEvents = ISharedObjec
     isRegistered(): boolean;
 
     /**
+     * Returns whether the given shared object is attached to parent component. Parent component
+     * should also be attached. It does not matter if the container is live or local.
+     * @returns True if the given shared object is attached
+     */
+    isAttached(): boolean;
+
+    /**
      * Gets a form of the object that can be serialized.
      * @returns A tree representing the snapshot of the shared object
      */
