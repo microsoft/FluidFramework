f0e7a97585e73be5ead47efc0200ac02e317f166
Curtis Man <curtism@microsoft.com>
2020-05-19 09:03:19 -0700

    Remove prerelease dependency to common-definitions 0.17.0
    Bump package version of common-utils to 0.18.1
    Bump package version of server to 0.1005.1
    Bump package version of client to 0.18.1

--- packages/drivers/create-new-driver/src/packageVersion.ts
+++ packages/drivers/create-new-driver/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@microsoft/fluid-experimental-creation-driver";
-export const pkgVersion = "0.18.0";
+export const pkgVersion = "0.18.1";
--- packages/runtime/matrix/src/packageVersion.ts
+++ packages/runtime/matrix/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@microsoft/fluid-matrix";
-export const pkgVersion = "0.18.0";
+export const pkgVersion = "0.18.1";
