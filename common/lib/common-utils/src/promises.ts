0cf4b599bba5c7e8dbdcfd71e937db10e10f317b
Tyler Butler <tylerbu@microsoft.com>
2020-05-20 12:42:30 -0700

    Update common packages to use new version of fluid-build-common with esModuleInterop=true (#2245)

--- common/lib/common-utils/src/promises.ts
+++ common/lib/common-utils/src/promises.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import * as assert from "assert";
+import assert from "assert";
 
 /**
  * A deferred creates a promise and the ability to resolve or reject it
--- common/lib/common-utils/src/test/typedEventEmitter.spec.ts
+++ common/lib/common-utils/src/test/typedEventEmitter.spec.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-import * as assert from "assert";
+import assert from "assert";
 import { IErrorEvent } from "@microsoft/fluid-common-definitions";
 import { TypedEventEmitter } from "../typedEventEmitter";
 
