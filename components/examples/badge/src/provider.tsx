c28774925db3bd19618797a2eed83ac703666330
Micah Godbolt <mgodbolt@microsoft.com>
2020-05-22 13:41:28 -0700

    remove unneeded provider

--- components/examples/badge/src/provider.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export const BadgeContext: React.Context<any> = React.createContext(undefined);
-
-export const useBadgeContext = () => React.useContext(BadgeContext);
