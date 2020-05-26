df06e709c845802e2e7d90ecfaed379c4f48851c
Vlad Sudzilouski <vlad@sudzilouski.com>
2020-05-25 18:34:03 -0700

    build break

--- packages/drivers/odsp-socket-storage/src/odspDocumentService.ts
+++ packages/drivers/odsp-socket-storage/src/odspDocumentService.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import * as assert from "assert";
+import assert from "assert";
 import { ITelemetryBaseLogger, ITelemetryLogger } from "@fluidframework/common-definitions";
 import {
     ChildLogger,
