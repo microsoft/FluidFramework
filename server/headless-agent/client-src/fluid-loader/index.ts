c6ad845ab4cf507a7adb25008092315e7040d1d0
Curtis Man <curtism@microsoft.com>
2020-05-27 20:54:38 -0700

    Fix loader

--- server/headless-agent/client-src/fluid-loader/index.ts
+++ server/headless-agent/client-src/fluid-loader/index.ts
@@ -7,13 +7,15 @@ import { BaseHost, IBaseHostConfig } from "@fluidframework/base-host";
 import { Container } from "@fluidframework/container-loader";
 import { IFluidResolvedUrl, IResolvedUrl } from "@fluidframework/driver-definitions";
 import { DefaultErrorTracking, RouterliciousDocumentServiceFactory } from "@fluidframework/routerlicious-driver";
+import { SemVerCdnCodeResolver } from "@fluidframework/web-code-loader";
+import { HTMLViewAdapter } from "@fluidframework/view-adapters";
 
 interface IWindow extends Window {
     startLoading(): Promise<void>;
     closeContainer(): void;
 }
 
-export async function startLoading(resolvedUrl: IResolvedUrl): Promise<Container> {
+export async function startLoading(resolvedUrl: IResolvedUrl) {
     const serviceFactory = new RouterliciousDocumentServiceFactory(
         false,
         new DefaultErrorTracking(),
@@ -23,15 +25,20 @@ export async function startLoading(resolvedUrl: IResolvedUrl): Promise<Container
         documentServiceFactory: serviceFactory,
         urlResolver: {
             resolve: () => Promise.resolve(resolvedUrl),
+            async getAbsoluteUrl(
+                resolvedUrl: IResolvedUrl,
+                relativeUrl: string,
+            ): Promise<string> {
+                throw new Error("Not implemented");
+            },
         },
+        codeResolver: new SemVerCdnCodeResolver(),
     };
 
-    const baseHost = new BaseHost(baseHostConfig, resolvedUrl, undefined, [] );
-    const container =  await baseHost.loadAndRender(
-        (resolvedUrl as IFluidResolvedUrl).url,
-        document.getElementById("content") as HTMLDivElement);
-
-    return container;
+    const baseHost = new BaseHost(baseHostConfig);
+    const component =  await baseHost.getComponent((resolvedUrl as IFluidResolvedUrl).url);
+    const adapter = new HTMLViewAdapter(component);
+    adapter.render(document.getElementById("content") as HTMLDivElement);
 }
 
 // Checks container quorum for connected clients. Once all client leaves,
