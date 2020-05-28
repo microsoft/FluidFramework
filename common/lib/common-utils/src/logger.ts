f162700d79ae43449c5a6c38f795e0811bf830a5
Curtis Man <curtism@microsoft.com>
2020-05-28 11:19:35 -0700

    Rename and add synced version'

--- common/lib/common-utils/src/logger.ts
+++ common/lib/common-utils/src/logger.ts
@@ -474,7 +474,23 @@ export class PerformanceEvent {
         return new PerformanceEvent(logger, event);
     }
 
-    public static async timedAsync<T>(
+    public static timedExec<T>(
+        logger: ITelemetryLogger,
+        event: ITelemetryGenericEvent,
+        callback: () => T,
+    ) {
+        const perfEvent = PerformanceEvent.start(logger, event);
+        try {
+            const ret = callback();
+            perfEvent.end();
+            return ret;
+        } catch (error) {
+            perfEvent.cancel(undefined, error);
+            throw error;
+        }
+    }
+
+    public static async timedExecAsync<T>(
         logger: ITelemetryLogger,
         event: ITelemetryGenericEvent,
         callback: () => Promise<T>,
