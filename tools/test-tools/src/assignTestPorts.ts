e270c42da63a0c3db3f1301fa1dfe9347344dd94
Helio Liu <59622401+heliocliu@users.noreply.github.com>
2020-05-29 13:49:51 -0700

    Add test-tools package for shared test support code (#2328)
    Pt1 of fixing #1341
    
    Add test-tools package to hold shared test support code, such as port management for jest tests (and maybe other things in the future?).
    
    For jest ports, the issue is that they're currently hardcoded in the test configs, and all tests are run in parallel. Meaning that anyone adding a new test needs to find the next available port and hardcode that as well. This change adds a script that sets up a mapping of unique ports so that packages running tests will not see collisions when run under lerna (and new test writers do not need to futz around with port values).
    
    Updating consumers coming in a followup after this new package becomes available for import (couldn't test some things such as the binary availability due to this)

---
+++ tools/test-tools/src/assignTestPorts.ts
@@ -0,0 +1,40 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import * as child_process from "child_process";
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
+
+function main() {
+    // Get the lerna output
+    let lernaOutput;
+    try {
+        lernaOutput = JSON.parse(child_process.execSync("npx lerna list --all --json").toString());
+        if (!Array.isArray(lernaOutput)) {
+            throw new Error("stdin input was not package array");
+        }
+    } catch (e) {
+        console.error(e.toString());
+        process.exit(-1);
+    }
+
+    // Assign a unique port to each package
+    const portMap: { [pkgName: string]: number } = {};
+    let port = 8081;
+    lernaOutput.forEach((pkg: {name: string}) => {
+        if (pkg.name === undefined) {
+            console.error("missing name in lerna package entry");
+            process.exit(-1);
+        }
+        portMap[pkg.name] = port++;
+    });
+
+    // Write the mappings to a temporary file as kv pairs
+    const portMapPath = path.join(os.tmpdir(), "testportmap.json");
+    fs.writeFileSync(portMapPath, JSON.stringify(portMap));
+}
+
+main();
