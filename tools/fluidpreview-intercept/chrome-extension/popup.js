43a0cbb2a42dfea6ec1a42f509813953b3f99e3f
sumedhb1995 <sumedhb1995@gmail.com>
2020-05-29 14:18:57 -0700

    Add initial UI for Intercept Chrome extension (#2331)
    * add init UI for extension
    
    * add copyright headers
    
    Co-authored-by: Sumedh Bhattacharya <sumbhatt@microsoft.com>

---
+++ tools/fluidpreview-intercept/chrome-extension/popup.js
@@ -0,0 +1,25 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+let toggleSwitch = document.getElementById('toggle-switch');
+toggleSwitch.onclick = function(element) {
+    if (element.target.className === "off") {
+        element.target.className = "on";
+        chrome.storage.sync.set({interceptDisabled: true})
+    } else {
+        element.target.className = "off";
+        chrome.storage.sync.set({interceptDisabled: false})
+    }
+};
+
+chrome.storage.sync.get('interceptDisabled', function(data) {
+    if(data.interceptDisabled === true){
+        toggleSwitch.checked = true;
+        toggleSwitch.className = "on";
+    } else {
+        toggleSwitch.checked = undefined;
+        toggleSwitch.className = "off"
+    }
+});
