9a7b5578b32a540905f572b3ca33da3110e233bf
Arin <artaylor@microsoft.com>
2020-06-02 17:26:39 -0400

    Depend on newer version of timers

--- packages/runtime/container-runtime/src/summarizer.ts
+++ packages/runtime/container-runtime/src/summarizer.ts
@@ -5,7 +5,7 @@
 
 import { EventEmitter } from "events";
 import { IDisposable, IEvent, IEventProvider, ITelemetryLogger } from "@fluidframework/common-definitions";
-import { ChildLogger, Deferred, PerformanceEvent, PromiseTimer, Timer } from "@fluidframework/common-utils";
+import { ChildLogger, Deferred, PerformanceEvent, PromiseTimer, Timer, IPromiseTimerResult } from "@fluidframework/common-utils";
 import {
     IComponentLoadable,
     IComponentRouter,
@@ -105,6 +105,13 @@ export interface ISummaryAttempt {
     summarySequenceNumber?: number;
 }
 
+const checkNotTimeout = <T>(something: T | IPromiseTimerResult | undefined): something is T => {
+    if (something === undefined) {
+        return false;
+    }
+    return (something as IPromiseTimerResult).timerResult === undefined;
+};
+
 /**
  * This class contains the heuristics for when to summarize.
  */
@@ -160,7 +167,7 @@ class SummarizerHeuristics {
         } else if (opCountSinceLastSummary > this.configuration.maxOps) {
             this.trySummarize("maxOps");
         } else {
-            this.idleTimer.start();
+            this.idleTimer.restart();
         }
     }
 
@@ -327,7 +334,7 @@ export class RunningSummarizer implements IDisposable {
         ]);
         this.pendingAckTimer.clear();
 
-        if (maybeLastAck) {
+        if (checkNotTimeout(maybeLastAck)) {
             this.heuristics.lastSent = {
                 refSequenceNumber: maybeLastAck.summaryOp.referenceSequenceNumber,
                 summaryTime: maybeLastAck.summaryOp.timestamp,
@@ -410,7 +417,7 @@ export class RunningSummarizer implements IDisposable {
         // Wait for broadcast
         const summaryOp = await Promise.race([summary.waitBroadcast(), pendingTimeoutP]);
         broadcastDef.resolve(); // broadcast means client is free to close
-        if (!summaryOp) {
+        if (!checkNotTimeout(summaryOp)) {
             return undefined;
         }
         this.heuristics.lastSent.summarySequenceNumber = summaryOp.sequenceNumber;
@@ -424,7 +431,7 @@ export class RunningSummarizer implements IDisposable {
 
         // Wait for ack/nack
         const ackNack = await Promise.race([summary.waitAckNack(), pendingTimeoutP]);
-        if (!ackNack) {
+        if (!checkNotTimeout(ackNack)) {
             return undefined;
         }
         this.logger.sendTelemetryEvent({
