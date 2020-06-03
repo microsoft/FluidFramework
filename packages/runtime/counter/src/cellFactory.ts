7d285cf7481e24850407d587bd98c7e2b27fc2ef
Matt Rakow <marakow@microsoft.com>
2020-06-02 18:58:33 -0700

    Copy over cell

---
+++ packages/runtime/counter/src/cellFactory.ts
@@ -0,0 +1,52 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {
+    IChannelAttributes,
+    IComponentRuntime,
+    ISharedObjectServices,
+} from "@fluidframework/component-runtime-definitions";
+import { ISharedObjectFactory } from "@fluidframework/shared-object-base";
+import { SharedCell } from "./cell";
+import { ISharedCell } from "./interfaces";
+import { pkgVersion } from "./packageVersion";
+
+/**
+ * The factory that defines the map
+ */
+export class CellFactory implements ISharedObjectFactory {
+    public static readonly Type = "https://graph.microsoft.com/types/cell";
+
+    public static readonly Attributes: IChannelAttributes = {
+        type: CellFactory.Type,
+        snapshotFormatVersion: "0.1",
+        packageVersion: pkgVersion,
+    };
+
+    public get type() {
+        return CellFactory.Type;
+    }
+
+    public get attributes() {
+        return CellFactory.Attributes;
+    }
+
+    public async load(
+        runtime: IComponentRuntime,
+        id: string,
+        services: ISharedObjectServices,
+        branchId: string,
+        attributes: IChannelAttributes): Promise<ISharedCell> {
+        const cell = new SharedCell(id, runtime, attributes);
+        await cell.load(branchId, services);
+        return cell;
+    }
+
+    public create(document: IComponentRuntime, id: string): ISharedCell {
+        const cell = new SharedCell(id, document, this.attributes);
+        cell.initializeLocal();
+        return cell;
+    }
+}
---
+++ packages/runtime/counter/src/test/cell.spec.ts
@@ -0,0 +1,31 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import assert from "assert";
+import { MockRuntime } from "@fluidframework/test-runtime-utils";
+import { CellFactory } from "../cellFactory";
+import { ISharedCell } from "..";
+
+describe("Routerlicious", () => {
+    describe("Api", () => {
+        describe("cell", () => {
+            let testCell: ISharedCell;
+
+            beforeEach(async () => {
+                const factory = new CellFactory();
+                testCell = factory.create(new MockRuntime(), "cell");
+            });
+
+            it("Can create a cell", () => {
+                assert.ok(testCell);
+            });
+
+            it("Can set and get cell data", async () => {
+                testCell.set("testValue");
+                assert.equal(testCell.get(), "testValue");
+            });
+        });
+    });
+});
