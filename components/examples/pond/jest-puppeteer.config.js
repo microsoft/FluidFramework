9ff6a3653070eed1178ed449ad1a7a205944c7f6
Helio Liu <59622401+heliocliu@users.noreply.github.com>
2020-06-03 22:20:14 -0700

    Update tests that use ports to not hardcode them (#2356)
    Fixes #1341
    Use the new test-tools infra to assign ports dynamically to tests instead of hardcoding them
    There's the question of not duplicating all these config files now that they are all identical, but there are some issues with relative paths that I didn't want to address here.

--- components/examples/pond/jest-puppeteer.config.js
+++ components/examples/pond/jest-puppeteer.config.js
@@ -5,8 +5,8 @@
 
 module.exports = {
   server: {
-    command: "npm run start -- --no-live-reload --port 8718",
-    port: 8718,
+    command: `npm run start -- --no-live-reload --port ${process.env["PORT"]}`,
+    port: process.env["PORT"],
     launchTimeout: 10000,
     usedPortAction: 'error'
   },
