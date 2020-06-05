dd01d2b8580878ed05c6546d2794ec22c261b950
Curtis Man <curtism@microsoft.com>
2020-06-05 10:44:15 -0700

    Update

--- packages/test/node-stress-test/src/nodeStressTest.ts
+++ packages/test/node-stress-test/src/nodeStressTest.ts
@@ -6,75 +6,26 @@
 import child_process from "child_process";
 import { IProxyLoaderFactory, IFluidCodeDetails } from "@fluidframework/container-definitions";
 import { Loader } from "@fluidframework/container-loader";
-import { IContainerRuntime } from "@fluidframework/container-runtime-definitions";
 import { OdspDocumentServiceFactory, OdspDriverUrlResolver } from "@fluidframework/odsp-driver";
 import { LocalCodeLoader } from "@fluidframework/test-utils";
-import {
-    ContainerRuntimeFactoryWithDefaultComponent,
-    PrimedComponent,
-    PrimedComponentFactory,
-} from "@fluidframework/aqueduct";
 
 import { OdspTokenManager, odspTokensCache } from "@fluidframework/tool-utils";
+import { pkgName, pkgVersion } from "./packageVersion";
+import { IRunConfig, fluidExport, ILoadTest } from "./loadTestComponent";
+const packageName = `${pkgName}@${pkgVersion}`;
 
-// TODO: Make this a parameter
+// TODO: Make these parameters
 const server = "a830edad9050849829E20060408.sharepoint.com";
 const tenant = `https://${server}`;
 const driveId = "b!o96WcQ93ck-dT5tlJfA7yZNP3Z9aM69JjJI6U4ASSXmZLLDGFcMBSqJ3iB3y04h0";
 
-const packageName = "@fluid-internal/node-stress-test@0.1.0";
-
-const wait = async (time: number) => new Promise((resolve) => setTimeout(resolve, time));
-
-interface IStressTest {
-    run(): Promise<void>;
-}
-class StressTestComponent extends PrimedComponent implements IStressTest {
-    public static ComponentName = "StressTestComponent";
-    private opCount = 0;
-
-    protected async componentHasInitialized() {
-        this.root.on("op", () => {
-            if (++this.opCount % 1000 === 0) {
-                console.log(`${process.argv[3]}> seen = ${this.opCount}`);
-            }
-        });
-    }
-    public async run() {
-        for (let i = 0; i < 10000; i++) {
-            if (i % 10 === 0) {
-                await wait(500 + Math.random() * 1000);
-            }
-            if (i % 100 === 0) {
-                console.log(`${process.argv[3]}> sent = ${i}`);
-            }
-            await this.runStep();
-        }
-    }
-    public async runStep() {
-        this.root.set(Math.floor(Math.random() * 32).toString(), Math.random());
-    }
-
-    public print() {
-        console.log((this.context.hostRuntime as IContainerRuntime).isDocumentDirty());
-        console.log(`Op count: ${this.opCount}`);
-        this.root.forEach((value, key) => {
-            console.log(key, value);
-        });
-    }
-}
-
-const StressTestComponentInstantiationFactory = new PrimedComponentFactory(
-    StressTestComponent.ComponentName,
-    StressTestComponent,
-    [],
-    {},
-);
-
-const fluidExport = new ContainerRuntimeFactoryWithDefaultComponent(
-    StressTestComponent.ComponentName,
-    new Map([[StressTestComponent.ComponentName, Promise.resolve(StressTestComponentInstantiationFactory)]]),
-);
+const runConfig: IRunConfig = {
+    runId: 0,
+    opRatePerMin: 15,
+    progressInterval: 15000,
+    numClients: 240,
+    totalSendCount: 10000000,
+};
 
 const codeDetails: IFluidCodeDetails = {
     package: packageName,
@@ -159,7 +110,7 @@ async function initialize() {
     });
     const request = urlResolver.createCreateNewRequest(tenant, driveId, "/test", "test");
     await container.attach(request);
-    const componentUrl = await urlResolver.getAbsoluteUrl(container.resolvedUrl, "/");
+    const componentUrl = await container.getAbsoluteUrl("/");
     console.log(componentUrl);
     container.close();
 
@@ -169,21 +120,58 @@ async function initialize() {
 async function load(url: string) {
     const loader = createLoader();
     const respond = await loader.request({ url });
-    return respond.value as StressTestComponent;
+    // TODO: Error checking
+    return respond.value as ILoadTest;
 }
 
 async function main() {
+    // TODO: switch to use commander
     if (process.argv[2] === "--run") {
         if (process.argv[3] !== undefined && process.argv[4] !== undefined) {
+            runConfig.runId = parseInt(process.argv[3], 10);
             const stressTest = await load(process.argv[4]);
-            await stressTest.run();
-            stressTest.print();
+            await stressTest.run(runConfig);
             process.exit(0);
         }
+        console.error("Missing arguments for run");
+        process.exit(-1);
     }
-    const componentUrl = await initialize();
+
+    let nextArg = process.argv[2];
+    let componentUrl: string;
+    if (process.argv[2] === "--spawn") {
+        // Use a pre-existing file
+        componentUrl = process.argv[3];
+        nextArg = process.argv[4];
+    }
+
+    if (nextArg === "--refresh") {
+        await odspTokenManager.getOdspTokens(
+            server, // REVIEW
+            getMicrosoftConfiguration(),
+            fluidFetchWebNavigator,
+            undefined,
+            undefined,
+            true,
+        );
+
+        await odspTokenManager.getPushTokens(
+            server,  // REVIEW
+            getMicrosoftConfiguration(),
+            fluidFetchWebNavigator,
+            undefined,
+            undefined,
+            true,
+        );
+    }
+
+    if (componentUrl === undefined) {
+        // Create a new file
+        componentUrl = await initialize();
+    }
+
     const p: Promise<void>[] = [];
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < runConfig.numClients; i++) {
         const process = child_process.spawn(
             "node",
             ["dist\\nodeStressTest.js", "--run", i.toString(), componentUrl],
