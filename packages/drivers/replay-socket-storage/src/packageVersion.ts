65a5f35ffd7f813ba97956504f91af2aea8b477e
Arin <artaylor@microsoft.com>
2020-06-05 12:32:13 -0400

    Bump development version for Client to 0.19.5
                @fluidframework/build-common:     0.16.1 (unchanged)
         @fluidframework/eslint-config-fluid:     0.17.1 (unchanged)
          @fluidframework/common-definitions:     0.18.2 (unchanged)
                @fluidframework/common-utils:     0.19.2 (unchanged)
                                      Server:   0.1006.2 (unchanged)
                                      Client:     0.19.4 -> 0.19.5
                  @microsoft/generator-fluid:     0.19.3 -> 0.19.4
                       @yo-fluid/dice-roller:     0.19.3 -> 0.19.4

--- packages/drivers/replay-socket-storage/src/packageVersion.ts
+++ packages/drivers/replay-socket-storage/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/replay-driver";
-export const pkgVersion = "0.19.4";
+export const pkgVersion = "0.19.5";
--- packages/runtime/consensus-ordered-collection/src/packageVersion.ts
+++ packages/runtime/consensus-ordered-collection/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/ordered-collection";
-export const pkgVersion = "0.19.4";
+export const pkgVersion = "0.19.5";
