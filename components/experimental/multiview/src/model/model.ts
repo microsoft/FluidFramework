b3404e4aa4f921dfe09c3786e85e9c07fb08cd83
Matt Rakow <marakow@microsoft.com>
2020-06-08 17:49:45 -0700

    Remove some comments

--- components/experimental/multiview/src/model/model.ts
+++ components/experimental/multiview/src/model/model.ts
@@ -19,12 +19,6 @@ const optionValueKey = "optionValue";
 export class OptionPicker extends PrimedComponent implements IOptionPicker {
     public static get ComponentName() { return "@fluid-example/option-picker"; }
 
-    /**
-     * ComponentInitializingFirstTime is called only once, it is executed only by the first client to open the
-     * component and all work will resolve before the view is presented to any user.
-     *
-     * This method is used to perform component setup, which can include setting an initial schema or initial values.
-     */
     protected async componentInitializingFirstTime() {
         this.root.set(optionValueKey, "First");
     }
@@ -46,10 +40,6 @@ export class OptionPicker extends PrimedComponent implements IOptionPicker {
     };
 }
 
-/**
- * The PrimedComponentFactory declares the component and defines any additional distributed data structures.
- * To add a SharedSequence, SharedMap, or any other structure, put it in the array below.
- */
 export const OptionPickerInstantiationFactory = new PrimedComponentFactory(
     OptionPicker.ComponentName,
     OptionPicker,
