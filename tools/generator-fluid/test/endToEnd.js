e6b12297152c063926a567644493114f65c7af37
Skyler Jokiel <skjokiel@microsoft.com>
2020-06-11 10:35:51 -0700

    Yo fluid add scaffolding option (#2488)
    Add scaffolding to `yo fluid`. This allows to have a beginner and advanced dice-roller output which will make it easier for developers to get bootstrapped.

--- tools/generator-fluid/test/endToEnd.js
+++ tools/generator-fluid/test/endToEnd.js
@@ -13,7 +13,7 @@ describe("Yo fluid", function () {
     this.timeout(300000);
 
     describe("End to End", () => {
-        describe("View - React", () => {
+        describe("View - React - beginner", () => {
             let runContext;
             let dirPath;
             let oldCwd;
@@ -22,7 +22,8 @@ describe("Yo fluid", function () {
                 runContext = helpers.run(path.join(__dirname, "../app/index.js"))
                     .withPrompts({
                         componentName: "foobar",
-                        viewFramework: "react"
+                        viewFramework: "react",
+                        scaffolding: "beginner",
                     }).inTmpDir((dir) => {
                         dirPath = dir
                     });
@@ -56,7 +57,7 @@ describe("Yo fluid", function () {
             });
         });
 
-        describe("View - None", () => {
+        describe("View - React - advanced", () => {
             let runContext;
             let dirPath;
             let oldCwd;
@@ -65,7 +66,96 @@ describe("Yo fluid", function () {
                 runContext = helpers.run(path.join(__dirname, "../app/index.js"))
                     .withPrompts({
                         componentName: "foobar",
-                        viewFramework: "none"
+                        viewFramework: "react",
+                        scaffolding: "advanced",
+                    }).inTmpDir((dir) => {
+                        dirPath = dir
+                    });
+                return runContext;
+            });
+
+            it("installs and runs tests tests with output", () => {
+                // Validate that there is an expected file
+                const expectedFiles = ["README.md"];
+                assert.file(expectedFiles);
+
+                // Navigate to the temp folder that 
+                const tempComponentPath = `${dirPath}/foobar`;
+                shell.echo(`Navigating to temp path ${tempComponentPath}`);
+                shell.cd(tempComponentPath);
+
+                // Navigate to the temp folder that is created
+                shell.echo("Running npm i - this can take some time...");
+                const installResponse = shell.exec("npm i", { silent: true });
+                assert.equal(installResponse.code, 0, `npm install failed with code: ${installResponse.code} - error: ${installResponse.stderr}`);
+                
+                // Run jest test for new component
+                shell.echo("Running npm test - this can take some time...");
+                const testResponse = shell.exec("npm test", { silent: true });
+                assert.equal(testResponse.code && testResponse.code, 0, `npm test failed with code: ${testResponse.code} - error: ${testResponse.stderr}`);
+            });
+
+            after(() => {
+                process.chdir(oldCwd);
+                runContext.cleanTestDirectory();
+            });
+        });
+
+        describe("View - None - beginner", () => {
+            let runContext;
+            let dirPath;
+            let oldCwd;
+            before(() => {
+                oldCwd = process.cwd();
+                runContext = helpers.run(path.join(__dirname, "../app/index.js"))
+                    .withPrompts({
+                        componentName: "foobar",
+                        viewFramework: "none",
+                        scaffolding: "beginner",
+                    }).inTmpDir((dir) => {
+                        dirPath = dir
+                    });
+                return runContext;
+            });
+
+            it("installs and runs tests tests with output", () => {
+                // Validate that there is an expected file
+                const expectedFiles = ["README.md"];
+                assert.file(expectedFiles);
+
+                // Navigate to the temp folder that 
+                const tempComponentPath = `${dirPath}/foobar`;
+                shell.echo(`Navigating to temp path ${tempComponentPath}`);
+                shell.cd(tempComponentPath);
+
+                // Navigate to the temp folder that is created
+                shell.echo("Running npm i - this can take some time...");
+                const installResponse = shell.exec("npm i", { silent: true });
+                assert.equal(installResponse.code, 0, `npm install failed with code: ${installResponse.code} - error: ${installResponse.stderr}`);
+                
+                // Run jest test for new component
+                shell.echo("Running npm test - this can take some time...");
+                const testResponse = shell.exec("npm test", { silent: true });
+                assert.equal(testResponse.code && testResponse.code, 0, `npm test failed with code: ${testResponse.code} - error: ${testResponse.stderr}`);
+            });
+
+            after(() => {
+                process.chdir(oldCwd);
+                runContext.cleanTestDirectory();
+            });
+        });
+        
+        describe("View - None - advance", () => {
+            let runContext;
+            let dirPath;
+            let oldCwd;
+            before(() => {
+                oldCwd = process.cwd();
+                runContext = helpers.run(path.join(__dirname, "../app/index.js"))
+                    .withPrompts({
+                        componentName: "foobar",
+                        viewFramework: "none",
+                        scaffolding: "advanced",
                     }).inTmpDir((dir) => {
                         dirPath = dir
                     });
