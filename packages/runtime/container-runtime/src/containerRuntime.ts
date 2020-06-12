cdc08d08cfd643d01f888a307b2d20553127f536
Vlad Sudzilouski <vlad@sudzilouski.com>
2020-06-11 17:40:45 -0700

    Back compat

--- packages/runtime/container-runtime/src/containerRuntime.ts
+++ packages/runtime/container-runtime/src/containerRuntime.ts
@@ -402,6 +402,14 @@ export class ContainerRuntime extends EventEmitter implements IContainerRuntime,
         runtimeOptions?: IContainerRuntimeOptions,
         containerScope: IComponent = context.scope,
     ): Promise<ContainerRuntime> {
+
+        // Back-compat: <= 0.18
+        if (context.deltaManager.lastSequenceNumber === undefined) {
+            Object.defineProperty(context.deltaManager, "lastSequenceNumber", {
+                get: function() { return (context.deltaManager as any).referenceSequenceNumber; }
+            });
+        }
+    
         const componentRegistry = new ContainerRuntimeComponentRegistry(registryEntries);
 
         const chunkId = context.baseSnapshot?.blobs[".chunks"];
