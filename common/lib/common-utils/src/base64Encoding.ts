4e0c6a7f6e8b7eb4f96de39cebf14cae44199360
Curtis Man <curtism@microsoft.com>
2020-06-23 18:48:13 -0700

    Remove node typings from common-utils (#2666)

--- common/lib/common-utils/src/base64Encoding.ts
+++ common/lib/common-utils/src/base64Encoding.ts
@@ -3,6 +3,8 @@
  * Licensed under the MIT License.
  */
 
+import { Buffer } from "buffer";
+
 export const fromBase64ToUtf8 = (input: string): string => Buffer.from(input, "base64").toString("utf-8");
 
 export const fromUtf8ToBase64 = (input: string): string => Buffer.from(input, "utf8").toString("base64");
