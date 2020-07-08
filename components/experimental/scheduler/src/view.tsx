a01e8eaa48a503b2e82f7e06cac082724f6df4a3
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-07-08 16:49:19 -0700

    del eslint ignore

--- components/experimental/scheduler/src/view.tsx
+++ components/experimental/scheduler/src/view.tsx
@@ -3,7 +3,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-/* eslint-disable import/no-internal-modules */
 
 import * as React from "react";
 import {
