bdfb3595167ff2514380a281fb36fc95ddac5b76
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-07-14 17:00:39 -0700

    remove extra

--- components/experimental/scheduler/src/data/index.ts
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
-export * from "./constants";
-export * from "./maps";
-export * from "./reducers";
