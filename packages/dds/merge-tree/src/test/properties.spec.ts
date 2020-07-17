ccdd74c92ede2fcc1f3f7529b155d42946d6ced1
Tony Murphy <anthonm@microsoft.com>
2020-07-17 16:52:28 -0700

    add copyright header

--- packages/dds/merge-tree/src/test/properties.spec.ts
+++ packages/dds/merge-tree/src/test/properties.spec.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import assert from "assert";
 import { matchProperties } from "../properties";
 
