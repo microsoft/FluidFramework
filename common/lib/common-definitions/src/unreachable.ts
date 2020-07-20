5f145525cb46e8e31419fb2cda1317bbf5d1cfc2
Mark Fields <markfields@users.noreply.github.com>
2020-07-20 14:07:09 -0700

    Add unreachableCase to common-definitions

---
+++ common/lib/common-definitions/src/unreachable.ts
@@ -0,0 +1,11 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * This function can be used to assert at compile time that a given value has type never.
+ * One common usage is in the default case of a switch block,
+ * to ensure that all cases are explicitly handled.
+ */
+export const unreachableCase: (value: never) => void = () => {};
