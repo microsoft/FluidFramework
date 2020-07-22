96343abf746d06beacfb6cc7ab752a06c0823364
Navin Agarwal <naagar@microsoft.com>
2020-07-22 14:42:06 -0700

    Fix for scribe not starting in tinylcious with detached container flow

--- server/tinylicious/src/utils.ts
+++ server/tinylicious/src/utils.ts
@@ -59,3 +59,22 @@ export interface IAlfredUser extends IUser {
 export function getParam(params: Params, key: string) {
     return Array.isArray(params) ? undefined : params[key];
 }
+
+/**
+ * Helper function to convert Request's query param to a number.
+ * @param value - The value to be converted to number.
+ */
+export function queryParamToNumber(value: any): number {
+    if (typeof value !== "string") { return undefined; }
+    const parsedValue = parseInt(value, 10);
+    return isNaN(parsedValue) ? undefined : parsedValue;
+}
+
+/**
+ * Helper function to convert Request's query param to a string.
+ * @param value - The value to be converted to number.
+ */
+export function queryParamToString(value: any): string {
+    if (typeof value !== "string") { return undefined; }
+    return value;
+}
