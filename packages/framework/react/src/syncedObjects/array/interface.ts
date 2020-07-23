f67ce169637b27bb9b5cb3aae556762a0bbabd38
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-07-23 14:35:06 -0700

    restore arrray

--- packages/framework/react/src/syncedObjects/array/interface.ts
+++ packages/framework/react/src/syncedObjects/array/interface.ts
@@ -11,33 +11,20 @@ import {
     FluidStateUpdateFunction,
 } from "../..";
 
-<<<<<<< HEAD
-=======
 /**
  * The state interface exposed to the view for the synced array
  */
->>>>>>> 53f0e4a434353df720e33ce5f452a6b9b0b1d2e1
 export interface ISyncedArrayViewState<T> extends IFluidFunctionalComponentViewState {
     values: T[];
 }
 
-<<<<<<< HEAD
-=======
 /**
  * The state interface for the Fluid data source that powers the synced array
  */
->>>>>>> 53f0e4a434353df720e33ce5f452a6b9b0b1d2e1
 export interface ISyncedArrayFluidState<T> extends IFluidFunctionalComponentFluidState {
     values: SharedObjectSequence<T>;
 }
 
-<<<<<<< HEAD
-export interface IPureSyncedArrayReducer<T> {
-    add: (value: T) => void;
-}
-
-export interface ISyncedArrayReducer<
-=======
 /**
  * The reducer interface for modifying the synced array
  * TODO: Add more functions that further expose the SharedObjectSequence interface for use
@@ -51,7 +38,6 @@ export interface ISyncedArrayReducer<T> {
  * state definitions together
  */
 export interface IFluidSyncedArrayReducer<
->>>>>>> 53f0e4a434353df720e33ce5f452a6b9b0b1d2e1
     SV extends IFluidFunctionalComponentViewState,
     SF extends IFluidFunctionalComponentFluidState
 > extends IFluidReducer<SV, SF, IFluidDataProps> {
