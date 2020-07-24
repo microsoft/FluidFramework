533507905b8b665c039c42996a1fb8295b989eea
Tanvir Aumi <mdaumi@microsoft.com>
2020-07-23 20:40:23 -0700

    Added one more check to deduplicate incoming ops.

--- server/routerlicious/packages/lambdas/src/scribe/lambda.ts
+++ server/routerlicious/packages/lambdas/src/scribe/lambda.ts
@@ -140,6 +140,11 @@ export class ScribeLambda extends SequencedLambda {
                     continue;
                 }
 
+                if (this.pendingMessages.length > 0 &&
+                    value.operation.sequenceNumber <= this.pendingMessages.peekBack().sequenceNumber) {
+                    continue;
+                }
+
                 // Add the message to the list of pending for this document and those that we need
                 // to include in the checkpoint
                 this.pendingMessages.push(value.operation);
