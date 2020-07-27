6c32d3370c3343f387dc7a7ffcc8c54f3e5fa7b5
Tyler Butler <tylerbu@microsoft.com>
2020-07-27 12:19:13 -0700

    tweak full build script

--- docs/build-full-repo-docs.ps1
+++ docs/build-full-repo-docs.ps1
@@ -1,14 +1,17 @@
 # Build docs for the whole repo
 # All generated files will be in _api-extractor-temp
 Push-Location ../
+Write-Output "===================================npm clean"
+npm run clean
+
 Write-Output "===================================npm install"
 npm install
 
-Write-Output "===================================npm build:fast --install"
-npm run build:fast -- --install
+Write-Output "===================================npm clean all"
+npm run build:fast -- --clean
 
-Write-Output "===================================npm build:fast --symlik"
-npm run build:fast -- --symlink
+Write-Output "===================================npm build:fast --install --symlink"
+npm run build:fast -- --install --symlink --all
 
 Write-Output "===================================npm build:fast --all"
 npm run build:fast -- --nolint --all
