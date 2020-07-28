7e8b43a2140daaf52c90a1f4d565237039786dc7
Jack Williams <jw@jackw.io>
2020-07-28 18:01:03 +0100

    Add forEachCell to Shared Matrix (#2909)
    * Add forEachCell to SM
    * Handle non-empty vectors with empty cells
    * Add more tests
    * Add in-memory non-shared annotations

--- packages/dds/matrix/test/matrix.spec.ts
+++ packages/dds/matrix/test/matrix.spec.ts
@@ -16,7 +16,7 @@ import {
     MockStorage,
 } from "@fluidframework/test-runtime-utils";
 import { SharedMatrix, SharedMatrixFactory } from "../src";
-import { fill, check, insertFragmented, extract, expectSize } from "./utils";
+import { fill, check, insertFragmented, extract, expectSize, checkValue } from "./utils";
 import { TestConsumer } from "./testconsumer";
 
 describe("Matrix", () => {
@@ -217,6 +217,58 @@ describe("Matrix", () => {
                 fill(matrix, /* row: */ 0, /* col: */ 0, /* rowCount: */ 16, /* colCount: */ 16);
                 check(matrix, /* row: */ 0, /* col: */ 0, /* rowCount: */ 16, /* colCount: */ 16);
             });
+
+            it("forEach 16x16", () => {
+                matrix.insertRows(0, 16);
+                matrix.insertCols(0, 16);
+                fill(matrix, /* row: */ 0, /* col: */ 0, /* rowCount: */ 16, /* colCount: */ 16);
+                matrix.forEachCell((v, row, col) => {
+                    checkValue(matrix, v, row, col, /* row: */ 0, /* rowCount: */ 16);
+                })
+            });
+
+            it("forEachCell 16x16 empty with blanks skipped", () => {
+                matrix.insertRows(0, 16);
+                matrix.insertCols(0, 16);
+                matrix.forEachCell(() => {
+                    assert.fail();
+                });
+            });
+
+            it("forEachCell 16x16 empty with blanks", () => {
+                matrix.insertRows(0, 16);
+                matrix.insertCols(0, 16);
+                let count = 0;
+                matrix.forEachCell((v) => {
+                    assert.equal(v, undefined);
+                    count++;
+                }, { includeEmpty: true });
+                assert.equal(count, 16 * 16);
+            });
+
+            it("forEachCell 16x16 empty with 1 cell", () => {
+                matrix.insertRows(0, 16);
+                matrix.insertCols(0, 16);
+                matrix.setCell(0, 0, -42);
+                let count = 0;
+                matrix.forEachCell((v) => {
+                    assert.equal(v, -42);
+                    count++;
+                });
+                assert.equal(count, 1);
+            });
+
+            it("forEachCell 16x16 empty with 1 cell and blanks", () => {
+                matrix.insertRows(0, 16);
+                matrix.insertCols(0, 16);
+                matrix.setCell(0, 0, -42);
+                let count = 0;
+                matrix.forEachCell((v, r, c) => {
+                    assert.equal(v, r === 0 && c === 0 ? -42 : undefined);
+                    count++;
+                }, { includeEmpty: true });
+                assert.equal(count, 16 * 16);
+            });
         });
 
         describe("fragmented", () => {
@@ -224,6 +276,18 @@ describe("Matrix", () => {
                 insertFragmented(matrix, 16, 16);
                 fill(matrix, /* row: */ 0, /* col: */ 0, /* rowCount: */ 16, /* colCount: */ 16);
                 check(matrix, /* row: */ 0, /* col: */ 0, /* rowCount: */ 16, /* colCount: */ 16);
+            });
+        });
+
+        describe("annotations", () => {
+            it("read/write 16x16", () => {
+                matrix.insertRows(0, 256);
+                matrix.insertCols(0, 256);
+                matrix.setAnnotation(0, 0, -42);
+                assert.equal(matrix.getAnnotation(0, 0), -42);
+                matrix.setCell(0, 0, 1);
+                // Setting a cell should clear existing annotation.
+                assert.equal(matrix.getAnnotation(0, 0), undefined);
             });
         });
 
