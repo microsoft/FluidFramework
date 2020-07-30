e92cb2b4df24a09e30eb7e2e0c9262ed070b7a7e
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-07-30 12:46:58 -0700

    Remove raw phaser js

--- components/experimental/clicker-react/clicker-function/src/index.tsx
+++ components/experimental/clicker-react/clicker-function/src/index.tsx
@@ -24,10 +24,7 @@ export class DemoScene extends phaser.Scene {
         this.load.image("ground", "assets/platform.png");
         this.load.image("star", "assets/star.png");
         this.load.image("bomb", "assets/bomb.png");
-        this.load.spritesheet("dude",
-            "assets/dude.png",
-            { frameWidth: 32, frameHeight: 48 },
-        );
+        this.load.spritesheet("dude", "assets/dude.png", { frameWidth: 32, frameHeight: 48 });
     }
 
     create()
