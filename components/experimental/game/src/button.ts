7c23cb86d4fd97c2b6d8abbe58b9ccd2d66d3def
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-07-30 19:46:56 -0700

    multiplayer

---
+++ components/experimental/game/src/button.ts
@@ -0,0 +1,27 @@
+export class TextButton extends Phaser.GameObjects.Text {
+    constructor(scene, x, y, text, style, callback, endCallback) {
+      super(scene, x, y, text, style);
+
+      this.setInteractive({ useHandCursor: true })
+        .on('pointerover', () => this.enterButtonHoverState() )
+        .on('pointerout', () => this.enterButtonRestState() )
+        .on('pointerdown', () => this.enterButtonActiveState(callback) )
+        .on('pointerup', () => {
+          this.enterButtonHoverState();
+          endCallback();
+        })
+    }
+
+    enterButtonHoverState() {
+      this.setStyle({ fill: '#ff0 '});
+    }
+
+    enterButtonRestState() {
+      this.setStyle({ fill: '#0f0 '});
+    }
+
+    enterButtonActiveState(callback) {
+      this.setStyle({ fill: '#0ff' });
+      callback();
+    }
+}
