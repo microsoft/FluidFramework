9ca9a8e8a4f5492750b4aa4b7808f9ca1e5c67c0
Helio Liu <59622401+heliocliu@users.noreply.github.com>
2020-08-06 14:52:43 -0700

    api extractor/lint errors

--- common/lib/common-utils/src/bufferNode.ts
+++ common/lib/common-utils/src/bufferNode.ts
@@ -8,12 +8,12 @@
  * exposing the entirely of Node's typings.  This should match the public interface
  * of the browser implementation, so any changes made in one should be made in both.
  */
-declare class Buffer extends Uint8Array {
+export declare class Buffer extends Uint8Array {
     toString(encoding?: string): string;
     /**
-     * @param value string | ArrayBuffer
-     * @param encodingOrOffset string | number
-     * @param length number
+     * @param value - string | ArrayBuffer
+     * @param encodingOrOffset - string | number
+     * @param length - number
      */
     static from(value, encodingOrOffset?, length?): Buffer;
     static fromArrayBuffer(arrayBuffer: ArrayBuffer, byteOffset?: number, byteLength?: number): Buffer;
