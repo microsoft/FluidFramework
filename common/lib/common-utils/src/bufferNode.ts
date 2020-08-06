c195a9da1140a3bd02680466c5b48f6e5ccaa694
Helio Liu <59622401+heliocliu@users.noreply.github.com>
2020-08-05 21:54:46 -0700

    Update bufferNode.ts

--- common/lib/common-utils/src/bufferNode.ts
+++ common/lib/common-utils/src/bufferNode.ts
@@ -8,4 +8,3 @@ export type IsoBuffer = Buffer;
 
 export const fromBase64ToUtf8 = (input: string): string => IsoBuffer.from(input, "base64").toString();
 export const fromUtf8ToBase64 = (input: string): string => IsoBuffer.from(input).toString("base64");
-
