5f57ecc0f2da2da3936f05af9ca018b61f27e0fe
DLehenbauer <DLehenbauer@users.noreply.github.com>
2020-08-07 09:21:11 -0700

    copyright

--- packages/dds/matrix/src/handlecache.ts
+++ packages/dds/matrix/src/handlecache.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 /* eslint-disable no-bitwise */
 
 import { strict as assert } from "assert";
--- packages/dds/matrix/src/range.ts
+++ packages/dds/matrix/src/range.ts
@@ -1,6 +1,14 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+ /**
+  * Ensures that 0 <= 'value' < 'limit'.  Throws a RangeError otherwise.
+  */
 export function ensureRange(value: number, limit: number) {
-    // eslint-disable-next-line no-param-reassign, no-bitwise
-    value >>>= 0;
+    // Coerce 'value' to Uint32 so that we can range check with a single branch.
+    value >>>= 0;   // eslint-disable-line no-param-reassign, no-bitwise
 
     if (value >= limit) {
         throw new RangeError("Invalid (row, col) coordinate.");
