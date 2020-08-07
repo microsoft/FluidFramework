8088f449eb79e408977b39551b5d7b410542434c
Helio Liu <59622401+heliocliu@users.noreply.github.com>
2020-08-06 19:14:32 -0700

    fix return types for class declare

--- common/lib/common-utils/src/bufferNode.ts
+++ common/lib/common-utils/src/bufferNode.ts
@@ -15,9 +15,9 @@ export declare class Buffer extends Uint8Array {
      * @param encodingOrOffset - string | number
      * @param length - number
      */
-    static from(value, encodingOrOffset?, length?): Buffer;
-    static fromArrayBuffer(arrayBuffer: ArrayBuffer, byteOffset?: number, byteLength?: number): Buffer;
-    static fromString(str: string, encoding?: string): Buffer;
+    static from(value, encodingOrOffset?, length?): IsoBuffer;
+    static fromArrayBuffer(arrayBuffer: ArrayBuffer, byteOffset?: number, byteLength?: number): IsoBuffer;
+    static fromString(str: string, encoding?: string): IsoBuffer;
 }
 export const IsoBuffer = Buffer;
 export type IsoBuffer = Buffer;
