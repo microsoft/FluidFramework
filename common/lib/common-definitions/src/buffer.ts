7696a59bea59b04df95f55664f48bd623340f98f
heliocliu <59622401+heliocliu@users.noreply.github.com>
2020-08-13 13:00:42 -0700

    add buffer type into common defs

---
+++ common/lib/common-definitions/src/buffer.ts
@@ -0,0 +1,20 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * Declare the subset of Buffer functionality we want to make available instead of
+ * exposing the entirely of Node's typings.  This should match the public interface
+ * of the browser implementation, so any changes made in one should be made in both.
+ */
+declare class Buffer extends Uint8Array {
+    toString(encoding?: string): string;
+    /**
+     * @param value - string | ArrayBuffer
+     * @param encodingOrOffset - string | number
+     * @param length - number
+     */
+    static from(value, encodingOrOffset?, length?): IsoBuffer;
+}
+export type IsoBuffer = Buffer;
--- common/lib/common-definitions/src/index.ts
+++ common/lib/common-definitions/src/index.ts
@@ -3,6 +3,7 @@
  * Licensed under the MIT License.
  */
 
+export * from "./buffer";
 export * from "./disposable";
 export * from "./events";
 export * from "./logger";
