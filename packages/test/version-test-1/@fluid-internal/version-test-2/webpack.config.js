bc8cf8f954cc51dc325d350ad5c07228335c1733
Helio Liu <59622401+heliocliu@users.noreply.github.com>
2020-08-15 17:58:29 -0700

    remove component references

--- packages/test/version-test-1/@fluid-internal/version-test-2/webpack.config.js
+++ packages/test/version-test-1/@fluid-internal/version-test-2/webpack.config.js
@@ -7,8 +7,7 @@ const fluidRoute = require("@fluidframework/webpack-fluid-loader");
 const path = require("path");
 const merge = require("webpack-merge");
 
-const pkg = require("./package.json");
-const componentName = pkg.name.slice(1);
+const fluidObjectName = "fluid-internal/version-test-2";
 
 module.exports = env => {
     const isProduction = env && env.production;
@@ -32,7 +31,7 @@ module.exports = env => {
             library: "[name]",
             // https://github.com/webpack/webpack/issues/5767
             // https://github.com/webpack/webpack/issues/7939
-            devtoolNamespace: componentName,
+            devtoolNamespace: fluidObjectName,
             libraryTarget: "umd",
         },
         devServer: {
