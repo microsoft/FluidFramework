eae7752b5adc277e9ab6bb17c982b4ba732cced0
Matt Aimonetti <113+mattetti@users.noreply.github.com>
2020-08-17 09:36:55 -0700

    remove the api-documenter plugin since we forked the plugin itself

--- docs/scripts/doc-plugin-fluid/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { IApiDocumenterPluginManifest } from '@microsoft/api-documenter';
-export declare const apiDocumenterPluginManifest: IApiDocumenterPluginManifest;
-//# sourceMappingURL=index.d.ts.map
