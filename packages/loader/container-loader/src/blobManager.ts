f81fddca38f67cccaa761d41a6e5cc65a8bd4dbc
Matt Rakow <ChumpChief@users.noreply.github.com>
2020-08-18 11:24:22 -0700

    Re-enable no-non-null-assertion rule in container-loader (#3191)
    Also make timer type isomorphic and tweak a few assertions to give better errors.

--- packages/loader/container-loader/src/blobManager.ts
+++ packages/loader/container-loader/src/blobManager.ts
@@ -26,16 +26,16 @@ export class BlobManager implements IBlobManager {
     }
 
     public async getBlob(blobId: string): Promise<IGenericBlob | undefined> {
-        if (!this.blobs.has(blobId)) {
-            return Promise.reject("Blob does not exist");
+        const blob = this.blobs.get(blobId);
+        if (blob === undefined) {
+            throw new Error("Blob does not exist");
         }
 
-        const blob = this.blobs.get(blobId);
         const blobContent = await this.storage.read(blobId);
         if (blobContent === undefined) {
             return undefined;
         }
-        blob!.content = Buffer.from(blobContent, "base64");
+        blob.content = Buffer.from(blobContent, "base64");
         return blob;
     }
 
