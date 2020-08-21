c2030857f97a496ae51d906b3e876a00ab616ede
Matt Aimonetti <113+mattetti@users.noreply.github.com>
2020-08-20 19:15:18 -0700

    Update packages/framework/undo-redo/src/index.ts
    Co-authored-by: Mark Fields <markfields@users.noreply.github.com>

--- packages/framework/undo-redo/src/index.ts
+++ packages/framework/undo-redo/src/index.ts
@@ -4,7 +4,7 @@
  */
 
 /**
-* This package provides and implementation of an in-memory undo redo stack, as
+* This package provides an implementation of an in-memory undo redo stack, as
 * well as handlers for the SharedMap, and SharedSegmentSequence distributed
 * datastructures.
 *
