8b7bcd66f07f45a190d63028fffd2b7df4eb0358
Curtis Man <curtism@microsoft.com>
2020-08-21 11:57:24 -0700

    Reduce export of internal classes in replay-driver (#3289)

--- packages/drivers/replay-driver/src/index.ts
+++ packages/drivers/replay-driver/src/index.ts
@@ -3,9 +3,7 @@
  * Licensed under the MIT License.
  */
 
-export * from "./emptyDeltaStorageService";
 export * from "./replayController";
-export * from "./replayDocumentDeltaConnection";
 export * from "./replayDocumentService";
 export * from "./replayDocumentServiceFactory";
 export * from "./storageImplementations";
