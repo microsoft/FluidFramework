3db9b20279fcb2168f0a371891c9816a31103343
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-08-20 17:53:43 -0700

    add mit header

--- examples/apps/likes-and-comments/webpack.config.js
+++ examples/apps/likes-and-comments/webpack.config.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 const path = require("path");
 const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
