2bbb3ca4204c323067261cb64008e601e37f02bb
REDMOND\andreize <andreize@microsoft.com>
2020-08-26 13:06:43 -0700

    Fixed param docs format

--- packages/drivers/odsp-driver/src/tokenFetch.ts
+++ packages/drivers/odsp-driver/src/tokenFetch.ts
@@ -15,9 +15,9 @@ export interface TokenResponse {
 
 /**
  * Method signature for callback method used to fetch Storage token
- * @param siteUrl Storage site url
- * @param refresh Flag indicating whether token fetch must bypass local cache
- * @param claims Optional string indicating claims that will be passed to token authority
+ * @param siteUrl - Storage site url
+ * @param refresh - Flag indicating whether token fetch must bypass local cache
+ * @param claims - Optional string indicating claims that will be passed to token authority
  * @returns If successful, TokenResponse object representing token value along with flag indicating
  * whether token came from cache. Legacy implementation may return a string for token value;
  * in this case it should be assumes that fromCache signal is undefined. Null is returned in case of failure.
@@ -27,8 +27,8 @@ export type StorageTokenFetcher = (siteUrl: string, refresh: boolean, claims?: s
 
 /**
  * Method signature for callback method used to fetch Push token
- * @param refresh Flag indicating whether token fetch must bypass local cache
- * @param claims Optional string indicating claims that will be passed to token authority
+ * @param refresh - Flag indicating whether token fetch must bypass local cache
+ * @param claims - Optional string indicating claims that will be passed to token authority
  * @returns If successful, TokenResponse object representing token value along with flag indicating
  * whether token came from cache. Legacy implementation may return a string for token value;
  * in this case it should be assumes that fromCache signal is undefined. Null is returned in case of failure.
@@ -37,7 +37,7 @@ export type PushTokenFetcher = (refresh: boolean, claims?: string) => Promise<st
 
 /**
  * Helper method which transforms return value for StorageTokenFetcher and PushTokenFetcher to token string
- * @param tokenResponse return value for StorageTokenFetcher and PushTokenFetcher methods
+ * @param tokenResponse - return value for StorageTokenFetcher and PushTokenFetcher methods
  * @returns Token value
  */
 export function tokenFromResponse(tokenResponse: string | TokenResponse | null): string | null {
@@ -48,7 +48,7 @@ export function tokenFromResponse(tokenResponse: string | TokenResponse | null):
 
 /**
  * Helper method which returns flag indicating whether token response comes from local cache
- * @param tokenResponse return value for StorageTokenFetcher and PushTokenFetcher methods
+ * @param tokenResponse - return value for StorageTokenFetcher and PushTokenFetcher methods
  * @returns Value indicating whether response came from cache.
  * Undefined is returned when we could not determine the source of token.
  */
