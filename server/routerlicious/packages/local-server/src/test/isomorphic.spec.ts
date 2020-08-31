14dc8124667f3a8cde66e04d2a2845323b5ad11f
Curtis Man <curtism@microsoft.com>
2020-08-31 15:06:39 -0700

    Update server to use npm registry (#3448)

--- server/routerlicious/packages/local-server/src/test/isomorphic.spec.ts
+++ server/routerlicious/packages/local-server/src/test/isomorphic.spec.ts
@@ -15,6 +15,7 @@ describe("Local server", () => {
                 if (err) {
                     assert.fail(err);
                     reject();
+                // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
                 } else if (stats.hasErrors()) {
                     assert.fail(stats.compilation.errors.map((value) => value.stack).join("\n"));
                     reject();
