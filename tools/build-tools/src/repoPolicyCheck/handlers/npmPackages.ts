b4cec7d78db810bd7ffd25718d5962cc8022626f
Curtis Man <curtism@microsoft.com>
2020-08-31 10:28:27 -0700

    Fix name of Webpack fluid loader (#3417)
    It is not webpack component loader any more
    
    Also fix line break in the URL in trademark verbiage.

--- tools/build-tools/src/repoPolicyCheck/handlers/npmPackages.ts
+++ tools/build-tools/src/repoPolicyCheck/handlers/npmPackages.ts
@@ -20,10 +20,9 @@ const repository = 'microsoft/FluidFramework';
 const trademark = `
 ## Trademark
 
-This project may contain Microsoft trademarks or logos for Microsoft projects, products, or services. Use of these
-trademarks or logos must follow Microsoft's [Trademark & Brand Guidelines](https://www.microsoft.com/en-us/legal/
-intellectualproperty/trademarks/usage/general). Use of Microsoft trademarks or logos in modified versions of this
-project must not cause confusion or imply Microsoft sponsorship.
+This project may contain Microsoft trademarks or logos for Microsoft projects, products, or services. Use of these trademarks
+or logos must follow Microsoft's [Trademark & Brand Guidelines](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/general).
+Use of Microsoft trademarks or logos in modified versions of this project must not cause confusion or imply Microsoft sponsorship.
 `;
 
 function packageShouldBePrivate(name: string): boolean {
