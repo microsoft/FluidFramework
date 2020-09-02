4e251aeefeced63e3ac99facf491bb0d7a965b3b
Micah Godbolt <mgodbolt@microsoft.com>
2020-09-02 16:52:38 -0700

    remove lib files and update lock

--- packages/framework/framework-interfaces/lib/discover.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-export const IComponentDiscoverableInterfaces = "IComponentDiscoverableInterfaces";
-export const IComponentDiscoverInterfaces = "IComponentDiscoverInterfaces";
-export const IComponentInterfacesRegistry = "IComponentInterfacesRegistry";
-//# sourceMappingURL=discover.js.map
