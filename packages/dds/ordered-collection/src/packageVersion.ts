38ca3566c8238d9ff296ef294267caac180adc09
heliocliu <59622401+heliocliu@users.noreply.github.com>
2020-09-10 12:44:08 -0700

    change interface to use runtimebase

--- packages/dds/ordered-collection/src/packageVersion.ts
+++ packages/dds/ordered-collection/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/ordered-collection";
-export const pkgVersion = "0.25.1";
+export const pkgVersion = "0.25.2";
--- packages/drivers/iframe-driver/src/packageVersion.ts
+++ packages/drivers/iframe-driver/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/iframe-driver";
-export const pkgVersion = "0.25.1";
+export const pkgVersion = "0.25.2";
