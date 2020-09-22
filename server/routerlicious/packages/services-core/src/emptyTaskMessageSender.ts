8447c2863b002c5885f182cf87e73e1a8c638b46
Tanvir Aumi <tanvir@Tanvirs-MacBook-Pro-2.local>
2020-09-23 04:30:18 +0600

    Option to remove rabbitmq from a deployment.

---
+++ server/routerlicious/packages/services-core/src/emptyTaskMessageSender.ts
@@ -0,0 +1,24 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { ITaskMessage, ITaskMessageSender } from "./taskMessages";
+
+export class EmptyTaskMessageSender implements ITaskMessageSender {
+    public async initialize(): Promise<void> {
+        return;
+    }
+
+    public sendTask(queueName: string, message: ITaskMessage): void {
+        // Do nothing
+    }
+
+    public on(event: string, listener: (...args: any[]) => void): this {
+        return this;
+    }
+
+    public async close(): Promise<void> {
+        return;
+    }
+}
