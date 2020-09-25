e3e14cddcda24c8c789849cd78e26dc3d9eb3de5
Mark Fields <markfields@users.noreply.github.com>
2020-09-25 13:45:40 -0700

    Write a single passing UT in tinylicious

---
+++ server/tinylicious/src/test/inMemoryCollection.spec.ts
@@ -0,0 +1,16 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "assert";
+
+describe("Tinylicious", () => {
+    describe("Services", () => {
+        describe("inMemoryCollection", () => {
+            it("Black Triangle", async () => {
+                assert(true);
+            });
+        });
+    });
+});
