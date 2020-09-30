880909be5959310936907483c3782cb112b89a56
Helio Liu <59622401+heliocliu@users.noreply.github.com>
2020-09-30 11:42:20 -0700

    port improved connection telemetry

--- packages/loader/container-loader/src/container.ts
+++ packages/loader/container-loader/src/container.ts
@@ -11,6 +11,7 @@ import {
     ITelemetryBaseLogger,
     ITelemetryLogger,
 } from "@fluidframework/common-definitions";
+import { performanceNow } from "@fluidframework/common-utils";
 import { IFluidObject, IRequest, IResponse, IFluidRouter } from "@fluidframework/core-interfaces";
 import {
     IAudience,
@@ -30,14 +31,7 @@ import {
     IThrottlingWarning,
     AttachState,
 } from "@fluidframework/container-definitions";
-import { performanceNow } from "@fluidframework/common-utils";
-import {
-    ChildLogger,
-    EventEmitterWithErrorHandling,
-    PerformanceEvent,
-    raiseConnectedEvent,
-    TelemetryLogger,
-} from "@fluidframework/telemetry-utils";
+import { CreateContainerError, GenericError } from "@fluidframework/container-utils";
 import {
     IDocumentService,
     IDocumentStorageService,
@@ -56,7 +50,6 @@ import {
     ensureFluidResolvedUrl,
     combineAppAndProtocolSummary,
 } from "@fluidframework/driver-utils";
-import { CreateContainerError } from "@fluidframework/container-utils";
 import {
     isSystemMessage,
     ProtocolOpHandler,
@@ -85,6 +78,13 @@ import {
     TreeEntry,
     ISummaryTree,
 } from "@fluidframework/protocol-definitions";
+import {
+    ChildLogger,
+    EventEmitterWithErrorHandling,
+    PerformanceEvent,
+    raiseConnectedEvent,
+    TelemetryLogger,
+} from "@fluidframework/telemetry-utils";
 import { Audience } from "./audience";
 import { BlobManager } from "./blobManager";
 import { ContainerContext } from "./containerContext";
@@ -98,6 +98,10 @@ import { PrefetchDocumentStorageService } from "./prefetchDocumentStorageService
 import { parseUrl, convertProtocolAndAppSummaryToSnapshotTree } from "./utils";
 
 const PackageNotFactoryError = "Code package does not implement IRuntimeFactory";
+
+interface ILocalSequencedClient extends ISequencedClient {
+    shouldHaveLeft?: boolean;
+}
 
 export interface IContainerConfig {
     resolvedUrl?: IResolvedUrl;
@@ -231,19 +235,7 @@ export class Container extends EventEmitterWithErrorHandling<IContainerEvents> i
     }
     private blobsCacheStorageService: IDocumentStorageService | undefined;
 
-    /**
-     * The current client id
-     */
     private _clientId: string | undefined;
-    /**
-     * Our current (and previous) clientIds, with the latest (and current) at the
-     * end of the array.  We keep a history so that messages with different IDs
-     * can be identified as originating from this client in cases of multiple
-     * disconnects/reconnects.  _clientId exists above as well to minimize/stage
-     * this change
-     */
-    private readonly clientIdHistory: string[] = [];
-    private readonly maxClientIdHistory = 100;
     private _id: string | undefined;
     private originalRequest: IRequest | undefined;
     private readonly _deltaManager: DeltaManager;
@@ -1396,12 +1388,15 @@ export class Container extends EventEmitterWithErrorHandling<IContainerEvents> i
         this._connectionState = value;
 
         if (value === ConnectionState.Connected) {
-            this._clientId = this.pendingClientId;
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            this.clientIdHistory.push(this._clientId!);
-            if (this.clientIdHistory.length > this.maxClientIdHistory) {
-                this.clientIdHistory.shift();
+            // Mark our old client should have left in the quorum if it's still there
+            if (this._clientId !== undefined) {
+                const client: ILocalSequencedClient | undefined =
+                    this._protocolHandler?.quorum.getMember(this._clientId);
+                if (client !== undefined) {
+                    client.shouldHaveLeft = true;
+                }
             }
+            this._clientId = this.pendingClientId;
             this._deltaManager.updateQuorumJoin();
         } else if (value === ConnectionState.Disconnected) {
             // Important as we process our own joinSession message through delta request
@@ -1461,19 +1456,32 @@ export class Container extends EventEmitterWithErrorHandling<IContainerEvents> i
     }
 
     private processRemoteMessage(message: ISequencedDocumentMessage): IProcessMessageResult {
-        const local = this._clientId === message.clientId;
-
-        // Report if we're getting messages from a clientId older than the most recent
-        if (!local && this.clientIdHistory.includes(message.clientId)) {
-            this.logger.sendErrorEvent({
-                eventName: "matchedOldClientIdInRemoteMessage",
-                clientId: this._clientId,
-                messageClientId: message.clientId,
-                historyIndex: this.clientIdHistory.indexOf(message.clientId),
-                sequenceNumber: message.sequenceNumber,
-                clientSequenceNumber: message.clientSequenceNumber,
-            });
+        // Check and report if we're getting messages from a clientId that we previously
+        // flagged as shouldHaveLeft, or from a client that's not in the quorum but should be
+        if (message.clientId != null) {
+            let errorMsg: string | undefined;
+            const client: ILocalSequencedClient | undefined =
+                this._protocolHandler?.quorum.getMember(message.clientId);
+            if (client === undefined && message.type !== MessageType.ClientJoin) {
+                errorMsg = "messageClientIdMissingFromQuorum";
+            } else if (client?.shouldHaveLeft === true) {
+                errorMsg = "messageClientIdShouldHaveLeft";
+            }
+            if (errorMsg !== undefined) {
+                const error = new GenericError(
+                    errorMsg,
+                    {
+                        clientId: this._clientId,
+                        messageClientId: message.clientId,
+                        sequenceNumber: message.sequenceNumber,
+                        clientSequenceNumber: message.clientSequenceNumber,
+                    },
+                );
+                this.close(CreateContainerError(error));
+            }
         }
+
+        const local = this._clientId === message.clientId;
 
         // Forward non system messages to the loaded runtime for processing
         if (!isSystemMessage(message)) {
