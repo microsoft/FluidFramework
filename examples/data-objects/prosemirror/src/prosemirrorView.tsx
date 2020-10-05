c2a4e2154f778e9b8ede81c2bf62df87d8b98ee0
PRERNA JAIN <prerna.jain@limetray.com>
2020-10-05 17:01:15 +0530

    initial loading from external file

--- examples/data-objects/prosemirror/src/prosemirrorView.tsx
+++ examples/data-objects/prosemirror/src/prosemirrorView.tsx
@@ -11,6 +11,7 @@ export class ProseMirrorView implements IFluidHTMLView {
     private content: HTMLDivElement;
     private editorView: EditorView;
     private textArea: HTMLDivElement;
+    private inputFile: HTMLInputElement;
     public get IFluidHTMLView() { return this; }
 
     public constructor(private readonly collabManager: FluidCollabManager) { }
@@ -23,14 +24,20 @@ export class ProseMirrorView implements IFluidHTMLView {
             this.content = document.createElement("div");
             this.content.style.display = "none";
             this.content.innerHTML = "";
+            this.inputFile = document.createElement("input");
+            this.inputFile.type = "file";
+            this.inputFile.style.padding = "10px";
+            this.inputFile.id = "input-file";
         }
 
         // Reparent if needed
         if (this.textArea.parentElement !== elm) {
             this.textArea.remove();
             this.content.remove();
+            this.inputFile.remove();
             elm.appendChild(this.textArea);
             elm.appendChild(this.content);
+            elm.appendChild(this.inputFile);
         }
 
         if (!this.editorView) {
