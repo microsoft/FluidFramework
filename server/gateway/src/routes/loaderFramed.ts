88d8e4a5a2517c621e3a7fb7e9d1119bab3b9a39
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2020-10-05 17:31:37 -0700

    update other loaders

--- server/gateway/src/routes/loaderFramed.ts
+++ server/gateway/src/routes/loaderFramed.ts
@@ -4,6 +4,7 @@
  */
 
 import { parse } from "url";
+import _ from "lodash";
 import { IFluidCodeDetails } from "@fluidframework/container-definitions";
 import { ScopeType } from "@fluidframework/protocol-definitions";
 import { IAlfredTenant } from "@fluidframework/server-services-client";
@@ -170,13 +171,14 @@ export function create(
                         // Bug in TS3.7: https://github.com/microsoft/TypeScript/issues/33752
                         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                         timings!.push(Date.now() - start);
-
+                        const configClientId = config.get("login:microsoft").clientId;
                         response.render(
                             "loaderHost",
                             {
                                 cache: fullTree !== undefined ? JSON.stringify(fullTree.cache) : undefined,
                                 chaincode: JSON.stringify(pkg),
-                                clientId: process.env.MICROSOFT_CONFIGURATION_CLIENT_ID,
+                                clientId: _.isEmpty(configClientId)
+                                    ? process.env.MICROSOFT_CONFIGURATION_CLIENT_ID : configClientId,
                                 config: workerConfig,
                                 jwt: jwtToken,
                                 npm: config.get("worker:npm"),
