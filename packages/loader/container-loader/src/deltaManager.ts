2653ae49204d8fa82eeb91b131489fa53f2fdc89
Vlad Sudzilouski <vlad@sudzilouski.com>
2020-10-14 10:31:09 -0700

    Add protection against data loss (#3900)
    Adding defensive detection of data loss scenario, based on issue #3840

--- packages/loader/container-loader/src/deltaManager.ts
+++ packages/loader/container-loader/src/deltaManager.ts
@@ -156,6 +156,8 @@ export class DeltaManager
     private lastProcessedSequenceNumber: number = 0;
     private baseTerm: number = 0;
 
+    private previouslyProcessedMessage: ISequencedDocumentMessage | undefined;
+
     // The sequence number we initially loaded from
     private initSequenceNumber: number = 0;
 
@@ -1139,6 +1141,16 @@ export class DeltaManager
         }
     }
 
+    // returns parts of message (in string format) that should never change for a given message.
+    // Used for message comparison. It attempts to avoid comparing fields that potentially may differ.
+    // for example, it's not clear if serverMetadata or timestamp property is a property of message or server state.
+    // We only extract the most obvious fields that are sufficient (with high probability) to detect sequence number
+    // reuse.
+    // Also payload goes to telemetry, so no PII, including content!!
+    private comparableMessagePayload(m: ISequencedDocumentMessage) {
+        return `${m.clientId}-${m.type}-${m.minimumSequenceNumber}-${m.referenceSequenceNumber}`;
+    }
+
     private enqueueMessages(
         messages: ISequencedDocumentMessage[],
         telemetryEventSuffix: string = "OutOfOrderMessage",
@@ -1171,12 +1183,31 @@ export class DeltaManager
                 if (duplicateEnd === undefined || duplicateEnd < message.sequenceNumber) {
                     duplicateEnd = message.sequenceNumber;
                 }
+
+                // Validate that we do not have data loss, i.e. sequencing is reset and started again
+                // with numbers that this client already observed before.
+                if (this.previouslyProcessedMessage?.sequenceNumber === message.sequenceNumber) {
+                    const message1 = this.comparableMessagePayload(this.previouslyProcessedMessage);
+                    const message2 = this.comparableMessagePayload(message);
+                    if (message1 !== message2) {
+                        const error = {
+                            errorType: ContainerErrorType.dataCorruption,
+                            message: "Two messages with same seq# and different payload!",
+                            clientId: this.connection?.details.clientId,
+                            sequenceNumber: message.sequenceNumber,
+                            message1,
+                            message2,
+                        };
+                        this.close(error);
+                    }
+                }
             } else if (message.sequenceNumber !== this.lastQueuedSequenceNumber + 1) {
                 this.pending.push(message);
                 // eslint-disable-next-line @typescript-eslint/no-floating-promises
                 this.fetchMissingDeltas(telemetryEventSuffix, this.lastQueuedSequenceNumber, message.sequenceNumber);
             } else {
                 this.lastQueuedSequenceNumber = message.sequenceNumber;
+                this.previouslyProcessedMessage = message;
                 this._inbound.push(message);
             }
         }
