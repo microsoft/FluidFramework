b4bd136312e73913bd8da74f11d6ebbefea8fd27
ksbrar <kabir@brar.xyz>
2020-10-16 17:56:52 -0400

    Remove old packages/utils/odsp-utils folder.

--- packages/utils/odsp-utils/lib/index.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-export * from "./odspAuth";
-export * from "./odspUtils";
-export * from "./odspRequest";
-export * from "./odspDrives";
-//# sourceMappingURL=index.js.map
--- packages/utils/odsp-utils/lib/odspUtils.d.ts
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-export declare function isSharepointURL(server: string): boolean;
-export declare function getSharepointTenant(server: string): string;
-export declare function getServer(tenantId: string): string;
-//# sourceMappingURL=odspUtils.d.ts.map
