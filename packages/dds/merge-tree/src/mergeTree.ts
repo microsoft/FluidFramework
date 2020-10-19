6f9ada6d46ed6df17a94ef9f956171ac8ba1003d
Tony Murphy <anthony.murphy@microsoft.com>
2020-10-19 13:11:33 -0700

    Remove Prototype External Segment (#3984)
    Co-authored-by: Tony Murphy <anthonm@microsoft.com>

--- packages/dds/merge-tree/src/mergeTree.ts
+++ packages/dds/merge-tree/src/mergeTree.ts
@@ -573,47 +573,6 @@ export abstract class BaseSegment extends MergeNode implements ISegment {
     protected abstract createSplitSegmentAt(pos: number): BaseSegment;
 }
 
-interface IJSONExternalSegment extends ops.IJSONSegment {
-    sequenceIndex: number;
-    sequenceLength: number;
-}
-/**
- * A non-shared placeholder for external content.
- */
-export class ExternalSegment extends BaseSegment {
-    public static readonly type = "ExternalSegment";
-    public readonly type = ExternalSegment.type;
-
-    constructor(
-        public placeholderSeq,
-        public sequenceLength: number,
-        public sequenceIndex: number) {
-        super();
-    }
-
-    toJSONObject() {
-        const obj: IJSONExternalSegment = { sequenceIndex: this.sequenceIndex, sequenceLength: this.sequenceLength };
-        super.addSerializedProps(obj);
-        return obj;
-    }
-
-    mergeTreeInsert(mergeTree: MergeTree, pos: number, refSeq: number, clientId: number, seq: number, opArgs: IMergeTreeDeltaOpArgs) {
-        mergeTree.insertSegments(pos, [this], refSeq, clientId, seq, opArgs);
-    }
-
-    clone(): ISegment {
-        throw new Error("clone not implemented");
-    }
-
-    append() {
-        throw new Error("Can not append to external segment");
-    }
-
-    protected createSplitSegmentAt(pos: number): BaseSegment {
-        throw new Error("Method not implemented.");
-    }
-}
-
 export const reservedTileLabelsKey = "referenceTileLabels";
 export const reservedRangeLabelsKey = "referenceRangeLabels";
 export const reservedMarkerIdKey = "markerId";
