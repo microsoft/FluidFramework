43b4804efddafac7878f726050859cdc44400757
Skyler Jokiel <skjokiel@microsoft.com>
2020-10-22 04:48:25 -0700

    0.26.5-0.26.6 (#4038)

--- packages/drivers/fluidapp-odsp-urlResolver/src/packageVersion.ts
+++ packages/drivers/fluidapp-odsp-urlResolver/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-internal/fluidapp-odsp-urlresolver";
-export const pkgVersion = "0.26.5";
+export const pkgVersion = "0.26.6";
--- packages/framework/undo-redo/src/packageVersion.ts
+++ packages/framework/undo-redo/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/undo-redo";
-export const pkgVersion = "0.26.5";
+export const pkgVersion = "0.26.6";
