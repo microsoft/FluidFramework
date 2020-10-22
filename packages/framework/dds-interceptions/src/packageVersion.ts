9897241d93a08c427e747fc5d14bf9b0680c9179
Skyler Jokiel <skjokiel@microsoft.com>
2020-10-21 17:48:01 -0700

    bump 0.26.4 - 0.26.5 (#4027)

--- packages/framework/dds-interceptions/src/packageVersion.ts
+++ packages/framework/dds-interceptions/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/dds-interceptions";
-export const pkgVersion = "0.26.4";
+export const pkgVersion = "0.26.5";
--- packages/runtime/datastore/src/packageVersion.ts
+++ packages/runtime/datastore/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/datastore";
-export const pkgVersion = "0.26.4";
+export const pkgVersion = "0.26.5";
