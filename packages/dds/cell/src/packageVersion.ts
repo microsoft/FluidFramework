d61042ab77a746e0f1d524b0ea013d7b79ddb685
Skyler Jokiel <skjokiel@microsoft.com>
2020-10-22 04:50:39 -0700

    0.27.3-0.27.4 (#4039)

--- packages/dds/cell/src/packageVersion.ts
+++ packages/dds/cell/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/cell";
-export const pkgVersion = "0.27.3";
+export const pkgVersion = "0.27.4";
--- packages/framework/dds-interceptions/src/packageVersion.ts
+++ packages/framework/dds-interceptions/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/dds-interceptions";
-export const pkgVersion = "0.27.3";
+export const pkgVersion = "0.27.4";
