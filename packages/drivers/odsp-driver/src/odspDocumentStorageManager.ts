85293a99f0502fda59dec6d15f92f3f1ad29e7c0
Jatin Garg <48029724+jatgarg@users.noreply.github.com>
2020-10-27 15:13:39 -0700

    Blobs should be 0 instead of undefined (#4123)

--- packages/drivers/odsp-driver/src/odspDocumentStorageManager.ts
+++ packages/drivers/odsp-driver/src/odspDocumentStorageManager.ts
@@ -553,7 +553,7 @@ export class OdspDocumentStorageService implements IDocumentStorageService {
             }
             // If the first snapshot request was with blobs and we either timed out or the size was too big, then try to fetch without blobs.
             if ((error.errorType === OdspErrorType.snapshotTooBig || error.errorType === OdspErrorType.fetchTimeout) && snapshotOptions.blobs) {
-                const snapshotOptionsWithoutBlobs: ISnapshotOptions = { ...snapshotOptions, blobs: undefined, mds: undefined };
+                const snapshotOptionsWithoutBlobs: ISnapshotOptions = { ...snapshotOptions, blobs: 0, mds: undefined };
                 return this.fetchSnapshotCore(snapshotOptionsWithoutBlobs, tokenFetchOptions, usePost);
             }
             throw error;
@@ -577,7 +577,9 @@ export class OdspDocumentStorageService implements IDocumentStorageService {
             postBody += `Authorization: Bearer ${storageToken}\r\n`;
             postBody += `X-HTTP-Method-Override: GET\r\n`;
             Object.entries(snapshotOptions).forEach(([key, value]) => {
-                postBody += `${key}: ${value}\r\n`;
+                if (value !== undefined) {
+                    postBody += `${key}: ${value}\r\n`;
+                }
             });
             const sharingLink = await this.sharingLinkP;
             if (sharingLink) {
