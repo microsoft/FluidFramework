d7c6782b122fd7e78a9c7ad95d83ef4e326c522b
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2020-10-27 08:34:35 -0700

    Add a browser friendly assert module

---
+++ common/lib/common-utils/src/assert.ts
@@ -0,0 +1,16 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * A browser friendly version of the node assert library. Use this instead of the 'assert' package, which has a big
+ * impact on bundle sizes.
+ * @param condition - The condition that should be truthy, if the condition is falsy an error will be thrown.
+ * @param message - The message to include in the error when the condition does not hold
+ */
+ export function assert(condition: any, message: string) {
+     if (!condition) {
+         throw new Error(message);
+     }
+ }
