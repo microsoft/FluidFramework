ac05fbf90cd6185ca94c187082ff18e7f8972a8c
ksbrar <kabir@brar.xyz>
2020-10-27 19:34:30 -0400

    Bump 0.27 from 0.27.7 to 0.27.8.

--- packages/dds/register-collection/src/packageVersion.ts
+++ packages/dds/register-collection/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/register-collection";
-export const pkgVersion = "0.27.8";
+export const pkgVersion = "0.27.9";
--- packages/runtime/runtime-utils/src/packageVersion.ts
+++ packages/runtime/runtime-utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/runtime-utils";
-export const pkgVersion = "0.27.8";
+export const pkgVersion = "0.27.9";
