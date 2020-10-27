6774470ea6159f1c61e0e2ea1c0c3c25bbe0cda0
Matt Rakow <marakow@microsoft.com>
2020-10-27 13:24:41 -0700

    Move documentation into Cell

--- packages/dds/cell/src/cell.ts
+++ packages/dds/cell/src/cell.ts
@@ -50,7 +50,51 @@ interface ICellValue {
 const snapshotFileName = "header";
 
 /**
- * Implementation of a cell shared object
+ * The SharedCell distributed data structure can be used to store a single serializable value.
+ *
+ * @remarks
+ * ## Creation
+ *
+ * To create a `SharedCell`, call the static create method:
+ *
+ * ```typescript
+ * const myCell = SharedCell.create(this.runtime, id);
+ * ```
+ *
+ * ## Usage
+ *
+ * The value stored in the cell can be set with the `.set()` method and retrieved with the `.get()` method:
+ *
+ * ```typescript
+ * myCell.set(3);
+ * console.log(myCell.get()); // 3
+ * ```
+ *
+ * The value must only be plain JS objects or `SharedObject` handles (e.g. to another DDS or Fluid object).
+ * In collaborative scenarios, the value is settled with a policy of _last write wins_.
+ *
+ * The `.delete()` method will delete the stored value from the cell:
+ *
+ * ```typescript
+ * myCell.delete();
+ * console.log(myCell.get()); // undefined
+ * ```
+ *
+ * The `.empty()` method will check if the value is undefined.
+ *
+ * ```typescript
+ * if (myCell.empty()) {
+ *   // myCell.get() will return undefined
+ * } else {
+ *   // myCell.get() will return a non-undefined value
+ * }
+ * ```
+ *
+ * ## Eventing
+ *
+ * `SharedCell` is an `EventEmitter`, and will emit events when other clients make modifications. You should
+ * register for these events and respond appropriately as the data is modified. `valueChanged` will be emitted
+ * in response to a `set`, and `delete` will be emitted in response to a `delete`.
  */
 export class SharedCell<T extends Serializable = any> extends SharedObject<ISharedCellEvents<T>>
     implements ISharedCell<T> {
