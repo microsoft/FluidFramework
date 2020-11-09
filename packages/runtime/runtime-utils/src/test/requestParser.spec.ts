19d7622ed8e67cf6161cbd40bd9c173bfaac8c70
Si Chen <chensi@microsoft.com>
2020-11-09 09:12:43 -0800

    refactored test code

--- packages/runtime/runtime-utils/src/test/requestParser.spec.ts
+++ packages/runtime/runtime-utils/src/test/requestParser.spec.ts
@@ -41,19 +41,19 @@ describe("RequestParser", () => {
             assert.strictEqual(pathParts[0], "data store Id");
         });
     });
-    describe(".createSubRequest", () => {
+    describe(".createSubRequest with special characters", () => {
         let requestParser: RequestParser;
         beforeEach(() => {
-            requestParser = RequestParser.create({ url: "/dataStoreId//some/route/" });
+            requestParser = RequestParser.create({ url: "//dataStoreId!@//some!@//route!@//" });
         });
         it("Create request from part 0", () => {
-            assert.strictEqual(requestParser.createSubRequest(0).url, "/dataStoreId/some/route");
+            assert.strictEqual(requestParser.createSubRequest(0).url, "/dataStoreId!@/some!@/route!@");
         });
         it("Create request from part 1", () => {
-            assert.strictEqual(requestParser.createSubRequest(1).url, "/some/route");
+            assert.strictEqual(requestParser.createSubRequest(1).url, "/some!@/route!@");
         });
         it("Create request from part 2", () => {
-            assert.strictEqual(requestParser.createSubRequest(2).url, "/route");
+            assert.strictEqual(requestParser.createSubRequest(2).url, "/route!@");
         });
         it("Create request from parts length ", () => {
             assert.strictEqual(requestParser.createSubRequest(3).url, "/");
@@ -63,31 +63,8 @@ describe("RequestParser", () => {
             assert.throws(() => requestParser.createSubRequest(-1));
         });
     });
-    describe(".createSubRequest with query params", () => {
-        let requestParser2: RequestParser;
-        beforeEach(() => {
-            requestParser2 = RequestParser.create({ url: "/dataStoreId/some/route/?query1=1&query2=2" });
-        });
-        it("Create request from part 0", () => {
-            assert.strictEqual(requestParser2.createSubRequest(0).url, "/dataStoreId/some/route/?query1=1&query2=2");
-        });
-        it("Create request from part 1", () => {
-            assert.strictEqual(requestParser2.createSubRequest(1).url, "/some/route/?query1=1&query2=2");
-        });
-        it("Create request from part 2", () => {
-            assert.strictEqual(requestParser2.createSubRequest(2).url, "/route/?query1=1&query2=2");
-        });
-        it("Create request from parts length", () => {
-            assert.strictEqual(requestParser2.createSubRequest(3).url, "/?query1=1&query2=2");
-        });
-        it("Create request from invalid part ", () => {
-            assert.throws(() => requestParser2.createSubRequest(4));
-            assert.throws(() => requestParser2.createSubRequest(-1));
-        });
-    });
+    
     describe(".createSubRequest with special urls", () => {
-        beforeEach(() => {
-        });
         it("Create request for `/`", () => {
             const testRequestParser = RequestParser.create({ url: "/" });
             assert.strictEqual(testRequestParser.createSubRequest(0).url, "/");
@@ -101,4 +78,31 @@ describe("RequestParser", () => {
             assert.throws(() => testRequestParser.createSubRequest(-1));
         });
     });
+    
+    const testSubRequest = function(uri: string){
+        describe(".createSubRequest with query params", () => {
+            let requestParser2: RequestParser;
+            beforeEach(() => {
+                requestParser2 = RequestParser.create({ url: uri });
+            });
+            it("Create request from part 0", () => {
+                assert.strictEqual(requestParser2.createSubRequest(0).url, "/dataStoreId/some/route/?query1=1&query2=2");
+            });
+            it("Create request from part 1", () => {
+                assert.strictEqual(requestParser2.createSubRequest(1).url, "/some/route/?query1=1&query2=2");
+            });
+            it("Create request from part 2", () => {
+                assert.strictEqual(requestParser2.createSubRequest(2).url, "/route/?query1=1&query2=2");
+            });
+            it("Create request from parts length", () => {
+                assert.strictEqual(requestParser2.createSubRequest(3).url, "/?query1=1&query2=2");
+            });
+            it("Create request from invalid part ", () => {
+                assert.throws(() => requestParser2.createSubRequest(4));
+                assert.throws(() => requestParser2.createSubRequest(-1));
+            });
+        });
+    }
+    testSubRequest("/dataStoreId/some/route/?query1=1&query2=2");
+    testSubRequest("dataStoreId/some/route/?query1=1&query2=2");
 });
