9d7046319e32ac42100cd292fbf21d8cc78f41bf
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2020-11-12 16:01:18 -0500

    Fall back to set timeout (#4324)
    There are issues getting 0.1015 integrated. Porting to 0.28 branch

--- server/routerlicious/packages/lambdas/src/broadcaster/lambda.ts
+++ server/routerlicious/packages/lambdas/src/broadcaster/lambda.ts
@@ -26,6 +26,10 @@ class BroadcasterBatch {
     }
 }
 
+// Set immediate is not available in all environments, specifically it does not work in a browser.
+// Fallback to set timeout in those cases
+const taskScheduleFunction: (cb: () => void) => void = typeof setImmediate === "function" ? setImmediate : setTimeout;
+
 export class BroadcasterLambda implements IPartitionLambda {
     private pending = new Map<string, BroadcasterBatch>();
     private pendingOffset: IQueuedMessage;
@@ -97,7 +101,7 @@ export class BroadcasterLambda implements IPartitionLambda {
         this.context.checkpoint(batchOffset);
 
         // Invoke the next send after a setImmediate to give IO time to create more batches
-        setImmediate(() => {
+        taskScheduleFunction(() => {
             this.current.clear();
             this.sendPending();
         });
