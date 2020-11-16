9d87ab118c8c3c8ff7b1c7c47c8c3dbf757edd60
Micah Godbolt <mgodbolt@microsoft.com>
2020-11-16 15:50:49 -0800

    [bump] package version for client
                @fluidframework/build-common:     0.20.0 (unchanged)
         @fluidframework/eslint-config-fluid:     0.21.1 (unchanged)
          @fluidframework/common-definitions:     0.20.0 (unchanged)
                @fluidframework/common-utils:     0.26.0 (unchanged)
                                      Server:   0.1015.0 (unchanged)
                                      Client:     0.29.0 -> 0.29.1
                             generator-fluid:      0.3.0 (unchanged)
                                 tinylicious:      0.2.0 (unchanged)
                                 dice-roller:      0.0.1 (unchanged)

--- packages/framework/last-edited-experimental/src/packageVersion.ts
+++ packages/framework/last-edited-experimental/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/last-edited-experimental";
-export const pkgVersion = "0.29.0";
+export const pkgVersion = "0.29.1";
--- packages/test/service-load-test/src/packageVersion.ts
+++ packages/test/service-load-test/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-internal/service-load-test";
-export const pkgVersion = "0.29.0";
+export const pkgVersion = "0.29.1";
