b9042814575a43409bd75ece200dc37bf41dadc3
Si Chen <chensi@microsoft.com>
2020-11-16 17:17:33 -0800

    keep fixing test

--- packages/framework/aqueduct/src/test/defaultRoute.spec.ts
+++ packages/framework/aqueduct/src/test/defaultRoute.spec.ts
@@ -24,7 +24,7 @@ class MockRuntime {
         if (id === "objectId") {
             return {
                 request: async (r) => {
-                    if (r.url === "" || r.url === "/route") {
+                    if (r.url === "/" || r.url === "/route") {
                         return createFluidObjectResponse({ route: r.url } as IFluidObject);
                     }
                     return { status: 404, mimeType: "text/plain", value: "not found" };
@@ -65,16 +65,17 @@ describe("defaultRouteRequestHandler", () => {
         const handler = defaultRouteRequestHandler("objectId");
 
         const requestParser = RequestParser.create({ url: "", headers: {} });
+        console.log(requestParser);
         const response = await handler(requestParser, runtime);
         assert(response);
         assert.equal(response.status, 200);
-        assert.equal(response.value.route, "");
+        assert.equal(response.value.route, "/");
 
         const requestParser2 = RequestParser.create({ url: "/", headers: {} });
         const response2 = await handler(requestParser2, runtime);
         assert(response2);
         assert.equal(response2.status, 200);
-        assert.equal(response.value.route, "");
+        assert.equal(response.value.route, "/");
     });
 
     it("Data store request with non-existing default ID", async () => {
