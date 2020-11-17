c9e3cc613e35bcc3a237f1d843f65153db6b3626
Sunny Huang <haoh@microsoft.com>
2020-11-16 17:24:53 -0800

    small fix

--- packages/drivers/odsp-driver/src/odspDocumentStorageManager.ts
+++ packages/drivers/odsp-driver/src/odspDocumentStorageManager.ts
@@ -671,7 +671,7 @@ export class OdspDocumentStorageService implements IDocumentStorageService {
             }
 
             const clientTime = networkTime ? overallTime - networkTime : undefined;
-            const isAfd = msEdge != null ? true : false;
+            const isAfd = msEdge != undefined ? true : false;
 
             event.end({
                 trees: content.trees?.length ?? 0,
