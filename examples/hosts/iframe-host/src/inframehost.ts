18c0a8c476361bfa4fbd81ed0f9cc7bb41462b42
Helio Liu <59622401+heliocliu@users.noreply.github.com>
2020-11-18 15:58:41 -0800

    remove unused proxyloader

--- examples/hosts/iframe-host/src/inframehost.ts
+++ examples/hosts/iframe-host/src/inframehost.ts
@@ -9,64 +9,11 @@ import {
 import { Container, Loader } from "@fluidframework/container-loader";
 import {
     ICodeLoader,
-    IContainerContext,
-    IRuntime,
-    IRuntimeFactory,
-    IRuntimeState,
-    AttachState,
     IProxyLoaderFactory,
 } from "@fluidframework/container-definitions";
 import { MultiDocumentServiceFactory } from "@fluidframework/driver-utils";
-import { IRequest, IResponse, IFluidObject } from "@fluidframework/core-interfaces";
+import { IRequest, IFluidObject } from "@fluidframework/core-interfaces";
 import { IDocumentServiceFactory, IUrlResolver } from "@fluidframework/driver-definitions";
-import { ISequencedDocumentMessage, ITree, ISummaryTree } from "@fluidframework/protocol-definitions";
-
-class ProxyRuntime implements IRuntime {
-    private _disposed = false;
-    public get disposed() { return this._disposed; }
-
-    public dispose(): void {
-        this._disposed = true;
-    }
-
-    async request(request: IRequest): Promise<IResponse> {
-        throw new Error("Method not implemented.");
-    }
-    async snapshot(tagMessage: string, fullTree?: boolean | undefined): Promise<ITree | null> {
-        throw new Error("Method not implemented.");
-    }
-    async setConnectionState(connected: boolean, clientId?: string) {
-    }
-    async stop(): Promise<IRuntimeState> {
-        throw new Error("Method not implemented.");
-    }
-    async process(message: ISequencedDocumentMessage, local: boolean, context: any) {
-    }
-    async processSignal(message: any, local: boolean) {
-    }
-    // TODO: Issue-2109 Implement detach container api or put appropriate comment.
-    createSummary(): ISummaryTree {
-        throw new Error("Method not implemented.");
-    }
-    setAttachState(state: AttachState.Attaching | AttachState.Attached) {
-    }
-}
-
-class ProxyChaincode implements IRuntimeFactory {
-    async instantiateRuntime(context: IContainerContext): Promise<IRuntime> {
-        return new ProxyRuntime();
-    }
-
-    get IRuntimeFactory() {
-        return this;
-    }
-}
-
-export class ProxyCodeLoader implements ICodeLoader {
-    async load() {
-        return Promise.resolve({ fluidExport: new ProxyChaincode() });
-    }
-}
 
 export interface IFrameOuterHostConfig {
     documentServiceFactory: IDocumentServiceFactory;
