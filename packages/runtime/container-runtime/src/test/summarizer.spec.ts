f502314d3bf1fdc107de7d3f918cba2bb0e2c7fb
Mark Fields <markfields@users.noreply.github.com>
2020-11-19 15:25:22 -0800

    Switch summarizer tests to shared MockLogger

--- packages/runtime/container-runtime/src/test/summarizer.spec.ts
+++ packages/runtime/container-runtime/src/test/summarizer.spec.ts
@@ -13,45 +13,9 @@ import {
     ISummaryProposal,
     MessageType,
 } from "@fluidframework/protocol-definitions";
-import { ITelemetryLogger, ITelemetryBaseEvent } from "@fluidframework/common-definitions";
-import { TelemetryLogger } from "@fluidframework/telemetry-utils";
+import { MockLogger } from "@fluidframework/telemetry-utils";
 import { RunningSummarizer } from "../summarizer";
 import { SummaryCollection } from "../summaryCollection";
-
-class MockLogger extends TelemetryLogger implements ITelemetryLogger {
-    events: ITelemetryBaseEvent[] = [];
-
-    constructor() { super(); }
-
-    send(event: ITelemetryBaseEvent): void {
-        this.events.push(event);
-    }
-
-    matchEvents(expectedEvents: Omit<ITelemetryBaseEvent, "category">[]) {
-        let iExpectedEvent = 0;
-        this.events.forEach((event) => {
-            if (iExpectedEvent < expectedEvents.length &&
-                MockLogger.eventsMatch(event, expectedEvents[iExpectedEvent])
-            ) {
-                // We found the next expected event; increment
-                ++iExpectedEvent;
-            }
-        });
-
-        // Remove the events so far; next call will just compare subsequent events from here
-        this.events = [];
-
-        // How many expected events were left over? Hopefully none.
-        const unmatchedExpectedEventCount = expectedEvents.length - iExpectedEvent;
-        assert(unmatchedExpectedEventCount >= 0);
-        return unmatchedExpectedEventCount === 0;
-    }
-
-    private static eventsMatch(actual: ITelemetryBaseEvent, expected: Omit<ITelemetryBaseEvent, "category">): boolean {
-        const masked = { ...actual, ...expected };
-        return JSON.stringify(masked) === JSON.stringify(actual);
-    }
-}
 
 describe("Runtime", () => {
     describe("Container Runtime", () => {
