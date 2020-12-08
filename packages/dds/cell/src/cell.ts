b9e3f8e2a67a4ae4831ed1f48770fa29f463157b
Si Chen <chensi@microsoft.com>
2020-12-07 16:18:56 -0800

    testing

--- packages/dds/cell/src/cell.ts
+++ packages/dds/cell/src/cell.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import { assert, fromBase64ToUtf8 } from "@fluidframework/common-utils";
+import { assert } from "@fluidframework/common-utils";
 import { IFluidSerializer, ISerializedHandle } from "@fluidframework/core-interfaces";
 
 import {
@@ -242,10 +242,10 @@ export class SharedCell<T extends Serializable = any> extends SharedObject<IShar
      * {@inheritDoc @fluidframework/shared-object-base#SharedObject.loadCore}
      */
     protected async loadCore(storage: IChannelStorageService): Promise<void> {
-        const rawContent = await storage.read(snapshotFileName);
+        const rawContent = await storage.readString(snapshotFileName);
 
         const content = rawContent !== undefined
-            ? JSON.parse(fromBase64ToUtf8(rawContent)) as ICellValue
+            ? JSON.parse(rawContent) as ICellValue
             : { type: ValueType[ValueType.Plain], value: undefined };
 
         this.data = this.fromSerializable(content);
