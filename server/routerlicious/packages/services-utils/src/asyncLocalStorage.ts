18ddd54251f146ed2eb954d1b80b7fb74b42f213
Ping Zhu <pizh@Pings-MacBook-Pro.local>
2020-12-08 14:10:03 -0800

    updated Buffer.from params to latest ver, refined naming in als functions

--- server/routerlicious/packages/services-utils/src/asyncLocalStorage.ts
+++ server/routerlicious/packages/services-utils/src/asyncLocalStorage.ts
@@ -9,12 +9,12 @@ import { Request, Response, NextFunction } from "express";
 
 const asyncLocalStorage = new AsyncLocalStorage();
 
-export function getId(): string | undefined {
+export function getCorrelationId(): string | undefined {
     const id = asyncLocalStorage.getStore();
     return typeof(id) === "string" ? id : undefined;
 }
 
-export function bindId(headerName: string = "x-correlation-id"):
+export function bindCorrelationId(headerName: string = "x-correlation-id"):
     (req: Request, res: Response, next: NextFunction) => void {
     const randId = uuid.v4();
     return (req, res, next) => {
