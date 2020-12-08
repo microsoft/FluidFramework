b2e171478c7e465b574610b81aa6c1640874aa24
Ping Zhu <pizh@Pings-MacBook-Pro.local>
2020-12-07 18:02:08 -0800

    changed @types/node version, added als middleware

---
+++ server/routerlicious/packages/services-utils/src/asyncLocalStorage.ts
@@ -0,0 +1,25 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { AsyncLocalStorage } from "async_hooks";
+import * as uuid from "uuid";
+import { Request, Response, NextFunction } from "express";
+
+const asyncLocalStorage = new AsyncLocalStorage();
+
+export function getId(): string | undefined {
+    const id = asyncLocalStorage.getStore();
+    return typeof(id) === "string" ? id : undefined;
+}
+
+export function bindId(headerName: string = "x-correlation-id"):
+    (req: Request, res: Response, next: NextFunction) => void {
+    const randId = uuid.v4();
+    return (req, res, next) => {
+        const id: string = req.header(headerName) || randId;
+        res.setHeader(headerName, id);
+        asyncLocalStorage.run(id, () => next());
+    };
+}
