a70dfd972eab7bfcdbb1c2bf0a3734508daeebff
heliocliu <59622401+heliocliu@users.noreply.github.com>
2020-12-09 16:39:23 -0800

    undo client changes

--- packages/tools/webpack-fluid-loader/src/routes.ts
+++ packages/tools/webpack-fluid-loader/src/routes.ts
@@ -28,7 +28,6 @@ let odspAuthLock: Promise<void> | undefined;
 const getThisOrigin = (options: RouteOptions): string => `http://localhost:${options.port}`;
 
 export const before = async (app: express.Application) => {
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     app.get("/getclientsidewebparts", async (req, res) => res.send(await createManifestResponse()));
     app.get("/", (req, res) => res.redirect(`/new`));
 };
@@ -153,7 +152,6 @@ export const after = (app: express.Application, server: WebpackDevServer, baseDi
         };
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     app.get("/odspLogin", async (req, res) => {
         if (options.mode !== "spo-df" && options.mode !== "spo") {
             res.write("Mode must be spo or spo-df to login to ODSP.");
@@ -172,7 +170,6 @@ export const after = (app: express.Application, server: WebpackDevServer, baseDi
         );
     });
 
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     app.get("/pushLogin", async (req, res) => {
         if (options.mode !== "spo-df" && options.mode !== "spo") {
             res.write("Mode must be spo or spo-df to login to Push.");
@@ -220,7 +217,6 @@ export const after = (app: express.Application, server: WebpackDevServer, baseDi
      * For urls of format - http://localhost:8080/doc/<id>.
      * This is when user is trying to load an existing document. We try to load a Container with `id` as documentId.
      */
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     app.get("/doc/:id*", async (req, res) => {
         const ready = await isReady(req, res);
         if (ready) {
@@ -234,7 +230,6 @@ export const after = (app: express.Application, server: WebpackDevServer, baseDi
      * For other `ids`, we treat this as the user trying to load an existing document. We redirect to
      * http://localhost:8080/doc/<id>.
      */
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     app.get("/:id*", async (req, res) => {
         // Ignore favicon.ico urls.
         if (req.url === "/favicon.ico") {
