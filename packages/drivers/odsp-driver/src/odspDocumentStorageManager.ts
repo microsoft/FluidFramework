19d33a1b0c971ad1b08ceab17cc8b9295dd27c4c
jatingarg <jatgarg@microsoft.com>
2020-12-15 14:03:09 -0800

    pr sugg

--- packages/drivers/odsp-driver/src/odspDocumentStorageManager.ts
+++ packages/drivers/odsp-driver/src/odspDocumentStorageManager.ts
@@ -385,24 +385,12 @@ export class OdspDocumentStorageService implements IDocumentStorageService {
                     this.logger.sendErrorEvent({ eventName: "TreeLatest_SecondCall", hasClaims: !!tokenFetchOptions.claims });
                 }
 
-                const snapshotOptions: ISnapshotOptions = {
-                    deltas: 1,
-                    channels: 1,
-                    blobs: 2,
-                    mds: this.maxSnapshotSizeLimit,
-                    ...this.hostPolicy.snapshotOptions,
-                    timeout: this.hostPolicy.snapshotOptions?.timeout ? Math.min(this.hostPolicy.snapshotOptions.timeout, this.maxSnapshotFetchTimeout) : this.maxSnapshotFetchTimeout,
-                };
-
-                // No limit on size of snapshot, as otherwise we fail all clients to summarize
-                if (this.hostPolicy.summarizerClient) {
-                    snapshotOptions.mds = undefined;
-                }
+                const hostSnapshotOptions = this.hostPolicy.snapshotOptions;
                 let cachedSnapshot: IOdspSnapshot | undefined;
                 const usePost = !!this.hostPolicy.usePostForTreesLatest;
                 // No need to ask cache twice - if first request was unsuccessful, cache unlikely to have data on second turn.
                 if (tokenFetchOptions.refresh) {
-                    cachedSnapshot = await this.fetchSnapshot(snapshotOptions, tokenFetchOptions, usePost);
+                    cachedSnapshot = await this.fetchSnapshot(hostSnapshotOptions, tokenFetchOptions, usePost);
                 } else {
                     cachedSnapshot = await PerformanceEvent.timedExecAsync(
                         this.logger,
@@ -420,7 +408,7 @@ export class OdspDocumentStorageService implements IDocumentStorageService {
 
                             let method: string;
                             if (this.hostPolicy.concurrentSnapshotFetch && !this.hostPolicy.summarizerClient) {
-                                const snapshotP = this.fetchSnapshot(snapshotOptions, tokenFetchOptions, usePost);
+                                const snapshotP = this.fetchSnapshot(hostSnapshotOptions, tokenFetchOptions, usePost);
 
                                 const promiseRaceWinner = await promiseRaceWithWinner([cachedSnapshotP, snapshotP]);
                                 cachedSnapshot = promiseRaceWinner.value;
@@ -439,7 +427,7 @@ export class OdspDocumentStorageService implements IDocumentStorageService {
                                 method = cachedSnapshot !== undefined ? "cache" : "network";
 
                                 if (cachedSnapshot === undefined) {
-                                    cachedSnapshot = await this.fetchSnapshot(snapshotOptions, tokenFetchOptions, usePost);
+                                    cachedSnapshot = await this.fetchSnapshot(hostSnapshotOptions, tokenFetchOptions, usePost);
                                 }
                             }
                             event.end({ method });
@@ -563,7 +551,20 @@ export class OdspDocumentStorageService implements IDocumentStorageService {
         });
     }
 
-    private async fetchSnapshot(snapshotOptions: ISnapshotOptions, tokenFetchOptions: TokenFetchOptions, usePost: boolean): Promise<IOdspSnapshot> {
+    private async fetchSnapshot(hostSnapshotOptions: ISnapshotOptions | undefined, tokenFetchOptions: TokenFetchOptions, usePost: boolean): Promise<IOdspSnapshot> {
+        const driverSnapshotOptions: ISnapshotOptions = {
+            deltas: 1,
+            channels: 1,
+            blobs: 2,
+            mds: this.maxSnapshotSizeLimit,
+            ...hostSnapshotOptions,
+            timeout: hostSnapshotOptions?.timeout ? Math.min(hostSnapshotOptions.timeout, this.maxSnapshotFetchTimeout) : this.maxSnapshotFetchTimeout,
+        };
+
+        // No limit on size of snapshot, as otherwise we fail all clients to summarize
+        if (this.hostPolicy.summarizerClient) {
+            driverSnapshotOptions.mds = undefined;
+        }
         let abortController: AbortController | undefined;
         if (this.hostPolicy.summarizerClient !== true) {
             abortController = new AbortController();
@@ -572,7 +573,7 @@ export class OdspDocumentStorageService implements IDocumentStorageService {
                     clearTimeout(timeout);
                     abortController?.abort();
                 },
-                snapshotOptions.timeout,
+                driverSnapshotOptions.timeout,
             );
         }
 
@@ -581,23 +582,23 @@ export class OdspDocumentStorageService implements IDocumentStorageService {
         // will retry, fallback to get call. In case of error for which getWithRetryForTokenRefresh will retry, let it retry
         // only if it is first failure, otherwise fallback to get call.
         try {
-            const odspSnapshot = await this.fetchSnapshotCore(snapshotOptions, tokenFetchOptions, usePost, abortController);
+            const odspSnapshot = await this.fetchSnapshotCore(driverSnapshotOptions, tokenFetchOptions, usePost, abortController);
             return odspSnapshot;
         } catch (error) {
             const errorType = error.errorType;
-            // If the snapshot size is too big and the host specified the size limitation, then don't try to fetch the snapshot again.
-            if ((errorType === OdspErrorType.snapshotTooBig && this.hostPolicy.snapshotOptions?.mds !== undefined) && (this.hostPolicy.summarizerClient !== true)) {
+            // If the snapshot size is too big and the host specified the size limitation(specified in hostSnapshotOptions), then don't try to fetch the snapshot again.
+            if ((errorType === OdspErrorType.snapshotTooBig && hostSnapshotOptions?.mds !== undefined) && (this.hostPolicy.summarizerClient !== true)) {
                 throw error;
             }
             // If the first snapshot request was with blobs and we either timed out or the size was too big, then try to fetch without blobs.
-            if ((errorType === OdspErrorType.snapshotTooBig || errorType === OdspErrorType.fetchTimeout) && snapshotOptions.blobs) {
-                const snapshotOptionsWithoutBlobs: ISnapshotOptions = { ...snapshotOptions, blobs: 0, mds: undefined };
+            if ((errorType === OdspErrorType.snapshotTooBig || errorType === OdspErrorType.fetchTimeout) && driverSnapshotOptions.blobs) {
+                const snapshotOptionsWithoutBlobs: ISnapshotOptions = { ...driverSnapshotOptions, blobs: 0, mds: undefined };
                 return this.fetchSnapshotCore(snapshotOptionsWithoutBlobs, tokenFetchOptions, usePost);
             }
             // If we used the post request first time and got a 400 error from server, then try with a GET request.
             if (usePost && error.statusCode === 400) {
                 this.logger.sendErrorEvent({ eventName: "TreeLatest_FallBackToGetRequest" }, error);
-                return this.fetchSnapshot(snapshotOptions, tokenFetchOptions, false);
+                return this.fetchSnapshot(driverSnapshotOptions, tokenFetchOptions, false);
             }
             throw error;
         }
