df9d4537e0ef05c131775aa6128259d84196c45b
Tony Murphy <anthony.murphy@microsoft.com>
2020-12-17 09:48:22 -0800

    Handle errors when resolving handles (#4620)
    Return 500 on channel load failure, rather than 404 which is handled differently throughout the stack
    
    related #4613

--- packages/runtime/runtime-utils/src/remoteFluidObjectHandle.ts
+++ packages/runtime/runtime-utils/src/remoteFluidObjectHandle.ts
@@ -50,7 +50,7 @@ export class RemoteFluidObjectHandle implements IFluidHandle {
                 .then<IFluidObject>((response) =>
                     response.mimeType === "fluid/object"
                         ? response.value as IFluidObject
-                        : Promise.reject(new Error("Not found")));
+                        : Promise.reject(new Error(response.value)));
         }
 
         return this.dataStoreP;
