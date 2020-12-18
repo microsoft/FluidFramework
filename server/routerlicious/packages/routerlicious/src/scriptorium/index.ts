6a0e3c82c202d3eb861b69c81dbf2c4954609bfb
Pradeep Vairamani <pradeeprv123@gmail.com>
2020-12-18 14:08:30 -0800

    Adding new field in mongo for TTL

--- server/routerlicious/packages/routerlicious/src/scriptorium/index.ts
+++ server/routerlicious/packages/routerlicious/src/scriptorium/index.ts
@@ -7,12 +7,15 @@ import { ScriptoriumLambdaFactory } from "@fluidframework/server-lambdas";
 import * as services from "@fluidframework/server-services";
 import { IPartitionLambdaFactory, MongoManager } from "@fluidframework/server-services-core";
 import { Provider } from "nconf";
+import * as winston from "winston";
 
 export async function create(config: Provider): Promise<IPartitionLambdaFactory> {
     const mongoUrl = config.get("mongo:endpoint") as string;
+    const mongoExpireAfterSeconds = 5 * 60; // config.get("mongo:expireAfterSeconds") as number;
     const deltasCollectionName = config.get("mongo:collectionNames:deltas");
     const mongoFactory = new services.MongoDbFactory(mongoUrl);
     const mongoManager = new MongoManager(mongoFactory, false);
+    winston.info(`TTL is ${mongoExpireAfterSeconds}`);
 
     const db = await mongoManager.getDatabase();
     const opCollection = db.collection(deltasCollectionName);
@@ -24,6 +27,13 @@ export async function create(config: Provider): Promise<IPartitionLambdaFactory>
             "tenantId": 1,
         },
         true);
+
+        await opCollection.createTTLIndex(
+            {
+                mongoTimestamp: 1,
+            },
+            mongoExpireAfterSeconds,
+        );
 
     return new ScriptoriumLambdaFactory(mongoManager, opCollection);
 }
