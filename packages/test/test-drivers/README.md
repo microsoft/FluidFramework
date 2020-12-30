c3f2bc36aef8d468cf451fcfbb60e49ad01eb2b2
Tony Murphy <anthonm@microsoft.com>
2020-12-30 12:04:44 -0800

    update readme

--- packages/test/test-drivers/README.md
+++ packages/test/test-drivers/README.md
@@ -1,6 +1,24 @@
-# @fluidframework/mocha-test-setup
+# @fluidframework/test-drivers
 
-This package contains beforeEach/afterEach root hooks for mocha test that hide console output by default. 
-Set FLUID_TEST_VERBOSE in the environment to get the console output back
+This package provides a simple and common driver abstraction that can be used by tests to be server agnostic.
 
-See [GitHub](https://github.com/microsoft/FluidFramework) for more details on the Fluid Framework and packages within.
+`createCreateNewRequest` and `createContainerUrl` both take a test id.
+The test id may not map directly to any specific Fluid Framework concept.
+Repeated calls will the same test id should return the same result.
+
+If you need more control you should disambiguate the driver based on its
+type, this should only be done it absolutely necessary for complex scenarios
+as the test may not  work against all supported servers if done.
+
+If mocha tests wish to not run or only run on specific servers in a mocha test they should do something like the following:
+
+``` typescript
+  before(function() {
+        const driver = getTestDriver();
+        if (driver.type !== "local") {
+            this.skip();
+        }
+    });
+```
+
+The `function` syntax must be used for `this.skip()` to be available, arrow function will not work.
--- packages/test/test-drivers/src/interfaces.ts
+++ packages/test/test-drivers/src/interfaces.ts
@@ -8,9 +8,38 @@ import { IDocumentServiceFactory, IUrlResolver } from "@fluidframework/driver-de
 export type TestDriverTypes = "local" | "tinylicious" | "routerlicious" | "odsp";
 
 export interface ITestDriver{
+    /**
+     * The type of server the test driver executes against
+     */
     type: TestDriverTypes;
+
+    /**
+     * Creates a document service factory targetting the server
+     */
     createDocumentServiceFactory(): IDocumentServiceFactory;
+
+    /**
+     * Creates a url resolver targetting the server
+     */
     createUrlResolver(): IUrlResolver;
+
+    /**
+     * Creates a create new request based on the test id.
+     * Repeated calls with the same test id will return the same request.
+     * The test id may not map directly to any specific Fluid Framework concept.
+     * If you need more control you should disambiguate the driver based on its
+     * type, this should only be done it absolutely necessary for complex scenarios
+     * as the test may not  work against all supported servers if done.
+     */
     createCreateNewRequest(testId: string): IRequest;
+
+    /**
+     * Creates a container url that can be resolved by the url resolver for this driver.
+     * Repeated calls with the same test id will return the same test id.
+     * The test id may not map directly to any specific Fluid Framework concept.
+     * If you need more control you should disambiguate the driver based on its
+     * type, this should only be done it absolutely necessary for complex scenarios
+     * as the test may not  work against all supported servers if done.
+     */
     createContainerUrl(testId: string): string;
 }
