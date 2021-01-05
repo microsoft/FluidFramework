f322152c778724bafdb73f768b5568ba3d85bd07
Pradeep Vairamani <pradeeprv123@gmail.com>
2021-01-05 15:30:19 -0800

    Passing expiry time as config

--- server/routerlicious/packages/routerlicious/src/scriptorium/index.ts
+++ server/routerlicious/packages/routerlicious/src/scriptorium/index.ts
@@ -7,15 +7,13 @@ import { ScriptoriumLambdaFactory } from "@fluidframework/server-lambdas";
 import * as services from "@fluidframework/server-services";
 import { IPartitionLambdaFactory, MongoManager } from "@fluidframework/server-services-core";
 import { Provider } from "nconf";
-import * as winston from "winston";
 
 export async function create(config: Provider): Promise<IPartitionLambdaFactory> {
     const mongoUrl = config.get("mongo:endpoint") as string;
-    const mongoExpireAfterSeconds = 5 * 60; // config.get("mongo:expireAfterSeconds") as number;
+    const mongoExpireAfterSeconds = config.get("mongo:expireAfterSeconds") as number;
     const deltasCollectionName = config.get("mongo:collectionNames:deltas");
     const mongoFactory = new services.MongoDbFactory(mongoUrl);
     const mongoManager = new MongoManager(mongoFactory, false);
-    winston.info(`TTL is ${mongoExpireAfterSeconds}`);
 
     const db = await mongoManager.getDatabase();
     const opCollection = db.collection(deltasCollectionName);
@@ -28,12 +26,14 @@ export async function create(config: Provider): Promise<IPartitionLambdaFactory>
         },
         true);
 
+    if (mongoExpireAfterSeconds > 0) {
         await opCollection.createTTLIndex(
             {
                 mongoTimestamp: 1,
             },
             mongoExpireAfterSeconds,
         );
+    }
 
     return new ScriptoriumLambdaFactory(mongoManager, opCollection);
 }
