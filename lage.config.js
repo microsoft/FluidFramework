463b2856c8292c38a72f5a83c7b92673e9a06d68
Tyler Butler <tyler@tylerbutler.com>
2021-01-08 12:34:25 -0800

    lage config

--- lage.config.js
+++ lage.config.js
@@ -1,20 +1,50 @@
 module.exports = {
     "pipeline": {
-        "build": [
-            "^tsc"
+        "l-build": [
+            "^tsc",
+            // "^build:compile",
+            // "^build:esnext",
+            "^build:copy",
         ],
-        "tsc": [
-            "^tsc"
+        "l-build:test": [
+            "l-build",
+            "^build:test",
         ],
-        "test": [
-            "build"
+        "l-lint": [
+            "eslint"
         ],
-        "lint": [
-            "^eslint"
-        ],
-        "clean": [
+        "l-clean": [
             "clean",
-        ]
+        ],
+        "l-webpack": [
+            "l-build",
+            "^webpack",
+        ],
+
+        // Tasks below this should *only* have dependencies on lage tasks
+        "l-build:full": [
+            "l-build",
+            "l-lint",
+        ],
+        // "tsc": [
+        //     "^tsc"
+        // ],
+        "l-test": [
+            "l-build"
+        ],
     },
-    "npmClient": "yarn"
+    "npmClient": "yarn",
+    "cacheOptions": {
+        // cacheStorageConfig: { provider: "local" },
+        // clearOutputFolder: false,
+        // internalCacheFolder: ".cache/backfill",
+        // logFolder: ".cache/backfill",
+        logLevel: "silly",
+        // mode: "READ_WRITE",
+        // name: "[name-of-package]",
+        outputGlob: ["lib/**", "dist/**"],
+        // packageRoot: ".",
+        // producePerformanceLogs: false,
+        // validateOutput: false
+    },
 };
