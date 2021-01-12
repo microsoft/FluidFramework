28a52362e495bb487aebce4c12ca77eec888d034
Micah Godbolt <mgodbolt@microsoft.com>
2021-01-12 09:30:18 -0800

    removed merge tree from main export

--- packages/utils/fluid-main/src/index.ts
+++ packages/utils/fluid-main/src/index.ts
@@ -8,7 +8,6 @@ export * from  "./Aqueduct";
 export * from  "./CoreInterfaces";
 export * from  "./DatastoreDefinitions";
 export * from  "./Map";
-export * from  "./MergeTree";
 export * from  "./ProtocolDefinitions";
 export * from  "./Sequence";
 export * from  "./ViewInterfaces";
