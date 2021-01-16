1aba0d1c6b10a4161370a94bf5dbe3b15b0944d2
Tony Murphy <anthonm@microsoft.com>
2021-01-15 17:36:13 -0800

    move local server tests to their own package

---
+++ packages/test/local-server-tests/.mocharc.js
@@ -0,0 +1,15 @@
+
+'use strict';
+
+// Here's a JavaScript-based config file.
+// If you need conditional logic, you might want to use this type of config.
+// Otherwise, JSON or YAML is recommended.
+
+module.exports = {
+  "exit": true,
+  "recursive": true,
+  "requires": ["node_modules/@fluidframework/mocha-test-setup"],
+  "reporter": process.env.FLUID_TEST_COVERAGE === "1" ? "mocha-junit-reporter" : undefined,
+  "reporter-options":[`mochaFile=nyc/junit-report.xml`],
+  "unhandled-rejections": "strict"
+};
