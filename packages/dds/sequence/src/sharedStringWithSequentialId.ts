b93581a618940a58b3b84f846ff88422acd5f958
Montse Lozano Dieck <malozano@microsoft.com>
2021-01-19 08:31:28 -0800

    changes to use sequenceDelta

--- packages/dds/sequence/src/sharedStringWithSequentialId.ts
+++ packages/dds/sequence/src/sharedStringWithSequentialId.ts
@@ -3,87 +3,71 @@
  * Licensed under the MIT License.
  */
 
-import { IMergeTreeOp, Marker,
+import { IChannelAttributes, IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions";
+import { Marker,
     MergeTreeDeltaType, reservedMarkerIdKey } from "@fluidframework/merge-tree";
-import { ISequencedDocumentMessage, MessageType } from "@fluidframework/protocol-definitions";
-import { parseHandles } from "@fluidframework/shared-object-base";
 import { createIdAfterMin, createIdBeforeMax } from "./generateSequentialId";
+import { SequenceDeltaEvent } from "./sequenceDeltaEvent";
 import { SharedString } from "./sharedString";
 
 export function sharedStringWithSequentialIdMixin(Base: typeof SharedString = SharedString): typeof SharedString {
     return class SequenceWithSequentialId extends Base {
-        protected processMergeTreeMsg(
-            rawMessage: ISequencedDocumentMessage): void {
-                const message = parseHandles(
-                    rawMessage,
-                    this.runtime.IFluidSerializer);
-                this.applySequentialId(message);
-                super.processMergeTreeMsg(rawMessage);
+        constructor(document: IFluidDataStoreRuntime, public id: string, attributes: IChannelAttributes) {
+            super(document, id, attributes);
+            this.on("sequenceDelta", this.applySequentialId);
         }
 
-        private applySequentialId(msg: ISequencedDocumentMessage) {
-            if (msg.type !== MessageType.Operation) {
-                return;
-            }
-
-            const op = msg.contents as IMergeTreeOp;
-            if (op.type === MergeTreeDeltaType.INSERT && op.seg !== undefined
-                && typeof op.seg === "object") {
-                // this is a marker
-                if (op.pos1 === undefined) {
-                    return;
-                }
-
-                const insertSegment = this.client.getContainingSegment(op.pos1);
-                if (!Marker.is(insertSegment.segment)) {
-                    return;
-                }
-                const newMarker = insertSegment.segment;
-                let newMarkerProps = newMarker.properties ?? {};
-                const newMarkerCp = this.getPosition(newMarker);
-                const beforeMarkers: Marker[] = [];
-                const afterMarkers: Marker[] = [];
-
-                this.walkSegments((segment) => {
-                    if (Marker.is(segment)) {
-                        beforeMarkers.push(segment);
+        private applySequentialId(deltaEvent: SequenceDeltaEvent) {
+            deltaEvent.ranges.forEach(range => {
+                const segment = range.segment;
+                if (deltaEvent.deltaOperation === MergeTreeDeltaType.INSERT && Marker.is(segment)) {
+                    let newMarkerProps = segment.properties ?? {};
+                    const newMarkerCp = this.getPosition(segment);
+                    const beforeMarkers: Marker[] = [];
+                    const afterMarkers: Marker[] = [];
+    
+                    this.walkSegments((segment) => {
+                        if (Marker.is(segment)) {
+                            beforeMarkers.push(segment);
+                        }
+                        return true;
+                    }, 0, newMarkerCp);
+    
+                    this.walkSegments((segment) => {
+                        if (Marker.is(segment)) {
+                            afterMarkers.push(segment);
+                        }
+                        return true;
+                    }, newMarkerCp + 1, this.getLength());
+    
+                    const previousMarker: Marker | undefined = beforeMarkers.pop();
+                    const nextMarker: Marker | undefined = afterMarkers.shift();
+                    const previousMarkerId = previousMarker ? previousMarker.getId() : "";
+                    const nextMarkerId =  nextMarker ? nextMarker.getId() : "";
+    
+                    // Generate sequentialId
+                    const previousMarkerCp = previousMarker ? this.getPosition(previousMarker) : 0;
+                    const nextMarkerCp = nextMarker ? this.getPosition(nextMarker) : 0;
+                    const distancePreviousMarker = Math.abs(newMarkerCp - previousMarkerCp);
+                    const distanceToNextMarker = Math.abs(newMarkerCp - nextMarkerCp);
+    
+                    let id: string;
+                    if ((previousMarkerId.length === 0 && nextMarkerId.length === 0) ||
+                     (previousMarkerId.length > 0 && nextMarkerId.length === 0)) {
+                        id = createIdAfterMin(previousMarkerId, nextMarkerId);
+                    } else if (nextMarkerId.length > 0 && previousMarkerId.length === 0) {
+                        id = createIdBeforeMax(previousMarkerId, nextMarkerId);
+                    } else {
+                        id = distancePreviousMarker <= distanceToNextMarker ?
+                         createIdAfterMin(previousMarkerId, nextMarkerId) :
+                          createIdBeforeMax(previousMarkerId, nextMarkerId);
                     }
-                    return true;
-                }, 0, newMarkerCp);
-
-                this.walkSegments((segment) => {
-                    if (Marker.is(segment)) {
-                        afterMarkers.push(segment);
-                    }
-                    return true;
-                }, newMarkerCp + 1, this.getLength());
-
-                const previousMarker: Marker | undefined = beforeMarkers.pop();
-                const nextMarker: Marker | undefined = afterMarkers.shift();
-                const previousMarkerId = previousMarker ? previousMarker.getId() : "";
-                const nextMarkerId =  nextMarker ? nextMarker.getId() : "";
-
-                // Generate sequentialId
-                const previousMarkerCp = previousMarker ? this.getPosition(previousMarker) : 0;
-                const nextMarkerCp = nextMarker ? this.getPosition(nextMarker) : 0;
-                const distancePreviousMarker = Math.abs(newMarkerCp - previousMarkerCp);
-                const distanceToNextMarker = Math.abs(newMarkerCp - nextMarkerCp);
-
-                let id: string;
-                if ((previousMarkerId.length === 0 && nextMarkerId.length === 0) ||
-                 (previousMarkerId.length > 0 && nextMarkerId.length === 0)) {
-                    id = createIdAfterMin(previousMarkerId, nextMarkerId);
-                } else if (nextMarkerId.length > 0 && previousMarkerId.length === 0) {
-                    id = createIdBeforeMax(previousMarkerId, nextMarkerId);
-                } else {
-                    id = distancePreviousMarker <= distanceToNextMarker ?
-                     createIdAfterMin(previousMarkerId, nextMarkerId) :
-                      createIdBeforeMax(previousMarkerId, nextMarkerId);
+    
+                    newMarkerProps = { ...newMarkerProps, [reservedMarkerIdKey]: id };
+                    segment.properties = newMarkerProps;
                 }
 
-                newMarkerProps = { ...newMarkerProps, [reservedMarkerIdKey]: id };
-                newMarker.properties = newMarkerProps;
-            }
+            });
         }
     };
 }
