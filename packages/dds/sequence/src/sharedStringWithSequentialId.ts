60e4f1c93d2b7aea0160b3f68aca15ea36bbedc0
Montse Lozano Dieck <malozano@microsoft.com>
2021-01-20 15:04:44 -0800

    make sortedMarkers lazy

--- packages/dds/sequence/src/sharedStringWithSequentialId.ts
+++ packages/dds/sequence/src/sharedStringWithSequentialId.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
+import { Lazy } from "@fluidframework/common-utils";
 import { IChannelAttributes, IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions";
 import { Marker, MergeTreeDeltaType, MergeTreeMaintenanceType,
      reservedMarkerIdKey, SortedSegmentSet } from "@fluidframework/merge-tree";
@@ -11,11 +12,19 @@ import { SharedString } from "./sharedString";
 
 export function sharedStringWithSequentialIdMixin(Base: typeof SharedString = SharedString): typeof SharedString {
     return class SequenceWithSequentialId extends Base {
-        private readonly sortedMarkers: SortedSegmentSet;
+        private readonly sortedMarkers: Lazy<SortedSegmentSet>;
         constructor(document: IFluidDataStoreRuntime, public id: string, attributes: IChannelAttributes) {
             super(document, id, attributes);
-            this.sortedMarkers = new SortedSegmentSet();
-            this.client.specToSegment
+            this.sortedMarkers = new Lazy<SortedSegmentSet>(() => {
+                const set = new SortedSegmentSet();
+                this.walkSegments((segment) => {
+                    if (segment.type === Marker.type) {
+                        set.addOrUpdate(segment);
+                    }
+                    return true;
+                });
+                return set;
+            });
             this.on("maintenance", this.applySequentialId);
         }
 
@@ -25,8 +34,8 @@ export function sharedStringWithSequentialIdMixin(Base: typeof SharedString = Sh
                     const markerSegment = range.segment;
                     if (Marker.is(markerSegment)) {
                         if (event.opArgs.op.type === MergeTreeDeltaType.INSERT) {
-                            this.sortedMarkers.addOrUpdate(markerSegment);
-                            const markerItems = this.sortedMarkers.items;
+                            this.sortedMarkers.value.addOrUpdate(markerSegment);
+                            const markerItems = this.sortedMarkers.value.items;
                             const newMarkerIndex = markerItems.indexOf(markerSegment);
                             const previousMarkerIndex = newMarkerIndex - 1;
                             const nextMarkerIndex = newMarkerIndex + 1;
@@ -36,14 +45,12 @@ export function sharedStringWithSequentialIdMixin(Base: typeof SharedString = Sh
                              markerItems[nextMarkerIndex] as Marker : undefined;
                             const previousId = previousMarker !== undefined ? previousMarker.getId() : "";
                             const nextId = nextMarker !== undefined ? nextMarker.getId() : "";
-                            // Generate sequentialId
-
                             let newMarkerProps = markerSegment.properties ?? {};
                             const id = createIdAfterMin(previousId, nextId);
                             newMarkerProps = { ...newMarkerProps, [reservedMarkerIdKey]: id };
                             markerSegment.properties = newMarkerProps;
                         } else if (event.opArgs.op.type === MergeTreeDeltaType.REMOVE) {
-                            this.sortedMarkers.remove(markerSegment);
+                            this.sortedMarkers.value.remove(markerSegment);
                         }
                     }
                 });
