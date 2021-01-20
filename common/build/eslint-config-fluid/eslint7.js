516e6ee52bac9d203ef603b73fad6b347b5dcf92
Tyler Butler <tyler@tylerbutler.com>
2021-01-20 14:20:02 -0800

    Update common/build/eslint-config-fluid/eslint7.js

--- common/build/eslint-config-fluid/eslint7.js
+++ common/build/eslint-config-fluid/eslint7.js
@@ -32,7 +32,7 @@
   }
 */
 
-// // This is a workaround for https://github.com/eslint/eslint/issues/3458
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
