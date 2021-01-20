f41f84c525880e10fdf69e95017a072620b7b92f
Curtis Man <curtism@microsoft.com>
2021-01-20 13:48:26 -0800

    Fix build:fast error (#4850)

--- packages/framework/experimental-fluidframework/.eslintrc.js
+++ packages/framework/experimental-fluidframework/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
         "@fluidframework/eslint-config-fluid/eslint7"
     ],
     "parserOptions": {
-        "project": ["./tsconfig.json", "./src/test/tsconfig.json"]
+        "project": ["./tsconfig.json"]
     },
     "rules": {
         "@typescript-eslint/strict-boolean-expressions": "off"
