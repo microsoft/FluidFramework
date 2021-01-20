07c740284a8135de5a322ee8ca22fad591bfbc69
Abram Sanderson <Abram.sanderson@gmail.com>
2021-01-20 14:28:48 -0800

    (split from Fluid Framework) Add an experimental tree DDS (#4821)
    * Squashed 'experimental/dds/tree/' content from commit 6227d0886
    
    git-subtree-dir: experimental/dds/tree
    git-subtree-split: 6227d08860d73c64c8b82fa34a3fca58565298c7
    
    * Resolve name differences and repo-specific experiences
    * Add experimental folder to lerna config and experimental/dds to a sensical place in the layer check
    
    Co-authored-by: Tyler Butler <tyler@tylerbutler.com>
    
    * Asymmetrical changes: remove api-extractor report file, make note on dual maintenance in README
    * Fix build:docs by exporting BlobId as an internal type.
    * Tree/Build: Remove '--pretty' to fix 'build:fast'
    * Lint: Disable 'no-unused-vars' for now
    * Tree: ESLint plugin deps
    * Update experimental/dds/tree/docs/Future.md
    
    Co-authored-by: Tyler Butler <tyler@tylerbutler.com>
    
    * Policy check
    
    Co-authored-by: Abram Sanderson <absander@microsoft.com>
    Co-authored-by: Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
    Co-authored-by: Tyler Butler <tyler@tylerbutler.com>

---
+++ src/Common.ts
@@ -0,0 +1,157 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+const defaultFailMessage = 'Assertion failed';
+
+/**
+ * Assertion failures in SharedTree will throw an exception containing this value as an `errorType`. The Fluid runtime propagates this field
+ * in its handlings of errors thrown by containers. See
+ * https://github.com/microsoft/FluidFramework/blob/main/packages/loader/container-utils/src/error.ts
+ *
+ * Exporting this enables users to safely filter telemetry handling of errors based on their type.
+ *
+ * @public
+ */
+export const sharedTreeAssertionErrorType = 'SharedTreeAssertion';
+
+/**
+ * Error object thrown by assertion failures in `SharedTree`.
+ */
+class SharedTreeAssertionError extends Error {
+	public readonly errorType = sharedTreeAssertionErrorType;
+
+	public constructor(message: string) {
+		super(message);
+		this.name = 'Assertion error';
+
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		Error.captureStackTrace?.(this);
+	}
+}
+
+/**
+ * Asserts against a boolean condition. Throws an Error if the assertion failed. Will run and throw in release builds.
+ * Use when violations are logic errors in the program.
+ * @param condition - A condition to assert is truthy
+ * @param message - Message to be printed if assertion fails. Will print "Assertion failed" by default
+ * @param containsPII - boolean flag for whether the message passed in contains personally identifying information (PII).
+ */
+export function assert(condition: unknown, message?: string, containsPII = false): asserts condition {
+	if (!condition) {
+		fail(message, containsPII);
+	}
+}
+
+/**
+ * Fails an assertion. Throws an Error that the assertion failed.
+ * Use when violations are logic errors in the program.
+ * @param message - Message to be printed if assertion fails. Will print "Assertion failed" by default
+ * @param containsPII - boolean flag for whether the message passed in contains personally identifying information (PII).
+ */
+export function fail(message: string = defaultFailMessage, containsPII = false): never {
+	if (process.env.NODE_ENV !== 'production') {
+		debugger;
+		console.error(message);
+	}
+
+	throw new SharedTreeAssertionError(containsPII ? 'Assertion failed' : message);
+}
+
+/**
+ * Asserts a value is not undefined, and returns the value.
+ * Use when violations are logic errors in the program.
+ * @param value - Value to assert against is non undefined.
+ * @param message - Message to be printed if assertion fails.
+ */
+export function assertNotUndefined<T>(value: T | undefined, message = 'value must not be undefined'): T {
+	assert(value !== undefined, message);
+	return value;
+}
+
+/**
+ * Asserts an array contains a single value and returns the value.
+ * @param array - array to assert contains a single value.
+ * @param message - Message to be printed if assertion fails.
+ */
+export function assertArrayOfOne<T>(array: readonly T[], message = 'array value must contain exactly one item'): T {
+	assert(array.length === 1, message);
+	return array[0];
+}
+
+/**
+ * Iterate through two iterables and return true if they yield equivalent elements in the same order.
+ * @param iterableA - the first iterable to compare
+ * @param iterableB - the second iterable to compare
+ * @param elementComparator - the function used to check if two `T`s are equivalent.
+ * Defaults to `Object.is()` equality (a shallow compare)
+ */
+export function compareIterables<T>(
+	iterableA: Iterable<T>,
+	iterableB: Iterable<T>,
+	elementComparator: (a: T, b: T) => boolean = Object.is
+): boolean {
+	return compareIterators<T>(iterableA[Symbol.iterator](), iterableB[Symbol.iterator](), elementComparator);
+}
+
+/**
+ * Iterate through two iterators and return true if they yield equivalent elements in the same order.
+ * @param iteratorA - the first iterator to compare
+ * @param iteratorB - the second iterator to compare
+ * @param elementComparator - the function used to check if two `T`s are equivalent.
+ * Defaults to `Object.is()` equality (a shallow compare)
+ */
+function compareIterators<T, TReturn extends T = T>(
+	iteratorA: Iterator<T, TReturn>,
+	iteratorB: Iterator<T, TReturn>,
+	elementComparator: (a: T, b: T) => boolean = Object.is
+): boolean {
+	let a: IteratorResult<T, TReturn>;
+	let b: IteratorResult<T, TReturn>;
+	for (
+		a = iteratorA.next(), b = iteratorB.next(); // Given two iterators...
+		!a.done && !b.done; // ...while both have elements remaining...
+		a = iteratorA.next(), b = iteratorB.next() // ...take one element at a time from each...
+	) {
+		// ...and ensure that their elements are equivalent
+		if (!elementComparator(a.value, b.value)) {
+			return false;
+		}
+	}
+
+	// If one iterator is done, but not the other, then they are not equivalent
+	return a.done === b.done;
+}
+
+/**
+ * Compare two arrays and return true if their elements are equivalent and in the same order.
+ * @param arrayA - the first array to compare
+ * @param arrayB - the second array to compare
+ * @param elementComparator - the function used to check if two `T`s are equivalent.
+ * Defaults to `Object.is()` equality (a shallow compare)
+ */
+export function compareArrays<T>(
+	arrayA: readonly T[],
+	arrayB: readonly T[],
+	elementComparator: (a: T, b: T) => boolean = Object.is
+): boolean {
+	if (arrayA.length !== arrayB.length) {
+		return false;
+	}
+
+	for (let i = 0; i < arrayA.length; i++) {
+		if (!elementComparator(arrayA[i], arrayB[i])) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+/**
+ * Function which does nothing (no-ops).
+ */
+export function noop(): void {
+	// noop
+}
