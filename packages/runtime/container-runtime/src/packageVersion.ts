946acae3a5246b51cc4f6132b5fa5bb4152d873f
Wes Carlson <49205066+wes-carlson@users.noreply.github.com>
2021-01-20 18:18:11 -0800

    [bump] package version for client (#4864)
    @fluidframework/build-common:     0.20.0 (unchanged)
         @fluidframework/eslint-config-fluid:     0.22.0 (unchanged)
          @fluidframework/common-definitions:     0.20.0 (unchanged)
                @fluidframework/common-utils:     0.27.0 (unchanged)
                                      Server:   0.1017.1 (unchanged)
                                      Client:     0.32.1 -> 0.32.2
                             generator-fluid:      0.3.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
                                 dice-roller:      0.0.1 (unchanged)

--- packages/runtime/container-runtime/src/packageVersion.ts
+++ packages/runtime/container-runtime/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/container-runtime";
-export const pkgVersion = "0.32.1";
+export const pkgVersion = "0.32.2";
--- packages/test/mocha-test-setup/src/packageVersion.ts
+++ packages/test/mocha-test-setup/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/mocha-test-setup";
-export const pkgVersion = "0.32.1";
+export const pkgVersion = "0.32.2";
