33e437f5ca19d988ce41d72e276f9c459ef9e3f1
Ping Zhu <pizh@Pings-MacBook-Pro.local>
2021-01-25 13:21:20 -0800

    added als in get bind correlation id functions

--- server/routerlicious/packages/services-utils/src/asyncLocalStorage.ts
+++ server/routerlicious/packages/services-utils/src/asyncLocalStorage.ts
@@ -9,14 +9,21 @@ import { Request, Response, NextFunction } from "express";
 
 const asyncLocalStorage = new AsyncLocalStorage<string>();
 
-export function getCorrelationId(): string | undefined {
-    const id = asyncLocalStorage.getStore();
-    return id;
+export function getCorrelationId(als?: AsyncLocalStorage<string>): string | undefined {
+    if (als) {
+        return als.getStore();
+    } else {
+        return asyncLocalStorage.getStore();
+    }
 }
 
-export const bindCorrelationId = (headerName: string = "x-correlation-id") =>
+export const bindCorrelationId = (als?: AsyncLocalStorage<string>, headerName: string = "x-correlation-id") =>
     ((req: Request, res: Response, next: NextFunction): void => {
         const id: string = req.header(headerName) || uuid.v4();
         res.setHeader(headerName, id);
-        asyncLocalStorage.run(id, () => next());
+        if (als) {
+            als.run(id, () => next());
+        } else {
+            asyncLocalStorage.run(id, () => next());
+        }
     });
