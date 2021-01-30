bf8b90dc9cafca5cadf1d093791dd5cbd57177eb
Tony Murphy <anthonm@microsoft.com>
2021-01-29 17:00:22 -0800

    clean up

--- packages/test/end-to-end-tests/.mocharc.js
+++ packages/test/end-to-end-tests/.mocharc.js
@@ -4,11 +4,6 @@
  */
 
 'use strict';
-
-// Here's a JavaScript-based config file.
-// If you need conditional logic, you might want to use this type of config.
-// Otherwise, JSON or YAML is recommended.
-
 module.exports = {
   "exit": true,
   "recursive": true,
@@ -18,6 +13,7 @@ module.exports = {
   ],
   "reporter": process.env.FLUID_TEST_COVERAGE === "1" ?  "mocha-junit-reporter" : undefined,
   "reporter-options":[
+    // give the report file a unique name based on driver config
     `mochaFile=nyc/${process.env.FLUID_TEST_DRIVER ?? "default"}-junit-report.xml`
   ],
   "unhandled-rejections": "strict"
