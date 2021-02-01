05409ff98279380f4485cd55186912bfadc71274
Tony Murphy <anthonm@microsoft.com>
2021-02-01 11:33:02 -0800

    get working with f5

---
+++ packages/test/end-to-end-tests/src/test/.mocharc.js
@@ -0,0 +1,31 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+const testDriver = process.env.FLUID_TEST_DRIVER ? process.env.FLUID_TEST_DRIVER  : "local";
+const packageDir = `${__dirname}/../..`;
+const testPackagesDir = `${packageDir}/..`;
+
+const config ={
+  "exit": true,
+  "recursive": true,
+  // "parallel": testDriver === "local",
+  "require": [
+    `${testPackagesDir}/mocha-test-setup`,
+    `${testPackagesDir}/test-drivers`,
+  ],
+  "unhandled-rejections": "strict"
+};
+
+if(process.env.FLUID_TEST_COVERAGE === "1"){
+  config["reporter"] = `mocha-junit-reporter`;
+  config["reporter-options"] = [
+    // give the report file a unique name based on driver config
+    `mochaFile=${packageDir}/nyc/${testDriver}-junit-report.xml`
+  ];
+}
+
+module.exports = config
