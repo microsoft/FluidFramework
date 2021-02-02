9bda5f8fd80fabe7ba3b627fbf1f21b527370fdd
Tony Murphy <anthonm@microsoft.com>
2021-02-02 15:55:58 -0800

    remove old LocalTestObjectProvider

--- packages/test/end-to-end-tests/src/test/oldVersion.ts
+++ packages/test/end-to-end-tests/src/test/oldVersion.ts
@@ -18,14 +18,10 @@ import {
 } from "old-aqueduct";
 import { SharedCell } from "old-cell";
 import { IContainer, IRuntimeFactory } from "old-container-definitions";
-import { Loader } from "old-container-loader";
 import { IContainerRuntimeOptions } from "old-container-runtime";
-import { IFluidCodeDetails } from "old-core-interfaces";
 import { SharedCounter } from "old-counter";
 import { IChannelFactory } from "old-datastore-definitions";
-import { IDocumentServiceFactory } from "old-driver-definitions";
 import { Ink } from "old-ink";
-import { LocalDocumentServiceFactory, LocalResolver } from "old-local-driver";
 import { SharedDirectory, SharedMap } from "old-map";
 import { SharedMatrix } from "old-matrix";
 import { ConsensusQueue } from "old-ordered-collection";
@@ -36,15 +32,11 @@ import {
     ChannelFactoryRegistry,
     createAndAttachContainer,
     createLocalLoader,
-    fluidEntryPoint,
-    LocalCodeLoader,
-    OpProcessingController,
     TestContainerRuntimeFactory,
     TestFluidObjectFactory,
     TestObjectProvider,
 } from "old-test-utils";
 
-import {v4 as uuid} from "uuid";
 import {
     createRuntimeFactory,
     DataObjectFactoryType,
@@ -56,166 +48,6 @@ import {
 } from "./compatUtils";
 import * as newVer from "./newVersion";
 /* eslint-enable import/no-extraneous-dependencies */
-
-const defaultCodeDetails: IFluidCodeDetails = {
-    package: "defaultTestPackage",
-    config: {},
-};
-
-// This is a replica of the code in localLoader.ts in test-utils, but bind to the old version.
-// TODO: once 0.27 is the back-compat version that we test, we can just use the version in the old-test-utils
-// However, if there are any changes to these class and code, we can shim it here.
-
-/**
- * A convenience class to manage a set of local test object to create loaders/containers with configurable way
- * to create a runtime factory from channels and factories to allow different version of the runtime to be created.
- * The objects includes the LocalDeltaConnectionServer, DocumentServiceFactory, UrlResolver.
- *
- * When creating and loading containers, it uses a default document id and code detail.
- *
- * Since the channel is just a pass thru to the call back, the type is parameterized to allow use channel
- * from different version. The only types that required to compatible when using different versions are:
- *   fluidEntryPoint
- *   IClientConfiguration
- *   ILocalDeltaConnectionServer
- */
-export class LocalTestObjectProvider<TestContainerConfigType> {
-    private _documentServiceFactory: IDocumentServiceFactory | undefined;
-    private _defaultUrlResolver: LocalResolver | undefined;
-    private _opProcessingController: OpProcessingController | undefined;
-    private _documentId?: string;
-
-    /**
-     * Create a set of object to
-     * @param createFluidEntryPoint - callback to create a fluidEntryPoint, with an optiona; set of channel name
-     * and factory for TestFluidObject
-     * @param serviceConfiguration - optional serviceConfiguration to create the LocalDeltaConnectionServer with
-     * @param _deltaConnectionServer - optional deltaConnectionServer to share documents between different provider
-     */
-    constructor(
-        private readonly createFluidEntryPoint: (testContainerConfig?: TestContainerConfigType) => fluidEntryPoint,
-        private readonly serviceConfiguration?: Partial<newVer.IClientConfiguration>,
-        private _deltaConnectionServer?: newVer.ILocalDeltaConnectionServer | undefined,
-    ) {
-
-    }
-
-    readonly driver: newVer.ITestDriver ={
-        type: "local",
-        version: "0.34.0",
-        createContainerUrl: (testId)=>`http://localhost${testId}`,
-        createCreateNewRequest: (testId)=>this.urlResolver.createCreateNewRequest(testId),
-        createDocumentServiceFactory: ()=>this.documentServiceFactory as any as newVer.IDocumentServiceFactory,
-        createUrlResolver: ()=>this.urlResolver,
-    };
-
-    get documentId(): string {
-        if(this._documentId === undefined) {
-            this._documentId = uuid();
-        }
-        return this._documentId;
-    }
-
-    get defaultCodeDetails() {
-        return defaultCodeDetails;
-    }
-
-    get deltaConnectionServer() {
-        if (!this._deltaConnectionServer) {
-            this._deltaConnectionServer = newVer.LocalDeltaConnectionServer.create(
-                undefined,
-                this.serviceConfiguration,
-            );
-        }
-        return this._deltaConnectionServer;
-    }
-
-    get documentServiceFactory() {
-        if (!this._documentServiceFactory) {
-            this._documentServiceFactory = new LocalDocumentServiceFactory(this.deltaConnectionServer as any);
-        }
-        return this._documentServiceFactory;
-    }
-
-    get urlResolver() {
-        if (!this._defaultUrlResolver) {
-            this._defaultUrlResolver = new LocalResolver();
-        }
-        return this._defaultUrlResolver;
-    }
-
-    get opProcessingController() {
-        if (!this._opProcessingController) {
-            this._opProcessingController = new OpProcessingController(this.deltaConnectionServer as any);
-        }
-        return this._opProcessingController;
-    }
-
-    private createLoader(packageEntries: Iterable<[IFluidCodeDetails, fluidEntryPoint]>) {
-        const codeLoader = new LocalCodeLoader(packageEntries);
-        return new Loader({
-            urlResolver: this.urlResolver,
-            documentServiceFactory: this.documentServiceFactory,
-            codeLoader,
-        });
-    }
-
-    /**
-     * Make a test loader
-     * @param testContainerConfig - optional configuring the test Container
-     */
-    public makeTestLoader(testContainerConfig?: TestContainerConfigType) {
-        return this.createLoader([[defaultCodeDetails, this.createFluidEntryPoint(testContainerConfig)]]);
-    }
-
-    /**
-     * Make a container using a default document id and code details
-     * @param testContainerConfig - optional configuring the test Container
-     */
-    public async makeTestContainer(testContainerConfig?: TestContainerConfigType) {
-        const loader = this.makeTestLoader(testContainerConfig);
-        const container = await createAndAttachContainer(
-            defaultCodeDetails,
-            loader,
-            this.urlResolver.createCreateNewRequest(this.documentId),
-        );
-
-        // TODO: the old version delta manager on the container doesn't do pause/resume count
-        // We can't use it to do pause/resume, or it will conflict with the call from the runtime's
-        // DeltaManagerProxy. Reach in to get in until > 0.28
-        const deltaManagerProxy = (container as any)._context.deltaManager;
-        this.opProcessingController.addDeltaManagers(deltaManagerProxy);
-        return container;
-    }
-
-    /**
-     * Load a container using a default document id and code details
-     * @param testContainerConfig - optional configuring the test Container
-     */
-    public async loadTestContainer(testContainerConfig?: TestContainerConfigType) {
-        const loader = this.makeTestLoader(testContainerConfig);
-        const container = await loader.resolve({ url: `http://localhost/${this.documentId}` });
-
-        // TODO: the old version delta manager on the container doesn't do pause/resume count
-        // We can't use it to do pause/resume, or it will conflict with the call from the runtime's
-        // DeltaManagerProxy. Reach in to get in until > 0.28
-        const deltaManagerProxy = (container as any)._context.deltaManager;
-        this.opProcessingController.addDeltaManagers(deltaManagerProxy);
-        return container;
-    }
-
-    /**
-     * Close out the DeltaConnectionServer and clear all the document and reset to original state.
-     * The object can continue to be used afterwards
-     */
-    public async reset() {
-        await this._deltaConnectionServer?.webSocketServer.close();
-        this._deltaConnectionServer = undefined;
-        this._documentServiceFactory = undefined;
-        this._opProcessingController = undefined;
-        this._documentId = undefined;
-    }
-}
 
 // A simple old-version dataStore with runtime/root exposed for testing
 // purposes. Used to test compatibility of context reload between
