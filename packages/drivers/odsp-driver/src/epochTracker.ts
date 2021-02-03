14f6b31e574fe5f8c9492de25aaebe418e5bec50
Jatin Garg <48029724+jatgarg@users.noreply.github.com>
2021-02-02 23:15:07 -0800

    Properly differentaite between epoch 409 and coherency 409 (#5028)

--- packages/drivers/odsp-driver/src/epochTracker.ts
+++ packages/drivers/odsp-driver/src/epochTracker.ts
@@ -85,6 +85,11 @@ export class EpochTracker {
             this.validateEpochFromResponse(epochFromResponse, fetchType);
             return response;
         } catch (error) {
+            // Get the server epoch from error in case we don't have it as if undefined we won't be able
+            // to mark it as epoch error.
+            if (epochFromResponse === undefined) {
+                epochFromResponse = error.serverEpoch;
+            }
             await this.checkForEpochError(error, epochFromResponse, fetchType);
             throw error;
         }
@@ -114,6 +119,11 @@ export class EpochTracker {
             this.validateEpochFromResponse(epochFromResponse, fetchType);
             return response;
         } catch (error) {
+            // Get the server epoch from error in case we don't have it as if undefined we won't be able
+            // to mark it as epoch error.
+            if (epochFromResponse === undefined) {
+                epochFromResponse = error.serverEpoch;
+            }
             await this.checkForEpochError(error, epochFromResponse, fetchType);
             throw error;
         }
