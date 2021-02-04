a4f9162849da72d93b0a9e95ca7f1fa377613d50
Sam Broner <sabroner@microsoft.com>
2021-02-03 20:15:38 -0800

    getR11sContainer

--- examples/utils/get-tinylicious-container/src/getRouterliciousContainer.ts
+++ examples/utils/get-tinylicious-container/src/getRouterliciousContainer.ts
@@ -7,14 +7,15 @@ import { IRuntimeFactory } from "@fluidframework/container-definitions";
 import { IRequest } from "@fluidframework/core-interfaces";
 import { IFluidResolvedUrl, IResolvedUrl, IUrlResolver } from "@fluidframework/driver-definitions";
 import { RouterliciousDocumentServiceFactory } from "@fluidframework/routerlicious-driver";
-import { InsecureTinyliciousTokenProvider } from "@fluidframework/tinylicious-driver";
+import { InsecureTokenProvider } from "@fluidframework/test-runtime-utils";
 import jwt from "jsonwebtoken";
 import { getContainer } from "./getContainer";
+import { IUser } from "@fluidframework/protocol-definitions";
 
-interface IRouterliciousConfig {
+export interface IRouterliciousConfig {
     orderer: string,
     storage: string,
-    tenant: string,
+    tenantId: string,
     key: string,
 }
 
@@ -33,10 +34,15 @@ export async function getRouterliciousContainer(
     createNew: boolean,
     config: IRouterliciousConfig,
 ) {
-    const tokenProvider = new InsecureTinyliciousTokenProvider();
+
+    const user = {
+        id: "unique-id",
+        name: "Unique Idee",
+    };
+    const tokenProvider = new InsecureTokenProvider(config.key, user);
     const documentServiceFactory = new RouterliciousDocumentServiceFactory(tokenProvider);
 
-    const urlResolver = new SimpleUrlResolver(config.orderer, config.storage, config.tenant, config.key);
+    const urlResolver = new SimpleUrlResolver(containerId, config, user);
 
     return getContainer(
         containerId,
@@ -52,33 +58,27 @@ class SimpleUrlResolver implements IUrlResolver {
     private readonly token: string;
 
     constructor(
-        private readonly orderer: string,
-        private readonly storage: string,
-        private readonly tenantId: string,
-        readonly key: string,
+        readonly containerId: string,
+        private readonly config: IRouterliciousConfig,
+        readonly user: IUser,
     ) {
-        const user = {
-            id: "unique-id",
-            name: "Unique Idee",
-        };
         this.token = jwt.sign(
             {
                 user,
-                // documentId: containerId,
-                tenantId,
+                documentId: containerId,
+                tenantId: config.tenantId,
                 scopes: ["doc:read", "doc:write", "summary:write"],
             },
-            key);
+            config.key);
     }
 
     public async resolve(request: IRequest): Promise<IFluidResolvedUrl> {
-        const documentUrl = `${this.orderer}/${this.tenantId}/${request.url}`;
-
+        const documentUrl = `${this.config.orderer}/${this.config.tenantId}/${request.url}`;
         return Promise.resolve({
             endpoints: {
-                deltaStorageUrl: `${this.orderer}/deltas/${this.tenantId}/${request.url}`,
-                ordererUrl: `${this.orderer}`,
-                storageUrl: `${this.storage}/repos/${this.tenantId}`,
+                deltaStorageUrl: `${this.config.orderer}/deltas/${this.config.tenantId}/${request.url}`,
+                ordererUrl: `${this.config.orderer}`,
+                storageUrl: `${this.config.storage}/repos/${this.config.tenantId}`,
             },
             tokens: { jwt: this.token },
             type: "fluid",
