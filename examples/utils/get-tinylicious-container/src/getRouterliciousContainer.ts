649af12de139f899c157cd2b67b40434d3e7f20f
Sam Broner <sabroner@microsoft.com>
2021-02-03 20:31:27 -0800

    clean up deps for IContainer

--- examples/utils/get-tinylicious-container/src/getRouterliciousContainer.ts
+++ examples/utils/get-tinylicious-container/src/getRouterliciousContainer.ts
@@ -2,56 +2,20 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-
 import { IRuntimeFactory } from "@fluidframework/container-definitions";
 import { IRequest } from "@fluidframework/core-interfaces";
 import { IFluidResolvedUrl, IResolvedUrl, IUrlResolver } from "@fluidframework/driver-definitions";
 import { RouterliciousDocumentServiceFactory } from "@fluidframework/routerlicious-driver";
 import { InsecureTokenProvider } from "@fluidframework/test-runtime-utils";
+import { IUser } from "@fluidframework/protocol-definitions";
 import jwt from "jsonwebtoken";
 import { getContainer } from "./getContainer";
-import { IUser } from "@fluidframework/protocol-definitions";
 
 export interface IRouterliciousConfig {
     orderer: string,
     storage: string,
     tenantId: string,
     key: string,
-}
-
-/**
- * Connect to an implementation of the Routerlicious service and retrieve a Container with
- * the given ID running the given code.
- *
- * @param containerId - The document id to retrieve or create
- * @param containerRuntimeFactory - The container factory to be loaded in the container
- * @param createNew - Is this a new container
- * @param config
- */
-export async function getRouterliciousContainer(
-    containerId: string,
-    containerRuntimeFactory: IRuntimeFactory,
-    createNew: boolean,
-    config: IRouterliciousConfig,
-) {
-
-    const user = {
-        id: "unique-id",
-        name: "Unique Idee",
-    };
-    const tokenProvider = new InsecureTokenProvider(config.key, user);
-    const documentServiceFactory = new RouterliciousDocumentServiceFactory(tokenProvider);
-
-    const urlResolver = new SimpleUrlResolver(containerId, config, user);
-
-    return getContainer(
-        containerId,
-        createNew,
-        { url: containerId },
-        urlResolver,
-        documentServiceFactory,
-        containerRuntimeFactory,
-    );
 }
 
 class SimpleUrlResolver implements IUrlResolver {
@@ -91,4 +55,39 @@ class SimpleUrlResolver implements IUrlResolver {
         }
         return `${resolvedUrl.url}/${relativeUrl}`;
     }
+}
+
+/**
+ * Connect to an implementation of the Routerlicious service and retrieve a Container with
+ * the given ID running the given code.
+ *
+ * @param containerId - The document id to retrieve or create
+ * @param containerRuntimeFactory - The container factory to be loaded in the container
+ * @param createNew - Is this a new container
+ * @param config
+ */
+export async function getRouterliciousContainer(
+    containerId: string,
+    containerRuntimeFactory: IRuntimeFactory,
+    createNew: boolean,
+    config: IRouterliciousConfig,
+) {
+    const user = {
+        id: "unique-id",
+        name: "Unique Idee",
+    };
+
+    const tokenProvider = new InsecureTokenProvider(config.key, user);
+    const documentServiceFactory = new RouterliciousDocumentServiceFactory(tokenProvider);
+
+    const urlResolver = new SimpleUrlResolver(containerId, config, user);
+
+    return getContainer(
+        containerId,
+        createNew,
+        { url: containerId },
+        urlResolver,
+        documentServiceFactory,
+        containerRuntimeFactory,
+    );
 }
