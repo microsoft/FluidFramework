4a2954797e8d63aa29e7f50475f2c42ea9e1b62c
ksbrar <kabir@brar.xyz>
2021-02-05 04:51:56 -0500

    First set of GenericError tests.

--- packages/loader/container-utils/src/test/errorTypePropagationTest.spec.ts
+++ packages/loader/container-utils/src/test/errorTypePropagationTest.spec.ts
@@ -20,7 +20,39 @@ describe("Check if the errorType field matches after sending/receiving via Conta
             assert(mockLogger.matchEvents([{
                 eventName: "A",
                 category: "error",
+                message: "genericError",
                 errorType: ContainerErrorType.genericError,
+                error: "genericError",
+            }]));
+        });
+
+        // Dangling error objects of any type will be ignored:
+        it("Send and receive a GenericError with a dangling error of any type.", () => {
+            const testError = new GenericError("genericError", "placeholder");
+            mockLogger.sendErrorEvent({ eventName: "A" }, testError);
+            assert(mockLogger.matchEvents([{
+                eventName: "A",
+                category: "error",
+                message: "genericError",
+                errorType: ContainerErrorType.genericError,
+                error: "genericError",
+            }]));
+        });
+        it("Send and receive a GenericError with a dangling error of object type.", () => {
+            const testErrorObj = {
+                clientId: "clientId",
+                messageClientId: "messageClientId",
+                sequenceNumber: 0,
+                clientSequenceNumber: 0,
+            };
+            const testError = new GenericError("genericError", testErrorObj);
+            mockLogger.sendErrorEvent({ eventName: "A" }, testError);
+            assert(mockLogger.matchEvents([{
+                eventName: "A",
+                category: "error",
+                message: "genericError",
+                errorType: ContainerErrorType.genericError,
+                error: "genericError",
             }]));
         });
    });
