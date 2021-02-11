60a8c19c68ee98896a9baeff546f0b2dc612bd67
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-02-11 23:13:56 +0000

    Nit: Test name

--- experimental/dds/tree-demo/tests/demo.test.ts
+++ experimental/dds/tree-demo/tests/demo.test.ts
@@ -5,7 +5,7 @@
 
 import { globals } from "../jest.config";
 
-describe("diceRoller", () => {
+describe("tree-demo", () => {
     beforeAll(async () => {
         // Wait for the page to load first before running any tests
         // so this time isn't attributed to the first test
