615e7b374f946b98431e3e43570c42a79d5ea23b
REDMOND\andreize <andreize@microsoft.com>
2021-02-12 10:27:41 -0800

    Renaming util methods to convey appropriate semantics

--- packages/utils/odsp-doclib-utils/src/odspDocLibUtils.ts
+++ packages/utils/odsp-doclib-utils/src/odspDocLibUtils.ts
@@ -8,19 +8,19 @@ const odspTenants = new Map<string, string>([
     ["spo-df", "microsoft-my.sharepoint-df.com"],
 ]);
 
-export function isOdspSiteUrl(server: string) {
-    return server.endsWith("sharepoint.com") || server.endsWith("sharepoint-df.com");
+export function isOdspHostname(hostname: string) {
+    return hostname.endsWith("sharepoint.com") || hostname.endsWith("sharepoint-df.com");
 }
 
-export function getAadTenant(server: string) {
-    let tenantName = server.substr(0, server.indexOf(".")).toLowerCase();
+export function getAadTenant(hostname: string) {
+    let tenantName = hostname.substr(0, hostname.indexOf(".")).toLowerCase();
     if (tenantName.endsWith("-my")) {
         tenantName = tenantName.substr(0, tenantName.length - 3);
     } else if (tenantName.endsWith("-admin")) {
         tenantName = tenantName.substr(0, tenantName.length - 6);
     }
 
-    let restOfTenantHostname = server.substr(tenantName.length).toLowerCase();
+    let restOfTenantHostname = hostname.substr(tenantName.length).toLowerCase();
     if (restOfTenantHostname.indexOf(".sharepoint.") === 0) {
         restOfTenantHostname = `.onmicrosoft.${restOfTenantHostname.substr(12)}`;
     }
--- packages/utils/odsp-doclib-utils/src/test/odspDocLibUtils.spec.ts
+++ packages/utils/odsp-doclib-utils/src/test/odspDocLibUtils.spec.ts
@@ -8,37 +8,37 @@ import { getAadTenant } from "../odspDocLibUtils";
 
 describe("getAadTenant", () => {
     test("returns `onmicrosoft` tenant if ODSP personal site url is passed", async () => {
-        const result = getAadTenant("https://contoso-my.sharepoint.com");
+        const result = getAadTenant("contoso-my.sharepoint.com");
         assert.strictEqual(result, "contoso.onmicrosoft.com");
     });
 
     test("returns `onmicrosoft` tenant if ODSP admin site url is passed", async () => {
-        const result = getAadTenant("https://contoso-admin.sharepoint.com");
+        const result = getAadTenant("contoso-admin.sharepoint.com");
         assert.strictEqual(result, "contoso.onmicrosoft.com");
     });
 
     test("returns `onmicrosoft` tenant if ODSP group site url is passed", async () => {
-        const result = getAadTenant("https://contoso.sharepoint.com");
+        const result = getAadTenant("contoso.sharepoint.com");
         assert.strictEqual(result, "contoso.onmicrosoft.com");
     });
 
     test("returns `onmicrosoft` tenant if ODSP dogfood personal site url is passed", async () => {
-        const result = getAadTenant("https://contoso-my.sharepoint-df.com");
+        const result = getAadTenant("contoso-my.sharepoint-df.com");
         assert.strictEqual(result, "contoso.onmicrosoft.com");
     });
 
     test("returns `onmicrosoft` tenant if ODSP dogfood admin site url is passed", async () => {
-        const result = getAadTenant("https://contoso-admin.sharepoint-df.com");
+        const result = getAadTenant("contoso-admin.sharepoint-df.com");
         assert.strictEqual(result, "contoso.onmicrosoft.com");
     });
 
     test("returns `onmicrosoft` tenant if ODSP dogfood group site url is passed", async () => {
-        const result = getAadTenant("https://contoso.sharepoint-df.com");
+        const result = getAadTenant("contoso.sharepoint-df.com");
         assert.strictEqual(result, "contoso.onmicrosoft.com");
     });
 
     test("returns unchanged url if vanity site url is passed", async () => {
-        const result = getAadTenant("https://vanity.com");
-        assert.strictEqual(result, "https://vanity.com");
+        const result = getAadTenant("vanity.com");
+        assert.strictEqual(result, "vanity.com");
     });
 });
