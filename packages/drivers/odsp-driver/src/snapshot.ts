972be240e9ceed506b39e0bef9a726d038d5a339
Vlad Sudzilouski <vlad@sudzilouski.com>
2021-02-12 13:39:53 -0800

    Psudo-code to demostrate ability to store props

--- packages/drivers/odsp-driver/src/snapshot.ts
+++ packages/drivers/odsp-driver/src/snapshot.ts
@@ -140,6 +140,13 @@ function writeTree(treeNode: Node, tree: ISnapshotTree, mapping: Map<string, num
             treeNode.addNumber(mapping.get(id));
         }
     }
+
+    if (tree.props) {
+        for (const [path, value] of Object.entries(tree.props)) {
+            treeNode.addNumber(mapping.get(path));
+            treeNode.addString(value);
+        }
+    }
 }
 
 /**
@@ -161,13 +168,16 @@ function readTree(treeNode: Node, mapping: string[]) {
 
         if (child instanceof Node) {
             tree.trees[path] = readTree(child, mapping);
-        } else {
-            assert (typeof child == "number");
+        } else if (typeof child == "number") {
             const id = mapping[child];
             assert(id !== undefined);
             tree.blobs[path] = id;
+        } else {
+            assert(child instanceof BlobCore);
+            tree.props[path] = child.toString();
         }
     }
+}
 
     return tree;
 }
@@ -189,6 +199,10 @@ function buildDictionary(tree: ISnapshotTree, dict: Set<string>) {
         dict.add(path);
         // Some blob payload might be missing, so their IDs will only show up in the tree
         dict.add(value);
+    }
+
+    for (const path of Object.keys(tree.props)) {
+        dict.add(path);
     }
 }
 
