57f517a866899b2c7dc11e9ae1b78cafd30f1734
Micah Godbolt <mgodbolt@microsoft.com>
2021-02-16 13:08:19 -0800

    fix indenting

--- examples/apps/cra-demo/src/getContainerId.ts
+++ examples/apps/cra-demo/src/getContainerId.ts
@@ -2,9 +2,9 @@
 export const getContainerId = (): { containerId: string; isNew: boolean } => {
     let isNew = false;
     if (location.hash.length === 0) {
-      isNew = true;
-      location.hash = Date.now().toString();
+        isNew = true;
+        location.hash = Date.now().toString();
     }
     const containerId = location.hash.substring(1);
     return { containerId, isNew };
-  };
+};
