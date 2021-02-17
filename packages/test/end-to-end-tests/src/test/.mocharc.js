f03dee447a5c4b41043f22e00ca86089aeaf313d
Mark Fields <markfields@users.noreply.github.com>
2021-02-16 23:32:47 -0800

    Only require @ff-internal/aria-logger when FLUID_DI_ROOT is defined

--- packages/test/end-to-end-tests/src/test/.mocharc.js
+++ packages/test/end-to-end-tests/src/test/.mocharc.js
@@ -8,19 +8,22 @@
 const testDriver = process.env.FLUID_TEST_DRIVER ? process.env.FLUID_TEST_DRIVER  : "local";
 const packageDir = `${__dirname}/../..`;
 const testPackagesDir = `${packageDir}/..`;
-const diRoot = process.env.FLUID_DI_ROOT;
 
-const req = [
-    `${testPackagesDir}/mocha-test-setup`,
-    `${testPackagesDir}/test-drivers`,
-    `${diRoot}/@ff-internal/aria-logger`, //* Only require this if FLUID_DI_ROOT is defined
+const requiredModules = [
+    `${testPackagesDir}/mocha-test-setup`, // General mocha setup e.g. suppresses console.log
+    `${testPackagesDir}/test-drivers`, // Inject implementation of getFluidTestDriver, configured via FLUID_TEST_DRIVER
 ];
+
+if (process.env.FLUID_DI_ROOT) {
+    // Inject implementation of getTestLogger that logs to Aria
+    requiredModules.push(`${process.env.FLUID_DI_ROOT}/@ff-internal/aria-logger`);
+}
 
 const config = {
   "exit": true,
   "recursive": true,
   // "parallel": testDriver === "local",
-  "require": req,
+  "require": requiredModules,
   "unhandled-rejections": "strict"
 };
 
