a719ee2f172148275db82b81d268ad10ac782e5c
Mark Fields <markfields@users.noreply.github.com>
2021-02-16 23:41:11 -0800

    Remove debugging helpers

--- packages/test/end-to-end-tests/src/test/error.spec.ts
+++ packages/test/end-to-end-tests/src/test/error.spec.ts
@@ -27,20 +27,6 @@ import { LoggingError } from "@fluidframework/telemetry-utils";
 import { createDocumentId, LocalCodeLoader, LoaderContainerTracker } from "@fluidframework/test-utils";
 import { ITestDriver } from "@fluidframework/test-driver-definitions";
 
-//* Remove.
-declare global {
-    // Temporarily defined in @ff-internal/aria-logger, just for hacking purposes.
-    export const otel: string;
-    // export function getTestLogger(): ITelemetryBaseLogger;
-}
-
-//* Remove
-describe.only("temp", () => {
-    it("temp validating DI worked", () => {
-        assert.strictEqual(otel, "Office.System.Activity");
-    });
-});
-
 describe("Errors Types", () => {
     let urlResolver: IUrlResolver;
     let testRequest: IRequest;
