70a6be630f080bf182ac5a80a01fcd3892423a68
Mark Fields <markfields@users.noreply.github.com>
2021-02-18 14:53:29 -0800

    Simplify env vars for identifying test logger pkg

--- packages/test/end-to-end-tests/src/test/.mocharc.js
+++ packages/test/end-to-end-tests/src/test/.mocharc.js
@@ -14,9 +14,9 @@ const requiredModules = [
     `${testPackagesDir}/test-drivers`, // Inject implementation of getFluidTestDriver, configured via FLUID_TEST_DRIVER
 ];
 
-if (process.env.FLUID_DI_ROOT && process.env.FLUID_DI_LOGGERPKG) {
+if (process.env.FLUID_TEST_LOGGER_PKG_PATH) {
     // Inject implementation of getTestLogger
-    requiredModules.push(`${process.env.FLUID_DI_ROOT}/${process.env.FLUID_DI_LOGGERPKG}`);
+    requiredModules.push(process.env.FLUID_TEST_LOGGER_PKG_PATH);
 }
 
 const config = {
