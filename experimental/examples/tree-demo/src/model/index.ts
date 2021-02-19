531189f86df32cb7e99638c00342cdae78ded6df
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-02-19 15:39:40 +0000

    Headers

--- experimental/examples/tree-demo/src/model/index.ts
+++ experimental/examples/tree-demo/src/model/index.ts
@@ -1,2 +1,7 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 export * from "./bubble";
 export * from "./clients";
--- experimental/examples/tree-demo/src/stats.ts
+++ experimental/examples/tree-demo/src/stats.ts
@@ -1,3 +1,7 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
 export class Stats {
     private frameCount = 0;
     private _glitchCount = 0;
