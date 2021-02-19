f0d5fb76524179b0137ca3f406537fe6c7d07d0e
Vlad Sudzilouski <vlad@sudzilouski.com>
2021-02-19 12:34:29 -0800

    Report  x-fluid-retries & sprequestguid for blob operations (#5199)
    Closes #5084: ODSP driver: Blob upload events do not carry sprequestguid property
    Closes #5193: ODSP driver: report x-fluid-retries to telemetry
    
    Note that for failed reads/writes, we should get automatically sprequestguid through centralized error reporting. The only place where it does not happen if read/write failed due to epoch check on client side. If that becomes a problem, we should address is separately, for all fetch calls that may fail client epoch check.

--- packages/drivers/odsp-driver/src/odspDocumentStorageManager.ts
+++ packages/drivers/odsp-driver/src/odspDocumentStorageManager.ts
@@ -311,7 +311,11 @@ export class OdspDocumentStorageService implements IDocumentStorageService {
                         },
                         "createBlob",
                     );
-                    event.end({ blobId: res.content.id });
+                    event.end({
+                        blobId: res.content.id,
+                        serverRetries: res.headers.get("x-fluid-retries") ?? undefined,
+                        sprequestguid: response.headers.get("sprequestguid") ?? undefined,
+                    });
                     return res;
                 },
             );
@@ -346,7 +350,9 @@ export class OdspDocumentStorageService implements IDocumentStorageService {
                         blob = await res.arrayBuffer();
                         event.end({
                             size: blob.byteLength,
+                            serverRetries: res.headers.get("x-fluid-retries") ?? undefined,
                             waitQueueLength: this.epochTracker.rateLimiter.waitQueueLength,
+                            sprequestguid: res.headers.get("sprequestguid") ?? undefined,
                         });
                         return blob;
                     },
