a5339ce3a59a92265cd002c0815e3af01300a1c5
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-02-22 20:52:13 +0000

    Tree-Demo: Bubbles inherit color from group

--- experimental/examples/tree-demo/src/bubble.tsx
+++ experimental/examples/tree-demo/src/bubble.tsx
@@ -12,27 +12,24 @@ const bubbleProxy = new BubbleProxy();
 export interface IBubbleProps {
     tree: Snapshot;
     id: NodeId;
-    color: string;
 }
 
-export const BubbleView: React.FC<IBubbleProps> = ({ tree, id, color }: IBubbleProps) => {
+export const BubbleView: React.FC<IBubbleProps> = ({ tree, id }: IBubbleProps) => {
     bubbleProxy.moveTo(tree, id);
 
     return (
         <g transform={`translate(${bubbleProxy.x},${bubbleProxy.y}) scale(${bubbleProxy.r})`}>
             <circle r="1"
-                fill={color}
                 fillOpacity="0.3"
-                stroke={color}
                 strokeWidth="0.1"
                 strokeOpacity="0.5">
             </circle>
-            <circle r="1" fill={color} fillOpacity="0.05"></circle>
+            <circle r="1" stroke="none" fillOpacity="0.05"></circle>
             <ellipse
                 cx="-0.38"
                 cy="-0.46"
                 rx="0.19"
                 ry="0.30"
-                transform={`rotate(50,-0.38,-0.46)`} fill="white" />
+                transform={`rotate(50,-0.38,-0.46)`} fill="white" stroke="none" />
         </g>);
 };
--- experimental/examples/tree-demo/src/stage.tsx
+++ experimental/examples/tree-demo/src/stage.tsx
@@ -21,21 +21,19 @@ export interface IStageProps {
 }
 
 export const StageView: React.FC<IStageProps> = ({ width, height, tree, mgr }: IStageProps) => {
-    const bubbles: JSX.Element[] = [];
+    const groups: JSX.Element[] = [];
 
     mgr.forEachClient(tree, (clientProxy) => {
         const color = clientProxy.color;
-        bubbles.splice(
-            bubbles.length,
-            0,
-            ...clientProxy.bubbles.map(
-                (id) => <BubbleView key={id} tree={tree} id={id} color={color}></BubbleView>));
+        groups.push(<g fill={color} stroke={color}>
+            {clientProxy.bubbles.map((id) => <BubbleView key={id} tree={tree} id={id}></BubbleView>)}
+        </g>);
     });
 
     return (
         <svg id="stage" xmlns="http://www.w3.org/2000/svg" version="1.1"
             style={{ position: "absolute", width: "100%", height: "100%", top: "0px" }}>
-            { bubbles }
+            { groups }
         </svg>
     );
 };
