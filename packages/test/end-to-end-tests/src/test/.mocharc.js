91b6767490f48ac4b5f2efa1c413e2f5c9280b74
Curtis Man <curtism@microsoft.com>
2021-02-24 13:27:45 -0800

    missing comma

--- packages/test/end-to-end-tests/src/test/.mocharc.js
+++ packages/test/end-to-end-tests/src/test/.mocharc.js
@@ -34,7 +34,7 @@ if(process.env.FLUID_TEST_REPORT === "1"){
   config["reporter"] = `xunit`;
   config["reporter-options"] = [
     // give the report file a unique name based on driver config
-    `output=${packageDir}/nyc/${testDriver}-junit-report.xml`
+    `output=${packageDir}/nyc/${testDriver}-junit-report.xml`,
     `suiteName="e2e - ${testDriver}"`
   ];
 }
