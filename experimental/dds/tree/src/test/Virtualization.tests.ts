f211516fd76eb6fde4f5d32ff66c28d12c1768e8
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-02-24 20:43:06 +0000

    Fix: Disable parallel test / enable non-local

--- experimental/dds/tree/src/test/Virtualization.tests.ts
+++ experimental/dds/tree/src/test/Virtualization.tests.ts
@@ -23,14 +23,6 @@ describe('SharedTree history virtualization', () => {
 	let sharedTree: SharedTree;
 	let localTestObjectProvider: TestObjectProvider<ITestContainerConfig>;
 
-	// eslint-disable-next-line @typescript-eslint/space-before-function-paren
-	before(function () {
-		const driver = getFluidTestDriver();
-		if (driver.type !== 'local') {
-			this.skip();
-		}
-	});
-
 	beforeEach(async () => {
 		const testingComponents = await setUpLocalServerTestSharedTree({ summarizer: fullHistorySummarizer_0_1_0 });
 		sharedTree = testingComponents.tree;
