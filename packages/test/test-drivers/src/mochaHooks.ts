34e0110803dc208774ffaba62c7e25f1db4bd194
Tony Murphy <anthonm@microsoft.com>
2021-02-23 17:58:14 -0800

    fix mocha hook

--- packages/test/test-drivers/src/mochaHooks.ts
+++ packages/test/test-drivers/src/mochaHooks.ts
@@ -8,16 +8,26 @@ import { createFluidTestDriver } from "./factory";
 
 const envVar = "FLUID_TEST_DRIVER";
 const _global = global as any;
-
+let fluidTestDriver: ITestDriver;
+_global.getFluidTestDriver = (): ITestDriver => {
+    return fluidTestDriver;
+};
 // can be async or not
-export const mochaGlobalSetup = async function() {
+export async function mochaGlobalSetup() {
     const fluidTestDriverType = process.env[envVar]?.toLocaleLowerCase() as TestDriverTypes | undefined | "";
-    const fluidTestDriver =
-        fluidTestDriverType === "" || fluidTestDriverType === undefined
-        ? await createFluidTestDriver("local")
-        : await createFluidTestDriver(fluidTestDriverType);
+    if(fluidTestDriver === undefined) {
+        fluidTestDriver = await createFluidTestDriver(
+            fluidTestDriverType === "" || fluidTestDriverType === undefined ? "local" : fluidTestDriverType);
+    }
 
-    _global.getFluidTestDriver = (): ITestDriver => {
-        return fluidTestDriver;
-    };
+    if (_global.getFluidTestDriver === undefined
+        || _global.getFluidTestDriver() === undefined) {
+        throw new Error("getFluidTestDriver does not exist or did not return a driver");
+    }
+}
+
+export const mochaHooks = {
+    async beforeAll() {
+        await mochaGlobalSetup();
+    },
 };
