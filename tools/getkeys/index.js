a1f516f5a52d6df4351c28d767b6f9f174628de9
Wes Carlson <49205066+wes-carlson@users.noreply.github.com>
2021-02-26 12:38:54 -0800

    Update tools/getkeys/index.js
    Co-authored-by: Mark Fields <markfields@users.noreply.github.com>

--- tools/getkeys/index.js
+++ tools/getkeys/index.js
@@ -58,6 +58,7 @@ function stdResponse(err, stdout, stderr) {
     await getKeys(client, rc, "https://prague-key-vault.vault.azure.net/");
 
     try {
+        // Key Vault with restricted access for the FF dev team only
         await getKeys(client, rc, "https://ff-internal-dev-secrets.vault.azure.net/");
     } catch (e) {
         console.log("Couldn't get secrets from FF internal keyvault. If you need access make sure you are in the relevant security group.")
