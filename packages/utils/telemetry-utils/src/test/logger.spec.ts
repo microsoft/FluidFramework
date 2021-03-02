bd0a9d91f337b91cea91283e034c7fc675bd8c1f
Mark Fields <markfields@users.noreply.github.com>
2021-03-02 11:46:51 -0800

    Finish tests

--- packages/utils/telemetry-utils/src/test/logger.spec.ts
+++ packages/utils/telemetry-utils/src/test/logger.spec.ts
@@ -36,12 +36,14 @@ describe("Logger", () => {
                 assert(event.error === "boom");
                 assert(!!event.stack);
             });
-            it("containsPII respected", () => {
+            it("containsPII respected (ish)", () => {
                 const event = freshEvent();
                 const error = new Error("boom");
                 (error as any).containsPII = true;
                 TelemetryLogger.prepareErrorObject(event, error, false);
                 assert(event.error !== "boom");
+                assert((event.stack as string).includes("boom"),
+                    "Surprisingly, the stack contains the supposedly-redacted message");
             });
             it("getTelemetryProperties absent - no further props added", () => {
                 const event = freshEvent();
@@ -49,13 +51,30 @@ describe("Logger", () => {
                 TelemetryLogger.prepareErrorObject(event, error, false);
                 assert(event.foo === undefined && event.bar === undefined);
             });
-            it("getTelemetryProperties present", () => {
+            it("getTelemetryProperties present - add additional props", () => {
                 const event = freshEvent();
-                const error = { foo: "foo", bar: 2, getTelemetryProperties: () => ({}) };
+                const error = { getTelemetryProperties: () => ({foo: "foo", bar: 2}) };
                 TelemetryLogger.prepareErrorObject(event, error, false);
-                assert(event.foo === undefined && event.bar === undefined);
+                assert(event.foo === "foo" && event.bar === 2);
             });
-            it("fetchStack", () => {});
+            it("fetchStack false - Don't add a stack if missing", () => {
+                const event = freshEvent();
+                const error = { message: "I have no stack" };
+                TelemetryLogger.prepareErrorObject(event, error, false);
+                assert.strictEqual(event.stack, undefined);
+            });
+            it("fetchStack true - Don't add a stack if present", () => {
+                const event = freshEvent();
+                const error = new Error("boom");
+                TelemetryLogger.prepareErrorObject(event, error, false);
+                assert.strictEqual(typeof (event.stack), "string");
+            });
+            it("fetchStack true - Add a stack if missing", () => {
+                const event = freshEvent();
+                const error = { message: "I have no stack" };
+                TelemetryLogger.prepareErrorObject(event, error, true);
+                assert.strictEqual(typeof (event.stack), "string");
+            });
         });
         describe("LoggingError", () => {
             it("props are assigned to the object which extends Error", () => {
