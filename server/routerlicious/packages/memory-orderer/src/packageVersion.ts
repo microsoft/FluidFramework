800a0ec0649625ba406ec97c451cfe6ab99e4c19
sharmakhushboo <sharmak@microsoft.com>
2021-03-02 19:45:56 -0800

    bump to 0.1021.0

--- server/routerlicious/packages/memory-orderer/src/packageVersion.ts
+++ server/routerlicious/packages/memory-orderer/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/server-memory-orderer";
-export const pkgVersion = "0.1020.1";
+export const pkgVersion = "0.1021.0";
--- server/routerlicious/packages/services-ordering-rdkafka/src/packageVersion.ts
+++ server/routerlicious/packages/services-ordering-rdkafka/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/server-services-ordering-rdkafka";
-export const pkgVersion = "0.1020.1";
+export const pkgVersion = "0.1021.0";
