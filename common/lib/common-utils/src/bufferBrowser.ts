25802cc551130497c06ef6273c18200636a2fb98
Tony Murphy <anthony.murphy@microsoft.com>
2021-03-04 09:33:41 -0800

    Typed arrays

--- common/lib/common-utils/src/bufferBrowser.ts
+++ common/lib/common-utils/src/bufferBrowser.ts
@@ -4,6 +4,7 @@
  */
 
 import * as base64js from "base64-js";
+import { assert } from "./assert";
 
 /**
  * Converts a Uint8Array to a string of the provided encoding
@@ -46,14 +47,23 @@ export const bufferToString = (blob: ArrayBufferLike, encoding: string): string 
 
 /**
  * Determines if an object is an array buffer
+ * Will detect and reject TypedArrays, like Uint8Array.
+ * Reason - they can be viewport into Array, they can be accepted, but caller has to deal with
+ * math properly (i.e. take into account byteOffset at minimum).
+ * For example, construction of new TypedArray can be in the form of new TypedArray(typedArray) or
+ * new TypedArray(buffer, byteOffset, length), but passing TypedArray will result in fist path (and
+ * ignoring byteOffice, length)
  * @param obj - The object to determine if it is an ArrayBuffer
  */
 export function isArrayBuffer(obj: any): obj is ArrayBuffer {
-    const maybe = obj as Partial<ArrayBuffer> | undefined;
+    const maybe = obj as (Partial<ArrayBuffer> & Partial<Uint8Array>) | undefined;
     return obj instanceof ArrayBuffer
     || (typeof maybe === "object"
+        && maybe !== null
         && typeof maybe.byteLength === "number"
-        && typeof maybe.slice === "function");
+        && typeof maybe.slice === "function"
+        && maybe.byteOffset === undefined
+        && maybe.buffer === undefined);
 }
 
 /**
@@ -78,6 +88,12 @@ export class IsoBuffer extends Uint8Array {
     static from(value, encodingOrOffset?, length?): IsoBuffer {
         if (typeof value === "string") {
             return IsoBuffer.fromString(value, encodingOrOffset as string | undefined);
+        // Capture any typed arrays, including Uint8Array (and thus - IsoBuffer!)
+        } else if (value !== null && typeof value === "object" && isArrayBuffer(value.buffer)) {
+            // Support currently for full array, no view ports! (though it can be added in future)
+            assert(value.byteOffset === 0);
+            assert(value.byteLength === value.buffer.byteLength);
+            return IsoBuffer.fromArrayBuffer(value.buffer, encodingOrOffset as number | undefined, length);
         } else if (isArrayBuffer(value)) {
             return IsoBuffer.fromArrayBuffer(value, encodingOrOffset as number | undefined, length);
         } else {
