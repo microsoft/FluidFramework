157753bc3c300a67664a3c0fa11c8f1014b06d62
Matt Rakow <ChumpChief@users.noreply.github.com>
2021-03-22 09:44:42 -0700

    Better handling of pending ops in task-manager (#5595)
    This change makes a few substantial changes in task-manager:
    
    1. Changes the task queue tracking to reflect the consensus state (previously would remove self on abandon before ack).
    2. Most importantly, tracks pending ops with messageId to better understand pending state. This allows us to better understand if we anticipate a future abandon/volunteer when deciding whether to declare ourselves the lock holder.
    3. Correctly handles abandon before the ack of a volunteer op by rejecting the promise.

--- packages/dds/task-manager/src/interfaces.ts
+++ packages/dds/task-manager/src/interfaces.ts
@@ -6,6 +6,10 @@
 import { ISharedObject, ISharedObjectEvents } from "@fluidframework/shared-object-base";
 
 export interface ITaskManagerEvents extends ISharedObjectEvents {
+    /**
+     * Notifies when the local client has reached or left the front of the queue.  Does not account for known pending
+     * ops, but instead only reflects the current state.
+     */
     (event: "assigned" | "lost", listener: (taskId: string) => void);
 }
 
@@ -22,19 +26,21 @@ export interface ITaskManager extends ISharedObject<ITaskManagerEvents> {
     lockTask(taskId: string): Promise<void>;
 
     /**
-     * Exit the queue immediately.
+     * Exit the queue, releasing the task if currently locked.
      * @param taskId - Identifier for the task
      */
     abandon(taskId: string): void;
 
     /**
-     * Check whether this client is the current assignee for the task.
+     * Check whether this client is the current assignee for the task and there is no outstanding abandon op that
+     * would release the lock.
      * @param taskId - Identifier for the task
      */
     haveTaskLock(taskId: string): boolean;
 
     /**
-     * Check whether this client is either the current assignee for the task or is waiting in line.
+     * Check whether this client is either the current assignee for the task or is waiting in line or we expect they
+     * will be in line after outstanding ops have been ack'd.
      * @param taskId - Identifier for the task
      */
     queued(taskId: string): boolean;
