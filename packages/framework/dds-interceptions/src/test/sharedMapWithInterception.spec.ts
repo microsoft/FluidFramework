d273121495327b4a44e5da79670e9e5ae42982d9
Jatin Garg <jatgarg@microsoft.com>
2021-03-25 18:36:35 -0700

    update tests

--- packages/framework/dds-interceptions/src/test/sharedMapWithInterception.spec.ts
+++ packages/framework/dds-interceptions/src/test/sharedMapWithInterception.spec.ts
@@ -110,7 +110,7 @@ describe("Shared Map with Interception", () => {
                 sharedMapWithInterception.set("color", "green");
             } catch (error) {
                 assert.strictEqual(error.message,
-                    "c0",
+                    "0x0c0",
                     "We should have caught an assert in replaceText because it detects an infinite recursion");
                 asserted = true;
             }
