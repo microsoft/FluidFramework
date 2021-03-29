661ee8aba5c993fd8be8d6fef60dc0ed5841db1a
Arin <artaylor@microsoft.com>
2021-03-29 15:03:24 -0400

    Add missing copyright header

--- packages/test/test-end-to-end-tests/src/test/flattenRuntimeOptions.ts
+++ packages/test/test-end-to-end-tests/src/test/flattenRuntimeOptions.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { IContainerRuntimeOptions } from "@fluidframework/container-runtime";
 
 /** Temporary function to make backwards compatible runtime options. */
