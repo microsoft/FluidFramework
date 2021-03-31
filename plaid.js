4ecd59b30765e64f1e059d2ea7a7ed238ed98e1c
Tyler Butler <tylerbu@microsoft.com>
2021-03-31 14:11:49 -0700

    script cleanup

--- plaid.js
+++ plaid.js
@@ -3,55 +3,55 @@
  * Licensed under the MIT License.
  */
 
-const esb = require('esbuild');
+const esb = require("esbuild");
 const fs = require("fs-extra");
-const globby = require('globby');
+const globby = require("globby");
 const shell = require("shelljs");
 
-// const { program } = require('commander');
+// const { program } = require("commander");
 
 // program
-//    .option('-d, --debug', 'output extra debugging')
-//    .option('-s, --small', 'small pizza size')
-//    .option('-p, --pizza-type <type>', 'flavour of pizza');
+//    .option("-d, --debug", "output extra debugging")
+//    .option("-s, --small", "small pizza size")
+//    .option("-p, --pizza-type <type>", "flavour of pizza");
 
 const build = async (mode, globs) => {
     if (mode === "commonjs") {
         esb.build({
             entryPoints: await globby(globs),
-            tsconfig: 'tsconfig.json',
+            tsconfig: "tsconfig.json",
             target: ["es2017"],
-            outbase: 'src',
-            outdir: 'dist',
-            format: 'cjs',
+            outbase: "src",
+            outdir: "dist",
+            format: "cjs",
             sourcemap: true,
-            logLevel: 'info',
+            logLevel: "warning",
             logLimit: 50,
             metafile: true,
         }).catch(() => process.exit(1));
-    } else if (mode === "test" && fs.existsSync('src/test/tsconfig.json')) {
+    } else if (mode === "test" && fs.existsSync("src/test/tsconfig.json")) {
         esb.build({
             entryPoints: await globby(globs),
-            tsconfig: 'src/test/tsconfig.json',
+            tsconfig: "src/test/tsconfig.json",
             target: ["es2017"],
-            outbase: 'src',
-            outdir: 'dist',
-            format: 'cjs',
+            outbase: "src",
+            outdir: "dist",
+            format: "cjs",
             sourcemap: true,
-            logLevel: 'info',
+            logLevel: "warning",
             logLimit: 50,
             metafile: true,
         }).catch(() => process.exit(1));
-    } else if (mode === "esnext" && fs.existsSync('tsconfig.esnext.json')) {
+    } else if (mode === "esnext" && fs.existsSync("tsconfig.esnext.json")) {
         esb.build({
             entryPoints: await globby(globs),
-            tsconfig: 'tsconfig.esnext.json',
+            tsconfig: "tsconfig.esnext.json",
             target: ["es2020"],
-            outbase: 'src',
-            outdir: 'lib',
-            format: 'esm',
+            outbase: "src",
+            outdir: "lib",
+            format: "esm",
             sourcemap: true,
-            logLevel: 'info',
+            logLevel: "warning",
             logLimit: 50,
             metafile: true,
         }).catch(() => process.exit(1));
@@ -59,14 +59,14 @@ const build = async (mode, globs) => {
 }
 
 const start = async () => {
-    const src = ['src/**/*.ts', 'src/**/*.tsx', '!src/test/**'];
-    const test = ['src/test/**/*.ts', 'src/test/**/*.tsx'];
+    const src = ["src/**/*.ts", "src/**/*.tsx", "!src/**/*.d.ts", "!src/test/**"];
+    const test = ["src/test/**/*.ts", "src/test/**/*.tsx", "!src/test/**/*.d.ts"];
 
     await build("commonjs", src);
     await build("esnext", src);
     await build("test", test);
 
-    if(process.argv.length >= 3) {
+    if (process.argv.length >= 3) {
         console.log(`Skipping type generation.`);
     } else {
         const result = shell.exec("npm run tsc -- --emitDeclarationOnly").code;
