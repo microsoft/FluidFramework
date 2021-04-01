660d7ec380bb79cce3813efe8ac56ff5e3a8fd0c
Tony Murphy <anthonm@microsoft.com>
2021-04-01 12:03:44 -0700

    add copyright header

--- packages/test/test-service-load/src/optionsMatrix.ts
+++ packages/test/test-service-load/src/optionsMatrix.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { ISummaryConfiguration } from "@fluidframework/protocol-definitions";
 import { ILoaderOptions  } from "@fluidframework/container-definitions";
 import { IContainerRuntimeOptions, IGCRuntimeOptions, ISummaryRuntimeOptions } from "@fluidframework/container-runtime";
