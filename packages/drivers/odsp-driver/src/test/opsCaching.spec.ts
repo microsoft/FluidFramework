5ec1dd77aa9cdf3da752d53fd61cfd21770b0d5c
Vlad Sudzilouski <vlad@sudzilouski.com>
2021-04-02 14:07:19 -0700

    More UTs: covering timer case & receiving same ops again.

--- packages/drivers/odsp-driver/src/test/opsCaching.spec.ts
+++ packages/drivers/odsp-driver/src/test/opsCaching.spec.ts
@@ -3,13 +3,27 @@
  * Licensed under the MIT License.
  */
 import { strict as assert } from "assert";
-import { OpsCache, ICache, IMessage } from "../opsCaching";
+import { OpsCache, ICache, IMessage, CacheEntry } from "../opsCaching";
 
 export type MyDataInput = IMessage & { data: string; };
 
+async function delay(timeMs: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(() => resolve(), timeMs));
+}
+
 class MockCache implements ICache {
+    public writeCount = 0;
+    public opsWritten = 0;
+
     public async write(batchNumber: string, data: string) {
+        this.writeCount++;
         this.data[batchNumber] = JSON.parse(data);
+        for (const op of this.data[batchNumber] as CacheEntry) {
+            // JSON.serialize converts undefined to null
+            if (op !== null) {
+                this.opsWritten++;
+            }
+        }
     }
 
     public async read(batchNumber: string) {
@@ -23,24 +37,11 @@ class MockCache implements ICache {
     public readonly data: { [key: string]: any; } = {};
 }
 
-async function runTest(
-    batchSize: number,
-    initialSeq: number,
-    mockData: MyDataInput[],
-    expected: { [key: number]: (MyDataInput | undefined)[] })
+async function validate(
+    mockCache: MockCache,
+    expected: { [key: number]: (MyDataInput | undefined)[]; },
+    cache: OpsCache, initialSeq: number)
 {
-    const mockCache = new MockCache();
-
-    const cache = new OpsCache(
-        initialSeq,
-        mockCache,
-        batchSize,
-        5000, // timerGranularity
-        5000, // totalOpsToCache
-    );
-
-    cache.addOps(mockData);
-
     assert.deepEqual(mockCache.data, JSON.parse(JSON.stringify(expected)));
 
     const expectedArr: MyDataInput[] = [];
@@ -83,6 +84,73 @@ async function runTest(
     }
 }
 
+async function runTestNoTimer(
+    batchSize: number,
+    initialSeq: number,
+    mockData: MyDataInput[],
+    expected: { [key: number]: (MyDataInput | undefined)[] })
+{
+    const mockCache = new MockCache();
+
+    const cache = new OpsCache(
+        initialSeq,
+        mockCache,
+        batchSize,
+        -1, // timerGranularity
+        5000, // totalOpsToCache
+    );
+
+    cache.addOps(mockData);
+
+    // Validate that writing same ops is not going to change anything
+    const writes = mockCache.writeCount;
+    cache.addOps(mockData);
+    assert.equal(writes, mockCache.writeCount);
+
+    await validate(mockCache, expected, cache, initialSeq);
+}
+
+export async function runTestWithTimer(
+    batchSize: number,
+    initialSeq: number,
+    mockData: MyDataInput[],
+    expected: { [key: number]: (MyDataInput | undefined)[] },
+    initialWritesExpected: number,
+    totalWritesExpected: number)
+{
+    const mockCache = new MockCache();
+
+    const cache = new OpsCache(
+        initialSeq,
+        mockCache,
+        batchSize,
+        1, // timerGranularity
+        5000, // totalOpsToCache
+    );
+
+    cache.addOps(mockData);
+    assert.equal(mockCache.writeCount, initialWritesExpected);
+    await validate(mockCache, expected, cache, initialSeq);
+
+    while (mockCache.writeCount < totalWritesExpected) {
+        await delay(1);
+    }
+    assert.equal(mockCache.writeCount, totalWritesExpected);
+    assert.equal(mockCache.opsWritten, mockData.length);
+}
+
+export async function runTest(
+    batchSize: number,
+    initialSeq: number,
+    mockData: MyDataInput[],
+    expected: { [key: number]: (MyDataInput | undefined)[] },
+    initialWritesExpected: number,
+    totalWritesExpected: number)
+{
+    await runTestNoTimer(batchSize, initialSeq, mockData, expected);
+    await runTestWithTimer(batchSize, initialSeq, mockData, expected, initialWritesExpected, totalWritesExpected);
+}
+
 describe("OpsCache write", () => {
     const mockData1: MyDataInput[] = [
         { sequenceNumber: 105, data: "105" },
@@ -97,11 +165,11 @@ describe("OpsCache write", () => {
     ];
 
     it("1 element in each batch of 5 should not commit", async () => {
-        await runTest(5, 100, mockData1, {});
+        await runTest(5, 100, mockData1, {}, 0, 9);
     });
 
     it("2 element in each batch of 10 should not commit", async () => {
-        await runTest(10, 100, mockData1, {});
+        await runTest(10, 100, mockData1, {}, 0, 5);
     });
 
     it("6 sequential elements with batch of 5 should commit 1 batch", async () => {
@@ -123,8 +191,10 @@ describe("OpsCache write", () => {
                 { sequenceNumber: 103, data: "103" },
                 { sequenceNumber: 104, data: "104" },
                 ],
-            });
-    });
+            },
+            1,
+            2);
+        });
 
     const mockData3: MyDataInput[] = [
         { sequenceNumber: 102, data: "102" },
@@ -141,7 +211,9 @@ describe("OpsCache write", () => {
                 { sequenceNumber: 102, data: "102" },
                 { sequenceNumber: 103, data: "103" },
             ],
-            });
+            },
+            1,
+            1);
     });
 
     it("with batch size of 1 all ops should commit in own batch", async () => {
@@ -152,6 +224,8 @@ describe("OpsCache write", () => {
             {
             102: [{ sequenceNumber: 102, data: "102" }],
             103: [{ sequenceNumber: 103, data: "103" }],
-            });
+            },
+            2,
+            2);
     });
 });
