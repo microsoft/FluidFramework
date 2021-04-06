1f0cd425906a70af907d67e27a581ec91ecf66e8
Matt Rakow <marakow@microsoft.com>
2021-04-05 17:48:06 -0700

    Add stub test

---
+++ packages/framework/deprecated-aqueduct/src/test/test.spec.ts
@@ -0,0 +1,7 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+// Need some test to generate eslint log correctly
+describe.skip("Deprecated functionality", () => { });
