a137f64f98e35c33d1cbf1ddecbc5d9d38b25122
Matt Rakow <marakow@microsoft.com>
2021-04-07 09:41:07 -0700

    Remove empty test

--- packages/framework/deprecated-agent-scheduler-container-runtime/src/test/test.spec.ts
+++ packages/framework/deprecated-agent-scheduler-container-runtime/src/test/test.spec.ts
@@ -2,6 +2,3 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-
-// Need some test to generate eslint log correctly
-describe.skip("Deprecated functionality", () => { });
