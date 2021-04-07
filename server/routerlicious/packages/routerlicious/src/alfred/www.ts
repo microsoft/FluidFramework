0a3aa43792888fa652fa9cd85640fad085fae1b4
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-04-07 14:25:07 -0700

    Move 'configureLogging' to service-utils (#5742)

--- server/routerlicious/packages/routerlicious/src/alfred/www.ts
+++ server/routerlicious/packages/routerlicious/src/alfred/www.ts
@@ -5,8 +5,7 @@
 
 import * as path from "path";
 import * as winston from "winston";
-import { configureLogging } from "@fluidframework/server-services";
-import { runService } from "@fluidframework/server-services-utils";
+import { configureLogging, runService } from "@fluidframework/server-services-utils";
 import { AlfredResourcesFactory, AlfredRunnerFactory } from "@fluidframework/server-routerlicious-base";
 
 const configPath = path.join(__dirname, "../../config/config.json");
