657b40a17fdd713932d63c4055cd93dd5787435c
Skyler Jokiel <skjokiel@microsoft.com>
2021-04-13 14:26:24 -0700

    fix tabbing

--- experimental/framework/fluid-static/src/utils.ts
+++ experimental/framework/fluid-static/src/utils.ts
@@ -31,36 +31,36 @@ export const isSharedObjectClass = (obj: any): obj is SharedObjectClass<any> => 
  * of both SharedObject or DataObject. This function seperates the two and returns a registery
  * of DataObject types and an array of SharedObjects.
  */
- export const parseDataObjectsFromSharedObjects = (config: ContainerConfig):
- [NamedFluidDataStoreRegistryEntry[], IChannelFactory[]] => {
- const registryEntries: Set<NamedFluidDataStoreRegistryEntry> = new Set();
- const sharedObjects: Set<IChannelFactory> = new Set();
+export const parseDataObjectsFromSharedObjects = (config: ContainerConfig):
+    [NamedFluidDataStoreRegistryEntry[], IChannelFactory[]] => {
+    const registryEntries: Set<NamedFluidDataStoreRegistryEntry> = new Set();
+    const sharedObjects: Set<IChannelFactory> = new Set();
 
- const tryAddObject = (obj: LoadableObjectClass<any>) => {
-     if (isSharedObjectClass(obj)) {
-         sharedObjects.add(obj.getFactory());
-     } else if (isDataObjectClass(obj)) {
-         registryEntries.add([obj.factory.type, Promise.resolve(obj.factory)]);
-     } else {
-         throw new Error(`Entry is neither a DataObject or a SharedObject`);
-     }
- };
+    const tryAddObject = (obj: LoadableObjectClass<any>) => {
+        if (isSharedObjectClass(obj)) {
+            sharedObjects.add(obj.getFactory());
+        } else if (isDataObjectClass(obj)) {
+            registryEntries.add([obj.factory.type, Promise.resolve(obj.factory)]);
+        } else {
+            throw new Error(`Entry is neither a DataObject or a SharedObject`);
+        }
+    };
 
- // Add the object types that will be initialized
- Object.values(config.initialObjects).forEach((obj) => {
-     tryAddObject(obj);
- });
+    // Add the object types that will be initialized
+    Object.values(config.initialObjects).forEach((obj) => {
+        tryAddObject(obj);
+    });
 
- // If there are dynamic object types we will add them now
- if (config.dynamicObjectTypes) {
-     for (const obj of config.dynamicObjectTypes) {
-         tryAddObject(obj);
-     }
- }
+    // If there are dynamic object types we will add them now
+    if (config.dynamicObjectTypes) {
+        for (const obj of config.dynamicObjectTypes) {
+            tryAddObject(obj);
+        }
+    }
 
- if (registryEntries.size === 0 && sharedObjects.size === 0) {
-     throw new Error("Container cannot be initialized without any DataTypes");
- }
+    if (registryEntries.size === 0 && sharedObjects.size === 0) {
+        throw new Error("Container cannot be initialized without any DataTypes");
+    }
 
- return [Array.from(registryEntries), Array.from(sharedObjects)];
+    return [Array.from(registryEntries), Array.from(sharedObjects)];
 };
