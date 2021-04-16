2ff2d787250d13b22d9e9211a686c836ed9f7e5d
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-04-16 20:31:11 +0000

    policy-check

--- experimental/dds/tree/src/SummaryBackCompatibility.ts
+++ experimental/dds/tree/src/SummaryBackCompatibility.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { IFluidSerializer } from '@fluidframework/core-interfaces';
 import { EditLog } from './EditLog';
 import { ChangeNode, Edit } from './PersistedTypes';
