a48d9cdc81432eaf327556e15fc48422ea2e5e15
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-04-20 19:16:40 +0000

    Update text: search and replace

--- packages/framework/aqueduct/src/utils/containerInteractions.ts
+++ packages/framework/aqueduct/src/utils/containerInteractions.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
 import { IFluidObject } from "@fluidframework/core-interfaces";
--- server/tinylicious/src/index.ts
+++ server/tinylicious/src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
 
