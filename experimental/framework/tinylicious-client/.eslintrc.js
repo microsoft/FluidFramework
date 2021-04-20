f1229f73d54c4d36ae9aa87b193a63bbc5075585
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2021-04-20 14:50:05 -0700

    pass updated policy check rules

--- experimental/framework/tinylicious-client/.eslintrc.js
+++ experimental/framework/tinylicious-client/.eslintrc.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
 
@@ -10,7 +10,4 @@ module.exports = {
     "parserOptions": {
         "project": ["./tsconfig.json"]
     },
-    "rules": {
-        "@typescript-eslint/strict-boolean-expressions": "off"
-    }
 }
--- experimental/framework/tinylicious-client/src/TinyliciousClient.ts
+++ experimental/framework/tinylicious-client/src/TinyliciousClient.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
 
@@ -108,7 +108,7 @@ export class TinyliciousClientInstance {
             container = await loader.resolve({ url: containerId });
             // If we didn't create the container properly, then it won't function correctly.  So we'll throw if we got a
             // new container here, where we expect this to be loading an existing container.
-            if (!container.existing) {
+            if (container.existing === undefined) {
                 throw new Error("Attempted to load a non-existing container");
             }
         }
