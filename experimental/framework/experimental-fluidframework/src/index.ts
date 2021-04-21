c28151c5d6589e7c5550206dc877e0fc4da78018
Micah Godbolt <mgodbolt@microsoft.com>
2021-04-21 13:21:49 -0700

    add exports of new packages

--- experimental/framework/experimental-fluidframework/src/index.ts
+++ experimental/framework/experimental-fluidframework/src/index.ts
@@ -5,11 +5,19 @@
 
 /* eslint-disable import/export */
 
-export * from  "./aqueduct";
-export * from  "./coreInterfaces";
-export * from  "./dataObjects";
-export * from  "./datastoreDefinitions";
-export * from  "./map";
-export * from  "./mergeTree";
-export * from  "./sequence";
-export * from  "./viewInterfaces";
+export * from "./aqueduct";
+export * from "./coreInterfaces";
+export * from "./dataObjects";
+export * from "./datastoreDefinitions";
+export * from "./map";
+export * from "./mergeTree";
+export * from "./sequence";
+export * from "./viewInterfaces";
+export * from "./taskManager";
+export * from "./fluidStatic";
+export * from "./cell";
+export * from "./counter";
+export * from "./matrix";
+export * from "./orderedCollection";
+export * from "./registerCollection";
+export * from "./sharedObjectBase";
