a0b57a756d932df045c7a998d711d57bfaefb905
arno-123 <77966788+arno-123@users.noreply.github.com>
2021-04-26 21:46:56 +0200

    Initial commit of experimental Property DDS  (#5767)
    Co-authored-by: Marcus Karlbowski <marcus.karlbowski@autodesk.com>
    Co-authored-by: Elchin Valiyev <elchin.valiyev@autodesk.com>
    Co-authored-by: Nedal Horany <nedal.horany@autodesk.com>
    Co-authored-by: Roland Ruiters-Christou <roland.ruiters@autodesk.com>
    Co-authored-by: Nedal Horany <nedalhy@gmail.com>
    Co-authored-by: Elchin Valiyev <valiyev.elchin@gmail.com>
    Co-authored-by: Marcus Karlbowski <43415869+karlbom@users.noreply.github.com>
    Co-authored-by: Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>

---
+++ experimental/PropertyDDS/examples/property-inspector/tests/property_inspector.test.ts
@@ -0,0 +1,28 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { globals } from "../jest.config";
+
+// Tests disabled -- requires Tinylicious to be running, which our test environment doesn't do.
+describe("diceRoller", () => {
+    beforeAll(async () => {
+        // Wait for the page to load first before running any tests
+        // so this time isn't attributed to the first test
+        await page.goto(globals.PATH, { waitUntil: "load", timeout: 0 });
+    }, 45000);
+
+    beforeEach(async () => {
+        await page.goto(globals.PATH, { waitUntil: "load" });
+        await page.waitFor(() => window["fluidStarted"]);
+    });
+
+    it("Inspector at root1 is rendered", async () => {
+        expect(await page.$eval('#root1', el => el.children.length)).toEqual(1);
+    });
+
+    it("Inspector at root2 is rendered", async () => {
+        expect(await page.$eval('#root2', el => el.children.length)).toEqual(1);
+    });
+});
---
+++ experimental/PropertyDDS/examples/property-inspector/webpack.config.js
@@ -0,0 +1,46 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+
+module.exports = env => {
+    const htmlTemplate = "./src/index.html";
+    return {
+        devtool: "inline-source-map",
+        entry: "./src/app.tsx",
+        mode: "development",
+        devServer: {
+            port: 9000
+        },
+        module: {
+            rules: [{
+                    test: /\.s?css$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader"
+                },
+                {
+                    test: /\.js$/,
+                    use: ["source-map-loader"],
+                }
+            ]
+        },
+        output: {
+            filename: "[name].[contenthash].js",
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: htmlTemplate
+            }),
+            new Dotenv()
+        ],
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"]
+        },
+    }
+}
