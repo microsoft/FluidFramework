a5b2a28bb492244ae60606e820c05c8f0ca33dcb
Jatin Garg <jatgarg@microsoft.com>
2021-04-28 14:04:59 -0700

    bundle size testr

--- examples/utils/bundle-size-tests/src/odspDriver.ts
+++ examples/utils/bundle-size-tests/src/odspDriver.ts
@@ -2,9 +2,11 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-import { OdspDocumentServiceFactory } from "@fluidframework/odsp-driver";
+import { OdspDocumentServiceFactory, prefetchLatestSnapshot } from "@fluidframework/odsp-driver";
 
 export function apisToBundle() {
      // Pass through dummy parameters, this file is only used for bundle analysis
     new OdspDocumentServiceFactory(undefined as any, undefined as any);
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    prefetchLatestSnapshot(undefined as any, undefined as any, undefined as any, undefined as any, undefined);
 }
