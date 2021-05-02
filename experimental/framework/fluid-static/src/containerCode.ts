d16db4668ec578b6874ca2b84ca708786076cb14
Himanshu <addyjeridiq@gmail.com>
2021-05-02 20:18:22 +0530

    Removed getDataObject function
    removed <code>getDataObject</code> function
    solves #5849

--- experimental/framework/fluid-static/src/containerCode.ts
+++ experimental/framework/fluid-static/src/containerCode.ts
@@ -142,11 +142,6 @@ export class RootDataObject
         throw new Error("Could not create new Fluid object because an unknown object was passed");
     }
 
-    public async getDataObject<T extends IFluidLoadable>(id: string) {
-        const handle = await this.root.wait<IFluidHandle<T>>(id);
-        return handle.get();
-    }
-
     private async createDataObject<T extends IFluidLoadable>(dataObjectClass: DataObjectClass<T>): Promise<T> {
         const factory = dataObjectClass.factory;
         const packagePath = [...this.context.packagePath, factory.type];
