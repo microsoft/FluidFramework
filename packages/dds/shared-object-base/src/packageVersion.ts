6d22be1708a1ec3f872e31bfcd243db23c63b79b
Helio Liu <59622401+heliocliu@users.noreply.github.com>
2021-05-03 11:57:32 -0700

    [bump] package version for client (patch) (#5978)
    @fluidframework/build-common:     0.22.1 (unchanged)
         @fluidframework/eslint-config-fluid:     0.24.0 (unchanged)
          @fluidframework/common-definitions:     0.20.1 (unchanged)
                @fluidframework/common-utils:     0.30.1 (unchanged)
                                      Server:   0.1024.1 (unchanged)
                                      Client:     0.39.0 -> 0.39.1
                             generator-fluid:      0.3.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
                                 dice-roller:      0.0.1 (unchanged)

--- packages/dds/shared-object-base/src/packageVersion.ts
+++ packages/dds/shared-object-base/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/shared-object-base";
-export const pkgVersion = "0.39.0";
+export const pkgVersion = "0.39.1";
--- packages/drivers/fluidapp-odsp-urlResolver/src/packageVersion.ts
+++ packages/drivers/fluidapp-odsp-urlResolver/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-internal/fluidapp-odsp-urlresolver";
-export const pkgVersion = "0.39.0";
+export const pkgVersion = "0.39.1";
