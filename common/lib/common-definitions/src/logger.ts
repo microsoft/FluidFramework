49b7a5550f9d3264acae2c15a1918b6040160be7
ksbrar <kabir@brar.xyz>
2021-05-04 17:48:44 -0400

    Small changes.

--- common/lib/common-definitions/src/logger.ts
+++ common/lib/common-definitions/src/logger.ts
@@ -18,7 +18,7 @@ export type TelemetryEventPropertyType = string | number | boolean | undefined;
  */
 export interface ITaggedTelemetryPropertyType {
     value: TelemetryEventPropertyType,
-    tag: string
+    tag: string,
 }
 export interface ITelemetryProperties {
     [index: string]: TelemetryEventPropertyType | ITaggedTelemetryPropertyType;
@@ -41,10 +41,10 @@ export interface ITelemetryBaseEvent extends ITelemetryProperties {
  */
 export interface ITelemetryBaseLogger {
     /**
-     * An optional boolean which indicates to the logger that tags (i.e. `ITaggedTelemetryPropertyType` objects) are
-     * in use. Eventually this will be a required property, but this is a stopgap that allows older hosts to
-     * continue to pass through telemetry without trouble (this property will simply show up undefined), while
-     * our current logger impl in `telmetry-utils` handles tags in a separate manner.
+     * An optional boolean which indicates to the user of this interface that tags (i.e. `ITaggedTelemetryPropertyType`
+     * objects) are in use. Eventually this will be a required property, but this is a stopgap that allows older hosts
+     * to continue to pass through telemetry without trouble (this property will simply show up undefined), while our
+     * current logger implementation in `telmetry-utils` handles tags in a separate manner.
      */
     supportsTags?: true;
     send(event: ITelemetryBaseEvent): void;
