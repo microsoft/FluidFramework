02b5c55ea2a8b9ea05d3c2f6972551c8fa4d7a25
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-05-06 18:13:17 +0000

    Nit: Correct camelization of 'resubmit'

--- packages/runtime/datastore-definitions/src/channel.ts
+++ packages/runtime/datastore-definitions/src/channel.ts
@@ -70,7 +70,7 @@ export interface IDeltaHandler {
      * @param message - The original message that was submitted.
      * @param localOpMetadata - The local metadata associated with the original message.
      */
-    reSubmit(message: any, localOpMetadata: unknown): void;
+    resubmit(message: any, localOpMetadata: unknown): void;
 
     applyStashedOp(message: any): unknown;
 }
