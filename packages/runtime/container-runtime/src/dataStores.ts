913b9425af118f30d9f83742293cdfa8f655c768
Navin Agarwal <45832642+agarwal-navin@users.noreply.github.com>
2021-05-10 12:37:27 -0700

    Fix for GC test mode assert not taking local storage flag into account (#6072)

--- packages/runtime/container-runtime/src/dataStores.ts
+++ packages/runtime/container-runtime/src/dataStores.ts
@@ -43,7 +43,7 @@ import { v4 as uuid } from "uuid";
 import { TreeTreeEntry } from "@fluidframework/protocol-base";
 import { GCDataBuilder, getChildNodesUsedRoutes } from "@fluidframework/garbage-collector";
 import { DataStoreContexts } from "./dataStoreContexts";
-import { ContainerRuntime, IContainerRuntimeOptions } from "./containerRuntime";
+import { ContainerRuntime } from "./containerRuntime";
 import {
     FluidDataStoreContext,
     RemotedFluidDataStoreContext,
@@ -81,7 +81,6 @@ export class DataStores implements IDisposable {
         private readonly getCreateChildSummarizerNodeFn:
             (id: string, createParam: CreateChildSummarizerNodeParam)  => CreateChildSummarizerNodeFn,
         baseLogger: ITelemetryBaseLogger,
-        private readonly runtimeOptions: IContainerRuntimeOptions,
         private readonly contexts: DataStoreContexts = new DataStoreContexts(baseLogger),
     ) {
         this.logger = ChildLogger.create(baseLogger);
@@ -508,10 +507,7 @@ export class DataStores implements IDisposable {
      * @param unusedRoutes - The routes that are unused in all data stores in this Container.
      */
     public deleteUnusedRoutes(unusedRoutes: string[]) {
-        assert(
-            this.runtimeOptions.gcOptions?.runGCInTestMode,
-            0x1df /* "Data stores should be deleted only in GC test mode" */,
-        );
+        assert(this.runtime.gcTestMode, 0x1df /* "Data stores should be deleted only in GC test mode" */);
         for (const route of unusedRoutes) {
             // Delete the contexts of unused data stores.
             const dataStoreId = route.split("/")[1];
