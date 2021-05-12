821e8911501ee5d26683ff97be9616650dca0a59
Vlad Sudzilouski <vlad@sudzilouski.com>
2021-05-12 15:54:09 -0700

    Bring back redemption code that was accidently wiped out as part of refactoring

--- packages/drivers/odsp-driver/src/epochTracker.ts
+++ packages/drivers/odsp-driver/src/epochTracker.ts
@@ -402,7 +402,7 @@ export function createOdspCacheAndTracker(
     fileEntry: IFileEntry,
     logger: ITelemetryLogger): ICacheAndTracker
 {
-    const epochTracker = new EpochTracker(persistedCacheArg, fileEntry, logger);
+    const epochTracker = new EpochTrackerWithRedemption(persistedCacheArg, fileEntry, logger);
     return {
         cache: {
             ...nonpersistentCache,
