efb65d6330a33403b990e80c9bed9af71274c694
Micah Godbolt <mgodbolt@microsoft.com>
2021-05-13 10:23:35 -0700

    Update utils.ts

--- examples/apps/likes-and-comments/src/utils.ts
+++ examples/apps/likes-and-comments/src/utils.ts
@@ -9,5 +9,5 @@ export function getAuthorName(syncedDataObject: SyncedDataObject) {
     const quorum = syncedDataObject.dataProps.runtime.getQuorum();
     const clientId = syncedDataObject.dataProps.runtime.clientId ?? "";
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return (quorum.getMember(clientId)?.client.user).name ?? "";
+    return (quorum.getMember(clientId)?.client.user as any).name ?? "";
 }
