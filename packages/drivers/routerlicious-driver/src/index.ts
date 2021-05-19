e3aea3a0591fa18f3c6a439bd6be8f22f9c8670f
Zach Newton <znewton@microsoft.com>
2021-05-19 11:49:02 -0700

    Remove superfluous R11s Driver factory params and functionality

--- packages/drivers/routerlicious-driver/src/index.ts
+++ packages/drivers/routerlicious-driver/src/index.ts
@@ -7,7 +7,6 @@ export * from "./deltaStorageService";
 export * from "./documentService";
 export * from "./documentStorageService";
 export * from "./tokens";
-export * from "./documentService2";
 export * from "./nullBlobStorageService";
 export * from "./documentServiceFactory";
 export * from "./defaultTokenProvider";
