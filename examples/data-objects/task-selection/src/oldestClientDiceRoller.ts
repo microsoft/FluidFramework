1c2d5519c2759f233095e1ca041210e325b444ea
Matt Rakow <marakow@microsoft.com>
2021-05-20 15:33:13 -0700

    Initial oldest client demo

---
+++ examples/data-objects/task-selection/src/oldestClientDiceRoller.ts
@@ -0,0 +1,112 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { OldestClientObserver } from "@fluid-experimental/task-manager";
+import { DataObject, DataObjectFactory } from "@fluidframework/aqueduct";
+import { IEvent } from "@fluidframework/common-definitions";
+import { assert } from "@fluidframework/common-utils";
+
+import { IDiceRoller } from "./interface";
+
+// The root is map-like, so we'll use this key for storing the value.
+const diceValueKey = "diceValue";
+
+/**
+ * The DiceRoller is our data object that implements the IDiceRoller interface.
+ */
+export class OldestClientDiceRoller extends DataObject implements IDiceRoller {
+    public static get ComponentName() { return "@fluid-example/task-manager-dice-roller"; }
+
+    private _oldestClientObserver: OldestClientObserver | undefined;
+    private autoRollInterval: ReturnType<typeof setInterval> | undefined;
+
+    /**
+     * initializingFirstTime is run only once by the first client to create the DataObject.  Here we use it to
+     * initialize the state of the DataObject.
+     */
+    protected async initializingFirstTime() {
+        this.root.set(diceValueKey, 1);
+    }
+
+    /**
+     * hasInitialized is run by each client as they load the DataObject.  Here we use it to set up usage of the
+     * DataObject, by registering an event listener for dice rolls.
+     */
+    protected async hasInitialized() {
+        this.root.on("valueChanged", (changed) => {
+            if (changed.key === diceValueKey) {
+                // When we see the dice value change, we'll emit the diceRolled event we specified in our interface.
+                this.emit("diceRolled");
+            }
+        });
+
+        this._oldestClientObserver = new OldestClientObserver(this.runtime);
+        // Set up oldest client observer here?
+
+        this.volunteerForAutoRoll();
+    }
+
+    private get oldestClientObserver() {
+        assert(this._oldestClientObserver !== undefined, "OldestClientObserver not initialized");
+        return this._oldestClientObserver;
+    }
+
+    public get value() {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return this.root.get(diceValueKey);
+    }
+
+    public readonly roll = () => {
+        const rollValue = Math.floor(Math.random() * 6) + 1;
+        this.root.set(diceValueKey, rollValue);
+    };
+
+    public volunteerForAutoRoll() {
+        if (this.oldestClientObserver.isOldest()) {
+            this.oldestClientObserver.once("lostOldest", () => {
+                this.emit("taskOwnershipChanged");
+                this.endAutoRollTask();
+                this.volunteerForAutoRoll();
+            });
+            this.emit("taskOwnershipChanged");
+            this.startAutoRollTask();
+        } else {
+            this.oldestClientObserver.once("gotOldest", () => {
+                this.volunteerForAutoRoll();
+            });
+        }
+    }
+
+    private startAutoRollTask() {
+        if (this.autoRollInterval === undefined) {
+            this.autoRollInterval = setInterval(() => {
+                this.roll();
+            }, 1000);
+        }
+    }
+
+    private endAutoRollTask() {
+        if (this.autoRollInterval !== undefined) {
+            clearInterval(this.autoRollInterval);
+            this.autoRollInterval = undefined;
+        }
+    }
+
+    public hasTask() {
+        return this.oldestClientObserver.isOldest();
+    }
+}
+
+/**
+ * The DataObjectFactory is used by Fluid Framework to instantiate our DataObject.  We provide it with a unique name
+ * and the constructor it will call.  In this scenario, the third and fourth arguments are not used.
+ */
+export const DiceRollerInstantiationFactory = new DataObjectFactory<OldestClientDiceRoller, undefined, undefined, IEvent>
+(
+    OldestClientDiceRoller.ComponentName,
+    OldestClientDiceRoller,
+    [],
+    {},
+);
