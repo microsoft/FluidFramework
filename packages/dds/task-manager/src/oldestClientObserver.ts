138a55c3ab8a26f9870b359a9776426b029dcddf
Matt Rakow <marakow@microsoft.com>
2021-05-21 11:22:56 -0700

    Move interfaces to interfaces.ts

--- packages/dds/task-manager/src/oldestClientObserver.ts
+++ packages/dds/task-manager/src/oldestClientObserver.ts
@@ -3,37 +3,10 @@
  * Licensed under the MIT License.
  */
 
-import { IEvent, IEventProvider } from "@fluidframework/common-definitions";
 import { assert, TypedEventEmitter } from "@fluidframework/common-utils";
 import { AttachState } from "@fluidframework/container-definitions";
 import { IQuorum } from "@fluidframework/protocol-definitions";
-import { IOldestClientObserver } from "./interfaces";
-
-export interface IOldestClientObservableEvents extends IEvent {
-    (event: "connected", listener: () => void);
-    // Typescript won't convert IFluidDataStoreRuntime and ContainerRuntime if we unify these,
-    // I believe this is because the "connected" event has a clientId arg in the runtimes.
-    // eslint-disable-next-line @typescript-eslint/unified-signatures
-    (event: "disconnected", listener: () => void);
-}
-
-/**
- * This is to make OldestClientObserver work with either a ContainerRuntime or an IFluidDataStoreRuntime
- * (both expose the relevant API surface and eventing).  However, really this info probably shouldn't live on either,
- * since neither is really the source of truth (they are just the only currently-available plumbing options).
- * It's information about the connection, so the real source of truth is lower (at the connection layer).
- */
-export interface IOldestClientObservable extends IEventProvider<IOldestClientObservableEvents> {
-    getQuorum(): IQuorum;
-    // Generic usage of attachState is a little unusual here.  We will treat ourselves as "the oldest client that
-    // has information about this [container | data store]", which in the case of detached data store may disagree
-    // with whether we're the oldest client on the connected container.  So in the data store case, it's only
-    // safe use this as an indicator about rights to tasks performed against this specific data store, and not
-    // more broadly.
-    attachState: AttachState;
-    connected: boolean;
-    clientId: string | undefined;
-}
+import { IOldestClientObservable, IOldestClientObservableEvents, IOldestClientObserver } from "./interfaces";
 
 /**
  * The `OldestClientObserver` is a utility inspect if the local client is the oldest amongst connected clients (in
