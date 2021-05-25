9d2dda8a56ab45f5a97da28b3a3efa9a9fc40d7b
Tyler Butler <tylerbu@microsoft.com>
2021-05-25 12:04:48 -0700

    policy-check

--- docs/.markdownlint-cli2.js
+++ docs/.markdownlint-cli2.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 module.exports = {
     "config": {
         "first-line-heading": { // MD041
