63ba5ac812af5b631279f81b064c437c4a670ece
Tyler Butler <tylerbu@microsoft.com>
2021-05-25 16:35:03 -0700

    rule config adjustments

--- docs/.markdownlint-cli2.js
+++ docs/.markdownlint-cli2.js
@@ -9,25 +9,35 @@ module.exports = {
         "markdownlint-rule-github-internal-links",
     ],
     "config": {
-        "emphasis-style": {
-            style: "*"
+        "code-block-style": { // MD046
+            "style": "fenced"
+        },
+        "code-fence-style": { // MD048
+            "style": "",
+        },
+        "emphasis-style": { // custom
+            style: "*",
         },
         "first-line-heading": { // MD041
             "level": 2,
         },
-        "github-internal-links": {
-            "verbose": false
-          },
-        "line_length": { // MD013
+        "github-internal-links": { // custom
+            "verbose": false,
+        },
+        "heading-style": { // MD003
+            "style": "atx",
+        },
+        "line-length": { // MD013
             "code_blocks": true,
             "line_length": 120,
             "tables": false,
         },
+        "no-empty-links": true, // MD042
         "no-inline-html": false, //MD033
         "no-multiple-blanks": { // MD012
             "maximum": 2,
         },
-        "proper-names": {
+        "proper-names": { // MD044
             "code_blocks": false,
             "names": [
                 "Fluid Framework",
