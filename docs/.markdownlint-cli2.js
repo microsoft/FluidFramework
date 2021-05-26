523ea072649e61a0d31bee66b3da2b495e1f8ce8
Tyler Butler <tylerbu@microsoft.com>
2021-05-25 17:04:17 -0700

    typography rule

--- docs/.markdownlint-cli2.js
+++ docs/.markdownlint-cli2.js
@@ -3,10 +3,53 @@
  * Licensed under the MIT License.
  */
 
+const { forEachLine, getLineMetadata } = require("markdownlint-rule-helpers");
+
+const excludedTypography = [
+    ["’", "'"],
+    ["“", "\""],
+    ["”", "\""],
+];
+
+const clamp = (number, min, max) => {
+    return Math.max(min, Math.min(number, max));
+};
+
+const extractContext = (line, column) => {
+    const contextPadding = 10;
+    return line.substr(clamp(column - contextPadding, 0, line.length - 1), contextPadding * 2);
+}
+
 module.exports = {
     "customRules": [
         "markdownlint-rule-emphasis-style",
         "markdownlint-rule-github-internal-links",
+        {
+            "names": ["proper-typography"],
+            "description": "Using improper typography",
+            "tags": ["style"],
+            "function": (params, onError) => {
+                forEachLine(getLineMetadata(params), (line, lineIndex) => {
+                    for (const [character, replacement] of excludedTypography) {
+                        const column = line.indexOf(character);
+                        if (column >= 0) {
+                            onError({
+                                "lineNumber": lineIndex + 1,
+                                "detail": `Found invalid character "${character}" at column ${column}`,
+                                "context": extractContext(line, column),
+                                "range": [column + 1, 1],
+                                "fixInfo": {
+                                    "lineNumber": lineIndex + 1,
+                                    "editColumn": column + 1,
+                                    "deleteCount": 1,
+                                    "insertText": replacement,
+                                },
+                            });
+                        }
+                    }
+                });
+            }
+        },
     ],
     "config": {
         "code-block-style": { // MD046
