2b0c76ee43a62e36355cc30172f0e427b2f3aa19
Andrei Iacob <andreiiacob@microsoft.com>
2021-05-26 00:31:38 -0700

    Add delay function in common-utils

---
+++ common/lib/common-utils/src/delay.ts
@@ -0,0 +1,10 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * Returns a promise that resolves after timeMs
+ */
+export const delay = async (timeMs: number): Promise<void> =>
+    new Promise((resolve) => setTimeout(() => resolve(), timeMs));
