a68f5eb1e6e9bfecdf44a9e92d8de6e90b64c0f8
Craig Macomber <Craig.Macomber@microsoft.com>
2021-05-27 14:58:39 -0700

    document exampleApp

--- experimental/dds/tree/src/interface-prototype/Examples.ts
+++ experimental/dds/tree/src/interface-prototype/Examples.ts
@@ -77,25 +77,46 @@ const baz: Definition = 'Bar' as Definition;
 
 const testTrait: TraitLabel = 'testTrait' as TraitLabel;
 
+/**
+ * Collaborative applications generally do two things:
+ * 1. They present some view based on reading the shared document. This includes responding to changes to the document.
+ * 2. Allow changes to be made to the document.
+ *
+ * This example is a minimal applications that does both of these.
+ * Rather than letting a user trigger changes, it just makes changes in response to incoming edits (this made for a smaller example, but is not very realistic).
+ * It shows off how an application can view the tree using Anchors then use those same anchors to make edits using commands.
+ */
 export async function exampleApp(tree: SharedTree): Promise<void> {
 	// Perform a full checkout of all data.
 	const checkout = await tree.checkout(commands, fetchAll);
 
 	// Example app policy: this app just watches the tree, and adds bar(baz) subtree under any new `foo` node.
+	// Normally an App would just hook ViewChange up to its invalidation system, but this example doesn't even have a view to invalidate, so we just put everything in here.
 	// TODO: Make TypedEventEmitter use strong types.
 	checkout.on(CheckoutEvent.ViewChange, (before: Snapshot, after: Snapshot) => {
 		const delta = before.delta(after);
+		// Note that this only actually looks at the root of newly inserted roots.
+		// Thus wouldn't be a good way to enforce any actual invariants about `foo` nodes,
+		// its just a contrived example of responded to changes, observing trees and performing edits.
 		for (const added of delta.added) {
 			const n: TreeNodeViewReadonly = checkout.contextualizeAnchor(anchorDataFromNodeId(added));
+			// Here we have an example of using the tree viewing APIs.
+			// In this trivial case the only thing we do with them is check the definition, but real apps would use these APIs to walk the tree and build the document view for the user.
 			if (n.definition === foo) {
+				// This shows how the application can perform an edit.
+				// Typically this would be done in response to some user action, but this example doesn't have any user interface, so we just perform it in response to an edit instead.
 				checkout.runCommand(doubleInsertExample, {}, { parent: n as TreeNodeData });
 			}
 		}
 	});
 
 	const treeRoot: TreeNodeViewReadonly = checkout.contextualizeAnchor(root);
+	// Here this example 'app' just prints the tree, demonstrating getting a simple json compatible view of the tree.
+	// A more realistic application would build a real view of the document here, which would be invalided using ViewChange.
 	console.log(treeRoot.queryJsonSnapshot.subtree);
+	// The app then waits a while (while maybe some other collaborator will perform edits triggering the logic above).
 	await wait(10000);
+	// Finally it prints the final tree.
 	console.log(treeRoot.queryJsonSnapshot.subtree);
 }
 
