14694264960c2cb697735b6e969162a789f2a624
Andrei Iacob <andreiiacob@microsoft.com>
2021-05-26 21:23:25 -0700

    Add param doc to function comment

--- common/lib/common-utils/src/delay.ts
+++ common/lib/common-utils/src/delay.ts
@@ -5,6 +5,7 @@
 
 /**
  * Returns a promise that resolves after timeMs
+ * @param timeMs - time in milliseconds to wait
  */
 export const delay = async (timeMs: number): Promise<void> =>
     new Promise((resolve) => setTimeout(() => resolve(), timeMs));
