6f1e4dc293db59c7df07356b9ea003b29a326c24
chensixx <34214774+chensixx@users.noreply.github.com>
2021-06-07 14:33:46 -0700

    [bump] package version for client (patch) (#6365)
    @fluidframework/build-common:     0.23.0 (unchanged)
         @fluidframework/eslint-config-fluid:     0.24.0 (unchanged)
          @fluidframework/common-definitions:     0.21.0 (unchanged)
                @fluidframework/common-utils:     0.31.0 (unchanged)
       @fluidframework/container-definitions:     0.40.0 (unchanged)
             @fluidframework/core-interfaces:     0.40.0 (unchanged)
          @fluidframework/driver-definitions:     0.40.0 (unchanged)
        @fluidframework/protocol-definitions:   0.1025.0 (unchanged)
                                      Server:   0.1025.2 (unchanged)
                                      Client:     0.40.2 -> 0.40.3
                             generator-fluid:      0.3.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
                                 dice-roller:      0.0.1 (unchanged)

--- packages/test/functional-tests/src/packageVersion.ts
+++ packages/test/functional-tests/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-internal/functional-tests";
-export const pkgVersion = "0.40.2";
+export const pkgVersion = "0.40.3";
--- packages/test/test-utils/src/packageVersion.ts
+++ packages/test/test-utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/test-utils";
-export const pkgVersion = "0.40.2";
+export const pkgVersion = "0.40.3";
