97d2066d391119948e0dd5f919f4c4b11cc3e5c9
Navin Agarwal <naagar@microsoft.com>
2021-06-07 17:39:39 -0700

    GC tests to validate unreferenced flag in snapshot returned by server

--- packages/test/test-end-to-end-tests/src/test/gcTests/gcReferenceUpdatesInLocalSummary.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/gcTests/gcReferenceUpdatesInLocalSummary.spec.ts
@@ -21,7 +21,7 @@ import { SharedString } from "@fluidframework/sequence";
 import { ITestObjectProvider } from "@fluidframework/test-utils";
 import { describeNoCompat } from "@fluidframework/test-version-utils";
 import { UndoRedoStackManager } from "@fluidframework/undo-redo";
-import { flattenRuntimeOptions } from "./flattenRuntimeOptions";
+import { flattenRuntimeOptions } from "../flattenRuntimeOptions";
 
 class TestDataObject extends DataObject {
     public get _root() {
