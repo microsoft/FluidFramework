9b3c00a69e7b19e70021fb79859b0b9f513a036f
Tony Murphy <anthonm@microsoft.com>
2021-06-08 14:20:45 -0700

    add copyright

--- packages/dds/merge-tree/src/test/client.replay.spec.ts
+++ packages/dds/merge-tree/src/test/client.replay.spec.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import * as fs from "fs";
 import assert from "assert";
 import { ISequencedDocumentMessage } from "@fluidframework/protocol-definitions";
