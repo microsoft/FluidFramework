e5f48cfa7bddf89a25e5d1bf8937b206eb70f403
Arin <artaylor@microsoft.com>
2021-06-09 13:22:12 -0400

    Use unique symbol type as placeholder instead of actual runtime const

--- packages/runtime/runtime-definitions/src/events.ts
+++ packages/runtime/runtime-definitions/src/events.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-export const eventThis = Symbol("EventThis");
+export declare const eventThis: unique symbol;
 export type EventThis = typeof eventThis;
 
 /**
