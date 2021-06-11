9a30d7562d333591f212dc11d37f1bdaa91e148b
Si Chen <chensi@microsoft.com>
2021-06-11 13:54:33 -0700

    fix

--- common/lib/common-utils/src/packageVersion.ts
+++ common/lib/common-utils/src/packageVersion.ts
@@ -1,4 +1,9 @@
 /*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/*!
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  *
