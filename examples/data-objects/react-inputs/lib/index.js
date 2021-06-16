b3e8807e2c9710618931e120da1794f385206f30
Tim Wang <wangtim@microsoft.com>
2021-06-16 02:10:15 -0500

    removed irrelevent files that were added

--- examples/data-objects/react-inputs/lib/index.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-export * from "./CollaborativeCheckbox";
-export * from "./CollaborativeInput";
-export * from "./CollaborativeTextArea";
-//# sourceMappingURL=index.js.map
