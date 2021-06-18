21d4a4b21bb6b1d548e007cc66501902612f1ad5
Mark Fields <markfields@users.noreply.github.com>
2021-06-17 22:15:41 -0700

    fix build break

--- packages/loader/container-loader/src/deltaManager.ts
+++ packages/loader/container-loader/src/deltaManager.ts
@@ -13,7 +13,6 @@ import {
     IDeltaManagerEvents,
     IDeltaQueue,
     ICriticalContainerError,
-    ContainerErrorType,
     IThrottlingWarning,
     ReadOnlyInfo,
 } from "@fluidframework/container-definitions";
