2c7702f2749d6c260ae01ccf8a832980bbcb36bd
hedasilv <hedasilv@microsoft.com>
2021-06-22 12:38:06 -0700

    Updating current list of lumber EventNames

--- server/routerlicious/packages/services-telemetry/src/lumberEventNames.ts
+++ server/routerlicious/packages/services-telemetry/src/lumberEventNames.ts
@@ -6,9 +6,6 @@
 // List of event names that should identify Lumber events throughout the code.
 // Values in the enum must be strings.
 export enum LumberEventName {
-    // Alfred
-    AlfredSocketConnectDocument = "AlfredSocketConnectDocument",
-
     // Unit Testing
     UnitTestEvent = "UnitTestEvent",
 }
