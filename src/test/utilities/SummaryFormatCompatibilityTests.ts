5f51943b0b7add1c7691bd46dd354592fbf73875
C. Clauson <coclauso@microsoft.com>
2021-07-02 18:04:35 -0700

    (split from Whiteboard) Fix formatting

--- src/test/utilities/SummaryFormatCompatibilityTests.ts
+++ src/test/utilities/SummaryFormatCompatibilityTests.ts
@@ -9,7 +9,14 @@ import { assert, expect } from 'chai';
 import { TestObjectProvider } from '@fluidframework/test-utils';
 import { Change, SharedTree } from '../../default-edits';
 import { EditId } from '../../Identifiers';
-import { Edit, fullHistorySummarizer, fullHistorySummarizer_0_1_0, SharedTreeSummarizer, SharedTreeSummary, SharedTreeSummaryBase } from '../../generic';
+import {
+	Edit,
+	fullHistorySummarizer,
+	fullHistorySummarizer_0_1_0,
+	SharedTreeSummarizer,
+	SharedTreeSummary,
+	SharedTreeSummaryBase,
+} from '../../generic';
 import { deserialize, getSummaryStatistics, SummaryStatistics } from '../../SummaryBackCompatibility';
 import { SharedTreeWithAnchors } from '../../anchored-edits';
 import {
@@ -219,23 +226,27 @@ export function runSummaryFormatCompatibilityTests<TSharedTree extends SharedTre
 				}
 
 				// In the special case in which a SharedTree loads a summary with handles (which would necessarily
-				// imply that the summary was version >= 0.1.0), then a 0.1.0 summary is written even if the 
+				// imply that the summary was version >= 0.1.0), then a 0.1.0 summary is written even if the
 				// summarizer is 0.0.2
-				if (includesHandles(deserialize(summaryFileContents['0.1.0'], testSerializer) as SharedTreeSummary<Change>)) {
+				if (
+					includesHandles(
+						deserialize(summaryFileContents['0.1.0'], testSerializer) as SharedTreeSummary<Change>
+					)
+				) {
 					it('since loaded summary includes handles, 0.1.0 is written by a client with a 0.0.2 summarizer', async () => {
 						const serializedSummary = summaryFileContents['0.1.0'];
 						const summary = deserialize(serializedSummary, testSerializer);
-	
+
 						// Wait for the ops to to be submitted and processed across the containers.
 						await testObjectProvider.ensureSynchronized();
 						expectedTree.loadSummary(summary);
-	
+
 						await testObjectProvider.ensureSynchronized();
-	
+
 						// Write a new summary with the 0.0.2 summarizer
 						const newSummary = JSON.parse(expectedTree.saveSerializedSummary(fullHistorySummarizer));
 						const expectedSummary = JSON.parse(serializedSummary);
-	
+
 						expect(newSummary).to.deep.equal(expectedSummary);
 					});
 				}
