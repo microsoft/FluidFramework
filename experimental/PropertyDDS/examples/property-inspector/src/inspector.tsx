093563b741ea07c91a5aeedcaaa8f45d4ef9a7db
Nedal Horany <nedalhy@gmail.com>
2021-07-06 13:13:03 +0200

    Using double quotes

--- experimental/PropertyDDS/examples/property-inspector/src/inspector.tsx
+++ experimental/PropertyDDS/examples/property-inspector/src/inspector.tsx
@@ -62,8 +62,8 @@ const useStyles = makeStyles({
     },
     tableContainer: {
         display: "flex",
-        height: '100%',
-        width: '100%',
+        height: "100%",
+        width: "100%",
     },
 }, { name: "InspectorApp" });
 
