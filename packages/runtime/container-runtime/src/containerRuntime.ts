c81037c061d0163272baa1cb7799301c618ece18
Arin Taylor <artaylor@microsoft.com>
2021-07-08 14:31:10 -0700

    Add some error hex codes (#6665)
    * Add some hex codes
    
    * Fix lint error for line length

--- packages/runtime/container-runtime/src/containerRuntime.ts
+++ packages/runtime/container-runtime/src/containerRuntime.ts
@@ -1743,7 +1743,7 @@ export class ContainerRuntime extends TypedEventEmitter<IContainerRuntimeEvents>
             const dataStoreTree = this.disableIsolatedChannels ? summarizeResult.summary :
                 summarizeResult.summary.tree[channelsTreeName];
 
-            assert(dataStoreTree.type === SummaryType.Tree, "summary is not a tree");
+            assert(dataStoreTree.type === SummaryType.Tree, 0x1fc /* "summary is not a tree" */);
             const handleCount = Object.values(dataStoreTree.tree).filter(
                 (value) => value.type === SummaryType.Handle).length;
 
@@ -2118,7 +2118,8 @@ export class ContainerRuntime extends TypedEventEmitter<IContainerRuntimeEvents>
         // Update the summaryGCVersion if GC is enabled and the latest summary tracked by this container was updated.
         if (this.gcEnabled && result.latestSummaryUpdated) {
             // Since there is not proposal handle for this summary, it should not have been tracked.
-            assert(!result.wasSummaryTracked, "Summary without proposal handle should not have been tracked");
+            assert(!result.wasSummaryTracked,
+                0x1fd /* "Summary without proposal handle should not have been tracked" */);
             // Update summaryGCVersion from the snapshot that was used to update the latest summary.
             await this.updateSummaryGCVersionFromSnapshot(result.snapshot);
         }
--- packages/runtime/container-runtime/src/dataStoreContext.ts
+++ packages/runtime/container-runtime/src/dataStoreContext.ts
@@ -724,7 +724,8 @@ export class RemotedFluidDataStoreContext extends FluidDataStoreContext {
 
             if (hasIsolatedChannels(attributes)) {
                 tree = tree.trees[channelsTreeName];
-                assert(tree !== undefined, "isolated channels subtree should exist in remote datastore snapshot");
+                assert(tree !== undefined,
+                    0x1fe /* "isolated channels subtree should exist in remote datastore snapshot" */);
             }
         }
 
@@ -862,7 +863,8 @@ export class LocalFluidDataStoreContextBase extends FluidDataStoreContext {
 
             if (hasIsolatedChannels(attributes)) {
                 snapshot = snapshot.trees[channelsTreeName];
-                assert(snapshot !== undefined, "isolated channels subtree should exist in local datastore snapshot");
+                assert(snapshot !== undefined,
+                    0x1ff /* "isolated channels subtree should exist in local datastore snapshot" */);
             }
         }
 
