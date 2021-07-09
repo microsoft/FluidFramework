21f397c37369a2368e9c7bf331098973dba90d4a
Tim Wang <wangtim@microsoft.com>
2021-07-08 22:37:12 -0500

    added first test case for FrsClient

--- experimental/framework/frs-client/src/test/FrsClient.spec.ts
+++ experimental/framework/frs-client/src/test/FrsClient.spec.ts
@@ -3,4 +3,47 @@
  * Licensed under the MIT License.
  */
 
-// TODO: Add tests here that mock a connection to FRS
+import { strict as assert } from "assert";
+import { v4 as uuid } from "uuid";
+import { SharedMap, ContainerSchema } from "@fluid-experimental/fluid-framework";
+import { generateUser } from "@fluidframework/server-services-client";
+import {
+    FrsClient,
+    FrsConnectionConfig,
+    FrsContainerConfig,
+    InsecureTokenProvider,
+} from "..";
+
+describe("FrsClient", () => {
+    let frsClient: FrsClient;
+    let documentId: string;
+    const user = generateUser();
+    const connectionConfig: FrsConnectionConfig = {
+        tenantId: "",
+        tokenProvider: new InsecureTokenProvider("", user),
+        orderer: "",
+        storage: "",
+    };
+    beforeEach(() => {
+        documentId = uuid();
+        frsClient = new FrsClient(connectionConfig);
+    });
+
+    it("can create container successfully", async () => {
+        const containerConfig: FrsContainerConfig = { id: documentId };
+        const schema: ContainerSchema = {
+            name: documentId,
+            initialObjects: {
+                map1: SharedMap,
+            },
+        };
+
+        const containerAndServices = await frsClient.createContainer(containerConfig, schema);
+
+        await assert.doesNotReject(
+            containerAndServices,
+            () => true,
+            "container cannot be created",
+        );
+    });
+});
