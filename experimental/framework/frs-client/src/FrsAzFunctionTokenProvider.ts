9910af7dec635939f6fe8464aeab84d927f47fe3
Sonali Deshpande <sdeshpande@microsoft.com>
2021-07-09 22:17:17 -0700

    frs azure token generation function

---
+++ experimental/framework/frs-client/src/FrsAzFunctionTokenProvider.ts
@@ -0,0 +1,42 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { ITokenProvider, ITokenResponse } from "@fluidframework/routerlicious-driver";
+import axios from "axios";
+import { FrsAzFuncUser } from "./interfaces";
+
+export class FrsAzFunctionTokenProvider implements ITokenProvider {
+    constructor(
+        private readonly azFunctionUrl: string,
+        private readonly user?: FrsAzFuncUser,
+    ) { }
+
+    public async fetchOrdererToken(tenantId: string, documentId: string): Promise<ITokenResponse> {
+        return {
+            jwt: await this.getToken(tenantId, documentId),
+        };
+    }
+
+    public async fetchStorageToken(tenantId: string, documentId: string): Promise<ITokenResponse> {
+        return {
+            jwt: await this.getToken(tenantId, documentId),
+        };
+    }
+
+    private async getToken(tenantId, documentId: string): Promise<string> {
+        return axios.get(this.azFunctionUrl, {
+            params: {
+                tenantId,
+                documentId,
+                userId: this.user?.userId,
+                userName: this.user?.userName,
+            },
+        }).then((response) => {
+            return response.data as string;
+        }).catch((err) => {
+            return err as string;
+        });
+    }
+}
