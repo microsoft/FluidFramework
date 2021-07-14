9141719e6a3b98174febccfcb2b13c3483d3dbe9
Matt Rakow <marakow@microsoft.com>
2021-07-14 11:42:06 -0700

    Add example-utils and starter ContainerViewRuntimeFactory

---
+++ examples/utils/example-utils/README.md
@@ -0,0 +1,5 @@
+# @fluid-example/fluid-object-interfaces
+
+This package contains common interfaces used by Fluid examples. These interfaces can be extended by Fluid objects in the examples.
+
+See [GitHub](https://github.com/microsoft/FluidFramework) for more details on the Fluid Framework and packages within.
---
+++ examples/utils/example-utils/src/containerViewRuntimeFactory.ts
@@ -0,0 +1,59 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { BaseContainerRuntimeFactory, mountableViewRequestHandler } from "@fluidframework/aqueduct";
+import { IContainerRuntime } from "@fluidframework/container-runtime-definitions";
+import { RuntimeRequestHandler } from "@fluidframework/request-handler";
+import { IFluidDataStoreFactory } from "@fluidframework/runtime-definitions";
+import { requestFluidObject, RequestParser } from "@fluidframework/runtime-utils";
+import { MountableView } from "@fluidframework/view-adapters";
+
+// ----- FACTORY SETUP -----
+
+const componentId = "modelComponent";
+
+export type ViewCallback = (fluidModel: any) => any;
+
+const makeViewRequestHandler = (viewCallback: ViewCallback) => {
+    const viewRequestHandler: RuntimeRequestHandler =
+        async (request: RequestParser, runtime: IContainerRuntime) => {
+            if (request.pathParts.length === 0) {
+                const clickerRequest = RequestParser.create({
+                    url: ``,
+                    headers: request.headers,
+                });
+                // TODO type the requestFluidObject
+                const fluidObject = await requestFluidObject(
+                    await runtime.getRootDataStore(componentId),
+                    clickerRequest);
+                const viewResponse = viewCallback(fluidObject);
+                return { status: 200, mimeType: "fluid/view", value: viewResponse };
+            }
+        };
+    return viewRequestHandler;
+};
+
+export class ContainerViewRuntimeFactory extends BaseContainerRuntimeFactory {
+    constructor(
+        private readonly dataStoreFactory: IFluidDataStoreFactory,
+        viewCallback: ViewCallback,
+    ) {
+        // We'll use a MountableView so webpack-fluid-loader can display us,
+        // and add our default view request handler.
+        super(
+            new Map([[dataStoreFactory.type, Promise.resolve(dataStoreFactory)]]),
+            [],
+            [mountableViewRequestHandler(MountableView, [makeViewRequestHandler(viewCallback)])],
+        );
+    }
+
+    /**
+     * Since we're letting the container define the default view it will respond with, it must do whatever setup
+     * it requires to produce that default view.  We'll create a single data store of the specified type.
+     */
+    protected async containerInitializingFirstTime(runtime: IContainerRuntime) {
+        await runtime.createRootDataStore(this.dataStoreFactory.type, componentId);
+    }
+}
