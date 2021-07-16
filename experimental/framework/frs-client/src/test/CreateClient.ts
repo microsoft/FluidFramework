b3b1540d7bbe1a256eec2b7babcedce302cd840a
Tim Wang <wangtim@microsoft.com>
2021-07-16 15:15:29 -0500

    added copyright to CreateClient

--- experimental/framework/frs-client/src/test/CreateClient.ts
+++ experimental/framework/frs-client/src/test/CreateClient.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { generateUser } from "@fluidframework/server-services-client";
 import {
     FrsClient,
