74f42929a7cc1494e96328223bc58206c64320d6
Elchin Valiyev <elchin.valiyev@autodesk.com>
2021-07-19 21:57:21 +0200

    fix indentation (#6791)

--- experimental/PropertyDDS/packages/property-common/test/strings/join_paths.spec.js
+++ experimental/PropertyDDS/packages/property-common/test/strings/join_paths.spec.js
@@ -6,13 +6,11 @@
 /**
  * @fileoverview In this file,we will test property-common.Strings.joinPaths
  */
-(function() {
 
-  const joinPaths = require('../..').Strings.joinPaths;
+const joinPaths = require('../..').Strings.joinPaths;
 
-  describe('property-common.Strings.joinPaths', function() {
-    it('should exist', function() {
-      expect(joinPaths).to.exist;
+describe('property-common.Strings.joinPaths', function () {
+    it('should exist', function () {
+        expect(joinPaths).to.exist;
     });
-  });
-})();
+});
