d7c9b10c40118d68f173cd9eeb1a0403b0fe15c7
Jatin Garg <48029724+jatgarg@users.noreply.github.com>
2021-07-19 16:51:43 -0700

    [bump] package version for client (patch) (#6801)
    @fluidframework/build-common:     0.23.0 (unchanged)
         @fluidframework/eslint-config-fluid:     0.24.0 (unchanged)
          @fluidframework/common-definitions:     0.21.0 (unchanged)
                @fluidframework/common-utils:     0.32.0 (unchanged)
       @fluidframework/container-definitions:     0.40.0 (unchanged)
             @fluidframework/core-interfaces:     0.40.0 (unchanged)
          @fluidframework/driver-definitions:     0.40.0 (unchanged)
        @fluidframework/protocol-definitions:   0.1025.0 (unchanged)
                                      Server:   0.1028.0 (unchanged)
                                      Client:     0.43.1 -> 0.43.2
                      @fluid-tools/benchmark:     0.40.0 (unchanged)
                             generator-fluid:      0.3.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
                                 dice-roller:      0.0.1 (unchanged)

--- packages/dds/cell/src/packageVersion.ts
+++ packages/dds/cell/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/cell";
-export const pkgVersion = "0.43.1";
+export const pkgVersion = "0.43.2";
--- packages/dds/matrix/src/packageVersion.ts
+++ packages/dds/matrix/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/matrix";
-export const pkgVersion = "0.43.1";
+export const pkgVersion = "0.43.2";
