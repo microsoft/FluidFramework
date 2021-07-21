bdae2bf9352b295d18f68ebc0efb6ff986984f69
Andrei Iacob <andreiiacob@microsoft.com>
2021-07-21 11:50:24 -0700

    Add some documentation

--- packages/utils/telemetry-utils/src/thresholdCounter.ts
+++ packages/utils/telemetry-utils/src/thresholdCounter.ts
@@ -5,29 +5,40 @@
 
 import { ITelemetryLogger } from "@fluidframework/common-definitions";
 
+/**
+ * Utility counter which will send event only if the provided value
+ * is above a configured threshold
+ */
 export class ThresholdCounter {
     public constructor(
         private readonly threshold: number,
         private readonly logger: ITelemetryLogger,
     ) {}
 
-    public send(event: string, count?: number) {
-        this.sendInternal(event, count);
+    /**
+     * Sends the value if it's above the treshold
+     */
+    public send(eventName: string, value?: number) {
+        this.sendInternal(eventName, value);
     }
 
-    public sendIfMultiple(event: string, count?: number) {
-        this.sendInternal(event, count, 0);
+    /**
+     * Sends the value if it's above the treshold
+     * and a multiple of the threshold.
+     */
+    public sendIfMultiple(eventName: string, value?: number) {
+        this.sendInternal(eventName, value, 0);
     }
 
-    private sendInternal(event: string, count?: number, delta?: number) {
-        if (count === undefined || count < this.threshold) {
+    private sendInternal(event: string, val?: number, delta?: number) {
+        if (val === undefined || val < this.threshold) {
             return;
         }
 
-        if (delta === undefined || count % this.threshold === delta) {
+        if (delta === undefined || val % this.threshold === delta) {
             this.logger.sendPerformanceEvent({
                 eventName: event,
-                value: count,
+                value: val,
             });
         }
     }
