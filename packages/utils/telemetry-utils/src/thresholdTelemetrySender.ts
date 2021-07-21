f88647529f04d3b77cb190a819a359bf86e6f6c0
Andrei Iacob <andreiiacob@microsoft.com>
2021-07-21 10:44:48 -0700

    Add (c) header

--- packages/utils/telemetry-utils/src/thresholdTelemetrySender.ts
+++ packages/utils/telemetry-utils/src/thresholdTelemetrySender.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { ITelemetryLogger } from "@fluidframework/common-definitions";
 
 export class ThresholdTelemetrySender {
