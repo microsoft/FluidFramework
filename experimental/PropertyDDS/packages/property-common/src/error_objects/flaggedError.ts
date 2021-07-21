60b4e7e2626818755aecaef7aa0cd8624dece4eb
Elchin Valiyev <elchin.valiyev@autodesk.com>
2021-07-22 00:42:49 +0200

    convert code 5/6

--- experimental/PropertyDDS/packages/property-common/src/error_objects/flaggedError.ts
+++ experimental/PropertyDDS/packages/property-common/src/error_objects/flaggedError.ts
@@ -3,45 +3,45 @@
  * Licensed under the MIT License.
  */
 
-var FlaggedError = function () { };
-
-/**
- * Flags that may be set on an error instance.
- * @type {{TRANSIENT: number, QUIET: number}}
- */
-FlaggedError.FLAGS = {
-    /**
-     * A transient error results from an operation that could succeed if retried.
-     */
-    TRANSIENT: 1,
-    /**
-     * A quiet error should not trigger an error log.
-     */
-    QUIET: 2
-};
-
 /**
  * Checks if a flag is set
- * @param {number} flag A flag value
- * @return {boolean} True if the flag is set in passed flags, false otherwise.
- * @private
+ * @param  flags - flags set within an error object
+ * @param  flag - A flag to be checked
+ * @returns   True if the flag is set in passed flags, false otherwise.
  */
-var _isFlagSet = function (flag) {
-    return (this.flags & flag) === flag;
+ const _isFlagSet = (flags: number, flag: number) =>   {
+    // eslint-disable-next-line no-bitwise
+    return (flags & flag) === flag;
 };
 
-/**
- * @return {boolean} True if the quiet flag is set.
- */
-FlaggedError.prototype.isQuiet = function () {
-    return _isFlagSet.call(this, FlaggedError.FLAGS.QUIET);
-};
+export class FlaggedError {
+    /**
+     * Flags that may be set on an error instance.
+     */
+    static FLAGS = {
+        /**
+         * A transient error results from an operation that could succeed if retried.
+         */
+        TRANSIENT: 1,
+        /**
+         * A quiet error should not trigger an error log.
+         */
+        QUIET: 2,
+    };
 
-/**
- * @return {boolean} True if the transient flag is set.
- */
-FlaggedError.prototype.isTransient = function () {
-    return _isFlagSet.call(this, FlaggedError.FLAGS.TRANSIENT);
-};
+    protected flags: number = 0;
 
-module.exports = FlaggedError;
+    /**
+     * @returns True if the quiet flag is set.
+     */
+    isQuiet(): boolean {
+        return  _isFlagSet(this.flags,FlaggedError.FLAGS.QUIET);
+    }
+
+    /**
+     * @returns True if the transient flag is set.
+     */
+    isTransient(): boolean {
+        return  _isFlagSet(this.flags, FlaggedError.FLAGS.TRANSIENT);
+    }
+}
--- experimental/PropertyDDS/packages/property-common/src/error_objects/httpError.ts
+++ experimental/PropertyDDS/packages/property-common/src/error_objects/httpError.ts
@@ -2,64 +2,66 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-
-var FlaggedError = require('./flagged_error');
+import { FlaggedError } from "./flaggedError";
 
 /**
  * Class extending Error with HTTP-specific error information like statusCode and statusMessage
- * @param {string} title The error title
- * @param {number} statusCode A numeric HTTP status code
- * @param {string} statusMessage A string message representing the response status message
- * @param {string} method The HTTP method used in the request
- * @param {string} url The URL that the request was sent to
- * @param {?number} flags Flags that characterize the error. See {@link FlaggedError#FLAGS}.
- * @constructor
- * @alias property-common.HTTPError
- * @private
+ * @param title - The error title
+ * @param statusCode - A numeric HTTP status code
+ * @param statusMessage - A string message representing the response status message
+ * @param method - The HTTP method used in the request
+ * @param url - The URL that the request was sent to
+ * @param flags - Flags that characterize the error. See {@link FlaggedError.FLAGS}.
  */
-var HTTPError = function (title, statusCode, statusMessage, method, url, flags) {
-    this.name = 'HTTPError';
-    this.title = title;
-    this.statusCode = statusCode;
-    this.statusMessage = statusMessage;
-    this.method = method;
-    this.url = url;
-    this.flags = flags || 0;
-    this.message = this._generateMessage(title, statusCode, statusMessage, method, url);
-    this.stack = (new Error(this.message)).stack;
-    Error.call(this);
-};
+export class HTTPError extends Error {
+    constructor(
+        public title?: string,
+        public statusCode?: number,
+        public statusMessage?: string,
+        public method?: string,
+        public url?: string,
+        public flags = 0,
+    ) {
+        super();
+        Object.setPrototypeOf(this, HTTPError.prototype);
+        this.name = "HTTPError";
+        this.message = this._generateMessage(title, statusCode, statusMessage, method, url);
+        this.stack = (new Error(this.message)).stack;
+    }
 
-HTTPError.prototype = Object.create(Error.prototype);
-HTTPError.FLAGS = FlaggedError.FLAGS;
-HTTPError.prototype.isQuiet = FlaggedError.prototype.isQuiet;
-HTTPError.prototype.isTransient = FlaggedError.prototype.isTransient;
+    static FLAGS = FlaggedError.FLAGS;
 
-HTTPError.prototype._generateMessage = function (title, statusCode, statusMessage, method, url) {
-    title = (title === undefined) ? '' : String(title);
-    statusCode = (statusCode === undefined) ? '' : String(statusCode);
-    statusMessage = (statusMessage === undefined) ? '' : String(statusMessage);
-    method = (method === undefined) ? '' : String(method);
-    url = (url === undefined) ? '' : String(url);
+    isQuiet(): boolean {
+        return FlaggedError.prototype.isQuiet.call(this);
+    }
 
-    return `HTTPError: ${title}, statusCode:${statusCode}, ` +
-        `statusMessage:${statusMessage}, method:${method}, url:${url}`;
-};
+    isTransient(): boolean {
+        return FlaggedError.prototype.isTransient.call(this);
+    }
 
-/**
- * Returns a string representing the HTTPError object
- * @return {string} a string representing the HTTPError object
- */
-HTTPError.prototype.toString = function () {
-    var stack = this.stack;
-    stack = (stack === undefined) ? '' : String(stack);
+    private _generateMessage(title, statusCode, statusMessage, method, url) {
+        const titleStr = (title === undefined) ? "" : String(title);
+        const statusCodeStr = (statusCode === undefined) ? "" : String(statusCode);
+        const statusMessageStr = (statusMessage === undefined) ? "" : String(statusMessage);
+        const methodStr = (method === undefined) ? "" : String(method);
+        const urlStr = (url === undefined) ? "" : String(url);
 
-    var isFirefox = typeof window !== 'undefined' &&
-        typeof window.navigator !== 'undefined' &&
-        typeof window.navigator.userAgent !== 'undefined' &&
-        window.navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+        return `HTTPError: ${titleStr}, statusCode:${statusCodeStr}, ` +
+            `statusMessage:${statusMessageStr}, method:${methodStr}, url:${urlStr}`;
+    }
 
-    return isFirefox ? `${this.message}, stack:${stack}` : `stack:${stack}`;
-};
+    /**
+     * Returns a string representing the HTTPError object
+     * @returns a string representing the HTTPError object
+     */
+    toString(): string {
+        const stack = (this.stack === undefined) ? "" : String(this.stack);
 
-module.exports = HTTPError;
+        const isFirefox = typeof window !== "undefined" &&
+            typeof window.navigator !== "undefined" &&
+            typeof window.navigator.userAgent !== "undefined" &&
+            window.navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
+
+        return isFirefox ? `${this.message}, stack:${stack}` : `stack:${stack}`;
+    }
+}
