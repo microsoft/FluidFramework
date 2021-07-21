95374508aab9c04cd9523bf0e06ca54767ad895f
Elchin Valiyev <elchin.valiyev@autodesk.com>
2021-07-22 00:39:28 +0200

    convert tests 2/3

--- experimental/PropertyDDS/packages/property-common/src/test/error_objects/flaggedError.spec.ts
+++ experimental/PropertyDDS/packages/property-common/src/test/error_objects/flaggedError.spec.ts
@@ -7,12 +7,13 @@
  * @fileoverview Tests the functions exported by error_objects/http_error.js
  */
 
-const FlaggedError = require('../..').FlaggedError;
+import { expect } from "chai";
+import { FlaggedError } from "../..";
 
-describe('property-common.FlaggedError', function () {
-    describe('Flags', function () {
-        it('can be extended', function (done) {
-            Object.keys(FlaggedError.FLAGS).forEach(function (key, index) {
+describe("property-common.FlaggedError", function() {
+    describe("Flags", function() {
+        it("can be extended", function(done) {
+            Object.keys(FlaggedError.FLAGS).forEach(function(key, index) {
                 expect(FlaggedError.FLAGS[key]).to.equal(Math.pow(2, index));
             });
             done();
