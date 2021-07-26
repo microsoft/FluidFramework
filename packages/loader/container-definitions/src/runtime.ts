6e868a9d65d447b1dfbe1c912fc3ababe6d06494
Wes Carlson <wesley.carlson@microsoft.com>
2021-07-26 14:01:07 -0700

    PR feedback

--- packages/loader/container-definitions/src/runtime.ts
+++ packages/loader/container-definitions/src/runtime.ts
@@ -84,8 +84,10 @@ export interface IRuntime extends IDisposable {
 
     /**
      * Create a summary. Used when attaching or serializing a detached container.
-     * @param blobRedirectTable - A table passed during the attach process. Used to support blobs in detached container
-     * by mapping IDs assigned locally while detached to IDs assigned by storage after attach.
+     *
+     * @param blobRedirectTable - A table passed during the attach process. While detached, blob upload is supported
+     * using IDs generated locally. After attach, these IDs cannot be used, so this table maps the old local IDs to the
+     * new storage IDs so requests can be redirected.
      */
     createSummary(blobRedirectTable?: Map<string, string>): ISummaryTree;
 
