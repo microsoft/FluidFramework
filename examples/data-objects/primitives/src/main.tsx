0fb52e4cc8fe18ac3fd423cbd006d3de056789bc
Matt Rakow <marakow@microsoft.com>
2021-07-27 08:53:08 -0700

    Add public API surface for primitives collection

--- examples/data-objects/primitives/src/main.tsx
+++ examples/data-objects/primitives/src/main.tsx
@@ -24,17 +24,18 @@ export class PrimitivesCollection extends DataObject implements IFluidHTMLView {
     protected get mapDir(): IDirectory { return this.tryGetDds(this.internalMapDir, "mapDir"); }
 
     /**
-     * ComponentInitializingFirstTime is called only once, it is executed only by the first client to open the
+     * initializingFirstTime is called only once, it is executed only by the first client to open the
      * component and all work will resolve before the view is presented to any user.
      *
      * This method is used to perform component setup, which can include setting an initial schema or initial values.
      */
     protected async initializingFirstTime() {
-        this.internalMapDir = this.root.createSubDirectory("map");
+        this.root.createSubDirectory("map");
     }
 
-    protected async initializingFromExisting() {
+    protected async hasInitialized() {
         this.internalMapDir = this.root.getSubDirectory("map");
+        this.mapDir.on("containedValueChanged", () => { this.emit("mapsChanged"); });
     }
 
     /**
@@ -53,6 +54,12 @@ export class PrimitivesCollection extends DataObject implements IFluidHTMLView {
 
         rerender();
     }
+
+    public readonly addMap = (name: string) => {
+        const newMap = SharedMap.create(this.runtime, name);
+        newMap.bindToContext();
+        this.mapDir.set(name, newMap.handle);
+    };
 
     private tryGetDds<T>(dds: T | undefined, id: string): T {
         if (dds === undefined) {
