7759a6386e0884d94ef0b061f963fe93cd31b85e
Matt Rakow <marakow@microsoft.com>
2021-07-27 10:01:38 -0700

    index.tsx

--- examples/data-objects/primitives/webpack.config.js
+++ examples/data-objects/primitives/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = env => {
 
     return merge({
         entry: {
-            main: "./src/index.ts"
+            main: "./src/index.tsx"
         },
         resolve: {
             extensions: [".ts", ".tsx", ".js"],
