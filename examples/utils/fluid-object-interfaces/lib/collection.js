e1c30bb530dfd90e2d5a0fa6da02d838a3dc3da2
Si Chen <chensi@microsoft.com>
2021-07-29 11:55:52 -0700

    delete files

--- examples/utils/fluid-object-interfaces/lib/collection.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-/**
- * @deprecated This example will be removed in a future release.
- */
-export const IFluidObjectCollection = "IFluidObjectCollection";
-//# sourceMappingURL=collection.js.map
