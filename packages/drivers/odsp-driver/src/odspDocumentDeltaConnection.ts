58635a26dde660fd6216f2e3ae08dba64fcdaf54
Vlad Sudzilouski <vlad@sudzilouski.com>
2021-08-02 13:34:59 -0700

    Fetching ops from PUSH (#6954)
    Implementing solution described in #6685.
    
    After implementing #6947, the client hits again "too many retries" issue (critical failure due to client not being able to get ops within 30 seconds).
    With this PR, client always asks PUSH for any missing ops in parallel to fetching same ops from storage and/or local cache.
    This reduces number of cases when we get "too many retries", but does not eliminate it.
    
    I've added minimum telemetry, but most request can be tracked by tracking storage request telemetry, as every call will be duplicated to PUSH if there is active connection.
    
    Flow can be optimized further by
    
    Not asking PUSH for ops ranges that are preceding first op on socket
    Ask for ops in sequence (not in parallel), in order of local cache / PUSH / storage.
    This PR (in current form) should unblock further investigations and understanding of "too many retries" problem, but also allow PUSH to be simpler (if needed / desired) by eliminating various work arounds, if we chose to go that route.
    Or, if we chose for PUSH to provide stronger guarantees and ensure ops are always coming in order, than lack of hits for newly added telemetry will allow us to remove this code and have confidence it's not needed.

--- packages/drivers/odsp-driver/src/odspDocumentDeltaConnection.ts
+++ packages/drivers/odsp-driver/src/odspDocumentDeltaConnection.ts
@@ -6,7 +6,7 @@
 import { ITelemetryLogger } from "@fluidframework/common-definitions";
 import { assert } from "@fluidframework/common-utils";
 import { DocumentDeltaConnection } from "@fluidframework/driver-base";
-import { IDocumentDeltaConnection, DriverError } from "@fluidframework/driver-definitions";
+import { DriverError } from "@fluidframework/driver-definitions";
 import {
     IClient,
     IConnect,
@@ -182,7 +182,7 @@ export class OdspDocumentDeltaConnection extends DocumentDeltaConnection {
         url: string,
         telemetryLogger: ITelemetryLogger,
         timeoutMs: number,
-        epochTracker: EpochTracker): Promise<IDocumentDeltaConnection>
+        epochTracker: EpochTracker): Promise<OdspDocumentDeltaConnection>
     {
         // enable multiplexing when the websocket url does not include the tenant/document id
         const parsedUrl = new URL(url);
@@ -244,6 +244,7 @@ export class OdspDocumentDeltaConnection extends DocumentDeltaConnection {
     }
 
     private socketReference: SocketReference | undefined;
+    private readonly requestOpsNoncePrefix: string;
 
     /**
      * Error raising for socket.io issues
@@ -309,6 +310,15 @@ export class OdspDocumentDeltaConnection extends DocumentDeltaConnection {
     {
         super(socket, documentId, logger);
         this.socketReference = socketReference;
+        this.requestOpsNoncePrefix = `${this.documentId}-`;
+    }
+
+    public requestOps(from: number, to: number) {
+        this.socket.emit("get_ops", this.clientId, {
+            nonce: `${this.requestOpsNoncePrefix}${uuid()}`,
+            from,
+            to,
+        });
     }
 
     protected async initialize(connectMessage: IConnect, timeout: number) {
@@ -320,6 +330,20 @@ export class OdspDocumentDeltaConnection extends DocumentDeltaConnection {
                 }
             };
         }
+
+        this.socket.on("get_ops_response", (result) => {
+            const messages = result.messages as ISequencedDocumentMessage[];
+            if ((result.nonce as string).startsWith(this.requestOpsNoncePrefix) && messages.length > 0) {
+                this.logger.sendTelemetryEvent({
+                    eventName: "GetOps",
+                    first: messages[0].sequenceNumber,
+                    last: messages[messages.length - 1].sequenceNumber,
+                    code: result.code,
+                    length: messages.length,
+                });
+                this.socket.emit("op", this.documentId, messages);
+            }
+        });
 
         return super.initialize(connectMessage, timeout);
     }
