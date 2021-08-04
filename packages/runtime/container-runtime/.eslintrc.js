8cac5c94be0ee82aaf41288b036d87ff60d34333
Tyler Butler <tylerbu@microsoft.com>
2021-08-04 15:05:00 -0700

    revert lint config

--- packages/runtime/container-runtime/.eslintrc.js
+++ packages/runtime/container-runtime/.eslintrc.js
@@ -5,27 +5,13 @@
 
 module.exports = {
     "extends": [
-        "@fluidframework/eslint-config-fluid/rushstack"
+        "@fluidframework/eslint-config-fluid/eslint7"
     ],
     "parserOptions": {
         "project": ["./tsconfig.json", "./src/test/tsconfig.json"]
     },
     "rules": {
         "@typescript-eslint/strict-boolean-expressions": "off",
-        "no-null/no-null": "off",
-        // RATIONALE:         Code is more readable when the type of every variable is immediately obvious.
-          //                    Even if the compiler may be able to infer a type, this inference will be unavailable
-          //                    to a person who is reviewing a GitHub diff.  This rule makes writing code harder,
-          //                    but writing code is a much less important activity than reading it.
-          //
-          // STANDARDIZED BY:   @typescript-eslint\eslint-plugin\dist\configs\recommended.json
-          '@typescript-eslint/explicit-function-return-type': [
-            'error',
-            {
-              allowExpressions: true,
-              allowTypedFunctionExpressions: true,
-              allowHigherOrderFunctions: false
-            }
-          ],
+        "no-null/no-null": "off"
     }
 }
