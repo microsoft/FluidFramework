e7ca2eeb758e6ec13acacc42a54448e655e2d619
Zach Newton <znewton@microsoft.com>
2021-08-06 16:18:11 -0700

    Use containerID from server as source of truth

--- packages/drivers/routerlicious-driver/src/documentServiceFactory.ts
+++ packages/drivers/routerlicious-driver/src/documentServiceFactory.ts
@@ -81,7 +81,7 @@ export class RouterliciousDocumentServiceFactory implements IDocumentServiceFact
             rateLimiter,
             resolvedUrl.endpoints.ordererUrl,
         );
-        await ordererRestWrapper.post(
+        const containerId = await ordererRestWrapper.post<string>(
             `/documents/${tenantId}`,
             {
                 id,
@@ -90,8 +90,15 @@ export class RouterliciousDocumentServiceFactory implements IDocumentServiceFact
                 values: quorumValues,
             },
         );
+        parsedUrl.pathname = parsedUrl.pathname.split("/").slice(0, -1).concat([containerId]).join("/");
 
-        return this.createDocumentService(resolvedUrl, logger);
+        return this.createDocumentService(
+            {
+                ...resolvedUrl,
+                url: parsedUrl.href,
+                id: containerId,
+            },
+            logger);
     }
 
     /**
@@ -116,7 +123,7 @@ export class RouterliciousDocumentServiceFactory implements IDocumentServiceFact
         }
 
         const parsedUrl = parse(fluidResolvedUrl.url);
-        const [, tenantId, documentId] = parsedUrl.pathname!.split("/");
+        const [, tenantId, documentId] = parsedUrl.pathname.split("/");
         if (!documentId || !tenantId) {
             throw new Error(
                 `Couldn't parse documentId and/or tenantId. [documentId:${documentId}][tenantId:${tenantId}]`);
