17311302dbacc2b9cc91869c29d8d767136b7527
Jatin Garg <jatgarg@microsoft.com>
2021-08-11 13:54:14 -0700

    Fix contents of blobs when binary contents are specified

---
+++ packages/drivers/routerlicious-driver/src/createNewUtils.ts
@@ -0,0 +1,34 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { Uint8ArrayToString } from "@fluidframework/common-utils";
+import { ISummaryTree, SummaryType } from "@fluidframework/protocol-definitions";
+
+export function convertSummaryToCreateNewSummary(summary: ISummaryTree): ISummaryTree {
+    const keys = Object.keys(summary.tree);
+    for (const key of keys) {
+        const summaryObject = summary.tree[key];
+
+        switch (summaryObject.type) {
+            case SummaryType.Tree: {
+                summary.tree[key] = convertSummaryToCreateNewSummary(summaryObject);
+                break;
+            }
+            case SummaryType.Blob: {
+                summaryObject.content = typeof summaryObject.content === "string" ?
+                    summaryObject.content : Uint8ArrayToString(summaryObject.content, "utf8");
+                break;
+            }
+            case SummaryType.Handle: {
+                throw new Error("No handle should be present for first summary!!");
+            }
+            default: {
+                throw new Error(`Unknown tree type ${summaryObject.type}`);
+            }
+        }
+    }
+
+    return summary;
+}
