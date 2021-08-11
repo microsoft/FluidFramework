aa9a9ecb809c8834643786d1c66703572e3bbb4d
Navin Agarwal <naagar@microsoft.com>
2021-08-11 14:58:07 -0700

    Some cleanup

--- packages/test/test-end-to-end-tests/src/test/audience.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/audience.spec.ts
@@ -13,11 +13,12 @@ import { Container } from "@fluidframework/container-loader";
 import { requestFluidObject } from "@fluidframework/runtime-utils";
 import { ITestObjectProvider } from "@fluidframework/test-utils";
 import { describeNoCompat } from "@fluidframework/test-version-utils";
-import { IContainerRuntimeOptions } from "@fluidframework/container-runtime";
 import { flattenRuntimeOptions } from "./flattenRuntimeOptions";
 
 class TestDataObject extends DataObject {
-    public clientList: Set<string> = new Set();
+    // Maintains a list of clients in the audience.
+    public audienceClientList: Set<string> = new Set();
+
     public get _root() {
         return this.root;
     }
@@ -32,21 +33,20 @@ class TestDataObject extends DataObject {
 
     protected async hasInitialized() {
         this.runtime.getAudience().on("addMember", (clientId: string) => {
-            this.clientList.add(clientId);
+            this.audienceClientList.add(clientId);
         });
 
         this.runtime.getAudience().on("removeMember", (clientId: string) => {
-            this.clientList.delete(clientId);
+            this.audienceClientList.delete(clientId);
         });
 
         const members = this.runtime.getAudience().getMembers();
-        for (const member of members) {
-            this.clientList.add(member[0]);
+        for (const [clientId] of members) {
+            this.audienceClientList.add(clientId);
         }
     }
 }
 
-// REVIEW: enable compat testing?
 describeNoCompat("Audience correctness", (getTestObjectProvider) => {
     let provider: ITestObjectProvider;
     const dataObjectFactory = new DataObjectFactory(
@@ -54,15 +54,6 @@ describeNoCompat("Audience correctness", (getTestObjectProvider) => {
         TestDataObject,
         [],
         []);
-
-    const runtimeOptions: IContainerRuntimeOptions = {
-        summaryOptions: {
-            generateSummaries: false,
-        },
-        gcOptions: {
-            gcAllowed: true,
-        },
-    };
     const runtimeFactory = new ContainerRuntimeFactoryWithDefaultDataStore(
         dataObjectFactory,
         [
@@ -70,7 +61,8 @@ describeNoCompat("Audience correctness", (getTestObjectProvider) => {
         ],
         undefined,
         undefined,
-        flattenRuntimeOptions(runtimeOptions),
+        // Disable summaries so the summarizer client doesn't interfere with the audience
+        flattenRuntimeOptions({ summaryOptions: { generateSummaries: false }}),
     );
 
     const createContainer = async (): Promise<Container> => await provider.createContainer(runtimeFactory) as Container;
@@ -81,28 +73,84 @@ describeNoCompat("Audience correctness", (getTestObjectProvider) => {
     });
 
     it("should add / remove clients in audience as expected", async () => {
-        const container1 = await createContainer();
-        const dataStore1 = await requestFluidObject<TestDataObject>(container1, "default");
+        // Create a client - client1.
+        const client1Container = await createContainer();
+        const client1DataStore = await requestFluidObject<TestDataObject>(client1Container, "default");
 
-        const container2 = await loadContainer();
-        const dataStore2 = await requestFluidObject<TestDataObject>(container2, "default");
+        // Ensure the client1 is connected and synchronized.
+        await provider.ensureSynchronized();
+        // Validate that client1 is in its own audience.
+        assert(client1Container.clientId !== undefined, "client1 does not have clientId");
+        assert(
+            client1DataStore.audienceClientList.has(client1Container.clientId),
+            "client1's audience does not have client1's clientId",
+        );
 
-        assert(container1.clientId !== undefined, "Container1 does not have clientId");
-        assert(container2.clientId !== undefined, "Container2 does not have clientId");
-        assert(dataStore1.clientList.has(container1.clientId), "Client1's audience does not have client1");
-        assert(dataStore1.clientList.has(container2.clientId), "Client1's audience does not have client2");
-        assert(dataStore2.clientList.has(container1.clientId), "Client2's audience does not have client1");
-        assert(dataStore2.clientList.has(container2.clientId), "Client2's audience does not have client2");
+        // Load a second client - client2.
+        const client2Container = await loadContainer();
+        const client2DataStore = await requestFluidObject<TestDataObject>(client2Container, "default");
+        // Ensure the client2 is connected and synchronized.
+        await provider.ensureSynchronized();
+        // Validate that client2 is in its own audience.
+        assert(client2Container.clientId !== undefined, "client does not have clientId");
+        assert(
+            client2DataStore.audienceClientList.has(client2Container.clientId),
+            "client2's audience does not have client2's clientId",
+        );
 
-        // Peform operations so that clients transtion to write mode (if not already).
-        dataStore1._root.set("key1", "value1");
-        dataStore2._root.set("key2", "value2");
+        // Validate that client1 is in client2's audience.
+        assert(
+            client2DataStore.audienceClientList.has(client1Container.clientId),
+            "client2's audience does not have client1",
+        );
 
+        // Validate that client2 is in client1's audience.
+        assert(
+            client1DataStore.audienceClientList.has(client2Container.clientId),
+            "Client1's audience does not have client2",
+        );
+    });
+
+    it("should add / remove clients in audience as expected in write mode", async () => {
+        // Create a client - client1.
+        const client1Container = await createContainer();
+        const client1DataStore = await requestFluidObject<TestDataObject>(client1Container, "default");
+
+        // Load a second client - client2.
+        const client2Container = await loadContainer();
+        const client2DataStore = await requestFluidObject<TestDataObject>(client2Container, "default");
+
+        // Perform operations to move the clients to "write" mode (if not already in write mode).
+        client1DataStore._root.set("testKey1", "testValue1");
+        client2DataStore._root.set("testKey2", "testValue2");
+
+        // Ensure that clients are connected and synchronized.
         await provider.ensureSynchronized();
 
-        assert(dataStore1.clientList.has(container1.clientId), "Write - Client1's audience does not have client1");
-        assert(dataStore1.clientList.has(container2.clientId), "Write - Client1's audience does not have client2");
-        assert(dataStore2.clientList.has(container1.clientId), "Write - Client2's audience does not have client1");
-        assert(dataStore2.clientList.has(container2.clientId), "Write - Client2's audience does not have client2");
+        // Validate that client1 is in its own audience.
+        assert(client1Container.clientId !== undefined, "client1 does not have clientId");
+        assert(
+            client1DataStore.audienceClientList.has(client1Container.clientId),
+            "client1's audience does not have client1's clientId",
+        );
+
+        // Validate that client2 is in its own audience.
+        assert(client2Container.clientId !== undefined, "client does not have clientId");
+        assert(
+            client2DataStore.audienceClientList.has(client2Container.clientId),
+            "client2's audience does not have client2's clientId",
+        );
+
+        // Validate that client1 is in client2's audience.
+        assert(
+            client2DataStore.audienceClientList.has(client1Container.clientId),
+            "client2's audience does not have client1",
+        );
+
+        // Validate that client2 is in client1's audience.
+        assert(
+            client1DataStore.audienceClientList.has(client2Container.clientId),
+            "Client1's audience does not have client2",
+        );
     });
 });
