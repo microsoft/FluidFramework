27fb64f0d2dc632976cf83eadda6925edcf1431b
Vlad Sudzilouski <vlad@sudzilouski.com>
2021-08-12 16:25:46 -0700

    get_ops flow is broken due to raising events on wrong object. The bug became obvious due to latest PUSH rollout to SPDF that removes initial Ops on connection, making this part of code critical in loading flow. Unfortunatly we did not see this issue despite a bunch of testing, and it was missed on PUSH rollout as PUSH is using 0.44.x bits. But shows up only if client uses IConnect.supportedFeatures.api_get_ops = true as that changes behavior of PUSH. Whle 0.45 itself should not have a regression as client does not provide IConnect.supportedFeatures.api_get_ops, it's safe to fix it and we know (from telemetry) that we run into "too many retries" issues in general (much less frequently than with this regresison, but we still run into these issues with some frequency).

--- packages/drivers/odsp-driver/src/odspDocumentDeltaConnection.ts
+++ packages/drivers/odsp-driver/src/odspDocumentDeltaConnection.ts
@@ -377,7 +377,7 @@ export class OdspDocumentDeltaConnection extends DocumentDeltaConnection {
                         duration: performance.now() - data.start,
                         length: messages.length,
                     });
-                    this.socket.emit("op", this.documentId, messages);
+                    this.emit("op", this.documentId, messages);
                 }
             }
         });
