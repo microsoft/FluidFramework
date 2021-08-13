f7b95a19a50444f67e126a3a239f380f0b6e6329
Sonali Deshpande <sdeshpande@microsoft.com>
2021-08-13 12:21:41 -0700

    Revert "frs client"
    This reverts commit 3fd3aae99c5e70ec335fecb627f53e3bbc190980.
    
    revert frs client additions

--- experimental/framework/frs-client/lib/debug.d.ts
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-import registerDebug from "debug";
-export declare const debug: registerDebug.Debugger;
-//# sourceMappingURL=debug.d.ts.map
--- experimental/framework/frs-client/lib/test/FrsClient.spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-// TODO: Add tests here that mock a connection to FRS
-//# sourceMappingURL=FrsClient.spec.js.map
