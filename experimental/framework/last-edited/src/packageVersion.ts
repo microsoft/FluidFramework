5969bb0018e974f28bc43cd29d16d8a70a3caa53
Helio Liu <59622401+heliocliu@users.noreply.github.com>
2021-08-17 12:30:54 -0700

    [bump] package version for client (patch) (#7160)
    @fluidframework/build-common:     0.23.0 (unchanged)
         @fluidframework/eslint-config-fluid:     0.24.0 (unchanged)
          @fluidframework/common-definitions:     0.21.0 (unchanged)
                @fluidframework/common-utils:     0.32.2 (unchanged)
       @fluidframework/container-definitions:     0.40.0 (unchanged)
             @fluidframework/core-interfaces:     0.40.0 (unchanged)
          @fluidframework/driver-definitions:     0.40.0 (unchanged)
        @fluidframework/protocol-definitions:   0.1025.0 (unchanged)
                                      Server:   0.1028.2 (unchanged)
                                      Client:     0.45.4 -> 0.45.5
                      @fluid-tools/benchmark:     0.40.0 (unchanged)
                             generator-fluid:      0.3.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
                                 dice-roller:      0.0.1 (unchanged)

--- experimental/framework/last-edited/src/packageVersion.ts
+++ experimental/framework/last-edited/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-experimental/last-edited";
-export const pkgVersion = "0.45.4";
+export const pkgVersion = "0.45.5";
--- packages/drivers/odsp-driver/src/packageVersion.ts
+++ packages/drivers/odsp-driver/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/odsp-driver";
-export const pkgVersion = "0.45.4";
+export const pkgVersion = "0.45.5";
