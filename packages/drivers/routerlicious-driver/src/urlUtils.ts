5439adc369b2209c49dc73f1a4852cd30339f57f
Zach Newton <znewton@microsoft.com>
2021-08-31 10:02:11 -0700

    r11s-driver: fix url parsing and patching (#7301)

---
+++ packages/drivers/routerlicious-driver/src/urlUtils.ts
@@ -0,0 +1,17 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export const parseFluidUrl = (fluidUrl: string): URL => {
+    return new URL(fluidUrl);
+};
+
+/**
+ * Assume documentId is at end of url path.
+ * This is true for Routerlicious' and Tinylicious' documentUrl and deltaStorageUrl.
+ * Routerlicious and Tinylicious do not use documentId in storageUrl nor ordererUrl.
+ * TODO: Ideally we would be able to regenerate the resolvedUrl, rather than patching the current one.
+ */
+export const replaceDocumentIdInPath = (urlPath: string, documentId: string): string =>
+    urlPath.split("/").slice(0, -1).concat([documentId]).join("/");
