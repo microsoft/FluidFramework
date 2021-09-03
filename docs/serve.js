5e6c9ecb7be5ab8a536cd28b37a3b24a0b13880e
Tyler Butler <tylerbu@microsoft.com>
2021-09-03 16:38:36 -0700

    updates

--- docs/serve.js
+++ docs/serve.js
@@ -3,11 +3,11 @@
  * Licensed under the MIT License.
  */
 
-// This script is just a wrapper around the serve package so we can use pm2 to spawn it in the background when checking
-// broken links in CI. Look at .github/workflows/broken-link-checker.yml to see how it is used.
-
 const handler = require("serve-handler");
 const http = require("http");
+
+// This script is just a wrapper around the serve package so we can use pm2 to spawn it in the background when checking
+// broken links in CI. Look at .github/workflows/broken-link-checker.yml to see how it is used.
 
 const server = http.createServer((request, response) => {
   return handler(request, response, {
