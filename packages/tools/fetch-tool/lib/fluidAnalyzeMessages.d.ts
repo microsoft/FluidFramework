fe1eb96b6c964fe7be6ad4cd629edfcfa18cc2d8
Si Chen <chensi@microsoft.com>
2021-09-02 23:51:00 -0700

    testing

--- packages/tools/fetch-tool/lib/fluidAnalyzeMessages.d.ts
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-export declare const formatNumber: (num: number) => string;
-export declare function printMessageStats(generator: any, // AsyncGenerator<ISequencedDocumentMessage[]>,
-dumpMessageStats: boolean, dumpMessages: boolean, messageTypeFilter?: Set<string>): Promise<void>;
-//# sourceMappingURL=fluidAnalyzeMessages.d.ts.map
--- packages/tools/fetch-tool/lib/fluidFetchMessages.d.ts
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-import { IDocumentService } from "@fluidframework/driver-definitions";
-export declare function fluidFetchMessages(documentService?: IDocumentService, saveDir?: string): Promise<void>;
-//# sourceMappingURL=fluidFetchMessages.d.ts.map
