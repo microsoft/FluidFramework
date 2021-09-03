e1dedb3944e64730956161ecc434fc0768854e22
Elchin Valiyev <elchin.valiyev@autodesk.com>
2021-09-03 05:19:13 +0200

    fix repo policy check

--- experimental/PropertyDDS/packages/property-properties/src/properties/setProperty.js
+++ experimental/PropertyDDS/packages/property-properties/src/properties/setProperty.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 /**
  * @fileoverview Definition of the set property class
  */
