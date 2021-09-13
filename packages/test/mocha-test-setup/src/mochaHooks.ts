microsoft/FluidFramework

commit fba2ab9d6524b8a5552e68f87291f242ee6a57f2

Include endpoint name into telemetry for frs e2e realsvc tests (#7319)
Also bump up the timeout for FRS test.

packages/test/test-drivers/src/factory.ts
@@ -6,7 +6,7 @@
 import http from "http";
 import * as path from "path";
 import Axios from "axios";
-import { TestDriverTypes } from "@fluidframework/test-driver-definitions";
+import { ITestDriver, TestDriverTypes } from "@fluidframework/test-driver-definitions";
 import { unreachableCase } from "@fluidframework/common-utils";
 import { LocalServerTestDriver } from "./localServerTestDriver";
 import { TinyliciousTestDriver } from "./tinyliciousTestDriver";
@@ -62,7 +62,7 @@ export async function createFluidTestDriver(
     fluidTestDriverType: TestDriverTypes = "local",
     config?: FluidTestDriverConfig,
     api: DriverApiType = DriverApi,
-): Promise<LocalServerTestDriver | TinyliciousTestDriver | RouterliciousTestDriver | OdspTestDriver> {
+): Promise<ITestDriver> {
     switch (fluidTestDriverType) {
         case "local":
             return new LocalServerTestDriver(api.LocalDriverApi);

packages/test/test-service-load/src/runner.ts
@@ -18,7 +18,7 @@ import { FaultInjectionDocumentServiceFactory } from "./faultInjectionDriver";
 import { generateLoaderOptions, generateRuntimeOptions } from "./optionsMatrix";
 
 function printStatus(runConfig: IRunConfig, message: string) {
-    if(runConfig.verbose) {
+    if (runConfig.verbose) {
         console.log(`${runConfig.runId.toString().padStart(3)}> ${message}`);
     }
 }
@@ -38,7 +38,7 @@ async function main() {
     const driver: TestDriverTypes = commander.driver;
     const profileArg: string = commander.profile;
     const url: string = commander.url;
-    const runId: number  = commander.runId;
+    const runId: number = commander.runId;
     const log: string | undefined = commander.log;
     const verbose: boolean = commander.verbose ?? false;
     const seed: number = commander.seed;
@@ -62,9 +62,9 @@ async function main() {
 
     const l = await loggerP;
     process.on("unhandledRejection", (reason, promise) => {
-        try{
-            l.sendErrorEvent({eventName: "UnhandledPromiseRejection"}, reason);
-        } catch(e) {
+        try {
+            l.sendErrorEvent({ eventName: "UnhandledPromiseRejection" }, reason);
+        } catch (e) {
             console.error("Error during logging unhandled promise rejection: ", e);
         }
     });
@@ -82,7 +82,7 @@ async function main() {
     await safeExit(result, url, runId);
 }
 
-function *factoryPermutations<T extends IDocumentServiceFactory>(create: () => T) {
+function* factoryPermutations<T extends IDocumentServiceFactory>(create: () => T) {
     let counter = 0;
     const factoryReused = create();
 
@@ -99,16 +99,16 @@ function *factoryPermutations<T extends IDocumentServiceFactory>(create: () => T
                 documentServiceFactory = create();
                 break;
             case 1:
-                headers = { [LoaderHeader.loadMode]: { opsBeforeReturn : "cached"} };
+                headers = { [LoaderHeader.loadMode]: { opsBeforeReturn: "cached" } };
                 break;
             case 2:
-                headers = { [LoaderHeader.loadMode]: { opsBeforeReturn : "all"} };
+                headers = { [LoaderHeader.loadMode]: { opsBeforeReturn: "all" } };
                 break;
             case 3:
-                headers = { [LoaderHeader.loadMode]: { deltaConnection : "none"} };
+                headers = { [LoaderHeader.loadMode]: { deltaConnection: "none" } };
                 break;
             case 4:
-                headers = { [LoaderHeader.loadMode]: { deltaConnection : "delayed"} };
+                headers = { [LoaderHeader.loadMode]: { deltaConnection: "delayed" } };
                 break;
         }
         yield { documentServiceFactory, headers };
@@ -130,8 +130,14 @@ async function runnerProcess(
 
         const testDriver = await createTestDriver(driver, seed, runConfig.runId);
         const baseLogger = await loggerP;
-        const logger = ChildLogger.create(
-            baseLogger, undefined, {all: { runId: runConfig.runId, driverType: testDriver.type }});
+        const logger = ChildLogger.create(baseLogger, undefined,
+            {
+                all: {
+                    runId: runConfig.runId,
+                    driverType: testDriver.type,
+                    driverEndpointName: testDriver.endpointName,
+                },
+            });
 
         // Cycle between creating new factory vs. reusing factory.
         // Certain behavior (like driver caches) are per factory instance, and by reusing it we hit those code paths
@@ -160,7 +166,7 @@ async function runnerProcess(
 
             const container = await loader.resolve({ url, headers });
             container.resume();
-            const test = await requestFluidObject<ILoadTest>(container,"/");
+            const test = await requestFluidObject<ILoadTest>(container, "/");
 
             scheduleContainerClose(container, runConfig);
             scheduleFaultInjection(documentServiceFactory, container, runConfig);
@@ -170,12 +176,12 @@ async function runnerProcess(
                 reset = false;
                 printStatus(runConfig, done ? `finished` : "closed");
             } catch (error) {
-                logger.sendErrorEvent({eventName: "RunnerFailed"}, error);
+                logger.sendErrorEvent({ eventName: "RunnerFailed" }, error);
             } finally {
                 if (!container.closed) {
                     container.close();
                 }
-                await baseLogger.flush({url, runId: runConfig.runId});
+                await baseLogger.flush({ url, runId: runConfig.runId });
             }
         }
         return 0;
@@ -190,18 +196,18 @@ function scheduleFaultInjection(
     ds: FaultInjectionDocumentServiceFactory,
     container: Container,
     runConfig: IRunConfig) {
-    const schedule = ()=>{
+    const schedule = () => {
         const injectionTime = runConfig.testConfig.readWriteCycleMs * random.real(0, 5)(runConfig.randEng);
-        printStatus(runConfig, `fault injection in ${(injectionTime / 60000).toString().substring(0,4)} min`);
+        printStatus(runConfig, `fault injection in ${(injectionTime / 60000).toString().substring(0, 4)} min`);
         setTimeout(() => {
-            if(container.connected && container.resolvedUrl !== undefined) {
+            if (container.connected && container.resolvedUrl !== undefined) {
                 const deltaConn =
                     ds.documentServices.get(container.resolvedUrl)?.documentDeltaConnection;
-                if(deltaConn !== undefined) {
+                if (deltaConn !== undefined) {
                     // 1 in numClients chance of non-retritable error to not overly conflict with container close
                     const canRetry =
-                        random.integer(0,  runConfig.testConfig.numClients - 1)(runConfig.randEng) === 0 ? false : true;
-                    switch(random.integer(0,  5)(runConfig.randEng)) {
+                        random.integer(0, runConfig.testConfig.numClients - 1)(runConfig.randEng) === 0 ? false : true;
+                    switch (random.integer(0, 5)(runConfig.randEng)) {
                         // dispreferr errors
                         case 0: {
                             deltaConn.injectError(canRetry);
@@ -224,7 +230,7 @@ function scheduleFaultInjection(
                     }
                 }
             }
-            if(!container.closed) {
+            if (!container.closed) {
                 schedule();
             }
         }, injectionTime);
@@ -233,38 +239,38 @@ function scheduleFaultInjection(
 }
 
 function scheduleContainerClose(container: Container, runConfig: IRunConfig) {
-    new Promise<void>((res)=>{
+    new Promise<void>((res) => {
         // wait for the container to connect write
         container.once("closed", res);
-        if(!container.deltaManager.active) {
-            container.once("connected", ()=>{
+        if (!container.deltaManager.active) {
+            container.once("connected", () => {
                 res();
                 container.off("closed", res);
             });
         }
-    }).then(()=>{
-        if(container.closed) {
+    }).then(() => {
+        if (container.closed) {
             return;
         }
         const quorum = container.getQuorum();
-        const scheduleLeave = ()=>{
+        const scheduleLeave = () => {
             const clientId = container.clientId;
-            if(clientId !== undefined && quorum.getMembers().has(clientId)) {
+            if (clientId !== undefined && quorum.getMembers().has(clientId)) {
                 // calculate the clients quorum position
-                const quorumIndex = [... quorum.getMembers().entries()]
-                    .sort((a,b)=>b[1].sequenceNumber - a[1].sequenceNumber)
-                    .map((m)=>m[0])
+                const quorumIndex = [...quorum.getMembers().entries()]
+                    .sort((a, b) => b[1].sequenceNumber - a[1].sequenceNumber)
+                    .map((m) => m[0])
                     .indexOf(clientId);
 
                 // only the oldest quarter of active clients are scheduled to leave this time.
                 // this will bias toward the summarizer client which is always quorum index 0.
-                if(quorumIndex >= 0 && quorumIndex <= runConfig.testConfig.numClients / 4) {
-                    quorum.off("removeMember",scheduleLeave);
-                    const leaveTime = runConfig.testConfig.readWriteCycleMs * random.real(0,  5)(runConfig.randEng);
-                    printStatus(runConfig, `closing in ${(leaveTime / 60000).toString().substring(0,4)} min`);
+                if (quorumIndex >= 0 && quorumIndex <= runConfig.testConfig.numClients / 4) {
+                    quorum.off("removeMember", scheduleLeave);
+                    const leaveTime = runConfig.testConfig.readWriteCycleMs * random.real(0, 5)(runConfig.randEng);
+                    printStatus(runConfig, `closing in ${(leaveTime / 60000).toString().substring(0, 4)} min`);
                     setTimeout(
-                        ()=>{
-                            if(!container.closed) {
+                        () => {
+                            if (!container.closed) {
                                 container.close();
                             }
                         },
@@ -274,15 +280,17 @@ function scheduleContainerClose(container: Container, runConfig: IRunConfig) {
         };
         quorum.on("removeMember", scheduleLeave);
         scheduleLeave();
-    }).catch(async (e)=>{
-        await loggerP.then(async (l)=>l.sendErrorEvent({eventName: "ScheduleLeaveFailed", runId: runConfig.runId}, e));
+    }).catch(async (e) => {
+        await loggerP.then(async (l) => l.sendErrorEvent({
+            eventName: "ScheduleLeaveFailed", runId: runConfig.runId,
+        }, e));
     });
 }
 
 main()
-.catch(
-    (error) => {
-        console.error(error);
-        process.exit(-1);
-    },
-);
+    .catch(
+        (error) => {
+            console.error(error);
+            process.exit(-1);
+        },
+    );

packages/test/test-driver-definitions/src/interfaces.ts
@@ -15,6 +15,11 @@ export interface ITestDriver{
     readonly type: TestDriverTypes;
 
     /**
+     * Specific endpoint name if there are any
+     */
+    readonly endpointName?: string;
+
+    /**
      * The semantic version of the test drivers package.
      * In general this version will match that of the  client
      * interfaces and implementation exposed and used by the test driver.

packages/test/test-drivers/src/routerliciousTestDriver.ts
@@ -82,6 +82,7 @@ export class RouterliciousTestDriver implements ITestDriver {
             tenantSecret,
             serviceEndpoint,
             api,
+            config?.r11sEndpointName,
         );
     }
 
@@ -92,6 +93,7 @@ export class RouterliciousTestDriver implements ITestDriver {
         private readonly tenantSecret: string,
         private readonly serviceEndpoints: IServiceEndpoint,
         private readonly api: RouterliciousDriverApiType = RouterliciousDriverApi,
+        public readonly endpointName?: string,
     ) {
     }
 

packages/test/test-end-to-end-tests/src/test/attachRegisterLocalApiTests.spec.ts
@@ -20,7 +20,6 @@ import { SharedObject } from "@fluidframework/shared-object-base";
 import { IContainerRuntimeBase } from "@fluidframework/runtime-definitions";
 import { SharedMap } from "@fluidframework/map";
 import { requestFluidObject } from "@fluidframework/runtime-utils";
-import { ChildLogger } from "@fluidframework/telemetry-utils";
 import { describeNoCompat } from "@fluidframework/test-version-utils";
 
 // REVIEW: enable compat testing?
@@ -66,7 +65,6 @@ describeNoCompat(`Attach/Bind Api Tests For Attached Container`, (getTestObjectP
             [mapId1, SharedMap.getFactory()],
             [mapId2, SharedMap.getFactory()],
         ]);
-        const driver = provider.driver;
         const urlResolver = provider.urlResolver;
         const codeLoader = new LocalCodeLoader([[codeDetails, factory]]);
         const documentServiceFactory = provider.documentServiceFactory;
@@ -74,7 +72,7 @@ describeNoCompat(`Attach/Bind Api Tests For Attached Container`, (getTestObjectP
             urlResolver,
             documentServiceFactory,
             codeLoader,
-            logger: ChildLogger.create(getTestLogger?.(), undefined, {all: {driverType: driver.type}}),
+            logger: provider.logger,
         });
         loaderContainerTracker.add(testLoader);
         return testLoader;

packages/test/test-end-to-end-tests/src/test/container.spec.ts
@@ -33,7 +33,6 @@ import {
 } from "@fluidframework/test-utils";
 import { ensureFluidResolvedUrl } from "@fluidframework/driver-utils";
 import { requestFluidObject } from "@fluidframework/runtime-utils";
-import { ChildLogger } from "@fluidframework/telemetry-utils";
 import {
     getDataStoreFactory,
     ITestDataObject,
@@ -59,7 +58,7 @@ describeNoCompat("Container", (getTestObjectProvider) => {
     });
     before(async ()=>{
         const loader = new Loader({
-            logger: ChildLogger.create(getTestLogger?.(), undefined, { all: { driverType: provider.driver.type } }),
+            logger: provider.logger,
             urlResolver: provider.urlResolver,
             documentServiceFactory:
                 provider.documentServiceFactory,
@@ -75,7 +74,7 @@ describeNoCompat("Container", (getTestObjectProvider) => {
     async function loadContainer(props?: Partial<ILoaderProps>) {
         const loader = new Loader({
             ...props,
-            logger: ChildLogger.create(getTestLogger?.(), undefined, { all: { driverType: provider.driver.type } }),
+            logger: provider.logger,
             urlResolver: props?.urlResolver ?? provider.urlResolver,
             documentServiceFactory:
                 props?.documentServiceFactory ?? provider.documentServiceFactory,

packages/test/test-end-to-end-tests/src/test/deRehydrateContainerTests.spec.ts
@@ -29,7 +29,6 @@ import { ConsensusQueue, ConsensusOrderedCollection } from "@fluidframework/orde
 import { SharedCounter } from "@fluidframework/counter";
 import { IRequest, IFluidCodeDetails } from "@fluidframework/core-interfaces";
 import { requestFluidObject } from "@fluidframework/runtime-utils";
-import { ChildLogger } from "@fluidframework/telemetry-utils";
 import { describeFullCompat } from "@fluidframework/test-version-utils";
 // eslint-disable-next-line import/no-internal-modules
 import { getSnapshotTreeFromSerializedContainer } from "@fluidframework/container-loader/dist/utils";
@@ -122,7 +121,7 @@ describeFullCompat(`Dehydrate Rehydrate Container Test`, (getTestObjectProvider)
             urlResolver: provider.urlResolver,
             documentServiceFactory: provider.documentServiceFactory,
             codeLoader,
-            logger: ChildLogger.create(getTestLogger?.(), undefined, {all: {driverType: provider.driver.type}}),
+            logger: provider.logger,
         });
         loaderContainerTracker.add(testLoader);
         return testLoader;

packages/test/test-end-to-end-tests/src/test/error.spec.ts
@@ -19,7 +19,7 @@ import {
 import {
     createOdspNetworkError,
 } from "@fluidframework/odsp-doclib-utils";
-import { ChildLogger, isILoggingError, normalizeError } from "@fluidframework/telemetry-utils";
+import { isILoggingError, normalizeError } from "@fluidframework/telemetry-utils";
 import {
     createDocumentId,
     LocalCodeLoader,
@@ -69,7 +69,7 @@ describeNoCompat("Errors Types", (getTestObjectProvider) => {
             urlResolver,
             documentServiceFactory: mockFactory,
             codeLoader,
-            logger: ChildLogger.create(getTestLogger?.(), undefined, { all: { driverType: provider.driver.type } }),
+            logger: provider.logger,
         });
         loaderContainerTracker.add(loader);
 

commit 9c75bbf562cf624e9919df872d9bab56d6478895

Test variant logging, extend timeout, and restore type (#7459)
- Restore the typing for createFluidTestDriver because it broke type narrowing in PR #7319 , but that means we need to force the type of the variable to `ITestDriver` in `test-service-load/src/runner.ts`.
- extend timeout (for now) to make e2e test pass.  Long term, we need to break the test into stages. (and investigate why it takes so long)
- Add `testVariant` to the `Test_start`/`Test_end` events.

examples/data-objects/table-document/src/test/.mocharc.js
@@ -7,6 +7,6 @@
 
 const packageDir = `${__dirname}/../..`;
 
-const getFluidTestMochaConfig = require("@fluidframework/mocha-test-setup/mocharc-common.js");
-const config = getFluidTestMochaConfig(packageDir, ["@fluidframework/test-version-utils"]);
+const common = require("@fluidframework/mocha-test-setup/mocharc-common.js");
+const config = common.getFluidTestMochaConfig(packageDir, ["@fluidframework/test-version-utils"]);
 module.exports = config;

examples/data-objects/webflow/src/test/.mocharc.js
@@ -7,8 +7,8 @@
 
 const packageDir = `${__dirname}/../..`;
 
-const getFluidTestMochaConfig = require("@fluidframework/mocha-test-setup/mocharc-common.js");
-const config = getFluidTestMochaConfig(packageDir,
+const common = require("@fluidframework/mocha-test-setup/mocharc-common.js");
+const config = common.getFluidTestMochaConfig(packageDir,
     [
         "@fluidframework/test-version-utils",
         "ignore-styles",

experimental/dds/tree/src/test/.mocharc.js
@@ -7,6 +7,6 @@
 
 const packageDir = `${__dirname}/../..`;
 
-const getFluidTestMochaConfig = require('@fluidframework/mocha-test-setup/mocharc-common.js');
-const config = getFluidTestMochaConfig(packageDir);
+const common = require('@fluidframework/mocha-test-setup/mocharc-common.js');
+const config = common.getFluidTestMochaConfig(packageDir);
 module.exports = config;

packages/test/mocha-test-setup/mocharc-common.js
@@ -8,12 +8,16 @@
 const { existsSync } = require("fs");
 const path = require("path");
 
-function getFluidTestMochaConfig(packageDir, additionalRequiredModules) {
-
+function getFluidTestVariant() {
     const testDriver = process.env.fluid__test__driver ? process.env.fluid__test__driver : "local";
     const r11sEndpointName = process.env.fluid__test__r11sEndpointName;
     const testVariant = (testDriver === "r11s" || testDriver === "routerlicious")
         && (r11sEndpointName !== undefined && r11sEndpointName !== "r11s") ? `r11s-${r11sEndpointName}` : testDriver;
+    return testVariant;
+}
+
+function getFluidTestMochaConfig(packageDir, additionalRequiredModules) {
+
     const moduleDir = `${packageDir}/node_modules`;
 
     const requiredModules = [
@@ -52,6 +56,7 @@ function getFluidTestMochaConfig(packageDir, additionalRequiredModules) {
     }
 
     if (process.env.FLUID_TEST_REPORT === "1") {
+        const testVariant = getFluidTestVariant();
         const packageJson = require(`${packageDir}/package.json`);
         config["reporter"] = `xunit`;
         config["reporter-options"] = [
@@ -63,4 +68,4 @@ function getFluidTestMochaConfig(packageDir, additionalRequiredModules) {
     return config;
 }
 
-module.exports = getFluidTestMochaConfig;
+module.exports = { getFluidTestMochaConfig, getFluidTestVariant };

packages/test/mocha-test-setup/src/mochaHooks.ts
@@ -6,7 +6,10 @@
 import { ITelemetryBufferedLogger } from "@fluidframework/test-driver-definitions";
 import { ITelemetryBaseEvent } from "@fluidframework/common-definitions";
 import { Context } from "mocha";
+import { getFluidTestVariant } from "../mocharc-common";
 import { pkgName } from "./packageVersion";
+
+const testVariant = getFluidTestVariant();
 
 const _global: any = global;
 class TestLogger implements ITelemetryBufferedLogger {
@@ -19,6 +22,7 @@ class TestLogger implements ITelemetryBufferedLogger {
         if (this.testName !== undefined) {
             event.testName = this.testName;
         }
+        event.testVariant = testVariant;
         event.hostName = pkgName;
         this.parentLogger.send(event);
     }
@@ -67,6 +71,7 @@ export const mochaHooks = {
             category: "generic",
             eventName: "fluid:telemetry:Test_start",
             testName: currentTestName,
+            testVariant,
         });
     },
     afterEach() {
@@ -78,6 +83,7 @@ export const mochaHooks = {
             testName: currentTestName,
             state: context.currentTest?.state,
             duration: context.currentTest?.duration,
+            testVariant,
         });
 
         console.log = log;

packages/test/test-drivers/src/factory.ts
@@ -6,7 +6,7 @@
 import http from "http";
 import * as path from "path";
 import Axios from "axios";
-import { ITestDriver, TestDriverTypes } from "@fluidframework/test-driver-definitions";
+import { TestDriverTypes } from "@fluidframework/test-driver-definitions";
 import { unreachableCase } from "@fluidframework/common-utils";
 import { LocalServerTestDriver } from "./localServerTestDriver";
 import { TinyliciousTestDriver } from "./tinyliciousTestDriver";
@@ -62,7 +62,7 @@ export async function createFluidTestDriver(
     fluidTestDriverType: TestDriverTypes = "local",
     config?: FluidTestDriverConfig,
     api: DriverApiType = DriverApi,
-): Promise<ITestDriver> {
+): Promise<LocalServerTestDriver | TinyliciousTestDriver | RouterliciousTestDriver | OdspTestDriver> {
     switch (fluidTestDriverType) {
         case "local":
             return new LocalServerTestDriver(api.LocalDriverApi);

packages/test/test-end-to-end-tests/src/test/.mocharc.js
@@ -7,8 +7,8 @@
 
 const packageDir = `${__dirname}/../..`;
 
-const getFluidTestMochaConfig = require("@fluidframework/mocha-test-setup/mocharc-common.js");
-const config = getFluidTestMochaConfig(packageDir, [
+const common = require("@fluidframework/mocha-test-setup/mocharc-common.js");
+const config = common.getFluidTestMochaConfig(packageDir, [
     "@fluidframework/test-version-utils"
 ]);
 module.exports = config;

packages/test/test-service-load/src/runner.ts
@@ -4,7 +4,7 @@
  */
 
 import commander from "commander";
-import { TestDriverTypes } from "@fluidframework/test-driver-definitions";
+import { ITestDriver, TestDriverTypes } from "@fluidframework/test-driver-definitions";
 import { Container, Loader } from "@fluidframework/container-loader";
 import random from "random-js";
 import { ChildLogger } from "@fluidframework/telemetry-utils";
@@ -129,7 +129,7 @@ async function runnerProcess(
         const loaderOptions = generateLoaderOptions(seed);
         const containerOptions = generateRuntimeOptions(seed);
 
-        const testDriver = await createTestDriver(driver, seed, runConfig.runId);
+        const testDriver: ITestDriver = await createTestDriver(driver, seed, runConfig.runId);
         const baseLogger = await loggerP;
         const logger = ChildLogger.create(baseLogger, undefined,
             {

tools/pipelines/test-real-service.yml
@@ -97,7 +97,7 @@ stages:
         poolBuild: Lite
         testPackage: "@fluidframework/test-end-to-end-tests"
         testWorkspace: ${{ variables.testWorkspace }}
-        timeoutInMinutes: 240
+        timeoutInMinutes: 360
         testSteps:
         - task: Npm@1
           displayName: '[end-to-end tests] npm run test:realsvc:frs:report'
@@ -120,7 +120,7 @@ stages:
         poolBuild: Lite
         testPackage: "@fluidframework/test-end-to-end-tests"
         testWorkspace: ${{ variables.testWorkspace }}
-        timeoutInMinutes: 240
+        timeoutInMinutes: 360
         testSteps:
         - task: Npm@1
           displayName: '[end-to-end tests] npm run test:realsvc:odsp:report'