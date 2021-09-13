b69bc06997293f1b2ef7089c4af0ed4a916e60a5
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-09-13 13:36:19 -0700

    Fix copyright whitespace

--- experimental/examples/ordered-list/src/test/orderedlist.spec.ts
+++ experimental/examples/ordered-list/src/test/orderedlist.spec.ts
@@ -1,7 +1,7 @@
 /*!
-* Copyright (c) Microsoft Corporation and contributors. All rights reserved.
-* Licensed under the MIT License.
-*/
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
 
 import { strict as assert } from "assert";
 import { requestFluidObject } from "@fluidframework/runtime-utils";
