56ba0f2e97b31ef5e812fbde9d79939b6ce6598a
Micah Godbolt <mgodbolt@microsoft.com>
2021-09-27 13:07:20 -0700

    [bump] package version for client (patch)
                @fluidframework/build-common:     0.24.0 (unchanged)
         @fluidframework/eslint-config-fluid:     0.24.0 (unchanged)
          @fluidframework/common-definitions:     0.21.0 (unchanged)
                @fluidframework/common-utils:     0.33.0 (unchanged)
       @fluidframework/container-definitions:     0.40.0 (unchanged)
             @fluidframework/core-interfaces:     0.40.0 (unchanged)
          @fluidframework/driver-definitions:     0.40.0 (unchanged)
        @fluidframework/protocol-definitions:   0.1025.0 (unchanged)
                                      Server:   0.1031.1 (unchanged)
                                      Client:     0.48.2 -> 0.48.3
                      @fluid-tools/benchmark:     0.40.0 (unchanged)
                             generator-fluid:      0.3.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
                                 dice-roller:      0.0.1 (unchanged)

--- packages/drivers/odsp-urlResolver/src/packageVersion.ts
+++ packages/drivers/odsp-urlResolver/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/odsp-urlresolver";
-export const pkgVersion = "0.48.2";
+export const pkgVersion = "0.48.3";
--- packages/drivers/routerlicious-urlResolver/src/packageVersion.ts
+++ packages/drivers/routerlicious-urlResolver/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/routerlicious-urlresolver";
-export const pkgVersion = "0.48.2";
+export const pkgVersion = "0.48.3";
