a91fed6103e430eb4ce3e57b260ff39a74ad4545
Gary Wilber <garywilb@microsoft.com>
2021-09-29 10:48:56 -0700

    more comments

--- server/routerlicious/packages/lambdas/src/deli/lambda.ts
+++ server/routerlicious/packages/lambdas/src/deli/lambda.ts
@@ -685,7 +685,9 @@ export class DeliLambda extends TypedEventEmitter<IDeliLambdaEvents> implements 
 
                         if (this.serviceConfiguration.deli.opEvent.enable) {
                             // ops were reliably stored
-                            this.emitOpEvent(OpEventType.UpdatedDurableSequenceNumber);
+                            // ensure op event timers & last sequenced op counters are reset
+                            // that will make the MaxTime & MaxOps op events will be accurate
+                            this.emitOpEvent(OpEventType.UpdatedDurableSequenceNumber, true);
                         }
                     }
 
@@ -1084,7 +1086,7 @@ export class DeliLambda extends TypedEventEmitter<IDeliLambdaEvents> implements 
 
     /**
      * Resets the op event MaxTime timer
-     * Called after an opEvent is emitted or when the dsn is updated
+     * Called after an opEvent is emitted
      */
     private updateOpMaxTimeTimer() {
         const maxTime = this.serviceConfiguration.deli.opEvent.maxTime;
@@ -1107,11 +1109,11 @@ export class DeliLambda extends TypedEventEmitter<IDeliLambdaEvents> implements 
     }
 
     /**
-     * Emits an opEvent based for the provided type
+     * Emits an opEvent for the provided type
      * Also resets the MaxTime timer
      */
-    private emitOpEvent(type: OpEventType) {
-        if (this.sequencedMessagesSinceLastOpEvent === 0) {
+    private emitOpEvent(type: OpEventType, force?: boolean) {
+        if (!force && this.sequencedMessagesSinceLastOpEvent === 0) {
             // no need to emit since no messages were handled since last time
             return;
         }
