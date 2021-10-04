28060e4af472c6969582498844e37c816828c492
appym31 <aparnamahadevan31@gmail.com>
2021-10-03 19:36:40 -0700

    fix comments

--- server/routerlicious/packages/services-utils/src/scheduleJob.ts
+++ server/routerlicious/packages/services-utils/src/scheduleJob.ts
@@ -5,34 +5,23 @@
 
 import { Lumberjack } from "@fluidframework/server-services-telemetry";
 import * as cron from "node-cron";
-import { ILogger } from "@fluidframework/server-services-core";
-import { Provider } from "nconf";
 
-export async function scheduleJob<T>(
-    api: () => Promise<T>,
-    config: Provider,
-    featureGateConfigName: string,
+export async function scheduleJob(
+    api: () => void,
+    cronExpression: string,
     callName: string,
-    cronExpressionConfigName: string,
-    logger?: ILogger,
-): Promise<any> {
-    let task;
-    if (config.get(featureGateConfigName)) {
-        const cronExpression = config.get(cronExpressionConfigName);
-        try {
-            if (cron.validate(cronExpression)) {
-                task = cron.schedule(cronExpression, api);
-                logger?.info(`Task ${callName} is scheduled, cron expression is ${cronExpression}`);
-                Lumberjack.info(`Task ${callName} is scheduled, cron expression is ${cronExpression}`);
-            } else {
-                return Promise.reject(new Error(`Invalid cron expression ${cronExpression}`));
-            }
-        } catch (error) {
-            logger?.error(`Error scheduling cron job for ${callName}, error ${error}`);
-            Lumberjack.error(`Error scheduling cron job for ${callName}`, undefined, error);
-            return Promise.reject(error);
+): Promise<cron.ScheduledTask> {
+    let task: cron.ScheduledTask;
+    try {
+        if (cron.validate(cronExpression)) {
+            task = cron.schedule(cronExpression, api);
+            Lumberjack.info(`Task ${callName} is scheduled, cron expression is ${cronExpression}`);
+        } else {
+            throw new Error(`Invalid cron expression ${cronExpression}`);
         }
+    } catch (error) {
+        Lumberjack.error(`Error scheduling cron job for ${callName}`, undefined, error);
+        return Promise.reject(error);
     }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return task;
 }
