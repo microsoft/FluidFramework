2023985f1415c6f5f1252e504e7f2678801c22d1
appym31 <aparnamahadevan31@gmail.com>
2021-10-06 23:46:21 -0700

    include retry and backoff logic

--- server/routerlicious/packages/services-utils/src/executeOnInterval.ts
+++ server/routerlicious/packages/services-utils/src/executeOnInterval.ts
@@ -5,24 +5,47 @@
 
 import { Lumberjack } from "@fluidframework/server-services-telemetry";
 
-export async function executeOnInterval<T>(
-    api: () => Promise<T>,
+export function executeOnInterval(
+    api: () => Promise<any>,
     intervalInMs: number,
-    callName: string): Promise<void> {
-    await api()
+    callName: string,
+    retryCount = 0,
+    onErrorFn?: () => void,
+    shouldRetryError?: (error) => boolean,
+    shouldBackOffRetry?: (error, retryCount) => boolean): void {
+    let error;
+    let retries = retryCount;
+    let interval = intervalInMs;
+    api()
         .then((res) => {
             Lumberjack.info(`Success executing ${callName}`, undefined);
+            // reset the retries count if api call succeeded
+            retries = 0;
         })
-        .catch((error) => {
-            Lumberjack.error(`Error running ${callName}`, undefined, error);
+        .catch((err) => {
+            error = err;
+            Lumberjack.error(`Error running ${callName}`, undefined, err);
+            if (onErrorFn !== undefined) {
+                onErrorFn();
+            }
         })
         .finally(() => {
-            if (intervalInMs) {
-                const timeoutP = async () => new Promise((resolve) => {
-                    setTimeout(resolve, intervalInMs);
-                });
-                // eslint-disable-next-line @typescript-eslint/no-floating-promises
-                timeoutP().then(async () => executeOnInterval(api, intervalInMs, callName));
+            if (error === undefined || shouldRetryError === undefined) {
+                // if no error or no retry logic is defined, always make the next call (retries will be 0)
+                setTimeout(() => executeOnInterval(api, intervalInMs, callName, retries), intervalInMs);
+                return;
             }
+
+            if (!shouldRetryError(error)) {
+                return;
+            }
+
+            Lumberjack.info(`Will retry error ${error}, retryCount is ${retries}`);
+            if (shouldBackOffRetry !== undefined && shouldBackOffRetry(error, retries)) {
+                interval = interval * 2 ** retries;
+                Lumberjack.info(`Will backoff next retry, interval is ${interval} milliseconds`);
+            }
+            retries += 1;
+            setTimeout(() => executeOnInterval(api, intervalInMs, callName, retries), interval);
         });
 }
