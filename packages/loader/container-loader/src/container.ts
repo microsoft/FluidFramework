microsoft/FluidFramework

commit b02ca5c244082bf1e7fc273d373d548ad245ed85

Make sendTelemetryEvent always use category = "generic" to keep caller in full control (#7363)
Found another place in code where intention was to use "generic" category but it ended up being "error" due to error passed in. So instead of keep patching these by caller overwriting (default) category, make it more deterministic on what category is used.

packages/utils/telemetry-utils/src/logger.ts
@@ -13,6 +13,7 @@ import {
     ITelemetryProperties,
     TelemetryEventPropertyType,
     ITaggedTelemetryPropertyType,
+    TelemetryEventCategory,
 } from "@fluidframework/common-definitions";
 import { BaseTelemetryNullLogger, performance } from "@fluidframework/common-utils";
 import {
@@ -123,10 +124,20 @@ export abstract class TelemetryLogger implements ITelemetryLogger {
      * @param error - optional error object to log
      */
     public sendTelemetryEvent(event: ITelemetryGenericEvent, error?: any) {
-        const newEvent: ITelemetryBaseEvent = {
-            ...event,
-            category: event.category ?? (error === undefined ?  "generic" : "error"),
-        };
+        this.sendTelemetryEventCore({ ...event, category: "generic"}, error);
+    }
+
+    /**
+     * Send a telemetry event with the logger
+     *
+     * @param event - the event to send
+     * @param error - optional error object to log
+     */
+     protected sendTelemetryEventCore(
+        event: ITelemetryGenericEvent & { category: TelemetryEventCategory },
+        error?: any)
+    {
+        const newEvent = { ...event };
         if (error !== undefined) {
             TelemetryLogger.prepareErrorObject(newEvent, error, false);
         }
@@ -146,7 +157,7 @@ export abstract class TelemetryLogger implements ITelemetryLogger {
      * @param error - optional error object to log
      */
     public sendErrorEvent(event: ITelemetryErrorEvent, error?: any) {
-        this.sendTelemetryEvent({ ...event, category: "error" }, error);
+        this.sendTelemetryEventCore({ ...event, category: "error" }, error);
     }
 
     /**
@@ -156,12 +167,12 @@ export abstract class TelemetryLogger implements ITelemetryLogger {
      * @param error - optional error object to log
      */
     public sendPerformanceEvent(event: ITelemetryPerformanceEvent, error?: any): void {
-        const perfEvent: ITelemetryGenericEvent = {
+        const perfEvent = {
             ...event,
             category: event.category ? event.category : "performance",
         };
 
-        this.sendTelemetryEvent(perfEvent, error);
+        this.sendTelemetryEventCore(perfEvent, error);
     }
 
     protected prepareEvent(event: ITelemetryBaseEvent): ITelemetryBaseEvent {

packages/loader/container-utils/src/error.ts
@@ -204,7 +204,6 @@ export function wrapErrorAndLog<T extends IFluidErrorBase>(
     logger.sendTelemetryEvent({
         eventName: "WrapError",
         wrappedByErrorInstanceId,
-        category: "generic", // avoids it being an "error" category due to  supplied error
     }, innerError);
 
     return newError;

packages/runtime/container-runtime/src/containerRuntime.ts
@@ -1044,7 +1044,6 @@ export class ContainerRuntime extends TypedEventEmitter<IContainerRuntimeEvents>
 
         this.logger.sendTelemetryEvent({
             eventName: "ContainerRuntimeDisposed",
-            category: "generic",
             isDirty: this.isDirty,
             lastSequenceNumber: this.deltaManager.lastSequenceNumber,
             attachState: this.attachState,

packages/runtime/container-runtime/src/summaryGenerator.ts
@@ -340,7 +340,7 @@ export class SummaryGenerator {
                 const retryAfterSeconds = summaryNack?.retryAfter;
 
                 const error = new LoggingError(`summaryNack: ${message}`, { retryAfterSeconds });
-                logger.sendTelemetryEvent(
+                logger.sendErrorEvent(
                     { eventName: "SummaryNack", ...generateTelemetryProps, retryAfterSeconds }, error);
                 assert(getRetryDelaySecondsFromError(error) === retryAfterSeconds, 0x25f /* "retryAfterSeconds" */);
                 // This will only set resultsBuilder.receivedSummaryAckOrNack, as other promises are already set.

packages/runtime/runtime-utils/src/summarizerNode/summarizerNodeUtils.ts
@@ -186,7 +186,6 @@ export function decodeSummary(
                             if (newEarliestSeq <= latestSeq) {
                                 logger.sendTelemetryEvent({
                                     eventName:"DuplicateOutstandingOps",
-                                    category: "generic",
                                     // eslint-disable-next-line max-len
                                     message: `newEarliestSeq <= latestSeq in decodeSummary: ${newEarliestSeq} <= ${latestSeq}`,
                                 });

packages/tools/replay-tool/src/replayMessages.ts
@@ -494,7 +494,7 @@ export class ReplayTool {
                         this.documentsFromStorageSnapshots.push(doc);
                     }
                 } catch (error) {
-                    doc.logger.sendTelemetryEvent({ eventName: "FailedToLoadSnapshot" }, error);
+                    doc.logger.sendErrorEvent({ eventName: "FailedToLoadSnapshot" }, error);
                 }
             }
             this.documentsFromStorageSnapshots.sort((a: Document, b: Document) => a.fromOp > b.fromOp ? 1 : -1);

commit 6d7686cc080503d9adb32a06aa4959b8fa10692a

Making sure that container errors are reported correctly on close (#7773)
Noticed by looking at telemetry and not finding errors there :(
That's a regression from #7363
We could force callers to use right API instead of overwriting category, but we already allow that for "performance" events, and the code is a bit shorter than way, so I feel supporting this flow is better.

packages/loader/container-loader/src/container.ts
@@ -797,6 +797,7 @@ export class Container extends EventEmitterWithErrorHandling<IContainerEvents> i
             {
                 eventName: "ContainerClose",
                 loaded: this.loaded,
+                category: error === undefined ? "generic" : "error",
             },
             error,
         );

packages/utils/telemetry-utils/src/logger.ts
@@ -124,7 +124,7 @@ export abstract class TelemetryLogger implements ITelemetryLogger {
      * @param error - optional error object to log
      */
     public sendTelemetryEvent(event: ITelemetryGenericEvent, error?: any) {
-        this.sendTelemetryEventCore({ ...event, category: "generic"}, error);
+        this.sendTelemetryEventCore({ ...event, category: event.category ?? "generic" }, error);
     }
 
     /**
@@ -169,7 +169,7 @@ export abstract class TelemetryLogger implements ITelemetryLogger {
     public sendPerformanceEvent(event: ITelemetryPerformanceEvent, error?: any): void {
         const perfEvent = {
             ...event,
-            category: event.category ? event.category : "performance",
+            category: event.category ?? "performance",
         };
 
         this.sendTelemetryEventCore(perfEvent, error);