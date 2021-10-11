6f44e825f7edabb9b0d70b388984d9e5d58b6182
Jatin Garg <48029724+jatgarg@users.noreply.github.com>
2021-10-11 14:57:03 -0700

    Update protocol definitions dependencies (#7805)

--- packages/test/test-end-to-end-tests/src/test/blobs.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/blobs.spec.ts
@@ -39,7 +39,7 @@ class MockDetachedBlobStorage implements IDetachedBlobStorage {
     public async createBlob(content: ArrayBufferLike): Promise<ICreateBlobResponse> {
         const id = this.size.toString();
         this.blobs.set(id, content);
-        return { id };
+        return { id, url: "" };
     }
 
     public async readBlob(blobId: string): Promise<ArrayBufferLike> {
