ac6f57bf95d8ed2eb97f924f882eebdcf7c3f38d
Andrei Iacob <andreiiacob@microsoft.com>
2021-10-14 10:31:52 -0700

    Try waiting a turn

--- packages/test/local-server-tests/src/test/opsOnReconnect.spec.ts
+++ packages/test/local-server-tests/src/test/opsOnReconnect.spec.ts
@@ -619,8 +619,11 @@ describe("Ops on Reconnect", () => {
                 ["key2", "value2", undefined /* batch */],
                 ["key3", "value3", false /* batch */],
             ];
-            assert.deepStrictEqual(
-                receivedValues, expectedValues, "Did not receive the ops that were re-sent");
+
+            setTimeout(() => {
+                assert.deepStrictEqual(
+                    receivedValues, expectedValues, "Did not receive the ops that were re-sent");
+            }, 0);
         });
     });
 
