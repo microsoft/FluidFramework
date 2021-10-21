15dc922cc41dabb2daa5dbaa86e922758fb0b4a0
appym31 <aparnamahadevan31@gmail.com>
2021-10-20 17:34:23 -0700

    test op deletion

--- server/routerlicious/packages/routerlicious/src/scriptorium/index.ts
+++ server/routerlicious/packages/routerlicious/src/scriptorium/index.ts
@@ -5,18 +5,26 @@
 
 import { ScriptoriumLambdaFactory } from "@fluidframework/server-lambdas";
 import * as services from "@fluidframework/server-services";
-import { IPartitionLambdaFactory, MongoManager } from "@fluidframework/server-services-core";
+import { ICollection, IDocument, IPartitionLambdaFactory, MongoManager } from "@fluidframework/server-services-core";
+import { deleteSummarizedOps, executeOnInterval } from "@fluidframework/server-services-utils";
 import { Provider } from "nconf";
 
 export async function create(config: Provider): Promise<IPartitionLambdaFactory> {
     const mongoUrl = config.get("mongo:endpoint") as string;
     const mongoExpireAfterSeconds = config.get("mongo:expireAfterSeconds") as number;
     const deltasCollectionName = config.get("mongo:collectionNames:deltas");
+    const documentsCollectionName = config.get("mongo:collectionNames:documents");
     const createCosmosDBIndexes = config.get("mongo:createCosmosDBIndexes");
+    const softDeletionRetentionPeriodMs = config.get("mongo:softDeletionRetentionPeriodMs");
+    const offlineWindowMs = config.get("mongo:offlineWindowMs");
+    const softDeletionEnabled = config.get("mongo:softDeletionEnabled");
+    const permanentDeletionEnabled = config.get("mongo:permanentDeletionEnabled");
+    const deletionIntervalMs = config.get("mongo:deletionIntervalMs");
     const mongoFactory = new services.MongoDbFactory(mongoUrl);
     const mongoManager = new MongoManager(mongoFactory, false);
 
     const db = await mongoManager.getDatabase();
+    const documentsCollection: ICollection<IDocument> = db.collection(documentsCollectionName);
     const opCollection = db.collection(deltasCollectionName);
     await opCollection.createIndex(
         {
@@ -50,6 +58,19 @@ export async function create(config: Provider): Promise<IPartitionLambdaFactory>
             );
         }
     }
+
+    executeOnInterval(
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        async () => deleteSummarizedOps(
+            opCollection,
+            documentsCollection,
+            softDeletionRetentionPeriodMs,
+            offlineWindowMs,
+            softDeletionEnabled,
+            permanentDeletionEnabled),
+        deletionIntervalMs,
+        "deleteSummarizedOps",
+    );
 
     return new ScriptoriumLambdaFactory(mongoManager, opCollection);
 }
