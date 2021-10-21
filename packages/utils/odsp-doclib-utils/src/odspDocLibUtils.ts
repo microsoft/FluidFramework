19f7887b23317e780646227a68d819a3c18e4fcc
Gary Wilber <garywilb@microsoft.com>
2021-10-21 15:49:39 -0700

    Add support for running load test tool directly against PushChannel

--- packages/utils/odsp-doclib-utils/src/odspDocLibUtils.ts
+++ packages/utils/odsp-doclib-utils/src/odspDocLibUtils.ts
@@ -8,11 +8,37 @@ const odspTenants = new Map<string, string>([
     ["spo-df", "microsoft-my.sharepoint-df.com"],
 ]);
 
-export function isOdspHostname(hostname: string) {
-    return hostname.endsWith("sharepoint.com") || hostname.endsWith("sharepoint-df.com");
+export function isOdspHostname(server: string) {
+    return server.endsWith("sharepoint.com") || server.endsWith("sharepoint-df.com");
 }
 
-export function getAadTenant(hostname: string) {
+export function isPushChannelHostname(server: string) {
+    return server === "localhost:3000" || (server.includes(".push") && server.endsWith(".svc.ms"));
+}
+
+export function getAadUrl(server: string) {
+    if (isPushChannelHostname(server)) {
+        // special case for local / pushchannel testing
+        // if the SPO url is pushchannel, use the pushchannel AAD mock
+        if (server === "localhost:3000") {
+            return `http://localhost:3000`;
+        }
+
+        return getSiteUrl(server);
+    }
+
+    return `https://login.microsoftonline.com`;
+}
+
+export function getAadTenant(server: string) {
+    let hostname = server;
+
+    if (hostname.startsWith("http://")) {
+        hostname = hostname.substring(7);
+    } else if (hostname.startsWith("https://")) {
+        hostname = hostname.substring(8);
+    }
+
     let tenantName = hostname.substr(0, hostname.indexOf(".")).toLowerCase();
     let restOfTenantHostname = hostname.substr(tenantName.length).toLowerCase();
 
@@ -37,4 +63,14 @@ export function getServer(tenantId: string): string {
         throw Error(`Invalid SPO tenantId ${tenantId}.`);
     }
     return server;
+}
+
+export function getSiteUrl(server: string) {
+    if (server.startsWith("http://") || server.startsWith("https://")) {
+        // server is already a site url
+        return server;
+    }
+
+    // server is likely {tenantId}.sharepoint.com. add https to make it the site url
+    return `https://${server}`;
 }
