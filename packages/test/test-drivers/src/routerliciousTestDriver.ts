a1a26a6c3fe44e118ed76de90d0a2fd81d8e28e7
hedasilv <hedasilv@microsoft.com>
2021-10-22 12:49:46 -0700

    Allowing docker endpoint in r11s test driver to have configurable driver policies

--- packages/test/test-drivers/src/routerliciousTestDriver.ts
+++ packages/test/test-drivers/src/routerliciousTestDriver.ts
@@ -19,16 +19,16 @@ export interface IServiceEndpoint {
     deltaStorageUrl: string;
 }
 
-const dockerConfig = {
-    serviceEndpoint: {
-        hostUrl: "http://localhost:3000",
-        ordererUrl: "http://localhost:3003",
-        deltaStorageUrl: "http://localhost:3001",
-    },
-    tenantId: "fluid",
-    tenantSecret: "create-new-tenants-if-going-to-production",
-    driverPolicies: undefined,
-};
+const dockerConfig = (driverPolicies?: IRouterliciousDriverPolicies) => ({
+        serviceEndpoint: {
+            hostUrl: "http://localhost:3000",
+            ordererUrl: "http://localhost:3003",
+            deltaStorageUrl: "http://localhost:3001",
+        },
+        tenantId: "fluid",
+        tenantSecret: "create-new-tenants-if-going-to-production",
+        driverPolicies,
+    });
 
 function getConfig(
     fluidHost?: string,
@@ -59,6 +59,10 @@ function getLegacyConfigFromEnv() {
 
 function getEndpointConfigFromEnv(r11sEndpointName: string) {
     const configStr = process.env[`fluid__test__driver__${r11sEndpointName}`];
+    if (r11sEndpointName === "docker") {
+        const dockerDriverPolicies = configStr === undefined ? configStr : JSON.parse(configStr).driverpolicies;
+        return dockerConfig(dockerDriverPolicies);
+    }
     if (r11sEndpointName === "r11s" && configStr === undefined) {
         // Allow legacy setting from fluid__webpack__ for r11s for now
         return getLegacyConfigFromEnv();
@@ -75,9 +79,9 @@ function getConfigFromEnv(r11sEndpointName?: string) {
             // default to get it with the per service env for r11s
             return getEndpointConfigFromEnv("r11s");
         }
-        return fluidHost.includes("localhost") ? dockerConfig : getLegacyConfigFromEnv();
+        return fluidHost.includes("localhost") ? dockerConfig() : getLegacyConfigFromEnv();
     }
-    return r11sEndpointName === "docker" ? dockerConfig : getEndpointConfigFromEnv(r11sEndpointName);
+    return getEndpointConfigFromEnv(r11sEndpointName);
 }
 
 export class RouterliciousTestDriver implements ITestDriver {
