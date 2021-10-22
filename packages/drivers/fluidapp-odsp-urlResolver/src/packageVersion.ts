524402e883443a2562e812133be134d7ba6d8954
Si Chen <chensi@microsoft.com>
2021-10-22 12:18:59 -0500

    [bump] package version to 0.51.0 for development after client release
                @fluidframework/build-common:     0.24.0 (unchanged)
         @fluidframework/eslint-config-fluid:     0.24.0 -> 0.25.0
          @fluidframework/common-definitions:     0.21.0 (unchanged)
                @fluidframework/common-utils:     0.33.0 (unchanged)
             @fluidframework/core-interfaces:     0.40.0 -> 0.41.0
        @fluidframework/protocol-definitions:   0.1026.0 (unchanged)
          @fluidframework/driver-definitions:     0.41.0 -> 0.42.0
       @fluidframework/container-definitions:     0.41.0 -> 0.42.0
                                      Server:   0.1033.0 -> 0.1034.0
                                      Client:     0.50.0 -> 0.51.0
                      @fluid-tools/benchmark:     0.40.0 (unchanged)
                             generator-fluid:      0.3.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
         @fluidframework/azure-local-service:      0.1.0 (unchanged)
                                 dice-roller:      0.0.1 (unchanged)

--- packages/drivers/fluidapp-odsp-urlResolver/src/packageVersion.ts
+++ packages/drivers/fluidapp-odsp-urlResolver/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-internal/fluidapp-odsp-urlresolver";
-export const pkgVersion = "0.50.0";
+export const pkgVersion = "0.51.0";
--- packages/test/test-utils/src/packageVersion.ts
+++ packages/test/test-utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/test-utils";
-export const pkgVersion = "0.50.0";
+export const pkgVersion = "0.51.0";
