a657339af41c30cffbb5fbdd25a7498e609b1602
Mark Fields <markfields@users.noreply.github.com>
2021-11-08 13:33:26 -0800

    Let IFluidErrorBase extend Error directly to appease no-throw-literal eslint rule (#8153)

--- packages/loader/container-loader/src/deltaManager.ts
+++ packages/loader/container-loader/src/deltaManager.ts
@@ -761,7 +761,6 @@ export class DeltaManager
                     if (!canRetryOnError(origError)) {
                         const error = normalizeError(origError);
                         this.close(error);
-                        // eslint-disable-next-line @typescript-eslint/no-throw-literal
                         throw error;
                     }
 
