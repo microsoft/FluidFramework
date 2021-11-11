47e62a2d27148854a9149ae389113a4eee505287
Jatin Garg <jatgarg@microsoft.com>
2021-11-11 00:34:09 -0800

    pr sugg

--- common/lib/protocol-definitions/src/sockets.ts
+++ common/lib/protocol-definitions/src/sockets.ts
@@ -160,7 +160,8 @@ export interface IConnected {
 
     /**
      * Properties that server can send to client to tell info about node that client is connected to. For ex, for spo
-     * it could contain info like build version, environment, region etc.
+     * it could contain info like build version, environment, region etc. These properties can be logged by client
+     * to better understand server environment etc. and use it in case error occurs.
      * Format: "prop1:val1;prop2:val2;prop3:val3"
      */
     relayServiceAgent?: string,
