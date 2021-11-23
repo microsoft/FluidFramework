10e634e2a3c60f053b4e1c44ef2a3a5015ebd584
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2021-11-22 17:16:29 -0800

    use existing library

--- packages/runtime/test-runtime-utils/src/insecureTokenProvider.ts
+++ packages/runtime/test-runtime-utils/src/insecureTokenProvider.ts
@@ -3,9 +3,9 @@
  * Licensed under the MIT License.
  */
 
-import { ScopeType, ITokenClaims, IUser } from "@fluidframework/protocol-definitions";
+import { ScopeType, IUser } from "@fluidframework/protocol-definitions";
 import { ITokenProvider, ITokenResponse } from "@fluidframework/routerlicious-driver";
-import { KJUR as jsrsasign } from "jsrsasign";
+import { generateToken } from "@fluidframework/server-services-client";
 
 /**
  * As the name implies this is not secure and should not be used in production. It simply makes the example easier
@@ -22,36 +22,28 @@ export class InsecureTokenProvider implements ITokenProvider {
     public async fetchOrdererToken(tenantId: string, documentId: string): Promise<ITokenResponse> {
         return {
             fromCache: true,
-            jwt: this.getSignedToken(tenantId, documentId),
+            jwt: generateToken(tenantId, documentId, "fooBar", [
+                ScopeType.DocRead,
+                ScopeType.DocWrite,
+                ScopeType.SummaryWrite,
+            ]),
         };
     }
 
     public async fetchStorageToken(tenantId: string, documentId: string): Promise<ITokenResponse> {
         return {
             fromCache: true,
-            jwt: this.getSignedToken(tenantId, documentId),
+            jwt: generateToken(
+                tenantId,
+                documentId,
+                this.tenantKey,
+                [
+                    ScopeType.DocRead,
+                    ScopeType.DocWrite,
+                    ScopeType.SummaryWrite,
+                ],
+                this.user,
+            ),
         };
-    }
-
-    private getSignedToken(
-        tenantId: string,
-        documentId: string,
-        lifetime: number = 60 * 60,
-        ver: string = "1.0"): string {
-        // Current time in seconds
-        const now = Math.round((new Date()).getTime() / 1000);
-
-        const claims: ITokenClaims = {
-            documentId,
-            scopes: [ScopeType.DocRead, ScopeType.DocWrite, ScopeType.SummaryWrite],
-            tenantId,
-            user: this.user,
-            iat: now,
-            exp: now + lifetime,
-            ver,
-        };
-
-        const utf8Key = { utf8: this.tenantKey };
-        return jsrsasign.jws.JWS.sign(null, JSON.stringify({ alg:"HS256", typ: "JWT" }), claims, utf8Key);
     }
 }
