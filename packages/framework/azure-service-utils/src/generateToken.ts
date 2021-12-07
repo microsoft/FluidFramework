8526c0cdd07293dc22780830b2fb0c13f2f0846e
Sumedh Bhattacharya <sumbhatt@microsoft.com>
2021-12-07 13:30:19 -0800

    move generateToken to client side package

---
+++ packages/framework/azure-service-utils/src/generateToken.ts
@@ -0,0 +1,53 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { ITokenClaims, IUser, ScopeType } from "@fluidframework/protocol-definitions";
+import { KJUR as jsrsasign } from "jsrsasign";
+import { v4 as uuid } from "uuid";
+
+/**
+ * Generates a JWT token to authorize routerlicious. This function uses a browser friendly auth library (jsrsasign)
+ * and should only be used in client context.
+ */
+export function generateToken(
+    tenantId: string,
+    documentId: string,
+    key: string,
+    scopes: ScopeType[],
+    user?: IUser,
+    lifetime: number = 60 * 60,
+    ver: string = "1.0"): string {
+    let userClaim = (user) ? user : generateUser();
+    if (userClaim.id === "" || userClaim.id === undefined) {
+        userClaim = generateUser();
+    }
+
+    // Current time in seconds
+    const now = Math.round((new Date()).getTime() / 1000);
+
+    const claims: ITokenClaims & { jti: string } = {
+        documentId,
+        scopes,
+        tenantId,
+        user: userClaim,
+        iat: now,
+        exp: now + lifetime,
+        ver,
+        jti: uuid(),
+    };
+
+    const utf8Key = { utf8: key };
+    // eslint-disable-next-line no-null/no-null
+    return jsrsasign.jws.JWS.sign(null, JSON.stringify({ alg:"HS256", typ: "JWT" }), claims, utf8Key);
+}
+
+export function generateUser(): IUser {
+    const randomUser = {
+        id: uuid(),
+        name: uuid(),
+    };
+
+    return randomUser;
+}
