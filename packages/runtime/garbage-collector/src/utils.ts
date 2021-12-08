720a205073ee6e939461c4e9469ad1feb724a1b3
Navin Agarwal <45832642+agarwal-navin@users.noreply.github.com>
2021-12-08 13:40:18 -0800

    Moved writing GC blobs into the root of the summary tree. (#8389)

--- packages/runtime/garbage-collector/src/utils.ts
+++ packages/runtime/garbage-collector/src/utils.ts
@@ -4,7 +4,12 @@
  */
 
 import { assert } from "@fluidframework/common-utils";
-import { IGarbageCollectionData } from "@fluidframework/runtime-definitions";
+import {
+    IGarbageCollectionData,
+    IGarbageCollectionNodeData,
+    IGarbageCollectionState,
+    IGarbageCollectionSummaryDetails,
+} from "@fluidframework/runtime-definitions";
 
 /**
  * Helper function that clones the GC data.
@@ -22,13 +27,74 @@ export function cloneGCData(gcData: IGarbageCollectionData): IGarbageCollectionD
 }
 
 /**
- * Helper function that generates the used routes of children from a given node's used routes.
+ * Helper function that unpacks the GC details of the children from a given node's GC details.
+ * @param gcDetails - The GC details of a node.
+ * @returns A map of GC details of each children of the the given node.
+ */
+ export function unpackChildNodesGCDetails(gcDetails: IGarbageCollectionSummaryDetails) {
+    const childGCDetailsMap: Map<string, IGarbageCollectionSummaryDetails> = new Map();
+
+    // If GC data is not available, bail out.
+    if (gcDetails.gcData === undefined) {
+        return childGCDetailsMap;
+    }
+
+    // Remove the node's self GC nodes, if any, and generate the children GC nodes.
+    const gcNodes = gcDetails.gcData.gcNodes;
+    delete gcNodes["/"];
+    for (const [id, outboundRoutes] of Object.entries(gcNodes)) {
+        assert(id.startsWith("/"), "node id should always be an absolute route");
+        const childId = id.split("/")[1];
+        let childGCNodeId = id.slice(childId.length + 1);
+        // GC node id always begins with "/". Handle the special case where a child's id in the parent's GC nodes is
+        // of format `/root`. In this case, the childId is root and childGCNodeId is "". Make childGCNodeId = "/".
+        if (childGCNodeId === "") {
+            childGCNodeId = "/";
+        }
+
+        let childGCDetails = childGCDetailsMap.get(childId);
+        if (childGCDetails === undefined) {
+            childGCDetails = { gcData: { gcNodes: {} }, usedRoutes: [] };
+        }
+        // gcData should not undefined as its always at least initialized as  empty above.
+        assert(childGCDetails.gcData !== undefined, "Child GC data should have been initialized");
+        childGCDetails.gcData.gcNodes[childGCNodeId] = Array.from(outboundRoutes);
+        childGCDetailsMap.set(childId, childGCDetails);
+    }
+
+    if (gcDetails.usedRoutes === undefined) {
+        return childGCDetailsMap;
+    }
+
+    // Remove the node's self used route, if any, and generate the children used routes.
+    const usedRoutes = gcDetails.usedRoutes.filter((route) => route !== "" && route !== "/");
+    for (const route of usedRoutes) {
+        assert(route.startsWith("/"), "Used route should always be an absolute route");
+        const childId = route.split("/")[1];
+        const childUsedRoute = route.slice(childId.length + 1);
+
+        const childGCDetails = childGCDetailsMap.get(childId);
+        assert(
+            childGCDetails?.usedRoutes !== undefined,
+            "This should have be initiallized when generate GC nodes above",
+        );
+
+        childGCDetails.usedRoutes.push(childUsedRoute);
+        childGCDetailsMap.set(childId, childGCDetails);
+    }
+    return childGCDetailsMap;
+}
+
+/**
+ * Helper function that unpacks the used routes of children from a given node's used routes.
  * @param usedRoutes - The used routes of a node.
  * @returns A map of used routes of each children of the the given node.
  */
-export function getChildNodesUsedRoutes(usedRoutes: string[]) {
+export function unpackChildNodesUsedRoutes(usedRoutes: string[]) {
+    // Remove the node's self used route, if any, and generate the children used routes.
+    const filteredUsedRoutes = usedRoutes.filter((route) => route !== "" && route !== "/");
     const childUsedRoutesMap: Map<string, string[]> = new Map();
-    for (const route of usedRoutes) {
+    for (const route of filteredUsedRoutes) {
         assert(route.startsWith("/"), 0x198 /* "Used route should always be an absolute route" */);
         const childId = route.split("/")[1];
         const childUsedRoute = route.slice(childId.length + 1);
@@ -44,36 +110,6 @@ export function getChildNodesUsedRoutes(usedRoutes: string[]) {
 }
 
 /**
- * Helper function that generates the GC data of children from a given node's GC data.
- * @param gcData - The GC data of a node.
- * @returns A map of GC data of each children of the the given node.
- */
-export function getChildNodesGCData(gcData: IGarbageCollectionData) {
-    const childGCDataMap: Map<string, IGarbageCollectionData> = new Map();
-    for (const [id, outboundRoutes] of Object.entries(gcData.gcNodes)) {
-        assert(id.startsWith("/"), 0x199 /* "id should always be an absolute route" */);
-        const childId = id.split("/")[1];
-        let childGCNodeId = id.slice(childId.length + 1);
-        // GC node id always begins with "/". Handle the special case where the id in parent's GC nodes is of the
-        // for `/root`. This would make `childId=root` and `childGCNodeId=""`.
-        if (childGCNodeId === "") {
-            childGCNodeId = "/";
-        }
-
-        // Create a copy of the outbound routes array in the parents GC data.
-        const childOutboundRoutes = Array.from(outboundRoutes);
-
-        let childGCData = childGCDataMap.get(childId);
-        if (childGCData === undefined) {
-            childGCData = { gcNodes: {} };
-        }
-        childGCData.gcNodes[childGCNodeId] = childOutboundRoutes;
-        childGCDataMap.set(childId, childGCData);
-    }
-    return childGCDataMap;
-}
-
-/**
  * Removes the given route from the outbound routes of all the given GC nodes.
  * @param gcNodes - The nodes from which the route is to be removed.
  * @param outboundRoute - The route to be removed.
@@ -85,6 +121,45 @@ export function removeRouteFromAllNodes(gcNodes: { [ id: string ]: string[] }, o
             outboundRoutes.splice(index, 1);
         }
     }
+}
+
+/**
+ * Concatenates the given GC states and returns the concatenated GC state.
+ */
+export function concatGarbageCollectionStates(
+    gcState1: IGarbageCollectionState,
+    gcState2: IGarbageCollectionState,
+): IGarbageCollectionState {
+    const combinedGCNodes: { [ id: string ]: IGarbageCollectionNodeData } = {};
+    for (const [nodeId, nodeData] of Object.entries(gcState1.gcNodes)) {
+        combinedGCNodes[nodeId] = {
+            outboundRoutes: Array.from(nodeData.outboundRoutes),
+            unreferencedTimestampMs: nodeData.unreferencedTimestampMs,
+        };
+    }
+
+    for (const [nodeId, nodeData] of Object.entries(gcState2.gcNodes)) {
+        let combinedNodedata = combinedGCNodes[nodeId];
+        if (combinedNodedata === undefined) {
+            combinedNodedata = {
+                outboundRoutes: Array.from(nodeData.outboundRoutes),
+                unreferencedTimestampMs: nodeData.unreferencedTimestampMs,
+            };
+        } else {
+            // Validate that same node doesn't have different unreferenced timestamp.
+            if (nodeData.unreferencedTimestampMs !== undefined
+                && combinedNodedata.unreferencedTimestampMs !== undefined) {
+                assert(nodeData.unreferencedTimestampMs === combinedNodedata.unreferencedTimestampMs,
+                    "Two entries for the same GC node with different unreferenced timestamp");
+            }
+            combinedNodedata = {
+                outboundRoutes: [ ...new Set([ ...nodeData.outboundRoutes, ...combinedNodedata.outboundRoutes ]) ],
+                unreferencedTimestampMs: nodeData.unreferencedTimestampMs ?? combinedNodedata.unreferencedTimestampMs,
+            };
+        }
+        combinedGCNodes[nodeId] = combinedNodedata;
+    }
+    return { gcNodes: combinedGCNodes };
 }
 
 export class GCDataBuilder implements IGarbageCollectionData {
