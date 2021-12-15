919e5bd3390c6fe564283a71215c68bebce5de26
Tyler Butler <tylerbu@microsoft.com>
2021-12-14 16:54:46 -0800

    eslint8 -> default in all eslintrcs

--- experimental/PropertyDDS/packages/property-proxy/.eslintrc.js
+++ experimental/PropertyDDS/packages/property-proxy/.eslintrc.js
@@ -5,7 +5,7 @@
 
 module.exports = {
     "extends": [
-        "@fluidframework/eslint-config-fluid/eslint7"
+        "@fluidframework/eslint-config-fluid/eslint8"
     ],
     "parserOptions": {
         "project": ["./tsconfig.json", "./src/test/tsconfig.json"]
