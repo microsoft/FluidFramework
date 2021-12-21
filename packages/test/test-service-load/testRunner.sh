f3e2a6c9361f14f69f7b51821cd7f68667a5eecb
Vikram Agrawal <vikagrawal@microsoft.com>
2021-12-21 17:41:15 +0530

    Refactor runner script.

--- packages/test/test-service-load/testRunner.sh
+++ packages/test/test-service-load/testRunner.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Wait for test trigger (until file exists.)
 FILE=/fluid-config-store/$FLUID_TEST_UID/${FLUID_TEST_UID}_Trigger.json
 while [ ! -f "$FILE" ]
 do
@@ -9,21 +10,17 @@ done
 sleep 2s
 cp /fluid-config-store/$FLUID_TEST_UID/${HOSTNAME}_PodConfig.json ./testUserConfig.json
 
+# Wait for test rampup time to avoid all instances starting at once.
 rampupTimeInSeconds=`jq .rampup /app/packages/test/test-service-load/testUserConfig.json`
 echo "Sleeping for: $rampupTimeInSeconds";
 sleep $rampupTimeInSeconds
 
+# Credentials for orchestrator
 credentials=`jq -c '.credentials|to_entries|first as $x | { ($x.key): ($x.value) }' ./testUserConfig.json`
 export login__odsp__test__accounts=$credentials
 
 docId=`jq -c .docId ./testUserConfig.json`
-
-if [ -n "$docId" ]
-then
-  node ./dist/nodeStressTest.js -p $TEST_PROFILE -c ./testUserConfig.json -m > testscenario.logs 2>&1
-else
-  node ./dist/nodeStressTest.js -p $TEST_PROFILE -c ./testUserConfig.json -id $docId -m > testscenario.logs 2>&1
-fi
+node ./dist/nodeStressTest.js -p $TEST_PROFILE -c ./testUserConfig.json -id $docId -m > testscenario.logs 2>&1
 
 echo "Test complete just keeping pod alive."
 while true;
