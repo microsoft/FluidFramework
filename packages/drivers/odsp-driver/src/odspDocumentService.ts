3f5f972c9732c5431b75bfaf4c9ada499aaa55e8
Jatin Garg <jatgarg@microsoft.com>
2022-01-05 23:29:12 +0530

    Pr sugg

--- packages/drivers/odsp-driver/src/odspDocumentService.ts
+++ packages/drivers/odsp-driver/src/odspDocumentService.ts
@@ -283,6 +283,8 @@ export class OdspDocumentService implements IDocumentService {
                 connection.on("op", (documentId, ops: ISequencedDocumentMessage[]) => {
                     this.opsReceived(ops);
                 });
+                // On disconnect with 401/403 error code, we can just clear the joinSession cache as we will again
+                // get the auth error on reconnecting and face latency.
                 connection.on("disconnect", (error: OdspError) => {
                     if (error.errorType === DriverErrorType.authorizationError) {
                         this.cache.sessionJoinCache.remove(this.joinSessionKey);
