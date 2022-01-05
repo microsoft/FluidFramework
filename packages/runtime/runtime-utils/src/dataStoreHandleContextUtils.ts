9a871758514a2aa7bba5491ba3e3fda28b58dac6
Navin Agarwal <naagar@microsoft.com>
2022-01-05 12:11:43 -0800

    Fixed path

--- packages/runtime/runtime-utils/src/dataStoreHandleContextUtils.ts
+++ packages/runtime/runtime-utils/src/dataStoreHandleContextUtils.ts
@@ -6,23 +6,27 @@
 import { IFluidHandleContext } from "@fluidframework/core-interfaces";
 
 /**
- * Generates the absolute path for an IFluidHandleContext given its path and its parent routeContext.
+ * Generates the absolute path for a Fluid object given its path and its parent routeContext.
+ * @param path - The path to the Fluid object relative to the route context.
+ * @param routeContext - The route context that contains the Fluid object.
+ * @returns The absolute path to the Fluid object from the root of the Container.
  */
 export function generateHandleContextPath(path: string, routeContext?: IFluidHandleContext): string {
-    let result: string;
+    if (path.includes("/")) {
+        throw new Error(`The relative object path should not include slashes as it will interfere with url routing`);
+    }
 
-    if (path === "" || path === "/") {
+    if (path === "") {
         // The `path` is empty.
         // If the routeContext does not exist, this is the root.
         // If the routeContext exists, the absolute path is the same as that of the routeContext.
-        result = routeContext === undefined ? "/" : routeContext.absolutePath;
+        return routeContext === undefined ? "/" : routeContext.absolutePath;
     } else {
         // If the routeContext does not exist, path is the absolute path.
-        // If the routeContext exists, absolute path is routeContext's absolute path plus the path.
-        result = routeContext === undefined
-            ? path.startsWith("/") ? path : `/${path}`
-            : `${routeContext.absolutePath}/${path}`;
+        // If the routeContext exists, absolute path is routeContext's absolute path plus the path to the Fluid object.
+        // Handle the special case where the routeContext is the root to avoid double slashes in the path.
+        return routeContext === undefined
+            ? `/${path}`
+            : `${routeContext.absolutePath  === "/" ? "" : routeContext.absolutePath}/${path}`;
     }
-
-    return result;
 }
