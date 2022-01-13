a27ad53dc83b5410b0b02319288e6f35c1ea6809
Scarlett Lee <90647596+scarlettjlee@users.noreply.github.com>
2022-01-13 02:07:15 -0800

    Put createFromSnapshot back

--- packages/dds/merge-tree/src/test/testClient.ts
+++ packages/dds/merge-tree/src/test/testClient.ts
@@ -5,7 +5,7 @@
 
 import { strict as assert } from "assert";
 import { DebugLogger } from "@fluidframework/telemetry-utils";
-import { ISequencedDocumentMessage, ISummaryTree, MessageType } from "@fluidframework/protocol-definitions";
+import { ISequencedDocumentMessage, ISummaryTree, ITree, MessageType } from "@fluidframework/protocol-definitions";
 import { IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions";
 import { MockStorage } from "@fluidframework/test-runtime-utils";
 import random from "random-js";
@@ -58,12 +58,24 @@ export class TestClient extends Client {
         return TestClient.createFromSummary(summaryTree, newLongClientId, client1.specToSegment);
     }
 
+    public static async createFromSnapshot(
+        snapshotTree: ITree,
+        newLongClientId: string,
+        specToSeg: (spec: IJSONSegment) => ISegment): Promise<TestClient> {
+        return TestClient.createFromStorage(new MockStorage(snapshotTree), newLongClientId, specToSeg);
+    }
+
     public static async createFromSummary(
         summaryTree: ISummaryTree,
         newLongClientId: string,
         specToSeg: (spec: IJSONSegment) => ISegment): Promise<TestClient> {
-        const services = MockStorage.createFromSummary(summaryTree);
+        return TestClient.createFromStorage(MockStorage.createFromSummary(summaryTree), newLongClientId, specToSeg);
+    }
 
+    public static async createFromStorage(
+        storage: MockStorage,
+        newLongClientId: string,
+        specToSeg: (spec: IJSONSegment) => ISegment): Promise<TestClient> {
         const client2 = new TestClient(undefined, specToSeg);
         const { catchupOpsP } = await client2.load(
             // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
@@ -71,7 +83,7 @@ export class TestClient extends Client {
                 logger: client2.logger,
                 clientId: newLongClientId,
             } as IFluidDataStoreRuntime,
-            services,
+            storage,
             TestClient.serializer);
         await catchupOpsP;
         return client2;
