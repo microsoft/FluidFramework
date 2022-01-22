d9e43e8202209c806b5be28036e0a8b07e6d77ec
Si Chen <chensi@microsoft.com>
2022-01-21 16:29:06 -0800

    remove

--- packages/runtime/container-runtime-definitions/src/containerRuntime.ts
+++ packages/runtime/container-runtime-definitions/src/containerRuntime.ts
@@ -76,10 +76,6 @@ export interface IContainerRuntime extends
     IProvideFluidDataStoreRegistry,
     IContainerRuntimeBaseWithCombinedEvents {
 
-    /**
-    * @deprecated This will be removed in a later release. Deprecated in 0.53
-    */
-    // readonly id: string;
     readonly options: ILoaderOptions;
     readonly clientId: string | undefined;
     readonly clientDetails: IClientDetails;
