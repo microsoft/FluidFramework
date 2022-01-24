8bbaf34efc270e49350a942b9fa2c719fd29dcf8
Tyler Butler <tylerbu@microsoft.com>
2022-01-24 11:27:43 -0800

    Upgrade r11s eslint config (#8696)

--- server/routerlicious/packages/services-shared/.eslintrc.js
+++ server/routerlicious/packages/services-shared/.eslintrc.js
@@ -5,7 +5,7 @@
 
 module.exports = {
     "extends": [
-        "@fluidframework/eslint-config-fluid/eslint7"
+        "@fluidframework/eslint-config-fluid"
     ],
     "parserOptions": {
         "project": ["./tsconfig.json", "./src/test/tsconfig.json"]
@@ -13,6 +13,5 @@ module.exports = {
     "rules": {
         "@typescript-eslint/strict-boolean-expressions": "off",
         "no-null/no-null": "off",
-        "prefer-arrow/prefer-arrow-functions": "off"
     }
 }
