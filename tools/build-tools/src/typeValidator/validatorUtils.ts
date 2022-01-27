d40bd4435dd25d8a59822f5fee88f7112b56ea68
Helio Liu <59622401+heliocliu@users.noreply.github.com>
2022-01-26 21:39:51 -0800

    refactor to be non class-specific

--- tools/build-tools/src/typeValidator/validatorUtils.ts
+++ tools/build-tools/src/typeValidator/validatorUtils.ts
@@ -3,6 +3,8 @@
  * Licensed under the MIT License.
  */
 
+import { Project } from "ts-morph";
+
 let shouldLog = false;
 export function enableLogging(enable: boolean) {
     shouldLog = enable;
@@ -12,4 +14,14 @@ export function log(output: any) {
     if (shouldLog) {
         console.log(output);
     }
+}
+
+export enum BreakingIncrement {
+    none = 0,
+    minor = 1,
+    major = minor << 1 | minor, // this makes comparisons easier
+};
+
+export interface IValidator {
+    validate(project: Project, pkgDir: string) : BreakingIncrement;
 }
