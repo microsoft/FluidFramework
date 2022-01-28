418cb4d1ec0cc75618f9c02f3282c8dbc42820eb
Tony Murphy <anthony.murphy@microsoft.com>
2022-01-27 16:47:56 -0800

    Apply suggestions from code review

--- packages/framework/synthesize/src/dependencyContainer.ts
+++ packages/framework/synthesize/src/dependencyContainer.ts
@@ -147,9 +147,9 @@ export class DependencyContainer implements IFluidDependencySynthesizer {
         const provider = this.providers.get(t);
         if (provider === undefined) {
             for(const parent of this.parents) {
-                // eslint-disable-next-line @typescript-eslint/ban-types 
+                // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
                 const sp = { [t]: t } as FluidObjectSymbolProvider<Pick<IFluidObject, T>>;
-                // eslint-disable-next-line @typescript-eslint/ban-types 
+                // eslint-disable-next-line @typescript-eslint/ban-types
                 const syn = parent.synthesize<Pick<IFluidObject, T>,{}>(
                     sp,
                     {});
