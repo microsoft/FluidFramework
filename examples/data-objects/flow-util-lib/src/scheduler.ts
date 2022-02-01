cb24ce8a37f52e4dd5c70fb3b606757ff3035d16
Matt Rakow <matt.rakow@gmail.com>
2022-01-31 22:59:50 -0800

    Reduce scheduler

--- examples/data-objects/flow-util-lib/src/scheduler.ts
+++ examples/data-objects/flow-util-lib/src/scheduler.ts
@@ -6,16 +6,10 @@
 type TaskCallback = () => void;
 type TaskQueue = (callback: TaskCallback) => void;
 
-const idleThresholdMS = 60;
-const idleTaskTurnBreak = 2;
-
 export class Scheduler {
     private static readonly done = Promise.resolve();
-    private lastDispatchMS = Date.now();
     private readonly turnTasks: TaskCallback[] = [];
     private readonly layoutTasks: TaskCallback[] = [];
-    private readonly postLayoutTasks: TaskCallback[] = [];
-    private readonly idleTasks: TaskCallback[] = [];
 
     public readonly onTurnEnd = (callback: TaskCallback) => {
         if (this.turnTasks.push(callback) === 1) {
@@ -31,21 +25,8 @@ export class Scheduler {
         }
     };
 
-    public readonly onPostLayout = (callback: TaskCallback) => {
-        this.postLayoutTasks.push(callback);
-        if (this.layoutQueueLength === 1) {
-            requestAnimationFrame(this.processLayoutTasks);
-        }
-    };
-
-    public readonly onIdle = (callback: TaskCallback) => {
-        if (this.idleTasks.push(callback) === 1) {
-            this.scheduleIdleTasks(this.idleDueMS(Date.now()));
-        }
-    };
-
     private get layoutQueueLength() {
-        return this.layoutTasks.length + this.postLayoutTasks.length;
+        return this.layoutTasks.length;
     }
 
     public coalesce(queue: TaskQueue, callback: TaskCallback) {
@@ -84,43 +65,9 @@ export class Scheduler {
 
     private readonly processTurnTasks = () => {
         this.dispatch(this.turnTasks);
-        this.lastDispatchMS = Date.now();
     };
 
     private readonly processLayoutTasks = () => {
         this.dispatch(this.layoutTasks);
-        this.dispatch(this.postLayoutTasks);
-        this.lastDispatchMS = Date.now();
-    };
-
-    private scheduleIdleTasks(dueMS: number) {
-        setTimeout(this.processIdleTasks, dueMS);
-    }
-
-    private idleDueMS(nowMS: number) {
-        return Math.max(this.lastDispatchMS + idleThresholdMS - nowMS, 0);
-    }
-
-    private readonly processIdleTasks = () => {
-        // If we've dispatched tasks recently, reschedule the idle queue.
-        const start = Date.now();
-        const due = this.idleDueMS(start);
-        if (start < due) {
-            this.scheduleIdleTasks(due);
-        }
-
-        const tasks = this.idleTasks;
-        const deadline = start + idleTaskTurnBreak;
-
-        for (let i = 0; i < tasks.length; i++) {
-            tasks[i]();
-
-            if (Date.now() > deadline) {
-                tasks.splice(0, i);
-                this.scheduleIdleTasks(0);
-            }
-        }
-
-        tasks.length = 0;
     };
 }
