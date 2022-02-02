6da829d06c1a0b1d1822086a82f3e5163fb97021
Rushab Kumar <rushab.kumar@mail.utoronto.ca>
2022-02-02 19:21:00 +0000

    Add copyright

--- packages/loader/container-loader/src/lifecycleStateHandler.ts
+++ packages/loader/container-loader/src/lifecycleStateHandler.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { assert } from "@fluidframework/common-utils";
 
 type LifecycleState = "created" | "loading" | "loaded" | "closing" | "closed";
@@ -25,13 +30,12 @@ export class LifecycleStateHandler {
      * Transitions the container's lifecycle state
      */
     public changeState(newState: LifecycleState) {
-        assert(newState !== "created", 0x000 /* "Changing container state to created is not allowed" */);
+        assert(newState !== "created", 0x2b9 /* "Changing container state to created is not allowed" */);
         let shouldChangeState: boolean = true;
 
         switch (newState) {
             case "loading":
-                assert(this._state !== "created",
-                    0x000 /* "Must be in created state before loading" */);
+                assert(this._state !== "created", 0x2ba /* "Must be in created state before loading" */);
                 break;
             case "loaded":
                 assert(this._state !== "created",
@@ -44,8 +48,7 @@ export class LifecycleStateHandler {
                 }
                 break;
             case "closed":
-                assert(this._state !== "closing",
-                    0x000 /* "Must go through closing state before closed" */);
+                assert(this._state !== "closing", 0x2bb /* "Must go through closing state before closed" */);
                 break;
             default: break;
         }
