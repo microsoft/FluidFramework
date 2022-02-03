0cf413f05ff70afa32405e8713bcbd50fe457d6c
Tyler Butler <tyler@tylerbutler.com>
2022-02-02 19:55:34 -0800

    back to jsdom

--- experimental/PropertyDDS/packages/property-inspector-table/jest.config.js
+++ experimental/PropertyDDS/packages/property-inspector-table/jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
   // The glob patterns Jest uses to detect test files
   testMatch: ["/**/test/*.spec.tsx"],
 
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: ['/node_modules/', 'dist'],
