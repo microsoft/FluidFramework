ac1bc07db5817623b98d5a46490030eb7a17dcf0
Joel Tianqi Zhu <66701969+tianzhu007@users.noreply.github.com>
2022-02-07 14:43:40 -0800

    Add the summary storage name as the optional value

--- server/routerlicious/packages/services-core/src/tenant.ts
+++ server/routerlicious/packages/services-core/src/tenant.ts
@@ -43,6 +43,9 @@ export interface ITenantStorage {
         // Password for the storage provider
         password: string;
     };
+
+    // Summary storage url of the tenant
+    summaryStorageName?: string
 }
 
 export interface ITenantOrderer {
