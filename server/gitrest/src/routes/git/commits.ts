ddef4cd190045bd36fed00daf0c7939b729d8e57
Matt Rakow <marakow@microsoft.com>
2022-02-11 12:11:45 -0800

    Fix break

--- server/gitrest/src/routes/git/commits.ts
+++ server/gitrest/src/routes/git/commits.ts
@@ -21,7 +21,8 @@ export async function createCommit(
     }
 
     const repository = await repoManager.open(owner, repo);
-    const signature = git.Signature.create(blob.author.name, blob.author.email, Math.floor(date), 0);s.length > 0 ? blob.parents : null;
+    const signature = git.Signature.create(blob.author.name, blob.author.email, Math.floor(date), 0);
+    const parents = blob.parents && blob.parents.length > 0 ? blob.parents : null;
     const commit = await repository.createCommit(null, signature, signature, blob.message, blob.tree, parents);
 
     return {
