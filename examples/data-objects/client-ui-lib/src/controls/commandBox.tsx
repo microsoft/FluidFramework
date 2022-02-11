525acff62cdc8bfcc74d8a761fb8aa13c8c956ef
Matt Rakow <matt.rakow@gmail.com>
2022-02-10 22:06:15 -0800

    Add basic arrowing

--- examples/data-objects/client-ui-lib/src/controls/commandBox.tsx
+++ examples/data-objects/client-ui-lib/src/controls/commandBox.tsx
@@ -16,11 +16,11 @@ export interface ICommandBoxProps {
     commands: ICommandBoxCommand[];
 }
 
-// hide on enter
 export const CommandBox: React.FC<ICommandBoxProps> = (props: ICommandBoxProps) => {
     const { registerShowListener, doneHandler, commands } = props;
     const [show, setShow] = React.useState<boolean>(false);
     const [textFilter, setTextFilter] = React.useState<string>("");
+    const [arrowedCommand, setArrowedCommand] = React.useState<number | undefined>(undefined);
     const filterRef = React.useRef<HTMLInputElement>(null);
     React.useEffect(() => {
         registerShowListener(() => { setShow(true); });
@@ -30,6 +30,8 @@ export const CommandBox: React.FC<ICommandBoxProps> = (props: ICommandBoxProps) 
         if (filterRef.current !== null) {
             const inputEl = filterRef.current;
             const filterCommands = () => {
+                // todo: actually compare match of filtered commands and only reset if different
+                setArrowedCommand(undefined);
                 setTextFilter(inputEl.value);
             };
             inputEl.addEventListener("input", filterCommands);
@@ -46,39 +48,69 @@ export const CommandBox: React.FC<ICommandBoxProps> = (props: ICommandBoxProps) 
         doneHandler();
     };
 
-    const keydownHandler = (e: KeyboardEvent) => {
+    const getMatchingCommands = () => commands.filter((command) => {
+        return command.friendlyName.toLowerCase().startsWith(textFilter.toLowerCase());
+    });
+
+    const keydownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        const matchingCommands = getMatchingCommands();
         if (e.key === "Escape") {
             dismissCommandBox();
+        } else if (e.key === "ArrowDown") {
+            if (arrowedCommand === undefined) {
+                if (matchingCommands.length > 0) {
+                    setArrowedCommand(0);
+                }
+            } else {
+                setArrowedCommand((arrowedCommand + 1) % matchingCommands.length);
+            }
+            e.preventDefault();
+        } else if (e.key === "ArrowUp") {
+            if (arrowedCommand === undefined) {
+                if (matchingCommands.length > 0) {
+                    setArrowedCommand(matchingCommands.length - 1);
+                }
+            } else {
+                setArrowedCommand((arrowedCommand - 1) % matchingCommands.length);
+            }
+            e.preventDefault();
         }
     };
 
-    React.useEffect(() => {
-        if (show) {
-            document.addEventListener("keydown", keydownHandler);
-        } else {
-            document.removeEventListener("keydown", keydownHandler);
+    const keypressHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            if (arrowedCommand !== undefined) {
+                getMatchingCommands()[arrowedCommand].exec();
+                dismissCommandBox();
+            }/* else if (textbox contents string match a known command) {
+                command.exec();
+                dismissCommandBox();
+            }*/
         }
-        return () => {
-            document.removeEventListener("keydown", keydownHandler);
-        };
-    }, [show]);
+    };
 
     const buildCommandElements = () => {
         if (textFilter === "") {
             return [];
         }
 
-        const commandElements = commands
-            .filter((command) => {
-                return command.friendlyName.toLowerCase().startsWith(textFilter.toLowerCase());
-            })
-            .map((command) => {
+        const commandElements = getMatchingCommands().map((command, index) => {
                 const clickCommand = () => {
                     command.exec();
                     dismissCommandBox();
                 };
-                return <div key={ command.friendlyName } onClick={ clickCommand }>{ command.friendlyName }</div>;
+
+                if (index === arrowedCommand) {
+                    return (
+                        <div key={ command.friendlyName } onClick={ clickCommand } style={{ backgroundColor: "#ccc" }}>
+                            { command.friendlyName }
+                        </div>
+                    );
+                } else {
+                    return <div key={ command.friendlyName } onClick={ clickCommand }>{ command.friendlyName }</div>;
+                }
             });
+
         return commandElements;
     }
     const commandElements = buildCommandElements();
@@ -87,9 +119,9 @@ export const CommandBox: React.FC<ICommandBoxProps> = (props: ICommandBoxProps) 
         return (
             <div style={{ position: "absolute", width: "100%", height: "100%" }}>
                 <div>
-                    <input type="text" ref={ filterRef } />
+                    <input type="text" ref={ filterRef } onKeyDown={ keydownHandler } onKeyPress={ keypressHandler }/>
                 </div>
-                <div>
+                <div style={{ backgroundColor: "#fff", border: "1px solid #000" }}>
                     { commandElements }
                 </div>
             </div>
