fb7939e14e9023617de785ed550e7196262c57c6
Matt Rakow <matt.rakow@gmail.com>
2022-02-17 23:55:25 -0800

    Convert to aqueduct for the factory too

--- examples/data-objects/shared-text/src/index.ts
+++ examples/data-objects/shared-text/src/index.ts
@@ -3,43 +3,17 @@
  * Licensed under the MIT License.
  */
 
-import { IContainerContext } from "@fluidframework/container-definitions";
-import { ContainerRuntime } from "@fluidframework/container-runtime";
 import {
     rootDataStoreRequestHandler,
-    buildRuntimeRequestHandler,
 } from "@fluidframework/request-handler";
-import { defaultRouteRequestHandler } from "@fluidframework/aqueduct";
-import { RuntimeFactoryHelper } from "@fluidframework/runtime-utils";
+import { ContainerRuntimeFactoryWithDefaultDataStore } from "@fluidframework/aqueduct";
 import { SharedTextDataStoreFactory } from "./component";
 
-const DefaultComponentName = "text";
-
-class SharedTextContainerRuntimeFactory extends RuntimeFactoryHelper {
-    public async instantiateFirstTime(runtime: ContainerRuntime): Promise<void> {
-        await runtime.createRootDataStore(SharedTextDataStoreFactory.type, DefaultComponentName);
-    }
-
-    public async preInitialize(
-        context: IContainerContext,
-        existing: boolean,
-    ): Promise<ContainerRuntime> {
-        const runtime: ContainerRuntime = await ContainerRuntime.load(
-            context,
-            [
-                [SharedTextDataStoreFactory.type, Promise.resolve(SharedTextDataStoreFactory)],
-            ],
-            buildRuntimeRequestHandler(
-                defaultRouteRequestHandler(DefaultComponentName),
-                rootDataStoreRequestHandler,
-            ),
-            undefined, // runtimeOptions
-            undefined, // containerScope
-            existing,
-        );
-
-        return runtime;
-    }
-}
-
-export const fluidExport = new SharedTextContainerRuntimeFactory();
+export const fluidExport = new ContainerRuntimeFactoryWithDefaultDataStore(
+    SharedTextDataStoreFactory,
+    new Map([
+        [SharedTextDataStoreFactory.type, Promise.resolve(SharedTextDataStoreFactory)]
+    ]),
+    undefined,
+    [rootDataStoreRequestHandler],
+);
