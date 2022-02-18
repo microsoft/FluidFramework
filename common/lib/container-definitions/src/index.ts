94119fcdfce5c8445d36713868c82d3c2fbd8867
Tony Murphy <anthony.murphy@microsoft.com>
2022-02-17 17:22:37 -0800

    Move Container Definitions to Type Exports

--- common/lib/container-definitions/src/index.ts
+++ common/lib/container-definitions/src/index.ts
@@ -2,13 +2,89 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-export * from "./audience";
-export * from "./browserPackage";
-export * from "./legacy";
-export * from "./deltas";
-export * from "./error";
-export * from "./loader";
-export * from "./fluidModule";
-export * from "./fluidPackage";
-export * from "./proxyLoader";
-export * from "./runtime";
+export type {
+    IAudience,
+} from "./audience";
+export { //Todo: move out of definition package
+    isFluidBrowserPackage,
+} from "./browserPackage"
+export type {
+    IFluidBrowserPackage,
+    IFluidBrowserPackageEnvironment,
+} from "./browserPackage";
+export { //Todo: move out of definition package
+    IFluidTokenProvider,
+} from "./legacy"
+export type {
+    IProvideFluidTokenProvider,
+} from "./legacy";
+export {
+    //Todo: move constant out of definition package
+    IDeltaSender,
+} from "./deltas"
+export type {
+    IConnectionDetails,
+    IDeltaHandlerStrategy,
+    IDeltaManager,
+    IDeltaManagerEvents,
+    IDeltaQueue,
+    IDeltaQueueEvents,
+    ReadOnlyInfo,
+    IProvideDeltaSender
+} from "./deltas";
+export { //Todo: move out of definition package
+    ContainerErrorType,
+} from "./error"
+export type {
+    ContainerWarning,
+    ICriticalContainerError,
+    IErrorBase,
+    IGenericError,
+    IThrottlingWarning,
+    IUsageError
+} from "./error";
+export type {
+    ConnectionState,
+    ICodeAllowList,
+    ICodeDetailsLoader,
+    IContainer,
+    IContainerEvents,
+    IContainerLoadMode,
+    IFluidCodeResolver,
+    IFluidModuleWithDetails,
+    IHostLoader,
+    ILoader,
+    ILoaderHeader,
+    ILoaderOptions,
+    IPendingLocalState,
+    IProvideLoader,
+    IResolvedFluidCodeDetails,
+    LoaderHeader,
+    ICodeLoader,
+} from "./loader";
+export type {IFluidModule} from "./fluidModule";
+export { //Todo: move out of definition package
+    isFluidCodeDetails,
+    isFluidPackage,
+    IFluidCodeDetailsComparer,
+} from "./fluidPackage"
+export type {
+    IFluidCodeDetails,
+    IFluidCodeDetailsConfig,
+    IFluidPackage,
+    IFluidPackageEnvironment,
+    IProvideFluidCodeDetailsComparer,
+} from "./fluidPackage";
+export type {
+    IProxyLoaderFactory
+} from "./proxyLoader";
+export { //Todo: move out of definition package
+    BindState,
+    IRuntimeFactory,
+    AttachState,
+} from "./runtime"
+export type {
+    IContainerContext,
+    IProvideRuntimeFactory,
+    IRuntime,
+} from "./runtime";
