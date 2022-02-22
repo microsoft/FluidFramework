6f694999962df435874ffff0f19cb6e2e03fa6a6
arikt-ms <62397921+arikt-ms@users.noreply.github.com>
2022-02-22 14:31:31 -0800

    Arikt/usagecounters (#9218)
    * Adding usageEmitter.
    
    * minor fix.
    
    * minor fix.
    
    * minor fix.
    
    * logging usageEmitter config parameter.
    
    * minor fix.
    
    * fixing usageEmitter config loading.

--- server/routerlicious/packages/routerlicious-base/src/alfred/runner.ts
+++ server/routerlicious/packages/routerlicious-base/src/alfred/runner.ts
@@ -70,9 +70,7 @@ export class AlfredRunner implements IRunner {
 
         const httpServer = this.server.httpServer;
 
-        const usageEmitter = this.config.get("usageEmitter") as IUsageEmitter;
-        const usageEmitterString = JSON.stringify(usageEmitter);
-        winston.info(`configureUsageEmitters: ${usageEmitterString}`);
+        const usageEmitter = this.config.get("usageEmitterInstance")?.[0] as IUsageEmitter;
 
         const maxNumberOfClientsPerDocument = this.config.get("alfred:maxNumberOfClientsPerDocument");
         const maxTokenLifetimeSec = this.config.get("auth:maxTokenLifetimeSec");
