8750212ffc27f54aba595de04de0477adad1e252
Alex Panov <8239356+alpaix@users.noreply.github.com>
2022-02-23 12:14:17 -0800

    Add schema upgrade demo app

---
+++ examples/hosts/app-integration/schema-upgrade/tests/inventoryList.test.ts
@@ -0,0 +1,25 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { globals } from "../jest.config";
+
+// Tests disabled -- requires Tinylicious to be running, which our test environment doesn't do.
+describe("inventoryList", () => {
+    beforeAll(async () => {
+        // Wait for the page to load first before running any tests
+        // so this time isn't attributed to the first test
+        await page.goto(globals.PATH, { waitUntil: "load", timeout: 0 });
+    }, 45000);
+
+    beforeEach(async () => {
+        await page.goto(globals.PATH, { waitUntil: "load" });
+        await page.waitFor(() => window["fluidStarted"]);
+    });
+
+    it("loads and there's an input", async () => {
+        // Validate the input shows up
+        await page.waitForSelector("input");
+    });
+});
