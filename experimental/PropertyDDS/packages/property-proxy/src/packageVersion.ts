e089101a89f52adee07be1164a3c7c04560cfb63
chensixx <34214774+chensixx@users.noreply.github.com>
2022-03-02 14:38:18 -0800

    [bump] package version for client (patch) (#9305)
    @fluidframework/build-common:     0.24.0 (unchanged)
         @fluidframework/eslint-config-fluid:     0.26.0 (unchanged)
          @fluidframework/common-definitions:     0.21.0 (unchanged)
                @fluidframework/common-utils:     0.33.0 (unchanged)
             @fluidframework/core-interfaces:     0.42.1 (unchanged)
        @fluidframework/protocol-definitions:   0.1027.0 (unchanged)
          @fluidframework/driver-definitions:     0.44.1 (unchanged)
       @fluidframework/container-definitions:     0.45.1 (unchanged)
                                      Server:   0.1035.0 (unchanged)
                                      Client:     0.56.5 -> 0.56.6
                      @fluid-tools/benchmark:     0.40.0 (unchanged)
                             generator-fluid:      0.3.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
         @fluidframework/azure-local-service:      0.1.0 (unchanged)
                                 dice-roller:      0.0.1 (unchanged)

--- experimental/PropertyDDS/packages/property-proxy/src/packageVersion.ts
+++ experimental/PropertyDDS/packages/property-proxy/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-experimental/property-proxy";
-export const pkgVersion = "0.56.5";
+export const pkgVersion = "0.56.6";
--- packages/dds/ink/src/packageVersion.ts
+++ packages/dds/ink/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/ink";
-export const pkgVersion = "0.56.5";
+export const pkgVersion = "0.56.6";
