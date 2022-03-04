01ef8d61194e0ea9f4f25a07a6a6d954ca900b61
Matt Rakow <matt.rakow@gmail.com>
2022-03-04 01:33:08 -0800

    Remove IFluidHTMLView implementation from MonacoRunner

--- examples/data-objects/monaco/src/dataObject.ts
+++ examples/data-objects/monaco/src/dataObject.ts
@@ -7,30 +7,17 @@
 import { DataObject } from "@fluidframework/aqueduct";
 import { IFluidHandle } from "@fluidframework/core-interfaces";
 import { SharedString } from "@fluidframework/sequence";
-import { IFluidHTMLView } from "@fluidframework/view-interfaces";
-import { MonacoRunnerView } from "./view";
 
 /**
  * Component for using the Monaco text editor.
  */
-export class MonacoRunner extends DataObject implements IFluidHTMLView {
-    public get IFluidHTMLView() { return this; }
-
+export class MonacoRunner extends DataObject {
     private _text: SharedString | undefined;
     public get text(): SharedString {
         if(this._text === undefined) {
             throw new Error("Text not loaded");
         }
         return this._text;
-    }
-
-    private view: MonacoRunnerView | undefined;
-
-    public render(elm: HTMLElement): void {
-        if (this.view === undefined) {
-            this.view = new MonacoRunnerView(this.text);
-        }
-        this.view.render(elm);
     }
 
     /**
