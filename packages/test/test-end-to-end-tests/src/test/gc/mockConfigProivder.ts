5e0ad60332a3619fc7b47ce4c5bba4755d98fb31
tyler-cai-microsoft <90650728+tyler-cai-microsoft@users.noreply.github.com>
2022-03-04 10:16:00 -0800

    Update rootiness for aliased datastores (#9312) (#9338)

--- packages/test/test-end-to-end-tests/src/test/gc/mockConfigProivder.ts
+++ packages/test/test-end-to-end-tests/src/test/gc/mockConfigProivder.ts
@@ -6,6 +6,7 @@
 import { ConfigTypes, IConfigProviderBase } from "@fluidframework/telemetry-utils";
 
 export const mockConfigProvider = ((settings: Record<string, ConfigTypes>): IConfigProviderBase => {
+    settings["Fluid.ContainerRuntime.UseDataStoreAliasing"] = "true";
     return {
         getRawConfig: (name: string): ConfigTypes => settings[name],
     };
