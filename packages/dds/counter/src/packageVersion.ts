d3eb69912489b491e5a77cc22760d93283bf2789
heliocliu <59622401+heliocliu@users.noreply.github.com>
2022-03-04 17:47:14 -0800

    [bump] package version to 0.58.1000 for development after client release
                @fluidframework/build-common:     0.24.0 (unchanged)
         @fluidframework/eslint-config-fluid:     0.27.0 (unchanged)
          @fluidframework/common-definitions:     0.21.0 (unchanged)
                @fluidframework/common-utils:     0.33.0 (unchanged)
             @fluidframework/core-interfaces:     0.43.0 (unchanged)
        @fluidframework/protocol-definitions:   0.1027.0 -> 0.1027.1000
          @fluidframework/driver-definitions:     0.45.0 -> 0.45.1000
       @fluidframework/container-definitions:     0.47.0 -> 0.47.1000
                                      Server:   0.1035.0 -> 0.1035.1000
                                      Client:     0.58.0 -> 0.58.1000
                      @fluid-tools/benchmark:     0.40.0 (unchanged)
                             generator-fluid:      0.3.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
         @fluidframework/azure-local-service:      0.1.0 (unchanged)
                                 dice-roller:      0.0.1 (unchanged)

--- packages/dds/counter/src/packageVersion.ts
+++ packages/dds/counter/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/counter";
-export const pkgVersion = "0.58.0";
+export const pkgVersion = "0.58.1000";
--- packages/framework/test-client-utils/src/packageVersion.ts
+++ packages/framework/test-client-utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/test-client-utils";
-export const pkgVersion = "0.58.0";
+export const pkgVersion = "0.58.1000";
