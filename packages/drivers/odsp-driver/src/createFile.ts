d7e5b2a310b14f287ec9a085d8ed16bb8ab35d80
Mark Fields <markfields@users.noreply.github.com>
2022-03-09 07:47:19 -0800

    Add comments with old error messages wherever it changed

--- packages/drivers/odsp-driver/src/createFile.ts
+++ packages/drivers/odsp-driver/src/createFile.ts
@@ -61,6 +61,7 @@ export async function createNewFluidFile(
     // Check for valid filename before the request to create file is actually made.
     if (isInvalidFileName(newFileInfo.filename)) {
         throw new NonRetryableError(
+            // pre-0.58 error message: createNewInvalidFilename
             "Invalid filename for createNew", OdspErrorType.invalidFileNameError, { driverVersion });
     }
 
