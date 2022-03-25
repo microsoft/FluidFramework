6f0b0072bd9d26584e4b1ed2ae6b38002a0d7196
Mark Fields <markfields@users.noreply.github.com>
2022-03-24 18:18:43 -0700

    Remove todo comment

--- packages/loader/driver-utils/src/networkUtils.ts
+++ packages/loader/driver-utils/src/networkUtils.ts
@@ -6,7 +6,6 @@
 import { ITelemetryErrorEvent, ITelemetryLogger } from "@fluidframework/common-definitions";
 import { isOnline, OnlineStatus, canRetryOnError } from "./network";
 
-//* UTs
 export function logNetworkFailure(logger: ITelemetryLogger, event: ITelemetryErrorEvent, error?: any) {
     const newEvent = { ...event };
 
