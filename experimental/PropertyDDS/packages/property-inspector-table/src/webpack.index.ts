86d1ce335b85f3272fb6bbcde57188fd99d52c07
Matt Rakow <marakow@microsoft.com>
2022-03-25 10:27:16 -0700

    Start fixing lint

--- experimental/PropertyDDS/packages/property-inspector-table/src/webpack.index.ts
+++ experimental/PropertyDDS/packages/property-inspector-table/src/webpack.index.ts
@@ -4,7 +4,7 @@
  */
 
 // This file serve for webpack, otherwise TS will complain about the following import,
-// which is required to transpile the SVGs. 
+// which is required to transpile the SVGs.
 import InspectorTableIcons from '../assets/icons/SVGStoreIcons';
 export { InspectorTableIcons };
 
