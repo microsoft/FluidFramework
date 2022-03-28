d941d7e3e61538554a3c740e59f286cefd30e0c4
Matt Rakow <marakow@microsoft.com>
2022-03-28 16:49:16 -0700

    Update summarization, comments

--- packages/dds/quorum/src/quorum.ts
+++ packages/dds/quorum/src/quorum.ts
@@ -131,20 +131,20 @@ const snapshotFileName = "header";
  */
 export class Quorum extends SharedObject<IQuorumEvents> implements IQuorum {
     /**
-     * Create a new TaskManager
+     * Create a new Quorum
      *
-     * @param runtime - data store runtime the new task queue belongs to
-     * @param id - optional name of the task queue
-     * @returns newly create task queue (but not attached yet)
+     * @param runtime - data store runtime the new quorum belongs to
+     * @param id - optional name of the quorum
+     * @returns newly created quorum (but not attached yet)
      */
     public static create(runtime: IFluidDataStoreRuntime, id?: string) {
         return runtime.createChannel(id, QuorumFactory.Type) as Quorum;
     }
 
     /**
-     * Get a factory for TaskManager to register with the data store.
+     * Get a factory for Quorum to register with the data store.
      *
-     * @returns a factory that creates and load TaskManager
+     * @returns a factory that creates and load Quorum
      */
     public static getFactory(): IChannelFactory {
         return new QuorumFactory();
@@ -152,23 +152,17 @@ export class Quorum extends SharedObject<IQuorumEvents> implements IQuorum {
 
     private readonly values: Map<string, QuorumValue> = new Map();
 
-    /**
-     * Mapping of taskId to a queue of clientIds that are waiting on the task.  Maintains the consensus state of the
-     * queue, even if we know we've submitted an op that should eventually modify the queue.
-     */
-    private readonly taskQueues: Map<string, string[]> = new Map();
-
     // disconnectWatcher emits an event whenever we get disconnected.
     private readonly disconnectWatcher: EventEmitter = new EventEmitter();
 
     private readonly incomingOp: EventEmitter = new EventEmitter();
 
     /**
-     * Constructs a new task manager. If the object is non-local an id and service interfaces will
+     * Constructs a new quorum. If the object is non-local an id and service interfaces will
      * be provided
      *
-     * @param runtime - data store runtime the task queue belongs to
-     * @param id - optional name of the task queue
+     * @param runtime - data store runtime the quorum belongs to
+     * @param id - optional name of the quorum
      */
     constructor(id: string, runtime: IFluidDataStoreRuntime, attributes: IChannelAttributes) {
         super(id, runtime, attributes);
@@ -178,16 +172,7 @@ export class Quorum extends SharedObject<IQuorumEvents> implements IQuorum {
 
         this.disconnectWatcher.on("disconnect", () => {
             assert(this.runtime.clientId !== undefined, 0x1d3 /* "Missing client id on disconnect" */);
-
-            // We don't modify the taskQueues on disconnect (they still reflect the latest known consensus state).
-            // After reconnect these will get cleaned up by observing the clientLeaves.
-            // However we do need to recognize that we lost the lock if we had it.  Calls to .queued() and
-            // .haveTaskLock() are also connection-state-aware to be consistent.
-            for (const [taskId, clientQueue] of this.taskQueues.entries()) {
-                if (clientQueue[0] === this.runtime.clientId) {
-                    this.emit("lost", taskId);
-                }
-            }
+            // TODO Handle appropriately
         });
     }
 
@@ -257,14 +242,13 @@ export class Quorum extends SharedObject<IQuorumEvents> implements IQuorum {
     }
 
     /**
-     * Create a summary for the task manager
+     * Create a summary for the quorum
      *
-     * @returns the summary of the current state of the task manager
+     * @returns the summary of the current state of the quorum
      * @internal
      */
     protected summarizeCore(serializer: IFluidSerializer): ISummaryTreeWithStats {
-        // TODO filter out tasks with no clients, some are still getting in.
-        const content = [...this.taskQueues.entries()];
+        const content = [...this.values.entries()];
         return createSingleBlobSummary(snapshotFileName, JSON.stringify(content));
     }
 
@@ -273,9 +257,9 @@ export class Quorum extends SharedObject<IQuorumEvents> implements IQuorum {
      * @internal
      */
     protected async loadCore(storage: IChannelStorageService): Promise<void> {
-        const content = await readAndParse<[string, string[]][]>(storage, snapshotFileName);
-        content.forEach(([taskId, clientIdQueue]) => {
-            this.taskQueues.set(taskId, clientIdQueue);
+        const content = await readAndParse<[string, QuorumValue][]>(storage, snapshotFileName);
+        content.forEach(([key, value]) => {
+            this.values.set(key, value);
         });
     }
 
@@ -292,14 +276,14 @@ export class Quorum extends SharedObject<IQuorumEvents> implements IQuorum {
     }
 
     /**
-     * Override resubmit core to avoid resubmission on reconnect.  On disconnect we accept our removal from the
-     * queues, and leave it up to the user to decide whether they want to attempt to re-enter a queue on reconnect.
+     * Override resubmit core to avoid resubmission on reconnect.
+     * TODO It's probably ok to resubmit these?
      * @internal
      */
     protected reSubmitCore() { }
 
     /**
-     * Process a task manager operation
+     * Process a quorum operation
      *
      * @param message - the message to prepare
      * @param local - whether the message was sent by the local client
