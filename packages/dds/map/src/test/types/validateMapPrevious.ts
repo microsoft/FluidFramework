218ed78778c3a3bf465380060984c6a51a9bcf24
Tony Murphy <anthony.murphy@microsoft.com>
2022-03-30 11:45:50 -0700

    map supression for breaking change

--- packages/dds/map/src/test/types/validateMapPrevious.ts
+++ packages/dds/map/src/test/types/validateMapPrevious.ts
@@ -408,6 +408,7 @@ declare function get_old_ClassDeclaration_SharedDirectory():
 declare function use_current_ClassDeclaration_SharedDirectory(
     use: TypeOnly<current.SharedDirectory>);
 use_current_ClassDeclaration_SharedDirectory(
+    // @ts-expect-error compatibility expected to be broken
     get_old_ClassDeclaration_SharedDirectory());
 
 /*
