1ad180901f55520017f0daf36c40e74ee5be4f87
Scott Norton <scottnorton@microsoft.com>
2022-04-04 16:45:37 -0400

    Update packages/framework/fluid-static/src/fluidContainer.ts
    Co-authored-by: Mark Fields <markfields@users.noreply.github.com>

--- packages/framework/fluid-static/src/fluidContainer.ts
+++ packages/framework/fluid-static/src/fluidContainer.ts
@@ -75,7 +75,7 @@ export interface IFluidContainer extends IEventProvider<IFluidContainerEvents> {
     /**
      * Whether the container is connected to the collaboration session.
      * @deprecated - 0.58, This API will be removed in 0.60.0
-     * Use `connectionState.Connected` instead
+     * Check `connectionState === ConnectionState.Connected` instead
      * See https://github.com/microsoft/FluidFramework/issues/9167 for context
      */
     readonly connected: boolean;
