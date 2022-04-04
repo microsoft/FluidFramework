75554fafa1b98a32732c5961e82e3c5f620504cf
Tyler Butler <tylerbu@microsoft.com>
2022-04-04 13:55:03 -0700

    print-config

--- common/build/eslint-config-fluid/src/file.ts
+++ common/build/eslint-config-fluid/src/file.ts
@@ -3,9 +3,4 @@
  * Licensed under the MIT License.
  */
 
-module.exports = {
-    "extends": [
-        "./minimal"
-    ],
-    "rules": {},
-}
+// This file is used only to run `eslint --print-config`. See the README for this project for more information.
