c0c0a459b86b179dd56a74a3b164bcfdd9ce1a61
Scarlett Lee <90647596+scarlettjlee@users.noreply.github.com>
2022-04-13 15:05:11 -0700

    Remove unused import

--- common/lib/core-interfaces/src/fluidObject.ts
+++ common/lib/core-interfaces/src/fluidObject.ts
@@ -9,7 +9,6 @@ import {
 } from "./fluidLoadable";
 import { IFluidRouter } from "./fluidRouter";
 import { IFluidHandle, IFluidHandleContext } from "./handles";
-import { IFluidSerializer } from "./serializer";
 
 /**
  * @deprecated Use `FluidObject` or the interface directly instead
