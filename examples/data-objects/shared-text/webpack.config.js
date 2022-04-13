6839858a8f6de2fed4382020b0cbf048aefe603b
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2022-04-12 18:25:46 -0700

    Update webpack-cli to version 4 (#9615)
    * Update webpack to lates v4, and webpacj-cli to latest
    
    * update several webpack cli ussages
    
    * Update webpack-dev-server to 4.0.0
    
    * update template for new webpack-dev-server
    
    * update webpack configs and fluid loader to support new dev server version
    
    * Fix devMiddleware publicPath
    
    * fix up a few more webpack configs
    
    * updage fluid-generate template
    
    * Fix comment in packages/tools/webpack-fluid-loader/src/routes.ts
    
    Co-authored-by: Helio Liu <59622401+heliocliu@users.noreply.github.com>
    
    * fix lint
    
    * fix merge
    
    * fix schema-upgrade webpack
    
    * fix a few ussages of webpack-dev-server
    
    * update package lock for merge
    
    Co-authored-by: Craig Macomber <Craig.Macomber@microsoft.com>
    Co-authored-by: Helio Liu <59622401+heliocliu@users.noreply.github.com>

--- examples/data-objects/shared-text/webpack.config.js
+++ examples/data-objects/shared-text/webpack.config.js
@@ -74,15 +74,7 @@ module.exports = env => {
                 tls: 'empty',
                 child_process: 'empty',
             },
-            devServer: {
-                publicPath: '/dist',
-                stats: "minimal",
-                before: (app, server) => fluidRoute.before(app, server, env),
-                after: (app, server) => fluidRoute.after(app, server, __dirname, env),
-                watchOptions: {
-                    ignored: "**/node_modules/**",
-                }
-            },
+            devServer: { devMiddleware: { stats: "minimal" }},
             output: {
                 filename: '[name].bundle.js',
                 chunkFilename: '[name].async.js',
@@ -100,5 +92,6 @@ module.exports = env => {
                 // new BundleAnalyzerPlugin()
             ]
         },
-        isProduction ? require("./webpack.prod") : require("./webpack.dev"));
+        isProduction ? require("./webpack.prod") : require("./webpack.dev"),
+        fluidRoute.devServerConfig(__dirname, env));
 };
--- packages/tools/webpack-fluid-loader/src/loader.ts
+++ packages/tools/webpack-fluid-loader/src/loader.ts
@@ -36,6 +36,7 @@ import { LocalDocumentServiceFactory, LocalResolver } from "@fluidframework/loca
 import { RequestParser, createDataStoreFactory } from "@fluidframework/runtime-utils";
 import { ensureFluidResolvedUrl } from "@fluidframework/driver-utils";
 import { IFluidDataStoreFactory } from "@fluidframework/runtime-definitions";
+import { Port } from "webpack-dev-server";
 import { MultiUrlResolver } from "./multiResolver";
 import { deltaConns, getDocumentServiceFactory } from "./multiDocumentServiceFactory";
 import { OdspPersistentCache } from "./odspPersistantCache";
@@ -45,7 +46,7 @@ export interface IDevServerUser extends IUser {
 }
 
 export interface IBaseRouteOptions {
-    port: number;
+    port: Port;
     npm?: string;
 }
 
