8bbe5a015b5d3e4b5601b3bc6cfd3785ead8d48c
Gary Wilber <41303831+GaryWilber@users.noreply.github.com>
2022-04-14 09:53:32 -0700

    Allow deli to manage read clients (#9191)

--- server/routerlicious/packages/services-core/src/clientManager.ts
+++ server/routerlicious/packages/services-core/src/clientManager.ts
@@ -3,7 +3,14 @@
  * Licensed under the MIT License.
  */
 
-import { IClient, ISignalClient } from "@fluidframework/protocol-definitions";
+import { IClient, ISignalClient, ISignalMessage } from "@fluidframework/protocol-definitions";
+
+export interface ITimedClient extends IClient {
+    /**
+     * Timestamp for the last time deli heard about this client
+     */
+    lastKeepAlive: number;
+}
 
 /**
  * Manages the list of clients connected to the websocket.
@@ -12,15 +19,26 @@ export interface IClientManager {
     /**
      * Adds a client to the list.
      */
-    addClient(tenantId: string, documentId: string, clientId: string, details: IClient): Promise<void>;
+    addClient(
+        tenantId: string,
+        documentId: string,
+        clientId: string,
+        details: IClient,
+        signalMessage?: ISignalMessage): Promise<void>;
 
     /**
      * Removes a client from the list.
      */
-    removeClient(tenantId: string, documentId: string, clientId: string): Promise<void>;
+    removeClient(tenantId: string, documentId: string, clientId: string, signalMessage?: ISignalMessage): Promise<void>;
 
     /**
      * Returns all clients currently connected.
      */
     getClients(tenantId: string, documentId: string): Promise<ISignalClient[]>;
+
+    /**
+     * Returns all clients currently connected including a keep alive time.
+     * Should be used with delis read only client functionality.
+     */
+    getTimedClients?(tenantId: string, documentId: string): Promise<Map<string, ITimedClient>>;
 }
