9b91c92bf6a9770991853d0698f84b01270b5902
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2022-04-18 10:39:16 -0700

    Update misc webpack related packages (#9889)
    As part of the effort to update to webpack5 (#9568) this updates a few webpack related packages, hopefully reducing the number of issues when updating webpack itself.
    
    The most significant change is migrating from duplicate-package-checker-webpack-plugin to its fork @cerner/duplicate-package-checker-webpack-plugin because the original has not merged support for webpack5, see darrenscerri/duplicate-package-checker-webpack-plugin#46
    
    Co-authored-by: Craig Macomber <Craig.Macomber@microsoft.com>

--- examples/data-objects/monaco/webpack.config.js
+++ examples/data-objects/monaco/webpack.config.js
@@ -5,7 +5,7 @@
 
 const fluidRoute = require("@fluid-tools/webpack-fluid-loader");
 const path = require('path');
-const merge = require('webpack-merge');
+const { merge } = require("webpack-merge");
 // const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = env => {
