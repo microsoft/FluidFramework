98d21e01b1e9bceccb2cc259e50262d331fdf023
Sasha Simic <simic.sasa@gmail.com>
2022-04-19 08:53:43 -0700

    Azure client container recreation API. (#9729)

--- packages/framework/azure-client/src/test/types/validateAzureClientPrevious.ts
+++ packages/framework/azure-client/src/test/types/validateAzureClientPrevious.ts
@@ -48,6 +48,7 @@ declare function get_old_ClassDeclaration_AzureClient():
 declare function use_current_ClassDeclaration_AzureClient(
     use: TypeOnly<current.AzureClient>);
 use_current_ClassDeclaration_AzureClient(
+    // @ts-expect-error compatibility expected to be broken
     get_old_ClassDeclaration_AzureClient());
 
 /*
