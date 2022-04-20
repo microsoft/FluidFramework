4d215880765a1f76dce654c4100ad10824a994f1
Vlad Sudzilouski <vlad@sudzilouski.com>
2022-04-20 14:00:34 -0700

    Fix early exit issue in summarizer (#9891)
    Closes #9884
    
    If we exit the process where new return "early exit after starting summarizer" is added when container did not yet connect (we exit because main container closed or lost connection), then summarizer container is not closed, and will not close when it connects, thus it will stick around for as long as this JS scope is running.
    
    This likely affects 2% of summarizer sessions longer than 1 minute (based on telemetry, though that maybe result on many causes).
    Note that this used to not cause any other issues (other than waste of resources), but I believe with recent changes in how we elect summarizer it will, as such client will be considered to be elected summarizer, and thus no new summarizers will start.
    Prior to that change, a summarizer container without parent container would not be considered an elected summarizer.

--- packages/runtime/container-runtime/src/test/summarizerClientElection.spec.ts
+++ packages/runtime/container-runtime/src/test/summarizerClientElection.spec.ts
@@ -67,6 +67,7 @@ describe("Summarizer Client Election", () => {
             // Approximation, as ideally it should become cancelled immediately after stop() call
             return this.state !== "running";
         }
+        public close() {}
         public stop(reason?: string): void {
             this.stopDeferred.resolve(reason);
         }
