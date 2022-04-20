5a52938075a676a0513daa8a1573adb5257195ea
Craig Macomber <Craig.Macomber@microsoft.com>
2022-04-20 14:44:04 -0700

    Revert "remove unused webpack"
    This reverts commit e8fe4315a959c8594af59183dccc2b493392d21e.

--- experimental/PropertyDDS/examples/property-inspector/webpack.config.js
+++ experimental/PropertyDDS/examples/property-inspector/webpack.config.js
@@ -14,14 +14,6 @@ module.exports = env => {
         devServer: {
             port: 9000
         },
-        // This impacts which files are watched by the dev server (and likely by webpack if watch is true).
-        // This should be configurable under devServer.static.watch
-        // (see https://github.com/webpack/webpack-dev-server/blob/master/migration-v4.md) but that does not seem to work.
-        // The CLI options for disabling watching don't seem to work either, so this may be a symptom of using webpack4 with the newer webpack-cli and webpack-dev-server.
-        watchOptions: {
-            ignored: "**",
-            // ignored: "**/node_modules/**",
-        },
         module: {
             rules: [{
                     test: /\.s?css$/,
@@ -38,7 +30,7 @@ module.exports = env => {
             ]
         },
         output: {
-            filename: "[name].[hash].js",
+            filename: "[name].[contenthash].js",
         },
         plugins: [
             new HtmlWebpackPlugin({
