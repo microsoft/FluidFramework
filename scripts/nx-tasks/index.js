ea34efd5e5ddbf1a3d659a818575491b238860b3
Tyler Butler <tyler@tylerbutler.com>
2022-04-21 02:40:42 +0000

    script updates

--- scripts/nx-tasks/index.js
+++ scripts/nx-tasks/index.js
@@ -10,54 +10,69 @@ const FLUID_ROOT = process.env._FLUID_ROOT_ ? process.env._FLUID_ROOT_ : 'C:\\co
 const STARTING_PATH = process.cwd();
 
 (async () => {
-  for await (const p of globbyStream('**/package.json', { gitignore: true })) {
-    const absPath = path.dirname(path.resolve(STARTING_PATH, p));
-    process.chdir(absPath);
+    for await (const p of globbyStream('**/package.json', { gitignore: true })) {
+        const absPath = path.dirname(path.resolve(STARTING_PATH, p));
+        process.chdir(absPath);
 
-    console.log(process.cwd());
-    console.log(absPath);
-    const relPath = path.relative(FLUID_ROOT, absPath).split(path.sep).join(path.posix.sep);
-    console.log(relPath);
+        console.log(process.cwd());
+        console.log(absPath);
+        const relPath = path.relative(FLUID_ROOT, absPath).split(path.sep).join(path.posix.sep);
+        console.log(relPath);
 
-    const pkgJson = json(path.join(absPath, 'package.json'));
+        const pkgJson = json(path.join(absPath, 'package.json'));
 
-    const pkgName = pkgJson.get('name');
-    const isPrivate = pkgJson.get('private');
+        const pkgName = pkgJson.get('name');
+        const isPrivate = pkgJson.get('private');
 
-    if (!isPrivate) {
-      let tags = [];
-      if (absPath.includes('common')) {
-        tags = ['scope:common'];
-      } else if (absPath.includes('examples')) {
-        tags = ['scope:examples'];
-      } else if (absPath.includes('server')) {
-        tags = ['scope:server'];
-      } else {
-        tags = ['scope:client'];
-      }
+        if (!isPrivate) {
+            let tags = [];
+            let projectType;
+            if (absPath.includes('common')) {
+                tags = ['scope:common'];
+                projectType = 'library';
+            } else if (absPath.includes('examples')) {
+                tags = ['scope:examples'];
+                projectType = 'app';
+            } else if (absPath.includes('server')) {
+                tags = ['scope:server'];
+                projectType = 'library';
+            } else {
+                tags = ['scope:client'];
+                projectType = 'library';
+            }
 
-      pkgJson
-        .set('nx.tags', tags)
-        .save();
+            pkgJson
+                .set('nx.tags', tags)
+                .set('nx.targets', {
+                    'build:esnext': {
+                        executor: '@nrwl/workspace:run-script',
+                        outputPath: ['libs'],
+                        options: {
+                            script: 'build:esnext',
+                        },
+                    }
+                })
+                .set('nx.projectType', projectType)
+                .save();
 
-      // json(path.join(absPath, 'package.json'))
-      //   .set('scripts.build', `nx build:compile ${pkgName} && nx lint ${pkgName} && nx build:docs ${pkgName}`)
-      //   .set('scripts.build:commonjs', `nx tsc ${pkgName} && nx build:test ${pkgName}`)
-      //   .set('scripts.build:compile', `nx build:commonjs ${pkgName} && nx build:esnext ${pkgName}`)
-      //   .set('scripts.build:full', `nx build ${pkgName}`)
-      //   .set('scripts.build:full:compile', `nx build:compile ${pkgName}`)
-      //   .set('scripts.lint', `nx eslint ${pkgName}`)
-      //   .set('scripts.lint:fix', `nx eslint:fix ${pkgName}`)
-      //   .save();
+            // json(path.join(absPath, 'package.json'))
+            //   .set('scripts.build', `nx build:compile ${pkgName} && nx lint ${pkgName} && nx build:docs ${pkgName}`)
+            //   .set('scripts.build:commonjs', `nx tsc ${pkgName} && nx build:test ${pkgName}`)
+            //   .set('scripts.build:compile', `nx build:commonjs ${pkgName} && nx build:esnext ${pkgName}`)
+            //   .set('scripts.build:full', `nx build ${pkgName}`)
+            //   .set('scripts.build:full:compile', `nx build:compile ${pkgName}`)
+            //   .set('scripts.lint', `nx eslint ${pkgName}`)
+            //   .set('scripts.lint:fix', `nx eslint:fix ${pkgName}`)
+            //   .save();
 
-      json(path.join(absPath, 'project.json'))
-        .set({
-          name: pkgName,
-          root: relPath,
-          sourceRoot: path.posix.join(relPath, 'src'),
-          projectType: 'library',
-          generators: {},
-          targets: {
+            // json(path.join(absPath, 'project.json'))
+            //     .set({
+            //         name: pkgName,
+            //         root: relPath,
+            //         sourceRoot: path.posix.join(relPath, 'src'),
+            //         projectType: 'library',
+            //         generators: {},
+            //         targets: {
             // build: {
             //   executor: '@nrwl/workspace:run-commands',
             //   options: {
@@ -121,13 +136,13 @@ const STARTING_PATH = process.cwd();
             //     ],
             //   },
             // },
-            'build:esnext': {
-              executor: '@nrwl/workspace:run-script',
-              outputPath: ['libs'],
-              options: {
-                script: 'build:esnext',
-              },
-            },
+            // 'build:esnext': {
+            //     executor: '@nrwl/workspace:run-script',
+            //         outputPath: ['libs'],
+            //             options: {
+            //         script: 'build:esnext',
+            //                 },
+            // },
             // lint: {
             //   executor: '@nrwl/workspace:run-commands',
             //   options: {
@@ -139,9 +154,10 @@ const STARTING_PATH = process.cwd();
             //     dependsOn: [],
             //   },
             // },
-          },
-        })
-        .save();
+            //         },
+            //     })
+            //                 .save();
+        }
     }
     //   .removeScript('build')
     //   .removeScript('build:commonjs')
@@ -153,7 +169,7 @@ const STARTING_PATH = process.cwd();
     //   .save();
 
     // process.chdir(STARTING_PATH);
-  }
+    // }
 
-  console.log('Done');
+    console.log('Done');
 })();
