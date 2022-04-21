74cad2b1d995260a7b64e76e5e2fa690cc52183c
Craig Macomber <Craig.Macomber@microsoft.com>
2022-04-20 20:28:24 -0700

    fix array proxies for numbers

--- experimental/PropertyDDS/packages/property-proxy/src/arrayProxyHandler.ts
+++ experimental/PropertyDDS/packages/property-proxy/src/arrayProxyHandler.ts
@@ -150,24 +150,24 @@ export const arrayProxyHandler: ProxyHandler<ComponentArray> = {
 
     /**
      * The trap for the in operator.
-     * Forwards the query to the has() method of the {@link external:ArrayProperty ArrayProperty}.
      * @param target The {@link ComponentArray} the Proxy handles.
      * @param key The name of the property/function that is to be accessed.
      * @return if the key is part of the {@link external:ArrayProperty ArrayProperty}, otherwise false.
      */
-    // Type safety note:
-    // Typescript is incorrectly typing the ProxyHandler<ComponentArray>.has to take a key of `string | symbol`.
-    // This is incorrect since numbers are allowed as keys.
-    // This function thus takes PropertyKey to allow numbers despite
-    // ProxyHandler<ComponentArray>.has's type claiming thats unneeded.
-    // This code is expected to return true for numbers which index an existing value, and false for those which do not.
-    // This is how "has" behaves for javascript arrays as well.
-    has: (target: ComponentArray, key: PropertyKey) => {
+    has: (target: ComponentArray, key: string | symbol) => {
         switch (typeof key) {
-            case "number":
-                return Number.isInteger(key) && key >= 0 && key < target.getProperty().getLength();
             case "string":
-                return key === "swap" || key in [];
+                {
+                    if (key === "swap" || key in []) {
+                        return true;
+                    }
+                    // Act like an array.
+                    // This means that "0" can be present, but not "0.0", "0.1", "-0", " 0" etc.
+                    // Simplest way to check for this is to round trip:
+                    const numeric = Number(key);
+                    return String(Number(key)) === key &&
+                        Number.isInteger(numeric) && numeric >= 0 && numeric < target.getProperty().getLength();
+                }
             default:
                 return key === proxySymbol;
         }
@@ -182,75 +182,78 @@ export const arrayProxyHandler: ProxyHandler<ComponentArray> = {
     ownKeys: (target: ComponentArray) => Reflect.ownKeys(Array.from(target.getProperty().getIds())),
 
     /**
-     * The set trap handles setting of properties. If key is a number >= 0 it sets the
-     * property at that index in the {@link external:ArrayProperty ArrayProperty}.
+     * The set trap handles setting of properties.
+     *
+     * If key is a numeric string it sets the property at that index in the
+     * {@link external:ArrayProperty ArrayProperty}.
+     * In this case the key must be a normalized positive integer.
+     *
      * If the key is 'length' it sets a new length for the {@link external:ArrayProperty ArrayProperty}.
+     *
      * Otherwise, it just sets it on the associated {@link ComponentArray}.
+     *
      * @param target The {@link ComponentArray} the Proxy handles.
      * @param key The name of the property/function that is to be accessed.
      * @param value The value to be set.
      * @return Returns a boolean.
      */
-    // Type safety note:
-    // Typescript is incorrectly typing the ProxyHandler<ComponentArray>.set to take a key of `string | symbol`.
-    // This is incorrect since numbers are allowed as keys.
-    // This function thus takes PropertyKey to allow numbers despite
-    // ProxyHandler<ComponentArray>.set's type claiming thats unneeded.
-    set(target: ComponentArray, key: PropertyKey, value: any) {
+    set(target: ComponentArray, key: string | symbol, value: any) {
         // process key for cases like "*1" 1 "string_key", "*string_key"
-        let processed_key = key;
+        let processed_key: string | symbol = key;
         const asteriskFound = Utilities.containsAsterisk(processed_key);
         if (asteriskFound) {
             // if we found an * we can safely treat it as string
             processed_key = (processed_key as string).slice(0, -1);
         }
         // convert strings of numbers to numbers
-        processed_key = isNaN(Number(processed_key)) ? processed_key : Number(processed_key);
-        // handle special cases for strigns and numbers
-        if (typeof processed_key === "string") {
+        const numeric_key = Number(processed_key);
+        // handle special cases for strings and numbers
+        if (isNaN(numeric_key)) {
             if (processed_key === "length") {
                 return setLength(target, Number(value));
             }
-        } else if (typeof processed_key === "number") {
-            if (!isNaN(processed_key) && processed_key >= 0) {
-                const property = target.getProperty();
-                const isReferenceArray = PropertyFactory.instanceOf(property, "Reference", "array");
-
-                let insert = false;
-                if (processed_key >= property.getLength()) {
-                    setLength(target, processed_key + 1);
-                    // Trying to set something that was currently not in the array,
-                    // means a new reference path is inserted
-                    insert = true;
-                }
-                const specialCases = setTrapSpecialCases.includes(target.lastCalledMethod);
-
-                if (isReferenceArray && forceType<ReferenceArrayProperty>(property)
-                    && !specialCases && !asteriskFound && !insert) {
-                    Utilities.setValueOfReferencedProperty(property, processed_key, value);
-                } else {
-                    if (asteriskFound && !isReferenceArray) {
-                        throw new Error(PropertyProxyErrors.NON_REFERENCE_ASSIGN);
-                    }
-                    if (property.isPrimitiveType() || property.get(processed_key)!.getContext() === "single") {
-                        Utilities.throwOnIterableForSingleProperty(value);
-                        property.set(processed_key,
-                            Utilities.prepareElementForInsertion(property, value, target.lastCalledMethod));
-                    } else {
-                        const child = property.get(processed_key);
-                        if (child) {
-                            Utilities.assign(child, value);
-                        } else {
-                            throw new Error(PropertyProxyErrors.INVALID_PROPERTY);
-                        }
-                    }
-                }
-                return true;
+            // If we land here there was no special case: simply assign.
+            target[processed_key] = value;
+            return true;
+        } else {
+            // Validate numeric property: must be a normalized numeric string for an non-negative integer.
+            if (numeric_key < 0 || !Number.isInteger(numeric_key) || String(numeric_key) !== processed_key) {
+                throw new Error(PropertyProxyErrors.INVALID_PROPERTY);
             }
+
+            const property = target.getProperty();
+            const isReferenceArray = PropertyFactory.instanceOf(property, "Reference", "array");
+
+            let insert = false;
+            if (numeric_key >= property.getLength()) {
+                setLength(target, numeric_key + 1);
+                // Trying to set something that was currently not in the array,
+                // means a new reference path is inserted
+                insert = true;
+            }
+            const specialCases = setTrapSpecialCases.includes(target.lastCalledMethod);
+
+            if (isReferenceArray && forceType<ReferenceArrayProperty>(property)
+                && !specialCases && !asteriskFound && !insert) {
+                Utilities.setValueOfReferencedProperty(property, numeric_key, value);
+            } else {
+                if (asteriskFound && !isReferenceArray) {
+                    throw new Error(PropertyProxyErrors.NON_REFERENCE_ASSIGN);
+                }
+                if (property.isPrimitiveType() || property.get(numeric_key)!.getContext() === "single") {
+                    Utilities.throwOnIterableForSingleProperty(value);
+                    property.set(numeric_key,
+                        Utilities.prepareElementForInsertion(property, value, target.lastCalledMethod));
+                } else {
+                    const child = property.get(numeric_key);
+                    if (child) {
+                        Utilities.assign(child, value);
+                    } else {
+                        throw new Error(PropertyProxyErrors.INVALID_PROPERTY);
+                    }
+                }
+            }
+            return true;
         }
-        // if we land here there was no special case
-        // simply assign simply assign
-        target[processed_key] = value;
-        return true;
     },
 };
