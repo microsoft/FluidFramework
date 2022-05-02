e32390b993d7792b5328bc489d34f475b1c926a0
REDMOND\kianthompson <kianthompson@microsoft.com>
2022-05-02 16:17:15 -0700

    Add error event when we are not caught up with summarizing

--- packages/runtime/container-runtime/src/runningSummarizer.ts
+++ packages/runtime/container-runtime/src/runningSummarizer.ts
@@ -337,6 +337,7 @@ export class RunningSummarizer implements IDisposable {
             // lockedSummaryAction() will retry heuristic-based summary at the end of current attempt
             // if it's still needed
             this.tryWhileSummarizing = true;
+            this.logger.sendErrorEvent({ eventName: "SummarizingNotCaughtUp" });
             return;
         }
 
