46d4a5d4644e4a9fdf27368b73a3bb3b8a985a62
Matt Rakow <ChumpChief@users.noreply.github.com>
2022-05-02 16:55:50 -0700

    1085f51e902b1cb80e3b92ac17ee6e7866fda508

--- examples/data-objects/client-ui-lib/src/ui/index.ts
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
-export * from "./browserContainerHost";
-export * from "./component";
-export * from "./geometry";
-export * from "./utils";
