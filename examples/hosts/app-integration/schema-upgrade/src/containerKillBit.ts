b017047e7d15d41bdc51194e81fc06708a75a2be
Matt Rakow <ChumpChief@users.noreply.github.com>
2022-05-02 12:07:01 -0700

    upto a16019bb71b67deef3924ab47036d1aa534bafa9

--- examples/hosts/app-integration/schema-upgrade/src/containerKillBit.ts
+++ examples/hosts/app-integration/schema-upgrade/src/containerKillBit.ts
@@ -6,9 +6,10 @@
 import { TaskManager } from "@fluid-experimental/task-manager";
 import { Quorum } from "@fluid-internal/quorum";
 import { DataObject, DataObjectFactory } from "@fluidframework/aqueduct";
-import { IEvent, IEventProvider } from "@fluidframework/common-definitions";
 import { ConsensusRegisterCollection } from "@fluidframework/register-collection";
 // import { IFluidHandle } from "@fluidframework/core-interfaces";
+
+import type { IContainerKillBit } from "./interfaces";
 
 const quorumKey = "quorum";
 const crcKey = "crc";
@@ -16,19 +17,6 @@ const taskManagerKey = "task-manager";
 const markedForDestructionKey = "marked";
 const destroyTaskName = "destroy";
 const deadKey = "dead";
-
-export interface IContainerKillBitEvents extends IEvent {
-    (event: "markedForDestruction" | "dead", listener: () => void);
-}
-
-export interface IContainerKillBit extends IEventProvider<IContainerKillBitEvents> {
-    dead: boolean;
-    setDead(): Promise<void>;
-    markedForDestruction: boolean;
-    markForDestruction(): Promise<void>;
-    volunteerForDestruction(): Promise<void>;
-    haveDestructionTask(): boolean;
-}
 
 export class ContainerKillBit extends DataObject implements IContainerKillBit {
     private _quorum: Quorum | undefined;
