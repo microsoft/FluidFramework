fd48aea38120dc2bb09d09d4c4394eb37819ed12
Scott Norton <scottnorton@microsoft.com>
2022-05-03 17:49:02 -0400

    [bump] package version for client (patch) (#10139)
    @fluidframework/build-common:     0.24.0 (unchanged)
         @fluidframework/eslint-config-fluid:  0.28.2000 (unchanged)
          @fluidframework/common-definitions:  0.21.1000 (unchanged)
                @fluidframework/common-utils:  0.33.1000 (unchanged)
             @fluidframework/core-interfaces:  0.43.2000 (unchanged)
        @fluidframework/protocol-definitions: 0.1028.2000 (unchanged)
          @fluidframework/driver-definitions:  0.46.2000 (unchanged)
       @fluidframework/container-definitions:  0.48.2000 (unchanged)
                                      Server: 0.1036.2001 (unchanged)
                                      Client:  0.59.2001 -> 0.59.2002
                      @fluid-tools/benchmark:     0.40.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
         @fluidframework/azure-local-service:      0.1.0 (unchanged)

--- packages/drivers/odsp-driver/src/packageVersion.ts
+++ packages/drivers/odsp-driver/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/odsp-driver";
-export const pkgVersion = "0.59.2001";
+export const pkgVersion = "0.59.2002";
--- packages/test/functional-tests/src/packageVersion.ts
+++ packages/test/functional-tests/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-internal/functional-tests";
-export const pkgVersion = "0.59.2001";
+export const pkgVersion = "0.59.2002";
