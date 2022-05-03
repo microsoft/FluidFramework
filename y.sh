a55cd95bf9a9a3e5e3abda129edbddc1fc704eec
Tyler Butler <tyler@tylerbutler.com>
2022-05-03 17:21:44 +0000

    use npm for tasks so fluid-build will work

--- y.sh
+++ y.sh
@@ -1,18 +1,36 @@
+# for file in $(fd package.json --type file); do
+#     # echo "$file"
+
+#     # echo '  npm:      ==> pnpm:'
+#     sd 'npm:' 'yarn:' "$file"
+
+#     # echo '  npm run   ==> pnpm run'
+#     sd 'npm run ' 'yarn run ' "$file"
+#     # sd '"tsc": "tsc"' '"tsc": "yarn pnpify tsc"' "$file"
+
+#     sd 'yarn:@' 'npm:@' "$file"
+#     sd 'yarn:fluid-framework' 'npm:fluid-framework' "$file"
+
+#     sd '\-\-typescript-compiler-folder \./node_modules/typescript &&' '&&' "$file"
+#     sd '\-\-typescript-compiler-folder \.\./\.\./\.\./node_modules/typescript &&' '&&' "$file"
+#     # sd '  ' ' ' "$file"
+# done
+
 for file in $(fd package.json --type file); do
     # echo "$file"
 
     # echo '  npm:      ==> pnpm:'
-    sd 'npm:' 'yarn:' "$file"
+    sd 'yarn:' 'npm:' "$file"
 
     # echo '  npm run   ==> pnpm run'
-    sd 'npm run ' 'yarn run ' "$file"
+    sd 'yarn run ' 'npm run ' "$file"
     # sd '"tsc": "tsc"' '"tsc": "yarn pnpify tsc"' "$file"
 
     sd 'yarn:@' 'npm:@' "$file"
     sd 'yarn:fluid-framework' 'npm:fluid-framework' "$file"
 
-    sd '\-\-typescript-compiler-folder \./node_modules/typescript &&' '&&' "$file"
-    sd '\-\-typescript-compiler-folder \.\./\.\./\.\./node_modules/typescript &&' '&&' "$file"
+    # sd '\-\-typescript-compiler-folder \./node_modules/typescript &&' '&&' "$file"
+    # sd '\-\-typescript-compiler-folder \.\./\.\./\.\./node_modules/typescript &&' '&&' "$file"
     # sd '  ' ' ' "$file"
 done
 
