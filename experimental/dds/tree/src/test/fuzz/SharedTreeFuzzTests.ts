2aee3de707c80e0ce7187a4b07effc0c1887b30d
Abram Sanderson <Abram.Sanderson@microsoft.com>
2022-05-03 10:29:58 -0700

    e85239320a86c29d20c5d322273aee6b4e00af8b

--- experimental/dds/tree/src/test/fuzz/SharedTreeFuzzTests.ts
+++ experimental/dds/tree/src/test/fuzz/SharedTreeFuzzTests.ts
@@ -168,13 +168,17 @@ export function runSharedTreeFuzzTests(title: string): void {
 			}).timeout(10000);
 		}
 
-		function runMixedVersionTests(summarizeHistory: boolean, testsPerSuite: number): void {
+		function runMixedVersionTests(summarizeHistory: boolean, testsPerSuite: number, testLength: number): void {
 			// TODO: fix these tests. See https://github.com/microsoft/FluidFramework/issues/10103
 			if (!summarizeHistory) {
 				describe('using 0.0.2 and 0.1.1 trees', () => {
 					for (let seed = 0; seed < testsPerSuite; seed++) {
 						runTest(
-							() => take(1000, makeOpGenerator({ joinConfig: { summarizeHistory: [summarizeHistory] } })),
+							() =>
+								take(
+									testLength,
+									makeOpGenerator({ joinConfig: { summarizeHistory: [summarizeHistory] } })
+								),
 							seed
 						);
 					}
@@ -185,7 +189,7 @@ export function runSharedTreeFuzzTests(title: string): void {
 						runTest(
 							() =>
 								take(
-									1000,
+									testLength,
 									makeOpGenerator({
 										joinConfig: {
 											writeFormat: [WriteFormat.v0_0_2],
@@ -203,7 +207,7 @@ export function runSharedTreeFuzzTests(title: string): void {
 						runTest(
 							() =>
 								take(
-									1000,
+									testLength,
 									makeOpGenerator({
 										joinConfig: {
 											writeFormat: [WriteFormat.v0_1_1],
@@ -271,12 +275,14 @@ export function runSharedTreeFuzzTests(title: string): void {
 		}
 
 		const testCount = 1;
+		const testLength = 200;
 		describe('with no-history summarization', () => {
-			runMixedVersionTests(false, testCount);
+			runMixedVersionTests(false, testCount, testLength);
 		});
 
-		describe('with history summarization', () => {
-			runMixedVersionTests(true, testCount);
+		// TODO: fix these tests. See https://github.com/microsoft/FluidFramework/issues/10103
+		describe.skip('with history summarization', () => {
+			runMixedVersionTests(true, testCount, testLength);
 		});
 	});
 }
