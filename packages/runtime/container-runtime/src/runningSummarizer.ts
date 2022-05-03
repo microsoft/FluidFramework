034abe78b66f34116fa8076b1de55f76e8d34a3d
REDMOND\kianthompson <kianthompson@microsoft.com>
2022-05-03 11:24:20 -0700

    Add telemetry event

--- packages/runtime/container-runtime/src/runningSummarizer.ts
+++ packages/runtime/container-runtime/src/runningSummarizer.ts
@@ -337,6 +337,10 @@ export class RunningSummarizer implements IDisposable {
             // lockedSummaryAction() will retry heuristic-based summary at the end of current attempt
             // if it's still needed
             this.tryWhileSummarizing = true;
+            this.logger.sendErrorEvent({
+                eventName: "SummarizingNotCaughtUp",
+                summarizeReason: summarizeReason,
+            });
             return;
         }
 
