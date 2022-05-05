95201059971a30681f6764894ca890fb52e52ce4
Abram Sanderson <Abram.Sanderson@microsoft.com>
2022-05-05 11:58:38 -0700

    Pull out stochastic-test-utils into its own package (#10150)
    * Pull out stochastic-test-utils into its own package, adding unit tests for generator utils
    
    Co-authored-by: Abram Sanderson <absander@microsoft.com>

---
+++ packages/test/stochastic-test-utils/.eslintrc.js
@@ -0,0 +1,19 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = {
+    "extends": [
+        "@fluidframework/eslint-config-fluid"
+    ],
+    "parserOptions": {
+        "project": ["./tsconfig.json", "./src/test/tsconfig.json"]
+    },
+    "rules": {
+        // Intentionally not unifying signatures can enable more scoped API documentation and a better developer experience.
+        // Once @typescript-eslint/eslint-plugin is updated to ^5.19.0, this could use the `ignoreDifferentlyNamedParameters`
+        // option instead.
+        "@typescript-eslint/unified-signatures": 'off',
+    }
+}
