78fe0f67a0f089d73cb98ab05496de26352e5e04
Tyler Butler <tyler@tylerbutler.com>
2022-05-05 02:19:16 +0000

    CI failure

--- packages/runtime/garbage-collector/src/garbageCollector.ts
+++ packages/runtime/garbage-collector/src/garbageCollector.ts
@@ -41,7 +41,6 @@ export function runGarbageCollection(
             // Log a telemetry event if there is a node missing for a referenced id. This should not happen but for now
             // we don't assert. We can monitor telemetry for a while to figure out next steps.
 
-            /* eslint-disable tsdoc/syntax */
             /**
              * This telemetry is currently too noisy. Start sending it GC is enabled end-to-end. See here for details -
              * https://github.com/microsoft/FluidFramework/issues/4939
@@ -51,7 +50,6 @@ export function runGarbageCollection(
              *    missingNodeId: id,
              * });
             */
-            /* eslint-enable tsdoc/syntax */
         }
     }
 
