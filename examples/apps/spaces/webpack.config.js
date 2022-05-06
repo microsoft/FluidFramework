5ae35f9f5759fed8a5f83fdbfae2d3a885b9fb11
Craig Macomber <Craig.Macomber@microsoft.com>
2022-05-05 21:02:13 -0700

    fix spaces example

--- examples/apps/spaces/webpack.config.js
+++ examples/apps/spaces/webpack.config.js
@@ -6,6 +6,7 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 const pkg = require("./package.json");
 const componentName = pkg.name.slice(1);
@@ -52,6 +53,24 @@ module.exports = (env) => {
             libraryTarget: "umd",
         },
         plugins: [
+            // Packages we use expect these to be defined (errors at runtime if they are not), so provide them:
+            new webpack.DefinePlugin({
+                'process.env.NODE_DEBUG': undefined,
+                'global': {
+                    'Symbol': 'Symbol',
+                    'BigInt64Array':'BigInt64Array',
+                    'BigUint64Array':'BigUint64Array',
+                    'Float32Array':'Float32Array',
+                    'Float64Array':'Float64Array',
+                    'Int16Array':'Int16Array',
+                    'Int32Array':'Int32Array',
+                    'Int8Array':'Int8Array',
+                    'Uint16Array':'Uint16Array',
+                    'Uint32Array':'Uint32Array',
+                    'Uint8Array':'Uint8Array',
+                    'Uint8ClampedArray':'Uint8ClampedArray',
+                }
+              }),
             new HtmlWebpackPlugin({
                 template: "./public/index.html",
             }),
