ed5ac3834bec926da1fe1d9b643de52845059b22
Craig Macomber <Craig.Macomber@microsoft.com>
2022-05-10 13:58:00 -0700

    split out typed schema tests from implementation

--- packages/dds/tree/src/schema/TypedSchema.ts
+++ packages/dds/tree/src/schema/TypedSchema.ts
@@ -3,18 +3,20 @@
  * Licensed under the MIT License.
  */
 
-import { Invariant, requireTrue } from "../TypeCheck";
+import { Invariant } from "../TypeCheck";
 import { fieldSchema, treeSchema, TreeSchemaBuilder } from "./Builders";
 import {
     FieldSchema,
     LocalFieldKey,
     FieldKind,
     TreeSchema,
-    ValueSchema,TreeSchemaIdentifier
+    ValueSchema,
+    TreeSchemaIdentifier,
 } from "./Schema";
 
 /**
  * APIs for building typescript types and schema together.
+ * This is an example schema language which can support schema aware APIs in typescript without code gen.
  */
 
 /**
@@ -33,49 +35,36 @@ interface FieldSchemaTypeInfo {
     kind: FieldKind;
 }
 
-function build<T extends TreeSchemaTypeInfo>(t: T): LabeledTreeSchema<T> {
+/**
+ * Builds a TreeSchema with the type information also captured in the
+ * typescript type to allow for deriving schema aware APIs.
+ */
+export function typedTreeSchema<T extends TreeSchemaTypeInfo>(t: T): LabeledTreeSchema<T> {
     return treeSchema(t) as LabeledTreeSchema<T>;
 }
 
-function field<T extends FieldSchemaTypeInfo>(t: T): LabeledFieldSchema<T> {
-    return fieldSchema(t.kind, [...Object.keys(t.types)] as TreeSchemaIdentifier[])
+/**
+ * Builds a FieldSchema with the type information also captured in the
+ * typescript type to allow for deriving schema aware APIs.
+ */
+export function typedFieldSchema<T extends FieldSchemaTypeInfo>(t: T): LabeledFieldSchema<T> {
+    return fieldSchema(t.kind, [...Object.keys(t.types)] as TreeSchemaIdentifier[]);
 }
 
-const lk1 = "localKey1Name" as const;
-
-export const lk2 = "localKey2Name" as const;
-
-export const testTypeIdentifier = "testType" as const;
-
-const testField = field({
-    types: { testType: 0 as unknown },
-    kind: FieldKind.Value,
-});
-
-export const testTreeSchema = build({
-    local: { localKey1Name: testField },
-    global: {},
-    extraLocalFields: testField,
-    extraGlobalFields: true as const,
-    value: ValueSchema.Serializable as const,
-});
-
-type TestTreeSchema = TypeInfo<typeof testTreeSchema>;
-
-export type _assert = requireTrue<TestTreeSchema["extraGlobalFields"]>;
-
-type TypeInfo<T extends LabeledTreeSchema<any>> = T extends LabeledTreeSchema<
+export type TypeInfo<T extends LabeledTreeSchema<any>> = T extends LabeledTreeSchema<
     infer R
 >
     ? R
     : unknown;
-type FieldInfo<T extends LabeledFieldSchema<any>> =
+
+export type FieldInfo<T extends LabeledFieldSchema<any>> =
     T extends LabeledFieldSchema<infer R> ? R : unknown;
 
 export interface LabeledTreeSchema<T extends TreeSchemaTypeInfo>
     extends TreeSchema {
     readonly typeCheck?: Invariant<T>;
 
+    // Allow reading localFields through the normal map, but without losing type information.
     readonly localFields: ObjectToMap<T["local"], LocalFieldKey>;
 }
 
@@ -84,15 +73,6 @@ export interface LabeledFieldSchema<T extends FieldSchemaTypeInfo>
     readonly typeCheck?: Invariant<T>;
 }
 
-export type child = FieldInfo<TestTreeSchema["local"][typeof lk1]>;
-
-// type child2 = FieldInfo<xx["local"][typeof lk2]>;
-
 type ObjectToMap<T, K extends number | string> = ReadonlyMap<K, FieldSchema> & {
     get<X extends keyof T>(key: X): T[X];
 };
-
-export const xxxx = testTreeSchema.localFields.get(lk1);
-
-// This is an error since this field does not exist:
-// const xxx2 = testTreeSchema.localFields.get(lk2);
---
+++ packages/dds/tree/src/test/TypedSchema.spec.ts
@@ -0,0 +1,44 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+/* eslint-disable import/no-internal-modules */
+
+// TODO: what is the pattern for testing packages which have folders inside src?
+import { FieldKind, ValueSchema } from "../schema/Schema";
+import { typedTreeSchema, typedFieldSchema, FieldInfo, TypeInfo } from "../schema/TypedSchema";
+import { requireTrue } from "../TypeCheck";
+
+// These tests currently just cover the type checking, so its all compile time.
+
+const lk1 = "localKey1Name" as const;
+
+export const lk2 = "localKey2Name" as const;
+
+export const testTypeIdentifier = "testType" as const;
+
+const testField = typedFieldSchema({
+    types: { testType: 0 as unknown },
+    kind: FieldKind.Value,
+});
+
+export const testTreeSchema = typedTreeSchema({
+    local: { localKey1Name: testField },
+    global: {},
+    extraLocalFields: testField,
+    extraGlobalFields: true as const,
+    value: ValueSchema.Serializable as const,
+});
+
+type TestTreeSchema = TypeInfo<typeof testTreeSchema>;
+
+export type _assert = requireTrue<TestTreeSchema["extraGlobalFields"]>;
+
+export type child = FieldInfo<TestTreeSchema["local"][typeof lk1]>;
+
+// type child2 = FieldInfo<xx["local"][typeof lk2]>;
+
+export const xxxx = testTreeSchema.localFields.get(lk1);
+
+// This is an error since this field does not exist:
+// const xxx2 = testTreeSchema.localFields.get(lk2);
