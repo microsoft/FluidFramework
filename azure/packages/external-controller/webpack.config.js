06b97dae3068a0a35c927ffe9a220ef0ec7d3dfd
Tyler Butler <tylerbu@microsoft.com>
2022-05-11 10:37:37 -0700

    add process polyfill

--- azure/packages/external-controller/webpack.config.js
+++ azure/packages/external-controller/webpack.config.js
@@ -6,7 +6,7 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = env => {
     const isProduction = env && env.production;
@@ -34,10 +34,12 @@ module.exports = env => {
             libraryTarget: "umd"
         },
         plugins: [
+            new webpack.ProvidePlugin({
+                process: 'process/browser'
+            }),
             new HtmlWebpackPlugin({
                 template: "./src/index.html",
             }),
-            // new CleanWebpackPlugin(),
         ],
     }, isProduction
         ? require("./webpack.prod")
