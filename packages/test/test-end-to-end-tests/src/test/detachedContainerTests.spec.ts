2dca2d5feff8de91974124054be381b7177d1d0f
justus-camp <102552460+justus-camp@users.noreply.github.com>
2022-05-11 21:04:26 +0000

    Re-add skip of detachedContainerTest for tinylicious

--- packages/test/test-end-to-end-tests/src/test/detachedContainerTests.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/detachedContainerTests.spec.ts
@@ -160,6 +160,10 @@ describeFullCompat("Detached Container", (getTestObjectProvider) => {
     });
 
     it("can create DDS in detached container and attach / update it", async function() {
+        // GitHub issue: #9534
+        if (provider.driver.type === "tinylicious") {
+            this.skip();
+        }
         const container = await loader.createDetachedContainer(provider.defaultCodeDetails);
         const dsClient1 = await requestFluidObject<ITestFluidObject>(container, "/");
 
