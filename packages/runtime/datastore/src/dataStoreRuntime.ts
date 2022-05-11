1fe6535d27c9bb145904f77c12572f43c91b7329
NicholasCouri <nichoc@microsoft.com>
2022-05-11 14:53:54 -0700

    Stop always summarizing DDS (because of blob aggregation)   (#10215)
    * Stop writting  full summary for data stores until BlobAggregationStorage gets used.
    
    * Remove BlobAggregationStorage from the runtime as it is not being used.
    
    * removing _storage

--- packages/runtime/datastore/src/dataStoreRuntime.ts
+++ packages/runtime/datastore/src/dataStoreRuntime.ts
@@ -708,13 +708,7 @@ IFluidDataStoreChannel, IFluidDataStoreRuntime, IFluidHandleContext {
                 // (i.e. it has a base mapping) - then we go ahead and summarize
                 return isAttached;
             }).map(async ([contextId, context]) => {
-                // If BlobAggregationStorage is engaged, we have to write full summary for data stores
-                // BlobAggregationStorage relies on this behavior, as it aggregates blobs across DDSs.
-                // Not generating full summary will mean data loss, as we will overwrite aggregate blob in new summary,
-                // and any virtual blobs that stayed (for unchanged DDSs) will need aggregate blob in previous summary
-                // that is no longer present in this summary.
-                // This is temporal limitation that can be lifted in future once BlobAggregationStorage becomes smarter.
-                const contextSummary = await context.summarize(true /* fullTree */, trackState);
+                const contextSummary = await context.summarize(fullTree, trackState);
                 summaryBuilder.addWithStats(contextId, contextSummary);
             }));
 
