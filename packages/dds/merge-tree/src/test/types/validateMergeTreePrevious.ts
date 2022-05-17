8420f48564c0477803a2f132372f3a66b85c1bd5
Ransom Richardson <rarichar@microsoft.com>
2022-05-17 15:37:09 -0400

    Fix missing backCompat

--- packages/dds/merge-tree/src/test/types/validateMergeTreePrevious.ts
+++ packages/dds/merge-tree/src/test/types/validateMergeTreePrevious.ts
@@ -686,6 +686,7 @@ declare function get_current_InterfaceDeclaration_IConsensusInfo():
 declare function use_old_InterfaceDeclaration_IConsensusInfo(
     use: TypeOnly<old.IConsensusInfo>);
 use_old_InterfaceDeclaration_IConsensusInfo(
+    // @ts-expect-error compatibility expected to be broken
     get_current_InterfaceDeclaration_IConsensusInfo());
 
 /*
