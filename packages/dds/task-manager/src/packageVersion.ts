6d4bafddbe3c7ab7a3b3692a5768c8f1ce5c1d97
Wes Carlson <wesley.carlson@microsoft.com>
2022-05-19 16:01:44 -0700

    [bump] package version for client (patch)
                @fluidframework/build-common:     0.24.0 (unchanged)
         @fluidframework/eslint-config-fluid:  0.28.2000 (unchanged)
          @fluidframework/common-definitions:  0.21.1000 (unchanged)
                @fluidframework/common-utils:  0.33.1000 (unchanged)
             @fluidframework/core-interfaces:  0.43.2000 (unchanged)
        @fluidframework/protocol-definitions: 0.1028.2000 (unchanged)
          @fluidframework/driver-definitions:  0.46.2000 (unchanged)
       @fluidframework/container-definitions:  0.48.2000 (unchanged)
                                      Server: 0.1036.2001 (unchanged)
                                      Client:  0.59.2002 -> 0.59.2003
                      @fluid-tools/benchmark:     0.40.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
         @fluidframework/azure-local-service:      0.1.0 (unchanged)

--- packages/dds/task-manager/src/packageVersion.ts
+++ packages/dds/task-manager/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-experimental/task-manager";
-export const pkgVersion = "0.59.2002";
+export const pkgVersion = "0.59.2003";
--- packages/framework/test-client-utils/src/packageVersion.ts
+++ packages/framework/test-client-utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/test-client-utils";
-export const pkgVersion = "0.59.2002";
+export const pkgVersion = "0.59.2003";
