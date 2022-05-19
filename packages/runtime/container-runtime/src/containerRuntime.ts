40da0c5f9c69ad5ec746a0e6e5bcdf09652c0d8f
Wes Carlson <49205066+wes-carlson@users.noreply.github.com>
2022-05-19 13:05:36 -0700

    policy-check fix (#10347)

--- packages/runtime/container-runtime/src/containerRuntime.ts
+++ packages/runtime/container-runtime/src/containerRuntime.ts
@@ -2202,7 +2202,7 @@ export class ContainerRuntime extends TypedEventEmitter<IContainerRuntimeEvents>
             case GCNodeType.SubDataStore:
                 return this.dataStores.getDataStorePackagePath(nodePath);
             default:
-                assert(false, "Package path requested for unsupported node type.");
+                assert(false, 0x2d8 /* "Package path requested for unsupported node type." */);
         }
     }
 
