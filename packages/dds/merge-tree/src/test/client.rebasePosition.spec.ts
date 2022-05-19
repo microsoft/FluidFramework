09a1c0ae5c0d0c9fc445f55ef3e111ab2fc363c1
Abram Sanderson <absander@microsoft.com>
2022-05-19 15:19:59 -0700

    Add unit tests for rebasePosition

---
+++ packages/dds/merge-tree/src/test/client.rebasePosition.spec.ts
@@ -0,0 +1,98 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "assert";
+import { IMergeTreeOp } from "../ops";
+import { TestClient } from "./testClient";
+
+describe("client.rebasePosition", () => {
+    const localUserLongId = "localUser";
+    const remoteUserLongId = "remoteUser";
+    let client: TestClient;
+    let seq: number;
+
+    const getTextAt = (pos: number): string => client.getText(pos, pos + 1);
+
+    beforeEach(() => {
+        client = new TestClient();
+        client.insertTextLocal(0, "hello world");
+        client.startOrUpdateCollaboration(localUserLongId);
+        seq = 0;
+    });
+
+    it("rebase past remote insert", () => {
+        client.insertTextRemote(0, "abc", undefined, ++seq, 0, remoteUserLongId);
+        const rebasedPos = client.rebasePosition(6 /* "w" */, 0, 0);
+        const text = getTextAt(rebasedPos);
+        assert.equal(text, "w", "rebased pos should still refer to 'w'");
+    });
+
+    it("rebase past remote delete", () => {
+        client.removeRangeRemote(0, 3, ++seq, 0, remoteUserLongId);
+        const rebasedPos = client.rebasePosition(6 /* w */, 0, 0);
+        const text = getTextAt(rebasedPos);
+        assert.equal(text, "w", "rebased pos should still refer to 'w'");
+    });
+
+    it("rebase amid remote delete", () => {
+        client.removeRangeRemote(5, 8 /* " wo" */, ++seq, 0, remoteUserLongId);
+        const rebasedPos = client.rebasePosition(6 /* w */, 0, 0);
+        const text = getTextAt(rebasedPos);
+        assert.equal(text, "r", "rebased pos should refer to position immediately following removed segment");
+    });
+
+    describe("with subsequent local changes", () => {
+        // Rebasing is made more complicated from the client perspective when there are local changes applied
+        // meanwhile, since the local state of the string contains segments that should not be considered
+        // when computing the position to rebase to (since they wouldn't be visible remotely)
+
+        let op1: IMergeTreeOp | undefined;
+        let op2: IMergeTreeOp | undefined;
+        beforeEach(() => {
+            op1 = client.insertTextLocal(5, "123456");
+            op2 = client.removeRangeLocal(3, 5);
+        });
+
+        // For these tests, rebasedPos conceptually refers to a position that a *remote* client should use in order
+        // to get an equivalent position to the one that was applied locally with a different refSeq.
+        // Since this suite doesn't actually spin up a remote client, we verify this equivalence by asking
+        // the local client to resolve that remote position and confirm the text matches what's expected.
+        const expectTextAtRebasedPosMatches = (pos: number, expected: string, message?: string) => {
+            const localViewOfRebasedPos = client.resolveRemoteClientPosition(pos, seq, remoteUserLongId);
+            assert(localViewOfRebasedPos !== undefined, "pos should be defined");
+            const text = getTextAt(localViewOfRebasedPos);
+            assert.equal(text, expected, message);
+        };
+
+        it("rebase past remote insert", () => {
+            const rebasedPos = client.rebasePosition(6 /* index 6 into "hello world" */, 0, 0);
+            const rebasedPos1 = client.rebasePosition(6 /* index 6 into "hello123456 world" */, 0, 1);
+            const rebasedPos2 = client.rebasePosition(6 /* index 6 into "hel123456 world" */, 0, 2);
+
+            expectTextAtRebasedPosMatches(rebasedPos, "w");
+
+            client.applyMsg(client.makeOpMessage(op1, ++seq, 0, localUserLongId), true);
+            expectTextAtRebasedPosMatches(rebasedPos1, "2");
+
+            client.applyMsg(client.makeOpMessage(op2, ++seq, 0, localUserLongId), true);
+            expectTextAtRebasedPosMatches(rebasedPos2, "4");
+        });
+
+        it("rebase past remote delete", () => {
+            client.removeRangeRemote(0, 2, ++seq, 0, remoteUserLongId);
+            const rebasedPos = client.rebasePosition(6 /* index 6 into "hello world" */, 0, 0);
+            const rebasedPos1 = client.rebasePosition(6 /* index 6 into "hello123456 world" */, 0, 1);
+            const rebasedPos2 = client.rebasePosition(6 /* index 6 into "hel123456 world" */, 0, 2);
+
+            expectTextAtRebasedPosMatches(rebasedPos, "w");
+
+            client.applyMsg(client.makeOpMessage(op1, ++seq, 0, localUserLongId), true);
+            expectTextAtRebasedPosMatches(rebasedPos1, "2");
+
+            client.applyMsg(client.makeOpMessage(op2, ++seq, 0, localUserLongId), true);
+            expectTextAtRebasedPosMatches(rebasedPos2, "4");
+        });
+    });
+});
