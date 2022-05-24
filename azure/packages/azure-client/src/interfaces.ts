556fc47fb19acd5f93ac13bd364f6eca8b7eb2c8
RonnyM <103609950+ms-avocado@users.noreply.github.com>
2022-05-24 11:59:40 -0700

    Update Azure Client connection props to utilize service discovery endpoint (#10158)
    * changed azure client connection interface
    
    * enabled discovery on routerlicious document service factory
    
    * now using discovery endpoint
    
    * updated docs
    
    * commented out backwards compatibility tests
    
    * fix policy check
    
    * split AzureConnectionConfig into explicit subtypes
    
    * now utilizing endpoint URL everywhere
    
    * updated docs
    
    * commented out more tests
    
    * removed redundant imports
    
    * fixed/updated compatibility validation tests
    
    * updated api doc
    
    * Updated BREAKING.md
    
    * fixed text in example docs
    
    * fixing example-controller code
    
    * fixed?
    
    * fixed example!
    
    * simplified expressions
    
    * Fix: enable Summary Upload if remote not local
    
    * removed redundant import
    
    * fix URL resolver extra `/`
    
    * reran typetests:gen
    
    * fixed trailing slash issue with endpoint
    
    * lint fix
    
    * added docstring to type
    
    * fixed comment typo
    
    * fixed eslint error and regen docs and tests
    
    * Update BREAKING.md
    
    Co-authored-by: Scott Norton <scottnorton@microsoft.com>
    
    * Update azure/packages/azure-client/README.md
    
    Co-authored-by: Scott Norton <scottnorton@microsoft.com>
    
    * Update azure/packages/azure-client/README.md
    
    Co-authored-by: Skyler Jokiel <skjokiel@microsoft.com>
    
    * breaking.md update
    
    * moved util functions out of interfaces
    
    * reran doc gen and fix policy error
    
    Co-authored-by: RonnyM <103609950+ronnymajani-microsoft@users.noreply.github.com>
    Co-authored-by: Scott Norton <scottnorton@microsoft.com>
    Co-authored-by: Skyler Jokiel <skjokiel@microsoft.com>

--- azure/packages/azure-client/src/interfaces.ts
+++ azure/packages/azure-client/src/interfaces.ts
@@ -57,25 +57,52 @@ export interface AzureGetVersionsOptions {
 }
 
 /**
+ * The type of connection.
+ * - "local" for local connections to a Fluid relay instance running on the localhost
+ * - "remote" for client connections to the Azure Fluid Relay service
+ */
+export type AzureConnectionConfigType = "local" | "remote";
+
+/**
  * Parameters for establishing a connection with the Azure Fluid Relay.
  */
 export interface AzureConnectionConfig {
     /**
-     * URI to the Azure Fluid Relay orderer endpoint
+     * The type of connection. Whether we're connecting to a remote Fluid relay server or a local instance.
      */
-    orderer: string;
+    type: AzureConnectionConfigType;
     /**
-     * URI to the Azure Fluid Relay storage endpoint
+     * URI to the Azure Fluid Relay service discovery endpoint.
      */
-    storage: string;
+    endpoint: string;
     /**
-     * Unique tenant identifier
-    */
-    tenantId: "local" | string;
-    /**
-     * Instance that provides Azure Fluid Relay endpoint tokens
+     * Instance that provides Azure Fluid Relay endpoint tokens.
      */
     tokenProvider: ITokenProvider;
+}
+
+/**
+ * Parameters for establishing a remote connection with the Azure Fluid Relay.
+ */
+export interface AzureRemoteConnectionConfig extends AzureConnectionConfig {
+    /**
+     * The type of connection. Set to a remote connection.
+     */
+    type: "remote";
+    /**
+     * Unique tenant identifier.
+     */
+    tenantId: string;
+}
+
+/**
+ * Parameters for establishing a local connection with a local instance of the Azure Fluid Relay.
+ */
+export interface AzureLocalConnectionConfig extends AzureConnectionConfig {
+    /**
+     * The type of connection. Set to a remote connection.
+     */
+    type: "local";
 }
 
 /**
