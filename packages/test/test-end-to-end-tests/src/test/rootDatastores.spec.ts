da030cb0c93f295cbc4d2774d8aa9dbe65885f75
justus-camp <102552460+justus-camp@users.noreply.github.com>
2022-05-25 17:54:29 +0000

    Fix ling

--- packages/test/test-end-to-end-tests/src/test/rootDatastores.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/rootDatastores.spec.ts
@@ -356,7 +356,7 @@ describeNoCompat("Named root data stores", (getTestObjectProvider) => {
         const events = [
             { eventName: "fluid:telemetry:Container:ContainerClose", error: "malformedDataStoreAliasMessage" },
             { eventName: "fluid:telemetry:Container:ContainerClose", error: "malformedDataStoreAliasMessage" },
-        ]; 
+        ];
         itExpects("Receiving a bad alias message breaks the container", events, async function() {
             const dataCorruption = allDataCorruption([container1, container2]);
             await corruptedAliasOp(runtimeOf(dataObject1), alias);
