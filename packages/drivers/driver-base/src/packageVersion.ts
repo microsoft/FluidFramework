9bda3db5cfb989537fbf63a89be3abbd4a85a7bd
Wes Carlson <wesley.carlson@microsoft.com>
2022-05-26 09:17:25 -0700

    [bump] package version for client (patch)
                @fluidframework/build-common:     0.24.0 (unchanged)
         @fluidframework/eslint-config-fluid:  0.28.2000 (unchanged)
          @fluidframework/common-definitions:  0.21.1000 (unchanged)
                @fluidframework/common-utils:  0.33.1000 (unchanged)
             @fluidframework/core-interfaces:  0.43.2000 (unchanged)
        @fluidframework/protocol-definitions: 0.1028.2000 (unchanged)
          @fluidframework/driver-definitions:  0.46.2000 (unchanged)
       @fluidframework/container-definitions:  0.48.2000 (unchanged)
                                      Server: 0.1036.3001 (unchanged)
                                      Client:  0.59.3001 -> 0.59.3002
                      @fluid-tools/benchmark:     0.40.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
         @fluidframework/azure-local-service:      0.1.0 (unchanged)

--- packages/drivers/driver-base/src/packageVersion.ts
+++ packages/drivers/driver-base/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/driver-base";
-export const pkgVersion = "0.59.3001";
+export const pkgVersion = "0.59.3002";
--- packages/framework/request-handler/src/packageVersion.ts
+++ packages/framework/request-handler/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/request-handler";
-export const pkgVersion = "0.59.3001";
+export const pkgVersion = "0.59.3002";
