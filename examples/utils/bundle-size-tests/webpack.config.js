4046111e537f498cc242fd9f9cecfe4295af893f
Scott Norton <scottnorton@microsoft.com>
2022-05-31 21:41:14 +0000

    Revert "add bundle size test"
    This reverts commit 1cae3e9c54207224b7c5e2688aa73f0e13efd8ec.

--- examples/utils/bundle-size-tests/webpack.config.js
+++ examples/utils/bundle-size-tests/webpack.config.js
@@ -12,7 +12,6 @@ const { BannedModulesPlugin } = require('@fluidframework/bundle-size-tools')
 module.exports = {
   entry: {
     'aqueduct': './src/aqueduct',
-    'connectionState': './src/connectionState',
     'containerRuntime': './src/containerRuntime',
     'loader': './src/loader',
     'map': './src/map',
