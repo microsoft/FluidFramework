8df4c42d8971d2b374da37600891858dd59abf14
Alejandro Villarreal <716334+alexvy86@users.noreply.github.com>
2022-05-31 14:41:01 -0500

    Change location of mapping file and exposed API to use it

--- tools/build-tools/src/repoPolicyCheck/handlers/assertShortCode.ts
+++ tools/build-tools/src/repoPolicyCheck/handlers/assertShortCode.ts
@@ -140,7 +140,7 @@ export const handler: Handler = {
         const result = errors.length > 0 ? {error: errors.join("\n")} : undefined;
         if (resolve) {
             var contents = Array.from(codeToMsgMap.entries()).reduce((accum, current) => { accum[current[0]] = current[1]; return accum }, {} as any);
-            const targetFolder = "packages/test/test-utils/data";
+            const targetFolder = "packages/runtime/test-runtime-utils/src";
 
             if (!fs.existsSync(targetFolder)){
                 fs.mkdirSync(targetFolder, { recursive: true });
