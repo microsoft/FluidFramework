e6d4ff1588fdca84e0d9c09645a64ed57609116d
Jatin Garg <jatgarg@microsoft.com>
2022-05-31 15:30:15 -0700

    Preserve the value of isClpCompliantApp on resolved url in odsp

--- packages/drivers/odsp-driver/src/test/createNewUtilsTests.spec.ts
+++ packages/drivers/odsp-driver/src/test/createNewUtilsTests.spec.ts
@@ -51,6 +51,18 @@ describe("Create New Utils Tests", () => {
         return summary;
     };
 
+    const siteUrl = "https://microsoft.sharepoint-df.com/siteUrl";
+    const driveId = "driveId";
+    const itemId = "itemId";
+    const resolvedUrl = ({ siteUrl, driveId, itemId, odspResolvedUrl: true } as any) as IOdspResolvedUrl;
+    const filePath = "path";
+    const newFileParams: INewFileInfo = {
+        driveId,
+        siteUrl,
+        filePath,
+        filename: "filename",
+    };
+
     const test = (snapshot: ISnapshotContents) => {
         const snapshotTree = snapshot.snapshotTree;
         assert.strictEqual(Object.entries(snapshotTree.trees).length, 2, "app and protocol should be there");
@@ -83,11 +95,7 @@ describe("Create New Utils Tests", () => {
     });
 
     it("Should cache converted summary during createNewFluidFile", async () => {
-        const siteUrl = "https://microsoft.sharepoint-df.com/siteUrl";
-        const driveId = "driveId";
-        const itemId = "itemId";
         const hashedDocumentId = await getHashedDocumentId(driveId, itemId);
-        const resolvedUrl = ({ siteUrl, driveId, itemId, odspResolvedUrl: true } as any) as IOdspResolvedUrl;
         const localCache = createUtLocalCache();
         // use null logger here as we expect errors
         const epochTracker = new EpochTracker(
@@ -97,14 +105,6 @@ describe("Create New Utils Tests", () => {
                 resolvedUrl,
             },
             new TelemetryNullLogger());
-
-        const filePath = "path";
-        const newFileParams: INewFileInfo = {
-            driveId,
-            siteUrl: "https://www.localhost.xxx",
-            filePath,
-            filename: "filename",
-        };
 
         const fileEntry: IFileEntry = {
             docId: hashedDocumentId,
@@ -131,12 +131,9 @@ describe("Create New Utils Tests", () => {
     });
 
     it("Should save share link information received during createNewFluidFile", async () => {
-        const siteUrl = "https://microsoft.sharepoint-df.com/siteUrl";
-        const driveId = "driveId";
-        const itemId = "itemId";
         const createLinkType = ShareLinkTypes.csl;
         const hashedDocumentId = await getHashedDocumentId(driveId, itemId);
-        const resolvedUrl = ({ siteUrl, driveId, itemId, odspResolvedUrl: true } as any) as IOdspResolvedUrl;
+
         // use null logger here as we expect errors
         const epochTracker = new EpochTracker(
             createUtLocalCache(),
@@ -146,13 +143,7 @@ describe("Create New Utils Tests", () => {
             },
             new TelemetryNullLogger());
 
-        const newFileParams: INewFileInfo = {
-            driveId,
-            siteUrl: "https://www.localhost.xxx",
-            filePath: "path",
-            filename: "filename",
-            createLinkType,
-        };
+        newFileParams.createLinkType = createLinkType;
 
         const fileEntry: IFileEntry = {
             docId: hashedDocumentId,
@@ -203,5 +194,40 @@ describe("Create New Utils Tests", () => {
             error: mockError,
         });
         await epochTracker.removeEntries().catch(() => {});
+    });
+
+    it("Should set the isClpCompliantApp prop on resolved url", async () => {
+        const hashedDocumentId = await getHashedDocumentId(driveId, itemId);
+        const localCache = createUtLocalCache();
+        // use null logger here as we expect errors
+        const epochTracker = new EpochTracker(
+            localCache,
+            {
+                docId: hashedDocumentId,
+                resolvedUrl,
+            },
+            new TelemetryNullLogger());
+
+        const fileEntry: IFileEntry = {
+            docId: hashedDocumentId,
+            resolvedUrl,
+        };
+
+        const odspResolvedUrl = await mockFetchOk(
+                async () => createNewFluidFile(
+                    async (_options) => "token",
+                    newFileParams,
+                    new TelemetryNullLogger(),
+                    createSummary(),
+                    epochTracker,
+                    fileEntry,
+                    true,
+                    false,
+                    true,
+                ),
+                { itemId: "itemId1", id: "Summary handle" },
+                { "x-fluid-epoch": "epoch1" },
+                );
+        assert(odspResolvedUrl.isClpCompliantApp, "isClpCompliantApp should be set");
     });
 });
