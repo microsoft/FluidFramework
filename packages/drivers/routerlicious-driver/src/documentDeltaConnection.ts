8e79d2c41815c66b90f661d3d53590e2269ed37e
sharmakhushboo <sharmak@microsoft.com>
2022-06-03 19:04:40 -0700

    checking in r11s driver latency temp changes

--- packages/drivers/routerlicious-driver/src/documentDeltaConnection.ts
+++ packages/drivers/routerlicious-driver/src/documentDeltaConnection.ts
@@ -6,18 +6,49 @@ import { ITelemetryLogger } from "@fluidframework/common-definitions";
 import { DocumentDeltaConnection } from "@fluidframework/driver-base";
 import { IDocumentDeltaConnection } from "@fluidframework/driver-definitions";
 import { IAnyDriverError } from "@fluidframework/driver-utils";
-import { IClient, IConnect, IDocumentMessage, ISequencedDocumentMessage } from "@fluidframework/protocol-definitions";
-import { PerformanceEvent } from "@fluidframework/telemetry-utils";
-import type { io as SocketIOClientStatic } from "socket.io-client";
+import { IClient, IConnect, IDocumentMessage } from "@fluidframework/protocol-definitions";
+import type { io as SocketIOClientStatic, Socket } from "socket.io-client";
 import { errorObjectFromSocketError, IR11sSocketError } from "./errorUtils";
 import { pkgVersion as driverVersion } from "./packageVersion";
-
 const protocolVersions = ["^0.4.0", "^0.3.0", "^0.2.0", "^0.1.0"];
-
 /**
  * Wrapper over the shared one for driver specific translation.
  */
+
+const getRandomInt = (range: number) =>
+Math.floor(Math.random() * range);
+
 export class R11sDocumentDeltaConnection extends DocumentDeltaConnection {
+    constructor(
+        socket: Socket,
+        documentId: string,
+        logger: ITelemetryLogger,
+        enableLongPollingDowngrades: boolean = false,
+    ) {
+        super(socket, documentId, logger, enableLongPollingDowngrades);
+        socket.on("op", (docId, messages) => {
+            if (getRandomInt(1000) === 0) {
+                const timestamp = Date.now();
+                messages.forEach((message) => {
+                    // console.log(`OP!! this.clientId=${this.clientId}
+                    // and this.details.clientId=${this.details.clientId}
+                    // and message.clientId=${message.clientId}`);
+                    if (this.details.clientId === message.clientId) {
+                        logger.sendTelemetryEvent({
+                            eventName: "Driver-Op-Received",
+                            category: "generic",
+                            timestamp,
+                            documentId: docId,
+                            clientId: message.clientId,
+                            clientSequenceNumber: message.clientSequenceNumber,
+                            sequenceNumber: message.sequenceNumber,
+                            traces: JSON.stringify(message.traces),
+                        });
+                    }
+                });
+            }
+        });
+    }
     public static async create(
         tenantId: string,
         id: string,
@@ -53,23 +84,39 @@ export class R11sDocumentDeltaConnection extends DocumentDeltaConnection {
         // TODO: expose to host at factory level
         const enableLongPollingDowngrades = true;
         const deltaConnection = new R11sDocumentDeltaConnection(socket, id, logger, enableLongPollingDowngrades);
-
         await deltaConnection.initialize(connectMessage, timeoutMs);
-        const opDurationMap: Record<number, PerformanceEvent> = {};
-        deltaConnection.addTrackedListener("submitOp", (messages: IDocumentMessage[]) => {
-            messages.forEach((message) => {
-                opDurationMap[message.clientSequenceNumber] = PerformanceEvent.start(
-                    logger,
-                    { eventName: "custom_op_log" },
-                );
-            });
-        });
-        deltaConnection.addTrackedListener("op", (messages: ISequencedDocumentMessage[]) => {
-            messages.forEach((message) => {
-                opDurationMap[message.clientSequenceNumber].end();
-            });
-        });
         return deltaConnection;
+    }
+
+    protected emitMessages(type: string, messages: IDocumentMessage[][]) {
+        super.emitMessages(type, messages);
+        if (getRandomInt(100) === 0) {
+            if (type === "submitOp") {
+                // console.log(`submit Op in driver = ${JSON.stringify(messages)}`);
+                const timestamp = Date.now();
+                messages.forEach((message) => {
+                    message.forEach((msg) => {
+                        const opType = JSON.stringify(JSON.parse(msg.contents)?.contents?.contents?.type) ?? msg.type;
+                        if (opType === "\"op\"") {
+                            //  && getRandomInt(100) === 0) {
+                            // console.log(`ksmessage-submitop; clientId=
+                            // ${this.details.clientId} and ${this.clientId}`);
+                            this.logger.sendTelemetryEvent({
+                                eventName: "Driver-SubmitOp-Emit",
+                                category: "generic",
+                                timestamp,
+                                documentId: this.documentId,
+                                clientId: this.clientId,
+                                clientSequenceNumber: msg.clientSequenceNumber,
+                                sequenceNumber: -1,
+                                traces: JSON.stringify(msg.traces),
+                                content: JSON.stringify(msg),
+                            });
+                        }
+                    });
+                });
+            }
+        }
     }
 
     /**
