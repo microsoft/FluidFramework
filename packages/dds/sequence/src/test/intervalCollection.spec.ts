f2068891f91dd13f96d085378df4797f7f353250
Abram Sanderson <absander@microsoft.com>
2022-06-06 17:55:31 -0700

    Add regression test for flickering behavior

--- packages/dds/sequence/src/test/intervalCollection.spec.ts
+++ packages/dds/sequence/src/test/intervalCollection.spec.ts
@@ -790,8 +790,7 @@ describe("SharedString interval collections", () => {
             assert.equal(Array.from(collection2).length, 0);
         });
 
-        it.only("remains consistent on add+change with concurrent remove", () => {
-            // WIP: oddly, doesn't repro.
+        it("doesn't slide references on ack if there are pending remote changes", () => {
             sharedString.insertText(0, "ABCDEF");
             const collection1 = sharedString.getIntervalCollection("test");
             const collection2 = sharedString2.getIntervalCollection("test");
@@ -799,10 +798,17 @@ describe("SharedString interval collections", () => {
             sharedString.removeRange(3, 6);
             const interval = collection2.add(3, 4, IntervalType.SlideOnRemove);
             collection2.change(interval.getIntervalId(), 1, 5);
-            containerRuntimeFactory.processAllMessages();
+
+            assert.equal(containerRuntimeFactory.outstandingMessageCount, 3, "Unexpected number of ops");
+            containerRuntimeFactory.processOneMessage();
+            assertIntervals(sharedString2, collection2, [{ start: 1, end: 3 /* hasn't yet been acked */ }]);
+            containerRuntimeFactory.processOneMessage();
+            assertIntervals(sharedString2, collection2, [{ start: 1, end: 3 /* hasn't yet been acked */ }]);
+            containerRuntimeFactory.processOneMessage();
+            assertIntervals(sharedString2, collection2, [{ start: 1, end: 2 }]);
+
             assert.equal(sharedString.getText(), "ABC");
             assertIntervals(sharedString, collection1, [{ start: 1, end: 2 }]);
-            assertIntervals(sharedString2, collection2, [{ start: 1, end: 2 }]);
         });
     });
 
