e9c69bc891ce24465128e6f84e21b7db17961aaa
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-06-13 15:41:45 -0700

    docs: Update capitalization (code review suggestion)
    Co-authored-by: Tyler Butler <tyler@tylerbutler.com>

--- common/lib/protocol-definitions/src/users.ts
+++ common/lib/protocol-definitions/src/users.ts
@@ -8,7 +8,7 @@
  */
 export interface IUser {
     /**
-     * Unique identifier of the user session. This id is established on each connection with the service.
+     * Unique identifier of the user session. This ID is established on each connection with the service.
      */
     id: string;
 }
