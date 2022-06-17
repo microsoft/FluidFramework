24dda53bf8160ae73a0579f77cdcb4400d2afeec
Matt Rakow <marakow@microsoft.com>
2022-06-17 14:17:27 -0700

    Split view from TableView

--- examples/data-objects/table-view/src/tableview.ts
+++ examples/data-objects/table-view/src/tableview.ts
@@ -14,28 +14,18 @@ export const tableViewType = "@fluid-example/table-view";
 
 const matrixKey = "matrixKey";
 
-export class TableView extends DataObject implements IFluidHTMLView {
-    public static getFactory() { return factory; }
-
+export class TableViewView implements IFluidHTMLView {
     public get IFluidHTMLView() { return this; }
 
     private templateRoot: HTMLDivElement | undefined;
 
     private readonly _formulaInput = document.createElement("input");
-    public get formulaInput(): string { return this._formulaInput.value; }
-    public set formulaInput(val: string) { this._formulaInput.value = val; }
 
     private readonly _selectionSummary = document.createElement("span");
     // eslint-disable-next-line accessor-pairs
     public set selectionSummary(val: string) { this._selectionSummary.textContent = val; }
 
-    private _tableMatrix: SharedMatrix | undefined;
-    public get tableMatrix() {
-        if (this._tableMatrix === undefined) {
-            throw new Error("Table matrix not fully initialized");
-        }
-        return this._tableMatrix;
-    }
+    public constructor(private readonly tableView: TableView) { }
 
     private generateView() {
         const root = document.createElement("div");
@@ -43,25 +33,25 @@ export class TableView extends DataObject implements IFluidHTMLView {
         const addRowBtn = document.createElement("button");
         addRowBtn.textContent = "R+";
         addRowBtn.addEventListener("click", () => {
-            this.tableMatrix.insertRows(this.tableMatrix.rowCount, 1);
+            this.tableView.tableMatrix.insertRows(this.tableView.tableMatrix.rowCount, 1);
         });
 
         const addColBtn = document.createElement("button");
         addColBtn.textContent = "C+";
         addColBtn.addEventListener("click", () => {
-            this.tableMatrix.insertCols(this.tableMatrix.colCount, 1);
+            this.tableView.tableMatrix.insertCols(this.tableView.tableMatrix.colCount, 1);
         });
 
         const addRowsBtn = document.createElement("button");
         addRowsBtn.textContent = "R++";
         addRowsBtn.addEventListener("click", () => {
-            this.tableMatrix.insertRows(this.tableMatrix.rowCount, 10 /* 1048576 */);
+            this.tableView.tableMatrix.insertRows(this.tableView.tableMatrix.rowCount, 10 /* 1048576 */);
         });
 
         const addColsBtn = document.createElement("button");
         addColsBtn.textContent = "C++";
         addColsBtn.addEventListener("click", () => {
-            this.tableMatrix.insertCols(this.tableMatrix.colCount, 10 /* 16384 */);
+            this.tableView.tableMatrix.insertCols(this.tableView.tableMatrix.colCount, 10 /* 16384 */);
         });
 
         const gridGroup = document.createElement("div");
@@ -71,7 +61,7 @@ export class TableView extends DataObject implements IFluidHTMLView {
         const grid = document.createElement("div");
         grid.classList.add(styles.grid);
 
-        const gridView = new GridView(this.tableMatrix, this);
+        const gridView = new GridView(this.tableView.tableMatrix, this.tableView);
         grid.append(gridView.root);
 
         this._formulaInput.addEventListener("keypress", gridView.formulaKeypress);
@@ -95,6 +85,39 @@ export class TableView extends DataObject implements IFluidHTMLView {
             this.templateRoot = this.generateView();
         }
         elm.append(this.templateRoot);
+    }
+    // #endregion IFluidHTMLView
+}
+
+export class TableView extends DataObject implements IFluidHTMLView {
+    public static getFactory() { return factory; }
+
+    public get IFluidHTMLView() { return this; }
+
+    private tableViewView: TableViewView | undefined;
+
+    private readonly _formulaInput = document.createElement("input");
+    public get formulaInput(): string { return this._formulaInput.value; }
+    public set formulaInput(val: string) { this._formulaInput.value = val; }
+
+    private readonly _selectionSummary = document.createElement("span");
+    // eslint-disable-next-line accessor-pairs
+    public set selectionSummary(val: string) { this._selectionSummary.textContent = val; }
+
+    private _tableMatrix: SharedMatrix | undefined;
+    public get tableMatrix() {
+        if (this._tableMatrix === undefined) {
+            throw new Error("Table matrix not fully initialized");
+        }
+        return this._tableMatrix;
+    }
+
+    // #region IFluidHTMLView
+    public render(elm: HTMLElement, options?: IFluidHTMLOptions): void {
+        if (this.tableViewView === undefined) {
+            this.tableViewView = new TableViewView(this);
+        }
+        this.tableViewView.render(elm, options);
     }
     // #endregion IFluidHTMLView
 
