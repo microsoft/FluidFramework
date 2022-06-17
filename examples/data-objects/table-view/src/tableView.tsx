4e00ac19c86bbadc3cb18721c8e4890d87ef67ce
Matt Rakow <marakow@microsoft.com>
2022-06-17 15:58:52 -0700

    Add react view

--- examples/data-objects/table-view/src/tableView.tsx
+++ examples/data-objects/table-view/src/tableView.tsx
@@ -4,9 +4,82 @@
  */
 
 import { IFluidHTMLOptions, IFluidHTMLView } from "@fluidframework/view-interfaces";
+import React, { useEffect, useRef } from "react";
 import { GridView } from "./grid";
 import * as styles from "./index.css";
 import { TableModel } from "./tableModel";
+
+interface ITableViewProps {
+    model: TableModel;
+}
+
+export const TableViewReact: React.FC<ITableViewProps> = (props: ITableViewProps) => {
+    const { model } = props;
+
+    const formulaInputRef = useRef<HTMLInputElement>(null);
+    const selectionSummarySpanRef = useRef<HTMLSpanElement>(null);
+    const goToInputRef = useRef<HTMLInputElement>(null);
+    const gridRootRef = useRef<HTMLDivElement>(null);
+    const gridView = useRef<GridView>();
+
+    const getFormula = () => formulaInputRef.current?.value ?? "";
+    const setFormula = (val: string) => {
+        if (formulaInputRef.current !== null) {
+            formulaInputRef.current.value = val;
+        }
+    };
+    const setSelectionSummary = (val: string) => {
+        if (selectionSummarySpanRef.current !== null) {
+            selectionSummarySpanRef.current.textContent = val;
+        }
+    };
+
+    useEffect(() => {
+        gridView.current = new GridView(
+            model.tableMatrix,
+            getFormula,
+            setFormula,
+            setSelectionSummary,
+        );
+        if (gridRootRef.current !== null) {
+            while (gridRootRef.current.firstChild !== null) {
+                gridRootRef.current.removeChild(gridRootRef.current.firstChild);
+            }
+            gridRootRef.current.append(gridView.current.root);
+        }
+    }, [model]);
+
+    const executeGoTo = () => {
+        if (gridView.current !== undefined && goToInputRef.current !== null) {
+            gridView.current.startRow = parseInt(goToInputRef.current.value, 10) - 1;
+        }
+    };
+
+    return (
+        <div>
+            <button>R+</button>
+            <button>C+</button>
+            <button>R++</button>
+            <button>C++</button>
+            <div>
+                <input
+                    type="text"
+                    ref={ formulaInputRef }
+                    onKeyPress={ (e) => { gridView.current?.formulaKeypress(e.nativeEvent); } }
+                    onBlur={ () => { gridView.current?.formulaFocusOut(); } }
+                    placeholder="Formula input"
+                />
+                <div ref={ gridRootRef } className={ styles.grid }></div>
+                <span ref={ selectionSummarySpanRef }></span>
+            </div>
+            <input
+                type="text"
+                ref={ goToInputRef }
+                onChange={ executeGoTo }
+            />
+        </div>
+    );
+};
 
 export class TableView implements IFluidHTMLView {
     public get IFluidHTMLView() { return this; }
