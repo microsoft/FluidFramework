476e2c8e01306072e8710dabf94ee70255897e5e
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-06-21 14:40:16 -0700

    build: Enable `strictNullChecks` in `@fluid-tools/webpack-fluid-loader`'s tsconfig (#10723)
    Enables [strictNullChecks](https://www.typescriptlang.org/tsconfig#strictNullChecks) in `@fluid-tools/webpack-fluid-loader`'s tsconfig, and fixes associated issues in the code.
    
    [ADO 543](https://dev.azure.com/fluidframework/internal/_workitems/edit/543)

--- packages/tools/webpack-fluid-loader/src/loader.ts
+++ packages/tools/webpack-fluid-loader/src/loader.ts
@@ -101,6 +101,7 @@ function wrapWithRuntimeFactoryIfNeeded(packageJson: IFluidPackage, fluidModule:
         fluidModule.fluidExport;
     if (fluidModuleExport.IRuntimeFactory === undefined) {
         const dataStoreFactory = fluidModuleExport.IFluidDataStoreFactory;
+        assert(dataStoreFactory !== undefined, "dataStoreFactory is undefined");
 
         const defaultFactory = createDataStoreFactory(packageJson.name, dataStoreFactory);
 
@@ -141,7 +142,7 @@ function makeSideBySideDiv(divId: string) {
 class WebpackCodeResolver implements IFluidCodeResolver {
     constructor(private readonly options: IBaseRouteOptions) { }
     async resolveCodeDetails(details: IFluidCodeDetails): Promise<IResolvedFluidCodeDetails> {
-        const baseUrl = details.config.cdn ?? `http://localhost:${this.options.port}`;
+        const baseUrl = details.config?.cdn ?? `http://localhost:${this.options.port}`;
         let pkg = details.package;
         if (typeof pkg === "string") {
             const resp = await fetch(`${baseUrl}/package.json`);
@@ -191,13 +192,19 @@ async function createWebLoader(
     // will be used for ops(like delta connection/delta ops) while for storage, local storage would be used.
     if (testOrderer) {
         const resolvedUrl = await urlResolver.resolve(await urlResolver.createRequestForCreateNew(documentId));
+        assert(resolvedUrl !== undefined, "resolvedUrl is undefined");
         const innerDocumentService = await documentServiceFactory.createDocumentService(
             resolvedUrl,
             undefined, // logger
             false, // clientIsSummarizer
         );
+
+        const localDeltaConnectionServer = deltaConns.get(documentId);
+        assert(
+            localDeltaConnectionServer !== undefined,
+            "No delta connection server associated with specified document ID");
         documentServiceFactory = new LocalDocumentServiceFactory(
-            deltaConns.get(documentId),
+            localDeltaConnectionServer,
             undefined,
             innerDocumentService);
     }
@@ -279,9 +286,14 @@ export async function start(
         if (window.location.hash === "#prefetch") {
             assert(options.mode === "spo-df" || options.mode === "spo",
                 0x1ea /* "Prefetch snapshot only available for odsp!" */);
+
+            const resolvedUrl = await urlResolver.resolve({ url: documentUrl });
+            assert(resolvedUrl !== undefined, "resolvedUrl is undefined");
+
             const prefetched = await prefetchLatestSnapshot(
-                await urlResolver.resolve({ url: documentUrl }),
-                async () => options.odspAccessToken,
+                resolvedUrl,
+                // TokenFetcher type is expressed using null instead of undefined
+                async () => options.odspAccessToken ?? null,
                 odspPersistantCache,
                 false /** forceAccessTokenViaAuthorizationHeader */,
                 new BaseTelemetryNullLogger(),
@@ -333,11 +345,12 @@ export async function start(
     }
 
     // For side by side mode, we need to create a second container and Fluid object.
-    if (rightDiv) {
+    if (rightDiv !== undefined) {
         // Create a new loader that is used to load the second container.
         const loader2 = await createWebLoader(documentId, fluidModule, options, urlResolver, codeDetails, testOrderer);
 
         // Create a new request url from the resolvedUrl of the first container.
+        assert(container1.resolvedUrl !== undefined, "container1.resolvedUrl is undefined");
         const requestUrl2 = await urlResolver.getAbsoluteUrl(container1.resolvedUrl, "");
         const container2 = await loader2.resolve({ url: requestUrl2 });
         containers.push(container2);
@@ -345,6 +358,7 @@ export async function start(
         await getFluidObjectAndRender(container2, fluidObjectUrl, rightDiv);
         // Handle the code upgrade scenario (which fires contextChanged)
         container2.on("contextChanged", () => {
+            assert(rightDiv !== undefined, "rightDiv is undefined");
             getFluidObjectAndRender(container2, fluidObjectUrl, rightDiv).catch(() => { });
         });
     }
@@ -372,7 +386,7 @@ async function getFluidObjectAndRender(container: IContainer, url: string, div: 
 
     // We should be retaining a reference to mountableView long-term, so we can call unmount() on it to correctly
     // remove it from the DOM if needed.
-    const mountableView: IFluidMountableView = fluidObject.IFluidMountableView;
+    const mountableView = fluidObject.IFluidMountableView;
     if (mountableView !== undefined) {
         mountableView.mount(div);
         return;
@@ -406,7 +420,7 @@ async function attachContainer(
     shouldUseContainerId: boolean,
 ) {
     // This is called once loading is complete to replace the url in the address bar with the new `url`.
-    const replaceUrl = (resolvedUrl: IResolvedUrl) => {
+    const replaceUrl = (resolvedUrl: IResolvedUrl | undefined) => {
         let [docUrl, title] = [url, documentId];
         if (shouldUseContainerId) {
             // for a r11s and t9s container we need to use the actual ID
