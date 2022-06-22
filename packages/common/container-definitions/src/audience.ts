cfa8b0a9b1992f3b5269df6070332889302e09d5
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-06-22 22:11:44 +0000

    docs: Add links

--- packages/common/container-definitions/src/audience.ts
+++ packages/common/container-definitions/src/audience.ts
@@ -8,9 +8,14 @@ import { IClient } from "@fluidframework/protocol-definitions";
 
 /**
  * Audience represents all clients connected to the op stream, both read-only and read/write.
+ *
+ * See {@link https://nodejs.org/api/events.html#class-eventemitter | here} for an overview of the `EventEmitter`
+ * class.
  */
 export interface IAudience extends EventEmitter {
-
+    /**
+     * See {@link https://nodejs.dev/learn/the-nodejs-event-emitter | here} for an overview of `EventEmitter.on`.
+     */
     on(event: "addMember" | "removeMember", listener: (clientId: string, client: IClient) => void): this;
 
     /** List all clients connected to the op stream, keyed off their clientId */
