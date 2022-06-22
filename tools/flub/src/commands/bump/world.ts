7c38d6198e091c6b977192f3e8c8c5d3049a8c9b
Tyler Butler <tyler@tylerbutler.com>
2022-06-22 19:32:29 +0000

    wip

---
+++ tools/flub/src/commands/bump/world.ts
@@ -0,0 +1,24 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { Command } from "@oclif/core";
+
+export default class World extends Command {
+    static description = "Say hello world";
+
+    static examples = [
+        `$ oex hello world
+hello world! (./src/commands/hello/world.ts)
+`,
+    ];
+
+    static flags = {};
+
+    static args = [];
+
+    async run(): Promise<void> {
+        this.log("hello world! (./src/commands/hello/world.ts)");
+    }
+}
