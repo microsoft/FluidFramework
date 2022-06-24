4f11b9ff7a9f0f2acfe7a95a78ffb30048b7f8de
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-06-24 20:14:13 +0000

    style: Fix linter violations

--- azure/packages/azure-client/src/AzureAudience.ts
+++ azure/packages/azure-client/src/AzureAudience.ts
@@ -5,7 +5,7 @@
 
 import { ServiceAudience } from "@fluidframework/fluid-static";
 import { IClient } from "@fluidframework/protocol-definitions";
-import { assert } from '@fluidframework/common-utils'
+import { assert } from "@fluidframework/common-utils";
 import { IAzureAudience, AzureMember, AzureUser } from "./interfaces";
 
 export class AzureAudience extends ServiceAudience<AzureMember> implements IAzureAudience {
@@ -14,7 +14,7 @@ export class AzureAudience extends ServiceAudience<AzureMember> implements IAzur
    */
   protected createServiceMember(audienceMember: IClient): AzureMember {
     const azureUser = audienceMember.user as AzureUser;
-    assert(azureUser !== undefined && azureUser.name !== undefined, "Provided user was not an \"AzureUser\".");
+    assert(azureUser?.name !== undefined, "Provided user was not an \"AzureUser\".");
 
     return {
       userId: audienceMember.user.id,
