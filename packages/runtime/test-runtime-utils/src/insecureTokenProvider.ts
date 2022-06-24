2a3d1a75eca9f9c30036421eb748e6c66e1262b2
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-06-24 20:28:34 +0000

    docs: Update comment

--- packages/runtime/test-runtime-utils/src/insecureTokenProvider.ts
+++ packages/runtime/test-runtime-utils/src/insecureTokenProvider.ts
@@ -8,9 +8,10 @@ import { ITokenProvider, ITokenResponse } from "@fluidframework/routerlicious-dr
 import { generateToken } from "./generateToken";
 
 /**
- * {@link @fluidframework/routerlicious-driver#ITokenProvider} intended for **test use only**.
- * Provides an in memory implementation of the ITokenProvider interface that can be used to insecurely connect to Azure Fluid Relay (and other routerlicious based fluid relays).
- * As the name implies this is not secure and should not be used in production.
+ * Provides an in memory implementation of {@link @fluidframework/routerlicious-driver#ITokenProvider} that can be
+ * used to insecurely connect to the Fluid Relay.
+ *
+ * As the name implies, this is not secure and should not be used in production.
  * It simply makes examples where authentication is not relevant easier to bootstrap.
  */
 export class InsecureTokenProvider implements ITokenProvider {
