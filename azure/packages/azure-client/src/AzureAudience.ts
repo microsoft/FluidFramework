132a42b8c5c25098e41c16aab737128e24d14f90
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-06-24 18:19:00 +0000

    improvement: Better type guard

--- azure/packages/azure-client/src/AzureAudience.ts
+++ azure/packages/azure-client/src/AzureAudience.ts
@@ -14,7 +14,7 @@ export class AzureAudience extends ServiceAudience<AzureMember> implements IAzur
    */
   protected createServiceMember(audienceMember: IClient): AzureMember {
     const azureUser = audienceMember.user as AzureUser;
-    assert(azureUser !== undefined, "Provided user was not an \"AzureUser\".");
+    assert(azureUser !== undefined && azureUser.name !== undefined, "Provided user was not an \"AzureUser\".");
 
     return {
       userId: audienceMember.user.id,
