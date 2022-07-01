f6bd510d2951eb73ed00daeecd53b2f40e7ae673
Alex Panov <8239356+alpaix@users.noreply.github.com>
2022-07-01 21:48:04 +0000

    Add missing header

--- examples/hosts/app-integration/schema-upgrade/src/root.tsx
+++ examples/hosts/app-integration/schema-upgrade/src/root.tsx
@@ -1,3 +1,7 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
 import React from "react";
 import ReactDOM from "react-dom";
 
