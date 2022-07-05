d5fc7829cd14a565092f2b3b5527b9798430e029
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-07-05 13:05:22 -0700

    docs: Update comments with typescript examples to better utilize TSDoc syntax (#10949)
    Namely:
    - Puts code blocks under `@example` or `@remarks` as content-appropriate
    - Adds typescript language specification to example code blocks
    - Fixes a couple typos and cleans up some formatting

--- experimental/PropertyDDS/packages/property-proxy/src/propertyProxy.ts
+++ experimental/PropertyDDS/packages/property-proxy/src/propertyProxy.ts
@@ -102,11 +102,10 @@ export namespace PropertyProxy {
      * Insertion of new properties into the workspace is triggered
      * if the specified property name does not yet exist on the parent and the parent is dynamic.
      * @example
-     * ```
-     *
+     * ```typescript
      * // The data can be accessed and modified using standard JavaScript syntax. Operations directly
      * // happen on the PropertyTree data, nothing is cached.
-     * import {PropertyProxy} from '@fluid-experimental/property-proxy';
+     * import { PropertyProxy } from '@fluid-experimental/property-proxy';
      *
      * // Given a workspace that contains some properties: someVector2D (with x = 1 and y = 2),
      * // someArray (storing [1, 2, 3, 4], ...
