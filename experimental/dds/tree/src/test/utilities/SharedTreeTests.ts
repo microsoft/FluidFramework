f317a79efa7ab67a6405c2e060fde24fcdae4cbc
Alejandro Villarreal <716334+alexvy86@users.noreply.github.com>
2022-07-06 12:54:19 -0700

    Spaces->tabs

--- experimental/dds/tree/src/test/utilities/SharedTreeTests.ts
+++ experimental/dds/tree/src/test/utilities/SharedTreeTests.ts
@@ -1193,9 +1193,9 @@ export function runSharedTreeOperationsTests(
 					containerRuntimeFactory.processAllMessages();
 					await sharedTree.logViewer.getRevisionView(Number.POSITIVE_INFINITY);
 
-                    // Other things might have caused things to be written to the logger. Make sure
-                    // to only look at the entries this test cares about.
-                    const eventsToCheck = events.filter((x) => x.isSharedTreeEvent);
+					// Other things might have caused things to be written to the logger. Make sure
+					// to only look at the entries this test cares about.
+					const eventsToCheck = events.filter((x) => x.isSharedTreeEvent);
 					expect(eventsToCheck.length).equals(2);
 					expect(eventsToCheck[1].eventName).equals('SharedTree:SequencedEditApplied:InvalidSharedTreeEdit');
 				});
