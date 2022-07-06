e46957bb1d8ac28dbeefab7df0ba68a6c33fcac5
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-07-06 17:37:18 +0000

    docs: Update comment

--- azure/packages/azure-client/src/interfaces.ts
+++ azure/packages/azure-client/src/interfaces.ts
@@ -150,6 +150,7 @@ export interface AzureUser<T = unknown> extends IUser {
  * Note: must be JSON-serializable.
  * Passing a non-serializable object (e.g. a `class`) will result in undefined behavior.
  */
+// TODO: this should be updated to use something other than `any` (unknown)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export interface AzureMember<T = any> extends IMember {
     /**
