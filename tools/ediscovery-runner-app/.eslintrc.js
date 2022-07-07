e316ff94369482b7395303530759c7729c1fc56f
REDMOND\kianthompson <kianthompson@microsoft.com>
2022-07-07 13:59:16 -0700

    Copying some implementation from build-tools

--- tools/ediscovery-runner-app/.eslintrc.js
+++ tools/ediscovery-runner-app/.eslintrc.js
@@ -1,5 +1,23 @@
-const tsconfig = require("./tsconfig.json");
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 module.exports = {
-  extends: ["plugin:@fluidx/eslint-plugin-ffx-rules/base"],
-  ignorePatterns: tsconfig.exclude
-};
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+      "@typescript-eslint",
+  ],
+  "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+  ],
+  "parserOptions": {
+      "project": ["./tsconfig.json", "./src/test/tsconfig.json"]
+  },
+  "rules": {
+      "@typescript-eslint/switch-exhaustiveness-check": "error",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/no-var-requires": "off",
+  },
+}
