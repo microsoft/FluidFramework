619244753b0a652d93c4d9d4373aab60f8eb1f18
Mark Fields <markfields@users.noreply.github.com>
2022-07-07 14:34:00 -0700

    Add draft of result-based runWithRetry2

--- packages/loader/driver-utils/src/runWithRetry.ts
+++ packages/loader/driver-utils/src/runWithRetry.ts
@@ -4,13 +4,14 @@
  */
 
 import { ITelemetryLogger } from "@fluidframework/common-definitions";
-import { delay, performance } from "@fluidframework/common-utils";
+import { delay, performance, unreachableCase } from "@fluidframework/common-utils";
 import { DriverErrorType } from "@fluidframework/driver-definitions";
 import { canRetryOnError, getRetryDelayFromError } from "./network";
 import { pkgVersion } from "./packageVersion";
 import { NonRetryableError } from ".";
 
 /**
+ * @deprecated - use IProgress2
  * Interface describing an object passed to various network APIs.
  * It allows caller to control cancellation, as well as learn about any delays.
  */
@@ -41,22 +42,58 @@ export interface IProgress {
     onRetry?(delayInMs: number, error: any): void;
 }
 
-export async function runWithRetry<T>(
+export interface IProgress2 {
+    /**
+     * Abort signal used to cancel operation
+     * Note that most of the layers do not use this signal yet. We need to change that over time.
+     * Please consult with API documentation / implementation.
+     * Note that  number of layers may not check this signal while holding this request in a queue,
+     * so it may take a while it takes effect. This can be improved in the future.
+     * Layers in question are:
+     *    - driver (RateLimiter)
+     *    - runWithRetry
+     */
+    cancel?: AbortSignal & { reason: string; };
+
+    /**
+     * Called whenever api returns cancellable error and the call is going to be retried.
+     * @param delayInMs - delay before next retry. This value will depend on internal back-off logic,
+     * as well as information provided by service (like 429 error asking to wait for some time before retry)
+     * @param error - error object returned from the call.
+     * @returns - A reason for aborting the operation, or undefined to continue retrying
+     */
+    onRetry?(delayInMs: number, error: unknown): string | undefined;
+}
+
+export type RunResult<T> = {
+    status: "succeeded";
+    result: T;
+} | {
+    status: "failed";
+    error: unknown;
+} | {
+    status: "aborted";
+    reason: string;
+};
+
+export async function runWithRetry2<T>(
     api: (cancel?: AbortSignal) => Promise<T>,
     fetchCallName: string,
     logger: ITelemetryLogger,
-    progress: IProgress,
-): Promise<T> {
-    let result: T | undefined;
-    let success = false;
+    progress: IProgress2,
+): Promise<RunResult<T>> {
+    let successResult: RunResult<T> | undefined;
     let retryAfterMs = 1000; // has to be positive!
     let numRetries = 0;
     const startTime = performance.now();
     let lastError: any;
     do {
         try {
-            result = await api(progress.cancel);
-            success = true;
+            const result = await api(progress.cancel);
+            successResult = {
+                status: "succeeded",
+                result,
+            };
         } catch (err) {
             // If it is not retriable, then just throw the error.
             if (!canRetryOnError(err)) {
@@ -66,25 +103,24 @@ export async function runWithRetry<T>(
                     duration: performance.now() - startTime,
                     fetchCallName,
                 }, err);
-                throw err;
+                return {
+                    status: "failed",
+                    error: err,
+                };
             }
 
             if (progress.cancel?.aborted === true) {
-                const cancelDescription = progress.cancelDescription;
                 logger.sendTelemetryEvent({
                     eventName: `${fetchCallName}_runWithRetryAborted`,
                     retry: numRetries,
                     duration: performance.now() - startTime,
                     fetchCallName,
-                    cancelDescription,
                 }, err);
 
-                // Disregard the retryable error from the previous attempt and throw a new error for the cancellation
-                throw new NonRetryableError(
-                    `runWithRetry for [${fetchCallName}] was aborted with cancelDescription [${cancelDescription}]`,
-                    DriverErrorType.genericError,
-                    { driverVersion: pkgVersion, fetchCallName, cancelDescription },
-                );
+                return {
+                    status: "aborted",
+                    reason: progress.cancel?.reason ?? "UNSPECIFIED",
+                };
             }
 
             numRetries++;
@@ -93,11 +129,17 @@ export async function runWithRetry<T>(
             // If the waitTime is not specified, then we start with retrying immediately to max of 8s.
             retryAfterMs = getRetryDelayFromError(err) ?? Math.min(retryAfterMs * 2, 8000);
             if (progress.onRetry) {
-                progress.onRetry(retryAfterMs, err);
+                const abortReason = progress.onRetry(retryAfterMs, err);
+                if (abortReason !== undefined) {
+                    return {
+                        status: "aborted",
+                        reason: abortReason,
+                    };
+                }
             }
             await delay(retryAfterMs);
         }
-    } while (!success);
+    } while (successResult === undefined);
     if (numRetries > 0) {
         logger.sendTelemetryEvent({
             eventName: `${fetchCallName}_lastError`,
@@ -107,6 +149,41 @@ export async function runWithRetry<T>(
         },
         lastError);
     }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return result!;
+    return successResult;
+}
+
+/** @deprecated - use runWithRetry2 */
+export async function runWithRetry<T>(
+    api: (cancel?: AbortSignal) => Promise<T>,
+    fetchCallName: string,
+    logger: ITelemetryLogger,
+    progress: IProgress,
+): Promise<T> {
+    const progress2: IProgress2 = {
+        cancel: Object.assign(progress.cancel, { reason: "UNSPECIFIED" }),
+        onRetry(delayInMs: number, error: unknown): string | undefined {
+            try {
+                progress.onRetry?.(delayInMs, error);
+                return undefined;
+            } catch (e: any) {
+                return e.message as string;
+            }
+        },
+    };
+
+    const runResult = await runWithRetry2(api, fetchCallName, logger, progress2);
+    switch (runResult.status) {
+        case "succeeded":
+            return runResult.result;
+        case "failed":
+            throw runResult.error;
+        case "aborted":
+            throw new NonRetryableError(
+                `runWithRetry for [${fetchCallName}] was aborted due to reason [${runResult.reason}]`,
+                DriverErrorType.genericError,
+                { driverVersion: pkgVersion, fetchCallName, reason: runResult.reason },
+            );
+        default:
+            unreachableCase(runResult);
+    }
 }
