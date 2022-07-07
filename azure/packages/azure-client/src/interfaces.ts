dd440207ab4f0674f71bcb30e968bf3eaf96f8f6
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-07-07 21:15:31 +0000

    revert: Breaking change

--- azure/packages/azure-client/src/interfaces.ts
+++ azure/packages/azure-client/src/interfaces.ts
@@ -129,7 +129,9 @@ export interface AzureContainerServices {
  * Note: must be JSON-serializable.
  * Passing a non-serializable object (e.g. a `class`) will result in undefined behavior.
  */
-export interface AzureUser<T = unknown> extends IUser {
+// TODO: this should be updated to use something other than `any` (unknown)
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface AzureUser<T = any> extends IUser {
     /**
      * The user's name
      */
