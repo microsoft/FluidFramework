1039346d0600b840a8e1680089b5da7d0ef8fb57
Scott Norton <scottnorton@microsoft.com>
2022-07-13 23:29:49 +0000

    [bump] package version for client (patch)
                @fluidframework/build-common:     0.25.0 (unchanged)
         @fluidframework/eslint-config-fluid:  0.29.1000 (unchanged)
          @fluidframework/common-definitions:  0.21.1000 (unchanged)
                @fluidframework/common-utils:  0.33.1000 (unchanged)
        @fluidframework/protocol-definitions: 0.1028.3000 (unchanged)
                                       Azure:      1.1.0 (unchanged)
                                      Server:   0.1037.0 (unchanged)
                                      Client:      1.2.1 -> 1.2.2
                      @fluid-tools/benchmark:     0.41.0 (unchanged)
                                 tinylicious:      0.4.0 (unchanged)
         @fluidframework/azure-local-service:      0.1.0 (unchanged)

--- packages/drivers/driver-base/src/packageVersion.ts
+++ packages/drivers/driver-base/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/driver-base";
-export const pkgVersion = "1.2.1";
+export const pkgVersion = "1.2.2";
--- packages/utils/tool-utils/src/packageVersion.ts
+++ packages/utils/tool-utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/tool-utils";
-export const pkgVersion = "1.2.1";
+export const pkgVersion = "1.2.2";
