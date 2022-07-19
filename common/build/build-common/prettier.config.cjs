113147c3d3dc849513d29ee39948a035c8ff1cea
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-07-19 23:57:17 +0000

    docs: Update comment and remove commented-out property

--- common/build/build-common/prettier.config.cjs
+++ common/build/build-common/prettier.config.cjs
@@ -1,32 +1,33 @@
-// prettier.config.js or .prettierrc.js
+// Shared prettier configuration for use in across the fluid-framework repository.
+// Individual packages may extend this and override rules as needed, though for consistent formatting, package-local
+// overrides should be avoided unless absolutely necessary.
 module.exports = {
-	printWidth: 100,
-	semi: true,
-	singleQuote: false,
-	trailingComma: "all",
-	useTabs: false, // TODO: reconsider in the future for accessibility
-	overrides: [
-		{
-			files: "lerna.json",
-			options: {
-				printWidth: 50,
-			},
-		},
-		{
-			files: "tsconfig*.json",
-			options: {
-				parser: "json5",
-				trailingComma: "all",
-				quoteProps: "preserve",
-			},
-		},
-		{
-			files: "*.json",
-			options: {
-				// parser: "json5",
-				trailingComma: "all",
-				quoteProps: "preserve",
-			},
-		},
-	],
+    printWidth: 100,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "all",
+    useTabs: false, // TODO: reconsider in the future for accessibility
+    overrides: [
+        {
+            files: "lerna.json",
+            options: {
+                printWidth: 50,
+            },
+        },
+        {
+            files: "tsconfig*.json",
+            options: {
+                parser: "json5",
+                trailingComma: "all",
+                quoteProps: "preserve",
+            },
+        },
+        {
+            files: "*.json",
+            options: {
+                trailingComma: "all",
+                quoteProps: "preserve",
+            },
+        },
+    ],
 };
