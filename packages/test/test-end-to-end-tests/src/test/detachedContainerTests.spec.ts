9cfec9d1eace4508f05175085aa6bc6a29cb9ae9
Daniel Madrid <105010181+dannimad@users.noreply.github.com>
2022-07-20 17:39:24 +0000

    fix one tinylicious test

--- packages/test/test-end-to-end-tests/src/test/detachedContainerTests.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/detachedContainerTests.spec.ts
@@ -17,6 +17,7 @@ import {
     LocalCodeLoader,
     SupportedExportInterfaces,
     TestFluidObjectFactory,
+    timeoutPromise,
 } from "@fluidframework/test-utils";
 import { SharedMap, SharedDirectory } from "@fluidframework/map";
 import { Deferred, TelemetryNullLogger } from "@fluidframework/common-utils";
@@ -71,6 +72,12 @@ const createFluidObject = (async (
         await dataStoreContext.containerRuntime.createDataStore(type),
         "");
 });
+
+async function ensureContainerConnected(container: Container): Promise<void> {
+    if (!container.connected) {
+        return timeoutPromise((resolve) => container.once("connected", () => resolve()));
+    }
+}
 
 describeFullCompat("Detached Container", (getTestObjectProvider) => {
     let provider: ITestObjectProvider;
@@ -160,10 +167,6 @@ describeFullCompat("Detached Container", (getTestObjectProvider) => {
     });
 
     it("can create DDS in detached container and attach / update it", async function() {
-        // GitHub issue: #9534
-        if (provider.driver.type === "tinylicious") {
-            this.skip();
-        }
         const container = await loader.createDetachedContainer(provider.defaultCodeDetails);
         const dsClient1 = await requestFluidObject<ITestFluidObject>(container, "/");
 
@@ -174,9 +177,11 @@ describeFullCompat("Detached Container", (getTestObjectProvider) => {
         // Attach the container and validate that the DDS is attached.
         await container.attach(provider.driver.createCreateNewRequest(provider.documentId));
         assert(mapClient1.isAttached(), "The map should be attached after the container attaches.");
-
+        await ensureContainerConnected(container as Container);
+        provider.updateDocumentId(container.resolvedUrl);
+        const url: any = await container.getAbsoluteUrl("");
         // Load a second container and validate it can load the DDS.
-        const container2 = await provider.loadTestContainer();
+        const container2 = await loader.resolve({ url });
         const dsClient2 = await requestFluidObject<ITestFluidObject>(container2, "/");
         const mapClient2 = await dsClient2.root.get<IFluidHandle<SharedMap>>("map")?.get();
         assert(mapClient2 !== undefined, "Map is not available in the second client");
