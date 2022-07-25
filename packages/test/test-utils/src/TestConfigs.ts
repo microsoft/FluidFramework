81ad84d4d2e2757ac37abbbaeb115fc7b109c536
Nicholas Couri <nichoc@microsoft.com>
2022-07-25 15:54:49 -0700

    Add header to TestUtils

--- packages/test/test-utils/src/TestConfigs.ts
+++ packages/test/test-utils/src/TestConfigs.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { ConfigTypes, IConfigProviderBase } from "@fluidframework/telemetry-utils";
 
 export const mockConfigProvider = ((settings: Record<string, ConfigTypes> = {}): IConfigProviderBase => {
