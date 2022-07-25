4275835eb612a5d3bf1bc59b0d8d499f905e4117
Matt Rakow <marakow@microsoft.com>
2022-07-25 15:18:56 -0700

    Start IModelCodeLoader

--- examples/hosts/app-integration/schema-upgrade/src/modelLoader.ts
+++ examples/hosts/app-integration/schema-upgrade/src/modelLoader.ts
@@ -33,10 +33,8 @@ const v2ModuleWithDetails: IFluidModuleWithDetails = {
     details: { package: "two" },
 };
 
-function createLoader(): IHostLoader {
-    const tinyliciousService = new TinyliciousService();
-
-    const load = async (source: IFluidCodeDetails): Promise<IFluidModuleWithDetails> => {
+const demoCodeLoader = {
+    load: async (source: IFluidCodeDetails): Promise<IFluidModuleWithDetails> => {
         const version = source.package;
         if (typeof version !== "string") {
             throw new Error("Unexpected code detail format");
@@ -46,40 +44,59 @@ function createLoader(): IHostLoader {
             case "two": return v2ModuleWithDetails;
             default: throw new Error("Unknown version");
         }
-    };
-    const codeLoader = { load };
+    },
+};
+
+function createLoader(): IHostLoader {
+    const tinyliciousService = new TinyliciousService();
 
     return new Loader({
         urlResolver: tinyliciousService.urlResolver,
         documentServiceFactory: tinyliciousService.documentServiceFactory,
-        codeLoader,
+        codeLoader: demoCodeLoader,
     });
 }
 
-const getModel = async (container: IContainer): Promise<IMigratable> => {
-    // Here I'm using the specified code details for convenience since it already exists (a real code proposal).
-    // However, it could be reasonable to use an alternative in-container storage for the container type (e.g. a
-    // standalone Quorum DDS).  The important thing is that we need a dependable way to discover the version of the
-    // container, so ideally it remains constant across versions.
-    const version = container.getSpecifiedCodeDetails()?.package;
-    if (typeof version !== "string") {
-        throw new Error("Unexpected code detail format");
-    }
+interface IModelCodeLoader {
+    /**
+     * Check if the IModelCodeLoader knows how to instantiate an appropriate model for the provided container code
+     * version.  It is async to permit dynamic model loading - e.g. referring to a remote service to determine if
+     * the requested model is available.
+     * @param version - the container code version to check
+     */
+    supportsVersion: (version: string) => Promise<boolean>;
+    getModel: (container: IContainer) => Promise<IMigratable>;
+}
+class DemoModelCodeLoader implements IModelCodeLoader {
+    public readonly supportsVersion = async (version: string) => {
+        return version === "one" || version === "two";
+    };
 
-    switch (version) {
-        case "one": {
-            const model = new InventoryListContainer1(container);
-            await model.initialize();
-            return model;
+    public readonly getModel = async (container: IContainer) => {
+        // Here I'm using the specified code details for convenience since it already exists (a real code proposal).
+        // However, it could be reasonable to use an alternative in-container storage for the container type (e.g. a
+        // standalone Quorum DDS).  The important thing is that we need a dependable way to discover the version of the
+        // container, so ideally it remains constant across versions.
+        const version = container.getSpecifiedCodeDetails()?.package;
+        if (typeof version !== "string") {
+            throw new Error("Unexpected code detail format");
         }
-        case "two": {
-            const model = new InventoryListContainer2(container);
-            await model.initialize();
-            return model;
+
+        switch (version) {
+            case "one": {
+                const model = new InventoryListContainer1(container);
+                await model.initialize();
+                return model;
+            }
+            case "two": {
+                const model = new InventoryListContainer2(container);
+                await model.initialize();
+                return model;
+            }
+            default: throw new Error("Unknown version");
         }
-        default: throw new Error("Unknown version");
-    }
-};
+    };
+}
 
 // This ModelLoader specifically supports versions one and two.  Other approaches might have network calls to
 // dynamically load in the appropriate model for unknown versions.
@@ -88,6 +105,11 @@ const getModel = async (container: IContainer): Promise<IMigratable> => {
 // functionality.  TODO: Determine if this demo should do that.
 export class ModelLoader implements IModelLoader {
     private readonly loader: IHostLoader = createLoader();
+    private readonly modelCodeLoader: IModelCodeLoader = new DemoModelCodeLoader();
+
+    // public constructor(props: ILoaderProps & { modelLoader: IModelCodeLoader }) {
+    //     this.loader =
+    // }
 
     // TODO: If I parameterize a modelCodeLoader, then the modelCodeLoader would implement this method.
     public async isVersionSupported(version: string): Promise<boolean> {
@@ -100,12 +122,12 @@ export class ModelLoader implements IModelLoader {
     public async createDetached(
         version: "one" | "two",
     ): Promise<{ model: IMigratable; attach: () => Promise<string>; }> {
-        const supported = await this.isVersionSupported(version);
+        const supported = await this.modelCodeLoader.supportsVersion(version);
         if (!supported) {
             throw new Error("Unknown accepted version");
         }
         const container = await this.loader.createDetachedContainer({ package: version });
-        const model = await getModel(container);
+        const model = await this.modelCodeLoader.getModel(container);
         // The attach callback lets us defer the attach so the caller can do whatever initialization pre-attach,
         // without leaking out the loader, service, etc.  We also return the container ID here so we don't have
         // to stamp it on something that would rather not know it (e.g. the model).
@@ -120,7 +142,7 @@ export class ModelLoader implements IModelLoader {
 
     public async loadExisting(id: string): Promise<IMigratable> {
         const container = await this.loader.resolve({ url: id });
-        const model = await getModel(container);
+        const model = await this.modelCodeLoader.getModel(container);
         return model;
     }
 }
