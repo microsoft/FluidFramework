6b98016f1499e23428f59459f175dca299faad7b
Yann Achard <achardy@microsoft.com>
2022-07-25 17:42:48 -0700

    More move tests and fixes

--- packages/dds/tree/src/feature-libraries/sequence-change-family/sequenceEditBuilder.ts
+++ packages/dds/tree/src/feature-libraries/sequence-change-family/sequenceEditBuilder.ts
@@ -24,96 +24,149 @@ export class SequenceEditBuilder extends ProgressiveEditBuilder<SequenceChangese
 
     public setValue(node: NodePath, value: Value) {
         const modify: T.Modify = { type: "Modify", value: { type: "Set", value } };
-        this.applyAtPath(modify, node);
+        this.applyMarkAtPath(modify, node);
     }
 
     public insert(place: PlacePath, cursor: ITreeCursor) {
         const id = this.opId++;
         const content = jsonableTreeFromCursor(cursor);
         const insert: T.Insert = { type: "Insert", id, content: [content] };
-        this.applyAtPath([insert], place);
+        this.applyMarkAtPath([insert], place);
     }
 
     public delete(place: PlacePath, count: number) {
         const id = this.opId++;
         const mark: T.Detach = { type: "Delete", id, count };
-        this.applyAtPath(mark, place);
+        this.applyMarkAtPath(mark, place);
     }
 
     public move(source: PlacePath, count: number, destination: PlacePath) {
         const id = this.opId++;
         const moveOut: T.Detach = { type: "MoveOut", id, count };
         const moveIn: T.AttachGroup = [{ type: "MoveIn", id, count }];
-        let a: NestBranch = {
-            marks: toFieldMarks(moveOut, source),
-            path: source,
-        };
-        let b: NestBranch = {
-            marks: toFieldMarks(moveIn, destination),
-            path: destination,
-        };
-        let depthDiff = getDepth(source) - getDepth(destination);
-        // Ensure that a represents the deeper mark
-        if (depthDiff < 0) {
-            [a, b] = [b, a];
-            depthDiff = -depthDiff;
-        }
-        // Nest the deeper mark so that they are both at the same depth
-        a = nestN(a.marks, a.path, depthDiff);
-        // Nest both marks one level at a time until they reach the same parent
-        while (a.path !== b.path) {
-            a = nestN(a.marks, a.path, 1);
-            b = nestN(b.marks, b.path, 1);
-        }
-        if (a.path === undefined) {
-            this.applyChange({ marks: { ...a.marks, ...b.marks } });
-        } else {
-            const aPath = a.path;
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            const bPath = b.path!;
-            const keyA = aPath.parentField() as string;
-            const keyB = bPath.parentField() as string;
-            let indexA = aPath.parentIndex();
-            let indexB = bPath.parentIndex();
-            if (keyA !== keyB) {
-                const modify: T.Modify = {
-                    type: "Modify",
-                    fields: { ...a.marks, ...b.marks },
-                };
-                this.applyAtPath(modify, aPath);
-            } else {
-                if (indexA === indexB) {
-                    fail(ERR_UP_PATH_NOT_VALID);
-                }
-                if (indexA > indexB) {
-                    [a, indexA, b, indexB] = [b, indexB, a, indexA];
-                }
-                const gap = indexB - indexA - 1;
+        if (source.parent() === destination.parent()) {
+            const srcIndex = source.parentIndex();
+            const dstIndex = destination.parentIndex();
+            if (source.parentField() === destination.parentField()) {
                 let marks;
-                if (indexA === 0) {
-                    if (gap === 0) {
-                        marks = [a.marks[keyA][0], b.marks[keyB][1]];
+                if (dstIndex <= srcIndex) {
+                    const gap = srcIndex - dstIndex;
+                    if (dstIndex === 0) {
+                        if (gap === 0) {
+                            marks = [moveIn, moveOut];
+                        } else {
+                            marks = [moveIn, gap, moveOut];
+                        }
                     } else {
-                        marks = [a.marks[keyA][0], gap, b.marks[keyB][1]];
+                        if (gap === 0) {
+                            marks = [moveIn, moveOut];
+                        } else {
+                            marks = [dstIndex, moveIn, gap, moveOut];
+                        }
                     }
                 } else {
-                    if (gap === 0) {
-                        marks = [indexA, a.marks[keyA][1], b.marks[keyB][1]];
+                    const gap = dstIndex - srcIndex;
+                    if (gap < count) {
+                        // The target attach point lies within the range of nodes being detached
+                        fail("Not implemented");
                     } else {
-                        marks = [indexA, a.marks[keyA][1], gap, b.marks[keyB][1]];
+                        const updatedGap = gap - count;
+                        if (srcIndex === 0) {
+                            if (updatedGap === 0) {
+                                marks = [moveOut, moveIn];
+                            } else {
+                                marks = [moveOut, updatedGap, moveIn];
+                            }
+                        } else {
+                            if (updatedGap === 0) {
+                                marks = [srcIndex, moveOut, moveIn];
+                            } else {
+                                marks = [srcIndex, moveOut, updatedGap, moveIn];
+                            }
+                        }
                     }
                 }
-                const modify: T.Modify = {
-                    type: "Modify",
-                    fields: { [keyA]: marks },
-                };
-                this.applyAtPath(modify, aPath);
+                this.applyFieldMarksAtPath({ [source.parentField() as string]: marks }, source.parent());
+            } else {
+                this.applyFieldMarksAtPath(
+                    {
+                        [source.parentField() as string]: srcIndex > 0 ? [srcIndex, moveOut] : [moveOut],
+                        [destination.parentField() as string]: dstIndex > 0 ? [dstIndex, moveIn] : [moveIn],
+                    },
+                    source.parent(),
+                );
+            }
+        } else {
+            // The source and destination are under different parent nodes
+            let a: NestBranch = {
+                marks: toFieldMarks(moveOut, source),
+                path: source.parent(),
+            };
+            let b: NestBranch = {
+                marks: toFieldMarks(moveIn, destination),
+                path: destination.parent(),
+            };
+            let depthDiff = getDepth(source) - getDepth(destination);
+            // Ensure that a represents the deeper mark
+            if (depthDiff < 0) {
+                [a, b] = [b, a];
+                depthDiff = -depthDiff;
+            }
+            // Nest the deeper mark so that they are both at the same depth
+            a = wrapN(a.marks, a.path, depthDiff);
+            // Nest both marks one level at a time until they reach the same parent
+            while (a.path?.parent() !== b.path?.parent()) {
+                a = wrapN(a.marks, a.path, 1);
+                b = wrapN(b.marks, b.path, 1);
+            }
+            if (a.path === undefined) {
+                this.applyChange({ marks: { ...a.marks, ...b.marks } });
+            } else {
+                const aPath = a.path;
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                const bPath = b.path!;
+                const keyA = aPath.parentField() as string;
+                const keyB = bPath.parentField() as string;
+                let indexA = aPath.parentIndex();
+                let indexB = bPath.parentIndex();
+                const aMarks = wrap1(a.marks, aPath);
+                const bMarks = wrap1(b.marks, bPath);
+                if (keyA !== keyB) {
+                    this.applyFieldMarksAtPath({ ...aMarks, ...bMarks }, aPath.parent());
+                } else {
+                    if (indexA === indexB) {
+                        fail(ERR_UP_PATH_NOT_VALID);
+                    }
+                    if (indexA > indexB) {
+                        [a, indexA, b, indexB] = [b, indexB, a, indexA];
+                    }
+                    const gap = indexB - indexA - 1;
+                    let marks;
+                    if (indexA === 0) {
+                        if (gap === 0) {
+                            marks = [aMarks[keyA][0], bMarks[keyB][1]];
+                        } else {
+                            marks = [aMarks[keyA][0], gap, bMarks[keyB][1]];
+                        }
+                    } else {
+                        if (gap === 0) {
+                            marks = [indexA, aMarks[keyA][1], bMarks[keyB][1]];
+                        } else {
+                            marks = [indexA, aMarks[keyA][1], gap, bMarks[keyB][1]];
+                        }
+                    }
+                    this.applyFieldMarksAtPath({ [keyA]: marks }, aPath.parent());
+                }
             }
         }
     }
 
-    private applyAtPath(mark: T.Mark, path: UpPath) {
-        this.applyChange({ marks: nest(mark, path) });
+    private applyMarkAtPath(mark: T.Mark, path: UpPath) {
+        this.applyFieldMarksAtPath(toFieldMarks(mark, path), path.parent());
+    }
+
+    private applyFieldMarksAtPath(marks: T.FieldMarks, path: UpPath | undefined) {
+        this.applyChange({ marks: wrap(marks, path) });
     }
 }
 
@@ -130,35 +183,33 @@ function toFieldMarks(mark: T.Mark, node: UpPath): T.FieldMarks {
     };
 }
 
-function nestN(mark: T.FieldMarks, node: UpPath | undefined, depth: number) {
+function wrapN(mark: T.FieldMarks, node: UpPath | undefined, depth: number) {
     let currentNode: UpPath | undefined = node;
     let out: T.FieldMarks = mark;
     let currentDepth = 0;
     while (currentNode !== undefined && currentDepth < depth) {
-        out = toFieldMarks({ type: "Modify", fields: out }, currentNode);
+        out = wrap1(out, currentNode);
         currentDepth += 1;
         currentNode = currentNode.parent();
     }
     return { marks: out, path: currentNode };
 }
 
-function nest(mark: T.Mark, node: UpPath): T.FieldMarks {
+function wrap(mark: T.FieldMarks, node: UpPath | undefined): T.FieldMarks {
     let currentNode: UpPath | undefined = node;
-    let out: T.FieldMarks;
-    let currMark = mark;
-    do {
-        const key = currentNode.parentField();
-        const index = currentNode.parentIndex();
-        out = {
-            [key as string]: index === 0 ? [currMark] : [index, currMark],
-        };
+    let out: T.FieldMarks = mark;
+    while (currentNode !== undefined) {
+        out = wrap1(out, currentNode);
         currentNode = currentNode.parent();
-        currMark = {
-            type: "Modify",
-            fields: out,
-        };
-    } while (currentNode !== undefined);
+    }
     return out;
+}
+
+function wrap1(marks: T.FieldMarks, node: UpPath | undefined): T.FieldMarks {
+    if (node !== undefined) {
+        return toFieldMarks({ type: "Modify", fields: marks }, node);
+    }
+    return marks;
 }
 
 type NodePath = UpPath;
--- packages/dds/tree/src/test/sequence-change-family/sequenceEditBuilder.spec.ts
+++ packages/dds/tree/src/test/sequence-change-family/sequenceEditBuilder.spec.ts
@@ -7,10 +7,12 @@ import { strict as assert } from "assert";
 import { jsonString } from "../../domains";
 import { AnchorSet, Delta, FieldKey, UpPath } from "../../tree";
 import { SequenceEditBuilder, TextCursor } from "../../feature-libraries";
-import { brand } from "../../util";
+import { brand, brandOpaque } from "../../util";
 
 const rootKey = brand<FieldKey>("root");
+const detachedKey = brand<FieldKey>("detached");
 const fooKey = brand<FieldKey>("foo");
+const barKey = brand<FieldKey>("bar");
 
 const root: UpPath = {
     parent: () => undefined,
@@ -18,20 +20,51 @@ const root: UpPath = {
     parentIndex: () => 0,
 };
 
-const child: UpPath = {
+const detached: UpPath = {
+    parent: () => undefined,
+    parentField: () => detachedKey,
+    parentIndex: () => 0,
+};
+
+const root_foo2: UpPath = {
     parent: () => root,
     parentField: () => fooKey,
     parentIndex: () => 2,
 };
 
-const grandChild: UpPath = {
-    parent: () => child,
+const root_bar2: UpPath = {
+    parent: () => root,
+    parentField: () => barKey,
+    parentIndex: () => 2,
+};
+
+const root_foo17: UpPath = {
+    parent: () => root,
     parentField: () => fooKey,
+    parentIndex: () => 17,
+};
+
+const root_foo2_foo5: UpPath = {
+    parent: () => root_foo2,
+    parentField: () => fooKey,
+    parentIndex: () => 5,
+};
+
+const root_foo17_foo5: UpPath = {
+    parent: () => root_foo17,
+    parentField: () => fooKey,
+    parentIndex: () => 5,
+};
+
+const root_bar2_bar5: UpPath = {
+    parent: () => root_bar2,
+    parentField: () => barKey,
     parentIndex: () => 5,
 };
 
 const nodeX = { type: jsonString.name, value: "X" };
 const content = [nodeX];
+const moveId = brandOpaque<Delta.MoveId>(0);
 
 function test(editor: (builder: SequenceEditBuilder) => void, expected: Delta.Root): void {
     let wasCalled = false;
@@ -87,7 +120,7 @@ describe("SequenceEditBuilder", () => {
             }],
         ]]);
         test(
-            (builder) => { builder.setValue(grandChild, 42); },
+            (builder) => { builder.setValue(root_foo2_foo5, 42); },
             expected,
         );
     });
@@ -133,7 +166,7 @@ describe("SequenceEditBuilder", () => {
             }],
         ]]);
         test(
-            (builder) => { builder.insert(grandChild, new TextCursor(nodeX)); },
+            (builder) => { builder.insert(root_foo2_foo5, new TextCursor(nodeX)); },
             expected,
         );
     });
@@ -179,7 +212,257 @@ describe("SequenceEditBuilder", () => {
             }],
         ]]);
         test(
-            (builder) => { builder.delete(grandChild, 10); },
+            (builder) => { builder.delete(root_foo2_foo5, 10); },
+            expected,
+        );
+    });
+
+    it("Can move nodes within a field", () => {
+        const expected: Delta.Root = new Map([[
+            rootKey,
+            [{
+                type: Delta.MarkType.Modify,
+                fields: new Map([[
+                    fooKey,
+                    [
+                        2,
+                        {
+                            type: Delta.MarkType.MoveOut,
+                            moveId,
+                            count: 10,
+                        },
+                        5,
+                        {
+                            type: Delta.MarkType.MoveIn,
+                            moveId,
+                        },
+                    ],
+                ]]),
+            }],
+        ]]);
+        test(
+            (builder) => { builder.move(root_foo2, 10, root_foo17); },
+            expected,
+        );
+    });
+
+    it("Can move nodes across fields of the same parent", () => {
+        const expected: Delta.Root = new Map([[
+            rootKey,
+            [{
+                type: Delta.MarkType.Modify,
+                fields: new Map([
+                    [
+                        fooKey,
+                        [
+                            2,
+                            {
+                                type: Delta.MarkType.MoveOut,
+                                moveId,
+                                count: 10,
+                            },
+                        ],
+                    ],
+                    [
+                        barKey,
+                        [
+                            2,
+                            {
+                                type: Delta.MarkType.MoveIn,
+                                moveId,
+                            },
+                        ],
+                    ],
+                ]),
+            }],
+        ]]);
+        test(
+            (builder) => { builder.move(root_foo2, 10, root_bar2); },
+            expected,
+        );
+    });
+
+    it("Can move nodes across subtrees of the same field", () => {
+        const expected: Delta.Root = new Map([[
+            rootKey,
+            [{
+                type: Delta.MarkType.Modify,
+                fields: new Map([
+                    [
+                        fooKey,
+                        [
+                            2,
+                            {
+                                type: Delta.MarkType.Modify,
+                                fields: new Map([
+                                    [
+                                        fooKey,
+                                        [
+                                            5,
+                                            {
+                                                type: Delta.MarkType.MoveOut,
+                                                moveId,
+                                                count: 3,
+                                            },
+                                        ],
+                                    ],
+                                ]),
+                            },
+                            14,
+                            {
+                                type: Delta.MarkType.Modify,
+                                fields: new Map([
+                                    [
+                                        fooKey,
+                                        [
+                                            5,
+                                            {
+                                                type: Delta.MarkType.MoveIn,
+                                                moveId,
+                                            },
+                                        ],
+                                    ],
+                                ]),
+                            },
+                        ],
+                    ],
+                ]),
+            }],
+        ]]);
+        test(
+            (builder) => { builder.move(root_foo2_foo5, 3, root_foo17_foo5); },
+            expected,
+        );
+    });
+
+    it("Can move nodes across subtrees of different fields", () => {
+        const expected: Delta.Root = new Map([[
+            rootKey,
+            [{
+                type: Delta.MarkType.Modify,
+                fields: new Map([
+                    [
+                        fooKey,
+                        [
+                            2,
+                            {
+                                type: Delta.MarkType.Modify,
+                                fields: new Map([
+                                    [
+                                        fooKey,
+                                        [
+                                            5,
+                                            {
+                                                type: Delta.MarkType.MoveOut,
+                                                moveId,
+                                                count: 3,
+                                            },
+                                        ],
+                                    ],
+                                ]),
+                            },
+                        ],
+                    ],
+                    [
+                        barKey,
+                        [
+                            2,
+                            {
+                                type: Delta.MarkType.Modify,
+                                fields: new Map([
+                                    [
+                                        barKey,
+                                        [
+                                            5,
+                                            {
+                                                type: Delta.MarkType.MoveIn,
+                                                moveId,
+                                            },
+                                        ],
+                                    ],
+                                ]),
+                            },
+                        ],
+                    ],
+                ]),
+            }],
+        ]]);
+        test(
+            (builder) => { builder.move(root_foo2_foo5, 3, root_bar2_bar5); },
+            expected,
+        );
+    });
+
+    it("Can move nodes to a detached tree", () => {
+        const expected: Delta.Root = new Map([
+            [
+                rootKey,
+                [{
+                    type: Delta.MarkType.Modify,
+                    fields: new Map([
+                        [
+                            fooKey,
+                            [
+                                2,
+                                {
+                                    type: Delta.MarkType.MoveOut,
+                                    moveId,
+                                    count: 10,
+                                },
+                            ],
+                        ],
+                    ]),
+                }],
+            ],
+            [
+                detachedKey,
+                [
+                    {
+                        type: Delta.MarkType.MoveIn,
+                        moveId,
+                    },
+                ],
+            ],
+        ]);
+        test(
+            (builder) => { builder.move(root_foo2, 10, detached); },
+            expected,
+        );
+    });
+
+    it("Can move nodes from a detached tree", () => {
+        const expected: Delta.Root = new Map([
+            [
+                rootKey,
+                [{
+                    type: Delta.MarkType.Modify,
+                    fields: new Map([
+                        [
+                            fooKey,
+                            [
+                                2,
+                                {
+                                    type: Delta.MarkType.MoveIn,
+                                    moveId,
+                                },
+                            ],
+                        ],
+                    ]),
+                }],
+            ],
+            [
+                detachedKey,
+                [
+                    {
+                        type: Delta.MarkType.MoveOut,
+                        moveId,
+                        count: 10,
+                    },
+                ],
+            ],
+        ]);
+        test(
+            (builder) => { builder.move(detached, 10, root_foo2); },
             expected,
         );
     });
