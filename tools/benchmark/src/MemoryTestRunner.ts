0f88c84b081f522274e84d8f9a14f2f074a3b001
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-07-26 20:57:53 +0000

    Missing copyright header

--- tools/benchmark/src/MemoryTestRunner.ts
+++ tools/benchmark/src/MemoryTestRunner.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import * as v8 from "v8";
 import { performance } from "perf_hooks";
 import { assert } from "chai";
