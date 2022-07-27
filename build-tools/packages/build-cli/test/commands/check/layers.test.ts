baeffd62cf18422b767c38ec3c28b923fec018b6
sdeshpande3 <sdeshpande@microsoft.com>
2022-07-27 12:09:22 -0700

    basic layer check testing

---
+++ build-tools/packages/build-cli/test/commands/check/layers.test.ts
@@ -0,0 +1,44 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { expect, test } from "@oclif/test";
+
+describe("layers", () => {
+    test.stdout()
+        .command(["check layers"])
+        .it("Runs basic layer check command", (ctx) => {
+            expect(ctx.stdout).to.contain("Layer check passed (170 packages)");
+        });
+
+    // checks for single flags
+    test.stdout()
+        .command(["check layers", "--md", "."])
+        .it("Run layer check command by passing md flag", (ctx) => {
+            expect(ctx.stdout).to.contain("Layer check passed (170 packages)");
+        });
+
+    test.stdout()
+        .command(["check layers", "--dot", ""])
+        .it("Run layer check command by passing dot flag", (ctx) => {
+            // expect(ctx.stdout).to.contain("Layer check passed (170 packages)");
+    });
+
+    test.stdout()
+        .command(["check layers", "--info", ""])
+        .it("Run layer check command by passing info flag", (ctx) => {
+            // expect(ctx.stdout).to.contain("Layer check passed (170 packages)");
+    });
+
+    test.stdout()
+        .command(["check layers", "--logtime", ""])
+        .it("Run layer check command by passing logtime flag", (ctx) => {
+            // expect(ctx.stdout).to.contain("Layer check passed (170 packages)");
+    });
+
+    // checks for multiple flags
+
+
+    // test layer check failures
+});
