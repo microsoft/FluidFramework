14071fa699d57e627b5fab1f1f2d45bef80a69de
sdeshpande3 <sdeshpande@microsoft.com>
2022-07-28 13:02:39 -0700

    remove test

--- build-tools/packages/build-cli/test/commands/check/layers.test.ts
+++ build-tools/packages/build-cli/test/commands/check/layers.test.ts
@@ -9,43 +9,17 @@ describe("layers", () => {
     test.stdout()
         .command(["check layers"])
         .it("Runs basic layer check command", (ctx) => {
-            expect(ctx.stdout).contains("Layer check passed ");
+            expect(ctx.stdout).contains("Layer check passed");
         });
 
     // checks for single flags
     test.stdout()
         .command(["check layers", "--md", "."])
         .it("Run layer check command by passing md flag", (ctx) => {
-            expect(ctx.stdout).contains("Layer check passed ");
+            expect(ctx.stdout).contains("Layer check passed");
         });
 
-    // test.stdout()
-    //     .command(["check layers", "--dot", ""])
-    //     .it("Run layer check command by passing dot flag", (ctx) => {
-    //         // expect(ctx.stdout).to.contain("Layer check passed (170 packages)");
-    //         expect(ctx.stdout);
-    //     });
-
-    // test.stdout()
-    //     .command(["check layers", "--info", ""])
-    //     .it("Run layer check command by passing info flag", (ctx) => {
-    //         // expect(ctx.stdout).to.contain("Layer check passed (170 packages)");
-    //         expect(ctx.stdout);
-    //     });
-
-    // test.stdout()
-    //     .command(["check layers", "--logtime", ""])
-    //     .it("Run layer check command by passing logtime flag", (ctx) => {
-    //         // expect(ctx.stdout).to.contain("Layer check passed (170 packages)");
-    //         expect(ctx.stdout);
-    //     });
-
-    // // checks for multiple flags
-    // test.stdout()
-    //     .command(["check layers", "--md", ".", "--info", ""])
-    //     .it("Run layer check command by multiple flags", (ctx) => {
-    //         // expect(ctx.stdout).to.contain("Layer check passed (170 packages)");
-    // });
+    // checks for multiple flags
 
     // test layer check failures
 });
