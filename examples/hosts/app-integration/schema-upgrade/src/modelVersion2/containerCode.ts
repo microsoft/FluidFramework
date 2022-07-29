38480d5d41700421ad1b79031944376f55e236ac
Matt Rakow <marakow@microsoft.com>
2022-07-29 14:21:36 -0700

    Fix summarizer blocking migration

--- examples/hosts/app-integration/schema-upgrade/src/modelVersion2/containerCode.ts
+++ examples/hosts/app-integration/schema-upgrade/src/modelVersion2/containerCode.ts
@@ -9,6 +9,7 @@ import {
 } from "@fluidframework/aqueduct";
 import { IContainerRuntime } from "@fluidframework/container-runtime-definitions";
 import { rootDataStoreRequestHandler } from "@fluidframework/request-handler";
+import { requestFluidObject } from "@fluidframework/runtime-utils";
 
 import { ContainerKillBitInstantiationFactory } from "../containerKillBit";
 import { InventoryListInstantiationFactory } from "./inventoryList";
@@ -43,5 +44,8 @@ export class InventoryListContainerRuntimeFactory extends BaseContainerRuntimeFa
 
     protected async containerHasInitialized(runtime: IContainerRuntime): Promise<void> {
         console.info("Using runtime factory version two");
+        // Force the killbit to instantiate in all cases.  The Quorum it uses must be loaded and running in order to
+        // respond with accept ops, and without this call the killbit won't be instantiated on the summarizer client.
+        await requestFluidObject(await runtime.getRootDataStore(containerKillBitId), "");
     }
 }
