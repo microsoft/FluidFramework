7245debb61ccab1501a06ccf2d43e4903359520d
Yann Achard <achardy@microsoft.com>
2022-07-29 10:30:35 -0700

    Add checking of anchor rebasing

--- packages/dds/tree/src/test/edit-manager/editManager.spec.ts
+++ packages/dds/tree/src/test/edit-manager/editManager.spec.ts
@@ -43,9 +43,15 @@ function isNonEmptyChange(
     return "inputContext" in change && "outputContext" in change;
 }
 
+interface AnchorRebaseData {
+    rebases: RecursiveReadonly<NonEmptyTestChangeset>[];
+    intentions: Set<number>;
+}
+
 class TestChangeRebaser implements ChangeRebaser<TestChangeset, TestChangeset, TestChangeset> {
     private contextCounter: number = 0;
     private intentionCounter: number = 0;
+    public readonly anchorRebases: Map<AnchorSet, AnchorRebaseData> = new Map();
 
     public compose(...changes: TestChangeset[]): TestChangeset {
         let inputContext: number | undefined;
@@ -106,8 +112,29 @@ class TestChangeRebaser implements ChangeRebaser<TestChangeset, TestChangeset, T
         return emptyChange;
     }
 
-    public rebaseAnchors(anchor: AnchorSet, over: TestChangeset): void {
-        throw new Error("Method not implemented.");
+    public rebaseAnchors(anchors: AnchorSet, over: TestChangeset): void {
+        if (isNonEmptyChange(over)) {
+            let data = this.anchorRebases.get(anchors);
+            if (data === undefined) {
+                data = { rebases: [], intentions: new Set() };
+                this.anchorRebases.set(anchors, data);
+            }
+            let lastChange: RecursiveReadonly<NonEmptyTestChangeset> | undefined;
+            const { rebases, intentions } = data;
+            for (let iChange = rebases.length - 1; iChange >= 0; --iChange) {
+                const change = rebases[iChange];
+                if (isNonEmptyChange(change)) {
+                    lastChange = change;
+                    break;
+                }
+            }
+            if (lastChange !== undefined) {
+                // The new change should apply to the context brought about by the previous change
+                assert.equal(over.inputContext, lastChange.outputContext);
+            }
+            updateIntentionSet(over.intentions, intentions);
+            rebases.push(over);
+        }
     }
 
     public import(change: TestChangeset): TestChangeset {
@@ -135,20 +162,7 @@ class TestChangeRebaser implements ChangeRebaser<TestChangeset, TestChangeset, T
         );
         let index = 0;
         for (const change of filtered) {
-            for (const intention of change.intentions) {
-                if (intention > 0) {
-                    // The same intention should never be applied multiple times
-                    assert(!intentionsSeen.has(intention));
-                    intentionsSeen.add(intention);
-                    // The intention should be part of the expected set for this client
-                    assert(intentionsExpected.has(intention));
-                } else if (intention < 0) {
-                    // We are dealing with the inverse of an intention.
-                    // In order for the inverse to apply, the non-inverse should have been applied already
-                    assert(intentionsSeen.has(-intention));
-                    intentionsSeen.delete(-intention);
-                }
-            }
+            updateIntentionSet(change.intentions, intentionsSeen);
             if (index > 0) {
                 const prev = filtered[index - 1];
                 // The current change should apply to the context brought about by the previous change
@@ -157,12 +171,31 @@ class TestChangeRebaser implements ChangeRebaser<TestChangeset, TestChangeset, T
             ++index;
         }
         // All expected intentions were present
-        assert.equal(intentionsSeen.size, intentionsExpected.size);
+        assert.deepEqual(intentionsSeen, intentionsExpected);
     }
 }
 
 type TestChangeFamily = ChangeFamily<unknown, TestChangeset>;
 type TestEditManager = EditManager<TestChangeset, TestChangeFamily>;
+
+function updateIntentionSet(
+    intentions: readonly number[],
+    intentionsSeen: Set<number>,
+) {
+    for (const intention of intentions) {
+        if (intention > 0) {
+            // The same intention should never be applied multiple times
+            assert(!intentionsSeen.has(intention));
+            intentionsSeen.add(intention);
+            // The intention should be part of the expected set for this client
+        } else if (intention < 0) {
+            // We are dealing with the inverse of an intention.
+            // In order for the inverse to apply, the non-inverse should have been applied already
+            assert(intentionsSeen.has(-intention));
+            intentionsSeen.delete(-intention);
+        }
+    }
+}
 
 function changeFamilyFactory(): {
     family: ChangeFamily<unknown, TestChangeset>;
@@ -505,6 +538,15 @@ describe("EditManager", () => {
         checkChangeList(manager, rebaser);
     });
 
+    /**
+     * This test case effectively tests most of the scenarios covered by the other test cases.
+     * Despite that, it's good to keep the other tests cases for the following reasons:
+     * - They give a clearer account of what the API usage is like.
+     * - They are easier to debug.
+     * - They are less reliant on the `EditManager` implementation in their construction of test input.
+     * - They help diagnose issues with the more complicated exhaustive test (e.g., if one of the above tests fails,
+     *   but this one doesn't, then there might be something wrong with this test).
+     */
     it("Can handle all possible interleaving of steps", () => {
         const meta = {
             clientData: makeArray(NUM_CLIENTS, () => ({ pulled: 0, numLocal: 0 })),
@@ -525,14 +567,17 @@ type ScenarioStep =
     | { type: "Receive"; client: number; }
 ;
 
-interface ScenarioMetadata {
+/**
+ * State needed by the scenario builder.
+ */
+interface ScenarioBuilderState {
     clientData: { pulled: SeqNumber; numLocal: number; }[];
     seq: SeqNumber;
 }
 
 function* buildScenario(
     scenario: ScenarioStep[],
-    meta: ScenarioMetadata,
+    meta: ScenarioBuilderState,
 ): Generator<readonly ScenarioStep[]> {
     if (scenario.length >= NUM_STEPS) {
         yield scenario;
@@ -598,6 +643,7 @@ function runScenario(scenario: readonly ScenarioStep[]): void {
         manager: new EditManager<TestChangeset, TestChangeFamily>(
             brand(iClient),
             family,
+            new AnchorSet(),
         ),
         localChanges: [],
         ref: 0,
@@ -630,6 +676,11 @@ function runScenario(scenario: readonly ScenarioStep[]): void {
         // Check the validity of the managers
         for (const client of clientData) {
             checkChangeList(client.manager, rebaser, client.intentions);
+            const intentionsThatAnchorsWereRebasedOver =
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                rebaser.anchorRebases.get(client.manager.anchors!)?.intentions;
+            // Check the anchors have been updated if applicable
+            assert.deepEqual(intentionsThatAnchorsWereRebasedOver ?? new Set(), client.intentions);
         }
     }
 }
