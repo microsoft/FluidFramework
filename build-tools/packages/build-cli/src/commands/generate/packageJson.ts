bc882184e7f5b53d6ac287ceb57c1a3f4fdad55b
sdeshpande3 <sdeshpande@microsoft.com>
2022-07-28 17:33:26 -0700

    use releaseGroup

--- build-tools/packages/build-cli/src/commands/generate/packageJson.ts
+++ build-tools/packages/build-cli/src/commands/generate/packageJson.ts
@@ -3,24 +3,19 @@
  * Licensed under the MIT License.
  */
 
-import { Flags } from "@oclif/core";
 import {
     generateMonoRepoInstallPackageJson,
-    MonoRepoKind,
+    isMonoRepoKind,
     Timer,
 } from "@fluidframework/build-tools";
 import { BaseCommand } from "../../base";
+import { releaseGroupFlag } from "../../flags";
 
 export class GeneratePackageJson extends BaseCommand {
-    static description = "describe the command here";
+    static description = `Generate mono repo package json`;
 
     static flags = {
-        monoRepoKind: Flags.enum({
-            description: `Generate package lock for specified (server/client/azure/build-tools) mono repo`,
-            options: ["--server", "--azure", "--build-tools"],
-            required: false,
-            default: "--client",
-        }),
+        releaseGroup: releaseGroupFlag({ required: true }),
         ...super.flags,
     };
 
@@ -34,32 +29,18 @@ export class GeneratePackageJson extends BaseCommand {
         const repo = context.repo;
         timer.time("Package scan completed");
 
-        let kind = MonoRepoKind.Client;
+        let releaseGroup;
 
-        // https://oclif.io/docs/base_class
-        switch (flags.monoRepoKind) {
-            case "--server":
-                if (flags.monoRepoKind === "--server") kind = MonoRepoKind.Server;
-                break;
+        if (typeof flags.releaseGroup === "string" && isMonoRepoKind(flags.releaseGroup)) {
+            releaseGroup = repo.monoRepos.get(flags.releaseGroup) ?? undefined;
 
-            case "--azure":
-                if (flags.monoRepoKind === "--azure") kind = MonoRepoKind.Azure;
-                break;
-
-            case "--build-tools":
-                if (flags.monoRepoKind === "--build-tools") kind = MonoRepoKind.BuildTools;
-                break;
-        }
-
-        try {
-            const releaseGroup = repo.monoRepos.get(kind);
             if (releaseGroup === undefined) {
                 this.error(`release group couldn't be found.`);
             }
 
             await generateMonoRepoInstallPackageJson(releaseGroup);
-        } catch (error_: unknown) {
-            this.error(error_ as string);
         }
+
+        this.error(`release group is not mono repo kind.`);
     }
 }
