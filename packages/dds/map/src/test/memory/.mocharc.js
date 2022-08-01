c6684a983cdf31ecdc09ce35053f7097523ee8c4
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-08-01 17:01:11 +0000

    Fixes for policy-check

--- packages/dds/map/src/test/memory/.mocharc.js
+++ packages/dds/map/src/test/memory/.mocharc.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 /**
  * Mocha configuration file to run memory-profiling tests
  */
