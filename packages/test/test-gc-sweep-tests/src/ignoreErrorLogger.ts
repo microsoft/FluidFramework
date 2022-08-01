2bc731b673d31d2aa1b11ef65fbd741fdfdaf979
tyler-cai-microsoft <tylercai@microsoft.com>
2022-08-01 10:40:36 -0700

    Copyright clause

--- packages/test/test-gc-sweep-tests/src/ignoreErrorLogger.ts
+++ packages/test/test-gc-sweep-tests/src/ignoreErrorLogger.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { strict as assert } from "assert";
 import { EventAndErrorTrackingLogger } from "@fluidframework/test-utils";
 import { ITelemetryBaseEvent, ITelemetryGenericEvent } from "@fluidframework/common-definitions";
