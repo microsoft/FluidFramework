04a179c0a1b2294bd1fe7d843188c2529ded32ad
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-08-03 23:04:26 +0000

    Fix linter violations in merge-tree

--- packages/dds/merge-tree/src/collections/rbTree.ts
+++ packages/dds/merge-tree/src/collections/rbTree.ts
@@ -277,11 +277,7 @@ export class RedBlackTree<TKey, TData> implements SortedDictionary<TKey, TData> 
                     if (kd.key) {
                         _node.key = kd.key;
                     }
-                    if (kd.data) {
-                        _node.data = kd.data;
-                    } else {
-                        _node.data = data;
-                    }
+                    _node.data = kd.data ? kd.data : data;
                 } else {
                     _node.data = data;
                 }
@@ -403,11 +399,7 @@ export class RedBlackTree<TKey, TData> implements SortedDictionary<TKey, TData> 
                 return this.nodeFloor(node.left, key);
             } else {
                 const rightFloor = this.nodeFloor(node.right, key);
-                if (rightFloor) {
-                    return rightFloor;
-                } else {
-                    return node;
-                }
+                return rightFloor ? rightFloor : node;
             }
         }
     }
@@ -427,11 +419,7 @@ export class RedBlackTree<TKey, TData> implements SortedDictionary<TKey, TData> 
                 return this.nodeCeil(node.right, key);
             } else {
                 const leftCeil = this.nodeCeil(node.left, key);
-                if (leftCeil) {
-                    return leftCeil;
-                } else {
-                    return node;
-                }
+                return leftCeil ? leftCeil : node;
             }
         }
     }
@@ -443,11 +431,7 @@ export class RedBlackTree<TKey, TData> implements SortedDictionary<TKey, TData> 
     }
 
     private nodeMin(node: RBNode<TKey, TData>): RBNode<TKey, TData> {
-        if (!node.left) {
-            return node;
-        } else {
-            return this.nodeMin(node.left);
-        }
+        return !node.left ? node : this.nodeMin(node.left);
     }
 
     public max() {
@@ -457,11 +441,7 @@ export class RedBlackTree<TKey, TData> implements SortedDictionary<TKey, TData> 
     }
 
     private nodeMax(node: RBNode<TKey, TData>): RBNode<TKey, TData> {
-        if (!node.right) {
-            return node;
-        } else {
-            return this.nodeMax(node.right);
-        }
+        return !node.right ? node : this.nodeMax(node.right);
     }
 
     private rotateRight(node: RBNode<TKey, TData>) {
