3cc059f1bd0143cac9c7f0328ac2a8c625d2aea7
daesun-park <108361046+daesun-park@users.noreply.github.com>
2022-08-04 08:09:47 +0000

    Comment addressing input/output array size

--- experimental/dds/tree/src/HistoryEditFactory.ts
+++ experimental/dds/tree/src/HistoryEditFactory.ts
@@ -29,8 +29,9 @@ import { getChangeNodeFromViewNode } from './SerializationUtilities';
  * @param changes - the changes for which to produce an inverse.
  * @param before - a view of the tree state before `changes` are/were applied - used as a basis for generating the inverse.
  * @returns if the changes could be reverted, a sequence of changes _r_ that will produce `before` if applied to a view _A_, where _A_ is the result of
- * applying `changes` to `before`. Applying _r_ to views other than _A_ is legal but may cause the changes to fail to apply or may
- * not be a true semantic inverse. If the changes could not be reverted given the state of `before`, returns undefined.
+ * applying `changes` to `before`. Note that the size of the array of reverted changes may not be the same as the input array, and may even be empty in cases where
+ * the view did not change. Applying _r_ to views other than _A_ is legal but may cause the changes to fail to apply or may not be a true semantic inverse.
+ * If the changes could not be reverted given the state of `before`, returns undefined.
  *
  * TODO: what should this do if `changes` fails to apply to `before`?
  * TODO:#68574: Pass a view that corresponds to the appropriate Fluid reference sequence number rather than the view just before
