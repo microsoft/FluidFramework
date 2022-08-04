dacd786d8746629c78ad1dd245c06444ebcef035
Tyler Butler <tylerbu@microsoft.com>
2022-08-04 12:28:59 -0700

    [port to 1.2] Deduping FluidDataStoreRegistry entries (#11358) (#11390) (#11427)

--- packages/framework/fluid-static/src/utils.ts
+++ packages/framework/fluid-static/src/utils.ts
@@ -22,7 +22,9 @@ export const isDataObjectClass = (obj: any): obj is DataObjectClass<any> => {
 /**
  * Runtime check to determine if a class is a SharedObject type
  */
-export const isSharedObjectClass = (obj: any): obj is SharedObjectClass<any> => {
+export const isSharedObjectClass = (
+    obj: any,
+): obj is SharedObjectClass<any> => {
     return obj?.getFactory !== undefined;
 };
 
@@ -31,8 +33,9 @@ export const isSharedObjectClass = (obj: any): obj is SharedObjectClass<any> => 
  * of both SharedObject or DataObject. This function seperates the two and returns a registery
  * of DataObject types and an array of SharedObjects.
  */
-export const parseDataObjectsFromSharedObjects = (schema: ContainerSchema):
-    [NamedFluidDataStoreRegistryEntry[], IChannelFactory[]] => {
+export const parseDataObjectsFromSharedObjects = (
+    schema: ContainerSchema,
+): [NamedFluidDataStoreRegistryEntry[], IChannelFactory[]] => {
     const registryEntries: Set<NamedFluidDataStoreRegistryEntry> = new Set();
     const sharedObjects: Set<IChannelFactory> = new Set();
 
@@ -40,26 +43,26 @@ export const parseDataObjectsFromSharedObjects = (schema: ContainerSchema):
         if (isSharedObjectClass(obj)) {
             sharedObjects.add(obj.getFactory());
         } else if (isDataObjectClass(obj)) {
-            registryEntries.add([obj.factory.type, Promise.resolve(obj.factory)]);
+            registryEntries.add([
+                obj.factory.type,
+                Promise.resolve(obj.factory),
+            ]);
         } else {
             throw new Error(`Entry is neither a DataObject or a SharedObject`);
         }
     };
 
     // Add the object types that will be initialized
-    Object.values(schema.initialObjects).forEach((obj) => {
-        tryAddObject(obj);
-    });
-
-    // If there are dynamic object types we will add them now
-    if (schema.dynamicObjectTypes) {
-        for (const obj of schema.dynamicObjectTypes) {
-            tryAddObject(obj);
-        }
-    }
+    const dedupedObjects = new Set([
+        ...Object.values(schema.initialObjects),
+        ...(schema.dynamicObjectTypes ?? []),
+    ]);
+    dedupedObjects.forEach(tryAddObject);
 
     if (registryEntries.size === 0 && sharedObjects.size === 0) {
-        throw new Error("Container cannot be initialized without any DataTypes");
+        throw new Error(
+            "Container cannot be initialized without any DataTypes",
+        );
     }
 
     return [Array.from(registryEntries), Array.from(sharedObjects)];
