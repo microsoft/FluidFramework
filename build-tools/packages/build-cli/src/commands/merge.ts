c1bc5f996a5576da320552a2c771d6ff9e4166df
sdeshpande3 <sdeshpande@microsoft.com>
2022-08-08 10:12:45 -0700

    commands

--- build-tools/packages/build-cli/src/commands/merge.ts
+++ build-tools/packages/build-cli/src/commands/merge.ts
@@ -1,38 +1,32 @@
 import { createBranch } from "@fluidframework/build-tools";
-import { Flags } from '@oclif/core';
-import { BaseCommand } from '../base';
-// import { Octokit } from '@octokit/core';
-// import { QueueServiceClient } from '@azure/storage-queue';
+import { Flags } from "@oclif/core";
+import { BaseCommand } from "../base";
 
-const owner = "microsoft";
-const repo = "FluidFramework";
+// const owner = 'microsoft';
+// const repo = 'FluidFramework';
 // const title = "Automation: Main Next Integrate";
 
-async function dequeue(connectionString: string, queueName: string, batchSize: number) {
-    // const queueServiceClient = QueueServiceClient.fromConnectionString(connectionString);
-    // const queueClient = queueServiceClient.getQueueClient(queueName);
-    // // Peek at messages in the queue
-    // const peekedMessages = await queueClient.peekMessages({ numberOfMessages: batchSize });
-    // let firstMessage;
-    // for (let i = 0; i < peekedMessages.peekedMessageItems.length; i++) {
-    //     // Display the peeked message
-    //     console.log("Peeked message: ", peekedMessages.peekedMessageItems[i].messageText);
-    //     firstMessage = JSON.parse(peekedMessages.peekedMessageItems[i].messageText);
-    // }
-    // return firstMessage;
+async function listLabels(token: string) {
+    // const octokit = new Octokit({ auth: token });
+    // await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/labels', {
+    //     owner: owner,
+    //     repo: repo,
+    //     issue_number: 'ISSUE_NUMBER'
+    // })
 }
 
-async function removeQueue(connectionString: string, queueName: string, batchSize: number)  {
-    // const queueServiceClient = QueueServiceClient.fromConnectionString(connectionString);
-    // const queueClient = queueServiceClient.getQueueClient(queueName);
-    // // Get up to batch size messages from the queue
-    // const receivedMsgsResp = await queueClient.receiveMessages({ numberOfMessages: batchSize, visibilityTimeout: 5 * 60 });
-    // let message;
-    // for (let i = 0; i < receivedMsgsResp.receivedMessageItems.length; i++) {
-    //     message = receivedMsgsResp.receivedMessageItems[i];
-    //     console.log("Dequeuing message: ", message.messageText);
-    //     await queueClient.deleteMessage(message.messageId, message.popReceipt);
-    // }
+async function addLabel(token: string, issueNumber: number, labels: string[]) {
+    // const octokit = new Octokit({ auth: token });
+    // await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
+    //     owner: owner,
+    //     repo: repo,
+    //     issue_number: issue_number,
+    //     labels: labels,
+    // });
+    // labels: [
+    //     'main-next-integrate',
+    //     'do-not-squash-merge'
+    // ]
 }
 
 async function prExists(token?: string, title?: string): Promise<boolean> {
@@ -49,12 +43,10 @@ async function prExists(token?: string, title?: string): Promise<boolean> {
     // }
 
     return false;
-
 }
 
 async function prInfo(commitSha: string) {
     // const octokit = new Octokit({ auth: token })
-
     //   await octokit.request('GET /repos/{owner}/{repo}/commits/{commit_sha}/pulls', {
     //     owner: 'OWNER',
     //     repo: 'REPO',
@@ -62,18 +54,21 @@ async function prInfo(commitSha: string) {
     //   })
 }
 
-async function createPR(token: string, sha: string, sourceBranch: string, targetBranch: string, author: string, reviewers: string[]) {
+async function createPR(
+    token: string,
+    sha: string,
+    sourceBranch: string,
+    targetBranch: string,
+    author: string,
+    reviewers: string[],
+) {
     // const description = `
     //     ## Main-next integrate PR
-
     //     The aim of this pull request is to sync main and next branch. The expectation from the assignee is as follows:
-
     //     > - Acknowledge the pull request by adding a comment -- "Actively working on it".
     //     > - Resolve any merge conflicts between this branch and next (and push the resolution to this branch). Merge next into this branch if needed. **Do NOT rebase or squash this branch: its history must be preserved**.
     //     > - Ensure CI is passing for this PR, fixing any issues. Please don't look into resolving **Real service e2e test** and **Stress test** failures as they are **non-required** CI failures.
-
     //     For more information about how to resolve merge conflicts and CI failures, visit [this wiki page](https://github.com/microsoft/FluidFramework/wiki/Main-next-Automation).`;
-
     // const octokit = new Octokit({ auth: token })
     // const newPr = await octokit.request('POST /repos/{owner}/{repo}/pulls', {
     //     owner: owner,
@@ -83,14 +78,12 @@ async function createPR(token: string, sha: string, sourceBranch: string, target
     //     head: sourceBranch,
     //     base: targetBranch,
     // });
-
     // await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/assignees', {
     //     owner: owner,
     //     repo: repo,
     //     issue_number: newPr.data.number,
     //     assignees: [ author ]
     // });
-
     // await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/requested_reviewers', {
     //     owner: owner,
     //     repo: repo,
@@ -100,73 +93,65 @@ async function createPR(token: string, sha: string, sourceBranch: string, target
 }
 
 export default class Merge extends BaseCommand<typeof BaseCommand.flags> {
-  static description = 'describe the command here'
+    static description = "describe the command here";
 
-  static examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
+    static examples = ["<%= config.bin %> <%= command.id %>"];
 
-  static flags = {
-    source: Flags.string({
-        description: "",
-        default: "main",
-        required: false
-    }),
-    target: Flags.string({
-        description: "",
-        default: "next",
-        required: false
-    }),
-    owner: Flags.string({
-        description: "",
-        default: "microsoft",
-        required: false
-    }),
-    repo: Flags.string({
-        description: "",
-        default: "FluidFramework",
-        required: false
-    }),
-    batchSize: Flags.integer({
-        description: "",
-        default: 5,
-        required: false
-    }),
-    ...BaseCommand.flags,
-  }
+    static flags = {
+        githubToken: Flags.string({
+            description: "",
+            required: true,
+        }),
+        source: Flags.string({
+            description: "",
+            default: "main",
+            required: false,
+        }),
+        target: Flags.string({
+            description: "",
+            default: "next",
+            required: false,
+        }),
+        batchSize: Flags.integer({
+            description: "",
+            default: 1,
+            required: false,
+        }),
+        ...BaseCommand.flags,
+    };
 
-  static args = [{name: 'file'}]
+    static args = [{ name: "file" }];
 
-  public async run(): Promise<void> {
-    const {args, flags} = await this.parse(Merge);
+    public async run(): Promise<void> {
+        const { args, flags } = await this.parse(Merge);
 
-    // check if PR exists
-    if (await prExists()) {
-        this.exit(-1);
+        // check if PR exists
+        if (await prExists()) {
+            this.exit(-1);
+        }
+
+        const lastMergedCommit = await mergeBase(flags.source, flags.target);
+        const nextHead = await switchBranch(flags.target);
+        const unmergedCommits = await revList(lastMergedCommit, nextHead);
+        this.log("unmerged commit------", unmergedCommits);
+
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let i = 0; i < unmergedCommits.length; i++) {
+            // check if commits equal to specific bacth size exists
+        }
+
+        const commitInfo = await prInfo(unmergedCommits); // iterate and get the last commit
+        this.log("commit info----", commitInfo);
+
+        // create-branch
+        createBranch("branchName");
+
+        // pull-request
+        const pullRequest = await createPR("", "", "branchName", flags.target, "", []);
+
+        if (pullRequest === undefined) {
+            this.error("Unable to create pull request");
+            // notify the process owner
+        }
     }
-
-    const lastMergedCommit = await mergeBase(flags.source, flags.target);
-    const nextHead = await switchBranch('next');
-    const unmergedCommits = await revList(lastMergedCommit, nextHead);
-    this.log("unmerged commit------", unmergedCommits);
-
-    // eslint-disable-next-line @typescript-eslint/prefer-for-of
-    for (let i=0; i<unmergedCommits.length; i++) {
-        // check if commits equal to specific bacth size exists
-    }
-
-    const commitInfo = await prInfo(unmergedCommits); // iterate and get the last commit
-    this.log("commit info----", commitInfo);
-
-    // create-branch
-    createBranch("branchName");
-
-    // pull-request
-    const pullRequest = await createPR("", "", "branchName", flags.target, "", []);
-
-    if(pullRequest === undefined) {
-        this.error("Unable to create pull request");
-        // notify the process owner
-    }
-  }
 }
--- build-tools/packages/build-cli/test/commands/merge.test.ts
+++ build-tools/packages/build-cli/test/commands/merge.test.ts
@@ -1,17 +1,15 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from "@oclif/test";
 
-describe('merge', () => {
-  test
-  .stdout()
-  .command(['merge'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+describe("merge", () => {
+    test.stdout()
+        .command(["merge"])
+        .it("runs hello", (ctx) => {
+            expect(ctx.stdout).to.contain("hello world");
+        });
 
-  test
-  .stdout()
-  .command(['merge', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
-})
+    test.stdout()
+        .command(["merge", "--name", "jeff"])
+        .it("runs hello --name jeff", (ctx) => {
+            expect(ctx.stdout).to.contain("hello jeff");
+        });
+});
