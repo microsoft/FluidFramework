6101387feef70d94ab5a029b4ac0016be63874f3
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-08-08 20:39:07 +0000

    docs: Add comment

--- packages/tools/api-markdown-documenter/src/DocIdentifiableHeading.ts
+++ packages/tools/api-markdown-documenter/src/DocIdentifiableHeading.ts
@@ -1,11 +1,16 @@
-// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
-// See LICENSE in the project root for license information.
 import { DocHeading, IDocHeadingParameters } from "@microsoft/api-documenter/lib/nodes/DocHeading";
+
+// TODO: file issue on Rushstack github to support IDs in headings.
+// This type should be removed if such support is added natively.
 
 /**
  * Constructor parameters for {@link DocHeading}.
  */
 export interface IDocIdentifiableHeadingParameters extends IDocHeadingParameters {
+    /**
+     * Unique heading identifier. Can be used to uniquely identify a heading on a page for links, etc.
+     * If not specified, no ID will associated with the heading.
+     */
     id?: string;
 }
 
