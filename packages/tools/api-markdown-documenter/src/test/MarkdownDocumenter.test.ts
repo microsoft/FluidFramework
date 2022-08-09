6cf789cf1e2d45ebbfc26a019e5d2e11242e3b36
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-08-09 23:15:44 +0000

    test: Re-enable tests

--- packages/tools/api-markdown-documenter/src/test/MarkdownDocumenter.test.ts
+++ packages/tools/api-markdown-documenter/src/test/MarkdownDocumenter.test.ts
@@ -2,15 +2,12 @@ import { CustomMarkdownEmitter } from "@microsoft/api-documenter/lib/markdown/Cu
 import { ApiModel } from "@microsoft/api-extractor-model";
 import { FileSystem } from "@rushstack/node-core-library";
 import { expect } from "chai";
-// import { compare } from "dir-compare";
+import { compare } from "dir-compare";
 import * as Path from "path";
 
-// import { MarkdownDocument } from "../MarkdownDocument";
-// import { renderDocuments, renderFiles } from "../MarkdownDocumenter";
-import {
-    // MarkdownDocumenterConfiguration,
-    markdownDocumenterConfigurationWithDefaults,
-} from "../MarkdownDocumenterConfiguration";
+import { MarkdownDocument } from "../MarkdownDocument";
+import { renderDocuments, renderFiles } from "../MarkdownDocumenter";
+import { markdownDocumenterConfigurationWithDefaults } from "../MarkdownDocumenterConfiguration";
 import { renderModelPage, renderPackagePage } from "../Rendering";
 import { CustomDocNodes } from "../doc-nodes";
 
@@ -23,7 +20,7 @@ const testTempDirPath = Path.resolve(__dirname, "test_temp");
  * Snapshot directory to which generated test data will be copied.
  * Relative to dist/test.
  */
-// const snapshotsDirPath = Path.resolve(__dirname, "..", "..", "src", "test", "snapshots");
+const snapshotsDirPath = Path.resolve(__dirname, "..", "..", "src", "test", "snapshots");
 
 const documenterConfiguration = markdownDocumenterConfigurationWithDefaults({
     uriRoot: "",
@@ -33,8 +30,8 @@ const tsdocConfiguration = CustomDocNodes.configuration;
 
 describe("api-markdown-documenter simple suite tests", async () => {
     const apiReportPath = Path.resolve(__dirname, "test-data", "simple-suite-test.json");
-    // const outputDirPath = Path.resolve(testTempDirPath, "simple-suite-test");
-    // const snapshotDirPath = Path.resolve(snapshotsDirPath, "simple-suite-test");
+    const outputDirPath = Path.resolve(testTempDirPath, "simple-suite-test");
+    const snapshotDirPath = Path.resolve(snapshotsDirPath, "simple-suite-test");
 
     let apiModel: ApiModel;
     before(async () => {
@@ -69,55 +66,55 @@ describe("api-markdown-documenter simple suite tests", async () => {
         // TODO: snapshot
     });
 
-    // it("Ensure no duplicate file paths", () => {
-    //     const documents = renderDocuments(
-    //         apiModel!,
-    //         documenterConfiguration,
-    //         new CustomMarkdownEmitter(apiModel),
-    //     );
+    it("Ensure no duplicate file paths", () => {
+        const documents = renderDocuments(
+            apiModel!,
+            documenterConfiguration,
+            new CustomMarkdownEmitter(apiModel),
+        );
 
-    //     const pathMap = new Map<string, MarkdownDocument>();
-    //     for (const document of documents) {
-    //         if (pathMap.has(document.path)) {
-    //             expect.fail(
-    //                 `Rendering generated multiple documents to be rendered to the same file path: "${
-    //                     document.path
-    //                 }". Requested by the following items: "${document.apiItemName}" & "${
-    //                     pathMap.get(document.path)!.apiItemName
-    //                 }".`,
-    //             );
-    //         } else {
-    //             pathMap.set(document.path, document);
-    //         }
-    //     }
-    // });
+        const pathMap = new Map<string, MarkdownDocument>();
+        for (const document of documents) {
+            if (pathMap.has(document.path)) {
+                expect.fail(
+                    `Rendering generated multiple documents to be rendered to the same file path: "${
+                        document.path
+                    }". Requested by the following items: "${document.apiItemName}" & "${
+                        pathMap.get(document.path)!.apiItemName
+                    }".`,
+                );
+            } else {
+                pathMap.set(document.path, document);
+            }
+        }
+    });
 
-    // /**
-    //  * Simple integration test that validates complete output from simple test package
-    //  */
-    // it("Compare sample suite against expected", async () => {
-    //     await renderFiles(
-    //         apiModel!,
-    //         outputDirPath,
-    //         {
-    //             uriRoot: "",
-    //         },
-    //         new CustomMarkdownEmitter(apiModel),
-    //     );
+    /**
+     * Simple integration test that validates complete output from simple test package
+     */
+    it("Compare sample suite against expected", async () => {
+        await renderFiles(
+            apiModel!,
+            outputDirPath,
+            {
+                uriRoot: "",
+            },
+            new CustomMarkdownEmitter(apiModel),
+        );
 
-    //     // Verify against expected contents
-    //     const result = await compare(outputDirPath, snapshotDirPath, { compareContent: true });
+        // Verify against expected contents
+        const result = await compare(outputDirPath, snapshotDirPath, { compareContent: true });
 
-    //     if (!result.same) {
-    //         await FileSystem.ensureEmptyFolderAsync(snapshotDirPath);
-    //         await FileSystem.copyFilesAsync({
-    //             sourcePath: outputDirPath,
-    //             destinationPath: snapshotDirPath,
-    //         });
-    //     }
+        if (!result.same) {
+            await FileSystem.ensureEmptyFolderAsync(snapshotDirPath);
+            await FileSystem.copyFilesAsync({
+                sourcePath: outputDirPath,
+                destinationPath: snapshotDirPath,
+            });
+        }
 
-    //     // If this fails, then the docs build has generated new content.
-    //     // View the diff in git and determine if the changes are appropriate or not.
-    //     expect(result.same).to.be.true;
-    // });
+        // If this fails, then the docs build has generated new content.
+        // View the diff in git and determine if the changes are appropriate or not.
+        expect(result.same).to.be.true;
+    });
 });
