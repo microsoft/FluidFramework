1152e55ff7f980ba115fa0a0ab0a89ebf9509e2c
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-08-09 23:02:11 +0000

    remove: Unneeded helpers

--- packages/tools/api-markdown-documenter/src/utilities/DocNodeUtilities.ts
+++ packages/tools/api-markdown-documenter/src/utilities/DocNodeUtilities.ts
@@ -1,28 +1,5 @@
-import { DocNodeKind, DocParagraph, DocSection, TSDocConfiguration } from "@microsoft/tsdoc";
+import { DocSection, TSDocConfiguration } from "@microsoft/tsdoc";
 
-// TODO: remove
-export function appendSection(output: DocSection | DocParagraph, docSection: DocSection): void {
-    for (const node of docSection.nodes) {
-        output.appendNode(node);
-    }
-}
-
-// TODO: remove
-export function appendAndMergeSection(output: DocSection, docSection: DocSection): void {
-    let firstNode: boolean = true;
-    for (const node of docSection.nodes) {
-        if (firstNode && node.kind === DocNodeKind.Paragraph) {
-            output.appendNodesInParagraph(node.getChildNodes());
-            firstNode = false;
-            continue;
-        }
-        firstNode = false;
-
-        output.appendNode(node);
-    }
-}
-
-// TODO: remove?
 export function mergeSections(
     sections: DocSection[],
     tsdocConfiguration: TSDocConfiguration,
