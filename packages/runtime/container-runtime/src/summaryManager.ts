1373db91df7153b4417433cd8e8c08fee2897072
Nicholas Couri <nichoc@microsoft.com>
2022-08-10 15:29:49 -0700

    Be more specific as to when the last summary will be allowed to run. Targetting explicitly the documents with a high number of unsummarized ops.

--- packages/runtime/container-runtime/src/summaryManager.ts
+++ packages/runtime/container-runtime/src/summaryManager.ts
@@ -222,7 +222,10 @@ export class SummaryManager implements IDisposable {
             // the summarizer at all.
             const shouldSummarizeState = this.getShouldSummarizeState();
             if (shouldSummarizeState.shouldSummarize === false) {
-                if (!Summarizer.stopReasonCanRunLastSummary(shouldSummarizeState.stopReason)) {
+                // In order to allow the last summary to run, we need not only a stop reason that would
+                // allow it but also, startWithDelay to be false (start the summarization immediately),
+                // which would happen when we have a high enough number of unsummarized ops.
+                if (startWithInitialDelay || !Summarizer.stopReasonCanRunLastSummary(shouldSummarizeState.stopReason)) {
                     this.state = SummaryManagerState.Starting;
                     summarizer.stop(shouldSummarizeState.stopReason);
                     return `early exit after starting summarizer ${shouldSummarizeState.stopReason}`;
