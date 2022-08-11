6eb2d917a2de00c2cab21bbf8c936c37de5ed848
Jenn <jenn.thakugan@icloud.com>
2022-08-11 20:47:27 +0000

    Add suite

---
+++ packages/dds/tree/src/test/forest/editableForest.spec.ts
@@ -0,0 +1,12 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { IEditableForest } from "../../forest";
+
+function runEditableForestTestSuite(testSuiteName: string, forestFactory: () => IEditableForest) {
+    describe(testSuiteName, () => {
+
+    })
+}
