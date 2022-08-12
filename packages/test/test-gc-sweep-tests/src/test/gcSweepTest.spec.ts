aeb6d7fc200f4acd862536a93e09ce9c23d491b0
tyler-cai-microsoft <tylercai@microsoft.com>
2022-08-12 13:42:19 -0700

    Add missing spaces for policy check

--- packages/test/test-gc-sweep-tests/src/test/gcSweepTest.spec.ts
+++ packages/test/test-gc-sweep-tests/src/test/gcSweepTest.spec.ts
@@ -1,7 +1,7 @@
 /*!
-* Copyright (c) Microsoft Corporation and contributors. All rights reserved.
-* Licensed under the MIT License.
-*/
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
 
 /* eslint-disable max-len */
 import { strict as assert } from "assert";
