fea6ab7c525cfde35b4083aeaf5334ee404e8f54
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-08-12 17:47:14 +0000

    docs: Space region comments

--- packages/tools/api-markdown-documenter/src/doc-nodes/index.ts
+++ packages/tools/api-markdown-documenter/src/doc-nodes/index.ts
@@ -2,9 +2,11 @@ export * from "./DocHeading";
 export * from "./CustomDocNodeKind";
 
 // #region Convenience re-exports of Doc builder types from api-documenter
+
 export * from "@microsoft/api-documenter/lib/nodes/DocEmphasisSpan";
 export * from "@microsoft/api-documenter/lib/nodes/DocNoteBox";
 export * from "@microsoft/api-documenter/lib/nodes/DocTable";
 export * from "@microsoft/api-documenter/lib/nodes/DocTableCell";
 export * from "@microsoft/api-documenter/lib/nodes/DocTableRow";
+
 // #endregion
