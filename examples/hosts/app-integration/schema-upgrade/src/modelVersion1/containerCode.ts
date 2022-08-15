26919c2622926d68a913d1bbc675df65ea19926a
Matt Rakow <ChumpChief@users.noreply.github.com>
2022-08-15 08:59:11 -0700

    Rename ContainerKillBit to MigrationTool and eliminate its usage of code details concept (#11529)

--- examples/hosts/app-integration/schema-upgrade/src/modelVersion1/containerCode.ts
+++ examples/hosts/app-integration/schema-upgrade/src/modelVersion1/containerCode.ts
@@ -9,14 +9,14 @@ import { IContainerRuntime } from "@fluidframework/container-runtime-definitions
 import { rootDataStoreRequestHandler } from "@fluidframework/request-handler";
 import { requestFluidObject } from "@fluidframework/runtime-utils";
 
-import { ContainerKillBitInstantiationFactory, IContainerKillBit } from "../containerKillBit";
+import { MigrationToolInstantiationFactory, IMigrationTool } from "../migrationTool";
 import { IInventoryList, IInventoryListContainer } from "../modelInterfaces";
 import { makeModelRequestHandler, ModelMakerCallback } from "../modelLoading";
 import { InventoryListContainer } from "./containerModel";
 import { InventoryListInstantiationFactory } from "./inventoryList";
 
 export const inventoryListId = "default-inventory-list";
-export const containerKillBitId = "container-kill-bit";
+export const migrationToolId = "migration-tool";
 
 const makeInventoryListModel: ModelMakerCallback<IInventoryListContainer> =
     async (runtime: IContainerRuntime, container: IContainer) => {
@@ -24,11 +24,11 @@ const makeInventoryListModel: ModelMakerCallback<IInventoryListContainer> =
             await runtime.getRootDataStore(inventoryListId),
             "",
         );
-        const containerKillBit = await requestFluidObject<IContainerKillBit>(
-            await runtime.getRootDataStore(containerKillBitId),
+        const migrationTool = await requestFluidObject<IMigrationTool>(
+            await runtime.getRootDataStore(migrationToolId),
             "",
         );
-        return new InventoryListContainer(inventoryList, containerKillBit, container);
+        return new InventoryListContainer(inventoryList, migrationTool, container);
     };
 
 export class InventoryListContainerRuntimeFactory extends BaseContainerRuntimeFactory {
@@ -36,7 +36,7 @@ export class InventoryListContainerRuntimeFactory extends BaseContainerRuntimeFa
         super(
             new Map([
                 InventoryListInstantiationFactory.registryEntry,
-                ContainerKillBitInstantiationFactory.registryEntry,
+                MigrationToolInstantiationFactory.registryEntry,
             ]), // registryEntries
             undefined,
             [
@@ -52,14 +52,15 @@ export class InventoryListContainerRuntimeFactory extends BaseContainerRuntimeFa
     protected async containerInitializingFirstTime(runtime: IContainerRuntime) {
         const inventoryList = await runtime.createDataStore(InventoryListInstantiationFactory.type);
         await inventoryList.trySetAlias(inventoryListId);
-        const containerKillBit = await runtime.createDataStore(ContainerKillBitInstantiationFactory.type);
-        await containerKillBit.trySetAlias(containerKillBitId);
+        const migrationTool = await runtime.createDataStore(MigrationToolInstantiationFactory.type);
+        await migrationTool.trySetAlias(migrationToolId);
     }
 
     protected async containerHasInitialized(runtime: IContainerRuntime): Promise<void> {
         console.info("Using runtime factory version one");
-        // Force the killbit to instantiate in all cases.  The Quorum it uses must be loaded and running in order to
-        // respond with accept ops, and without this call the killbit won't be instantiated on the summarizer client.
-        await requestFluidObject(await runtime.getRootDataStore(containerKillBitId), "");
+        // Force the MigrationTool to instantiate in all cases.  The Quorum it uses must be loaded and running in
+        // order to respond with accept ops, and without this call the MigrationTool won't be instantiated on the
+        // summarizer client.
+        await requestFluidObject(await runtime.getRootDataStore(migrationToolId), "");
     }
 }
