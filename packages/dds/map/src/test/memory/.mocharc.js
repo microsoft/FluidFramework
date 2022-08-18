af32ee3b750a6e1343031b0fccdbd12d5de8c1e6
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-08-17 19:48:57 -0500

    Reapply path fix

--- packages/dds/map/src/test/memory/.mocharc.js
+++ packages/dds/map/src/test/memory/.mocharc.js
@@ -12,7 +12,7 @@ module.exports = {
     fgrep: "@Benchmark",
     "node-option": ["expose-gc", "gc-global", "unhandled-rejections=strict"], // without leading "--"
     recursive: true,
-    reporter: "node_modules/@fluid-tools/benchmark/dist/MochaMemoryTestReporter.js", // Lerna hoists the external dependency on @fluid-tools/benchmark to the root
+    reporter: "../../../node_modules/@fluid-tools/benchmark/dist/MochaMemoryTestReporter.js", // Lerna hoists the external dependency on @fluid-tools/benchmark to the root
     reporterOptions: ["reportDir=.memoryTestsOutput/"],
     require: ["@fluidframework/mocha-test-setup"],
     spec: ["dist/test/memory/**/*.spec.js", "--perfMode"],
