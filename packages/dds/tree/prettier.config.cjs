a35a3ed039f38db0a4c3de80d610e5c14fc884be
daesun-park <108361046+daesun-park@users.noreply.github.com>
2022-08-22 16:29:04 +0000

    add prettier

---
+++ packages/dds/tree/prettier.config.cjs
@@ -0,0 +1,40 @@
+// TODO: Use shared config in build-common once version with config has been published.
+module.exports = {
+    printWidth: 100,
+    quoteProps: "consistent",
+    semi: true,
+    singleQuote: false,
+    trailingComma: "all",
+    overrides: [
+        {
+            files: "lerna.json",
+            options: {
+                printWidth: 50,
+                tabWidth: 2,
+            },
+        },
+        {
+            files: "tsconfig*.json",
+            options: {
+                parser: "json5",
+                tabWidth: 2,
+                trailingComma: "all",
+                quoteProps: "preserve",
+            },
+        },
+        {
+            files: "*.json",
+            options: {
+                tabWidth: 2,
+                trailingComma: "all",
+                quoteProps: "preserve",
+            },
+        },
+        {
+            files: "*.{js,ts}",
+            options: {
+                useTabs: true,
+            },
+        },
+    ],
+};
