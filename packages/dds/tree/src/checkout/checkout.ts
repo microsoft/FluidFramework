09aee0c584143c020916a84643dddc1527ecfcc2
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2022-08-24 14:22:55 -0700

    Tree: cleanup and use checkout (#11650)

--- packages/dds/tree/src/checkout/checkout.ts
+++ packages/dds/tree/src/checkout/checkout.ts
@@ -3,11 +3,16 @@
  * Licensed under the MIT License.
  */
 
-import { IForestSubscription, IEditableForest } from "../forest";
+import { IForestSubscription } from "../forest";
 
-// TODO: this whole module is very unfinished.
+export interface ICheckout<TEditBuilder> {
+    /**
+     * Current contents.
+     * Updated by edits (local and remote).
+     * Use `runTransaction` to create a local edit.
+     */
+    readonly forest: IForestSubscription;
 
-export interface ICheckout extends IForestSubscription {
     /**
      * Run `transaction` to edit this forest.
      * While `transaction` is running, its intermediate states will be visible on the IForestSubscription.
@@ -17,5 +22,11 @@ export interface ICheckout extends IForestSubscription {
      * TODO: support nesting (perhaps via "commands"),
      * and do this in a way where there is control over which ones intermediate versions are displayed.
      */
-    runTransaction<T>(transaction: (forest: IEditableForest) => T): T;
+    runTransaction(
+        transaction: (forest: IForestSubscription, editor: TEditBuilder) => TransactionResult): TransactionResult;
+}
+
+export enum TransactionResult {
+    Abort,
+    Apply,
 }
