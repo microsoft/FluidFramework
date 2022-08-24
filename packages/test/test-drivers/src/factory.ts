0edc076d136bfb7305b5c3e87f59aef7b3f05554
Kajari Ghosh <ghoshkaj@gmail.com>
2022-08-24 16:18:23 -0400

    Revert "use agentkeepalive for client calls as well"
    This reverts commit 2e73072262ac35680c74f89b7a8133b50ade8b6b.

--- packages/test/test-drivers/src/factory.ts
+++ packages/test/test-drivers/src/factory.ts
@@ -4,7 +4,6 @@
  */
 
 import http from "http";
-import Agent from "agentkeepalive";
 import { TestDriverTypes } from "@fluidframework/test-driver-definitions";
 import { unreachableCase } from "@fluidframework/common-utils";
 import { LocalServerTestDriver } from "./localServerTestDriver";
@@ -37,7 +36,7 @@ function patchHttpRequestToForceKeepAlive() {
 
     httpRequestPatched = true;
 
-    const httpAgent = new Agent({ keepAlive: true, freeSocketTimeout: 4000 });
+    const httpAgent = new http.Agent({ keepAlive: true, scheduling: "fifo" });
     const oldRequest = http.request;
     http.request = ((url, options, callback) => {
         // There are two variant of the API
