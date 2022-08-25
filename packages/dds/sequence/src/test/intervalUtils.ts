1141235a731dcf6b52099547d994e0297ad119df
Connor Skees <connorskees@microsoft.com>
2022-08-25 13:55:24 -0400

    add copyright header

--- packages/dds/sequence/src/test/intervalUtils.ts
+++ packages/dds/sequence/src/test/intervalUtils.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { strict as assert } from "assert";
 import { MockContainerRuntimeForReconnection } from "@fluidframework/test-runtime-utils";
 import { SharedString } from "../sharedString";
