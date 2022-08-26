f433c0ea926942419bcc63106ef7fba91c78b842
Tyler Butler <tyler@tylerbutler.com>
2022-08-26 19:10:07 +0000

    policy

--- build-tools/packages/build-cli/test/commands/generate/buildVersion.test.ts
+++ build-tools/packages/build-cli/test/commands/generate/buildVersion.test.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { expect, test } from "@oclif/test";
 
 const test_tags = [
