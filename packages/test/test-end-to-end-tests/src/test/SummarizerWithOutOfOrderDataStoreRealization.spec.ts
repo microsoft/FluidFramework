35773d66e25e8ab56751a5271092e8f77aac1d65
Nicholas Couri <nichoc@microsoft.com>
2022-08-29 14:12:58 -0700

    addressing review comments

--- packages/test/test-end-to-end-tests/src/test/SummarizerWithOutOfOrderDataStoreRealization.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/SummarizerWithOutOfOrderDataStoreRealization.spec.ts
@@ -198,9 +198,9 @@ function createDataStoreRuntime(factory: typeof FluidDataStoreRuntime = FluidDat
 }
 
 /**
- * Validates whether or not a GC Tree Summary Handle should be written to the summary.
+ * Validates the scenario in which, during summarization, a data store is loaded out of order.
  */
-describeNoCompat("Summary with Search Blobs and DataStore realization during Summarization",
+describeNoCompat("Summary where data store is loaded out of order",
  (getTestObjectProvider) => {
     let provider: ITestObjectProvider;
     let mainContainer: IContainer;
@@ -217,62 +217,60 @@ describeNoCompat("Summary with Search Blobs and DataStore realization during Sum
         return summaryResult.summaryVersion;
     }
 
-    describe("Realize DataStore during Search and make sure pendingSummaries does not get corrupted", () => {
-        beforeEach(async () => {
-            provider = getTestObjectProvider({ syncSummarizer: true });
-            mainContainer = await createContainer();
-            // Set an initial key. The Container is in read-only mode so the first op it sends will get nack'd and is
-            // re-sent. Do it here so that the extra events don't mess with rest of the test.
-            mainDataStore = await requestFluidObject<TestDataObject1>(mainContainer, "default");
-            mainDataStore._root.set("anytest", "anyvalue");
-            await waitForContainerConnection(mainContainer);
-        });
+    beforeEach(async () => {
+        provider = getTestObjectProvider({ syncSummarizer: true });
+        mainContainer = await createContainer();
+        // Set an initial key. The Container is in read-only mode so the first op it sends will get nack'd and is
+        // re-sent. Do it here so that the extra events don't mess with rest of the test.
+        mainDataStore = await requestFluidObject<TestDataObject1>(mainContainer, "default");
+        mainDataStore._root.set("anytest", "anyvalue");
+        await waitForContainerConnection(mainContainer);
+    });
 
-        it("No Summary Upload Error when DS gets realized between summarize and completeSummary", async () => {
-            const summarizerClient = await createSummarizer(provider, mainContainer);
-            await provider.ensureSynchronized();
-            mainDataStore.matrix.setCell(0, 0, "value");
+    it("No Summary Upload Error when DS gets realized between summarize and completeSummary", async () => {
+        const summarizerClient = await createSummarizer(provider, mainContainer);
+        await provider.ensureSynchronized();
+        mainDataStore.matrix.setCell(0, 0, "value");
 
-            // Here are the steps that would cause bug 1633 to repro:
-            // Additional information: https://dev.azure.com/fluidframework/internal/_workitems/edit/1633
-            // 1) Summary starts
-            // 2) The summarize method from the DataStore2 (TestDataObject2) will be executed but, as it has not
-            //    been realized, it has no child nodes and hasn't changed, we will use a handle instead.
-            // 3) During the summarization from the other DataStore1 (TestDataObject1),
-            // due to the mixinSummaryHandler (search) we explicitly realize the DataStore2 and
-            // new Summarizer Nodes are added to it.
-            // 4) That would (without the fix) corrupt the pendingSummaries/lastSummary from one of the child nodes.
-            // 5) Next Summarization starts, the lastSummary data would be used to upload the summary and we
-            //  would get an error
-            // "Cannot locate node with path '.app/.channels/guid1/root' under '<handle>'."
-            //  instead of .app/.channels/guid1/.channels/root
-            // Note: In this scenario, the corruption is caused due to the fact that the datastore's
-            // summarizer node does not update the handle paths with ".channels" for its children when it is
-            // summarized. This happens later when the data store is realized but its too late because
-            // the work-in-progress path (wipLocalPath) has already been updated.
+        // Here are the steps that would cause bug to repro:
+        // Additional info: https://github.com/microsoft/FluidFramework/pull/11697
+        // 1) Summary starts
+        // 2) The summarize method from the DataStore2 (TestDataObject2) will be executed but, as it has not
+        //    been realized, it has no child nodes and hasn't changed, we will use a handle instead.
+        // 3) During the summarization from the other DataStore1 (TestDataObject1),
+        // due to the mixinSummaryHandler (search) we explicitly realize the DataStore2 and
+        // new Summarizer Nodes are added to it.
+        // 4) That would (without the fix) corrupt the pendingSummaries/lastSummary from one of the child nodes.
+        // 5) Next Summarization starts, the lastSummary data would be used to upload the summary and we
+        //  would get an error
+        // "Cannot locate node with path '.app/.channels/guid1/root' under '<handle>'."
+        //  instead of .app/.channels/guid1/.channels/root
+        // Note: In this scenario, the corruption is caused due to the fact that the datastore's
+        // summarizer node does not update the handle paths with ".channels" for its children when it is
+        // summarized. This happens later when the data store is realized but its too late because
+        // the work-in-progress path (wipLocalPath) has already been updated.
 
-            const summaryVersion = await waitForSummary(summarizerClient);
-            assert(summaryVersion, "Summary version should be defined");
+        const summaryVersion = await waitForSummary(summarizerClient);
+        assert(summaryVersion, "Summary version should be defined");
 
-            mainDataStore.matrix.setCell(0, 0, "value1");
-            // The new summarization would immediately trigger bug 1633.
-            const summaryVersion1 = await waitForSummary(summarizerClient);
-            assert(summaryVersion1, "Summary version should be defined");
+        mainDataStore.matrix.setCell(0, 0, "value1");
+        // The new summarization would immediately trigger bug 1633.
+        const summaryVersion1 = await waitForSummary(summarizerClient);
+        assert(summaryVersion1, "Summary version should be defined");
 
-            // Make sure the next summarization succeeds.
-            mainDataStore.matrix.setCell(0, 0, "value1");
-            const summaryVersion2 = await waitForSummary(summarizerClient);
-            assert(summaryVersion2, "Summary version should be defined");
+        // Make sure the next summarization succeeds.
+        mainDataStore.matrix.setCell(0, 0, "value1");
+        const summaryVersion2 = await waitForSummary(summarizerClient);
+        assert(summaryVersion2, "Summary version should be defined");
 
-            summarizerClient.close();
+        summarizerClient.close();
 
-            // Just make sure new summarizer will be able to load and execute successfully.
-            const summarizerClient2 = await createSummarizer(provider, mainContainer,
-                summaryVersion2);
+        // Just make sure new summarizer will be able to load and execute successfully.
+        const summarizerClient2 = await createSummarizer(provider, mainContainer,
+            summaryVersion2);
 
-            mainDataStore.matrix.setCell(0, 0, "value2");
-            const summaryVersion3 = await waitForSummary(summarizerClient2);
-            assert(summaryVersion3, "Summary version should be defined");
-        });
+        mainDataStore.matrix.setCell(0, 0, "value2");
+        const summaryVersion3 = await waitForSummary(summarizerClient2);
+        assert(summaryVersion3, "Summary version should be defined");
     });
 });
