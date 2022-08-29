eef2049a689bbe1ec12a299098653c335abaed9d
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-08-29 18:39:45 +0000

    docs: Update package documentation

--- packages/framework/fluid-framework/src/index.ts
+++ packages/framework/fluid-framework/src/index.ts
@@ -4,9 +4,35 @@
  */
 
 /**
- * The **fluid-framework** package bundles a collection of Fluid Framework client packages for easy use when paired with
- * a corresponding service client package (for example, `\@fluidframework/azure-client` or
- * `\@fluidframework/tinylicious-client`).
+ * The `fluid-framework` package is the primary entry-point into the Fluid client ecosystem when paired with
+ * a corresponding service client package (for example, `@fluidframework/azure-client` or
+ * `@fluidframework/tinylicious-client`).
+ *
+ * It contains necessary types and functionality for creating and loading
+ * {@link https://fluidframework.com/docs/build/containers/ | Containers}.
+ *
+ * It also includes a few {@link https://fluidframework.com/docs/build/dds/ | Distributed Data Structures (DDSes)}
+ * to get you started using Fluid. These include:
+ *
+ * - {@link @fluidframework/map#SharedMap}
+ *
+ * - {@link @fluidframework/map#SharedDirectory}
+ *
+ * - {@link @fluidframework/sequence#SharedString}
+ *
+ * Other DDSes and related libraries are published separately.
+ *
+ * @remarks This package is implemented by re-exporting contents from the following library packages:
+ *
+ * - `@fluidframwork/container-definitions`
+ *
+ * - `@fluidframwork/container-loader`
+ *
+ * - `@fluidframwork/fluid-static`
+ *
+ * - `@fluidframwork/map`
+ *
+ * - `@fluidframwork/sequence`
  *
  * @packageDocumentation
  */
