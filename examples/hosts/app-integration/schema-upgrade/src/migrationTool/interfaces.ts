e5cae64f1d9169ea47167b0b89af532496ac6506
Matt Rakow <marakow@microsoft.com>
2022-08-30 07:33:46 -0700

    Add comments for migrationTool interfaces

--- examples/hosts/app-integration/schema-upgrade/src/migrationTool/interfaces.ts
+++ examples/hosts/app-integration/schema-upgrade/src/migrationTool/interfaces.ts
@@ -10,12 +10,45 @@ export interface IMigrationToolEvents extends IEvent {
 }
 
 export interface IMigrationTool extends IEventProvider<IMigrationToolEvents> {
+    /**
+     * Whether the migration has fully completed.
+     */
     migrated: boolean;
+
+    /**
+     * The container id where the migrated content can be found, if the migration has fully completed.
+     */
     newContainerId: string | undefined;
+    /**
+     * Set the container id where the migrated content can be found.
+     * @param id - the container id
+     */
     setNewContainerId(id: string): Promise<void>;
+
+    /**
+     * The version string of the proposed new version to use, if one has been proposed.
+     */
     proposedVersion: string | undefined;
+    /**
+     * The version string of the accepted new version to use, if one has been accepted.
+     */
     acceptedVersion: string | undefined;
+    /**
+     * Propose a new version to use.
+     * @param newVersion - the version string
+     * @returns A promnise which resolves when a version has been accepted, which may or may not be the version
+     * proposed by the local client.
+     */
     proposeVersion(newVersion: string): Promise<void>;
+
+    /**
+     * Volunteer to perform the migration.
+     * @returns A promise which resolves when the local client has been selected to perform the migration.  The
+     * migration may have already been completed prior to being selected.
+     */
     volunteerForMigration(): Promise<void>;
+    /**
+     * Whether the local client is selected to perform the migration.
+     */
     haveMigrationTask(): boolean;
 }
