929e77eb39dbf3e2883cc17439fefd017e6de98d
RishhiB <107130183+RishhiB@users.noreply.github.com>
2022-08-31 16:42:33 +0000

    formatting

--- build-tools/packages/build-tools/src/repoPolicyCheck/handlers/index.ts
+++ build-tools/packages/build-tools/src/repoPolicyCheck/handlers/index.ts
@@ -2,18 +2,18 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-import { handler as assertShortCodeHandler} from "./assertShortCode"
-import { handlers as copyrightFileHeaderHandlers} from "./copyrightFileHeader"
-import { handler as dockerfilePackageHandler} from "./dockerfilePackages"
-import { handler as fluidCaseHandler} from "./fluidCase"
-import { handlers as lockfilesHandlers} from "./lockfiles"
-import { handlers as npmPackageContentsHandlers} from "./npmPackages"
-import { Handler } from "../common"
+import { handler as assertShortCodeHandler } from "./assertShortCode";
+import { handlers as copyrightFileHeaderHandlers } from "./copyrightFileHeader";
+import { handler as dockerfilePackageHandler } from "./dockerfilePackages";
+import { handler as fluidCaseHandler } from "./fluidCase";
+import { handlers as lockfilesHandlers } from "./lockfiles";
+import { handlers as npmPackageContentsHandlers } from "./npmPackages";
+import { Handler } from "../common";
 
 /**
  * declared file handlers
  */
- export const policyHandlers: Handler[] = [
+export const policyHandlers: Handler[] = [
     ...copyrightFileHeaderHandlers,
     ...npmPackageContentsHandlers,
     dockerfilePackageHandler,
