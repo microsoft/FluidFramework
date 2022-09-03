357f9efd4dd06d31585d6030bb9b74c6643a276a
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-09-03 00:20:37 +0000

    remove: Duplicated files

--- docs/md-magic-website.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
-const pathLib = require("path");
-const mdMagic = require("./md-magic");
-
-mdMagic(pathLib.resolve(__dirname), "**/*.md");
