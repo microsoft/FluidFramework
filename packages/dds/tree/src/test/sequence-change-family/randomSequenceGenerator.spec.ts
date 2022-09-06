3de00ed187da8196f12d47a474561fee9e40ef58
daesun-park <108361046+daesun-park@users.noreply.github.com>
2022-09-06 04:23:52 +0000

    update test to use set of parent keys

--- packages/dds/tree/src/test/sequence-change-family/randomSequenceGenerator.spec.ts
+++ packages/dds/tree/src/test/sequence-change-family/randomSequenceGenerator.spec.ts
@@ -12,13 +12,13 @@ const testSeed = 432167897;
 describe("Test randomSequenceBuilder", () => {
     it("consistency of generateRandomUpPaths with same seed.", () => {
         const fooKey = brand<FieldKey>("foo");
-        const upPaths1 = generateRandomUpPaths(fooKey, testSeed, 10);
-        const upPaths2 = generateRandomUpPaths(fooKey, testSeed, 10);
+        const upPaths1 = generateRandomUpPaths(new Set([fooKey]), testSeed, 10);
+        const upPaths2 = generateRandomUpPaths(new Set([fooKey]), testSeed, 10);
         assert.deepStrictEqual(upPaths1, upPaths2);
     });
     it("consistency of the generateRandomChange with the same seed.", () => {
         const fooKey = brand<FieldKey>("foo");
-        const upPaths = generateRandomUpPaths(fooKey, testSeed, 10);
+        const upPaths = generateRandomUpPaths(new Set([fooKey]), testSeed, 10);
 
         const change1 = generateRandomChange(upPaths, testSeed);
         const change2 = generateRandomChange(upPaths, testSeed);
