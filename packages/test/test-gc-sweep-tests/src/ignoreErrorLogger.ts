bc307dee3b38cfc530d9cbb448556e7de1bec888
tyler-cai-microsoft <tylercai@microsoft.com>
2022-09-06 12:13:09 -0700

    Remove unused ignoreExpectedEventTypes

--- packages/test/test-gc-sweep-tests/src/ignoreErrorLogger.ts
+++ packages/test/test-gc-sweep-tests/src/ignoreErrorLogger.ts
@@ -4,24 +4,17 @@
  */
 
 import { EventAndErrorTrackingLogger } from "@fluidframework/test-utils";
-import { ITelemetryBaseEvent, ITelemetryGenericEvent } from "@fluidframework/common-definitions";
+import { ITelemetryBaseEvent } from "@fluidframework/common-definitions";
 
 /**
  * Ignores certain error types (does not pay attention to count)
  * Potentially, we may want to raise the severity of telemetry as an error. i.e - inactiveObjectX telemetry
  */
 export class IgnoreErrorLogger extends EventAndErrorTrackingLogger {
-    private readonly ignoredEvents: Map<string, ITelemetryGenericEvent> = new Map();
     public readonly events: ITelemetryBaseEvent[] = [];
     public readonly inactiveObjectEvents: ITelemetryBaseEvent[] = [];
     public readonly errorEvents: ITelemetryBaseEvent[] = [];
     public readonly errorEventStats: { [key: string]: number; } = {};
-
-    public ignoreExpectedEventTypes(...anyIgnoredEvents: ITelemetryGenericEvent[]) {
-        for (const event of anyIgnoredEvents) {
-            this.ignoredEvents.set(event.eventName, event);
-        }
-    }
 
     send(event: ITelemetryBaseEvent): void {
         this.events.push(event);
