341dbc5f7dfd29c91511ae37e6cf660236725f7c
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-09-06 20:41:15 +0000

    docs: Fix copyright notice

--- tools/api-markdown-documenter/src/doc-nodes/DocAlert.ts
+++ tools/api-markdown-documenter/src/doc-nodes/DocAlert.ts
@@ -1,5 +1,7 @@
-// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
-// See LICENSE in the project root for license information.
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
 import { DocNode, IDocNodeParameters } from "@microsoft/tsdoc";
 
 import { CustomDocNodeKind } from "./CustomDocNodeKind";
