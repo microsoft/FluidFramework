444844fc174a225fcda9c1d65d54c8384a7517ed
daesun-park <108361046+daesun-park@users.noreply.github.com>
2022-09-07 16:46:29 +0000

    Test case showing random combinations generated

--- packages/dds/tree/src/test/rebase/generateFuzzyCombinedChange.spec.ts
+++ packages/dds/tree/src/test/rebase/generateFuzzyCombinedChange.spec.ts
@@ -39,4 +39,29 @@ describe("generateFuzzyCombinedChange", () => {
         );
         assert.deepStrictEqual(change1, change2);
     });
+
+    it("generates random combination of changes", () => {
+        const change1 = generateFuzzyCombinedChange(
+            testRebaser,
+            generateRandomChange,
+            testSeed,
+            15,
+        );
+        const expected = {
+            C: {
+                C: {
+                    C: [
+                        {
+                            I: 432167897,
+                        },
+                        0.9933325823949589,
+                    ],
+                    O: 0.6981920829135596,
+                },
+                O: 0.013476674234232933,
+            },
+            O: 0.9375291806987182,
+        };
+        assert.deepStrictEqual(change1, expected);
+    });
 });
