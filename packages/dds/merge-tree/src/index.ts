4471797dd835d665bc0866f0950b188403ca7b8a
Kajari Ghosh <ghoshkaj@gmail.com>
2022-09-08 11:39:04 -0400

    named exports for merge-tree

--- packages/dds/merge-tree/src/index.ts
+++ packages/dds/merge-tree/src/index.ts
@@ -3,8 +3,8 @@
  * Licensed under the MIT License.
  */
 
-export * from "./base";
-export * from "./client";
+export { IIntegerRange } from "./base";
+export { Client } from "./client";
 
 export 	{
     ConflictAction,
@@ -22,20 +22,139 @@ export 	{
     SortedDictionary,
     Stack,
 } from "./collections";
-export * from "./constants";
+export {
+    UniversalSequenceNumber,
+    UnassignedSequenceNumber,
+    TreeMaintenanceSequenceNumber,
+    LocalClientId,
+    NonCollabClient,
+} from "./constants";
 export {
     createDetachedLocalReferencePosition,
     LocalReferencePosition,
     LocalReferenceCollection,
  } from "./localReference";
-export * from "./mergeTreeNodes";
-export * from "./mergeTreeDeltaCallback";
-export * from "./mergeTreeTracking";
-export * from "./opBuilder";
-export * from "./ops";
-export * from "./properties";
-export * from "./segmentGroupCollection";
-export * from "./segmentPropertiesManager";
-export * from "./sortedSegmentSet";
-export * from "./textSegment";
-export * from "./referencePositions";
+export {
+    IMergeNodeCommon,
+    IMergeNode,
+    IMergeBlock,
+    IHierBlock,
+    IRemovalInfo,
+    toRemovalInfo,
+    ISegment,
+    IMarkerModifiedAction,
+    ISegmentAction,
+    ISegmentChanges,
+    BlockAction,
+    NodeAction,
+    IncrementalSegmentAction,
+    IncrementalBlockAction,
+    BlockUpdateActions,
+    InsertContext,
+    SegmentActions,
+    IncrementalSegmentActions,
+    SearchResult,
+    MergeTreeStats,
+    SegmentGroup,
+    MergeNode,
+    ordinalToArray,
+    MaxNodesInBlock,
+    MergeBlock,
+    BaseSegment,
+    reservedMarkerIdKey,
+    reservedMarkerSimpleTypeKey,
+    IJSONMarkerSegment,
+    Marker,
+    IncrementalExecOp,
+    IncrementalMapState,
+    CollaborationWindow,
+    compareNumbers,
+    compareStrings,
+    internedSpaces,
+    IConsensusInfo,
+    SegmentAccumulator,
+    MinListener,
+    debugMarkerToString,
+} from "./mergeTreeNodes";
+export {
+    MergeTreeDeltaOperationType,
+    MergeTreeDeltaOperationTypes,
+    IMergeTreeDeltaCallbackArgs,
+    MergeTreeMaintenanceType,
+    IMergeTreeSegmentDelta,
+    IMergeTreeDeltaOpArgs,
+    IMergeTreeClientSequenceArgs,
+    MergeTreeDeltaCallback,
+    IMergeTreeMaintenanceCallbackArgs,
+    MergeTreeMaintenanceCallback,
+} from "./mergeTreeDeltaCallback";
+export {
+    TrackingGroup,
+    TrackingGroupCollection,
+} from "./mergeTreeTracking";
+export {
+    createAnnotateMarkerOp,
+    createAnnotateRangeOp,
+    createRemoveRangeOp,
+    createInsertSegmentOp,
+    createInsertOp,
+    createGroupOp,
+} from "./opBuilder";
+export {
+    ReferenceType,
+    IMarkerDef,
+    MergeTreeDeltaType,
+    IMergeTreeDelta,
+    IRelativePosition,
+    IMergeTreeInsertMsg,
+    IMergeTreeRemoveMsg,
+    ICombiningOp,
+    IMergeTreeAnnotateMsg,
+    IMergeTreeGroupMsg,
+    IJSONSegment,
+    IMergeTreeDeltaOp,
+    IMergeTreeOp,
+} from "./ops";
+export {
+    MapLike,
+    PropertySet,
+    IConsensusValue,
+    combine,
+    matchProperties,
+    extend,
+    clone,
+    addProperties,
+    extendIfUndefined,
+    createMap,
+} from "./properties";
+export { SegmentGroupCollection } from "./segmentGroupCollection";
+export {
+    PropertiesRollback,
+    PropertiesManager,
+} from "./segmentPropertiesManager";
+export {
+    SortedSegmentSet,
+    SortedSegmentSetItem,
+} from "./sortedSegmentSet";
+export {
+    IJSONTextSegment,
+    TextSegment,
+    IMergeTreeTextHelper,
+} from "./textSegment";
+export {
+    reservedTileLabelsKey,
+    reservedRangeLabelsKey,
+    refTypeIncludesFlag,
+    refGetTileLabels,
+    refGetRangeLabels,
+    refHasTileLabel,
+    refHasRangeLabel,
+    refHasTileLabels,
+    refHasRangeLabels,
+    ReferencePosition,
+    RangeStackMap,
+    DetachedReferencePosition,
+    minReferencePosition,
+    maxReferencePosition,
+    compareReferencePositions,
+} from "./referencePositions";
