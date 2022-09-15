1d9d8735f27fc9b1ecb8bdceef84b6d5764794ce
Tyler Butler <tyler@tylerbutler.com>
2022-09-15 15:18:17 -0700

    tests

--- build-tools/packages/build-cli/test/machines/fluidReleaseMachine.test.ts
+++ build-tools/packages/build-cli/test/machines/fluidReleaseMachine.test.ts
@@ -3,48 +3,130 @@
  * Licensed under the MIT License.
  */
 
-import { assert } from "chai";
+import chai, { expect } from "chai";
+import assertArrays from "chai-arrays";
 import { FluidReleaseMachine as machine } from "../../src/machines/fluidReleaseMachine";
 
+chai.use(assertArrays);
+
+/**
+ * The expected path patch releases will go through. When the state machine is updated these will likely change and need
+ * to be updated.
+ */
+const expectedPatchPath = [
+    "DoPatchRelease",
+    "CheckDoesReleaseFromReleaseBranch",
+    "CheckOnReleaseBranch",
+    "CheckReleaseIsDone",
+    "CheckReleaseGroupIsBumpedPatch",
+    "CheckTypeTestPrepare",
+    "CheckTypeTestGenerate",
+    "ReleaseComplete",
+    "PromptToRunTypeTests",
+    "DoReleaseGroupBump",
+    "CheckShouldCommitBump",
+    "PromptToPRBump",
+    "PromptToCommitBump",
+    "PromptToRelease",
+    "Failed",
+];
+
+/**
+ * The expected path minor releases will go through. When the state machine is updated these will likely change and need
+ * to be updated.
+ */
+const expectedMinorPath = [
+    "DoMinorRelease",
+    "CheckDoesReleaseFromReleaseBranch2",
+    "CheckOnReleaseBranch2",
+    "CheckReleaseGroupIsBumpedPatch2",
+    "CheckReleaseIsDone2",
+    "CheckTypeTestPrepare2",
+    "CheckTypeTestGenerate2",
+    "ReleaseComplete",
+    "PromptToRelease",
+    "DoReleaseGroupBump",
+    "CheckShouldCommitBump",
+    "PromptToPRBump",
+    "PromptToCommitBump",
+    "CheckReleaseGroupIsBumpedMinor",
+    "CheckReleaseBranchExists",
+    "CheckReleaseIsDone",
+    "CheckReleaseGroupIsBumpedPatch",
+    "CheckTypeTestPrepare",
+    "CheckTypeTestGenerate",
+    "PromptToRunTypeTests",
+    "PromptToCreateReleaseBranch",
+];
+
+/**
+ * The expected path major releases will go through. When the state machine is updated these will likely change and need
+ * to be updated.
+ */
+const expectedMajorPath = [
+    "DoMajorRelease",
+    "CheckDoesReleaseFromReleaseBranch3",
+    "CheckMainNextIntegrated",
+    "CheckReleaseIsDone3",
+    "DoMinorRelease",
+    "CheckDoesReleaseFromReleaseBranch2",
+    "CheckOnReleaseBranch2",
+    "CheckReleaseGroupIsBumpedPatch2",
+    "CheckReleaseIsDone2",
+    "CheckTypeTestPrepare2",
+    "CheckTypeTestGenerate2",
+    "ReleaseComplete",
+    "PromptToRelease",
+    "DoReleaseGroupBump",
+    "CheckShouldCommitBump",
+    "PromptToPRBump",
+    "PromptToCommitBump",
+    "CheckReleaseGroupIsBumpedMinor",
+    "CheckReleaseBranchExists",
+    "CheckReleaseIsDone",
+    "CheckReleaseGroupIsBumpedPatch",
+    "CheckTypeTestPrepare",
+    "CheckTypeTestGenerate",
+    "PromptToRunTypeTests",
+    "PromptToCreateReleaseBranch",
+    "CheckOnReleaseBranch3",
+    "Failed",
+    "CheckBranchName",
+    "PromptToIntegrateNext",
+];
+
 describe("FluidReleaseMachine", () => {
-    it("DoPatchRelease states", () => {
-        // const state = machine.state();
-        // console.log(`state: ${state}`);
+    it("DoPatchRelease path matches expected", () => {
         const states = new Set<string>();
-        let state = `DoPatchRelease`;
-        walkExits(state, states);
-        console.log(JSON.stringify([...states]));
+        const startingState = `DoPatchRelease`;
+
+        walkExits(startingState, states);
+        expect([...states]).to.be.equalTo(expectedPatchPath);
     });
 
-    it("DoMinorRelease states", () => {
-        // const state = machine.state();
-        // console.log(`state: ${state}`);
+    it("DoMinorRelease path matches expected", () => {
         const states = new Set<string>();
-        let state = `DoMinorRelease`;
-        walkExits(state, states);
-        console.log(JSON.stringify([...states]));
+        const startingState = `DoMinorRelease`;
+
+        walkExits(startingState, states);
+        expect([...states]).to.be.equalTo(expectedMinorPath);
     });
 
-    it("DoMajorRelease states", () => {
-        // const state = machine.state();
-        // console.log(`state: ${state}`);
+    it("DoMajorRelease path matches expected", () => {
         const states = new Set<string>();
-        let state = `DoMajorRelease`;
-        walkExits(state, states);
-        console.log(JSON.stringify([...states]));
+        const startingState = `DoMajorRelease`;
+        walkExits(startingState, states);
+        expect([...states]).to.be.equalTo(expectedMajorPath);
     });
 });
 
 function walkExits(state: string, collector: Set<string>, step = 0): void {
-    // console.log(`state: ${state}`);
     collector.add(state);
     if (machine.state_is_terminal(state)) {
         return;
     }
 
     const transitions = machine.list_transitions(state);
-    // console.log(JSON.stringify(transitions));
-
     for (const next of transitions.exits) {
         walkExits(next, collector, step++);
     }
