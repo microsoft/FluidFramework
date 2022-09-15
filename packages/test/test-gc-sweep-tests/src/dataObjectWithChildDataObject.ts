662b6972bff464a44c3d694e613c5a53f273b3c7
tyler-cai-microsoft <tylercai@microsoft.com>
2022-09-15 15:51:53 -0700

    Add comments about future TODO work

--- packages/test/test-gc-sweep-tests/src/dataObjectWithChildDataObject.ts
+++ packages/test/test-gc-sweep-tests/src/dataObjectWithChildDataObject.ts
@@ -12,6 +12,7 @@ import { DataObjectWithCounter, dataObjectWithCounterFactory } from "./dataObjec
 export class RootDataObjectWithChildDataObject extends DataObjectWithCounter {
     private unrefTimeStampMs?: number;
     private unreferencedChild?: DataObjectWithCounter;
+    // TODO: logic to always keep a child referenced. A getter should retrieve this value and add some asserts
     private child?: DataObjectWithCounter;
     private get childKey(): string {
         assert(this.context.clientId !== undefined, `client id needs to be defined to retrieve the child key!`);
@@ -28,6 +29,7 @@ export class RootDataObjectWithChildDataObject extends DataObjectWithCounter {
         // TODO: Start logic goes here after we agree on what it should be.
     }
 
+    // TODO: Make private - left public so the build passes
     public async unreferenceChild() {
         assert(this.child !== undefined, `Child should be defined when unreferencing!`);
         this.unreferencedChild = this.child;
@@ -36,17 +38,18 @@ export class RootDataObjectWithChildDataObject extends DataObjectWithCounter {
         this.unrefTimeStampMs = Date.now();
     }
 
-    public async createAndReferenceChild() {
+    private async createAndReferenceChild() {
         this.child = await dataObjectWithCounterFactory.createInstance(this.context.containerRuntime);
         await this.referenceChild(this.child.handle);
     }
 
-    public async referenceChild(handle: IFluidHandle<DataObjectWithCounter>) {
+    private async referenceChild(handle: IFluidHandle<DataObjectWithCounter>) {
         this.unreferencedChild = this.child;
         this.root.set(this.childKey, handle);
     }
 
     // This should only be called when inactiveObjectX time isn't greater than unrefTimeStampMs
+    // TODO: Make private - left public so the build passes
     public async reviveChild() {
         assert(this.unreferencedChild !== undefined, `An old handle should exist when reviving`);
         assert(this.unrefTimeStampMs !== undefined, `An unreferenceTimestamp should exist when reviving`);
@@ -55,9 +58,7 @@ export class RootDataObjectWithChildDataObject extends DataObjectWithCounter {
         this.unrefTimeStampMs = undefined;
     }
 
-    public async doActivity() {
-        // This is a placeholder method, there is value in leaving this for a later discussion.
-    }
+    // TODO: create a doActivity method that does interesting GC tasks
 }
 
 export const rootDataObjectWithChildDataObjectFactory = new DataObjectFactory(
