4cd3f86f4edd1646c8973b18e98d7207c74c2a88
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-09-15 20:47:34 +0000

    docs: Add comments

--- azure/prettier.config.cjs
+++ azure/prettier.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
     ...require("@fluidframework/build-common/prettier.config.cjs"),
     importOrder: [
-        "^node:(.*)$",
+        "^node:(.*)$", // Special-case `node:` imports
         "<THIRD_PARTY_MODULES>",
-        "^fluid-framework$",
-        "^@fluidframework/(.*)$",
-        "^@fluid-(.*?)/(.*)$",
-        "^[./]"
+        "^fluid-framework$", // Special match for `fluid-framework` package
+        "^@fluidframework/(.*)$", // Match all `@fluidframework/` packages
+        "^@fluid-(.*?)/(.*)$", // Match other `@fluid-` scoped packages (`fluid-experimental`, `fluid-tools`, etc.)
+        "^[./]" // Match package-local file imports
     ],
     importOrderSeparation: true,
     importOrderSortSpecifiers: true,
