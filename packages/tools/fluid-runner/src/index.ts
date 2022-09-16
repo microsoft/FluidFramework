cc15d44f365b8dc3ae39eae9905c84c0d6a9b147
Kian Thompson <102998837+kian-thompson@users.noreply.github.com>
2022-09-16 19:50:03 +0000

    Explicitly define exports

--- packages/tools/fluid-runner/src/index.ts
+++ packages/tools/fluid-runner/src/index.ts
@@ -3,9 +3,15 @@
  * Licensed under the MIT License.
  */
 
+/* eslint-disable import/no-internal-modules */
 export { ICodeLoaderBundle, IFluidFileConverter } from "./codeLoaderBundle";
 export * from "./exportFile";
 export { fluidRunner } from "./fluidRunner";
-// eslint-disable-next-line import/no-internal-modules
-export * from "./logger/fileLogger";
+export {
+    OutputFormat,
+    createLogger,
+    getTelemetryFileValidationError,
+    validateAndParseTelemetryOptions,
+} from "./logger/fileLogger";
 export * from "./parseBundleAndExportFile";
+/* eslint-enable import/no-internal-modules */
