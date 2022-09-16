microsoft/FluidFramework

commit c3a11a9e1f39de8a6e8fb52f37b017edede46a48

Update md-magic config to offer more degrees of freedom for generating standard package README content (#11874)
Also updates the SharedCounter README to utilize this, and moves
package's overview from the website to the README (the website now
embeds the README content).

docs/md-magic.config.js
@@ -7,47 +7,101 @@ const fs = require("fs");
 const pathLib = require("path");
 const template = require("markdown-magic-template");
 const fetch = require("node-fetch");
+const scripts = require("markdown-magic-package-scripts");
+const os = require("os");
 
+const mdMagicTemplatesPath = pathLib.join(__dirname, "..", ".md-magic-templates");
+
+const generatedContentNotice = `<!-- This section is automatically generated.
+To update it, edit docs/md-magic.config.js  then run 'npm run build:md-magic' in the docs folder. -->`
+
+/**
+ * Gets the path to the repo root.
+ */
 const getRepoRoot = () => {
     const root = pathLib.normalize(pathLib.join(__dirname, ".."));
     return root;
 }
 
-const getPackageMetadata = (path) => {
+/**
+ * Gets the path to the package's `package.json` file, given the path to the document including this.
+ *
+ * @param {string} originalPath - Path to the file for which `md-magic` is being invoked.
+ */
+const getPackageJsonPathFromOriginalPath = (originalPath) => {
+    const dir = pathLib.dirname(originalPath);
+    return pathLib.join(dir, "package.json");
+}
+
+/**
+ * Gets the package's `package.json` contents, given the path to the document including this.
+ *
+ * @param {string} originalPath - Path to the file for which `md-magic` is being invoked.
+ */
+const getPackageJsonFromOriginalPath = (originalPath) => {
+    const packageJsonPath = getPackageJsonPathFromOriginalPath(originalPath);
+    const packageJson = getPackageMetadata(packageJsonPath);
+    return packageJson;
+}
+
+/**
+ * Gets the package's `package.json` contents, given the path to its package.json file.
+ *
+ * @param {string} packageJsonFilePath - Path to a `package.json` file.
+ */
+const getPackageMetadata = (packageJsonFilePath) => {
     try {
-        const pkg = JSON.parse(fs.readFileSync(path, "utf8"));
-        return pkg;
+        const packageJson = JSON.parse(fs.readFileSync(packageJsonFilePath, "utf8"));
+        packageJson.shortName = getShortPackageName(packageJson.name);
+        return packageJson;
     } catch (ex) {
         console.log(ex);
     }
 }
 
-const getPackageName = (path) => {
-    return getPackageMetadata(path).name;
-}
-
-const getShortName = (longName) => {
-    const arr = longName.split("/", 2);
+/**
+ * Gets a package short-name (unscoped-name) from a scoped package name.
+ *
+ * @param {string} scopedPackageName - A scoped package name.
+ */
+const getShortPackageName = (scopedPackageName) => {
+    const arr = scopedPackageName.split("/", 2);
     if (arr[1]) {
         return arr[1];
     }
     return arr[0];
 }
 
-const getPackagePath = (path) => {
-    path = pathLib.dirname(pathLib.relative(getRepoRoot(), path));
-    return toPosix(path);
+/**
+ * Gets the path (relative to the `docs` directory) to the package directory given the path to its `package.json` file.
+ *
+ * @param {string} packageJsonPath - Path to the package's `package.json` file.
+ */
+const getPackagePath = (packageJsonPath) => {
+    packageJsonPath = pathLib.dirname(pathLib.relative(getRepoRoot(), packageJsonPath));
+    return toPosix(packageJsonPath);
 }
 
+/**
+ * Converts the provided path to POSIX form.
+ *
+ * @param {string} path - The path to convert.
+ */
 const toPosix = (path) => {
     const inPosix = path.split(pathLib.sep).join(pathLib.posix.sep);
     return inPosix;
 }
 
-const getStartedInfo = (path, includeTinylicious = false) => {
-    // console.log(`path: ${path}`);
-    const preamble = `<!-- The getting started instructions are automatically generated.
-To update them, edit docs/md-magic.config.js, then run 'npm run build:md-magic' -->
+/**
+ * Generates a `Getting Started` heading and contents for the specified package.
+ *
+ * @param {string} packageJsonPath - Path to the package's `package.json` file.
+ * @param {boolean} includeTinylicious - Whether or not to include the `Tinylicious` step in the instructions.
+ */
+const getStartedInfo = (packageJsonPath, includeTinylicious = false) => {
+    const packageJson = getPackageMetadata(packageJsonPath);
+
+    const preamble = `${generatedContentNotice}
 
 ## Getting Started
 
@@ -55,10 +109,10 @@ You can run this example using the following steps:
 
 1. Run \`npm install\` and \`npm run build:fast -- --nolint\` from the \`FluidFramework\` root directory.
     - For an even faster build, you can add the package name to the build command, like this:
-      \`npm run build:fast -- --nolint ${getPackageName(path)}\``;
+      \`npm run build:fast -- --nolint ${packageJson.name}\``;
 
     const tinyliciousStep = `1. In a separate terminal, start a Tinylicious server by following the instructions in [Tinylicious](../../../server/tinylicious).`;
-    const finalStep = `1. Run \`npm run start\` from this directory (${getPackagePath(path)}) and open <http://localhost:8080> in a web browser to see the app running.`;
+    const finalStep = `1. Run \`npm run start\` from this directory (${getPackagePath(packageJsonPath)}) and open <http://localhost:8080> in a web browser to see the app running.`;
 
     const steps = [
         preamble,
@@ -67,6 +121,67 @@ You can run this example using the following steps:
     ].filter(item => item !== undefined);
 
     return `${steps.join("\n")}\n`;
+}
+
+/**
+ * Generats a simple Markdown heading and contents with package installation instructions.
+ *
+ * @param {object} packageJson - `package.json` contents
+ * @param {boolean} devDependency - Whether or not the package is intended to be installed as a dev dependency.
+ * @param {boolean} includeHeading - Whether or not to include the heading in the generated contents.
+ */
+const generateInstallationSection = (packageJson, devDependency, includeHeading) => {
+    const sectionBody = `To get started, install the package by running the following command:
+
+\`\`\`bash
+npm i ${packageJson.name}${devDependency ? " -D" : ""}
+\`\`\``;
+
+    return includeHeading
+        ? `## Installation${os.EOL}${os.EOL}${sectionBody}`
+        : sectionBody;
+}
+
+/**
+ * Generats a simple Markdown heading and contents with trademark information.
+ *
+ * @param {boolean} includeHeading - Whether or not to include the heading in the generated contents.
+ */
+const generateTrademarkSection = (includeHeading) => {
+    const sectionBody = `This project may contain Microsoft trademarks or logos for Microsoft projects, products, or services.
+Use of these trademarks or logos must follow Microsoft's [Trademark & Brand Guidelines](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/general).
+Use of Microsoft trademarks or logos in modified versions of this project must not cause confusion or imply Microsoft sponsorship.`;
+
+    return includeHeading
+        ? `## Trademark${os.EOL}${os.EOL}${sectionBody}`
+        : sectionBody;
+}
+
+/**
+ * Generats a simple Markdown heading and contents with information about API documentation for the package.
+ *
+ * @param {object} packageJson - `package.json` contents
+ * @param {boolean} includeHeading - Whether or not to include the heading in the generated contents.
+ */
+const generateApiDocsLinkSection = (packageJson, includeHeading) => {
+    const sectionBody = `API documentation for **${packageJson.name}** is available at <https://fluidframework.com/docs/apis/${packageJson.shortName}>.`;
+
+    return includeHeading
+        ? `## API Documentation${os.EOL}${os.EOL}${sectionBody}`
+        : sectionBody;
+}
+
+/**
+ * Generats a simple Markdown heading and contents with a table describing all of the package's npm scripts.
+ *
+ * @param {string} scriptsTable - Table of scripts to display.
+ * See `markdown-magic-package-scripts` (imported as `scripts`).
+ * @param {boolean} includeHeading - Whether or not to include the heading in the generated contents.
+ */
+const generateScriptsSection = (scriptsTable, includeHeading) => {
+    return includeHeading
+        ? `## Scripts${os.EOL}${os.EOL}${scriptsTable}`
+        : scriptsTable;
 }
 
 const fetchFunc = async (content, options) => {
@@ -124,8 +239,7 @@ const mdMagicConfig = {
         },
         /* Match <!-- AUTO-GENERATED-CONTENT:START (GET_STARTED) --> */
         GET_STARTED(content, options, config) {
-            const dir = pathLib.dirname(config.originalPath);
-            const jsonPath = pathLib.join(dir, "package.json");
+            const jsonPath = getPackageJsonPathFromOriginalPath(config.originalPath);
             if (options && options.tinylicious) {
                 return getStartedInfo(jsonPath, options.tinylicious);
             } else {
@@ -133,21 +247,64 @@ const mdMagicConfig = {
             }
         },
         PKGJSON(content, options, config) {
-            const dir = pathLib.dirname(config.originalPath);
-            const jsonPath = pathLib.join(dir, "package.json");
-            options.pkg = jsonPath;
+            options.pkg = getPackageJsonPathFromOriginalPath(config.originalPath);
             return require("markdown-magic-package-json")(content, options, config);
         },
+        /* Match <!-- AUTO-GENERATED-CONTENT:START (README_SIMPLE:installation=TRUE&apiDocs=TRUE&scripts=TRUE&trademark=TRUE&devDependency=FALSE) --> */
+        README_SIMPLE(content, options, config) {
+            const pkg = getPackageJsonFromOriginalPath(config.originalPath);
+
+            const sections = [generatedContentNotice];
+
+            if(options.installation !== "FALSE") {
+                sections.push(generateInstallationSection(pkg, options.devDependency, true));
+            }
+
+            if(options.apiDocs !== "FALSE") {
+                sections.push(generateApiDocsLinkSection(pkg, true));
+            }
+
+            if(options.scripts !== "FALSE") {
+                const scriptsTable = scripts(content, options, config);
+                sections.push(generateScriptsSection(scriptsTable, true));
+            }
+
+            if(options.trademark !== "FALSE") {
+                sections.push(generateTrademarkSection(true));
+            }
+
+            return sections.join(`${os.EOL}${os.EOL}`);
+        },
+        /* Match <!-- AUTO-GENERATED-CONTENT:START (README_API_DOCS_SECTION:includeHeading=TRUE) --> */
+        README_API_DOCS_SECTION(content, options, config) {
+            const includeHeading = options.includeHeading !== "FALSE";
+            const pkg = getPackageJsonFromOriginalPath(config.originalPath);
+            return generateApiDocsLinkSection(pkg, includeHeading);
+        },
+        /* Match <!-- AUTO-GENERATED-CONTENT:START (README_INSTALLATION_SECTION:includeHeading=TRUE&devDependency=FALSE) --> */
+        README_INSTALLATION_SECTION(content, options, config) {
+            const includeHeading = options.includeHeading !== "FALSE";
+            const pkg = getPackageJsonFromOriginalPath(config.originalPath);
+            return generateInstallationSection(pkg, options.devDependency, includeHeading);
+        },
+        /* Match <!-- AUTO-GENERATED-CONTENT:START (README_TRADEMARK_SECTION:includeHeading=TRUE) --> */
+        README_TRADEMARK_SECTION(content, options, config) {
+            const includeHeading = options.includeHeading !== "FALSE";
+            return generateTrademarkSection(includeHeading);
+        },
         TEMPLATE(content, options, config) {
-            const dir = pathLib.dirname(config.originalPath);
-            const jsonPath = pathLib.join(dir, "package.json");
-            const pkg = getPackageMetadata(jsonPath);
-            pkg.shortName = getShortName(pkg.name);
+            const pkg = getPackageJsonFromOriginalPath(config.originalPath);
+
             options = options || {};
-            options.src = pathLib.relative(dir, pathLib.join(__dirname, "../.md-magic-templates/", options.src));
+            options.src = pathLib.relative(dir, pathLib.join(mdMagicTemplatesPath, options.src));
             return template({ pkg: pkg })(content, options, config);
         },
-        SCRIPTS: require("markdown-magic-package-scripts"),
+        /* Match <!-- AUTO-GENERATED-CONTENT:START (SCRIPTS:includeHeading=TRUE) --> */
+        SCRIPTS(content, options, config) {
+            const includeHeading = options.includeHeading !== "FALSE";
+            const scriptsTable = scripts(content, options, config);
+            return generateScriptsSection(scriptsTable, includeHeading);
+        },
         FETCH: fetchFunc,
     },
     callback: function () {

docs/config.yml
@@ -16,6 +16,8 @@ module:
     target: content
   - source: _replacements
     target: content
+  - source: ../packages/dds/counter/README.md
+    target: content/docs/counter-readme.md
   - source: ../packages/dds/sequence/README.md
     target: content/docs/sequence-readme.md
 

commit f684e606e1ded4d567da224143c443bafe0806fb

Fix md magic config (#12012)
Bug introduced in #11874.

Issue discovered in main->next integration (#12008), and a fix was
pushed directly to that branch.

docs/md-magic.config.js
@@ -292,7 +292,9 @@ const mdMagicConfig = {
             const includeHeading = options.includeHeading !== "FALSE";
             return generateTrademarkSection(includeHeading);
         },
+        /* Match <!-- AUTO-GENERATED-CONTENT:START (TEMPLATE:src="<name-of-template-file under `/.md-magic-templates`>") --> */
         TEMPLATE(content, options, config) {
+            const dir = pathLib.dirname(config.originalPath);
             const pkg = getPackageJsonFromOriginalPath(config.originalPath);
 
             options = options || {};