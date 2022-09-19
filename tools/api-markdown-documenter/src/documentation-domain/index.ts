bf3bc6378b65f54e28126e97576fbb4cc9c34998
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-09-19 18:52:21 +0000

    [WIP] Playing around with types

--- tools/api-markdown-documenter/src/documentation-domain/index.ts
+++ tools/api-markdown-documenter/src/documentation-domain/index.ts
@@ -1,4 +1,4 @@
-import {Node} from 'unist';
+import { Parent as UnistParent, Literal as UnistLiteral, Node as UnistNode } from 'unist';
 import {paragraph} from 'mdast';
 
 export enum DocumentNodeKind {
@@ -6,17 +6,25 @@ export enum DocumentNodeKind {
     NestedSection = "NestedSection",
 }
 
-export interface DocumentNode extends Node<DocumentNode[]> {
-    readonly type: DocumentNodeKind;
+export type DocumentNode = DocumentParentNode | DocumentLiteralNode;
+
+export abstract class DocumentParentNode implements UnistParent<DocumentNode> {
+    public readonly type: DocumentNodeKind;
+    public readonly children: DocumentNode[];
 }
 
-export class Document implements DocumentNode {
+export abstract class DocumentLiteralNode implements UnistLiteral<string> {
+    public readonly type: DocumentNodeKind;
+    public readonly value: string;
+}
+
+export class Document implements DocumentParentNode {
     public readonly type = DocumentNodeKind.Document;
 
     public readonly filePath: string;
 }
 
-export class NestedSection implements DocumentNode {
+export class NestedSection implements DocumentParentNode {
     public readonly type = DocumentNodeKind.NestedSection;
 }
 
