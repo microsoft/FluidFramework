bef58c7a4270bfd72539c7f1e74d7be28dc2ede8
Mark Fields <markfields@users.noreply.github.com>
2022-09-20 23:02:15 +0000

    Added a new datastore to stress tests

---
+++ packages/test/test-service-load/src/gcDataStore.ts
@@ -0,0 +1,66 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { DataObject, DataObjectFactory } from "@fluidframework/aqueduct";
+import { assert, delay } from "@fluidframework/common-utils";
+import { IFluidHandle } from "@fluidframework/core-interfaces";
+import { SharedCounter } from "@fluidframework/counter";
+
+/**
+ * DataObjectWithCounter increments a SharedCounter as a way of sending ops.
+ *
+ * The SharedCounter is retrieved via handle
+ */
+const counterKey = "counter";
+export class DataObjectWithCounter extends DataObject {
+    private _counter?: SharedCounter;
+    protected isRunning: boolean = false;
+    protected readonly delayPerOpMs = 100;
+    public static get type(): string {
+        return "DataObjectWithCounter";
+    }
+
+    protected get counter(): SharedCounter {
+        assert(this._counter !== undefined, "Need counter to be defined before retreiving!");
+        return this._counter;
+    }
+
+    protected async initializingFirstTime(props?: any): Promise<void> {
+        this.root.set<IFluidHandle>(counterKey, SharedCounter.create(this.runtime).handle);
+    }
+
+    protected async hasInitialized(): Promise<void> {
+        const handle = this.root.get<IFluidHandle<SharedCounter>>(counterKey);
+        assert(handle !== undefined, `The counter handle should exist on initialization!`);
+        this._counter = await handle.get();
+    }
+
+    public stop() {
+        this.isRunning = false;
+    }
+
+    public start() {
+        this.isRunning = true;
+        this.run().catch((error) => { console.log(error); });
+    }
+
+    protected async run() {
+        assert(this.isRunning === true, "Should be running to send ops");
+        while (this.isRunning && !this.disposed) {
+            this.counter.increment(1);
+            if (this.counter.value % 10 === 1) {
+                console.log(`########## GC DATA STORE [${this.runtime.clientId}]: ${this.counter.value}`);
+            }
+            await delay(this.delayPerOpMs);
+        }
+    }
+}
+
+export const dataObjectWithCounterFactory = new DataObjectFactory(
+    DataObjectWithCounter.type,
+    DataObjectWithCounter,
+    [SharedCounter.getFactory()],
+    {},
+);
