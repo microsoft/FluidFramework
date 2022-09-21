9e26692e48218ea99ad1e9d3bc34fee8832ec586
Mark Fields <markfields@users.noreply.github.com>
2022-09-21 21:44:17 +0000

    Missed merge resolutions

--- packages/dds/sequence/src/packageVersion.ts
+++ packages/dds/sequence/src/packageVersion.ts
@@ -6,8 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/sequence";
-<<<<<<< HEAD
 export const pkgVersion = "2.0.0-internal.1.2.0";
-=======
-export const pkgVersion = "2.0.0-internal.1.1.3";
->>>>>>> fork/restore-bindToContext
