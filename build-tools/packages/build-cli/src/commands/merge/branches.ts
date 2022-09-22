e1a700f3b6ea7bfbb13e69b43cb3ebf6ac4ad9fa
sdeshpande3 <sdeshpande@microsoft.com>
2022-09-22 13:16:14 -0700

    user name

--- build-tools/packages/build-cli/src/commands/merge/branches.ts
+++ build-tools/packages/build-cli/src/commands/merge/branches.ts
@@ -142,11 +142,9 @@ export default class MergeBranch extends BaseCommand<typeof MergeBranch.flags> {
             await gitRepo.setUpstream(`${flags.source}-${flags.target}-${unmergedCommitList[0]}`);
             await gitRepo.resetBranch(unmergedCommitList[0]);
             // fetch name of owner associated to the pull request
-            const data = await api.pullRequestInfo(flags.auth, unmergedCommitList[0]);
-            const data1 = JSON.stringify(data);
-            this.info(`Author name: ${data[0].user.login}`);
-            this.log(
-                `Fetch pull request info for single commit id ${unmergedCommitList[0]}: ${data1}`,
+            const prInfo = await api.pullRequestInfo(flags.auth, unmergedCommitList[0]);
+            this.info(
+                `Fetch pull request info for single commit id ${unmergedCommitList[0]}: ${prInfo[0].data[0].user.login}`,
             );
             prNumber = await api.createPullRequest(
                 flags.auth,
@@ -159,12 +157,10 @@ export default class MergeBranch extends BaseCommand<typeof MergeBranch.flags> {
             );
         } else {
             // fetch name of owner associated to the pull request
-            const data = await api.pullRequestInfo(flags.auth, unmergedCommitList[commit]);
-            const data1 = JSON.stringify(data);
+            const prInfo = await api.pullRequestInfo(flags.auth, unmergedCommitList[commit]);
             this.info(
-                `Fetch pull request info for bulk commit id ${unmergedCommitList[commit]}: ${data1}`,
+                `Fetch pull request info for bulk commit id ${unmergedCommitList[commit]}: ${prInfo[0].data[0].user.login}`,
             );
-            this.info(`Author name: ${data[0].user.login}`);
             prNumber = await api.createPullRequest(
                 flags.auth,
                 branchName,
