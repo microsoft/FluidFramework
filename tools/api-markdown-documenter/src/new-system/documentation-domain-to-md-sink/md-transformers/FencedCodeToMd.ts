19c3b35325446e18a463cdf59f7cc5c93b1caaa3
Justin Myhres <jumyhre@microsoft.com>
2022-09-22 14:48:50 -0700

    Improved fenced code block output

--- tools/api-markdown-documenter/src/new-system/documentation-domain-to-md-sink/md-transformers/FencedCodeToMd.ts
+++ tools/api-markdown-documenter/src/new-system/documentation-domain-to-md-sink/md-transformers/FencedCodeToMd.ts
@@ -1,17 +1,18 @@
 import { FencedCodeBlockNode } from "../../documentation-domain";
 import type { DocumentationNodeRenderer } from "./DocumentationNodeRenderer";
-import { markdownEOL, standardEOL } from "./Utilities";
+import { addNewlineOrBlank, standardEOL } from "./Utilities";
 
 export function FencedCodeBlockToMarkdown(
     blockNode: FencedCodeBlockNode,
     renderer: DocumentationNodeRenderer,
 ): string {
-    const output = [markdownEOL, "```" + blockNode.language, standardEOL];
+    const output = [addNewlineOrBlank(renderer.getLastRenderedCharacter()), "```" + blockNode.language, standardEOL];
     renderer.setInsideCodeBlock();
     const children = renderer.renderNodes(blockNode.children);
     output.push(children);
-    output.push(standardEOL);
+    output.push(addNewlineOrBlank(renderer.getLastRenderedCharacter()));
     output.push("```");
+    output.push(standardEOL);
 
     return output.join("");
 }
