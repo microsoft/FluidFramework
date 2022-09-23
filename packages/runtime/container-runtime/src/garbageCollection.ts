256e8465e35851720095b3c776f63bec6571afb3
Mark Fields <markfields@users.noreply.github.com>
2022-09-23 15:12:47 -0700

    [GC-Stress] Enable shrinking SweepTimeout for testing (#12092)
    This is WIP version of #12044. I'm pausing that work in terms of merging
    to main until next week.
    
    This way we can shrink Session Expiry and the Sweep Timeout in the
    stress test and watch for SweepReady errors.
    
    Here are the settings to set:
    
    * `Fluid.GarbageCollection.TestOverride.SessionExpiryMs`
    * `Fluid.Driver.Odsp.TestOverride.DisableSnapshotCache` (disables
    snapshot caching in driver and uses 0 for the Sweep Timeout calculation)
    * `Fluid.GarbageCollection.TestOverride.SweepBufferMs`
    
    Then the total SweepTimeout will be `sessionExpiryMs + sweepBufferMs`

--- packages/runtime/container-runtime/src/garbageCollection.ts
+++ packages/runtime/container-runtime/src/garbageCollection.ts
@@ -505,6 +505,11 @@ export class GarbageCollector implements IGarbageCollector {
 
         let prevSummaryGCVersion: number | undefined;
 
+        // This override applies to new documents, and will always adjust the timeout used in this session
+        // But it will not modify the value persisted in an existing container, and must not exceed that value.
+        const overrideSessionExpiryTimeoutMs =
+                this.mc.config.getNumber("Fluid.GarbageCollection.TestOverride.SessionExpiryMs");
+
         /**
          * Sweep timeout is the time after which unreferenced content can be swept.
          * Sweep timeout = session expiry timeout + snapshot cache expiry timeout + one day buffer.
@@ -557,7 +562,10 @@ export class GarbageCollector implements IGarbageCollector {
 
             // Set the Session Expiry only if the flag is enabled and GC is enabled.
             if (this.mc.config.getBoolean(runSessionExpiryKey) && this.gcEnabled) {
-                this.sessionExpiryTimeoutMs = this.gcOptions.sessionExpiryTimeoutMs ?? defaultSessionExpiryDurationMs;
+                this.sessionExpiryTimeoutMs =
+                    overrideSessionExpiryTimeoutMs
+                    ?? this.gcOptions.sessionExpiryTimeoutMs
+                    ?? defaultSessionExpiryDurationMs;
             }
             this.sweepTimeoutMs =
                 testOverrideSweepTimeoutMs
@@ -570,6 +578,8 @@ export class GarbageCollector implements IGarbageCollector {
             const overrideSessionExpiryTimeoutMs =
                 this.mc.config.getNumber("Fluid.GarbageCollection.TestOverride.SessionExpiryMs");
             const timeoutMs = overrideSessionExpiryTimeoutMs ?? this.sessionExpiryTimeoutMs;
+            assert(timeoutMs <= this.sessionExpiryTimeoutMs,
+                "Cannot extend sessionExpiry via TestOverride setting");
 
             this.sessionExpiryTimer = new Timer(
                 timeoutMs,
