46b7900ab66d8fe52c23e1fc8a018305ab351e62
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-09-24 01:00:05 +0000

    fix: Heading IDs

--- tools/api-markdown-documenter/src/new-system/documentation-domain-to-md-sink/md-transformers/HeadingToMd.ts
+++ tools/api-markdown-documenter/src/new-system/documentation-domain-to-md-sink/md-transformers/HeadingToMd.ts
@@ -1,10 +1,13 @@
 import { HeadingNode } from "../../documentation-domain";
 import type { DocumentationNodeRenderer } from "./DocumentationNodeRenderer";
-import {
-    addNewlineOrBlank,
-    addNewlineOrBlank as newlineOrBlankSpace,
-    standardEOL,
-} from "./Utilities";
+import { addNewlineOrBlank, standardEOL } from "./Utilities";
+
+/**
+ * Maximum heading level supported by most systems.
+ *
+ * @remarks This corresponds with the max HTML heading level.
+ */
+export const maxHeadingLevel = 6;
 
 /**
  * Converts a HeadingNode to markdown. Will use the renderer's hierarchyDepth to set an appropriate depth for the header if no override is supplied on the node.
@@ -17,28 +20,35 @@ export function HeadingToMarkdown(
     headingNode: HeadingNode,
     renderer: DocumentationNodeRenderer,
 ): string {
-    const output: string[] = [newlineOrBlankSpace(renderer.countTrailingNewlines < 2)];
+    const output: string[] = [addNewlineOrBlank(
+        renderer.countTrailingNewlines < 2,
+    )];
     const headingLevel = headingNode.level ?? renderer.hierarchyDepth;
     const headerLine: string[] = [];
-    switch (headingLevel) {
-        case 1:
-            headerLine.push("##");
-            break;
-        case 2:
-            headerLine.push("###");
-            break;
-        case 3:
-            headerLine.push("###");
-            break;
-        default:
-            headerLine.push("####");
+
+    const renderAsMarkdownHeading = headingLevel <= maxHeadingLevel;
+
+    if (renderAsMarkdownHeading) {
+        const prefix = "#".repeat(headingLevel);
+        headerLine.push(prefix);
+    } else {
+        // TODO: ID above bold text? Confirm support.
+        // If the heading level is beyond the max, we will simply render the title as bolded text
+        renderer.setBold();
     }
+
     headerLine.push(renderer.renderNodes(headingNode.children));
-    output.push(
-        `${headerLine.join(" ")}${addNewlineOrBlank(
-            renderer.countTrailingNewlines < 1,
-        )}${standardEOL}`, // Markdown best practices: Include one extra newline after a header
-    );
+
+    if (renderAsMarkdownHeading && headingNode.id) {
+        headerLine.push(`{#${headingNode.id}}`);
+    }
+
+    output.push(headerLine.join(" "));
+    output.push(addNewlineOrBlank(
+        renderer.countTrailingNewlines < 1,
+    ));
+    // Markdown best practices: Include one extra newline after a header
+    output.push(standardEOL);
 
     return output.join("");
 }
