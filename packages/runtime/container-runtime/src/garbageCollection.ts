caa79be7a4bebd45912f52efae3724c010d41649
Navin Agarwal <45832642+agarwal-navin@users.noreply.github.com>
2022-09-26 23:58:24 +0000

    Added timestamp to GarbageCollection event after GC runs.

--- packages/runtime/container-runtime/src/garbageCollection.ts
+++ packages/runtime/container-runtime/src/garbageCollection.ts
@@ -855,7 +855,7 @@ export class GarbageCollector implements IGarbageCollector {
             const gcResult = runGarbageCollection(gcData.gcNodes, ["/"]);
 
             const gcStats = await this.runPostGCSteps(gcData, gcResult, logger, currentReferenceTimestampMs);
-            event.end({ ...gcStats });
+            event.end({ ...gcStats, timestamp: currentReferenceTimestampMs });
             this.completedRuns++;
             return gcStats;
         }, { end: true, cancel: "error" });
