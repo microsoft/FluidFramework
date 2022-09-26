76c8c261348a01e0658166125eadb72cfb2bd56e
daesun-park <108361046+daesun-park@users.noreply.github.com>
2022-09-26 18:18:56 +0000

    fix config to use shared config

--- packages/dds/tree/prettier.config.cjs
+++ packages/dds/tree/prettier.config.cjs
@@ -1,38 +1,3 @@
-// Shared prettier configuration for use in across the fluid-framework repository.
-// Individual packages may extend this and override rules as needed, though for consistent formatting, package-local
-// overrides should be avoided unless absolutely necessary.
 module.exports = {
-    printWidth: 100,
-    quoteProps: "consistent",
-    semi: true,
-    singleQuote: false,
-    tabWidth: 4,
-    trailingComma: "all",
-    useTabs: false, // TODO: reconsider in the future for accessibility
-    overrides: [
-        {
-            files: "lerna.json",
-            options: {
-                printWidth: 50,
-                tabWidth: 2,
-            },
-        },
-        {
-            files: "tsconfig*.json",
-            options: {
-                parser: "json5",
-                tabWidth: 2,
-                trailingComma: "all",
-                quoteProps: "preserve",
-            },
-        },
-        {
-            files: "*.json",
-            options: {
-                tabWidth: 2,
-                trailingComma: "all",
-                quoteProps: "preserve",
-            },
-        },
-    ],
+    ...require("@fluidframework/build-common/prettier.config.cjs"),
 };
