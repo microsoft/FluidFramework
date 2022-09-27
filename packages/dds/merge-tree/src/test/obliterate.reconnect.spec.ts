a3cd70b5ffae8afad72164b0b2dd55b2415ffcbb
Connor Skees <connor1skees@gmail.com>
2022-09-27 10:52:28 -0400

    use helper to create initial client state

--- packages/dds/merge-tree/src/test/obliterate.reconnect.spec.ts
+++ packages/dds/merge-tree/src/test/obliterate.reconnect.spec.ts
@@ -7,63 +7,50 @@ import { strict as assert } from "assert";
 import { ISequencedDocumentMessage } from "@fluidframework/protocol-definitions";
 import { IMergeTreeDeltaOp } from "../ops";
 import { TestClient } from "./testClient";
-import { TestClientLogger } from "./testClientLogger";
+import { createClientsAtInitialState, TestClientLogger } from "./testClientLogger";
 
-type ClientName = "A" | "B" | "C" | "D";
+const ClientIds = ["A", "B", "C", "D"] as const;
+type ClientName = typeof ClientIds[number];
 
 class ReconnectTestHelper {
-    A = new TestClient();
-    B = new TestClient();
-    C = new TestClient();
-    D = new TestClient();
-
-    get allClients() {
-        return [this.A, this.B, this.C, this.D];
-    }
+    clients = createClientsAtInitialState("", ...ClientIds);
 
     idxFromName(name: ClientName): number {
         return name.charCodeAt(0) - "A".charCodeAt(0);
     }
 
-    logger = new TestClientLogger(this.allClients);
+    logger = new TestClientLogger(this.clients.all);
 
     ops: ISequencedDocumentMessage[] = [];
-    perClientOps: ISequencedDocumentMessage[][] = this.allClients.map(() => []);
+    perClientOps: ISequencedDocumentMessage[][] = this.clients.all.map(() => []);
 
     seq: number = 0;
 
-    constructor() {
-        this.A.startOrUpdateCollaboration("A");
-        this.B.startOrUpdateCollaboration("B");
-        this.C.startOrUpdateCollaboration("C");
-        this.D.startOrUpdateCollaboration("D");
-    }
-
     public insertText(clientName: ClientName, pos: number, text: string): void {
-        const client = this[clientName];
+        const client = this.clients[clientName];
         this.ops.push(client.makeOpMessage(client.insertTextLocal(pos, text), ++this.seq));
     }
 
     public removeRange(clientName: ClientName, start: number, end: number): void {
-        const client = this[clientName];
+        const client = this.clients[clientName];
         this.ops.push(client.makeOpMessage(client.removeRangeLocal(start, end), ++this.seq));
     }
 
     public obliterateRange(clientName: ClientName, start: number, end: number): void {
-        const client = this[clientName];
+        const client = this.clients[clientName];
         this.ops.push(client.makeOpMessage(client.obliterateRangeLocal(start, end), ++this.seq));
     }
 
     public disconnect(clientNames: ClientName[]): void {
         const clientIdxs = clientNames.map(this.idxFromName);
-        this.ops.splice(0).forEach((op) => this.allClients.forEach(
+        this.ops.splice(0).forEach((op) => this.clients.all.forEach(
             (c, i) => clientIdxs.includes(i)
                 ? this.perClientOps[i].push(op)
                 : c.applyMsg(op)));
     }
 
     public processAllOps(): void {
-        this.ops.splice(0).forEach((op) => this.allClients.forEach((c) => c.applyMsg(op)));
+        this.ops.splice(0).forEach((op) => this.clients.all.forEach((c) => c.applyMsg(op)));
     }
 
     public reconnect(clientNames: ClientName[]): void {
@@ -71,14 +58,14 @@ class ReconnectTestHelper {
         this.perClientOps.forEach(
             (clientOps, i) => {
                 if (clientIdxs.includes(i)) {
-                    clientOps.splice(0).forEach((op) => this.allClients[i].applyMsg(op));
+                    clientOps.splice(0).forEach((op) => this.clients.all[i].applyMsg(op));
                 }
             },
         );
     }
 
     public submitDisconnectedOp(clientName: ClientName, op: IMergeTreeDeltaOp): void {
-        const client = this[clientName];
+        const client = this.clients[clientName];
         const pendingSegmentGroups = client.peekPendingSegmentGroups();
         assert(pendingSegmentGroups);
         this.ops.push(
@@ -95,13 +82,13 @@ describe("obliterate", () => {
         helper.processAllOps();
         helper.removeRange("B", 0, 3);
         helper.disconnect(["C"]);
-        const cOp = helper.C.obliterateRangeLocal(0, 1);
+        const cOp = helper.clients.C.obliterateRangeLocal(0, 1);
         assert(cOp);
         helper.reconnect(["C"]);
         helper.submitDisconnectedOp("C", cOp);
         helper.processAllOps();
 
-        assert.equal(helper.A.getText(), "D");
+        assert.equal(helper.clients.A.getText(), "D");
 
         helper.logger.validate();
     });
@@ -113,13 +100,13 @@ describe("obliterate", () => {
         helper.processAllOps();
         helper.obliterateRange("B", 0, 3);
         helper.disconnect(["C"]);
-        const cOp = helper.C.insertTextLocal(2, "aaa");
+        const cOp = helper.clients.C.insertTextLocal(2, "aaa");
         assert(cOp);
         helper.reconnect(["C"]);
         helper.submitDisconnectedOp("C", cOp);
         helper.processAllOps();
 
-        assert.equal(helper.A.getText(), "D");
+        assert.equal(helper.clients.A.getText(), "D");
 
         helper.logger.validate();
     });
@@ -131,13 +118,13 @@ describe("obliterate", () => {
         helper.processAllOps();
         helper.obliterateRange("B", 0, 3);
         helper.disconnect(["C"]);
-        const cOp = helper.C.insertTextLocal(0, "aaa");
+        const cOp = helper.clients.C.insertTextLocal(0, "aaa");
         assert(cOp);
         helper.reconnect(["C"]);
         helper.submitDisconnectedOp("C", cOp);
         helper.processAllOps();
 
-        assert.equal(helper.A.getText(), "aaaD");
+        assert.equal(helper.clients.A.getText(), "aaaD");
 
         helper.logger.validate();
     });
@@ -149,13 +136,13 @@ describe("obliterate", () => {
         helper.processAllOps();
         helper.obliterateRange("B", 0, 3);
         helper.disconnect(["C"]);
-        const cOp = helper.C.insertTextLocal(3, "aaa");
+        const cOp = helper.clients.C.insertTextLocal(3, "aaa");
         assert(cOp);
         helper.reconnect(["C"]);
         helper.submitDisconnectedOp("C", cOp);
         helper.processAllOps();
 
-        assert.equal(helper.A.getText(), "aaaD");
+        assert.equal(helper.clients.A.getText(), "aaaD");
 
         helper.logger.validate();
     });
