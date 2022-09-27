107034cd9e38bc8003a05d1db044a1d98187b400
tyler-cai-microsoft <tylercai@microsoft.com>
2022-09-27 16:58:28 -0700

    [bump] client: 1.2.7 => 1.2.8 (patch)
    Post-release patch bump of client.

--- experimental/PropertyDDS/packages/property-common/src/packageVersion.ts
+++ experimental/PropertyDDS/packages/property-common/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-experimental/property-common";
-export const pkgVersion = "1.2.7";
+export const pkgVersion = "1.2.8";
--- packages/drivers/driver-base/src/packageVersion.ts
+++ packages/drivers/driver-base/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/driver-base";
-export const pkgVersion = "1.2.7";
+export const pkgVersion = "1.2.8";
