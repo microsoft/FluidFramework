f41aa48e61adb5e7e8d38258a59a52f892f628b0
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2022-09-28 19:58:57 +0000

    adding eslint rules to only allow url

--- packages/test/test-end-to-end-tests/.eslintrc.js
+++ packages/test/test-end-to-end-tests/.eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
     "rules": {
         "prefer-arrow-callback": "off",
         "@typescript-eslint/strict-boolean-expressions": "off", // requires strictNullChecks=true in tsconfig
+
+        // This library is used in the browser, so we don't want dependencies on most node libraries.
+        "import/no-nodejs-modules": ["error", {"allow": ["url"]}],
     },
     "parserOptions": {
         "project": [ "./src/test/tsconfig.json" ]
