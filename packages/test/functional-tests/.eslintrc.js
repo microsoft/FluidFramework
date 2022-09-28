b75422e5f4954863e7b9b9842bb36b6212fc773d
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2022-09-28 20:51:28 +0000

    meeting with tyler

--- packages/test/functional-tests/.eslintrc.js
+++ packages/test/functional-tests/.eslintrc.js
@@ -4,11 +4,19 @@
  */
 
 module.exports = {
-    "extends": [
-        require.resolve("@fluidframework/eslint-config-fluid")
-    ],
-    "rules": {
+    extends: [require.resolve("@fluidframework/eslint-config-fluid")],
+    rules: {
         // This library is used in the browser, so we don't want dependencies on most node libraries.
-        "import/no-nodejs-modules": ["error", {"allow": ["events"]}],
-    }
-}
+        "import/no-nodejs-modules": ["error", { allow: ["events"] }],
+    },
+    overrides: [
+        {
+            // Rules only for test files
+            files: ["*.spec.ts", "src/test/**"],
+            rules: {
+                // This library is used in the browser, so we don't want dependencies on most node libraries.
+                "import/no-nodejs-modules": ["error", { allow: ["assert","events"] }],
+            },
+        },
+    ],
+};
