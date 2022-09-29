046de288f7186a34406f966609913ac7c272a77d
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2022-09-29 15:16:08 +0000

    fetch-tool does not have tests

--- packages/test/test-end-to-end-tests/.eslintrc.js
+++ packages/test/test-end-to-end-tests/.eslintrc.js
@@ -4,17 +4,28 @@
  */
 
 module.exports = {
-    "extends": [
-        require.resolve("@fluidframework/eslint-config-fluid")
-    ],
-    "rules": {
+    extends: [require.resolve("@fluidframework/eslint-config-fluid")],
+    rules: {
         "prefer-arrow-callback": "off",
         "@typescript-eslint/strict-boolean-expressions": "off", // requires strictNullChecks=true in tsconfig
 
         // This library is used in the browser, so we don't want dependencies on most node libraries.
-        "import/no-nodejs-modules": ["error", {"allow": ["url"]}],
+        "import/no-nodejs-modules": ["error", { allow: ["url"] }],
     },
-    "parserOptions": {
-        "project": [ "./src/test/tsconfig.json" ]
-    }
-}
+    parserOptions: {
+        project: ["./src/test/tsconfig.json"],
+    },
+    overrides: [
+        {
+            // Rules only for test files
+            files: ["*.spec.ts", "src/test/**"],
+            rules: {
+                // This library is used in the browser, so we don't want dependencies on most node libraries.
+                "import/no-nodejs-modules": [
+                    "error",
+                    { allow: ["assert", "url"] },
+                ],
+            },
+        },
+    ],
+};
