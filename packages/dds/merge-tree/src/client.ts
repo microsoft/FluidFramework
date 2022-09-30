959c32fa832fbb4bf9c3b8ac91d1d15526819bb7
Abram Sanderson <absander@microsoft.com>
2022-09-30 15:25:42 -0700

    fix lint

--- packages/dds/merge-tree/src/client.ts
+++ packages/dds/merge-tree/src/client.ts
@@ -68,8 +68,7 @@ function elapsedMicroseconds(trace: Trace) {
 }
 
 /**
- * @internal
- * Emitted before this client's merge-tree normalizes its segments on reconnect, potentially
+ * @internal - Emitted before this client's merge-tree normalizes its segments on reconnect, potentially
  * ordering them. Useful for DDS-like consumers built atop the merge-tree to compute any information
  * they need for rebasing their ops on reconnection.
  */
