b77dcba3e144b53a2704cdd4404d2ed4bf69e92e
Vlad Sudzilouski <vlad@sudzilouski.com>
2022-10-04 11:39:23 -0700

    Back compat

--- packages/loader/driver-utils/src/test/types/validateDriverUtilsPrevious.ts
+++ packages/loader/driver-utils/src/test/types/validateDriverUtilsPrevious.ts
@@ -618,6 +618,18 @@ use_old_InterfaceDeclaration_IProgress(
 /*
 * Validate forward compat by using old type in place of current type
 * If breaking change required, add in package.json under typeValidation.broken:
+* "RemovedFunctionDeclaration_isClientMessage": {"forwardCompat": false}
+*/
+
+/*
+* Validate back compat by using current type in place of old type
+* If breaking change required, add in package.json under typeValidation.broken:
+* "RemovedFunctionDeclaration_isClientMessage": {"backCompat": false}
+*/
+
+/*
+* Validate forward compat by using old type in place of current type
+* If breaking change required, add in package.json under typeValidation.broken:
 * "VariableDeclaration_isFluidResolvedUrl": {"forwardCompat": false}
 */
 declare function get_old_VariableDeclaration_isFluidResolvedUrl():
