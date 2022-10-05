397d6aca2d6fcfa7e46c1ed7be9219b17f5ab895
Tyler Butler <tyler@tylerbutler.com>
2022-10-05 10:11:30 -0700

    policy

--- build-tools/.cz-config.js
+++ build-tools/.cz-config.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 module.exports = {
     types: [
         { value: 'feat', name: 'feat:     A new feature' },
