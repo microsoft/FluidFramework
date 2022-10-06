f339ccea618301e1ba2f1ce9f47bad07b60703f1
Wayne Ferrao <wayne.ferrao93@gmail.com>
2022-10-06 20:48:06 +0000

    Fixed linting issues

--- common/lib/common-utils/src/idleTaskScheduler.ts
+++ common/lib/common-utils/src/idleTaskScheduler.ts
@@ -9,19 +9,19 @@ import { ITelemetryProperties } from "@fluidframework/common-definitions";
  * API used to schedule non-essential tasks
  * Time measurements are in milliseconds as a floating point with a decimal
  */
-export default class IdleTaskScheduler {
+export class IdleTaskScheduler {
   /*
   Takes in and runs a callback during idle time. Fallback to setTimeout if window doesn't
   support requestIdleCallback.
   @returns A promise pertaining to the callback that was passed in.
   */
-  public scheduleIdleTask<T>(callback: () => T, timeout: number, props: ITelemetryProperties = {}): Promise<T> {
-    let promise;
-    //Check for the availability in window.
+  public async scheduleIdleTask<T>(callback: () => T, timeout: number, props: ITelemetryProperties = {}): Promise<T> {
+    // let promise;
+    // Check for the availability in window.
     if (typeof window?.requestIdleCallback === "function") {
-      promise = new Promise((resolve, reject) => {
-        requestIdleCallback(doLowPriorityTask, { timeout: timeout });
-        function doLowPriorityTask(deadline) {
+      return  new Promise<T>((resolve, reject) => {
+        requestIdleCallback(doLowPriorityTask, { timeout });
+        function doLowPriorityTask(deadline): void {
           try {
             resolve(callback());
           } catch (err: any) {
@@ -32,7 +32,7 @@ export default class IdleTaskScheduler {
       });
     }
     else {
-      promise = new Promise((resolve, reject) => {
+      return new Promise<T>((resolve, reject) => {
         setTimeout(() => {
           try {
             resolve(callback())
@@ -42,7 +42,6 @@ export default class IdleTaskScheduler {
         }, timeout);
       });
     }
-    return promise;
   }
 
 }
