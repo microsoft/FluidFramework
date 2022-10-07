45fe833b47d269cac0d9e1ebbf81db2eca2a53ad
Wayne Ferrao <wayne.ferrao93@gmail.com>
2022-10-07 17:59:36 +0000

    PR fixes

--- common/lib/common-utils/src/idleTaskScheduler.ts
+++ common/lib/common-utils/src/idleTaskScheduler.ts
@@ -3,45 +3,31 @@
  * Licensed under the MIT License.
  */
 
-import { ITelemetryProperties } from "@fluidframework/common-definitions";
-
 /**
  * API used to schedule non-essential tasks
  * Time measurements are in milliseconds as a floating point with a decimal
- */
-export class IdleTaskScheduler {
-  /*
-  Takes in and runs a callback during idle time. Fallback to setTimeout if window doesn't
-  support requestIdleCallback.
-  @returns A promise pertaining to the callback that was passed in.
+ * Takes in and runs a callback during idle time. Fallback to setTimeout if window doesn't
+ * support requestIdleCallback.
+  * @returns A promise pertaining to the callback that was passed in.
   */
-  public async scheduleIdleTask<T>(callback: () => T, timeout: number, props: ITelemetryProperties = {}): Promise<T> {
-    // let promise;
-    // Check for the availability in window.
-    if (typeof window?.requestIdleCallback === "function") {
-      return  new Promise<T>((resolve, reject) => {
-        requestIdleCallback(doLowPriorityTask, { timeout });
-        function doLowPriorityTask(deadline): void {
-          try {
-            resolve(callback());
-          } catch (err: any) {
-            props.responseMessage = err.message;
-            reject(err);
-          }
-        }
-      });
+export async function scheduleIdleTask<T>(callback: () => T, timeout: number): Promise<T> {
+  // Check for the availability in window.
+  return typeof window?.requestIdleCallback === "function" ? new Promise<T>((resolve, reject) => {
+    function doLowPriorityTask(deadline): void {
+      try {
+        resolve(callback());
+      } catch (err: any) {
+        reject(err);
+      }
     }
-    else {
-      return new Promise<T>((resolve, reject) => {
-        setTimeout(() => {
-          try {
-            resolve(callback())
-          } catch (e) {
-            reject(e);
-          }
-        }, timeout);
-      });
-    }
-  }
-
+    requestIdleCallback(doLowPriorityTask, { timeout });
+  }) : new Promise<T>((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        resolve(callback())
+      } catch (e) {
+        reject(e);
+      }
+    }, timeout);
+  });
 }
