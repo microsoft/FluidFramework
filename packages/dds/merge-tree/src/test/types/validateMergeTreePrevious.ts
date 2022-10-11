0b9e2bc35dd8f3500ec2ef8749574f25c714317e
Abram Sanderson <absander@microsoft.com>
2022-10-11 11:03:03 -0700

    fix type compat tests

--- packages/dds/merge-tree/src/test/types/validateMergeTreePrevious.ts
+++ packages/dds/merge-tree/src/test/types/validateMergeTreePrevious.ts
@@ -132,6 +132,7 @@ declare function get_old_ClassDeclaration_Client():
 declare function use_current_ClassDeclaration_Client(
     use: TypeOnly<current.Client>);
 use_current_ClassDeclaration_Client(
+    // @ts-expect-error compatibility expected to be broken
     get_old_ClassDeclaration_Client());
 
 /*
@@ -3008,6 +3009,7 @@ declare function get_old_InterfaceDeclaration_SegmentGroup():
 declare function use_current_InterfaceDeclaration_SegmentGroup(
     use: TypeOnly<current.SegmentGroup>);
 use_current_InterfaceDeclaration_SegmentGroup(
+    // @ts-expect-error compatibility expected to be broken
     get_old_InterfaceDeclaration_SegmentGroup());
 
 /*
