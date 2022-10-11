2e4ed46724ee0fd9e7f3613010e7b4e9b0cb7541
Kajari Ghosh <ghoshkaj@gmail.com>
2022-10-11 21:43:23 +0000

    ordered-collection

--- packages/dds/ordered-collection/src/index.ts
+++ packages/dds/ordered-collection/src/index.ts
@@ -3,7 +3,15 @@
  * Licensed under the MIT License.
  */
 
-export * from "./consensusOrderedCollection";
-export * from "./consensusQueue";
-export * from "./interfaces";
-export * from "./testUtils";
+export {
+	ConsensusCallback,
+	ConsensusResult,
+	IConsensusOrderedCollection,
+	IConsensusOrderedCollectionEvents,
+	IConsensusOrderedCollectionFactory,
+	IOrderedCollection,
+	ISnapshotable,
+} from "./interfaces";
+export { ConsensusOrderedCollection } from "./consensusOrderedCollection";
+export { ConsensusQueue } from "./consensusQueue";
+export { acquireAndComplete, waitAcquireAndComplete } from "./testUtils";
