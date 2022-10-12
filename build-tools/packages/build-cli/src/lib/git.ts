78e2f480b7d08f0380d7c9d6a85a2b84b92cab2c
Tyler Butler <tyler@tylerbutler.com>
2022-10-12 15:28:59 -0700

    fixes

--- build-tools/packages/build-cli/src/lib/git.ts
+++ build-tools/packages/build-cli/src/lib/git.ts
@@ -1,7 +1,15 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { SimpleGit, SimpleGitOptions, simpleGit } from "simple-git";
 
 import { CommandLogger } from "../logging";
 
+/**
+ * Default options passed to the git client.
+ */
 const defaultGitOptions: Partial<SimpleGitOptions> = {
     baseDir: process.cwd(),
     binary: "git",
@@ -9,21 +17,39 @@ const defaultGitOptions: Partial<SimpleGitOptions> = {
     trimmed: true,
 };
 
+/**
+ * A small wrapper around a git repo to provide API access to it.
+ *
+ * @remarks
+ *
+ * Eventually this should replace the legacy GitRepo class in build-tools. That class exec's git commands directly,
+ * while this class uses a library wrapper around git instead.
+ *
+ * @internal
+ */
 export class Repository {
     private readonly git: SimpleGit;
 
+    /**
+     * A git client for the repository that can be used to call git directly.
+     *
+     * @internal
+     */
     public get gitClient(): SimpleGit {
         return this.git;
     }
 
     constructor(
-        public readonly resolvedRoot: string,
+        public readonly rootPath: string,
         gitOptions?: Partial<SimpleGitOptions>,
         protected readonly log?: CommandLogger,
     ) {
         this.git = simpleGit(gitOptions ?? defaultGitOptions);
     }
 
+    /**
+     * Returns the SHA hash for a branch. If a remote is provided, the SHA for the remote ref is returned.
+     */
     public async getShaForBranch(branch: string, remote?: string): Promise<string> {
         const refspec =
             remote === undefined ? `refs/heads/${branch}` : `refs/remotes/${remote}/${branch}`;
@@ -33,16 +59,14 @@ export class Repository {
     }
 
     /**
-     * Get the remote based on the partial Url.
-     * It will match the first remote that contains the partialUrl case insensitively.
+     * Get the remote based on the partial Url. It will match the first remote that contains the partialUrl case
+     * insensitively.
      *
      * @param partialUrl - partial url to match case insensitively
      */
     public async getRemote(partialUrl: string): Promise<string | undefined> {
         const lowerPartialUrl = partialUrl.toLowerCase();
         const remotes = await this.git.getRemotes(/* verbose */ true);
-
-        // this.log.info(JSON.stringify(remotes));
 
         for (const r of remotes) {
             if (r.refs.fetch.toLowerCase().includes(lowerPartialUrl)) {
