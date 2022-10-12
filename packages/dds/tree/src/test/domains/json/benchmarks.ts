dbec363a7c85a4e4601489a6000e1d8d6d098dae
Jenn <jennle@microsoft.com>
2022-10-12 13:48:55 -0700

    Update packages/dds/tree/src/test/domains/json/benchmarks.ts
    Co-authored-by: Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>

--- packages/dds/tree/src/test/domains/json/benchmarks.ts
+++ packages/dds/tree/src/test/domains/json/benchmarks.ts
@@ -24,7 +24,7 @@ export function sumMap(cursor: ITreeCursorNew): number {
         total += value;
     }
 
-    forEachField(cursor, () => forEachNode(cursor, sumMap));
+    forEachField(cursor, () => forEachNode(cursor, (c) => { total += sumMap(c); }));
 
     return total;
 }
