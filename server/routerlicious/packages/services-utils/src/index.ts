3ad250444e9ad6f1acaa806ba024173e00a11792
Kajari Ghosh <kajarighosh@microsoft.com>
2022-10-13 09:21:05 -0400

    Change all `export *` in `server/routerlicious/packages` to named exports  (#12433)
    This PR converts all `export *` in `server/routerlicious/packages` to named exports.
    Related issue: https://github.com/microsoft/FluidFramework/issues/10062
    Bundle size does not change when the entire repo is converted: https://github.com/microsoft/FluidFramework/pull/12321#issue-1400290954.

--- server/routerlicious/packages/services-utils/src/index.ts
+++ server/routerlicious/packages/services-utils/src/index.ts
@@ -3,17 +3,29 @@
  * Licensed under the MIT License.
  */
 
-export * from "./asyncLocalStorage";
-export * from "./auth";
-export * from "./conversion";
-export * from "./deleteSummarizedOps";
-export * from "./dns";
-export * from "./errorUtils";
-export * from "./executeOnInterval";
-export * from "./generateNames";
-export * from "./logger";
-export * from "./morganLoggerMiddleware";
-export * from "./port";
-export * from "./redisUtils";
-export * from "./throttlerMiddleware";
-export * from "./winstonLumberjackEngine";
+export { bindCorrelationId, getCorrelationId, getCorrelationIdWithHttpFallback } from "./asyncLocalStorage";
+export {
+	generateToken,
+	generateUser,
+	getCreationToken,
+	getParam,
+	respondWithNetworkError,
+	validateTokenClaims,
+	verifyStorageToken,
+} from "./auth";
+export { parseBoolean } from "./conversion";
+export { deleteSummarizedOps } from "./deleteSummarizedOps";
+export { getHostIp } from "./dns";
+export { FluidServiceError, FluidServiceErrorCode } from "./errorUtils";
+export { executeOnInterval, ScheduledJob } from "./executeOnInterval";
+export { choose, getRandomName } from "./generateNames";
+export { configureLogging, IWinstonConfig } from "./logger";
+export { alternativeMorganLoggerMiddleware, jsonMorganLoggerMiddleware } from "./morganLoggerMiddleware";
+export { normalizePort } from "./port";
+export {
+	executeRedisMultiWithHmsetExpire,
+	executeRedisMultiWithHmsetExpireAndLpush,
+	IRedisParameters,
+} from "./redisUtils";
+export { IThrottleMiddlewareOptions, throttle } from "./throttlerMiddleware";
+export { WinstonLumberjackEngine } from "./winstonLumberjackEngine";
