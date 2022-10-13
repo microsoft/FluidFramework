a01a7e4c888329ccabefca67bd4dc3ad77c0add8
Kajari Ghosh <kajarighosh@microsoft.com>
2022-10-13 09:20:07 -0400

    Change all `export *` in `server/historian` and `server/tinylicious` to named exports (#12436)
    This PR converts all `export *` in `server/historian` and `server/tinylicious` to named exports.
    Related issue: https://github.com/microsoft/FluidFramework/issues/10062

--- server/tinylicious/src/services/index.ts
+++ server/tinylicious/src/services/index.ts
@@ -3,8 +3,8 @@
  * Licensed under the MIT License.
  */
 
-export * from "./dbFactory";
-export * from "./pubSubPublisher";
-export * from "./taskMessageSender";
-export * from "./tenantManager";
-export * from "./webServerFactory";
+export { getDbFactory } from "./dbFactory";
+export { PubSubPublisher } from "./pubSubPublisher";
+export { TaskMessageSender } from "./taskMessageSender";
+export { TenantManager, TinyliciousTenant } from "./tenantManager";
+export { WebServerFactory } from "./webServerFactory";
