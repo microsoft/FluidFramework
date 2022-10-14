d81d333c7bb7c71c861e4ef7169e7ecc4cce4081
Mark Fields <markfields@users.noreply.github.com>
2022-10-14 21:52:03 +0000

    Revert interface changes, for release branch port

--- packages/framework/agent-scheduler/src/agent.ts
+++ packages/framework/agent-scheduler/src/agent.ts
@@ -4,7 +4,6 @@
  */
 
 import { IEvent, IEventProvider } from "@fluidframework/common-definitions";
-import { IFluidLoadable } from "@fluidframework/core-interfaces";
 
 export const IAgentScheduler: keyof IProvideAgentScheduler = "IAgentScheduler";
 
@@ -36,7 +35,7 @@ export interface IAgentSchedulerEvents extends IEvent {
 /**
  * Agent scheduler distributes a set of tasks/variables across connected clients.
  */
-export interface IAgentScheduler extends IProvideAgentScheduler, IEventProvider<IAgentSchedulerEvents>, IFluidLoadable {
+export interface IAgentScheduler extends IProvideAgentScheduler, IEventProvider<IAgentSchedulerEvents> {
     /**
      * Registers a set of new tasks to distribute amongst connected clients. Only use this if a client wants
      * a new agent to run but does not have the capability to run the agent inside the host.
