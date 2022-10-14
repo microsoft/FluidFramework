de22d5c1f64254f2550c604d7f00831797ca075a
Sasha Simic <simic.sasa@gmail.com>
2022-10-14 12:13:06 -0700

    Update azure/packages/test/scenario-runner/src/TestOrchestrator.ts
    Co-authored-by: Scott Norton <scottnorton@microsoft.com>

--- azure/packages/test/scenario-runner/src/TestOrchestrator.ts
+++ azure/packages/test/scenario-runner/src/TestOrchestrator.ts
@@ -117,7 +117,7 @@ export class TestOrchestrator {
             const runner = this.createRunner(stage);
             if (runner) {
                 try {
-                    console.log("starting stage", stage.name);
+                    console.log("Starting stage:", stage.name);
                     const r = await this.runStage(runner, stage);
                     if (r !== undefined && stage.out !== undefined) {
                         this.env.set(stage.out, r);
