745ccf199e48fb49c8f6a4254d6c7a6b4a6cb6e6
Yann Achard <achardy@microsoft.com>
2022-10-14 17:49:37 -0700

    Better test

--- packages/dds/tree/src/test/feature-libraries/editManagerIndex.spec.ts
+++ packages/dds/tree/src/test/feature-libraries/editManagerIndex.spec.ts
@@ -10,13 +10,58 @@ import { loadSummary, encodeSummary, commitEncoderFromChangeEncoder, } from "../
 
 import { MutableSummaryData, ReadonlySummaryData } from "../../edit-manager";
 import { TestChange } from "../testChange";
+import { brand } from "../../util";
 
 describe("EditManagerIndex", () => {
     it("roundtrip", () => {
         const encoder = commitEncoderFromChangeEncoder(TestChange.encoder);
         const input: ReadonlySummaryData<TestChange> = {
-            trunk: [],
-            branches: new Map([]),
+            trunk: [
+                {
+                    seqNumber: brand(1),
+                    refNumber: brand(0),
+                    sessionId: "1",
+                    changeset: TestChange.mint([0], 1),
+                },
+                {
+                    seqNumber: brand(2),
+                    refNumber: brand(1),
+                    sessionId: "2",
+                    changeset: TestChange.mint([0, 1], 2),
+                },
+            ],
+            branches: new Map([
+                [
+                    "3",
+                    {
+                        isDivergent: false,
+                        refSeq: brand(0),
+                        localChanges: [
+                            {
+                                seqNumber: brand(3),
+                                refNumber: brand(0),
+                                sessionId: "3",
+                                changeset: TestChange.mint([0], 3),
+                            },
+                        ],
+                    },
+                ],
+                [
+                    "4",
+                    {
+                        isDivergent: true,
+                        refSeq: brand(1),
+                        localChanges: [
+                            {
+                                seqNumber: brand(4),
+                                refNumber: brand(1),
+                                sessionId: "4",
+                                changeset: TestChange.mint([0, 1], 4),
+                            },
+                        ],
+                    },
+                ],
+            ]),
         };
         const output: MutableSummaryData<TestChange> = {
             trunk: [],
@@ -24,6 +69,7 @@ describe("EditManagerIndex", () => {
         };
         const s1 = encodeSummary(input, encoder);
         loadSummary(s1, encoder, output);
+        assert.deepEqual(output, input);
         const s2 = encodeSummary(output, encoder);
         assert.equal(s1, s2);
     });
