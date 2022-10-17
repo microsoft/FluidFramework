5508e1d6dbe0c9798f1e53c6b0fdcd0af402e167
jikim-msft <111468570+jikim-msft@users.noreply.github.com>
2022-10-17 23:08:55 +0000

    Revert "remove dupliate config"
    This reverts commit 923ef20331d3591af0007ecce0dc2ef418fe5956.

--- common/build/eslint-config-fluid/minimal.js
+++ common/build/eslint-config-fluid/minimal.js
@@ -86,6 +86,8 @@ module.exports = {
         "max-len": [
             "error",
             {
+                ignoreRegExpLiterals: false,
+                ignoreStrings: false,
                 code: 120,
                 ignoreTrailingComments: true,
                 ignoreUrls: true,
