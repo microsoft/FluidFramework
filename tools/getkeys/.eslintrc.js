c91b543de046265f93545bacc59e9d05ed23979c
Sasha Simic <simic.sasa@gmail.com>
2022-10-17 12:33:09 -0700

    azure client dep bump. (#12519)
    Azure-client dependency bump. Now consuming "1.3.0-100520".

---
+++ tools/getkeys/.eslintrc.js
@@ -0,0 +1,29 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = {
+    "extends": [
+        require.resolve("@fluidframework/eslint-config-fluid")
+    ],
+    "parserOptions": {
+        project: ["./tsconfig.json"],
+    },
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "rules": {
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
+        "@typescript-eslint/restrict-plus-operands": "off",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "promise/param-names": "off"
+    }
+};
