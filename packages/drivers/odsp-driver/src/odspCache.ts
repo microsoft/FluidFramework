58b2b7012b59b57c79807b1de627dd04e06981e9
Mark Fields <markfields@users.noreply.github.com>
2022-10-18 18:29:20 +0000

    Trying to rebase

--- packages/drivers/odsp-driver/src/odspCache.ts
+++ packages/drivers/odsp-driver/src/odspCache.ts
@@ -2,6 +2,8 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
+import { IDisposable } from "@fluidframework/common-definitions";
 import { PromiseCache } from "@fluidframework/common-utils";
 import {
     IOdspResolvedUrl,
@@ -21,15 +23,113 @@ export interface IPersistedFileCache {
 }
 
 /**
+ * Handles garbage collection of expiring cache entries.
+ * Not exported.
+ * (Based off of the same class in promiseCache.ts, could be consolidated)
+ */
+// class GarbageCollector<TKey> {
+//     private readonly gcTimeouts = new Map<TKey, ReturnType<typeof setTimeout>>();
+
+//     constructor(
+//         private readonly cleanup: (key: TKey) => void,
+//     ) { }
+
+//     /**
+//      * Schedule GC for the given key, as applicable
+//      */
+//     public schedule(key: TKey, durationMs: number) {
+//         this.gcTimeouts.set(
+//             key,
+//             setTimeout(
+//                 () => { this.cleanup(key); this.cancel(key); },
+//                 durationMs,
+//             ),
+//         );
+//     }
+
+//     /**
+//      * Cancel any pending GC for the given key
+//      */
+//     public cancel(key: TKey) {
+//         const timeout = this.gcTimeouts.get(key);
+//         if (timeout !== undefined) {
+//             clearTimeout(timeout);
+//             this.gcTimeouts.delete(key);
+//         }
+//     }
+// }
+
+class MapWithExpiration<TKey, TValue> extends Map<TKey, TValue> implements IDisposable {
+    public disposed: boolean = false;
+    private readonly expirationTimeouts = new Map<TKey, ReturnType<typeof setTimeout>>();
+
+    constructor(
+        private readonly expiryMs: number,
+    ) {
+        super();
+    }
+    private scheduleExpiration(key: TKey) {
+        this.expirationTimeouts.set(
+            key,
+            setTimeout(
+                () => { this.delete(key); },
+                this.expiryMs,
+            ),
+        );
+    }
+
+    private cancelExpiration(key: TKey) {
+        const timeout = this.expirationTimeouts.get(key);
+        if (timeout !== undefined) {
+            clearTimeout(timeout);
+            this.expirationTimeouts.delete(key);
+        }
+    }
+
+    get(key: TKey): TValue | undefined {
+        return super.get(key);
+    }
+
+    set(key: TKey, value: TValue): this {
+        // Sliding window expiration policy (on write)
+        this.cancelExpiration(key);
+        this.scheduleExpiration(key);
+
+        return super.set(key, value);
+    }
+
+    delete(key: TKey): boolean {
+        this.cancelExpiration(key);
+        return super.delete(key);
+    }
+
+    dispose(_error?: Error): void {
+        if (this.disposed) {
+            return;
+        }
+        this.disposed = true;
+        Array.from(this).forEach(([key]) => this.cancelExpiration(key));
+    }
+}
+
+/**
  * Default local-only implementation of IPersistedCache,
  * used if no persisted cache is provided by the host
  */
-export class LocalPersistentCache implements IPersistedCache {
-    private readonly cache = new Map<string, any>();
+export class LocalPersistentCache implements IPersistedCache, IDisposable {
+    public get disposed(): boolean { return this.cache.disposed; }
+    private readonly cache: MapWithExpiration<string, any>;
+
     // For every document id there will be a single expiration entry inspite of the number of cache entries.
     private readonly docIdExpirationMap = new Map<string, ReturnType<typeof setTimeout>>();
 
-    public constructor(private readonly snapshotExpiryPolicy = 3600 * 1000) { }
+    public constructor(snapshotExpiryPolicy = 30 * 1000) {
+        this.cache = new MapWithExpiration<string, any>(snapshotExpiryPolicy);
+    }
+
+    public dispose(error?: Error): void {
+        this.cache.dispose(error);
+    }
 
     async get(entry: ICacheEntry): Promise<any> {
         const key = this.keyFromEntry(entry);
@@ -40,6 +140,7 @@ export class LocalPersistentCache implements IPersistedCache {
     async put(entry: ICacheEntry, value: any) {
         const key = this.keyFromEntry(entry);
         this.cache.set(key, value);
+        //*
         this.updateExpirationEntry(entry.file.docId);
     }
 
