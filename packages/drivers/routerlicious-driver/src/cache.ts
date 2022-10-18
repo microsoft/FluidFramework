6814a8785abcd89cc060c4b2ba71b1547ea50eb2
Mark Fields <markfields@users.noreply.github.com>
2022-10-18 19:53:10 +0000

    Fix IDisposed impl

--- packages/drivers/routerlicious-driver/src/cache.ts
+++ packages/drivers/routerlicious-driver/src/cache.ts
@@ -15,7 +15,7 @@ export interface ICache<T> extends IDisposable {
 const fiveDaysMs: FiveDaysMs = 432000000;
 
 export class InMemoryCache<T> implements ICache<T> {
-    public readonly disposed: boolean = false;
+    public get disposed(): boolean { return this.cache.disposed; }
     public dispose() { this.cache.dispose(); }
 
     private readonly cache: MapWithExpiration<string, T> = new MapWithExpiration(fiveDaysMs);
@@ -30,8 +30,8 @@ export class InMemoryCache<T> implements ICache<T> {
 }
 
 export class NullCache<T> implements ICache<T> {
-    public readonly disposed: boolean = false;
-    public dispose() {}
+    public disposed: boolean = false;
+    public dispose() { this.disposed = true; }
 
     public async get(key: string): Promise<T | undefined> {
         return undefined;
