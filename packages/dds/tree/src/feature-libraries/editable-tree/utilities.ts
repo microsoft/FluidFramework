bc032c754230de614c44fa9cca6f6fd57c426ed1
Tyler Butler <tyler@tylerbutler.com>
2022-10-19 16:24:15 +0000

    revert

--- packages/dds/tree/src/feature-libraries/editable-tree/utilities.ts
+++ packages/dds/tree/src/feature-libraries/editable-tree/utilities.ts
@@ -68,7 +68,7 @@ export function getFieldSchema(
     if (isGlobalFieldKey(field)) {
         return lookupGlobalFieldSchema(schemaData, keyFromSymbol(field));
     }
-    assert(schema !== undefined, 0x423 /* The field is a local field, a parent schema is required. */);
+    assert(schema !== undefined, "The field is a local field, a parent schema is required.");
     return schema.localFields.get(field) ?? schema.extraLocalFields;
 }
 
