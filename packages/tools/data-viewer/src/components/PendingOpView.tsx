512bcdafb42e162a11707a4968d5d6d5dee8dbb0
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-10-20 23:40:25 +0000

    build: Add placeholder PendingOpView

---
+++ packages/tools/data-viewer/src/components/PendingOpView.tsx
@@ -0,0 +1,64 @@
+import { Stack } from "@fluentui/react";
+import React from "react";
+import ReactJson from "react-json-view";
+
+import { IPendingMessage } from "@fluidframework/container-runtime";
+
+import { Accordion } from "./Accordion";
+
+// TODOs:
+// - Copy raw data to clipboard button (outside of json viewer)
+// - Raw data as modal dialogue rather than drop-down
+
+/**
+ * {@link OpView} input props.
+ */
+export interface OpViewProps {
+    /**
+     * The op (message) to render.
+     */
+    message: IPendingMessage;
+}
+
+/**
+ * Simple view for a single op (message).
+ */
+export function OpView(props: OpViewProps): React.ReactElement {
+    const { message } = props;
+
+    const header = (
+        <Stack>
+            <div>
+                <b>Pending Op #{message.clientSequenceNumber}</b>:{" "}
+            </div>
+        </Stack>
+    );
+
+    let dataView: React.ReactElement = <></>;
+    if (message.content !== undefined) {
+        /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument */
+        const json = JSON.parse(message.content);
+        dataView = (
+            <Accordion header={<b>Raw Data</b>}>
+                <ReactJson src={json} />
+            </Accordion>
+        );
+        /* eslint-enable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument */
+    }
+
+    return (
+        <Accordion header={header}>
+            <Stack>
+                <div>
+                    <b>Type: </b>
+                    {message.type}
+                </div>
+                <div>
+                    <b>Reference sequence number: </b>
+                    {message.referenceSequenceNumber}
+                </div>
+                {dataView}
+            </Stack>
+        </Accordion>
+    );
+}
