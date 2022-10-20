efffaf9b5db28d490dc60e83de8086093ad206da
tyler-cai-microsoft <tylercai@microsoft.com>
2022-10-19 17:56:30 -0700

    Add send and receive tests

--- packages/test/test-end-to-end-tests/src/test/gc/gcTombstone.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/gc/gcTombstone.spec.ts
@@ -42,7 +42,7 @@ class TestDataObject extends DataObject {
 /**
  * Validates this scenario: When a datastore should be tombstoned that tombstoned and unable to send ops
  */
-describeNoCompat("GC DataStore Tombstoned", (getTestObjectProvider) => {
+describeNoCompat("GC DataStore Tombstoned When It Is Sweep Ready", (getTestObjectProvider) => {
     let provider: ITestObjectProvider;
     const dataObjectFactory = new DataObjectFactory(
         "TestDataObject",
@@ -50,14 +50,13 @@ describeNoCompat("GC DataStore Tombstoned", (getTestObjectProvider) => {
         [],
         [],
     );
-    const inactiveTimeoutMs = 1;
-    const sessionExpiryTimeoutMs = 200;
-    const sweepTimeoutMs = sessionExpiryTimeoutMs + 10;
+    const inactiveTimeoutMs = 0;
+    const wait = 100;
+    const sweepTimeoutMs = 200;
 
     const gcOptions: IGCRuntimeOptions = {
         gcAllowed: true,
         sweepAllowed: true,
-        sessionExpiryTimeoutMs,
         snapshotCacheExpiryMs: 0,
         inactiveTimeoutMs,
     };
@@ -89,7 +88,6 @@ describeNoCompat("GC DataStore Tombstoned", (getTestObjectProvider) => {
     );
 
     const settings = {
-        "Fluid.GarbageCollection.RunSessionExpiry": "true",
         "Fluid.GarbageCollection.Test.Tombstone": "true",
         "Fluid.GarbageCollection.TestOverride.SweepTimeoutMs": sweepTimeoutMs,
     };
@@ -114,47 +112,52 @@ describeNoCompat("GC DataStore Tombstoned", (getTestObjectProvider) => {
         await waitForContainerConnection(mainContainer);
     });
 
-    // If this test starts failing due to runtime is closed errors try first adjusting `sessionExpiryTimeoutMs` above
-    itExpects("GC tombstones datastores when they are sweep ready.",
-    [
-        {
-            eventName: "fluid:telemetry:Container:ContainerClose",
-            errorType: "clientSessionExpiredError",
-        },
-        {
-            eventName: "fluid:telemetry:Container:ContainerClose",
-            errorType: "clientSessionExpiredError",
-        },
-    ],
-    async () => {
+    const createUnreferencedDataStore = async () => {
         const handleKey = "handle";
         const testDataObject = await dataObjectFactory.createInstance(mainDataStore.containerRuntime);
         const testDataObjectId = testDataObject.id;
 
         // Reference a datastore and summarize
         mainDataStore._root.set(handleKey, testDataObject.handle);
-        const summarizer = (await loadSummarizerAndContainer()).summarizer;
-        await summarize(summarizer);
+        const {
+            container: summarizingContainer1,
+            summarizer: summarizer1,
+        } = await loadSummarizerAndContainer();
+        await summarize(summarizer1);
 
         // Unreference a datastore and summarize
         mainDataStore._root.delete(handleKey);
-        const summaryVersion = (await summarize(summarizer)).summaryVersion;
+        const summaryVersion = (await summarize(summarizer1)).summaryVersion;
+        testDataObject._root.set("send while unreferenced", "op");
+        await provider.ensureSynchronized();
+        mainContainer.close();
+        summarizingContainer1.close();
+
+        return {
+            testDataObjectId,
+            summaryVersion,
+        };
+    };
+
+    // If this test starts failing due to runtime is closed errors try first adjusting `sessionExpiryTimeoutMs` above
+    it("Container loaded after sweep timeout expires can't send ops for tombstoned datastores", async () => {
+        const { testDataObjectId, summaryVersion } = await createUnreferencedDataStore();
 
         // Wait a sweep worthy amount of time (all containers should have closed by now)
         await delay(sweepTimeoutMs);
 
         // Load a new container and summarizer based on the latest summary, summarize
         const {
-            container: container2,
-            summarizer: closingSummarizer,
+            container: summarizingContainer2,
+            summarizer: summarizer2,
         } = await loadSummarizerAndContainer(summaryVersion);
 
         // Use the request pattern to get the testDataObject - this is unsafe and no one should do this in their
         // production application
-        const testDataObject2 = await requestFluidObject<TestDataObject>(container2, testDataObjectId);
+        const testDataObject2 = await requestFluidObject<TestDataObject>(summarizingContainer2, testDataObjectId);
 
         // The datastore should be tombstoned now
-        await summarize(closingSummarizer);
+        await summarize(summarizer2);
 
         // The request pattern should fail!
         assert.throws(() => testDataObject2._root.set("send", "op"),
@@ -164,5 +167,125 @@ describeNoCompat("GC DataStore Tombstoned", (getTestObjectProvider) => {
                 return correctErrorType && correctErrorMessage;
             },
             `Should not be able to send ops for a tombstoned datastore.`);
+    });
+
+    // If this test starts failing due to runtime is closed errors try first adjusting `sessionExpiryTimeoutMs` above
+    it("Container loaded before sweep timeout expires can't send ops for tombstoned datastores", async () => {
+        const { testDataObjectId, summaryVersion } = await createUnreferencedDataStore();
+
+        // Wait some time, the datastore should not be sweep ready after this wait
+        await delay(sweepTimeoutMs - wait);
+
+        // Load a new container and summarizer based on the latest summary, summarize
+        const {
+            container: summarizingContainer2,
+            summarizer: summarizer2,
+        } = await loadSummarizerAndContainer(summaryVersion);
+
+        // Use the request pattern to get the testDataObject - this is unsafe and no one should do this in their
+        // production application.
+        const testDataObject2 = await requestFluidObject<TestDataObject>(summarizingContainer2, testDataObjectId);
+
+        // Wait enough time so that the datastore is sweep ready
+        await delay(wait);
+
+        // Send an op to update the currentTimestampMs to now
+        const mainDataStore2 = await requestFluidObject<TestDataObject>(summarizingContainer2, "default");
+        mainDataStore2._root.set("send a", "op");
+
+        // The datastore should be tombstoned now
+        await summarize(summarizer2);
+
+        // The request pattern should fail!
+        assert.throws(() => testDataObject2._root.set("send", "op"),
+            (error) => {
+                const correctErrorType = error.errorType = "dataCorruptionError";
+                const correctErrorMessage = error.errorMessage?.startsWith(`Context was tombstoned`) === true;
+                return correctErrorType && correctErrorMessage;
+            },
+            `Should not be able to send ops for a tombstoned datastore.`);
+    });
+
+    // If this test starts failing due to runtime is closed errors try first adjusting `sessionExpiryTimeoutMs` above
+    itExpects("Container loaded after sweep timeout expires can't receive ops for tombstoned datastores",
+    [
+        {
+            eventName: "fluid:telemetry:ContainerRuntime:GarbageCollector:SweepReadyObject_Loaded",
+        },
+        {
+            eventName: "fluid:telemetry:Container:ContainerClose",
+            error: "Context was tombstoned during call of process!",
+            errorType: "dataCorruptionError",
+        },
+    ],
+    async () => {
+        const { testDataObjectId, summaryVersion } = await createUnreferencedDataStore();
+
+        // Wait a sweep worthy amount of time (all containers should have closed by now)
+        await delay(sweepTimeoutMs);
+
+        // Load a new container and summarizer based on the latest summary, summarize
+        const {
+            container: summarizingContainer2,
+            summarizer: summarizer2,
+        } = await loadSummarizerAndContainer(summaryVersion);
+
+        const container2 = await provider.loadContainer(runtimeFactory, { configProvider }, { summaryVersion });
+        // Use the request pattern to get the testDataObject - this is unsafe and no one should do this in their
+        // production application
+        const testDataObject2 = await requestFluidObject<TestDataObject>(container2, testDataObjectId);
+
+        // The datastore should be tombstoned now
+        await summarize(summarizer2);
+
+        // Receive an op
+        testDataObject2._root.set("receive", "op");
+        await provider.ensureSynchronized();
+        assert(summarizingContainer2.closed === true, `Summarizing container should close.`);
+    });
+
+    // If this test starts failing due to runtime is closed errors try first adjusting `sessionExpiryTimeoutMs` above
+    itExpects("Container loaded before sweep timeout expires can't receive ops for tombstoned datastores",
+    [
+        {
+            eventName: "fluid:telemetry:ContainerRuntime:GarbageCollector:InactiveObject_Loaded",
+        },
+        {
+            eventName: "fluid:telemetry:Container:ContainerClose",
+            error: "Context was tombstoned during call of process!",
+            errorType: "dataCorruptionError",
+        },
+    ],
+    async () => {
+        const { testDataObjectId, summaryVersion } = await createUnreferencedDataStore();
+
+        // Wait some time, the datastore should not be sweep ready after this wait
+        await delay(sweepTimeoutMs - wait);
+
+        // Load a new container and summarizer based on the latest summary, summarize
+        const {
+            container: summarizingContainer2,
+            summarizer: summarizer2,
+        } = await loadSummarizerAndContainer(summaryVersion);
+
+        const container2 = await provider.loadContainer(runtimeFactory, { configProvider }, { summaryVersion });
+        // Use the request pattern to get the testDataObject - this is unsafe and no one should do this in their
+        // production application.
+        const testDataObject2 = await requestFluidObject<TestDataObject>(container2, testDataObjectId);
+
+        // Wait enough time so that the datastore is sweep ready
+        await delay(wait);
+
+        // Send an op to update the currentTimestampMs to now
+        const mainDataStore2 = await requestFluidObject<TestDataObject>(summarizingContainer2, "default");
+        mainDataStore2._root.set("send a", "op");
+
+        // The datastore should be tombstoned now
+        await summarize(summarizer2);
+
+        // Send an op
+        testDataObject2._root.set("receive", "op");
+        await provider.ensureSynchronized();
+        assert(summarizingContainer2.closed === true, `Summarizing container should close.`);
     });
 });
