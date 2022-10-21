0a434b57d93aed0b1aa03701897e5c443ed04ae0
Abram Sanderson <abram.sanderson@gmail.com>
2022-10-21 16:50:36 -0700

    Reduce changeset to only contain infrastructure changes

--- packages/dds/attributor/README.md
+++ packages/dds/attributor/README.md
@@ -1,3 +1,4 @@
 # @fluidframework/attributor
 
-TODO
+This package contains code for framework support for attribution of content.
+It's currently in early development and should not be relied on for any production code; backwards compatibility is not yet guaranteed.
--- packages/dds/attributor/src/index.ts
+++ packages/dds/attributor/src/index.ts
@@ -1,18 +1,6 @@
-export {
-	Attributor,
-	AttributionInfo,
-	IAttributor,
-	SerializedAttributor,
-	SummaryEncoder,
-} from "./attributor";
-export {
-	makeGzipEncoder,
-	deltaEncoder,
-	Encoder,
-	TimestampEncoder,
-} from "./encoders";
-export {
-	InternedStringId,
-	MutableStringInterner,
-	StringInterner,
-} from "./stringInterner";
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export const TODO: undefined = undefined;
