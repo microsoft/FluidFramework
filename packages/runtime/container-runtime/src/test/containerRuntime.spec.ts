db37e25e527fc9c1078eecdb24d8c15068b96e13
Andrei Iacob <andreiiacob@microsoft.com>
2022-10-24 16:49:39 -0700

    Fix lint

--- packages/runtime/container-runtime/src/test/containerRuntime.spec.ts
+++ packages/runtime/container-runtime/src/test/containerRuntime.spec.ts
@@ -218,7 +218,7 @@ describe("Runtime", () => {
             let containerRuntime: ContainerRuntime;
             const getMockContext = (
                 settings: Record<string, ConfigTypes> = {},
-                logger: ITelemetryLogger = new MockLogger()
+                logger: ITelemetryLogger = new MockLogger(),
             ): Partial<IContainerContext> => ({
                 attachState: AttachState.Attached,
                 deltaManager: new MockDeltaManager(),
