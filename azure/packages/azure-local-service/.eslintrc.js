d78e246b76332a35def1133a8f4dd6b39f546149
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-10-27 22:37:57 +0000

    tools: Upgrade to strict config

--- azure/packages/azure-local-service/.eslintrc.js
+++ azure/packages/azure-local-service/.eslintrc.js
@@ -4,7 +4,7 @@
  */
 
 module.exports = {
-    extends: [require.resolve("@fluidframework/eslint-config-fluid")],
+    extends: [require.resolve("@fluidframework/eslint-config-fluid/strict")],
     plugins: ["eslint-plugin-jsdoc"],
     parserOptions: {
         project: ["./tsconfig.json"],
