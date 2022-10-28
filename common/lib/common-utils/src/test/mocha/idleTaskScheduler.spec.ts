dae3621161c89c8d543d79abbecd951895d96e1f
Wayne Ferrao <wayneferrao@microsoft.com>
2022-10-28 22:04:55 +0000

    Add fallback test

--- common/lib/common-utils/src/test/mocha/idleTaskScheduler.spec.ts
+++ common/lib/common-utils/src/test/mocha/idleTaskScheduler.spec.ts
@@ -7,27 +7,37 @@ import { strict as assert } from "assert";
 import { scheduleIdleTask } from "../../idleTaskScheduler";
 
 describe("Idle task scheduler", () => {
-
     function someTask(x: number): void {
         if (x < 10) {
-            console.log("errror")
             throw new Error("Something awful happened");
-        };
+        }
     }
 
     it("Should schedule and run a synchronous task during idle time", () => {
-        assert.equal(4,5);
-        scheduleIdleTask(() => { someTask(4) }, 1000).then( val => {
-            assert.strictEqual(val, 4);
-        }).catch((err) => {
-            console.log(err);
-        })
-
+        let success = false;
+        scheduleIdleTask(() => {
+            someTask(4);
+        }, 1000)
+            .then(val => {
+                assert(val)
+                success = true;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        assert(success);
     });
 
     it("Should fall back to setTimeout when idle Task API is not available", () => {
-        assert.equal(4,5);
-
+        let success = false;
+        setTimeout(() => {
+            try {
+                someTask(4);
+                success = true;
+            } catch (e) {
+                console.log(e);
+            }
+            assert(success);
+        }, 1000);
     });
-
 });
