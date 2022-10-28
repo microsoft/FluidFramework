37ea36404123fafd06b187c6ebe3ad79ad56268d
Tyler Butler <tyler@tylerbutler.com>
2022-10-28 20:25:45 +0000

    wip

--- build-tools/conventional-changelog.cjs
+++ build-tools/conventional-changelog.cjs
@@ -4,7 +4,7 @@ const config = require('conventional-changelog-conventionalcommits')
 module.exports = config({
     tagPrefix: "build-tools_v",
     preMajor: true,
-    gitRawCommitsOpts : {
-        merges: null
-    }
+    //gitRawCommitsOpts : {
+   
+    //}
 })
