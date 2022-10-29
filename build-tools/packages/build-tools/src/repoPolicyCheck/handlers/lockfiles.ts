2d8dac08533d2051c0f334366ab32c547ee87068
Tyler Butler <tyler@tylerbutler.com>
2022-10-28 17:56:59 -0700

    feat(policy:check): Add policy handler to check for extraneous lockfiles

--- build-tools/packages/build-tools/src/repoPolicyCheck/handlers/lockfiles.ts
+++ build-tools/packages/build-tools/src/repoPolicyCheck/handlers/lockfiles.ts
@@ -2,6 +2,10 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+import { EOL as newline } from "os";
+import path from "path";
+
+import { getPackageManifest } from "../../common/fluidUtils";
 import { Handler, readFile } from "../common";
 
 const lockFilePattern = /.*?package-lock\.json$/i;
@@ -44,6 +48,46 @@ export const handlers: Handler[] = [
                 return `Unexpected 'lockFileVersion' (Please use NPM v6: 'npm i -g npm@latest-6'): ${file}`;
             }
             return;
+        },
+    },
+    {
+        name: "extraneous-lockfiles",
+        match: lockFilePattern,
+        handler: (file, root) => {
+            const manifest = getPackageManifest(root);
+            // Add the root path (.) because a lockfile is expected there
+            const knownPaths: string[] = (manifest.additionalLockfilePaths ?? []).concat(".");
+
+            // Add paths to known monorepos and packages
+            const vals = Object.values(manifest.repoPackages)
+                .filter((p) => typeof p === "string")
+                .map((p) => p.toString());
+            knownPaths.push(...vals);
+
+            // Add paths from entries that are arrays
+            const arrayVals = Object.values(manifest.repoPackages)
+                .filter((p) => typeof p !== "string")
+                .map((p) => p);
+            for (const arr of arrayVals) {
+                if (Array.isArray(arr)) {
+                    knownPaths.push(...arr.map((p) => p.toString()));
+                }
+            }
+
+            const returnData: string[] = [];
+            if (path.basename(file) === "package-lock.json") {
+                if (!knownPaths.includes(path.dirname(file))) {
+                    returnData.push(`Unexpected package-lock.json file at: ${file}`);
+                }
+            }
+
+            if (returnData.length > 1) {
+                return `${returnData.join(newline)}`;
+            } else if (returnData.length === 1) {
+                return returnData[0];
+            }
+
+            return undefined;
         },
     },
 ];
