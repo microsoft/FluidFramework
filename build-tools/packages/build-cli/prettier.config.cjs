44fd7497ca6942554bb1b09867bb05a60f939f58
jikim-msft <111468570+jikim-msft@users.noreply.github.com>
2022-11-04 23:07:23 +0000

    retry after pull request labeler error

--- build-tools/packages/build-cli/prettier.config.cjs
+++ build-tools/packages/build-cli/prettier.config.cjs
@@ -5,6 +5,7 @@
 
 module.exports = {
     ...require("@fluidframework/build-common/prettier.config.cjs"),
+    useTabs: false,
     importOrder: [
         "^node:(.*)$", // Special-case `node:` imports
         "<THIRD_PARTY_MODULES>",
@@ -15,5 +16,4 @@ module.exports = {
     ],
     importOrderSeparation: true,
     importOrderSortSpecifiers: true,
-    useTabs: false,
 };
