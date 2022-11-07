eb4e86d6e764c54a7d83cfa046a415a6dcc1cf1b
jikim-msft <111468570+jikim-msft@users.noreply.github.com>
2022-11-07 21:46:26 +0000

    discard changes

--- common/lib/common-definitions/prettier.config.cjs
+++ common/lib/common-definitions/prettier.config.cjs
@@ -6,11 +6,4 @@
 module.exports = {
     ...require("@fluidframework/build-common/prettier.config.cjs"),
     useTabs: false, // @fluidframework/build-common ^1.2.0, change to TRUE after prettier infra
-    overrides: [
-        {
-            "rules": {
-                "@typescript-eslint/parser": off
-            }
-        }
-    ]
 };
