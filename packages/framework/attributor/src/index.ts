161d00357d2dca5548eb9d1d3899535362c865d2
Abram Sanderson <abram.sanderson@gmail.com>
2022-11-09 13:49:43 -0800

    move attributor code to framework folder

--- packages/framework/attributor/src/index.ts
+++ packages/framework/attributor/src/index.ts
@@ -2,5 +2,21 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-
-export const TODO: undefined = undefined;
+export {
+	Attributor,
+	AttributionInfo,
+	IAttributor,
+	SerializedAttributor,
+	SummaryEncoder,
+} from "./attributor";
+export {
+	makeGzipEncoder,
+	deltaEncoder,
+	Encoder,
+	TimestampEncoder,
+} from "./encoders";
+export {
+	InternedStringId,
+	MutableStringInterner,
+	StringInterner,
+} from "./stringInterner";
--- packages/framework/attributor/src/test/attributor.spec.ts
+++ packages/framework/attributor/src/test/attributor.spec.ts
@@ -3,9 +3,81 @@
  * Licensed under the MIT License.
  */
 import { strict as assert } from "assert";
+import { IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions";
+import { IAudience } from "@fluidframework/container-definitions";
+import { ISequencedDocumentMessage } from "@fluidframework/protocol-definitions";
+import { MockFluidDataStoreRuntime } from "@fluidframework/test-runtime-utils";
+import { Attributor } from "../attributor";
+import { makeMockAudience } from "./utils";
+
+const clientIds = ["A", "B", "C"];
+const defaultAudience = makeMockAudience(clientIds);
+
+class OpFactory {
+	private seq = 0;
+
+	public makeOp({ timestamp, clientId }: { timestamp: number; clientId: string; }): ISequencedDocumentMessage {
+		// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+		return {
+			timestamp,
+			clientId,
+			sequenceNumber: this.seq++,
+		} as ISequencedDocumentMessage;
+	}
+}
+
+function makeMockRuntime(clientId: string, audience: IAudience = defaultAudience): IFluidDataStoreRuntime {
+	const runtime = new MockFluidDataStoreRuntime({ clientId });
+	runtime.getAudience = () => audience;
+	return runtime;
+}
 
 describe("Attributor", () => {
-	it("dummy test", () => {
-        assert.equal(true, true);
-    });
+	let opFactory: OpFactory;
+	beforeEach(() => {
+		opFactory = new OpFactory();
+	});
+
+	describe("can retrieve user information", () => {
+		it("from ops submitted during the current session", () => {
+			const runtime = makeMockRuntime(clientIds[0]);
+			const attributor = new Attributor(runtime);
+			const clientId = clientIds[1];
+			const timestamp = 50;
+			const op = opFactory.makeOp({ timestamp, clientId });
+			(runtime.deltaManager as any).emit("op", op);
+			assert.deepEqual(
+				attributor.getAttributionInfo(op.sequenceNumber),
+				{ user: runtime.getAudience().getMember(clientId)?.user, timestamp },
+			);
+		});
+
+		it("from ops submitted during a previous session", () => {
+			const runtime = makeMockRuntime(clientIds[0]);
+			const originalAttributor = new Attributor(runtime);
+			const clientId = clientIds[1];
+			const timestamp = 50;
+			const op = opFactory.makeOp({ timestamp, clientId });
+			(runtime.deltaManager as any).emit("op", op);
+			const attributor = new Attributor(makeMockRuntime(clientIds[0]), originalAttributor.serialize());
+			assert.deepEqual(
+				attributor.getAttributionInfo(op.sequenceNumber),
+				{ user: runtime.getAudience().getMember(clientId)?.user, timestamp },
+			);
+		});
+	});
+
+	it("getAttributionInfo throws on attempt to retrieve user information for an invalid key", () => {
+		const attributor = new Attributor(makeMockRuntime(clientIds[0]));
+		assert.throws(
+			() => attributor.getAttributionInfo(42),
+			/Requested attribution information for unstored key/,
+			"invalid key should throw",
+		);
+	});
+
+	it("tryGetAttributionInfo returns undefined to retrieve user information for an invalid key", () => {
+		const attributor = new Attributor(makeMockRuntime(clientIds[0]));
+		assert.equal(attributor.tryGetAttributionInfo(42), undefined);
+	});
 });
