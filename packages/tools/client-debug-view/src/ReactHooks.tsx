a67e3e88bb920718962284f57a5da8963f0c3600
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-11-09 01:01:29 +0000

    refactor: Add some helper React hooks for using data derived from the debugger

---
+++ packages/tools/client-debug-view/src/ReactHooks.tsx
@@ -0,0 +1,67 @@
+
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { IFluidClientDebugger } from "@fluid-tools/client-debugger";
+import { IClient } from "@fluidframework/protocol-definitions";
+import React from "react";
+
+/**
+ * Contains React hooks for shared use within the library.
+ */
+
+/**
+ * React hook for getting the session user's client ID.
+ *
+ * @internal
+ */
+export function useMyClientId(clientDebugger: IFluidClientDebugger): string | undefined {
+    const [myClientId, setMyClientId] = React.useState<string | undefined>(clientDebugger.getMyClientId());
+
+    React.useEffect(() => {
+        function onContainerConnectionChange(): void {
+            setMyClientId(clientDebugger.getMyClientId());
+        }
+
+        clientDebugger.on("containerConnected", onContainerConnectionChange);
+        clientDebugger.on("containerDisconnected", onContainerConnectionChange);
+
+        return (): void => {
+            clientDebugger.off("containerConnected", onContainerConnectionChange);
+            clientDebugger.off("containerDisconnected", onContainerConnectionChange);
+        };
+    },
+    [clientDebugger, setMyClientId]);
+
+    return myClientId;
+}
+
+/**
+ * React hook for getting the member data for the session user.
+ *
+ * @internal
+ */
+export function useMyAudienceData(clientDebugger: IFluidClientDebugger): IClient | undefined {
+    const myClientId = useMyClientId(clientDebugger);
+
+    const [audienceMembers, setAudienceMembers] = React.useState<Map<string, IClient>>(clientDebugger.getAudienceMembers());
+
+    React.useEffect(() => {
+        function onAudienceMembersChange(): void {
+            setAudienceMembers(clientDebugger.getAudienceMembers());
+        }
+
+        clientDebugger.on("audienceMemberAdded", onAudienceMembersChange);
+        clientDebugger.on("audienceMemberRemoved", onAudienceMembersChange);
+
+        return (): void => {
+            clientDebugger.off("audienceMemberAdded", onAudienceMembersChange);
+            clientDebugger.off("audienceMemberRemoved", onAudienceMembersChange);
+        };
+    },
+    [clientDebugger, setAudienceMembers]);
+
+    return myClientId === undefined ? undefined : audienceMembers.get(myClientId);
+}
