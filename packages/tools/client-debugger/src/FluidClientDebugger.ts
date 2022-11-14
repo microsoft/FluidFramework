fc09f771c2d79f96bec9c16243417117c1c2065f
Josmithr <josmithr@microsoft.com>
2022-11-14 15:29:49 -0800

    Revert "refactor: Move event handlers"
    This reverts commit ff6275c1f27f0e49ca04a6862f6f7436d5f2c644.

--- packages/tools/client-debugger/src/FluidClientDebugger.ts
+++ packages/tools/client-debugger/src/FluidClientDebugger.ts
@@ -63,6 +63,70 @@ export class FluidClientDebugger
 
 	// #endregion
 
+	// #region Container-related event handlers
+
+	private readonly containerAttachedHandler = (): void => {
+		this.emit("containerAttached");
+	};
+
+	private readonly containerConnectedHandler = (clientId: string): void => {
+		this._connectionStateLog.push({
+			newState: ConnectionState.Connected,
+			timestamp: Date.now(),
+			clientId,
+		});
+		this.emit("containerConnected", clientId);
+	};
+
+	private readonly containerDisconnectedHandler = (): void => {
+		this._connectionStateLog.push({
+			newState: ConnectionState.Disconnected,
+			timestamp: Date.now(),
+			clientId: undefined,
+		});
+		this.emit("containerDisconnected");
+	};
+
+	private readonly containerDirtyHandler = (): void => {
+		// TODO: dirtiness history log?
+		this.emit("containerDirty");
+	};
+
+	private readonly containerSavedHandler = (): void => {
+		// TODO: dirtiness history log?
+		this.emit("containerSaved");
+	};
+
+	private readonly containerClosedHandler = (error?: ICriticalContainerError): void => {
+		this.emit("containerClosed", error);
+	};
+
+	// #endregion
+
+	// #region Audience-related event handlers
+
+	private readonly audienceMemberAddedHandler = (clientId: string, client: IClient): void => {
+		this._audienceChangeLog.push({
+			clientId,
+			client,
+			changeKind: "added",
+			timestamp: Date.now(),
+		});
+		this.emit("audienceMemberChange", MemberChangeKind.Added, clientId, client);
+	};
+
+	private readonly audienceMemberRemovedHandler = (clientId: string, client: IClient): void => {
+		this._audienceChangeLog.push({
+			clientId,
+			client,
+			changeKind: "removed",
+			timestamp: Date.now(),
+		});
+		this.emit("audienceMemberChange", MemberChangeKind.Removed, clientId, client);
+	};
+
+	// #endregion
+
 	private readonly debuggerDisposedHandler = (): boolean => this.emit("debuggerDisposed");
 
 	/**
@@ -157,42 +221,6 @@ export class FluidClientDebugger
 		return this.container.closed;
 	}
 
-	private readonly containerAttachedHandler = (): void => {
-		this.emit("containerAttached");
-	};
-
-	private readonly containerConnectedHandler = (clientId: string): void => {
-		this._connectionStateLog.push({
-			newState: ConnectionState.Connected,
-			timestamp: Date.now(),
-			clientId,
-		});
-		this.emit("containerConnected", clientId);
-	};
-
-	private readonly containerDisconnectedHandler = (): void => {
-		this._connectionStateLog.push({
-			newState: ConnectionState.Disconnected,
-			timestamp: Date.now(),
-			clientId: undefined,
-		});
-		this.emit("containerDisconnected");
-	};
-
-	private readonly containerDirtyHandler = (): void => {
-		// TODO: dirtiness history log?
-		this.emit("containerDirty");
-	};
-
-	private readonly containerSavedHandler = (): void => {
-		// TODO: dirtiness history log?
-		this.emit("containerSaved");
-	};
-
-	private readonly containerClosedHandler = (error?: ICriticalContainerError): void => {
-		this.emit("containerClosed", error);
-	};
-
 	// #endregion
 
 	// #region Audience data
@@ -211,26 +239,6 @@ export class FluidClientDebugger
 		// Clone array contents so consumers don't see local changes
 		return this._audienceChangeLog.map((value) => value);
 	}
-
-	private readonly audienceMemberAddedHandler = (clientId: string, client: IClient): void => {
-		this._audienceChangeLog.push({
-			clientId,
-			client,
-			changeKind: "added",
-			timestamp: Date.now(),
-		});
-		this.emit("audienceMemberChange", MemberChangeKind.Added, clientId, client);
-	};
-
-	private readonly audienceMemberRemovedHandler = (clientId: string, client: IClient): void => {
-		this._audienceChangeLog.push({
-			clientId,
-			client,
-			changeKind: "removed",
-			timestamp: Date.now(),
-		});
-		this.emit("audienceMemberChange", MemberChangeKind.Removed, clientId, client);
-	};
 
 	// #endregion
 
