c7749a1902749a86709be2746d869c98a0740d89
tyler-cai-microsoft <tylercai@microsoft.com>
2022-11-14 14:30:08 -0800

    Comments for inactive and sweepready telemetry logs

--- packages/runtime/container-runtime/src/garbageCollection.ts
+++ packages/runtime/container-runtime/src/garbageCollection.ts
@@ -1505,6 +1505,9 @@ export class GarbageCollector implements IGarbageCollector {
         // For summarizer client, queue the event so it is logged the next time GC runs if the event is still valid.
         // For non-summarizer client, log the event now since GC won't run on it. This may result in false positives
         // but it's a good signal nonetheless and we can consume it with a grain of salt.
+        // Events generated:
+        // InactiveObject_Loaded, InactiveObject_Changed, InactiveObject_Revived
+        // SweepReadyObject_Loaded, SweepReadyObject_Changed, SweepReadyObject_Revived
         if (this.isSummarizerClient) {
             this.pendingEventsQueue.push({ ...propsToLog, usageType, state });
         } else {
