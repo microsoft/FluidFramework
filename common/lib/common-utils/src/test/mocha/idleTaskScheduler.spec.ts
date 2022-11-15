afc748f6c01e45da4e0b795924390897b602d373
Wayne Ferrao <wayneferrao@microsoft.com>
2022-11-15 18:08:10 +0000

    Updated test with fake timers

--- common/lib/common-utils/src/test/mocha/idleTaskScheduler.spec.ts
+++ common/lib/common-utils/src/test/mocha/idleTaskScheduler.spec.ts
@@ -4,39 +4,47 @@
  */
 
 import { strict as assert } from "assert";
-import { scheduleIdleTask } from "../../idleTaskScheduler";
+import { SinonFakeTimers, useFakeTimers } from "sinon";
+import * as idleTask from "../../idleTaskScheduler";
 
 describe("Idle task scheduler", () => {
-    function someTask(x: number): void {
-        if (x < 3) {
-            throw new Error("Something awful happened");
-        }
+    let clock: SinonFakeTimers;
+
+    before(() => {
+        clock = useFakeTimers();
+    });
+
+    afterEach(() => {
+        clock.reset();
+    });
+
+    after(() => {
+        clock.restore();
+    });
+
+    function someTask(x: number): boolean {
+        return (x > 3) ? true : false;
     }
 
     it("Should schedule and run a synchronous task during idle time", async () => {
-        let success = false;
-        await scheduleIdleTask(() => {
-            someTask(5);
-        }, 1000)
-            .then(() => {
-                success = true;
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        assert(success);
+        const promise = idleTask.scheduleIdleTask(() => { return someTask(5) }, 1000);
+
+        clock.tick(1100);
+        return promise.then((result) => assert(result))
+
     });
 
     it("Should fall back to setTimeout when idle Task API is not available", async () => {
         let success = false;
         await new Promise((resolve, reject) => {
-            setTimeout(() => {
-                try {
-                    resolve(someTask(5));
-                } catch (e) {
-                    reject(e);
-                }
-            }, 1000);
+            try {
+                resolve(async () => {
+                    await idleTask.scheduleIdleTask(() => { someTask(5) }, 1000);
+                    clock.tick(1100);
+                });
+            } catch (e) {
+                reject(e);
+            }
         }).then(() => {
             success = true;
         });
