0678fd29a4207f7090d8fe6301f5910597b3adde
Tyler Butler <tylerbu@microsoft.com>
2022-11-16 11:01:48 -0800

    refactor(release:report): Split release report commands  (#12850)
    The `release report` command arguments were confusing, especially after
    the `--all` flag was added. This PR refactors the `release report`
    command into two separate commands, `release report` and `release
    history`.
    
    - When `flub release report` is run for a single release group (i.e.
    `flub release report -g azure`), the specified release group's version
    will be loaded from the repo, and its immediate Fluid dependencies will
    be included in the report. This is useful when we do a client or azure
    release and we want to include the dependency versions that the release
    group depends on.
    - To run the previous default mode where you interactively choose each
    release, pass the `--interactive` flag.
    - `flub release history` replaces `flub release report --all`. It will
    output a list of all releases for a single release group when the
    `--releaseGroup` flag is passed. The number of releases shown can be
    limited using the `--limit` flag.
    
    BREAKING CHANGE: The `--all` and `--limit` flags have been removed from
    `flub release report`. Use `flub release history` instead.

--- build-tools/packages/build-cli/src/lib/package.ts
+++ build-tools/packages/build-cli/src/lib/package.ts
@@ -3,6 +3,7 @@
  * Licensed under the MIT License.
  */
 import { PackageName } from "@rushstack/node-core-library";
+import { strict as assert } from "assert";
 import { compareDesc, differenceInBusinessDays } from "date-fns";
 import ncu from "npm-check-updates";
 import type { Index } from "npm-check-updates/build/src/types/IndexType";
@@ -24,7 +25,7 @@ import { indentString } from "./text";
  * @internal
  */
 export interface PackageVersionMap {
-    [packageName: ReleasePackage]: ReleaseVersion;
+    [packageName: ReleasePackage | ReleaseGroup]: ReleaseVersion;
 }
 
 /**
@@ -496,4 +497,58 @@ export function filterVersionsOlderThan(
         const diff = v.date === undefined ? 0 : differenceInBusinessDays(Date.now(), v.date);
         return diff <= numBusinessDays;
     });
+}
+
+/**
+ * Gets the direct Fluid dependencies for a given package or release group. A Fluid dependency is a dependency on
+ * other packages or release groups in the repo.
+ *
+ * @param context - The {@link Context}.
+ * @param releaseGroupOrPackage - The release group or package to check.
+ * @returns A tuple of {@link PackageVersionMap} objects, one of which contains release groups on which the package
+ * depends, and the other contains independent packages on which the package depends.
+ *
+ * @internal
+ */
+export function getFluidDependencies(
+    context: Context,
+    releaseGroupOrPackage: ReleaseGroup | ReleasePackage,
+): [releaseGroups: PackageVersionMap, packages: PackageVersionMap] {
+    const releaseGroups: PackageVersionMap = {};
+    const packages: PackageVersionMap = {};
+    let packagesToCheck: Package[];
+
+    if (isReleaseGroup(releaseGroupOrPackage)) {
+        packagesToCheck = context.packagesInReleaseGroup(releaseGroupOrPackage);
+    } else {
+        const independentPackage = context.fullPackageMap.get(releaseGroupOrPackage);
+        assert(
+            independentPackage !== undefined,
+            `Package not found in context: ${releaseGroupOrPackage}`,
+        );
+        packagesToCheck = [independentPackage];
+    }
+
+    for (const p of packagesToCheck) {
+        for (const dep of p.combinedDependencies) {
+            const pkg = context.fullPackageMap.get(dep.name);
+            if (pkg === undefined) {
+                continue;
+            }
+
+            const minVer = semver.minVersion(dep.version);
+            if (minVer === null) {
+                throw new Error(`Failed to parse depVersion: ${dep.version}`);
+            }
+
+            if (pkg.monoRepo !== undefined) {
+                releaseGroups[pkg.monoRepo.kind] = minVer.version;
+                continue;
+            }
+
+            packages[pkg.name] = minVer.version;
+        }
+    }
+
+    return [releaseGroups, packages];
 }
