df456c5a6dcd99d011ccce7191a10907905c0c92
Josmithr <josmithr@microsoft.com>
2022-11-17 13:34:53 -0800

    test: Restructure test infra

--- packages/tools/client-debugger-chrome-extension/src/test/Extension.test.ts
+++ packages/tools/client-debugger-chrome-extension/src/test/Extension.test.ts
@@ -2,39 +2,17 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-// eslint-disable-next-line unicorn/import-style, unicorn/prefer-node-protocol
-import * as Path from "path";
-import Puppeteer from "puppeteer";
+import * as chrome from "sinon-chrome";
 
-// Relative to dist/test
-const extensionBuildPath = Path.resolve(__dirname, "..");
-
-const testPageUrl = "http://localhost:8080/";
+/**
+ * Tests end-to-end functionality of the extension.
+ */
 
 describe("Debugger Browser Extension tests", () => {
-	let browser: Puppeteer.Browser | undefined;
-	let page: Puppeteer.Page | undefined;
-
-	beforeEach(async () => {
-		// Launch the browser and navigate to the app page.
-		browser = await Puppeteer.launch({
-			// slowMo: 250,
-			headless: false, // extension are allowed only in head-full mode
-			args: [
-				`--load-extension=${extensionBuildPath}`,
-				"--no-sandbox",
-				"--disable-setuid-sandbox",
-			],
-		});
-
-		page = await browser.newPage();
-		await page.goto(testPageUrl);
-	});
+	beforeEach(async () => {});
 
 	afterEach(async () => {
-		await browser?.close();
-		// eslint-disable-next-line require-atomic-updates
-		browser = undefined;
+		chrome.flush();
 	});
 
 	it("Debugger only appears after being activated", () => {
