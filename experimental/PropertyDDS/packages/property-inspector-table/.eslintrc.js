f433ad1426a7d2e1b1fd1f4f517152ef744a7c37
jikim-msft <111468570+jikim-msft@users.noreply.github.com>
2022-11-18 23:06:52 +0000

    add prettier experimental

--- experimental/PropertyDDS/packages/property-inspector-table/.eslintrc.js
+++ experimental/PropertyDDS/packages/property-inspector-table/.eslintrc.js
@@ -5,7 +5,7 @@
 
  module.exports = {
   "extends": [
-      "@fluidframework/eslint-config-fluid"
+      "@fluidframework/eslint-config-fluid", "prettier"
   ],
   "rules": {
     "@typescript-eslint/consistent-type-assertions": "off",
