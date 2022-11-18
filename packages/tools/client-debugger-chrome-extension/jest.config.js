13577eb034fcd7df58beea727350b72083ba69f5
Josmithr <josmithr@microsoft.com>
2022-11-18 15:22:39 -0800

    tools: Omit test infra code from code coverage reporting

--- packages/tools/client-debugger-chrome-extension/jest.config.js
+++ packages/tools/client-debugger-chrome-extension/jest.config.js
@@ -13,4 +13,5 @@ module.exports = {
 	testPathIgnorePatterns: ["/node_modules/", "dist"],
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 	setupFiles: ["./testSetup.js"],
+	coveragePathIgnorePatterns: ["/node_modules/", "/src/test/"],
 };
