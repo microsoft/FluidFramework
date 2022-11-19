16127ba8b71398900c159abeb9ba324265c814c5
Tyler Butler <tyler@tylerbutler.com>
2022-11-18 16:39:52 -0800

    updates

--- packages/framework/fluid-framework/.eslintrc.js
+++ packages/framework/fluid-framework/.eslintrc.js
@@ -10,5 +10,13 @@ module.exports = {
     "parserOptions": {
         "project": ["./tsconfig.json", "./src/test/tsconfig.json"]
     },
-    "rules": {}
+    "rules": {},
+    "overrides": [{
+        // Rules only for type validation files
+        files: ["**/types/*validate*Previous*.ts"],
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unsafe-argument": "off",
+        },
+    },],
 }
