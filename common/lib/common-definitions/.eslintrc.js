87fc4ff68b13d57bbe395103a7e2862833ab4494
Tyler Butler <tyler@tylerbutler.com>
2022-11-18 16:53:44 -0800

    override

--- common/lib/common-definitions/.eslintrc.js
+++ common/lib/common-definitions/.eslintrc.js
@@ -6,4 +6,14 @@
 module.exports = {
     extends: [require.resolve("@fluidframework/eslint-config-fluid/recommended"), "prettier"],
     rules: {},
+    overrides: [
+        {
+            // Rules only for type validation files
+            files: ["**/types/*validate*Previous*.ts"],
+            rules: {
+                "@typescript-eslint/no-explicit-any": "off",
+                "@typescript-eslint/no-unsafe-argument": "off",
+            },
+        },
+    ],
 };
