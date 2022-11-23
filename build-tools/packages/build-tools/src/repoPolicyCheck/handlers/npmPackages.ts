35e35743f0d48ea4c4922a459b223e6795016833
jikim-msft <111468570+jikim-msft@users.noreply.github.com>
2022-11-23 19:22:56 +0000

    fix lint

--- build-tools/packages/build-tools/src/repoPolicyCheck/handlers/npmPackages.ts
+++ build-tools/packages/build-tools/src/repoPolicyCheck/handlers/npmPackages.ts
@@ -592,13 +592,16 @@ export const handlers: Handler[] = [
             let json;
 
             try {
-                json = JSON.parse(readFile(file))
+                json = JSON.parse(readFile(file));
             } catch (err) {
                 return "Error parsing JSON file: " + file;
             }
 
             const hasPrettierProperty = Object.prototype.hasOwnProperty.call(json, "prettier");
-            const hasPrettierFixProperty = Object.prototype.hasOwnProperty.call(json, "prettier:fix");
+            const hasPrettierFixProperty = Object.prototype.hasOwnProperty.call(
+                json,
+                "prettier:fix",
+            );
             const hasFormatProperty = Object.prototype.hasOwnProperty.call(json, "format");
 
             if (!(hasPrettierProperty && hasPrettierFixProperty && hasFormatProperty)) {
