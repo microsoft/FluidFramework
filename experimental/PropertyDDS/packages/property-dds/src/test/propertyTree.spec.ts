778b42308a8e6abe63099d0e6e09a0f9c97072d3
Nedal Horany <nedalhy@gmail.com>
2022-11-23 16:41:17 +0100

    Adding repro test + summarizaiton fix

--- experimental/PropertyDDS/packages/property-dds/src/test/propertyTree.spec.ts
+++ experimental/PropertyDDS/packages/property-dds/src/test/propertyTree.spec.ts
@@ -12,15 +12,150 @@ import { requestFluidObject } from "@fluidframework/runtime-utils";
 import { LocalDeltaConnectionServer, ILocalDeltaConnectionServer } from "@fluidframework/server-local-server";
 import { IUrlResolver } from "@fluidframework/driver-definitions";
 import {
-	createAndAttachContainer,
-	createLoader,
-	LoaderContainerTracker,
-	ITestFluidObject,
-	TestFluidObjectFactory,
+    createAndAttachContainer,
+    createLoader,
+    LoaderContainerTracker,
+    ITestFluidObject,
+    TestFluidObjectFactory,
+    TestObjectProvider,
+    TestContainerRuntimeFactory,
+    ChannelFactoryRegistry,
+    ITestObjectProvider,
+    createSummarizer,
+    summarizeNow
 } from "@fluidframework/test-utils";
-import { PropertyFactory, StringProperty, BaseProperty } from "@fluid-experimental/property-properties";
+import { PropertyFactory, StringProperty, BaseProperty, NodeProperty, ArrayProperty } from "@fluid-experimental/property-properties";
+import { Loader as ContainerLoader } from "@fluidframework/container-loader";
+import { LocalServerTestDriver } from "@fluidframework/test-drivers";
 import { DeflatedPropertyTree, LZ4PropertyTree } from "../propertyTreeExt";
 import { SharedPropertyTree } from "../propertyTree";
+import { PropertyTreeFactory } from "../propertyTreeFactory";
+
+interface Result {
+    container: IContainer;
+    client: SharedPropertyTree;
+}
+
+interface withSummarizer extends Result {
+    summarizer: Awaited<ReturnType<typeof createSummarizer>>;
+}
+
+describe("PropertyDDS summarizer", () => {
+    let objProvider: ITestObjectProvider;
+    const propertyDdsId = "PropertyTree";
+    const USERS = "users";
+    const getClient = async (withSummarizer = false, load = false) => {
+        const container = await (load ? objProvider.loadTestContainer() : objProvider.makeTestContainer());
+        const dataObject = await requestFluidObject<ITestFluidObject>(container, "/");
+
+        let summarizer;
+        if (withSummarizer) {
+            summarizer = await getSummarizer(container);
+        }
+        const client = await dataObject.getSharedObject<SharedPropertyTree>(propertyDdsId);
+        const res: withSummarizer = {
+            summarizer,
+            client,
+            container
+        };
+        return res;
+    };
+
+    const getSummarizer = async (container) => {
+        const summarizer = await createSummarizer(objProvider, container);
+        return summarizer;
+    };
+
+    const createUserNode = (name: string) => {
+        const node = PropertyFactory.create<NodeProperty>("NodeProperty");
+        node.insert("name", PropertyFactory.create("String", undefined, name));
+    };
+
+    beforeEach(async () => {
+        const driver = new LocalServerTestDriver();
+        const registry = [[propertyDdsId, new PropertyTreeFactory()]] as ChannelFactoryRegistry;
+
+        objProvider = new TestObjectProvider(
+            ContainerLoader as any,
+            driver,
+            () =>
+                new TestContainerRuntimeFactory(
+                    "@fluid-experimental/test-propertyTree",
+                    new TestFluidObjectFactory(registry)
+                )
+        );
+
+    });
+
+    it("Scenario 1", async function() {
+        /**
+         * This test produces a scenario where a summarizer creates a summary with an empty remoteChanges array.
+         * This can happen, if the summarizer client prunes all remote changes. However, all remote changes will only be
+         * pruned, when the last operation in the stream is not a changeset op (e.g. a join/remove operation).
+         * In addition to that, the minimum sequence number has to point to the last operation in the stream. For this,
+         * all clients must be synced and have updated their referenceSequenceNumber.
+         *
+         * We create this scenario to reproduce a bug, where a client that joined after such a summary had been created,
+         * submitted an operation with an incorrect referenceGuid. Clients that already had been joined to the session
+         * before the summarization, then performed an incorrect rebase which resulted in exceptions or an incorrect
+         * state in the property tree after the rebase.
+         */
+        this.timeout(30000);
+
+        // 1- U1 joins together with summarizer
+        const { client: u1, summarizer, container } = await getClient(true);
+        await objProvider.ensureSynchronized();
+
+        // Test debugging code
+        // container.deltaManager.inbound.on("op", (task: any) => {
+        //     console.info(task);
+        // });
+
+        // container.deltaManager.outbound.on("op", (task: any) => {
+        //     console.info(task);
+        // });
+
+        // 2- Insert array with u1 as user
+        u1.root.insert(USERS, PropertyFactory.create("NodeProperty", "array"));
+        u1.commit();
+        await objProvider.ensureSynchronized();
+
+        let users = u1.root.get<ArrayProperty>(USERS);
+        users?.push(createUserNode("u1"));
+
+        users?.push(createUserNode("u2"));
+        u1.commit();
+
+        await objProvider.ensureSynchronized();
+
+        // Wait enough time to send noop
+        // @TODO is there a way to make sure all clients sent the "noop" without waiting ?
+        // This is required to make sure all clients has the same referenceSequenceNumber
+        await new Promise((resolve) => setTimeout(() => {
+            resolve(undefined);
+        }, 20000));
+
+        const { client: u2 } = await getClient(false, true);
+        await objProvider.ensureSynchronized();
+
+        // Summarize
+        await summarizeNow(summarizer);
+
+        await objProvider.ensureSynchronized();
+
+        // U3 joins and remove a user
+        const { client: u3 } = await getClient(false, true);
+
+        users = u3.root.get<ArrayProperty>(USERS);
+
+        users?.remove(1);
+        u3.commit();
+        await objProvider.ensureSynchronized();
+
+        expect(u1.root.get<ArrayProperty>(USERS)?.getValues().length).to.equal(1);
+    });
+});
+
 
 describe("PropertyTree", () => {
     const documentId = "localServerTest";
