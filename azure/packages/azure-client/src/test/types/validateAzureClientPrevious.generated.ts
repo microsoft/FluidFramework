e440bf40319452987f42469410938d4bf5273856
Tyler Butler <tyler@tylerbutler.com>
2022-11-23 16:56:20 -0800

    typetests generate

--- azure/packages/azure-client/src/test/types/validateAzureClientPrevious.generated.ts
+++ azure/packages/azure-client/src/test/types/validateAzureClientPrevious.generated.ts
@@ -71,7 +71,6 @@ declare function get_old_InterfaceDeclaration_AzureClientProps():
 declare function use_current_InterfaceDeclaration_AzureClientProps(
     use: TypeOnly<current.AzureClientProps>);
 use_current_InterfaceDeclaration_AzureClientProps(
-    // @ts-expect-error compatibility expected to be broken
     get_old_InterfaceDeclaration_AzureClientProps());
 
 /*
@@ -301,6 +300,30 @@ declare function use_old_InterfaceDeclaration_AzureRemoteConnectionConfig(
     use: TypeOnly<old.AzureRemoteConnectionConfig>);
 use_old_InterfaceDeclaration_AzureRemoteConnectionConfig(
     get_current_InterfaceDeclaration_AzureRemoteConnectionConfig());
+
+/*
+* Validate forward compat by using old type in place of current type
+* If breaking change required, add in package.json under typeValidation.broken:
+* "InterfaceDeclaration_AzureUser": {"forwardCompat": false}
+*/
+declare function get_old_InterfaceDeclaration_AzureUser():
+    TypeOnly<old.AzureUser>;
+declare function use_current_InterfaceDeclaration_AzureUser(
+    use: TypeOnly<current.AzureUser>);
+use_current_InterfaceDeclaration_AzureUser(
+    get_old_InterfaceDeclaration_AzureUser());
+
+/*
+* Validate back compat by using current type in place of old type
+* If breaking change required, add in package.json under typeValidation.broken:
+* "InterfaceDeclaration_AzureUser": {"backCompat": false}
+*/
+declare function get_current_InterfaceDeclaration_AzureUser():
+    TypeOnly<current.AzureUser>;
+declare function use_old_InterfaceDeclaration_AzureUser(
+    use: TypeOnly<old.AzureUser>);
+use_old_InterfaceDeclaration_AzureUser(
+    get_current_InterfaceDeclaration_AzureUser());
 
 /*
 * Validate forward compat by using old type in place of current type
