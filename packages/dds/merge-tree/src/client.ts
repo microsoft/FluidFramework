3e5ef0311de6ef0014821270d1ea469bfe00477d
Clarence Li <clarenceli@microsoft.com>
2022-11-29 12:46:53 -0500

    Temporarily fix the reconnect tests failure

--- packages/dds/merge-tree/src/client.ts
+++ packages/dds/merge-tree/src/client.ts
@@ -1060,10 +1060,10 @@ export class Client {
         const args = this.getClientSequenceArgsForMessage(op);
         return this._mergeTree.getContainingSegment<T>(pos, args.referenceSequenceNumber, clientId ?? args.clientId, localSeq);
     }
-    /*
+
     getContainingSegmentWithSeqNumber<T extends ISegment>(pos: number, seqNumberFrom: number, clientId: number, localSeq: number) {
         return this._mergeTree.getContainingSegment<T>(pos, seqNumberFrom, clientId, localSeq);
-    } */
+    }
 
     /**
      * Returns the position to slide a reference to if a slide is required.
