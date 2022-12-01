3fc6a2477eeada68d495d2518d13cf27488e5129
chentong7 <chentong@microsoft.com>
2022-11-30 17:30:13 -0800

    update code review

--- packages/tools/client-debug-view/src/test/app/index.tsx
+++ packages/tools/client-debug-view/src/test/app/index.tsx
@@ -5,7 +5,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { renderClientDebuggerView } from "../../RenderClientDebugger";
 import { App } from "./App";
 
 console.log("Rendering app!");
