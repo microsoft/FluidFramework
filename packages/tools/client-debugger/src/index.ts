f6be3d114f4fcbe1442b808203e563776503c479
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-12-05 22:22:12 +0000

    docs: Fill in packageDocumentation comment

--- packages/tools/client-debugger/src/index.ts
+++ packages/tools/client-debugger/src/index.ts
@@ -6,7 +6,37 @@
 /**
  * Contains APIs for debugging Fluid Client sessions.
  *
- * TODO: more details
+ * Debugger instances are created per {@link @fluidframework/container-definitions#IContainer} instance and are bound
+ * to the `globalThis` context to be accessible to related tools.
+ *
+ * - See the package README for more details on related tools that are enabled via these debugger instances.
+ *
+ * @remarks
+ *
+ * The general usage pattern for this library is to first initialize a debugger for a given Fluid Client
+ * ({@link @fluidframework/container-definitions#IContainer} and {@link @fluidframework/container-definitions#IAudience})
+ * by calling {@link initializeFluidClientDebugger} during application setup / any time after your container has been
+ * attached.
+ *
+ * Then, during application teardown, call {@link closeFluidClientDebugger} to clean up the debugger and its resources.
+ *
+ * @example Initialization
+ *
+ * ```typescript
+ * initializeFluidClientDebugger({
+ *  containerId,
+ *  container,
+ *  containerData: {
+ *      rootMap: sharedMap
+ *  },
+ * });
+ * ```
+ *
+ * @example Disposal
+ *
+ * ```typescript
+ * closeFluidClientDebugger(containerId);
+ * ```
  *
  * @packageDocumentation
  */
