a69e4c5f53e5e4f5127a7858f05313970b3247ec
chentong7 <chentong@microsoft.com>
2022-12-05 16:24:29 -0800

    Update Chrome extension prototype to use React-agnostic entry-point

--- packages/tools/client-debugger-chrome-extension/src/OpenDebuggerPanel.ts
+++ packages/tools/client-debugger-chrome-extension/src/OpenDebuggerPanel.ts
@@ -2,16 +2,12 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-import React from "react";
-import ReactDOM from "react-dom";
+import { renderClientDebuggerView } from "@fluid-tools/client-debug-view";
 
-import { debuggerPanelId } from "./Constants";
-import { DebuggerView } from "./DebuggerView";
 import { isDebuggerPanelOpen } from "./Utilities";
 
 /**
  * Appends the debugger view panel to the document (as a child under `body`).
- * If a debugger view panel is already active, it does not append a new instance.
  *
  * @returns Whether or not a new debugger view was appended to the document.
  *
@@ -22,18 +18,5 @@ export async function openDebuggerPanel(): Promise<boolean> {
 		return false;
 	}
 
-	const element = document.createElement("div");
-	element.id = debuggerPanelId;
-	document.body.append(element);
-
-	return new Promise<boolean>((resolve) => {
-		try {
-			ReactDOM.render(React.createElement(DebuggerView), element, () => {
-				resolve(true);
-			});
-		} catch (error) {
-			console.error(`Could not open the debugger view due to an error: ${error}.`);
-			return false;
-		}
-	});
+	return renderClientDebuggerView(document.body);
 }
