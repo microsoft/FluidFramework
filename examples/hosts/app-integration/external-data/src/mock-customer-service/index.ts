2b34f93c186a575123c7b4ba5fafd3f5b3acbaab
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-12-09 01:25:32 +0000

    WIP: Start adding scaffolding docs

---
+++ examples/hosts/app-integration/external-data/src/mock-customer-service/index.ts
@@ -0,0 +1,13 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * This directoy contains a mock implementation of a customer service that manages a webhook subscription.
+ *
+ * When prompted (TODO), it initializes the mock webhook simulating a connection with some external service (e.g. Jira).
+ * The corresponding REST API will then be "activated" and will begin submitting updates
+ *
+ * When prompted via a debug request (TODO), it simulates a change to the external data, which
+ */
