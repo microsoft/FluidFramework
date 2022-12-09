3711bb5003a05ab420d8c118835d41a6e58559b8
Navin Agarwal <45832642+agarwal-navin@users.noreply.github.com>
2022-12-09 13:03:38 -0800

    GC: Fix for unreferenced timestamp not always reset when reference is added between summaries (#13316)
    ## Bug
    When a reference is added to unreferenced objects between two summaries,
    their unreferenced timestamp is reset only if the reference is added
    from another referenced object. See
    https://github.com/microsoft/FluidFramework/blob/3cd0cd0618b2963b2e24261da9a4ae6794c90d4e/packages/runtime/container-runtime/src/garbageCollection.ts#L1309.
    However, the unreferenced timestamp of such objects (say objA) should be
    reset irrespective of whether the object that adds the reference (say
    objB) is itself referenced or not. That's because objB can be in memory
    of certain clients even if it is unreferenced and such clients could
    then access objA and store an in-memory reference to it. The session for
    these clients can last upto the session expiry timeout and so, their
    unreference timestamp needs to be reset.
    
    ## Fix
    When updating the unreferenced timestamp of references between
    summaries, do it for all all references irrespective of whether the node
    that added the reference was referenced or not.
    
    
    [AB#2801](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/2801)

--- packages/runtime/container-runtime/src/test/garbageCollection.spec.ts
+++ packages/runtime/container-runtime/src/test/garbageCollection.spec.ts
@@ -1460,15 +1460,6 @@ describe("Garbage Collection Tests", () => {
 
         describe("References to unreferenced nodes", () => {
             /**
-             * Function that asserts the given value is not true. Used in these tests to demonstrate that because of
-             * a bug we are not getting the expected results. Once the bug is fixed, these asserts should start working
-             * as expected.
-             */
-            function assertNotTrue(value: boolean, message: string) {
-                assert(!value, message);
-            }
-
-            /**
              * Validates that we can detect references that are added from an unreferenced node to another.
              * 1. Summary 1 at t1. V = [A*, B, C]. E = []. B and C have unreferenced time t1.
              * 2. Reference from B to C. E = [B -\> C].
@@ -1502,7 +1493,7 @@ describe("Garbage Collection Tests", () => {
                 const nodeBTime2 = timestamps2.get(nodeB);
                 const nodeCTime2 = timestamps2.get(nodeC);
                 assert(nodeBTime2 === nodeBTime1, "B's timestamp should be unchanged");
-                assertNotTrue(nodeCTime2 !== undefined && nodeCTime2 > nodeCTime1, "C's timestamp should have updated");
+                assert(nodeCTime2 !== undefined && nodeCTime2 > nodeCTime1, "C's timestamp should have updated");
             });
 
             /*
@@ -1544,8 +1535,8 @@ describe("Garbage Collection Tests", () => {
                 const nodeCTime2 = timestamps2.get(nodeC);
                 const nodeDTime2 = timestamps2.get(nodeD);
                 assert(nodeBTime2 === nodeBTime1, "B's timestamp should be unchanged");
-                assertNotTrue(nodeCTime2 !== undefined && nodeCTime2 > nodeCTime1, "C's timestamp should have updated");
-                assertNotTrue(nodeDTime2 !== undefined && nodeDTime2 > nodeDTime1, "D's timestamp should have updated");
+                assert(nodeCTime2 !== undefined && nodeCTime2 > nodeCTime1, "C's timestamp should have updated");
+                assert(nodeDTime2 !== undefined && nodeDTime2 > nodeDTime1, "D's timestamp should have updated");
             });
 
             /*
@@ -1558,7 +1549,7 @@ describe("Garbage Collection Tests", () => {
              * 4. Summary 2 at t2. V = [A*, B, C]. E = [B -> C]. C and D have unreferenced time t2.
              * Validates that the unreferenced time for C and D is t2 which is > t1.
              */
-            it(`Scenario 2 - Reference added to a list of unreferenced nodes and a reference is removed`, async () => {
+            it(`Scenario 3 - Reference added to a list of unreferenced nodes and a reference is removed`, async () => {
                 // Initialize nodes A, B and C.
                 defaultGCData.gcNodes["/"] = [nodeA];
                 defaultGCData.gcNodes[nodeA] = [];
@@ -1592,8 +1583,8 @@ describe("Garbage Collection Tests", () => {
                 const nodeCTime2 = timestamps2.get(nodeC);
                 const nodeDTime2 = timestamps2.get(nodeD);
                 assert(nodeBTime2 === nodeBTime1, "B's timestamp should be unchanged");
-                assertNotTrue(nodeCTime2 !== undefined && nodeCTime2 > nodeCTime1, "C's timestamp should have updated");
-                assertNotTrue(nodeDTime2 !== undefined && nodeDTime2 > nodeDTime1, "D's timestamp should have updated");
+                assert(nodeCTime2 !== undefined && nodeCTime2 > nodeCTime1, "C's timestamp should have updated");
+                assert(nodeDTime2 !== undefined && nodeDTime2 > nodeDTime1, "D's timestamp should have updated");
             });
         });
     });
