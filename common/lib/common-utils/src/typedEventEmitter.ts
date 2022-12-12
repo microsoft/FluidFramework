7cc595bf291a5530b26e7b8686786a95b2000e89
Mark Fields <markfields@users.noreply.github.com>
2022-12-12 18:02:00 +0000

    Revert "DOES NOT WORK - Try modeling via array not mapping"
    This reverts commit 9c65931f3a902c0e09d02e463873ba691663350b.

--- common/lib/common-utils/src/typedEventEmitter.ts
+++ common/lib/common-utils/src/typedEventEmitter.ts
@@ -96,79 +96,11 @@ type StringKeys<TEventSpec> =
 ;
 
 /** Signature for emit */
-export type EventEmitSignatures_Orig<TThis, TEvent> =
+type EventEmitSignatures<TThis, TEvent> =
     <TEventKey extends StringKeys<ToEventArgsMapping<TEvent>>>(
         event: TEventKey,
         ...args: ReplaceIEventThisPlaceHolder<ToEventArgsMapping<TEvent>[TEventKey], TThis>
     ) => boolean;
-
-    export type ToEventArgsArrayCore<TEvent extends IEvent> =
-    // ...Start with all 15 like IEventTransformer
-    TEvent extends
-    {
-        (event: infer E0, listener: (...args: infer A0) => void),
-        (event: infer E1, listener: (...args: infer A1) => void),
-        (event: infer E2, listener: (...args: infer A2) => void),
-        (event: infer E3, listener: (...args: infer A3) => void),
-        (event: string, listener: (...args: any[]) => void),
-    }
-    ? SingleEventArgsArray<E0, A0> | SingleEventArgsArray<E1, A1> | SingleEventArgsArray<E2, A2> | SingleEventArgsArray<E3, A3>
-    : TEvent extends
-    {
-        (event: infer E0, listener: (...args: infer A0) => void),
-        (event: infer E1, listener: (...args: infer A1) => void),
-        (event: infer E2, listener: (...args: infer A2) => void),
-        (event: string, listener: (...args: any[]) => void),
-    }
-    ? SingleEventArgsArray<E0, A0> | SingleEventArgsArray<E1, A1> | SingleEventArgsArray<E2, A2>
-    : TEvent extends
-    {
-        (event: infer E0, listener: (...args: infer A0) => void),
-        (event: infer E1, listener: (...args: infer A1) => void),
-        (event: string, listener: (...args: any[]) => void),
-    }
-    ? SingleEventArgsArray<E0, A0> | SingleEventArgsArray<E1, A1>
-    : TEvent extends
-    {
-        (event: infer E0, listener: (...args: infer A0) => void),
-        (event: string, listener: (...args: any[]) => void),
-    }
-    ? SingleEventArgsArray<E0, A0>
-    : SingleEventArgsArray<string, any[]>
-    ;
-
-export type ToEventArgsArray<TEvent> =
-    ToEventArgsArrayCore<TEvent & IEvent> |
-    SingleEventArgsArray<"addListener", [event: string, listener: (...args: any[]) => void]> |
-    SingleEventArgsArray<"removeListener", [event: string, listener: (...args: any[]) => void]>;
-    //* Uncomment this to allow emitting anything. But themn emit loses intellisense for event keys
-    // & EventSpecEntry<string, any[]>;
-
-type AM =
-    SingleEventArgsMapping<"removeListener", [event: string, listener: (...args: any[]) => void]>;
-
-
-type AA =
-    SingleEventArgsArray<"removeListener", [event: string, listener: (...args: any[]) => void]>;
-
-type B =
-    ToEventArgsArray<IOldEvents>;
-
-export type SingleEventArgsArray<TEventKey, TListenerArgs extends any[]> =
-    TEventKey extends string ?
-    [TEventKey, TListenerArgs] //* ORiginally tried spreading [TEventKey, ...TListenerArgs] but this loses the named tuple members
-    : never;
-
-type EventEmitSignatures<TThis, TEvent> =
-    ToEventArgsArray<TEvent> extends [event: infer TKey, args: [...rest: infer TRest]]
-    ?
-    <TKey2 extends TKey>(
-        event: TKey2,
-        ...args: TRest
-        // ...args: ReplaceIEventThisPlaceHolder<ToEventArgsArray<TEvent>, TThis>
-    ) => boolean
-    :
-    never;
 
 /**
  * Event Emitter helper class the supports emitting typed events
@@ -205,12 +137,3 @@ export class TypedEventEmitter<TEvent>
 
     readonly emit: EventEmitSignatures<this, TEvent>;
 }
-
-export interface IOldEvents extends IEvent {
-    (event: "asdf", listener: (y: boolean, z: string) => void);
-    (event: "qwer", listener: () => void);
-}
-
-const tee1: TypedEventEmitter<IOldEvents> = new TypedEventEmitter();
-
-tee1.emit("asdf", )
