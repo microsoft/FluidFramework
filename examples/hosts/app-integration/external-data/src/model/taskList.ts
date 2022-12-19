cf3559e63f0092de635bcf4b05b4dc3465a27de5
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2022-12-19 13:58:06 -0800

    docs: Update TODO

--- examples/hosts/app-integration/external-data/src/model/taskList.ts
+++ examples/hosts/app-integration/external-data/src/model/taskList.ts
@@ -214,7 +214,7 @@ export class TaskList extends DataObject implements ITaskList {
         } catch (error) {
             console.error(`Task list fetch failed due to an error:\n${error}`);
 
-            // TODO: display error status to user?
+            // TODO: Display error status to user? Attempt some number of retries on failure? 
 
             return;
         }
