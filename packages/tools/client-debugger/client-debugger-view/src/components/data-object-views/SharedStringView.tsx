b2d4119d1af7285001b2044a6dafdfab718ca81f
chentong7 <chentong@microsoft.com>
2022-12-21 15:55:48 -0800

    Add SharedMap visualizer/editor

--- packages/tools/client-debugger/client-debugger-view/src/components/data-object-views/SharedStringView.tsx
+++ packages/tools/client-debugger/client-debugger-view/src/components/data-object-views/SharedStringView.tsx
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-import { Stack, StackItem } from "@fluentui/react";
+import { IStackProps, IStackStyles, PrimaryButton, Stack, TextField } from "@fluentui/react";
 import React from "react";
 
 import { SharedString } from "@fluidframework/sequence";
@@ -25,9 +25,12 @@ export function SharedStringView(props: SharedStringViewProps): React.ReactEleme
 
 	const [text, setText] = React.useState<string>(sharedString.getText());
 
+	const [deltaText, setDeltaText] = React.useState("");
+
 	React.useEffect(() => {
 		function updateText(): void {
 			const newText = sharedString.getText();
+			setDeltaText(newText);
 			setText(newText);
 		}
 
@@ -38,12 +41,41 @@ export function SharedStringView(props: SharedStringViewProps): React.ReactEleme
 		};
 	}, []);
 
+	const stackStyles: Partial<IStackStyles> = { root: { width: 650 } };
+	const stackTokens = { childrenGap: 50 };
+	const columnProps: Partial<IStackProps> = {
+		tokens: { childrenGap: 15 },
+		styles: { root: { width: 300 } },
+	};
+
+	function updateText(): void {
+		sharedString.replaceText(0, text.length, deltaText);
+		sharedString.emit("sequenceDelta");
+		setDeltaText("");
+	}
+
+	const updateDeltaText = (
+		event: React.FormEvent<HTMLTextAreaElement | HTMLInputElement>,
+		newValue?: string | undefined,
+	): void => {
+		setDeltaText(newValue ?? "");
+	};
+
 	return (
-		<Stack>
-			<StackItem>
-				<b>SharedString</b>
-			</StackItem>
-			<StackItem>{text}</StackItem>
+		<Stack horizontal tokens={stackTokens} styles={stackStyles}>
+			<Stack {...columnProps}>
+				<TextField
+					id="sharedStringDebuggerEditorId"
+					label="SharedString"
+					multiline
+					rows={3}
+					defaultValue={text}
+					onChange={updateDeltaText}
+				/>
+				<PrimaryButton disabled={deltaText === ""} onClick={updateText}>
+					Submit Text
+				</PrimaryButton>
+			</Stack>
 		</Stack>
 	);
 }
