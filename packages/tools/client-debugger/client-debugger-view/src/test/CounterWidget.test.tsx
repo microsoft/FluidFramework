308aa18325a0cb626b91cc4e22205f41599384d4
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-01-04 23:01:31 +0000

    style: Update prettier config to better line up with other client packages and update formatting

--- packages/tools/client-debugger/client-debugger-view/src/test/CounterWidget.test.tsx
+++ packages/tools/client-debugger/client-debugger-view/src/test/CounterWidget.test.tsx
@@ -16,65 +16,65 @@ import { createFluidContainer } from "./ClientUtilities";
 import { CounterWidget } from "./widgets";
 
 describe("CounterWidget component tests", () => {
-	let client: TinyliciousClient | undefined;
-	let container: IFluidContainer | undefined;
+    let client: TinyliciousClient | undefined;
+    let container: IFluidContainer | undefined;
 
-	const containerSchema: ContainerSchema = {
-		initialObjects: {
-			counter: SharedCounter,
-		},
-	};
+    const containerSchema: ContainerSchema = {
+        initialObjects: {
+            counter: SharedCounter,
+        },
+    };
 
-	beforeEach(async () => {
-		client = new TinyliciousClient();
-		({ container } = await createFluidContainer(client, containerSchema));
-	});
+    beforeEach(async () => {
+        client = new TinyliciousClient();
+        ({ container } = await createFluidContainer(client, containerSchema));
+    });
 
-	afterEach(async () => {
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		container!.dispose();
-		container = undefined;
-		client = undefined;
-	});
+    afterEach(async () => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        container!.dispose();
+        container = undefined;
+        client = undefined;
+    });
 
-	async function createSharedCounter(): Promise<SharedCounter> {
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		return container!.create(SharedCounter);
-	}
+    async function createSharedCounter(): Promise<SharedCounter> {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        return container!.create(SharedCounter);
+    }
 
-	test("Has expected elements", async (): Promise<void> => {
-		const sharedCounter = await createSharedCounter();
-		render(<CounterWidget counter={sharedCounter} />);
+    test("Has expected elements", async (): Promise<void> => {
+        const sharedCounter = await createSharedCounter();
+        render(<CounterWidget counter={sharedCounter} />);
 
-		// Verify initial component text presence and value
-		await screen.findByText("0"); // Will throw if exact text not found
+        // Verify initial component text presence and value
+        await screen.findByText("0"); // Will throw if exact text not found
 
-		// Verify button presence and state
-		const buttons = await screen.findAllByRole("button");
-		expect(buttons).toHaveLength(2);
-		expect(buttons[0]).toBeDisabled(); // Initial counter value is 0, so the decrement button should be disabled.
-		expect(buttons[1]).toBeEnabled();
-	});
+        // Verify button presence and state
+        const buttons = await screen.findAllByRole("button");
+        expect(buttons).toHaveLength(2);
+        expect(buttons[0]).toBeDisabled(); // Initial counter value is 0, so the decrement button should be disabled.
+        expect(buttons[1]).toBeEnabled();
+    });
 
-	test("Responds to increment (via UI)", async (): Promise<void> => {
-		const sharedCounter = await createSharedCounter();
-		render(<CounterWidget counter={sharedCounter} />);
+    test("Responds to increment (via UI)", async (): Promise<void> => {
+        const sharedCounter = await createSharedCounter();
+        render(<CounterWidget counter={sharedCounter} />);
 
-		let buttons = await screen.findAllByRole("button");
-		expect(buttons).toHaveLength(2);
+        let buttons = await screen.findAllByRole("button");
+        expect(buttons).toHaveLength(2);
 
-		// Click increment button
-		await userEvent.click(buttons[1]);
+        // Click increment button
+        await userEvent.click(buttons[1]);
 
-		// Verify change in DDS
-		expect(sharedCounter.value).toEqual(1); // Value should have been incremented from 0 to 1
+        // Verify change in DDS
+        expect(sharedCounter.value).toEqual(1); // Value should have been incremented from 0 to 1
 
-		// Verify component text
-		await screen.findByText("1"); // Will throw if exact text not found
+        // Verify component text
+        await screen.findByText("1"); // Will throw if exact text not found
 
-		// Verify that decrement button is now enabled
-		buttons = await screen.findAllByRole("button");
-		expect(buttons).toHaveLength(2);
-		expect(buttons[0]).toBeEnabled();
-	});
+        // Verify that decrement button is now enabled
+        buttons = await screen.findAllByRole("button");
+        expect(buttons).toHaveLength(2);
+        expect(buttons[0]).toBeEnabled();
+    });
 });
--- packages/tools/client-debugger/client-debugger/src/test/ClientDebugger.test.ts
+++ packages/tools/client-debugger/client-debugger/src/test/ClientDebugger.test.ts
@@ -7,18 +7,18 @@ import { expect } from "chai";
 import { SharedCounter } from "@fluidframework/counter";
 import { ContainerSchema, FluidContainer, IFluidContainer } from "@fluidframework/fluid-static";
 import {
-	TinyliciousClient,
-	TinyliciousContainerServices,
+    TinyliciousClient,
+    TinyliciousContainerServices,
 } from "@fluidframework/tinylicious-client";
 
 import { IFluidClientDebugger } from "../IFluidClientDebugger";
 import {
-	FluidClientDebuggerProps,
-	clearDebuggerRegistry,
-	closeFluidClientDebugger,
-	getDebuggerRegistry,
-	getFluidClientDebugger,
-	initializeFluidClientDebugger,
+    FluidClientDebuggerProps,
+    clearDebuggerRegistry,
+    closeFluidClientDebugger,
+    getDebuggerRegistry,
+    getFluidClientDebugger,
+    initializeFluidClientDebugger,
 } from "../Registry";
 
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
@@ -27,102 +27,102 @@ import {
  * Type returned from when creating / loading the Container.
  */
 interface ContainerLoadResult {
-	container: IFluidContainer;
-	services: TinyliciousContainerServices;
+    container: IFluidContainer;
+    services: TinyliciousContainerServices;
 }
 
 /**
  * Schema used by the app.
  */
 const containerSchema: ContainerSchema = {
-	initialObjects: {
-		counter: SharedCounter,
-	},
+    initialObjects: {
+        counter: SharedCounter,
+    },
 };
 
 describe("ClientDebugger unit tests", () => {
-	let _debuggerProps: FluidClientDebuggerProps | undefined;
+    let _debuggerProps: FluidClientDebuggerProps | undefined;
 
-	beforeEach(async () => {
-		const client = new TinyliciousClient();
+    beforeEach(async () => {
+        const client = new TinyliciousClient();
 
-		// Create the container
-		console.log("Creating new container...");
-		let createContainerResult: ContainerLoadResult;
-		try {
-			createContainerResult = await client.createContainer(containerSchema);
-		} catch (error) {
-			console.error(`Encountered error creating Fluid container: "${error}".`);
-			throw error;
-		}
-		console.log("Container created!");
+        // Create the container
+        console.log("Creating new container...");
+        let createContainerResult: ContainerLoadResult;
+        try {
+            createContainerResult = await client.createContainer(containerSchema);
+        } catch (error) {
+            console.error(`Encountered error creating Fluid container: "${error}".`);
+            throw error;
+        }
+        console.log("Container created!");
 
-		const { container: tinyliciousContainer } = createContainerResult;
+        const { container: tinyliciousContainer } = createContainerResult;
 
-		// Attach container
-		let containerId: string;
-		console.log("Awaiting container attach...");
-		try {
-			containerId = await tinyliciousContainer.attach();
-		} catch (error) {
-			console.error(`Encountered error attaching Fluid container: "${error}".`);
-			throw error;
-		}
-		console.log("Fluid container attached!");
+        // Attach container
+        let containerId: string;
+        console.log("Awaiting container attach...");
+        try {
+            containerId = await tinyliciousContainer.attach();
+        } catch (error) {
+            console.error(`Encountered error attaching Fluid container: "${error}".`);
+            throw error;
+        }
+        console.log("Fluid container attached!");
 
-		_debuggerProps = {
-			containerId,
-			container: (tinyliciousContainer as FluidContainer).INTERNAL_CONTAINER_DO_NOT_USE!(),
-			containerData: tinyliciousContainer.initialObjects,
-		};
-	});
+        _debuggerProps = {
+            containerId,
+            container: (tinyliciousContainer as FluidContainer).INTERNAL_CONTAINER_DO_NOT_USE!(),
+            containerData: tinyliciousContainer.initialObjects,
+        };
+    });
 
-	afterEach(() => {
-		clearDebuggerRegistry();
-	});
+    afterEach(() => {
+        clearDebuggerRegistry();
+    });
 
-	function getDebuggerProps(): FluidClientDebuggerProps {
-		if (_debuggerProps === undefined) {
-			expect.fail("Container initialization failed.");
-		}
-		return _debuggerProps;
-	}
+    function getDebuggerProps(): FluidClientDebuggerProps {
+        if (_debuggerProps === undefined) {
+            expect.fail("Container initialization failed.");
+        }
+        return _debuggerProps;
+    }
 
-	function initializeDebugger(props: FluidClientDebuggerProps): IFluidClientDebugger {
-		initializeFluidClientDebugger(props);
-		return getFluidClientDebugger(props.containerId)!;
-	}
+    function initializeDebugger(props: FluidClientDebuggerProps): IFluidClientDebugger {
+        initializeFluidClientDebugger(props);
+        return getFluidClientDebugger(props.containerId)!;
+    }
 
-	it("Initializing debugger populates global (window) registry", () => {
-		let debuggerRegistry = getDebuggerRegistry();
-		expect(debuggerRegistry.size).to.equal(0); // There should be no registered debuggers yet.
+    it("Initializing debugger populates global (window) registry", () => {
+        let debuggerRegistry = getDebuggerRegistry();
+        expect(debuggerRegistry.size).to.equal(0); // There should be no registered debuggers yet.
 
-		initializeDebugger(getDebuggerProps());
+        initializeDebugger(getDebuggerProps());
 
-		debuggerRegistry = getDebuggerRegistry();
-		expect(debuggerRegistry.size).to.equal(1);
-	});
+        debuggerRegistry = getDebuggerRegistry();
+        expect(debuggerRegistry.size).to.equal(1);
+    });
 
-	it("Closing debugger removes it from global (window) registry and disposes it.", () => {
-		const debuggerProps = getDebuggerProps();
-		const { containerId } = debuggerProps;
+    it("Closing debugger removes it from global (window) registry and disposes it.", () => {
+        const debuggerProps = getDebuggerProps();
+        const { containerId } = debuggerProps;
 
-		const clientDebugger = initializeDebugger(debuggerProps);
+        const clientDebugger = initializeDebugger(debuggerProps);
 
-		// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-		expect(clientDebugger.disposed).to.be.false;
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        expect(clientDebugger.disposed).to.be.false;
 
-		let debuggerRegistry = getDebuggerRegistry();
-		expect(debuggerRegistry.size).to.equal(1);
+        let debuggerRegistry = getDebuggerRegistry();
+        expect(debuggerRegistry.size).to.equal(1);
 
-		closeFluidClientDebugger(containerId);
+        closeFluidClientDebugger(containerId);
 
-		// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-		expect(clientDebugger.disposed).to.be.true;
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        expect(clientDebugger.disposed).to.be.true;
 
-		debuggerRegistry = getDebuggerRegistry();
-		expect(debuggerRegistry.size).to.equal(0);
-	});
+        debuggerRegistry = getDebuggerRegistry();
+        expect(debuggerRegistry.size).to.equal(0);
+    });
 });
 
 /* eslint-enable @typescript-eslint/no-non-null-assertion */
