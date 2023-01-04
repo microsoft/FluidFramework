42c5bff2d64bb3f9090d09a34d31e0f9f90feb8c
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-01-04 22:56:50 +0000

    docs: Update comment

--- packages/dds/cell/src/interfaces.ts
+++ packages/dds/cell/src/interfaces.ts
@@ -26,8 +26,55 @@ export interface ISharedCellEvents<T> extends ISharedObjectEvents {
 }
 
 /**
- * Distributed Data Structure (DDS), which stores a single shared value that can be edited or deleted.
+ * A Distributed Data Structure (DDS), which stores a single shared value that can be edited or deleted.
+ *
+ * @typeParam T - The type of cell data. Must be serializable.
+ *
+ * @example Creation:
+ *
+ * To create a `SharedCell`, call the static create method:
+ *
+ * ```typescript
+ * const myCell = SharedCell.create(this.runtime, id);
+ * ```
+ *
+ * @example Usage:
+ *
+ * The value stored in the cell can be set with the `.set()` method and retrieved with the `.get()` method:
+ *
+ * ```typescript
+ * myCell.set(3);
+ * console.log(myCell.get()); // 3
+ * ```
+ *
+ * The value must only be plain JS objects or `SharedObject` handles (e.g. to another DDS or Fluid object).
+ * In collaborative scenarios, the value is settled with a policy of _last write wins_.
+ *
+ * The `.delete()` method will delete the stored value from the cell:
+ *
+ * ```typescript
+ * myCell.delete();
+ * console.log(myCell.get()); // undefined
+ * ```
+ *
+ * The `.empty()` method will check if the value is undefined.
+ *
+ * ```typescript
+ * if (myCell.empty()) {
+ *   // myCell.get() will return undefined
+ * } else {
+ *   // myCell.get() will return a non-undefined value
+ * }
+ * ```
+ *
+ * @example Eventing:
+ *
+ * `SharedCell` is an `EventEmitter`, and will emit events when other clients make modifications. You should
+ * register for these events and respond appropriately as the data is modified. `valueChanged` will be emitted
+ * in response to a `set`, and `delete` will be emitted in response to a `delete`.
  */
+// TODO: use `unknown` instead (breaking change).
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export interface ISharedCell<T = any> extends ISharedObject<ISharedCellEvents<T>> {
     /**
      * Retrieves the cell value.
