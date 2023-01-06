be896105e40bc816239edc5ffd3dfa3081f01fe0
jikim-msft <111468570+jikim-msft@users.noreply.github.com>
2023-01-06 21:37:39 +0000

    update TODO comment

--- packages/drivers/routerlicious-driver/src/errorUtils.ts
+++ packages/drivers/routerlicious-driver/src/errorUtils.ts
@@ -67,8 +67,7 @@ export function createR11sNetworkError(
             // a network error with no status code (e.g. err:ERR_CONN_REFUSED or err:ERR_FAILED) and
             // the error message will start with NetworkError as defined in restWrapper.ts
             // If there exists a self-signed SSL certificates error, throw a NonRetryableError
-            // Instead of relying on string matching, filter error based on the error code / ID
-
+            // TODO: instead of relying on string matching, filter error based on the error code like we do for websocket connections
             if (errorMessage.includes("failed, reason: self signed certificate")) {
                 return new NonRetryableError(errorMessage, R11sErrorType.sslCertError, props);
             }
--- packages/tools/fluid-runner/src/test/parseBundleAndExportFile.spec.ts
+++ packages/tools/fluid-runner/src/test/parseBundleAndExportFile.spec.ts
@@ -70,7 +70,6 @@ describe("parseBundleAndExportFile", () => {
                 telemetryFile,
             );
 
-            // Instead of relying on string matching, filter error based on the error code / ID
             assert(!result.success, "result should not be successful");
             assert(result.errorMessage.includes("IFluidFileConverter"),
                 `error message does not contain "IFluidFileConverter" [${result.errorMessage}]`);
