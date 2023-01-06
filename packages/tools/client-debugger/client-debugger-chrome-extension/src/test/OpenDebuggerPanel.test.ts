f4767d814ee370ead79609cc0f24065f4d5e268e
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-01-06 15:31:15 -0800

    feat: Add initial Fluid Client Debugger prototype libraries (#13425)
    ### Libraries
    
    - @fluidframework/client-debugger
    - Contains library for initializing / closing debuggers associated with
    Container instances. The majority of this library's exports are
    `@internal` intended to be used by related libraries (this policy can
    always be changed in the future if we wish to allow external users to
    write their own tooling against the types here).
    - @fluidframework/client-debugger-view
    - A visualizer library that displays basic stats about any debugger
    instances registered with `@fluidframework/client-debugger`.
    - @fluidframework/client-debugger-chrome-extension
    - A thin wrapper around `@fluidframework/client-debugger-view`, which
    generates an installable Chrome extension.
    
    NOTE: All of the above libraries are still in early development. They
    are all marked as private, and will not be published until they have
    been deemed feature complete.
    
    Co-authored-by: Tong Chen <114451900+chentong7@users.noreply.github.com>
    Co-authored-by: jikim-msft <111468570+jikim-msft@users.noreply.github.com>

---
+++ packages/tools/client-debugger/client-debugger-chrome-extension/src/test/OpenDebuggerPanel.test.ts
@@ -0,0 +1,23 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+import { openDebuggerPanel } from "../OpenDebuggerPanel";
+import { isDebuggerPanelOpen } from "../Utilities";
+
+describe("OpenDebuggerPanelScript tests", () => {
+    beforeEach(() => {
+        document.body.innerHTML = `<div id="test">test</div>`;
+    });
+
+    it("Verify that debugger panel is launched by script", async () => {
+        // Verify that the panel is not live
+        expect(isDebuggerPanelOpen()).toBe(false);
+
+        // Execute script
+        await openDebuggerPanel();
+
+        // Verify that the panel has been launched
+        expect(isDebuggerPanelOpen()).toBe(true);
+    });
+});
---
+++ packages/tools/client-debugger/client-debugger/src/index.ts
@@ -0,0 +1,64 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * Contains a simple API for initializing debug sessions for recording and propogating information
+ * about a given {@link @fluidframework/container-definitions#IContainer | Fluid Container} and its
+ * {@link @fluidframework/container-definitions#Audience}.
+ *
+ * Debugger instances are created per `Container` instance and are bound to the `globalThis` context
+ * to be accessible to related tools.
+ *
+ * - See the package README for more details on related tools that are enabled via these debugger instances.
+ *
+ * @remarks
+ *
+ * The general usage pattern for this library is to first initialize a debugger for a given Fluid Client
+ * ({@link @fluidframework/container-definitions#IContainer} and {@link @fluidframework/container-definitions#IAudience})
+ * by calling {@link initializeFluidClientDebugger} during application setup / any time after your container has been
+ * attached.
+ *
+ * Then, during application teardown, call {@link closeFluidClientDebugger} to clean up the debugger and its resources.
+ *
+ * @example Initialization
+ *
+ * ```typescript
+ * initializeFluidClientDebugger({
+ *  containerId,
+ *  container,
+ *  containerData: {
+ *      rootMap: sharedMap
+ *  },
+ * });
+ * ```
+ *
+ * @example Disposal
+ *
+ * ```typescript
+ * closeFluidClientDebugger(containerId);
+ * ```
+ *
+ * @packageDocumentation
+ */
+
+export { MemberChangeKind } from "./Audience";
+
+export { IFluidClientDebugger, IFluidClientDebuggerEvents } from "./IFluidClientDebugger";
+
+export {
+    AudienceChangeLogEntry,
+    ConnectionStateChangeLogEntry,
+    LogEntry,
+    StateChangeLogEntry,
+} from "./Logs";
+
+export {
+    clearDebuggerRegistry,
+    closeFluidClientDebugger,
+    FluidClientDebuggerProps,
+    getFluidClientDebugger,
+    getFluidClientDebuggers,
+    initializeFluidClientDebugger,
+} from "./Registry";
