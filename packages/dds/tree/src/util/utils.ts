4e29a769c527f62eb435e4d9ddcdadaef2bf8e26
justus-camp <102552460+justus-camp@users.noreply.github.com>
2023-01-10 21:17:18 +0000

    Put back utilities that shouldn't have been deleted

--- packages/dds/tree/src/util/utils.ts
+++ packages/dds/tree/src/util/utils.ts
@@ -12,6 +12,11 @@ export type RecursiveReadonly<T> = {
     readonly [P in keyof T]: RecursiveReadonly<T[P]>;
 };
 
+/**
+ * Remove `readonly` from all fields.
+ */
+export type Mutable<T> = { -readonly [P in keyof T]: T[P] };
+
 export function clone<T>(original: T): T {
     return structuredClone(original);
 }
@@ -132,6 +137,22 @@ export function compareSets<T>({
         }
     }
     return true;
+}
+
+/**
+ * Retrieve a value from a map with the given key, or create a new entry if the key is not in the map.
+ * @param map - The map to query/update
+ * @param key - The key to lookup in the map
+ * @param defaultValue - a function which returns a default value. This is called and used to set an initial value for the given key in the map if none exists
+ * @returns either the existing value for the given key, or the newly-created value (the result of `defaultValue`)
+ */
+export function getOrCreate<K, V>(map: Map<K, V>, key: K, defaultValue: (key: K) => V): V {
+    let value = map.get(key);
+    if (value === undefined) {
+        value = defaultValue(key);
+        map.set(key, value);
+    }
+    return value;
 }
 
 /**
