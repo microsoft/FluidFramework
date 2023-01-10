a053495091ef606d984683158c50d995b267928d
Kajari Ghosh <kajarighosh@microsoft.com>
2023-01-10 23:06:49 +0000

    another fix

--- server/tinylicious/src/runner.ts
+++ server/tinylicious/src/runner.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-import EventEmitter from "events";
+import { EventEmitter } from "events";
 import {
     IDocumentStorage,
     IOrdererManager,
