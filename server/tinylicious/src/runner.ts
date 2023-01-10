55880d48e5d2716b0fe5f377dbd0386ee81c9112
Kajari Ghosh <kajarighosh@microsoft.com>
2023-01-10 21:12:23 +0000

    create POST route in tinylicious toenable triggering web-hook

--- server/tinylicious/src/runner.ts
+++ server/tinylicious/src/runner.ts
@@ -3,6 +3,7 @@
  * Licensed under the MIT License.
  */
 
+import EventEmitter from "events";
 import {
     IDocumentStorage,
     IOrdererManager,
@@ -52,7 +53,8 @@ export class TinyliciousRunner implements IRunner {
             throw e;
         }
 
-        const alfred = app.create(this.config, this.storage, this.mongoManager);
+        const eventEmitter = new EventEmitter();
+        const alfred = app.create(this.config, this.storage, this.mongoManager, eventEmitter);
         alfred.set("port", this.port);
 
         this.server = this.serverFactory.create(alfred);
@@ -78,6 +80,7 @@ export class TinyliciousRunner implements IRunner {
             undefined,
             undefined,
             httpServer,
+            eventEmitter,
         );
 
         // Listen on provided port, on all network interfaces.
