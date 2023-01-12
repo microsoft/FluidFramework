b531b6fbcde70d527431ebe886b800014c3bd994
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-01-12 01:02:21 +0000

    revert: Infra changes

--- packages/tools/client-debugger/client-debugger-chrome-extension/jest.config.js
+++ packages/tools/client-debugger/client-debugger-chrome-extension/jest.config.js
@@ -3,18 +3,8 @@
  * Licensed under the MIT License.
  */
 
-// Get the test port from the global map and set it in env for this test
-const testTools = require("@fluidframework/test-tools");
-const { name } = require("./package.json");
-
-const mappedPort = testTools.getTestPort(name);
-process.env["PORT"] = mappedPort;
-
 module.exports = {
     preset: "ts-jest",
-    globals: {
-        PATH: `http://localhost:${mappedPort}`,
-    },
     roots: ["<rootDir>/src"],
     transform: {
         "^.+\\.tsx?$": "ts-jest",
@@ -22,8 +12,6 @@ module.exports = {
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx)?$",
     testPathIgnorePatterns: ["/node_modules/", "dist"],
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    setupFiles: ["./jestTestSetup.js"],
-    globalSetup: "./jestGlobalSetup.js",
-    globalTeardown: "./jestGlobalTeardown.js",
+    setupFiles: ["./testSetup.js"],
     coveragePathIgnorePatterns: ["/node_modules/", "/src/test/"],
 };
