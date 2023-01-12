d4ad514041c84d44005d1c3d38db5e69c787a9ab
Wayne Ferrao <wayneferrao@microsoft.com>
2023-01-12 19:58:07 +0000

    Test restructure debug

--- examples/hosts/app-integration/external-data/tests/taskList.test.ts
+++ examples/hosts/app-integration/external-data/tests/taskList.test.ts
@@ -7,7 +7,6 @@ import { globals } from "../jest.config";
 
 describe("taskList", () => {
     beforeAll(async () => {
-        jest.setTimeout(30_000);
         // Wait for the page to load first before running any tests
         // so this time isn't attributed to the first test
         await page.goto(globals.PATH, { waitUntil: "load", timeout: 0 });
@@ -15,13 +14,12 @@ describe("taskList", () => {
 
     beforeEach(async () => {
         await page.goto(globals.PATH, { waitUntil: "load" });
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-explicit-any
-        await page.waitFor(() => (window as any).fluidStarted);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/dot-notation
+        await page.waitFor(() => window["fluidStarted"]);
     });
 
     it("loads and there's an input", async () => {
         // Validate the input shows up
-        console.log(globals.PATH)
         await page.waitForSelector("input");
     });
 });
