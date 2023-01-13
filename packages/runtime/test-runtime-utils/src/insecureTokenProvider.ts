70ef04cd444a278f55d83f670cac37a62581ba6a
RonnyM <103609950+ms-avocado@users.noreply.github.com>
2023-01-13 01:16:12 +0000

    added new custom type for InsecureTokenProvider

--- packages/runtime/test-runtime-utils/src/insecureTokenProvider.ts
+++ packages/runtime/test-runtime-utils/src/insecureTokenProvider.ts
@@ -8,6 +8,17 @@ import { ITokenProvider, ITokenResponse } from "@fluidframework/routerlicious-dr
 import { generateToken } from "./generateToken";
 
 /**
+ * Insecure user definition.
+ * It extends the base IUser interface with a `name` property.
+ */
+export interface IInsecureUser extends IUser {
+	/**
+	 * Name of the user making the connection to the service.
+	 */
+	name: string;
+}
+
+/**
  * Provides an in memory implementation of {@link @fluidframework/routerlicious-driver#ITokenProvider} that can be
  * used to insecurely connect to the Fluid Relay.
  *
@@ -15,67 +26,57 @@ import { generateToken } from "./generateToken";
  * It simply makes examples where authentication is not relevant easier to bootstrap.
  */
 export class InsecureTokenProvider implements ITokenProvider {
-    constructor(
-        /**
-         * Private server tenantKey for generating tokens.
-         */
-        private readonly tenantKey: string,
+	constructor(
+		/**
+		 * Private server tenantKey for generating tokens.
+		 */
+		private readonly tenantKey: string,
 
-        /**
-         * User with whom generated tokens will be associated.
-         */
-        private readonly user: IUser,
+		/**
+		 * User with whom generated tokens will be associated.
+		 */
+		private readonly user: IInsecureUser,
 
-        /**
-         * Optional. Override of scopes. If a param is not provided, InsecureTokenProvider
-         * will use the default scopes which are document read, write and summarizer write.
-         *
-         * @param scopes - See {@link @fluidframework/protocol-definitions#ITokenClaims.scopes}
-         *
-         * @defaultValue - [ ScopeType.DocRead, ScopeType.DocWrite, ScopeType.SummaryWrite ]
-         */
-        private readonly scopes?: ScopeType[],
-    ) {
+		/**
+		 * Optional. Override of scopes. If a param is not provided, InsecureTokenProvider
+		 * will use the default scopes which are document read, write and summarizer write.
+		 *
+		 * @param scopes - See {@link @fluidframework/protocol-definitions#ITokenClaims.scopes}
+		 *
+		 * @defaultValue - [ ScopeType.DocRead, ScopeType.DocWrite, ScopeType.SummaryWrite ]
+		 */
+		private readonly scopes?: ScopeType[],
+	) {}
 
-    }
+	/**
+	 * {@inheritDoc @fluidframework/routerlicious-driver#ITokenProvider.fetchOrdererToken}
+	 */
+	public async fetchOrdererToken(tenantId: string, documentId?: string): Promise<ITokenResponse> {
+		return {
+			fromCache: true,
+			jwt: generateToken(
+				tenantId,
+				this.tenantKey,
+				this.scopes ?? [ScopeType.DocRead, ScopeType.DocWrite, ScopeType.SummaryWrite],
+				documentId,
+				this.user,
+			),
+		};
+	}
 
-    /**
-     * {@inheritDoc @fluidframework/routerlicious-driver#ITokenProvider.fetchOrdererToken}
-     */
-    public async fetchOrdererToken(tenantId: string, documentId?: string): Promise<ITokenResponse> {
-        return {
-            fromCache: true,
-            jwt: generateToken(
-                tenantId,
-                this.tenantKey,
-                this.scopes ?? [
-                    ScopeType.DocRead,
-                    ScopeType.DocWrite,
-                    ScopeType.SummaryWrite,
-                ],
-                documentId,
-                this.user,
-            ),
-        };
-    }
-
-    /**
-     * {@inheritDoc @fluidframework/routerlicious-driver#ITokenProvider.fetchStorageToken}
-     */
-    public async fetchStorageToken(tenantId: string, documentId: string): Promise<ITokenResponse> {
-        return {
-            fromCache: true,
-            jwt: generateToken(
-                tenantId,
-                this.tenantKey,
-                this.scopes ?? [
-                    ScopeType.DocRead,
-                    ScopeType.DocWrite,
-                    ScopeType.SummaryWrite,
-                ],
-                documentId,
-                this.user,
-            ),
-        };
-    }
+	/**
+	 * {@inheritDoc @fluidframework/routerlicious-driver#ITokenProvider.fetchStorageToken}
+	 */
+	public async fetchStorageToken(tenantId: string, documentId: string): Promise<ITokenResponse> {
+		return {
+			fromCache: true,
+			jwt: generateToken(
+				tenantId,
+				this.tenantKey,
+				this.scopes ?? [ScopeType.DocRead, ScopeType.DocWrite, ScopeType.SummaryWrite],
+				documentId,
+				this.user,
+			),
+		};
+	}
 }
