6bae80fdfa8fba2c3d4591aa37c655f250f234ea
justus-camp <102552460+justus-camp@users.noreply.github.com>
2023-01-17 18:04:27 +0000

    Disable idCompressor by default

--- packages/runtime/container-runtime/src/containerRuntime.ts
+++ packages/runtime/container-runtime/src/containerRuntime.ts
@@ -688,7 +688,7 @@ export class ContainerRuntime extends TypedEventEmitter<IContainerRuntimeEvents>
             },
             maxBatchSizeInBytes = defaultMaxBatchSizeInBytes,
             chunkSizeInBytes = Number.POSITIVE_INFINITY,
-            enableRuntimeCompressor = true,
+            enableRuntimeCompressor = false,
             enableOpReentryCheck = false,
         } = runtimeOptions;
 
