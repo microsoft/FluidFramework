b2f2340bbaaeb04089148d71430353e442c9fa1a
Wayne Ferrao <wayneferrao@microsoft.com>
2023-01-19 14:16:54 -0500

    Restructured data to use objects and Responses (#13510)
    [AB#2678](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/2678)
    
    This PR restructures the data sent back from the mock external service
    to be more realistic and use a Response object.

--- examples/hosts/app-integration/external-data/.eslintrc.js
+++ examples/hosts/app-integration/external-data/.eslintrc.js
@@ -4,12 +4,11 @@
  */
 
 module.exports = {
-    extends: [
-        require.resolve("@fluidframework/eslint-config-fluid/strict"),
-        "prettier",
+    "extends": [
+        require.resolve("@fluidframework/eslint-config-fluid"), "prettier",
     ],
-    rules: { },
-    overrides: [
+    "rules": { },
+    "overrides": [
         {
             files: ["tests/*"],
             rules: {
