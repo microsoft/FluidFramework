3598d4a76882bc5964112c6e4509594db926b98a
Tyler Butler <tyler@tylerbutler.com>
2023-01-19 16:18:07 -0800

    fix

--- build-tools/packages/build-cli/src/commands/generate/readme.ts
+++ build-tools/packages/build-cli/src/commands/generate/readme.ts
@@ -4,6 +4,7 @@
  */
 import { Interfaces } from "@oclif/core";
 import { default as BaseReadme } from "oclif/lib/commands/readme";
+import * as semver from "semver";
 
 export default class Readme extends BaseReadme {
     static summary = "Adds commands to README.md in current directory.";
@@ -25,13 +26,22 @@ Customize the code URL prefix by setting oclif.repositoryPrefix in package.json.
             ...(config.pjson.oclif.additionalVersionFlags ?? []).sort(),
         ];
         const versionFlagsString = `(${versionFlags.join("|")})`;
+        const version = semver.parse(config.version) ?? config.version;
+
+        // We change the version in CI, which causes the readmes to get updated during a CI build. Including the
+        // version section in the readme isn't valuable, so we strip off thhe prerelease section here.
+        const versionString =
+            typeof version === "string"
+                ? version
+                : `${version.major}.${version.minor}.${version.patch}`;
+
         return [
             `\`\`\`sh-session
 $ npm install -g ${config.name}
 $ ${config.bin} COMMAND
 running command...
 $ ${config.bin} ${versionFlagsString}
-${config.name}/${config.version}
+${config.name}/${versionString}
 $ ${config.bin} --help [COMMAND]
 USAGE
   $ ${config.bin} COMMAND
