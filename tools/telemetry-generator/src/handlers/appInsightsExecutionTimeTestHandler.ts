c2ae79cd29ac82430f415a0442f302be121fe5ed
seanimam <105244057+seanimam@users.noreply.github.com>
2023-01-25 23:31:32 +0000

    adds try/catch with error logging for metric emission and attempts to fix passing in keyvault value as connection string

--- tools/telemetry-generator/src/handlers/appInsightsExecutionTimeTestHandler.ts
+++ tools/telemetry-generator/src/handlers/appInsightsExecutionTimeTestHandler.ts
@@ -9,36 +9,47 @@
 module.exports = async function handler(fileData, logger) {
     fileData.benchmarks.forEach(async (testData) => {
         const arithmeticMeanMetricName = `${fileData.suiteName}_${testData.benchmarkName}_arithmeticMean`;
-        await logger.trackMetric({
-            name: arithmeticMeanMetricName,
-            value: testData.stats.arithmeticMean,
-            namespace: "performance_benchmark_executionTime",
-            properties: {
-                buildId: process.env.BUILD_ID,
-                branchName: process.env.BRANCH_NAME,
-                category: "performance",
-                eventName: "Benchmark",
-                benchmarkType: "ExecutionTime",
-                suiteName: fileData.suiteName,
-                benchmarkName: testData.benchmarkName,
-            }
-        });
+        try {
+            console.log(`emitting metric ${arithmeticMeanMetricName} with value ${testData.stats.arithmeticMean}`)
+            await logger.trackMetric({
+                name: arithmeticMeanMetricName,
+                value: testData.stats.arithmeticMean,
+                namespace: "performance_benchmark_executionTime",
+                properties: {
+                    buildId: process.env.BUILD_ID,
+                    branchName: process.env.BRANCH_NAME,
+                    category: "performance",
+                    eventName: "Benchmark",
+                    benchmarkType: "ExecutionTime",
+                    suiteName: fileData.suiteName,
+                    benchmarkName: testData.benchmarkName,
+                }
+            });
+        } catch (error) {
+            console.error(`failed to emit metric ${arithmeticMeanMetricName}`, error);
+        }
 
         const marginOfErrorMetricName = `${fileData.suiteName}_${testData.benchmarkName}_marginOfError`;
-        await logger.trackMetric({
-            name: marginOfErrorMetricName,
-            value: testData.stats.marginOfError,
-            namespace: "performance_benchmark_executionTime",
-            properties: {
-                buildId: process.env.BUILD_ID,
-                branchName: process.env.BRANCH_NAME,
-                category: "performance",
-                eventName: "Benchmark",
-                benchmarkType: "ExecutionTime",
-                suiteName: fileData.suiteName,
-                benchmarkName: testData.benchmarkName,
-            }
-        });
+        try {
+            console.log(`emitting metric ${arithmeticMeanMetricName} with value ${testData.stats.marginOfError}`)
+            await logger.trackMetric({
+                name: marginOfErrorMetricName,
+                value: testData.stats.marginOfError,
+                namespace: "performance_benchmark_executionTime",
+                properties: {
+                    buildId: process.env.BUILD_ID,
+                    branchName: process.env.BRANCH_NAME,
+                    category: "performance",
+                    eventName: "Benchmark",
+                    benchmarkType: "ExecutionTime",
+                    suiteName: fileData.suiteName,
+                    benchmarkName: testData.benchmarkName,
+                }
+            });
+        } catch (error) {
+            console.error(`failed to emit metric ${marginOfErrorMetricName}`, error);
+        }
+
 
     });
 };
--- tools/telemetry-generator/src/handlers/appInsightsMemoryUsageTestHandler.ts
+++ tools/telemetry-generator/src/handlers/appInsightsMemoryUsageTestHandler.ts
@@ -9,35 +9,41 @@
 module.exports = async function handler(fileData, logger) {
     fileData.tests.forEach(async (testData) => {
         const heapUsedAvgMetricName = `${fileData.suiteName}_${testData.benchmarkName}_heapUsedAvg`;
-        console.log(`emitting metric ${heapUsedAvgMetricName} with value ${testData.testData.stats.mean}`)
-        await logger.trackMetric({
-             name: heapUsedAvgMetricName,
-             value: testData.testData.stats.mean,
-             namespace: 'performance_benchmark_memoryUsage',
-             properties: {
-                 category: "performance",
-                 eventName: "Benchmark",
-                 benchmarkType: "MemoryUsage",
-                 suiteName: fileData.suiteName,
-                 testName: testData.testName,
-             }
-         });
-         console.log(`successfully emitted metric!`);
+        try {
+            console.log(`emitting metric ${heapUsedAvgMetricName} with value ${testData.testData.stats.mean}`);
+            await logger.trackMetric({
+                 name: heapUsedAvgMetricName,
+                 value: testData.testData.stats.mean,
+                 namespace: 'performance_benchmark_memoryUsage',
+                 properties: {
+                     category: "performance",
+                     eventName: "Benchmark",
+                     benchmarkType: "MemoryUsage",
+                     suiteName: fileData.suiteName,
+                     testName: testData.testName,
+                 }
+             });
+        } catch (error) {
+            console.error(`failed to emit metric ${heapUsedAvgMetricName}`, error);
+        }
 
         const heapUsedStdDevMetricName = `${fileData.suiteName}_${testData.benchmarkName}_heapUsedStdDev`;
-        console.log(`emitting metric ${heapUsedStdDevMetricName} with value ${testData.testData.stats.deviation}`)
-        await logger.trackMetric({
-            name: heapUsedStdDevMetricName,
-            value: testData.testData.stats.deviation,
-            namespace: 'performance_benchmark_memoryUsage',
-            properties: {
-                category: "performance",
-                eventName: "Benchmark",
-                benchmarkType: "MemoryUsage",
-                suiteName: fileData.suiteName,
-                testName: testData.testName,
-            }
-        });
-        console.log(`successfully emitted metric!`);
+        try {
+            console.log(`emitting metric ${heapUsedStdDevMetricName} with value ${testData.testData.stats.deviation}`)
+            await logger.trackMetric({
+                name: heapUsedStdDevMetricName,
+                value: testData.testData.stats.deviation,
+                namespace: 'performance_benchmark_memoryUsage',
+                properties: {
+                    category: "performance",
+                    eventName: "Benchmark",
+                    benchmarkType: "MemoryUsage",
+                    suiteName: fileData.suiteName,
+                    testName: testData.testName,
+                }
+            });
+        } catch (error) {
+            console.error(`failed to emit metric ${heapUsedStdDevMetricName}`, error);
+        }
     });
 };
