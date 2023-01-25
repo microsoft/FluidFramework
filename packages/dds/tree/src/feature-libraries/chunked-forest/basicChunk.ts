90261404c02ea968dabb5470b987547034e24ace
Craig Macomber <Craig.Macomber@microsoft.com>
2023-01-24 22:49:49 -0800

    Fix basic chunk cursor and chunked forest cursors

--- packages/dds/tree/src/feature-libraries/chunked-forest/basicChunk.ts
+++ packages/dds/tree/src/feature-libraries/chunked-forest/basicChunk.ts
@@ -13,9 +13,10 @@ import {
     TreeValue,
     Value,
     TreeType,
+    PathRootPrefix,
 } from "../../core";
 import { fail } from "../../util";
-import { SynchronousCursor } from "../treeCursorUtils";
+import { prefixPath, SynchronousCursor } from "../treeCursorUtils";
 import { ChunkedCursor, dummyRoot, ReferenceCountedBase, TreeChunk } from "./chunk";
 
 /**
@@ -162,23 +163,30 @@ export class BasicChunkCursor extends SynchronousCursor implements ChunkedCursor
         assert(found, "child must exist at index");
     }
 
-    public getPath(): UpPath | undefined {
+    public getPath(prefix?: PathRootPrefix): UpPath {
         assert(this.mode === CursorLocationType.Nodes, "must be in nodes mode");
-        return this.getOffsetPath(0);
+        const path = this.getOffsetPath(0, prefix);
+        assert(path !== undefined, "field root cursor should never have undefined path");
+        return path;
     }
 
-    public getFieldPath(): FieldUpPath {
+    public getFieldPath(prefix?: PathRootPrefix): FieldUpPath {
         assert(this.mode === CursorLocationType.Fields, "must be in fields mode");
         return {
-            field: this.getFieldKey(),
-            parent: this.getOffsetPath(1),
+            field:
+                this.indexStack.length === 1
+                    ? prefix?.rootFieldOverride ?? this.getFieldKey()
+                    : this.getFieldKey(),
+            parent: this.getOffsetPath(1, prefix),
         };
     }
 
-    private getOffsetPath(offset: number): UpPath | undefined {
+    private getOffsetPath(offset: number, prefix: PathRootPrefix | undefined): UpPath | undefined {
+        // It is more efficient to handle prefix directly in here rather than delegating to PrefixedPath.
+
         const length = this.indexStack.length - offset;
         if (length === -1) {
-            return undefined; // At root
+            return prefix?.parent; // At root
         }
 
         assert(length > 0, "invalid offset to above root");
@@ -193,21 +201,25 @@ export class BasicChunkCursor extends SynchronousCursor implements ChunkedCursor
         // When navigating up, adjust cached anchor if present.
 
         let path: UpPath | undefined;
+        function updatePath(newPath: UpPath): void {
+            path = path === undefined ? prefixPath(prefix, newPath) : newPath;
+        }
+
         // Skip top level, since root node in path is "undefined" and does not have a parent or index.
         for (let height = 1; height < length; height += 2) {
             const key = this.getStackedFieldKey(height - 1);
-            path = {
+            updatePath({
                 parent: path,
                 parentIndex: this.getStackedNodeIndex(height),
                 parentField: key,
-            };
+            });
         }
 
-        path = {
+        updatePath({
             parent: path,
             parentIndex: offset === 0 ? this.index : this.getStackedNodeIndex(length),
             parentField: this.getStackedFieldKey(length - 1),
-        };
+        });
         return path;
     }
 
