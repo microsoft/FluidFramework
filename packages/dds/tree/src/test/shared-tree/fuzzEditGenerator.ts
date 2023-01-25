3557bbb9b7647c5bbc69bba0945fdf00916b7758
daesun-park <108361046+daesun-park@users.noreply.github.com>
2023-01-25 19:28:45 +0000

    added copyright header

--- packages/dds/tree/src/test/shared-tree/fuzzEditGenerator.ts
+++ packages/dds/tree/src/test/shared-tree/fuzzEditGenerator.ts
@@ -1,3 +1,7 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
 import { strict as assert } from "assert";
 import {
     AcceptanceCondition,
