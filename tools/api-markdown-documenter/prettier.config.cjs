c3aaa1c9b339e3369f9e66845d84489c65e7cb7a
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-01-26 15:46:38 -0800

    build(api-markdown-documenter): Update dependency on `build-common` and run updated prettier config (#13825)
    Rule config changing spaces to tabs occurred between 1.0 and 1.1 in
    build-common. This PR updates our dependency and runs formatting on the
    code.

--- tools/api-markdown-documenter/prettier.config.cjs
+++ tools/api-markdown-documenter/prettier.config.cjs
@@ -4,12 +4,12 @@
  */
 
 module.exports = {
-    ...require("@fluidframework/build-common/prettier.config.cjs"),
-    importOrder: [
-        "^node:(.*)$", // Special-case `node:` imports
-        "<THIRD_PARTY_MODULES>",
-        "^[./]",
-    ],
-    importOrderSeparation: true,
-    importOrderSortSpecifiers: true,
+	...require("@fluidframework/build-common/prettier.config.cjs"),
+	importOrder: [
+		"^node:(.*)$", // Special-case `node:` imports
+		"<THIRD_PARTY_MODULES>",
+		"^[./]",
+	],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
 };
