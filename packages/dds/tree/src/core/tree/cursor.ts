2f5f7f7fdaa6f21a3aac717e6486bd1f78332e7f
Jenn <jenn.thakugan@icloud.com>
2023-01-26 01:09:23 +0000

    Raw text

--- packages/dds/tree/src/core/tree/cursor.ts
+++ packages/dds/tree/src/core/tree/cursor.ts
@@ -14,6 +14,16 @@ export interface ITreeCursor {
     /**
      * What kind of place the cursor is at.
      * Determines which operations are allowed.
+     *
+     * @remarks Cursers of cursors frequently need to refer to places in trees, both fields and nodes.
+     * Approaches other than having the cursor have separate modes for these cases had issues even worse than having the two modes.
+     * For example modeling fields as parent+key has issues when there is no parent, and doesn't provide a great way to do iteration over
+     * fields that has a nice API and makes it easy for the implementation to track state while traversing (like its current location inside
+     * a sequence tree of fields) without having to allocate some state management for that. Another approach, of using arrays of cursors
+     * for fields (like we currently do for inserting content) is very inefficent and better addressed by a duel mode cursor.
+     * Another approach, of using the first node in a field when refering to the field gets confusing since its unclear if a given cursor
+     * means that node, or that node, and the ones after it, and in the second case its hard to restor the cursor back to the right state
+     * when returning: also it doeesn't work for empty fields. Overall there just didn't seem to be a way that sucked less than the duel mode API.
      */
     readonly mode: CursorLocationType;
 
