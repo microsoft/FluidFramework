384e24f564e7f483c08661884a520a81fabe7318
Tyler Butler <tyler@tylerbutler.com>
2023-01-26 13:59:30 -0800

    config

---
+++ examples/hosts/app-integration/external-data/prettier.config.cjs
@@ -0,0 +1,9 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = {
+    ...require("@fluidframework/build-common/prettier.config.cjs"),
+    useTabs: false, // changed from false to true
+};
