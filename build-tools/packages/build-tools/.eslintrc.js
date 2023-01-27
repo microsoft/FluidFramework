9c8671de8be93e4bb8eb07754cb9b94cbfffe0f7
Tyler Butler <tyler@tylerbutler.com>
2023-01-27 15:41:47 -0800

    config

--- build-tools/packages/build-tools/.eslintrc.js
+++ build-tools/packages/build-tools/.eslintrc.js
@@ -4,7 +4,7 @@
  */
 
 module.exports = {
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
 	plugins: ["@typescript-eslint"],
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
