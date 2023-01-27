900c1c0302fea51206ba27c116cc71df51ba2335
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-01-27 00:15:18 +0000

    tools: Update eslint config

--- examples/hosts/app-integration/external-data/.eslintrc.js
+++ examples/hosts/app-integration/external-data/.eslintrc.js
@@ -4,25 +4,31 @@
  */
 
 module.exports = {
-    "extends": [
-        require.resolve("@fluidframework/eslint-config-fluid"), "prettier",
+    extends: [
+        require.resolve("@fluidframework/eslint-config-fluid"),
+        "prettier",
     ],
     rules: {
-        // Fine for tests to use node.js modules.
-        // Tests will ensure our webpack configuration is correctly set up to support any that we use.
-        "import/no-nodejs-modules": "off",
-     },
+        "import/no-nodejs-modules": ["error", { allow: ["http"] }],
+    },
     overrides: [
         {
             files: ["tests/*"],
             rules: {
+                // Fine for tests to import from dev dependencies
+                "import/no-extraneous-dependencies": [
+                    "error",
+                    { devDependencies: true },
+                ],
+
                 // Since the "tests" directory is adjacent to "src", and this package (intentionally) does not expose
                 // a single exports roll-up, reaching into "src" is required.
                 "import/no-internal-modules": "off",
 
-                // Fine for tests to import from dev dependencies
-                "import/no-extraneous-dependencies": ["error", { devDependencies: true }]
-            }
-        }
-    ]
+                // Fine for tests to use node.js modules.
+                // Tests will ensure our webpack configuration is correctly set up to support any that we use.
+                "import/no-nodejs-modules": "off",
+            },
+        },
+    ],
 };
