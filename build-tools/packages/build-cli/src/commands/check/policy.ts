18093a590976f15b03c6e5c9e6a6c67c76cac652
Tyler Butler <tylerbu@microsoft.com>
2023-01-27 14:21:55 -0800

    build(build-tools): Change formatting to tabs (#13859)
    Updates the build-tools prettier config to use tabs and reformats all
    projects.

--- build-tools/packages/build-cli/src/commands/check/policy.ts
+++ build-tools/packages/build-cli/src/commands/check/policy.ts
@@ -13,23 +13,23 @@ import { Handler, policyHandlers, readJsonAsync } from "@fluidframework/build-to
 import { BaseCommand } from "../../base";
 
 const readStdin: () => Promise<string | undefined> = async () => {
-    return new Promise((resolve) => {
-        const stdin = process.openStdin();
-        stdin.setEncoding("utf-8");
+	return new Promise((resolve) => {
+		const stdin = process.openStdin();
+		stdin.setEncoding("utf-8");
 
-        let data = "";
-        stdin.on("data", (chunk) => {
-            data += chunk;
-        });
+		let data = "";
+		stdin.on("data", (chunk) => {
+			data += chunk;
+		});
 
-        stdin.on("end", () => {
-            resolve(data);
-        });
+		stdin.on("end", () => {
+			resolve(data);
+		});
 
-        if (stdin.isTTY) {
-            resolve("");
-        }
-    });
+		if (stdin.isTTY) {
+			resolve("");
+		}
+	});
 };
 
 type policyAction = "handle" | "resolve" | "final";
@@ -47,268 +47,268 @@ type policyAction = "handle" | "resolve" | "final";
  * `fluid-repo-policy-check -s` is equivalent to `flub check policy --stdin`
  */
 export class CheckPolicy extends BaseCommand<typeof CheckPolicy.flags> {
-    static description =
-        "Checks and applies policies to the files in the repository, such as ensuring a consistent header comment in files, assert tagging, etc.";
+	static description =
+		"Checks and applies policies to the files in the repository, such as ensuring a consistent header comment in files, assert tagging, etc.";
 
-    static flags = {
-        fix: Flags.boolean({
-            description: `Fix errors if possible.`,
-            required: false,
-            char: "f",
-        }),
-        handler: Flags.string({
-            description: `Filter handler names by <regex>.`,
-            required: false,
-            char: "d",
-        }),
-        excludeHandler: Flags.string({
-            char: "D",
-            description: `Exclude handler by name. Can be specified multiple times to exclude multiple handlers.`,
-            exclusive: ["handler"],
-            multiple: true,
-        }),
-        path: Flags.string({
-            description: `Filter file paths by <regex>.`,
-            required: false,
-            char: "p",
-        }),
-        exclusions: Flags.file({
-            description: `Path to the exclusions.json file.`,
-            exists: true,
-            required: true,
-            char: "e",
-        }),
-        stdin: Flags.boolean({
-            description: `Read list of files from stdin.`,
-            required: false,
-        }),
-        listHandlers: Flags.boolean({
-            description: `List all policy handlers by name.`,
-            required: false,
-            exclusive: ["stdin", "path", "fix", "handler"],
-        }),
-        ...BaseCommand.flags,
-    };
+	static flags = {
+		fix: Flags.boolean({
+			description: `Fix errors if possible.`,
+			required: false,
+			char: "f",
+		}),
+		handler: Flags.string({
+			description: `Filter handler names by <regex>.`,
+			required: false,
+			char: "d",
+		}),
+		excludeHandler: Flags.string({
+			char: "D",
+			description: `Exclude handler by name. Can be specified multiple times to exclude multiple handlers.`,
+			exclusive: ["handler"],
+			multiple: true,
+		}),
+		path: Flags.string({
+			description: `Filter file paths by <regex>.`,
+			required: false,
+			char: "p",
+		}),
+		exclusions: Flags.file({
+			description: `Path to the exclusions.json file.`,
+			exists: true,
+			required: true,
+			char: "e",
+		}),
+		stdin: Flags.boolean({
+			description: `Read list of files from stdin.`,
+			required: false,
+		}),
+		listHandlers: Flags.boolean({
+			description: `List all policy handlers by name.`,
+			required: false,
+			exclusive: ["stdin", "path", "fix", "handler"],
+		}),
+		...BaseCommand.flags,
+	};
 
-    static handlerActionPerf = new Map<policyAction, Map<string, number>>();
-    static processed = 0;
-    static count = 0;
-    static pathToGitRoot = "";
+	static handlerActionPerf = new Map<policyAction, Map<string, number>>();
+	static processed = 0;
+	static count = 0;
+	static pathToGitRoot = "";
 
-    async run() {
-        let handlersToRun: Handler[] = policyHandlers.filter((h) => {
-            if (
-                this.processedFlags.excludeHandler === undefined ||
-                this.processedFlags.excludeHandler.length === 0
-            ) {
-                return true;
-            }
-            const shouldRun = this.processedFlags.excludeHandler?.includes(h.name) === false;
-            if (!shouldRun) {
-                this.info(`Excluding handler: ${h.name}`);
-            }
-            return shouldRun;
-        });
+	async run() {
+		let handlersToRun: Handler[] = policyHandlers.filter((h) => {
+			if (
+				this.processedFlags.excludeHandler === undefined ||
+				this.processedFlags.excludeHandler.length === 0
+			) {
+				return true;
+			}
+			const shouldRun = this.processedFlags.excludeHandler?.includes(h.name) === false;
+			if (!shouldRun) {
+				this.info(`Excluding handler: ${h.name}`);
+			}
+			return shouldRun;
+		});
 
-        // list the handlers then exit
-        if (this.processedFlags.listHandlers) {
-            for (const h of handlersToRun) {
-                this.log(
-                    `${h.name}\nresolver: ${h.resolver !== undefined} finalHandler: ${
-                        h.final !== undefined
-                    }\n`,
-                );
-            }
-            this.log(`${handlersToRun.length} TOTAL POLICY HANDLERS`);
-            this.exit(0);
-        }
+		// list the handlers then exit
+		if (this.processedFlags.listHandlers) {
+			for (const h of handlersToRun) {
+				this.log(
+					`${h.name}\nresolver: ${h.resolver !== undefined} finalHandler: ${
+						h.final !== undefined
+					}\n`,
+				);
+			}
+			this.log(`${handlersToRun.length} TOTAL POLICY HANDLERS`);
+			this.exit(0);
+		}
 
-        const pathRegex: RegExp =
-            this.processedFlags.path === undefined
-                ? /.?/
-                : new RegExp(this.processedFlags.path, "i");
+		const pathRegex: RegExp =
+			this.processedFlags.path === undefined
+				? /.?/
+				: new RegExp(this.processedFlags.path, "i");
 
-        if (this.processedFlags.handler !== undefined) {
-            const handlerRegex: RegExp = new RegExp(this.processedFlags.handler, "i");
-            this.info(`Filtering handlers by regex: ${handlerRegex}`);
-            handlersToRun = handlersToRun.filter((h) => handlerRegex.test(h.name));
-        }
+		if (this.processedFlags.handler !== undefined) {
+			const handlerRegex: RegExp = new RegExp(this.processedFlags.handler, "i");
+			this.info(`Filtering handlers by regex: ${handlerRegex}`);
+			handlersToRun = handlersToRun.filter((h) => handlerRegex.test(h.name));
+		}
 
-        if (this.processedFlags.path !== undefined) {
-            this.info(`Filtering file paths by regex: ${pathRegex}`);
-        }
+		if (this.processedFlags.path !== undefined) {
+			this.info(`Filtering file paths by regex: ${pathRegex}`);
+		}
 
-        if (this.processedFlags.fix) {
-            this.info("Resolving errors if possible.");
-        }
+		if (this.processedFlags.fix) {
+			this.info("Resolving errors if possible.");
+		}
 
-        if (this.processedFlags.exclusions === undefined) {
-            this.error("No exclusions file provided.");
-        }
+		if (this.processedFlags.exclusions === undefined) {
+			this.error("No exclusions file provided.");
+		}
 
-        let exclusionsFile: string[];
-        try {
-            exclusionsFile = await readJsonAsync(this.processedFlags.exclusions);
-        } catch {
-            this.error("Unable to locate or parse path to exclusions file");
-        }
+		let exclusionsFile: string[];
+		try {
+			exclusionsFile = await readJsonAsync(this.processedFlags.exclusions);
+		} catch {
+			this.error("Unable to locate or parse path to exclusions file");
+		}
 
-        const exclusions: RegExp[] = exclusionsFile.map((e: string) => new RegExp(e, "i"));
+		const exclusions: RegExp[] = exclusionsFile.map((e: string) => new RegExp(e, "i"));
 
-        if (this.processedFlags.stdin) {
-            const pipeString = await readStdin();
+		if (this.processedFlags.stdin) {
+			const pipeString = await readStdin();
 
-            if (pipeString !== undefined) {
-                try {
-                    pipeString
-                        .split("\n")
-                        .map((line: string) =>
-                            this.handleLine(line, pathRegex, exclusions, handlersToRun),
-                        );
-                } finally {
-                    try {
-                        runPolicyCheck(handlersToRun, this.processedFlags.fix);
-                    } finally {
-                        this.logStats();
-                    }
-                }
-            }
+			if (pipeString !== undefined) {
+				try {
+					pipeString
+						.split("\n")
+						.map((line: string) =>
+							this.handleLine(line, pathRegex, exclusions, handlersToRun),
+						);
+				} finally {
+					try {
+						runPolicyCheck(handlersToRun, this.processedFlags.fix);
+					} finally {
+						this.logStats();
+					}
+				}
+			}
 
-            return;
-        }
+			return;
+		}
 
-        CheckPolicy.pathToGitRoot = childProcess
-            .execSync("git rev-parse --show-cdup", { encoding: "utf8" })
-            .trim();
+		CheckPolicy.pathToGitRoot = childProcess
+			.execSync("git rev-parse --show-cdup", { encoding: "utf8" })
+			.trim();
 
-        const p = childProcess.spawn("git", [
-            "ls-files",
-            "-co",
-            "--exclude-standard",
-            "--full-name",
-        ]);
+		const p = childProcess.spawn("git", [
+			"ls-files",
+			"-co",
+			"--exclude-standard",
+			"--full-name",
+		]);
 
-        let scriptOutput = "";
-        p.stdout.on("data", (data) => {
-            scriptOutput = `${scriptOutput}${data.toString()}`;
-        });
-        p.stdout.on("close", () => {
-            try {
-                scriptOutput
-                    .split("\n")
-                    .map((line: string) =>
-                        this.handleLine(line, pathRegex, exclusions, handlersToRun),
-                    );
-            } finally {
-                try {
-                    runPolicyCheck(handlersToRun, this.processedFlags.fix);
-                } finally {
-                    this.logStats();
-                }
-            }
-        });
-    }
+		let scriptOutput = "";
+		p.stdout.on("data", (data) => {
+			scriptOutput = `${scriptOutput}${data.toString()}`;
+		});
+		p.stdout.on("close", () => {
+			try {
+				scriptOutput
+					.split("\n")
+					.map((line: string) =>
+						this.handleLine(line, pathRegex, exclusions, handlersToRun),
+					);
+			} finally {
+				try {
+					runPolicyCheck(handlersToRun, this.processedFlags.fix);
+				} finally {
+					this.logStats();
+				}
+			}
+		});
+	}
 
-    // route files to their handlers by regex testing their full paths
-    // synchronize output, exit code, and resolve decision for all handlers
-    routeToHandlers(file: string, handlers: Handler[]): void {
-        handlers
-            .filter((handler) => handler.match.test(file))
-            // eslint-disable-next-line unicorn/no-array-for-each
-            .forEach((handler) => {
-                const result = runWithPerf(handler.name, "handle", () =>
-                    handler.handler(file, CheckPolicy.pathToGitRoot),
-                );
-                if (result !== undefined && result !== "") {
-                    let output = `${newline}file failed policy check: ${file}${newline}${result}`;
-                    const resolver = handler.resolver;
-                    if (this.processedFlags.fix && resolver) {
-                        output += `${newline}attempting to resolve: ${file}`;
-                        const resolveResult = runWithPerf(handler.name, "resolve", () =>
-                            resolver(file, CheckPolicy.pathToGitRoot),
-                        );
+	// route files to their handlers by regex testing their full paths
+	// synchronize output, exit code, and resolve decision for all handlers
+	routeToHandlers(file: string, handlers: Handler[]): void {
+		handlers
+			.filter((handler) => handler.match.test(file))
+			// eslint-disable-next-line unicorn/no-array-for-each
+			.forEach((handler) => {
+				const result = runWithPerf(handler.name, "handle", () =>
+					handler.handler(file, CheckPolicy.pathToGitRoot),
+				);
+				if (result !== undefined && result !== "") {
+					let output = `${newline}file failed policy check: ${file}${newline}${result}`;
+					const resolver = handler.resolver;
+					if (this.processedFlags.fix && resolver) {
+						output += `${newline}attempting to resolve: ${file}`;
+						const resolveResult = runWithPerf(handler.name, "resolve", () =>
+							resolver(file, CheckPolicy.pathToGitRoot),
+						);
 
-                        if (resolveResult?.message !== undefined) {
-                            output += newline + resolveResult.message;
-                        }
+						if (resolveResult?.message !== undefined) {
+							output += newline + resolveResult.message;
+						}
 
-                        if (!resolveResult.resolved) {
-                            process.exitCode = 1;
-                        }
-                    } else {
-                        process.exitCode = 1;
-                    }
+						if (!resolveResult.resolved) {
+							process.exitCode = 1;
+						}
+					} else {
+						process.exitCode = 1;
+					}
 
-                    if (process.exitCode === 1) {
-                        this.warning(output);
-                    } else {
-                        this.info(output);
-                    }
-                }
-            });
-    }
+					if (process.exitCode === 1) {
+						this.warning(output);
+					} else {
+						this.info(output);
+					}
+				}
+			});
+	}
 
-    logStats() {
-        this.log(
-            `Statistics: ${CheckPolicy.processed} processed, ${
-                CheckPolicy.count - CheckPolicy.processed
-            } excluded, ${CheckPolicy.count} total`,
-        );
-        for (const [action, handlerPerf] of CheckPolicy.handlerActionPerf.entries()) {
-            this.log(`Performance for "${action}":`);
-            for (const [handler, dur] of handlerPerf.entries()) {
-                this.log(`\t${handler}: ${dur / 1000}:`);
-            }
-        }
-    }
+	logStats() {
+		this.log(
+			`Statistics: ${CheckPolicy.processed} processed, ${
+				CheckPolicy.count - CheckPolicy.processed
+			} excluded, ${CheckPolicy.count} total`,
+		);
+		for (const [action, handlerPerf] of CheckPolicy.handlerActionPerf.entries()) {
+			this.log(`Performance for "${action}":`);
+			for (const [handler, dur] of handlerPerf.entries()) {
+				this.log(`\t${handler}: ${dur / 1000}:`);
+			}
+		}
+	}
 
-    handleLine(line: string, pathRegex: RegExp, exclusions: RegExp[], handlers: Handler[]) {
-        const filePath = path.join(CheckPolicy.pathToGitRoot, line).trim().replace(/\\/g, "/");
+	handleLine(line: string, pathRegex: RegExp, exclusions: RegExp[], handlers: Handler[]) {
+		const filePath = path.join(CheckPolicy.pathToGitRoot, line).trim().replace(/\\/g, "/");
 
-        if (!pathRegex.test(line) || !fs.existsSync(filePath)) {
-            return;
-        }
+		if (!pathRegex.test(line) || !fs.existsSync(filePath)) {
+			return;
+		}
 
-        CheckPolicy.count++;
-        if (!exclusions.every((value) => !value.test(line))) {
-            this.verbose(`Excluded: ${line}`);
-            return;
-        }
+		CheckPolicy.count++;
+		if (!exclusions.every((value) => !value.test(line))) {
+			this.verbose(`Excluded: ${line}`);
+			return;
+		}
 
-        try {
-            this.routeToHandlers(filePath, handlers);
-        } catch (error: any) {
-            throw new Error(`Line error: ${error}`);
-        }
+		try {
+			this.routeToHandlers(filePath, handlers);
+		} catch (error: any) {
+			throw new Error(`Line error: ${error}`);
+		}
 
-        CheckPolicy.processed++;
-    }
+		CheckPolicy.processed++;
+	}
 }
 
 function runWithPerf<T>(name: string, action: policyAction, run: () => T): T {
-    const actionMap = CheckPolicy.handlerActionPerf.get(action) ?? new Map<string, number>();
-    let dur = actionMap.get(name) ?? 0;
+	const actionMap = CheckPolicy.handlerActionPerf.get(action) ?? new Map<string, number>();
+	let dur = actionMap.get(name) ?? 0;
 
-    const start = Date.now();
-    const result = run();
-    dur += Date.now() - start;
+	const start = Date.now();
+	const result = run();
+	dur += Date.now() - start;
 
-    actionMap.set(name, dur);
-    CheckPolicy.handlerActionPerf.set(action, actionMap);
-    return result;
+	actionMap.set(name, dur);
+	CheckPolicy.handlerActionPerf.set(action, actionMap);
+	return result;
 }
 
 function runPolicyCheck(handlers: Handler[], fix: boolean) {
-    for (const h of handlers) {
-        const final = h.final;
-        if (final) {
-            const result = runWithPerf(h.name, "final", () =>
-                final(CheckPolicy.pathToGitRoot, fix),
-            );
-            // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-            if (result?.error) {
-                throw new Error(result.error);
-            }
-        }
-    }
+	for (const h of handlers) {
+		const final = h.final;
+		if (final) {
+			const result = runWithPerf(h.name, "final", () =>
+				final(CheckPolicy.pathToGitRoot, fix),
+			);
+			// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+			if (result?.error) {
+				throw new Error(result.error);
+			}
+		}
+	}
 }
--- build-tools/packages/build-cli/src/commands/release/history.ts
+++ build-tools/packages/build-cli/src/commands/release/history.ts
@@ -27,126 +27,126 @@ const DEFAULT_MIN_VERSION = "0.0.0";
  * The number of results can be limited using the --limit argument.
  */
 export default class ReleaseHistoryCommand<
-    T extends typeof ReleaseHistoryCommand.flags,
+	T extends typeof ReleaseHistoryCommand.flags,
 > extends ReleaseReportBaseCommand<T> {
-    static description = `Prints a list of released versions of a package or release group. Releases are gathered from the git tags in repo containing the working directory.
+	static description = `Prints a list of released versions of a package or release group. Releases are gathered from the git tags in repo containing the working directory.
 
     Use 'npm view' to list published packages based on the public npm registry.
 
     The number of results can be limited using the --limit argument.`;
 
-    static examples = [
-        {
-            description: "List all the releases of the azure release group.",
-            command: "<%= config.bin %> <%= command.id %> -g azure",
-        },
-        {
-            description: "List the 10 most recent client releases.",
-            command: "<%= config.bin %> <%= command.id %> -g client --limit 10",
-        },
-    ];
+	static examples = [
+		{
+			description: "List all the releases of the azure release group.",
+			command: "<%= config.bin %> <%= command.id %> -g azure",
+		},
+		{
+			description: "List the 10 most recent client releases.",
+			command: "<%= config.bin %> <%= command.id %> -g client --limit 10",
+		},
+	];
 
-    static flags = {
-        releaseGroup: releaseGroupFlag({
-            required: false,
-            exclusive: ["package"],
-        }),
-        package: packageSelectorFlag({
-            required: false,
-            exclusive: ["releaseGroup"],
-        }),
-        limit: Flags.integer({
-            char: "l",
-            description: `Limits the number of displayed releases for each release group. Results are sorted by semver, so '--limit 10' will return the 10 highest semver releases for the release group.`,
-        }),
-        ...ReleaseReportBaseCommand.flags,
-    };
+	static flags = {
+		releaseGroup: releaseGroupFlag({
+			required: false,
+			exclusive: ["package"],
+		}),
+		package: packageSelectorFlag({
+			required: false,
+			exclusive: ["releaseGroup"],
+		}),
+		limit: Flags.integer({
+			char: "l",
+			description: `Limits the number of displayed releases for each release group. Results are sorted by semver, so '--limit 10' will return the 10 highest semver releases for the release group.`,
+		}),
+		...ReleaseReportBaseCommand.flags,
+	};
 
-    static enableJsonFlag = true;
+	static enableJsonFlag = true;
 
-    defaultMode: ReleaseSelectionMode = "date";
-    releaseGroupOrPackage: ReleaseGroup | ReleasePackage | undefined;
+	defaultMode: ReleaseSelectionMode = "date";
+	releaseGroupOrPackage: ReleaseGroup | ReleasePackage | undefined;
 
-    public async run(): Promise<{ reports: ReleaseReport[] }> {
-        this.releaseGroupOrPackage =
-            this.processedFlags.releaseGroup ?? this.processedFlags.package;
+	public async run(): Promise<{ reports: ReleaseReport[] }> {
+		this.releaseGroupOrPackage =
+			this.processedFlags.releaseGroup ?? this.processedFlags.package;
 
-        const context = await this.getContext();
-        this.releaseData = await this.collectReleaseData(
-            context,
-            this.defaultMode,
-            this.releaseGroupOrPackage,
-            false,
-        );
-        if (this.releaseData === undefined) {
-            this.error(`No releases found for ${this.releaseGroupOrPackage}`);
-        }
+		const context = await this.getContext();
+		this.releaseData = await this.collectReleaseData(
+			context,
+			this.defaultMode,
+			this.releaseGroupOrPackage,
+			false,
+		);
+		if (this.releaseData === undefined) {
+			this.error(`No releases found for ${this.releaseGroupOrPackage}`);
+		}
 
-        const reports: ReleaseReport[] = [];
+		const reports: ReleaseReport[] = [];
 
-        for (const [pkgOrReleaseGroup, data] of Object.entries(this.releaseData)) {
-            const versions = sortVersions([...data.versions], "version");
-            const releaseTable = this.generateAllReleasesTable(pkgOrReleaseGroup, versions);
+		for (const [pkgOrReleaseGroup, data] of Object.entries(this.releaseData)) {
+			const versions = sortVersions([...data.versions], "version");
+			const releaseTable = this.generateAllReleasesTable(pkgOrReleaseGroup, versions);
 
-            this.log(
-                table(releaseTable, {
-                    singleLine: true,
-                }),
-            );
-        }
+			this.log(
+				table(releaseTable, {
+					singleLine: true,
+				}),
+			);
+		}
 
-        // When the --json flag is passed, the command will return the raw data as JSON.
-        return { reports };
-    }
+		// When the --json flag is passed, the command will return the raw data as JSON.
+		return { reports };
+	}
 
-    /**
-     * Generates table data for all versions of a package/release group.
-     */
-    private generateAllReleasesTable(
-        pkgOrReleaseGroup: ReleasePackage | ReleaseGroup,
-        versions: VersionDetails[],
-    ): string[][] {
-        const tableData: string[][] = [];
-        const releases = sortVersions(versions, "version").reverse();
+	/**
+	 * Generates table data for all versions of a package/release group.
+	 */
+	private generateAllReleasesTable(
+		pkgOrReleaseGroup: ReleasePackage | ReleaseGroup,
+		versions: VersionDetails[],
+	): string[][] {
+		const tableData: string[][] = [];
+		const releases = sortVersions(versions, "version").reverse();
 
-        let index = 0;
-        for (const ver of releases) {
-            const displayPreviousVersion =
-                index >= 1 ? releases[index - 1].version : DEFAULT_MIN_VERSION;
+		let index = 0;
+		for (const ver of releases) {
+			const displayPreviousVersion =
+				index >= 1 ? releases[index - 1].version : DEFAULT_MIN_VERSION;
 
-            const displayDate = getDisplayDate(ver.date);
-            const highlight = this.isRecentReleaseByDate(ver.date) ? chalk.green : chalk.white;
-            const displayRelDate = highlight(getDisplayDateRelative(ver.date));
+			const displayDate = getDisplayDate(ver.date);
+			const highlight = this.isRecentReleaseByDate(ver.date) ? chalk.green : chalk.white;
+			const displayRelDate = highlight(getDisplayDateRelative(ver.date));
 
-            const bumpType = detectBumpType(displayPreviousVersion, ver.version);
-            const displayBumpType = highlight(`${bumpType}`);
+			const bumpType = detectBumpType(displayPreviousVersion, ver.version);
+			const displayBumpType = highlight(`${bumpType}`);
 
-            const displayVersionSection = chalk.grey(
-                `${highlight(ver.version)} <-- ${displayPreviousVersion}`,
-            );
+			const displayVersionSection = chalk.grey(
+				`${highlight(ver.version)} <-- ${displayPreviousVersion}`,
+			);
 
-            tableData.push([
-                pkgOrReleaseGroup,
-                displayBumpType,
-                displayRelDate,
-                displayDate,
-                displayVersionSection,
-            ]);
+			tableData.push([
+				pkgOrReleaseGroup,
+				displayBumpType,
+				displayRelDate,
+				displayDate,
+				displayVersionSection,
+			]);
 
-            index++;
-        }
+			index++;
+		}
 
-        const limit = this.processedFlags.limit;
-        if (limit !== undefined && tableData.length > limit) {
-            this.info(
-                `Reached the release limit (${limit}), ignoring the remaining ${
-                    tableData.length - limit
-                } releases.`,
-            );
-            // The most recent releases are last, so slice from the end.
-            return tableData.slice(-limit);
-        }
+		const limit = this.processedFlags.limit;
+		if (limit !== undefined && tableData.length > limit) {
+			this.info(
+				`Reached the release limit (${limit}), ignoring the remaining ${
+					tableData.length - limit
+				} releases.`,
+			);
+			// The most recent releases are last, so slice from the end.
+			return tableData.slice(-limit);
+		}
 
-        return tableData;
-    }
+		return tableData;
+	}
 }
