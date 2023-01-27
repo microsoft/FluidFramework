db8843330a8e7100aa35a877835b5f45312c5c71
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-01-27 01:27:33 +0000

    style(schemas): Prettier

--- experimental/PropertyDDS/examples/schemas/src/squares_demo/2DPoint-1.0.0.ts
+++ experimental/PropertyDDS/examples/schemas/src/squares_demo/2DPoint-1.0.0.ts
@@ -3,9 +3,9 @@
  * Licensed under the MIT License.
  */
 export default {
-    typeid: "autofluid:2DPoint-1.0.0",
-    properties: [
-        { id: "x", typeid: "Float32" },
-        { id: "y", typeid: "Float32" },
-    ],
+	typeid: "autofluid:2DPoint-1.0.0",
+	properties: [
+		{ id: "x", typeid: "Float32" },
+		{ id: "y", typeid: "Float32" },
+	],
 };
