8f5fa8a80a322e448e491f4105b1f5c4e181caf2
Tyler Butler <tyler@tylerbutler.com>
2023-01-27 13:25:38 -0800

    npm run format

--- build-tools/packages/build-cli/src/lib/dates.ts
+++ build-tools/packages/build-cli/src/lib/dates.ts
@@ -8,12 +8,12 @@ import { formatDistanceToNow, formatISO9075 } from "date-fns";
  * Formats a date for display in the terminal.
  */
 export function getDisplayDate(date?: Date): string {
-    return date === undefined ? "--no date--" : formatISO9075(date, { representation: "date" });
+	return date === undefined ? "--no date--" : formatISO9075(date, { representation: "date" });
 }
 
 /**
  * Formats a date relative to the current time for display in the terminal.
  */
 export function getDisplayDateRelative(date?: Date): string {
-    return date === undefined ? "" : `${formatDistanceToNow(date)} ago`;
+	return date === undefined ? "" : `${formatDistanceToNow(date)} ago`;
 }
