6fb259b55b61f6a3b832110b3ace300349898f49
Abram Sanderson <abram.sanderson@gmail.com>
2023-01-30 09:42:11 -0800

    Fix bad merge

--- packages/dds/merge-tree/src/test/attributionCollection.spec.ts
+++ packages/dds/merge-tree/src/test/attributionCollection.spec.ts
@@ -12,8 +12,8 @@ import {
 	performFuzzActions,
 	take,
 } from "@fluid-internal/stochastic-test-utils";
+import { AttributionKey } from "@fluidframework/runtime-definitions";
 import { AttributionCollection, SerializedAttributionCollection } from "../attributionCollection";
-import { AttributionKey, BaseSegment, ISegment } from "../mergeTreeNodes";
 
 describe("AttributionCollection", () => {
 	describe(".getAtOffset", () => {
