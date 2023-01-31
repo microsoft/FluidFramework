d1534102df6e890e3db0e705c9449cdfe1f66e52
Jenn <jenn.thakugan@icloud.com>
2023-01-31 18:48:19 +0000

    Add skipped chunkTree test

--- packages/dds/tree/src/test/feature-libraries/chunked-forest/basicChunk.spec.ts
+++ packages/dds/tree/src/test/feature-libraries/chunked-forest/basicChunk.spec.ts
@@ -3,19 +3,37 @@
  * Licensed under the MIT License.
  */
 
+import { strict as assert } from "assert";
 import { testGeneralPurposeTreeCursor } from "../../cursorTestSuite";
-import { ITreeCursor } from "../../../core";
+import { ITreeCursor, JsonableTree } from "../../../core";
 import { jsonableTreeFromCursor, singleTextCursor, chunkTree } from "../../../feature-libraries";
+import { brand } from "../../../util";
 
-testGeneralPurposeTreeCursor(
-    "basic chunk cursor",
-    (data): ITreeCursor => {
+describe("basic chunk", () => {
+    it.skip("calling chunkTree on existing chunk adds a reference", () => {
+        const data: JsonableTree = { type: brand("Foo"), value: "test" };
         const inputCursor = singleTextCursor(data);
         const chunk = chunkTree(inputCursor);
-        const cursor: ITreeCursor = chunk.cursor();
-        cursor.enterNode(0);
-        return cursor;
-    },
-    jsonableTreeFromCursor,
-    true,
-);
+        assert(!chunk.isShared(), "newly created chunk should not have more than one reference");
+
+        const chunkCursor = chunk.cursor();
+        const newChunk = chunkTree(chunkCursor);
+        assert(
+            newChunk.isShared() && chunk.isShared(),
+            "chunk created off of existing chunk should be shared",
+        );
+    });
+
+    testGeneralPurposeTreeCursor(
+        "",
+        (data): ITreeCursor => {
+            const inputCursor = singleTextCursor(data);
+            const chunk = chunkTree(inputCursor);
+            const cursor: ITreeCursor = chunk.cursor();
+            cursor.enterNode(0);
+            return cursor;
+        },
+        jsonableTreeFromCursor,
+        true,
+    );
+});
