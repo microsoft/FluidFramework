e4ae7c160e0aa43a7d6d6f1b73f7b4436bebd670
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-01-31 23:28:58 +0000

    style: Prettier

--- examples/hosts/app-integration/external-data/src/utilities/webhook.ts
+++ examples/hosts/app-integration/external-data/src/utilities/webhook.ts
@@ -93,7 +93,7 @@ export class MockWebhook<TData = unknown> implements IDisposable {
 			`WEBHOOK: External data has been updated. Notifying ${this._subscribers.size} subscribers...`,
 		);
 
-        const messageBody = JSON.stringify({ data });
+		const messageBody = JSON.stringify({ data });
 		for (const subscriberUrl of this._subscribers) {
 			fetch(subscriberUrl, {
 				method: "POST",
@@ -101,7 +101,7 @@ export class MockWebhook<TData = unknown> implements IDisposable {
 					"Access-Control-Allow-Origin": "*",
 					"Content-Type": "application/json",
 				},
-				body: messageBody
+				body: messageBody,
 			}).catch((error) => {
 				console.error("WEBHOOK: Encountered an error while notifying subscribers:", error);
 			});
