b6bb106316d9d249ff1cccd6e0ff323f52ad032f
chentong7 <chentong@microsoft.com>
2023-01-30 18:10:48 -0800

    better way to fix webpack

--- packages/tools/client-debugger/client-debugger-view/webpack.config.js
+++ packages/tools/client-debugger/client-debugger-view/webpack.config.js
@@ -22,7 +22,6 @@ module.exports = (env) => {
 			},
 			resolve: {
 				extensions: [".ts", ".tsx", ".js"],
-				fallback: { "process/browser": require.resolve("process/browser") },
 			},
 			module: {
 				rules: [
@@ -48,7 +47,7 @@ module.exports = (env) => {
 			},
 			plugins: [
 				new webpack.ProvidePlugin({
-					process: "process/browser",
+					process: require.resolve("process/browser"),
 				}),
 			],
 			// This impacts which files are watched by the dev server (and likely by webpack if watch is true).
