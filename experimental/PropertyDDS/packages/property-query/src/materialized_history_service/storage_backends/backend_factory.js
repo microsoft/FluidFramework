4afd5f37800d50237f66a3263c8d90dad47e8fba
Tyler Butler <tyler@tylerbutler.com>
2023-02-02 10:31:51 -0800

    format

--- experimental/PropertyDDS/packages/property-query/src/materialized_history_service/storage_backends/backend_factory.js
+++ experimental/PropertyDDS/packages/property-query/src/materialized_history_service/storage_backends/backend_factory.js
@@ -3,8 +3,7 @@
  * Licensed under the MIT License.
  */
 const InMemoryBackend = require("./in_memory");
-const OperationError =
-    require("@fluid-experimental/property-common").OperationError;
+const OperationError = require("@fluid-experimental/property-common").OperationError;
 const HTTPStatus = require("http-status");
 
 /**
@@ -12,33 +11,32 @@ const HTTPStatus = require("http-status");
  * on the configuration for the service
  */
 class BackendFactory {
-    /**
-     * Constructor to the backend factory
-     * @param {Settings} in_params.settings - Service settings
-     */
-    constructor({ settings }) {
-        this._settings = settings;
-    }
+	/**
+	 * Constructor to the backend factory
+	 * @param {Settings} in_params.settings - Service settings
+	 */
+	constructor({ settings }) {
+		this._settings = settings;
+	}
 
-    /**
-     * Returns the storage backend based on the service configuration
-     * @return {StorageBackend} = The Storage Backend
-     */
-    getBackend() {
-        switch (this._settings.get("mh:storageBackend")) {
-            case "InMemory":
-                return new InMemoryBackend({
-                    settings: this._settings.get("mh:inMemoryBackend"),
-                });
-            default:
-                throw new OperationError(
-                    "Unknown backend: " +
-                        this._settings.get("mh:storageBackend"),
-                    "Initialization",
-                    HTTPStatus.INTERNAL_SERVER_ERROR
-                );
-        }
-    }
+	/**
+	 * Returns the storage backend based on the service configuration
+	 * @return {StorageBackend} = The Storage Backend
+	 */
+	getBackend() {
+		switch (this._settings.get("mh:storageBackend")) {
+			case "InMemory":
+				return new InMemoryBackend({
+					settings: this._settings.get("mh:inMemoryBackend"),
+				});
+			default:
+				throw new OperationError(
+					"Unknown backend: " + this._settings.get("mh:storageBackend"),
+					"Initialization",
+					HTTPStatus.INTERNAL_SERVER_ERROR,
+				);
+		}
+	}
 }
 
 module.exports = BackendFactory;
