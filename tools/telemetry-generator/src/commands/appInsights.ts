6b7449e7d789d7a0bc4c0624accb5eb341369af0
seanimam <105244057+seanimam@users.noreply.github.com>
2023-02-02 18:46:06 +0000

    awaits handler execution in command file now to prevent early termination

--- tools/telemetry-generator/src/commands/appInsights.ts
+++ tools/telemetry-generator/src/commands/appInsights.ts
@@ -93,14 +93,16 @@ export class EntryPoint extends Command {
 			}
 		}
 
-		filesToProcess.forEach((fullPath) => {
-			try {
-				console.log(`Processing file '${fullPath}'`);
-				const data = JSON.parse(fs.readFileSync(fullPath, "utf8"));
-				handler(data, telemetryClient, rateLimiter);
-			} catch (err: any) {
-				console.error(`Unexpected error processing file '${fullPath}'.\n${err.stack}`);
-			}
+		filesToProcess.forEach( (fullPath) => {
+            void (async () => {
+            	try {
+				    console.log(`Processing file '${fullPath}'`);
+				    const data = JSON.parse(fs.readFileSync(fullPath, "utf8"));
+				    await handler(data, telemetryClient, rateLimiter);
+			    } catch (err: any) {
+				    console.error(`Unexpected error processing file '${fullPath}'.\n${err.stack}`);
+			    }
+              })();
 		});
 
 		telemetryClient.flush();
