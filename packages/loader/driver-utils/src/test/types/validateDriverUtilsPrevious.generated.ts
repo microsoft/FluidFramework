b340604d3d0ea89cecadfbdd2dbd64f0d17f2dc4
Tyler Butler <tylerbu@microsoft.com>
2023-02-03 14:36:22 -0800

    build(client): Prepare and generate typetests (#14002)
    Cleans up type tests on the 2.3 release branch.

--- packages/loader/driver-utils/src/test/types/validateDriverUtilsPrevious.generated.ts
+++ packages/loader/driver-utils/src/test/types/validateDriverUtilsPrevious.generated.ts
@@ -23,6 +23,7 @@ declare function get_old_ClassDeclaration_AuthorizationError():
 declare function use_current_ClassDeclaration_AuthorizationError(
     use: TypeOnly<current.AuthorizationError>);
 use_current_ClassDeclaration_AuthorizationError(
+    // @ts-expect-error compatibility expected to be broken
     get_old_ClassDeclaration_AuthorizationError());
 
 /*
@@ -311,6 +312,7 @@ declare function get_old_ClassDeclaration_DeltaStreamConnectionForbiddenError():
 declare function use_current_ClassDeclaration_DeltaStreamConnectionForbiddenError(
     use: TypeOnly<current.DeltaStreamConnectionForbiddenError>);
 use_current_ClassDeclaration_DeltaStreamConnectionForbiddenError(
+    // @ts-expect-error compatibility expected to be broken
     get_old_ClassDeclaration_DeltaStreamConnectionForbiddenError());
 
 /*
@@ -455,6 +457,7 @@ declare function get_old_ClassDeclaration_FluidInvalidSchemaError():
 declare function use_current_ClassDeclaration_FluidInvalidSchemaError(
     use: TypeOnly<current.FluidInvalidSchemaError>);
 use_current_ClassDeclaration_FluidInvalidSchemaError(
+    // @ts-expect-error compatibility expected to be broken
     get_old_ClassDeclaration_FluidInvalidSchemaError());
 
 /*
@@ -479,6 +482,7 @@ declare function get_old_ClassDeclaration_GenericNetworkError():
 declare function use_current_ClassDeclaration_GenericNetworkError(
     use: TypeOnly<current.GenericNetworkError>);
 use_current_ClassDeclaration_GenericNetworkError(
+    // @ts-expect-error compatibility expected to be broken
     get_old_ClassDeclaration_GenericNetworkError());
 
 /*
@@ -791,6 +795,7 @@ declare function get_old_ClassDeclaration_LocationRedirectionError():
 declare function use_current_ClassDeclaration_LocationRedirectionError(
     use: TypeOnly<current.LocationRedirectionError>);
 use_current_ClassDeclaration_LocationRedirectionError(
+    // @ts-expect-error compatibility expected to be broken
     get_old_ClassDeclaration_LocationRedirectionError());
 
 /*
@@ -1295,6 +1300,7 @@ declare function get_old_ClassDeclaration_ThrottlingError():
 declare function use_current_ClassDeclaration_ThrottlingError(
     use: TypeOnly<current.ThrottlingError>);
 use_current_ClassDeclaration_ThrottlingError(
+    // @ts-expect-error compatibility expected to be broken
     get_old_ClassDeclaration_ThrottlingError());
 
 /*
@@ -1319,6 +1325,7 @@ declare function get_old_ClassDeclaration_UsageError():
 declare function use_current_ClassDeclaration_UsageError(
     use: TypeOnly<current.UsageError>);
 use_current_ClassDeclaration_UsageError(
+    // @ts-expect-error compatibility expected to be broken
     get_old_ClassDeclaration_UsageError());
 
 /*
