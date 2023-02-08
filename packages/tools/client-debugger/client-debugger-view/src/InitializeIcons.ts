4a9b54501604dbee1df6918b33d22a62034aad80
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-02-08 14:41:52 -0800

    style: Line wrap

--- packages/tools/client-debugger/client-debugger-view/src/InitializeIcons.ts
+++ packages/tools/client-debugger/client-debugger-view/src/InitializeIcons.ts
@@ -11,7 +11,8 @@ let iconsInitialized = false;
  * Initialize Fluent icons used this library's components, if they have not already been initialized.
  *
  * @remarks Wraps FluentUI's icon initialization, ensuring it is only called once.
- * FluentUI reports errors if this is called more than once, but as this library exports multiple entry-points, it is impossible to say which module needs to call this.
+ * FluentUI reports errors if this is called more than once, but as this library exports multiple entry-points,
+ * it is impossible to say which module needs to call this.
  */
 export function initializeFluentUiIcons(): void {
 	if (!iconsInitialized) {
