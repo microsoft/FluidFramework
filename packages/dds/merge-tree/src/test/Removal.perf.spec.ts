8e3d327680e17ad5d8bd669aa82c8824280c2fa9
Connor Skees <connor1skees@gmail.com>
2023-02-09 10:34:32 -0500

    add more benchmarks

---
+++ packages/dds/merge-tree/src/test/Removal.perf.spec.ts
@@ -0,0 +1,145 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { benchmark, BenchmarkType } from "@fluid-tools/benchmark";
+import { MergeTreeDeltaType } from "../ops";
+import { markRangeRemoved } from "./testUtils";
+import { loadSnapshot, TestString } from "./snapshot.utils";
+
+describe("removal perf", () => {
+    let summary;
+
+	benchmark({
+		type: BenchmarkType.Measurement,
+		// baseline summary benchmark to compare to other remove tests. such a
+        // comparison should give a (rough) sense of overhead caused by summary
+        // loading
+        title: "baseline summary load",
+        category: "remove",
+        before: () => {
+			const str = new TestString("id", {});
+			for (let i = 0; i < 1000; i++) {
+				str.append("a", false);
+			}
+
+			summary = str.getSummary();
+		},
+		benchmarkFn: async () => {
+            await loadSnapshot(summary);
+		},
+	});
+
+    benchmark({
+		type: BenchmarkType.Measurement,
+		title: "remove large range of large tree",
+        category: "remove",
+        before: () => {
+			const str = new TestString("id", {});
+			for (let i = 0; i < 1000; i++) {
+				str.append("a", false);
+			}
+
+			summary = str.getSummary();
+		},
+		benchmarkFn: async () => {
+            const str = await loadSnapshot(summary);
+
+            markRangeRemoved({
+				mergeTree: str.mergeTree,
+				start: 0,
+				end: 1000,
+				refSeq: 1000,
+				clientId: 0,
+				seq: 1001,
+				opArgs: { op: { type: MergeTreeDeltaType.REMOVE } },
+				overwrite: false,
+			});
+		},
+	});
+
+	benchmark({
+		type: BenchmarkType.Measurement,
+		title: "remove start of large tree",
+        category: "remove",
+        before: () => {
+			const str = new TestString("id", {});
+			for (let i = 0; i < 1000; i++) {
+				str.append("a", false);
+			}
+
+			summary = str.getSummary();
+		},
+		benchmarkFn: async () => {
+            const str = await loadSnapshot(summary);
+
+			markRangeRemoved({
+				mergeTree: str.mergeTree,
+				start: 0,
+				end: 1,
+				refSeq: 1000,
+				clientId: 0,
+				seq: 1001,
+				opArgs: { op: { type: MergeTreeDeltaType.REMOVE } },
+				overwrite: false,
+			});
+		},
+	});
+
+	benchmark({
+		type: BenchmarkType.Measurement,
+		title: "remove middle of large tree",
+        category: "remove",
+        before: () => {
+			const str = new TestString("id", {});
+			for (let i = 0; i < 1000; i++) {
+				str.append("a", false);
+			}
+
+			summary = str.getSummary();
+		},
+		benchmarkFn: async () => {
+            const str = await loadSnapshot(summary);
+
+			markRangeRemoved({
+				mergeTree: str.mergeTree,
+				start: 499,
+				end: 501,
+				refSeq: 1000,
+				clientId: 0,
+				seq: 1001,
+				opArgs: { op: { type: MergeTreeDeltaType.REMOVE } },
+				overwrite: false,
+			});
+		},
+	});
+
+	benchmark({
+		type: BenchmarkType.Measurement,
+		title: "remove end of large tree",
+        category: "remove",
+        before: () => {
+			const str = new TestString("id", {});
+			for (let i = 0; i < 1000; i++) {
+				str.append("a", false);
+			}
+
+			summary = str.getSummary();
+		},
+		benchmarkFn: async () => {
+            const str = await loadSnapshot(summary);
+
+			markRangeRemoved({
+				mergeTree: str.mergeTree,
+				start: 999,
+				end: 1000,
+				refSeq: 1000,
+				clientId: 0,
+				seq: 1001,
+				opArgs: { op: { type: MergeTreeDeltaType.REMOVE } },
+				overwrite: false,
+			});
+		},
+	});
+});
---
+++ packages/dds/merge-tree/src/test/Snapshot.perf.spec.ts
@@ -0,0 +1,32 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { benchmark, BenchmarkType } from "@fluid-tools/benchmark";
+import { loadSnapshot, TestString } from "./snapshot.utils";
+
+describe("snapshot perf", () => {
+	let summary;
+
+    for (const summarySize of [10, 100, 1000, 5000]) {
+        benchmark({
+            type: BenchmarkType.Measurement,
+            title: `load snapshot with ${summarySize} segments`,
+            category: "snapshot loading",
+            before: () => {
+                const str = new TestString("id", {});
+                for (let i = 0; i < summarySize; i++) {
+                    str.append("a", false);
+                }
+                summary = str.getSummary();
+            },
+            benchmarkFn: async () => {
+                await loadSnapshot(summary);
+            },
+            after: () => {
+                summary = undefined;
+            },
+        });
+    }
+});
