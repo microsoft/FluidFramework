03e1f1b72ad05047c2833d1e8c2652d9e47a0bd1
sharmakhushboo <sharmak@microsoft.com>
2023-02-09 00:29:21 -0800

    Templogs (#14089)
    [How contribute to this
    repo](https://github.com/microsoft/FluidFramework/blob/main/CONTRIBUTING.md).
    
    [Guidelines for Pull
    Requests](https://github.com/microsoft/FluidFramework/wiki/PR-Guidelines#guidelines).
    
    The sections included below are suggestions for what you may want to
    include.
    Feel free to remove or alter parts of this template that do not offer
    value for your specific change.
    
    ## Description
    
    > A concise description of the changes (bug or feature) and their
    impact/motivation.
    > If this description is short enough to be used as the title, delete
    this section and just use the title.
    
    > For bug fixes, also include specifics of how to reproduce it / confirm
    it is fixed.
    
    > If this Pull Request should close/resolve any issues when merged, use
    [the special syntax for
    that](https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword)
    here.
    
    ## Breaking Changes
    
    > If this introduces a breaking change, please describe the impact and
    migration path for existing applications below.
    > See
    [Breaking-vs-Non-breaking-Changes](https://github.com/microsoft/FluidFramework/wiki/Breaking-vs-Non-breaking-Changes)
    for details.
    > If there are no breaking changes, delete this section.
    
    ## Reviewer Guidance
    
    The review process is outlined on [this wiki
    page](https://github.com/microsoft/FluidFramework/wiki/PR-Guidelines#guidelines).
    
    > List any specific things you want to get reviewer opinions on, and
    anything a reviewer would need to know to review this PR effectively.
    > Things you might want to include:
    >
    > - Questions about how to properly make automated tests for your
    changes.
    > -   Questions about design choices you made.
    > - Descriptions of how to manually test the changes (and how much of
    that you have done).
    > -   etc.
    >
    > If you have any questions in this section, consider making the PR a
    draft until all questions have been resolved.

--- server/routerlicious/packages/lambdas/src/alfred/index.ts
+++ server/routerlicious/packages/lambdas/src/alfred/index.ts
@@ -335,7 +335,10 @@ export function configureWebSocketServices(
             // increment connection count after the client is added to the room.
             // excluding summarizer for total client count.
             if (!isSummarizer) {
-                connectionCountLogger.incrementConnectionCount();
+                connectionCountLogger.incrementConnectionCount()
+                .catch((error) => {
+                    Lumberjack.error("Error in connectionCountLogger on Connect.", undefined, error);
+                });
             }
 
             // Iterate over the version ranges provided by the client and select the best one that works
@@ -675,6 +678,10 @@ export function configureWebSocketServices(
 
         // eslint-disable-next-line @typescript-eslint/no-misused-promises
         socket.on("disconnect", async () => {
+            Lumberjack.info(
+                `Temp-log at disconnect. connectiontimemap = ${JSON.stringify(connectionTimeMap.values())}`,
+                undefined,
+            );
             clearExpirationTimer();
             const removeAndStoreP: Promise<void>[] = [];
             // Send notification messages for all client IDs in the connection map
@@ -703,9 +710,16 @@ export function configureWebSocketServices(
             // Send notification messages for all client IDs in the room map
             for (const [clientId, room] of roomMap) {
                 const messageMetaData = getMessageMetadata(room.documentId, room.tenantId);
+                Lumberjack.info(
+                    `Temp-log at disconnect of room ${connectionTimeMap.has(clientId)}`,
+                    getLumberBaseProperties(room.documentId, room.tenantId),
+                );
                 // excluding summarizer for total client count.
                 if (connectionTimeMap.has(clientId)) {
-                    connectionCountLogger.decrementConnectionCount();
+                    connectionCountLogger.decrementConnectionCount()
+                    .catch((error) => {
+                        Lumberjack.error("Error in connectionCountLogger on Disconnect.", undefined, error);
+                    });
                 }
                 logger.info(`Disconnect of ${clientId} from room`, { messageMetaData });
                 Lumberjack.info(
