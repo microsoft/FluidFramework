b095f7da562ee1e9d81518ee1ff0995da8d0b413
Matt Rakow <ChumpChief@users.noreply.github.com>
2023-02-10 15:21:22 -0800

    Add scopeOverride to enable per-container-resolve customization of runtime initialization (#14114)
    Related: #13779 
    
    Currently, the scope is set once (at Loader creation time). However,
    it's possible hosts may want to pass different scope information on a
    per-resolve basis. This is particularly true when the load is performed
    in a different context, e.g. a top-level container vs. loading a "guest"
    container as a sub-part of an experience.
    
    Normally we recommend against using scope, but it does serve a purpose
    here of allowing hosts to pass objects to their container runtime at
    initialization time. Since the initialization phase (instantiateRuntime)
    is buried in the Container, this is the best entrypoint we currently
    make available for host customization of that initialization.

--- packages/loader/container-loader/src/container.ts
+++ packages/loader/container-loader/src/container.ts
@@ -13,7 +13,7 @@ import {
 	TelemetryEventCategory,
 } from "@fluidframework/common-definitions";
 import { assert, performance, unreachableCase } from "@fluidframework/common-utils";
-import { IRequest, IResponse, IFluidRouter } from "@fluidframework/core-interfaces";
+import { IRequest, IResponse, IFluidRouter, FluidObject } from "@fluidframework/core-interfaces";
 import {
 	IAudience,
 	IConnectionDetails,
@@ -129,6 +129,10 @@ export interface IContainerLoadOptions {
 	 * use the loader's logger, `Loader.services.subLogger`.
 	 */
 	baseLogger?: ITelemetryBaseLogger;
+	/**
+	 * A scope object to replace the one provided on the Loader.
+	 */
+	scopeOverride?: FluidObject;
 }
 
 export interface IContainerConfig {
@@ -147,6 +151,10 @@ export interface IContainerConfig {
 	 * use the loader's logger, `Loader.services.subLogger`.
 	 */
 	baseLogger?: ITelemetryBaseLogger;
+	/**
+	 * A scope object to replace the one provided on the Loader.
+	 */
+	scopeOverride?: FluidObject;
 }
 
 /**
@@ -303,6 +311,7 @@ export class Container
 				canReconnect: loadOptions.canReconnect,
 				serializedContainerState: pendingLocalState,
 				baseLogger: loadOptions.baseLogger,
+				scopeOverride: loadOptions.scopeOverride,
 			},
 			protocolHandlerBuilder,
 		);
@@ -455,6 +464,7 @@ export class Container
 	}
 
 	private readonly clientDetailsOverride: IClientDetails | undefined;
+	private readonly _scopeOverride: FluidObject | undefined;
 	private readonly _deltaManager: DeltaManager<ConnectionManager>;
 	private service: IDocumentService | undefined;
 
@@ -605,7 +615,7 @@ export class Container
 	}
 	public readonly options: ILoaderOptions;
 	private get scope() {
-		return this.loader.services.scope;
+		return this._scopeOverride ?? this.loader.services.scope;
 	}
 	private get codeLoader() {
 		return this.loader.services.codeLoader;
@@ -627,6 +637,7 @@ export class Container
 		});
 
 		this.clientDetailsOverride = config.clientDetailsOverride;
+		this._scopeOverride = config.scopeOverride;
 		this._resolvedUrl = config.resolvedUrl;
 		if (config.canReconnect !== undefined) {
 			this._canReconnect = config.canReconnect;
