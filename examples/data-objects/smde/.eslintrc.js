cee55168bed33894fd61bc03747774a248404228
Matt Rakow <marakow@microsoft.com>
2023-02-10 12:23:56 -0800

    Remove strict-boolean-expressions exception

--- examples/data-objects/smde/.eslintrc.js
+++ examples/data-objects/smde/.eslintrc.js
@@ -6,6 +6,5 @@
 module.exports = {
 	extends: [require.resolve("@fluidframework/eslint-config-fluid/minimal"), "prettier"],
 	rules: {
-		"@typescript-eslint/strict-boolean-expressions": "off",
 	},
 };
