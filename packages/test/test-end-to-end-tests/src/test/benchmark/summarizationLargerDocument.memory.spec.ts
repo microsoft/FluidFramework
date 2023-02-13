868e07176b0d66a47d8e0d3626f3097fc920ce90
Nicholas Couri <nichoc@microsoft.com>
2023-02-13 10:16:32 -0800

    workaround error

--- packages/test/test-end-to-end-tests/src/test/benchmark/summarizationLargerDocument.memory.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/benchmark/summarizationLargerDocument.memory.spec.ts
@@ -52,7 +52,7 @@ const chunkingBatchesConfig: ITestContainerConfig = {
 	runtimeOptions: {
 		compressionOptions: {
 			minimumBatchSizeInBytes: 1024 * 1024,
-			compressionAlgorithm: "lz4",
+			compressionAlgorithm: "lz4" as any,
 		},
 		chunkSizeInBytes: 600 * 1024,
 		//		summaryOptions: { summaryConfigOverrides: { state: "disabled" } },
