01b7be83e5f731f930188451695ff89d86bc2e01
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2023-02-14 11:35:57 -0800

    tree: cleanup JsObject performance testing in tree

--- packages/dds/tree/src/test/domains/json/jsonCursor.bench.ts
+++ packages/dds/tree/src/test/domains/json/jsonCursor.bench.ts
@@ -5,7 +5,6 @@
 
 import { strict as assert } from "assert";
 import { benchmark, BenchmarkType, isInPerformanceTestingMode } from "@fluid-tools/benchmark";
-import { Jsonable } from "@fluidframework/datastore-definitions";
 import {
 	ITreeCursor,
 	singleJsonCursor,
@@ -34,38 +33,7 @@ import { Canada, generateCanada } from "./canada";
 import { averageTwoValues, sum, sumMap } from "./benchmarks";
 import { generateTwitterJsonByByteSize, Twitter } from "./twitter";
 import { CitmCatalog, generateCitmJson } from "./citm";
-
-// IIRC, extracting this helper from clone() encourages V8 to inline the terminal case at
-// the leaves, but this should be verified.
-function cloneObject<T, J = Jsonable<T>>(obj: J): J {
-	if (Array.isArray(obj)) {
-		// PERF: 'Array.map()' was ~44% faster than looping over the array. (node 14 x64)
-		return obj.map(clone) as unknown as J;
-	} else {
-		const result: any = {};
-		// PERF: Nested array allocs make 'Object.entries()' ~2.4x slower than reading
-		//       value via 'value[key]', even when destructuring. (node 14 x64)
-		for (const key of Object.keys(obj)) {
-			// Like `result[key] = clone((obj as any)[key]);` but safe for when key == "__proto__"
-			Object.defineProperty(result, key, {
-				enumerable: true,
-				configurable: true,
-				writable: true,
-				value: clone((obj as any)[key]),
-			});
-		}
-		return result as J;
-	}
-}
-
-// Optimized deep clone implementation for "Jsonable" object trees.  Used as a real-world-ish
-// baseline to measure the overhead of using ITreeCursor in a scenario where we're reifying a
-// domain model for the application.
-function clone<T>(value: Jsonable<T>): Jsonable<T> {
-	// PERF: Separate clone vs. cloneObject yields an ~11% speedup in 'canada.json',
-	//       likely due to inlining short-circuiting recursing at leaves (node 14 x64).
-	return typeof value !== "object" || value === null ? value : cloneObject(value);
-}
+import { clone } from "./jsObjectUtil";
 
 /**
  * Performance test suite that measures a variety of access patterns using ITreeCursor.
@@ -91,7 +59,7 @@ function bench(
 				type: BenchmarkType.Measurement,
 				title: "Clone JS Object",
 				before: () => {
-					const cloned = clone<any>(json);
+					const cloned = clone(json);
 					assert.deepEqual(cloned, json, "clone() must return an equivalent tree.");
 					assert.notEqual(
 						cloned,
@@ -100,7 +68,7 @@ function bench(
 					);
 				},
 				benchmarkFn: () => {
-					clone<any>(json);
+					clone(json);
 				},
 			});
 
