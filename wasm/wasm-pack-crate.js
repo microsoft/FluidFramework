a1fbfc236f280468576916476c0a01b75312931e
justus-camp <102552460+justus-camp@users.noreply.github.com>
2023-02-15 21:36:34 +0000

    More progress

--- wasm/wasm-pack-crate.js
+++ wasm/wasm-pack-crate.js
@@ -7,14 +7,17 @@ const path = require('node:path');
 const fs = require('fs');
 const toml = require('toml');
 const child_process = require('child_process');
+const rimraf = require('rimraf');
+
+const webPackagesPath = path.resolve("./web-packages");
 
 const element = process.argv[2];
 const name = path.basename(element);
-const subTomlData = fs.readFileSync(`${element}/Cargo.toml`, 'utf-8');
+const subTomlData = fs.readFileSync(path.join(element, "Cargo.toml"), 'utf-8');
 const parsedSubToml = toml.parse(subTomlData);
 const existingPackageJsonTemplateData = fs.readFileSync('package-combo-template.json', 'utf-8');
 const parsedJson = JSON.parse(existingPackageJsonTemplateData);
-const outputJsonPath = path.join("web-packages", element, "package.json");
+const outputJsonPath = path.join(webPackagesPath, element, "package.json");
 parsedJson.name = `@fluid-experimental/${parsedSubToml.package.name}`;
 parsedJson.module = "./" + path.join("bundler", `${parsedSubToml.package.name}.js`);
 parsedJson.types = "./" + path.join("nodejs", `${parsedSubToml.package.name}.d.ts`);
@@ -25,7 +28,17 @@ if (parsedSubToml.package.description !== undefined) {
 } else {
     delete parsedJson.description;
 }
-child_process.execSync(`wasm-pack build --target bundler --out-dir ../web-packages/${name}/bundler ${element}`);
-child_process.execSync(`wasm-pack build --target nodejs --out-dir ../web-packages/${name}/nodejs ${element}`);
-console.log("writing file", outputJsonPath);
+parsedJson.files = [ 
+    `/nodejs/${name}_bg.wasm`,
+    `/nodejs/${name}.js`,
+    `/nodejs/${name}.d.ts`,
+    `/bundler/${name}_bg.wasm`,
+    `/bundler/${name}.js`,
+    `/bundler/${name}_bg.js`
+];
+
+child_process.execSync(`wasm-pack build --target bundler --out-dir ${path.join(webPackagesPath, name, "bundler")} ${element}`);
+rimraf.sync(path.join(webPackagesPath, name, "bundler", "package.json"));
+child_process.execSync(`wasm-pack build --target nodejs --out-dir ${path.join(webPackagesPath, name, "nodejs")} ${element}`);
+rimraf.sync(path.join(webPackagesPath, name, "nodejs", "package.json"));
 fs.writeFileSync(outputJsonPath, JSON.stringify(parsedJson, undefined, 4));
