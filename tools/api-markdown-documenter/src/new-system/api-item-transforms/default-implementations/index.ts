b9495dc2946e89fc610c3d27442a018ebac06c9d
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-02-15 22:38:53 +0000

    refactor: Clean up default-transforms API

--- tools/api-markdown-documenter/src/new-system/api-item-transforms/default-implementations/index.ts
+++ tools/api-markdown-documenter/src/new-system/api-item-transforms/default-implementations/index.ts
@@ -7,18 +7,13 @@
  * Default {@link @microsoft/api-extractor-model#ApiItem} transformation implementations.
  */
 
-// TODO: fix before merging
-/* eslint-disable no-restricted-syntax */
-
-export * from "./TransformApiClass";
-export * from "./TransformApiEnum";
-export * from "./TransformApiFunctionLike";
-export * from "./TransformApiInterface";
-export * from "./TransformApiItemWithoutChildren";
-export * from "./TransformApiModel";
-export * from "./TransformApiModuleLike";
-export * from "./TransformApiNamespace";
-export * from "./TransformApiPackage";
-export * from "./CreateSectionWithChildContent";
-
-/* eslint-enable no-restricted-syntax */
+export { createSectionWithChildContent } from "./CreateSectionWithChildContent";
+export { transformApiClass } from "./TransformApiClass";
+export { transformApiEnum } from "./TransformApiEnum";
+export { transformApiFunctionLike } from "./TransformApiFunctionLike";
+export { transformApiInterface } from "./TransformApiInterface";
+export { transformApiItemWithoutChildren } from "./TransformApiItemWithoutChildren";
+export { transformApiModel } from "./TransformApiModel";
+export { transformApiModuleLike } from "./TransformApiModuleLike";
+export { transformApiNamespace } from "./TransformApiNamespace";
+export { transformApiPackage } from "./TransformApiPackage";
