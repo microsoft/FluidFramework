ca10c888a4d9aa4efc657864bc6f0a76b4c4780c
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-02-15 22:31:40 +0000

    test: Restructure tests

--- tools/api-markdown-documenter/src/new-system/markdown-renderer/test/RenderOrderedList.test.ts
+++ tools/api-markdown-documenter/src/new-system/markdown-renderer/test/RenderOrderedList.test.ts
@@ -8,52 +8,56 @@ import { OrderedListNode } from "../../documentation-domain";
 import { testRender } from "./Utilities";
 
 describe("OrderedListNode rendering tests", () => {
-	it("Empty list (Markdown)", () => {
-		expect(testRender(OrderedListNode.Empty)).to.equal("\n");
+	describe("Markdown", () => {
+		it("Empty list", () => {
+			expect(testRender(OrderedListNode.Empty)).to.equal("\n");
+		});
+
+		it("Simple list", () => {
+			const text1 = "Item 1";
+			const text2 = "Item 2";
+			const text3 = "Item 3";
+
+			const input = OrderedListNode.createFromPlainTextEntries([text1, text2, text3]);
+			const result = testRender(input);
+
+			const expected = ["", `1. ${text1}`, `1. ${text2}`, `1. ${text3}`, "", ""].join("\n");
+
+			expect(result).to.equal(expected);
+		});
 	});
 
-	it("Simple list (Markdown)", () => {
-		const text1 = "Item 1";
-		const text2 = "Item 2";
-		const text3 = "Item 3";
+	describe("HTML", () => {
+		it("Empty list", () => {
+			expect(testRender(OrderedListNode.Empty, undefined, { insideHtml: true })).to.equal(
+				"<ol>\n</ol>\n",
+			);
+		});
 
-		const input = OrderedListNode.createFromPlainTextEntries([text1, text2, text3]);
-		const result = testRender(input);
+		it("Simple list", () => {
+			const text1 = "Item 1";
+			const text2 = "Item 2";
+			const text3 = "Item 3";
 
-		const expected = ["", `1. ${text1}`, `1. ${text2}`, `1. ${text3}`, "", ""].join("\n");
+			const input = OrderedListNode.createFromPlainTextEntries([text1, text2, text3]);
+			const result = testRender(input, undefined, { insideHtml: true });
 
-		expect(result).to.equal(expected);
-	});
+			const expected = [
+				"<ol>",
+				"  <li>",
+				`    ${text1}`,
+				"  </li>",
+				"  <li>",
+				`    ${text2}`,
+				"  </li>",
+				"  <li>",
+				`    ${text3}`,
+				"  </li>",
+				"</ol>",
+				"",
+			].join("\n");
 
-	it("Empty list (HTML)", () => {
-		expect(testRender(OrderedListNode.Empty, undefined, { insideHtml: true })).to.equal(
-			"<ol>\n</ol>\n",
-		);
-	});
-
-	it("Simple list (HTML)", () => {
-		const text1 = "Item 1";
-		const text2 = "Item 2";
-		const text3 = "Item 3";
-
-		const input = OrderedListNode.createFromPlainTextEntries([text1, text2, text3]);
-		const result = testRender(input, undefined, { insideHtml: true });
-
-		const expected = [
-			"<ol>",
-			"  <li>",
-			`    ${text1}`,
-			"  </li>",
-			"  <li>",
-			`    ${text2}`,
-			"  </li>",
-			"  <li>",
-			`    ${text3}`,
-			"  </li>",
-			"</ol>",
-			"",
-		].join("\n");
-
-		expect(result).to.equal(expected);
+			expect(result).to.equal(expected);
+		});
 	});
 });
