fb7e97c31d12d79fd5b247da2e0683c15efa0577
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-02-15 22:34:02 +0000

    test: Restructure test hierarchy

--- tools/api-markdown-documenter/src/new-system/markdown-renderer/test/RenderParagraph.test.ts
+++ tools/api-markdown-documenter/src/new-system/markdown-renderer/test/RenderParagraph.test.ts
@@ -8,35 +8,39 @@ import { ParagraphNode, PlainTextNode } from "../../documentation-domain";
 import { testRender } from "./Utilities";
 
 describe("ParagraphNode rendering tests", () => {
-	it("Empty paragraph (Markdown)", () => {
-		expect(testRender(ParagraphNode.Empty)).to.equal("\n"); // Paragraphs always create a trailing blank line in Markdown
+	describe("Markdown", () => {
+		it("Empty paragraph", () => {
+			expect(testRender(ParagraphNode.Empty)).to.equal("\n"); // Paragraphs always create a trailing blank line in Markdown
+		});
+
+		it("Simple paragraph", () => {
+			const text1 = "This is some text. ";
+			const text2 = "This is more text!";
+
+			const input = new ParagraphNode([new PlainTextNode(text1), new PlainTextNode(text2)]);
+			const result = testRender(input);
+
+			const expected = [`${text1}${text2}`, "", ""].join("\n");
+			expect(result).to.equal(expected);
+		});
 	});
 
-	it("Simple paragraph (Markdown)", () => {
-		const text1 = "This is some text. ";
-		const text2 = "This is more text!";
+	describe("HTML", () => {
+		it("Empty paragraph", () => {
+			expect(testRender(ParagraphNode.Empty, undefined, { insideHtml: true })).to.equal(
+				"<p>\n</p>\n",
+			);
+		});
 
-		const input = new ParagraphNode([new PlainTextNode(text1), new PlainTextNode(text2)]);
-		const result = testRender(input);
+		it("Simple paragraph", () => {
+			const text1 = "This is some text. ";
+			const text2 = "This is more text!";
 
-		const expected = [`${text1}${text2}`, "", ""].join("\n");
-		expect(result).to.equal(expected);
-	});
+			const input = new ParagraphNode([new PlainTextNode(text1), new PlainTextNode(text2)]);
+			const result = testRender(input, undefined, { insideHtml: true });
 
-	it("Empty paragraph (HTML)", () => {
-		expect(testRender(ParagraphNode.Empty, undefined, { insideHtml: true })).to.equal(
-			"<p>\n</p>\n",
-		);
-	});
-
-	it("Simple paragraph (HTML)", () => {
-		const text1 = "This is some text. ";
-		const text2 = "This is more text!";
-
-		const input = new ParagraphNode([new PlainTextNode(text1), new PlainTextNode(text2)]);
-		const result = testRender(input, undefined, { insideHtml: true });
-
-		const expected = ["<p>", `  ${text1}${text2}`, "</p>", ""].join("\n");
-		expect(result).to.equal(expected);
+			const expected = ["<p>", `  ${text1}${text2}`, "</p>", ""].join("\n");
+			expect(result).to.equal(expected);
+		});
 	});
 });
--- tools/api-markdown-documenter/src/new-system/markdown-renderer/test/RenderTable.test.ts
+++ tools/api-markdown-documenter/src/new-system/markdown-renderer/test/RenderTable.test.ts
@@ -8,184 +8,188 @@ import { TableCellNode, TableNode, TableRowNode } from "../../documentation-doma
 import { testRender } from "./Utilities";
 
 describe("Table rendering tests", () => {
-	it("Empty table (Markdown)", () => {
-		expect(testRender(TableNode.Empty)).to.equal("\n");
-	});
+	describe("Markdown", () => {
+		it("Empty table", () => {
+			expect(testRender(TableNode.Empty)).to.equal("\n");
+		});
 
-	it("Simple table without header (Markdown)", () => {
-		const input = new TableNode([
-			new TableRowNode([
-				TableCellNode.createFromPlainText("Cell 1A"),
-				TableCellNode.createFromPlainText("Cell 1B"),
-				TableCellNode.createFromPlainText("Cell 1C"),
-			]),
-			new TableRowNode([
-				TableCellNode.createFromPlainText("Cell 2A"),
-				TableCellNode.createFromPlainText("Cell 2B"),
-			]),
-		]);
-
-		const result = testRender(input);
-
-		const expected = [
-			"",
-			"| Cell 1A | Cell 1B | Cell 1C |",
-			"| Cell 2A | Cell 2B |",
-			"",
-			"",
-		].join("\n");
-
-		expect(result).to.equal(expected);
-	});
-
-	it("Simple table with header (Markdown)", () => {
-		const input = new TableNode(
-			[
+		it("Simple table without header", () => {
+			const input = new TableNode([
 				new TableRowNode([
 					TableCellNode.createFromPlainText("Cell 1A"),
 					TableCellNode.createFromPlainText("Cell 1B"),
+					TableCellNode.createFromPlainText("Cell 1C"),
 				]),
 				new TableRowNode([
 					TableCellNode.createFromPlainText("Cell 2A"),
 					TableCellNode.createFromPlainText("Cell 2B"),
-					TableCellNode.createFromPlainText("Cell 2C"),
 				]),
-			],
-			/* headingRow: */ new TableRowNode([
-				TableCellNode.createFromPlainText("Header A"),
-				TableCellNode.createFromPlainText("Header B"),
-				TableCellNode.createFromPlainText("Header C"),
-			]),
-		);
+			]);
 
-		const result = testRender(input);
+			const result = testRender(input);
 
-		const expected = [
-			"",
-			"| Header A | Header B | Header C |",
-			"| --- | --- | --- |",
-			"| Cell 1A | Cell 1B |",
-			"| Cell 2A | Cell 2B | Cell 2C |",
-			"",
-			"",
-		].join("\n");
+			const expected = [
+				"",
+				"| Cell 1A | Cell 1B | Cell 1C |",
+				"| Cell 2A | Cell 2B |",
+				"",
+				"",
+			].join("\n");
 
-		expect(result).to.equal(expected);
+			expect(result).to.equal(expected);
+		});
+
+		it("Simple table with header", () => {
+			const input = new TableNode(
+				[
+					new TableRowNode([
+						TableCellNode.createFromPlainText("Cell 1A"),
+						TableCellNode.createFromPlainText("Cell 1B"),
+					]),
+					new TableRowNode([
+						TableCellNode.createFromPlainText("Cell 2A"),
+						TableCellNode.createFromPlainText("Cell 2B"),
+						TableCellNode.createFromPlainText("Cell 2C"),
+					]),
+				],
+				/* headingRow: */ new TableRowNode([
+					TableCellNode.createFromPlainText("Header A"),
+					TableCellNode.createFromPlainText("Header B"),
+					TableCellNode.createFromPlainText("Header C"),
+				]),
+			);
+
+			const result = testRender(input);
+
+			const expected = [
+				"",
+				"| Header A | Header B | Header C |",
+				"| --- | --- | --- |",
+				"| Cell 1A | Cell 1B |",
+				"| Cell 2A | Cell 2B | Cell 2C |",
+				"",
+				"",
+			].join("\n");
+
+			expect(result).to.equal(expected);
+		});
 	});
 
-	it("Empty table (HTML)", () => {
-		expect(testRender(TableNode.Empty, undefined, { insideHtml: true })).to.equal(
-			"<table>\n</table>\n",
-		);
-	});
+	describe("HTML", () => {
+		it("Empty table", () => {
+			expect(testRender(TableNode.Empty, undefined, { insideHtml: true })).to.equal(
+				"<table>\n</table>\n",
+			);
+		});
 
-	it("Simple table without header (HTML)", () => {
-		const input = new TableNode([
-			new TableRowNode([
-				TableCellNode.createFromPlainText("Cell 1A"),
-				TableCellNode.createFromPlainText("Cell 1B"),
-				TableCellNode.createFromPlainText("Cell 1C"),
-			]),
-			new TableRowNode([
-				TableCellNode.createFromPlainText("Cell 2A"),
-				TableCellNode.createFromPlainText("Cell 2B"),
-			]),
-		]);
-
-		const result = testRender(input, undefined, { insideHtml: true });
-
-		const expected = [
-			"<table>",
-			"  <tbody>",
-			"    <tr>",
-			"      <td>",
-			"        Cell 1A",
-			"      </td>",
-			"      <td>",
-			"        Cell 1B",
-			"      </td>",
-			"      <td>",
-			"        Cell 1C",
-			"      </td>",
-			"    </tr>",
-			"    <tr>",
-			"      <td>",
-			"        Cell 2A",
-			"      </td>",
-			"      <td>",
-			"        Cell 2B",
-			"      </td>",
-			"    </tr>",
-			"  </tbody>",
-			"</table>",
-			"",
-		].join("\n");
-
-		expect(result).to.equal(expected);
-	});
-
-	it("Simple table with header (Markdown)", () => {
-		const input = new TableNode(
-			[
+		it("Simple table without header", () => {
+			const input = new TableNode([
 				new TableRowNode([
 					TableCellNode.createFromPlainText("Cell 1A"),
 					TableCellNode.createFromPlainText("Cell 1B"),
+					TableCellNode.createFromPlainText("Cell 1C"),
 				]),
 				new TableRowNode([
 					TableCellNode.createFromPlainText("Cell 2A"),
 					TableCellNode.createFromPlainText("Cell 2B"),
-					TableCellNode.createFromPlainText("Cell 2C"),
 				]),
-			],
-			/* headingRow: */ new TableRowNode([
-				TableCellNode.createFromPlainText("Header A"),
-				TableCellNode.createFromPlainText("Header B"),
-				TableCellNode.createFromPlainText("Header C"),
-			]),
-		);
+			]);
 
-		const result = testRender(input, undefined, { insideHtml: true });
+			const result = testRender(input, undefined, { insideHtml: true });
 
-		const expected = [
-			"<table>",
-			"  <thead>",
-			"    <tr>",
-			"      <td>",
-			"        Header A",
-			"      </td>",
-			"      <td>",
-			"        Header B",
-			"      </td>",
-			"      <td>",
-			"        Header C",
-			"      </td>",
-			"    </tr>",
-			"  </thead>",
-			"  <tbody>",
-			"    <tr>",
-			"      <td>",
-			"        Cell 1A",
-			"      </td>",
-			"      <td>",
-			"        Cell 1B",
-			"      </td>",
-			"    </tr>",
-			"    <tr>",
-			"      <td>",
-			"        Cell 2A",
-			"      </td>",
-			"      <td>",
-			"        Cell 2B",
-			"      </td>",
-			"      <td>",
-			"        Cell 2C",
-			"      </td>",
-			"    </tr>",
-			"  </tbody>",
-			"</table>",
-			"",
-		].join("\n");
+			const expected = [
+				"<table>",
+				"  <tbody>",
+				"    <tr>",
+				"      <td>",
+				"        Cell 1A",
+				"      </td>",
+				"      <td>",
+				"        Cell 1B",
+				"      </td>",
+				"      <td>",
+				"        Cell 1C",
+				"      </td>",
+				"    </tr>",
+				"    <tr>",
+				"      <td>",
+				"        Cell 2A",
+				"      </td>",
+				"      <td>",
+				"        Cell 2B",
+				"      </td>",
+				"    </tr>",
+				"  </tbody>",
+				"</table>",
+				"",
+			].join("\n");
 
-		expect(result).to.equal(expected);
+			expect(result).to.equal(expected);
+		});
+
+		it("Simple table with header", () => {
+			const input = new TableNode(
+				[
+					new TableRowNode([
+						TableCellNode.createFromPlainText("Cell 1A"),
+						TableCellNode.createFromPlainText("Cell 1B"),
+					]),
+					new TableRowNode([
+						TableCellNode.createFromPlainText("Cell 2A"),
+						TableCellNode.createFromPlainText("Cell 2B"),
+						TableCellNode.createFromPlainText("Cell 2C"),
+					]),
+				],
+				/* headingRow: */ new TableRowNode([
+					TableCellNode.createFromPlainText("Header A"),
+					TableCellNode.createFromPlainText("Header B"),
+					TableCellNode.createFromPlainText("Header C"),
+				]),
+			);
+
+			const result = testRender(input, undefined, { insideHtml: true });
+
+			const expected = [
+				"<table>",
+				"  <thead>",
+				"    <tr>",
+				"      <td>",
+				"        Header A",
+				"      </td>",
+				"      <td>",
+				"        Header B",
+				"      </td>",
+				"      <td>",
+				"        Header C",
+				"      </td>",
+				"    </tr>",
+				"  </thead>",
+				"  <tbody>",
+				"    <tr>",
+				"      <td>",
+				"        Cell 1A",
+				"      </td>",
+				"      <td>",
+				"        Cell 1B",
+				"      </td>",
+				"    </tr>",
+				"    <tr>",
+				"      <td>",
+				"        Cell 2A",
+				"      </td>",
+				"      <td>",
+				"        Cell 2B",
+				"      </td>",
+				"      <td>",
+				"        Cell 2C",
+				"      </td>",
+				"    </tr>",
+				"  </tbody>",
+				"</table>",
+				"",
+			].join("\n");
+
+			expect(result).to.equal(expected);
+		});
 	});
 });
