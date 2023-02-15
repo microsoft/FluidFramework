ec09d51120de94462a8a7ff889e45588fa951f99
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-02-15 19:59:26 +0000

    refactor: Add Empty singleton and update docs

--- tools/api-markdown-documenter/src/new-system/documentation-domain/BlockQuoteNode.ts
+++ tools/api-markdown-documenter/src/new-system/documentation-domain/BlockQuoteNode.ts
@@ -4,23 +4,38 @@
  */
 import { DocumentationNodeType } from "./DocumentationNodeType";
 import { DocumentationNode, ParentNodeBase } from "./DocumentionNode";
+import { PlainTextNode } from "./PlainTextNode";
 import { createNodesFromPlainText } from "./Utilities";
 
 /**
  * TODO
  *
- * @example
+ * @example Markdown
+ *
  * ```md
  * > Foo
  * >
  * > Bar
  * ```
+ *
+ * @example HTML
+ *
+ * <blockquote>
+ * 	Foo
+ *	<br/>
+ * 	Bar
+ * </blockquote>
  */
 export class BlockQuoteNode extends ParentNodeBase {
 	/**
 	 * {@inheritDoc DocumentationNode."type"}
 	 */
 	public readonly type = DocumentationNodeType.BlockQuote;
+
+	/**
+	 * Static singleton representing an empty Block Quote node.
+	 */
+	public static readonly Empty: BlockQuoteNode = new BlockQuoteNode([PlainTextNode.Empty]);
 
 	public constructor(children: DocumentationNode[]) {
 		super(children);
