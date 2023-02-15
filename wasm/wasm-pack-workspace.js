83af21084b5b2f15633ab1069f4bdfc89b7fa474
justus-camp <102552460+justus-camp@users.noreply.github.com>
2023-02-15 19:08:56 +0000

    Generate package.json per npm package

--- wasm/wasm-pack-workspace.js
+++ wasm/wasm-pack-workspace.js
@@ -4,12 +4,29 @@ const child_process = require('child_process');
 const path = require('node:path');
 
 try {
-  const data = fs.readFileSync('./Cargo.toml', 'utf8');
+  const data = fs.readFileSync('./Cargo.toml', 'utf-8');
   const parsed = toml.parse(data);
   parsed.workspace.members.forEach(element => {
     const name = path.basename(element);
-    child_process.execSync(`wasm-pack build --target bundler --out-dir ../target-web/${name}-bundler ${element}`);
-    child_process.execSync(`wasm-pack build --target nodejs --out-dir ../target-web/${name}-nodejs ${element}`);
+    const subTomlData = fs.readFileSync(`${element}/Cargo.toml`, 'utf-8');
+    const parsedSubToml = toml.parse(subTomlData);
+    const existingPackageJsonTemplateData = fs.readFileSync('package-combo-template.json', 'utf-8');
+    const parsedJson = JSON.parse(existingPackageJsonTemplateData);
+    const outputJsonPath = path.join("web-packages", element, "package.json");
+    parsedJson.name = `@fluid-experimental/${parsedSubToml.package.name}`;
+    parsedJson.module = "./" + path.join("bundler", `${parsedSubToml.package.name}.js`);
+    parsedJson.types = "./" + path.join("nodejs", `${parsedSubToml.package.name}.d.ts`);
+    parsedJson.main = "./" + path.join("nodejs", `${parsedSubToml.package.name}.js`);
+    parsedJson.version = parsedSubToml.package.version;
+    if (parsedSubToml.package.description !== undefined) {
+      parsedJson.description = parsedSubToml.package.description;
+    } else {
+      delete parsedJson.description;
+    }
+    child_process.execSync(`wasm-pack build --target bundler --out-dir ../web-packages/${name}/bundler ${element}`);
+    child_process.execSync(`wasm-pack build --target nodejs --out-dir ../web-packages/${name}/nodejs ${element}`);
+    console.log("writing file", outputJsonPath);
+    fs.writeFileSync(outputJsonPath, JSON.stringify(parsedJson, undefined, 4));
   });
 } catch (err) {
   console.error(err);
