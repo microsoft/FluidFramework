80ecfa33e8baa37ff7bfd366ec42a2104b115ab6
Noah Encke <noencke@microsoft.com>
2023-02-21 09:13:39 -0800

    Fix slow assert

--- packages/dds/tree/src/core/rebase/rebaser.ts
+++ packages/dds/tree/src/core/rebase/rebaser.ts
@@ -46,8 +46,7 @@ export class Rebaser<TChange> {
 		// Find where `base` is in the target branch
 		const baseIndex = targetPath.findIndex((r) => r === base);
 		if (baseIndex === -1) {
-			// TODO: Ideally, this would be an "assertExpensive"
-			assert(findCommonAncestor(base, target) !== undefined, "base is not in target branch");
+			assert(base === target, "base is not in target branch");
 			// Here, `base` must be `ancestor`, and therefore the source branch is already up to date with `base`.
 			return [source, this.changeRebaser.compose([])];
 		}
