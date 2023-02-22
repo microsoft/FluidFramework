17e882e2f03ea7c5f1aa32219f5efd105e8d6bf7
Matt Rakow <marakow@microsoft.com>
2023-02-22 13:39:17 -0800

    Fix typo

--- packages/loader/driver-utils/src/networkUtils.ts
+++ packages/loader/driver-utils/src/networkUtils.ts
@@ -39,7 +39,7 @@ export function logNetworkFailure(
  * But there should be no false negatives.
  * The only exception - Opera returns false when user enters "Work Offline" mode, regardless of actual connectivity.
  *
- * @deprecated - @deprecated 2.0.0-internal.3.2.0 Not recommended for general purpose use.
+ * @deprecated - 2.0.0-internal.3.2.0 Not recommended for general purpose use.
  */
 // eslint-disable-next-line @typescript-eslint/promise-function-async
 export function waitForConnectedState(minDelay: number): Promise<void> {
