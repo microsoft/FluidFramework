fe9982ed6cbdef06d5ec96174da472fc4aeff392
Tyler Butler <tyler@tylerbutler.com>
2023-02-24 08:04:42 -0800

    updates

--- build-tools/packages/build-cli/src/repoConfig.ts
+++ build-tools/packages/build-cli/src/repoConfig.ts
@@ -3,7 +3,28 @@
  * Licensed under the MIT License.
  */
 
+import minimatch from "minimatch";
+import { ReleaseGroup, ReleasePackage } from "./releaseGroups";
+
+// Mapping of branch to a list of release groups/packages that should run policy by default.
+// TODO: This should be configured in the fluid-build config, like type test defaults are.
+const defaults = {
+	"main": ["client"],
+	"release/*": ["client"],
+};
+
 /**
- * The branches in this list should run policy check by default during release-related activities.
+ * Returns true if policy-check should run by default for a given branch/release group combination.
  */
-export const branchesRunDefaultPolicy = ["main"];
+export const getPolicyRunDefault = (
+	releaseGroupOrPackage: ReleaseGroup | ReleasePackage,
+	branch: string,
+): boolean => {
+	for (const [branchPattern, shouldRunPolicy] of Object.entries(defaults)) {
+		if (minimatch(branch, branchPattern) === true) {
+			return shouldRunPolicy.includes(releaseGroupOrPackage);
+		}
+	}
+
+	return false;
+};
