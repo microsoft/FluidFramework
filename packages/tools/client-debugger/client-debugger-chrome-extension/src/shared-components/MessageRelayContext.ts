37460df5fa2e2f29f1f8955f52073a83101df227
Josmithr <josmithr@microsoft.com>
2023-02-24 14:04:46 -0800

    docs: Update docs

--- packages/tools/client-debugger/client-debugger-chrome-extension/src/shared-components/MessageRelayContext.ts
+++ packages/tools/client-debugger/client-debugger-chrome-extension/src/shared-components/MessageRelayContext.ts
@@ -10,6 +10,8 @@ import { IMessageRelay } from "../messaging";
 /**
  * Context for accessing a shared {@link IMessageRelay} for communicating messages with the webpage.
  *
+ * @remarks
+ *
  * Any message listening / posting should go through here, rather than directly through the
  * `window` (`globalThis`) to ensure general compatibility regardless of how the Chrome Extension
  * is configured / what context the components are run in.
