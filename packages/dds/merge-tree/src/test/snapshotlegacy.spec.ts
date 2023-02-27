28cb77b30cdc969b9f8da9e52fbeb524fa9cb52c
Abram Sanderson <abram.sanderson@gmail.com>
2023-02-27 14:16:30 -0800

    fix up some snapshot legacy tests

--- packages/dds/merge-tree/src/test/snapshotlegacy.spec.ts
+++ packages/dds/merge-tree/src/test/snapshotlegacy.spec.ts
@@ -9,12 +9,10 @@ import { IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions";
 import { MockStorage } from "@fluidframework/test-runtime-utils";
 import { ISequencedDocumentMessage } from "@fluidframework/protocol-definitions";
 import { SnapshotLegacy } from "../snapshotlegacy";
-import { createInsertOnlyAttributionPolicy } from "../attributionCollection";
+import { createInsertOnlyAttributionPolicy } from "../attributionPolicy";
 import { TestSerializer } from "./testSerializer";
 import { createClientsAtInitialState } from "./testClientLogger";
 import { TestClient } from ".";
-import { combineInterpreters, trackProperties } from "./testUtils";
-import { defaultInterpreter } from "../mergeTree";
 
 describe("snapshot", () => {
 	it("header only", async () => {
@@ -89,7 +87,8 @@ describe("snapshot", () => {
 		client: TestClient,
 		expected: {
 			root: (number | undefined)[];
-			channels?: { [name: string]: (number | undefined)[] }; }
+			channels?: { [name: string]: (number | undefined)[] };
+		},
 	): Promise<void> {
 		assert.deepEqual(
 			client.getAllAttributionSeqs(),
@@ -156,7 +155,7 @@ describe("snapshot", () => {
 		applyAllOps();
 
 		// "hello " has key 1 (i.e. seq 1), "new " has key 2 (i.e. seq 2), "world" has key 1.
-		const expectedAttribution = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1];
+		const expectedAttribution = { root: [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1] };
 		await assertAttributionKeysMatch(clients.A, expectedAttribution);
 	});
 
@@ -164,128 +163,31 @@ describe("snapshot", () => {
 		const clients = createClientsAtInitialState(
 			{
 				initialState: "",
-				options: {
-					attribution: {
-						track: false,
-						policyFactory: createInsertOnlyAttributionPolicy,
-					},
-				},
+				options: { attribution: { track: false } },
 			},
-		): Promise<void> {
-			assert.deepEqual(
-				client.getAllAttributionSeqs(),
-				expected.root,
-				"Keys don't match before round-tripping",
-			);
-			for (const [channel, channelExpectation] of Object.entries(expected.channels ?? {})) {
-				assert.deepEqual(
-					client.getAllAttributionSeqs(channel),
-					channelExpectation,
-					`Keys for channel ${channel} don't match before round-trip.`,
-				);
-			}
-
-			const serializer = new TestSerializer();
-			// This avoids necessitating handling catchup ops.
-			client.mergeTree.setMinSeq(client.mergeTree.collabWindow.currentSeq);
-			const snapshot = new SnapshotLegacy(client.mergeTree, client.logger);
-			snapshot.extractSync();
-			const summaryTree = snapshot.emit([], serializer, undefined!);
-			const services = MockStorage.createFromSummary(summaryTree.summary);
-
-			const roundTripClient = new TestClient({ attribution: { track: true } });
-			const runtime: Partial<IFluidDataStoreRuntime> = {
-				logger: roundTripClient.logger,
-				clientId: "round-trips summary",
-			};
-			await roundTripClient.load(runtime as IFluidDataStoreRuntime, services, serializer);
-			assert.deepEqual(
-				roundTripClient.getAllAttributionSeqs(),
-				expected.root,
-				"Keys don't match after round-tripping",
-			);
-			for (const [channel, channelExpectation] of Object.entries(expected.channels ?? {})) {
-				assert.deepEqual(
-					roundTripClient.getAllAttributionSeqs(channel),
-					channelExpectation,
-					`Keys for channel ${channel} don't match after round-trip.`,
-				);
-			}
-		}
-
-		it("preserves attribution information", async () => {
-			const clients = createClientsAtInitialState(
-				{
-					initialState: "",
-					options: {
-						attribution: {
-							track: true,
-							interpreter: combineInterpreters(
-								trackProperties("foo"),
-								defaultInterpreter,
-							),
-						},
-					},
-				},
-				"A",
-				"B",
-			);
-
-			ops.push(
-				clients.B.makeOpMessage(
-					clients.B.insertTextLocal(6, "new "),
-					/* seq */ 2,
-					/* refSeq */ 1,
-				),
-			);
-
-			applyAllOps();
-
-			// "hello " has key 1 (i.e. seq 1), "new " has key 2 (i.e. seq 2), "world" has key 1.
-			const expectedAttribution = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1];
-			await assertAttributionKeysMatch(clients.A, expectedAttribution);
-		});
-
-		it("doesn't include attribution information when attribution tracking is false on doc creation", async () => {
-			const clients = createClientsAtInitialState(
-				{
-					initialState: "",
-					options: { attribution: { track: false } },
-				},
-				"A",
-			);
-
-			ops.push(
-				clients.B.makeOpMessage(
-					clients.B.annotateRangeLocal(0, 14, { foo: "bar" }, undefined),
-					/* seq */ 3,
-					/* refSeq */ 2,
-				),
-			);
-
-			applyAllOps();
-
-			await assertAttributionKeysMatch(clients.A, {
-				// "hello " has key 1 (i.e. seq 1), "new " has key 2 (i.e. seq 2), "world" has key 1.
-				root: [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1],
-				channels: {
-					foo: Array.from({ length: 15 }, (_, i) => (i < 14 ? 3 : undefined)),
-				},
-			});
-		});
-
-		applyAllOps();
+			"A",
+		);
 
 		const ops: ISequencedDocumentMessage[] = [];
 		const applyAllOps = () =>
 			ops.splice(0).forEach((op) => clients.all.map((client) => client.applyMsg(op)));
 
-		ops.push(clients.A.makeOpMessage(clients.A.insertTextLocal(0, "hello world"), /* seq */ 1));
+		ops.push(
+			clients.B.makeOpMessage(
+				clients.B.annotateRangeLocal(0, 14, { foo: "bar" }, undefined),
+				/* seq */ 3,
+				/* refSeq */ 2,
+			),
+		);
 
 		applyAllOps();
 
 		await assertAttributionKeysMatch(clients.A, {
-			root: Array.from({ length: "hello world".length }, () => undefined),
+			// "hello " has key 1 (i.e. seq 1), "new " has key 2 (i.e. seq 2), "world" has key 1.
+			root: [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1],
+			channels: {
+				foo: Array.from({ length: 15 }, (_, i) => (i < 14 ? 3 : undefined)),
+			},
 		});
 	});
 });
