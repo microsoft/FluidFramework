3ba50d719257e3b74cf272288a095e848bb7397f
Navin Agarwal <naagar@microsoft.com>
2023-02-27 19:00:01 +0000

    use path

--- packages/test/snapshots/src/metadata.ts
+++ packages/test/snapshots/src/metadata.ts
@@ -4,6 +4,7 @@
  */
 
 import fs from "fs";
+import path from "path";
 
 interface ITestMetadata {
 	/** Tells whether testSummaries functionality is true. If so, the testSummaries ReplayArg is true. */
@@ -16,13 +17,16 @@ const currentMetadata: ITestMetadata = {
 };
 
 const metadataFileName = "metadata.json";
+function getFullMetadataFileName(folder: string): string {
+	return path.join(folder, "/", metadataFileName);
+}
 
 /**
  * Reads the content of the metadata folder in a given test folder and returns it. For older test folders before
  * metadata was added, returns an empty metadata.
  */
 export function getMetadata(folder: string): ITestMetadata {
-	const metadataFile = `${folder}/${metadataFileName}`;
+	const metadataFile = getFullMetadataFileName(folder);
 	if (!fs.existsSync(metadataFile)) {
 		return {};
 	}
@@ -37,7 +41,7 @@ export function getMetadata(folder: string): ITestMetadata {
  * Writes the metadata file in the given test folder.
  */
 export function writeMetadataFile(folder: string) {
-	const metadataFile = `${folder}/${metadataFileName}`;
+	const metadataFile = getFullMetadataFileName(folder);
 	fs.writeFileSync(metadataFile, JSON.stringify(currentMetadata), {
 		encoding: "utf-8",
 	});
