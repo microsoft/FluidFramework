4a45e89a198280a30a51321211f3e79c7cd65e55
nedalhy <nedalhy@gmail.com>
2023-03-01 12:22:56 +0100

    Fix linting

--- experimental/PropertyDDS/packages/property-changeset/src/test/map.spec.ts
+++ experimental/PropertyDDS/packages/property-changeset/src/test/map.spec.ts
@@ -8,8 +8,12 @@ import { ChangeSet } from "../changeset";
 
 describe("Map rebase ChangeSets", function () {
 	it("Case 1", () => {
-		const originalCS = JSON.parse('{"modify":{"NodeProperty":{"a":{"NodeProperty":{"b":{"map<Bool>":{"c":{"modify":{"1":{"value":true,"oldValue":true}}}}}}}}}}');
-		const toRebaseCS = JSON.parse('{"modify":{"NodeProperty":{"a":{"NodeProperty":{"b":{"map<Bool>":{"c":{"remove":{"1":true},"modify":{"2":{"value":true,"oldValue":true}}},"d":{"remove":{"1":true}}}}}}}}}');
+		const originalCS = JSON.parse(
+			'{"modify":{"NodeProperty":{"a":{"NodeProperty":{"b":{"map<Bool>":{"c":{"modify":{"1":{"value":true,"oldValue":true}}}}}}}}}}',
+		);
+		const toRebaseCS = JSON.parse(
+			'{"modify":{"NodeProperty":{"a":{"NodeProperty":{"b":{"map<Bool>":{"c":{"remove":{"1":true},"modify":{"2":{"value":true,"oldValue":true}}},"d":{"remove":{"1":true}}}}}}}}}',
+		);
 
 		const cs = new ChangeSet(toRebaseCS);
 		const changes = cs._rebaseChangeSet(originalCS, [], {});
