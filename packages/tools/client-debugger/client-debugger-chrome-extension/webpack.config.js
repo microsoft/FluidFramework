3b794f789591be6fc2fadae7aa4f9405135480ff
Josmithr <josmithr@microsoft.com>
2023-03-01 15:11:40 -0800

    revert: Unnecessary variable rename

--- packages/tools/client-debugger/client-debugger-chrome-extension/webpack.config.js
+++ packages/tools/client-debugger/client-debugger-chrome-extension/webpack.config.js
@@ -7,32 +7,32 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
 
-const packageSourcePath = path.resolve(__dirname, "src");
-const packageBuildPath = path.resolve(__dirname, "dist");
+const sourceDirectoryPath = path.resolve(__dirname, "src");
+const buildDirectoryPath = path.resolve(__dirname, "dist");
 
 module.exports = {
 	mode: "development", // TODO: production
 	devtool: "inline-source-map", // TODO: remove this
 	entry: {
 		// The Devtools script and view
-		"devtools/DevtoolsScript": path.join(packageSourcePath, "devtools", "DevtoolsScript.ts"),
-		"devtools/RootView": path.join(packageSourcePath, "devtools", "RootView.tsx"),
+		"devtools/DevtoolsScript": path.join(sourceDirectoryPath, "devtools", "DevtoolsScript.ts"),
+		"devtools/RootView": path.join(sourceDirectoryPath, "devtools", "RootView.tsx"),
 
 		// The Background script
 		"background/BackgroundScript": path.join(
-			packageSourcePath,
+			sourceDirectoryPath,
 			"background",
 			"BackgroundScript.ts",
 		),
 
 		// The Content script
-		"content/ContentScript": path.join(packageSourcePath, "content", "ContentScript.ts"),
+		"content/ContentScript": path.join(sourceDirectoryPath, "content", "ContentScript.ts"),
 
 		// The action button pop-up script
-		"popup/PopupScript": path.join(packageSourcePath, "popup", "PopupScript.ts"),
+		"popup/PopupScript": path.join(sourceDirectoryPath, "popup", "PopupScript.ts"),
 	},
 	output: {
-		path: packageBuildPath,
+		path: buildDirectoryPath,
 		filename: "[name].js",
 		publicPath: "",
 	},
