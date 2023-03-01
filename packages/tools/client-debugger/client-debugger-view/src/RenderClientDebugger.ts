17b8f4453247c686af3ac5dd06d71ef54e92cbe1
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-03-01 23:01:36 +0000

    docs: Add note to function comment

--- packages/tools/client-debugger/client-debugger-view/src/RenderClientDebugger.ts
+++ packages/tools/client-debugger/client-debugger-view/src/RenderClientDebugger.ts
@@ -11,6 +11,11 @@ import { FluidClientDebuggers } from "./Debugger";
  * Renders Fluid client debug view by appending it to the provided DOM element.
  *
  * @param targetElement - The HTML element takes the client debugger view.
+ * 
+ * @remarks
+ * 
+ * Note: this should only be called once for the lifetime of the `targetElement`.
+ * Subsequent calls will result in undesired behavior.
  *
  * @returns A promise that resolves once the debugger view has been rendered for the first time.
  * If rendering fails for any reason, the promise will be rejected.
