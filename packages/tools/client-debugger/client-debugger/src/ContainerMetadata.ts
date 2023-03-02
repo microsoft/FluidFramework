2c43024d6113e9b20fb00cdaca39e7f02f518022
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-03-01 17:44:48 -0800

    feat(client-debugger): Add Chrome devtools extension prototype (#14322)
    Updates chrome-extension library to be a devtools extension. Also
    updates the core debugger package to communicate Container summary
    details via message passing (+ related infrastructure), and updates
    visuals library to make some components reusable across direct-Container
    (i.e. the simple React library model) and extension scenarios.
    
    Future work will add message-passing capabilities for other Container
    state / data.
    
    ### Notes for reviewers:
    
    This PR touched a lot of files, but the primary changes here are:
    1. The overhaul of the `client-debugger-chrome-extension` package to
    include multiple extension prototype variants for consideration (see the
    package README for details on the variants).
    2. The message-passing-based eventing added to `Registry.ts` and
    `FluidClientDebugger.ts` (+ related infrastructure)
    
    #### Other notes
    
    1. The Chrome extension path is pretty orthogonal to the existing React
    library path we took for the early prototype. Both views want to
    visualize (mostly) the same data, but they have to access it in
    different ways. I have attempted to reuse components from the existing
    `client-debugger-view` package in the chrome extension logic as much as
    was reasonable, but there is still some code that will appear to be
    duplicated between the packages. Once we have a definitive pathway for
    our MVP, we can likely begin removing code from the `view` package (or
    possibly even remove that package altogether, and move the needed pieces
    into the extension package).
    
    3. I tried to document the message passing contracts on `Registry` and
    `FluidClientDebugger`, but since none of the message-passing aspects
    appear on the API surface, I don't know how useful / discoverable they
    are. Definitely interested in feedback / suggestions related to
    surfacing this better.
    - Suggestions will likely become follow-up work items to be addressed in
    the future, since this is still an early prototype, but I am definitely
    interested in any feedback folks have now :)
    
    ---------
    
    Co-authored-by: Alex Villarreal <716334+alexvy86@users.noreply.github.com>

--- packages/tools/client-debugger/client-debugger/src/ContainerMetadata.ts
+++ packages/tools/client-debugger/client-debugger/src/ContainerMetadata.ts
@@ -3,6 +3,9 @@
  * Licensed under the MIT License.
  */
 
+import { AttachState } from "@fluidframework/container-definitions";
+import { ConnectionState } from "@fluidframework/container-loader";
+
 /**
  * Metadata describing a {@link @fluidframework/container-definitions#IContainer} registered with a debugger.
  *
@@ -26,4 +29,38 @@ export interface ContainerMetadata {
 	 * container instances.
 	 */
 	nickname?: string;
+}
+
+/**
+ * Metadata describing a {@link @fluidframework/container-definitions#IContainer}'s core state.
+ *
+ * @public
+ */
+export interface ContainerStateMetadata extends ContainerMetadata {
+	/**
+	 * Whether or not the Container has been closed (disposed).
+	 */
+	closed: boolean;
+
+	/**
+	 * {@inheritDoc @fluidframework/container-definitions#IContainer.attachState}
+	 */
+	attachState: AttachState;
+
+	/**
+	 * {@inheritDoc @fluidframework/container-definitions#IContainer.connectionState}
+	 */
+	connectionState: ConnectionState;
+
+	/**
+	 * {@inheritDoc @fluidframework/container-definitions#IContainer.clientId}
+	 */
+	clientId?: string;
+
+	/**
+	 * The active audience identifier when the Container is connected.
+	 *
+	 * @remarks Will be undefined when the Container is not connected.
+	 */
+	audienceId?: string;
 }
