9abf95626eb6a81a9ad95d58e54ed6f3a88ee942
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-03-10 10:47:30 -0600

    WIP

--- packages/tools/client-debugger/client-debugger-view/src/components/MainView.tsx
+++ packages/tools/client-debugger/client-debugger-view/src/components/MainView.tsx
@@ -8,8 +8,9 @@ import {
 	IStackStyles,
 	IStackTokens,
 	Stack,
+	StackItem,
 } from "@fluentui/react";
-import { SelectTabData, SelectTabEvent, Tab, TabList, TabValue } from "@fluentui/react-tabs";
+import { ContainerMetadata } from "@fluid-tools/client-debugger";
 import React from "react";
 
 import { initializeFluentUiIcons } from "../InitializeIcons";
@@ -38,59 +39,49 @@ export const clientDebugViewClassName = `fluid-client-debugger-view`;
  * @internal
  */
 export function MainView(): React.ReactElement {
-	const [selectedValue, setSelectedValue] = React.useState<TabValue>("conditions");
+	const [clickedOption, setClickedOption] = React.useState<string>("");
+	const [rightView, setRightView] = React.useState<React.ReactElement>();
 
-	const onTabSelect = (event: SelectTabEvent, data: SelectTabData): void => {
-		setSelectedValue(data.value);
+	const onTelemetryClicked = (): void => {
+		setClickedOption("telemetry");
 	};
 
-	// eslint-disable-next-line prefer-arrow-callback
-	const Arrivals = React.memo(function Arrivals() {
-		return (
-			<div role="tabpanel" aria-labelledby="Arrivals">
-				<table>
-					<thead>
-						<th>Origin</th>
-						<th>Gate</th>
-						<th>ETA</th>
-					</thead>
-					<tbody>
-						<tr>
-							<td>DEN</td>
-							<td>C3</td>
-							<td>12:40 PM</td>
-						</tr>
-						<tr>
-							<td>SMF</td>
-							<td>D1</td>
-							<td>1:18 PM</td>
-						</tr>
-						<tr>
-							<td>SFO</td>
-							<td>E18</td>
-							<td>1:42 PM</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		);
-	});
+	const onAudienceClicked = (): void => {
+		setClickedOption("audience");
+	};
 
+	const onContainerClicked = (containerId: string): void => {
+		setClickedOption(containerId);
+	};
+
+	React.useEffect(() => {
+		if (clickedOption === "telemetry") {
+			setRightView(<TelemetryView />);
+		} else if (clickedOption === "audience") {
+			setRightView(<div>Audience view goes here</div>);
+		} else if (clickedOption.startsWith("container:")) {
+			setRightView(<div>View for container {clickedOption}</div>);
+		}
+	}, [clickedOption, setClickedOption]);
 	// Styles definition
 	const stackStyles: IStackStyles = {
 		root: {
 			background: DefaultPalette.themeTertiary,
-			height: 500
+			height: 500,
 		},
 	};
-	const stackItemStyles: IStackItemStyles = {
+	const contentViewStyles: IStackItemStyles = {
 		root: {
 			alignItems: "center",
-			background: DefaultPalette.themePrimary,
+			background: DefaultPalette.themeLight,
 			color: DefaultPalette.white,
 			display: "flex",
 			justifyContent: "center",
 		},
+	};
+
+	const menuStyles: IStackItemStyles = {
+		root: { ...contentViewStyles, "display": "flex", "flex-direction": "column" },
 	};
 
 	// Tokens definition
@@ -99,28 +90,106 @@ export function MainView(): React.ReactElement {
 		padding: 10,
 	};
 
+	const menuSectionStyles: IStackStyles = {
+		root: {
+			background: DefaultPalette.themeLight,
+			border: `1px 1px 0px 1px solid ${DefaultPalette.themePrimary}`,
+			padding: "3px",
+		},
+	};
+	const menuSectionHeaderStyles: IStackStyles = {
+		root: {
+			border: `1px solid ${DefaultPalette.themePrimary}`,
+			background: DefaultPalette.themeLighterAlt,
+			fontWeight: "bold",
+			paddingLeft: "2px",
+		},
+	};
+	const menuSectionItemStyles: IStackStyles = {
+		root: {
+			paddingLeft: "20px",
+			cursor: "pointer",
+		},
+	};
+
 	return (
 		<Stack enableScopedSelectors horizontal styles={stackStyles} tokens={stackTokens}>
-			<Stack.Item styles={stackItemStyles}>
-				<TabList vertical selectedValue={selectedValue} onTabSelect={onTabSelect}>
-					<Tab id="Arrivals" value="arrivals">
-						Arrivals
-					</Tab>
-					<Tab id="Audience" value="audience">
-						Audience
-					</Tab>
-					<Tab id="Telemetry" value="telemetry">
-						Telemetry
-					</Tab>
-				</TabList>
+			<Stack.Item grow={1} styles={menuStyles}>
+				<Stack styles={menuSectionStyles}>
+					<Stack.Item styles={menuSectionHeaderStyles}>Containers</Stack.Item>
+					<ContainersList
+						options={[
+							{ id: "1", nickname: "Container 1" },
+							{ id: "2", nickname: "Container 2" },
+						]}
+						onClickHandler={onContainerClicked}
+						styles={menuSectionItemStyles}
+					/>
+				</Stack>
+				<Stack styles={menuSectionStyles}>
+					<Stack.Item styles={menuSectionHeaderStyles}>Audience</Stack.Item>
+					<Stack.Item styles={menuSectionItemStyles} onClick={onAudienceClicked}>
+						See Audience
+					</Stack.Item>
+				</Stack>
+				<Stack styles={menuSectionStyles}>
+					<Stack.Item styles={menuSectionHeaderStyles}>Telemetry</Stack.Item>
+					<Stack.Item styles={menuSectionItemStyles} onClick={onTelemetryClicked}>
+						See Telemetry
+					</Stack.Item>
+				</Stack>
 			</Stack.Item>
-			<Stack.Item grow={2} styles={stackItemStyles}>
-				<div style={{ width: "100%", height: "100%", overflowY: "auto" }}>
-					{selectedValue === "arrivals" && <Arrivals />}
-					{/* {selectedValue === "audience" && <AudienceView />} */}
-					{selectedValue === "telemetry" && <TelemetryView />}
-				</div>
+			<Stack.Item grow={5} styles={contentViewStyles}>
+				<div style={{ width: "100%", height: "100%", overflowY: "auto" }}>{rightView}</div>
 			</Stack.Item>
 		</Stack>
+	);
+}
+
+/**
+ * {@link ContainersList} input props.
+ *
+ * @internal
+ */
+interface ContainersListProps {
+	/**
+	 * Full list of drop-down options.
+	 */
+	options: ContainerMetadata[];
+
+	/**
+	 * Called when the an option is clicked.
+	 * @param containerId - The Container ID of the container that was clicked.
+	 */
+	onClickHandler(containerId: string): void;
+
+	/**
+	 * Styles for each item
+	 */
+	styles: IStackItemStyles;
+}
+
+/**
+ * A list of Fluid Containers to display in the menu.
+ *
+ * @internal
+ */
+function ContainersList(props: ContainersListProps): React.ReactElement {
+	const { options, onClickHandler, styles } = props;
+
+	return (
+		<>
+			{options.map((o) => (
+				<StackItem
+					key={o.id}
+					onClick={(event): void => {
+						onClickHandler(`container:${o.id}`);
+					}}
+					styles={styles}
+				>
+					{o.nickname ?? o.id}
+				</StackItem>
+			))}
+		</>
 	);
 }
