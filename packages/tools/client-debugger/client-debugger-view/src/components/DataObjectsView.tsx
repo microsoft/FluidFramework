e6c8fabd821626c6f7beb31169b9b9d4af7a2567
Josmithr <josmithr@microsoft.com>
2023-03-14 11:37:42 -0700

    feat [WIP]: Prototype summary viewer

--- packages/tools/client-debugger/client-debugger-view/src/components/DataObjectsView.tsx
+++ packages/tools/client-debugger/client-debugger-view/src/components/DataObjectsView.tsx
@@ -2,11 +2,18 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+import { IconButton, Stack, StackItem, TooltipHost } from "@fluentui/react";
+import { useId } from "@fluentui/react-hooks";
+import { ISummaryTree } from "@fluidframework/protocol-definitions";
 import React from "react";
 
 import { HasClientDebugger } from "../CommonProps";
+import { initializeFluentUiIcons } from "../InitializeIcons";
 import { SharedObjectRenderOptions } from "../RendererOptions";
-import { DynamicDataView } from "./data-object-views";
+import { SummaryTreeView } from "./data-object-views";
+
+// Initialize icons if they have not yet been initialized.
+initializeFluentUiIcons();
 
 /**
  * {@link DataObjectsView} input props.
@@ -26,17 +33,40 @@ export interface DataObjectsViewProps extends HasClientDebugger {
  * Dispatches data object rendering based on those provided view {@link DataObjectsViewProps.renderOptions}.
  */
 export function DataObjectsView(props: DataObjectsViewProps): React.ReactElement {
-	const { clientDebugger, renderOptions } = props;
+	const { clientDebugger } = props;
+	const { container } = clientDebugger;
 
-	const { containerData } = clientDebugger;
+	const [summary, setSummary] = React.useState<ISummaryTree | undefined>(undefined);
+
+	React.useEffect(() => {
+		if (container._createSummary !== undefined) {
+			const _summary = container._createSummary();
+			setSummary(_summary);
+		}
+	}, [container]);
+
+	const refreshButtonTooltipId = useId("refresh-button-tooltip");
 
 	return (
 		<div className="data-objects-view">
-			<h3>Container Data</h3>
-			{containerData === undefined ? (
-				<div>No Container data provided at debugger initialization.</div>
+			{summary === undefined ? (
+				<div>Couldn&apos;t generate summary</div>
 			) : (
-				<DynamicDataView data={containerData} renderOptions={renderOptions} />
+				<Stack>
+					<Stack horizontal>
+						<StackItem>
+							<h3>Container Data</h3>
+						</StackItem>
+						<StackItem>
+							<TooltipHost content="Refresh Data" id={refreshButtonTooltipId}>
+								<IconButton
+									onClick={(): void => setSummary(container._createSummary?.())}
+								></IconButton>
+							</TooltipHost>
+						</StackItem>
+					</Stack>
+					<SummaryTreeView summary={summary} />
+				</Stack>
 			)}
 		</div>
 	);
--- packages/tools/client-debugger/client-debugger-view/src/components/data-object-views/SummaryTreeView.tsx
+++ packages/tools/client-debugger/client-debugger-view/src/components/data-object-views/SummaryTreeView.tsx
@@ -3,21 +3,36 @@
  * Licensed under the MIT License.
  */
 
+import { Stack, StackItem } from "@fluentui/react";
 import React from "react";
 
-import { ISummaryTree } from "@fluidframework/protocol-definitions";
+import {
+	ISummaryAttachment,
+	ISummaryBlob,
+	ISummaryHandle,
+	ISummaryTree,
+	SummaryObject,
+	SummaryType,
+} from "@fluidframework/protocol-definitions";
 
-import { DynamicDataView } from "./DynamicDataView";
+import { Accordion } from "../utility-components";
+import { RecordDataView } from "./RecordView";
+
+/**
+ * Base props interface for {@link @fluidframework/protocol-definitions#SummaryObject} data visualization components.
+ */
+export interface SummaryPropsBase<TSummaryObject extends SummaryObject> {
+	/**
+	 * Fluid summary data.
+	 */
+	summary: TSummaryObject;
+}
 
 /**
  * {@link SummaryTreeView} input props.
  */
-export interface SummaryTreeViewProps {
-	/**
-	 * Container data summary.
-	 */
-	summary: ISummaryTree;
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SummaryTreeViewProps extends SummaryPropsBase<ISummaryTree> {}
 
 /**
  * Renders a tree-like visualization of the provided Container data summary.
@@ -25,6 +40,101 @@ export interface SummaryTreeViewProps {
 export function SummaryTreeView(props: SummaryTreeViewProps): React.ReactElement {
 	const { summary } = props;
 
-	// TODO: Something better
-	return <DynamicDataView data={summary} renderOptions={{}} />;
+	const children = Object.entries(summary.tree);
+
+	if (children.length === 0) {
+		return <></>;
+	}
+
+	const renderedChildren = children.map(([key, value]) => {
+		return (
+			<StackItem key={key}>
+				<Accordion header={key}>
+					<SummaryObjectView summary={value} />
+				</Accordion>
+			</StackItem>
+		);
+	});
+
+	return <Stack>{renderedChildren}</Stack>;
+}
+
+/**
+ * {@link SummaryObjectView} input props.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SummaryObjectViewProps extends SummaryPropsBase<SummaryObject> {}
+
+/**
+ * Visualization handler for general {@link @fluidframework/protocol-definitions#SummaryObject}s.
+ */
+export function SummaryObjectView(props: SummaryObjectViewProps): React.ReactElement {
+	const { summary } = props;
+	switch (summary.type) {
+		case SummaryType.Attachment:
+			return <SummaryAttachmentView summary={summary} />;
+		case SummaryType.Blob:
+			return <SummaryBlobView summary={summary} />;
+		case SummaryType.Handle:
+			return <SummaryHandleView summary={summary} />;
+		case SummaryType.Tree:
+			return <SummaryTreeView summary={summary} />;
+		default:
+			throw new Error("Unrecognized SummaryObject type.");
+	}
+}
+
+/**
+ * {@link SummaryBlobView} input props.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SummaryBlobViewProps extends SummaryPropsBase<ISummaryBlob> {}
+
+/**
+ * Visualization handler for {@link @fluidframework/protocol-definitions#SummaryBlob}s.
+ */
+export function SummaryBlobView(props: SummaryBlobViewProps): React.ReactElement {
+	const { summary } = props;
+
+	if (typeof summary.content === "string") {
+		const parsedContent = JSON.parse(summary.content) as Record<
+			string | number | symbol,
+			unknown
+		>;
+		return <RecordDataView data={parsedContent} renderOptions={{}} />;
+	} else {
+		// Otherwise, content is a binary blob, which we can just dump.
+		// Likely not particularly useful, but probably better than not displaying anything?
+		return <div>{summary.content}</div>;
+	}
+}
+
+/**
+ * {@link SummaryAttachmentView} input props.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SummaryAttachmentViewProps extends SummaryPropsBase<ISummaryAttachment> {}
+
+/**
+ * Visualization handler for {@link @fluidframework/protocol-definitions#SummaryAttachment}s.
+ */
+export function SummaryAttachmentView(props: SummaryAttachmentViewProps): React.ReactElement {
+	const { summary } = props;
+	return <div>{summary.id}</div>;
+}
+
+/**
+ * {@link SummaryHandleView} input props.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SummaryHandleViewProps extends SummaryPropsBase<ISummaryHandle> {}
+
+/**
+ * Visualization handler for {@link @fluidframework/protocol-definitions#SummaryAttachment}s.
+ */
+export function SummaryHandleView(props: SummaryHandleViewProps): React.ReactElement {
+	const { summary } = props;
+
+	// TODO: link to the data location in the tree.
+	return <div>{summary.handle}</div>;
 }
