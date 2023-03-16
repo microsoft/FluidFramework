f99e17779a1f6ca47d35e12040650a5a01df738b
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2023-03-15 19:21:13 -0700

    cleanup

--- tools/benchmark/src/runBenchmark.ts
+++ tools/benchmark/src/runBenchmark.ts
@@ -3,8 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import _ from "lodash";
-import { Benchmark, BenchmarkData, Options, Stats } from "./benchmark";
+import { BenchmarkData, Stats } from "./benchmark";
 import {
 	validateBenchmarkArguments,
 	BenchmarkRunningOptions,
@@ -12,79 +11,6 @@ import {
 	BenchmarkRunningOptionsAsync,
 } from "./Configuration";
 import { getArrayStatistics } from "./ReporterUtilities";
-
-/**
- * Run a performance benchmark and return its results.
- *
- * Here is how benchmarking works:
- *
- * ```
- *  For each benchmark
- *      For each sampled run
- *          // Run fn once to check for errors
- *          fn()
- *          // Run fn multiple times and measure results.
- *          for each Benchmark.count
- *              fn()
- * ```
- *
- * For the first few sampled runs, the benchmarking library is in an analysis phase. It uses these sample runs to
- * determine an iteration number that his at most 1% statistical uncertainty. It does this by incrementally increasing
- * the iterations until it hits a low uncertainty point.
- *
- * Optionally, setup and teardown functions can be provided via the `before` and `after` options.
- *
- * @public
- */
-export async function runBenchmarkLegacy(args: BenchmarkRunningOptions): Promise<BenchmarkData> {
-	const options = {
-		...defaults,
-		...args,
-	};
-	const { isAsync, benchmarkFn: argsBenchmarkFn } = validateBenchmarkArguments(args);
-
-	await options.before?.();
-
-	const benchmarkFunction: (deferred: { resolve: Mocha.Done }) => void | Promise<unknown> =
-		isAsync
-			? async (deferred: { resolve: Mocha.Done }) => {
-					// We have to do a little translation because the Benchmark library expects callback-based asynchronicity.
-					await argsBenchmarkFn();
-					deferred.resolve();
-			  }
-			: argsBenchmarkFn;
-
-	return new Promise<BenchmarkData>((resolve) => {
-		const benchmarkOptions: Options = {
-			maxTime: options.maxBenchmarkDurationSeconds,
-			minSamples: options.minSampleCount,
-			minTime: options.minSampleDurationSeconds,
-			defer: isAsync,
-			onCycle: options.onCycle,
-			onComplete: async () => {
-				const stats: BenchmarkData = {
-					aborted: benchmarkInstance.aborted,
-					count: benchmarkInstance.count,
-					cycles: benchmarkInstance.cycles,
-					error: benchmarkInstance.error,
-					hz: benchmarkInstance.hz,
-					stats: benchmarkInstance.stats,
-					times: benchmarkInstance.times,
-				};
-				await options.after?.();
-				resolve(stats);
-			},
-			fn: benchmarkFunction,
-		};
-
-		const benchmarkInstance = new Benchmark(benchmarkOptions);
-		// Run a garbage collection, if possible, before the test.
-		// This helps noise from allocations before the test (ex: from previous tests or startup) from
-		// impacting the test.
-		global?.gc?.();
-		benchmarkInstance.run();
-	});
-}
 
 export async function runBenchmark(args: BenchmarkRunningOptions): Promise<BenchmarkData> {
 	const options = {
@@ -133,7 +59,7 @@ export async function runBenchmark(args: BenchmarkRunningOptions): Promise<Bench
  * @public
  */
 export function runBenchmarkSync(args: BenchmarkRunningOptionsSync): BenchmarkData {
-	const timeStamp = +_.now();
+	const timeStamp = timer.now();
 
 	const options = {
 		...defaults,
@@ -215,7 +141,7 @@ function doBatch(
 export async function runBenchmarkAsync(
 	args: BenchmarkRunningOptionsAsync,
 ): Promise<BenchmarkData> {
-	const timeStamp = +_.now();
+	const timeStamp = timer.now();
 
 	const options = {
 		...defaults,
@@ -273,16 +199,16 @@ async function doBatchAsync(
 	return timer.toSeconds(before, after);
 }
 
-function computeData(samples: number[], count: number, timeStamp: number): BenchmarkData {
-	const now = +_.now();
+function computeData(samples: number[], count: number, timeStamp: unknown): BenchmarkData {
+	const now = timer.now();
 	const stats: Stats = getArrayStatistics(samples.map((v) => v / count));
 	const data: BenchmarkData = {
 		hz: 1 / stats.mean,
 		times: {
 			cycle: stats.mean * count,
 			period: stats.mean,
-			elapsed: (now - timeStamp) / 1e3,
-			timeStamp,
+			elapsed: timer.toSeconds(timeStamp, now),
+			timeStamp: Number(timeStamp),
 		},
 		aborted: false,
 		cycles: samples.length,
