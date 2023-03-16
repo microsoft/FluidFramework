591f718111b68c87c266f215d413efb48a9cf95e
Jenn <jenn.thakugan@icloud.com>
2023-03-16 22:33:00 +0000

    Clean up

---
+++ packages/dds/tree/src/core/internalDocumentationIndex.ts
@@ -0,0 +1,21 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * This file is meant to ensure that all public members have corresponding documentation published.
+ * This includes code that is not normally package-exported.
+ */
+/* eslint-disable no-restricted-syntax */
+export * from "./change-family";
+export * from "./checkout";
+export * from "./dependency-tracking";
+export * from "./edit-manager";
+export * from "./forest";
+export * from "./rebase";
+export * from "./repair";
+export * from "./schema-stored";
+export * from "./schema-view";
+export * from "./transaction";
+export * from "./tree";
