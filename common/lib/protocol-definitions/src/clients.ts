93ae0b225f97a853611c4f6ac25968a7d76e92a9
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-03-17 23:35:35 +0000

    docs: Use TSDoc syntax

--- common/lib/protocol-definitions/src/clients.ts
+++ common/lib/protocol-definitions/src/clients.ts
@@ -73,9 +73,13 @@ export interface ISignalClient {
  * Contents sent with a ClientJoin message
  */
 export interface IClientJoin {
-	// The ID of the joining client
+	/**
+	 * The ID of the joining client.
+	 */
 	clientId: string;
 
-	// Details about the joining client (i.e. browser based, server, CPU, memory, etc...)
+	/**
+	 * Details about the joining client (i.e. browser based, server, CPU, memory, etc...).
+	 */
 	detail: IClient;
 }
