a72b1da97f4d39753b0645627f0779daf9c60e8d
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-03-17 21:53:39 +0000

    fix: Argument handling and restore working directory arg support

--- tools/markdown-magic/src/index.js
+++ tools/markdown-magic/src/index.js
@@ -5,6 +5,7 @@
 
 const chalk = require("chalk");
 const markdownMagic = require("@tylerbu/markdown-magic");
+const path = require("path");
 const process = require("process");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
@@ -16,15 +17,22 @@ const defaultMatchPattern = "**/*.md";
 const argv = yargs(hideBin(process.argv))
 	.usage("Usage: $0 [options]")
 	.alias("f", "files")
-	.describe("f", "Glob pattern(s) indicating the files to process.")
+	.describe("f", `Glob pattern(s) indicating the files to process. Default: "${defaultMatchPattern}".`)
 	.example("$0 -f docs/**/*.md !docs/README.md", "Run on all Markdown files under 'docs', except 'README.md'.")
-	.default("f", defaultMatchPattern)
+	.alias("w", "workingDirectory")
+	.describe("w", "The working directory in which to run the script. Default: the current Node.js working directory.")
 	.help("h")
 	.alias("h", "--help").argv;
 
 const matchPattern = argv.files ?? defaultMatchPattern;
 
-console.log(`Searching for files matching pattern "${matchPattern}" under ${process.cwd()}...`);
+let workingDirectory = process.cwd();
+if (argv.workingDirectory) {
+	workingDirectory = path.resolve(argv.workingDirectory);
+	process.chdir(workingDirectory);
+}
+
+console.log(`Searching for files matching pattern "${matchPattern}" under "${workingDirectory}"...`);
 
 markdownMagic(matchPattern, config).then(
 	() => {
