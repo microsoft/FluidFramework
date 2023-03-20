e18044f6cecfa81ad6f8a8f92cb94dd9430285cd
justus-camp <102552460+justus-camp@users.noreply.github.com>
2023-03-20 17:21:56 +0000

    Copyright header

--- experimental/examples/bubblebench/tree/src/cursor/tree-utils/index.ts
+++ experimental/examples/bubblebench/tree/src/cursor/tree-utils/index.ts
@@ -1,2 +1,6 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
 export { SharedTreeNodeHelper } from "./sharedTreeNodeHelper";
 export { SharedTreeSequenceHelper } from "./sharedTreeSequenceHelper";
