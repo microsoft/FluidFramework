a5b5ba663e5191d1b6ad4bdc0f42a92a04e56fec
Jenn <jenn.thakugan@icloud.com>
2023-03-21 18:23:27 +0000

    Fix

--- packages/dds/tree/src/core/internalDocumentationIndex.ts
+++ packages/dds/tree/src/core/internalDocumentationIndex.ts
@@ -9,7 +9,6 @@
  */
 /* eslint-disable no-restricted-syntax */
 export * from "./change-family";
-export * from "./checkout";
 export * from "./dependency-tracking";
 export * from "./edit-manager";
 export * from "./forest";
@@ -17,5 +16,4 @@ export * from "./rebase";
 export * from "./repair";
 export * from "./schema-stored";
 export * from "./schema-view";
-export * from "./transaction";
 export * from "./tree";
