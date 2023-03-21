ccc440e4c6d78be309c30162da5628713105b054
Tyler Butler <tyler@tylerbutler.com>
2023-03-21 16:57:35 -0700

    update

--- scripts/set-package-version.sh
+++ scripts/set-package-version.sh
@@ -1,11 +1,15 @@
-echo SETVERSION_VERSION=$SETVERSION_VERSION
-if [ -f "lerna.json" ]; then
-  if [ "$VERSION_RELEASE" = "release" ]; then
-    # no need to run anything here, as the version in the package should be correct
-    npx lerna exec "if [ \`npm -s run env echo \"\$npm_package_version\"\` != \"$SETVERSION_VERSION\" ]; then ( exit 1 ) fi"
-    exit $?
-  fi
-  npx lerna version $SETVERSION_VERSION --no-git-tag-version --no-push --yes --exact
-else
-  npm version $SETVERSION_VERSION --no-git-tag-version -f --allow-same-version
+# pwd
+# echo SETVERSION_VERSION=$SETVERSION_VERSION
+# echo npm_package_version=$npm_package_version
+# node -p "require('./package.json').version"
+# pkg_version=$(npm -s run env echo "$npm_package_version")
+pkg_version=$(node -p "require('./package.json').version")
+# echo pkg_version=$pkg_version
+
+if [ "$pkg_version" != "$SETVERSION_VERSION" ]; then
+  exit 1
 fi
+
+exit 0
+
+# echo END $(pwd)
