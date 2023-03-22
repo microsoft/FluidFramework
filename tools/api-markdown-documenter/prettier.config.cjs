32296846212e5d6d5ed90805d94c7c01b18c7f2f
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-03-22 22:57:04 +0000

    remove: Obsolete prettier config settings

--- tools/api-markdown-documenter/prettier.config.cjs
+++ tools/api-markdown-documenter/prettier.config.cjs
@@ -5,11 +5,4 @@
 
 module.exports = {
 	...require("@fluidframework/build-common/prettier.config.cjs"),
-	importOrder: [
-		"^node:(.*)$", // Special-case `node:` imports
-		"<THIRD_PARTY_MODULES>",
-		"^[./]",
-	],
-	importOrderSeparation: true,
-	importOrderSortSpecifiers: true,
 };
