321383772b49f0b40979d05124934f321e2bdbfa
Tyler Butler <tyler@tylerbutler.com>
2023-03-21 17:31:02 -0700

    cleanup

--- scripts/set-package-version.sh
+++ scripts/set-package-version.sh
@@ -1,15 +1,9 @@
-# pwd
-# echo SETVERSION_VERSION=$SETVERSION_VERSION
-# echo npm_package_version=$npm_package_version
-# node -p "require('./package.json').version"
-# pkg_version=$(npm -s run env echo "$npm_package_version")
-pkg_version=$(node -p "require('./package.json').version")
-# echo pkg_version=$pkg_version
+# This script is used in CI to compare the version in a package's package.json to the expected version we're releasing,
+# which is ser in the SETVERSION_VERSION environment variable.
 
+pkg_version=$(node -p "require('./package.json').version")
 if [ "$pkg_version" != "$SETVERSION_VERSION" ]; then
   exit 1
 fi
 
 exit 0
-
-# echo END $(pwd)
