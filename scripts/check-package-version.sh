dec0802c7d834455427b8c839d0d1b49399f48a1
Tyler Butler <tyler@tylerbutler.com>
2023-03-21 17:43:26 -0700

    typo

--- scripts/check-package-version.sh
+++ scripts/check-package-version.sh
@@ -1,5 +1,5 @@
 # This script is used in CI to compare the version in a package's package.json to the expected version we're releasing,
-# which is ser in the SETVERSION_VERSION environment variable.
+# which is set in the SETVERSION_VERSION environment variable.
 
 pkg_version=$(node -p "require('./package.json').version")
 if [ "$pkg_version" != "$SETVERSION_VERSION" ]; then
