9f6c7eeb78c8f85dca70d719000d856287063e1c
kkachmar <kkachmar@microsoft.com>
2023-03-23 17:59:29 -0400

    kafka partition/offset logs

--- server/routerlicious/packages/lambdas-driver/src/kafka-service/partitionManager.ts
+++ server/routerlicious/packages/lambdas-driver/src/kafka-service/partitionManager.ts
@@ -37,6 +37,12 @@ export class PartitionManager extends EventEmitter {
 	) {
 		super();
 
+        this.consumer.on("connected", (partitionId) => {
+            Lumberjack.info(`Connected successfully`, {
+                partitionId
+            });
+        });
+
 		// Place new Kafka messages into our processing queue
 		this.consumer.on("data", (message) => {
 			this.process(message);
