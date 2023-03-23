1d8621ea74d6366d3ff4b11a3db075e91a794eef
Nicholas Couri <nichoc@microsoft.com>
2023-03-23 15:37:40 -0700

    update comment

--- packages/test/mocha-test-setup/src/mochaHooks.ts
+++ packages/test/mocha-test-setup/src/mochaHooks.ts
@@ -19,7 +19,7 @@ class TestLogger implements ITelemetryBufferedLogger {
 			return;
 		}
 
-		// This test logger is currently only instantiated once and, in case the testName is undefined, we will make use of the currentTestName.
+		// The test logger is currently instantiated once and for each event triggered between begin and end of a test, in case the testName is undefined, we will use of the currentTestName.
 		event.testName = this.testName ?? currentTestName;
 		event.testVariant = testVariant;
 		event.hostName = pkgName;
