0faa752353b281b59fb2d7573547e3fcd581d387
Tyler Butler <tyler@tylerbutler.com>
2023-03-27 15:46:41 -0700

    formatting

--- fluidBuild.config.cjs
+++ fluidBuild.config.cjs
@@ -85,7 +85,7 @@ module.exports = {
 			"@fluidframework/eslint-config-fluid",
 			"@fluidframework/protocol-definitions",
 			"@fluidframework/test-tools",
-      "tinylicious",
+			"tinylicious",
 		],
 	},
 
