60217e912cd06734a351e196e94a47adb5fd56ac
Navin Agarwal <45832642+agarwal-navin@users.noreply.github.com>
2023-03-27 11:02:44 -0700

    [Port v2int/3.3] Remove SweepReadyUsageDetectionHandler in garbage collector (#14780)
    Port from v2int/3.0 -
    https://github.com/microsoft/FluidFramework/pull/14760
    
    SweepReadyUsageDetectionHandler access localStorage which can fail if
    access to localStorage is not allowed. We are seeing this happening in
    production.
    Removing the usage of SweepReadyUsageDetectionHandler in garbage
    collector - It was added by
    https://github.com/microsoft/FluidFramework/pull/12004 but the sweep
    ready detection feature was never enabled.
    
    
    [AB#3793](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/3793)

--- packages/runtime/container-runtime/src/gc/garbageCollection.ts
+++ packages/runtime/container-runtime/src/gc/garbageCollection.ts
@@ -54,7 +54,6 @@ import {
 } from "./gcDefinitions";
 import { getSnapshotDataFromOldSnapshotFormat, sendGCUnexpectedUsageEvent } from "./gcHelpers";
 import { GCSummaryStateTracker } from "./gcSummaryStateTracker";
-import { SweepReadyUsageDetectionHandler } from "./gcSweepReadyUsageDetection";
 import { UnreferencedStateTracker } from "./gcUnreferencedStateTracker";
 
 /** The event that is logged when unreferenced node is used after a certain time. */
@@ -156,9 +155,6 @@ export class GarbageCollector implements IGarbageCollector {
 		return this.summaryStateTracker.doesSummaryStateNeedReset();
 	}
 
-	/** Handler to respond to when a SweepReady object is used */
-	private readonly sweepReadyUsageHandler: SweepReadyUsageDetectionHandler;
-
 	protected constructor(createParams: IGarbageCollectorCreateParams) {
 		this.runtime = createParams.runtime;
 		this.isSummarizerClient = createParams.isSummarizerClient;
@@ -174,12 +170,6 @@ export class GarbageCollector implements IGarbageCollector {
 			ChildLogger.create(createParams.baseLogger, "GarbageCollector", {
 				all: { completedGCRuns: () => this.completedRuns },
 			}),
-		);
-
-		this.sweepReadyUsageHandler = new SweepReadyUsageDetectionHandler(
-			createParams.getContainerDiagnosticId(),
-			this.mc,
-			this.runtime.closeFn,
 		);
 
 		this.configs = generateGCConfigs(this.mc, createParams);
@@ -1206,16 +1196,6 @@ export class GarbageCollector implements IGarbageCollector {
 				} else {
 					this.mc.logger.sendErrorEvent(event);
 				}
-			}
-
-			// If SweepReady Usage Detection is enabled, the handler may close the interactive container.
-			// Once Sweep is fully implemented, this will be removed since the objects will be gone
-			// and errors will arise elsewhere in the runtime
-			if (state === UnreferencedState.SweepReady) {
-				this.sweepReadyUsageHandler.usageDetectedInInteractiveClient({
-					...propsToLog,
-					usageType,
-				});
 			}
 		}
 	}
