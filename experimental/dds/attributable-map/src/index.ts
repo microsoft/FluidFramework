0d0520c8ba3b874acbc22e1be4fb7c0ff5f218f7
Clarence Li <clarenceli@microsoft.com>
2023-03-28 18:43:05 -0400

    Add the original SharedMap to experimental

---
+++ experimental/dds/attributable-map/src/index.ts
@@ -0,0 +1,26 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * The `map` library provides interfaces and implementing classes for map-like distributed data structures.
+ *
+ * @remarks The following distributed data structures are defined in this library:
+ *
+ * - {@link SharedMap}
+ *
+ * - {@link SharedDirectory}
+ *
+ * @packageDocumentation
+ */
+
+export {
+	ISerializableValue,
+	ISerializedValue,
+	ISharedMap,
+	ISharedMapEvents,
+	IValueChanged,
+} from "./interfaces";
+export { LocalValueMaker, ILocalValue } from "./localValues";
+export { MapFactory, SharedMap } from "./map";
