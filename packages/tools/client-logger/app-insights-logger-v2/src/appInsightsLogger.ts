cc05476edee52bb9c8deeff842cdabf054747881
seanimam <105244057+seanimam@users.noreply.github.com>
2023-03-28 15:06:33 +0000

    adds app insights logger with app view test

---
+++ packages/tools/client-logger/app-insights-logger-v2/src/appInsightsLogger.ts
@@ -0,0 +1,47 @@
+import * as appInsights from "applicationinsights";
+
+import { ITelemetryBaseEvent } from "@fluidframework/common-definitions";
+import { ITelemetryBufferedLogger } from "@fluidframework/test-driver-definitions";
+
+export interface AppInsightsLoggerConfig {
+	appInsightsClient?: appInsights.TelemetryClient;
+	connectionString?: string;
+}
+
+export abstract class AppInsightsLogger implements ITelemetryBufferedLogger {
+	protected readonly baseLoggingClient: appInsights.TelemetryClient;
+
+	public constructor(config: AppInsightsLoggerConfig) {
+		if (config.appInsightsClient) {
+			this.baseLoggingClient = config.appInsightsClient;
+		} else {
+			if (config.connectionString === undefined) {
+				throw Error(
+					"Cannot initialize AppInsightsLogger without a connection string if no app insights client is provided.",
+				);
+			}
+			appInsights.setup().start();
+			this.baseLoggingClient = appInsights.defaultClient;
+		}
+	}
+
+	getBaseLoggingClient() {
+		return this.baseLoggingClient;
+	}
+
+	send(event: ITelemetryBaseEvent): void {
+		this.baseLoggingClient.trackEvent({
+			name: event.eventName,
+			properties: event,
+		});
+	}
+
+	async flush(runInfo?: { url: string; runId?: number }): Promise<void> {
+		// await until data is posted to the server.
+		await new Promise<void>((resolve) => {
+			this.baseLoggingClient.flush({
+				callback: () => resolve(),
+			});
+		});
+	}
+}
---
+++ packages/tools/client-logger/app-insights-logger-v2/src/test/widgets/index.ts
@@ -0,0 +1,11 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+// TODO: re-enable rule and fix violations before merging into main
+/* eslint-disable no-restricted-syntax */
+
+export * from "./CounterWidget";
+
+/* eslint-enable no-restricted-syntax */
