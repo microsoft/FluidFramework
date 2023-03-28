5ff4ad82e3fd2065e6079fc13060603e9fe9f611
Nicholas Couri <nichoc@microsoft.com>
2023-03-28 13:05:47 -0700

    reflecting changes from another checkin

--- packages/test/test-end-to-end-tests/src/test/benchmark/summarizationDocument.all.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/benchmark/summarizationDocument.all.spec.ts
@@ -9,6 +9,7 @@ import { describeE2EDocRun, getCurrentBenchmarkType } from "@fluidframework/test
 import {
 	benchmarkAll,
 	createDocument,
+	IBenchmarkParameters,
 	IDocumentLoaderAndSummarizer,
 	ISummarizeResult,
 } from "./DocumentCreator";
@@ -41,30 +42,29 @@ describeE2EDocRun(scenarioTitle, (getTestObjectProvider, getDocumentInfo) => {
 	 * a. Benchmark Time tests: {@link https://benchmarkjs.com/docs#options} or  {@link BenchmarkOptions}
 	 * b. Benchmark Memory tests: {@link MemoryTestObjectProps}
 	 */
-	class PerformanceTestWrapper {
-		container: IContainer | undefined;
-		summarizerClient: ISummarizeResult | undefined;
-		minSampleCount = getDocumentInfo().minSampleCount;
-	}
 
-	const obj = new PerformanceTestWrapper();
+	benchmarkAll(
+		scenarioTitle,
+		new (class PerformanceTestWrapper implements IBenchmarkParameters {
+			container: IContainer | undefined;
+			summarizerClient: ISummarizeResult | undefined;
+			minSampleCount = getDocumentInfo().minSampleCount;
+			async run(): Promise<void> {
+				this.container = await documentWrapper.loadDocument();
+				assert(this.container !== undefined, "container needs to be defined.");
+				await provider.ensureSynchronized();
 
-	benchmarkAll(scenarioTitle, obj, {
-		run: async () => {
-			obj.container = await documentWrapper.loadDocument();
-			assert(obj.container !== undefined, "container needs to be defined.");
-			await provider.ensureSynchronized();
-
-			obj.summarizerClient = await documentWrapper.summarize(summaryVersion);
-			assert(
-				obj.summarizerClient.summaryVersion !== undefined,
-				"summaryVersion needs to be defined.",
-			);
-			summaryVersion = obj.summarizerClient.summaryVersion;
-		},
-		beforeIteration: () => {
-			obj.container = undefined;
-			obj.summarizerClient = undefined;
-		},
-	});
+				this.summarizerClient = await documentWrapper.summarize(summaryVersion);
+				assert(
+					this.summarizerClient.summaryVersion !== undefined,
+					"summaryVersion needs to be defined.",
+				);
+				summaryVersion = this.summarizerClient.summaryVersion;
+			};
+			beforeIteration(): void {
+				this.container = undefined;
+				this.summarizerClient = undefined;
+			}
+		})(),
+	);
 });
