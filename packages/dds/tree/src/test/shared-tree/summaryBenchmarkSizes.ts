0d709bf15749f936d82e378e7e178446e4572425
Andrei Iacob <andreiiacob@microsoft.com>
2023-03-29 14:58:11 -0700

    Increase test timeout

--- packages/dds/tree/src/test/shared-tree/summaryBenchmarkSizes.ts
+++ packages/dds/tree/src/test/shared-tree/summaryBenchmarkSizes.ts
@@ -75,13 +75,13 @@ describe("Summary size benchmark", () => {
 		assert(summarySize < 2000000);
 	});
 	// The test passes, however it needs a longer timeout
-	it.skip("for a deep tree with 1000 nodes.", async () => {
+	it("for a deep tree with 1000 nodes.", async () => {
 		const summaryTree = await getInsertsSummaryTree(1000, TreeShape.Deep);
 		const summaryString = JSON.stringify(summaryTree);
 		const summarySize = IsoBuffer.from(summaryString).byteLength;
 		assert(summarySize > 1000);
 		assert(summarySize < 200000000);
-	});
+	}).timeout(50000);
 });
 
 /**
