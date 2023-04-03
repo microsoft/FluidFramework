9a8a29b149bb5aa87ea68bba7a31af1b8bbb9c0f
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-04-03 14:34:59 -0700

    docs: Fix grammar

--- packages/common/driver-definitions/src/urlResolver.ts
+++ packages/common/driver-definitions/src/urlResolver.ts
@@ -95,7 +95,7 @@ export interface IUrlResolver {
 	 * 
 	 * @privateRemarks
 	 * 
-	 * Like DNS should be able to cache resolution requests. Then possibly just have a token provider go and do stuff?
+	 * Like DNS, this should be able to cache resolution requests. Then possibly just have a token provider go and do stuff?
 	 * the expiration of it could be relative to the lifetime of the token? Requests after need to refresh?
 	 * or do we split the token access from this?
 	 */
