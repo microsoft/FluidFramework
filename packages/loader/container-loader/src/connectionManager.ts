c0f66d672aa93ff8e0848233dccdd83befbed774
Wes Carlson <wesley.carlson@microsoft.com>
2023-04-06 16:55:38 -0700

    or with previous behavior

--- packages/loader/container-loader/src/connectionManager.ts
+++ packages/loader/container-loader/src/connectionManager.ts
@@ -30,6 +30,7 @@ import {
 	createGenericNetworkError,
 	getRetryDelayFromError,
 	logNetworkFailure,
+	isRuntimeMessage,
 } from "@fluidframework/driver-utils";
 import {
 	ConnectionMode,
@@ -194,6 +195,8 @@ export class ConnectionManager implements IConnectionManager {
 
 	private clientSequenceNumber = 0;
 	private clientSequenceNumberObserved = 0;
+	/** Counts the number of non-runtime ops sent by the client which may not be acked. */
+	private localOpsToIgnore = 0;
 
 	/** track clientId used last time when we sent any ops */
 	private lastSubmittedClientId: string | undefined;
@@ -273,6 +276,24 @@ export class ConnectionManager implements IConnectionManager {
 			  };
 	}
 
+	public shouldJoinWrite(): boolean {
+		// We don't have to wait for ack for topmost NoOps. So subtract those.
+		const outstandingOps =
+			this.clientSequenceNumberObserved < this.clientSequenceNumber - this.localOpsToIgnore;
+
+		// Previous behavior was to force write mode here only when there are outstanding ops (besides
+		// no-ops). The dirty signal from runtime should provide the same behavior, but also support
+		// stashed ops that weren't submitted to container layer yet. For safety, we want to retain the
+		// same behavior whenever dirty is false.
+		const isDirty = this.containerDirty();
+		if (outstandingOps !== isDirty) {
+			this.logger.sendTelemetryEvent({
+				eventName: "DesiredConnectionModeMismatch",
+			});
+		}
+		return outstandingOps || isDirty;
+	}
+
 	/**
 	 * Tells if container is in read-only mode.
 	 * Data stores should listen for "readonly" notifications and disallow user
@@ -322,7 +343,7 @@ export class ConnectionManager implements IConnectionManager {
 
 	constructor(
 		private readonly serviceProvider: () => IDocumentService | undefined,
-		public readonly shouldJoinWrite: () => boolean,
+		public readonly containerDirty: () => boolean,
 		private client: IClient,
 		reconnectAllowed: boolean,
 		private readonly logger: ITelemetryLogger,
@@ -945,6 +966,12 @@ export class ConnectionManager implements IConnectionManager {
 			this.lastSubmittedClientId = this.connection?.clientId;
 			this.clientSequenceNumber = 0;
 			this.clientSequenceNumberObserved = 0;
+		}
+
+		if (!isRuntimeMessage(message)) {
+			this.localOpsToIgnore++;
+		} else {
+			this.localOpsToIgnore = 0;
 		}
 
 		return {
