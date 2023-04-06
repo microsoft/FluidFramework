887d78fdb170d1098cf9ea6adc0dde9ec9eca0d1
Yann Achard <achardy@microsoft.com>
2023-04-06 12:33:37 -0700

    Remove cell dependence on modular

--- packages/dds/tree/src/feature-libraries/cell/cell.ts
+++ packages/dds/tree/src/feature-libraries/cell/cell.ts
@@ -3,35 +3,15 @@
  * Licensed under the MIT License.
  */
 
-import { Delta, TaggedChange, tagChange, RevisionTag } from "../../core";
+import { Delta, TaggedChange, tagChange } from "../../core";
 import { Brand, brand, JsonCompatibleReadOnly, Mutable } from "../../util";
-import {
-	ToDelta,
-	FieldChangeRebaser,
-	FieldChangeEncoder,
-	FieldEditor,
-	ChangesetLocalId,
-	CrossFieldManager,
-	CrossFieldTarget,
-} from "../modular-schema";
 import { populateChildModifications } from "../deltaUtils";
 
-export type DetachedRangeId = Brand<number, "DetachedRangeId">;
+export type CellId = Brand<unknown, "CellId">;
 
-export interface Cell<
-	TDeepChange,
-	TEditor extends FieldEditor<TDeepChange> = FieldEditor<TDeepChange>,
-> {
-	rebaser: FieldChangeRebaser<CellChange<TDeepChange>>;
-	encoder: FieldChangeEncoder<CellChange<TDeepChange>>;
-	editor: TEditor;
-	intoDelta(change: CellChange<TDeepChange>, deltaFromChild: ToDelta): Delta.MarkList;
-
-	/**
-	 * Returns whether this change is empty, meaning that it represents no modifications to the field
-	 * and could be removed from the ModularChangeset tree without changing its behavior.
-	 */
-	isEmpty(change: CellChange<TDeepChange>): boolean;
+export interface CrossCellManager<TDeepChange> {
+	send(to: CellId, change: TDeepChange): void;
+	receive(from: CellId): TDeepChange | undefined;
 }
 
 export interface ShallowChange {
@@ -39,13 +19,13 @@ export interface ShallowChange {
 	 * The source for the content that will be in the cell after this change.
 	 * If undefined, the cell will be empty after this change.
 	 */
-	newContentSrc?: DetachedRangeId;
+	newContentSrc?: CellId;
 
 	/**
 	 * The destination for the content that was in the cell before this change.
 	 * If undefined, the cell was be empty before this change.
 	 */
-	oldContentDst?: DetachedRangeId;
+	oldContentDst?: CellId;
 }
 
 export interface CellChange<TDeepChange> {
@@ -133,31 +113,27 @@ export function editCellContent<TDeepChange>(deep: TDeepChange): CellChange<TDee
 	return { deep };
 }
 
-export function clearContent(oldContentDst: DetachedRangeId): CellChange<never> {
+export function clearContent(oldContentDst: CellId): CellChange<never> {
 	return { shallow: { oldContentDst } };
 }
 
-export function setContent(newContentSrc: DetachedRangeId): CellChange<never> {
+export function setContent(newContentSrc: CellId): CellChange<never> {
 	return { shallow: { newContentSrc } };
 }
 
-export function replaceContent(
-	oldContentDst: DetachedRangeId,
-	newContentSrc: DetachedRangeId,
-): CellChange<never> {
+export function replaceContent(oldContentDst: CellId, newContentSrc: CellId): CellChange<never> {
 	return { shallow: { oldContentDst, newContentSrc } };
 }
 
 export function compose<TDeepChange>(
 	changes: TaggedChange<CellChange<TDeepChange>>[],
 	composeDeep: (changes: TaggedChange<TDeepChange>[]) => TDeepChange,
-	genId: () => DetachedRangeId,
-	crossFieldManager: CrossFieldManager,
+	genId: () => CellId,
+	crossCellManager: CrossCellManager<TDeepChange>,
 ): CellChange<TDeepChange> {
 	const composed: CellChange<TDeepChange> = {};
 	let shallow: ShallowChange | undefined;
-	let deepSource: DetachedRangeId | undefined;
-	let deepSourceRevision: RevisionTag | undefined;
+	let deepSource: CellId | undefined;
 	let deepChanges: TaggedChange<TDeepChange>[] = [];
 	for (const { change, revision } of changes) {
 		if (change.deep !== undefined) {
@@ -175,18 +151,10 @@ export function compose<TDeepChange>(
 					// The changes since the last shallow change apply to the content present in the cell after the
 					// last shallow change.
 					// Those changes should be represented in the source cell of the new content.
-					crossFieldManager.getOrCreate(
-						CrossFieldTarget.Destination,
-						deepSourceRevision,
-						// Can we make the type system happy here?
-						deepSource as unknown as ChangesetLocalId,
-						composedDeep,
-						true,
-					);
+					crossCellManager.send(deepSource, composedDeep);
 				}
 				deepChanges = [];
 				deepSource = change.shallow.newContentSrc;
-				deepSourceRevision = revision;
 			}
 			if (shallow === undefined) {
 				shallow = { ...change.shallow };
@@ -209,8 +177,8 @@ export function compose<TDeepChange>(
 export function invert<TDeepChange>(
 	{ change, revision }: TaggedChange<CellChange<TDeepChange>>,
 	invertDeep: (change: TDeepChange) => TDeepChange,
-	genId: () => DetachedRangeId,
-	crossFieldManager: CrossFieldManager,
+	genId: () => CellId,
+	crossCellManager: CrossCellManager<TDeepChange>,
 ): CellChange<TDeepChange> {
 	const inverted: CellChange<TDeepChange> = {};
 	if (change.shallow !== undefined) {
@@ -220,24 +188,13 @@ export function invert<TDeepChange>(
 		};
 		// We need to put on this changeset the inverse of any deep changes that were applied to the source of the
 		// original content.
-		// TODO: implement amendInverse.
-		crossFieldManager.get(
-			CrossFieldTarget.Source,
-			revision,
-			// Can we make the type system happy here?
-			change.shallow.newContentSrc as unknown as ChangesetLocalId,
-			true,
-		);
+		const deep = crossCellManager.receive(change.shallow.newContentSrc);
+		if (deep !== undefined) {
+			inverted.deep = deep;
+		}
 		if (change.deep !== undefined) {
 			// The inverse of the changes to the original content should reside where that content was sent.
-			crossFieldManager.getOrCreate(
-				CrossFieldTarget.Destination,
-				revision,
-				// Can we make the type system happy here?
-				change.shallow.oldContentDst as unknown as ChangesetLocalId,
-				invertDeep(change.deep),
-				true,
-			);
+			crossCellManager.send(change.shallow.oldContentDst, invertDeep(change.deep));
 		}
 	} else if (change.deep !== undefined) {
 		inverted.deep = invertDeep(change.deep);
@@ -252,41 +209,31 @@ export function rebase<TDeepChange>(
 		change: TDeepChange | undefined,
 		baseChange: TDeepChange | undefined,
 	) => TDeepChange | undefined,
-	genId: () => DetachedRangeId,
-	crossFieldManager: CrossFieldManager,
+	genId: () => CellId,
+	crossCellManager: CrossCellManager<TDeepChange>,
 ): CellChange<TDeepChange> {
 	const deep = rebaseDeep(change.deep, over.change.deep);
 	if (over.change.shallow !== undefined) {
 		if (deep !== undefined) {
 			// Since the content has been replaced by the concurrent change, the deep changes to it should
 			// reside at its new location.
-			crossFieldManager.getOrCreate(
-				CrossFieldTarget.Destination,
-				over.revision,
-				// Can we make the type system happy here?
-				over.change.shallow.oldContentDst as unknown as ChangesetLocalId,
-				deep,
-				true,
-			);
+			crossCellManager.send(over.change.shallow.oldContentDst, deep);
 		}
 		if (change.shallow !== undefined) {
 			const shallow: ShallowChange = { ...change.shallow };
+			const rebased: CellChange<TDeepChange> = { shallow };
 			if (over.change.shallow.newContentSrc !== undefined) {
 				shallow.oldContentDst = change.shallow.oldContentDst ?? genId();
 				// Any deep changes (within this revision) that apply to the content put in place by `over` should
 				// be represented as part of the change on this cell.
-				// TODO: implement amendRebase.
-				crossFieldManager.get(
-					CrossFieldTarget.Source,
-					over.revision,
-					// Can we make the type system happy here?
-					over.change.shallow.newContentSrc as unknown as ChangesetLocalId,
-					true,
-				);
+				const newDeep = crossCellManager.receive(over.change.shallow.newContentSrc);
+				if (newDeep !== undefined) {
+					rebased.deep = newDeep;
+				}
 			} else {
 				delete shallow.oldContentDst;
 			}
-			return { shallow };
+			return rebased;
 		}
 		return {};
 	}
