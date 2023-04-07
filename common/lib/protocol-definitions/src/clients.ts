f61ad5d040bd29bd2fe7d37f591f78d2b60a6b0c
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-04-07 20:24:56 +0000

    docs: Update `user` property comment

--- common/lib/protocol-definitions/src/clients.ts
+++ common/lib/protocol-definitions/src/clients.ts
@@ -83,9 +83,7 @@ export interface IClient {
 	permission: string[];
 
 	/**
-	 * The user associated with this client.
-	 *
-	 * @remarks There may be more than 1 client associated with the same user.
+	 * The user information associated with this client connection.
 	 */
 	user: IUser;
 
