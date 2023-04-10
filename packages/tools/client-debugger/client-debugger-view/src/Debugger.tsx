a562dc5e2924a52d4a73c4afe52da65da0d3eff5
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-04-10 21:45:41 +0000

    feat: Wait for "features" handshake

--- packages/tools/client-debugger/client-debugger-view/src/Debugger.tsx
+++ packages/tools/client-debugger/client-debugger-view/src/Debugger.tsx
@@ -7,7 +7,11 @@ import React from "react";
 import {
 	ContainerList,
 	ContainerMetadata,
+	DevtoolsFeature,
+	DevtoolsFeatureFlags,
+	DevtoolsFeatures,
 	GetContainerList,
+	GetDevtoolsFeatures,
 	handleIncomingMessage,
 	IMessageRelay,
 	InboundHandlers,
@@ -16,7 +20,14 @@ import {
 
 import { IStackItemStyles, IStackStyles, Stack } from "@fluentui/react";
 import { FluentProvider } from "@fluentui/react-components";
-import { ContainerView, TelemetryView, MenuItem, MenuSection, LandingView } from "./components";
+import {
+	ContainerView,
+	TelemetryView,
+	MenuItem,
+	MenuSection,
+	LandingView,
+	Waiting,
+} from "./components";
 import { initializeFluentUiIcons } from "./InitializeIcons";
 import { useMessageRelay } from "./MessageRelayContext";
 import { getFluentUIThemeToUse } from "./ThemeHelper";
@@ -25,6 +36,11 @@ const loggingContext = "INLINE(DebuggerPanel)";
 
 // Ensure FluentUI icons are initialized.
 initializeFluentUiIcons();
+
+/**
+ * Message sent to the webpage to query for the supported set of Devtools features.
+ */
+const getSupportedFeaturesMessage = GetDevtoolsFeatures.createMessage();
 
 /**
  * Message sent to the webpage to query for the full container list.
@@ -66,6 +82,58 @@ interface TelemetryMenuSelection {
  */
 type MenuSelection = TelemetryMenuSelection | ContainerMenuSelection;
 
+// Styles definition
+const stackStyles: IStackStyles = {
+	root: {
+		"display": "flex",
+		"flexDirection": "row",
+		"flexWrap": "nowrap",
+		"width": "auto",
+		"height": "auto",
+		"boxSizing": "border-box",
+		"> *": {
+			textOverflow: "ellipsis",
+		},
+		"> :not(:first-child)": {
+			marginTop: "0px",
+		},
+		"> *:not(.ms-StackItem)": {
+			flexShrink: 1,
+		},
+	},
+};
+const contentViewStyles: IStackItemStyles = {
+	root: {
+		"alignItems": "center",
+		"display": "flex",
+		"justifyContent": "center",
+		"flexDirection": "column",
+		"flexWrap": "nowrap",
+		"width": "auto",
+		"height": "auto",
+		"boxSizing": "border-box",
+		"> *": {
+			textOverflow: "ellipsis",
+		},
+		"> :not(:first-child)": {
+			marginTop: "0px",
+		},
+		"> *:not(.ms-StackItem)": {
+			flexShrink: 1,
+		},
+	},
+};
+
+const menuStyles: IStackItemStyles = {
+	root: {
+		...contentViewStyles,
+		display: "flex",
+		flexDirection: "column",
+		borderRight: `2px solid`,
+		minWidth: 150,
+	},
+};
+
 /**
  * Renders drop down to show more than 2 containers and manage the selected container in the debug view for an active
  * debugger session registered using {@link @fluid-tools/client-debugger#initializeFluidClientDebugger}.
@@ -73,6 +141,9 @@ type MenuSelection = TelemetryMenuSelection | ContainerMenuSelection;
  * @remarks If no debugger has been initialized, will display a note to the user and a refresh button to search again.
  */
 export function FluidClientDebuggers(): React.ReactElement {
+	const [supportedFeatures, setSupportedFeatures] = React.useState<
+		DevtoolsFeatureFlags | undefined
+	>();
 	const [containers, setContainers] = React.useState<ContainerMetadata[] | undefined>();
 	const [menuSelection, setMenuSelection] = React.useState<MenuSelection | undefined>();
 
@@ -83,6 +154,11 @@ export function FluidClientDebuggers(): React.ReactElement {
 		 * Handlers for inbound messages related to the registry.
 		 */
 		const inboundMessageHandlers: InboundHandlers = {
+			[DevtoolsFeatures.MessageType]: (untypedMessage) => {
+				const message = untypedMessage as DevtoolsFeatures.Message;
+				setSupportedFeatures(message.data.features);
+				return true;
+			},
 			[ContainerList.MessageType]: (untypedMessage) => {
 				const message = untypedMessage as ContainerList.Message;
 				setContainers(message.data.containers);
@@ -101,12 +177,16 @@ export function FluidClientDebuggers(): React.ReactElement {
 
 		messageRelay.on("message", messageHandler);
 
+		// Query for supported feature set
+		messageRelay.postMessage(getSupportedFeaturesMessage);
+
+		// Query for list of Containers
 		messageRelay.postMessage(getContainerListMessage);
 
 		return (): void => {
 			messageRelay.off("message", messageHandler);
 		};
-	}, [setContainers, messageRelay]);
+	}, [messageRelay, setSupportedFeatures, setContainers]);
 
 	let innerView: React.ReactElement;
 	switch (menuSelection?.type) {
@@ -128,94 +208,15 @@ export function FluidClientDebuggers(): React.ReactElement {
 			break;
 	}
 
-	// Styles definition
-	const stackStyles: IStackStyles = {
-		root: {
-			"display": "flex",
-			"flexDirection": "row",
-			"flexWrap": "nowrap",
-			"width": "auto",
-			"height": "auto",
-			"boxSizing": "border-box",
-			"> *": {
-				textOverflow: "ellipsis",
-			},
-			"> :not(:first-child)": {
-				marginTop: "0px",
-			},
-			"> *:not(.ms-StackItem)": {
-				flexShrink: 1,
-			},
-		},
-	};
-	const contentViewStyles: IStackItemStyles = {
-		root: {
-			"alignItems": "center",
-			"display": "flex",
-			"justifyContent": "center",
-			"flexDirection": "column",
-			"flexWrap": "nowrap",
-			"width": "auto",
-			"height": "auto",
-			"boxSizing": "border-box",
-			"> *": {
-				textOverflow: "ellipsis",
-			},
-			"> :not(:first-child)": {
-				marginTop: "0px",
-			},
-			"> *:not(.ms-StackItem)": {
-				flexShrink: 1,
-			},
-		},
-	};
-
-	const menuStyles: IStackItemStyles = {
-		root: {
-			...contentViewStyles,
-			display: "flex",
-			flexDirection: "column",
-			borderRight: `2px solid`,
-			minWidth: 150,
-		},
-	};
-
-	function onContainerClicked(id: string): void {
-		setMenuSelection({ type: "containerMenuSelection", containerId: id });
-	}
-
-	function onTelemetryClicked(): void {
-		setMenuSelection({ type: "telemetryMenuSelection" });
-	}
-
 	return (
 		<FluentProvider theme={getFluentUIThemeToUse()}>
 			<Stack enableScopedSelectors horizontal styles={stackStyles}>
-				<Stack.Item grow={1} styles={menuStyles}>
-					{/* TODO: button to refresh list of containers */}
-					<MenuSection header="Containers">
-						{containers?.map((container) => (
-							<MenuItem
-								key={container.id}
-								isActive={
-									menuSelection?.type === "containerMenuSelection" &&
-									menuSelection.containerId === container.id
-								}
-								text={container.nickname ?? container.id}
-								onClick={(event): void => {
-									onContainerClicked(`${container.id}`);
-								}}
-							/>
-						))}
-					</MenuSection>
-					<MenuSection header="Telemetry">
-						<MenuItem
-							isActive={menuSelection?.type === "telemetryMenuSelection"}
-							text="See Telemetry"
-							onClick={onTelemetryClicked}
-						/>
-					</MenuSection>
-				</Stack.Item>
+				<Menu
+					currentSelection={menuSelection}
+					setSelection={setMenuSelection}
+					containers={containers}
+					supportedFeatures={supportedFeatures}
+				/>
 				<Stack.Item grow={5} styles={contentViewStyles}>
 					<div
 						id="debugger-view-content"
@@ -226,5 +227,90 @@ export function FluidClientDebuggers(): React.ReactElement {
 				</Stack.Item>
 			</Stack>
 		</FluentProvider>
+	);
+}
+
+/**
+ * {@link Menu} input props.
+ */
+interface MenuProps {
+	/**
+	 * The current menu selection (if any).
+	 */
+	currentSelection?: MenuSelection;
+
+	/**
+	 * Sets the menu selection to the specified value.
+	 *
+	 * @remarks Passing `undefined` clears the selection.
+	 */
+	setSelection(newSelection: MenuSelection | undefined): void;
+
+	/**
+	 * Set of features supported by the {@link FluidDevtools} instance being used by the page application.
+	 */
+	supportedFeatures?: DevtoolsFeatureFlags;
+
+	/**
+	 * The set of Containers to offer as selection options.
+	 */
+	containers?: ContainerMetadata[];
+}
+
+/**
+ * Menu component for {@link FluidClientDebuggers}.
+ */
+function Menu(props: MenuProps): React.ReactElement {
+	const { currentSelection, setSelection, supportedFeatures, containers } = props;
+
+	function onContainerClicked(id: string): void {
+		setSelection({ type: "containerMenuSelection", containerId: id });
+	}
+
+	function onTelemetryClicked(): void {
+		setSelection({ type: "telemetryMenuSelection" });
+	}
+
+	const menuSections: React.ReactElement[] = [];
+
+	// Display the Containers menu section only if we have a non-empty Container list.
+	if (containers !== undefined && containers.length > 0) {
+		menuSections.push(
+			<MenuSection header="Containers" key="container-selection-menu-section">
+				{containers.map((container) => (
+					<MenuItem
+						key={container.id}
+						isActive={
+							currentSelection?.type === "containerMenuSelection" &&
+							currentSelection.containerId === container.id
+						}
+						text={container.nickname ?? container.id}
+						onClick={(event): void => {
+							onContainerClicked(`${container.id}`);
+						}}
+					/>
+				))}
+			</MenuSection>,
+		);
+	}
+
+	// Display the Telemetry menu section only if the corresponding Devtools instance supports telemetry messaging.
+	if (supportedFeatures?.[DevtoolsFeature.Telemetry] === true) {
+		menuSections.push(
+			<MenuSection header="Telemetry" key="telemetry-menu-section">
+				<MenuItem
+					isActive={currentSelection?.type === "telemetryMenuSelection"}
+					text="See Telemetry"
+					onClick={onTelemetryClicked}
+				/>
+			</MenuSection>,
+		);
+	}
+
+	return (
+		<Stack.Item grow={1} styles={menuStyles}>
+			{/* TODO: button to refresh list of containers */}
+			{menuSections.length === 0 ? <Waiting /> : menuSections}
+		</Stack.Item>
 	);
 }
