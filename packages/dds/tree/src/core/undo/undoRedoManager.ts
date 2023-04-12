e6b6e2c52cff07a06c2ca2d3099a01c4ece64782
Jenn <jennle@microsoft.com>
2023-04-12 09:05:53 -0700

    Update packages/dds/tree/src/core/undo/undoRedoManager.ts
    Co-authored-by: yann-achard-MS <97201204+yann-achard-MS@users.noreply.github.com>

--- packages/dds/tree/src/core/undo/undoRedoManager.ts
+++ packages/dds/tree/src/core/undo/undoRedoManager.ts
@@ -30,7 +30,7 @@ export class UndoRedoManager<TChange, TEditor extends ChangeFamilyEditor> {
 			revision?: RevisionTag,
 			type?: GraphCommitType,
 		) => void,
-		protected headUndoCommit?: UndoableCommit<TChange>,
+		protected headUndoableCommit?: UndoableCommit<TChange>,
 	) {}
 
 	/**
