fc74ff201a738a44c42fdc91323d8469ec6a50f2
Scott Norton <scottnorton@microsoft.com>
2023-04-13 13:37:45 -0400

    [AzureClient] Expose feature gates for AzureClient users (#15029)
    ## Description
    
    This PR exposes feature gates for AzureClient users to allow the use of
    experimental features.

--- azure/packages/test/end-to-end-tests/src/test/containerCreate.spec.ts
+++ azure/packages/test/end-to-end-tests/src/test/containerCreate.spec.ts
@@ -10,7 +10,12 @@ import { ContainerSchema } from "@fluidframework/fluid-static";
 import { SharedMap } from "@fluidframework/map";
 import { timeoutPromise } from "@fluidframework/test-utils";
 
+import { ConfigTypes, IConfigProviderBase, MockLogger } from "@fluidframework/telemetry-utils";
 import { createAzureClient } from "./AzureClientFactory";
+
+const configProvider = (settings: Record<string, ConfigTypes>): IConfigProviderBase => ({
+	getRawConfig: (name: string): ConfigTypes => settings[name],
+});
 
 describe("Container create scenarios", () => {
 	const connectTimeoutMs = 1000;
@@ -142,5 +147,44 @@ describe("Container create scenarios", () => {
 		);
 		// eslint-disable-next-line require-atomic-updates
 		console.error = consoleErrorFn;
+	});
+});
+
+describe("Container create with feature flags", () => {
+	let client: AzureClient;
+	let schema: ContainerSchema;
+	let mockLogger: MockLogger;
+
+	beforeEach(() => {
+		mockLogger = new MockLogger();
+		client = createAzureClient(
+			undefined,
+			undefined,
+			mockLogger,
+			configProvider({
+				"Fluid.ContainerRuntime.DisableOpReentryCheck": true,
+			}),
+		);
+		schema = {
+			initialObjects: {
+				map1: SharedMap,
+			},
+		};
+	});
+
+	/**
+	 * Scenario: Test if AzureClient can create a container with feature gates.
+	 *
+	 * Expected behavior: An error should not be thrown and the logger should have logged the enabled feature gates.
+	 */
+	it("can create containers with feature gates", async () => {
+		await client.createContainer(schema);
+		mockLogger.assertMatchAny([
+			{
+				featureGates: JSON.stringify({
+					disableOpReentryCheck: true,
+				}),
+			},
+		]);
 	});
 });
