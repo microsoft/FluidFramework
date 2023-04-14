e11d63d9310bf060a460c0743061998c2d15894c
tyler-cai-microsoft <90650728+tyler-cai-microsoft@users.noreply.github.com>
2023-04-14 15:23:03 -0700

    Reduce the number of restart errors (#15105)
    [AB#4015](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/4015)
    
    When the summarizer is stopped because we want to restart it instead of
    refreshing from an ack, we throw an error to close the container and
    prevent a network call.
    
    PR that introduced the restart change:
    https://github.com/microsoft/FluidFramework/pull/15009
    PR that tried to remove all error telemetry:
    https://github.com/microsoft/FluidFramework/pull/15036
    
    The goal of this PR is to 
    - send a generic telemetry event 
    - reduce error telemetry events
    - prevent the execution of making a network call to fetch the snapshot
    (I don't know if this is important or not)
    
    If we don't implement this fix, we'll see these events, some as error
    events.
    
    Spot checking a few docs, we would raise 5 error events each time we
    close the summarizer instead of refreshing.
    ```
    fluid:telemetry:Container:ContainerClose | Restarting summarizer instead of refreshing | genericError | Restarting summarizer instead of refreshing
    fluid:telemetry:SummarizerNode:refreshLatestSummary_cancel | Restarting summarizer instead of refreshing | genericError | Restarting summarizer instead of refreshing
    fluid:telemetry:Summarizer:Running:HandleLastSummaryAckError | Restarting summarizer instead of refreshing | genericError | Restarting summarizer instead of refreshing
    fluid:telemetry:Summarizer:Running:FailToSummarize | Restarting summarizer instead of refreshing | genericError | submitSummaryFailure: Error while generating, uploading, or submitting summary
    
    ```
    
    ---------
    
    Co-authored-by: Navin Agarwal <45832642+agarwal-navin@users.noreply.github.com>

--- packages/runtime/container-runtime/src/containerRuntime.ts
+++ packages/runtime/container-runtime/src/containerRuntime.ts
@@ -3215,10 +3215,19 @@ export class ContainerRuntime
 			"Fluid.ContainerRuntime.Test.SummarizationRecoveryMethod",
 		);
 		if (recoveryMethod === "restart") {
-			this._summarizer?.stop("latestSummaryStateStale");
-			// TODO: stop execution, but don't cause a cascading series of errors.
 			const error = new GenericError("Restarting summarizer instead of refreshing");
-			this.closeFn(error);
+			this.mc.logger.sendTelemetryEvent(
+				{
+					...event,
+					eventName: "ClosingSummarizerOnSummaryStale",
+					codePath: event.eventName,
+					message: "Stopping fetch from storage",
+					versionId: versionId != null ? versionId : undefined,
+				},
+				error,
+			);
+			this._summarizer?.stop("latestSummaryStateStale");
+			this.closeFn();
 			throw error;
 		}
 
