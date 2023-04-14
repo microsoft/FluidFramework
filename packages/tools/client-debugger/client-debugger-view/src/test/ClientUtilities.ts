8a90474c8b8997080fd36f4cd7ff956532a7919f
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2023-04-14 15:49:38 -0700

    feat(client-debugger): Prototype Visualizing Summary Tree for Client Debugger  (#14951)
    #### Description
    https://dev.azure.com/fluidframework/internal/_workitems/edit/3577/
    
    This PR works closely with
    https://github.com/microsoft/FluidFramework/pull/14770 which generates
    types for visual summary tree for the Fluid DevTools.
    
    #### Visualization using Accordion
    <img width="1385" alt="image"
    src="https://user-images.githubusercontent.com/111468570/232143389-5951354c-2983-40f0-825b-6b04c5b85397.png">
    
    #### Unit Test
    Test Suites: 2 passed, 2 total
    Tests:       4 passed, 4 total
    Snapshots:   0 total
    Time:        5.952 s, estimated 10 s
    
    #### Subsequent PR:
    Apply `fluentUI` using TreeView UI.

--- packages/tools/client-debugger/client-debugger-view/src/test/ClientUtilities.ts
+++ packages/tools/client-debugger/client-debugger-view/src/test/ClientUtilities.ts
@@ -97,6 +97,7 @@ export async function createFluidContainer(
 		console.error(`Encountered error attaching Fluid container: "${error}".`);
 		throw error;
 	}
+
 	console.log("Fluid container attached!");
 
 	return {
