a9e5083731391c9f398ff4dfe615b915723fa48e
Tyler Butler <tyler@tylerbutler.com>
2023-04-14 12:35:52 -0700

    comments

--- scripts/update-package-version.sh
+++ scripts/update-package-version.sh
@@ -1,21 +1,30 @@
+# This script is used in the "update package versions" step in our CI build pipelines.
+# It takes version data from environment variables and updates the package versions accordingly.
+# The environment variables are set earlier in the pipeline, in the "set package version" step.
+
 echo SETVERSION_VERSION=$SETVERSION_VERSION
 echo SETVERSION_CODEVERSION=$SETVERSION_CODEVERSION
 echo RELEASE_GROUP=$RELEASE_GROUP
-echo INTERDEPENDENCY_RANGE="$INTERDEPENDENCY_RANGE"
+echo INTERDEPENDENCY_RANGE="'$INTERDEPENDENCY_RANGE'"
 
 if [ -f "lerna.json" ]; then
   if [ "$VERSION_RELEASE" = "release" ]; then
+    # this is a release build of a release group
     if [ "$INTERDEPENDENCY_RANGE" != " " ]; then
+      # tilde or caret dependency ranges
       echo command="flub bump $RELEASE_GROUP --exact $SETVERSION_VERSION --exactDepType=\"$INTERDEPENDENCY_RANGE\" -xv"
       flub bump $RELEASE_GROUP --exact $SETVERSION_VERSION --exactDepType="$INTERDEPENDENCY_RANGE" -xv
     else
+      # exact dependencies
       echo command="flub bump $RELEASE_GROUP --exact $SETVERSION_VERSION --exactDepType=\"\" -xv"
       flub bump $RELEASE_GROUP --exact $SETVERSION_VERSION --exactDepType="$INTERDEPENDENCY_RANGE" -xv
     fi
   else
+    # this is a dev/test build of a release group
     echo command="flub bump $RELEASE_GROUP --exact $SETVERSION_VERSION --exactDepType=\"\" -xv"
     flub bump $RELEASE_GROUP --exact $SETVERSION_VERSION --exactDepType="" -xv
   fi
 else
+  # this is an independent package
   npm version $SETVERSION_VERSION --no-git-tag-version -f --allow-same-version
 fi
