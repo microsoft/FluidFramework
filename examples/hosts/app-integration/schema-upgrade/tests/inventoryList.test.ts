0a348cf10f1a598847c684f88d46e19e25101d60
Matt Rakow <marakow@microsoft.com>
2023-04-18 10:54:07 -0700

    Add initial tests

--- examples/hosts/app-integration/schema-upgrade/tests/inventoryList.test.ts
+++ examples/hosts/app-integration/schema-upgrade/tests/inventoryList.test.ts
@@ -22,4 +22,52 @@ describe("inventoryList", () => {
 		// Validate the input shows up
 		await page.waitForSelector("input");
 	});
+
+	it("can click the migrate debug button", async () => {
+		// Validate there is a button that can be clicked
+		await expect(page).toClick("button", { text: '"two"' });
+	});
+
+	it("shows the correct code version at pageload", async () => {
+		// Validate the migration status shows "one"
+		await page.waitForSelector(".migration-status");
+		const containsOne = await page.evaluate(() => {
+			const migrationStatusElement = document.querySelector(".migration-status");
+			return migrationStatusElement?.textContent?.includes("one") === true;
+		});
+		await expect(containsOne).toEqual(true);
+	});
+
+	it("migrates and shows the correct code version after migration", async () => {
+		// Validate the migration status shows "one" initially
+		await page.waitForSelector(".migration-status");
+		const leftContainsOne = await page.evaluate(() => {
+			const migrationStatusElements = document.querySelectorAll(".migration-status");
+			return migrationStatusElements[0]?.textContent?.includes("one") === true;
+		});
+		const rightContainsOne = await page.evaluate(() => {
+			const migrationStatusElements = document.querySelectorAll(".migration-status");
+			return migrationStatusElements[1]?.textContent?.includes("one") === true;
+		});
+		await expect(leftContainsOne).toEqual(true);
+		await expect(rightContainsOne).toEqual(true);
+
+		await expect(page).toClick("button", { text: '"two"' });
+
+		await new Promise((resolve) => {
+			setTimeout(resolve, 500);
+		});
+
+		// Validate the migration status shows "two" after the migration
+		const leftContainsTwo = await page.evaluate(() => {
+			const migrationStatusElements = document.querySelectorAll(".migration-status");
+			return migrationStatusElements[0]?.textContent?.includes("two") === true;
+		});
+		const rightContainsTwo = await page.evaluate(() => {
+			const migrationStatusElements = document.querySelectorAll(".migration-status");
+			return migrationStatusElements[1]?.textContent?.includes("two") === true;
+		});
+		await expect(leftContainsTwo).toEqual(true);
+		await expect(rightContainsTwo).toEqual(true);
+	});
 });
