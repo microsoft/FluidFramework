bb3e1a225aee7df46812b4f6f59d544ffb63d51c
Pradeep Vairamani <pradeeprv123@gmail.com>
2023-04-17 23:03:57 -0700

    skip service summary for trasient tenants

--- server/routerlicious/packages/lambdas/src/scribe/lambdaFactory.ts
+++ server/routerlicious/packages/lambdas/src/scribe/lambdaFactory.ts
@@ -72,6 +72,7 @@ export class ScribeLambdaFactory extends EventEmitter implements IPartitionLambd
 		private readonly serviceConfiguration: IServiceConfiguration,
 		private readonly enableWholeSummaryUpload: boolean,
 		private readonly getDeltasViaAlfred: boolean,
+		private readonly transientTenants: string[],
 	) {
 		super();
 	}
@@ -289,6 +290,7 @@ export class ScribeLambdaFactory extends EventEmitter implements IPartitionLambd
 			latestSummary.protocolHead,
 			opsSinceLastSummary,
 			scribeSessionMetric,
+			new Set(this.transientTenants),
 		);
 
 		await this.sendLambdaStartResult(tenantId, documentId, {
