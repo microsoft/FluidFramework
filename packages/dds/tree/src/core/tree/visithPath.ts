558ea8af2f72637f1d9ff56558962c8597957609
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2023-04-18 14:13:21 -0700

    Document more of PathVisitor and remove redundent field from onSetValue

--- packages/dds/tree/src/core/tree/visithPath.ts
+++ packages/dds/tree/src/core/tree/visithPath.ts
@@ -4,7 +4,7 @@
  */
 import * as Delta from "./delta";
 import { UpPath } from "./pathTree";
-import { FieldKey, Value } from "./types";
+import { Value } from "./types";
 
 /**
  * Delta visitor for the path tree.
@@ -13,7 +13,22 @@ import { FieldKey, Value } from "./types";
  * @alpha
  */
 export interface PathVisitor {
+	/**
+	 * A sequence of nodes of length `count` is being deleted starting with `path`.
+	 * Called when these nodes are no longer parented under their previous parent, and do not have a new parent.
+	 * It is possible they may be un-deleted in the future (for example by a conflicted merge or undo).
+	 *
+	 * Not called for children of deleted nodes.
+	 *
+	 * @param path - first node in the deleted range.
+	 * @param count - length of deleted range.
+	 */
 	onDelete(path: UpPath, count: number): void;
-	onInsert(path: UpPath, content: readonly Delta.ProtoNode[]): void;
-	onSetValue(path: UpPath, field: FieldKey | undefined, value: Value): void;
+	/**
+	 * @param path - location which first node of inserted range will have after insert.
+	 * Any nodes at this index (or after it) will be moved to the right (have their indexes increased by `content.length`).
+	 * @param content - content which is being inserted.
+	 */
+	onInsert(path: UpPath, content: Delta.ProtoNodes): void;
+	onSetValue(path: UpPath, value: Value): void;
 }
