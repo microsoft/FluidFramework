f3b175422f038887aec1c7168ff220b75e056cf1
jzaffiro <110866475+jzaffiro@users.noreply.github.com>
2023-04-18 20:40:07 +0000

    Add tests for revert with remote string edits

--- packages/dds/sequence/src/revertibles.ts
+++ packages/dds/sequence/src/revertibles.ts
@@ -102,7 +102,7 @@ export function appendLocalPropertyChangedToRevertibles(
 
 	return revertibles;
 }
-
+// Uses of referenceRangeLabels will be removed once AB#4081 is completed.
 function revertLocalAdd(
 	string: SharedString,
 	revertible: TypedRevertible<typeof IntervalEventType.ADD>,
--- packages/dds/sequence/src/test/revertibles.spec.ts
+++ packages/dds/sequence/src/test/revertibles.spec.ts
@@ -3,8 +3,12 @@
  * Licensed under the MIT License.
  */
 
-import assert from "assert";
-import { MockFluidDataStoreRuntime } from "@fluidframework/test-runtime-utils";
+import { strict as assert } from "assert";
+import {
+	MockContainerRuntimeFactory,
+	MockFluidDataStoreRuntime,
+	MockStorage,
+} from "@fluidframework/test-runtime-utils";
 import {
 	appendLocalAddToRevertibles,
 	appendLocalChangeToRevertibles,
@@ -18,7 +22,7 @@ import { IntervalCollection, IntervalType, SequenceInterval } from "../intervalC
 import { SharedStringFactory } from "../sequenceFactory";
 import { assertIntervals } from "./intervalUtils";
 
-describe.only("Sequence.Revertibles", () => {
+describe("Sequence.Revertibles with Local Edits", () => {
 	let sharedString: SharedString;
 	let dataStoreRuntime1: MockFluidDataStoreRuntime;
 	let collection: IntervalCollection<SequenceInterval>;
@@ -34,12 +38,6 @@ describe.only("Sequence.Revertibles", () => {
 	});
 
 	it("revert direct interval insert", () => {
-		/* how the test should work:
-		- make a sharedstring to put intervals on (driver)
-		- add an interval to the collection, and append the add to the revertible array 
-		- call revert insert
-		- validate that intervals are the same as they were before 
-		*/
 		collection.on("addInterval", (interval, local, op) => {
 			appendLocalAddToRevertibles(interval, revertibles);
 		});
@@ -87,7 +85,93 @@ describe.only("Sequence.Revertibles", () => {
 		revertIntervalRevertibles(sharedString, revertibles.splice(0));
 		assertIntervals(sharedString, collection, [{ start: 0, end: 5 }]);
 		const int = collection.getIntervalById(id);
-		assert.strictEqual(int?.properties.foo, "one");
+		assert.equal(int?.properties.foo, "one");
 	});
-	// TODO: add tests for remote edits to sharedstring over the interval range
+});
+describe("Sequence.Revertibles with Remote Edits", () => {
+	let sharedString: SharedString;
+	let dataStoreRuntime1: MockFluidDataStoreRuntime;
+	let collection: IntervalCollection<SequenceInterval>;
+	let revertibles: IntervalRevertible[];
+
+	let sharedString2: SharedString;
+	let containerRuntimeFactory: MockContainerRuntimeFactory;
+
+	beforeEach(() => {
+		dataStoreRuntime1 = new MockFluidDataStoreRuntime({ clientId: "1" });
+		sharedString = new SharedString(
+			dataStoreRuntime1,
+			"shared-string-1",
+			SharedStringFactory.Attributes,
+		);
+
+		containerRuntimeFactory = new MockContainerRuntimeFactory();
+
+		// Connect the first SharedString.
+		dataStoreRuntime1.local = false;
+		const containerRuntime1 = containerRuntimeFactory.createContainerRuntime(dataStoreRuntime1);
+		const services1 = {
+			deltaConnection: containerRuntime1.createDeltaConnection(),
+			objectStorage: new MockStorage(),
+		};
+		sharedString.initializeLocal();
+		sharedString.connect(services1);
+
+		// Create and connect a second SharedString.
+		const dataStoreRuntime2 = new MockFluidDataStoreRuntime({ clientId: "2" });
+		const containerRuntime2 = containerRuntimeFactory.createContainerRuntime(dataStoreRuntime2);
+		const services2 = {
+			deltaConnection: containerRuntime2.createDeltaConnection(),
+			objectStorage: new MockStorage(),
+		};
+
+		sharedString2 = new SharedString(
+			dataStoreRuntime2,
+			"shared-string-2",
+			SharedStringFactory.Attributes,
+		);
+		sharedString2.initializeLocal();
+		sharedString2.connect(services2);
+
+		revertibles = [];
+		collection = sharedString.getIntervalCollection("test");
+	});
+	it("remote delete interacting with reverting an interval remove", () => {
+		collection.on("deleteInterval", (interval, local, op) => {
+			appendLocalDeleteToRevertibles(sharedString, interval, revertibles);
+		});
+		sharedString.insertText(0, "hello world");
+		const id = collection.add(0, 5, IntervalType.Simple).getIntervalId();
+		collection.removeIntervalById(id);
+		containerRuntimeFactory.processAllMessages();
+
+		sharedString2.removeRange(1, 3);
+		containerRuntimeFactory.processAllMessages();
+
+		revertIntervalRevertibles(sharedString, revertibles.splice(0));
+		containerRuntimeFactory.processAllMessages();
+
+		assert.equal(sharedString.getText(), sharedString2.getText());
+		// These endpoints will be updated when interval sliding is fixed.
+		assertIntervals(sharedString, collection, [{ start: 0, end: 3 /* end: 5 */ }]);
+		assertIntervals(sharedString2, collection, [{ start: 0, end: 3 /* end: 5 */ }]);
+	});
+	it("remote add interacting with reverting an interval delete", () => {
+		collection.on("deleteInterval", (interval, local, op) => {
+			appendLocalDeleteToRevertibles(sharedString, interval, revertibles);
+		});
+		sharedString.insertText(0, "hello world");
+		const id = collection.add(0, 7, IntervalType.Simple).getIntervalId();
+		containerRuntimeFactory.processAllMessages();
+
+		sharedString2.insertText(5, " hi");
+		collection.removeIntervalById(id);
+		containerRuntimeFactory.processAllMessages();
+
+		revertIntervalRevertibles(sharedString, revertibles.splice(0));
+		containerRuntimeFactory.processAllMessages();
+
+		assert.equal(sharedString.getText(), sharedString2.getText());
+		assertIntervals(sharedString, collection, [{ start: 0, end: 10 }]);
+	});
 });
