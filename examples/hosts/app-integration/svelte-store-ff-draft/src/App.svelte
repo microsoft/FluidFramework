2b5e465895fe709eb865b2b9b4f1b35c20c057c3
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2023-04-19 03:18:01 +0000

    first push

---
+++ examples/hosts/app-integration/svelte-store-ff-draft/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { onDestroy, onMount } from 'svelte';
+  import { getTinyliciousContainer } from './lib/getTinyliciousContainer';
+  import { fluidWritable } from './lib/fluidWritable';
+  import { writable } from "svelte/store";
+  
+
+  let text = "";
+  let syncedText;
+
+  onMount(async () => {
+    const containerId = location.hash.substring(1);
+    const container = await getTinyliciousContainer(containerId);
+    syncedText = fluidWritable(container);
+    syncedText.subscribe((value) => {
+      text = value;
+    });
+  });
+
+  function updateText() {
+    syncedText.set(text);
+  }
+</script>
+
+<textarea bind:value={text} on:input={updateText}></textarea>
+{$syncedText}
