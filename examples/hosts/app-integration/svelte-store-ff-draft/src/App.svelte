5cd21a043aff6c04d3e60370e20f23d8f44a7cd2
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2023-04-19 03:38:03 +0000

    format

--- examples/hosts/app-integration/svelte-store-ff-draft/src/App.svelte
+++ examples/hosts/app-integration/svelte-store-ff-draft/src/App.svelte
@@ -1,23 +1,23 @@
 <script>
-  import { onDestroy, onMount } from 'svelte';
-  import { getTinyliciousContainer } from './util/getTinyliciousContainer';
-  import { fluidWritable } from './util/fluidWritable';
+	import { onDestroy, onMount } from "svelte";
+	import { getTinyliciousContainer } from "./util/getTinyliciousContainer";
+	import { fluidWritable } from "./util/fluidWritable";
 
-  let text = "";
-  let syncedText;
+	let text = "";
+	let syncedText;
 
-  onMount(async () => {
-    const container = await getTinyliciousContainer();
-    syncedText = fluidWritable(container);
-    syncedText.subscribe((value) => {
-      text = value;
-    });
-  });
+	onMount(async () => {
+		const container = await getTinyliciousContainer();
+		syncedText = fluidWritable(container);
+		syncedText.subscribe((value) => {
+			text = value;
+		});
+	});
 
-  function updateText() {
-    syncedText.set(text);
-  }
+	function updateText() {
+		syncedText.set(text);
+	}
 </script>
 
-<textarea bind:value={text} on:input={updateText}></textarea>
+<textarea bind:value={text} on:input={updateText} />
 {$syncedText}
--- examples/hosts/app-integration/svelte-store-ff-draft/src/util/getTinyliciousContainer.ts
+++ examples/hosts/app-integration/svelte-store-ff-draft/src/util/getTinyliciousContainer.ts
@@ -4,30 +4,29 @@ import { SharedMap } from "@fluidframework/map";
 import type { IFluidContainer, ContainerSchema } from "fluid-framework";
 
 const containerSchema: ContainerSchema = {
-  initialObjects: {
-    root: SharedMap,
-  },
+	initialObjects: {
+		root: SharedMap,
+	},
 };
 
-export async function getTinyliciousContainer(
-): Promise<IFluidContainer> {
-  const client = new TinyliciousClient();
-  let container: IFluidContainer;
-  const containerId = location.hash.substring(1);
+export async function getTinyliciousContainer(): Promise<IFluidContainer> {
+	const client = new TinyliciousClient();
+	let container: IFluidContainer;
+	const containerId = location.hash.substring(1);
 
-  if (containerId) {
-    try {
-      ({ container } = await client.getContainer(containerId, containerSchema));
-    } catch {
-      ({ container } = await client.createContainer(containerSchema));
-      const id = await container.attach();
-      location.hash = id;
-    }
-  } else {
-    ({ container } = await client.createContainer(containerSchema));
-    const id = await container.attach();
-    location.hash = id;
-  }
+	if (containerId) {
+		try {
+			({ container } = await client.getContainer(containerId, containerSchema));
+		} catch {
+			({ container } = await client.createContainer(containerSchema));
+			const id = await container.attach();
+			location.hash = id;
+		}
+	} else {
+		({ container } = await client.createContainer(containerSchema));
+		const id = await container.attach();
+		location.hash = id;
+	}
 
-  return container;
+	return container;
 }
