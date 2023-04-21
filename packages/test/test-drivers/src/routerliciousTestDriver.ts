ea67f9dd7e8bc0b92442822557992a2f1faf7d6b
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2023-04-21 20:58:27 +0000

    update to frsCanary

--- packages/test/test-drivers/src/routerliciousTestDriver.ts
+++ packages/test/test-drivers/src/routerliciousTestDriver.ts
@@ -114,7 +114,7 @@ export function assertRouterliciousEndpoint(
 	if (
 		endpoint === undefined ||
 		endpoint === "frs" ||
-		endpoint === "frs-canary" ||
+		endpoint === "frsCanary" ||
 		endpoint === "r11s" ||
 		endpoint === "docker"
 	) {
