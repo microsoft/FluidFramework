7d8168d1abc003c868fb045a7960b576406d262a
Scott Norton <scottnorton@microsoft.com>
2023-04-21 20:00:16 +0000

    throw error if undefined

--- azure/packages/test/end-to-end-tests/src/test/AzureClientFactory.ts
+++ azure/packages/test/end-to-end-tests/src/test/AzureClientFactory.ts
@@ -33,6 +33,9 @@ export function createAzureClient(
 		name: userName ?? uuid(),
 	};
 	const endPoint = process.env.azure__fluid__relay__service__endpoint as string;
+	if (endPoint === undefined) {
+		throw new Error("Azure FRS endpoint is missing");
+	}
 
 	// use AzureClient remote mode will run against live Azure Fluid Relay.
 	// Default to running Tinylicious for PR validation
