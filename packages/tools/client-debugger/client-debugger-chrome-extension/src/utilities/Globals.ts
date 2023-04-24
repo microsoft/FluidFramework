bc155fe5c7e1de7109249b4027c09d4e6f252417
Josmithr <josmithr@microsoft.com>
2023-04-24 13:30:09 -0700

    remove: Commented-out code

--- packages/tools/client-debugger/client-debugger-chrome-extension/src/utilities/Globals.ts
+++ packages/tools/client-debugger/client-debugger-chrome-extension/src/utilities/Globals.ts
@@ -13,7 +13,6 @@ const _browser: typeof chrome = typeof browser !== "undefined" ? browser : chrom
 // Include references to web browser globals to facilitate mocks during testing.
 const _document = document;
 const _eval = eval; // eslint-disable-line no-eval
-// const _fetch = fetch.bind(window);
 const _location = location;
 const _MutationObserver = MutationObserver;
 const _window = window;
@@ -22,7 +21,6 @@ export {
 	_browser as browser,
 	_document as document,
 	_eval as eval,
-	// _fetch as fetch,
 	_location as location,
 	_MutationObserver as MutationObserver,
 	_window as window,
