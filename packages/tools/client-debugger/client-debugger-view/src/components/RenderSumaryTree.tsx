dafbac28addb7265dc6c1761c3418805cac43f0c
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2023-04-25 02:40:05 +0000

    policy fix

--- packages/tools/client-debugger/client-debugger-view/src/components/RenderSumaryTree.tsx
+++ packages/tools/client-debugger/client-debugger-view/src/components/RenderSumaryTree.tsx
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import React from "react";
 // eslint-disable-next-line import/no-internal-modules
 import { Tree, TreeItem, TreeItemLayout } from "@fluentui/react-components/unstable";
