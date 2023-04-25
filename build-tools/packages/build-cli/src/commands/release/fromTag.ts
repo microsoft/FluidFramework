0d9c7cb9778245a4d11e4a927f86e23282d31ce1
Tyler Butler <tyler@tylerbutler.com>
2023-04-25 16:48:46 -0700

    get correct previous versions

--- build-tools/packages/build-cli/src/commands/release/fromTag.ts
+++ build-tools/packages/build-cli/src/commands/release/fromTag.ts
@@ -11,65 +11,48 @@ import { ReleaseReportBaseCommand, ReleaseSelectionMode } from "./report";
 import { ReleaseVersion, VersionBumpType, detectBumpType } from "@fluid-tools/version-tools";
 
 /**
- * The `version latest` command is used to find the latest (highest) version in a list of versions. The command takes
- * the Fluid internal version scheme into account, and handles prerelease versions properly.
+ * The `release fromTag` command is used to get release information from a git tag.
  *
- * Once scenario where this is useful is for Fluid customers who want to consume the most recent version from npm. The
- * standard tools (e.g. `npm show versions`) don't fully work in that scenario because the Fluid internal version scheme
- * overloads the semver prerelease field.
  */
 export default class FromTagCommand extends ReleaseReportBaseCommand<typeof FromTagCommand> {
-	static description = "Determines release information from a git tag.";
+	static summary = "Determines release information based on a git tag argument.";
+
+	static description =
+		"This command is used in CI to determine release information when a new release tag is pushed.";
 
 	static enableJsonFlag = true;
 
 	static args = {
 		tag: Args.string({
 			required: true,
-			description: "A git tag that represents a release.",
+			description: "A git tag that represents a release. May begin with 'refs/tags/'.",
 		}),
 	};
 
 	defaultMode: ReleaseSelectionMode = "inRepo";
 	releaseGroupOrPackage: ReleaseGroup | ReleasePackage | undefined;
 
-	static flags = {
-		releaseType: Flags.boolean({
-			default: false,
-			exclusive: ["releaseGroup", "version"],
-		}),
-		releaseGroup: Flags.boolean({
-			default: false,
-			exclusive: ["releaseType", "version"],
-		}),
-		version: Flags.boolean({
-			default: false,
-			exclusive: ["releaseGroup", "releaseType"],
-		}),
-	};
-
-	// static examples = [
-	// 	{
-	// 		description: "You can use the --versions (-r) flag multiple times.",
-	// 		command:
-	// 			"<%= config.bin %> <%= command.id %> -r 2.0.0 -r 2.0.0-internal.1.0.0 -r 1.0.0 -r 0.56.1000",
-	// 	},
-	// 	{
-	// 		description:
-	// 			"You can omit the repeated --versions (-r) flag and pass a space-delimited list instead.",
-	// 		command:
-	// 			"<%= config.bin %> <%= command.id %> -r 2.0.0 2.0.0-internal.1.0.0 1.0.0 0.56.1000",
-	// 	},
-	// ];
+	static examples = [
+		{
+			description: "You can use the --versions (-r) flag multiple times.",
+			command: "<%= config.bin %> <%= command.id %> refs/tags/build-tools_v0.13.0",
+		},
+		{
+			description:
+				"You can omit the repeated --versions (-r) flag and pass a space-delimited list instead.",
+			command:
+				"<%= config.bin %> <%= command.id %> -r 2.0.0 2.0.0-internal.1.0.0 1.0.0 0.56.1000",
+		},
+	];
 
 	async run(): Promise<{
 		packageOrReleaseGroup: ReleaseGroup | ReleasePackage;
-    tag: string;
-    date?: Date;
+		tag: string;
+		date?: Date;
 		releaseType: VersionBumpType;
 		version: ReleaseVersion;
-    previousVersion?: ReleaseVersion;
-    previousTag?: string,
+		previousVersion?: ReleaseVersion;
+		previousTag?: string;
 	}> {
 		const tagInput = this.args.tag;
 		const context = await this.getContext();
@@ -85,50 +68,54 @@ export default class FromTagCommand extends ReleaseReportBaseCommand<typeof From
 		);
 
 		const release = this.releaseData[this.releaseGroupOrPackage];
+		this.verbose(JSON.stringify(release, undefined, 2));
 
 		const versions = sortVersions([...release.versions], "version");
 		const taggedReleaseIndex = versions.findIndex((v) => v.version === version.version);
 		if (taggedReleaseIndex === -1) {
 			this.error(`Release matching version '${version.version}' not found`);
 		}
-		const prevVersion = release.previousReleasedVersion;
-		if (prevVersion === undefined) {
+		const prevVersionDetails = versions[taggedReleaseIndex + 1];
+		this.warning(
+			`Previous index: ${taggedReleaseIndex + 1} version: ${prevVersionDetails?.version}`,
+		);
+		if (prevVersionDetails === undefined) {
 			this.error(`No previous release found`);
 		}
 
-		const releaseType = detectBumpType(prevVersion?.version, version);
+		const releaseType = detectBumpType(prevVersionDetails?.version, version);
 		if (releaseType === undefined) {
 			this.error(
-				`Unable to determine release type for ${prevVersion?.version} -> ${version.version}`,
+				`Unable to determine release type for ${prevVersionDetails?.version} -> ${version.version}`,
 			);
 		}
 
-		if (this.flags.releaseGroup) {
-			this.log(this.releaseGroupOrPackage);
-		} else if (this.flags.version) {
-			this.log(version.version);
-		} else if (this.flags.releaseType) {
-			this.log(releaseType);
-		} else {
-			this.log(`${this.releaseGroupOrPackage} v${version.version} (${releaseType})`);
-		}
+		this.log(`${this.releaseGroupOrPackage} v${version.version} (${releaseType})`);
 
 		// When the --json flag is passed, the command will return the raw data as JSON.
-    const previousVersion = release.previousReleasedVersion?.version;
+		const previousVersion = prevVersionDetails?.version;
 		return {
 			packageOrReleaseGroup: this.releaseGroupOrPackage,
-      tag,
-      date: release.latestReleasedVersion.date,
+			tag,
+			date: release.latestReleasedVersion.date,
 			releaseType,
 			version: version.version,
-      previousVersion,
-      previousTag: previousVersion === undefined ? undefined : `${this.releaseGroupOrPackage}_v${previousVersion}`
+			previousVersion,
+			previousTag:
+				prevVersionDetails === undefined
+					? undefined
+					: `${this.releaseGroupOrPackage}_v${previousVersion}`,
 		};
 	}
 }
 
 const pre = "refs/tags/";
 
+/**
+ * Parses a git tag string into a release group and a semver version.
+ * @param input - A git tag as a string.
+ * @returns A 3-tuple of the release group, the semver version, and the original tag.
+ */
 const parseTag = (input: string): [ReleaseGroup, semver.SemVer, string] => {
 	const tag = input.startsWith(pre) ? input.slice(pre.length) : input;
 	const [rg, ver] = tag.split("_v");
