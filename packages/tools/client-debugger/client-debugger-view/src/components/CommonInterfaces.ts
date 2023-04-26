df0a30239e5a86e7390865f635c7dd9bdc1109f8
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2023-04-26 00:29:04 +0000

    policy fix

--- packages/tools/client-debugger/client-debugger-view/src/components/CommonInterfaces.ts
+++ packages/tools/client-debugger/client-debugger-view/src/components/CommonInterfaces.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 /**
  * Base interface used in passing key of the child node from Record {@link VisauTree}
  *
