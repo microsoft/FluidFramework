f5b763b537c1558cacffb18bac2e6338ff5ff16f
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-05-02 22:36:19 +0000

    test: Add tests for new methods

--- packages/tools/client-debugger/client-debugger/src/test/FluidDevtools.test.ts
+++ packages/tools/client-debugger/client-debugger/src/test/FluidDevtools.test.ts
@@ -8,6 +8,7 @@ import { expect } from "chai";
 import {
 	FluidDevtools,
 	getContainerAlreadyRegisteredErrorText,
+	initializeDevtools,
 	useAfterDisposeErrorText,
 } from "../FluidDevtools";
 import { ContainerDevtoolsProps } from "../ContainerDevtools";
@@ -37,15 +38,14 @@ describe("FluidDevtools unit tests", () => {
 		expect(devtools.getAllContainerDevtools().length).to.equal(1);
 
 		const containerDevtools = devtools.getContainerDevtools(containerId);
-		// eslint-disable-next-line @typescript-eslint/no-unused-expressions
 		expect(containerDevtools).to.not.be.undefined;
-		// eslint-disable-next-line @typescript-eslint/no-unused-expressions, @typescript-eslint/no-non-null-assertion
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 		expect(containerDevtools!.disposed).to.be.false;
 
 		devtools.closeContainerDevtools(containerId);
 
 		expect(devtools.getAllContainerDevtools().length).to.equal(0);
-		// eslint-disable-next-line @typescript-eslint/no-unused-expressions, @typescript-eslint/no-non-null-assertion
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 		expect(containerDevtools!.disposed).to.be.true;
 
 		devtools.dispose();
@@ -56,7 +56,6 @@ describe("FluidDevtools unit tests", () => {
 
 		devtools.dispose();
 
-		// eslint-disable-next-line @typescript-eslint/no-unused-expressions
 		expect(devtools.disposed).to.be.true;
 
 		const container = createMockContainer();
@@ -96,5 +95,29 @@ describe("FluidDevtools unit tests", () => {
 		expect(() => devtools.registerContainerDevtools(container2Props)).to.throw(
 			getContainerAlreadyRegisteredErrorText(containerId),
 		);
+	});
+
+	it("tryGet", () => {
+		expect(FluidDevtools.tryGet()).to.be.undefined;
+
+		const devtools = initializeDevtools({});
+
+		expect(FluidDevtools.tryGet()).to.not.be.undefined;
+
+		devtools.dispose();
+
+		expect(FluidDevtools.tryGet()).to.be.undefined;
+	});
+
+	it("getOrThrow", () => {
+		expect(() => FluidDevtools.getOrThrow()).to.throw();
+
+		const devtools = initializeDevtools({});
+
+		expect(() => FluidDevtools.getOrThrow()).to.not.throw();
+
+		devtools.dispose();
+
+		expect(() => FluidDevtools.getOrThrow()).to.throw();
 	});
 });
