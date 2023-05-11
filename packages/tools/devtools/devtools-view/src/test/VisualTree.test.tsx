c7e567c793d006c65d1f6a6f279a736e8ee15182
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-05-11 21:02:29 +0000

    test: Fix test and add another

--- packages/tools/devtools/devtools-view/src/test/VisualTree.test.tsx
+++ packages/tools/devtools/devtools-view/src/test/VisualTree.test.tsx
@@ -16,10 +16,11 @@ import {
 	DataVisualization,
 	FluidObjectValueNode,
 	FluidObjectTreeNode,
+	FluidUnknownObjectNode,
 	UnknownObjectNode,
 	VisualNodeKind,
 } from "@fluid-experimental/devtools-core";
-import { UnknownDataView, FluidTreeView } from "../components";
+import { UnknownDataView, FluidTreeView, UnknownFluidObjectView } from "../components";
 import { MessageRelayContext } from "../MessageRelayContext";
 import { MockMessageRelay } from "./MockMessageRelay";
 
@@ -35,7 +36,20 @@ describe("VisualTreeView component tests", () => {
 
 		render(<UnknownDataView label="test-label" node={input} />);
 
-		await screen.findByText(/Encountered an unrecognized kind of data object/); // Will throw if exact text not found
+		await screen.findByText(/Unrecognized kind of data./); // Will throw if exact text not found
+	});
+
+	// eslint-disable-next-line jest/expect-expect
+	it("UnknownFluidObjectView", async (): Promise<void> => {
+		const input: FluidUnknownObjectNode = {
+			fluidObjectId: "test-fluid-object-id",
+			typeMetadata: "test-fluid-object-type",
+			nodeKind: VisualNodeKind.FluidUnknownObjectNode,
+		};
+
+		render(<UnknownFluidObjectView label="test-label" node={input} />);
+
+		await screen.findByText(/Unrecognized kind of Fluid Object./); // Will throw if exact text not found
 	});
 
 	// eslint-disable-next-line jest/expect-expect
