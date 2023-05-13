89e8a3e583e40fd0e50784f4d1476f6fc94e115a
Clarence Li <clarenceli@microsoft.com>
2023-05-12 20:05:46 -0400

    move dom to index.html

--- examples/apps/attributable-map/src/view.ts
+++ examples/apps/attributable-map/src/view.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /*!
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
@@ -11,96 +12,20 @@ export function renderHitCounter(
 	runtimeAttributor: IRuntimeAttributor | undefined,
 	div: HTMLDivElement,
 ) {
-	// Create wrapper div and add styling
-	const wrapperDiv = document.createElement("div");
-	wrapperDiv.style.display = "flex";
-	wrapperDiv.style.flexDirection = "column";
-	wrapperDiv.style.justifyContent = "center";
-	wrapperDiv.style.alignItems = "center";
-	wrapperDiv.style.minHeight = "100vh";
-	div.appendChild(wrapperDiv);
+	const redCounterContainerDiv = document.getElementById("red-counter-container-div")!;
+	const greenCounterContainerDiv = document.getElementById("green-counter-container-div")!;
+	const redAttributionContainerDiv = document.getElementById("red-attribution-container-div");
+	const greenAttributionContainerDiv = document.getElementById("green-attribution-container-div");
 
-	// Create container div for counters and add styling
-	const countersContainerDiv = document.createElement("div");
-	countersContainerDiv.style.display = "flex";
-	countersContainerDiv.style.width = "100%";
-	countersContainerDiv.style.justifyContent = "space-around";
-	countersContainerDiv.style.marginBottom = "2rem";
-	wrapperDiv.appendChild(countersContainerDiv);
-
-	const redCounterContainerDiv = document.createElement("div");
-	redCounterContainerDiv.style.fontSize = "5rem";
-	redCounterContainerDiv.style.fontWeight = "bold";
-	redCounterContainerDiv.style.textAlign = "center";
-	countersContainerDiv.appendChild(redCounterContainerDiv);
-
-	const greenCounterContainerDiv = document.createElement("div");
-	greenCounterContainerDiv.style.fontSize = "5rem";
-	greenCounterContainerDiv.style.fontWeight = "bold";
-	greenCounterContainerDiv.style.textAlign = "center";
-	countersContainerDiv.appendChild(greenCounterContainerDiv);
-
-	// Create container div for buttons and add styling
-	const buttonsContainerDiv = document.createElement("div");
-	buttonsContainerDiv.style.display = "flex";
-	buttonsContainerDiv.style.width = "100%";
-	buttonsContainerDiv.style.justifyContent = "space-around";
-	buttonsContainerDiv.style.marginBottom = "2rem";
-	wrapperDiv.appendChild(buttonsContainerDiv);
-
-	// Create red button element and add styling
-	const redButton = document.createElement("button");
-	redButton.style.fontSize = "2rem";
-	redButton.style.padding = "1rem 2rem";
-	redButton.style.borderRadius = "1rem";
-	redButton.style.backgroundColor = "#F84F31";
-	redButton.style.color = "white";
-	redButton.style.border = "none";
-	redButton.style.cursor = "pointer";
-	redButton.textContent = "Hit";
-	buttonsContainerDiv.appendChild(redButton);
-
+	const redButton = document.getElementById("red-button")!;
 	redButton.addEventListener("click", () => {
 		hitCounter.hit("red");
 	});
 
-	// Create green button element and add styling
-	const greenButton = document.createElement("button");
-	greenButton.style.fontSize = "2rem";
-	greenButton.style.padding = "1rem 2rem";
-	greenButton.style.borderRadius = "1rem";
-	greenButton.style.backgroundColor = "#23C552";
-	greenButton.style.color = "white";
-	greenButton.style.border = "none";
-	greenButton.style.cursor = "pointer";
-	greenButton.textContent = "Hit";
-	buttonsContainerDiv.appendChild(greenButton);
-
+	const greenButton = document.getElementById("green-button")!;
 	greenButton.addEventListener("click", () => {
 		hitCounter.hit("green");
 	});
-
-	// Create container div for attribution text and add styling
-	const attributionContainerDiv = document.createElement("div");
-	attributionContainerDiv.style.display = "flex";
-	attributionContainerDiv.style.width = "100%";
-	attributionContainerDiv.style.justifyContent = "space-around";
-	attributionContainerDiv.style.marginBottom = "2rem";
-	wrapperDiv.appendChild(attributionContainerDiv);
-
-	const redAttributionContainerDiv = document.createElement("div");
-	redAttributionContainerDiv.style.fontSize = "2rem";
-	redAttributionContainerDiv.style.fontStyle = "italic";
-	redAttributionContainerDiv.style.textAlign = "center";
-	redAttributionContainerDiv.style.marginBottom = "2rem";
-	attributionContainerDiv.appendChild(redAttributionContainerDiv);
-
-	const greenAttributionContainerDiv = document.createElement("div");
-	greenAttributionContainerDiv.style.fontSize = "2rem";
-	greenAttributionContainerDiv.style.fontStyle = "italic";
-	greenAttributionContainerDiv.style.textAlign = "center";
-	greenAttributionContainerDiv.style.marginBottom = "2rem";
-	attributionContainerDiv.appendChild(greenAttributionContainerDiv);
 
 	// Function to update counter value and attribution text
 	const updateView = () => {
