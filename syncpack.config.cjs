ba317756ab6915f9d21ecf3bc4bf85ae7641cad4
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2023-05-15 11:26:40 -0700

    Add syncpack workarounds

--- syncpack.config.cjs
+++ syncpack.config.cjs
@@ -26,6 +26,42 @@ module.exports = {
 	 * `syncpack lint-semver-ranges`, the output is grouped by label.
 	 */
 	semverGroups: [
+		// Workaround for compatibility issues.
+		// Ideally this section would be empty (and removed).
+		// Items should be removed from here when possible.
+		{
+			label: "Version compatibility workarounds should be used, or removed from syncpack.config.cjs if no longer needed.",
+			dependencies: [
+				"@fluidframework/build-tools>npm-package-json-lint@^6.0.0",
+				"@oclif/core",
+			],
+			dependencyTypes: ["pnpmOverrides"],
+			packages: ["**"],
+			range: "~",
+		},
+
+		// Workaround for compatibility issues.
+		// Ideally this section would be empty (and removed).
+		// Items should be removed from here when possible.
+		{
+			label: "Version compatibility workarounds should be used, or removed from syncpack.config.cjs if no longer needed.",
+			dependencies: ["@types/react", "@types/react-dom"],
+			dependencyTypes: ["pnpmOverrides"],
+			packages: ["**"],
+			range: "",
+		},
+
+		// Workaround for compatibility issues.
+		// Ideally this section would be empty (and removed).
+		// Items should be removed from here when possible.
+		{
+			label: "Version compatibility workarounds should be used, or removed from syncpack.config.cjs if no longer needed.",
+			dependencies: ["office-ui-fabric-react"],
+			dependencyTypes: ["prod"],
+			packages: ["**"],
+			range: "",
+		},
+
 		{
 			label: "engines.node should always use >= ranges",
 			dependencyTypes: ["engines"],
