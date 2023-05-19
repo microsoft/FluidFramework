afb9387d346b61b09cda7150f34ffcfc56169e68
Navin Agarwal <naagar@microsoft.com>
2023-05-19 00:22:20 +0000

    Added test

--- packages/test/test-end-to-end-tests/src/test/gc/gcIncrementalSummaries.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/gc/gcIncrementalSummaries.spec.ts
@@ -5,9 +5,10 @@
 
 import { strict as assert } from "assert";
 import { IContainer } from "@fluidframework/container-definitions";
-import { ISummarizer } from "@fluidframework/container-runtime";
+import { ContainerRuntime, ISummarizer } from "@fluidframework/container-runtime";
 import { ISummaryTree, SummaryType } from "@fluidframework/protocol-definitions";
 import { requestFluidObject } from "@fluidframework/runtime-utils";
+import { MockLogger } from "@fluidframework/telemetry-utils";
 import {
 	ITestObjectProvider,
 	createSummarizer,
@@ -17,6 +18,7 @@ import {
 import {
 	describeNoCompat,
 	ITestDataObject,
+	itExpects,
 	TestDataObjectType,
 } from "@fluid-internal/test-version-utils";
 import { channelsTreeName } from "@fluidframework/runtime-definitions";
@@ -218,4 +220,70 @@ describeNoCompat("GC incremental summaries", (getTestObjectProvider) => {
 		dataStoreSummaryTypesMap.set(dataStoreB._context.id, SummaryType.Handle);
 		await validateIncrementalSummary(summarizer3, dataStoreSummaryTypesMap);
 	});
+
+	itExpects.only(
+		"does not log incrementalSummaryViolation when summary fails with gc state updated data stores",
+		[
+			{
+				eventName: "fluid:telemetry:Summarizer:Running:Summarize_cancel",
+				error: "Summary failed simulation",
+			},
+		],
+		async () => {
+			const mockLogger = new MockLogger();
+			const { summarizer: summarizer1 } = await createSummarizer(
+				provider,
+				mainContainer,
+				undefined /* summaryVersion */,
+				undefined /* gcOptions */,
+				undefined /* configProvider */,
+				mockLogger,
+			);
+
+			// Create data stores B and C, and mark them as referenced.
+			const dataStoreB = await requestFluidObject<ITestDataObject>(
+				await dataStoreA._context.containerRuntime.createDataStore(TestDataObjectType),
+				"",
+			);
+			dataStoreA._root.set("dataStoreB", dataStoreB.handle);
+
+			for (let i = 1; i < 10; i++) {
+				const newDataStore = await requestFluidObject<ITestDataObject>(
+					await dataStoreA._context.containerRuntime.createDataStore(TestDataObjectType),
+					"",
+				);
+				dataStoreB._root.set(`dataStoreB-${i}`, newDataStore.handle);
+			}
+			await provider.ensureSynchronized();
+			await assert.doesNotReject(summarizeNow(summarizer1), "Summarize should have passed");
+
+			const containerRuntime = (summarizer1 as any).runtime as ContainerRuntime;
+			const uploadSummaryWithContextFunc = containerRuntime.storage.uploadSummaryWithContext;
+			const uploadSummaryWithContextOverride = async () => {
+				throw new Error("Summary failed simulation");
+			};
+			containerRuntime.storage.uploadSummaryWithContext = uploadSummaryWithContextOverride;
+
+			dataStoreA._root.delete("dataStoreB");
+			await provider.ensureSynchronized();
+			const errorFn = (error: Error): boolean => {
+				assert.strictEqual(
+					error.message,
+					"Summary failed simulation",
+					"unexpected summary failures",
+				);
+				return true;
+			};
+			await assert.rejects(
+				summarizeNow(summarizer1),
+				errorFn,
+				"Summarize should have failed",
+			);
+			mockLogger.assertMatchNone([{ eventName: "IncrementalSummaryViolation" }]);
+			containerRuntime.storage.uploadSummaryWithContext = uploadSummaryWithContextFunc;
+
+			await assert.doesNotReject(summarizeNow(summarizer1), "Summarize should have passed");
+			mockLogger.assertMatchNone([{ eventName: "IncrementalSummaryViolation" }]);
+		},
+	);
 });
