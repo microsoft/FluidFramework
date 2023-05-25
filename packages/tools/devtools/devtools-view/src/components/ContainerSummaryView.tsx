17abc932b9247264102d9523d8f711e38c9af07b
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-05-25 17:31:26 +0000

    refactor: Make DataRow a React component

--- packages/tools/devtools/devtools-view/src/components/ContainerSummaryView.tsx
+++ packages/tools/devtools/devtools-view/src/components/ContainerSummaryView.tsx
@@ -80,27 +80,45 @@ interface Item {
 }
 
 /**
- * Displays a row with basic stats about the Container.
- *
- * @param label - Row label text.
- * @param infoTooltipContent - (optional) Tooltip content to display via an info badge.
- * If not provided, no info badge will be displayed.
- * @param value - The value text associated with the label.
- * @param columnProps - Column props consumed by FluentUI.
+ * {@link DataRow} input props.
  */
-function DataRow(
-	label: React.ReactElement | string,
-	infoTooltipContent: React.ReactElement | string | undefined,
-	value: React.ReactElement | string | undefined,
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	columnProps: any,
-): React.ReactElement {
+interface DataRowProps {
+	/**
+	 * Row label content (first column).
+	 */
+	label: React.ReactElement | string;
+
+	/**
+	 * Tooltip content to display via an info badge.
+	 * If not provided, no info badge will be displayed.
+	 */
+	infoTooltipContent: React.ReactElement | string | undefined;
+
+	/**
+	 * The value text associated with the label (second column).
+	 */
+	value: React.ReactElement | string | undefined;
+
+	/**
+	 * Column props consumed by FluentUI.
+	 *
+	 * @privateRemarks `@fluentui/react-components` does not export the type we need here: `TableColumnSizingState`.
+	 */
+	columnProps: unknown;
+}
+
+/**
+ * Displays a row with basic stats about the Container.
+ */
+function DataRow(props: DataRowProps): React.ReactElement {
+	const { label, infoTooltipContent, value, columnProps } = props;
+
 	return (
 		<TableRow>
 			<TableCell
 				{
-					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-					...columnProps.getTableCellProps("containerProperty")
+					// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+					...(columnProps as any).getTableCellProps("containerProperty")
 				}
 			>
 				{infoTooltipContent === undefined ? (
@@ -267,24 +285,24 @@ export function ContainerSummaryView(props: ContainerSummaryViewProps): React.Re
 			</StackItem>
 			<StackItem>
 				<Table size="extra-small" ref={tableRef}>
-					{DataRow(
-						"Status",
-						containerStatusTooltipText,
-						containerStatusValueCell(statusComponents),
-						columnSizing_unstable,
-					)}
-					{DataRow(
-						"Client ID",
-						clientIdTooltipText,
-						containerState.clientId,
-						columnSizing_unstable,
-					)}
-					{DataRow(
-						"User ID",
-						userIdTooltipText,
-						containerState.userId,
-						columnSizing_unstable,
-					)}
+					<DataRow
+						label="Status"
+						infoTooltipContent={containerStatusTooltipText}
+						value={containerStatusValueCell(statusComponents)}
+						columnProps={columnSizing_unstable}
+					/>
+					<DataRow
+						label="Client ID"
+						infoTooltipContent={clientIdTooltipText}
+						value={containerState.clientId}
+						columnProps={columnSizing_unstable}
+					/>
+					<DataRow
+						label="User ID"
+						infoTooltipContent={userIdTooltipText}
+						value={containerState.userId}
+						columnProps={columnSizing_unstable}
+					/>
 				</Table>
 			</StackItem>
 			<StackItem align="start">
