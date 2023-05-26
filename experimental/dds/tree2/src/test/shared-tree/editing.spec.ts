38986d2e81ce85d035ad0e40d9cb35eb1fafc0de
Yann Achard <achardy@microsoft.com>
2023-05-26 16:59:52 -0700

    Exhaustive remove & undo test

--- experimental/dds/tree2/src/test/shared-tree/editing.spec.ts
+++ experimental/dds/tree2/src/test/shared-tree/editing.spec.ts
@@ -5,9 +5,10 @@
 import { strict as assert } from "assert";
 
 import { singleJsonCursor } from "../../domains";
-import { rootFieldKeySymbol, UpPath, moveToDetachedField } from "../../core";
-import { brand } from "../../util";
+import { rootFieldKeySymbol, UpPath, moveToDetachedField, FieldUpPath } from "../../core";
+import { brand, makeArray } from "../../util";
 import { makeTreeFromJson, remove, insert, expectJsonTree } from "../utils";
+import { SharedTreeView } from "../../shared-tree";
 
 describe("Editing", () => {
 	describe("Sequence Field", () => {
@@ -352,6 +353,171 @@ describe("Editing", () => {
 			tree2.rebaseOnto(tree);
 
 			expectJsonTree([tree, tree1, tree2], [{ foo: [{}, "b"] }]);
+		});
+
+		describe("Exhaustive removal tests", () => {
+			// Toggle the constant below to run each scenario as a separate test.
+			// This is useful to debug a specific scenario but makes CI and the test browser slower.
+			// Note that if the numbers of nodes and peers are too high (more than 3 nodes and 3 peers),
+			// then the number of scenarios overwhelms the test browser.
+			// Should be committed with the constant set to false.
+			const individualTests = false;
+			const nbNodes = 3;
+			const nbPeers = 3;
+			const testRemoveRevive = true;
+			const testMoveReturn = true;
+			assert(testRemoveRevive || testMoveReturn, "No scenarios to run");
+
+			const [outerFixture, innerFixture] = individualTests
+				? [describe, it]
+				: [it, (title: string, fn: () => void) => fn()];
+
+			type ScenarioStep =
+				| { readonly removeIdx: number; readonly peer: number }
+				| { readonly peer: number };
+
+			/**
+			 * Generates all permutations for `nbNodes` and `nbPeers` such that:
+			 * - Each node is removed exactly once.
+			 * - Each removal is undone by the peer that removed it.
+			 * The order of removals and undos is unique when considering which peer does what.
+			 * This does mean that this function produces symmetrical scenarios such as:
+			 * - D(i:0 p:0) D(i:1 p:1) U(1) U(0)
+			 * - D(i:0 p:1) D(i:1 p:0) U(0) U(1)
+			 * This is taken advantage of to test different network conditions (see {@link runScenario}).
+			 */
+			function buildScenarios(): Generator<readonly ScenarioStep[]> {
+				interface ScenarioBuilderState {
+					removed: boolean[];
+					peerQueue: number[];
+				}
+
+				const buildState: ScenarioBuilderState = {
+					removed: makeArray(nbNodes, () => false),
+					peerQueue: makeArray(nbPeers, () => 0),
+				};
+
+				function* buildScenariosImpl(
+					scenario: ScenarioStep[] = [],
+				): Generator<readonly ScenarioStep[]> {
+					let done = true;
+					for (let p = 0; p < nbPeers; p++) {
+						for (let i = 0; i < nbNodes; i++) {
+							if (!buildState.removed[i]) {
+								buildState.removed[i] = true;
+								buildState.peerQueue[p] += 1;
+								for (const built of buildScenariosImpl([
+									...scenario,
+									{ removeIdx: i, peer: p },
+								])) {
+									yield built;
+								}
+								buildState.peerQueue[p] -= 1;
+								buildState.removed[i] = false;
+								done = false;
+							}
+						}
+						if (buildState.peerQueue[p] > 0) {
+							buildState.peerQueue[p] -= 1;
+							for (const built of buildScenariosImpl([...scenario, { peer: p }])) {
+								yield built;
+							}
+							buildState.peerQueue[p] += 1;
+							done = false;
+						}
+					}
+					if (done) {
+						yield scenario;
+					}
+				}
+				return buildScenariosImpl();
+			}
+
+			const delAction = (peer: SharedTreeView, idx: number) => remove(peer, idx, 1);
+			const srcField: FieldUpPath = { parent: undefined, field: rootFieldKeySymbol };
+			const dstField: FieldUpPath = { parent: undefined, field: brand("dst") };
+			const moveAction = (peer: SharedTreeView, idx: number) =>
+				peer.editor.move(srcField, idx, 1, dstField, 0);
+
+			/**
+			 * Runs the given `scenario` using either delete or move operations.
+			 * Verifies that the final state is the same as the initial state.
+			 * Simulates different peers learning of the same edit at different times.
+			 * For example, given the following two (otherwise symmetrical) scenarios:
+			 * 1) D(i:0 p:0) D(i:1 p:1) U(1) U(0)
+			 * 2) D(i:0 p:1) D(i:1 p:0) U(0) U(1)
+			 * In scenario 1, the peer that deletes N1 learns of the deletion of N0 beforehand.
+			 * In scenario 2, the peer that deletes N1 learns of the deletion of N0 afterwards.
+			 * @param scenario - The scenario to run through.
+			 * @param useMove - When `true`, uses move operations. Otherwise, uses delete operations.
+			 */
+			function runScenario(scenario: readonly ScenarioStep[], useMove: boolean): void {
+				const [verb, action] = useMove ? ["M", moveAction] : ["D", delAction];
+				const title = scenario
+					.map((s) => {
+						if ("removeIdx" in s) {
+							return `${verb}(i:${s.removeIdx} p:${s.peer})`;
+						}
+						return `U(${s.peer})`;
+					})
+					.join(" ");
+				innerFixture(title, () => {
+					// Keeps track of which nodes are present in the root field for a given peer.
+					// Used to compute the index of the next node to remove.
+					const present = makeArray(nbPeers, () => makeArray(nbNodes, () => 1));
+					// The number of remaining undos available for each peer.
+					const undoQueues: number[][] = makeArray(nbPeers, () => []);
+
+					const tree = makeTreeFromJson(startState);
+					const peers = makeArray(nbPeers, () => tree.fork());
+					for (const step of scenario) {
+						const iPeer = step.peer;
+						const peer = peers[iPeer];
+						let presence: number;
+						let affectedNode: number;
+						if ("removeIdx" in step) {
+							const idx = present[iPeer]
+								.slice(0, step.removeIdx)
+								.reduce((a, b) => a + b, 0);
+							action(peer, idx);
+							presence = 0;
+							affectedNode = step.removeIdx;
+							undoQueues[iPeer].push(step.removeIdx);
+						} else {
+							peer.undo();
+							presence = 1;
+							// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+							affectedNode = undoQueues[iPeer].pop()!;
+						}
+						tree.merge(peer);
+						// We only let peers with a higher index learn of this edit.
+						// This break the symmetry between scenarios where the permutation of actions is the same
+						// except for which peer does which set of actions.
+						// It also helps simulate different peers learning of the same edit at different times.
+						for (let downhillPeer = iPeer + 1; downhillPeer < nbPeers; downhillPeer++) {
+							peers[downhillPeer].rebaseOnto(peer);
+							present[downhillPeer][affectedNode] = presence;
+						}
+						present[iPeer][affectedNode] = presence;
+					}
+					peers.forEach((peer) => peer.rebaseOnto(tree));
+					expectJsonTree([tree, ...peers], startState);
+				});
+			}
+
+			const startState = makeArray(nbNodes, (n) => `N${n}`);
+			const scenarios = buildScenarios();
+
+			outerFixture("All Scenarios", () => {
+				for (const scenario of scenarios) {
+					if (testRemoveRevive) {
+						runScenario(scenario, false);
+					}
+					if (testMoveReturn) {
+						runScenario(scenario, true);
+					}
+				}
+			});
 		});
 	});
 
