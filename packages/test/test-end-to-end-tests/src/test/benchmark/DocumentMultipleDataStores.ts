9a6d4bc6e98a00d1ad918b600f6abf83575501dd
Nicholas Couri <nichoc@microsoft.com>
2023-05-26 15:13:10 -0700

    Initial refactoring to add parameters to E2E cfg

--- packages/test/test-end-to-end-tests/src/test/benchmark/DocumentMultipleDataStores.ts
+++ packages/test/test-end-to-end-tests/src/test/benchmark/DocumentMultipleDataStores.ts
@@ -20,6 +20,10 @@ import { IFluidHandle, IRequest } from "@fluidframework/core-interfaces";
 import { IContainer, LoaderHeader } from "@fluidframework/container-definitions";
 import { createSummarizerFromFactory, summarizeNow } from "@fluidframework/test-utils";
 import { ITelemetryLogger } from "@fluidframework/common-definitions";
+import {
+	assertDocumentTypeInfo,
+	isDocumentMultipleDataStoresInfo,
+} from "@fluid-internal/test-version-utils";
 import { IDocumentLoaderAndSummarizer, IDocumentProps, ISummarizeResult } from "./DocumentCreator";
 
 // Tests usually make use of the default data object provided by the test object provider.
@@ -66,14 +70,13 @@ const runtimeOptions: IContainerRuntimeOptions = {
 	},
 };
 
-const dsCountsPerIteration = 250;
-
 // implement IDocumentLoader methods
 export class DocumentMultipleDds implements IDocumentLoaderAndSummarizer {
 	private _mainContainer: IContainer | undefined;
 	private containerRuntime: ContainerRuntime | undefined;
 	private mainDataStore: TestDataObject | undefined;
-	private readonly dataStoreCounts: number;
+	private readonly numberDataStoreCounts: number;
+	private readonly dsCountsPerIteration: number;
 	private readonly _dataObjectFactory: DataObjectFactory<TestDataObject>;
 	public get dataObjectFactory() {
 		return this._dataObjectFactory;
@@ -119,9 +122,9 @@ export class DocumentMultipleDds implements IDocumentLoaderAndSummarizer {
 		);
 		// Data stores are not created as to not generate too many ops, and hit the # of ops limit.
 		// This is a workaround to prevent that.
-		const totalIterations = this.dataStoreCounts / dsCountsPerIteration;
+		const totalIterations = this.numberDataStoreCounts / this.dsCountsPerIteration;
 		for (let i = 0; i < totalIterations; i++) {
-			for (let j = 0; j < dsCountsPerIteration; j++) {
+			for (let j = 0; j < this.dsCountsPerIteration; j++) {
 				const dataStore = await this.dataObjectFactory.createInstance(
 					this.containerRuntime,
 				);
@@ -157,12 +160,16 @@ export class DocumentMultipleDds implements IDocumentLoaderAndSummarizer {
 			runtimeOptions,
 		);
 
+		assertDocumentTypeInfo(this.props.documentTypeInfo, this.props.documentType);
+		// Now TypeScript knows that info.documentTypeInfo is either DocumentMapInfo or DocumentMultipleDataStoresInfo
+		// and info.documentType is either "DocumentMap" or "DocumentMultipleDataStores"
+		assert(isDocumentMultipleDataStoresInfo(this.props.documentTypeInfo));
+
 		switch (this.props.documentType) {
-			case "MediumDocumentMultipleDataStores":
-				this.dataStoreCounts = 250;
-				break;
-			case "LargeDocumentMultipleDataStores":
-				this.dataStoreCounts = 500;
+			case "DocumentMultipleDataStores":
+				this.numberDataStoreCounts = this.props.documentTypeInfo.numberDataStores;
+				this.dsCountsPerIteration =
+					this.props.documentTypeInfo.numberDataStoresPerIteration;
 				break;
 			default:
 				throw new Error("Invalid document type");
