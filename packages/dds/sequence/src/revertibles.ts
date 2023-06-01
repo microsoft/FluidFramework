7beb0f22174c211344c21d36b3e9e0c0da725dd8
jzaffiro <110866475+jzaffiro@users.noreply.github.com>
2023-06-01 14:32:01 +0000

    Add changeset and fix policy check issue

--- packages/dds/sequence/src/revertibles.ts
+++ packages/dds/sequence/src/revertibles.ts
@@ -1,8 +1,8 @@
-/* eslint-disable no-bitwise */
 /*!
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+/* eslint-disable no-bitwise */
 
 import { assert, unreachableCase } from "@fluidframework/common-utils";
 import {
