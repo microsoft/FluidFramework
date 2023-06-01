214a523b73d902ead0c4b85aefd28cd906a7324f
Tyler Butler <tyler@tylerbutler.com>
2023-05-31 18:10:40 -0700

    Apply suggestions from code review

--- build-tools/packages/changelog-generator/.eslintrc.cjs
+++ build-tools/packages/changelog-generator/.eslintrc.cjs
@@ -10,17 +10,4 @@ module.exports = {
 		require.resolve("@fluidframework/eslint-config-fluid/minimal"),
 		"prettier",
 	],
-	rules: {
-		// oclif uses default exports for commands
-		"import/no-default-export": "off",
-
-		// This package uses interfaces and types that are not exposed directly by npm-check-updates.
-		// We also call commands' run method directly in some cases, so these are all excluded.
-		"import/no-internal-modules": [
-			"error",
-			{
-				allow: ["npm-check-updates/build/src/types/**", "**/commands/**"],
-			},
-		],
-	},
 };
