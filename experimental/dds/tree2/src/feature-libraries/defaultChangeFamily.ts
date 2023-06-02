5f1ff73822afad0f4e00ed0b306ea1e27865182f
Abram Sanderson <abram.sanderson@gmail.com>
2023-06-02 10:28:33 -0700

    remove commented out code

--- experimental/dds/tree2/src/feature-libraries/defaultChangeFamily.ts
+++ experimental/dds/tree2/src/feature-libraries/defaultChangeFamily.ts
@@ -66,8 +66,6 @@ export class DefaultChangeFamily implements ChangeFamily<DefaultEditBuilder, Def
 	}
 }
 
-// export const defaultChangeFamily = new DefaultChangeFamily();
-
 /**
  * Default editor for transactions.
  * @alpha
