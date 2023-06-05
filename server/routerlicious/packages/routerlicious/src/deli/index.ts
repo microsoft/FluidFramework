3fc0bb68f2a5f0eea31ba4882c84547a460e2e46
Andrew Stanovsky <astanovsky@microsoft.com>
2023-06-06 01:55:15 +0200

    merging with latest main

--- server/routerlicious/packages/routerlicious/src/deli/index.ts
+++ server/routerlicious/packages/routerlicious/src/deli/index.ts
@@ -6,9 +6,9 @@
 import { BroadcasterLambda, DeliLambdaFactory } from "@fluidframework/server-lambdas";
 import { createDocumentRouter } from "@fluidframework/server-routerlicious-base";
 import {
-    LocalKafka,
-    LocalContext,
-    LocalLambdaController,
+	LocalKafka,
+	LocalContext,
+	LocalLambdaController,
 } from "@fluidframework/server-memory-orderer";
 import * as services from "@fluidframework/server-services";
 import * as core from "@fluidframework/server-services-core";
@@ -18,166 +18,166 @@ import { RedisOptions } from "ioredis";
 import * as winston from "winston";
 
 export async function deliCreate(
-    config: Provider,
-    customizations?: Record<string, any>,
+	config: Provider,
+	customizations?: Record<string, any>,
 ): Promise<core.IPartitionLambdaFactory> {
-    const kafkaEndpoint = config.get("kafka:lib:endpoint");
-    const kafkaLibrary = config.get("kafka:lib:name");
-    const kafkaProducerPollIntervalMs = config.get("kafka:lib:producerPollIntervalMs");
-    const kafkaNumberOfPartitions = config.get("kafka:lib:numberOfPartitions");
-    const kafkaReplicationFactor = config.get("kafka:lib:replicationFactor");
-    const kafkaMaxBatchSize = config.get("kafka:lib:maxBatchSize");
-    const kafkaSslCACertFilePath: string = config.get("kafka:lib:sslCACertFilePath");
-    const eventHubConnString: string = config.get("kafka:lib:eventHubConnString");
+	const kafkaEndpoint = config.get("kafka:lib:endpoint");
+	const kafkaLibrary = config.get("kafka:lib:name");
+	const kafkaProducerPollIntervalMs = config.get("kafka:lib:producerPollIntervalMs");
+	const kafkaNumberOfPartitions = config.get("kafka:lib:numberOfPartitions");
+	const kafkaReplicationFactor = config.get("kafka:lib:replicationFactor");
+	const kafkaMaxBatchSize = config.get("kafka:lib:maxBatchSize");
+	const kafkaSslCACertFilePath: string = config.get("kafka:lib:sslCACertFilePath");
+	const eventHubConnString: string = config.get("kafka:lib:eventHubConnString");
 
-    const kafkaForwardClientId = config.get("deli:kafkaClientId");
-    const kafkaReverseClientId = config.get("alfred:kafkaClientId");
+	const kafkaForwardClientId = config.get("deli:kafkaClientId");
+	const kafkaReverseClientId = config.get("alfred:kafkaClientId");
 
-    const forwardSendTopic = config.get("deli:topics:send");
-    const reverseSendTopic = config.get("alfred:topic");
+	const forwardSendTopic = config.get("deli:topics:send");
+	const reverseSendTopic = config.get("alfred:topic");
 
-    const documentsCollectionName = config.get("mongo:collectionNames:documents");
-    const checkpointsCollectionName = config.get("mongo:collectionNames:checkpoints");
+	const documentsCollectionName = config.get("mongo:collectionNames:documents");
+	const checkpointsCollectionName = config.get("mongo:collectionNames:checkpoints");
 
-    const localCheckpointEnabled = config.get("checkpoints:localCheckpointEnabled");
+	const localCheckpointEnabled = config.get("checkpoints:localCheckpointEnabled");
 
-    const restartOnCheckpointFailure =
-        (config.get("deli:restartOnCheckpointFailure") as boolean) ?? true;
+	const restartOnCheckpointFailure =
+		(config.get("deli:restartOnCheckpointFailure") as boolean) ?? true;
 
-    const kafkaCheckpointOnReprocessingOp =
-        (config.get("checkpoints:kafkaCheckpointOnReprocessingOp") as boolean) ?? true;
+	const kafkaCheckpointOnReprocessingOp =
+		(config.get("checkpoints:kafkaCheckpointOnReprocessingOp") as boolean) ?? true;
 
-    // Generate tenant manager which abstracts access to the underlying storage provider
-    const authEndpoint = config.get("auth:endpoint");
-    const internalHistorianUrl = config.get("worker:internalBlobStorageUrl");
-    const tenantManager = new services.TenantManager(authEndpoint, internalHistorianUrl);
-    const globalDbEnabled = config.get("mongo:globalDbEnabled") as boolean;
-    // Database connection for global db if enabled
-    const factory = await services.getDbFactory(config);
+	// Generate tenant manager which abstracts access to the underlying storage provider
+	const authEndpoint = config.get("auth:endpoint");
+	const internalHistorianUrl = config.get("worker:internalBlobStorageUrl");
+	const tenantManager = new services.TenantManager(authEndpoint, internalHistorianUrl);
+	const globalDbEnabled = config.get("mongo:globalDbEnabled") as boolean;
+	// Database connection for global db if enabled
+	const factory = await services.getDbFactory(config);
 
-    const checkpointHeuristics = config.get(
-        "deli:checkpointHeuristics",
-    ) as core.ICheckpointHeuristicsServerConfiguration;
-    // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
-    if (checkpointHeuristics && checkpointHeuristics.enable) {
-        core.DefaultServiceConfiguration.deli.checkpointHeuristics = checkpointHeuristics;
-    }
+	const checkpointHeuristics = config.get(
+		"deli:checkpointHeuristics",
+	) as core.ICheckpointHeuristicsServerConfiguration;
+	// eslint-disable-next-line @typescript-eslint/prefer-optional-chain
+	if (checkpointHeuristics && checkpointHeuristics.enable) {
+		core.DefaultServiceConfiguration.deli.checkpointHeuristics = checkpointHeuristics;
+	}
 
-    let globalDb: core.IDb;
-    if (globalDbEnabled) {
-        const globalDbReconnect = (config.get("mongo:globalDbReconnect") as boolean) ?? false;
-        const globalDbManager = new core.MongoManager(factory, globalDbReconnect, null, true);
-        globalDb = await globalDbManager.getDatabase();
-    }
+	let globalDb: core.IDb;
+	if (globalDbEnabled) {
+		const globalDbReconnect = (config.get("mongo:globalDbReconnect") as boolean) ?? false;
+		const globalDbManager = new core.MongoManager(factory, globalDbReconnect, null, true);
+		globalDb = await globalDbManager.getDatabase();
+	}
 
-    const operationsDbManager = new core.MongoManager(factory, false);
-    const operationsDb = await operationsDbManager.getDatabase();
+	const operationsDbManager = new core.MongoManager(factory, false);
+	const operationsDb = await operationsDbManager.getDatabase();
 
-    const db: core.IDb = globalDbEnabled ? globalDb : operationsDb;
+	const db: core.IDb = globalDbEnabled ? globalDb : operationsDb;
 
-    // eslint-disable-next-line @typescript-eslint/await-thenable
-    const collection = await db.collection<core.IDocument>(documentsCollectionName);
-    // eslint-disable-next-line @typescript-eslint/await-thenable
-    const localCollection = await operationsDb.collection<core.ICheckpoint>(
-        checkpointsCollectionName,
-    );
-    const documentRepository =
-        customizations?.documentRepository ?? new core.MongoDocumentRepository(collection);
-    const checkpointRepository = new core.MongoCheckpointRepository(localCollection, "deli");
+	// eslint-disable-next-line @typescript-eslint/await-thenable
+	const collection = await db.collection<core.IDocument>(documentsCollectionName);
+	// eslint-disable-next-line @typescript-eslint/await-thenable
+	const localCollection = await operationsDb.collection<core.ICheckpoint>(
+		checkpointsCollectionName,
+	);
+	const documentRepository =
+		customizations?.documentRepository ?? new core.MongoDocumentRepository(collection);
+	const checkpointRepository = new core.MongoCheckpointRepository(localCollection, "deli");
 
-    const forwardProducer = services.createProducer(
-        kafkaLibrary,
-        kafkaEndpoint,
-        kafkaForwardClientId,
-        forwardSendTopic,
-        true,
-        kafkaProducerPollIntervalMs,
-        kafkaNumberOfPartitions,
-        kafkaReplicationFactor,
-        kafkaMaxBatchSize,
-        kafkaSslCACertFilePath,
-        eventHubConnString,
-    );
-    const reverseProducer = services.createProducer(
-        kafkaLibrary,
-        kafkaEndpoint,
-        kafkaReverseClientId,
-        reverseSendTopic,
-        false,
-        kafkaProducerPollIntervalMs,
-        kafkaNumberOfPartitions,
-        kafkaReplicationFactor,
-        kafkaMaxBatchSize,
-        kafkaSslCACertFilePath,
-        eventHubConnString,
-    );
+	const forwardProducer = services.createProducer(
+		kafkaLibrary,
+		kafkaEndpoint,
+		kafkaForwardClientId,
+		forwardSendTopic,
+		true,
+		kafkaProducerPollIntervalMs,
+		kafkaNumberOfPartitions,
+		kafkaReplicationFactor,
+		kafkaMaxBatchSize,
+		kafkaSslCACertFilePath,
+		eventHubConnString,
+	);
+	const reverseProducer = services.createProducer(
+		kafkaLibrary,
+		kafkaEndpoint,
+		kafkaReverseClientId,
+		reverseSendTopic,
+		false,
+		kafkaProducerPollIntervalMs,
+		kafkaNumberOfPartitions,
+		kafkaReplicationFactor,
+		kafkaMaxBatchSize,
+		kafkaSslCACertFilePath,
+		eventHubConnString,
+	);
 
-    const redisConfig = config.get("redis");
-    const redisOptions: RedisOptions = {
-        host: redisConfig.host,
-        port: redisConfig.port,
-        password: redisConfig.pass,
-    };
-    if (redisConfig.tls) {
-        redisOptions.tls = {
-            servername: redisConfig.host,
-        };
-    }
-    const publisher = new services.SocketIoRedisPublisher(redisOptions);
-    publisher.on("error", (err) => {
-        winston.error("Error with Redis Publisher:", err);
-        Lumberjack.error("Error with Redis Publisher:", undefined, err);
-    });
+	const redisConfig = config.get("redis");
+	const redisOptions: RedisOptions = {
+		host: redisConfig.host,
+		port: redisConfig.port,
+		password: redisConfig.pass,
+	};
+	if (redisConfig.tls) {
+		redisOptions.tls = {
+			servername: redisConfig.host,
+		};
+	}
+	const publisher = new services.SocketIoRedisPublisher(redisOptions);
+	publisher.on("error", (err) => {
+		winston.error("Error with Redis Publisher:", err);
+		Lumberjack.error("Error with Redis Publisher:", undefined, err);
+	});
 
-    const localContext = new LocalContext(winston);
+	const localContext = new LocalContext(winston);
 
-    const localProducer = new LocalKafka();
-    const combinedProducer = new core.CombinedProducer([forwardProducer, localProducer], true);
+	const localProducer = new LocalKafka();
+	const combinedProducer = new core.CombinedProducer([forwardProducer, localProducer], true);
 
-    const broadcasterLambda = new LocalLambdaController(
-        localProducer,
-        undefined,
-        localContext,
-        async (_, context: LocalContext) =>
-            new BroadcasterLambda(publisher, context, core.DefaultServiceConfiguration, undefined),
-    );
+	const broadcasterLambda = new LocalLambdaController(
+		localProducer,
+		undefined,
+		localContext,
+		async (_, context: LocalContext) =>
+			new BroadcasterLambda(publisher, context, core.DefaultServiceConfiguration, undefined),
+	);
 
-    await broadcasterLambda.start();
+	await broadcasterLambda.start();
 
-    const externalOrdererUrl: string = config.get("worker:serverUrl");
-    const enforceDiscoveryFlow: boolean = config.get("worker:enforceDiscoveryFlow");
-    const serviceConfiguration: core.IServiceConfiguration = {
-        ...core.DefaultServiceConfiguration,
-        externalOrdererUrl,
-        enforceDiscoveryFlow,
-    };
+	const externalOrdererUrl: string = config.get("worker:serverUrl");
+	const enforceDiscoveryFlow: boolean = config.get("worker:enforceDiscoveryFlow");
+	const serviceConfiguration: core.IServiceConfiguration = {
+		...core.DefaultServiceConfiguration,
+		externalOrdererUrl,
+		enforceDiscoveryFlow,
+	};
 
-    const checkpointService = new core.CheckpointService(
-        checkpointRepository,
-        documentRepository,
-        localCheckpointEnabled,
-    );
+	const checkpointService = new core.CheckpointService(
+		checkpointRepository,
+		documentRepository,
+		localCheckpointEnabled,
+	);
 
-    return new DeliLambdaFactory(
-        operationsDbManager,
-        documentRepository,
-        checkpointService,
-        tenantManager,
-        undefined,
-        combinedProducer,
-        undefined,
-        reverseProducer,
-        serviceConfiguration,
-        restartOnCheckpointFailure,
-        kafkaCheckpointOnReprocessingOp,
-    );
+	return new DeliLambdaFactory(
+		operationsDbManager,
+		documentRepository,
+		checkpointService,
+		tenantManager,
+		undefined,
+		combinedProducer,
+		undefined,
+		reverseProducer,
+		serviceConfiguration,
+		restartOnCheckpointFailure,
+		kafkaCheckpointOnReprocessingOp,
+	);
 }
 
 export async function create(
-    config: Provider,
-    customizations?: Record<string, any>,
+	config: Provider,
+	customizations?: Record<string, any>,
 ): Promise<core.IPartitionLambdaFactory> {
-    // Nconf has problems with prototype methods which prevents us from storing this as a class
-    config.set("documentLambda", { create: deliCreate });
-    return createDocumentRouter(config, customizations);
+	// Nconf has problems with prototype methods which prevents us from storing this as a class
+	config.set("documentLambda", { create: deliCreate });
+	return createDocumentRouter(config, customizations);
 }
--- server/routerlicious/packages/routerlicious/src/scribe/index.ts
+++ server/routerlicious/packages/routerlicious/src/scribe/index.ts
@@ -6,175 +6,175 @@
 import { ScribeLambdaFactory } from "@fluidframework/server-lambdas";
 import { createDocumentRouter } from "@fluidframework/server-routerlicious-base";
 import {
-    createProducer,
-    getDbFactory,
-    DeltaManager,
-    TenantManager,
+	createProducer,
+	getDbFactory,
+	DeltaManager,
+	TenantManager,
 } from "@fluidframework/server-services";
 import * as core from "@fluidframework/server-services-core";
 import {
-    DefaultServiceConfiguration,
-    ICheckpointHeuristicsServerConfiguration,
-    ICheckpoint,
-    IDb,
-    IDocument,
-    IPartitionLambdaFactory,
-    ISequencedOperationMessage,
-    IServiceConfiguration,
-    MongoDocumentRepository,
-    MongoManager,
+	DefaultServiceConfiguration,
+	ICheckpointHeuristicsServerConfiguration,
+	ICheckpoint,
+	IDb,
+	IDocument,
+	IPartitionLambdaFactory,
+	ISequencedOperationMessage,
+	IServiceConfiguration,
+	MongoDocumentRepository,
+	MongoManager,
 } from "@fluidframework/server-services-core";
 import { Provider } from "nconf";
 
 export async function scribeCreate(
-    config: Provider,
-    customizations?: Record<string, any>,
+	config: Provider,
+	customizations?: Record<string, any>,
 ): Promise<IPartitionLambdaFactory> {
-    // Access config values
-    const globalDbEnabled = config.get("mongo:globalDbEnabled") as boolean;
-    const documentsCollectionName = config.get("mongo:collectionNames:documents");
-    const checkpointsCollectionName = config.get("mongo:collectionNames:checkpoints");
-    const messagesCollectionName = config.get("mongo:collectionNames:scribeDeltas");
-    const createCosmosDBIndexes = config.get("mongo:createCosmosDBIndexes");
+	// Access config values
+	const globalDbEnabled = config.get("mongo:globalDbEnabled") as boolean;
+	const documentsCollectionName = config.get("mongo:collectionNames:documents");
+	const checkpointsCollectionName = config.get("mongo:collectionNames:checkpoints");
+	const messagesCollectionName = config.get("mongo:collectionNames:scribeDeltas");
+	const createCosmosDBIndexes = config.get("mongo:createCosmosDBIndexes");
 
-    const kafkaEndpoint = config.get("kafka:lib:endpoint");
-    const kafkaLibrary = config.get("kafka:lib:name");
-    const kafkaProducerPollIntervalMs = config.get("kafka:lib:producerPollIntervalMs");
-    const kafkaNumberOfPartitions = config.get("kafka:lib:numberOfPartitions");
-    const kafkaReplicationFactor = config.get("kafka:lib:replicationFactor");
-    const kafkaMaxBatchSize = config.get("kafka:lib:maxBatchSize");
-    const kafkaSslCACertFilePath: string = config.get("kafka:lib:sslCACertFilePath");
-    const eventHubConnString: string = config.get("kafka:lib:eventHubConnString");
-    const sendTopic = config.get("lambdas:deli:topic");
-    const kafkaClientId = config.get("scribe:kafkaClientId");
-    const mongoExpireAfterSeconds = config.get("mongo:expireAfterSeconds") as number;
-    const enableWholeSummaryUpload = config.get("storage:enableWholeSummaryUpload") as boolean;
-    const internalHistorianUrl = config.get("worker:internalBlobStorageUrl");
-    const internalAlfredUrl = config.get("worker:alfredUrl");
-    const getDeltasViaAlfred = config.get("scribe:getDeltasViaAlfred") as boolean;
-    const verifyLastOpPersistence =
-        (config.get("scribe:verifyLastOpPersistence") as boolean) ?? false;
-    const transientTenants = config.get("shared:transientTenants") as string[];
-    const disableTransientTenantFiltering =
-        (config.get("scribe:disableTransientTenantFiltering") as boolean) ?? true;
-    const localCheckpointEnabled = config.get("checkpoints:localCheckpointEnabled") as boolean;
-    const restartOnCheckpointFailure =
-        (config.get("scribe:restartOnCheckpointFailure") as boolean) ?? true;
-    const kafkaCheckpointOnReprocessingOp =
-        (config.get("checkpoints:kafkaCheckpointOnReprocessingOp") as boolean) ?? true;
+	const kafkaEndpoint = config.get("kafka:lib:endpoint");
+	const kafkaLibrary = config.get("kafka:lib:name");
+	const kafkaProducerPollIntervalMs = config.get("kafka:lib:producerPollIntervalMs");
+	const kafkaNumberOfPartitions = config.get("kafka:lib:numberOfPartitions");
+	const kafkaReplicationFactor = config.get("kafka:lib:replicationFactor");
+	const kafkaMaxBatchSize = config.get("kafka:lib:maxBatchSize");
+	const kafkaSslCACertFilePath: string = config.get("kafka:lib:sslCACertFilePath");
+	const eventHubConnString: string = config.get("kafka:lib:eventHubConnString");
+	const sendTopic = config.get("lambdas:deli:topic");
+	const kafkaClientId = config.get("scribe:kafkaClientId");
+	const mongoExpireAfterSeconds = config.get("mongo:expireAfterSeconds") as number;
+	const enableWholeSummaryUpload = config.get("storage:enableWholeSummaryUpload") as boolean;
+	const internalHistorianUrl = config.get("worker:internalBlobStorageUrl");
+	const internalAlfredUrl = config.get("worker:alfredUrl");
+	const getDeltasViaAlfred = config.get("scribe:getDeltasViaAlfred") as boolean;
+	const verifyLastOpPersistence =
+		(config.get("scribe:verifyLastOpPersistence") as boolean) ?? false;
+	const transientTenants = config.get("shared:transientTenants") as string[];
+	const disableTransientTenantFiltering =
+		(config.get("scribe:disableTransientTenantFiltering") as boolean) ?? true;
+	const localCheckpointEnabled = config.get("checkpoints:localCheckpointEnabled") as boolean;
+	const restartOnCheckpointFailure =
+		(config.get("scribe:restartOnCheckpointFailure") as boolean) ?? true;
+	const kafkaCheckpointOnReprocessingOp =
+		(config.get("checkpoints:kafkaCheckpointOnReprocessingOp") as boolean) ?? true;
 
-    // Generate tenant manager which abstracts access to the underlying storage provider
-    const authEndpoint = config.get("auth:endpoint");
-    const tenantManager = new TenantManager(authEndpoint, internalHistorianUrl);
+	// Generate tenant manager which abstracts access to the underlying storage provider
+	const authEndpoint = config.get("auth:endpoint");
+	const tenantManager = new TenantManager(authEndpoint, internalHistorianUrl);
 
-    const deltaManager = new DeltaManager(authEndpoint, internalAlfredUrl);
-    const factory = await getDbFactory(config);
+	const deltaManager = new DeltaManager(authEndpoint, internalAlfredUrl);
+	const factory = await getDbFactory(config);
 
-    const checkpointHeuristics = config.get(
-        "scribe:checkpointHeuristics",
-    ) as ICheckpointHeuristicsServerConfiguration;
-    if (checkpointHeuristics?.enable) {
-        core.DefaultServiceConfiguration.scribe.checkpointHeuristics = checkpointHeuristics;
-    }
+	const checkpointHeuristics = config.get(
+		"scribe:checkpointHeuristics",
+	) as ICheckpointHeuristicsServerConfiguration;
+	if (checkpointHeuristics?.enable) {
+		core.DefaultServiceConfiguration.scribe.checkpointHeuristics = checkpointHeuristics;
+	}
 
-    let globalDb;
-    if (globalDbEnabled) {
-        const globalDbReconnect = (config.get("mongo:globalDbReconnect") as boolean) ?? false;
-        const globalDbMongoManager = new MongoManager(factory, globalDbReconnect, null, true);
-        globalDb = await globalDbMongoManager.getDatabase();
-    }
+	let globalDb;
+	if (globalDbEnabled) {
+		const globalDbReconnect = (config.get("mongo:globalDbReconnect") as boolean) ?? false;
+		const globalDbMongoManager = new MongoManager(factory, globalDbReconnect, null, true);
+		globalDb = await globalDbMongoManager.getDatabase();
+	}
 
-    const operationsDbManager = new MongoManager(factory, false);
-    const operationsDb = await operationsDbManager.getDatabase();
+	const operationsDbManager = new MongoManager(factory, false);
+	const operationsDb = await operationsDbManager.getDatabase();
 
-    const documentsCollectionDb: IDb = globalDbEnabled ? globalDb : operationsDb;
+	const documentsCollectionDb: IDb = globalDbEnabled ? globalDb : operationsDb;
 
-    const scribeDeltas =
-        operationsDb.collection<ISequencedOperationMessage>(messagesCollectionName);
-    const documentRepository =
-        customizations?.documentRepository ??
-        new MongoDocumentRepository(
-            documentsCollectionDb.collection<IDocument>(documentsCollectionName),
-        );
+	const scribeDeltas =
+		operationsDb.collection<ISequencedOperationMessage>(messagesCollectionName);
+	const documentRepository =
+		customizations?.documentRepository ??
+		new MongoDocumentRepository(
+			documentsCollectionDb.collection<IDocument>(documentsCollectionName),
+		);
 
-    const checkpointRepository = new core.MongoCheckpointRepository(
-        operationsDb.collection<ICheckpoint>(checkpointsCollectionName),
-        "scribe",
-    );
+	const checkpointRepository = new core.MongoCheckpointRepository(
+		operationsDb.collection<ICheckpoint>(checkpointsCollectionName),
+		"scribe",
+	);
 
-    if (createCosmosDBIndexes) {
-        await scribeDeltas.createIndex({ documentId: 1 }, false);
-        await scribeDeltas.createIndex({ tenantId: 1 }, false);
-        await scribeDeltas.createIndex({ "operation.sequenceNumber": 1 }, false);
-    } else {
-        await scribeDeltas.createIndex(
-            {
-                "documentId": 1,
-                "operation.sequenceNumber": 1,
-                "tenantId": 1,
-            },
-            true,
-        );
-    }
+	if (createCosmosDBIndexes) {
+		await scribeDeltas.createIndex({ documentId: 1 }, false);
+		await scribeDeltas.createIndex({ tenantId: 1 }, false);
+		await scribeDeltas.createIndex({ "operation.sequenceNumber": 1 }, false);
+	} else {
+		await scribeDeltas.createIndex(
+			{
+				"documentId": 1,
+				"operation.sequenceNumber": 1,
+				"tenantId": 1,
+			},
+			true,
+		);
+	}
 
-    if (mongoExpireAfterSeconds > 0) {
-        await (createCosmosDBIndexes
-            ? scribeDeltas.createTTLIndex({ _ts: 1 }, mongoExpireAfterSeconds)
-            : scribeDeltas.createTTLIndex({ mongoTimestamp: 1 }, mongoExpireAfterSeconds));
-    }
+	if (mongoExpireAfterSeconds > 0) {
+		await (createCosmosDBIndexes
+			? scribeDeltas.createTTLIndex({ _ts: 1 }, mongoExpireAfterSeconds)
+			: scribeDeltas.createTTLIndex({ mongoTimestamp: 1 }, mongoExpireAfterSeconds));
+	}
 
-    const producer = createProducer(
-        kafkaLibrary,
-        kafkaEndpoint,
-        kafkaClientId,
-        sendTopic,
-        false,
-        kafkaProducerPollIntervalMs,
-        kafkaNumberOfPartitions,
-        kafkaReplicationFactor,
-        kafkaMaxBatchSize,
-        kafkaSslCACertFilePath,
-        eventHubConnString,
-    );
+	const producer = createProducer(
+		kafkaLibrary,
+		kafkaEndpoint,
+		kafkaClientId,
+		sendTopic,
+		false,
+		kafkaProducerPollIntervalMs,
+		kafkaNumberOfPartitions,
+		kafkaReplicationFactor,
+		kafkaMaxBatchSize,
+		kafkaSslCACertFilePath,
+		eventHubConnString,
+	);
 
-    const externalOrdererUrl = config.get("worker:serverUrl");
-    const enforceDiscoveryFlow: boolean = config.get("worker:enforceDiscoveryFlow");
-    const serviceConfiguration: IServiceConfiguration = {
-        ...DefaultServiceConfiguration,
-        externalOrdererUrl,
-        enforceDiscoveryFlow,
-    };
+	const externalOrdererUrl = config.get("worker:serverUrl");
+	const enforceDiscoveryFlow: boolean = config.get("worker:enforceDiscoveryFlow");
+	const serviceConfiguration: IServiceConfiguration = {
+		...DefaultServiceConfiguration,
+		externalOrdererUrl,
+		enforceDiscoveryFlow,
+	};
 
-    const checkpointService = new core.CheckpointService(
-        checkpointRepository,
-        documentRepository,
-        localCheckpointEnabled,
-    );
+	const checkpointService = new core.CheckpointService(
+		checkpointRepository,
+		documentRepository,
+		localCheckpointEnabled,
+	);
 
-    return new ScribeLambdaFactory(
-        operationsDbManager,
-        documentRepository,
-        scribeDeltas,
-        producer,
-        deltaManager,
-        tenantManager,
-        serviceConfiguration,
-        enableWholeSummaryUpload,
-        getDeltasViaAlfred,
-        verifyLastOpPersistence,
-        transientTenants,
-        disableTransientTenantFiltering,
-        checkpointService,
-        restartOnCheckpointFailure,
-        kafkaCheckpointOnReprocessingOp,
-    );
+	return new ScribeLambdaFactory(
+		operationsDbManager,
+		documentRepository,
+		scribeDeltas,
+		producer,
+		deltaManager,
+		tenantManager,
+		serviceConfiguration,
+		enableWholeSummaryUpload,
+		getDeltasViaAlfred,
+		verifyLastOpPersistence,
+		transientTenants,
+		disableTransientTenantFiltering,
+		checkpointService,
+		restartOnCheckpointFailure,
+		kafkaCheckpointOnReprocessingOp,
+	);
 }
 
 export async function create(
-    config: Provider,
-    customizations?: Record<string, any>,
+	config: Provider,
+	customizations?: Record<string, any>,
 ): Promise<IPartitionLambdaFactory> {
-    // Nconf has problems with prototype methods which prevents us from storing this as a class
-    config.set("documentLambda", { create: scribeCreate });
-    return createDocumentRouter(config, customizations);
+	// Nconf has problems with prototype methods which prevents us from storing this as a class
+	config.set("documentLambda", { create: scribeCreate });
+	return createDocumentRouter(config, customizations);
 }
