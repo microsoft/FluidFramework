76d71e41e8d409a44bb68d60e5272ac4e0634735
Arik Ter-Galstyan <arikt@microsoft.com>
2023-06-07 14:00:35 -0700

    Remove temp logging.

--- server/routerlicious/packages/lambdas/src/alfred/index.ts
+++ server/routerlicious/packages/lambdas/src/alfred/index.ts
@@ -867,6 +867,13 @@ export function configureWebSocketServices(
 				[CommonProperties.roomClients]: JSON.stringify(Array.from(roomMap.keys())),
 			});
 
+			if (roomMap.size >= 1) {
+				const rooms = Array.from(roomMap.values());
+				const documentId = rooms[0].documentId;
+				const tenantId = rooms[0].tenantId;
+				disconnectMetric.setProperties({ ...getLumberBaseProperties(documentId, tenantId) });
+			}
+
 			try {
 				clearExpirationTimer();
 				const removeAndStoreP: Promise<void>[] = [];
@@ -938,16 +945,7 @@ export function configureWebSocketServices(
 					socketTracker.removeSocket(socket.id);
 				}
 				await Promise.all(removeAndStoreP);
-				if (roomMap.size >= 1) {
-					const rooms = Array.from(roomMap.values());
-					const documentId = rooms[0].documentId;
-					const tenantId = rooms[0].tenantId;
-					const clients = await clientManager.getClients(tenantId, documentId);
-					disconnectMetric.setProperties({
-						...getLumberBaseProperties(documentId, tenantId),
-						[CommonProperties.clientCount]: clients.length,
-					});
-				}
+				
 				disconnectMetric.success(`Successfully disconnected.`);
 			} catch (error) {
 				disconnectMetric.error(`Disconnect failed.`, error);
