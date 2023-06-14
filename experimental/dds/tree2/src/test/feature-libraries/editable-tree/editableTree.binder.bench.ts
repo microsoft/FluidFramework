7b52a409fd839e79264f9a3c7d2a4aeb79256167
Dragos Stanescu <dstanesc@gmail.com>
2023-06-14 14:24:08 -0400

    benchmark: listener dispatch scalability

--- experimental/dds/tree2/src/test/feature-libraries/editable-tree/editableTree.binder.bench.ts
+++ experimental/dds/tree2/src/test/feature-libraries/editable-tree/editableTree.binder.bench.ts
@@ -149,6 +149,68 @@ describe("Data binder benchmarks", () => {
 		});
 	});
 
+	describe("Buffering data binder, invoke 10,000nd listener of 20,000", () => {
+		const { tree, root, address } = retrieveNodes();
+		const bindTree: BindTree = compileSyntaxTree({ address: true });
+		const options: FlushableBinderOptions<ViewEvents> = createFlushableBinderOptions({
+			autoFlushPolicy: "afterBatch",
+			matchPolicy: "subtree",
+		});
+		const dataBinder: FlushableDataBinder<OperationBinderEvents> = createDataBinderBuffering(
+			tree.events,
+			options,
+		);
+
+		for (let i = 0; i < 10000; i++) {
+			dataBinder.register(
+				root,
+				BindingType.Insert,
+				[bindTree],
+				(insertContext: InsertBindingContext) => {},
+			);
+		}
+
+		const promise = new Promise<void>((resolve) => {
+			dataBinder.register(
+				root,
+				BindingType.Insert,
+				[bindTree],
+				(insertContext: InsertBindingContext) => {
+					resolve();
+				},
+			);
+		});
+
+		for (let i = 0; i < 10000; i++) {
+			dataBinder.register(
+				root,
+				BindingType.Insert,
+				[bindTree],
+				(insertContext: InsertBindingContext) => {},
+			);
+		}
+
+		benchmark({
+			type: BenchmarkType.Measurement,
+			title: `Buffering data binder: single insert callback, invoke 10,000nd listener of 20,000`,
+			after: () => {
+				dataBinder.unregisterAll();
+			},
+			benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
+				let duration = 0;
+				do {
+					address.phones = [111, 112];
+					const before = state.timer.now();
+					await promise;
+					const after = state.timer.now();
+					duration = state.timer.toSeconds(before, after);
+				} while (state.recordBatch(duration));
+			},
+			minBatchDurationSeconds: 0,
+			maxBenchmarkDurationSeconds: 1,
+		});
+	});
+
 	describe("Buffering data binder, batched notification", () => {
 		const { tree, root, address } = retrieveNodes();
 		const bindTree: BindTree = compileSyntaxTree({ address: true });
