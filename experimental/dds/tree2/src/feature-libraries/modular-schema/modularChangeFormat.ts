77bc82b7904f04ca84f0b2f1783d937150265d5e
yann-achard-MS <97201204+yann-achard-MS@users.noreply.github.com>
2023-06-15 15:28:51 -0700

    Update experimental/dds/tree2/src/feature-libraries/modular-schema/modularChangeFormat.ts
    Co-authored-by: Abram Sanderson <Abram.sanderson@gmail.com>

--- experimental/dds/tree2/src/feature-libraries/modular-schema/modularChangeFormat.ts
+++ experimental/dds/tree2/src/feature-libraries/modular-schema/modularChangeFormat.ts
@@ -43,8 +43,7 @@ const EncodedFieldChange = Type.Object({
 	fieldKind: FieldKindIdentifierSchema,
 	// Implementation note: node and field change encoding is mutually recursive.
 	// This field marks a boundary in that recursion to avoid constructing excessively complex
-	// recursive types. Encoded changes are validated at this boundary at runtime--see logic
-	// later in this file's codec.
+	// recursive types. Encoded changes are validated at this boundary at runtime--see modularChangeCodecs.ts.
 	change: JsonCompatibleReadOnlySchema,
 });
 
