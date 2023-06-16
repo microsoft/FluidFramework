7e9700a1119ecac0b7df1e0443d5d1ba505ef65e
Dragos Stanescu <dstanesc@gmail.com>
2023-06-16 13:15:04 -0400

    Adjust benchmarks to measure correct dispatch time

--- experimental/dds/tree2/src/test/feature-libraries/editable-tree/editableTree.binder.bench.ts
+++ experimental/dds/tree2/src/test/feature-libraries/editable-tree/editableTree.binder.bench.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import { benchmark, BenchmarkTimer, BenchmarkType } from "@fluid-tools/benchmark";
 import {
 	BatchBindingContext,
@@ -15,6 +16,7 @@ import {
 	createDataBinderInvalidating,
 	createFlushableBinderOptions,
 	DataBinder,
+	EditableTree,
 	FlushableBinderOptions,
 	FlushableDataBinder,
 	InsertBindingContext,
@@ -32,35 +34,28 @@ describe("Data binder benchmarks", () => {
 		const options: BinderOptions = createBinderOptions({
 			matchPolicy: "subtree",
 		});
-		const dataBinder: DataBinder<OperationBinderEvents> = createDataBinderDirect(
-			tree.events,
-			options,
-		);
-		const promise = new Promise<void>((resolve) => {
-			dataBinder.register(
-				root,
-				BindingType.Insert,
-				[bindTree],
-				(insertContext: InsertBindingContext) => {
-					resolve();
-				},
-			);
-		});
 		benchmark({
 			type: BenchmarkType.Measurement,
 			title: `Direct data binder: single insert callback`,
-			after: () => {
-				dataBinder.unregisterAll();
-			},
 			benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
-				let duration = 0;
+				let time = 0;
 				do {
-					address.phones = [111, 112];
-					const before = state.timer.now();
-					await promise;
-					const after = state.timer.now();
-					duration = state.timer.toSeconds(before, after);
-				} while (state.recordBatch(duration));
+					const dataBinder: DataBinder<OperationBinderEvents> = createDataBinderDirect(
+						tree.events,
+						options,
+					);
+					const promise = new Promise<void>((resolve) => {
+						dataBinder.register(
+							root,
+							BindingType.Insert,
+							[bindTree],
+							(insertContext: InsertBindingContext) => {
+								resolve();
+							},
+						);
+					});
+					time = await computeTime<T>(state, address, promise, dataBinder);
+				} while (state.recordBatch(time));
 			},
 			minBatchDurationSeconds: 0,
 			maxBenchmarkDurationSeconds: 1,
@@ -74,33 +69,26 @@ describe("Data binder benchmarks", () => {
 			autoFlushPolicy: "afterBatch",
 			matchPolicy: "subtree",
 		});
-		const dataBinder: FlushableDataBinder<InvalidationBinderEvents> =
-			createDataBinderInvalidating(tree.events, options);
-		const promise = new Promise<void>((resolve) => {
-			dataBinder.register(
-				root,
-				BindingType.Invalidation,
-				[bindTree],
-				(insertContext: InvalidationBindingContext) => {
-					resolve();
-				},
-			);
-		});
 		benchmark({
 			type: BenchmarkType.Measurement,
 			title: `Invalidation data binder: single insert invalidation callback`,
-			after: () => {
-				dataBinder.unregisterAll();
-			},
 			benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
-				let duration = 0;
+				let time = 0;
 				do {
-					address.phones = [111, 112];
-					const before = state.timer.now();
-					await promise;
-					const after = state.timer.now();
-					duration = state.timer.toSeconds(before, after);
-				} while (state.recordBatch(duration));
+					const dataBinder: FlushableDataBinder<InvalidationBinderEvents> =
+						createDataBinderInvalidating(tree.events, options);
+					const promise = new Promise<void>((resolve) => {
+						dataBinder.register(
+							root,
+							BindingType.Invalidation,
+							[bindTree],
+							(insertContext: InvalidationBindingContext) => {
+								resolve();
+							},
+						);
+					});
+					time = await computeTime<T>(state, address, promise, dataBinder);
+				} while (state.recordBatch(time));
 			},
 			minBatchDurationSeconds: 0,
 			maxBenchmarkDurationSeconds: 1,
@@ -114,103 +102,70 @@ describe("Data binder benchmarks", () => {
 			autoFlushPolicy: "afterBatch",
 			matchPolicy: "subtree",
 		});
-		const dataBinder: FlushableDataBinder<OperationBinderEvents> = createDataBinderBuffering(
-			tree.events,
-			options,
-		);
-		const promise = new Promise<void>((resolve) => {
-			dataBinder.register(
-				root,
-				BindingType.Insert,
-				[bindTree],
-				(insertContext: InsertBindingContext) => {
-					resolve();
-				},
-			);
-		});
 		benchmark({
 			type: BenchmarkType.Measurement,
 			title: `Buffering data binder: single insert callback`,
-			after: () => {
-				dataBinder.unregisterAll();
-			},
 			benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
-				let duration = 0;
+				let time = 0;
 				do {
-					address.phones = [111, 112];
-					const before = state.timer.now();
-					await promise;
-					const after = state.timer.now();
-					duration = state.timer.toSeconds(before, after);
-				} while (state.recordBatch(duration));
+					const dataBinder: FlushableDataBinder<OperationBinderEvents> =
+						createDataBinderBuffering(tree.events, options);
+					const promise = new Promise<void>((resolve) => {
+						dataBinder.register(
+							root,
+							BindingType.Insert,
+							[bindTree],
+							(insertContext: InsertBindingContext) => {
+								resolve();
+							},
+						);
+					});
+					time = await computeTime<T>(state, address, promise, dataBinder);
+				} while (state.recordBatch(time));
 			},
 			minBatchDurationSeconds: 0,
 			maxBenchmarkDurationSeconds: 1,
 		});
 	});
 
-	describe("Buffering data binder, invoke 10,000nd listener of 20,000", () => {
-		const { tree, root, address } = retrieveNodes();
-		const bindTree: BindTree = compileSyntaxTree({ address: true });
-		const options: FlushableBinderOptions<ViewEvents> = createFlushableBinderOptions({
-			autoFlushPolicy: "afterBatch",
-			matchPolicy: "subtree",
-		});
-		const dataBinder: FlushableDataBinder<OperationBinderEvents> = createDataBinderBuffering(
-			tree.events,
-			options,
-		);
-
-		for (let i = 0; i < 10000; i++) {
-			dataBinder.register(
-				root,
-				BindingType.Insert,
-				[bindTree],
-				(insertContext: InsertBindingContext) => {},
-			);
-		}
-
-		const promise = new Promise<void>((resolve) => {
-			dataBinder.register(
-				root,
-				BindingType.Insert,
-				[bindTree],
-				(insertContext: InsertBindingContext) => {
-					resolve();
+	for (const listeners of [10, 50, 100, 500, 1000]) {
+		describe(`Buffering data binder, invoke ${listeners} listener of ${2 * listeners}`, () => {
+			const { tree, root, address } = retrieveNodes();
+			const bindTree: BindTree = compileSyntaxTree({ address: true });
+			const options: FlushableBinderOptions<ViewEvents> = createFlushableBinderOptions({
+				autoFlushPolicy: "afterBatch",
+				matchPolicy: "subtree",
+			});
+			benchmark({
+				type: BenchmarkType.Measurement,
+				title: `Buffering data binder: single insert callback, invoke  ${listeners} listener of ${
+					2 * listeners
+				}`,
+				benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
+					let time = 0;
+					do {
+						const dataBinder: FlushableDataBinder<OperationBinderEvents> =
+							createDataBinderBuffering(tree.events, options);
+						registerLoop(listeners, dataBinder, root, bindTree);
+						const promise = new Promise<void>((resolve) => {
+							dataBinder.register(
+								root,
+								BindingType.Insert,
+								[bindTree],
+								(insertContext: InsertBindingContext) => {
+									resolve();
+								},
+							);
+						});
+						registerLoop(listeners, dataBinder, root, bindTree);
+						time = await computeTime<T>(state, address, promise, dataBinder);
+					} while (state.recordBatch(time));
 				},
-			);
+				minBatchDurationSeconds: 0,
+				maxBenchmarkDurationSeconds: 2,
+			});
 		});
-
-		for (let i = 0; i < 10000; i++) {
-			dataBinder.register(
-				root,
-				BindingType.Insert,
-				[bindTree],
-				(insertContext: InsertBindingContext) => {},
-			);
-		}
-
-		benchmark({
-			type: BenchmarkType.Measurement,
-			title: `Buffering data binder: single insert callback, invoke 10,000nd listener of 20,000`,
-			after: () => {
-				dataBinder.unregisterAll();
-			},
-			benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
-				let duration = 0;
-				do {
-					address.phones = [111, 112];
-					const before = state.timer.now();
-					await promise;
-					const after = state.timer.now();
-					duration = state.timer.toSeconds(before, after);
-				} while (state.recordBatch(duration));
-			},
-			minBatchDurationSeconds: 0,
-			maxBenchmarkDurationSeconds: 1,
-		});
-	});
-
+	}
 	describe("Buffering data binder, batched notification", () => {
 		const { tree, root, address } = retrieveNodes();
 		const bindTree: BindTree = compileSyntaxTree({ address: true });
@@ -218,39 +173,64 @@ describe("Data binder benchmarks", () => {
 			autoFlushPolicy: "afterBatch",
 			matchPolicy: "subtree",
 		});
-		const dataBinder: FlushableDataBinder<OperationBinderEvents> = createDataBinderBuffering(
-			tree.events,
-			options,
-		);
-		dataBinder.register(root, BindingType.Insert, [bindTree]);
-		const promise = new Promise<void>((resolve) => {
-			dataBinder.register(
-				root,
-				BindingType.Batch,
-				[bindTree],
-				(batchContext: BatchBindingContext) => {
-					resolve();
-				},
-			);
-		});
 		benchmark({
 			type: BenchmarkType.Measurement,
 			title: `Buffering data binder: batch callback`,
-			after: () => {
-				dataBinder.unregisterAll();
-			},
 			benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
-				let duration = 0;
+				let time = 0;
 				do {
-					address.phones = [111, 112];
-					const before = state.timer.now();
-					await promise;
-					const after = state.timer.now();
-					duration = state.timer.toSeconds(before, after);
-				} while (state.recordBatch(duration));
+					const dataBinder: FlushableDataBinder<OperationBinderEvents> =
+						createDataBinderBuffering(tree.events, options);
+					dataBinder.register(root, BindingType.Insert, [bindTree]);
+					const promise = new Promise<void>((resolve) => {
+						dataBinder.register(
+							root,
+							BindingType.Batch,
+							[bindTree],
+							(batchContext: BatchBindingContext) => {
+								resolve();
+							},
+						);
+					});
+					time = await computeTime<T>(state, address, promise, dataBinder);
+				} while (state.recordBatch(time));
 			},
 			minBatchDurationSeconds: 0,
 			maxBenchmarkDurationSeconds: 1,
 		});
 	});
 });
+function registerLoop(
+	listeners: number,
+	dataBinder: FlushableDataBinder<OperationBinderEvents>,
+	root: EditableTree,
+	bindTree: BindTree,
+) {
+	for (let i = 0; i < listeners; i++) {
+		dataBinder.register(
+			root,
+			BindingType.Insert,
+			[bindTree],
+			(insertContext: InsertBindingContext) => {},
+		);
+	}
+}
+
+async function computeTime<T>(
+	state: BenchmarkTimer<T>,
+	address: EditableTree,
+	promise: Promise<void>,
+	dataBinder: DataBinder<OperationBinderEvents>,
+) {
+	const before1 = state.timer.now();
+	address.phones = [111, 112];
+	await promise;
+	const after1 = state.timer.now();
+	const duration1 = state.timer.toSeconds(before1, after1);
+	dataBinder.unregisterAll();
+	const before2 = state.timer.now();
+	address.phones = [111, 112];
+	const after2 = state.timer.now();
+	const duration2 = state.timer.toSeconds(before2, after2);
+	return duration1 - duration2;
+}
