cbc9a8db38e90f4fff00a901dbdb316eb898d4f7
Andrei Iacob <andreiiacob@microsoft.com>
2023-06-19 16:03:53 -0700

    Unused import

--- packages/test/test-end-to-end-tests/src/test/opReentrancy.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/opReentrancy.spec.ts
@@ -17,7 +17,6 @@ import {
 import { describeNoCompat, itExpects } from "@fluid-internal/test-version-utils";
 import { SharedString } from "@fluidframework/sequence";
 import { IContainer } from "@fluidframework/container-definitions";
-import { IMergeTreeInsertMsg } from "@fluidframework/merge-tree";
 import { FlushMode } from "@fluidframework/runtime-definitions";
 
 describeNoCompat("Concurrent op processing via DDS event handlers", (getTestObjectProvider) => {
