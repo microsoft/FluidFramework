7bf6fecf13886f418eb035e1a69ec64c4877505f
yann-achard-MS <97201204+yann-achard-MS@users.noreply.github.com>
2023-06-19 15:16:58 -0700

    test(tree): Edit manager perf tests (#16055)

---
+++ experimental/dds/tree2/src/test/shared-tree-core/editManager.bench.ts
@@ -0,0 +1,100 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "assert";
+import { benchmark, BenchmarkTimer, BenchmarkType } from "@fluid-tools/benchmark";
+import { NoOpChangeRebaser } from "../testChange";
+import {
+	rebaseLocalEditsOverTrunkEdits,
+	rebasePeerEditsOverTrunkEdits,
+} from "./editManagerTestUtils";
+
+describe("EditManager - Bench", () => {
+	interface Scenario {
+		readonly type: BenchmarkType;
+		readonly rebasedEditCount: number;
+		readonly trunkEditCount: number;
+	}
+
+	const scenarios: Scenario[] = [
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 1, trunkEditCount: 1 },
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 10, trunkEditCount: 1 },
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 100, trunkEditCount: 1 },
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 1000, trunkEditCount: 1 },
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 1, trunkEditCount: 10 },
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 1, trunkEditCount: 100 },
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 1, trunkEditCount: 1000 },
+		{ type: BenchmarkType.Measurement, rebasedEditCount: 100, trunkEditCount: 100 },
+	];
+
+	describe("Local commit rebasing", () => {
+		for (const { type, rebasedEditCount, trunkEditCount } of scenarios) {
+			benchmark({
+				type,
+				title: `Rebase ${rebasedEditCount} local commits over ${trunkEditCount} trunk commits`,
+				benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
+					let duration: number;
+					do {
+						// Since this setup one collects data from one iteration, assert that this is what is expected.
+						assert.equal(state.iterationsPerBatch, 1);
+
+						// Setup
+						const rebaser = new NoOpChangeRebaser();
+						const rebasing = rebaseLocalEditsOverTrunkEdits(
+							rebasedEditCount,
+							trunkEditCount,
+							rebaser,
+							true,
+						);
+						assert(typeof rebasing === "function");
+
+						// Measure
+						const before = state.timer.now();
+						rebasing();
+						const after = state.timer.now();
+						duration = state.timer.toSeconds(before, after);
+						// Collect data
+					} while (state.recordBatch(duration));
+				},
+				// Force batch size of 1
+				minBatchDurationSeconds: 0,
+			});
+		}
+	});
+	describe("Peer commit rebasing", () => {
+		for (const { type, rebasedEditCount, trunkEditCount } of scenarios) {
+			benchmark({
+				type,
+				title: `Rebase ${rebasedEditCount} peer commits over ${trunkEditCount} trunk commits`,
+				benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
+					let duration: number;
+					do {
+						// Since this setup one collects data from one iteration, assert that this is what is expected.
+						assert.equal(state.iterationsPerBatch, 1);
+
+						// Setup
+						const rebaser = new NoOpChangeRebaser();
+						const rebasing = rebasePeerEditsOverTrunkEdits(
+							rebasedEditCount,
+							trunkEditCount,
+							rebaser,
+							true,
+						);
+						assert(typeof rebasing === "function");
+
+						// Measure
+						const before = state.timer.now();
+						rebasing();
+						const after = state.timer.now();
+						duration = state.timer.toSeconds(before, after);
+						// Collect data
+					} while (state.recordBatch(duration));
+				},
+				// Force batch size of 1
+				minBatchDurationSeconds: 0,
+			});
+		}
+	});
+});
