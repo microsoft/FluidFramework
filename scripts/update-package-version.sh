a2a54a66a77d14acd95e0d52269544883e634ab4
tyler-cai-microsoft <tylercai@microsoft.com>
2023-06-19 11:09:22 -0700

    Fix update-package-version bash

--- scripts/update-package-version.sh
+++ scripts/update-package-version.sh
@@ -16,8 +16,13 @@ if [ -f "lerna.json" ]; then
     # this is a non-release build of a release group, so always use exact interdependencies, otherwise
     # dev/test builds may accidentally bring in different versions.
     echo "release group non-release build"
-    echo command="flub bump $RELEASE_GROUP --exact $SETVERSION_CODEVERSION --exactDepType=\"\" -xv"
-    flub bump $RELEASE_GROUP --exact $SETVERSION_CODEVERSION --exactDepType="" -xv
+	if [[ -z "$SETVERSION_CODEVERSION" ]]; then
+		echo command="flub bump $RELEASE_GROUP --exact $SETVERSION_CODEVERSION --exactDepType=\"\" -xv"
+		flub bump $RELEASE_GROUP --exact $SETVERSION_CODEVERSION --exactDepType="" -xv
+	else
+		echo command="flub bump $RELEASE_GROUP --exact $SETVERSION_VERSION --exactDepType=\"\" -xv"
+		flub bump $RELEASE_GROUP --exact $SETVERSION_VERSION --exactDepType="" -xv
+	fi
   fi
 else
   echo "independent package"
