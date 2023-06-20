96036266a179970e6707537905fe2469a8f63f0a
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2023-06-20 18:05:53 +0000

    wip tyler's feedback not tested

--- build-tools/packages/build-cli/src/commands/generate/changelog.ts
+++ build-tools/packages/build-cli/src/commands/generate/changelog.ts
@@ -4,12 +4,13 @@
  */
 
 import { Flags } from "@oclif/core";
-import { command, Options } from "execa";
+import { command as execCommand } from "execa";
 import { PackageCommand, PackageKind } from "../../BasePackageCommand";
 import { Repository } from "../../lib";
 import { readFile, writeFile } from "fs/promises";
 import { CleanOptions } from "simple-git";
-import { Package } from "@fluidframework/build-tools";
+import { Package, FluidRepo } from "@fluidframework/build-tools";
+import { isReleaseGroup } from "../../releaseGroups";
 
 async function replaceInFile(search: string, replace: string, path: string): Promise<void> {
 	const content = await readFile(path, "utf8");
@@ -36,7 +37,6 @@ export default class GenerateChangeLogCommand extends PackageCommand<
 		},
 	];
 
-	private options?: Options;
 	private repo?: Repository;
 
 	protected async processPackage(directory: string, kind: PackageKind): Promise<void> {
@@ -60,32 +60,41 @@ export default class GenerateChangeLogCommand extends PackageCommand<
 
 		const context = await this.getContext();
 
-		this.options = { cwd: context.gitRepo.resolvedRoot };
+		const gitRoot = context.gitRepo.resolvedRoot;
 
-		await command("pnpm exec changeset version", this.options);
+		const { releaseGroup } = this.flags;
 
-		// which property do I check on context so I can not install if its already installed?
-		await command(`pnpm i`, this.options);
+		if (releaseGroup === undefined) {
+			this.error("ReleaseGroup is possibly 'undefined'");
+		}
 
-		this.repo = new Repository({ baseDir: context.gitRepo.resolvedRoot });
+		const packagesToCheck = isReleaseGroup(releaseGroup)
+			? context.packagesInReleaseGroup(releaseGroup)
+			: // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			  [context.fullPackageMap.get(releaseGroup)!];
+
+		const installed = await FluidRepo.ensureInstalled(packagesToCheck, true);
+
+		await execCommand("pnpm exec changeset version", { cwd: gitRoot });
+
+		if (!installed) {
+			await execCommand(`pnpm i`, { cwd: gitRoot });
+		}
+
+		this.repo = new Repository({ baseDir: gitRoot });
 
 		await this.repo.gitClient.add(".changeset");
 	}
 
 	public async run(): Promise<void> {
 		if (this.repo === undefined) {
-			this.error("repo is possibly 'undefined'");
+			this.error("Repository is possibly 'undefined'");
 		}
 
 		// Calls processPackage on all packages.
 		await super.run();
 
-		await command(
-			"pnpm -r --workspace-concurrency=1 exec -- git add CHANGELOG.md",
-			this.options,
-		);
-
-		await this.repo.gitClient.raw("restore", ".");
+		await this.repo.gitClient.add("**CHANGELOG.md");
 
 		await this.repo.gitClient.clean(CleanOptions.RECURSIVE + CleanOptions.FORCE);
 
