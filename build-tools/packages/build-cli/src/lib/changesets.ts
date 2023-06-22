380c64780053ee162e9dc46a0e63c9326c58269f
Tyler Butler <tylerbu@microsoft.com>
2023-06-21 18:40:16 -0700

    cleanup

--- build-tools/packages/build-cli/src/lib/changesets.ts
+++ build-tools/packages/build-cli/src/lib/changesets.ts
@@ -4,7 +4,7 @@
  */
 
 import { VersionBumpType } from "@fluid-tools/version-tools";
-import { Logger } from "@fluidframework/build-tools";
+import { Logger, Package } from "@fluidframework/build-tools";
 import { compareAsc, parseISO } from "date-fns";
 import globby from "globby";
 import matter from "gray-matter";
@@ -12,10 +12,17 @@ import matter from "gray-matter";
 import { ReleasePackage } from "../releaseGroups";
 import { Repository } from "./git";
 
+/**
+ * A ChangesetEntry is an object containing flattened content and file metadata from a changesets file. Changeset files
+ * themselves include a mapping of package to version bump type. This object includes the change type and a single
+ * package, effectively flattening the changesets.
+ */
 export interface ChangesetEntry {
+	pkg: string;
+	changeType: VersionBumpType;
 	content: string;
-	added?: Date;
 	summary?: string;
+	added?: Date;
 }
 
 function compareChangesetEntry(a: ChangesetEntry, b: ChangesetEntry) {
@@ -26,27 +33,15 @@ function compareChangesetEntry(a: ChangesetEntry, b: ChangesetEntry) {
 }
 
 /**
- *
  * @param dir - The directory containing changesets.
- * @param repo - A Repository
  * @param log - An optional logger.
  * @returns An object with the bumpt type and a map of packages to changeset entries.
  */
-export async function loadChangesets(
-	dir: string,
-	log?: Logger,
-): Promise<{
-	type: VersionBumpType;
-	map: Map<ReleasePackage, ChangesetEntry[]>;
-	ordered: ChangesetEntry[];
-}> {
+export async function loadChangesets(dir: string, log?: Logger): Promise<ChangesetEntry[]> {
 	const repo = new Repository({ baseDir: dir });
-	const changesetMap = new Map<ReleasePackage, ChangesetEntry[]>();
 	const changesetFiles = await globby(["*.md", "!README.md"], { cwd: dir, absolute: true });
 	const changesetEntries: ChangesetEntry[] = [];
-	let changeType: VersionBumpType | undefined;
 
-	const hadWarnings = new Set<string>();
 	for (const file of changesetFiles) {
 		// Get the date the changeset file was added to git.
 		// eslint-disable-next-line no-await-in-loop
@@ -56,43 +51,45 @@ export async function loadChangesets(
 		// Read the changeset file into content and metadata (front-matter)
 		const md = matter.read(file);
 		const paragraphs = md.content.trim().split("\n\n");
+
+		if (paragraphs.length < 2) {
+			log?.warning(`No changeset content found in ${file}. Skipping!`);
+			continue;
+		}
+
 		const summary = paragraphs[0];
 		const content = paragraphs.slice(1).join("\n\n");
 
-		// changesetEntries should contain one entry per changeset file...
-		changesetEntries.push({ content, added, summary });
-
 		// ...while the map contains the entries as they apply to each released package.
-		for (const [pkgName, type] of Object.entries(md.data)) {
-			if (changeType === undefined) {
-				changeType = type;
-				log?.verbose(
-					`${file}: Found ${changeType} bump type; expecting all others to match.`,
-				);
-			}
-
-			if (type !== changeType && !hadWarnings.has(file)) {
-				log?.warning(
-					`Unexpected change type in ${file}: ${type} (expected ${changeType}). Check the changeset.`,
-				);
-				hadWarnings.add(file);
-			}
-
-			const entries = changesetMap.get(pkgName) ?? [];
-			entries.push({ content, added });
-			changesetMap.set(pkgName, entries);
+		for (const [pkgName, changeType] of Object.entries(md.data)) {
+			changesetEntries.push({
+				pkg: pkgName,
+				content,
+				added,
+				summary,
+				changeType,
+			});
 		}
 	}
 
 	// Sort all the entries by date
 	changesetEntries.sort(compareChangesetEntry);
+
+	return changesetEntries;
+}
+
+export function groupByPackage(changesets: ChangesetEntry[]): Map<string, ChangesetEntry[]> {
+	const changesetMap = new Map<ReleasePackage, ChangesetEntry[]>();
+	for (const changeset of changesets) {
+		const entries = changesetMap.get(changeset.pkg) ?? [];
+		entries.push(changeset);
+		changesetMap.set(changeset.pkg, entries);
+	}
+
+	// Sort all the entries by date
 	for (const entries of changesetMap.values()) {
 		entries.sort(compareChangesetEntry);
 	}
 
-	if (changeType === undefined) {
-		throw new Error(`No change type found.`);
-	}
-
-	return { type: changeType, map: changesetMap, ordered: changesetEntries };
+	return changesetMap;
 }
