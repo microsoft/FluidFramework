07224ec513da85769313e7f08840446a11853c84
Matt Rakow <marakow@microsoft.com>
2023-06-30 16:11:39 -0700

    Deprecate IDisposable portion of IDeltaManager

--- packages/common/container-definitions/src/deltas.ts
+++ packages/common/container-definitions/src/deltas.ts
@@ -174,10 +174,7 @@ export interface IDeltaManagerEvents extends IEvent {
 /**
  * Manages the transmission of ops between the runtime and storage.
  */
-export interface IDeltaManager<T, U>
-	extends IEventProvider<IDeltaManagerEvents>,
-		IDeltaSender,
-		IDisposable {
+export interface IDeltaManager<T, U> extends IEventProvider<IDeltaManagerEvents>, IDeltaSender {
 	/** The queue of inbound delta messages */
 	readonly inbound: IDeltaQueue<T>;
 
@@ -227,6 +224,18 @@ export interface IDeltaManager<T, U>
 
 	/** Submit a signal to the service to be broadcast to other connected clients, but not persisted */
 	submitSignal(content: any): void;
+
+	/**
+	 * @deprecated - 2.0.0-internal.5.2.0 - The IDeltaManager's dispose state is not recommended for observation
+	 * and will be removed in an upcoming release.
+	 */
+	readonly disposed: boolean;
+
+	/**
+	 * @deprecated - 2.0.0-internal.5.2.0 - Disposing the IDeltaManager results in inconsistent system state.
+	 * This member will be removed in an upcoming release.
+	 */
+	dispose(error?: Error): void;
 }
 
 /**
