95a9a5a14ebb741f722773d437d209cf64ea40ab
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2023-07-06 23:41:07 +0000

    add default taskId

--- packages/tools/devtools/devtools-example/src/widgets/TaskManagerWidget.tsx
+++ packages/tools/devtools/devtools-example/src/widgets/TaskManagerWidget.tsx
@@ -1,88 +1,46 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
 import React from "react";
-import sillyname from "sillyname";
 import { TaskManager } from "@fluidframework/task-manager";
-import { EditRegular, DocumentRegular, CheckmarkRegular } from "@fluentui/react-icons";
+
+import sillyname from "sillyname";
+import { AddCircle32Regular, CheckmarkRegular, DocumentRegular, EditRegular } from "@fluentui/react-icons";
 import {
 	TableBody,
-	TableCell,
 	TableRow,
 	Table,
 	TableHeader,
 	TableHeaderCell,
+	TableCell,
 	TableCellLayout,
 } from "@fluentui/react-components";
 
 /**
- * {@link TaskManagerWidget} input props.
+ * TODO
  */
 export interface TaskManagerWidgetProps {
 	taskManager: TaskManager;
 }
 
 /**
- * Simple TaskManager Widget.
- * Queues the request to a task by the order it was received.
+ * TODO
  */
 export function TaskManagerWidget(props: TaskManagerWidgetProps): React.ReactElement {
 	const { taskManager } = props;
 
-	const taskManagerId = "taskmManagerWidget-Id";
+	const defaultTaskId = "strawberry-fields" // default taskId shared across different clients 
+	const [taskQueues, setTaskQueues] = React.useState<string[]>([defaultTaskId]);
 
-	const [assigned, setAssigned] = React.useState<boolean>(false);
-	const [queued, setQueued] = React.useState<boolean>(false);
-	const [subscribed, setSubscribed] = React.useState<boolean>(false);
+	const addTask = async (): Promise<void> => {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+		const taskId = (sillyname() as unknown as string).toLowerCase().split(" ").join("-");
 
-	React.useEffect(() => {
-		if (taskManager !== undefined) {
-			const updateState = (): void => {
-				setAssigned(taskManager.assigned(taskManagerId));
-				setQueued(taskManager.queued(taskManagerId));
-				setSubscribed(taskManager.subscribed(taskManagerId));
-			};
+        setTaskQueues(prevQueues => [...prevQueues, taskId])
+	};
 
-			taskManager.on("assigned", updateState);
-			taskManager.on("lost", updateState);
-			taskManager.on("completed", updateState);
+	const addAuxTask = async (): Promise<void> => {
+		const fixedId = "penny-lane"; 
 
-			taskManager.subscribeToTask(taskManagerId);
-
-			return (): void => {
-				taskManager.off("assigned", updateState);
-				taskManager.off("lost", updateState);
-				taskManager.off("completed", updateState);
-			};
-		}
-	}, [taskManager]);
-
-	if (taskManager === undefined) return <div />;
-
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-	const taskLabel = (sillyname() as unknown as string).toLowerCase().split(" ").join("-");
-
-	const items = [
-		{
-			task: { label: taskLabel, icon: <DocumentRegular /> },
-			assigned: { label: assigned, icon: <CheckmarkRegular /> },
-			queued: { label: queued, status: <DocumentRegular /> },
-			actions: {
-				label: (
-					<ActionButtons
-						taskManager={taskManager}
-						taskManagerId={taskManagerId}
-						assigned={assigned}
-						subscribed={subscribed}
-						queued={queued}
-					/>
-				),
-				icon: <EditRegular />,
-			},
-		},
-	];
+        setTaskQueues(prevQueues => [...prevQueues, fixedId])
+	};
 
 	const columns = [
 		{ columnKey: "task", label: "Task" },
@@ -92,73 +50,129 @@ export function TaskManagerWidget(props: TaskManagerWidgetProps): React.ReactEle
 	];
 
 	return (
-		<Table size="small" aria-label="Task-Manager-Table">
-			<TableHeader>
-				<TableRow>
-					{columns.map((column) => (
-						<TableHeaderCell key={column.columnKey}>{column.label}</TableHeaderCell>
-					))}
-				</TableRow>
-			</TableHeader>
-			<TableBody>
-				{items.map((item) => (
-					<TableRow key={item.task.label}>
-						<TableCell>
-							<TableCellLayout>{item.task.label}</TableCellLayout>
-						</TableCell>
-						<TableCell>
-							<TableCellLayout>
-								{item.assigned.label ? "True" : "False"}
-							</TableCellLayout>
-						</TableCell>
-						<TableCell>
-							<TableCellLayout>
-								{item.queued.label ? "True" : "False"}
-							</TableCellLayout>
-						</TableCell>
-						<TableCell>
-							<TableCellLayout>{item.actions.label}</TableCellLayout>
-						</TableCell>
+		<>
+			<Table size="small" aria-label="Task-Manager-Table">
+				<TableHeader>
+					<TableRow>
+						{columns.map((column) => (
+							<TableHeaderCell key={column.columnKey}>{column.label}</TableHeaderCell>
+						))}
 					</TableRow>
-				))}
-			</TableBody>
-		</Table>
+				</TableHeader>
+				<TableBody>
+					{
+						[...taskQueues].map((taskId) => (
+							<TaskRow key={taskId} taskManager={taskManager} taskId={taskId} />
+						))
+					}
+				</TableBody>
+			</Table>
+			<AddCircle32Regular onClick={addTask} />
+			<AddCircle32Regular onClick={addAuxTask} />
+		</>
 	);
 }
 
-/**
- * TODO
- */
-interface ActionButtonsProps {
+interface TaskRowProps {
 	taskManager: TaskManager;
-	taskManagerId: string;
-	assigned: boolean;
-	subscribed: boolean;
-	queued: boolean;
+	taskId: string;
+}
+
+function TaskRow(props: TaskRowProps): React.ReactElement {
+	const { taskManager, taskId } = props;
+
+	console.log(taskManager);
+	
+	const [assigned, setAssigned] = React.useState<boolean>(false);
+	const [queued, setQueued] = React.useState<boolean>(false);
+	const [subscribed, setSubscribed] = React.useState<boolean>(false);
+
+	React.useEffect(() => {
+	    const updateState = (): void => {
+	        setAssigned(taskManager.assigned(taskId));
+	        setQueued(taskManager.queued(taskId));
+	        setSubscribed(taskManager.subscribed(taskId));
+	    };
+
+	    taskManager.on("assigned", updateState);
+	    taskManager.on("lost", updateState);
+	    taskManager.on("completed", updateState);
+
+	    taskManager.subscribeToTask(taskId);
+
+	    return (): void => {
+	        taskManager.off("assigned", updateState);
+	        taskManager.off("lost", updateState);
+	        taskManager.off("completed", updateState);
+	    };
+	}, [taskManager, taskId]);
+
+	const items = {
+	    task: { label: taskId, icon: <DocumentRegular /> },
+	    assigned: { label: assigned, icon: <CheckmarkRegular /> },
+	    queued: { label: queued, status: <DocumentRegular /> },
+	    actions: {
+	        label: (
+	            <ActionButtons
+	                taskManager={taskManager}
+	                taskManagerId={taskId}
+	                assigned={assigned}
+	                subscribed={subscribed}
+	                queued={queued}
+	            />
+	        ),
+	        icon: <EditRegular />,
+	    },
+	};
+
+	return (
+	    <TableRow key={items.task.label}>
+	        <TableCell>
+	            <TableCellLayout>{items.task.label}</TableCellLayout>
+	        </TableCell>
+	        <TableCell>
+	            <TableCellLayout>{items.assigned.label ? "True" : "False"}</TableCellLayout>
+	        </TableCell>
+	        <TableCell>
+	            <TableCellLayout>{items.queued.label ? "True" : "False"}</TableCellLayout>
+	        </TableCell>
+	        <TableCell>
+	            <TableCellLayout>{items.actions.label}</TableCellLayout>
+	        </TableCell>
+	    </TableRow>
+	);
+}
+
+interface ActionButtonsProps {
+    taskManager: TaskManager;
+    taskManagerId: string;
+    assigned: boolean;
+    subscribed: boolean;
+    queued: boolean;
 }
 
 function ActionButtons(props: ActionButtonsProps): React.ReactElement {
-	const { taskManager, taskManagerId, assigned, subscribed, queued } = props;
+    const { taskManager, taskManagerId, assigned, subscribed, queued } = props;
 
-	const abandon = (): void => taskManager.abandon(taskManagerId);
-	const volunteer = async (): Promise<boolean> => taskManager.volunteerForTask(taskManagerId);
-	const subscribe = (): void => taskManager.subscribeToTask(taskManagerId);
-	const complete = (): void => taskManager.complete(taskManagerId);
+    const abandon = (): void => taskManager.abandon(taskManagerId);
+    const volunteer = async (): Promise<boolean> => taskManager.volunteerForTask(taskManagerId);
+    const subscribe = (): void => taskManager.subscribeToTask(taskManagerId);
+    const complete = (): void => taskManager.complete(taskManagerId);
 
-	return (
-		<div className="task-manager-controls" style={{ margin: "7px 0px 7px 0px" }}>
-			<button disabled={!queued} onClick={abandon}>
-				Abandon
-			</button>
-			<button disabled={queued} onClick={volunteer}>
-				Volunteer
-			</button>
-			<button disabled={queued && subscribed} onClick={subscribe}>
-				Subscribe
-			</button>
-			<button disabled={!assigned} onClick={complete}>
-				Complete
-			</button>
-		</div>
-	);
+    return (
+        <div className="task-manager-controls" style={{ margin: "7px 0px 7px 0px" }}>
+            <button disabled={!queued} onClick={abandon}>
+                Abandon
+            </button>
+            <button disabled={queued} onClick={volunteer}>
+                Volunteer
+            </button>
+            <button disabled={queued && subscribed} onClick={subscribe}>
+                Subscribe
+            </button>
+            <button disabled={!assigned} onClick={complete}>
+                Complete
+            </button>
+        </div>
+    );
 }
