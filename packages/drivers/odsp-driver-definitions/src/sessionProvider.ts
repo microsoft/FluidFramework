17aa183aaba07386cc62e927a99be04a83c06c0b
Kajari Ghosh <kajarighosh@microsoft.com>
2023-07-06 21:43:00 +0000

    add liicense header

--- packages/drivers/odsp-driver-definitions/src/sessionProvider.ts
+++ packages/drivers/odsp-driver-definitions/src/sessionProvider.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { IResolvedUrl } from "@fluidframework/driver-definitions";
 
 export interface ISocketStorageDiscovery {
