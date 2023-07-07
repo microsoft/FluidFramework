718c6e31f351865d763ea8daabd5d3334dc095ec
Kian Thompson <102998837+kian-thompson@users.noreply.github.com>
2023-07-07 00:35:04 +0000

    Add note about removing cast

--- packages/loader/container-loader/src/deltaManager.ts
+++ packages/loader/container-loader/src/deltaManager.ts
@@ -613,6 +613,7 @@ export class DeltaManager<TConnectionManager extends IConnectionManager>
 			// This is useless for known ranges (to is defined) as it means request is over either way.
 			// And it will cancel unbound request too early, not allowing us to learn where the end of the file is.
 			if (!opsFromFetch && cancelFetch(op)) {
+				// Remove case to any when @types/node is upgraded to 16 (AB#4884)
 				(controller as any).abort("DeltaManager getDeltas fetch cancelled");
 				this._inbound.off("push", opListener);
 			}
@@ -622,6 +623,7 @@ export class DeltaManager<TConnectionManager extends IConnectionManager>
 			this._inbound.on("push", opListener);
 			assert(this.closeAbortController.signal.onabort === null, 0x1e8 /* "reentrancy" */);
 			this.closeAbortController.signal.onabort = () =>
+				// Remove case to any when @types/node is upgraded to 16 (AB#4884)
 				// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 				(controller as any).abort((this.closeAbortController.signal as any).reason);
 
@@ -701,6 +703,7 @@ export class DeltaManager<TConnectionManager extends IConnectionManager>
 	}
 
 	private clearQueues() {
+		// Remove case to any when @types/node is upgraded to 16 (AB#4884)
 		(this.closeAbortController as any).abort("DeltaManager was closed");
 
 		this._inbound.clear();
