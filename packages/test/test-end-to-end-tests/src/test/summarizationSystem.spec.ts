f395687079d8e32b4aa590acb74a89b8d6e8e12a
tyler-cai-microsoft <tylercai@microsoft.com>
2023-07-10 14:22:40 -0700

    Speed up test

--- packages/test/test-end-to-end-tests/src/test/summarizationSystem.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/summarizationSystem.spec.ts
@@ -10,11 +10,18 @@ import { ITestContainerConfig, ITestObjectProvider } from "@fluidframework/test-
 import { requestFluidObject } from "@fluidframework/runtime-utils";
 import { ContainerRuntime, DefaultSummaryConfiguration } from "@fluidframework/container-runtime";
 import { Deferred } from "@fluidframework/common-utils";
+import { MessageType } from "@fluidframework/protocol-definitions";
 
 describeNoCompat("Do not need summarize on demand", (getTestObjectProvider) => {
 	let provider: ITestObjectProvider;
+	const summaryConfigOverrides = {
+		...DefaultSummaryConfiguration,
+		maxOps: 5,
+		maxIdleTime: 100,
+		initialSummarizerDelayMs: 0,
+	};
 	const testContainerConfig: ITestContainerConfig = {
-		runtimeOptions: { summaryOptions: { summaryConfigOverrides: DefaultSummaryConfiguration } },
+		runtimeOptions: { summaryOptions: { summaryConfigOverrides } },
 	};
 
 	const createContainer = async (): Promise<IContainer> => {
@@ -27,9 +34,15 @@ describeNoCompat("Do not need summarize on demand", (getTestObjectProvider) => {
 
 	it("Can get summaries", async () => {
 		const container = await createContainer();
+		const waitForFirstSummary = new Deferred<void>();
+		container.on("op", (message) => {
+			if (message.type === MessageType.Summarize) {
+				waitForFirstSummary.resolve();
+			}
+		});
 		const dataObject = await requestFluidObject<ITestDataObject>(container, "default");
 		const containerRuntime = dataObject._context.containerRuntime;
-		const waitForSummary = new Deferred<void>();
+		const waitForSummary = new Deferred();
 		(containerRuntime as any).summarizerClientElection.on("electedSummarizerChanged", () => {
 			const runtime = (containerRuntime as any).summaryManager.summarizer
 				?.runtime as ContainerRuntime;
@@ -44,6 +57,7 @@ describeNoCompat("Do not need summarize on demand", (getTestObjectProvider) => {
 		dataObject._root.set("an", "op Or 3");
 		dataObject._root.set("an", "op Or 4");
 		dataObject._root.set("an", "op Or 5");
+		await waitForFirstSummary.promise;
 		dataObject._root.set("an", "op Or 6");
 		dataObject._root.set("an", "op Or 7");
 		dataObject._root.set("an", "op Or 8");
