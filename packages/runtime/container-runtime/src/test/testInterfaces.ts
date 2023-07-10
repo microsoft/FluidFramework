20e243fa32755303b70037af29b5578569f1c542
Daniel Madrid <105010181+dannimad@users.noreply.github.com>
2023-07-10 20:17:34 +0000

    copyright header

--- packages/runtime/container-runtime/src/test/testInterfaces.ts
+++ packages/runtime/container-runtime/src/test/testInterfaces.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { IPendingBlobs } from "../blobManager";
 import { IPendingLocalState } from "../pendingStateManager";
 
