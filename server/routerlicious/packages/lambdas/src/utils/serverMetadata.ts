0079cc82f94e78a9a540a3e2faccc9c29101eeed
Matt Rakow <marakow@microsoft.com>
2023-07-11 12:55:36 -0700

    Add interface comment

--- server/routerlicious/packages/lambdas/src/utils/serverMetadata.ts
+++ server/routerlicious/packages/lambdas/src/utils/serverMetadata.ts
@@ -3,6 +3,9 @@
  * Licensed under the MIT License.
  */
 
+/**
+ * Server makes assumptions about what might be on the metadata. This interface codifies those assumptions, but does not validate them.
+ */
 export interface IServerMetadata {
 	createSignal?: boolean;
 	noClient?: boolean;
