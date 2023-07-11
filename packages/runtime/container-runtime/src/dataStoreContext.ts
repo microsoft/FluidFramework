e3f80ee9c6b1573015edc3df23cba4a5e06c0c0f
Tony Murphy <anthonm@microsoft.com>
2023-07-11 14:13:30 -0700

    Use the correct logger for data store context errors

--- packages/runtime/container-runtime/src/dataStoreContext.ts
+++ packages/runtime/container-runtime/src/dataStoreContext.ts
@@ -396,7 +396,7 @@ export abstract class FluidDataStoreContext
 					packageName: packagePathToTelemetryProperty(this.pkg),
 				});
 				this.channelDeferred?.reject(errorWrapped);
-				this.logger.sendErrorEvent({ eventName: "RealizeError" }, errorWrapped);
+				this.mc.logger.sendErrorEvent({ eventName: "RealizeError" }, errorWrapped);
 			});
 		}
 		return this.channelDeferred.promise;
@@ -786,7 +786,7 @@ export abstract class FluidDataStoreContext
 			this.channelDeferred.resolve(this.channel);
 		} catch (error) {
 			this.channelDeferred?.reject(error);
-			this.logger.sendErrorEvent(
+			this.mc.logger.sendErrorEvent(
 				{
 					eventName: "BindRuntimeError",
 					fluidDataStoreId: {
