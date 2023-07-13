21481909c3be945a236ebe7a68b6935728ac5faf
Clarence Li <clarenceli@microsoft.com>
2023-07-13 10:53:46 -0400

    removing the skip for non-reproducible tests

--- packages/test/test-end-to-end-tests/src/test/cellAttributionEndToEnd.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/cellAttributionEndToEnd.spec.ts
@@ -109,10 +109,6 @@ describeNoCompat("Attributor for SharedCell", (getTestObjectProvider) => {
 	});
 
 	it("Can attribute content from multiple collaborators", async function () {
-		// Tracked by AB#4130, the test run on the tinylicous driver is disabled temporarily to ensure normal operation of the build-client package pipeline
-		if (provider.driver.type === "tinylicious" || provider.driver.type === "t9s") {
-			this.skip();
-		}
 		const attributor = createRuntimeAttributor();
 		const container1 = await provider.makeTestContainer(getTestConfig(attributor));
 		const sharedCell1 = await sharedCellFromContainer(container1);
