f205b47a981f2fba9638dd1afee0d5a2a8fd34d1
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-07-18 00:04:04 +0000

    test: Add TreeHeader test story

---
+++ packages/tools/devtools/devtools-view/src/test/screenshot-tests/TreeHeader.stories.tsx
@@ -0,0 +1,22 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { TreeHeader } from "../../components/data-visualization/TreeHeader";
+import { testContextDecorator } from "./ScreenshotTestUtilities";
+
+export default {
+	title: "TreeHeader",
+	component: TreeHeader,
+	decorators: [testContextDecorator],
+};
+
+/**
+ * {@link TreeHeader} with no metadata.
+ */
+export const Simple = {
+	args: {
+		label: "Hello world!",
+	},
+};
