0d78ea814bb3e5241886633261f017d84e04d9ea
Pradeep Vairamani <pradeeprv123@gmail.com>
2023-07-20 10:58:05 -0700

    fixing policy errors

--- server/gitrest/packages/gitrest-base/src/utils/fileSystemHelper.ts
+++ server/gitrest/packages/gitrest-base/src/utils/fileSystemHelper.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import fs from "fs";
 
 export const packedRefsFileName = "packed-refs";
