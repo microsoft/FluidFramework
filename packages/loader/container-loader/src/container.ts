3cc8b44cc70ad02429ab6978ddeb5a0f7947f27a
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-07-26 13:30:21 -0500

    Fix import order

--- packages/loader/container-loader/src/container.ts
+++ packages/loader/container-loader/src/container.ts
@@ -97,6 +97,7 @@ import {
 	ITelemetryLoggerExt,
 	formatTick,
 } from "@fluidframework/telemetry-utils";
+import { cloneDeep } from "lodash";
 import { Audience } from "./audience";
 import { ContainerContext } from "./containerContext";
 import { ReconnectMode, IConnectionManagerFactoryArgs, getPackageName } from "./contracts";
@@ -122,7 +123,6 @@ import {
 	ProtocolHandlerBuilder,
 	protocolHandlerShouldProcessSignal,
 } from "./protocol";
-import { cloneDeep } from "lodash";
 
 const detachedContainerRefSeqNumber = 0;
 
