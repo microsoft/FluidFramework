7d2de0ee43bc937a003d33e1bcf03148d00c5e73
daesunp <138815173+daesunp@users.noreply.github.com>
2023-07-28 19:11:13 +0000

    remove .only

--- experimental/dds/tree2/src/test/shared-tree/fuzz/targetedFuzz.spec.ts
+++ experimental/dds/tree2/src/test/shared-tree/fuzz/targetedFuzz.spec.ts
@@ -96,7 +96,7 @@ const fuzzComposedVsIndividualReducer = combineReducersAsync<Operation, Branched
  *
  * See the "Fuzz - Top-Level" test suite for tests are more general in scope.
  */
-describe.only("Fuzz - Targeted", () => {
+describe("Fuzz - Targeted", () => {
 	const opsPerRun = 20;
 	const runsPerBatch = 20;
 	const editGeneratorOpWeights: Partial<EditGeneratorOpWeights> = { insert: 1 };
