e43927f801f49f735af014b15c8556b28df68254
Andrei Iacob <84357545+andre4i@users.noreply.github.com>
2023-07-31 10:16:50 -0700

    Cleanup mock APIs (#16568)
    ## Description
    
    These capabilities have been added as optional in `main` for
    compatibility reasons. In `next` we can make them required.
    
    Leftover from
    [AB#4980](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/4980)

--- packages/runtime/test-runtime-utils/src/mocks.ts
+++ packages/runtime/test-runtime-utils/src/mocks.ts
@@ -182,10 +182,6 @@ export class MockContainerRuntime {
 	}
 
 	public createDeltaConnection(): MockDeltaConnection {
-		assert(
-			this.dataStoreRuntime.createDeltaConnection !== undefined,
-			"Unsupported datastore runtime version",
-		);
 		const deltaConnection = this.dataStoreRuntime.createDeltaConnection();
 		this.deltaConnections.push(deltaConnection);
 		return deltaConnection;
@@ -223,7 +219,7 @@ export class MockContainerRuntime {
 	 * If flush mode is set to FlushMode.TurnBased, it will send all messages queued since the last time
 	 * this method was called. Otherwise, calling the method does nothing.
 	 */
-	public flush?() {
+	public flush() {
 		if (this.runtimeOptions.flushMode !== FlushMode.TurnBased) {
 			return;
 		}
@@ -240,7 +236,7 @@ export class MockContainerRuntime {
 	 *
 	 * The method requires `runtimeOptions.enableGroupedBatching` to be enabled.
 	 */
-	public rebase?() {
+	public rebase() {
 		if (this.runtimeOptions.flushMode !== FlushMode.TurnBased) {
 			return;
 		}
@@ -594,7 +590,7 @@ export class MockFluidDataStoreRuntime
 	public quorum = new MockQuorumClients();
 	public containerRuntime?: MockContainerRuntime;
 	private readonly deltaConnections: MockDeltaConnection[] = [];
-	public createDeltaConnection?(): MockDeltaConnection {
+	public createDeltaConnection(): MockDeltaConnection {
 		const deltaConnection = new MockDeltaConnection(
 			(messageContent: any, localOpMetadata: unknown) =>
 				this.submitMessageInternal(messageContent, localOpMetadata),
