70c63b9df53c93309389a2006d35fdd37bc72927
Kian Thompson <102998837+kian-thompson@users.noreply.github.com>
2023-08-01 23:40:31 +0000

    Fix spelling

--- packages/runtime/test-runtime-utils/src/mocks.ts
+++ packages/runtime/test-runtime-utils/src/mocks.ts
@@ -576,7 +576,7 @@ export class MockFluidDataStoreRuntime
 	}
 
 	/**
-	 * @deprecated - TWill be removed in future major release. Migrate all usage of IFluidRouter to the "entryPoint" pattern. Refer to Removing-IFluidRouter.md
+	 * @deprecated - Will be removed in future major release. Migrate all usage of IFluidRouter to the "entryPoint" pattern. Refer to Removing-IFluidRouter.md
 	 */
 	public get IFluidRouter() {
 		return this;
