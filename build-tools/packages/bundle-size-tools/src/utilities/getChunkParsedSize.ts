12f448ec84c7d74c75c4d9393fda4c18343f6dac
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-08-01 10:19:03 -0500

    Bring in changes from https://github.com/microsoft/FluidFramework/pull/16232

--- build-tools/packages/bundle-size-tools/src/utilities/getChunkParsedSize.ts
+++ build-tools/packages/bundle-size-tools/src/utilities/getChunkParsedSize.ts
@@ -14,7 +14,8 @@ export function getChunkParsedSize(stats: StatsCompilation, chunkId: string | nu
 		);
 	}
 
-	const matchingAsset = stats.assets.find((asset) => {
+	//Find all the assets that contain the chunk. Note: an asset may contain more than one chunk.
+	const matchingAssets = stats.assets.filter((asset) => {
 		// Make sure to only look at js files and not source maps (assumes source maps don't end in .js)
 		if (asset.name.endsWith(".js")) {
 			// If the asset contains the chunk, it should be considered when calculating the total size.
@@ -24,9 +25,22 @@ export function getChunkParsedSize(stats: StatsCompilation, chunkId: string | nu
 		return false;
 	});
 
-	if (matchingAsset === undefined) {
-		throw new Error(`Could not find asset for chunk with id '${chunkId}' in the webpack stats`);
+	if (matchingAssets.length === 0) {
+		throw new Error(
+			`Could not find an asset for chunk with id '${chunkId}' in the webpack stats`,
+		);
 	}
 
-	return matchingAsset.size;
+	if (matchingAssets.length > 1) {
+		// Typically we expect a single asset to be found per chunk (this is maybe not typical of all webpack projects, but
+		// it seems to be the case in our usage here), so if we find more than one, log a warning so we can investigate more
+		// easily if needed.
+		console.warn(
+			`${matchingAssets.length} assets contain chunk with id '${chunkId}'; will return total size of all matching assets.`,
+		);
+	}
+
+	// The total size is the sum of the sizes of all assets with the chunk.
+	const totalSize = matchingAssets.reduce((acc, asset) => acc + asset.size, 0);
+	return totalSize;
 }
