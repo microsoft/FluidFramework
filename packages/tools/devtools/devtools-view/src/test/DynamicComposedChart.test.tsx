8c6f28a36ab6f8831a13937ae3efd2eb805976aa
seanimam <105244057+seanimam@users.noreply.github.com>
2023-08-01 17:02:11 +0000

    adds missing copyright header

--- packages/tools/devtools/devtools-view/src/test/DynamicComposedChart.test.tsx
+++ packages/tools/devtools/devtools-view/src/test/DynamicComposedChart.test.tsx
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import React from "react";
 
 // eslint-disable-next-line import/no-unassigned-import
