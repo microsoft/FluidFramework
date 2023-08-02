3035c973f6f3ed3be418a90102092efaa407b900
Jatin Garg <jatingarg@Jatins-MacBook-Pro-2.local>
2023-08-02 15:36:21 -0700

    Policy check fix before minor release

--- experimental/PropertyDDS/packages/property-shared-tree-interop/src/schemaConverter.ts
+++ experimental/PropertyDDS/packages/property-shared-tree-interop/src/schemaConverter.ts
@@ -130,7 +130,7 @@ function buildTreeSchema(
 				return cache.treeSchema;
 			}
 			if (PropertyFactory.inheritsFrom(typeid, nodePropertyType)) {
-				assert(!fields.has(nodePropertyField), "name collision for nodePropertyField");
+				assert(!fields.has(nodePropertyField), 0x712 /* name collision for nodePropertyField */);
 				fields.set(nodePropertyField, SchemaBuilder.fieldValue(nodePropertySchema));
 			}
 			const fieldsObject = mapToObject(fields);
