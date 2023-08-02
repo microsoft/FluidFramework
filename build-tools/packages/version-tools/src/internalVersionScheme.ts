9660432396582e2598ea02caf5487c58b10ceec8
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2023-08-02 20:11:32 +0000

    logging parsed version

--- build-tools/packages/version-tools/src/internalVersionScheme.ts
+++ build-tools/packages/version-tools/src/internalVersionScheme.ts
@@ -196,9 +196,11 @@ export function validateVersionScheme(
 		}
 	}
 
+	console.log({ parsedVersion });
+
 	if (parsedVersion.major < MINIMUM_PUBLIC_MAJOR) {
 		throw new Error(
-			`The public major version must be >= ${MINIMUM_PUBLIC_MAJOR}; found ${parsedVersion.major}`,
+			`The public major version must be >= ${MINIMUM_PUBLIC_MAJOR}; found ${parsedVersion.major} Rishhi`,
 		);
 	}
 
