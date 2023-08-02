d099a85f145c6eaba592f82de5381ad57ad39f4b
justus-camp <102552460+justus-camp@users.noreply.github.com>
2023-08-02 14:23:11 -0700

    Generate placeholder marks in RebaseQueue (#16627)
    ## Description
    This refactors `rebaseMarkList` to remove cases where the `RebaseQueue`
    would return `undefined` for the `baseMark` and `currMark` and instead
    generates a placeholder NoOp mark with a `cellId`. This allows us to
    greatly simplify the code-paths present in `rebaseMarkList`. I've also
    removed some cases where we would generate 0-length NoOps in
    `rebaseMark` and have also replaced those placeholder NoOps.
    
    ## Reviewer Guidance
    
    The review process is outlined on [this wiki
    page](https://github.com/microsoft/FluidFramework/wiki/PR-Guidelines#guidelines).

--- experimental/dds/tree2/src/feature-libraries/sequence-field/rebase.ts
+++ experimental/dds/tree2/src/feature-libraries/sequence-field/rebase.ts
@@ -4,6 +4,7 @@
  */
 
 import { assert, unreachableCase } from "@fluidframework/common-utils";
+import { StableId } from "@fluidframework/runtime-definitions";
 import { fail } from "../../util";
 import { RevisionTag, TaggedChange } from "../../core";
 import {
@@ -16,8 +17,6 @@ import {
 } from "../modular-schema";
 import {
 	getInputLength,
-	getOutputLength,
-	isAttach,
 	isDetachMark,
 	isNewAttach,
 	cloneMark,
@@ -32,8 +31,8 @@ import {
 	getNodeChange,
 	withNodeChange,
 	getMarkMoveId,
-	isNoopMark,
 	areOverlappingIdRanges,
+	areOutputCellsEmpty,
 } from "./utils";
 import {
 	Changeset,
@@ -133,93 +132,63 @@ function rebaseMarkList<TNodeChange>(
 	const lineageEntries: IdRange[] = [];
 	while (!queue.isEmpty()) {
 		const { baseMark, newMark: currMark } = queue.pop();
-		if (baseMark !== undefined && "revision" in baseMark) {
+		if ("revision" in baseMark) {
 			// TODO support rebasing over composite changeset
 			assert(
 				baseMark.revision === baseRevision,
 				0x4f3 /* Unable to keep track of the base input offset in composite changeset */,
 			);
 		}
-		if (baseMark === undefined) {
-			assert(
-				currMark !== undefined,
-				0x4f4 /* Non-empty queue should return at least one mark */,
-			);
-			if (areInputCellsEmpty(currMark)) {
-				const rebasedMark = cloneMark(currMark);
-				handleLineage(rebasedMark, lineageRecipients, lineageEntries, baseIntention);
-				factory.push(rebasedMark);
-			} else {
-				factory.push(cloneMark(currMark));
-			}
-		} else if (currMark === undefined) {
-			if (markEmptiesCells(baseMark)) {
-				assert(isDetachMark(baseMark), 0x708 /* Only detach marks should empty cells */);
-				lineageEntries.push({ id: baseMark.id, count: baseMark.count });
-			} else if (isAttach(baseMark)) {
-				if (baseMark.type === "MoveIn" || baseMark.type === "ReturnTo") {
-					const movedMark = getMovedMark(
-						moveEffects,
-						baseMark.revision ?? baseRevision,
-						baseMark.id,
-						baseMark.count,
-					);
-					if (movedMark !== undefined) {
-						factory.push(movedMark);
-					} else {
-						factory.pushOffset(getOutputLength(baseMark));
-					}
-				} else if (baseMark.transientDetach === undefined) {
-					factory.pushOffset(getOutputLength(baseMark));
-				}
-				lineageRecipients.length = 0;
-				lineageEntries.length = 0;
-			}
-		} else {
-			assert(
-				getInputLength(baseMark) === getInputLength(currMark),
-				0x4f6 /* The two marks should be the same size */,
-			);
+		assert(
+			getInputLength(baseMark) === getInputLength(currMark),
+			0x4f6 /* The two marks should be the same size */,
+		);
 
-			const rebasedMark = rebaseMark(
-				currMark,
-				baseMark,
-				baseRevision,
-				baseIntention,
-				rebaseChild,
-				moveEffects,
-				nodeExistenceState,
-			);
+		const rebasedMark = rebaseMark(
+			currMark,
+			baseMark,
+			baseRevision,
+			baseIntention,
+			rebaseChild,
+			moveEffects,
+			nodeExistenceState,
+		);
 
-			// Note that we first add lineage for `baseMark` to `lineageRecipients`, then handle adding lineage to `rebasedMark`,
-			// then add `baseMark` to `lineageEntries` so that `rebasedMark` does not get an entry for `baseMark`.
-			// `rebasedMark` should already have a detach event for `baseMark`.
-			if (markEmptiesCells(baseMark)) {
-				assert(isDetachMark(baseMark), 0x709 /* Only detach marks should empty cells */);
-				addLineageToRecipients(
-					lineageRecipients,
-					baseIntention,
-					baseMark.id,
-					baseMark.count,
-				);
-			}
+		// Note that we first add lineage for `baseMark` to `lineageRecipients`, then handle adding lineage to `rebasedMark`,
+		// then add `baseMark` to `lineageEntries` so that `rebasedMark` does not get an entry for `baseMark`.
+		// `rebasedMark` should already have a detach event for `baseMark`.
+		if (markEmptiesCells(baseMark)) {
+			assert(isDetachMark(baseMark), 0x709 /* Only detach marks should empty cells */);
+			addLineageToRecipients(lineageRecipients, baseIntention, baseMark.id, baseMark.count);
+		}
 
-			if (areInputCellsEmpty(rebasedMark)) {
-				handleLineage(rebasedMark, lineageRecipients, lineageEntries, baseIntention);
-			}
-			factory.push(rebasedMark);
+		if (areInputCellsEmpty(rebasedMark)) {
+			handleLineage(rebasedMark, lineageRecipients, lineageEntries, baseIntention);
+		}
+		factory.push(rebasedMark);
 
-			if (markEmptiesCells(baseMark)) {
-				assert(isDetachMark(baseMark), 0x70a /* Only detach marks should empty cells */);
-				lineageEntries.push({ id: baseMark.id, count: baseMark.count });
-			} else {
-				lineageRecipients.length = 0;
-				lineageEntries.length = 0;
-			}
+		if (markEmptiesCells(baseMark)) {
+			assert(isDetachMark(baseMark), 0x70a /* Only detach marks should empty cells */);
+			lineageEntries.push({ id: baseMark.id, count: baseMark.count });
+		} else if (!areOutputCellsEmpty(baseMark)) {
+			lineageRecipients.length = 0;
+			lineageEntries.length = 0;
 		}
 	}
 
 	return factory.list;
+}
+
+/**
+ * Generates a NoOp mark that targets the same cells as the input mark.
+ * @param mark - The mark the NoOp should target.
+ * @param revision - The revision, if available.
+ * @returns A NoOp mark that targets the same cells as the input mark.
+ */
+function generateNoOpWithCellId<T>(mark: Mark<T>, revision?: StableId): NoopMark {
+	const length = getMarkLength(mark);
+	const cellId = getCellId(mark, revision);
+	return cellId === undefined ? { count: length } : { count: length, cellId };
 }
 
 interface IdRange {
@@ -251,27 +220,34 @@ class RebaseQueue<T> {
 		const baseMark = this.baseMarks.peek();
 		const newMark = this.newMarks.peek();
 
-		if (baseMark === undefined && newMark === undefined) {
-			return {};
-		} else if (baseMark === undefined) {
-			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-			const length = getInputLength(newMark!);
+		assert(!(baseMark === undefined && newMark === undefined), "Cannot pop from empty queue");
+
+		if (baseMark === undefined) {
+			const dequeuedNewMark = this.newMarks.dequeue();
 			return {
-				baseMark: length > 0 ? { count: length } : undefined,
-				newMark: this.newMarks.tryDequeue(),
+				baseMark: generateNoOpWithCellId(dequeuedNewMark),
+				newMark: dequeuedNewMark,
 			};
 		} else if (newMark === undefined) {
-			const length = getInputLength(baseMark);
+			const dequeuedBaseMark = this.baseMarks.dequeue();
 			return {
-				baseMark: this.baseMarks.tryDequeue(),
-				newMark: length > 0 ? { count: length } : undefined,
+				baseMark: dequeuedBaseMark,
+				newMark: generateNoOpWithCellId(dequeuedBaseMark, this.baseIntention),
 			};
 		} else if (areInputCellsEmpty(baseMark) && areInputCellsEmpty(newMark)) {
 			const cmp = compareCellPositions(this.baseIntention, baseMark, newMark);
 			if (cmp < 0) {
-				return { baseMark: this.baseMarks.dequeueUpTo(-cmp) };
+				const dequeuedBaseMark = this.baseMarks.dequeueUpTo(-cmp);
+				return {
+					baseMark: dequeuedBaseMark,
+					newMark: generateNoOpWithCellId(dequeuedBaseMark, this.baseIntention),
+				};
 			} else if (cmp > 0) {
-				return { newMark: this.newMarks.dequeueUpTo(cmp) };
+				const dequeuedNewMark = this.newMarks.dequeueUpTo(cmp);
+				return {
+					newMark: dequeuedNewMark,
+					baseMark: generateNoOpWithCellId(dequeuedNewMark),
+				};
 			} else {
 				return this.dequeueBoth();
 			}
@@ -285,11 +261,13 @@ class RebaseQueue<T> {
 	}
 
 	private dequeueBase(): RebaseMarks<T> {
-		return { baseMark: this.baseMarks.dequeue() };
+		const baseMark = this.baseMarks.dequeue();
+		return { baseMark, newMark: generateNoOpWithCellId(baseMark, this.baseIntention) };
 	}
 
 	private dequeueNew(): RebaseMarks<T> {
-		return { newMark: this.newMarks.dequeue() };
+		const newMark = this.newMarks.dequeue();
+		return { newMark, baseMark: generateNoOpWithCellId(newMark) };
 	}
 
 	private dequeueBoth(): RebaseMarks<T> {
@@ -312,8 +290,8 @@ class RebaseQueue<T> {
  * If `baseMark` and `newMark` are both defined, then they are `SizedMark`s covering the same range of nodes.
  */
 interface RebaseMarks<T> {
-	baseMark?: Mark<T>;
-	newMark?: Mark<T>;
+	baseMark: Mark<T>;
+	newMark: Mark<T>;
 }
 
 function rebaseMark<TNodeChange>(
@@ -377,6 +355,20 @@ function rebaseMark<TNodeChange>(
 			isExistingCellMark(rebasedMark),
 			0x69e /* Only an ExistingCellMark can target an empty cell */,
 		);
+
+		if (isMoveMark(baseMark)) {
+			const movedMark = getMovedMark(
+				moveEffects,
+				baseMark.revision ?? baseRevision,
+				baseMark.id,
+				baseMark.count,
+			);
+
+			if (movedMark !== undefined) {
+				return movedMark;
+			}
+		}
+
 		if (isMoveMark(rebasedMark)) {
 			if (
 				(rebasedMark.type === "MoveOut" || rebasedMark.type === "ReturnFrom") &&
@@ -543,10 +535,6 @@ function makeDetachedMark<T>(
 	detachIntention: RevisionTag,
 	detachId: ChangesetLocalId,
 ): Mark<T> {
-	if (isNoopMark(mark)) {
-		return { count: 0 };
-	}
-
 	assert(mark.cellId === undefined, 0x69f /* Expected mark to be attached */);
 	return { ...mark, cellId: { revision: detachIntention, localId: detachId } };
 }
