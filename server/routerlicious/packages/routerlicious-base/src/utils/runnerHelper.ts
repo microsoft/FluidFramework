7aaffc8de2d34db7048e50f350a73a319c314f88
Shubhangi Agarwal <shuagarwal@microsoft.com>
2023-08-08 10:29:17 -0700

    Refactor runner.stop code to a helper for riddler and alfred

---
+++ server/routerlicious/packages/routerlicious-base/src/utils/runnerHelper.ts
@@ -0,0 +1,51 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { serializeError } from "serialize-error";
+import { IWebServer } from "@fluidframework/server-services-core";
+import { Lumber } from "@fluidframework/server-services-telemetry";
+import { promiseTimeout } from "@fluidframework/server-services-client";
+import { Deferred } from "@fluidframework/common-utils";
+
+export async function runnerHttpServerStop(
+	server: IWebServer,
+	runningDeferredPromise: Deferred<void>,
+	runnerServerCloseTimeoutMs: number,
+	runnerMetric: Lumber,
+	caller: string | undefined,
+	uncaughtException: any | undefined,
+): Promise<void> {
+	try {
+		// Close the underlying server and then resolve the runner once closed
+		await promiseTimeout(runnerServerCloseTimeoutMs, server.close());
+		if (caller === "uncaughtException") {
+			runningDeferredPromise?.reject({
+				uncaughtException: serializeError(uncaughtException),
+			}); // reject the promise so that the runService exits the process with exit(1)
+		} else {
+			runningDeferredPromise?.resolve();
+		}
+		// runningDeferredPromise = undefined;
+		if (!runnerMetric.isCompleted()) {
+			runnerMetric.success(`${runnerMetric.eventName} stopped`);
+		}
+	} catch (error) {
+		if (!runnerMetric.isCompleted()) {
+			runnerMetric.error(`${runnerMetric.eventName} encountered an error during stop`, error);
+		}
+		if (caller === "sigterm") {
+			runningDeferredPromise?.resolve();
+		} else {
+			// uncaughtException
+			runningDeferredPromise?.reject({
+				forceKill: true,
+				uncaughtException: serializeError(uncaughtException),
+				runnerStopException: serializeError(error),
+			});
+		}
+		// runningDeferredPromise = undefined;
+		throw error;
+	}
+}
