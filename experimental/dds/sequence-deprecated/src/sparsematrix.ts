e607de3f9b68a8caba4f6c2815c3bd4a60c88517
Tony Murphy <anthonm@microsoft.com>
2023-08-11 16:46:24 -0700

    fix lints

--- experimental/dds/sequence-deprecated/src/sparsematrix.ts
+++ experimental/dds/sequence-deprecated/src/sparsematrix.ts
@@ -7,6 +7,7 @@ import { assert } from "@fluidframework/common-utils";
 import { IFluidHandle } from "@fluidframework/core-interfaces";
 import {
 	BaseSegment,
+	createGroupOp,
 	IJSONSegment,
 	IMergeTreeDeltaOp,
 	ISegment,
