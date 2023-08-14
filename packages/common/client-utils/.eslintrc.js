576827b45ce4a94fd65c68ce669249fa4ede1834
Tyler Butler <tyler@tylerbutler.com>
2023-08-14 15:59:10 -0700

    add client-utils

---
+++ packages/common/client-utils/.eslintrc.js
@@ -0,0 +1,20 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = {
+	extends: [require.resolve("@fluidframework/eslint-config-fluid/minimal"), "prettier"],
+	parserOptions: {
+		project: [
+			"./tsconfig.json",
+			"./src/test/mocha/tsconfig.json",
+			"./src/test/jest/tsconfig.json",
+			"./src/test/types/tsconfig.json",
+		],
+	},
+	rules: {
+		// TODO: Remove once this config extends `recommended` or `strict` above.
+		"@typescript-eslint/explicit-function-return-type": "error",
+	},
+};
