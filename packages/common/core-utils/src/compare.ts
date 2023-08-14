ed637bb62cc0b1a39ed70f7837878b83a2ac37b0
Mark Fields <markfields@users.noreply.github.com>
2023-08-14 13:32:34 +0000

    WIP: Two versions, sloppy tests

--- packages/common/core-utils/src/compare.ts
+++ packages/common/core-utils/src/compare.ts
@@ -31,3 +31,95 @@ export const compareArrays = <T>(
 			left.every((leftItem, index) => comparator(leftItem, right[index], index)))
 	);
 };
+
+export function compareObjects(left: unknown, right: unknown) {
+	// Helper function to check if two values are equal.
+	// Only handles values round-trippable through JSON.
+	function deepEquals(a: unknown, b: unknown) {
+		// Start with strict equality
+		if (a === b) {
+			return true;
+		}
+
+		// Strict equality failed. Deep equality only applies to objects,
+		// so only proceed if they're both non-null objects.
+		if (typeof a !== "object" || !a || typeof b !== "object" || !b) {
+			return false;
+		}
+
+		// Ensure the property count matches
+		const keysA = Object.keys(a);
+		const keysB = Object.keys(b);
+		if (keysA.length !== keysB.length) {
+			return false;
+		}
+
+		// Ensure all properties of A are found identically in B
+		for (const key of keysA) {
+			if (!deepEquals(a[key], b[key])) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	// Roundtrip each object through JSON
+	const leftRoundtripped = JSON.parse(JSON.stringify(left));
+	const rightRoundtripped = JSON.parse(JSON.stringify(right));
+
+	// Compare the roundtripped objects
+	return deepEquals(leftRoundtripped, rightRoundtripped);
+}
+
+/**
+ * Deep compare left and right to see if their serializations would be equivalent.
+ * Uses Object.entries to recurse through each object's enumerable properties.
+ * Can't use JSON.stringify because properties aren't ordered, and may miss some edge
+ * cases where serialization is lossy and different values become equivalent
+ * (e.g. NaN serializes to null but that comparison will return false)
+ */
+export const deepCompareForSerialization = (
+	left: unknown, //* Readonly<Record<any, unknown>> | Readonly<Record<number, unknown>>,
+	right: unknown, //* Readonly<Record<any, unknown>> | Readonly<Record<number, unknown>>,
+): boolean => {
+	if (left === right) {
+		return true;
+	}
+
+	// Do not use for comparing primitives - only proceed if both are objects
+	if (typeof left !== "object" || !left || typeof right !== "object" || !right) {
+		return false;
+	}
+
+	const leftEntries = Object.entries(left);
+	const rightEntries = Object.entries(right);
+	if (leftEntries.length !== rightEntries.length) {
+		return false;
+	}
+
+	for (const [key, leftValue] of leftEntries) {
+		//* Necessary?  Maybe for props equal to undefined? Don't think we care.
+		if (!Object.prototype.hasOwnProperty.call(right, key)) {
+			return false;
+		}
+		const rightValue = right[key];
+
+		if (
+			typeof leftValue === "object" &&
+			typeof rightValue === "object" &&
+			deepCompareForSerialization(
+				leftValue as Record<any, unknown>,
+				rightValue as Record<any, unknown>,
+			)
+		) {
+			continue;
+		}
+
+		if (rightValue !== leftValue) {
+			return false;
+		}
+	}
+
+	return true;
+};
---
+++ packages/common/core-utils/src/test/compare/deepCompareObjectEntries.spec.ts
@@ -0,0 +1,120 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "assert";
+import { compareObjects, deepCompareForSerialization } from "../../compare";
+
+const o = { o: "o" };
+const s = Symbol("s");
+
+export const tests_old: [unknown, unknown, boolean][] = [
+	[[], [], true],
+	[[0], [], false],
+	[[], [0], false],
+	[[0], [0], true],
+	[[1], [0], false],
+	[[0], [1], false],
+
+	[[0, 1], [0], false],
+	[[0], [0, 1], false],
+	[[0, 1], [0, 2], false],
+	[[0, 1], [-1, 1], false],
+	[[0, 1], [0, 1], true],
+
+	// Object.is() semantics:
+	[[Number.NaN], [Number.NaN], true],
+	[[0], [-0], false],
+
+	[[null], [undefined], false],
+	[[""], [0], false],
+	[[{}], [{}], false],
+	[[o], [o], true],
+	[[Symbol("sl")], [Symbol("sr")], false],
+	[[s], [s], true],
+];
+
+describe("deepCompareObjectEntries", () => {
+	const jsonEquivalenceClassCases: [any, any, boolean][] = [
+		[-0, +0, true],
+		[null, NaN, true],
+		[[undefined], [null], true],
+		[[1, undefined, 3], [1, null, 3], true],
+	];
+
+	const objectCases: [any, any, boolean][] = [
+		[{}, {}, true],
+		[null, {}, false],
+		[null, null, true],
+		[{ a: undefined }, {}, true],
+		[{ a: undefined }, { a: undefined }, true],
+		[{ a: 1 }, { a: 1 }, true],
+		[{ a: 1 }, { a: 2 }, false],
+		[{ a: 1 }, { b: 1 }, false],
+		[{ a: { b: 1 } }, { a: { b: 1 } }, true],
+		[{ a: { b: 1 } }, { a: { b: 2 } }, false],
+		[{ a: undefined }, { a: null }, false],
+		[{ a: null }, { a: null }, true],
+		[{ a: 1, b: 2 }, { a: 1 }, false],
+		[{ a: 1 }, { a: 1, b: 2 }, false],
+	];
+
+	const bingCases: [any, any, boolean][] = [
+		[{ a: 1, b: 2 }, { a: 1, b: 2 }, true],
+		[{ a: 1, b: 2 }, { a: 1, b: 3 }, false],
+		[{ a: { c: 3 }, b: 2 }, { a: { c: 3 }, b: 2 }, true],
+		[{ a: { c: 3 }, b: 2 }, { a: { c: 4 }, b: 2 }, false],
+		[{ a: 1 }, { a: 1, b: 2 }, false],
+		[{ a: { c: 3 } }, { a: { c: 3 }, b: 2 }, false],
+	];
+
+	const arrayCases: [any, any, boolean][] = [
+		[[], [], true],
+		[[], [0], false],
+		[[0], [0], true],
+		[[1], [0], false],
+		[[1, 2, 3], [1, 2, 3], true],
+		[[1, 2, 3], [1, 2, 4], false],
+		[[1, 2, 3], [1, 2], false],
+		[["one"], { "0": "one" }, true],
+	];
+
+	const nonObjectCases: [any, any, boolean][] = [
+		[0, 0, true],
+		["", "", true],
+		[true, true, true],
+		[undefined, undefined, true],
+		[null, null, true],
+		[Symbol("s"), undefined, true],
+	];
+
+	function check(left: any, right: any, expected: boolean) {
+		it(`${JSON.stringify(left)} and ${JSON.stringify(right)} must ${
+			expected ? "" : "not "
+		}be equal`, () => {
+			const actual = deepCompareForSerialization(left, right);
+			assert.equal(actual, expected);
+			const commuted = compareObjects(right, left);
+			assert.equal(commuted, expected);
+		});
+	}
+
+	const allCases = [
+		...arrayCases,
+		...bingCases,
+		...objectCases,
+		...arrayCases,
+		...nonObjectCases,
+		...jsonEquivalenceClassCases,
+	];
+	const debug: [any, any, boolean][] = [
+		//
+		[{ a: { b: 1 } }, { a: { b: 1 } }, true],
+	];
+	for (const [left, right, expected] of allCases) {
+		check(left, right, expected);
+	}
+	//*
+	return debug;
+});
