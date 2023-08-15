24ec051a095eda4cf8a2b182a6b97189d4af2a63
Jenn <jennle@microsoft.com>
2023-08-15 15:57:38 -0700

    Update experimental/dds/tree2/src/core/tree/visitDelta.ts
    Co-authored-by: yann-achard-MS <97201204+yann-achard-MS@users.noreply.github.com>

--- experimental/dds/tree2/src/core/tree/visitDelta.ts
+++ experimental/dds/tree2/src/core/tree/visitDelta.ts
@@ -260,8 +260,8 @@ function secondPass(delta: Delta.MarkList, visitor: DeltaVisitor, config: PassCo
 					}
 					break;
 				case Delta.MarkType.MoveIn: {
-					// If this move in is the result of a delete and should be treated as a normal delete,
-					// it is handled in the move out case instead.
+					// If this move-in is the result of a removal and the visitor wishes to treat removals as deletions,
+					// then there is no data to move in.
 					if (!config.allowRepairDataMoves && mark.isDelete) {
 						break;
 					}
