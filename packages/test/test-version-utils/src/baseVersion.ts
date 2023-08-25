3942c5c73f0dbe5d343953dcfa303ab8c0ad1cf7
tyler-cai-microsoft <tylercai@microsoft.com>
2023-08-25 14:38:11 -0700

    Use getBaseVersion to sync test version across all versions

--- packages/test/test-version-utils/src/baseVersion.ts
+++ packages/test/test-version-utils/src/baseVersion.ts
@@ -3,13 +3,23 @@
  * Licensed under the MIT License.
  */
 import nconf from "nconf";
+import { isInternalVersionScheme } from "@fluid-tools/version-tools";
 import { resolveVersion } from "./versionUtils.js";
 // This import ensures nconf has been configured to load from correct sources before we compute the right baseVersion.
 // eslint-disable-next-line import/no-unassigned-import
 import "../compatOptions.cjs";
 import { pkgVersion } from "./packageVersion.js";
 
-export const baseVersion = resolveVersion(
-	(nconf.get("fluid:test:baseVersion") as string) ?? pkgVersion,
-	false,
-);
+export function getBaseVersion() {
+	const configVersion = nconf.get("fluid:test:baseVersion");
+	if (configVersion !== undefined) {
+		return configVersion as string;
+	}
+	const codeVersion = process.env.SETVERSION_CODEVERSION;
+	if (codeVersion !== undefined && isInternalVersionScheme(codeVersion, true, true)) {
+		return codeVersion;
+	}
+	return pkgVersion;
+}
+
+export const baseVersion = resolveVersion(getBaseVersion(), false);
