38ecc478aac3e72d5356e610d5fc3adb3a3ada17
Brandon Diaz <BrandonLouisDiaz@gmail.com>
2023-08-29 16:56:45 -0400

    Resolve PR comments

--- server/routerlicious/packages/services-utils/src/documentKeyRetriever.ts
+++ server/routerlicious/packages/services-utils/src/documentKeyRetriever.ts
@@ -4,19 +4,11 @@
  */
 
 import { IDocumentManager } from "@fluidframework/server-services-core";
-import { Lumberjack } from "@fluidframework/server-services-telemetry";
+import { Lumberjack, getLumberBaseProperties } from "@fluidframework/server-services-telemetry";
 
 export class DocumentKeyRetriever {
 	public constructor(
-		/* The type of the redis variable is currently set to "any," which is not ideal. The reason for this
-		 * is that there is currently a different RedisCache and ICache implementation for Routerlicious, Gitrest, and Historian, which all differ slightly.
-		 * The RedisCache intented to be passed into this constructor is the one from Historian, however I am not able to import Historian's ICache or RedisCache into r11s.
-		 * Using r11s' ICache also doesn't work because it has different return types than the Historian ICache, which has templated outputs/inputs for cache.get and cache.set.
-		 *
-		 * Here is what I propose:
-		 * 1. We leave it as type "any" for now
-		 * 2. During Microsoft's FHL week (September 11-15), I will work on refactoring RedisCache such that Routerlicious, Gitrest, and Historian will all use the same ICache/RedisCache (with the prefix being passed into the constructor)
-		 */
+		// TODO: Consolidate implementations of RedisCache class across all services, then change "any" type to Redis cache
 		private readonly redis: any,
 		private readonly documentManager: IDocumentManager,
 		private readonly loggingEnabled: boolean = true,
@@ -35,22 +27,20 @@ export class DocumentKeyRetriever {
 		documentId: string,
 		useCosmosCache: boolean = true,
 	): Promise<ValType> {
-		this.infoLog(
-			`Retrieving value of ${keyName} from cosmosDB for tenant ${tenantId} and document ${documentId}.`,
-		);
+		this.infoLog(`Retrieving value of ${keyName} from cosmosDB.`, documentId, tenantId);
 
 		// Retrieve the cached document details, if it exists for this document
-		const cachedDetails: Record<string, any> | undefined = await this.getCachedDocumentDetails(
-			documentId,
-		);
+		const cachedDetails: Record<string, any> | undefined = useCosmosCache
+			? await this.getCachedDocumentDetails(documentId)
+			: undefined;
 
 		let val: ValType = cachedDetails?.[keyName];
-		if (useCosmosCache && val) {
+		if (val) {
 			// If the cached document details contain the key and useCosmosCache is true, use the cached value
-			this.infoLog("Using cached cosmosDB document details.");
+			this.infoLog("Using cached cosmosDB document details.", documentId, tenantId);
 		} else {
 			// Otherwise, call cosmosDB, and cache the result
-			this.infoLog("Querying cosmosDB and caching the results.");
+			this.infoLog("Querying cosmosDB and caching the results.", documentId, tenantId);
 			const documentDetails: Record<string, any> = await this.documentManager.readDocument(
 				tenantId,
 				documentId,
@@ -78,12 +68,14 @@ export class DocumentKeyRetriever {
 		let val: ValType = await this.getKeyRedis<ValType>(redisKeyName);
 		if (val !== undefined && val !== null) {
 			// Return the value if redis has it
-			this.infoLog(`Found value for ${redisKeyName} in redis.`);
+			this.infoLog(`Found value for ${redisKeyName} in redis.`, documentId, tenantId);
 			return val;
 		} else {
 			// Otherwise, get the value from cosmos and cache the result in redis
 			this.infoLog(
 				`Could not find value for ${redisKeyName} in redis. Falling back to cosmosDB for keyname ${cosmosKeyName}.`,
+				documentId,
+				tenantId,
 			);
 
 			val = await this.getKeyCosmos<ValType>(
@@ -124,9 +116,11 @@ export class DocumentKeyRetriever {
 		return `documentDetails:${documentId}`;
 	}
 
-	private infoLog(message: string) {
+	private infoLog(message: string, documentId?: string, tenantId?: string) {
 		if (this.loggingEnabled) {
-			Lumberjack.info(message);
+			documentId && tenantId
+				? Lumberjack.info(message, getLumberBaseProperties(documentId, tenantId))
+				: Lumberjack.info(message);
 		}
 	}
 }
