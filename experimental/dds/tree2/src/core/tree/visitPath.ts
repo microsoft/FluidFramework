a373c87f8761fc7fb86a1fae88489451d5bfa067
Yann Achard <achardy@microsoft.com>
2023-08-30 13:25:44 -0700

    Draft API

--- experimental/dds/tree2/src/core/tree/visitPath.ts
+++ experimental/dds/tree2/src/core/tree/visitPath.ts
@@ -2,31 +2,84 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-import * as Delta from "./delta";
-import { UpPath } from "./pathTree";
+import { AttachedRangeUpPath, DetachedRangeUpPath } from "./pathTree";
 
 /**
  * Delta visitor for the path tree.
  *
- * TODO: additional callbacks
+ * For any of these events, the paths are guaranteed to be valid at the time of the event,
+ * and it is valid to read from the Forest at that path.
+ *
  * @alpha
  */
 export interface PathVisitor {
 	/**
-	 * A sequence of nodes of length `count` is being deleted starting with `path`.
-	 * Called when these nodes are no longer parented under their previous parent, and do not have a new parent.
-	 * It is possible they may be un-deleted in the future (for example by a conflicted merge or undo).
-	 *
-	 * Not called for children of deleted nodes.
-	 *
-	 * @param path - first node in the deleted range.
-	 * @param count - length of deleted range.
+	 * Invoked after the creation of a range of nodes.
+	 * @param content - The content that was created.
 	 */
-	onDelete(path: UpPath, count: number): void;
+	afterCreate(content: DetachedRangeUpPath): void;
+
 	/**
-	 * @param path - location which first node of inserted range will have after insert.
-	 * Any nodes at this index (or after it) will be moved to the right (have their indexes increased by `content.length`).
-	 * @param content - content which is being inserted.
+	 * Invoked before the replacement of a range of nodes.
+	 * @param oldContent - The content that will be detached.
+	 * @param newContent - The content that will be attached.
+	 * @param kind - The kind of replacement that will occur.
 	 */
-	onInsert(path: UpPath, content: Delta.ProtoNodes): void;
+	beforeReplace(
+		oldContent: AttachedRangeUpPath,
+		newContent: DetachedRangeUpPath,
+		kind: ReplaceKind,
+	): void;
+
+	/**
+	 * Invoked after the replacement of a range of nodes.
+	 * @param oldContent - The content that was detached.
+	 * @param newContent - The content that was attached.
+	 * @param kind - The kind of replacement that occurred.
+	 */
+	afterReplace(
+		oldContent: DetachedRangeUpPath,
+		newContent: AttachedRangeUpPath,
+		kind: ReplaceKind,
+	): void;
+
+	/**
+	 * Invoked before content is destroyed.
+	 * @param content - The content that will be destroyed
+	 */
+	beforeDestroy(content: DetachedRangeUpPath): void;
+}
+
+/**
+ * Describes the specific nature of a replacement.
+ * @alpha
+ */
+enum ReplaceKind {
+	/**
+	 * The new content will truly be at the same location as the old content.
+	 * When that's the case, if user 1 replaces nodes ABC with nodes DEF,
+	 * while user 2 concurrently inserts node X and Y such that they locally see AXBYC,
+	 * the result is guaranteed to be DXEYF.
+	 * This is also true if the inserts of X and Y are performed concurrently by two separate users.
+	 */
+	CellPerfect,
+	/**
+	 * The new content will at location that, so long as concurrent edits are not taken into account,
+	 * is indistinguishable from that of the old content.
+	 * When that's the case, if user 1 replaces nodes ABC with nodes DEF,
+	 * while user 2 concurrently inserts node X and Y such that they locally see AXBYC,
+	 * the result may be any of the following (depending on tie-breaking flags):
+	 * - XYDEF
+	 * - XDEFY
+	 * - DEFXY
+	 * If the inserts of X and Y are performed concurrently by two separate users,
+	 * then the result may be any of the following (depending on tie-breaking flags and sequence ordering):
+	 * - XYDEF (as above)
+	 * - YXDEF (new)
+	 * - XDEFY (as above)
+	 * - YDEFX (new)
+	 * - DEFXY (as above)
+	 * - DEFYX (new)
+	 */
+	SpliceLike,
 }
