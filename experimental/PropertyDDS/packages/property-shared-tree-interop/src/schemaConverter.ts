8bda2a6a85990b59ae5ea777aa941684595a426f
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2023-08-30 11:55:01 -0700

    Fix build

--- experimental/PropertyDDS/packages/property-shared-tree-interop/src/schemaConverter.ts
+++ experimental/PropertyDDS/packages/property-shared-tree-interop/src/schemaConverter.ts
@@ -282,7 +282,7 @@ function buildFieldSchema<Kind extends FieldKindTypes = FieldKindTypes>(
 	allChildrenByType: InheritingChildrenByType,
 	fieldKind: Kind,
 	...fieldTypes: readonly string[]
-): FieldSchema {
+): FieldSchema<Kind> {
 	const allowedTypes: Set<LazyTreeSchema> = new Set();
 	let isAny = false;
 	for (const typeid of fieldTypes) {
