a1b9990d23797bdb57c10ed0d5209bbba2b1af87
alteutli <alteutli@alteutlis-MacBook-Pro.local>
2023-09-04 12:48:43 -0700

    Add lumberjack log

--- server/routerlicious/packages/routerlicious-base/src/nexus/runner.ts
+++ server/routerlicious/packages/routerlicious-base/src/nexus/runner.ts
@@ -107,13 +107,11 @@ export class NexusRunner implements IRunner {
 		httpServer.listen(this.port);
 		httpServer.on("error", (error) => this.onError(error));
 		httpServer.on("listening", () => this.onListening());
-		httpServer.on("upgrade", (req, socket, initialMsgBuffer) => {
-			console.log("UPGRADE This is HTTPSERVER logger middleware");
-			console.log(socket.server._connections);
-			socket.on("error", (error) => {
-				console.log("SOCKET ERROR SS");
-				console.log(error);
-			});
+		httpServer.on("upgrade", (req, socket, initialMsgBuffer) => {    
+		    Lumberjack.info(`WS: Upgraded http request connections: ${socket.server._connections}`);
+		    socket.on("error", (error) => {
+			Lumberjack.error("WS: error", { bytesRead: socket.bytesRead, bytesWritten: socket.bytesWritten, error: error.toString() }, error);
+		    });
 		});
 
 		// Start token manager
