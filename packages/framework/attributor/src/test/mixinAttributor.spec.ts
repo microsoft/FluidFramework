597c1bda82b8925aa83aa73da1ef16fd4c99a9b5
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-09-06 01:04:59 +0000

    Add tests for attributor mixin using loadRuntime

--- packages/framework/attributor/src/test/mixinAttributor.spec.ts
+++ packages/framework/attributor/src/test/mixinAttributor.spec.ts
@@ -85,198 +85,398 @@ describe("mixinAttributor", () => {
 
 	const AttributingContainerRuntime = mixinAttributor();
 
-	it("Attributes ops", async () => {
-		setEnableOnNew(true);
-		const context = getMockContext() as IContainerContext;
-		const containerRuntime = await AttributingContainerRuntime.load(
-			context,
-			[],
-			undefined, // requestHandler
-			{}, // runtimeOptions
-			getScope(),
-		);
+	describe("using ContainerRuntime.load()", () => {
+		it("Attributes ops", async () => {
+			setEnableOnNew(true);
+			const context = getMockContext() as IContainerContext;
+			const containerRuntime = await AttributingContainerRuntime.load(
+				context,
+				[],
+				undefined, // requestHandler
+				{}, // runtimeOptions
+				getScope(),
+			);
 
-		const maybeProvidesAttributor: FluidObject<IProvideRuntimeAttributor> =
-			containerRuntime.scope;
-		assert(maybeProvidesAttributor.IRuntimeAttributor !== undefined);
-		const runtimeAttribution = maybeProvidesAttributor.IRuntimeAttributor;
+			const maybeProvidesAttributor: FluidObject<IProvideRuntimeAttributor> =
+				containerRuntime.scope;
+			assert(maybeProvidesAttributor.IRuntimeAttributor !== undefined);
+			const runtimeAttribution = maybeProvidesAttributor.IRuntimeAttributor;
 
-		const op: Partial<ISequencedDocumentMessage> = {
-			type: "op",
-			sequenceNumber: 7,
-			clientId,
-			timestamp: 1006,
-		};
+			const op: Partial<ISequencedDocumentMessage> = {
+				type: "op",
+				sequenceNumber: 7,
+				clientId,
+				timestamp: 1006,
+			};
 
-		(context.deltaManager as MockDeltaManager).emit("op", op);
+			(context.deltaManager as MockDeltaManager).emit("op", op);
 
-		assert.deepEqual(runtimeAttribution.get({ type: "op", seq: op.sequenceNumber! }), {
-			timestamp: op.timestamp,
-			user: context.audience?.getMember(op.clientId!)?.user,
-		});
-	});
-
-	it("includes attribution association data in the summary tree", async () => {
-		setEnableOnNew(true);
-		const context = getMockContext() as IContainerContext;
-		const containerRuntime = await AttributingContainerRuntime.load(
-			context,
-			[],
-			undefined, // requestHandler
-			{}, // runtimeOptions
-			getScope(),
-		);
-
-		const op: Partial<ISequencedDocumentMessage> = {
-			type: "op",
-			sequenceNumber: 7,
-			clientId,
-			timestamp: 1006,
-		};
-
-		(context.deltaManager as MockDeltaManager).emit("op", op);
-		const { summary } = await containerRuntime.summarize({
-			fullTree: true,
-			trackState: false,
-			runGC: false,
-		});
-
-		const { ".attributor": attributor } = summary.tree;
-		assert(
-			attributor !== undefined && attributor.type === SummaryType.Tree,
-			"summary should contain attributor data",
-		);
-		const opAttributorBlob = attributor.tree.op;
-		assert(
-			opAttributorBlob.type === SummaryType.Blob &&
-				typeof opAttributorBlob.content === "string",
-		);
-		const decoder = chain(
-			new AttributorSerializer((entries) => new Attributor(entries), deltaEncoder),
-			makeLZ4Encoder(),
-		);
-		const decoded = decoder.decode(opAttributorBlob.content);
-		assert.deepEqual(decoded.getAttributionInfo(op.sequenceNumber!), {
-			timestamp: op.timestamp,
-			user: context.audience?.getMember(op.clientId!)?.user,
-		});
-	});
-
-	it("repopulates attribution association data using the summary tree", async () => {
-		const op: Partial<ISequencedDocumentMessage> = {
-			sequenceNumber: 7,
-			clientId,
-			timestamp: 1006,
-		};
-
-		const encoder = chain(
-			new AttributorSerializer((entries) => new Attributor(entries), deltaEncoder),
-			makeLZ4Encoder(),
-		);
-		const context = getMockContext() as Mutable<IContainerContext>;
-		const sampleAttributor = new Attributor([
-			[
-				op.sequenceNumber!,
-				{ timestamp: op.timestamp!, user: context.audience!.getMember(op.clientId!)!.user },
-			],
-		]);
-
-		const opAttributorBlobId = "mock attributor blob id";
-		const mockStorage: IDocumentStorageService = {
-			readBlob: async (blobId: string) => {
-				assert(blobId === opAttributorBlobId);
-				return encoder.encode(sampleAttributor);
-			},
-		} as unknown as IDocumentStorageService;
-		const snapshot: ISnapshotTree = {
-			blobs: {},
-			trees: {
-				".attributor": {
-					blobs: { op: opAttributorBlobId },
-					trees: {},
-				},
-			},
-		};
-		context.baseSnapshot = snapshot;
-		context.storage = mockStorage;
-		const containerRuntime = await AttributingContainerRuntime.load(
-			context,
-			[],
-			undefined, // requestHandler
-			{}, // runtimeOptions
-			getScope(),
-		);
-
-		const maybeProvidesAttributor: FluidObject<IProvideRuntimeAttributor> =
-			containerRuntime.scope;
-		assert(maybeProvidesAttributor.IRuntimeAttributor !== undefined);
-		const runtimeAttribution = maybeProvidesAttributor.IRuntimeAttributor;
-
-		assert.deepEqual(runtimeAttribution.get({ type: "op", seq: op.sequenceNumber! }), {
-			timestamp: op.timestamp,
-			user: context.audience?.getMember(op.clientId!)?.user,
-		});
-	});
-
-	describe("Doesn't summarize attributor", () => {
-		const testCases: { getContext: () => IContainerContext; testName: string }[] = [
-			{
-				testName: "for existing documents that had no attributor",
-				getContext: () => {
-					setEnableOnNew(true);
-					const context = getMockContext() as Mutable<IContainerContext>;
-					const snapshot: ISnapshotTree = {
-						blobs: {},
-						trees: {},
-					};
-					context.baseSnapshot = snapshot;
-					return context;
-				},
-			},
-			{
-				testName: `for new documents with ${enableOnNewFileKey} unset`,
-				getContext: () => {
-					return getMockContext() as IContainerContext;
-				},
-			},
-			{
-				testName: `for new documents with ${enableOnNewFileKey} set to false`,
-				getContext: () => {
-					setEnableOnNew(false);
-					const context = getMockContext() as Mutable<IContainerContext>;
-					const snapshot: ISnapshotTree = {
-						blobs: {},
-						trees: {},
-					};
-					context.baseSnapshot = snapshot;
-					return context;
-				},
-			},
-		];
-
-		for (const { getContext, testName } of testCases) {
-			it(testName, async () => {
-				const context = getContext();
-				const containerRuntime = await AttributingContainerRuntime.load(
-					context,
-					[],
-					undefined, // requestHandler
-					{}, // runtimeOptions
-					getScope(),
-				);
-
-				const maybeProvidesAttributor: FluidObject<IProvideRuntimeAttributor> =
-					containerRuntime.scope;
-				assert(maybeProvidesAttributor.IRuntimeAttributor !== undefined);
-
-				const { summary } = await containerRuntime.summarize({
-					fullTree: true,
-					trackState: false,
-					runGC: false,
-				});
-				assert(summary.tree[".attributor"] === undefined);
+			assert.deepEqual(runtimeAttribution.get({ type: "op", seq: op.sequenceNumber! }), {
+				timestamp: op.timestamp,
+				user: context.audience?.getMember(op.clientId!)?.user,
 			});
-		}
+		});
+
+		it("includes attribution association data in the summary tree", async () => {
+			setEnableOnNew(true);
+			const context = getMockContext() as IContainerContext;
+			const containerRuntime = await AttributingContainerRuntime.load(
+				context,
+				[],
+				undefined, // requestHandler
+				{}, // runtimeOptions
+				getScope(),
+			);
+
+			const op: Partial<ISequencedDocumentMessage> = {
+				type: "op",
+				sequenceNumber: 7,
+				clientId,
+				timestamp: 1006,
+			};
+
+			(context.deltaManager as MockDeltaManager).emit("op", op);
+			const { summary } = await containerRuntime.summarize({
+				fullTree: true,
+				trackState: false,
+				runGC: false,
+			});
+
+			const { ".attributor": attributor } = summary.tree;
+			assert(
+				attributor !== undefined && attributor.type === SummaryType.Tree,
+				"summary should contain attributor data",
+			);
+			const opAttributorBlob = attributor.tree.op;
+			assert(
+				opAttributorBlob.type === SummaryType.Blob &&
+					typeof opAttributorBlob.content === "string",
+			);
+			const decoder = chain(
+				new AttributorSerializer((entries) => new Attributor(entries), deltaEncoder),
+				makeLZ4Encoder(),
+			);
+			const decoded = decoder.decode(opAttributorBlob.content);
+			assert.deepEqual(decoded.getAttributionInfo(op.sequenceNumber!), {
+				timestamp: op.timestamp,
+				user: context.audience?.getMember(op.clientId!)?.user,
+			});
+		});
+
+		it("repopulates attribution association data using the summary tree", async () => {
+			const op: Partial<ISequencedDocumentMessage> = {
+				sequenceNumber: 7,
+				clientId,
+				timestamp: 1006,
+			};
+
+			const encoder = chain(
+				new AttributorSerializer((entries) => new Attributor(entries), deltaEncoder),
+				makeLZ4Encoder(),
+			);
+			const context = getMockContext() as Mutable<IContainerContext>;
+			const sampleAttributor = new Attributor([
+				[
+					op.sequenceNumber!,
+					{
+						timestamp: op.timestamp!,
+						user: context.audience!.getMember(op.clientId!)!.user,
+					},
+				],
+			]);
+
+			const opAttributorBlobId = "mock attributor blob id";
+			const mockStorage: IDocumentStorageService = {
+				readBlob: async (blobId: string) => {
+					assert(blobId === opAttributorBlobId);
+					return encoder.encode(sampleAttributor);
+				},
+			} as unknown as IDocumentStorageService;
+			const snapshot: ISnapshotTree = {
+				blobs: {},
+				trees: {
+					".attributor": {
+						blobs: { op: opAttributorBlobId },
+						trees: {},
+					},
+				},
+			};
+			context.baseSnapshot = snapshot;
+			context.storage = mockStorage;
+			const containerRuntime = await AttributingContainerRuntime.load(
+				context,
+				[],
+				undefined, // requestHandler
+				{}, // runtimeOptions
+				getScope(),
+			);
+
+			const maybeProvidesAttributor: FluidObject<IProvideRuntimeAttributor> =
+				containerRuntime.scope;
+			assert(maybeProvidesAttributor.IRuntimeAttributor !== undefined);
+			const runtimeAttribution = maybeProvidesAttributor.IRuntimeAttributor;
+
+			assert.deepEqual(runtimeAttribution.get({ type: "op", seq: op.sequenceNumber! }), {
+				timestamp: op.timestamp,
+				user: context.audience?.getMember(op.clientId!)?.user,
+			});
+		});
+
+		describe("Doesn't summarize attributor", () => {
+			const testCases: { getContext: () => IContainerContext; testName: string }[] = [
+				{
+					testName: "for existing documents that had no attributor",
+					getContext: () => {
+						setEnableOnNew(true);
+						const context = getMockContext() as Mutable<IContainerContext>;
+						const snapshot: ISnapshotTree = {
+							blobs: {},
+							trees: {},
+						};
+						context.baseSnapshot = snapshot;
+						return context;
+					},
+				},
+				{
+					testName: `for new documents with ${enableOnNewFileKey} unset`,
+					getContext: () => {
+						return getMockContext() as IContainerContext;
+					},
+				},
+				{
+					testName: `for new documents with ${enableOnNewFileKey} set to false`,
+					getContext: () => {
+						setEnableOnNew(false);
+						const context = getMockContext() as Mutable<IContainerContext>;
+						const snapshot: ISnapshotTree = {
+							blobs: {},
+							trees: {},
+						};
+						context.baseSnapshot = snapshot;
+						return context;
+					},
+				},
+			];
+
+			for (const { getContext, testName } of testCases) {
+				it(testName, async () => {
+					const context = getContext();
+					const containerRuntime = await AttributingContainerRuntime.load(
+						context,
+						[],
+						undefined, // requestHandler
+						{}, // runtimeOptions
+						getScope(),
+					);
+
+					const maybeProvidesAttributor: FluidObject<IProvideRuntimeAttributor> =
+						containerRuntime.scope;
+					assert(maybeProvidesAttributor.IRuntimeAttributor !== undefined);
+
+					const { summary } = await containerRuntime.summarize({
+						fullTree: true,
+						trackState: false,
+						runGC: false,
+					});
+					assert(summary.tree[".attributor"] === undefined);
+				});
+			}
+		});
+	});
+
+	describe("using ContainerRuntime.loadRuntime()", () => {
+		it("Attributes ops", async () => {
+			setEnableOnNew(true);
+			const context = getMockContext() as IContainerContext;
+			const containerRuntime = await AttributingContainerRuntime.loadRuntime({
+				context,
+				registryEntries: [],
+				containerScope: getScope(),
+				existing: false,
+			});
+
+			const maybeProvidesAttributor: FluidObject<IProvideRuntimeAttributor> =
+				containerRuntime.scope;
+			assert(maybeProvidesAttributor.IRuntimeAttributor !== undefined);
+			const runtimeAttribution = maybeProvidesAttributor.IRuntimeAttributor;
+
+			const op: Partial<ISequencedDocumentMessage> = {
+				type: "op",
+				sequenceNumber: 7,
+				clientId,
+				timestamp: 1006,
+			};
+
+			(context.deltaManager as MockDeltaManager).emit("op", op);
+
+			assert.deepEqual(runtimeAttribution.get({ type: "op", seq: op.sequenceNumber! }), {
+				timestamp: op.timestamp,
+				user: context.audience?.getMember(op.clientId!)?.user,
+			});
+		});
+
+		it("includes attribution association data in the summary tree", async () => {
+			setEnableOnNew(true);
+			const context = getMockContext() as IContainerContext;
+			const containerRuntime = await AttributingContainerRuntime.loadRuntime({
+				context,
+				registryEntries: [],
+				containerScope: getScope(),
+				existing: false,
+			});
+
+			const op: Partial<ISequencedDocumentMessage> = {
+				type: "op",
+				sequenceNumber: 7,
+				clientId,
+				timestamp: 1006,
+			};
+
+			(context.deltaManager as MockDeltaManager).emit("op", op);
+			const { summary } = await containerRuntime.summarize({
+				fullTree: true,
+				trackState: false,
+				runGC: false,
+			});
+
+			const { ".attributor": attributor } = summary.tree;
+			assert(
+				attributor !== undefined && attributor.type === SummaryType.Tree,
+				"summary should contain attributor data",
+			);
+			const opAttributorBlob = attributor.tree.op;
+			assert(
+				opAttributorBlob.type === SummaryType.Blob &&
+					typeof opAttributorBlob.content === "string",
+			);
+			const decoder = chain(
+				new AttributorSerializer((entries) => new Attributor(entries), deltaEncoder),
+				makeLZ4Encoder(),
+			);
+			const decoded = decoder.decode(opAttributorBlob.content);
+			assert.deepEqual(decoded.getAttributionInfo(op.sequenceNumber!), {
+				timestamp: op.timestamp,
+				user: context.audience?.getMember(op.clientId!)?.user,
+			});
+		});
+
+		it("repopulates attribution association data using the summary tree", async () => {
+			const op: Partial<ISequencedDocumentMessage> = {
+				sequenceNumber: 7,
+				clientId,
+				timestamp: 1006,
+			};
+
+			const encoder = chain(
+				new AttributorSerializer((entries) => new Attributor(entries), deltaEncoder),
+				makeLZ4Encoder(),
+			);
+			const context = getMockContext() as Mutable<IContainerContext>;
+			const sampleAttributor = new Attributor([
+				[
+					op.sequenceNumber!,
+					{
+						timestamp: op.timestamp!,
+						user: context.audience!.getMember(op.clientId!)!.user,
+					},
+				],
+			]);
+
+			const opAttributorBlobId = "mock attributor blob id";
+			const mockStorage: IDocumentStorageService = {
+				readBlob: async (blobId: string) => {
+					assert(blobId === opAttributorBlobId);
+					return encoder.encode(sampleAttributor);
+				},
+			} as unknown as IDocumentStorageService;
+			const snapshot: ISnapshotTree = {
+				blobs: {},
+				trees: {
+					".attributor": {
+						blobs: { op: opAttributorBlobId },
+						trees: {},
+					},
+				},
+			};
+			context.baseSnapshot = snapshot;
+			context.storage = mockStorage;
+			const containerRuntime = await AttributingContainerRuntime.loadRuntime({
+				context,
+				registryEntries: [],
+				containerScope: getScope(),
+				existing: false,
+			});
+
+			const maybeProvidesAttributor: FluidObject<IProvideRuntimeAttributor> =
+				containerRuntime.scope;
+			assert(maybeProvidesAttributor.IRuntimeAttributor !== undefined);
+			const runtimeAttribution = maybeProvidesAttributor.IRuntimeAttributor;
+
+			assert.deepEqual(runtimeAttribution.get({ type: "op", seq: op.sequenceNumber! }), {
+				timestamp: op.timestamp,
+				user: context.audience?.getMember(op.clientId!)?.user,
+			});
+		});
+
+		describe("Doesn't summarize attributor", () => {
+			const testCases: { getContext: () => IContainerContext; testName: string }[] = [
+				{
+					testName: "for existing documents that had no attributor",
+					getContext: () => {
+						setEnableOnNew(true);
+						const context = getMockContext() as Mutable<IContainerContext>;
+						const snapshot: ISnapshotTree = {
+							blobs: {},
+							trees: {},
+						};
+						context.baseSnapshot = snapshot;
+						return context;
+					},
+				},
+				{
+					testName: `for new documents with ${enableOnNewFileKey} unset`,
+					getContext: () => {
+						return getMockContext() as IContainerContext;
+					},
+				},
+				{
+					testName: `for new documents with ${enableOnNewFileKey} set to false`,
+					getContext: () => {
+						setEnableOnNew(false);
+						const context = getMockContext() as Mutable<IContainerContext>;
+						const snapshot: ISnapshotTree = {
+							blobs: {},
+							trees: {},
+						};
+						context.baseSnapshot = snapshot;
+						return context;
+					},
+				},
+			];
+
+			for (const { getContext, testName } of testCases) {
+				it(testName, async () => {
+					const context = getContext();
+					const containerRuntime = await AttributingContainerRuntime.loadRuntime({
+						context,
+						registryEntries: [],
+						containerScope: getScope(),
+						existing: false,
+					});
+
+					const maybeProvidesAttributor: FluidObject<IProvideRuntimeAttributor> =
+						containerRuntime.scope;
+					assert(maybeProvidesAttributor.IRuntimeAttributor !== undefined);
+
+					const { summary } = await containerRuntime.summarize({
+						fullTree: true,
+						trackState: false,
+						runGC: false,
+					});
+					assert(summary.tree[".attributor"] === undefined);
+				});
+			}
+		});
 	});
 });
 
