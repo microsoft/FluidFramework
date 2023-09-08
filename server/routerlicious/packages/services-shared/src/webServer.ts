a3bfb8465647037e0e082865a916510f197a584a
Zach Newton <znewton@microsoft.com>
2023-09-08 13:40:56 -0700

    Polish cluster server factories

--- server/routerlicious/packages/services-shared/src/webServer.ts
+++ server/routerlicious/packages/services-shared/src/webServer.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import cluster from "cluster";
+import cluster, { Worker } from "cluster";
 // Note: `availableParallelism` Node 18 is required for this functionality
 import { availableParallelism } from "os";
 import * as http from "http";
@@ -112,121 +112,232 @@ export class BasicWebServerFactory implements core.IWebServerFactory {
 /**
  * Node.js Clustering POC.
  * TODO:
- * - Allow configuring heartbeat interval and timeout
  * - Add more to the heartbeat, like CPU and Memory usage, with related process kill checks: process.cpuUsage() and process.memoryUsage()
- * - Allow configuring max number of spawned workers
- * - Gracefully kill workers with worker.disconnect(): https://nodejs.org/docs/latest-v16.x/api/cluster.html#workerdisconnect
- * - Add timeout for graceful worker shutdown: https://nodejs.org/docs/latest-v16.x/api/cluster.html#workerkillsignal
- * - Spawn new worker after killing a worker
- * - Add fork/listening timeouts: https://nodejs.org/docs/latest-v16.x/api/cluster.html#event-fork
  */
 
-// TODO: Move all of this to a shared space and configure whether to use clustering or not
 interface IWorkerMessage<T> {
 	type: string;
 	data?: T;
 }
-interface IWorkerHeartbeat extends IWorkerMessage<undefined> {
+interface IWorkerHeartbeatMessage extends IWorkerMessage<undefined> {
 	type: "heartbeat";
 	// TODO: Add relevant data to heartbeat message, like CPU usage, memory usage, etc.
 	// See process.cpuUsage() and process.memoryUsage(). We could use these to pre-emptively kill runaway processes.
 }
-type WorkerMessage = IWorkerHeartbeat;
+interface IWorkerShutdownMessage extends IWorkerMessage<undefined> {
+	type: "shutdown";
+}
+export interface IClusterConfig {
+	workerHeartbeatIntervalMs: number;
+	workerTimeoutNumMissedHeartbeats: number;
+	workerForkTimeoutMs: number;
+	numMaxWorkers: number;
+}
+type WorkerMessage = IWorkerHeartbeatMessage | IWorkerShutdownMessage;
 
-export class SocketIoClusterWebServerFactory implements core.IWebServerFactory {
+export class ClusterWebServerFactory implements core.IWebServerFactory {
+	private readonly lastHeartbeatMap: Map<number, number> = new Map();
+	private readonly newForkTimeouts: Map<number, NodeJS.Timeout> = new Map();
+	private readonly disconnectTimeouts: Map<number, NodeJS.Timeout> = new Map();
+	private readonly heartbeatIntervalMs: number;
+
+	constructor(
+		private readonly httpServerConfig?: IHttpServerConfig,
+		private readonly clusterConfig?: Partial<IClusterConfig>,
+	) {
+		this.heartbeatIntervalMs = this.clusterConfig?.workerHeartbeatIntervalMs ?? 5000;
+	}
+
+	public create(requestListener: RequestListener): core.IWebServer {
+		const httpServer = cluster.isPrimary
+			? this.initializePrimaryThread()
+			: this.initializeWorkerThread(requestListener);
+
+		return new WebServer(new HttpServer(httpServer), null as unknown as core.IWebSocketServer);
+	}
+
+	protected initializePrimaryThread(
+		additionalServerSetup?: (server: http.Server) => void,
+	): http.Server {
+		// Init Primary cluster thread
+		Lumberjack.info(`Primary cluster process is running.`, { pid: process.pid });
+
+		// Create a blank HTTP Server that will distribute incoming requests to worker nodes.
+		const server = createAndConfigureHttpServer(undefined, undefined);
+		additionalServerSetup?.(server);
+
+		// Regularly kill stuck workers
+		const heartbeatTimeoutMs =
+			(this.clusterConfig?.workerTimeoutNumMissedHeartbeats ?? 3) * this.heartbeatIntervalMs;
+		setInterval(() => {
+			for (const [workerId, lastHeartbeat] of this.lastHeartbeatMap.entries()) {
+				const msSinceLastHeartbeat = Date.now() - lastHeartbeat;
+				if (msSinceLastHeartbeat >= heartbeatTimeoutMs) {
+					const worker = cluster.workers?.[workerId];
+					if (!worker) {
+						Lumberjack.error("Could not find worker after heartbeat timeout.", {
+							id: workerId,
+						});
+						return;
+					}
+					Lumberjack.error(`Worker heartbeat timeout.`, {
+						pid: worker?.process.pid,
+						msSinceLastHeartbeat,
+					});
+					this.killWorker(worker);
+				}
+			}
+		}, heartbeatTimeoutMs);
+
+		// Spawn initial number of workers according to configs or available CPUs
+		const numWorkers = this.clusterConfig?.numMaxWorkers ?? availableParallelism();
+		Lumberjack.info(`Spawning ${numWorkers} cluster workers.`);
+		for (let i = 0; i < numWorkers; i++) {
+			this.spawnWorker();
+		}
+
+		// Kill workers when they take too long to spawn.
+		cluster.on("fork", (worker) => {
+			const timeout = setTimeout(() => {
+				Lumberjack.error("Timed out waiting for worker to spawn.", {
+					id: worker.id,
+					pid: worker.process.pid,
+				});
+
+				// Make sure worker dies.
+				this.killWorker(worker);
+
+				// Remove timeout from map.
+				this.newForkTimeouts.delete(worker.id);
+			}, this.clusterConfig?.workerForkTimeoutMs);
+			this.newForkTimeouts.set(worker.id, timeout);
+		});
+		cluster.on("listening", (worker, address) => {
+			clearTimeout(this.newForkTimeouts.get(worker.id));
+			this.newForkTimeouts.delete(worker.id);
+		});
+
+		// Listen for exiting workers.
+		cluster.on("exit", (worker, code, signal) => {
+			const logLevel = signal || code !== 0 ? "error" : "info";
+			Lumberjack[logLevel](`Worker died.`, {
+				code,
+				signal,
+				pid: worker.process.pid,
+				id: worker.id,
+			});
+
+			// Clear timeout trackers
+			this.lastHeartbeatMap.delete(worker.id);
+			clearTimeout(this.newForkTimeouts.get(worker.id));
+			this.newForkTimeouts.delete(worker.id);
+			clearTimeout(this.disconnectTimeouts.get(worker.id));
+			this.disconnectTimeouts.delete(worker.id);
+
+			// Spawn a new worker to replace dead worker if died due to error.
+			if (logLevel === "error") {
+				Lumberjack.info(`Spawning new worker to replace dead worker.`);
+				this.spawnWorker();
+			}
+		});
+
+		return server;
+	}
+
+	protected initializeWorkerThread(requestListener: RequestListener): http.Server {
+		// Init Worker cluster thread
+		Lumberjack.info(`Worker process is running.`, { pid: process.pid });
+
+		// Send a heartbeat message on an interval
+		setInterval(() => {
+			const heartbeatMsg: IWorkerHeartbeatMessage = { type: "heartbeat" };
+			process.send?.(heartbeatMsg);
+		}, this.heartbeatIntervalMs);
+
+		// Create the base HTTP server and register the provided request listener
+		const server = createAndConfigureHttpServer(requestListener, this.httpServerConfig);
+
+		// Handle messages from primary thread in worker thread.
+		process.on("message", (message: WorkerMessage) => {
+			if (message.type === "shutdown") {
+				server.closeAllConnections();
+			}
+		});
+
+		return server;
+	}
+
+	protected spawnWorker(): Worker {
+		const worker = cluster.fork();
+		// Handle messages from worker thread in primary thread
+		worker.on("message", (message: WorkerMessage) => {
+			if (message.type === "heartbeat") {
+				this.lastHeartbeatMap.set(worker.id, Date.now());
+				return;
+			}
+		});
+		return worker;
+	}
+
+	protected killWorker(worker: Worker): void {
+		Lumberjack.info("Killing worker.", {
+			id: worker.id,
+			pid: worker.process.pid,
+		});
+
+		// Attempt to gracefully shutdown the worker.
+		const shutdownMessage: IWorkerShutdownMessage = { type: "shutdown" };
+		worker.send(shutdownMessage);
+		worker.disconnect();
+		// Forcefully kill the worker if it takes too long to gracefully close.
+		const disconnectTimeout = setTimeout(() => {
+			worker.kill();
+		}, 2000);
+		this.disconnectTimeouts.set(worker.id, disconnectTimeout);
+		worker.on("disconnect", () => {
+			clearTimeout(disconnectTimeout);
+			this.disconnectTimeouts.delete(worker.id);
+		});
+
+		// TODO: Add timeout for worker to exit gracefully?
+		// https://nodejs.org/docs/latest-v18.x/api/cluster.html#workerkillsignal
+	}
+}
+
+export class SocketIoClusterWebServerFactory extends ClusterWebServerFactory {
 	constructor(
 		private readonly redisConfig: any,
 		private readonly socketIoAdapterConfig?: any,
-		private readonly httpServerConfig?: IHttpServerConfig,
+		httpServerConfig?: IHttpServerConfig,
 		private readonly socketIoConfig?: any,
-	) {}
+		clusterConfig?: Partial<IClusterConfig>,
+	) {
+		super(httpServerConfig, clusterConfig);
+	}
 
 	public create(requestListener: RequestListener): core.IWebServer {
-		// TODO: move these to configs
-		const heartbeatIntervalMs = 5000;
-		const heartbeatTimeoutMs = 3 * heartbeatIntervalMs;
+		const httpServer = cluster.isPrimary
+			? this.initializePrimaryThread((server) => {
+					// Configure Socket.io Sticky load balancing
+					setupMaster(server, {
+						loadBalancingMethod: "least-connection", // either "random", "round-robin" or "least-connection"
+					});
+			  })
+			: this.initializeWorkerThread(requestListener);
 		if (cluster.isPrimary) {
-			// Init Primary cluster thread
-			Lumberjack.info(`Primary cluster process is running.`, { pid: process.pid });
-			// Create a blank HTTP Server that will distribute incoming requests to worker nodes.
-			const server = createAndConfigureHttpServer(undefined, undefined);
-			const httpServer = new HttpServer(server);
-			setupMaster(server, {
-				loadBalancingMethod: "least-connection", // either "random", "round-robin" or "least-connection"
-			});
-			const numCPUs = availableParallelism();
-			Lumberjack.info(`Spawning ${numCPUs} cluster workers.`);
-			const lastHeartbeatMap: Record<string, number> = {};
-			// Regularly kill stuck workers
-			setInterval(() => {
-				for (const [workerId, lastHeartbeat] of Object.entries(lastHeartbeatMap)) {
-					if (lastHeartbeat < Date.now() - heartbeatTimeoutMs) {
-						const worker = cluster.workers?.[workerId];
-						Lumberjack.error(`Worker heartbeat timeout.`, { pid: worker?.process.pid });
-						// TODO: Should we do a more graceful shutdown with worker.disconnect()?
-						// https://nodejs.org/docs/latest-v16.x/api/cluster.html#workerdisconnect
-						worker?.kill();
-						// TODO: Add timeout for worker to exit gracefully?
-						// https://nodejs.org/docs/latest-v16.x/api/cluster.html#workerkillsignal
-						// TODO: delete worker from heartbeat map?
-					}
-				}
-			}, heartbeatTimeoutMs);
-			const spawnWorker = () => {
-				const worker = cluster.fork();
-				worker.on("message", (message: WorkerMessage) => {
-					if (message.type === "heartbeat") {
-						lastHeartbeatMap[worker.id.toString()] = Date.now();
-						return;
-					}
-				});
-			};
-			for (let i = 0; i < numCPUs; i++) {
-				spawnWorker();
-			}
-
-			// TODO: add fork/listening timeouts?
-			// https://nodejs.org/docs/latest-v16.x/api/cluster.html#event-fork
-
-			cluster.on("exit", (worker, code, signal) => {
-				Lumberjack.info(`Worker died.`, { code, signal, pid: worker.process.pid });
-				// TODO: spawn a new worker to replace dead worker?
-			});
-
-			return new WebServer(httpServer, null as unknown as core.IWebSocketServer);
+			return new WebServer(
+				new HttpServer(httpServer),
+				null as unknown as core.IWebSocketServer,
+			);
 		} else {
-			// Init Worker cluster thread
-			Lumberjack.info(`Worker process is running.`, { pid: process.pid });
-
-			setInterval(() => {
-				const heartbeatMsg: IWorkerHeartbeat = { type: "heartbeat" };
-				process.send?.(heartbeatMsg);
-			}, heartbeatIntervalMs);
-
-			// Create the base HTTP server and register the provided request listener
-			const server = createAndConfigureHttpServer(requestListener, this.httpServerConfig);
-			const httpServer = new HttpServer(server);
 			const socketIoServer = socketIo.create(
 				this.redisConfig,
-				server,
+				httpServer,
 				this.socketIoAdapterConfig,
 				this.socketIoConfig,
 				setupWorker,
 			);
-
-			return new WebServer(httpServer, socketIoServer);
+			return new WebServer(new HttpServer(httpServer), socketIoServer);
 		}
-	}
-}
-
-export class ClusterWebServerFactory implements core.IWebServerFactory {
-	constructor(private readonly httpServerConfig?: IHttpServerConfig) {}
-
-	public create(requestListener: RequestListener): core.IWebServer {
-		// Create the base HTTP server and register the provided request listener
-		const server = createAndConfigureHttpServer(requestListener, this.httpServerConfig);
-		const httpServer = new HttpServer(server);
-
-		return new WebServer(httpServer, null as unknown as core.IWebSocketServer);
 	}
 }
