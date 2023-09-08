9e657be9371a5d271b18b0527f2af2b40fb8efaf
Willie Habimana <williehabimana@Willies-MacBook-Pro.local>
2023-09-08 11:39:43 -0700

    formatting

--- examples/apps/presence-tracker/webpack.config.js
+++ examples/apps/presence-tracker/webpack.config.js
@@ -41,7 +41,7 @@ module.exports = (env) => {
 					template: "./src/index.html",
 				}),
 				new webpack.ProvidePlugin({
-      		process: 'process/browser',
+      				process: 'process/browser',
     			}),
 			],
 		},
