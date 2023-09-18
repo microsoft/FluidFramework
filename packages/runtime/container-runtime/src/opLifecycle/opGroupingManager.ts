39f2c092140f3dad8ae1e58bd671416353c2fc35
Jason Hartman <jasonha@microsoft.com>
2023-09-18 11:35:28 -0700

    refactor: let assertion take `any`

--- packages/runtime/container-runtime/src/opLifecycle/opGroupingManager.ts
+++ packages/runtime/container-runtime/src/opLifecycle/opGroupingManager.ts
@@ -22,12 +22,8 @@ interface IGroupedMessage {
 	compression?: string;
 }
 
-function isGroupContents(opContents: unknown): opContents is IGroupedBatchMessageContents {
-	return (
-		typeof opContents === "object" &&
-		// eslint-disable-next-line @typescript-eslint/dot-notation
-		opContents?.["type"] === OpGroupingManager.groupedBatchOp
-	);
+function isGroupContents(opContents: any): opContents is IGroupedBatchMessageContents {
+	return opContents?.type === OpGroupingManager.groupedBatchOp;
 }
 
 export class OpGroupingManager {
