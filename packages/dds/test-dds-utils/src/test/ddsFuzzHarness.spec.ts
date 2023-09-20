7f1ab0e2d9b274869a5a027b8eece479e3f2c9bd
Tyler Butler <tyler@tylerbutler.com>
2023-09-19 17:34:04 -0700

    update

--- packages/dds/test-dds-utils/src/test/ddsFuzzHarness.spec.ts
+++ packages/dds/test-dds-utils/src/test/ddsFuzzHarness.spec.ts
@@ -866,7 +866,6 @@ describe("DDS Fuzz Harness", () => {
 				assert(fs.existsSync(path.join(jsonDir, "0.json")));
 				assert(fs.existsSync(path.join(jsonDir, "1.json")));
 				const contents: unknown = JSON.parse(
-					// Our ES target doesn't support JSON.parsing a Buffer directly
 					// eslint-disable-next-line unicorn/prefer-json-parse-buffer
 					fs.readFileSync(path.join(jsonDir, "0.json"), { encoding: "utf8" }),
 				);
