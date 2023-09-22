9e404b02919aa3019047ac1f06f02120ec9f45bc
Sonali Deshpande <48232592+sonalideshpandemsft@users.noreply.github.com>
2023-09-22 20:01:44 +0000

    feedback

--- build-tools/packages/build-cli/src/commands/release/setDtsTypes.ts
+++ build-tools/packages/build-cli/src/commands/release/setDtsTypes.ts
@@ -2,36 +2,38 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-import { Flags } from "@oclif/core";
+import { Flags, Config } from "@oclif/core";
 import { Package, updatePackageJsonFile } from "@fluidframework/build-tools";
 import path from "node:path";
-import { PackageCommand } from "../BasePackageCommand";
+import { PackageCommand } from "../../BasePackageCommand";
 import * as fs from "fs";
 
-/**
- * A type defining the four release types
- */
-type UpdatePackageJsonType = "alpha" | "beta" | "public";
+interface PackageTypesList {
+	packagesUpdated: string[];
+	packagesNotUpdated: string[];
+}
 
-/**
- * A typeguard to check if a string is a {@link UpdatePackageJsonEnum}.
- */
-function isUpdatePackageJsonTypes(type: string): type is UpdatePackageJsonType {
-	return type === "alpha" || type === "beta" || type === "public";
+const knownReleaseTag = ["alpha", "beta", "public"] as const;
+
+type ReleaseTag = typeof knownReleaseTag[number];
+
+function isReleaseTag(str: string | undefined): str is ReleaseTag {
+	return str === undefined ? false : knownReleaseTag.includes(str as any);
 }
 
 export default class SetReleaseTagPublishingCommand extends PackageCommand<
 	typeof SetReleaseTagPublishingCommand
 > {
 	static readonly description =
-		"Updates the types in package.json based on the release flag. This command is intended for use in publishing pipelines and may not be intended for regular developer use.";
+		"Updates the .d.ts types that are included in the `types` field in package.json. This command is intended to be used in CI only.";
 
-	static flags = {
-		types: Flags.custom<UpdatePackageJsonType>({
-			description:
-				"The types flag is used to specify the type of release when updating a package.json file. It accepts a custom enumeration called UpdatePackageJsonEnum that defines different release types.",
+	static readonly enableJsonFlag = true;
+
+	static readonly flags = {
+		types: Flags.custom<ReleaseTag>({
+			description: "Which .d.ts types to include in the published package.",
 			parse: async (input) => {
-				if (isUpdatePackageJsonTypes(input)) {
+				if (isReleaseTag(input)) {
 					return input;
 				}
 				throw new Error(`Invalid release type: ${input}`);
@@ -40,15 +42,23 @@ export default class SetReleaseTagPublishingCommand extends PackageCommand<
 		...PackageCommand.flags,
 	};
 
-	private readonly packageTypesList: string[] = [];
+	private readonly packageList: PackageTypesList;
+
+	constructor(argv: string[], config: Config) {
+		super(argv, config);
+		this.packageList = {
+			packagesNotUpdated: [],
+			packagesUpdated: [],
+		};
+	}
 
 	protected async processPackage(pkg: Package): Promise<void> {
+		let packageUpdate: boolean = false;
 		// Define paths to api-extractor.json
 		const apiExtractorConfigFilePath = path.join(pkg.directory, "api-extractor.json");
 
 		// Check if api-extractor.json exists in the package directory
 		const apiExtractorConfigExists = fs.existsSync(apiExtractorConfigFilePath);
-
 		updatePackageJsonFile(pkg.directory, (json) => {
 			const types: string | undefined = json.types ?? json.typings;
 
@@ -74,31 +84,32 @@ export default class SetReleaseTagPublishingCommand extends PackageCommand<
 
 					if (config) {
 						json.types = `${typesFolder}/${pkg.nameUnscoped}-${this.flags.types}.d.ts`;
-						this.packageTypesList.push(pkg.name);
+						packageUpdate = true;
 					}
 				} else if (includesAlphaBeta) {
 					json.types = `${typesFolder}/index.d.ts`;
-					this.packageTypesList.push(pkg.name);
+					packageUpdate = true;
+				}
+
+				if (packageUpdate) {
+					this.packageList.packagesUpdated.push(pkg.name);
+				} else {
+					this.packageList.packagesNotUpdated.push(pkg.name);
 				}
 			}
 		});
 	}
 
-	public async run(): Promise<void> {
+	public async run(): Promise<PackageTypesList> {
 		// Calls processPackage on all packages.
 		await super.run();
 
 		const flags = this.flags;
 
-		if (this.packageTypesList.length === 0) {
-			this.log(`No updates in package.json for ${flags.types} release`);
-			return;
+		if (this.packageList.packagesUpdated.length === 0) {
+			this.log(`No updates in package.json for ${flags.types} release tag`);
 		}
 
-		this.log(
-			`package.json types field updated for the following packages to release type ${
-				flags.types
-			}: ${JSON.stringify(this.packageTypesList)}`,
-		);
+		return this.packageList;
 	}
 }
