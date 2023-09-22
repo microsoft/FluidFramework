e01f59623d5b247f45f7494c74bd2cdf2f137343
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2023-09-21 17:07:02 -0700

    Update domains

---
+++ experimental/dds/tree2/src/domains/leafDomain.ts
@@ -0,0 +1,43 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { SchemaBuilder } from "../feature-libraries";
+import { ValueSchema } from "../core";
+
+// https://en.wikipedia.org/wiki/Reverse_domain_name_notation
+const builder = new SchemaBuilder("Leaf");
+
+/**
+ * @alpha
+ */
+export const number = builder.leaf("fluidframework.com.leaf.1.number", ValueSchema.Number);
+/**
+ * @alpha
+ */
+export const boolean = builder.leaf("fluidframework.com.leaf.1.boolean", ValueSchema.Boolean);
+/**
+ * @alpha
+ */
+export const string = builder.leaf("fluidframework.com.leaf.1.string", ValueSchema.String);
+/**
+ * @alpha
+ */
+export const handle = builder.leaf("fluidframework.com.leaf.1.handle", ValueSchema.FluidHandle);
+
+/**
+ * @alpha
+ */
+export const primitives = [number, boolean, string] as const;
+
+/**
+ * Types allowed as roots of Json content.
+ * @alpha
+ */
+export const all = [primitives, ...primitives] as const;
+
+/**
+ * @alpha
+ */
+export const library = builder.intoLibrary();
