7c11c377fc23125b0f6f32be5d85353b59d2dde1
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2023-09-22 14:14:34 -0700

    Fix scoping

--- experimental/dds/tree2/src/domains/leafDomain.ts
+++ experimental/dds/tree2/src/domains/leafDomain.ts
@@ -7,26 +7,26 @@ import { SchemaBuilder } from "../feature-libraries";
 import { ValueSchema } from "../core";
 
 /**
- * Names in this domain follow https://en.wikipedia.org/wiki/Reverse_domain_name_notation, and are versioned.
+ * Names in this domain follow https://en.wikipedia.org/wiki/Reverse_domain_name_notation
  */
 const builder = new SchemaBuilder("Leaf");
 
 /**
  * @alpha
  */
-export const number = builder.leaf("fluidframework.com.leaf.1.number", ValueSchema.Number);
+export const number = builder.leaf("com.fluidframework.leaf.number", ValueSchema.Number);
 /**
  * @alpha
  */
-export const boolean = builder.leaf("fluidframework.com.leaf.1.boolean", ValueSchema.Boolean);
+export const boolean = builder.leaf("com.fluidframework.leaf.boolean", ValueSchema.Boolean);
 /**
  * @alpha
  */
-export const string = builder.leaf("fluidframework.com.leaf.1.string", ValueSchema.String);
+export const string = builder.leaf("com.fluidframework.leaf.string", ValueSchema.String);
 /**
  * @alpha
  */
-export const handle = builder.leaf("fluidframework.com.leaf.1.handle", ValueSchema.FluidHandle);
+export const handle = builder.leaf("com.fluidframework.leaf.handle", ValueSchema.FluidHandle);
 
 /**
  * @alpha
