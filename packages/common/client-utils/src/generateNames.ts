4d3a97dc722301622184b6e5624a1676ee24da0e
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2023-09-26 21:39:41 +0000

    remove server dep & unnecessary method

--- packages/common/client-utils/src/generateNames.ts
+++ packages/common/client-utils/src/generateNames.ts
@@ -22,7 +22,3 @@ export function getRandomName(connector = "_", capitalize = false): string {
 
 	return `${first}${connector}${last}`;
 }
-
-// Exposing a choose() function to align with moniker's API. Moniker is
-// server-only, and we can swap it out with this function for the browser.
-export const choose = (): string => getRandomName();
