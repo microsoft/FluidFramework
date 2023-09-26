db9e146bd764891d6069469ed853c7a8bfced180
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2023-09-26 17:21:09 +0000

    address nits

--- tools/api-markdown-documenter/src/api-item-transforms/helpers/Helpers.ts
+++ tools/api-markdown-documenter/src/api-item-transforms/helpers/Helpers.ts
@@ -457,13 +457,14 @@ export function createRemarksSection(
  *
  * @param apiItem - The API item whose `@throws` documentation will be rendered.
  * @param config - See {@link ApiItemTransformationConfiguration}.
+ * @param headingText - The text to use for the heading in the throws section. Defaults to "Throws".
  *
  * @returns The doc section if the API item had any `@throws` comments, otherwise `undefined`.
  */
 export function createThrowsSection(
 	apiItem: ApiItem,
 	config: Required<ApiItemTransformationConfiguration>,
-	title: string = "Throws",
+	headingText: string = "Throws",
 ): SectionNode | undefined {
 	const throwsBlocks = getThrowsBlocks(apiItem);
 	if (throwsBlocks === undefined || throwsBlocks.length === 0) {
@@ -477,7 +478,7 @@ export function createThrowsSection(
 	);
 
 	return wrapInSection(paragraphs, {
-		title,
+		title: headingText,
 		id: `${getQualifiedApiItemName(apiItem)}-throws`,
 	});
 }
