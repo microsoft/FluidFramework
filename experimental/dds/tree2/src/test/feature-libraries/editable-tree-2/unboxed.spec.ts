0bc20eacd7063ce0b6a20b2aa0f66892cd2fc395
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-09-26 18:18:28 +0000

    [WIP]

--- experimental/dds/tree2/src/test/feature-libraries/editable-tree-2/unboxed.spec.ts
+++ experimental/dds/tree2/src/test/feature-libraries/editable-tree-2/unboxed.spec.ts
@@ -3,6 +3,51 @@
  * Licensed under the MIT License.
  */
 
+/* eslint-disable import/no-internal-modules */
+
+import { strict as assert } from "node:assert";
+
+import {
+	IEditableForest,
+	ITreeCursorSynchronous,
+	ITreeSubscriptionCursor,
+	TreeNavigationResult,
+} from "../../../core";
+import { AllowedTypes, FieldSchema, SchemaAware, SchemaBuilder } from "../../../feature-libraries";
+import { Context } from "../../../feature-libraries/editable-tree-2/context";
+import { forestWithContent } from "../../utils";
+import { getReadonlyContext } from "./utils";
+
+function createSingleValueTree<Kind extends FieldKindTypes, Types extends AllowedTypes>(
+	builder: SchemaBuilder,
+	rootSchema: FieldSchema<Kind, Types>,
+	initialTree?:
+		| SchemaAware.TypedField<FieldSchema, SchemaAware.ApiMode.Flexible>
+		| readonly ITreeCursorSynchronous[]
+		| ITreeCursorSynchronous,
+): {
+	context: Context;
+	cursor: ITreeSubscriptionCursor;
+	forest: IEditableForest;
+} {
+	const schema = builder.intoDocumentSchema(rootSchema);
+	const forest = forestWithContent({ schema, initialTree });
+
+	const context = getReadonlyContext(forest, schema);
+	const cursor = context.forest.allocateCursor();
+
+	assert.equal(
+		context.forest.tryMoveCursorToField(rootFieldAnchor, cursor),
+		TreeNavigationResult.Ok,
+	);
+
+	return {
+		forest,
+		context,
+		cursor,
+	};
+}
+
 describe.only("unboxed unit tests", () => {
 	describe("unboxedField", () => {
 		it("Optional field with no value", () => {
