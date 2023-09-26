de3f6f8e0586ff3d039f4e8324040da83d20e077
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-09-26 17:55:41 +0000

    test: Scaffolding

---
+++ experimental/dds/tree2/src/test/feature-libraries/editable-tree-2/unboxed.spec.ts
@@ -0,0 +1,20 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+describe.only("unboxed unit tests", () => {
+	describe("unboxedField", () => {
+		it("Optional field with no value", () => {
+			console.log("TODO");
+		});
+
+		// TODO cases:
+		// * primitives
+		// * struct
+		// * union?
+	});
+
+	describe("unboxedTree", () => {});
+	describe("unboxedUnion", () => {});
+});
