f9429858cb4e5a8c59233f20f557ef1cf489e22f
Sonali Deshpande <48232592+sonalideshpandemsft@users.noreply.github.com>
2023-09-26 23:27:12 +0000

    feedback

--- build-tools/packages/build-cli/src/commands/release/setDtsTypes.ts
+++ build-tools/packages/build-cli/src/commands/release/setDtsTypes.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-import { Flags, Config } from "@oclif/core";
+import { Flags } from "@oclif/core";
 import { Package, updatePackageJsonFile } from "@fluidframework/build-tools";
 import path from "node:path";
 import { PackageCommand } from "../../BasePackageCommand";
@@ -11,16 +11,43 @@ import type { PackageJson } from "type-fest";
 import { ExtractorConfig } from "@microsoft/api-extractor";
 import { CommandLogger } from "../../logging";
 
+/**
+ * Represents a list of package categorized into two arrays
+ */
 interface PackageTypesList {
+	/**
+	 * An array of strings containing package names that have been updated.
+	 */
 	packagesUpdated: string[];
+	/**
+	 * An array of strings containing package names that have not been updated.
+	 */
 	packagesNotUpdated: string[];
 }
 
+/**
+ * Represents a configuration object for updating a package.
+ */
 interface UpdateConfig {
+	/**
+	 * The package to be updated.
+	 */
 	pkg: Package;
-	releaseType: string;
+	/**
+	 * The type of release for the package (e.g., "alpha", "beta", "public").
+	 */
+	releaseType: ReleaseTag;
+	/**
+	 * The JSON data for the package.
+	 */
 	json: PackageJson;
+	/**
+	 * The first part of the split string, representing the folder name.
+	 */
 	typesFolder: string;
+	/**
+	 * A boolean value indicating whether alpha/beta keyword is included.
+	 */
 	includesAlphaBeta: boolean;
 }
 
@@ -54,10 +81,11 @@ export default class SetReleaseTagPublishingCommand extends PackageCommand<
 		...PackageCommand.flags,
 	};
 
-	private readonly packageList: PackageTypesList;
+	private packageList!: PackageTypesList;
 
-	constructor(argv: string[], config: Config) {
-		super(argv, config);
+	// Override the init method to initialize packageList
+	public async init() {
+		await super.init();
 		this.packageList = {
 			packagesNotUpdated: [],
 			packagesUpdated: [],
@@ -105,24 +133,30 @@ export default class SetReleaseTagPublishingCommand extends PackageCommand<
 	}
 }
 
+/**
+ * Updates the types/typing field in package.json.
+ * @param config - The update configuration specifying the package, release type, JSON data, types folder, and inclusion of alpha/beta releases.
+ * @param log - The logger used for logging verbose information.
+ * @returns true if the update was successful, false otherwise.
+ */
 function updatePackageJsonTypes(config: UpdateConfig, log: CommandLogger): boolean {
 	const apiExtractorConfigFilePath = path.join(config.pkg.directory, "api-extractor.json");
 	let loadFile;
 	try {
 		loadFile = ExtractorConfig.loadFile(apiExtractorConfigFilePath);
 		const dtsRollupEnabled = loadFile.dtsRollup?.enabled;
-		if (dtsRollupEnabled !== undefined && dtsRollupEnabled) {
-			log.log(`Config exists: ${JSON.stringify(config.pkg.directory)}`);
+		if (dtsRollupEnabled === true) {
+			log.verbose(`Config exists: ${JSON.stringify(config.pkg.directory)}`);
 
 			const alpha = loadFile.dtsRollup?.alphaTrimmedFilePath;
 			const beta = loadFile.dtsRollup?.betaTrimmedFilePath;
 
-			if (config.releaseType === "alpha" && alpha !== undefined) {
+			if (config.releaseType === "alpha" && alpha !== "") {
 				config.json.types = `${config.typesFolder}/${config.pkg.nameUnscoped}-alpha.d.ts`;
 				return true;
 			}
 
-			if (config.releaseType === "beta" && beta !== undefined) {
+			if (config.releaseType === "beta" && beta !== "") {
 				config.json.types = `${config.typesFolder}/${config.pkg.nameUnscoped}-beta.d.ts`;
 				return true;
 			}
@@ -134,12 +168,20 @@ function updatePackageJsonTypes(config: UpdateConfig, log: CommandLogger): boole
 		}
 	} catch {
 		if (loadFile === undefined) {
-			log.log(`Config not exists: ${JSON.stringify(config.pkg.directory)}`);
+			log.verbose(`Config not exists: ${JSON.stringify(config.pkg.directory)}`);
 		}
 	}
 	return false;
 }
 
+/**
+ * Updates a package types list based on whether a package was updated or not.
+ *
+ * @param packageList - The package types list to be updated.
+ * @param packageUpdate - A boolean indicating whether the package was updated.
+ * @param pkg - The package being processed.
+ * @returns The updated package types list.
+ */
 function updatePackageLists(
 	packageList: PackageTypesList,
 	packageUpdate: boolean,
