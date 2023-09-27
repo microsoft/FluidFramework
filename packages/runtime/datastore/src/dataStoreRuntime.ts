a0cef1f20ef6c51d20cae8f3900de6161b116315
Mark Fields <markfields@users.noreply.github.com>
2023-09-26 17:18:39 -0700

    Switch to try-catch

--- packages/runtime/datastore/src/dataStoreRuntime.ts
+++ packages/runtime/datastore/src/dataStoreRuntime.ts
@@ -1183,14 +1183,16 @@ export const mixinSummaryHandler = (
 		async summarize(...args: any[]) {
 			const summary = await super.summarize(...args);
 
-			// Any error coming from app-provided handler should be marked as DataProcessingError
-			const content = await handler(this).catch((e: unknown) => {
+			try {
+				const content = await handler(this);
+				if (content !== undefined) {
+					this.addBlob(summary, content.path, content.content);
+				}
+			} catch (e) {
+				// Any error coming from app-provided handler should be marked as DataProcessingError
 				throw DataProcessingError.wrapIfUnrecognized(e, "mixinSummaryHandler");
-			});
-
-			if (content !== undefined) {
-				this.addBlob(summary, content.path, content.content);
 			}
+
 			return summary;
 		}
 	} as typeof FluidDataStoreRuntime;
