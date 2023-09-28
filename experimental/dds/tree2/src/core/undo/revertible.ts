d78b9aaf0c3f6714953a49c7a979c2066cde7a00
Jenn <jenn.thakugan@icloud.com>
2023-09-28 21:26:45 +0000

    add revertible interface

---
+++ experimental/dds/tree2/src/core/undo/revertible.ts
@@ -0,0 +1,37 @@
+export interface Revertible { 
+    readonly kind: RevertibleKind; 
+    readonly origin: { 
+
+        readonly isLocal: boolean; 
+
+        readonly view: ISharedTreeView; 
+
+    }; 
+    revert(): RevertResult; 
+    discard(): DiscardResult; 
+
+} 
+
+/**
+ * The type of revertible commit.
+ *
+ * @alpha
+ */
+export enum RevertibleKind {
+	/** A typical local commit */
+	Default,
+	/** A commit that is the result of an undo. */
+	Undo,
+	/** A commit that is the result of a redo. */
+	Redo,
+}
+
+/**
+ * The result of a revert operation.
+ *
+ * @alpha
+ */
+export enum RevertResult {
+	Success,
+	Failure,
+}
