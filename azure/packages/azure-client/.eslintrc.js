443d7009cab7128a7449cabd2e8f2aef0333018b
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-09-29 21:03:12 +0000

    refactor: Enable no-abbreviations eslint rule and fix violations

--- azure/packages/azure-client/.eslintrc.js
+++ azure/packages/azure-client/.eslintrc.js
@@ -5,8 +5,32 @@
 
 module.exports = {
 	extends: [require.resolve("@fluidframework/eslint-config-fluid/strict"), "prettier"],
-	plugins: ["eslint-plugin-jsdoc"],
 	parserOptions: {
 		project: ["./tsconfig.json", "./src/test/tsconfig.json"],
 	},
+	rules: {
+		// Useful for developer accessibility
+		"unicorn/prevent-abbreviations": [
+			"error",
+			{
+				allowList: {
+					// Industry-standard index variable name.
+					i: true,
+
+					// Existing export - renaming would be a breaking change.
+					// Leaving this alone for now, but this can be reconsidered in the future.
+					AzureClientProps: true,
+				},
+			},
+		],
+	},
+	overrides: [
+		{
+			// Overrides for type-tests
+			files: ["src/test/types/*"],
+			rules: {
+				"unicorn/prevent-abbreviations": "off",
+			},
+		},
+	],
 };
