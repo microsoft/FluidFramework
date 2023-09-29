61e4d8b1d1746306e770037b136c63ab338dd1e2
Connor Skees <connor1skees@gmail.com>
2023-09-29 04:34:49 +0000

    delete webflow
    i'm ~intending to revert this change, but for now the webflow build
    seems quite broken and i want to see the test run results in CI

--- examples/data-objects/webflow/src/document/debug.ts
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
-import { IDebugger } from "debug";
-import { debug as parent } from "../debug.js";
-
-export const debug: IDebugger = parent.extend("document");
--- examples/data-objects/webflow/src/util/random.ts
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
-/**
- * Returns a pseudo-random string suitable for avoiding 'id' collisions between DOM elements.
- */
-export const randomId = () => Math.random().toString(36).slice(2);
