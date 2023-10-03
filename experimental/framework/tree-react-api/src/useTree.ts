aae93dacf14973cc03ebc95e3b38e72c093493bb
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2023-10-03 12:18:10 -0700

    Update inventory sample to new API

--- experimental/framework/tree-react-api/src/useTree.ts
+++ experimental/framework/tree-react-api/src/useTree.ts
@@ -4,6 +4,8 @@
  */
 
 import {
+	TreeContext,
+	TreeNode,
 	FieldSchema,
 	ISharedTreeView,
 	InitializeAndSchematizeConfiguration,
@@ -19,6 +21,8 @@ import React from "react";
  * Use deep clone if saving a copy for comparison.
  *
  * Not currently compatible with 'React.memo'.
+ *
+ * @deprecated Prefer using the new TypedTree entry point and tree types, and the hooks for them, {@link useTreeContext} and {@link useSubtree}.
  */
 export function useTree<TRoot extends FieldSchema>(
 	tree: ISharedTreeView,
@@ -41,3 +45,40 @@ export function useTree<TRoot extends FieldSchema>(
 
 	return typedTree.context.root as unknown as SchemaAware.TypedField<TRoot>;
 }
+
+/**
+ * React Hook to trigger invalidation of the current component if anything in the document changes.
+ */
+export function useTreeContext(document: TreeContext): void {
+	// This proof-of-concept implementation allocates a state variable this is modified
+	// when the tree changes to trigger re-render.
+	const [invalidations, setInvalidations] = React.useState(0);
+
+	// Register for tree deltas when the component mounts
+	React.useEffect(() => {
+		// Returns the cleanup function to be invoked when the component unmounts.
+		return document.on("afterChange", () => {
+			setInvalidations(invalidations + 1);
+		});
+	}, [document]);
+}
+
+/**
+ * React Hook to trigger invalidation of the current component if anything in the provided subtree changes.
+ * This does NOT include if this subtree is moved into a different parent!
+ */
+export function useSubtree(tree: TreeNode): void {
+	// This proof-of-concept implementation allocates a state variable this is modified
+	// when the tree changes to trigger re-render.
+	const [invalidations, setInvalidations] = React.useState(0);
+
+	// Register for tree deltas when the component mounts
+	React.useEffect(() => {
+		// Returns the cleanup function to be invoked when the component unmounts.
+		return tree.on("subtreeChanging", () => {
+			setInvalidations(invalidations + 1);
+		});
+	}, [tree, setInvalidations, invalidations]);
+}
+
+// TODO: schematize component which shows error (with proper invalidation), or passes tree to sub componet.
