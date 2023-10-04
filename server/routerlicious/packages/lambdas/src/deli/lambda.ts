84a308714876e3fec2d795c3d5ede3240bfa8e99
kkachmar <kkachmar@microsoft.com>
2023-10-04 18:41:42 -0400

    add ActivityTimeout before noClient log

--- server/routerlicious/packages/lambdas/src/deli/lambda.ts
+++ server/routerlicious/packages/lambdas/src/deli/lambda.ts
@@ -714,6 +714,12 @@ export class DeliLambda extends TypedEventEmitter<IDeliLambdaEvents> implements 
 
 		if (this.serviceConfiguration.enableLumberjack) {
 			this.logSessionEndMetrics(closeType);
+			if (!this.globalCheckpointOnly && closeType === LambdaCloseType.ActivityTimeout) {
+				Lumberjack.info(
+					`Closing due to ActivityTimeout before NoClient op`,
+					getLumberBaseProperties(this.documentId, this.tenantId),
+				);
+			}
 		}
 	}
 
