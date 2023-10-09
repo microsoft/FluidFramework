1047d4a6a76066b058cb54f6ae6bfae0ed4e53d1
tyler-cai-microsoft <tylercai@microsoft.com>
2023-10-09 11:01:58 -0700

    Create experimental hot-swap dds package

---
+++ packages/dds/hot-swap/src/index.ts
@@ -0,0 +1,13 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * This library contains the {@link HotSwap} distributed data structure.
+ * A `HotSwap` is a shared object which allows a client to migrate from one data structure to another on the fly.
+ *
+ * @packageDocumentation
+ */
+
+export { HotSwap } from "./hotSwap";
