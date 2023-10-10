bd0f18a324ac23b7b96cf3fb6c1059be8948ba99
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-10-10 19:19:09 +0000

    docs: Clarify comments

--- experimental/dds/tree2/src/feature-libraries/editable-tree-2/editableTreeTypes.ts
+++ experimental/dds/tree2/src/feature-libraries/editable-tree-2/editableTreeTypes.ts
@@ -454,14 +454,14 @@ export type StructFields<TFields extends RestrictiveReadonlyRecord<string, Field
 	// Unboxed fields
 	readonly [key in keyof TFields]: UnboxField<TFields[key]>;
 } & {
-	// Add setter (make property writable) when schema type is a value.
+	// Add setter (make property writable) when the field is of a kind that has a logical set operation.
 	// If we could map to getters and setters separately, we would preferably do that, but we can't.
 	// See https://github.com/microsoft/TypeScript/issues/43826 for more details on this limitation.
 	[key in keyof TFields as TFields[key]["kind"] extends AssignableFieldKinds
 		? key
 		: never]: UnboxField<TFields[key]>;
 } & {
-	// Setter method (When schema type is a value)
+	// Setter method (when the field is of a kind that has a logical set operation).
 	readonly [key in keyof TFields as TFields[key]["kind"] extends AssignableFieldKinds
 		? `set${Capitalize<key & string>}`
 		: never]: (content: FlexibleFieldContent<TFields[key]>) => void;
