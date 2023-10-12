e9648cf3578b71e930d502d95ab095db343fee52
Noah Encke <noencke@microsoft.com>
2023-10-11 22:07:12 -0700

    Add proxy types

--- experimental/dds/tree2/src/feature-libraries/index.ts
+++ experimental/dds/tree2/src/feature-libraries/index.ts
@@ -253,6 +253,15 @@ export {
 	CheckTypesOverlap,
 	TreeStatus,
 	getProxyForField,
+	ObjectFields,
+	ProxyField,
+	ProxyFieldInner,
+	ProxyNode,
+	ProxyNodeUnion,
+	SharedTreeList,
+	SharedTreeMap,
+	SharedTreeObject,
+	is,
 } from "./editable-tree-2";
 
 // Split into separate import and export for compatibility with API-Extractor.
