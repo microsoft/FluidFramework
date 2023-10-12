24d150cbf5e0c06cbd3050a243571f44472b1917
Clarence Li <clarenceli@microsoft.com>
2023-10-12 10:28:33 -0400

    Simplify the summary function

--- packages/dds/map/src/map.ts
+++ packages/dds/map/src/map.ts
@@ -285,21 +285,13 @@ export class SharedMap extends SharedObject<ISharedMapEvents> implements IShared
 				const blobName = `blob${counter}`;
 				counter++;
 				blobs.push(blobName);
-				const content: IMapDataObjectSerializable =
-					value.index !== undefined
-						? {
-								[key]: {
-									type: value.type,
-									value: JSON.parse(value.value) as unknown,
-									index: value.index,
-								},
-						  }
-						: {
-								[key]: {
-									type: value.type,
-									value: JSON.parse(value.value) as unknown,
-								},
-						  };
+				const content: IMapDataObjectSerializable = {
+					[key]: {
+						type: value.type,
+						value: JSON.parse(value.value) as unknown,
+						index: value.index,
+					},
+				};
 
 				builder.addBlob(blobName, JSON.stringify(content));
 			} else {
@@ -316,23 +308,14 @@ export class SharedMap extends SharedObject<ISharedMapEvents> implements IShared
 					headerBlob = {};
 					currentSize = 0;
 				}
-				headerBlob[key] =
-					value.index !== undefined
-						? {
-								type: value.type,
-								value:
-									value.value === undefined
-										? undefined
-										: (JSON.parse(value.value) as unknown),
-								index: value.index,
-						  }
-						: {
-								type: value.type,
-								value:
-									value.value === undefined
-										? undefined
-										: (JSON.parse(value.value) as unknown),
-						  };
+				headerBlob[key] = {
+					type: value.type,
+					value:
+						value.value === undefined
+							? undefined
+							: (JSON.parse(value.value) as unknown),
+					index: value.index,
+				};
 			}
 		}
 
