fb6c3fd82a5e26458f8e0a8f8ede39981f7891c4
Abram Sanderson <abram.sanderson@gmail.com>
2023-10-12 11:05:18 -0700

    First cut of more exhaustive style optional field tests. Needs mechanical cleanup

--- experimental/dds/tree2/src/test/feature-libraries/default-field-kinds/optionalChangeRebaser.spec.ts
+++ experimental/dds/tree2/src/test/feature-libraries/default-field-kinds/optionalChangeRebaser.spec.ts
@@ -4,12 +4,17 @@
  */
 
 import { strict as assert } from "assert";
-import { CrossFieldManager, NodeChangeset, singleTextCursor } from "../../../feature-libraries";
+import {
+	CrossFieldManager,
+	NodeChangeset,
+	OptionalField,
+	RevisionMetadataSource,
+	singleTextCursor,
+} from "../../../feature-libraries";
 import {
 	ChangesetLocalId,
 	Delta,
 	makeAnonChange,
-	mintRevisionTag,
 	RevisionTag,
 	tagChange,
 	TaggedChange,
@@ -32,6 +37,12 @@ import {
 // eslint-disable-next-line import/no-internal-modules
 import { OptionalChangeset } from "../../../feature-libraries/default-field-kinds/defaultFieldChangeTypes";
 
+// Rather than use UUIDs, allocate these sequentially for an easier time debugging tests.
+let currentRevision = 0;
+function mintRevisionTag(): RevisionTag {
+	return `rev${currentRevision++}` as RevisionTag;
+}
+
 const type: TreeSchemaIdentifier = brand("Node");
 const tag1: RevisionTag = mintRevisionTag();
 const tag2: RevisionTag = mintRevisionTag();
@@ -39,6 +50,16 @@ const tag3: RevisionTag = mintRevisionTag();
 const tag4: RevisionTag = mintRevisionTag();
 const tag5: RevisionTag = mintRevisionTag();
 const tag6: RevisionTag = mintRevisionTag();
+const tag7: RevisionTag = mintRevisionTag();
+const tags = [tag1, tag2, tag3, tag4, tag5, tag6, tag7];
+
+function getTag(index: number): RevisionTag {
+	const tag = tags[index];
+	if (!tag) {
+		throw new Error("Not enough tags in test setup: add more tags to the tags array");
+	}
+	return tag;
+}
 
 const OptionalChange = {
 	set(value: string | undefined, wasEmpty: boolean, id: ChangesetLocalId = brand(0)) {
@@ -123,6 +144,32 @@ function rebaseTagged(
 	}
 
 	return currChange;
+}
+
+function rebaseComposed(
+	metadata: RevisionMetadataSource,
+	change: TaggedChange<OptionalChangeset>,
+	...baseChanges: TaggedChange<OptionalChangeset>[]
+): TaggedChange<OptionalChangeset> {
+	baseChanges.forEach((base) => deepFreeze(base));
+	deepFreeze(change);
+
+	const composed = compose(baseChanges);
+	const moveEffects = failCrossFieldManager;
+	const idAllocator = idAllocatorFromMaxId(getMaxId(composed));
+	return tagChange(
+		optionalChangeRebaser.rebase(
+			change.change,
+			// TODO: Is anon change good enough here?
+			makeAnonChange(composed),
+			TestChange.rebase as any,
+			idAllocator,
+			moveEffects,
+			metadata,
+			undefined,
+		),
+		change.revision,
+	);
 }
 
 function compose(changes: TaggedChange<OptionalChangeset>[]): OptionalChangeset {
@@ -255,6 +302,287 @@ describe("OptionalField - Rebaser Axioms", () => {
 				const actual = compose(changes);
 				const delta = toDelta(actual);
 				assert.equal(isDeltaVisible(delta), false);
+			});
+		}
+	});
+
+	describe("A ↷ [B, B⁻¹, C] === A ↷ compose([B, B⁻¹, C])", () => {
+		for (const [nameA, changeA] of testChanges) {
+			for (const [nameB, changeB] of testChanges) {
+				for (const [nameC, changeC] of testChanges) {
+					// if (!(nameA === "ChangeChild" && nameB === "SetA" && nameC === "ChangeChild")) {
+					// 	continue;
+					// }
+
+					it(`${nameA} ↷ [${nameB}, ${nameB}⁻¹, ${nameC}] === ${nameA} ↷ compose([${nameB}, ${nameB}⁻¹, ${nameC}])`, () => {
+						const taggedChangeA = tagChange(changeA, tag1);
+						const taggedChangeB = tagChange(changeB, tag2);
+						const taggedChangeC = tagChange(changeC, tag3);
+						const invB = tagRollbackInverse(
+							invert(taggedChangeB),
+							tag4,
+							taggedChangeB.revision,
+						);
+						const changes = [taggedChangeB, invB, taggedChangeC];
+						const r1 = rebaseTagged(taggedChangeA, ...changes);
+						const metadata = defaultRevisionMetadataFromChanges([
+							...changes,
+							taggedChangeA,
+						]);
+						const rebaseOverCompose = rebaseComposed(
+							metadata,
+							taggedChangeA,
+							...changes,
+						);
+						assert.deepEqual(r1.change, rebaseOverCompose.change);
+					});
+				}
+			}
+		}
+	});
+
+	interface OptionalFieldTestState {
+		contents: string | undefined;
+		allocateIntention: () => number;
+		mostRecentEdit: OptionalChangeset | undefined;
+		mostRecentIntention: number | undefined;
+		parent: OptionalFieldTestState | undefined;
+		mostRecentEditDescription: string | undefined;
+	}
+
+	function getInputContext(state: OptionalFieldTestState): number[] {
+		// todo: recursive might be clearer
+		// if (state.mostRecentIntention === undefined) {
+		// 	return [];
+		// }
+		const inputContext: number[] = [];
+		for (
+			let current: OptionalFieldTestState | undefined = state;
+			current?.mostRecentIntention !== undefined;
+			current = current.parent
+		) {
+			inputContext.push(current.mostRecentIntention);
+		}
+		inputContext.reverse();
+		return inputContext;
+	}
+
+	function* generateChildStates(state: OptionalFieldTestState): Iterable<OptionalFieldTestState> {
+		// Valid operations:
+		// - set to a new value
+		// - set to undefined
+		// - change child
+
+		// TODO: inverse and undo of previous change
+
+		const inputContext = getInputContext(state);
+		if (state.contents !== undefined) {
+			// Make a change to the child
+			const changeChildIntention = state.allocateIntention();
+			yield {
+				contents: state.contents,
+				allocateIntention: state.allocateIntention,
+				mostRecentEdit: OptionalChange.buildChildChange(
+					TestChange.mint(inputContext, changeChildIntention),
+				),
+				mostRecentIntention: changeChildIntention,
+				parent: state,
+				mostRecentEditDescription: `ChildChange ${changeChildIntention}`,
+			};
+
+			const setUndefinedIntention = state.allocateIntention();
+			yield {
+				contents: undefined,
+				allocateIntention: state.allocateIntention,
+				mostRecentEdit: OptionalChange.set(undefined, false),
+				mostRecentIntention: setUndefinedIntention,
+				parent: state,
+				mostRecentEditDescription: "Set undefined",
+			};
+		}
+
+		const setIntention = state.allocateIntention();
+		const newContents = `set at depth ${inputContext.length}`;
+		yield {
+			contents: newContents,
+			allocateIntention: state.allocateIntention,
+			mostRecentEdit: OptionalChange.set(newContents, state.contents === undefined),
+			mostRecentIntention: setIntention,
+			parent: state,
+			mostRecentEditDescription: `Set ${inputContext.length}`,
+		};
+	}
+
+	function createInitialState(contents: string | undefined): OptionalFieldTestState {
+		let nextIntention = 0;
+		return {
+			contents,
+			allocateIntention: () => nextIntention++,
+			mostRecentEdit: undefined,
+			mostRecentIntention: undefined,
+			parent: undefined,
+			mostRecentEditDescription: undefined,
+		};
+	}
+
+	function* walkOptionalFieldTestStateTree(
+		initialState: OptionalFieldTestState,
+		depth: number,
+	): Iterable<OptionalFieldTestState> {
+		yield initialState;
+		if (depth > 0) {
+			for (const childState of generateChildStates(initialState)) {
+				yield* walkOptionalFieldTestStateTree(childState, depth - 1);
+			}
+		}
+	}
+
+	function* generatePossibleSequenceOfEdits(
+		initialState: OptionalFieldTestState,
+		numberOfEdits: number,
+	): Iterable<{ name: string; edit: OptionalChangeset }[]> {
+		for (const state of walkOptionalFieldTestStateTree(initialState, numberOfEdits)) {
+			const edits: { name: string; edit: OptionalChangeset }[] = [];
+			for (
+				let current: OptionalFieldTestState | undefined = state;
+				current?.mostRecentEdit !== undefined;
+				current = current.parent
+			) {
+				edits.push({
+					edit: current.mostRecentEdit,
+					name: current.mostRecentEditDescription!,
+				});
+			}
+
+			if (edits.length === numberOfEdits) {
+				edits.reverse();
+				yield edits;
+			}
+		}
+	}
+
+	// To limit combinatorial explosion, we test 'rebasing over a compose is equivalent to rebasing over the individual edits'
+	// by:
+	// - Rebasing a single edit over N sequential edits
+	// - Rebasing N sequential edits over a single edit, sandwich-rebasing style
+	//   (meaning [A, B, C] ↷ D involves B ↷ compose([A⁻¹, D, A]) and C ↷ compose([B⁻¹, A⁻¹, D, A, B]))
+	describe("Rebase over compose exhaustive", () => {
+		for (const fieldContent of [undefined, "A"]) {
+			describe(`starting with contents ${fieldContent}`, () => {
+				const initialState = createInitialState(fieldContent);
+				for (const [{ name, edit }] of generatePossibleSequenceOfEdits(initialState, 1)) {
+					for (const editsToRebaseOver of generatePossibleSequenceOfEdits(
+						initialState,
+						2,
+					)) {
+						const title = `Rebase ${name} over compose ${JSON.stringify(
+							editsToRebaseOver.map(({ name }) => name),
+						)}`;
+
+						// if (
+						// 	title !== 'Rebase ChildChange 0 over compose ["Set undefined","Set 1"]'
+						// ) {
+						// 	continue;
+						// }
+						it(title, () => {
+							const taggedEditToRebase = tagChange(edit, tag1);
+							const taggedTrunkEdits = editsToRebaseOver.map(({ edit }, i) =>
+								tagChange(edit, getTag(i + 1)),
+							);
+
+							const rebaseWithoutCompose = rebaseTagged(
+								taggedEditToRebase,
+								...taggedTrunkEdits,
+							);
+							const metadata = defaultRevisionMetadataFromChanges([
+								...taggedTrunkEdits,
+								taggedEditToRebase,
+							]);
+							const rebaseWithCompose = rebaseComposed(
+								metadata,
+								taggedEditToRebase,
+								...taggedTrunkEdits,
+							);
+							assert.deepEqual(rebaseWithCompose.change, rebaseWithoutCompose.change);
+						});
+					}
+				}
+			});
+		}
+	});
+
+	describe.only("Sandwich rebase over compose exhaustive", () => {
+		for (const fieldContent of [undefined, "A"]) {
+			describe(`starting with contents ${fieldContent}`, () => {
+				const initialState = createInitialState(fieldContent);
+				for (const sourceEdits of generatePossibleSequenceOfEdits(initialState, 2)) {
+					for (const [
+						{ name, edit: editToRebaseOver },
+					] of generatePossibleSequenceOfEdits(initialState, 1)) {
+						const title = `Rebase ${JSON.stringify(
+							sourceEdits.map(({ name }) => name),
+						)} over ${name}`;
+
+						if (title !== 'Rebase ["Set 0","ChildChange 1"] over Set 0') {
+							continue;
+						}
+						it(title, () => {
+							const taggedEditToRebaseOver = tagChange(editToRebaseOver, tag1);
+							const taggedSourceEdits = sourceEdits.map(({ edit }, i) =>
+								tagChange(edit, getTag(i + 1)),
+							);
+
+							const inverses = taggedSourceEdits.map((change, i) =>
+								tagRollbackInverse(
+									invert(change),
+									getTag(i + sourceEdits.length + 1),
+									change.revision,
+								),
+							);
+
+							const rebasedEditsWithoutCompose: TaggedChange<OptionalChangeset>[] =
+								[];
+							const rebasedEditsWithCompose: TaggedChange<OptionalChangeset>[] = [];
+
+							for (let i = 0; i < taggedSourceEdits.length; i++) {
+								const edit = taggedSourceEdits[i];
+								const editsToRebaseOver = [
+									...inverses.slice(0, i),
+									taggedEditToRebaseOver,
+									...rebasedEditsWithoutCompose,
+								];
+								rebasedEditsWithoutCompose.push(
+									rebaseTagged(edit, ...editsToRebaseOver),
+								);
+							}
+
+							let currentComposedEdit = taggedEditToRebaseOver;
+							// This needs to be used to pass an updated RevisionMetadataSource to rebase.
+							const allTaggedEdits = [...inverses, taggedEditToRebaseOver];
+							for (let i = 0; i < taggedSourceEdits.length; i++) {
+								const metadata = defaultRevisionMetadataFromChanges(allTaggedEdits);
+								const edit = taggedSourceEdits[i];
+								const rebasedEdit = rebaseComposed(
+									metadata,
+									edit,
+									currentComposedEdit,
+								);
+								rebasedEditsWithCompose.push(rebasedEdit);
+								currentComposedEdit = makeAnonChange(
+									compose([inverses[i], currentComposedEdit, rebasedEdit]),
+								);
+								allTaggedEdits.push(rebasedEdit);
+							}
+
+							for (let i = 0; i < rebasedEditsWithoutCompose.length; i++) {
+								assert.deepEqual(
+									rebasedEditsWithoutCompose[i].change,
+									rebasedEditsWithCompose[i].change,
+								);
+							}
+						});
+					}
+				}
 			});
 		}
 	});
