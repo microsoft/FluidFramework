db68f3e0a073675a613cfb5e77f670a153fa29ca
Abram Sanderson <abram.sanderson@gmail.com>
2023-10-13 10:20:16 -0700

    Refactor

--- experimental/dds/tree2/src/test/feature-libraries/default-field-kinds/optionalChangeRebaser.spec.ts
+++ experimental/dds/tree2/src/test/feature-libraries/default-field-kinds/optionalChangeRebaser.spec.ts
@@ -35,20 +35,13 @@ import {
 } from "../../../feature-libraries/default-field-kinds/optionalField";
 // eslint-disable-next-line import/no-internal-modules
 import { OptionalChangeset } from "../../../feature-libraries/default-field-kinds/defaultFieldChangeTypes";
+import {
+	FieldStateTree,
+	getInputContext,
+	generatePossibleSequenceOfEdits,
+} from "./exhaustiveRebaserUtils";
 
 type RevisionTagMinter = () => RevisionTag;
-
-interface OptionalFieldTestState {
-	contents: string | undefined;
-	mostRecentEdit?: NamedChangeset;
-	parent?: OptionalFieldTestState;
-}
-
-interface NamedChangeset {
-	changeset: TaggedChange<OptionalChangeset>;
-	intention: number;
-	description: string;
-}
 
 function makeRevisionTagMinter(prefix = "rev"): RevisionTagMinter {
 	// Rather than use UUIDs, allocate these sequentially for an easier time debugging tests.
@@ -56,15 +49,9 @@ function makeRevisionTagMinter(prefix = "rev"): RevisionTagMinter {
 	return () => `${prefix}${currentRevision++}` as RevisionTag;
 }
 
-function makeIntentionMinter(): () => number {
-	let intent = 0;
-	return () => intent++;
-}
-
 const type: TreeSchemaIdentifier = brand("Node");
 const mintRevisionTag = makeRevisionTagMinter();
-const tags: RevisionTag[] = Array.from({ length: 7 }, mintRevisionTag);
-const [tag1, tag2, tag3, tag4, tag5, tag6] = tags;
+const tag1 = mintRevisionTag();
 
 const OptionalChange = {
 	set(value: string | undefined, wasEmpty: boolean, id: ChangesetLocalId = brand(0)) {
@@ -189,41 +176,18 @@ function compose(changes: TaggedChange<OptionalChangeset>[]): OptionalChangeset 
 	);
 }
 
-function getInputContext(state: OptionalFieldTestState): number[] {
-	const inputContext: number[] = [];
-	for (
-		let current: OptionalFieldTestState | undefined = state;
-		current?.mostRecentEdit !== undefined;
-		current = current.parent
-	) {
-		inputContext.push(current.mostRecentEdit.intention);
-	}
-	inputContext.reverse();
-	return inputContext;
-}
-
-interface FieldStateUpTree<TContent, TChangeset> {
-	content: TContent;
-	mostRecentEdit?: NamedChangeset;
-	parent?: FieldStateUpTree<TContent, TChangeset>;
-}
+type OptionalFieldTestState = FieldStateTree<string | undefined, OptionalChangeset>;
 
 function* generateChildStates(
 	state: OptionalFieldTestState,
 	tagFromIntention: (intention: number) => RevisionTag,
 	mintIntention: () => number,
 ): Iterable<OptionalFieldTestState> {
-	// Valid operations:
-	// - set to a new value
-	// - set to undefined
-	// - change child
-
 	const inputContext = getInputContext(state);
-	if (state.contents !== undefined) {
-		// Make a change to the child
+	if (state.content !== undefined) {
 		const changeChildIntention = mintIntention();
 		yield {
-			contents: state.contents,
+			content: state.content,
 			mostRecentEdit: {
 				changeset: tagChange(
 					OptionalChange.buildChildChange(
@@ -239,7 +203,7 @@ function* generateChildStates(
 
 		const setUndefinedIntention = mintIntention();
 		yield {
-			contents: undefined,
+			content: undefined,
 			mostRecentEdit: {
 				changeset: tagChange(
 					OptionalChange.set(undefined, false),
@@ -258,10 +222,10 @@ function* generateChildStates(
 		// values, which should tend to be easier to debug
 		const newContents = `${value},${inputContext.length}`;
 		yield {
-			contents: newContents,
+			content: newContents,
 			mostRecentEdit: {
 				changeset: tagChange(
-					OptionalChange.set(newContents, state.contents === undefined),
+					OptionalChange.set(newContents, state.content === undefined),
 					tagFromIntention(setIntention),
 				),
 				intention: setIntention,
@@ -274,7 +238,7 @@ function* generateChildStates(
 	if (state.mostRecentEdit !== undefined) {
 		const undoIntention = mintIntention();
 		yield {
-			contents: state.parent?.contents,
+			content: state.parent?.content,
 			mostRecentEdit: {
 				changeset: tagChange(
 					invert(state.mostRecentEdit.changeset),
@@ -291,74 +255,21 @@ function* generateChildStates(
 	}
 }
 
-function* walkOptionalFieldTestStateTree(
-	initialState: OptionalFieldTestState,
-	depth: number,
-	tagFromIntention: (intention: number) => RevisionTag,
-	mintIntention: () => number,
-): Iterable<OptionalFieldTestState> {
-	yield initialState;
-	if (depth > 0) {
-		for (const childState of generateChildStates(
-			initialState,
-			tagFromIntention,
-			mintIntention,
-		)) {
-			yield* walkOptionalFieldTestStateTree(
-				childState,
-				depth - 1,
-				tagFromIntention,
-				mintIntention,
-			);
-		}
-	}
-}
-
-function* generatePossibleSequenceOfEdits(
-	initialState: OptionalFieldTestState,
-	numberOfEdits: number,
-	tagPrefix: string,
-): Iterable<NamedChangeset[]> {
-	for (const state of walkOptionalFieldTestStateTree(
-		initialState,
-		numberOfEdits,
-		(intention: number) => `${tagPrefix}${intention}` as RevisionTag,
-		makeIntentionMinter(),
-	)) {
-		const edits: NamedChangeset[] = [];
-		for (
-			let current: OptionalFieldTestState | undefined = state;
-			current?.mostRecentEdit !== undefined;
-			current = current.parent
-		) {
-			edits.push(current.mostRecentEdit);
-		}
-
-		if (edits.length === numberOfEdits) {
-			edits.reverse();
-			yield edits;
-		}
-	}
-}
-
+/**
+ * Runs a suite of axiomatic tests which use combinations of single edits that are valid to apply from an initial state.
+ */
 function runSingleEditRebaseAxiomSuite(initialState: OptionalFieldTestState) {
-	const testChanges: [string, OptionalChangeset][] = Array.from(
-		generatePossibleSequenceOfEdits(initialState, 1, "rev"),
-		// TODO: Use generated tags rather than mint new ones.
-		([{ description, changeset }]) => [description, changeset.change],
-	);
-	deepFreeze(testChanges);
+	const singleTestChanges = (prefix: string) =>
+		generatePossibleSequenceOfEdits(initialState, generateChildStates, 1, prefix);
 
 	/**
 	 * This test simulates rebasing over an do-inverse pair.
 	 */
 	describe("A ↷ [B, B⁻¹] === A", () => {
-		for (const [name1, untaggedChange1] of testChanges) {
-			for (const [name2, untaggedChange2] of testChanges) {
+		for (const [{ description: name1, changeset: change1 }] of singleTestChanges("A")) {
+			for (const [{ description: name2, changeset: change2 }] of singleTestChanges("B")) {
 				it(`(${name1} ↷ ${name2}) ↷ ${name2}⁻¹ => ${name1}`, () => {
-					const change1 = tagChange(untaggedChange1, tag5);
-					const change2 = tagChange(untaggedChange2, tag3);
-					const inv = tagRollbackInverse(invert(change2), tag4, tag3);
+					const inv = tagRollbackInverse(invert(change2), tag1, change2.revision);
 					const r1 = rebaseTagged(change1, change2);
 					const r2 = rebaseTagged(r1, inv);
 					assert.deepEqual(r2.change, change1.change);
@@ -374,13 +285,11 @@ function runSingleEditRebaseAxiomSuite(initialState: OptionalFieldTestState) {
 	 * - The inverse produced by undo(B) is not a rollback
 	 */
 	describe("A ↷ [B, undo(B)] => A", () => {
-		for (const [name1, untaggedChange1] of testChanges) {
-			for (const [name2, untaggedChange2] of testChanges) {
+		for (const [{ description: name1, changeset: change1 }] of singleTestChanges("A")) {
+			for (const [{ description: name2, changeset: change2 }] of singleTestChanges("B")) {
 				const title = `${name1} ↷ [${name2}, undo(${name2})] => ${name1}`;
 				it(title, () => {
-					const change1 = tagChange(untaggedChange1, tag5);
-					const change2 = tagChange(untaggedChange2, tag3);
-					const inv = tagChange(invert(change2), tag4);
+					const inv = tagChange(invert(change2), tag1);
 					const r1 = rebaseTagged(change1, change2);
 					const r2 = rebaseTagged(r1, inv);
 					assert.deepEqual(r2.change, change1.change);
@@ -398,13 +307,11 @@ function runSingleEditRebaseAxiomSuite(initialState: OptionalFieldTestState) {
 	 * apply the inverse of some change.
 	 */
 	describe("(A ↷ B) ↷ [B⁻¹, B] === A ↷ B", () => {
-		for (const [name1, untaggedChange1] of testChanges) {
-			for (const [name2, untaggedChange2] of testChanges) {
+		for (const [{ description: name1, changeset: change1 }] of singleTestChanges("A")) {
+			for (const [{ description: name2, changeset: change2 }] of singleTestChanges("B")) {
 				const title = `${name1} ↷ [${name2}, ${name2}⁻¹, ${name2}] => ${name1} ↷ ${name2}`;
 				it(title, () => {
-					const change1 = tagChange(untaggedChange1, tag6);
-					const change2 = tagChange(untaggedChange2, tag3);
-					const inverse2 = tagRollbackInverse(invert(change2), tag4, change2.revision);
+					const inverse2 = tagRollbackInverse(invert(change2), tag1, change2.revision);
 					const r1 = rebaseTagged(change1, change2);
 					const r2 = rebaseTagged(r1, inverse2);
 					const r3 = rebaseTagged(r2, change2);
@@ -415,8 +322,11 @@ function runSingleEditRebaseAxiomSuite(initialState: OptionalFieldTestState) {
 	});
 
 	describe("A ○ A⁻¹ === ε", () => {
-		for (const [name, change] of testChanges) {
-			if (["SetA", "SetB", "SetUndefined"].includes(name)) {
+		for (const [{ description: name, changeset: change }] of singleTestChanges("A")) {
+			if (
+				initialState.content !== undefined &&
+				["SetA,0", "SetB,0", "Delete"].includes(name)
+			) {
 				// TODO:AB#4622: OptionalChangeset should obey group axioms, but the current compose implementation does not
 				// cancel changes from inverses, and in some cases the representation isn't sufficient for doing so.
 				// Set operations fail to satisfy this test because they generate explicit deltas which set the trait to be
@@ -425,13 +335,8 @@ function runSingleEditRebaseAxiomSuite(initialState: OptionalFieldTestState) {
 			}
 
 			it(`${name} ○ ${name}⁻¹ === ε`, () => {
-				const taggedChange = tagChange(change, tag1);
-				const inv = invert(taggedChange);
-				const changes = [
-					taggedChange,
-					tagRollbackInverse(inv, tag2, taggedChange.revision),
-				];
-				const actual = compose(changes);
+				const inv = invert(change);
+				const actual = compose([change, tagRollbackInverse(inv, tag1, change.revision)]);
 				const delta = toDelta(actual);
 				assert.equal(isDeltaVisible(delta), false);
 			});
@@ -439,8 +344,8 @@ function runSingleEditRebaseAxiomSuite(initialState: OptionalFieldTestState) {
 	});
 
 	describe("A⁻¹ ○ A === ε", () => {
-		for (const [name, change] of testChanges) {
-			if (["SetA", "SetB", "SetUndefined"].includes(name)) {
+		for (const [{ description: name, changeset: change }] of singleTestChanges("A")) {
+			if (["SetA,0", "SetB,0"].includes(name)) {
 				// TODO:AB#4622: OptionalChangeset should obey group axioms, but the current compose implementation does not
 				// cancel changes from inverses, and in some cases the representation isn't sufficient for doing so.
 				// Set operations fail to satisfy this test because they generate explicit deltas which set the trait to be
@@ -448,59 +353,22 @@ function runSingleEditRebaseAxiomSuite(initialState: OptionalFieldTestState) {
 				continue;
 			}
 			it(`${name}⁻¹ ○ ${name} === ε`, () => {
-				const taggedChange = tagChange(change, tag1);
-				const inv = tagRollbackInverse(invert(taggedChange), tag2, taggedChange.revision);
-				const changes = [inv, taggedChange];
-				const actual = compose(changes);
+				const inv = tagRollbackInverse(invert(change), tag1, change.revision);
+				const actual = compose([inv, change]);
 				const delta = toDelta(actual);
 				assert.equal(isDeltaVisible(delta), false);
 			});
 		}
 	});
-
-	describe("A ↷ [B, B⁻¹, C] === A ↷ compose([B, B⁻¹, C])", () => {
-		for (const [nameA, changeA] of testChanges) {
-			for (const [nameB, changeB] of testChanges) {
-				for (const [nameC, changeC] of testChanges) {
-					// if (!(nameA === "ChangeChild" && nameB === "SetA" && nameC === "ChangeChild")) {
-					// 	continue;
-					// }
-
-					it(`${nameA} ↷ [${nameB}, ${nameB}⁻¹, ${nameC}] === ${nameA} ↷ compose([${nameB}, ${nameB}⁻¹, ${nameC}])`, () => {
-						const taggedChangeA = tagChange(changeA, tag1);
-						const taggedChangeB = tagChange(changeB, tag2);
-						const taggedChangeC = tagChange(changeC, tag3);
-						const invB = tagRollbackInverse(
-							invert(taggedChangeB),
-							tag4,
-							taggedChangeB.revision,
-						);
-						const changes = [taggedChangeB, invB, taggedChangeC];
-						const r1 = rebaseTagged(taggedChangeA, ...changes);
-						const metadata = defaultRevisionMetadataFromChanges([
-							...changes,
-							taggedChangeA,
-						]);
-						const rebaseOverCompose = rebaseComposed(
-							metadata,
-							taggedChangeA,
-							...changes,
-						);
-						assert.deepEqual(r1.change, rebaseOverCompose.change);
-					});
-				}
-			}
-		}
-	});
 }
 
-describe.only("OptionalField - Rebaser Axioms", () => {
-	describe.only("Using valid edits from an undefined field", () => {
-		runSingleEditRebaseAxiomSuite({ contents: undefined });
+describe("OptionalField - Rebaser Axioms", () => {
+	describe("Using valid edits from an undefined field", () => {
+		runSingleEditRebaseAxiomSuite({ content: undefined });
 	});
 
-	describe.only("Using valid edits from a field with content", () => {
-		runSingleEditRebaseAxiomSuite({ contents: "A" });
+	describe("Using valid edits from a field with content", () => {
+		runSingleEditRebaseAxiomSuite({ content: "A" });
 	});
 
 	// To limit combinatorial explosion, we test 'rebasing over a compose is equivalent to rebasing over the individual edits'
@@ -508,26 +376,30 @@ describe.only("OptionalField - Rebaser Axioms", () => {
 	// - Rebasing a single edit over N sequential edits
 	// - Rebasing N sequential edits over a single edit, sandwich-rebasing style
 	//   (meaning [A, B, C] ↷ D involves B ↷ compose([A⁻¹, D, A]) and C ↷ compose([B⁻¹, A⁻¹, D, A, B]))
-	describe("Rebase over compose exhaustive", () => {
-		for (const initialState of [{ contents: undefined }, { contents: "A" }]) {
-			describe(`starting with contents ${initialState.contents}`, () => {
+	const numberOfEditsToRebaseOver = 2;
+	const numberOfEditsToRebase = numberOfEditsToRebaseOver;
+
+	describe.skip("Rebase over compose exhaustive", () => {
+		for (const initialState of [{ content: undefined }, { content: "A" }]) {
+			describe(`starting with contents ${initialState.content}`, () => {
 				for (const [
 					{ description: name, changeset: edit },
-				] of generatePossibleSequenceOfEdits(initialState, 1, "local-rev-")) {
+				] of generatePossibleSequenceOfEdits(
+					initialState,
+					generateChildStates,
+					1,
+					"local-rev-",
+				)) {
 					for (const namedEditsToRebaseOver of generatePossibleSequenceOfEdits(
 						initialState,
-						2,
+						generateChildStates,
+						numberOfEditsToRebaseOver,
 						"trunk-rev-",
 					)) {
 						const title = `Rebase ${name} over compose ${JSON.stringify(
 							namedEditsToRebaseOver.map(({ description }) => description),
 						)}`;
 
-						// if (
-						// 	title !== 'Rebase ChildChange 0 over compose ["Set undefined","Set 1"]'
-						// ) {
-						// 	continue;
-						// }
 						it(title, () => {
 							const editsToRebaseOver = namedEditsToRebaseOver.map(
 								({ changeset }) => changeset,
@@ -550,24 +422,27 @@ describe.only("OptionalField - Rebaser Axioms", () => {
 		}
 	});
 
-	describe("Sandwich rebase over compose exhaustive", () => {
-		for (const initialState of [{ contents: undefined }, { contents: "A" }]) {
-			describe(`starting with contents ${initialState.contents}`, () => {
+	describe.skip("Sandwich rebase over compose exhaustive", () => {
+		for (const initialState of [{ content: undefined }, { content: "A" }]) {
+			describe(`starting with contents ${initialState.content}`, () => {
 				for (const namedSourceEdits of generatePossibleSequenceOfEdits(
 					initialState,
-					2,
+					generateChildStates,
+					numberOfEditsToRebase,
 					"local-rev-",
 				)) {
 					for (const [
 						{ description: name, changeset: namedEditToRebaseOver },
-					] of generatePossibleSequenceOfEdits(initialState, 1, "trunk-rev-")) {
+					] of generatePossibleSequenceOfEdits(
+						initialState,
+						generateChildStates,
+						1,
+						"trunk-rev-",
+					)) {
 						const title = `Rebase ${JSON.stringify(
 							namedSourceEdits.map(({ description }) => description),
 						)} over ${name}`;
 
-						// if (title !== 'Rebase ["Set 0","ChildChange 1"] over Set 0') {
-						// 	continue;
-						// }
 						it(title, () => {
 							const editToRebaseOver = namedEditToRebaseOver;
 							const sourceEdits = namedSourceEdits.map(({ changeset }) => changeset);
