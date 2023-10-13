16558793c83dc2f0249a8ca229069e75c6e11dcc
Yann Achard <achardy@microsoft.com>
2023-10-13 13:24:44 -0700

    refactor(tree): unify attaches and reattaches representation

--- experimental/dds/tree2/src/feature-libraries/sequence-field/helperTypes.ts
+++ experimental/dds/tree2/src/feature-libraries/sequence-field/helperTypes.ts
@@ -3,31 +3,14 @@
  * Licensed under the MIT License.
  */
 
-import {
-	CellId,
-	HasMarkFields,
-	Detach,
-	Insert,
-	Mark,
-	NewAttach,
-	Revive,
-	Transient,
-} from "./format";
+import { CellId, HasMarkFields, Detach, Insert, Mark, Transient } from "./format";
 
 export type EmptyInputCellMark<TNodeChange> = Mark<TNodeChange> & DetachedCellMark;
-
-/**
- * A mark that spans one or more cells.
- * The spanned cells may be populated (e.g., "Delete") or not (e.g., "Revive").
- */
-export type CellSpanningMark<TNodeChange> = Exclude<Mark<TNodeChange>, NewAttach<TNodeChange>>;
 
 export interface DetachedCellMark extends HasMarkFields {
 	cellId: CellId;
 }
 
-export type GenerativeMark<TNodeChange> = Insert<TNodeChange> | Revive<TNodeChange>;
-
-export type TransientMark<TNodeChange> = GenerativeMark<TNodeChange> & Transient;
+export type TransientMark<TNodeChange> = Insert<TNodeChange> & Transient;
 
 export type EmptyOutputCellMark<TNodeChange> = TransientMark<TNodeChange> | Detach<TNodeChange>;
