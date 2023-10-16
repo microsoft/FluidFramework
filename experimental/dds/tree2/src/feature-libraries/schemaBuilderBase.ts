8696fc2c0c1b64dfae55a0cb8806737fce63b8d1
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-10-16 16:23:07 +0000

    refactor: Combine `finalize` and `toDocumentSchema` into a single `finalize` method on SchemaBuilder

--- experimental/dds/tree2/src/feature-libraries/schemaBuilderBase.ts
+++ experimental/dds/tree2/src/feature-libraries/schemaBuilderBase.ts
@@ -4,7 +4,7 @@
  */
 
 import { assert } from "@fluidframework/core-utils";
-import { Adapters, TreeSchemaIdentifier } from "../core";
+import { Adapters, TreeNavigationResult, TreeSchemaIdentifier } from "../core";
 import { Assume, RestrictiveReadonlyRecord, transformObjectMap } from "../util";
 import {
 	SchemaLibraryData,
@@ -141,10 +141,61 @@ export class SchemaBuilderBase<
 	}
 
 	/**
+	 * Finalizes the schema builder, producing {@link SchemaLibrary}s that capture the content added to this builder
+	 * and any additional `SchemaLibrary`s that were added to it.
+	 *
+	 * @remarks
+	 * May only be called once after adding content to builder is complete.
+	 *
+	 * @returns A {@link SchemaLibrary} usable with future `SchemaBuilder`s
+	 */
+	public finalize(): SchemaLibrary;
+
+	/**
+	 * Finalizes the schema builder, producing SchemaLibraries that capture the content added to this builder,
+	 * any additional SchemaLibraries that were added to it, and the schema of a root field.
+	 *
+	 * @remarks
+	 * May only be called once after adding content to builder is complete.
+	 *
+	 * @param root - The root field schema.
+	 * @typeParam TSchema - The kind of field schema of `root`.
+	 *
+	 * @returns A {@link TypedSchemaCollection} that may be used in the construction of trees.
+	 */
+	public finalize<const TSchema extends ImplicitFieldSchema>(
+		root: TSchema,
+	): TypedSchemaCollection<NormalizeField<TSchema, TDefaultKind>>;
+
+	/**
+	 * Finalizes the schema builder, producing SchemaLibraries that capture the content added to this builder,
+	 * any additional SchemaLibraries that were added to it, and (optionally) the schema of a root field.
+	 *
+	 * @remarks
+	 * May only be called once after adding content to builder is complete.
+	 *
+	 * @param root - Optional root field schema.
+	 * If provided, this will return a {@link TypedSchemaCollection} that may be used in the construction of trees.
+	 * If not provided, this will return a {@link SchemaLibrary} for use with future `SchemaBuilder`s.
+	 *
+	 * @typeParam TSchema - The kind of field schema of `root`.
+	 *
+	 * @returns A {@link TypedSchemaCollection} if given a `root` field schema, otherwise a {@link SchemaLibrary}
+	 * usable with future `SchemaBuilder`s.
+	 */
+	public finalize<const TSchema extends ImplicitFieldSchema>(
+		root?: TSchema,
+	): SchemaLibrary | TypedSchemaCollection<NormalizeField<TSchema, TDefaultKind>> {
+		return root === undefined ? this.toLibrarySchema() : this.toDocumentSchema(root);
+	}
+
+	/**
 	 * Produce SchemaLibraries which capture the content added to this builder, as well as any additional SchemaLibraries that were added to it.
+	 *
+	 * @remarks
 	 * May only be called once after adding content to builder is complete.
 	 */
-	public finalize(): SchemaLibrary {
+	private toLibrarySchema(): SchemaLibrary {
 		this.finalizeCommon();
 
 		// Check for errors:
@@ -160,7 +211,7 @@ export class SchemaBuilderBase<
 	 * @remarks
 	 * May only be called once after adding content to builder is complete.
 	 */
-	public toDocumentSchema<const TSchema extends ImplicitFieldSchema>(
+	private toDocumentSchema<const TSchema extends ImplicitFieldSchema>(
 		root: TSchema,
 	): TypedSchemaCollection<NormalizeField<TSchema, TDefaultKind>> {
 		// return this.toDocumentSchemaInternal(normalizeField(root, DefaultFieldKind));
