0e2695a40282e36a0bee19d93860ffa53412f7ca
justus-camp <102552460+justus-camp@users.noreply.github.com>
2023-10-23 23:57:48 +0000

    Sandwich rebase axioms passing

--- experimental/dds/tree2/src/test/feature-libraries/default-field-kinds/optionalChangeRebaser.spec.ts
+++ experimental/dds/tree2/src/test/feature-libraries/default-field-kinds/optionalChangeRebaser.spec.ts
@@ -325,22 +325,7 @@ function runSingleEditRebaseAxiomSuite(initialState: OptionalFieldTestState) {
 	});
 
 	describe("A ○ A⁻¹ === ε", () => {
-<<<<<<< HEAD
-		for (const [name, change] of testChanges) {
-=======
 		for (const [{ description: name, changeset: change }] of singleTestChanges("A")) {
-			if (
-				initialState.content !== undefined &&
-				["SetA,0", "SetB,0", "Delete"].includes(name)
-			) {
-				// TODO:AB#4622: OptionalChangeset should obey group axioms, but the current compose implementation does not
-				// cancel changes from inverses, and in some cases the representation isn't sufficient for doing so.
-				// Set operations fail to satisfy this test because they generate explicit deltas which set the trait to be
-				// the previous value, rather than noops.
-				continue;
-			}
-
->>>>>>> main
 			it(`${name} ○ ${name}⁻¹ === ε`, () => {
 				const inv = invert(change);
 				const actual = compose([change, tagRollbackInverse(inv, tag1, change.revision)]);
@@ -351,18 +336,7 @@ function runSingleEditRebaseAxiomSuite(initialState: OptionalFieldTestState) {
 	});
 
 	describe("A⁻¹ ○ A === ε", () => {
-<<<<<<< HEAD
-		for (const [name, change] of testChanges) {
-=======
 		for (const [{ description: name, changeset: change }] of singleTestChanges("A")) {
-			if (["SetA,0", "SetB,0"].includes(name)) {
-				// TODO:AB#4622: OptionalChangeset should obey group axioms, but the current compose implementation does not
-				// cancel changes from inverses, and in some cases the representation isn't sufficient for doing so.
-				// Set operations fail to satisfy this test because they generate explicit deltas which set the trait to be
-				// the previous value, rather than noops.
-				continue;
-			}
->>>>>>> main
 			it(`${name}⁻¹ ○ ${name} === ε`, () => {
 				const inv = tagRollbackInverse(invert(change), tag1, change.revision);
 				const actual = compose([inv, change]);
@@ -382,7 +356,7 @@ describe("OptionalField - Rebaser Axioms", () => {
 		runSingleEditRebaseAxiomSuite({ content: "A" });
 	});
 
-	describe.skip("Exhaustive", () => {
+	describe("Exhaustive", () => {
 		runExhaustiveComposeRebaseSuite(
 			[{ content: undefined }, { content: "A" }],
 			generateChildStates,
