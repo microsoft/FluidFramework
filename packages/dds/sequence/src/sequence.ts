02fb5bcef2426f17da5cff4e1104f7284ec3ae7a
Kian Thompson <102998837+kian-thompson@users.noreply.github.com>
2023-10-24 10:29:23 -0700

    [Port v2int 7.1] Fix grouped batching asserts (#17950)
    These asserts are currently blocking the rollout of the grouped batching
    feature.
    
    [0x071](https://github.com/microsoft/FluidFramework/pull/17836)
    [0x072](https://github.com/microsoft/FluidFramework/pull/17775)
    [0x331](https://github.com/microsoft/FluidFramework/pull/17913)

--- packages/dds/sequence/src/sequence.ts
+++ packages/dds/sequence/src/sequence.ts
@@ -621,7 +621,8 @@ export abstract class SharedSegmentSequence<T extends ISegment>
 							m.minimumSequenceNumber < collabWindow.minSeq ||
 							m.referenceSequenceNumber < collabWindow.minSeq ||
 							m.sequenceNumber <= collabWindow.minSeq ||
-							m.sequenceNumber <= collabWindow.currentSeq
+							// sequenceNumber could be the same if messages are part of a grouped batch
+							m.sequenceNumber < collabWindow.currentSeq
 						) {
 							throw new Error(
 								`Invalid catchup operations in snapshot: ${JSON.stringify({
