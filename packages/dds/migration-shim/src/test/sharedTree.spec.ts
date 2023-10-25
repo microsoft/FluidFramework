2e8377259a0d170c3517432501c8e0e57656277d
tyler-cai-microsoft <tylercai@microsoft.com>
2023-10-25 10:10:05 -1000

    Repro bug with just SharedTree

---
+++ packages/dds/migration-shim/src/test/sharedTree.spec.ts
@@ -0,0 +1,167 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "node:assert";
+
+import {
+	createSummarizerFromFactory,
+	summarizeNow,
+	type ITestObjectProvider,
+} from "@fluidframework/test-utils";
+import { describeNoCompat } from "@fluid-internal/test-version-utils";
+import {
+	ContainerRuntimeFactoryWithDefaultDataStore,
+	DataObject,
+	DataObjectFactory,
+} from "@fluidframework/aqueduct";
+import { type IChannel } from "@fluidframework/datastore-definitions";
+import {
+	AllowedUpdateType,
+	type ISharedTree,
+	SchemaBuilder,
+	SharedTreeFactory,
+	type Typed,
+	type ISharedTreeView,
+	typeboxValidator,
+	ForestType,
+} from "@fluid-experimental/tree2";
+import { type IFluidHandle } from "@fluidframework/core-interfaces";
+import { type IContainerRuntimeOptions } from "@fluidframework/container-runtime";
+import { LoaderHeader } from "@fluidframework/container-definitions";
+
+const treeKey = "treeKey";
+
+class TestDataObject extends DataObject {
+	// Allows us to get the SharedObject with whatever type we want
+	public async getTree(): Promise<ISharedTree> {
+		const handle: IFluidHandle<IChannel> | undefined =
+			this.root.get<IFluidHandle<IChannel>>(treeKey);
+		assert(handle !== undefined, "No handle found");
+		return (await handle.get()) as ISharedTree;
+	}
+
+	public createTree(type: string): ISharedTree {
+		const channel = this.runtime.createChannel(treeKey, type);
+		this.root.set(treeKey, channel.handle);
+		return channel as ISharedTree;
+	}
+}
+
+const builder = new SchemaBuilder({ scope: "test" });
+// For now this is the schema of the view.root
+const someType = builder.object("abc", {
+	quantity: builder.number,
+});
+
+// This is some schema to be updated later
+const schema = builder.intoSchema(SchemaBuilder.required(someType));
+
+function getNewTreeView(tree: ISharedTree): ISharedTreeView {
+	return tree.schematizeView({
+		initialTree: {
+			quantity: 0,
+		},
+		allowedSchemaModifications: AllowedUpdateType.None,
+		schema,
+	});
+}
+
+const testValue = 5;
+
+describeNoCompat("SharedTree", (getTestObjectProvider) => {
+	// Allow us to control summaries
+	const runtimeOptions: IContainerRuntimeOptions = {
+		summaryOptions: {
+			summaryConfigOverrides: {
+				state: "disabled",
+			},
+		},
+	};
+
+	// V2 of the registry (the migration registry) -----------------------------------------
+	// V2 of the code: Registry setup to migrate the document
+	const sharedTreeFactory = new SharedTreeFactory({
+		jsonValidator: typeboxValidator,
+		forest: ForestType.Reference,
+	});
+
+	const dataObjectFactory = new DataObjectFactory(
+		"TestDataObject",
+		TestDataObject,
+		[sharedTreeFactory],
+		{},
+	);
+
+	// The 2nd runtime factory, V2 of the code
+	const runtimeFactory = new ContainerRuntimeFactoryWithDefaultDataStore({
+		defaultFactory: dataObjectFactory,
+		registryEntries: [dataObjectFactory.registryEntry],
+		runtimeOptions,
+	});
+
+	let provider: ITestObjectProvider;
+
+	beforeEach(async () => {
+		provider = getTestObjectProvider();
+	});
+
+	it("Can create and retrieve tree", async () => {
+		// Setup containers and get Migration Shims instead of LegacySharedTrees
+		const container1 = await provider.createContainer(runtimeFactory);
+		const testObj1 = (await container1.getEntryPoint()) as TestDataObject;
+		const tree1 = testObj1.createTree(sharedTreeFactory.type);
+
+		const container2 = await provider.loadContainer(runtimeFactory);
+		const testObj2 = (await container2.getEntryPoint()) as TestDataObject;
+
+		await provider.ensureSynchronized();
+		const tree2 = await testObj2.getTree();
+
+		// Get the migrated values from the new tree
+
+		const view1 = getNewTreeView(tree1);
+		const view2 = getNewTreeView(tree2);
+		const treeNode1 = view1.root as unknown as Typed<typeof someType>;
+		const treeNode2 = view2.root as unknown as Typed<typeof someType>;
+
+		await provider.ensureSynchronized();
+
+		// Test that we can modify/send ops with the new Shared Tree
+		treeNode1.quantity = testValue;
+
+		await provider.ensureSynchronized();
+		assert(treeNode2.quantity === treeNode1.quantity, "Failed to update the new tree via op");
+		assert(treeNode2.quantity === testValue, "Failed to update the new tree via op");
+
+		// Summarize
+		const { summarizer } = await createSummarizerFromFactory(
+			provider,
+			container1,
+			dataObjectFactory,
+		);
+		await provider.ensureSynchronized();
+		const { summaryVersion, summaryTree } = await summarizeNow(summarizer);
+		console.log(summaryTree);
+
+		// Load a new container
+		const container3 = await provider.loadContainer(runtimeFactory, undefined, {
+			[LoaderHeader.version]: summaryVersion,
+		});
+
+		const testObj3 = (await container3.getEntryPoint()) as TestDataObject;
+		const tree3 = await testObj3.getTree();
+		const view3 = getNewTreeView(tree3);
+		const treeNode3 = view3.root as unknown as Typed<typeof someType>;
+		await provider.ensureSynchronized();
+		console.log(treeNode3);
+		treeNode3.quantity = 4;
+		await provider.ensureSynchronized();
+		assert(
+			treeNode3.quantity === treeNode1.quantity,
+			`Failed to summarize the tree data 3: ${treeNode3.quantity}, 1: ${treeNode1.quantity}`,
+		);
+		assert(treeNode3.quantity === testValue, "Failed to update the tree at all");
+	});
+});
