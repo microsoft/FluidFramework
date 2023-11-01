b8eca94deca594d4427fea6ed82dcf800972be79
Yann Achard <achardy@microsoft.com>
2023-11-01 12:19:08 -0700

    Add missing case to sequence implementation

--- experimental/dds/tree2/src/feature-libraries/sequence-field/getRelevantRemovedTrees.ts
+++ experimental/dds/tree2/src/feature-libraries/sequence-field/getRelevantRemovedTrees.ts
@@ -6,16 +6,20 @@
 import { Delta } from "../../core";
 import { nodeIdFromChangeAtom } from "../deltaUtils";
 import { Changeset } from "./format";
+import { isDetachMark, isInsert, isReattach } from "./utils";
 
 export function getRelevantRemovedTrees(changeset: Changeset): Delta.DetachedNodeId[] {
 	const nodes: Delta.DetachedNodeId[] = [];
 	for (const mark of changeset) {
 		if (mark.cellId !== undefined) {
-			if (mark.type === "Insert") {
-				// This tree is being restored by this change, so it is a relevant removed tree.
+			if (isInsert(mark) && isReattach(mark)) {
+				// This tree is being restored.
+				nodes.push(nodeIdFromChangeAtom(mark.cellId));
+			} else if (isDetachMark(mark)) {
+				// This removed tree is being moved.
 				nodes.push(nodeIdFromChangeAtom(mark.cellId));
 			} else if (mark.type !== "MoveIn" && mark.changes !== undefined) {
-				// This tree removed is being edited by this change, so it is a relevant removed tree.
+				// This removed tree is being edited.
 				nodes.push(nodeIdFromChangeAtom(mark.cellId));
 			}
 		}
