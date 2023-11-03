c7c1f828c7f1ec8a10016eb09c9a3d7fa0117767
Kian Thompson <102998837+kian-thompson@users.noreply.github.com>
2023-11-03 16:17:04 -0700

    Remove request pattern from examples (#16575)
    Remove 'request' API usages from FF examples. Most request APIs have
    been deprecated and will be removed in a future major release.
    
    List of examples checked:
    - CodeMirror
    - BubbleBench baseline
    - BubbleBench ot (really slow and glitchy)
    - BubbleBench sharedtree
    - BubbleBench editable shared tree (fails with same error as main)
    - canvas
    - clicker
    - diceroller
    - inventory app
    - monaco
    - shared text
    - table view
    - smde
    - prosemirror
    - data-object-grid
    - webflow (fails with same error as main)
    - multiview/container
    
    
    [AB#4990](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/4990)

--- examples/data-objects/prosemirror/src/prosemirror.tsx
+++ examples/data-objects/prosemirror/src/prosemirror.tsx
@@ -6,19 +6,12 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import { EventEmitter } from "events";
-// eslint-disable-next-line import/no-deprecated
-import { defaultFluidObjectRequestHandler } from "@fluidframework/aqueduct";
+import { IFluidLoadable, IFluidHandle, IRequest, IResponse } from "@fluidframework/core-interfaces";
 import {
-	IFluidLoadable,
-	IRequest,
-	IResponse,
-	IFluidHandle,
-	FluidObject,
-	// eslint-disable-next-line import/no-deprecated
-	IFluidRouter,
-} from "@fluidframework/core-interfaces";
-// eslint-disable-next-line import/no-deprecated
-import { FluidObjectHandle, mixinRequestHandler } from "@fluidframework/datastore";
+	FluidDataStoreRuntime,
+	FluidObjectHandle,
+	mixinRequestHandler,
+} from "@fluidframework/datastore";
 import { ISharedMap, SharedMap } from "@fluidframework/map";
 import {
 	IMergeTreeInsertMsg,
@@ -35,7 +28,7 @@ import {
 import { IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions";
 import { SharedString } from "@fluidframework/sequence";
 import { EditorView } from "prosemirror-view";
-import { ILoader } from "@fluidframework/container-definitions";
+import { create404Response } from "@fluidframework/runtime-utils";
 
 import React, { useEffect, useRef } from "react";
 
@@ -77,17 +70,9 @@ function createTreeMarkerOps(
  * It has its own implementation of IFluidLoadable and does not extend PureDataObject / DataObject. This is
  * done intentionally to serve as an example of exposing the URL and handle via IFluidLoadable.
  */
-export class ProseMirror
-	extends EventEmitter
-	// eslint-disable-next-line import/no-deprecated
-	implements IFluidLoadable, IFluidRouter, IProvideRichTextEditor
-{
-	public static async load(
-		runtime: IFluidDataStoreRuntime,
-		context: IFluidDataStoreContext,
-		existing: boolean,
-	) {
-		const collection = new ProseMirror(runtime, context);
+export class ProseMirror extends EventEmitter implements IFluidLoadable, IProvideRichTextEditor {
+	public static async load(runtime: IFluidDataStoreRuntime, existing: boolean) {
+		const collection = new ProseMirror(runtime);
 		await collection.initialize(existing);
 
 		return collection;
@@ -100,10 +85,7 @@ export class ProseMirror
 	public get IFluidLoadable() {
 		return this;
 	}
-	// eslint-disable-next-line import/no-deprecated
-	public get IFluidRouter() {
-		return this;
-	}
+
 	public get IRichTextEditor() {
 		return this._collabManager!;
 	}
@@ -119,18 +101,10 @@ export class ProseMirror
 	}
 	private readonly innerHandle: IFluidHandle<this>;
 
-	constructor(
-		private readonly runtime: IFluidDataStoreRuntime,
-		private readonly context: IFluidDataStoreContext,
-	) {
+	constructor(private readonly runtime: IFluidDataStoreRuntime) {
 		super();
 
 		this.innerHandle = new FluidObjectHandle(this, "", runtime.objectsRoutingContext);
-	}
-
-	public async request(request: IRequest): Promise<IResponse> {
-		// eslint-disable-next-line import/no-deprecated
-		return defaultFluidObjectRequestHandler(this, request);
 	}
 
 	private async initialize(existing: boolean) {
@@ -149,15 +123,17 @@ export class ProseMirror
 		this.root = (await this.runtime.getChannel("root")) as ISharedMap;
 		this.text = await this.root.get<IFluidHandle<SharedString>>("text")!.get();
 
-		const scope: FluidObject<ILoader> = this.context.scope;
-		if (scope.ILoader === undefined) {
-			throw new Error("scope must include ILoader");
-		}
-		this._collabManager = new FluidCollabManager(this.text, scope.ILoader);
+		this._collabManager = new FluidCollabManager(this.text);
 
 		// Access for debugging
 		// eslint-disable-next-line @typescript-eslint/dot-notation
 		window["easyComponent"] = this;
+	}
+
+	public async request(req: IRequest): Promise<IResponse> {
+		return req.url === "" || req.url === "/" || req.url.startsWith("/?")
+			? { mimeType: "fluid/object", status: 200, value: this }
+			: create404Response(req);
 	}
 }
 
@@ -170,13 +146,17 @@ export class ProseMirrorFactory implements IFluidDataStoreFactory {
 	}
 
 	public async instantiateDataStore(context: IFluidDataStoreContext, existing: boolean) {
-		// eslint-disable-next-line import/no-deprecated
-		const runtimeClass = mixinRequestHandler(async (request: IRequest) => {
-			const router = await routerP;
-			return router.request(request);
-		});
+		// request mixin in
+		const runtimeClass = mixinRequestHandler(
+			async (request: IRequest, runtimeArg: FluidDataStoreRuntime) => {
+				// The provideEntryPoint callback below always returns ProseMirror, so this cast is safe
+				const dataObject = (await runtimeArg.entryPoint.get()) as ProseMirror;
+				return dataObject.request?.(request);
+			},
+			FluidDataStoreRuntime,
+		);
 
-		const runtime = new runtimeClass(
+		return new runtimeClass(
 			context,
 			new Map(
 				[SharedMap.getFactory(), SharedString.getFactory()].map((factory) => [
@@ -185,14 +165,8 @@ export class ProseMirrorFactory implements IFluidDataStoreFactory {
 				]),
 			),
 			existing,
-			() => {
-				// TODO: AB#4993
-				throw new Error("TODO");
-			},
+			async (runtime: IFluidDataStoreRuntime) => ProseMirror.load(runtime, existing),
 		);
-		const routerP = ProseMirror.load(runtime, context, existing);
-
-		return runtime;
 	}
 }
 
