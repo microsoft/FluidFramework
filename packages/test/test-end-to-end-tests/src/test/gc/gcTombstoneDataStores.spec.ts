f233f0b66f08086219da97323654c1a11a74aaf7
Navin Agarwal <45832642+agarwal-navin@users.noreply.github.com>
2023-11-07 11:05:09 -0800

    GC: Add a GC option to control throwing on loading a tombstoned object (#18185)
    Currently, throwing on loading a tombstoned object is disabled by
    default. It can only be enabled via config flags. Since config provider
    is passed to the loader, it means this behavior can be controlled at the
    loader layer not at the container layer.
    This is not always desirable for all customers and some of them may want
    to control this behavior at the container level. This will also let them
    progagate changes to this behavior quickly since container changes are
    propagated faster.

--- packages/test/test-end-to-end-tests/src/test/gc/gcTombstoneDataStores.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/gc/gcTombstoneDataStores.spec.ts
@@ -226,7 +226,7 @@ describeNoCompat("GC data store tombstone tests", (getTestObjectProvider) => {
 	describe("Using tombstone data stores not allowed (per config)", () => {
 		beforeEach(() => {
 			// Allow Loading but not Usage
-			settings["Fluid.GarbageCollection.ThrowOnTombstoneLoad"] = false;
+			settings["Fluid.GarbageCollection.ThrowOnTombstoneLoadOverride"] = false;
 			settings["Fluid.GarbageCollection.ThrowOnTombstoneUsage"] = true;
 		});
 
@@ -416,7 +416,7 @@ describeNoCompat("GC data store tombstone tests", (getTestObjectProvider) => {
 
 		beforeEach(() => {
 			// Allow Usage but not Loading
-			settings["Fluid.GarbageCollection.ThrowOnTombstoneLoad"] = true;
+			settings["Fluid.GarbageCollection.ThrowOnTombstoneLoadOverride"] = true;
 			settings["Fluid.GarbageCollection.ThrowOnTombstoneUsage"] = false;
 		});
 
@@ -834,7 +834,7 @@ describeNoCompat("GC data store tombstone tests", (getTestObjectProvider) => {
 			},
 		],
 		async () => {
-			settings["Fluid.GarbageCollection.ThrowOnTombstoneLoad"] = false;
+			settings["Fluid.GarbageCollection.ThrowOnTombstoneLoadOverride"] = false;
 			settings["Fluid.GarbageCollection.ThrowOnTombstoneUsage"] = false;
 			const { unreferencedId, summarizingContainer, summarizer } =
 				await summarizationWithUnreferencedDataStoreAfterTime(sweepTimeoutMs);
@@ -843,7 +843,7 @@ describeNoCompat("GC data store tombstone tests", (getTestObjectProvider) => {
 			// The datastore should be tombstoned now
 			const { summaryVersion } = await summarize(summarizer);
 			const container = await loadContainer(summaryVersion);
-			// Requesting the tombstoned data store should succeed since ThrowOnTombstoneLoad is not enabled.
+			// Requesting the tombstoned data store should succeed since ThrowOnTombstoneLoadOverride is not enabled.
 			// Logs a tombstone and sweep ready error
 			let dataObject: ITestDataObject;
 			await assert.doesNotReject(async () => {
@@ -893,7 +893,7 @@ describeNoCompat("GC data store tombstone tests", (getTestObjectProvider) => {
 
 		beforeEach(() => {
 			// This is not the typical configuration we expect (usage may be allowed), but keeping it more strict for the tests
-			settings["Fluid.GarbageCollection.ThrowOnTombstoneLoad"] = true;
+			settings["Fluid.GarbageCollection.ThrowOnTombstoneLoadOverride"] = true;
 			settings["Fluid.GarbageCollection.ThrowOnTombstoneUsage"] = true;
 		});
 
