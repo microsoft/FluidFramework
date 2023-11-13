65cb9dccf1fb36235875e2692bf13391492ae9e9
tyler-cai-microsoft <90650728+tyler-cai-microsoft@users.noreply.github.com>
2023-11-13 15:05:30 -0800

    Update packages/dds/migration-shim/src/migrationDeltaHandler.ts
    Co-authored-by: Matt Rakow <ChumpChief@users.noreply.github.com>

--- packages/dds/migration-shim/src/migrationDeltaHandler.ts
+++ packages/dds/migration-shim/src/migrationDeltaHandler.ts
@@ -9,7 +9,7 @@ import { assert } from "@fluidframework/core-utils";
 import { type IStampedContents, type IShimDeltaHandler } from "./types.js";
 import {
 	attributesMatch,
-	messageStampMatchesAttributes as messageStampMatchesAttributes,
+	messageStampMatchesAttributes,
 } from "./utils.js";
 
 /**
