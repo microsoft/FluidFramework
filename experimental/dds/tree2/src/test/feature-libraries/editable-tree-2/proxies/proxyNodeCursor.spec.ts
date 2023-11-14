19c9bc36ac926c1c81018bb5ecd935e7bf24366f
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-11-14 18:37:39 +0000

    test: Add more tests

--- experimental/dds/tree2/src/test/feature-libraries/editable-tree-2/proxies/proxyNodeCursor.spec.ts
+++ experimental/dds/tree2/src/test/feature-libraries/editable-tree-2/proxies/proxyNodeCursor.spec.ts
@@ -9,6 +9,7 @@ import { createProxyTreeAdapter } from "../../../../feature-libraries/editable-t
 import { SchemaBuilder } from "../../../../domains";
 import { viewWithContent } from "../../../utils";
 import { brand } from "../../../../util";
+import { EmptyKey } from "../../../../core";
 
 // Note: the behaviors here are more heavily tested by `proxies.spec.ts`.
 // This adds some basic unit test for the generated cursor adapter, but since the adaptor is an implementation
@@ -67,12 +68,105 @@ describe("cursorFromProxyTree", () => {
 		const fieldD = adapter.getFieldFromNode(root, brand("d"));
 		assert.equal(fieldD.length, 0);
 
-		const fieldE = adapter.getFieldFromNode(root, brand("d")); // No such property as "e" in the schema or initial tree
+		const fieldE = adapter.getFieldFromNode(root, brand("e")); // No such property as "e" in the schema or initial tree
 		assert.equal(fieldE.length, 0);
 
 		// Quick test of recursive invokation
 		assert.equal(adapter.value(root.a), a);
 		assert.equal(adapter.type(root.a), schemaBuilder.string.name);
 		assert.deepEqual(adapter.keysFromNode(root.a), []);
+	});
+
+	it("string", () => {
+		const schemaBuilder = new SchemaBuilder({ scope: "test" });
+		const schema = schemaBuilder.intoSchema(schemaBuilder.string);
+
+		const view = viewWithContent({
+			schema,
+			initialTree: "Hello world",
+		});
+		const root = view.root;
+
+		const adapter = createProxyTreeAdapter(view.context, schema.rootFieldSchema.types);
+
+		assert.equal(adapter.value(root), "Hello world");
+		assert.equal(adapter.type(root), schemaBuilder.string.name);
+
+		const rootKeys = adapter.keysFromNode(root);
+		assert.deepEqual(rootKeys, []);
+	});
+
+	it("list", () => {
+		const schemaBuilder = new SchemaBuilder({ scope: "test" });
+		const rootSchema = schemaBuilder.list("list", [schemaBuilder.number]);
+		const schema = schemaBuilder.intoSchema(rootSchema);
+
+		const entries = [42, 37, -1]; // TODO: undefined?
+
+		const view = viewWithContent({
+			schema,
+			initialTree: entries as any,
+		});
+		const root = view.root;
+
+		const adapter = createProxyTreeAdapter(view.context, schema.rootFieldSchema.types);
+
+		assert.equal(adapter.value(root), undefined);
+		assert.equal(adapter.type(root), rootSchema.name);
+
+		const rootKeys = adapter.keysFromNode(root);
+		assert.deepEqual(rootKeys, [EmptyKey]);
+
+		const field = adapter.getFieldFromNode(root, EmptyKey);
+		assert.deepEqual(field, entries);
+	});
+
+	it("map", () => {
+		const schemaBuilder = new SchemaBuilder({ scope: "test" });
+		const rootSchema = schemaBuilder.map("map", [
+			schemaBuilder.number,
+			schemaBuilder.string,
+			schemaBuilder.boolean,
+		]);
+		const schema = schemaBuilder.intoSchema(rootSchema);
+
+		const entries: [string, boolean | number | string | undefined][] = [
+			["a", 42],
+			["b", "Hello world"],
+			["c", false],
+			// ["d", undefined],
+		];
+
+		const view = viewWithContent({
+			schema,
+			initialTree: new Map<string, boolean | number | string | undefined>(entries) as any,
+		});
+		const root = view.root;
+
+		const adapter = createProxyTreeAdapter(view.context, schema.rootFieldSchema.types);
+
+		assert.equal(adapter.value(root), undefined);
+		assert.equal(adapter.type(root), rootSchema.name);
+
+		const rootKeys = adapter.keysFromNode(root);
+		assert.deepEqual(rootKeys, ["a", "b", "c"]); // We don't expect `d` because it is optional and missing.
+
+		const fieldA = adapter.getFieldFromNode(root, brand("a"));
+		assert.equal(fieldA.length, 1);
+		assert.deepEqual(fieldA[0], 42);
+
+		const fieldB = adapter.getFieldFromNode(root, brand("b"));
+		assert.equal(fieldB.length, 1);
+		assert.deepEqual(fieldB[0], "Hello world");
+
+		const fieldC = adapter.getFieldFromNode(root, brand("c"));
+		assert.equal(fieldC.length, 1);
+		assert.deepEqual(fieldC[0], false);
+
+		const fieldD = adapter.getFieldFromNode(root, brand("d")); // Entry was specified in initialTree was `undefined`
+		assert.equal(fieldD.length, 0);
+
+		const fieldE = adapter.getFieldFromNode(root, brand("e")); // No entry specified in initialTree
+		assert.equal(fieldE.length, 0);
 	});
 });
