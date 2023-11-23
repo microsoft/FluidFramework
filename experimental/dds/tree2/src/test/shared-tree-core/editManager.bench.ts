bee80aff90713160b934465c89b79cafb6b9be1c
Yann Achard <achardy@microsoft.com>
2023-11-22 20:23:38 -0800

    Revert "Better comments"
    This reverts commit 0ce5e1b00cf412716ff68576b460f7d8938c1d20.

--- experimental/dds/tree2/src/test/shared-tree-core/editManager.bench.ts
+++ experimental/dds/tree2/src/test/shared-tree-core/editManager.bench.ts
@@ -12,7 +12,7 @@ import {
 	rebasePeerEditsOverTrunkEdits,
 } from "./editManagerTestUtils";
 
-describe.only("EditManager - Bench", () => {
+describe("EditManager - Bench", () => {
 	interface Scenario {
 		readonly type: BenchmarkType;
 		readonly rebasedEditCount: number;
@@ -20,12 +20,12 @@ describe.only("EditManager - Bench", () => {
 	}
 
 	const scenarios: Scenario[] = [
-		// { type: BenchmarkType.Perspective, rebasedEditCount: 1, trunkEditCount: 1 },
-		// { type: BenchmarkType.Perspective, rebasedEditCount: 10, trunkEditCount: 1 },
-		// { type: BenchmarkType.Perspective, rebasedEditCount: 100, trunkEditCount: 1 },
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 1, trunkEditCount: 1 },
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 10, trunkEditCount: 1 },
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 100, trunkEditCount: 1 },
 		{ type: BenchmarkType.Perspective, rebasedEditCount: 1000, trunkEditCount: 1 },
-		// { type: BenchmarkType.Perspective, rebasedEditCount: 1, trunkEditCount: 10 },
-		// { type: BenchmarkType.Perspective, rebasedEditCount: 1, trunkEditCount: 100 },
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 1, trunkEditCount: 10 },
+		{ type: BenchmarkType.Perspective, rebasedEditCount: 1, trunkEditCount: 100 },
 		{ type: BenchmarkType.Perspective, rebasedEditCount: 1, trunkEditCount: 1000 },
 		{ type: BenchmarkType.Measurement, rebasedEditCount: 100, trunkEditCount: 100 },
 	];
