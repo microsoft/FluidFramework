611e1d2baa835240981009e09f27788ed85d5331
justus-camp <102552460+justus-camp@users.noreply.github.com>
2023-11-27 18:17:28 +0000

    Migrate object.spec.ts off of itWithRoot

--- experimental/dds/tree2/src/test/simple-tree/object.spec.ts
+++ experimental/dds/tree2/src/test/simple-tree/object.spec.ts
@@ -8,7 +8,7 @@ import { LeafNodeSchema, NewFieldContent, TreeSchema } from "../../feature-libra
 import { leaf, SchemaBuilder } from "../../domains";
 import { TreeValue } from "../../core";
 import { treeViewWithContent } from "../utils";
-import { itWithRoot, makeSchema, pretty } from "./utils";
+import { makeSchema, pretty } from "./utils";
 
 interface TestCase {
 	initialTree: object;
@@ -64,7 +64,12 @@ function testObjectLike(testCases: TestCase[]) {
 
 			for (const { schema, initialTree } of testCases) {
 				describe("instanceof Object", () => {
-					itWithRoot(`${pretty(initialTree)} -> true`, schema, initialTree, (root) => {
+					it(`${pretty(initialTree)} -> true`, () => {
+						const view = treeViewWithContent({
+							schema,
+							initialTree: initialTree as NewFieldContent,
+						});
+						const root = view.root;
 						assert(root instanceof Object, "object must be instanceof Object");
 					});
 				});
@@ -73,47 +78,45 @@ function testObjectLike(testCases: TestCase[]) {
 					for (const [key, descriptor] of Object.entries(
 						Object.getOwnPropertyDescriptors(Object.prototype),
 					)) {
-						itWithRoot(
-							`Object.getOwnPropertyDescriptor(${pretty(
-								initialTree,
-							)}, ${key}) -> ${pretty(descriptor)}}`,
-							schema,
+						it(`Object.getOwnPropertyDescriptor(${pretty(
 							initialTree,
-							(root) => {
-								assert.deepEqual(
-									Object.getOwnPropertyDescriptor(findObjectPrototype(root), key),
-									descriptor,
-									`Proxy must expose Object.prototype.${key}`,
-								);
-							},
-						);
+						)}, ${key}) -> ${pretty(descriptor)}}`, () => {
+							const view = treeViewWithContent({
+								schema,
+								initialTree: initialTree as NewFieldContent,
+							});
+							const root = view.root;
+							assert.deepEqual(
+								Object.getOwnPropertyDescriptor(findObjectPrototype(root), key),
+								descriptor,
+								`Proxy must expose Object.prototype.${key}`,
+							);
+						});
 					}
 				});
 
 				describe("methods inherited from Object.prototype", () => {
-					itWithRoot(
-						`${pretty(initialTree)}.isPrototypeOf(Object.create(root)) -> true`,
-						schema,
-						initialTree,
+					it(`${pretty(initialTree)}.isPrototypeOf(Object.create(root)) -> true`, () => {
+						const view = treeViewWithContent({
+							schema,
+							initialTree: initialTree as NewFieldContent,
+						});
+						const root = view.root;
+						const asObject = root as object;
+						// eslint-disable-next-line no-prototype-builtins -- compatibility test
+						assert.equal(asObject.isPrototypeOf(Object.create(asObject)), true);
+					});
 
-						(root) => {
-							const asObject = root as object;
-							// eslint-disable-next-line no-prototype-builtins -- compatibility test
-							assert.equal(asObject.isPrototypeOf(Object.create(asObject)), true);
-						},
-					);
-
-					itWithRoot(
-						`${pretty(initialTree)}.isPrototypeOf(root) -> false`,
-						schema,
-						initialTree,
-
-						(root) => {
-							const asObject = root as object;
-							// eslint-disable-next-line no-prototype-builtins -- compatibility test
-							assert.equal(asObject.isPrototypeOf(asObject), false);
-						},
-					);
+					it(`${pretty(initialTree)}.isPrototypeOf(root) -> false`, () => {
+						const view = treeViewWithContent({
+							schema,
+							initialTree: initialTree as NewFieldContent,
+						});
+						const root = view.root;
+						const asObject = root as object;
+						// eslint-disable-next-line no-prototype-builtins -- compatibility test
+						assert.equal(asObject.isPrototypeOf(asObject), false);
+					});
 				});
 
 				describe(`${pretty(initialTree)}.propertyIsEnumerable`, () => {
@@ -123,7 +126,12 @@ function testObjectLike(testCases: TestCase[]) {
 							key,
 						);
 
-						itWithRoot(`${key} -> ${expected}`, schema, initialTree, (root) => {
+						it(`${key} -> ${expected}`, () => {
+							const view = treeViewWithContent({
+								schema,
+								initialTree: initialTree as NewFieldContent,
+							});
+							const root = view.root;
 							const asObject = root as object;
 							// eslint-disable-next-line no-prototype-builtins -- compatibility test
 							assert.equal(asObject.propertyIsEnumerable(key), expected);
@@ -138,15 +146,15 @@ function testObjectLike(testCases: TestCase[]) {
 				const real = structuredClone(initialTree);
 				const expected = fn(real);
 
-				itWithRoot(
-					`${pretty(real)} -> ${pretty(expected)}`,
-					schema,
-					initialTree,
-					(proxy) => {
-						const actual = fn(proxy as object);
-						assert.deepEqual(actual, expected);
-					},
-				);
+				it(`${pretty(real)} -> ${pretty(expected)}`, () => {
+					const view = treeViewWithContent({
+						schema,
+						initialTree: initialTree as NewFieldContent,
+					});
+					const root = view.root;
+					const actual = fn(root as object);
+					assert.deepEqual(actual, expected);
+				});
 			}
 		}
 
@@ -313,17 +321,18 @@ testObjectLike(tcs);
 
 describe("Object-like", () => {
 	describe("setting an invalid field", () => {
-		itWithRoot(
-			"throws TypeError in strict mode",
-			makeSchema((_) => _.object("no fields", {})),
-			{},
-			(root) => {
-				assert.throws(() => {
-					// The actual error "'TypeError: 'set' on proxy: trap returned falsish for property 'foo'"
-					(root as unknown as any).foo = 3;
-				}, "attempting to set an invalid field must throw.");
-			},
-		);
+		it("throws TypeError in strict mode", () => {
+			const schema = makeSchema((_) => _.object("no fields", {}));
+			const view = treeViewWithContent({
+				schema,
+				initialTree: {},
+			});
+			const root = view.root;
+			assert.throws(() => {
+				// The actual error "'TypeError: 'set' on proxy: trap returned falsish for property 'foo'"
+				(root as unknown as any).foo = 3;
+			}, "attempting to set an invalid field must throw.");
+		});
 	});
 
 	describe("supports setting", () => {
@@ -334,31 +343,31 @@ describe("Object-like", () => {
 				after: TreeValue<TSchema["leafValue"]>,
 			) {
 				describe(`required ${typeof before} `, () => {
-					itWithRoot(
-						`(${pretty(before)} -> ${pretty(after)})`,
-						makeSchema((_) => _.object("", { _value: schema })),
-						{ _value: before },
-						(root) => {
-							assert.equal(root._value, before);
-							root._value = after;
-							assert.equal(root._value, after);
-						},
-					);
+					it(`(${pretty(before)} -> ${pretty(after)})`, () => {
+						const view = treeViewWithContent({
+							schema: makeSchema((_) => _.object("", { _value: schema })),
+							initialTree: { _value: before },
+						});
+						const root = view.root;
+						assert.equal(root._value, before);
+						root._value = after;
+						assert.equal(root._value, after);
+					});
 				});
 
 				describe(`optional ${typeof before}`, () => {
-					itWithRoot(
-						`(undefined -> ${pretty(before)} -> ${pretty(after)})`,
-						makeSchema((_) => _.object("", { _value: _.optional(schema) })),
-						{ _value: undefined },
-						(root) => {
-							assert.equal(root._value, undefined);
-							root._value = before;
-							assert.equal(root._value, before);
-							root._value = after;
-							assert.equal(root._value, after);
-						},
-					);
+					it(`(undefined -> ${pretty(before)} -> ${pretty(after)})`, () => {
+						const view = treeViewWithContent({
+							schema: makeSchema((_) => _.object("", { _value: _.optional(schema) })),
+							initialTree: { _value: undefined },
+						});
+						const root = view.root;
+						assert.equal(root._value, undefined);
+						root._value = before;
+						assert.equal(root._value, before);
+						root._value = after;
+						assert.equal(root._value, after);
+					});
 				});
 			}
 
@@ -380,16 +389,16 @@ describe("Object-like", () => {
 			const before = { objId: 0 };
 			const after = { objId: 1 };
 
-			itWithRoot(
-				`(${pretty(before)} -> ${pretty(after)})`,
-				schema,
-				{ child: before },
-				(root) => {
-					assert.equal(root.child.objId, 0);
-					root.child = after;
-					assert.equal(root.child.objId, 1);
-				},
-			);
+			it(`(${pretty(before)} -> ${pretty(after)})`, () => {
+				const view = treeViewWithContent({
+					schema,
+					initialTree: { child: before },
+				});
+				const root = view.root;
+				assert.equal(root.child.objId, 0);
+				root.child = after;
+				assert.equal(root.child.objId, 1);
+			});
 		});
 
 		describe("optional object", () => {
@@ -405,19 +414,19 @@ describe("Object-like", () => {
 			const before = { objId: 0 };
 			const after = { objId: 1 };
 
-			itWithRoot(
-				`(undefined -> ${pretty(before)} -> ${pretty(after)})`,
-				schema,
-				// TODO: Remove explicit undefined once implicit undefined is supported.
-				{ child: undefined },
-				(root) => {
-					assert.equal(root.child, undefined);
-					root.child = before;
-					assert.equal(root.child.objId, 0);
-					root.child = after;
-					assert.equal(root.child.objId, 1);
-				},
-			);
+			it(`(undefined -> ${pretty(before)} -> ${pretty(after)})`, () => {
+				const view = treeViewWithContent({
+					schema,
+					// TODO: Remove explicit undefined once implicit undefined is supported.
+					initialTree: { child: undefined },
+				});
+				const root = view.root;
+				assert.equal(root.child, undefined);
+				root.child = before;
+				assert.equal(root.child.objId, 0);
+				root.child = after;
+				assert.equal(root.child.objId, 1);
+			});
 		});
 
 		describe.skip("required list", () => {
