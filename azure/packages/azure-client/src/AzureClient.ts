8b3282a57534a56f59aae21ad56368c59bacafe8
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-11-29 22:14:03 +0000

    revert: Unwanted changes

--- azure/packages/azure-client/src/AzureClient.ts
+++ azure/packages/azure-client/src/AzureClient.ts
@@ -59,13 +59,13 @@ const MAX_VERSION_COUNT = 5;
 export class AzureClient {
 	private readonly documentServiceFactory: IDocumentServiceFactory;
 	private readonly urlResolver: IUrlResolver;
-	public readonly configProvider: IConfigProviderBase | undefined;
+	private readonly configProvider: IConfigProviderBase | undefined;
 
 	/**
 	 * Creates a new client instance using configuration parameters.
 	 * @param properties - Properties for initializing a new AzureClient instance
 	 */
-	public constructor(public readonly properties: AzureClientProps) {
+	public constructor(private readonly properties: AzureClientProps) {
 		// remove trailing slash from URL if any
 		properties.connection.endpoint = properties.connection.endpoint.replace(/\/$/, "");
 		this.urlResolver = new AzureUrlResolver();
