e6a26a9c7357aa4b391d1e84ac3de27c727be933
Sonali Deshpande <48232592+sonalideshpandemsft@users.noreply.github.com>
2023-11-30 00:17:46 +0000

    wip

--- examples/service-clients/odsp-client/shared-tree-demo/src/index.tsx
+++ examples/service-clients/odsp-client/shared-tree-demo/src/index.tsx
@@ -4,13 +4,18 @@
  */
 
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
+console.log("Rendering app...");
+
+ReactDOM.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
+	document.querySelector("#content"),
+	() => {
+		console.log("App rendered!");
+	},
 );
