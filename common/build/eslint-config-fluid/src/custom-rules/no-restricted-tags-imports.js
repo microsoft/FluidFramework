92061068b49502a9c56a462b36d7698dc79d648d
Wayne Ferrao <wayne.ferrao93@gmail.com>
2023-11-30 14:10:22 -0500

    Refactored rule and code to use ts-morph. Updated tests to use distinct, local test files.

--- common/build/eslint-config-fluid/src/custom-rules/no-restricted-tags-imports.js
+++ common/build/eslint-config-fluid/src/custom-rules/no-restricted-tags-imports.js
@@ -2,7 +2,66 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-const { TSDocParser } = require("@microsoft/tsdoc");
+
+const path = require("path");
+const { Project } = require("ts-morph");
+
+/**
+ * Validates if a tag is correctly formatted.
+ * @param tag - The tag to be validated.
+ * @param context - The context object provided by ESLint.
+ * @returns true if valid tag, else false.
+ */
+function isTagValid(tag, context) {
+	if (!tag.startsWith("@")) {
+		context.report({
+			loc: { line: 1, column: 0 },
+			message: `Invalid tag format in rule configuration: '{${tag}}'. Tags should start with '@'.`,
+			data: { tag },
+		});
+	} else {
+		return true;
+	}
+}
+
+/**
+ * Processes the tags array to ensure all tags are valid.
+ * @param tags - Array of tags to be processed.
+ * @param context - The context object provided by ESLint.
+ * @returns A set of validated tags.
+ */
+function processTags(tags, context) {
+	return new Set(tags.filter((tag) => isTagValid(tag, context)));
+}
+
+/**
+ * Validate the exceptions object, ensuring all tags and paths are valid.
+ * @param exceptions - The exceptions object from the rule configuration.
+ * @param context - The context object provided by ESLint.
+ * @returns - An object with tags as keys and Sets of paths as values.
+ */
+function processExceptions(exceptions, context) {
+	const processedExceptions = {};
+	Object.keys(exceptions).forEach((tag) => {
+		if (isTagValid(tag, context)) {
+			processedExceptions[tag] = new Set(exceptions[tag]);
+		}
+	});
+	return processedExceptions;
+}
+
+/**
+ * Resolves the path of an imported module to an absolute path.
+ *
+ * @param importPath - The path used in the import statement.
+ * @param currentFilePath - The absolute path of the current file.
+ * @return - The absolute path of the imported module.
+ */
+function resolveImportPath(importPath, currentFilePath) {
+	return path.isAbsolute(importPath)
+		? importPath
+		: path.resolve(path.dirname(currentFilePath), importPath);
+}
 
 module.exports = {
 	meta: {
@@ -39,73 +98,64 @@ module.exports = {
 		},
 	},
 	create(context) {
-		const options = context.options[0];
-		const restrictedTags = new Set();
-		const exceptions = {};
-		// Get restricted tags and throw error if not formatted correctly, ie: doesn't start with '@'
-		(options.tags || []).forEach((tag) => {
-			if (isTagValid(tag, context)) {
-				restrictedTags.add(tag);
-			}
-		});
-		// Validate and save the exceptions object being passed in.
-		Object.keys(options.exceptions || {}).forEach((tag) => {
-			if (isTagValid(tag, context)) {
-				if (!exceptions[tag]) {
-					exceptions[tag] = new Set();
-				}
-				options.exceptions[tag].forEach((exceptionPath) => {
-					exceptions[tag].add(exceptionPath);
-				});
-			}
-		});
+		const options = context.options[0] || {};
+		const restrictedTags = processTags(options.tags, context);
+		const exceptions = processExceptions(options.exceptions, context);
+		const project = new Project();
+
+		let tsConfigPath;
+		if (context.parserOptions.project) {
+			// Resolve the relative path to an absolute path
+			tsConfigPath = path.resolve(context.getCwd(), context.parserOptions.project);
+		} else {
+			// Fallback to a default/test path or handle absence of tsconfig.json as needed
+			tsConfigPath = path.join(__dirname, "../test/mockFiles/**/*.ts");
+		}
+		project.addSourceFilesAtPaths(tsConfigPath);
+
+		// const sourceFile = project.getSourceFileOrThrow("mockModule.ts");
 		return {
 			ImportDeclaration(node) {
 				const importSource = node.source.value;
-				node.specifiers.forEach((items) => {
-					const variable = context.getDeclaredVariables(items)[0];
-					const sourceCode = context.getSourceCode();
-					const comments = sourceCode.getAllComments();
-					// JSDocs comments are block comments, so we're only interested in those.
-					comments.forEach((comment) => {
-						if (comment.type !== "Block") {
-							return;
-						}
-						const tsdocParser = new TSDocParser();
-						// The leading and trailing new line characters were trimmed so we need to readd them for tsdoc to parse the comment correctly.
-						let commentText = `/**\n` + comment.value + `\n */`;
-						const parserContext = tsdocParser.parseString(commentText);
+				const currentFilePath = context.getFilename();
 
-						restrictedTags.forEach((tag) => {
-							if (parserContext.docComment.modifierTagSet.hasTagName(tag)) {
-								if (exceptions[tag] && exceptions[tag].has(importSource)) {
-									return; // This import is an exception, so it's allowed
-								}
-								context.report({
-									node,
-									messageId: "importWithRestrictedTag",
-									data: {
-										name: variable.name,
-										tag: tag.title,
-									},
+				// For each item being imported
+				node.specifiers.forEach((specifier) => {
+					if (specifier.type === "ImportSpecifier") {
+						// Name of imported item
+						const importedName = specifier.imported.name;
+
+						const importedFilePath = resolveImportPath(
+							specifier.parent.source.value,
+							currentFilePath,
+						);
+						// File it was imported from
+						const importedFile = project.addSourceFileAtPath(importedFilePath);
+						const declaration = importedFile.getFunction(importedName);
+						if (declaration) {
+							const jsDocs = declaration.getJsDocs();
+							jsDocs.forEach((doc) => {
+								const docTags = doc.getTags().map((docTag) => {
+									return docTag.getFullText().trim();
 								});
-							}
-						});
-					});
+								docTags.forEach((tag) => {
+									// Check if the tag is restricted
+									if (restrictedTags.has(tag)) {
+										if (exceptions[tag] && exceptions[tag].has(importSource)) {
+											return; // This import is an exception, so it's allowed
+										}
+										// The imported item has a restricted tag, throw an error.
+										context.report({
+											node: specifier,
+											message: `Importing ${tag} tagged items is not allowed: ${importedName}`,
+										});
+									}
+								});
+							});
+						}
+					}
 				});
 			},
 		};
 	},
 };
-
-function isTagValid(tag, context) {
-	if (!tag.startsWith("@")) {
-		context.report({
-			loc: { line: 1, column: 0 },
-			message: `Invalid tag format in rule configuration: '{${tag}}'. Tags should start with '@'.`,
-			data: { tag },
-		});
-	} else {
-		return true;
-	}
-}
--- common/build/eslint-config-fluid/src/test/enforce-no-restricted-tags-imports.test.js
+++ common/build/eslint-config-fluid/src/test/enforce-no-restricted-tags-imports.test.js
@@ -2,119 +2,102 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+const assert = require("assert");
+const path = require("path");
+const { ESLint } = require("eslint");
 
-const { RuleTester } = require("eslint");
-const noRestrictedTagsImports = require("../custom-rules/no-restricted-tags-imports");
+describe("ESLint Rule Tests", function () {
+	function createESLintInstance(config) {
+		return new ESLint({
+			useEslintrc: false,
+			overrideConfig: config,
+			rulePaths: [path.join(__dirname, "../custom-rules")],
+		});
+	}
 
-const ruleTester = new RuleTester({
-	parser: require.resolve("@typescript-eslint/parser"),
+	it("Should report an error for restricted tag imports", async function () {
+		const eslint = createESLintInstance({
+			rules: {
+				"no-restricted-tags-imports": [
+					"error",
+					{
+						tags: ["@internal", "@alpha"],
+						exceptions: { "@alpha": ["./exceptionFile.ts"] },
+					},
+				],
+			},
+			parser: "@typescript-eslint/parser",
+		});
+		const filesToLint = ["fileWithImports.ts", "mockModule.ts"].map((file) =>
+			path.join(__dirname, "mockFiles", file),
+		);
+		const results = await eslint.lintFiles(filesToLint);
+		const result = results[0];
+
+		assert.strictEqual(result.errorCount, 2, "Should have two errors");
+		assert.strictEqual(
+			result.messages[0].message,
+			"Importing @internal tagged items is not allowed: internalFunction",
+		);
+		assert.strictEqual(
+			result.messages[1].message,
+			"Importing @alpha tagged items is not allowed: alphaFunction",
+		);
+	});
+
+	it("Should not report an error for restricted tag imports for exceptions", async function () {
+		const eslint = createESLintInstance({
+			rules: {
+				"no-restricted-tags-imports": [
+					"error",
+					{
+						tags: ["@internal", "@alpha"],
+						exceptions: {
+							"@alpha": ["./exceptionFile.ts"],
+							"@internal": ["./exceptionFile.ts"],
+						},
+					},
+				],
+			},
+			parser: "@typescript-eslint/parser",
+		});
+		const filesToLint = ["fileWithExceptionImports.ts", "exceptionFile.ts"].map((file) =>
+			path.join(__dirname, "mockFiles", file),
+		);
+		const results = await eslint.lintFiles(filesToLint);
+		const result = results[0];
+
+		assert.strictEqual(result.errorCount, 0, "Should have no errors");
+	});
+	it("Should report an error for tsconfig provided config", async function () {
+		const eslint = createESLintInstance({
+			rules: {
+				"no-restricted-tags-imports": [
+					"error",
+					{
+						tags: ["@internal", "@alpha"],
+						exceptions: {
+							"@alpha": ["./exceptionFile.ts"],
+							"@internal": ["./exceptionFile.ts"],
+						},
+					},
+				],
+			},
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				project: path.join(__dirname, "./tsconfig.json"),
+			},
+		});
+		const filesToLint = ["fileWithImports.ts", "mockModule.ts"].map((file) =>
+			path.join(__dirname, "mockFiles", file),
+		);
+		const results = await eslint.lintFiles(filesToLint);
+		const result = results[0];
+
+		assert.strictEqual(result.errorCount, 2, "Should have two errors");
+		assert.strictEqual(
+			result.messages[0].message,
+			"Importing @internal tagged items is not allowed: internalFunction",
+		);
+	});
 });
-
-const fileWithPublicFunction = `
-/**
- * @public
- */
-export function publicFunction() {}`;
-
-const fileWithPublicImport = `
-import { publicFunction } from "./someModule";
-`;
-
-const fileWithInternalImportException = `
-import { internalExceptionFunction } from "./foo";
-`;
-
-const fileWithInternalTaggedFunction = `
-/**
- * @internal
- */
-export function internalFunction() {}
-`;
-
-const fileWithAlphaTaggedFunction = `
-/**
- * @alpha
- */
-export function alphaFunction() {}
-`;
-
-const fileWithInternalImport = `
-import { internalFunction } from "./internalModule";
-`;
-
-const fileWithAlphaImport = `
-import { AlphaFunction } from "./foo";
-`;
-
-ruleTester.run("no-restricted-tags-imports", noRestrictedTagsImports, {
-	// Checks cases that should pass
-	valid: [
-		// Importing something with a non-restricted tag
-		{
-			code: fileWithPublicImport + fileWithPublicFunction,
-			options: [
-				{
-					tags: ["@internal", "@alpha"],
-				},
-			],
-		},
-		// Restricted tag imported from exception
-		{
-			code: fileWithInternalImportException + fileWithInternalTaggedFunction,
-			options: [
-				{
-					tags: ["@internal", "@alpha"],
-					exceptions: { "@internal": ["./foo"] },
-				},
-			],
-		},
-	],
-	// Checks cases that should not pass
-	invalid: [
-		// Import with restricted tag
-		{
-			code: fileWithInternalTaggedFunction + fileWithInternalImport,
-			options: [
-				{
-					tags: ["@internal", "@alpha"],
-				},
-			],
-			errors: 1,
-		},
-		// Invalid: tags should start with '@'
-		{
-			code: fileWithPublicFunction + fileWithPublicImport,
-			options: [
-				{
-					tags: ["internal", "alpha"],
-					exceptions: { "@internal": ["./foo"] },
-				},
-			],
-			errors: 2,
-		},
-		// Invalid: Tag isn't excepted
-		{
-			code: fileWithAlphaTaggedFunction + fileWithAlphaImport,
-			options: [
-				{
-					tags: ["@internal", "@alpha"],
-					exceptions: { "@internal": ["./vfoo"] },
-				},
-			],
-			errors: 1,
-		},
-		// Invalid: Tag excepted but wrong filepath
-		{
-			code: fileWithAlphaTaggedFunction + fileWithAlphaImport,
-			options: [
-				{
-					tags: ["@internal", "@alpha"],
-					exceptions: { "@alpha": ["./bar"] },
-				},
-			],
-			errors: 1,
-		},
-	],
-});
-
-console.log("All tests passed!");
