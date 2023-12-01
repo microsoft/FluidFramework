9329aacef24a256c29474808f33c7903754c7fe2
Tony Murphy <anthonm@microsoft.com>
2023-12-01 12:01:40 -0800

    fix import

--- packages/service-clients/end-to-end-tests/odsp-client/src/test/OdspClientFactory.ts
+++ packages/service-clients/end-to-end-tests/odsp-client/src/test/OdspClientFactory.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-import { type ITelemetryBaseLogger } from "@fluidframework/common-definitions";
+import { type ITelemetryBaseLogger } from "@fluidframework/core-interfaces";
 import { OdspClient, OdspConnectionConfig } from "@fluid-experimental/odsp-client";
 
 import {
