25ba1de448dff3245a0c971037e49cdff1eb8282
Jatin Garg <jatingarg@Jatins-MacBook-Pro-2.local>
2023-12-04 16:09:08 -0800

    test

--- packages/test/snapshots/src/test/replay.spec.ts
+++ packages/test/snapshots/src/test/replay.spec.ts
@@ -6,7 +6,7 @@
 import { Mode, processContent } from "../replayMultipleFiles";
 import { getTestContent, skipOrFailIfTestContentMissing } from "../testContent";
 
-describe.skip("Snapshots", function () {
+describe("Snapshots", function () {
 	this.timeout(300000);
 
 	const contentLocation = getTestContent("snapshotTestContent");
