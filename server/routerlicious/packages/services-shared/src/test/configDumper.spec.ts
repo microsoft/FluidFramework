65f77f0410fc2660e50f29ca2e4297d829938e6d
Dhruv Verma <dhruvverma@microsoft.com>
2023-12-06 09:12:38 -0800

    Added copyright info

--- server/routerlicious/packages/services-shared/src/test/configDumper.spec.ts
+++ server/routerlicious/packages/services-shared/src/test/configDumper.spec.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import assert from "assert";
 import { ConfigDumper } from "../configDumper";
 
