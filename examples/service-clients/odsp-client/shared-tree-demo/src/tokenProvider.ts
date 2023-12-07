b23b19b19170ca91847c39f588363c3054a7d670
Sonali Deshpande <48232592+sonalideshpandemsft@users.noreply.github.com>
2023-12-07 21:46:40 +0000

    wip

--- examples/service-clients/odsp-client/shared-tree-demo/src/tokenProvider.ts
+++ examples/service-clients/odsp-client/shared-tree-demo/src/tokenProvider.ts
@@ -10,7 +10,6 @@ import {
 } from "@azure/msal-browser";
 import { IOdspTokenProvider } from "@fluid-experimental/odsp-client";
 import { TokenResponse } from "@fluidframework/odsp-driver-definitions";
-import { WEBSOCKET_TOKEN, STORAGE_TOKEN } from "./clientProps";
 
 export async function fetchTokens(
 	siteUrl: string,
@@ -83,16 +82,86 @@ export class OdspTestTokenProvider implements IOdspTokenProvider {
 	constructor() {}
 
 	public async fetchWebsocketToken(siteUrl: string, refresh: boolean): Promise<TokenResponse> {
+		const token = await this.fetchTokens(siteUrl, "");
 		return {
 			fromCache: true,
-			token: localStorage.get(WEBSOCKET_TOKEN),
+			token: token.pushToken,
 		};
 	}
 
 	public async fetchStorageToken(siteUrl: string, refresh: boolean): Promise<TokenResponse> {
+		const token = await this.fetchTokens(siteUrl, "");
 		return {
 			fromCache: true,
-			token: localStorage.get(STORAGE_TOKEN),
+			token: token.storageToken,
 		};
+	}
+
+	private async fetchTokens(
+		siteUrl: string,
+		clientId: string,
+	): Promise<{ storageToken: string; pushToken: string }> {
+		const msalConfig = {
+			auth: {
+				clientId,
+				authority: "https://login.microsoftonline.com/common/",
+			},
+		};
+
+		const graphScopes = ["FileStorageContainer.Selected"];
+
+		const msalInstance = new PublicClientApplication(msalConfig);
+		const pushScope = ["offline_access https://pushchannel.1drv.ms/PushChannel.ReadWrite.All"];
+		const storageScope = [`${siteUrl}/Container.Selected`];
+		const response = await msalInstance.loginPopup({ scopes: graphScopes });
+
+		msalInstance.setActiveAccount(response.account);
+
+		try {
+			// Attempt to acquire token silently
+			const storageRequest = {
+				scopes: storageScope,
+			};
+			const storageResult: AuthenticationResult =
+				await msalInstance.acquireTokenSilent(storageRequest);
+
+			const pushRequest = {
+				scopes: pushScope,
+			};
+			const pushResult: AuthenticationResult =
+				await msalInstance.acquireTokenSilent(pushRequest);
+
+			// Return token
+			return {
+				storageToken: storageResult.accessToken,
+				pushToken: pushResult.accessToken,
+			};
+		} catch (error) {
+			if (error instanceof InteractionRequiredAuthError) {
+				msalInstance.setActiveAccount(null);
+				// If silent token acquisition fails, fall back to interactive flow
+				const storageRequest = {
+					scopes: storageScope,
+				};
+				const storageResult: AuthenticationResult =
+					await msalInstance.acquireTokenPopup(storageRequest);
+
+				const pushRequest = {
+					scopes: pushScope,
+				};
+				const pushResult: AuthenticationResult =
+					await msalInstance.acquireTokenSilent(pushRequest);
+
+				// Return token
+				return {
+					storageToken: storageResult.accessToken,
+					pushToken: pushResult.accessToken,
+				};
+			} else {
+				// Handle any other error
+				console.error(error);
+				throw error;
+			}
+		}
 	}
 }
