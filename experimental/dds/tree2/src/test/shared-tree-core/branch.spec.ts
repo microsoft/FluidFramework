e11c587ffa3856d6edb655f8b46c8ed41d2b5eb8
Yann Achard <achardy@microsoft.com>
2023-12-06 20:12:11 -0800

    fix(tree2): revertibles work after rebasing branch

--- experimental/dds/tree2/src/test/shared-tree-core/branch.spec.ts
+++ experimental/dds/tree2/src/test/shared-tree-core/branch.spec.ts
@@ -22,6 +22,7 @@ import {
 } from "../../feature-libraries";
 import { brand, fail } from "../../util";
 import { noopValidator } from "../../codec";
+import { createTestUndoRedoStacks } from "../utils";
 
 const defaultChangeFamily = new DefaultChangeFamily({ jsonValidator: noopValidator });
 
@@ -157,6 +158,7 @@ describe("Branches", () => {
 		// Create a parent branch and a child fork
 		const parent = create();
 		const child = parent.fork();
+		const stacks = createTestUndoRedoStacks(child);
 		// Apply a change to the parent
 		const tagParent = change(parent);
 		// Apply a change to the child
@@ -170,6 +172,15 @@ describe("Branches", () => {
 		child.rebaseOnto(parent);
 		assertBased(child, parent);
 		assertHistory(child, tagParent, tagChild, tagParent2, tagChild2);
+
+		// It should still be possible to revert the the child branch's revertibles
+		assert.equal(stacks.undoStack.length, 2);
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		stacks.undoStack.pop()!.revert();
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		stacks.undoStack.pop()!.revert();
+
+		stacks.unsubscribe();
 	});
 
 	it("emit a change event after each change", () => {
