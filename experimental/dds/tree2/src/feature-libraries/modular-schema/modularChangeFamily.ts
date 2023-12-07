16339e04343038ac2b221bab83bc35c4a5c8f290
Yann Achard <achardy@microsoft.com>
2023-12-07 12:02:12 -0800

    Fixes and refactor

--- experimental/dds/tree2/src/feature-libraries/modular-schema/modularChangeFamily.ts
+++ experimental/dds/tree2/src/feature-libraries/modular-schema/modularChangeFamily.ts
@@ -843,6 +843,9 @@ function deltaFromNodeChange(
  * For example, when rebasing change B from a local branch [A, B, C] over a branch [X, Y], the `revInfos` must include
  * the changes [A⁻¹ X, Y, A'] for each rebase step of B.
  * @param baseRevisions - The set of revisions in the changeset being rebased over.
+ * For example, when rebasing change B from a local branch [A, B, C] over a branch [X, Y], the `baseRevisions` must include
+ * revisions [A⁻¹ X, Y, A'] if rebasing over the composition of all those changes, or
+ * revision [A⁻¹] for the first rebase, then [X], etc. if rebasing over edits individually.
  * @returns - RebaseRevisionMetadata to be passed to `FieldChangeRebaser.rebase`*
  */
 export function rebaseRevisionMetadataFromInfo(
