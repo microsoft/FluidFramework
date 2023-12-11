b72d7a1b413f06eb95fca26e1cba98369bf87586
Zach Newton <znewton@microsoft.com>
2023-12-11 15:04:25 -0800

    Make tests work for base case

--- server/gitrest/packages/gitrest-base/src/test/utils.ts
+++ server/gitrest/packages/gitrest-base/src/test/utils.ts
@@ -68,3 +68,32 @@ export function initializeBeforeAfterTestHooks(provider: nconf.Provider) {
 		return rimraf(storageDirConfig.baseDir);
 	});
 }
+
+export function convertAllUtf8ToBase64<T>(obj: Record<string, any>): T {
+	return JSON.parse(
+		JSON.stringify(obj, (key, value: any): any => {
+			// console.log(key, value);
+			if (
+				typeof value === "object" &&
+				value !== null &&
+				value.encoding === "utf-8" &&
+				value.content
+			) {
+				const originalValue = value;
+				const newValue = {
+					content: Buffer.from(value.content, "utf-8").toString("base64"),
+					encoding: "base64",
+					size: undefined,
+				};
+				if (originalValue.size !== undefined) {
+					newValue.size = newValue.content.length;
+				}
+				return {
+					...originalValue,
+					...newValue,
+				};
+			}
+			return value;
+		}),
+	) as unknown as T;
+}
