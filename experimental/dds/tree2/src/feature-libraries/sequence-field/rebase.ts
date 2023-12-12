f47160c3322a2b1e1bece2eea0a6712709b534a1
Alexander Pardes <apardes@microsoft.com>
2023-12-12 09:19:34 -0800

    Remove stale comment

--- experimental/dds/tree2/src/feature-libraries/sequence-field/rebase.ts
+++ experimental/dds/tree2/src/feature-libraries/sequence-field/rebase.ts
@@ -152,8 +152,6 @@ function rebaseMarkList<TNodeChange>(
 			nodeExistenceState,
 		);
 
-		// Inverse attaches do not contribute to lineage as they are effectively reinstating
-		// an older detach which cells should already have any necessary lineage for.
 		if (markEmptiesCells(baseMark) || isImpactfulCellRename(baseMark, baseRevision, metadata)) {
 			// Note that we want the revision in the detach ID to be the actual revision, not the intention.
 			// We don't pass a `RevisionMetadataSource` to `getOutputCellId` so that we get the true revision.
