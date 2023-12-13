a0bdd3693bada439a4689f5ade92a93c8c93c606
Yann Achard <achardy@microsoft.com>
2023-12-13 10:26:54 -0800

    refactor(tree2): rename and clarify purpose of detachIdOverride

--- experimental/dds/tree2/src/test/feature-libraries/sequence-field/invert.spec.ts
+++ experimental/dds/tree2/src/test/feature-libraries/sequence-field/invert.spec.ts
@@ -92,9 +92,9 @@ describe("SequenceField - Invert", () => {
 	});
 
 	it("delete => revive (with override ID)", () => {
-		const detachIdOverride: ChangeAtomId = { revision: tag2, localId: brand(0) };
-		const input: TestChangeset = [Mark.delete(2, brand(5), { detachIdOverride })];
-		const expected = [Mark.revive(2, detachIdOverride, { id: brand(5) })];
+		const redetachId: ChangeAtomId = { revision: tag2, localId: brand(0) };
+		const input: TestChangeset = [Mark.delete(2, brand(5), { redetachId })];
+		const expected = [Mark.revive(2, redetachId, { id: brand(5) })];
 		const actual = invert(input);
 		assert.deepEqual(actual, expected);
 	});
@@ -102,7 +102,7 @@ describe("SequenceField - Invert", () => {
 	it("active revive => delete", () => {
 		const cellId: ChangeAtomId = { revision: tag1, localId: brand(0) };
 		const input = Change.revive(0, 2, cellId);
-		const expected: TestChangeset = [Mark.delete(2, brand(0), { detachIdOverride: cellId })];
+		const expected: TestChangeset = [Mark.delete(2, brand(0), { redetachId: cellId })];
 		const actual = invert(input);
 		assert.deepEqual(actual, expected);
 	});
@@ -154,11 +154,11 @@ describe("SequenceField - Invert", () => {
 			Mark.returnTo(2, brand(42), { revision: tag1, localId: brand(42) }),
 			{ count: 3 },
 			Mark.moveOut(1, brand(42), {
-				detachIdOverride: cellId,
+				redetachId: cellId,
 				changes: inverseChildChange1,
 			}),
 			Mark.moveOut(1, brand(43), {
-				detachIdOverride: { ...cellId, localId: brand(1) },
+				redetachId: { ...cellId, localId: brand(1) },
 			}),
 		];
 		const actual = invert(input);
@@ -177,7 +177,7 @@ describe("SequenceField - Invert", () => {
 		const input = [Mark.pin(1, brand(0), { cellId, changes: childChange1 })];
 		const expected: TestChangeset = [
 			Mark.delete(1, brand(0), {
-				detachIdOverride: cellId,
+				redetachId: cellId,
 				changes: inverseChildChange1,
 			}),
 		];
@@ -218,7 +218,7 @@ describe("SequenceField - Invert", () => {
 			Mark.delete(1, detachId.localId, {
 				cellId: detachId,
 				changes: inverseChildChange1,
-				detachIdOverride: startId,
+				redetachId: startId,
 			}),
 		];
 		assert.deepEqual(inverse, expected);
@@ -263,7 +263,7 @@ describe("SequenceField - Invert", () => {
 			Mark.attachAndDetach(
 				Mark.returnTo(1, detachId.localId, detachId),
 				Mark.delete(1, detachId.localId, {
-					detachIdOverride: startId,
+					redetachId: startId,
 				}),
 			),
 			{ count: 1 },
@@ -365,7 +365,7 @@ describe("SequenceField - Invert", () => {
 				Mark.delete(1, brand(0), {
 					changes: inverseChildChange1,
 					cellId: endId,
-					detachIdOverride: startId,
+					redetachId: startId,
 				}),
 			];
 			assert.deepEqual(actual, expected);
