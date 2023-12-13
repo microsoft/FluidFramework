e7c33b5a6a1d9bb9bc24d959ba74003acbd68fc0
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2023-12-13 00:42:25 +0000

    fix policy

--- common/build/eslint-config-fluid/src/custom-rules/no-release-tags.js
+++ common/build/eslint-config-fluid/src/custom-rules/no-release-tags.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 const { TSDocParser } = require("@microsoft/tsdoc");
 
 function hasReleaseTag(comment) {
