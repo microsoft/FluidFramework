e039008ce226b79c69ea4987938e9678f71fdf78
Taylor Williams <60717813+taylorsw04@users.noreply.github.com>
2023-12-15 20:48:21 +0000

    Make commit eviction policy slightly less aggressive

--- experimental/dds/tree2/src/test/shared-tree-core/sharedTreeCore.spec.ts
+++ experimental/dds/tree2/src/test/shared-tree-core/sharedTreeCore.spec.ts
@@ -200,6 +200,8 @@ describe("SharedTreeCore", () => {
 		// Calling `factory.processAllMessages()` will result in the minimum sequence number being set to the the
 		// sequence number just before the most recently received changed. Thus, eviction from this point of view
 		// is "off by one"; a commit is only evicted once another commit is sequenced after it.
+		// Eviction is performed up to the trunk commit that no branch has as its trunk base.
+		// Additionally, by policy, the base commit of the trunk is never evicted, which adds another "off by one".
 		//
 		//                                            trunk: [seqNum1, (branchBaseA, branchBaseB, ...), seqNum2, ...]
 		changeTree(tree);
@@ -212,25 +214,25 @@ describe("SharedTreeCore", () => {
 		factory.processAllMessages(); //                     [x (b1, b2, b3), 2]
 		changeTree(tree);
 		factory.processAllMessages(); //                     [x (b1, b2, b3), 2, 3]
-		assert.equal(getTrunkLength(tree), 2);
+		assert.equal(getTrunkLength(tree), 3);
 		branch1.dispose(); //                                [x (b2, b3), 2, 3]
-		assert.equal(getTrunkLength(tree), 2);
+		assert.equal(getTrunkLength(tree), 3);
 		branch2.dispose(); //                                [x (b3), 2, 3]
-		assert.equal(getTrunkLength(tree), 2);
+		assert.equal(getTrunkLength(tree), 3);
 		branch3.dispose(); //                                [x, x, 3]
 		assert.equal(getTrunkLength(tree), 1);
 		const branch4 = tree.getLocalBranch().fork(); //     [x, x, 3 (b4)]
 		changeTree(tree);
 		changeTree(tree);
 		factory.processAllMessages(); //                     [x, x, x (b4), 4, 5]
-		assert.equal(getTrunkLength(tree), 2);
+		assert.equal(getTrunkLength(tree), 3);
 		const branch5 = tree.getLocalBranch().fork(); //     [x, x, x (b4), 4, 5 (b5)]
 		branch4.rebaseOnto(branch5); //                      [x, x, x, 4, 5 (b4, b5)]
 		branch4.dispose(); //                                [x, x, x, 4, 5 (b5)]
 		assert.equal(getTrunkLength(tree), 2);
 		changeTree(tree);
 		factory.processAllMessages(); //                     [x, x, x, x, 5 (b5), 6]
-		assert.equal(getTrunkLength(tree), 1);
+		assert.equal(getTrunkLength(tree), 2);
 		changeTree(tree);
 		branch5.dispose(); //                                [x, x, x, x, x, x, 7]
 		assert.equal(getTrunkLength(tree), 1);
