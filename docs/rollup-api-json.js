d65ee02ab23e06f5efd5337c49fd927822fca7e9
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2023-12-15 23:01:16 +0000

    build: Simplify api docs rollup stage

--- docs/rollup-api-json.js
+++ docs/rollup-api-json.js
@@ -17,47 +17,20 @@
 
 const cpy = require("cpy");
 const fs = require("fs-extra");
-const path = require("path");
-const replace = require("replace-in-file");
 
 /**
  * This purpose of this method is to transfer the api json content from originalPath to targetPath.
  * Currently, the paths are configured such that originalPath is the _api-extractor-temp-{version}-doc-models
- * directory in the root of the repo (where the content either downloaded or generated with api-extractor). 
- * targetPath is currently configured to be docs/_api-extractor-temp/{version}/(_build or _staging) 
+ * directory in the root of the repo (where the content either downloaded or generated with api-extractor).
+ * targetPath is currently configured to be docs/_api-extractor-temp/{version}/(_build or _staging)
  */
 const main = async (originalPath, targetPath) => {
-	const stagingPath = path.join(targetPath, "_staging");
-	const outputPath = path.join(targetPath, "_build");
-
 	// Clear output folders.
-	await fs.emptyDir(stagingPath);
-	await fs.emptyDir(outputPath);
-
-	const apiExtractorInputDir = originalPath;
+	await fs.emptyDir(targetPath);
 
 	// Copy all the files to staging that need to be present for member processing.
-	await cpy(apiExtractorInputDir, stagingPath);
-
-	// Rewrite any remaining references in the output files using replace-in-files
-	const from = [];
-	const to = [];
-
-	try {
-		const options = {
-			files: `${path.resolve(stagingPath)}/**`,
-			from: from,
-			to: to,
-		};
-
-		await replace(options);
-	} catch (error) {
-		console.error("Error occurred:", error);
-	}
-
-	// Copy all processed files that should be published on the site to the output dir.
-	console.log(`Copying final files from ${stagingPath} to ${outputPath}`);
-	await cpy(stagingPath, outputPath).on("progress", (progress) => {
+	console.log(`Copying final files from ${originalPath} to ${targetPath}`);
+	await cpy(originalPath, targetPath).on("progress", (progress) => {
 		if (progress.percent === 1) {
 			console.log(`\tCopied ${progress.totalFiles} files.`);
 		}
