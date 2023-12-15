090301cfd8a4cf6da0a64648be74978164a78972
Michael Zhen <112977307+zhenmichael@users.noreply.github.com>
2023-12-15 20:20:03 +0000

    update download-apis

--- docs/download-apis.js
+++ docs/download-apis.js
@@ -1,3 +1,16 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * This download script is used in docs/package.json to download the api json docs from the azure storage.
+ * This script accepts an optional boolean parameter which controls if all versions or only the latest version 
+ * of the doc models get downloaded. (pass in true for all versions)
+ * e.g. "node ./download-apis.js true"
+ */
+
+const chalk = require("chalk");
 const download = require("download");
 const versions = require("./data/versions.json");
 
@@ -11,18 +24,21 @@ const downloadConfigs = [];
 
 docVersions.forEach((version) => {
 	version = version === versions.params.currentVersion ? "" : "-" + version;
-	const downloadConfig = {
-		url: `https://fluidframework.blob.core.windows.net/api-extractor-json/latest${version}.tar.gz`,
-		destination: `../_api-extractor-temp${version}/doc-models/`,
-	};
-	downloadConfigs.push(downloadConfig);
-	console.log("DOWNLOAD CONFIGS", downloadConfigs);
+	const url = `https://fluidframework.blob.core.windows.net/api-extractor-json/latest${version}.tar.gz`;
+	const destination = `../_api-extractor-temp${version}/doc-models/`;
+
+	downloadConfigs.push(download(url, destination, { extract: true }));
 });
 
-(async () => {
-	await Promise.all(
-		downloadConfigs.map((config) => {
-			download(config.url, config.destination, { extract: true });
-		}),
-	);
-})();
+Promise.all(
+	downloadConfigs
+).then(
+	() => {
+		console.log(chalk.green("API doc models downloaded!"));
+		process.exit(0);
+	},
+	(error) => {
+		console.error(error);
+		process.exit(1);
+	},
+);
