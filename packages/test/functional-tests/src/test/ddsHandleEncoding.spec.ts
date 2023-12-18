fdb21572df21da89c8f693e7b5c00eb1fffb9c71
Mark Fields <markfields@users.noreply.github.com>
2023-12-18 17:37:52 +0000

    Empty test file

---
+++ packages/test/functional-tests/src/test/ddsHandleEncoding.spec.ts
@@ -0,0 +1,18 @@
+// /*!
+//  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+//  * Licensed under the MIT License.
+//  */
+
+// /**
+//  * The purpose of these tests is to demonstrate that DDSes do not do opaque encoding of handles
+//  * when preparing the op payload (e.g. prematurely serializing).
+//  * This is important because the runtime needs to inspect the full op payload for handles.
+//  */
+// describe("DDS Handle Encoding", () => {
+// 	describe("SharedMap", () => {
+// 		it("Adding a handle", async () => {
+// 			const factory = new ();
+// 			return factory.create(new MockFluidDataStoreRuntime(), id);
+// 		});
+// 	}
+// });
