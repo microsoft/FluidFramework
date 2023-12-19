f03798af853bb5bd05d2eb5e603053044b7886c1
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2023-12-19 14:55:12 -0800

    Make brand required, improve docs and tests

--- packages/dds/tree/src/simple-tree/types.ts
+++ packages/dds/tree/src/simple-tree/types.ts
@@ -38,14 +38,11 @@ export type Unhydrated<T> = T;
  * @remarks
  * Base type which all nodes implement.
  *
- * This can be used as a type to indicate/document values which should be tree nodes,
- * but currently does not provide strong type checking for this.
- * Additionally runtime use of this class object (for example when used with `instanceof` or subclassed), is not supported:
+ * This can be used as a type to indicate/document values which should be tree nodes.
+ * Runtime use of this class object (for example when used with `instanceof` or subclassed), is not supported:
  * it may be replaced with an interface or union in the future.
  * @privateRemarks
- * Adding a member which all nodes have, like a type symbol, would produce much stronger typing for this.
- * This is only a class to enable stronger typing in a future change,
- * but other future changes may want to replace it with a branded interface if the runtime oddities related to this are not cleaned up.
+ * Future changes may replace this with a branded interface if the runtime oddities related to this are not cleaned up.
  *
  * Currently not all node implications include this in their prototype chain (some hide it with a proxy), and thus cause `instanceof` to fail.
  * This results in the runtime and compile time behavior of `instanceof` differing.
@@ -55,7 +52,23 @@ export type Unhydrated<T> = T;
  * @beta
  */
 export abstract class TreeNode {
-	readonly #brand?: unknown;
+	/**
+	 * This is added to prevent TypeScript from implicitly allowing non-TreeNode types to be used as TreeNodes.
+	 * @privateRemarks
+	 * This is a JavaScript private field, so is not accessible from outside this class.
+	 * This prevents it from having name collisions with object fields.
+	 * Since this is private, the type of this field is stripped in the d.ts file.
+	 * To get matching type checking within and from outside the package, the least informative type (`unknown`) is used.
+	 * To avoid this having any runtime impact, the field is uninitialized.
+	 *
+	 * Making this field optional results in different type checking within this project than outside of it, since the d.ts file drops the optional aspect of the field.
+	 * This is extra confusing since sin ce the tests get in-project typing for intellisense and separate project checking at build time.
+	 * To avoid all this mess, this field is required, not optional.
+	 *
+	 * Another option would be to use a symbol (possibly as a private field).
+	 * That approach ran into some strange difficulties causing SchemaFactory to fail to compile, and was not investigated further.
+	 */
+	readonly #brand!: unknown;
 }
 
 /**
