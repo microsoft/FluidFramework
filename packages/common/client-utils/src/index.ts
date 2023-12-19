a92ab168e7d0a4cdbd0274eeb10c8d326ba880ca
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-12-19 15:43:11 -0600

    Cleanup comment

--- packages/common/client-utils/src/index.ts
+++ packages/common/client-utils/src/index.ts
@@ -6,7 +6,5 @@
 // THIS FILE IS NOT ACTUALLY USED (see indexBrowser.ts and indexNode.ts for that).
 // It's only here so type-test-generator doesn't fail, until we update it to support packages that don't have an
 // index.ts file.
-// If we remove this file, we need to update api-extractor-lint.json and api-extractor.json so they override
-// "mainEntryPointFilePath": "<projectFolder>/dist/indexNode.d.ts" (or wherever the main type declarations file ends up).
 // eslint-disable-next-line no-restricted-syntax
 export * from "./indexNode";
