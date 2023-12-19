e5d9885b9d33773edebd1025bbd09eb8e45b5879
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-12-19 20:55:28 +0000

    Reexport

--- packages/common/client-utils/src/index.ts
+++ packages/common/client-utils/src/index.ts
@@ -6,20 +6,5 @@
 // THIS FILE IS NOT ACTUALLY USED (see indexBrowser.ts and indexNode.ts for that).
 // It's only here so type-test-generator doesn't fail, until we update it to support packages that don't have an
 // index.ts file.
-
-export {
-	bufferToString,
-	Buffer,
-	IsoBuffer,
-	stringToBuffer,
-	Uint8ArrayToString,
-} from "./bufferNode";
-export { gitHashFile, hashFile } from "./hashFileNode";
-export { performance } from "./performanceIsomorphic";
-
-export { fromBase64ToUtf8, fromUtf8ToBase64, toUtf8 } from "./base64Encoding";
-export { Uint8ArrayToArrayBuffer } from "./bufferShared";
-export { EventForwarder } from "./eventForwarder";
-export { IsomorphicPerformance } from "./performanceIsomorphic";
-export { ITraceEvent, Trace } from "./trace";
-export { EventEmitterEventType, TypedEventEmitter, TypedEventTransform } from "./typedEventEmitter";
+// eslint-disable-next-line no-restricted-syntax
+export * from "./indexNode";
