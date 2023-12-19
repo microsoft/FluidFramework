b8691a9a55b50ad7df790a0c132c1d35644eae2e
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2023-12-19 14:06:25 -0800

    Simplify snapshot testing

--- packages/dds/tree/src/test/snapshots/schema.spec.ts
+++ packages/dds/tree/src/test/snapshots/schema.spec.ts
@@ -3,38 +3,17 @@
  * Licensed under the MIT License.
  */
 
-import path from "path";
 import { encodeTreeSchema, intoStoredSchema } from "../../feature-libraries";
-import { testTrees as schemaTestTrees } from "../testTrees";
-import {
-	createSchemaSnapshot,
-	dirPathTail,
-	regenTestDirectory,
-	regenerateSnapshots,
-	verifyEqualPastSchemaSnapshot,
-} from "./utils";
-
-const schemaDirPath = path.join(__dirname, `../../../${dirPathTail}/schema-files`);
-
-function getSchemaFilepath(name: string): string {
-	return path.join(schemaDirPath, `${name}.json`);
-}
+import { testTrees } from "../testTrees";
+import { takeJsonSnapshot, useTestDirectory } from "./snapshotTools";
 
 describe("schema snapshots", () => {
-	if (regenerateSnapshots) {
-		regenTestDirectory(schemaDirPath);
-	}
+	useTestDirectory("schema-files");
 
-	for (const { name, schemaData } of schemaTestTrees) {
-		it(`${regenerateSnapshots ? "regenerate " : ""}for ${name}`, async () => {
+	for (const { name, schemaData } of testTrees) {
+		it(name, () => {
 			const encoded = encodeTreeSchema(intoStoredSchema(schemaData));
-
-			// eslint-disable-next-line unicorn/prefer-ternary
-			if (regenerateSnapshots) {
-				await createSchemaSnapshot(getSchemaFilepath(name), encoded);
-			} else {
-				await verifyEqualPastSchemaSnapshot(getSchemaFilepath(name), encoded, name);
-			}
+			takeJsonSnapshot(encoded);
 		});
 	}
 });
