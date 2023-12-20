d8dd414bcf025c475bb9feceeae9edb30a2f1bd1
Mark Fields <markfields@users.noreply.github.com>
2023-12-20 13:17:40 +0000

    Switch to generic test case infra to support multiple DDS types

--- packages/test/functional-tests/src/test/ddsHandleEncoding.spec.ts
+++ packages/test/functional-tests/src/test/ddsHandleEncoding.spec.ts
@@ -21,9 +21,13 @@ import {
 	// MapFactory,
 	DirectoryFactory,
 	IDirectory,
-	// IDirectory,
-	// ISharedMap,
+	// ISharedDirectory,
 } from "@fluidframework/map";
+import {
+	IChannel,
+	IChannelFactory,
+	// IFluidDataStoreRuntime,
+} from "@fluidframework/datastore-definitions";
 
 /**
  * The purpose of these tests is to demonstrate that DDSes do not do opaque encoding of handles
@@ -77,16 +81,88 @@ describe("DDS Handle Encoding", () => {
 		return directory;
 	}
 
-	describe("SharedMap", () => {
-		it("should not obscure handles in message contents", async () => {
-			const messages: any[] = [];
-			const directory: IDirectory = createDirectoryWithSubmitCallback(
-				"directory",
-				(message) => {
+	describe("Generic test pattern", () => {
+		const messages: any[] = [];
+
+		beforeEach(() => {
+			messages.length = 0;
+		});
+
+		//* Finalize name and add comment
+		function createAndDoStuff<T extends IChannel>(
+			id: string,
+			factory: IFactory<T>,
+			doStuff: (dds: T) => void,
+		) {
+			const dataStoreRuntime = new MockFluidDataStoreRuntime();
+			const deltaConnection = new MockDeltaConnection(
+				/* submitFn: */ (message) => {
 					messages.push(message);
 					return 0; // unused
 				},
+				/* dirtyFn: */ () => {},
 			);
+			const services = {
+				deltaConnection,
+				objectStorage: new MockStorage(),
+			};
+			const dds = factory.create(dataStoreRuntime, id);
+			dds.connect(services);
+
+			doStuff(dds);
+		}
+
+		interface IFactory<T extends IChannel> {
+			create: (...args: Parameters<IChannelFactory["create"]>) => T;
+		}
+
+		interface ITestCase {
+			name: string;
+			createAndDoStuff(id: string): void;
+			expectHandlesDetected: boolean;
+		}
+
+		const testCases: ITestCase[] = [
+			{
+				name: "SharedDirectory",
+				createAndDoStuff: (id: string) => {
+					createAndDoStuff(id, new DirectoryFactory(), (dds: IDirectory) => {
+						dds.set("whatever", new MockHandle("whatever"));
+					});
+				},
+				expectHandlesDetected: true,
+			},
+		];
+
+		testCases.forEach((testCase) => {
+			it(`should not obscure handles in ${testCase.name} message contents`, async () => {
+				testCase.createAndDoStuff(testCase.name);
+
+				assert.equal(messages.length, 1, "Expected a single message to be submitted");
+				assert.equal(
+					detectHandleViaJsonStingify(messages[0]),
+					testCase.expectHandlesDetected,
+					`The handle ${
+						testCase.expectHandlesDetected ? "should" : "should not"
+					} be detected`,
+				);
+			});
+		});
+	});
+
+	//* SKIP
+	//* SKIP
+	//* SKIP
+	//* SKIP
+	//* SKIP
+	//* SKIP
+	describe.skip("SharedMap", () => {
+		it("should not obscure handles in message contents", async () => {
+			const messages: any[] = [];
+			const directory = createDirectoryWithSubmitCallback("directory", (message) => {
+				messages.push(message);
+				return 0; // unused
+			});
 			const handle = new MockHandle("whatever");
 			directory.set("map", handle);
 
