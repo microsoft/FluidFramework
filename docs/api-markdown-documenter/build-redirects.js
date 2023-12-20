4e0b98b51b77e60bcedd31787d5c367f833c118a
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2023-12-20 23:10:02 +0000

    fix redirect without wildcard support

--- docs/api-markdown-documenter/build-redirects.js
+++ docs/api-markdown-documenter/build-redirects.js
@@ -13,9 +13,9 @@ const configFileName = "staticwebapp.config.json";
 const config = require(`../static/${configFileName}`);
 
 const routes = {
-	"/docs/api/v1/{*}": `/docs/api/${currentVersion}/{*}`,
-	"/docs/api/current/{*}": `/docs/api/${currentVersion}/{*}`,
-	"/docs/api/lts/{*}": `/docs/api/${ltsVersion}/{*}`,
+	"/docs/apis": `/docs/api/${currentVersion}`,
+	// "/docs/api/current/*": `/docs/api/${currentVersion}/*`,
+	// "/docs/api/lts/*": `/docs/api/${ltsVersion}/*`,
 };
 
 /**
