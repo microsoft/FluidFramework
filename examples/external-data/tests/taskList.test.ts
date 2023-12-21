caa9242d28363b21cd95e076ddb9b7acbef72eb9
Abram Sanderson <absander@microsoft.com>
2023-12-21 12:52:50 -0800

    fix lint

--- examples/external-data/tests/taskList.test.ts
+++ examples/external-data/tests/taskList.test.ts
@@ -10,6 +10,7 @@ describe("taskList", () => {
 		// Wait for the page to load first before running any tests
 		// so this time isn't attributed to the first test
 		await page.goto(globals.PATH, { waitUntil: "load", timeout: 0 });
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/dot-notation
 		await page.waitForFunction(() => window["fluidStarted"]);
 	}, 45_000);
 	beforeEach(async () => {
