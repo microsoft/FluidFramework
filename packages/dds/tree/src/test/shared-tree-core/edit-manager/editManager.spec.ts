94c3bb7f0418d8a78719fa8d1186aee4fc7f9a25
Yann Achard <achardy@microsoft.com>
2024-01-02 10:21:37 -0800

    Revert changes to benchmark

--- packages/dds/tree/src/test/shared-tree-core/edit-manager/editManager.spec.ts
+++ packages/dds/tree/src/test/shared-tree-core/edit-manager/editManager.spec.ts
@@ -3,7 +3,6 @@
  * Licensed under the MIT License.
  */
 
-import { runBenchmark } from "./editManager.bench";
 import { testCodec } from "./editManagerCodecs.spec";
 import { testCorrectness } from "./editManagerCorrectness.spec";
 import { testPerf } from "./editManagerPerf.spec";
@@ -12,5 +11,4 @@ describe("EditManager", () => {
 	testCodec();
 	testCorrectness();
 	testPerf();
-	runBenchmark();
 });
