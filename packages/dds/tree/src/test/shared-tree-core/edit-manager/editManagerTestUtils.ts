c2c10be9a299ae6024c46713fa2f0a8f0e3129b6
Taylor Williams <60717813+taylorsw04@users.noreply.github.com>
2024-01-03 00:19:13 +0000

    fix lint

--- packages/dds/tree/src/test/shared-tree-core/edit-manager/editManagerTestUtils.ts
+++ packages/dds/tree/src/test/shared-tree-core/edit-manager/editManagerTestUtils.ts
@@ -3,8 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import { createIdCompressor } from "@fluidframework/id-compressor";
-import { SessionId } from "@fluidframework/id-compressor";
+import { createIdCompressor, SessionId } from "@fluidframework/id-compressor";
 import {
 	ChangeFamily,
 	ChangeRebaser,
