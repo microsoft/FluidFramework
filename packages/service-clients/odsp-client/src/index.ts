2d820acc133aafb91bad8ae67842a8a952f8740f
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-01-05 23:58:45 +0000

    docs(odsp-client): Add `packageDocumentation` and update README

--- packages/service-clients/odsp-client/src/index.ts
+++ packages/service-clients/odsp-client/src/index.ts
@@ -3,6 +3,16 @@
  * Licensed under the MIT License.
  */
 
+/**
+ * The odsp-client package provides a simple and powerful way to consume collaborative Fluid data with OneDrive/SharePoint (ODSP) storage.
+ *
+ * @remarks
+ * Please note that odsp-client is currently an experimental package.
+ * We'd love for you to try it out and provide feedback but it is not yet recommended/supported for production scnearios.
+ *
+ * @packageDocumentation
+ */
+
 export { type TokenResponse } from "@fluidframework/odsp-driver-definitions";
 export type {
 	OdspConnectionConfig,
