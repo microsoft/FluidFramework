789dcec4cd45711c4f0fbffef3721399580a2ab8
Mark Fields <markfields@users.noreply.github.com>
2024-01-10 15:16:11 +0000

    Only trigger auto-recovery on Load (not Changed)

--- packages/runtime/container-runtime/src/gc/garbageCollection.ts
+++ packages/runtime/container-runtime/src/gc/garbageCollection.ts
@@ -979,9 +979,11 @@ export class GarbageCollector implements IGarbageCollector {
 			headers: headerData,
 		});
 
-		// Any time we log a Tombstone error (via Telemetry Tracker),
+		// Any time we log a Tombstone Loaded error (via Telemetry Tracker),
 		// we want to also trigger autorecovery to avoid the object being deleted
-		if (isTombstoned) {
+		// Note: We don't need to trigger on "Changed" because any change will cause the object
+		// to be loaded by the Summarizer, and auto-recovery will be triggered then.
+		if (isTombstoned && reason === "Loaded") {
 			this.triggerAutoRecovery(nodePath);
 		}
 
