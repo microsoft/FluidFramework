eba692b0ad5ce6129f3859e5cd464e23eaab810a
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2024-01-11 01:14:37 +0000

    small code clean up

--- docs/api/fallback/index.js
+++ docs/api/fallback/index.js
@@ -15,24 +15,28 @@ const routes = {
 
 /**
  * Handles incoming HTTP requests and redirects them to the appropriate URL based on the current and LTS versions.
- * It reads the versions from data/versions.json and matches the incoming URL to a set of predefined routes.
+ * It reads the versions from /docs/data/versions.json and matches the incoming URL to a set of predefined routes.
  * If a matching route is found, it constructs and returns the redirect URL. Otherwise, it returns a 404 response.
  *
  * @param {object} context - The context object provided by the server framework, used here to set the response.
  * @param {object} req.headers - The headers of the HTTP request object.
  */
 module.exports = async (context, { headers }) => {
-	const parsedURL = new URL(headers["x-ms-original-url"], `http://${headers.host}`);
-	const route = findRoute(parsedURL, routes);
+	const { pathname, search } = new URL(headers["x-ms-original-url"], `http://${headers.host}`);
+	const route = findRoute(pathname, routes);
 
-	if (route === undefined) {
-		return context.res = { status: 404, headers: headers["x-ms-original-url"] };
-	}
-
-	context.res = { status: 302, headers: { location: getRedirectUrl(parsedURL, route) } };
+	context.res = {
+		status: route === undefined ? 404 : 302,
+		headers: {
+			location:
+				route === undefined
+					? headers["x-ms-original-url"]
+					: getRedirectUrl(pathname, search, route),
+		},
+	};
 };
 
-const findRoute = ({ pathname }, routes) =>
+const findRoute = (pathname) =>
 	Object.entries(routes).find(([path, _]) => new RegExp(path).test(pathname));
 
-const getRedirectUrl = ({ pathname, search }, route) => `${pathname.replace(...route)}${search}`;
+const getRedirectUrl = (pathname, search, route) => `${pathname.replace(...route)}${search}`;
