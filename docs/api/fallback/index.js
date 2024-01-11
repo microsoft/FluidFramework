99c4a6d6d3a23ac1f8df39df34b439507bd98d72
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2024-01-11 02:42:34 +0000

    Remove unused jsdocs parameters from jsdocs

--- docs/api/fallback/index.js
+++ docs/api/fallback/index.js
@@ -17,9 +17,6 @@ const routes = new Map([
  * Handles incoming HTTP requests and redirects them to the appropriate URL based on the current and LTS versions.
  * It reads the versions from /docs/data/versions.json and matches the incoming URL to a set of predefined routes.
  * If a matching route is found, it constructs and returns the redirect URL. Otherwise, it returns a 404 response.
- *
- * @param {object} context - The context object provided by the server framework, used here to set the response.
- * @param {object} req.headers - The headers of the HTTP request object.
  */
 module.exports = async (context, { headers }) => {
 	const { pathname, search } = new URL(headers["x-ms-original-url"], `http://${headers.host}`);
