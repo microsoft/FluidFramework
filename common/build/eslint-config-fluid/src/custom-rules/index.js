3815b1846e7b2fa3f23cd70e090d6ca24563f37c
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-01-12 23:03:00 +0000

    change version number / add comment / change name

--- common/build/eslint-config-fluid/src/custom-rules/index.js
+++ common/build/eslint-config-fluid/src/custom-rules/index.js
@@ -3,6 +3,10 @@
  * Licensed under the MIT License.
  */
 
+/**
+ * Root of the export of Fluid's custom ESLint rules {@link eslint-plugin-fluid-custom-rules}.
+ * Custom rules exported as a module which are then used as a dependency in {@link @fluidframework/eslint-config-fluid}.
+ */
 module.exports = {
 	rules: {
 		"no-member-release-tags": require("./rules/no-member-release-tags"),
