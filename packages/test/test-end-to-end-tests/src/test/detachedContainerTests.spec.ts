7f9994216f7bcdfbbe8ec575ed4d5a67fd37e5de
Tony Murphy <anthonm@microsoft.com>
2024-01-16 13:59:05 -0800

    fix imports

--- packages/test/test-end-to-end-tests/src/test/detachedContainerTests.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/detachedContainerTests.spec.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import { strict as assert, rejects } from "assert";
+import { strict as assert } from "assert";
 
 import { SharedCell } from "@fluidframework/cell";
 import { Deferred } from "@fluidframework/core-utils";
