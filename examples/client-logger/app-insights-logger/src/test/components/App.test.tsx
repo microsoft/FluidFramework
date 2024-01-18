2241b224861b8788ed6825481f2f99aa183d6b52
Michael Zhen <112977307+zhenmichael@users.noreply.github.com>
2024-01-18 19:12:03 +0000

    skip test with follow up comment

--- examples/client-logger/app-insights-logger/src/test/components/App.test.tsx
+++ examples/client-logger/app-insights-logger/src/test/components/App.test.tsx
@@ -7,9 +7,9 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import { App } from "../../components";
 
-// TODO: update ESM configuration and re-enable test
 describe("App Insights Example App UI test", () => {
-	xit("App renders", async (): Promise<void> => {
+	// TODO: update ESM configuration and re-enable test: ADO 7001
+	it.skip("App renders", async (): Promise<void> => {
 		render(<App />);
 		await screen.findByText("Loading Shared container...");
 	});
