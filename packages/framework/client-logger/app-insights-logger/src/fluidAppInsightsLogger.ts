779a1d10a05a528026efe10f6163404fad0835cb
Michael Zhen <112977307+zhenmichael@users.noreply.github.com>
2024-01-18 18:14:02 +0000

    run prettier

--- packages/framework/client-logger/app-insights-logger/src/fluidAppInsightsLogger.ts
+++ packages/framework/client-logger/app-insights-logger/src/fluidAppInsightsLogger.ts
@@ -133,11 +133,15 @@ class FluidAppInsightsLogger implements ITelemetryBaseLogger {
 	 */
 	private readonly baseLoggingClient: ApplicationInsights;
 	private readonly config: FluidAppInsightsLoggerConfig;
+
+	private cloneConfig(config: FluidAppInsightsLoggerConfig): FluidAppInsightsLoggerConfig {
+		return structuredClone(config) as FluidAppInsightsLoggerConfig;
+	}
 	public constructor(client: ApplicationInsights, config?: FluidAppInsightsLoggerConfig) {
 		this.baseLoggingClient = client;
 		// Deep copy config to prevent issues if user mutates the object they passed in
 		this.config = config
-			? structuredClone(config) as FluidAppInsightsLoggerConfig
+			? (structuredClone(config) as FluidAppInsightsLoggerConfig)
 			: {
 					filtering: {
 						mode: "exclusive",
