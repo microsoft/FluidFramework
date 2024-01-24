6a4a2bf3ae83f14fa1a08dd7d65b6bd93f39d2a9
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-01-24 00:08:26 +0000

    style: Fix linter violation

--- packages/framework/data-object-base/src/runtimeFactory.ts
+++ packages/framework/data-object-base/src/runtimeFactory.ts
@@ -30,7 +30,9 @@ export class RuntimeFactory extends RuntimeFactoryHelper {
 	constructor(props: {
 		defaultStoreFactory: IFluidDataStoreFactory;
 		storeFactories: IFluidDataStoreFactory[];
-		/** @deprecated Will be removed once Loader LTS version is "2.0.0-internal.7.0.0". Migrate all usage of IFluidRouter to the "entryPoint" pattern. Refer to Removing-IFluidRouter.md */
+		/**
+		 * @deprecated Will be removed once Loader LTS version is "2.0.0-internal.7.0.0". Migrate all usage of IFluidRouter to the "entryPoint" pattern. Refer to Removing-IFluidRouter.md
+		 */
 		requestHandlers?: RuntimeRequestHandler[];
 		provideEntryPoint: (runtime: IContainerRuntime) => Promise<FluidObject>;
 	}) {
