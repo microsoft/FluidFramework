8ae802cf3c6ea5ab2487bc2d6d4160c49ea2f5a5
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-01-26 19:38:54 +0000

    tools: Update rules to work around false positives

--- packages/framework/attributor/.eslintrc.cjs
+++ packages/framework/attributor/.eslintrc.cjs
@@ -8,6 +8,36 @@ module.exports = {
 	parserOptions: {
 		project: ["./tsconfig.json", "./src/test/tsconfig.json"],
 	},
+	rules: {
+		// Override base rules due to false-positives being reported in this package.
+		"jsdoc/require-jsdoc": [
+			"error",
+			{
+				// Indicates that only module exports should be flagged for lacking jsdoc comments
+				publicOnly: true,
+				// Prevents eslint from adding empty comment blocks when run with `--fix`
+				enableFixer: false,
+				require: {
+					ArrowFunctionExpression: true,
+					ClassDeclaration: true,
+					ClassExpression: true,
+					FunctionDeclaration: true,
+					FunctionExpression: true,
+
+					// Will report for *any* methods on exported classes, regardless of whether or not they are public
+					MethodDefinition: false,
+				},
+				contexts: [
+					"TSEnumDeclaration",
+					"TSInterfaceDeclaration",
+					"TSTypeAliasDeclaration",
+
+					// Yields many false positives in this package.
+					// "VariableDeclaration",
+				],
+			},
+		],
+	},
 	overrides: [
 		{
 			// Rules only for test files
