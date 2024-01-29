eb1afef18a6b8ededb5502e268de112b9b903785
Alexander Pardes <apardes@microsoft.com>
2024-01-29 15:32:16 -0800

    Avoid incorrect second rebase of moved nodes

--- packages/dds/tree/src/feature-libraries/sequence-field/utils.ts
+++ packages/dds/tree/src/feature-libraries/sequence-field/utils.ts
@@ -919,7 +919,7 @@ export function splitMark<T, TMark extends Mark<T> & Partial<VestigialEndpoint>>
 	return [mark1, mark2];
 }
 
-function splitMarkEffect<TEffect extends MarkEffect>(
+export function splitMarkEffect<TEffect extends MarkEffect>(
 	effect: TEffect,
 	length: number,
 ): [TEffect, TEffect] {
