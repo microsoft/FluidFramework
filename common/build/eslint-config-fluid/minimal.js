b2434dc12ac9f7e5e991237fcce5232ee5abb4c4
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-02-01 13:21:09 -0800

    Add eslint-custom-rules package to eslint-config-fluid (#19217)
    #### Description
    
    [ADO
    6834](https://dev.azure.com/fluidframework/internal/_workitems/edit/6834/)
    
    This PR creates a separate custom-eslint-rules module inside the
    `eslint-config-fluid` package. This step is necessary to use a local
    module as a dependency to the `eslint-config-fluid` package which will
    be used across the repository.
    
    Pre-releasing the `eslint-config-fluid` package and testing in actual
    dev setting are still needed to ensure stability.
    
    #### Relevant Docs
    - https://ronvalstar.nl/custom-local-eslint-rules
    - https://pnpm.io/package_json#dependenciesmetainjected
    
    ### More Updates
    
    #### `file:path` vs. `path`
    
    Currently, every packages in FF install
    `@fluidframework/eslint-config-fluid` from the npm registry, whereas
    `tools/api-markdown-documenter` uses relative path (e.g.,
    `"@fluidframework/eslint-config-fluid":
    "file:../../common/build/eslint-config-fluid"`) unlike other packages.
    And I believe, we are planning to use relative path for the
    `eslint-config-fluid` package in the future @Josmithr .
    
    @alexvy86 and I investigated on how the inclusion of `file:` in the
    `package.json` affects the linking within `node_modules`. Running `ls -l
    ./node_modules/@fluidframework` in `api-markdown-documenter`:
    
    With `file:`: `lrwxrwxrwx 1 node node 128 Jan 18 02:40
    eslint-config-fluid ->
    ../.pnpm/file+..+..+common+build+eslint-config-fluid_bpztyfltmpuv6lhsgzfwtmxhte/node_modules/@fluidframework/eslint-config-fluid`
    
    Without `file:`: `lrwxrwxrwx 1 node node 44 Jan 18 02:43
    eslint-config-fluid -> ../../../../common/build/eslint-config-fluid`
    
    Two things to consider about the result above:
    - `pnpm i` does not complain even if `file:` is not included in the
    path, and rather links the package to the `node_modules` differently.
    - Relative path without `file` points to the direct location of the
    `devDependencies`.
    
    @Josmithr, I think the error that I had experienced yesterday might be
    due to `pnpm i` working even when mistakenly omitting the `file:`
    script. Then the problem narrows down not to the instability of `pnpm`
    but to correctly including the `file:` script with the correct relative
    path.
    
    
    #### Update
    
    `tools/api-markdown-documenter` 
    <img width="1039" alt="Screenshot 2024-01-30 at 14 46 48"
    src="https://github.com/microsoft/FluidFramework/assets/111468570/32c5d216-8331-4dad-a890-61ab7ac07357">
    
    `examples/benchmarks/odspsnapshotfetch-perftestapp/src/fetchApp.ts`
    <img width="1042" alt="Screenshot 2024-01-30 at 15 09 01"
    src="https://github.com/microsoft/FluidFramework/assets/111468570/d39d4315-f2c2-42c9-9e8b-d831310d6b27">
    
    ---------
    
    Co-authored-by: Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>

--- common/build/eslint-config-fluid/minimal.js
+++ common/build/eslint-config-fluid/minimal.js
@@ -57,6 +57,8 @@ module.exports = {
 		"unused-imports",
 		// Plugin documentation: https://www.npmjs.com/package/eslint-plugin-unicorn
 		"unicorn",
+		// Custom ESLint rules
+		"fluid-internal",
 	],
 	reportUnusedDisableDirectives: true,
 	ignorePatterns: [
@@ -64,6 +66,11 @@ module.exports = {
 		"**/packageVersion.ts",
 	],
 	rules: {
+		/**
+		 * Restrict including release tags inside the member class / interface.
+		 */
+		"fluid-internal/no-member-release-tags": "error",
+
 		/**
 		 * The @rushstack rules are documented in the package README:
 		 * {@link https://www.npmjs.com/package/@rushstack/eslint-plugin}
