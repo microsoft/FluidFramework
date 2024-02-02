fe2a2c3c671a1b5cc87b9f6a31c47f4447073157
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2024-02-02 15:52:45 +0000

    Remove .only

--- packages/test/test-end-to-end-tests/src/test/opPerfTelemetry.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/opPerfTelemetry.spec.ts
@@ -42,102 +42,101 @@ describeCompat.noCompat(
 		beforeEach("getTestObjectProvider", () => {
 			provider = getTestObjectProvider();
 		});
-		describe.only("Tests", () => {
-			itExpects(
-				"Create with read connection and load with read connection",
-				[
-					// Expect one OpRoundtripTime event from each client
-					{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
-					{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
-				],
-				async () => {
-					// Create container with read-connection
-					const container1 = await provider.makeTestContainer(configWithReadConnection);
-					const dataObject1 = (await container1.getEntryPoint()) as ITestFluidObject;
-					const sharedString1 = await dataObject1.getSharedObject<SharedString>(stringId);
-					sharedString1.insertText(0, "a");
 
-					// Load the container with read-connection
-					const container2 = await provider.loadTestContainer(configWithReadConnection);
-					const dataObject2 = (await container2.getEntryPoint()) as ITestFluidObject;
-					const sharedString2 = await dataObject2.getSharedObject<SharedString>(stringId);
-					sharedString2.insertText(0, "b");
+		itExpects(
+			"Create with read connection and load with read connection",
+			[
+				// Expect one OpRoundtripTime event from each client
+				{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
+				{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
+			],
+			async () => {
+				// Create container with read-connection
+				const container1 = await provider.makeTestContainer(configWithReadConnection);
+				const dataObject1 = (await container1.getEntryPoint()) as ITestFluidObject;
+				const sharedString1 = await dataObject1.getSharedObject<SharedString>(stringId);
+				sharedString1.insertText(0, "a");
 
-					await provider.ensureSynchronized();
-				},
-			);
+				// Load the container with read-connection
+				const container2 = await provider.loadTestContainer(configWithReadConnection);
+				const dataObject2 = (await container2.getEntryPoint()) as ITestFluidObject;
+				const sharedString2 = await dataObject2.getSharedObject<SharedString>(stringId);
+				sharedString2.insertText(0, "b");
 
-			itExpects(
-				"Create with read connection and load with write connection",
-				[
-					// Expect one OpRoundtripTime event from each client
-					{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
-					{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
-				],
-				async () => {
-					// Create container with read-connection
-					const container1 = await provider.makeTestContainer(configWithReadConnection);
-					const dataObject1 = (await container1.getEntryPoint()) as ITestFluidObject;
-					const sharedString1 = await dataObject1.getSharedObject<SharedString>(stringId);
-					sharedString1.insertText(0, "a");
+				await provider.ensureSynchronized();
+			},
+		);
 
-					// Load the container with write-connection
-					const container2 = await provider.loadTestContainer(configWithWriteConnection);
-					const dataObject2 = (await container2.getEntryPoint()) as ITestFluidObject;
-					const sharedString2 = await dataObject2.getSharedObject<SharedString>(stringId);
-					sharedString2.insertText(0, "b");
+		itExpects(
+			"Create with read connection and load with write connection",
+			[
+				// Expect one OpRoundtripTime event from each client
+				{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
+				{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
+			],
+			async () => {
+				// Create container with read-connection
+				const container1 = await provider.makeTestContainer(configWithReadConnection);
+				const dataObject1 = (await container1.getEntryPoint()) as ITestFluidObject;
+				const sharedString1 = await dataObject1.getSharedObject<SharedString>(stringId);
+				sharedString1.insertText(0, "a");
 
-					await provider.ensureSynchronized();
-				},
-			);
+				// Load the container with write-connection
+				const container2 = await provider.loadTestContainer(configWithWriteConnection);
+				const dataObject2 = (await container2.getEntryPoint()) as ITestFluidObject;
+				const sharedString2 = await dataObject2.getSharedObject<SharedString>(stringId);
+				sharedString2.insertText(0, "b");
 
-			itExpects(
-				"Create with write connection and load with read connection",
-				[
-					// Expect one OpRoundtripTime event from each client
-					{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
-					{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
-				],
-				async () => {
-					// Create container with write-connection
-					const container1 = await provider.makeTestContainer(configWithWriteConnection);
-					const dataObject1 = (await container1.getEntryPoint()) as ITestFluidObject;
-					const sharedString1 = await dataObject1.getSharedObject<SharedString>(stringId);
-					sharedString1.insertText(0, "a");
+				await provider.ensureSynchronized();
+			},
+		);
 
-					// Load the container with read-connection
-					const container2 = await provider.loadTestContainer(configWithReadConnection);
-					const dataObject2 = (await container2.getEntryPoint()) as ITestFluidObject;
-					const sharedString2 = await dataObject2.getSharedObject<SharedString>(stringId);
-					sharedString2.insertText(0, "b");
+		itExpects(
+			"Create with write connection and load with read connection",
+			[
+				// Expect one OpRoundtripTime event from each client
+				{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
+				{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
+			],
+			async () => {
+				// Create container with write-connection
+				const container1 = await provider.makeTestContainer(configWithWriteConnection);
+				const dataObject1 = (await container1.getEntryPoint()) as ITestFluidObject;
+				const sharedString1 = await dataObject1.getSharedObject<SharedString>(stringId);
+				sharedString1.insertText(0, "a");
 
-					await provider.ensureSynchronized();
-				},
-			);
+				// Load the container with read-connection
+				const container2 = await provider.loadTestContainer(configWithReadConnection);
+				const dataObject2 = (await container2.getEntryPoint()) as ITestFluidObject;
+				const sharedString2 = await dataObject2.getSharedObject<SharedString>(stringId);
+				sharedString2.insertText(0, "b");
 
-			itExpects(
-				"Create with write connection and load with write connection",
-				[
-					// Expect one OpRoundtripTime event from each client
-					{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
-					{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
-				],
-				async () => {
-					// Create container with write-connection
-					const container1 = await provider.makeTestContainer(configWithWriteConnection);
-					const dataObject1 = (await container1.getEntryPoint()) as ITestFluidObject;
-					const sharedString1 = await dataObject1.getSharedObject<SharedString>(stringId);
-					sharedString1.insertText(0, "a");
+				await provider.ensureSynchronized();
+			},
+		);
 
-					// Load the container with write-connection
-					const container2 = await provider.loadTestContainer(configWithWriteConnection);
-					const dataObject2 = (await container2.getEntryPoint()) as ITestFluidObject;
-					const sharedString2 = await dataObject2.getSharedObject<SharedString>(stringId);
-					sharedString2.insertText(0, "b");
+		itExpects(
+			"Create with write connection and load with write connection",
+			[
+				// Expect one OpRoundtripTime event from each client
+				{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
+				{ eventName: "fluid:telemetry:OpRoundtripTime", clientType: "interactive" },
+			],
+			async () => {
+				// Create container with write-connection
+				const container1 = await provider.makeTestContainer(configWithWriteConnection);
+				const dataObject1 = (await container1.getEntryPoint()) as ITestFluidObject;
+				const sharedString1 = await dataObject1.getSharedObject<SharedString>(stringId);
+				sharedString1.insertText(0, "a");
 
-					await provider.ensureSynchronized();
-				},
-			);
-		});
+				// Load the container with write-connection
+				const container2 = await provider.loadTestContainer(configWithWriteConnection);
+				const dataObject2 = (await container2.getEntryPoint()) as ITestFluidObject;
+				const sharedString2 = await dataObject2.getSharedObject<SharedString>(stringId);
+				sharedString2.insertText(0, "b");
+
+				await provider.ensureSynchronized();
+			},
+		);
 	},
 );
