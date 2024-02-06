242970fc2c893ec15c8c294750426e955fe51c70
Matt Rakow <marakow@microsoft.com>
2024-02-06 15:50:22 -0800

    Remove EventForwarder and convert LazyLoadedDataObject to a TypedEventEmitter

--- packages/common/client-utils/src/indexNode.ts
+++ packages/common/client-utils/src/indexNode.ts
@@ -10,7 +10,6 @@ export { performance } from "./performanceIsomorphic";
 
 export { fromBase64ToUtf8, fromUtf8ToBase64, toUtf8 } from "./base64Encoding";
 export { Uint8ArrayToArrayBuffer } from "./bufferShared";
-export { EventForwarder } from "./eventForwarder";
 export { IsomorphicPerformance } from "./performanceIsomorphic";
 export { ITraceEvent, Trace } from "./trace";
 export { EventEmitterEventType, TypedEventEmitter, TypedEventTransform } from "./typedEventEmitter";
