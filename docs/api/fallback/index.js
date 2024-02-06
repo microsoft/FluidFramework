9e1b9508708d8e27c3aad7f85b58abadbb11918a
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2024-02-06 22:13:19 +0000

    add policy check

--- docs/api/fallback/index.js
+++ docs/api/fallback/index.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 module.exports = async function (context, req) {
     context.res.json({
         text: "Hello from the API"
