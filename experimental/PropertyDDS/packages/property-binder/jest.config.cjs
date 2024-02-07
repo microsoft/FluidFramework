c0a8032495367e8232410fb03ab30df0c43afdcc
Tyler Butler <tyler@tylerbutler.com>
2024-02-07 15:37:17 -0800

    additional changes

--- experimental/PropertyDDS/packages/property-binder/jest.config.cjs
+++ experimental/PropertyDDS/packages/property-binder/jest.config.cjs
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-// jest.config.js
+// jest.config.cjs
 module.exports = {
 	reporters: [
 		"default",
