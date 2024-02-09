bec10f93d5f1909d113b9b13bbddb6d2239c555d
Tyler Butler <tyler@tylerbutler.com>
2024-02-08 16:42:27 -0800

    feedback

--- packages/common/driver-definitions/src/urlResolver.ts
+++ packages/common/driver-definitions/src/urlResolver.ts
@@ -92,6 +92,7 @@ export interface IDriverHeader {
 }
 
 declare module "@fluidframework/core-interfaces" {
+	// TODO: This interface should be documented. AB#7164
 	// eslint-disable-next-line @typescript-eslint/no-empty-interface, jsdoc/require-jsdoc
 	export interface IRequestHeader extends Partial<IDriverHeader> {}
 }
