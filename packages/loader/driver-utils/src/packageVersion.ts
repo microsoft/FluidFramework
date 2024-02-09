e70c7352e49ee619ec41b51a14fc0860c86d380f
daesunp <138815173+daesunp@users.noreply.github.com>
2024-02-09 00:48:56 +0000

    [bump] client: 2.0.0-rc.1.0.2 => 2.0.0-rc.1.0.3 (patch)
    Bumped client from 2.0.0-rc.1.0.2 to 2.0.0-rc.1.0.3.

--- packages/loader/driver-utils/src/packageVersion.ts
+++ packages/loader/driver-utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/driver-utils";
-export const pkgVersion = "2.0.0-rc.1.0.2";
+export const pkgVersion = "2.0.0-rc.1.0.3";
--- packages/test/test-end-to-end-tests/src/packageVersion.ts
+++ packages/test/test-end-to-end-tests/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-private/test-end-to-end-tests";
-export const pkgVersion = "2.0.0-rc.1.0.2";
+export const pkgVersion = "2.0.0-rc.1.0.3";
