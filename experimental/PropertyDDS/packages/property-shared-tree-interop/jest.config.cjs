df15d41504d7f6569866eff0d84e7821b818afe2
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-02-12 18:06:01 +0000

    jest workaround

--- experimental/PropertyDDS/packages/property-shared-tree-interop/jest.config.cjs
+++ experimental/PropertyDDS/packages/property-shared-tree-interop/jest.config.cjs
@@ -17,15 +17,21 @@ module.exports = {
 	],
 	testEnvironment: "node",
 	transform: {
-		"^.+\\.ts$": "ts-jest",
-	},
-	globals: {
-		"ts-jest": {
-			tsconfig: "src/test/tsconfig.json",
-		},
+		"^.+\\.ts$": [
+			"ts-jest",
+			{
+				tsconfig: "src/test/tsconfig.json",
+			},
+		],
 	},
 	testPathIgnorePatterns: ["/node_modules/", "dist"],
 	// While we still have transitive dependencies on 'uuid<9.0.0', force the CJS entry point:
 	// See: https://stackoverflow.com/questions/73203367/jest-syntaxerror-unexpected-token-export-with-uuid-library
-	moduleNameMapper: { "^uuid$": "uuid" },
+	moduleNameMapper: {
+		"^uuid$": "uuid",
+		// Jest (v29) default module resolution has trouble resolving files ESM modules with a '*.js' extension.
+		// The popular workaround is to remove the *.js or *.jsx extension for imports that begin with "./*" and "../*".
+		// (See: https://github.com/kulshekhar/ts-jest/issues/1057)
+		"^(\\.\\.?\\/.+)\\.jsx?$": "$1",
+	},
 };
