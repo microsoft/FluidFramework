8ad420b6839b6ab5626be836b89bd215d2638fee
Matt Rakow <marakow@microsoft.com>
2024-02-15 13:00:02 -0800

    PR feedback - docs and links

--- packages/drivers/routerlicious-driver/src/routerliciousResolvedUrl.ts
+++ packages/drivers/routerlicious-driver/src/routerliciousResolvedUrl.ts
@@ -7,15 +7,24 @@ import type { IResolvedUrl } from "@fluidframework/driver-definitions";
 
 /**
  * Routerlicious extends the resolved url with additional properties to control Routerlicious-specific behaviors.
+ *
+ * @privateRemarks
+ * {@link isRouterliciousResolvedUrl} can be used to detect whether an {@link @fluidframework/driver-definitions#IResolvedUrl}
+ * is an IRouterliciousResolvedUrl.
+ *
  * @alpha
  */
 export interface IRouterliciousResolvedUrl extends IResolvedUrl {
 	/**
-	 * A flag to facilitate type narrowing from IResolvedUrl to IRouterliciousResolvedUrl.
+	 * A flag to facilitate type narrowing from {@link @fluidframework/driver-definitions#IResolvedUrl} to IRouterliciousResolvedUrl.
 	 */
 	routerliciousResolvedUrl: true;
 	/**
-	 * Controls whether a newly created container will be ephemeral. Only affects createContainer requests.
+	 * Controls whether a newly created container will be ephemeral, which means the service will not retain it
+	 * after the collaboration session ends. Only affects
+	 * {@link @fluidframework/driver-definitions#IDocumentServiceFactory.createContainer} requests.
+	 *
+	 * @defaultValue If left undefined, treated as `false`
 	 */
 	createAsEphemeral?: boolean;
 }
