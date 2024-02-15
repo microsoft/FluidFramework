0351a2044e1ac6b6a882674ce4dcc86c05a1eaf3
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-02-15 20:11:47 +0000

    build(telemetry-utils): node16

--- packages/utils/telemetry-utils/src/test/telemetryLogger.spec.ts
+++ packages/utils/telemetry-utils/src/test/telemetryLogger.spec.ts
@@ -10,8 +10,8 @@ import {
 	ITelemetryLoggerPropertyBag,
 	TelemetryLogger,
 	convertToBasePropertyType,
-} from "../logger";
-import { ITaggedTelemetryPropertyTypeExt, TelemetryEventPropertyTypeExt } from "../telemetryTypes";
+} from "../logger.js";
+import { ITaggedTelemetryPropertyTypeExt, TelemetryEventPropertyTypeExt } from "../telemetryTypes.js";
 
 class TestTelemetryLogger extends TelemetryLogger {
 	public events: ITelemetryBaseEvent[] = [];
