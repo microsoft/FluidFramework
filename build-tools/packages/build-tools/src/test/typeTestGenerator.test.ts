9df79c9a2e71e7cfeb027c731ae17a40c37ea22a
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-02-16 12:52:48 -0500

    added test

--- build-tools/packages/build-tools/src/test/typeTestGenerator.test.ts
+++ build-tools/packages/build-tools/src/test/typeTestGenerator.test.ts
@@ -1,1 +1,16 @@
-import * as TypeTestUtils from "../type-test-generator/typeTestUtils";
+import * as utils from "../type-test-generator/typeTestUtils";
+import { readJsonSync } from "fs-extra";
+import { PackageJson } from "../common/npmPackage";
+
+
+describe('typeTestUtils', () => {
+	describe('ensureDevDependencyExists', () => {
+		it.only('Should not throw an error if dev dependency exists', () => {
+			const packageObject: PackageJson = readJsonSync("mockPackage.json");
+			const previousPackageName = `${packageObject.name}-previous`;
+			utils.ensureDevDependencyExists(packageObject, previousPackageName);
+
+		  });
+		
+	  });
+})
