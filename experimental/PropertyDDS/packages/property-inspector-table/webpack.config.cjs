e236d16a31d724e95d4cc3dee53ebd7362d649a8
Matt Rakow <marakow@microsoft.com>
2024-02-15 16:20:53 -0800

    Fix a couple errors for storybook and webpack config

--- experimental/PropertyDDS/packages/property-inspector-table/webpack.config.cjs
+++ experimental/PropertyDDS/packages/property-inspector-table/webpack.config.cjs
@@ -80,6 +80,7 @@ module.exports = (env) => {
 			watchOptions: {
 				ignored: "**/node_modules/**",
 			},
+			mode: isProduction ? "production" : "development",
 		},
 		fluidRoute.devServerConfig(__dirname, env),
 	);
