43162d711166303db877eb26c47f630006d20913
Jason Hartman <jasonha@microsoft.com>
2024-02-20 01:54:07 -0800

    feat(build-tools): fluid-tsc replaces tsc-multi with package overrides
    `tsCompile.ts` refactored from `tscWorker.ts` leaving `Worker` interface behind and adding custom host with readFile that may override the "type"field of current directories package.json.
    
    - Add `fluid-tsc` command to run tsc with package.json override in effect.
    - Add task recognition of `fluid-tsc` as a `tsc` task with handling of command line difference.
    - Fix `getTscCommandDependencies` detection of directory or file reference specification.

---
+++ build-tools/packages/build-tools/src/common/tsCompile.ts
@@ -0,0 +1,122 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import type * as tsTypes from "typescript";
+import path from "path";
+import { getTscUtils } from "./tscUtils.js";
+
+// Any paths given by typescript will be normalized to forward slashes.
+// Local paths should be normalized to make any comparisons.
+const directorySeparator = "/";
+const backslashRegExp = /\\/g;
+function normalizeSlashes(path: string): string {
+	return path.includes("\\") ? path.replace(backslashRegExp, directorySeparator) : path;
+}
+
+export function tsCompile({
+	command,
+	cwd,
+	packageJsonTypeOverride,
+}: {
+	command: string;
+	cwd: string;
+	packageJsonTypeOverride?: "commonjs" | "module";
+}): number {
+	// Load the typescript version that is in the cwd scope
+	const tscUtils = getTscUtils(cwd);
+
+	const ts = tscUtils.tsLib;
+	let commandLine = tscUtils.parseCommandLine(command);
+	const diagnostics: tsTypes.Diagnostic[] = [];
+	if (commandLine) {
+		const configFileName = tscUtils.findConfigFile(cwd, commandLine);
+		if (configFileName) {
+			commandLine = ts.getParsedCommandLineOfConfigFile(configFileName, commandLine.options, {
+				...ts.sys,
+				getCurrentDirectory: () => cwd,
+				onUnRecoverableConfigFileDiagnostic: (diagnostic: tsTypes.Diagnostic) => {
+					diagnostics.push(diagnostic);
+				},
+			});
+		} else {
+			throw new Error("Unknown config file in command line");
+		}
+	}
+
+	let code = ts.ExitStatus.DiagnosticsPresent_OutputsSkipped;
+	if (commandLine && diagnostics.length === 0) {
+		const incremental = !!(commandLine.options.incremental || commandLine.options.composite);
+
+		const host = incremental
+			? ts.createIncrementalCompilerHost(commandLine.options)
+			: ts.createCompilerHost(commandLine.options);
+		// When specified overrides current directory's package.json type field so tsc may cleanly
+		// transpile .ts files to CommonJS or ESM using compilerOptions.module Node16 or NodeNext.
+		if (packageJsonTypeOverride) {
+			const origReadFile = host.readFile;
+			const packageJsonPath = normalizeSlashes(path.join(cwd, "package.json"));
+			host.readFile = (fileName: string) => {
+				const rawFile = origReadFile(fileName);
+				if (fileName === packageJsonPath && rawFile !== undefined) {
+					// Reading local package.json: override type field
+					const packageJson = JSON.parse(rawFile);
+					return JSON.stringify({ ...packageJson, type: packageJsonTypeOverride });
+				}
+				return rawFile;
+			};
+		}
+
+		const param = {
+			rootNames: commandLine.fileNames,
+			options: tscUtils.convertOptionPaths(commandLine.options, cwd, path.resolve),
+			host,
+			projectReferences: commandLine.projectReferences,
+		};
+		const program = incremental ? ts.createIncrementalProgram(param) : ts.createProgram(param);
+
+		diagnostics.push(...program.getConfigFileParsingDiagnostics());
+		diagnostics.push(...program.getSyntacticDiagnostics());
+		diagnostics.push(...program.getOptionsDiagnostics());
+		diagnostics.push(...program.getGlobalDiagnostics());
+		diagnostics.push(...program.getSemanticDiagnostics());
+
+		const emitResult = program.emit();
+		diagnostics.push(...emitResult.diagnostics);
+
+		if (emitResult.emitSkipped && diagnostics.length > 0) {
+			code = ts.ExitStatus.DiagnosticsPresent_OutputsSkipped;
+		} else {
+			code =
+				diagnostics.length !== 0
+					? ts.ExitStatus.DiagnosticsPresent_OutputsGenerated
+					: ts.ExitStatus.Success;
+		}
+	}
+
+	if (diagnostics.length > 0) {
+		const sortedDiagnostics = ts.sortAndDeduplicateDiagnostics(diagnostics);
+
+		const formatDiagnosticsHost: tsTypes.FormatDiagnosticsHost = {
+			getCurrentDirectory: () => ts.sys.getCurrentDirectory(),
+			getCanonicalFileName: tscUtils.getCanonicalFileName,
+			getNewLine: () => ts.sys.newLine,
+		};
+
+		// TODO: tsc has more complicated summary than this
+		if (commandLine?.options?.pretty !== false) {
+			console.log(
+				ts.formatDiagnosticsWithColorAndContext(sortedDiagnostics, formatDiagnosticsHost),
+			);
+			console.log(
+				`${ts.sys.newLine}Found ${sortedDiagnostics.length} error${
+					sortedDiagnostics.length > 1 ? "s" : ""
+				}.`,
+			);
+		} else {
+			console.log(ts.formatDiagnostics(sortedDiagnostics, formatDiagnosticsHost));
+		}
+	}
+	return code;
+}
---
+++ build-tools/packages/build-tools/src/fluidTsc/fluidTsc.ts
@@ -0,0 +1,42 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { defaultLogger } from "../common/logging";
+import { tsCompile } from "../common/tsCompile";
+
+const { log, errorLog: error } = defaultLogger;
+
+function printUsage() {
+	log(
+		`
+Usage: fluid-tsc [commonjs|module] [<tsc args>...]
+    [commonjs|module] value for package.json "type" property
+    [<tsc args>...] arguments passed to Typescript compiler (see tsc -?)
+`,
+	);
+}
+
+async function main() {
+	const firstArg = process.argv[2];
+
+	if (firstArg === "-?" || firstArg === "--help") {
+		printUsage();
+		process.exit(0);
+	}
+
+	if (firstArg !== "commonjs" && firstArg !== "module") {
+		throw new Error(
+			`fluid-tsc's first argument must be 'commonjs' or 'module'. Was: ${firstArg}`,
+		);
+	}
+
+	const command = `tsc ${process.argv.slice(3).join(" ")}`;
+	process.exit(tsCompile({ command, cwd: process.cwd(), packageJsonTypeOverride: firstArg }));
+}
+
+main().catch((e) => {
+	error(`Unexpected error. ${e.message}`);
+	error(e.stack);
+});
