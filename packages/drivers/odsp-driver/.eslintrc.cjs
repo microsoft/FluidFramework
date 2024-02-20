a03325ef3fbd37cec20a98311d19ed8b5ab36814
Josmithr <josmithr@microsoft.com>
2024-02-20 11:06:11 -0800

    tools: Disable eslint rule

--- packages/drivers/odsp-driver/.eslintrc.cjs
+++ packages/drivers/odsp-driver/.eslintrc.cjs
@@ -14,6 +14,9 @@ module.exports = {
 		"@typescript-eslint/strict-boolean-expressions": "off",
 		"@rushstack/no-new-null": "error",
 		"import/no-deprecated": "warn", // This package uses the deprecated ShareLinkTypes type. Once we remove that, we can remove this override. It's set as a warning instead of "off" to serve as a reminder.
+
+		// This library uses and serializes "utf-8".
+		"unicorn/text-encoding-identifier-case": "off",
 	},
 	overrides: [
 		{
