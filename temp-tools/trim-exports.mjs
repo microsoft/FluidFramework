0ac4faf5e4f011f93968f6f48cb9aa527123cc61
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-02-23 19:52:39 +0000

    add trim-exports tool

---
+++ temp-tools/trim-exports.mjs
@@ -0,0 +1,60 @@
+// alias c='node /workspaces/f0/temp-tools/convert.mjs'
+// flub exec "node /workspaces/f0/temp-tools/convert.mjs" -g client
+
+import { repoRoot } from "./git.mjs";
+import JSON5 from "json5";
+import path from "path";
+import fs from "fs";
+import { format } from "./format.mjs";
+
+// Assume current working directory is the package root.
+const packageRoot = process.cwd();
+
+// Get root of Git repo.
+const repoRootPath = repoRoot();
+
+// From the package root, compute how many repetitions of '../' get us to the repo root.
+const workspaceRoot = (() => {
+	const gitRootLength = repoRootPath.split(path.sep).length;
+	let relativePath = "../".repeat(packageRoot.split(path.sep).length - gitRootLength);
+	return relativePath.slice(0, relativePath.length - 1);
+})();
+
+// Load 'package.json'
+const pkgPath = path.join(packageRoot, "package.json");
+const pkgSrc = fs.readFileSync(pkgPath, "utf8");
+
+const pkg = JSON5.parse(pkgSrc);
+
+function loadDts(exportName) {
+	const dtsPath = pkg.exports[exportName]?.import?.types;
+
+	if (dtsPath !== undefined) {
+		return fs.readFileSync(path.join(packageRoot, dtsPath), "utf8")
+			.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g, "")
+			.split("\n")
+			.filter((line) => line.trim().length > 0)
+			.join("\n");
+	}
+
+	return undefined;
+}
+
+const dtsNames = ["./public", "./beta", "./alpha", "./internal"];
+
+let prevDts = loadDts(dtsNames[0]);
+for (let i = 1; i < dtsNames.length; i++) {
+	let dtsName = dtsNames[i];
+	const currentDts = loadDts(dtsName) ?? prevDts;
+	if (currentDts === prevDts) {
+		delete pkg.exports[dtsName];
+	}
+	prevDts = currentDts;
+}
+
+delete pkg.exports["./fruit"];
+
+// Write package.json
+fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 4));
+
+format();
