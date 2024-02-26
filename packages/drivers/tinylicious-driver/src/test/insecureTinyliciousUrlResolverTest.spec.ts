0b26e7f71f86262989c336436c84dbb6d2d810a4
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-02-26 21:22:36 +0000

    build(tinylicious-driver): use fluid-tsc

--- packages/drivers/tinylicious-driver/src/test/insecureTinyliciousUrlResolverTest.spec.ts
+++ packages/drivers/tinylicious-driver/src/test/insecureTinyliciousUrlResolverTest.spec.ts
@@ -6,7 +6,7 @@
 import { strict as assert } from "node:assert";
 import { IRequest } from "@fluidframework/core-interfaces";
 import { DriverHeader } from "@fluidframework/driver-definitions";
-import { InsecureTinyliciousUrlResolver } from "../insecureTinyliciousUrlResolver";
+import { InsecureTinyliciousUrlResolver } from "../insecureTinyliciousUrlResolver.js";
 
 describe("Insecure Url Resolver Test", () => {
 	const documentId = "fileName";
