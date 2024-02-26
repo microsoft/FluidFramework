a957a14862c1468041c8a0020c6f3d8e6983eb59
Jason Hartman <jasonha@microsoft.com>
2024-02-26 14:33:17 -0800

    test(client): remove node:events import allowance node events import is not okay by default. appearance of import snuck in and now has been resolved (as best we can tell as settings are complex)

--- packages/drivers/local-driver/.eslintrc.cjs
+++ packages/drivers/local-driver/.eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
 		"@typescript-eslint/strict-boolean-expressions": "off",
 
 		// This library is used in the browser, so we don't want dependencies on most node libraries.
-		"import/no-nodejs-modules": ["error", { allow: ["url", "events"] }],
+		"import/no-nodejs-modules": ["error", { allow: ["url"] }],
 	},
 	overrides: [
 		{
@@ -23,7 +23,7 @@ module.exports = {
 			files: ["*.spec.ts", "src/test/**"],
 			rules: {
 				// Test files are run in node only so additional node libraries can be used.
-				"import/no-nodejs-modules": ["error", { allow: ["assert", "url", "events"] }],
+				"import/no-nodejs-modules": ["error", { allow: ["assert", "url"] }],
 			},
 		},
 	],
