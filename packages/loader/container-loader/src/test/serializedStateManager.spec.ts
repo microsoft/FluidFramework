9fbe04e2bd6ba7a2b20b51f79f0e4ea6dfb9d04a
Daniel Madrid <105010181+dannimad@users.noreply.github.com>
2024-02-26 03:49:53 +0000

    min set of tests

--- packages/loader/container-loader/src/test/serializedStateManager.spec.ts
+++ packages/loader/container-loader/src/test/serializedStateManager.spec.ts
@@ -10,6 +10,7 @@ import {
 	ISnapshotTree,
 	MessageType,
 	ISequencedDocumentMessage,
+	IDocumentAttributes,
 } from "@fluidframework/protocol-definitions";
 import {
 	FetchSource,
@@ -19,33 +20,47 @@ import {
 	ISnapshotFetchOptions,
 } from "@fluidframework/driver-definitions";
 import { IGetPendingLocalStateProps, IRuntime } from "@fluidframework/container-definitions";
+import { stringToBuffer } from "@fluid-internal/client-utils";
 import { IPendingContainerState } from "../container";
 import { SerializedStateManager } from "../serializedStateManager";
-import { ISerializableBlobContents } from "../containerStorageAdapter";
 
 type ISerializedStateManagerDocumentTorageService = Pick<
 	IDocumentStorageService,
 	"getSnapshot" | "getSnapshotTree" | "getVersions" | "readBlob"
 >;
 
-abstract class BaseMockStorageAdapter implements ISerializedStateManagerDocumentTorageService {
-	public abstract getSnapshot(
-		snapshotFetchOptions?: ISnapshotFetchOptions | undefined,
-	): Promise<ISnapshot>;
-	public abstract getSnapshotTree(
-		version?: IVersion | undefined,
-		scenarioName?: string | undefined,
-	): Promise<ISnapshotTree | null>;
-	public abstract getVersions(
-		versionId: string | null,
-		count: number,
-		scenarioName?: string | undefined,
-		fetchSource?: FetchSource | undefined,
-	): Promise<IVersion[]>;
-	public abstract readBlob(id: string): Promise<ArrayBufferLike>;
+interface IPendingMessage {
+	type: "message";
+	referenceSequenceNumber: number;
+	content: string;
 }
 
-class MockStorageAdapter extends BaseMockStorageAdapter {
+class MockStorageAdapter implements ISerializedStateManagerDocumentTorageService {
+	private readonly blobs = new Map<string, ArrayBufferLike>();
+	private readonly snapshots: ISnapshotTree[] = [];
+
+	constructor() {
+		const baseSnapshot: ISnapshotTree = {
+			id: "SnapshotId",
+			blobs: {},
+			trees: {
+				".protocol": {
+					blobs: { attributes: "attributesId" },
+					trees: {},
+				},
+				".app": {
+					blobs: {},
+					trees: {},
+				},
+			},
+		};
+		this.snapshots.push(baseSnapshot);
+		this.blobs.set(
+			"attributesId",
+			stringToBuffer(`{"minimumSequenceNumber" : 0, "sequenceNumber": 0}`, "utf8"),
+		);
+	}
+
 	public async getSnapshot(
 		snapshotFetchOptions?: ISnapshotFetchOptions | undefined,
 	): Promise<ISnapshot> {
@@ -54,88 +69,45 @@ class MockStorageAdapter extends BaseMockStorageAdapter {
 	public async getSnapshotTree(
 		version?: IVersion | undefined,
 		scenarioName?: string | undefined,
+		// eslint-disable-next-line @rushstack/no-new-null
 	): Promise<ISnapshotTree | null> {
-		throw new Error("Method not implemented.");
+		const lastSnapshot = this.snapshots[this.snapshots.length - 1];
+		return lastSnapshot ?? null;
 	}
+
 	public async getVersions(
+		// eslint-disable-next-line @rushstack/no-new-null
 		versionId: string | null,
 		count: number,
 		scenarioName?: string | undefined,
 		fetchSource?: FetchSource | undefined,
 	): Promise<IVersion[]> {
-		throw new Error("Method not implemented.");
+		return [{ id: "test", treeId: "test" }];
 	}
 	public async readBlob(id: string): Promise<ArrayBufferLike> {
-		throw new Error("Method not implemented.");
+		return this.blobs.get(id) as ArrayBufferLike;
 	}
 }
+
 type ISerializedStateManagerRuntime = Pick<IRuntime, "getPendingLocalState">;
 
 class MockRuntime implements ISerializedStateManagerRuntime {
-	private generatePendingOp() {
-		return {
+	private pendingOps: IPendingMessage[] = [];
+	public generatePendingOp(rfs) {
+		this.pendingOps.push({
 			type: "message",
-			referenceSequenceNumber: 0,
-			content: {},
-			localOpMetadata: {},
-			opMetadata: "",
-		};
+			referenceSequenceNumber: rfs,
+			content: "",
+		});
 	}
 
 	public getPendingLocalState(props?: IGetPendingLocalStateProps | undefined): unknown {
-		return { pending: this.generatePendingOp() };
+		const result = [...this.pendingOps];
+		this.pendingOps = [];
+		return { pending: result };
 	}
 }
 
-const snapshotTreeWithGroupId: ISnapshotTree = {
-	id: "SnapshotId",
-	blobs: {},
-	trees: {
-		".protocol": {
-			blobs: {},
-			trees: {},
-		},
-		".app": {
-			blobs: { ".metadata": "bARD4RKvW4LL1KmaUKp6hUMSp" },
-			trees: {
-				".channels": {
-					blobs: {},
-					trees: {
-						default: {
-							blobs: {},
-							trees: {
-								dds: {
-									blobs: {},
-									trees: {},
-								},
-							},
-							groupId: "G3",
-						},
-					},
-					unreferenced: true,
-					groupId: "G2",
-				},
-				".blobs": { blobs: {}, trees: {} },
-			},
-			unreferenced: true,
-			groupId: "G4",
-		},
-	},
-};
-
-const blobContents: ISerializableBlobContents = {
-	key1: "value1",
-	key2: "value2",
-	key3: "value3",
-};
-
-const pendingLocalState: IPendingContainerState = {
-	baseSnapshot: { blobs: {}, trees: {} },
-	snapshotBlobs: {},
-	pendingRuntimeState: {},
-	savedOps: [],
-	url: "fluid",
-};
 const resolvedUrl: IResolvedUrl = {
 	type: "fluid",
 	id: "",
@@ -150,15 +122,24 @@ const errorFn = (error: Error, expected: string): boolean => {
 	return true;
 };
 
+const getAttributesFromPendingState = (
+	pendingState: IPendingContainerState,
+): IDocumentAttributes => {
+	if (pendingState.baseSnapshot === undefined) {
+		throw new Error("base snapshot should be valid");
+	}
+	const attributesId = pendingState.baseSnapshot.trees[".protocol"].blobs.attributes;
+	const attributes = pendingState.snapshotBlobs[attributesId];
+	return JSON.parse(attributes) as IDocumentAttributes;
+};
+
 describe("serializedStateManager", () => {
-	let clientSeqNumber = 0;
 	let seq: number;
 	let logger: ITelemetryLoggerExt;
 
-	function generateOp(type: MessageType = MessageType.Operation): ISequencedDocumentMessage {
+	function generateSavedOp(type: MessageType = MessageType.Operation): ISequencedDocumentMessage {
 		return {
 			clientId: "Some client ID",
-			clientSequenceNumber: ++clientSeqNumber,
 			minimumSequenceNumber: 0,
 			sequenceNumber: seq++,
 			type,
@@ -167,14 +148,13 @@ describe("serializedStateManager", () => {
 
 	beforeEach(async () => {
 		seq = 1;
-		clientSeqNumber = 0;
 		logger = createChildLogger({ namespace: "fluid:testSerializedStateManager" });
 	});
 
 	it("can't get pending local state when offline load disabled", async () => {
 		const storageAdapter = new MockStorageAdapter();
 		const serializedStateManager = new SerializedStateManager(
-			pendingLocalState,
+			undefined,
 			logger,
 			storageAdapter,
 			false,
@@ -199,7 +179,7 @@ describe("serializedStateManager", () => {
 	it("can't get pending local state when there is no base snapshot", async () => {
 		const storageAdapter = new MockStorageAdapter();
 		const serializedStateManager = new SerializedStateManager(
-			pendingLocalState,
+			undefined,
 			logger,
 			storageAdapter,
 			true,
@@ -220,17 +200,18 @@ describe("serializedStateManager", () => {
 		);
 	});
 
-	it("can get pending local state", async () => {
+	it("can get pending local state after attach", async () => {
 		const storageAdapter = new MockStorageAdapter();
 		const serializedStateManager = new SerializedStateManager(
-			pendingLocalState,
+			undefined,
 			logger,
 			storageAdapter,
 			true,
 		);
-		serializedStateManager.setSnapshot({ tree: snapshotTreeWithGroupId, blobs: blobContents });
-		for (let num = 0; num < 100; ++num) {
-			serializedStateManager.addSavedOp(generateOp());
+		// equivalent to attach
+		serializedStateManager.setSnapshot({ tree: { trees: {}, blobs: {} }, blobs: {} });
+		for (let num = 0; num < 10; ++num) {
+			serializedStateManager.addSavedOp(generateSavedOp());
 		}
 		await serializedStateManager.getPendingLocalStateCore(
 			{ notifyImminentClosure: false },
@@ -238,5 +219,64 @@ describe("serializedStateManager", () => {
 			new MockRuntime(),
 			resolvedUrl,
 		);
+	});
+
+	it("get state from previous", async () => {
+		const pendingLocalState: IPendingContainerState = {
+			baseSnapshot: { id: "fromPending", blobs: {}, trees: {} },
+			snapshotBlobs: {},
+			pendingRuntimeState: {},
+			savedOps: [],
+			url: "fluid",
+		};
+		const storageAdapter = new MockStorageAdapter();
+		const serializedStateManager = new SerializedStateManager(
+			pendingLocalState,
+			logger,
+			storageAdapter,
+			true,
+		);
+		const { snapshotTree, version } = await serializedStateManager.fetchSnapshot(
+			undefined,
+			undefined,
+		);
+		assert(snapshotTree);
+		assert.strictEqual(version, undefined);
+
+		const state = await serializedStateManager.getPendingLocalStateCore(
+			{ notifyImminentClosure: false },
+			"clientId",
+			new MockRuntime(),
+			resolvedUrl,
+		);
+		assert.strictEqual(JSON.parse(state).baseSnapshot.id, "fromPending");
+	});
+
+	it("can fetch snapshot and get state from it", async () => {
+		const storageAdapter = new MockStorageAdapter();
+		const serializedStateManager = new SerializedStateManager(
+			undefined,
+			logger,
+			storageAdapter,
+			true,
+		);
+		const { snapshotTree, version } = await serializedStateManager.fetchSnapshot(
+			undefined,
+			undefined,
+		);
+		assert(snapshotTree);
+		assert.strictEqual(version?.id, "test");
+		assert.strictEqual(version.treeId, "test");
+		const state = await serializedStateManager.getPendingLocalStateCore(
+			{ notifyImminentClosure: false },
+			"clientId",
+			new MockRuntime(),
+			resolvedUrl,
+		);
+		const parsed = JSON.parse(state);
+		assert.strictEqual(parsed.baseSnapshot.id, "SnapshotId");
+		const attributes = getAttributesFromPendingState(parsed);
+		assert.strictEqual(attributes.sequenceNumber, 0);
+		assert.strictEqual(attributes.minimumSequenceNumber, 0);
 	});
 });
