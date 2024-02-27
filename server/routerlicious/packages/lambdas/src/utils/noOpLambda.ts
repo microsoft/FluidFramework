42a2914a69d6b34c0d4264be26294b7b9e25cc10
kkachmar <kkachmar@microsoft.com>
2024-02-27 17:09:31 -0500

    make noOpLambda checkpoints configurable

--- server/routerlicious/packages/lambdas/src/utils/noOpLambda.ts
+++ server/routerlicious/packages/lambdas/src/utils/noOpLambda.ts
@@ -9,12 +9,62 @@ import { IContext, IQueuedMessage, IPartitionLambda } from "@fluidframework/serv
  * @internal
  */
 export class NoOpLambda implements IPartitionLambda {
-	constructor(private readonly context: IContext) {}
+	private opsCount = 0;
+	private lastCheckpointOffset = 0;
+	private timer: NodeJS.Timer | undefined;
+
+	constructor(
+		private readonly context: IContext,
+		private readonly checkpointConfiguration?: NoOpLambdaCheckpointConfiguration,
+	) {
+		this.timer = undefined;
+	}
 
 	public handler(message: IQueuedMessage) {
-		this.context.checkpoint(message);
+		// default
+		if (!this.checkpointConfiguration || !this.checkpointConfiguration?.enabled) {
+			this.context.checkpoint(message);
+			return undefined;
+		}
+
+		this.opsCount++;
+		if (this.opsCount >= this.checkpointConfiguration.maxMessages) {
+			this.configurableCheckpoint(message);
+		}
+
+		if (this.checkpointConfiguration?.enabled) {
+			this.resetTimer(message, this.checkpointConfiguration.maxTime);
+		}
+
 		return undefined;
 	}
 
 	public close(): void {}
+
+	private configurableCheckpoint(message: IQueuedMessage) {
+		if (message.offset > this.lastCheckpointOffset) {
+			this.context.checkpoint(message);
+			if (this.timer) {
+				clearTimeout(this.timer);
+				this.timer = undefined;
+			}
+			this.opsCount = 0;
+			this.lastCheckpointOffset = message.offset;
+		}
+	}
+
+	private resetTimer(message: IQueuedMessage, timeout: number) {
+		if (this.timer) {
+			clearTimeout(this.timer);
+		}
+		this.timer = setInterval(() => {
+			this.configurableCheckpoint(message);
+		}, timeout);
+	}
+}
+
+export interface NoOpLambdaCheckpointConfiguration {
+	enabled: boolean;
+	maxMessages: number;
+	maxTime: number;
 }
