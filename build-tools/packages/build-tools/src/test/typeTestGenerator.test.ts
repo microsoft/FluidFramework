51ff9c3a442ee2261dfdfea665e640a8b6b49826
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-02-27 16:41:15 -0500

    Added unit test

--- build-tools/packages/build-tools/src/test/typeTestGenerator.test.ts
+++ build-tools/packages/build-tools/src/test/typeTestGenerator.test.ts
@@ -6,19 +6,29 @@ import { PackageJson } from "../common/npmPackage";
 
 describe("typeTestUtils", () => {
 	describe("ensureDevDependencyExists", () => {
+		const packageJsonPath = path.join(__dirname, "mockPackage.json");
+		const packageObject: PackageJson = readJsonSync(packageJsonPath);
 		it("Should not throw an error if dev dependency exists", () => {
-			const packageJsonPath = path.join(__dirname, "mockPackage.json");
-			const packageObject: PackageJson = readJsonSync(packageJsonPath);
 			const previousPackageName = `${packageObject.name}-previous`;
 			utils.ensureDevDependencyExists(packageObject, previousPackageName);
 		});
 
 		it("Should throw an error if dev dependency does not exist", () => {
-			const packageJsonPath = path.join(__dirname, "mockPackage.json");
-			const packageObject: PackageJson = readJsonSync(packageJsonPath);
 			const previousPackageName = `${packageObject.name}-does-not-exist`;
 			assert.throws(() => {
 				utils.ensureDevDependencyExists(packageObject, previousPackageName);
+			});
+		});
+	});
+
+	describe("getPreviousPackageJsonPath", () => {
+		it("Should throw an error", () => {
+			const packageJsonPath = path.join(__dirname, "mockPackage.json");
+			const packageObject: PackageJson = readJsonSync(packageJsonPath);
+			const previousPackageName = `${packageObject.name}-previous`;
+			const previousBasePath = path.join("node_modules", previousPackageName);
+			assert.throws(() => {
+				utils.getPreviousPackageJsonPath(previousBasePath);
 			});
 		});
 	});
