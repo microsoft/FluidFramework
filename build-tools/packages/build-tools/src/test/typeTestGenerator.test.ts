c7b8e143ca27bffb6abc45ff59682a0f7ffc51f7
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-02-27 14:09:57 -0500

    Updated build to copy mock package

--- build-tools/packages/build-tools/src/test/typeTestGenerator.test.ts
+++ build-tools/packages/build-tools/src/test/typeTestGenerator.test.ts
@@ -1,13 +1,25 @@
+import { strict as assert } from "assert";
 import * as utils from "../type-test-generator/typeTestUtils";
 import { readJsonSync } from "fs-extra";
+import * as path from "path";
 import { PackageJson } from "../common/npmPackage";
 
 describe("typeTestUtils", () => {
 	describe("ensureDevDependencyExists", () => {
-		it.only("Should not throw an error if dev dependency exists", () => {
-			const packageObject: PackageJson = readJsonSync("mockPackage.json");
+		it("Should not throw an error if dev dependency exists", () => {
+			const packageJsonPath = path.join(__dirname, "mockPackage.json");
+			const packageObject: PackageJson = readJsonSync(packageJsonPath);
 			const previousPackageName = `${packageObject.name}-previous`;
 			utils.ensureDevDependencyExists(packageObject, previousPackageName);
+		});
+
+		it("Should throw an error if dev dependency does not exist", () => {
+			const packageJsonPath = path.join(__dirname, "mockPackage.json");
+			const packageObject: PackageJson = readJsonSync(packageJsonPath);
+			const previousPackageName = `${packageObject.name}-does-not-exist`;
+			assert.throws(() => {
+				utils.ensureDevDependencyExists(packageObject, previousPackageName);
+			});
 		});
 	});
 });
