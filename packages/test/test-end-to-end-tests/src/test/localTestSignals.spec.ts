5834820ba3c4c0565bae2f5856c9efd205a094ee
Willie Habimana <whabimana@microsoft.com>
2024-02-28 14:24:09 -0800

    remove lodash

--- packages/test/test-end-to-end-tests/src/test/localTestSignals.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/localTestSignals.spec.ts
@@ -4,7 +4,6 @@
  */
 
 import { strict as assert } from "assert";
-import _ from "lodash";
 import { IInboundSignalMessage } from "@fluidframework/runtime-definitions";
 import {
 	ITestObjectProvider,
