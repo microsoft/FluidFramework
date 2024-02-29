b9d079461103169134b8746756471441e5a31c4f
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2024-02-29 21:57:44 +0000

    remove mocha from tsconfig and fix import

--- experimental/PropertyDDS/packages/property-binder/src/internal/activationQueryCacheHelper.ts
+++ experimental/PropertyDDS/packages/property-binder/src/internal/activationQueryCacheHelper.ts
@@ -8,7 +8,7 @@ import { TypeIdHelper } from "@fluid-experimental/property-changeset";
 import * as _ from "underscore";
 import { getLocalOrRemoteSchema } from "../data_binder/internalUtils.js";
 import { DataBinder, DataBinding } from "../index.js";
-import { ExtractedContext } from "@fluid-experimental/property-changeset/dist/helpers/typeidHelper";
+import { ExtractedContext } from "@fluid-experimental/property-changeset";
 
 export interface ActivationInfo {
 	dataBinder: DataBinder;
