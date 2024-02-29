647f14bb9b5d719958dce73ce9564648e850e067
Willie Habimana <whabimana@microsoft.com>
2024-02-29 08:57:12 -0800

    higher threshold

--- build-tools/packages/build-cli/src/library/dangerfile.ts
+++ build-tools/packages/build-cli/src/library/dangerfile.ts
@@ -64,7 +64,7 @@ export async function dangerfile(): Promise<void> {
 		?.map((bundle: BundleComparison) => {
 			const totalMetric = bundle.commonBundleMetrics[totalSizeMetricName];
 			const totalParsedSizeDiff = totalMetric.compare.parsedSize - totalMetric.baseline.parsedSize;
-			return totalParsedSizeDiff > 100;
+			return totalParsedSizeDiff > 5120;
 
 		})
 		.reduce((prev: boolean, current: boolean) => {
