4edb304860cb56e14c6b1b00d45f28c2d8592d1f
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-02-29 19:01:54 +0000

    tools

--- temp-tools/patch.mjs
+++ temp-tools/patch.mjs
@@ -15,18 +15,24 @@ function patchMochaConfig(cjsPath) {
 	}
 }
 
-function patchMochaCjs(pkgRoot) {
-	const fromText = `"npm run test:mocha:cjs && npm run test:mocha:esm"`;
+function disableCjsTests(pkgRoot) {
+	const fromTexts = [
+		`"npm run test:mocha:cjs && npm run test:mocha:esm"`,
+		`"npm run test:mocha:esm && npm run test:mocha:cjs"`,
+	];
 	const toText = `"npm run test:mocha:esm && echo skipping cjs to avoid overhead - npm run test:mocha:cjs"`;
-
 	const pkgPath = path.join(pkgRoot, "package.json");
 	let pkgText = fs.readFileSync(pkgPath, "utf8");
-	if (pkgText.indexOf(fromText) !== -1) {
-		console.log(`Patching ${pkgPath}`);
-		pkgText = pkgText.replaceAll(fromText, toText);
-		fs.writeFileSync(pkgPath, pkgText);
+
+	for (const fromText of fromTexts) {
+		if (pkgText.indexOf(fromText) !== -1) {
+			console.log(`Patching ${pkgPath}`);
+			pkgText = pkgText.replaceAll(fromText, toText);
+			fs.writeFileSync(pkgPath, pkgText);
+			return;
+		}
 	}
 }
 
 const pkgRoot = process.cwd();
-patchMochaCjs(pkgRoot);
+disableCjsTests(pkgRoot);
