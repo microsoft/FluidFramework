75b32bd9716a0d2f11ee17b6d2728b6b8d0b4e05
Jason Hartman <jasonha@microsoft.com>
2024-02-29 10:47:34 -0800

    build(client-client-utils): use fluid-tsc (#19858)
    + import file extensions added by ts2esm
    + test ESM when possible (jest gitHash.spec.ts uses rewire which only
    functions for CJS)
    - remove unused browser remap in telemetry-utils
    
    Note: package is internal so has simple exports structure
    browser remap requires custom injected package.json as webpack applies
    browser remap from the context of export destination. (The CJS browser
    remap in root package.json are unused.)
    
    ---------
    
    Co-authored-by: Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>

--- packages/common/client-utils/src/hashFileBrowser.ts
+++ packages/common/client-utils/src/hashFileBrowser.ts
@@ -4,7 +4,7 @@
  */
 
 import * as base64js from "base64-js";
-import { IsoBuffer } from "./bufferBrowser";
+import { IsoBuffer } from "./bufferBrowser.js";
 
 async function digestBuffer(file: IsoBuffer, algorithm: "SHA-1" | "SHA-256"): Promise<Uint8Array> {
 	const hash = await crypto.subtle.digest(algorithm, file);
