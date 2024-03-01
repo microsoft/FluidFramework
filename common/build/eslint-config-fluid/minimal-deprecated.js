8cec8bcb35cd51cf381b5887367d80991f846e21
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-03-01 00:17:34 +0000

    Revert "Reapply "add eslint-plugin-fluid""
    This reverts commit 0a89978d2a8216eaa49078395b270e6dc79a0545.

--- common/build/eslint-config-fluid/minimal-deprecated.js
+++ common/build/eslint-config-fluid/minimal-deprecated.js
@@ -62,8 +62,6 @@ module.exports = {
 		"unused-imports",
 		// Plugin documentation: https://www.npmjs.com/package/eslint-plugin-unicorn
 		"unicorn",
-		// TODO
-		"@fluid-internal/eslint-plugin-fluid",
 	],
 	reportUnusedDisableDirectives: true,
 	ignorePatterns: [
@@ -71,11 +69,6 @@ module.exports = {
 		"**/packageVersion.ts",
 	],
 	rules: {
-		/**
-		 * TODO
-		 */
-		"@fluid-internal/fluid/no-member-release-tags": "error",
-
 		/**
 		 * The @rushstack rules are documented in the package README:
 		 * {@link https://www.npmjs.com/package/@rushstack/eslint-plugin}
