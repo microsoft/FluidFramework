70e88726bfa7753de50d81492445159d835b9e35
seanimam <105244057+seanimam@users.noreply.github.com>
2024-03-06 21:26:21 +0000

    update type on consumer

--- packages/framework/client-logger/external-telemetry/telemetry-manager/common/consumers/appInsightsTelemetryConsumer.ts
+++ packages/framework/client-logger/external-telemetry/telemetry-manager/common/consumers/appInsightsTelemetryConsumer.ts
@@ -1,10 +1,11 @@
 import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 import type { ITelemetryConsumer } from ".";
+import type { IExternalTelemetry } from "../telemetry";
 
 export class AppInsightsTelemetryConsumer implements ITelemetryConsumer {
 	constructor(private readonly appInsightsClient: ApplicationInsights) {}
 
-	consume(event: Record<string, any>) {
+	consume(event: IExternalTelemetry) {
 		this.appInsightsClient.trackEvent({
 			name: event.eventName,
 			properties: event,
