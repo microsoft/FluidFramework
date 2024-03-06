f7ef2f1c42399963b2c1cd8e72ba1b72bac1d1d1
Michael Zhen <112977307+zhenmichael@users.noreply.github.com>
2024-03-06 21:54:18 +0000

    1.3.8 version bump

--- packages/dds/quorum/src/packageVersion.ts
+++ packages/dds/quorum/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-internal/quorum";
-export const pkgVersion = "1.3.7";
+export const pkgVersion = "1.3.8";
--- packages/drivers/fluidapp-odsp-urlResolver/src/packageVersion.ts
+++ packages/drivers/fluidapp-odsp-urlResolver/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluid-tools/fluidapp-odsp-urlresolver";
-export const pkgVersion = "1.3.7";
+export const pkgVersion = "1.3.8";
