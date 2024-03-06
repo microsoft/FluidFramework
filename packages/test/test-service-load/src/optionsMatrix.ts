4b3f722a8cfaf90f22b6a9be1d7512a910187ca3
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-03-06 20:58:49 +0000

    build(test-service-load): use node16 module resolution

--- packages/test/test-service-load/src/optionsMatrix.ts
+++ packages/test/test-service-load/src/optionsMatrix.ts
@@ -19,7 +19,7 @@ import { ILoaderOptions } from "@fluidframework/container-loader";
 import { LoggingError } from "@fluidframework/telemetry-utils";
 import { TestDriverTypes } from "@fluidframework/test-driver-definitions";
 import { ConfigTypes } from "@fluidframework/core-interfaces";
-import { ILoadTestConfig, OptionOverride } from "./testConfigFile";
+import { ILoadTestConfig, OptionOverride } from "./testConfigFile.js";
 
 const loaderOptionsMatrix: OptionsMatrix<ILoaderOptions> = {
 	cache: booleanCases,
