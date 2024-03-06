605f7828dc3618ead34fabd6b4aa4c24edeb4660
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-03-06 21:39:25 +0000

    test: Add test

--- packages/dds/tree/src/test/simple-tree/toFlexSchema.spec.ts
+++ packages/dds/tree/src/test/simple-tree/toFlexSchema.spec.ts
@@ -8,9 +8,11 @@ import { strict as assert } from "node:assert";
 // eslint-disable-next-line import/no-internal-modules
 import { toFlexSchema } from "../../simple-tree/toFlexSchema.js";
 import { SchemaFactory } from "../../simple-tree/index.js";
+import { brand } from "../../util/index.js";
+import { schemaIsObjectNode } from "../../feature-libraries/index.js";
 
 describe("toFlexSchema", () => {
-	it("minimal", () => {
+	it("smoke test", () => {
 		const schema = new SchemaFactory("com.example");
 		class A extends schema.object("A", {}) {}
 		toFlexSchema(A);
@@ -26,5 +28,36 @@ describe("toFlexSchema", () => {
 		const schema = new SchemaFactory("com.example");
 		const schema2 = new SchemaFactory("com.example");
 		assert.equal(schema.number, schema2.number);
+	});
+	it("stableName", () => {
+		const schema = new SchemaFactory("test");
+		const testSchema = schema.object("root", {
+			optionalWithoutStableName: schema.optional(schema.number),
+			optionalWithStableName: schema.optional(schema.number, {
+				stableName: "stable-optional",
+			}),
+			requiredWithoutStableName: schema.required(schema.number),
+			requiredWithStableName: schema.required(schema.number, {
+				stableName: "stable-required",
+			}),
+		});
+
+		// Get the schema of the node under the root
+		const result = toFlexSchema(testSchema).nodeSchema.get(brand("test.root"));
+		assert(result !== undefined);
+		assert(schemaIsObjectNode(result));
+		const fields = result.info;
+
+		// For consistency
+		/* eslint-disable @typescript-eslint/dot-notation */
+
+		assert(fields["optionalWithoutStableName"] !== undefined);
+		assert(fields["optionalWithStableName"] === undefined);
+		assert(fields["stable-optional"] !== undefined);
+		assert(fields["requiredWithoutStableName"] !== undefined);
+		assert(fields["requiredWithStableName"] === undefined);
+		assert(fields["stable-required"] !== undefined);
+
+		/* eslint-enable @typescript-eslint/dot-notation */
 	});
 });
