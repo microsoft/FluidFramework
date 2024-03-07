80677b9b29eb7ff058df15c5e24611e5ffbc5a7d
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2024-03-06 17:29:02 -0800

    Apply suggestions from code review
    Co-authored-by: Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>

--- packages/dds/tree/src/simple-tree/schemaFactoryRecursive.ts
+++ packages/dds/tree/src/simple-tree/schemaFactoryRecursive.ts
@@ -43,7 +43,7 @@ export function createFieldSchemaUnsafe<
  * Extends SchemaFactory with utilities for recursive schema.
  *
  * @remarks
- * This is separated from SchemaFactory as these APIs are more experimental, and may be stabilized independently.
+ * This is separated from {@link SchemaFactory} as these APIs are more experimental and may be stabilized independently.
  *
  * The non-recursive versions of the schema building methods will run into several issues when used recursively.
  * Consider the following example:
@@ -58,11 +58,11 @@ export function createFieldSchemaUnsafe<
  * Structurally named schema derive their name from the names of their child types, which is not possible when the type is recursive since its name would include itself.
  * Instead a name must be explicitly provided.
  *
- * 2. The schema accesses itself before its defined.
+ * 2. The schema accesses itself before it's defined.
  * This would be a runtime error if the TypeScript compiler allowed it.
- * This can be fixed by wrapping the type in a function, which also requires explicitly listing the allowed types an an array (`[() => Test]`)
+ * This can be fixed by wrapping the type in a function, which also requires explicitly listing the allowed types in an array (`[() => Test]`).
  *
- * 3. TypeScript fails to infer the recursive type, and fall back to any with the warning or error (depending on the compiler configuration):
+ * 3. TypeScript fails to infer the recursive type and falls back to `any` with the warning or error (depending on the compiler configuration):
  * `'Test' implicitly has type 'any' because it does not have a type annotation and is referenced directly or indirectly in its own initializer.ts(7022)`.
  * This issue is what the specialized recursive schema building methods fix.
  * This fix comes at a cost: to make the recursive cases work, the `extends` clauses had to be removed.
@@ -71,7 +71,7 @@ export function createFieldSchemaUnsafe<
  * This means that new nested {@link Unhydrated} nodes can not be created like `new Test([[]])`.
  * Instead the nested nodes must be created explicitly using the construction like`new Test([new Test([])])`.
  *
- * 4. It is using "POJO" mode since its not explicitly declaring a new class.
+ * 4. It is using "POJO" mode since it's not explicitly declaring a new class.
  * This means that if the schema generated d.ts files replace recursive references with `any`, breaking use of recursive schema across compilation boundaries.
  * This is fixed by explicitly creating a class which extends the returned schema.
  *
@@ -81,15 +81,15 @@ export function createFieldSchemaUnsafe<
  * ```
  *
  * Be very careful when declaring recursive schema.
- * Due to the removed extends clauses, subtly mistake will compile just fine, but cause strange errors when the schema is used.
+ * Due to the removed extends clauses, subtle mistakes will compile just fine but cause strange errors when the schema is used.
  *
- * For example if the square branches around the allowed types are forgotten:
+ * For example if the square brackets around the allowed types are forgotten:
  *
  * ```typescript
  * class Test extends sf.arrayRecursive("Test", () => Test) {} // Bad
  * ```
  * This schema will still compile, and some (but not all) usages of it may look like they work correctly while other usages will produce generally unintelligible compile errors.
- * This issue can be partially mitigated using{@link ValidateRecursiveSchema}.
+ * This issue can be partially mitigated using {@link ValidateRecursiveSchema}.
  * @sealed @beta
  */
 export class SchemaFactoryRecursive<
@@ -248,18 +248,18 @@ export class SchemaFactoryRecursive<
  *
  * @example
  * ```typescript
- * 	class Test extends sf.arrayRecursive("Test", [() => Test]) {}
- *	{
- *		type _check = ValidateRecursiveSchema<typeof Test>;
- *	}
+ * class Test extends sf.arrayRecursive("Test", [() => Test]) {}
+ * {
+ *     type _check = ValidateRecursiveSchema<typeof Test>;
+ * }
  * ```
  * @remarks
  * The type of a recursive schema can be passed to this, and a compile error will be produced for some of the cases which the schema in malformed.
  * This can be used to help mitigate the issue that recursive schema definitions are {@link Unenforced}.
  * If an issue is encountered where a mistake in a recursive schema is made which produces an invalid schema but is not rejected by this checker,
- * it should be considered a bug and updated to handle that case (or have a disclaimer added to these docs that it misses that case).
+ * it should be considered a bug and this should be updated to handle that case (or have a disclaimer added to these docs that it misses that case).
  * @privateRemarks
- * There are probably mistakes this misses: its hard to guess all the wrong things people will accidentally do and defend against them.
+ * There are probably mistakes this misses: it's hard to guess all the wrong things people will accidentally do and defend against them.
  * Hopefully over time this can grow toward being robust, at least for common mistakes.
  * @beta
  */
