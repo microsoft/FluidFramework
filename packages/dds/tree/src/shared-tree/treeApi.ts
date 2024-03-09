81907441c4ceeea12018e136693ec561cc990628
Noah Encke <78610362+noencke@users.noreply.github.com>
2024-03-08 16:34:24 -0800

    Add top-level transaction APIs to SharedTree (#20033)
    ## Description
    
    This PR adds `Tree.runTransaction` to allow synchronous transactions to
    be easily run from either a node or a tree view. This is a small
    departure from the existing functions on the global `Tree` object:
    1. It is the first non-read-only function
    2. It requires types that are exposed in the shared-tree directory,
    rather than solely in the simple-tree directory.
    
    The shared-tree directory depends on simple-tree, not the other way
    around, and therefore the `Tree` API needed to be lifted into
    shared-tree from simple-tree to accommodate departure 2, above. I chose
    to do this by creating a second `Tree` object in shared-tree that wraps
    the `Tree` object in simple-tree and leaves the latter alone. This means
    we don't have to expose many of the helper functions used by `Tree` in
    simple-tree that are otherwise local to simple-tree and would leak up
    into shared-tree. Accordingly, the `Tree` in simple-tree has been
    renamed to just `treeNodeApi`, and the one in shared-tree is now the new
    `Tree`. Tests that directly use `treeNodeApi` still alias it to `Tree`
    because it is recognizable.
    
    Also:
    - I moved the `Tree.on` tests into the same file as the rest of the
    simple-tree `Tree` tests (this gets rid of node.spec.ts).
    - I lifted the `getView` test helper into shared-tree from simple-tree
    since it's used by both treeApi.spec test files.
    
    Support for constraints in transactions will come in a later PR.

---
+++ packages/dds/tree/src/shared-tree/treeApi.ts
@@ -0,0 +1,104 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+import { assert } from "@fluidframework/core-utils";
+import { fail } from "../util/index.js";
+import { Context } from "../feature-libraries/index.js";
+import { TreeNode, TreeNodeApi, TreeView, treeNodeApi, getFlexNode } from "../simple-tree/index.js";
+import { SchematizingSimpleTreeView } from "./schematizingTreeView.js";
+import { TreeCheckout } from "./treeCheckout.js";
+import { contextToTreeView } from "./treeView.js";
+
+/**
+ * Provides various functions for interacting with {@link TreeNode}s.
+ * @public
+ */
+export interface TreeApi extends TreeNodeApi {
+	/**
+	 * Apply one or more edits to the tree as a single atomic unit.
+	 * @param node - The node that will be passed to `transaction`.
+	 * This is typically the root node of the subtree that will be modified by the transaction.
+	 * @param transaction - The function to run as the body of the transaction.
+	 * This function is passed the provided `node`.
+	 * At any point during the transaction, the function may return the value `"rollback"` to abort the transaction and discard any changes it made so far.
+	 * @remarks
+	 * All of the changes in the transaction are applied synchronously and therefore no other changes (either from this client or from a remote client) can be interleaved with those changes.
+	 * Note that this is guaranteed by Fluid for any sequence of changes that are submitted synchronously, whether in a transaction or not.
+	 * However, using a transaction has the following additional consequences:
+	 * - If reverted (e.g. via an "undo" operation), all the changes in the transaction are reverted together.
+	 * - The internal data representation of a transaction with many changes is generally smaller and more efficient than that of the changes when separate.
+	 *
+	 * Local change events will be emitted for each change as the transaction is being applied.
+	 * If the transaction is cancelled and rolled back, a corresponding change event will also be emitted for the rollback.
+	 */
+	runTransaction<TNode extends TreeNode>(
+		node: TNode,
+		transaction: (node: TNode) => void | "rollback",
+	): void;
+	/**
+	 * Apply one or more edits to the tree as a single atomic unit.
+	 * @param tree - The tree which will be edited by the transaction
+	 * @param transaction - The function to run as the body of the transaction.
+	 * This function is passed the root of the tree.
+	 * At any point during the transaction, the function may return the value `"rollback"` to abort the transaction and discard any changes it made so far.
+	 * @remarks
+	 * All of the changes in the transaction are applied synchronously and therefore no other changes (either from this client or from a remote client) can be interleaved with those changes.
+	 * Note that this is guaranteed by Fluid for any sequence of changes that are submitted synchronously, whether in a transaction or not.
+	 * However, using a transaction has the following additional consequences:
+	 * - If reverted (e.g. via an "undo" operation), all the changes in the transaction are reverted together.
+	 * - The internal data representation of a transaction with many changes is generally smaller and more efficient than that of the changes when separate.
+	 *
+	 * Local change events will be emitted for each change as the transaction is being applied.
+	 * If the transaction is cancelled and rolled back, a corresponding change event will also be emitted for the rollback.
+	 */
+	runTransaction<TRoot>(
+		tree: TreeView<TRoot>,
+		transaction: (root: TRoot) => void | "rollback",
+	): void;
+}
+
+/**
+ * The `Tree` object holds various functions for interacting with {@link TreeNode}s.
+ * @public
+ */
+export const treeApi: TreeApi = {
+	...treeNodeApi,
+	runTransaction<TNode extends TreeNode, TRoot>(
+		treeOrNode: TNode | TreeView<TRoot>,
+		transaction: ((node: TNode) => void | "rollback") | ((root: TRoot) => void | "rollback"),
+	) {
+		if (treeOrNode instanceof SchematizingSimpleTreeView) {
+			const t = transaction as (root: TRoot) => void | "rollback";
+			runTransaction(treeOrNode.checkout, () => t(treeOrNode.root as TRoot));
+		} else {
+			const node = treeOrNode as TNode;
+			const t = transaction as (node: TNode) => void | "rollback";
+			const context = getFlexNode(node).context;
+			assert(context instanceof Context, "Unsupported context");
+			const treeView =
+				contextToTreeView.get(context) ??
+				fail("Expected view to be registered for context");
+
+			runTransaction(treeView.checkout, () => t(node));
+		}
+	},
+};
+
+function runTransaction(checkout: TreeCheckout, transaction: () => void | "rollback"): void {
+	checkout.transaction.start();
+	let result: void | "rollback";
+	try {
+		result = transaction();
+	} catch (e) {
+		// If the transaction has an unhandled error, abort and rollback the transaction but continue to propagate the error.
+		checkout.transaction.abort();
+		throw e;
+	}
+
+	if (result === "rollback") {
+		checkout.transaction.abort();
+	} else {
+		checkout.transaction.commit();
+	}
+}
