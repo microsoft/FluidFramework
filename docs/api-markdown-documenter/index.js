0b7915489cb7e7f61526b3be3fdecedada02479b
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-03-12 18:19:32 +0000

    refactor: Leverage top-level await

--- docs/api-markdown-documenter/index.js
+++ docs/api-markdown-documenter/index.js
@@ -23,34 +23,40 @@ const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const docVersions = versions.params.previousVersions.concat(versions.params.currentVersion);
 
-Promise.all(
-	docVersions.map(async (version) => {
-		const apiReportsDirectoryPath = path.resolve(dirname, "..", "_doc-models", version);
-		const apiDocsDirectoryPath = path.resolve(dirname, "..", "content", "docs", "api", version);
+try {
+	await Promise.all(
+		docVersions.map(async (version) => {
+			const apiReportsDirectoryPath = path.resolve(dirname, "..", "_doc-models", version);
+			const apiDocsDirectoryPath = path.resolve(
+				dirname,
+				"..",
+				"content",
+				"docs",
+				"api",
+				version,
+			);
 
-		// Note: the leading slash in the URI root is important.
-		// It tells Hugo to enterpret the links as relative to the site root, rather than
-		// relative to the document containing the link.
-		// See documentation here: https://gohugo.io/content-management/urls/#relative-urls
-		const uriRootDirectoryPath = `/docs/api/${version}`;
+			// Note: the leading slash in the URI root is important.
+			// It tells Hugo to enterpret the links as relative to the site root, rather than
+			// relative to the document containing the link.
+			// See documentation here: https://gohugo.io/content-management/urls/#relative-urls
+			const uriRootDirectoryPath = `/docs/api/${version}`;
 
-		await renderApiDocumentation(
-			apiReportsDirectoryPath,
-			apiDocsDirectoryPath,
-			uriRootDirectoryPath,
-			version,
-		);
+			await renderApiDocumentation(
+				apiReportsDirectoryPath,
+				apiDocsDirectoryPath,
+				uriRootDirectoryPath,
+				version,
+			);
 
-		console.log(chalk.green(`(${version}) API docs written!`));
-	}),
-).then(
-	() => {
-		console.log(chalk.green("All API docs written!"));
-		process.exit(0);
-	},
-	(error) => {
-		console.error(chalk.red("API docs generation failed due to one or more errors:"));
-		console.error(error);
-		process.exit(1);
-	},
-);
+			console.log(chalk.green(`(${version}) API docs written!`));
+		}),
+	);
+} catch (error) {
+	console.error(chalk.red("API docs generation failed due to one or more errors:"));
+	console.error(error);
+	process.exit(1);
+}
+
+console.log(chalk.green("All API docs written!"));
+process.exit(0);
