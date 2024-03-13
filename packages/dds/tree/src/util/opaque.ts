0c65f994c3326330b3a66220e1dc3919584b6cc7
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2024-03-13 14:19:46 -0700

    Fix headers

--- packages/dds/tree/src/util/opaque.ts
+++ packages/dds/tree/src/util/opaque.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { isAny } from "./typeCheck.js";
 import { Brand, BrandedType, ValueFromBranded, NameFromBranded } from "./brand.js";
 
--- packages/dds/tree/src/util/typeboxBrand.ts
+++ packages/dds/tree/src/util/typeboxBrand.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { NumericOptions, TUnsafe, Type } from "@sinclair/typebox";
 
 /**
