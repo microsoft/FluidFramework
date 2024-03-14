80cf3d244506391d81ff25636c64b2b65ef9a737
Tyler Butler <tylerbu@microsoft.com>
2024-03-14 11:26:24 -0700

    formatting

--- azure/packages/test/scenario-runner/src/AzureClientRunner.ts
+++ azure/packages/test/scenario-runner/src/AzureClientRunner.ts
@@ -1,7 +1,7 @@
 /*!
-* Copyright (c) Microsoft Corporation and contributors. All rights reserved.
-* Licensed under the MIT License.
-*/
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
 
 import { TypedEventEmitter } from "@fluid-internal/client-utils";
 import { AzureClient } from "@fluidframework/azure-client";
