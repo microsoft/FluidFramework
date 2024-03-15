f770bcc7e52fc05a3a16bc42463a45aa69b24281
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-03-15 20:46:48 +0000

    chore: Add directory exports

---
+++ tools/api-markdown-documenter/src/documentation-domain-to-html/index.ts
@@ -0,0 +1,6 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export { documentToHtml, documentationNodeToHtml, documentationNodesToHtml } from "./ToHtml.js";
