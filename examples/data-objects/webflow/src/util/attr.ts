a0702de078d11571b83dc542375ab12da6d73796
Tyler Butler <tyler@tylerbutler.com>
2024-03-14 19:19:39 -0700

    webflow: group manually since index.js exports something itself

--- examples/data-objects/webflow/src/util/attr.ts
+++ examples/data-objects/webflow/src/util/attr.ts
@@ -4,8 +4,9 @@
  */
 
 import { ISegment } from "@fluidframework/sequence";
-import { emptyObject } from "./index.js";
 import { areStringsEquivalent } from "./string.js";
+
+import { emptyObject } from "./index.js";
 
 export interface IHTMLAttributes {
 	src?: string;
