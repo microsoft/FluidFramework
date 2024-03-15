aeeff4da7b0d1cf223f6a74e9cc179d23542d4eb
Jason Hartman <jasonha@microsoft.com>
2024-03-15 15:02:22 -0700

    test(client-devtools-view): fix jest testing use Node16 resolution and build jest tests for CJS run jest against the pre-built javascript
    ts-jest settings are commented out as a precaution.

--- packages/tools/devtools/devtools-view/jest.config.cjs
+++ packages/tools/devtools/devtools-view/jest.config.cjs
@@ -5,7 +5,25 @@
 
 module.exports = {
 	preset: "ts-jest",
-	roots: ["<rootDir>/src"],
+	// Run jest against pre-built CommonJS javascript.
+	// 1. ts-jest does not handle Node16 config and use of .cts files sufficiently. It
+	//    is possible to run directly against the TypeScript source (<rootDir>/src),
+	//    if tsconfig is changed to CommonJs+Bundler.
+	// 2. ESNext+Bundler with "cross-env NODE_OPTIONS=--experimental-vm-modules" is also
+	//    not sufficient to address .cts issues even with a patch to address
+	//    https://github.com/kulshekhar/ts-jest/issues/3996.
+	//
+	// Two other options were not investigated:
+	// A. expose internal/test exports for components directly tested here and use self-reference
+	//    (see https://nodejs.org/api/packages.html#self-referencing-a-package-using-its-name),
+	//    which should use the built package files (remove references in tsconfig for good
+	//    measure).
+	// B. Replace ts-jest with babel.
+	//
+	// It is good to run against the pre-built source as we know that we are testing exactly the
+	// production source. Testing against ESM would be preferred. So option A has merit even if
+	// other things come along.
+	roots: ["<rootDir>/dist"],
 	reporters: [
 		"default",
 		[
@@ -17,12 +35,13 @@ module.exports = {
 		],
 	],
 	transform: {
-		"^.+\\.c?tsx?$": [
-			"ts-jest",
-			{
-				tsconfig: "src/test/jest/tsconfig.json",
-			},
-		],
+		// uncomment when attempting run against TypeScript source:
+		// "^.+\\.c?tsx?$": [
+		// 	"ts-jest",
+		// 	{
+		// 		tsconfig: "src/test/jest/tsconfig.json",
+		// 	},
+		// ],
 	},
 	// This regex will match source (TypeScript) or transpiled (JavaScript) files.
 	// Change `roots` to select between those.
@@ -33,6 +52,6 @@ module.exports = {
 		"^(\\.{1,2}/.*)\\.c?js$": "$1",
 	},
 	moduleFileExtensions: ["ts", "tsx", "cts", "mts", "js", "cjs", "mjs", "jsx", "json", "node"],
-	coveragePathIgnorePatterns: ["/node_modules/", "/src/test/"],
+	coveragePathIgnorePatterns: ["/node_modules/", "/src/test/", "/dist/test/"],
 	testEnvironment: "jsdom",
 };
