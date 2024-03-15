85cd42fd166428ce7d144e9c2edada2919766210
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-03-15 23:40:52 +0000

    test: Add unit tests

---
+++ tools/api-markdown-documenter/src/documentation-domain-to-html/test/TableToHtml.test.ts
@@ -0,0 +1,122 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+import {
+	TableBodyCellNode,
+	TableBodyRowNode,
+	TableHeaderCellNode,
+	TableHeaderRowNode,
+	TableNode,
+} from "../../documentation-domain/index.js";
+import { assertExpectedHtml } from "./Utilities.js";
+
+describe("Table HTML rendering tests", () => {
+	it("Empty table", () => {
+		assertExpectedHtml(TableNode.Empty, "<table />");
+	});
+
+	it("Simple table without header", () => {
+		const input = new TableNode([
+			new TableBodyRowNode([
+				TableBodyCellNode.createFromPlainText("Cell 1A"),
+				TableBodyCellNode.createFromPlainText("Cell 1B"),
+				TableBodyCellNode.createFromPlainText("Cell 1C"),
+			]),
+			new TableBodyRowNode([
+				TableBodyCellNode.createFromPlainText("Cell 2A"),
+				TableBodyCellNode.createFromPlainText("Cell 2B"),
+			]),
+		]);
+
+		const expected = [
+			"<table>",
+			"<tbody>",
+			"<tr>",
+			"<td>",
+			"Cell 1A",
+			"</td>",
+			"<td>",
+			"Cell 1B",
+			"</td>",
+			"<td>",
+			"Cell 1C",
+			"</td>",
+			"</tr>",
+			"<tr>",
+			"<td>",
+			"Cell 2A",
+			"</td>",
+			"<td>",
+			"Cell 2B",
+			"</td>",
+			"</tr>",
+			"</tbody>",
+			"</table>",
+		].join("");
+
+		assertExpectedHtml(input, expected);
+	});
+
+	it("Simple table with header", () => {
+		const input = new TableNode(
+			[
+				new TableBodyRowNode([
+					TableBodyCellNode.createFromPlainText("Cell 1A"),
+					TableBodyCellNode.createFromPlainText("Cell 1B"),
+				]),
+				new TableBodyRowNode([
+					TableBodyCellNode.createFromPlainText("Cell 2A"),
+					TableBodyCellNode.createFromPlainText("Cell 2B"),
+					TableBodyCellNode.createFromPlainText("Cell 2C"),
+				]),
+			],
+			/* headingRow: */ new TableHeaderRowNode([
+				TableHeaderCellNode.createFromPlainText("Header A"),
+				TableHeaderCellNode.createFromPlainText("Header B"),
+				TableHeaderCellNode.createFromPlainText("Header C"),
+			]),
+		);
+
+		const expected = [
+			"<table>",
+			"<thead>",
+			"<tr>",
+			"<th>",
+			"Header A",
+			"</th>",
+			"<th>",
+			"Header B",
+			"</th>",
+			"<th>",
+			"Header C",
+			"</th>",
+			"</tr>",
+			"</thead>",
+			"<tbody>",
+			"<tr>",
+			"<td>",
+			"Cell 1A",
+			"</td>",
+			"<td>",
+			"Cell 1B",
+			"</td>",
+			"</tr>",
+			"<tr>",
+			"<td>",
+			"Cell 2A",
+			"</td>",
+			"<td>",
+			"Cell 2B",
+			"</td>",
+			"<td>",
+			"Cell 2C",
+			"</td>",
+			"</tr>",
+			"</tbody>",
+			"</table>",
+		].join("");
+
+		assertExpectedHtml(input, expected);
+	});
+});
