c849ce286f1db20f76466daf64c349d393fd8a13
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-03-16 04:07:10 +0000

    test: Update test infra

--- tools/api-markdown-documenter/src/documentation-domain-to-html/test/Utilities.ts
+++ tools/api-markdown-documenter/src/documentation-domain-to-html/test/Utilities.ts
@@ -25,6 +25,7 @@ export function testTransformation(
 /**
  * Since HTML content could contain formatting, which we don't want to validate, this function can be used to strip
  * formatting from a raw HTML string by parsing it into a HAST tree and then converting it back to HTML.
+ * @deprecated Don't use.
  */
 export function sanitizeExpected(rawHtml: string): string {
 	const tree = fromHtml(rawHtml, { fragment: true });
@@ -32,7 +33,8 @@ export function sanitizeExpected(rawHtml: string): string {
 }
 
 /**
- * TODO
+ * Don't use.
+ * @deprecated This does weird things in some cased. Remove before checking in. Use {@link assertTransformation} instead.
  */
 export function assertExpectedHtml(
 	input: DocumentationNode,
@@ -42,4 +44,17 @@ export function assertExpectedHtml(
 	const actual = testTransformation(input, transformationConfig);
 	const sanitizedExpected = sanitizeExpected(expectedHtml);
 	expect(toHtml(actual)).to.equal(sanitizedExpected);
+}
+
+/**
+ * Runs the {@link documentationNodeToHtml} transformation on the input and asserts the output matches the expected
+ * `hast` tree.
+ */
+export function assertTransformation(
+	input: DocumentationNode,
+	expected: HastNodes,
+	transformationConfig?: TransformationConfig,
+): void {
+	const actual = testTransformation(input, transformationConfig);
+	expect(actual).to.deep.equal(expected);
 }
