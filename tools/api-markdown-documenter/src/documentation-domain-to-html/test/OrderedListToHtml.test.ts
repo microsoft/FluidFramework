5b5e69b37a7a93c6032d52e25ddf75d455bbf6fe
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-03-16 04:27:08 +0000

    test: Add unit tests

---
+++ tools/api-markdown-documenter/src/documentation-domain-to-html/test/OrderedListToHtml.test.ts
@@ -0,0 +1,30 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+import { h } from "hastscript";
+
+import { OrderedListNode } from "../../documentation-domain/index.js";
+import { assertTransformation } from "./Utilities.js";
+
+describe("OrderedListNode HTML rendering tests", () => {
+	it("Empty list", () => {
+		assertTransformation(OrderedListNode.Empty, h("ol"));
+	});
+
+	it("Simple list", () => {
+		const text1 = "Item 1";
+		const text2 = "Item 2";
+		const text3 = "Item 3";
+
+		const input = OrderedListNode.createFromPlainTextEntries([text1, text2, text3]);
+
+		const expected = h("ol", [
+			h("li", { type: "text", value: text1 }),
+			h("li", { type: "text", value: text2 }),
+			h("li", { type: "text", value: text3 }),
+		]);
+
+		assertTransformation(input, expected);
+	});
+});
