microsoft/FluidFramework

commit b6d03eac33cf7cf5adadb1550b4f0b333676f055

remove user details from quorum members (#19000)
Sets the user property of the IScribeProtocolState to null to remove
this information from all checkpoints
This is so we do not store any personably identifiable information
stored with the metadata in our checkpoints

server/routerlicious/packages/protocol-base/src/protocol.ts
@@ -154,10 +154,27 @@ export class ProtocolOpHandler implements IProtocolHandler {
 	public getProtocolState(): IScribeProtocolState {
 		// return a new object every time
 		// this ensures future state changes will not affect outside callers
+
+		const snapshot = this._quorum.snapshot();
+
+		const quorumMembers = snapshot.members;
+
+		// Removing any identifying client information
+		quorumMembers.forEach((member) => {
+			member[1] = {
+				...member[1],
+				client: {
+					...member[1].client,
+					user: { id: "" },
+				},
+			};
+		});
+
 		return {
 			sequenceNumber: this.sequenceNumber,
 			minimumSequenceNumber: this.minimumSequenceNumber,
 			...this._quorum.snapshot(),
+			members: quorumMembers,
 		};
 	}
 }

commit 04a2cc9ee88d4dbfc14bf44320456aa01749990c

server: provided user was not an azure user fixes (#20150)
## Description

This PR _begins_ the process for fixing the `Provided user was not an
"AzureUser"` bug, which was caused by #19000. However, we cannot just
revert #19000 because it was implemented to resolve privacy and data
residency concerns.


![image](https://github.com/microsoft/FluidFramework/assets/14362200/948f8823-895e-4945-8e4a-b84dd6cdbfc7)

[ADO
7422](https://dev.azure.com/fluidframework/internal/_workitems/edit/7422)

Included here is part 1 & 2 of the code changes needed to resolve the
incident. Configs added are pretty self-explanatory:

- `scrubUserDataInGlobalCheckpoints`: this is going to stay enabled
- `scrubUserDataInLocalCheckpoints`: this is going to eventually be
disabled once work lands to enable a 24hr TTL in local db
- `scrubUserDataInSummaries`: this is going to stay disabled, but
including for completeness

Otherwise, the main change is to validate the quorum member information
on Scribe boot in an effort to recover from a global DB quorum read by
loading checkpoint from the summary instead.

### Follow-up Work

Follow-up work will include making some changes to how we create/update
the Checkpoint DB TTL index. Once that is complete we can stop scrubbing
local checkpoint user data.

server/routerlicious/kubernetes/routerlicious/templates/fluid-configmap.yaml
@@ -186,7 +186,10 @@ data:
             "verifyLastOpPersistence": {{ .Values.scribe.verifyLastOpPersistence }},
             "restartOnCheckpointFailure": {{ .Values.scribe.restartOnCheckpointFailure }},
             "disableTransientTenantFiltering": {{ .Values.scribe.disableTransientTenantFiltering }},
-            "maxLogtailLength": {{ .Values.scribe.maxLogtailLength }}
+            "maxLogtailLength": {{ .Values.scribe.maxLogtailLength }},
+            "scrubUserDataInGlobalCheckpoints": {{ .Values.scribe.scrubUserDataInGlobalCheckpoints }},
+            "scrubUserDataInLocalCheckpoints": {{ .Values.scribe.scrubUserDataInLocalCheckpoints }},
+            "scrubUserDataInSummaries": {{ .Values.scribe.scrubUserDataInSummaries }}
         },
         "system": {
             "httpServer": {

server/routerlicious/kubernetes/routerlicious/values.yaml
@@ -103,6 +103,9 @@ scribe:
     maxTime: 60000
     maxMessages: 500
   restartOnCheckpointFailure: true
+  scrubUserDataInGlobalCheckpoints: false
+  scrubUserDataInLocalCheckpoints: false
+  scrubUserDataInSummaries: false
 
 riddler:
   name: riddler

server/routerlicious/packages/lambdas/src/scribe/checkpointManager.ts
@@ -17,6 +17,7 @@ import {
 } from "@fluidframework/server-services-core";
 import { getLumberBaseProperties, Lumberjack } from "@fluidframework/server-services-telemetry";
 import { ICheckpointManager } from "./interfaces";
+import { isLocalCheckpoint } from "./utils";
 
 /**
  * MongoDB specific implementation of ICheckpointManager
@@ -49,7 +50,7 @@ export class CheckpointManager implements ICheckpointManager {
 		globalCheckpointOnly: boolean,
 		markAsCorrupt: boolean = false,
 	) {
-		const isLocalCheckpoint = !noActiveClients && !globalCheckpointOnly;
+		const isLocal = isLocalCheckpoint(noActiveClients, globalCheckpointOnly);
 		if (this.getDeltasViaAlfred) {
 			if (pending.length > 0 && this.verifyLastOpPersistence) {
 				// Verify that the last pending op has been persisted to op storage
@@ -109,7 +110,7 @@ export class CheckpointManager implements ICheckpointManager {
 				this.tenantId,
 				"scribe",
 				checkpoint,
-				isLocalCheckpoint,
+				isLocal,
 				markAsCorrupt,
 			);
 		} else {
@@ -149,7 +150,7 @@ export class CheckpointManager implements ICheckpointManager {
 				this.tenantId,
 				"scribe",
 				checkpoint,
-				isLocalCheckpoint,
+				isLocal,
 				markAsCorrupt,
 			);
 

server/routerlicious/packages/lambdas/src/scribe/lambda.ts
@@ -48,7 +48,7 @@ import {
 	DocumentCheckpointManager,
 } from "../utils";
 import { ICheckpointManager, IPendingMessageReader, ISummaryWriter } from "./interfaces";
-import { getClientIds, initializeProtocol, sendToDeli } from "./utils";
+import { getClientIds, initializeProtocol, isGlobalCheckpoint, sendToDeli } from "./utils";
 
 /**
  * @internal
@@ -246,6 +246,7 @@ export class ScribeLambda implements IPartitionLambda {
 							try {
 								const scribeCheckpoint = this.generateScribeCheckpoint(
 									this.lastOffset,
+									this.serviceConfiguration.scribe.scrubUserDataInSummaries,
 								);
 								const operation =
 									value.operation as ISequencedDocumentAugmentedMessage;
@@ -361,7 +362,10 @@ export class ScribeLambda implements IPartitionLambda {
 						enableServiceSummaryForTenant
 					) {
 						const operation = value.operation as ISequencedDocumentAugmentedMessage;
-						const scribeCheckpoint = this.generateScribeCheckpoint(this.lastOffset);
+						const scribeCheckpoint = this.generateScribeCheckpoint(
+							this.lastOffset,
+							this.serviceConfiguration.scribe.scrubUserDataInSummaries,
+						);
 						try {
 							const summaryResponse = await this.summaryWriter.writeServiceSummary(
 								operation,
@@ -478,7 +482,15 @@ export class ScribeLambda implements IPartitionLambda {
 		skipKafkaCheckpoint?: boolean,
 	) {
 		// Get checkpoint context
-		const checkpoint = this.generateScribeCheckpoint(message.offset);
+		const checkpoint = this.generateScribeCheckpoint(
+			message.offset,
+			isGlobalCheckpoint(
+				this.documentCheckpointManager.getNoActiveClients(),
+				this.globalCheckpointOnly,
+			)
+				? this.serviceConfiguration.scribe.scrubUserDataInGlobalCheckpoints
+				: this.serviceConfiguration.scribe.scrubUserDataInLocalCheckpoints,
+		);
 		this.documentCheckpointManager.updateCheckpointMessages(message);
 
 		// write the checkpoint with the current up-to-date state
@@ -606,8 +618,8 @@ export class ScribeLambda implements IPartitionLambda {
 		this.pendingMessages = new Deque(pendingOps);
 	}
 
-	private generateScribeCheckpoint(logOffset: number): IScribe {
-		const protocolState = this.protocolHandler.getProtocolState();
+	private generateScribeCheckpoint(logOffset: number, scrubUserData = false): IScribe {
+		const protocolState = this.protocolHandler.getProtocolState(scrubUserData);
 		const checkpoint: IScribe = {
 			lastSummarySequenceNumber: this.lastSummarySequenceNumber,
 			lastClientSummaryHead: this.lastClientSummaryHead,
@@ -835,7 +847,15 @@ export class ScribeLambda implements IPartitionLambda {
 
 	private readonly idleTimeCheckpoint = (initialScribeCheckpointMessage: IQueuedMessage) => {
 		if (initialScribeCheckpointMessage) {
-			const checkpoint = this.generateScribeCheckpoint(initialScribeCheckpointMessage.offset);
+			const checkpoint = this.generateScribeCheckpoint(
+				initialScribeCheckpointMessage.offset,
+				isGlobalCheckpoint(
+					this.documentCheckpointManager.getNoActiveClients(),
+					this.globalCheckpointOnly,
+				)
+					? this.serviceConfiguration.scribe.scrubUserDataInGlobalCheckpoints
+					: this.serviceConfiguration.scribe.scrubUserDataInLocalCheckpoints,
+			);
 			this.checkpointCore(checkpoint, initialScribeCheckpointMessage, this.clearCache);
 			const checkpointResult = `Writing checkpoint. Reason: IdleTime`;
 			const checkpointInfo = this.documentCheckpointManager.getCheckpointInfo();

server/routerlicious/packages/lambdas/src/scribe/lambdaFactory.ts
@@ -44,7 +44,7 @@ import { CheckpointManager } from "./checkpointManager";
 import { ScribeLambda } from "./lambda";
 import { SummaryReader } from "./summaryReader";
 import { SummaryWriter } from "./summaryWriter";
-import { getClientIds, initializeProtocol, sendToDeli } from "./utils";
+import { getClientIds, initializeProtocol, isCheckpointQuorumScrubbed, sendToDeli } from "./utils";
 import { ILatestSummaryState } from "./interfaces";
 import { PendingMessageReader } from "./pendingMessageReader";
 
@@ -189,15 +189,26 @@ export class ScribeLambdaFactory
 			throw error;
 		}
 
-		if (document.scribe === undefined || document.scribe === null) {
+		const useDefaultCheckpointForNewDocument =
+			document.scribe === undefined || document.scribe === null;
+		const checkpointIsBlank = document.scribe === "";
+		const checkpointQuorumIsScrubbed = isCheckpointQuorumScrubbed(document.scribe);
+		const useLatestSummaryCheckpointForExistingDocument =
+			checkpointIsBlank || checkpointQuorumIsScrubbed;
+
+		if (useDefaultCheckpointForNewDocument) {
 			// Restore scribe state if not present in the cache. Mongodb casts undefined as null so we are checking
 			// both to be safe. Empty sring denotes a cache that was cleared due to a service summary
 			const message = "New document. Setting empty scribe checkpoint";
 			context.log?.info(message, { messageMetaData });
 			Lumberjack.info(message, lumberProperties);
 			lastCheckpoint = DefaultScribe;
-		} else if (document.scribe === "") {
-			const message = "Existing document. Fetching checkpoint from summary";
+		} else if (useLatestSummaryCheckpointForExistingDocument) {
+			const message = `Existing document${
+				!checkpointIsBlank && checkpointQuorumIsScrubbed
+					? " with invalid quorum members"
+					: ""
+			}. Fetching checkpoint from summary`;
 			context.log?.info(message, { messageMetaData });
 			Lumberjack.info(message, lumberProperties);
 			if (!latestSummary.fromSummary) {
@@ -205,6 +216,12 @@ export class ScribeLambdaFactory
 				Lumberjack.error(`Summary can't be fetched`, lumberProperties);
 				lastCheckpoint = DefaultScribe;
 			} else {
+				if (isCheckpointQuorumScrubbed(latestSummary.scribe)) {
+					Lumberjack.error(
+						"Quorum from summary is invalid. Continuing.",
+						lumberProperties,
+					);
+				}
 				lastCheckpoint = JSON.parse(latestSummary.scribe);
 				opMessages = latestSummary.messages;
 				// Since the document was originated elsewhere or cache was cleared, logOffset info is irrelavant.

server/routerlicious/packages/lambdas/src/scribe/utils.ts
@@ -8,11 +8,13 @@ import {
 	IDocumentMessage,
 	IDocumentSystemMessage,
 	IProtocolState,
+	IUser,
 } from "@fluidframework/protocol-definitions";
 import {
 	IProducer,
 	IRawOperationMessage,
 	RawOperationType,
+	type IScribe,
 } from "@fluidframework/server-services-core";
 
 export const initializeProtocol = (protocolState: IProtocolState): ProtocolOpHandler =>
@@ -50,4 +52,42 @@ export const sendToDeli = (
 
 export const getClientIds = (protocolState: IProtocolState, clientCount: number) => {
 	return protocolState.members.slice(0, clientCount).map((member) => member[0]);
+};
+
+/**
+ * Whether to write checkpoint to local db.
+ * @param noActiveClients - whether there are any active clients
+ * @param globalCheckpointOnly - whether to always write checkpoints to global db
+ * @returns whether to write checkpoint to local db
+ */
+export const isLocalCheckpoint = (noActiveClients: boolean, globalCheckpointOnly: boolean) => {
+	return !isGlobalCheckpoint(noActiveClients, globalCheckpointOnly);
+};
+/**
+ * Whether to write checkpoint to global db.
+ * @param noActiveClients - whether there are any active clients
+ * @param globalCheckpointOnly - whether to always write checkpoints to global db
+ * @returns whether to write checkpoint to global db
+ */
+export const isGlobalCheckpoint = (noActiveClients: boolean, globalCheckpointOnly: boolean) => {
+	return noActiveClients || globalCheckpointOnly;
+};
+
+/**
+ * Whether the quorum members represented in the checkpoint's protocol state have had their user data scrubbed
+ * for privacy compliance.
+ */
+export const isCheckpointQuorumScrubbed = (stringifiedCheckpoint: string): boolean => {
+	if (!stringifiedCheckpoint) {
+		return false;
+	}
+	const checkpoint: IScribe = JSON.parse(stringifiedCheckpoint);
+	for (const [, sequencedClient] of checkpoint.protocolState.members) {
+		const user: IUser = sequencedClient.client.user;
+		// User information was scrubbed.
+		if (!user.id) {
+			return true;
+		}
+	}
+	return false;
 };

server/routerlicious/packages/protocol-base/src/protocol.ts
@@ -150,31 +150,30 @@ export class ProtocolOpHandler implements IProtocolHandler {
 
 	/**
 	 * Gets the scribe protocol state
+	 * @param scrubUserData - whether to remove all user data from the quorum members. CAUTION: this will corrupt the quorum if used in a summary.
 	 */
-	public getProtocolState(): IScribeProtocolState {
+	public getProtocolState(scrubUserData = false): IScribeProtocolState {
 		// return a new object every time
 		// this ensures future state changes will not affect outside callers
-
 		const snapshot = this._quorum.snapshot();
 
-		const quorumMembers = snapshot.members;
-
-		// Removing any identifying client information
-		quorumMembers.forEach((member) => {
-			member[1] = {
-				...member[1],
-				client: {
-					...member[1].client,
-					user: { id: "" },
-				},
-			};
-		});
+		if (scrubUserData) {
+			// In place, remove any identifying client information
+			snapshot.members.forEach((member) => {
+				member[1] = {
+					...member[1],
+					client: {
+						...member[1].client,
+						user: { id: "" },
+					},
+				};
+			});
+		}
 
 		return {
 			sequenceNumber: this.sequenceNumber,
 			minimumSequenceNumber: this.minimumSequenceNumber,
-			...this._quorum.snapshot(),
-			members: quorumMembers,
+			...snapshot,
 		};
 	}
 }

server/routerlicious/packages/routerlicious/src/scribe/index.ts
@@ -142,10 +142,25 @@ export async function scribeCreate(
 
 	const externalOrdererUrl = config.get("worker:serverUrl");
 	const enforceDiscoveryFlow: boolean = config.get("worker:enforceDiscoveryFlow");
+	const scrubUserDataInGlobalCheckpoints: boolean =
+		config.get("scribe:scrubUserDataInGlobalCheckpoints") ??
+		DefaultServiceConfiguration.scribe.scrubUserDataInGlobalCheckpoints;
+	const scrubUserDataInLocalCheckpoints: boolean =
+		config.get("scribe:scrubUserDataInLocalCheckpoints") ??
+		DefaultServiceConfiguration.scribe.scrubUserDataInLocalCheckpoints;
+	const scrubUserDataInSummaries: boolean =
+		config.get("scribe:scrubUserDataInSummaries") ??
+		DefaultServiceConfiguration.scribe.scrubUserDataInSummaries;
 	const serviceConfiguration: IServiceConfiguration = {
 		...DefaultServiceConfiguration,
 		externalOrdererUrl,
 		enforceDiscoveryFlow,
+		scribe: {
+			...DefaultServiceConfiguration.scribe,
+			scrubUserDataInGlobalCheckpoints,
+			scrubUserDataInLocalCheckpoints,
+			scrubUserDataInSummaries,
+		},
 	};
 
 	const checkpointService = new core.CheckpointService(

server/routerlicious/packages/services-core/src/configuration.ts
@@ -133,6 +133,15 @@ export interface IScribeServerConfiguration {
 
 	// Controls how often scribe should checkpoint
 	checkpointHeuristics: ICheckpointHeuristicsServerConfiguration;
+
+	// Enables scrubbing user data from protocol state quorum in Summaries
+	scrubUserDataInSummaries: boolean;
+
+	// Enables scrubbing user data from protocol state quorum in local checkpoints
+	scrubUserDataInLocalCheckpoints: boolean;
+
+	// Enables scrubbing user data from protocol state quorum in global checkpoints
+	scrubUserDataInGlobalCheckpoints: boolean;
 }
 
 /**
@@ -274,6 +283,9 @@ export const DefaultServiceConfiguration: IServiceConfiguration = {
 			maxTime: 1 * 60 * 1000,
 			maxMessages: 500,
 		},
+		scrubUserDataInSummaries: false,
+		scrubUserDataInLocalCheckpoints: false,
+		scrubUserDataInGlobalCheckpoints: false,
 	},
 	moira: {
 		enable: false,

server/routerlicious/packages/protocol-base/src/test/protocol.spec.ts
@@ -1,0 +1,235 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "assert";
+import {
+	MessageType,
+	type ISequencedDocumentMessage,
+	type ISequencedDocumentSystemMessage,
+	type IClientJoin,
+	type IClientDetails,
+	type IUser,
+} from "@fluidframework/protocol-definitions";
+import { ProtocolOpHandler } from "../protocol";
+
+describe("Protocol", () => {
+	let protocolOpHandler: ProtocolOpHandler;
+
+	beforeEach(() => {
+		protocolOpHandler = new ProtocolOpHandler(
+			0 /* minimumSequenceNumber */,
+			0 /* sequenceNumber */,
+			[] /* members */,
+			[] /* proposals */,
+			[] /* values */,
+			() => {
+				return 0;
+			} /* sendProposal */,
+		);
+	});
+
+	describe("ProtocolOpHandler", () => {
+		it("tracks sequence numbers", () => {
+			const messages: ISequencedDocumentMessage[] = [
+				{
+					sequenceNumber: 1,
+					minimumSequenceNumber: 1,
+					clientSequenceNumber: 1,
+					type: MessageType.Operation,
+					clientId: "test",
+					referenceSequenceNumber: 0,
+					contents: "test content",
+					timestamp: 0,
+				},
+				{
+					sequenceNumber: 2,
+					minimumSequenceNumber: 1,
+					clientSequenceNumber: 2,
+					type: MessageType.Operation,
+					clientId: "test-2",
+					referenceSequenceNumber: 0,
+					contents: "test content",
+					timestamp: 1,
+				},
+			];
+			messages.forEach((message) => {
+				assert.doesNotThrow(() => protocolOpHandler.processMessage(message, false));
+			});
+			assert.strictEqual(protocolOpHandler.attributes.sequenceNumber, 2);
+		});
+
+		it("throws when state not moving sequentially", () => {
+			const messages: ISequencedDocumentMessage[] = [
+				{
+					sequenceNumber: 1,
+					minimumSequenceNumber: 1,
+					clientSequenceNumber: 1,
+					type: MessageType.Operation,
+					clientId: "test",
+					referenceSequenceNumber: 0,
+					contents: "test content",
+					timestamp: 0,
+				},
+				{
+					sequenceNumber: 3,
+					minimumSequenceNumber: 1,
+					clientSequenceNumber: 2,
+					type: MessageType.Operation,
+					clientId: "test-2",
+					referenceSequenceNumber: 0,
+					contents: "test content",
+					timestamp: 1,
+				},
+			];
+			assert.doesNotThrow(() => protocolOpHandler.processMessage(messages[0], false));
+			assert.throws(() => protocolOpHandler.processMessage(messages[1], false));
+		});
+
+		it("scrubs user data from protocol state quorum", () => {
+			const clientJoin1: IClientJoin = {
+				clientId: "test",
+				detail: {
+					mode: "write",
+					details: {} as IClientDetails,
+					permission: [],
+					user: {
+						id: "test-user",
+						name: "Test User",
+						additionalDetails: { favoriteColor: "red" },
+					} as IUser,
+					scopes: [],
+					timestamp: 0,
+				},
+			};
+			const clientJoin2: IClientJoin = {
+				clientId: "test-2",
+				detail: {
+					mode: "write",
+					details: {} as IClientDetails,
+					permission: [],
+					user: {
+						id: "test-user-2",
+						name: "Test User2",
+						additionalDetails: { favoriteColor: "blue" },
+					} as IUser,
+					scopes: [],
+					timestamp: 0,
+				},
+			};
+			const messages: ISequencedDocumentSystemMessage[] = [
+				{
+					sequenceNumber: 1,
+					minimumSequenceNumber: 1,
+					clientSequenceNumber: 1,
+					type: MessageType.ClientJoin,
+					clientId: null,
+					referenceSequenceNumber: 0,
+					contents: "test content",
+					timestamp: 0,
+					data: JSON.stringify(clientJoin1),
+				},
+				{
+					sequenceNumber: 2,
+					minimumSequenceNumber: 1,
+					clientSequenceNumber: 2,
+					type: MessageType.ClientJoin,
+					clientId: null,
+					referenceSequenceNumber: 0,
+					contents: "test content",
+					timestamp: 1,
+					data: JSON.stringify(clientJoin2),
+				},
+			];
+			messages.forEach((message) => {
+				assert.doesNotThrow(() => protocolOpHandler.processMessage(message, false));
+			});
+			assert.strictEqual(protocolOpHandler.attributes.sequenceNumber, 2);
+
+			const scrubbedProtocolState = protocolOpHandler.getProtocolState(true);
+			scrubbedProtocolState.members.forEach(([, member]) => {
+				assert(!member.client.user.id, "user id should be empty");
+				assert(
+					!(member.client.user as unknown as any).name,
+					"user name should not be present",
+				);
+				assert(
+					!(member.client.user as unknown as any).additionalDetails?.favoriteColor,
+					"user additional details should not be present",
+				);
+			});
+		});
+
+		it("does not scrub user data from protocol state quorum", () => {
+			const clientJoin1: IClientJoin = {
+				clientId: "test",
+				detail: {
+					mode: "write",
+					details: {} as IClientDetails,
+					permission: [],
+					user: {
+						id: "test-user",
+						name: "Test User",
+						additionalDetails: { favoriteColor: "red" },
+					} as IUser,
+					scopes: [],
+					timestamp: 0,
+				},
+			};
+			const clientJoin2: IClientJoin = {
+				clientId: "test-2",
+				detail: {
+					mode: "write",
+					details: {} as IClientDetails,
+					permission: [],
+					user: {
+						id: "test-user-2",
+						name: "Test User2",
+						additionalDetails: { favoriteColor: "blue" },
+					} as IUser,
+					scopes: [],
+					timestamp: 0,
+				},
+			};
+			const messages: ISequencedDocumentSystemMessage[] = [
+				{
+					sequenceNumber: 1,
+					minimumSequenceNumber: 1,
+					clientSequenceNumber: 1,
+					type: MessageType.ClientJoin,
+					clientId: null,
+					referenceSequenceNumber: 0,
+					contents: "test content",
+					timestamp: 0,
+					data: JSON.stringify(clientJoin1),
+				},
+				{
+					sequenceNumber: 2,
+					minimumSequenceNumber: 1,
+					clientSequenceNumber: 2,
+					type: MessageType.ClientJoin,
+					clientId: null,
+					referenceSequenceNumber: 0,
+					contents: "test content",
+					timestamp: 1,
+					data: JSON.stringify(clientJoin2),
+				},
+			];
+			messages.forEach((message) => {
+				assert.doesNotThrow(() => protocolOpHandler.processMessage(message, false));
+			});
+			assert.strictEqual(protocolOpHandler.attributes.sequenceNumber, 2);
+
+			const protocolState = protocolOpHandler.getProtocolState();
+			protocolState.members.forEach(([, member]) => {
+				assert(member.client.user.id, "user id should be present");
+				assert((member.client.user as unknown as any).name, "user name should be present");
+				assert(
+					(member.client.user as unknown as any).additionalDetails?.favoriteColor,
+					"user additional details should be present",
+				);
+			});
+		});
+	});
+});

server/routerlicious/packages/services-core/src/test/types/validateServerServicesCorePrevious.generated.ts
@@ -1983,6 +1983,7 @@ declare function get_old_InterfaceDeclaration_IScribeServerConfiguration():
 declare function use_current_InterfaceDeclaration_IScribeServerConfiguration(
     use: TypeOnly<current.IScribeServerConfiguration>): void;
 use_current_InterfaceDeclaration_IScribeServerConfiguration(
+    // @ts-expect-error compatibility expected to be broken
     get_old_InterfaceDeclaration_IScribeServerConfiguration());
 
 /*