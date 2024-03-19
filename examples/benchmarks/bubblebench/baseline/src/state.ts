bc31c6f974ef88346a5aa996bb86d766df48b20f
Clarence Li <clarenceli@microsoft.com>
2024-03-18 23:52:20 -0400

    Avoid lint disabling, and clean schema get/set

--- examples/benchmarks/bubblebench/baseline/src/state.ts
+++ examples/benchmarks/bubblebench/baseline/src/state.ts
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 
 import {
 	IAppState,
--- examples/benchmarks/bubblebench/simple-tree/src/schema.ts
+++ examples/benchmarks/bubblebench/simple-tree/src/schema.ts
@@ -21,41 +21,6 @@ export class Bubble
 	constructor(public readonly bubble: IBubble) {
 		super(bubble);
 	}
-
-	public get x() {
-		return this.x;
-	}
-	public set x(value: number) {
-		this.x = value;
-	}
-
-	public get y() {
-		return this.y;
-	}
-	public set y(value: number) {
-		this.y = value;
-	}
-
-	public get vx() {
-		return this.vx;
-	}
-	public set vx(value: number) {
-		this.vx = value;
-	}
-
-	public get vy() {
-		return this.vy;
-	}
-	public set vy(value: number) {
-		this.vy = value;
-	}
-
-	public get r() {
-		return this.r;
-	}
-	public set r(value: number) {
-		this.r = value;
-	}
 }
 
 export class Client
