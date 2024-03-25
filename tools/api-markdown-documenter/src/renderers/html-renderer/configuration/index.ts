645ffd76e00198d24fc3f748ae246d164dd7f2f0
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-03-25 17:29:37 +0000

    temporarily delete render code to create nice diff for reviewers

--- tools/api-markdown-documenter/src/renderers/html-renderer/configuration/index.ts
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
-export { getRenderConfigurationWithDefaults, type RenderConfiguration } from "./Configuration.js";
-export { defaultRenderers, type Renderers } from "./RenderOptions.js";
