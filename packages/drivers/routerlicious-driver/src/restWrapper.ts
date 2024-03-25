dd0733078f665d108d868c6df0541a8d94a623c3
Michael Zhen <112977307+zhenmichael@users.noreply.github.com>
2024-03-25 16:33:27 +0000

    update generateHeaders return type to AxiosRequestHeaders

--- packages/drivers/routerlicious-driver/src/restWrapper.ts
+++ packages/drivers/routerlicious-driver/src/restWrapper.ts
@@ -105,7 +105,7 @@ export class RouterliciousRestWrapper extends RestWrapper {
         );
     }
 
-    private generateHeaders(requestHeaders?: AxiosRequestHeaders | undefined): Record<string, any> {
+    private generateHeaders(requestHeaders?: AxiosRequestHeaders | undefined): AxiosRequestHeaders {
         const correlationId = requestHeaders?.["x-correlation-id"] || uuid();
 
         return {
