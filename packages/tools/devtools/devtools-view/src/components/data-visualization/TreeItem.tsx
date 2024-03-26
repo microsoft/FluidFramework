8afc6a6dd520cc83f51bb27a07ede278bb3d25b5
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-03-26 18:02:29 +0000

    add 9.4.7.2

--- packages/tools/devtools/devtools-view/src/components/data-visualization/TreeItem.tsx
+++ packages/tools/devtools/devtools-view/src/components/data-visualization/TreeItem.tsx
@@ -8,8 +8,9 @@ import {
 	TreeItem as FluentTreeItem,
 	TreeItemLayout as FluentTreeItemLayout,
 	treeItemLevelToken,
-	useTreeItem_unstable,
-} from "@fluentui/react-components/unstable";
+	useSubtreeContext_unstable,
+	useTreeItemContext_unstable,
+} from "@fluentui/react-components";
 import React from "react";
 
 /**
@@ -25,39 +26,21 @@ export type TreeItemProps = React.PropsWithChildren<{
 }>;
 
 /**
- * TODO
- */
-export declare const useSubtreeContext_unstable: () => SubtreeContextValue;
-
-/**
- * TODO
- */
-export interface SubtreeContextValue {
-	contextType: "subtree";
-	level: number;
-}
-
-/**
  * Constructs a tree element from the provided header and child contents.
  *
  * Intended to be used inside an outer {@link @fluentui/react-components/unstable#Tree} context.
  */
 export function TreeItem(props: TreeItemProps): React.ReactElement {
 	const { children, header } = props;
-
-	// TODO: Need level.
 	const { level } = useSubtreeContext_unstable();
 
-	// TODO: Need open or closed boolean state.
-	const open = true;
-
-	const isLeaf = React.Children.count(children) === 0;
+	const open = useTreeItemContext_unstable((ctx) => ctx.open || level === 1);
 
 	return (
 		<FluentTreeItem
 			value={level}
-			leaf={isLeaf}
 			data-testid="tree-button"
+			itemType="branch"
 			style={{ [treeItemLevelToken]: level }}
 		>
 			<FluentTreeItemLayout>{header}</FluentTreeItemLayout>
