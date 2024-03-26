6ed8469fc356ee5f69bcb91e91a047bb68e80829
Tyler Butler <tyler@tylerbutler.com>
2024-03-26 13:32:52 -0700

    use mjs extension and add copyright headers

--- packages/tools/fluid-runner/bin/fluid-runner.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-import { fluidRunner } from "../lib/fluidRunner.js";
-fluidRunner();
--- packages/tools/fluid-runner/src/test/fluidRunner.spec.ts
+++ packages/tools/fluid-runner/src/test/fluidRunner.spec.ts
@@ -193,7 +193,7 @@ describe("fluid-runner from command line", () => {
 });
 
 describe("custom fluidFileConverter provided", () => {
-	const command = path.join(_dirname, "../../src/test/sampleCodeLoaders", "sample-executable");
+	const command = path.join(_dirname, "../../src/test/sampleCodeLoaders", "sample-executable.mjs");
 
 	describe("exportFile", () => {
 		const folderRoot = path.join(_dirname, "../../src/test");
