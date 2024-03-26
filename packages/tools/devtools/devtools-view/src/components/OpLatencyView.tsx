78acd9a34d4417c3760235fef7bcd6891e66d7b3
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-03-26 12:31:03 -0700

    Fixed linting

--- packages/tools/devtools/devtools-view/src/components/OpLatencyView.tsx
+++ packages/tools/devtools/devtools-view/src/components/OpLatencyView.tsx
@@ -104,7 +104,7 @@ export function OpLatencyView(): React.ReactElement {
 					return false;
 				}
 				// If any of the required fields are missing, we can't use this data
-				if(eventContents.durationNetwork === undefined) {
+				if (eventContents.durationNetwork === undefined) {
 					return false;
 				}
 
