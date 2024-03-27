d5313138e50b19e21fb4fd1935a713fed6d9e35c
Abram Sanderson <absander@microsoft.com>
2024-03-27 13:21:15 -0700

    Add copyright header

--- packages/dds/tree/src/test/snapshots/opFormat.spec.ts
+++ packages/dds/tree/src/test/snapshots/opFormat.spec.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { AttachState } from "@fluidframework/container-definitions";
 import { type SessionId, createIdCompressor } from "@fluidframework/id-compressor";
 import {
