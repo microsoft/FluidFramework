050440533c9b18de24bd20f1aa6edec43557bb1b
Abram Sanderson <Abram.Sanderson@microsoft.com>
2024-04-01 10:01:28 -0700

    Support multiple SharedTree format versions (#20378)
    ## Description
    
    This PR refactors SharedTree to support multiple format versions, adding
    more explicit guidance around how to update code to leverage this
    support.
    
    See `compatibility.md` changes in this PR to get an idea of what form
    that guidance takes.
    
    In addition to refactors to make code align with this guidance, I've
    tweaked some classes which took in lots of parameters solely in order to
    form a codec to instead let the caller construct the codec.
    
    Lastly, this PR stamps an explicit version on SharedTree ops and adds
    some test coverage in this realm.
    
    ---------
    
    Co-authored-by: Yann Achard <achardy@microsoft.com>
    Co-authored-by: Abram Sanderson <absander@microsoft.com>

--- packages/dds/tree/src/shared-tree-core/editManagerCodecs.ts
+++ packages/dds/tree/src/shared-tree-core/editManagerCodecs.ts
@@ -4,11 +4,14 @@
  */
 
 import {
+	type ICodecFamily,
 	ICodecOptions,
 	IJsonCodec,
 	IMultiFormatCodec,
-	makeVersionedValidatedCodec,
+	makeCodecFamily,
+	withSchemaValidation,
 } from "../codec/index.js";
+import { makeVersionDispatchingCodec } from "../codec/index.js";
 import { ChangeEncodingContext, EncodedRevisionTag, RevisionTag } from "../core/index.js";
 import { SchemaAndPolicy } from "../feature-libraries/index.js";
 import {
@@ -31,6 +34,39 @@ export interface EditManagerEncodingContext {
 }
 
 export function makeEditManagerCodec<TChangeset>(
+	changeCodecs: ICodecFamily<TChangeset, ChangeEncodingContext>,
+	revisionTagCodec: IJsonCodec<
+		RevisionTag,
+		EncodedRevisionTag,
+		EncodedRevisionTag,
+		ChangeEncodingContext
+	>,
+	options: ICodecOptions,
+	writeVersion: number,
+): IJsonCodec<
+	SummaryData<TChangeset>,
+	JsonCompatibleReadOnly,
+	JsonCompatibleReadOnly,
+	EditManagerEncodingContext
+> {
+	const family = makeEditManagerCodecs(changeCodecs, revisionTagCodec, options);
+	return makeVersionDispatchingCodec(family, { ...options, writeVersion });
+}
+
+export function makeEditManagerCodecs<TChangeset>(
+	changeCodecs: ICodecFamily<TChangeset, ChangeEncodingContext>,
+	revisionTagCodec: IJsonCodec<
+		RevisionTag,
+		EncodedRevisionTag,
+		EncodedRevisionTag,
+		ChangeEncodingContext
+	>,
+	options: ICodecOptions,
+): ICodecFamily<SummaryData<TChangeset>, EditManagerEncodingContext> {
+	return makeCodecFamily([[1, makeV1Codec(changeCodecs.resolve(0), revisionTagCodec, options)]]);
+}
+
+function makeV1Codec<TChangeset>(
 	changeCodec: IMultiFormatCodec<
 		TChangeset,
 		JsonCompatibleReadOnly,
@@ -48,7 +84,7 @@ export function makeEditManagerCodec<TChangeset>(
 	SummaryData<TChangeset>,
 	JsonCompatibleReadOnly,
 	JsonCompatibleReadOnly,
-	ChangeEncodingContext
+	EditManagerEncodingContext
 > {
 	const format = EncodedEditManager(
 		changeCodec.json.encodedSchema ?? JsonCompatibleReadOnlySchema,
@@ -77,62 +113,71 @@ export function makeEditManagerCodec<TChangeset>(
 		EncodedEditManager<TChangeset>,
 		EncodedEditManager<TChangeset>,
 		EditManagerEncodingContext
-	> = makeVersionedValidatedCodec(options, new Set([version]), format, {
-		encode: (data, context: EditManagerEncodingContext) => {
-			const json: EncodedEditManager<TChangeset> = {
-				trunk: data.trunk.map((commit) =>
-					encodeCommit(commit, {
-						originatorId: commit.sessionId,
-						schema: context.schema,
-					}),
-				),
-				branches: Array.from(data.peerLocalBranches.entries(), ([sessionId, branch]) => [
-					sessionId,
-					{
-						base: revisionTagCodec.encode(branch.base, {
-							originatorId: sessionId,
-						}),
-						commits: branch.commits.map((commit) =>
-							encodeCommit(commit, {
-								originatorId: commit.sessionId,
-								schema: context.schema,
-							}),
-						),
-					},
-				]),
-				version,
-			};
-			return json;
-		},
-		decode: (json: EncodedEditManager<TChangeset>): SummaryData<TChangeset> => {
-			// TODO: sort out EncodedCommit vs Commit, and make this type check without `any`.
-			const trunk: readonly any[] = json.trunk;
-			return {
-				trunk: trunk.map(
-					(commit): SequencedCommit<TChangeset> =>
-						// TODO: sort out EncodedCommit vs Commit, and make this type check without `as`.
-						// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-						decodeCommit(commit, {
+	> = withSchemaValidation(
+		format,
+		{
+			encode: (data, context: EditManagerEncodingContext) => {
+				const json: EncodedEditManager<TChangeset> = {
+					trunk: data.trunk.map((commit) =>
+						encodeCommit(commit, {
 							originatorId: commit.sessionId,
+							schema: context.schema,
 						}),
-				),
-				peerLocalBranches: new Map(
-					mapIterable(json.branches, ([sessionId, branch]) => [
-						sessionId,
-						{
-							base: revisionTagCodec.decode(branch.base, { originatorId: sessionId }),
-							commits: branch.commits.map((commit) =>
-								// TODO: sort out EncodedCommit vs Commit, and make this type check without `as`.
-								decodeCommit(commit as EncodedCommit<JsonCompatibleReadOnly>, {
-									originatorId: commit.sessionId,
+					),
+					branches: Array.from(
+						data.peerLocalBranches.entries(),
+						([sessionId, branch]) => [
+							sessionId,
+							{
+								base: revisionTagCodec.encode(branch.base, {
+									originatorId: sessionId,
 								}),
-							),
-						},
-					]),
-				),
-			};
+								commits: branch.commits.map((commit) =>
+									encodeCommit(commit, {
+										originatorId: commit.sessionId,
+										schema: context.schema,
+									}),
+								),
+							},
+						],
+					),
+					version,
+				};
+				return json;
+			},
+			decode: (json: EncodedEditManager<TChangeset>): SummaryData<TChangeset> => {
+				// TODO: sort out EncodedCommit vs Commit, and make this type check without `any`.
+				const trunk: readonly any[] = json.trunk;
+				return {
+					trunk: trunk.map(
+						(commit): SequencedCommit<TChangeset> =>
+							// TODO: sort out EncodedCommit vs Commit, and make this type check without `as`.
+							// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+							decodeCommit(commit, {
+								originatorId: commit.sessionId,
+							}),
+					),
+					peerLocalBranches: new Map(
+						mapIterable(json.branches, ([sessionId, branch]) => [
+							sessionId,
+							{
+								base: revisionTagCodec.decode(branch.base, {
+									originatorId: sessionId,
+								}),
+								commits: branch.commits.map((commit) =>
+									// TODO: sort out EncodedCommit vs Commit, and make this type check without `as`.
+									decodeCommit(commit as EncodedCommit<JsonCompatibleReadOnly>, {
+										originatorId: commit.sessionId,
+									}),
+								),
+							},
+						]),
+					),
+				};
+			},
 		},
-	});
+		options.jsonValidator,
+	);
 	// TODO: makeVersionedValidatedCodec and withSchemaValidation should allow the codec to decode JsonCompatibleReadOnly, or Versioned or something like that,
 	// and not leak the internal encoded format in the API surface.
 	// Fixing that would remove the need for this cast.
@@ -140,6 +185,6 @@ export function makeEditManagerCodec<TChangeset>(
 		SummaryData<TChangeset>,
 		JsonCompatibleReadOnly,
 		JsonCompatibleReadOnly,
-		ChangeEncodingContext
+		EditManagerEncodingContext
 	>;
 }
