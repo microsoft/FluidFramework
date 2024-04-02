9ed4ee61342f42f56822ed6e6eb01e012a772911
Jason Hartman <jasonha@microsoft.com>
2024-04-02 14:55:23 -0700

    test(fluid-runner): fix dual-emit import limitation

--- packages/tools/fluid-runner/src/test/sampleCodeLoaders/sample-executable.cjs.ts
+++ packages/tools/fluid-runner/src/test/sampleCodeLoaders/sample-executable.cjs.ts
@@ -4,9 +4,13 @@
  * Licensed under the MIT License.
  */
 
-import { fluidExport } from "./sampleCodeLoader.js";
+// In practice a caller would import @fluidframework/fluid-runner as seen in the
+// ESM version.
+// We use relative index.js here per the injected package.json above this file in
+// the output folder for dual-emit, that does not have name and exports redefined.
+import { fluidRunner } from "../../index.js";
 
-import { fluidRunner } from "@fluidframework/fluid-runner";
+import { fluidExport } from "./sampleCodeLoader.js";
 
 // CommonJS modules may not use await at top level. Catch is best that can be done.
 fluidRunner(fluidExport).catch(process.abort);
