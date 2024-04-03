microsoft/FluidFramework

commit fea195ee542ee6aa2ba2116f8fed6dde7f84b9f8

Added customization for gitrest and historian (#20243)
## Description

This PR introduces customizations for Historian and Gitrest that allow
custom Redis connection implementations. These changes are needed to
allow custom authentication for Redis connections such as authentication
to Redis using [Microsoft
entra-ID](https://learn.microsoft.com/en-us/azure/azure-cache-for-redis/cache-azure-active-directory-for-authentication).
The interace IRedisClientConnectionManager can be implemented to have
custom connection management to Redis. This customization can be passed
to all services that use Redis. If no customization is passed, the
default RedisClientConnectionManager that connects to Redis via
connection passwords will be used.

These changes are linked to
https://github.com/microsoft/FluidFramework/pull/20214.

## Testing

Currently, these changes have been tested locally by running the server
on Docker. Additionally, they have also been tested in an FRS-dev
cluster where I injected a customized implementation of
`IRedisClientConnectionManager`.

## Breaking Changes

This change will change the constructors for the following APIs:

1) `server/gitrest/packages/gitrest-base/src/utils/filesystems.ts`
2) `server/gitrest/packages/gitrest-base/src/utils/redisFs/redis.ts`
3)
`server/gitrest/packages/gitrest-base/src/utils/redisFs/redisFsManager.ts`
4) `server/historian/packages/historian-base/src/services/redisCache.ts`
5)
`server/historian/packages/historian-base/src/services/redisTenantCache.ts`

For all these APIs, the change gets rid of any `ioredis` specific
constructor args and uses the `IRedisClientConnectionManager` instead.

After this PR goes through, we would need to update the Gitrest and
Historian implementations of FRS to use `IRedisClientConnectionManager`
once we pull in the latest release.

## Reviewer Guidance

Ensure that there is no regression with the support added for Redis
cluster clients.

server/historian/packages/historian-base/src/routes/utils.ts
@@ -17,6 +17,7 @@ import {
 	getLumberBaseProperties,
 } from "@fluidframework/server-services-telemetry";
 import {
+	runWithRetry,
 	IStorageNameRetriever,
 	IRevokedTokenChecker,
 	IDocumentManager,
@@ -93,9 +94,37 @@ export async function createGitService(createArgs: createGitServiceArgs): Promis
 				getLumberBaseProperties(documentId, tenantId),
 			);
 			isEphemeral = isEphemeralContainer;
-			await cache?.set(isEphemeralKey, isEphemeral);
+			runWithRetry(
+				async () => cache?.set(isEphemeralKey, isEphemeral) /* api */,
+				"utils.createGitService.set" /* callName */,
+				3 /* maxRetries */,
+				1000 /* retryAfterMs */,
+				getLumberBaseProperties(documentId, tenantId) /* telemetryProperties */,
+			).catch((error) => {
+				Lumberjack.error(
+					`Error setting ${isEphemeralKey} in redis`,
+					getLumberBaseProperties(documentId, tenantId),
+					error,
+				);
+			});
 		} else {
-			isEphemeral = await cache?.get(isEphemeralKey);
+			runWithRetry<boolean>(
+				async () => cache?.get(isEphemeralKey) /* api */,
+				"utils.createGitService.get" /* callName */,
+				3 /* maxRetries */,
+				1000 /* retryAfterMs */,
+				getLumberBaseProperties(documentId, tenantId) /* telemetryProperties */,
+			)
+				.then((value) => {
+					isEphemeral = value;
+				})
+				.catch((error) => {
+					Lumberjack.error(
+						`Error getting ${isEphemeralKey} from redis`,
+						getLumberBaseProperties(documentId, tenantId),
+						error,
+					);
+				});
 			if (typeof isEphemeral !== "boolean") {
 				// If isEphemeral was not in the cache, fetch the value from database
 				try {

server/gitrest/packages/gitrest-base/src/customizations.ts
@@ -1,0 +1,10 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+import { IRedisClientConnectionManager } from "@fluidframework/server-services-utils";
+
+export interface IGitrestResourcesCustomizations {
+	redisClientConnectionManagerForDefaultFileSystem?: IRedisClientConnectionManager;
+	redisClientConnectionManagerForEphemeralFileSystem?: IRedisClientConnectionManager;
+}

server/gitrest/packages/gitrest-base/src/runnerFactory.ts
@@ -6,7 +6,11 @@
 import { AsyncLocalStorage } from "async_hooks";
 import * as core from "@fluidframework/server-services-core";
 import * as services from "@fluidframework/server-services-shared";
-import { normalizePort } from "@fluidframework/server-services-utils";
+import {
+	normalizePort,
+	IRedisClientConnectionManager,
+	RedisClientConnectionManager,
+} from "@fluidframework/server-services-utils";
 import { Provider } from "nconf";
 import { ExternalStorageManager } from "./externalStorageManager";
 import { GitrestRunner } from "./runner";
@@ -18,6 +22,8 @@ import {
 	NodeFsManagerFactory,
 	RedisFsManagerFactory,
 } from "./utils";
+import { IGitrestResourcesCustomizations } from "./customizations";
+
 export class GitrestResources implements core.IResources {
 	public webServerFactory: core.IWebServerFactory;
 
@@ -39,11 +45,17 @@ export class GitrestResources implements core.IResources {
 }
 
 export class GitrestResourcesFactory implements core.IResourcesFactory<GitrestResources> {
-	public async create(config: Provider): Promise<GitrestResources> {
+	public async create(
+		config: Provider,
+		customizations?: IGitrestResourcesCustomizations,
+	): Promise<GitrestResources> {
 		const port = normalizePort(process.env.PORT || "3000");
 		const asyncLocalStorage = config.get("asyncLocalStorageInstance")?.[0];
 
-		const fileSystemManagerFactories = this.getFileSystemManagerFactories(config);
+		const fileSystemManagerFactories = this.getFileSystemManagerFactories(
+			config,
+			customizations,
+		);
 		const repositoryManagerFactory = this.getRepositoryManagerFactory(
 			config,
 			fileSystemManagerFactories,
@@ -58,18 +70,25 @@ export class GitrestResourcesFactory implements core.IResourcesFactory<GitrestRe
 		);
 	}
 
-	private getFileSystemManagerFactories(config: Provider): IFileSystemManagerFactories {
+	private getFileSystemManagerFactories(
+		config: Provider,
+		customizations?: IGitrestResourcesCustomizations,
+	): IFileSystemManagerFactories {
 		const defaultFileSystemName: string = config.get("git:filesystem:name") ?? "nodeFs";
 		const ephemeralFileSystemName: string =
 			config.get("git:ephemeralfilesystem:name") ?? "redisFs";
 
+		// Creating two customizations for redisClientConnectionManager for now.
+		// This may be changed to a single customization in the future.
 		const defaultFileSystemManagerFactory = this.getFileSystemManagerFactoryByName(
 			defaultFileSystemName,
 			config,
+			customizations?.redisClientConnectionManagerForDefaultFileSystem,
 		);
 		const ephemeralFileSystemManagerFactory = this.getFileSystemManagerFactoryByName(
 			ephemeralFileSystemName,
 			config,
+			customizations?.redisClientConnectionManagerForEphemeralFileSystem,
 		);
 
 		return {
@@ -78,11 +97,24 @@ export class GitrestResourcesFactory implements core.IResourcesFactory<GitrestRe
 		};
 	}
 
-	private getFileSystemManagerFactoryByName(fileSystemName: string, config: Provider) {
+	private getFileSystemManagerFactoryByName(
+		fileSystemName: string,
+		config: Provider,
+		redisClientConnectionManagerCustomization?: IRedisClientConnectionManager,
+	) {
 		if (!fileSystemName || fileSystemName === "nodeFs") {
 			return new NodeFsManagerFactory();
 		} else if (fileSystemName === "redisFs") {
-			return new RedisFsManagerFactory(config);
+			const redisConfig = config.get("redis");
+			const redisClientConnectionManager =
+				redisClientConnectionManagerCustomization ??
+				new RedisClientConnectionManager(
+					undefined,
+					redisConfig,
+					redisConfig.enableClustering,
+					redisConfig.slotsRefreshTimeout,
+				);
+			return new RedisFsManagerFactory(config, redisClientConnectionManager);
 		}
 		throw new Error("Invalid file system name.");
 	}

server/gitrest/packages/gitrest-base/src/test/summaries.spec.ts
@@ -7,7 +7,7 @@ import assert from "assert";
 import { v4 as uuid } from "uuid";
 import { SinonSpiedInstance, restore, spy } from "sinon";
 import { Provider } from "nconf";
-import IoRedisMock from "ioredis-mock";
+import { RedisOptions } from "ioredis-mock";
 import {
 	IWholeFlatSummary,
 	LatestSummaryId,
@@ -56,6 +56,7 @@ import {
 	ElaborateSecondContainerPayload,
 	ElaborateSecondContainerResult,
 } from "./examples2";
+import { TestRedisClientConnectionManager } from "@fluidframework/server-test-utils";
 
 // Github Copilot wizardry.
 function permuteFlags(obj: Record<string, boolean>): Record<string, boolean>[] {
@@ -154,9 +155,18 @@ const getFsManagerFactory = (
 				keyExpireAfterSeconds: 60 * 60, // 1 hour
 			},
 		});
+		const redisOptions: RedisOptions = {
+			host: "localhost",
+			port: 6379,
+			connectTimeout: 10000,
+			maxRetriesPerRequest: 20,
+			enableAutoPipelining: false,
+			enableOfflineQueue: true,
+		};
+		const testRedisClientConnectionManager = new TestRedisClientConnectionManager(redisOptions);
 		const redisfsManagerFactory = new RedisFsManagerFactory(
 			redisConfig,
-			(options) => new IoRedisMock(options),
+			testRedisClientConnectionManager,
 		);
 		// Internally, this will create a new RedisFs instance that is shared across all `create` calls.
 		const redisFsManager = redisfsManagerFactory.create();

server/gitrest/packages/gitrest-base/src/utils/filesystems.ts
@@ -4,9 +4,9 @@
  */
 
 import fs from "node:fs";
-import { Redis as IoRedis, RedisOptions as IoRedisOptions, type Cluster } from "ioredis";
 import { Volume } from "memfs";
 import { Provider } from "nconf";
+import { IRedisClientConnectionManager } from "@fluidframework/server-services-utils";
 import {
 	IFileSystemManager,
 	IFileSystemManagerFactory,
@@ -30,13 +30,11 @@ export class MemFsManagerFactory implements IFileSystemManagerFactory {
 
 export class RedisFsManagerFactory implements IFileSystemManagerFactory {
 	private readonly redisParams: RedisParams;
-	private readonly redisOptions: IoRedisOptions;
 	private readonly redisFsConfig: RedisFsConfig;
-	private readonly enableClustering: boolean;
 
 	constructor(
 		config: Provider,
-		private readonly createRedisClient?: (options: IoRedisOptions) => IoRedis | Cluster,
+		private readonly redisClientConnectionManager: IRedisClientConnectionManager,
 	) {
 		this.redisFsConfig = {
 			enableRedisFsMetrics: (config.get("git:enableRedisFsMetrics") as boolean) ?? true,
@@ -45,30 +43,6 @@ export class RedisFsManagerFactory implements IFileSystemManagerFactory {
 			enableOptimizedStat: (config.get("git:enableRedisFsOptimizedStat") as boolean) ?? false,
 		};
 		const redisConfig = config.get("redis");
-		this.redisOptions = {
-			host: redisConfig.host,
-			port: redisConfig.port,
-			password: redisConfig.pass,
-			connectTimeout: redisConfig.connectTimeout,
-			enableReadyCheck: true,
-			maxRetriesPerRequest: redisConfig.maxRetriesPerRequest,
-			enableOfflineQueue: redisConfig.enableOfflineQueue,
-			retryStrategy: (attempts: number) => Math.min(attempts * 50, 2000),
-		};
-		if (redisConfig.enableAutoPipelining) {
-			/**
-			 * When enabled, all commands issued during an event loop iteration are automatically wrapped in a
-			 * pipeline and sent to the server at the same time. This can improve performance by 30-50%.
-			 * More info: https://github.com/luin/ioredis#autopipelining
-			 */
-			this.redisOptions.enableAutoPipelining = true;
-			this.redisOptions.autoPipeliningIgnoredCommands = ["ping"];
-		}
-		if (redisConfig.tls) {
-			this.redisOptions.tls = {
-				servername: redisConfig.host,
-			};
-		}
 
 		const enableHashmapRedisFs = (config.get("git:enableHashmapRedisFs") as boolean) ?? false;
 		this.redisParams = {
@@ -77,18 +51,14 @@ export class RedisFsManagerFactory implements IFileSystemManagerFactory {
 			enableRedisMetrics: this.redisFsConfig.enableRedisFsMetrics,
 			redisApiMetricsSamplingPeriod: this.redisFsConfig.redisApiMetricsSamplingPeriod,
 		};
-
-		this.enableClustering = redisConfig.enableClustering;
 	}
 
 	public create(fsManagerParams?: IFileSystemManagerParams): IFileSystemManager {
 		return new RedisFsManager(
 			this.redisParams,
-			this.redisOptions,
 			this.redisFsConfig,
+			this.redisClientConnectionManager,
 			fsManagerParams,
-			this.createRedisClient,
-			this.enableClustering,
 		);
 	}
 }

server/gitrest/packages/gitrest-base/src/utils/redisFs/redis.ts
@@ -4,9 +4,11 @@
  */
 
 import { runWithRetry } from "@fluidframework/server-services-core";
-import { IRedisParameters } from "@fluidframework/server-services-utils";
+import {
+	IRedisParameters,
+	IRedisClientConnectionManager,
+} from "@fluidframework/server-services-utils";
 import { Lumberjack } from "@fluidframework/server-services-telemetry";
-import * as IoRedis from "ioredis";
 import { RedisFsApis, executeRedisFsApiWithMetric } from "./helpers";
 
 export interface RedisParams extends IRedisParameters {
@@ -55,7 +57,7 @@ export class Redis implements IRedis {
 	private readonly prefix: string = "fs";
 
 	constructor(
-		private readonly client: IoRedis.default | IoRedis.Cluster,
+		private readonly redisClientConnectionManager: IRedisClientConnectionManager,
 		private readonly parameters?: RedisParams,
 	) {
 		if (parameters?.expireAfterSeconds) {
@@ -66,13 +68,15 @@ export class Redis implements IRedis {
 			this.prefix = parameters.prefix;
 		}
 
-		client.on("error", (error) => {
+		redisClientConnectionManager.getRedisClient().on("error", (error) => {
 			Lumberjack.error("Redis Cache Error", undefined, error);
 		});
 	}
 
 	public async get<T>(key: string): Promise<T | undefined> {
-		const stringValue = await this.client.get(this.getKey(key));
+		const stringValue = await this.redisClientConnectionManager
+			.getRedisClient()
+			.get(this.getKey(key));
 		if (!stringValue) {
 			// Cannot JSON parse an empty string or null value, so return undefined.
 			return undefined;
@@ -85,12 +89,9 @@ export class Redis implements IRedis {
 		value: T,
 		expireAfterSeconds: number = this.expireAfterSeconds,
 	): Promise<void> {
-		const result = await this.client.set(
-			this.getKey(key),
-			JSON.stringify(value),
-			"EX",
-			expireAfterSeconds,
-		);
+		const result = await this.redisClientConnectionManager
+			.getRedisClient()
+			.set(this.getKey(key), JSON.stringify(value), "EX", expireAfterSeconds);
 
 		if (result !== "OK") {
 			throw new Error(result);
@@ -108,7 +109,9 @@ export class Redis implements IRedis {
 	}
 
 	public async peek(key: string): Promise<number> {
-		const strlen = await this.client.strlen(this.getKey(key));
+		const strlen = await this.redisClientConnectionManager
+			.getRedisClient()
+			.strlen(this.getKey(key));
 		// If the key does not exist, strlen will return 0.
 		// Otherwise, we are stringifying everything we store in Redis, so strlen will always be at least 2 from the stringified quotes.
 		return strlen === 0 ? -1 : strlen - 2;
@@ -120,7 +123,7 @@ export class Redis implements IRedis {
 		// If 'appendPrefixToKey' is false, we assume that the 'key' parameter with prefix is already passed in by the caller,
 		// and no additional prefix needs to be added.
 		const keyToDelete = appendPrefixToKey ? this.getKey(key) : key;
-		const result = await this.client.unlink(keyToDelete);
+		const result = await this.redisClientConnectionManager.getRedisClient().unlink(keyToDelete);
 		// The UNLINK API in Redis returns the number of keys that were removed.
 		// We always call Redis DEL with one key only, so we expect a result equal to 1
 		// to indicate that the key was removed. 0 would indicate that the key does not exist.
@@ -132,13 +135,16 @@ export class Redis implements IRedis {
 		if (keys.length === 0) {
 			return false;
 		}
-		const result = await this.client.unlink(...keys);
+		const result = await this.redisClientConnectionManager.getRedisClient().unlink(...keys);
 		return result === keys.length;
 	}
 
 	public async keysByPrefix(keyPrefix: string): Promise<string[]> {
 		const result = await executeRedisFsApiWithMetric(
-			async () => this.client.keys(`${this.getKey(keyPrefix)}*`),
+			async () =>
+				this.redisClientConnectionManager
+					.getRedisClient()
+					.keys(`${this.getKey(keyPrefix)}*`),
 			RedisFsApis.KeysByPrefix,
 			this.parameters?.enableRedisMetrics,
 			this.parameters?.redisApiMetricsSamplingPeriod,
@@ -167,7 +173,7 @@ export class HashMapRedis implements IRedis {
 		 * Key that points to the HashMap in Redis.
 		 */
 		private readonly hashMapKey: string,
-		private readonly client: IoRedis.default | IoRedis.Cluster,
+		private readonly redisClientConnectionManager: IRedisClientConnectionManager,
 		private readonly parameters?: RedisParams,
 	) {
 		if (parameters?.expireAfterSeconds) {
@@ -178,7 +184,7 @@ export class HashMapRedis implements IRedis {
 			this.prefix = parameters.prefix;
 		}
 
-		client.on("error", (error) => {
+		redisClientConnectionManager.getRedisClient().on("error", (error) => {
 			Lumberjack.error("Redis Cache Error", undefined, error);
 		});
 	}
@@ -188,7 +194,9 @@ export class HashMapRedis implements IRedis {
 			// Field is part of the root hashmap key, so return empty string.
 			return "" as unknown as T;
 		}
-		const stringValue = await this.client.hget(this.getMapKey(), this.getMapField(field));
+		const stringValue = await this.redisClientConnectionManager
+			.getRedisClient()
+			.hget(this.getMapKey(), this.getMapField(field));
 		if (!stringValue) {
 			// Cannot JSON parse an empty string or null value, so return undefined.
 			return undefined;
@@ -208,7 +216,9 @@ export class HashMapRedis implements IRedis {
 		}
 		// Set values in the hash map and returns the count of set field/value pairs.
 		// However, if it's a duplicate field, it will return 0, so we can't rely on the return value to determine success.
-		await this.client.hset(this.getMapKey(), this.getMapField(field), JSON.stringify(value));
+		await this.redisClientConnectionManager
+			.getRedisClient()
+			.hset(this.getMapKey(), this.getMapField(field), JSON.stringify(value));
 	}
 
 	public async setMany<T>(
@@ -227,7 +237,9 @@ export class HashMapRedis implements IRedis {
 		}
 		// Set values in the hash map and returns the count of set field/value pairs.
 		// However, if it's a duplicate field, it will return 0, so we can't rely on the return value to determine success.
-		await this.client.hset(this.getMapKey(), fieldValueArgs);
+		await this.redisClientConnectionManager
+			.getRedisClient()
+			.hset(this.getMapKey(), fieldValueArgs);
 	}
 
 	public async peek(field: string): Promise<number> {
@@ -235,7 +247,9 @@ export class HashMapRedis implements IRedis {
 			// Field is part of the root hashmap key, so it exists but is empty.
 			return 0;
 		}
-		const strlen = await this.client.hstrlen(this.getMapKey(), this.getMapField(field));
+		const strlen = await this.redisClientConnectionManager
+			.getRedisClient()
+			.hstrlen(this.getMapKey(), this.getMapField(field));
 		// If the key does not exist, strlen will return 0.
 		// Otherwise, we are stringifying everything we store in Redis, so strlen will always be at least 2 from the stringified quotes.
 		return strlen === 0 ? -1 : strlen - 2;
@@ -246,7 +260,9 @@ export class HashMapRedis implements IRedis {
 			// The deleted field is a root directory, so we need to delete the whole HashMap.
 			return this.delAll(field);
 		}
-		const result = await this.client.hdel(this.getMapKey(), this.getMapField(field));
+		const result = await this.redisClientConnectionManager
+			.getRedisClient()
+			.hdel(this.getMapKey(), this.getMapField(field));
 		// The HDEL API in Redis returns the number of keys that were removed.
 		// We always call Redis HDEL with one key only, so we expect a result equal to 1
 		// to indicate that the key was removed. 0 would indicate that the key does not exist.
@@ -256,7 +272,9 @@ export class HashMapRedis implements IRedis {
 	public async delAll(keyPrefix: string): Promise<boolean> {
 		if (this.isFieldRootDirectory(keyPrefix)) {
 			// The key prefix matches a root directory, so we need to delete the whole HashMap.
-			const unlinkResult = await this.client.unlink(this.getMapKey());
+			const unlinkResult = await this.redisClientConnectionManager
+				.getRedisClient()
+				.unlink(this.getMapKey());
 			// The UNLINK API in Redis returns the number of keys that were removed.
 			// We always call Redis DEL with one key only, so we expect a result equal to 1
 			// to indicate that the key was removed. 0 would indicate that the key does not exist.
@@ -266,13 +284,15 @@ export class HashMapRedis implements IRedis {
 		if (keys.length === 0) {
 			return false;
 		}
-		const hDelResult = await this.client.hdel(this.getMapKey(), ...keys);
+		const hDelResult = await this.redisClientConnectionManager
+			.getRedisClient()
+			.hdel(this.getMapKey(), ...keys);
 		return hDelResult === keys.length;
 	}
 
 	public async keysByPrefix(keyPrefix: string): Promise<string[]> {
 		const result = await executeRedisFsApiWithMetric(
-			async () => this.client.hkeys(this.getMapKey()),
+			async () => this.redisClientConnectionManager.getRedisClient().hkeys(this.getMapKey()),
 			RedisFsApis.HKeysByPrefix,
 			this.parameters?.enableRedisMetrics,
 			this.parameters?.redisApiMetricsSamplingPeriod,
@@ -307,13 +327,19 @@ export class HashMapRedis implements IRedis {
 			return;
 		}
 		const initializeHashMapIfNotExists = async (): Promise<void> => {
-			const exists = await this.client.exists(this.getMapKey());
+			const exists = await this.redisClientConnectionManager
+				.getRedisClient()
+				.exists(this.getMapKey());
 			if (!exists) {
 				await executeRedisFsApiWithMetric(
 					async () => {
 						// Set a blank field/value pair to initialize the hashmap.
-						await this.client.hset(this.getMapKey(), "", "");
-						await this.client.expire(this.getMapKey(), this.expireAfterSeconds);
+						await this.redisClientConnectionManager
+							.getRedisClient()
+							.hset(this.getMapKey(), "", "");
+						await this.redisClientConnectionManager
+							.getRedisClient()
+							.expire(this.getMapKey(), this.expireAfterSeconds);
 					},
 					RedisFsApis.InitHashmapFs,
 					this.parameters?.enableRedisMetrics,

server/gitrest/packages/gitrest-base/src/utils/redisFs/redisFsManager.ts
@@ -20,8 +20,8 @@ import type {
 import { FileHandle } from "fs/promises";
 import { Stream } from "stream";
 import { Abortable } from "events";
-import { Redis as IoRedis, RedisOptions as IoRedisOptions, Cluster } from "ioredis";
 import { Lumberjack } from "@fluidframework/server-services-telemetry";
+import type { IRedisClientConnectionManager } from "@fluidframework/server-services-utils";
 import { IFileSystemManager, IFileSystemManagerParams, IFileSystemPromises } from "../definitions";
 import { getStats, packedRefsFileName, SystemErrors } from "../fileSystemHelper";
 import { HashMapRedis, IRedis, Redis, RedisParams } from "./redis";
@@ -47,64 +47,49 @@ export class RedisFsManager implements IFileSystemManager {
 
 	constructor(
 		redisParam: RedisParams,
-		redisOptions: IoRedisOptions,
 		redisFsConfig: RedisFsConfig,
+		private readonly redisClientConnectionManager: IRedisClientConnectionManager,
 		fsManagerParams?: IFileSystemManagerParams,
-		createRedisClient?: (options: IoRedisOptions) => IoRedis | Cluster,
-		enableClustering: boolean = false,
 	) {
 		this.promises = RedisFs.getInstance(
 			redisParam,
-			redisOptions,
 			redisFsConfig,
+			this.redisClientConnectionManager,
 			fsManagerParams,
-			createRedisClient,
-			enableClustering,
 		);
 	}
 }
 
 export class RedisFs implements IFileSystemPromises {
-	private static redisClientInstance: IoRedis | Cluster;
 	public readonly redisFsClient: IRedis;
 
 	constructor(
 		redisParams: RedisParams,
-		redisOptions: IoRedisOptions,
 		private readonly redisFsConfig: RedisFsConfig,
+		private readonly redisClientConnectionManager: IRedisClientConnectionManager,
 		fsManagerParams?: IFileSystemManagerParams,
-		createRedisClient: (options: IoRedisOptions) => IoRedis | Cluster = (opts) =>
-			new IoRedis(opts),
-		enableClustering: boolean = false,
 	) {
-		if (!RedisFs.redisClientInstance) {
-			RedisFs.redisClientInstance = createRedisClient(redisOptions);
-		}
 		this.redisFsClient =
 			fsManagerParams?.rootDir && redisParams.enableHashmapRedisFs
 				? new HashMapRedis(
 						fsManagerParams.rootDir,
-						RedisFs.redisClientInstance,
+						this.redisClientConnectionManager,
 						redisParams,
 				  )
-				: new Redis(RedisFs.redisClientInstance, redisParams);
+				: new Redis(this.redisClientConnectionManager, redisParams);
 	}
 
 	public static getInstance(
 		redisParams: RedisParams,
-		redisOptions: IoRedisOptions,
 		redisFsConfig: RedisFsConfig,
+		redisClientConnectionManager: IRedisClientConnectionManager,
 		fsManagerParams?: IFileSystemManagerParams,
-		createRedisClient?: (options: IoRedisOptions) => IoRedis | Cluster,
-		enableClustering: boolean = false,
 	): RedisFs {
 		return new RedisFs(
 			redisParams,
-			redisOptions,
 			redisFsConfig,
+			redisClientConnectionManager,
 			fsManagerParams,
-			createRedisClient,
-			enableClustering,
 		);
 	}
 

commit a003006b4d604e2559a5276ffdf4c92511023cac

Await promises from runWithRetry (#20436)
## Description

This PR fixes a regression that was introduced in
https://github.com/microsoft/FluidFramework/pull/20243.
https://github.com/microsoft/FluidFramework/pull/20243 added the use of
`runWithRetry` to historian which caused a bug in the ephemeral
containers workflow. The bug was that the `isEphemeral` flag was not
always set to `true` as the async call to set/get it from the cache was
not awaited. This PR adds an `await` to fix the issue.

server/historian/packages/historian-base/src/routes/utils.ts
@@ -94,7 +94,7 @@ export async function createGitService(createArgs: createGitServiceArgs): Promis
 				getLumberBaseProperties(documentId, tenantId),
 			);
 			isEphemeral = isEphemeralContainer;
-			runWithRetry(
+			await runWithRetry(
 				async () => cache?.set(isEphemeralKey, isEphemeral) /* api */,
 				"utils.createGitService.set" /* callName */,
 				3 /* maxRetries */,
@@ -108,7 +108,7 @@ export async function createGitService(createArgs: createGitServiceArgs): Promis
 				);
 			});
 		} else {
-			runWithRetry<boolean>(
+			await runWithRetry<boolean>(
 				async () => cache?.get(isEphemeralKey) /* api */,
 				"utils.createGitService.get" /* callName */,
 				3 /* maxRetries */,