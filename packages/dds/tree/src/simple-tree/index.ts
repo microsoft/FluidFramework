8f16f6fdef851b40ee0fa05dd57e73fd7b511a34
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2024-04-05 09:17:56 -0700

    Centralize objectNode logic in one file

--- packages/dds/tree/src/simple-tree/index.ts
+++ packages/dds/tree/src/simple-tree/index.ts
@@ -24,12 +24,9 @@ export {
 	ApplyKind,
 	FieldKind,
 	FieldSchema,
-	InsertableObjectFromSchemaRecord,
 	InsertableTreeFieldFromImplicitField,
 	InsertableTypedNode,
 	NodeBuilderData,
-	ObjectFromSchemaRecord,
-	TreeObjectNode,
 	type FieldProps,
 } from "./schemaTypes.js";
 export { SchemaFactory, type ScopedSchemaName } from "./schemaFactory.js";
@@ -70,4 +67,9 @@ export {
 } from "./testRecursiveDomain.js";
 
 export { TreeNode, Unhydrated } from "./types.js";
-export { TreeArrayNode, IterableTreeArrayContent, TreeArrayNodeBase } from "./treeArrayNode.js";
+export { TreeArrayNode, IterableTreeArrayContent, TreeArrayNodeBase } from "./arrayNode.js";
+export {
+	InsertableObjectFromSchemaRecord,
+	ObjectFromSchemaRecord,
+	TreeObjectNode,
+} from "./objectNode.js";
