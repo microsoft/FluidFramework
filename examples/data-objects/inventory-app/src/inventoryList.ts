52fe8f03a454a2a5ffd6b3bc0dc601593800919e
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2024-04-05 13:59:08 -0700

    Move containerSchema to test

--- examples/data-objects/inventory-app/src/inventoryList.ts
+++ examples/data-objects/inventory-app/src/inventoryList.ts
@@ -6,35 +6,8 @@
 // eslint-disable-next-line import/no-internal-modules
 import { DataObjectFactory } from "@fluidframework/aqueduct/internal";
 
-import { ContainerSchema } from "fluid-framework";
-
-import { TreeDataObject, factory, treeDataObject } from "./reactSharedTreeView.js";
+import { TreeDataObject, factory } from "./reactSharedTreeView.js";
 import { type Inventory, treeConfiguration } from "./schema.js";
-
-/**
- * For use with high level service-client container API's like AzureClient and OdspClient.
- * @privateRemarks
- * TODO: There is no service implementation agnostic client abstraction that can be referred to here (ex: shared by AzureClient and OdspClient).
- * This makes documenting compatibility with that implicit common API difficult.
- * It also makes writing service agnostic code at that abstraction level harder.
- * This should be fixed.
- *
- * TODO:
- * Writing an app at this abstraction level currently requires a lot of boilerplate which also requires extra dependencies.
- * Since `@fluid-example/example-utils` doesn't provide that boilerplate and neither do the public packages, there isn't a concise way to actually use this container in this example.
- * This should be fixed.
- *
- * TODO:
- * The commonly used boilerplate for setting up a ContainerSchema based application configures the dev-tools, which would be great to include in this example,
- * but can't be included due to dependency layering issues.
- */
-export const containerSchema = {
-	initialObjects: {
-		// TODO: it seems odd that DataObjects in container schema need both a key under initialObjects where they are,
-		// as well as a key under the root data object, and SharedObjects only need one key.
-		tree: treeDataObject("tree", treeConfiguration),
-	},
-} satisfies ContainerSchema;
 
 // For use with lower level APIs, like ContainerViewRuntimeFactory from "@fluid-example/example-utils".
 export class InventoryList extends TreeDataObject<typeof Inventory> {
--- examples/data-objects/inventory-app/tests/main.test.ts
+++ examples/data-objects/inventory-app/tests/main.test.ts
@@ -3,9 +3,12 @@
  * Licensed under the MIT License.
  */
 
+import { ContainerSchema } from "fluid-framework";
+import { treeDataObject } from "../src/reactSharedTreeView.js";
+import { treeConfiguration } from "../src/schema.js";
 import { globals } from "../jest.config.cjs";
 
-describe.skip("Main", () => {
+describe.skip("Integration Tests", () => {
 	beforeAll(async () => {
 		// Wait for the page to load first before running any tests
 		// so this time isn't attributed to the first test
@@ -20,5 +23,35 @@ describe.skip("Main", () => {
 
 	it("The page loads", async () => {
 		// await expect(page).toClick("button", { text: "Roll" });
+	});
+
+	it("works with container schema", async () => {
+		// await expect(page).toClick("button", { text: "Roll" });
+	});
+});
+
+describe("Unit tests", () => {
+	// Demonstrates use with high level service-client container API's like AzureClient and OdspClient.
+	it("treeDataObject works with container schema", () => {
+		// TODO: There is no service implementation agnostic client abstraction that can be referred to here (ex: shared by AzureClient and OdspClient).
+		// This makes documenting compatibility with that implicit common API difficult.
+		// It also makes writing service agnostic code at that abstraction level harder.
+		// This should be fixed.
+		//
+		// TODO:
+		// Writing an app at this abstraction level currently requires a lot of boilerplate which also requires extra dependencies.
+		// Since `@fluid-example/example-utils` doesn't provide that boilerplate and neither do the public packages, there isn't a concise way to actually use this container in this example.
+		// This should be fixed.
+		//
+		// TODO:
+		// The commonly used boilerplate for setting up a ContainerSchema based application configures the dev-tools, which would be great to include in this example,
+		// but can't be included due to dependency layering issues.
+		const containerSchema = {
+			initialObjects: {
+				// TODO: it seems odd that DataObjects in container schema need both a key under initialObjects where they are,
+				// as well as a key under the root data object, and SharedObjects only need one key.
+				tree: treeDataObject("tree", treeConfiguration),
+			},
+		} satisfies ContainerSchema;
 	});
 });
