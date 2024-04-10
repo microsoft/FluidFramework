185cdcb3bf1c31526cdc6f3b1cb3f5bd52d754b3
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2024-04-10 15:52:47 +0000

    wip

--- fix-package.mjs
+++ fix-package.mjs
@@ -6,22 +6,30 @@ import path from "path";
 const pkgPath = path.resolve("./package.json");
 const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
 
+let hasNonDefaultExports = false;
+
 // Insert '/lib' into export maps
 let exportMap = {};
 for (let [key, map] of Object.entries(pkg.exports)) {
 	// Skip root key.
 	if (key !== "." && key !== "./") {
+		hasNonDefaultExports = true;
 		const parts = key.split("/");
 		if (parts[1] !== "lib") {
 			parts.splice(/* start: */ 1, /* deleteCount: */ 0, "lib");
 			key = parts.join("/");
 		}
 	}
+
 	exportMap = { ...exportMap, [key]: map };
 }
 pkg.exports = exportMap;
 
 // Update ATTW
-pkg.scripts["check:are-the-types-wrong"] = "attw --pack";
+if (hasNonDefaultExports) {
+	pkg.scripts["check:are-the-types-wrong"] = "attw --pack";
+} else {
+	delete pkg.scripts["check:are-the-types-wrong"];
+}
 
-console.log(JSON.stringify(pkg.exports, undefined, "\t"));
+// Update node10 entry points to ESM
