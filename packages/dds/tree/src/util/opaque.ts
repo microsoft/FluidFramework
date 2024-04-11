1fd352e213cede3626ebc78c6944955116e86e7f
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2024-04-11 14:11:20 -0700

    Move and refine type erasure tooling from tree to core-interfaces (#20601)
    ## Description
    
    Move and refine type erasure tooling from tree to core-interfaces.
    
    This will be used by
    https://github.com/microsoft/FluidFramework/pull/20123 to type erase the
    internals of IFluidHandle

--- packages/dds/tree/src/util/opaque.ts
+++ packages/dds/tree/src/util/opaque.ts
@@ -10,7 +10,7 @@ import type { isAny } from "./typeCheck.js";
  * Converts a Branded type into an "opaque" handle.
  * This prevents the value from being used directly, but does not fully type erase it.
  * @remarks
- * Like {@link Erased},
+ * Like {@link @fluidframework/core-interfaces#ErasedType},
  * but more type safe and cannot be used to hide the internal type from API extractor at package boundaries.
  *
  * The type can be recovered using {@link extractFromOpaque},
@@ -24,7 +24,7 @@ import type { isAny } from "./typeCheck.js";
  * ```
  * @internal
  */
-export type Opaque<T extends Brand<any, string>> = T extends BrandedType<
+export type Opaque<T extends Brand<any, unknown>> = T extends BrandedType<
 	infer ValueType,
 	infer Name
 >
@@ -35,7 +35,7 @@ export type Opaque<T extends Brand<any, string>> = T extends BrandedType<
  * See {@link extractFromOpaque}.
  * @internal
  */
-export type ExtractFromOpaque<TOpaque extends BrandedType<any, string>> =
+export type ExtractFromOpaque<TOpaque extends BrandedType<any, unknown>> =
 	TOpaque extends BrandedType<infer ValueType, infer Name>
 		? isAny<ValueType> extends true
 			? unknown
@@ -49,7 +49,7 @@ export type ExtractFromOpaque<TOpaque extends BrandedType<any, string>> =
  * allowing these handles to be considered opaque.
  * @internal
  */
-export function extractFromOpaque<TOpaque extends BrandedType<any, string>>(
+export function extractFromOpaque<TOpaque extends BrandedType<any, unknown>>(
 	value: TOpaque,
 ): ExtractFromOpaque<TOpaque> {
 	return value as ExtractFromOpaque<TOpaque>;
@@ -61,7 +61,7 @@ export function extractFromOpaque<TOpaque extends BrandedType<any, string>>(
  * Only do this when specifically allowed by the requirements of the type being converted to.
  * @internal
  */
-export function brandOpaque<T extends BrandedType<any, string>>(
+export function brandOpaque<T extends BrandedType<any, unknown>>(
 	value: isAny<ValueFromBranded<T>> extends true ? never : ValueFromBranded<T>,
 ): BrandedType<ValueFromBranded<T>, NameFromBranded<T>> {
 	return value as BrandedType<ValueFromBranded<T>, NameFromBranded<T>>;
