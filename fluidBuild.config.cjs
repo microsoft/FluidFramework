55aa15fe64b93633d50ca51d87d60cca386a81bc
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2024-04-11 17:28:18 +0000

    Remove common-definitions package

--- fluidBuild.config.cjs
+++ fluidBuild.config.cjs
@@ -167,7 +167,6 @@ module.exports = {
 
 		// Independent packages
 		"build": "common/build",
-		"common-def": "common/lib/common-definitions",
 		"common-utils": "common/lib/common-utils",
 		"protocol-def": "common/lib/protocol-definitions",
 
@@ -448,7 +447,6 @@ module.exports = {
 			"@fluid-tools/markdown-magic",
 			"@fluid-tools/telemetry-generator",
 			"@fluidframework/build-common",
-			"@fluidframework/common-definitions",
 			"@fluidframework/common-utils",
 			"@fluidframework/eslint-config-fluid",
 			"@fluid-internal/eslint-plugin-fluid",
