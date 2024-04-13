c421fc40923850c92387595ea5407a0c597c7899
Jason Hartman <jasonha@microsoft.com>
2024-04-12 20:44:55 -0500

    fix(experimental/tree): restore test/EditLog
    This reverts commit 645d6d9f8f55623cca24e338b2f448082c119f9b with the exception of the ATTW exclusion.

--- packages/test/test-end-to-end-tests/src/test/migration-shim/storingHandles.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/migration-shim/storingHandles.spec.ts
@@ -17,7 +17,7 @@ import {
 	type TraitLabel,
 } from "@fluid-experimental/tree";
 // eslint-disable-next-line import/no-internal-modules
-import { type EditLog } from "@fluid-experimental/tree/internal/test/EditLog";
+import { type EditLog } from "@fluid-experimental/tree/test/EditLog";
 import { bufferToString, stringToBuffer } from "@fluid-internal/client-utils";
 import { describeCompat } from "@fluid-private/test-version-utils";
 import { LoaderHeader } from "@fluidframework/container-definitions/internal";
