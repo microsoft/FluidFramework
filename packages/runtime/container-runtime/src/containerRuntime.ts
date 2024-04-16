53abd48c00d93df622a54155bb130b48a0b95933
Vlad Sudzilouski <vladsud@microsoft.com>
2024-04-16 16:08:54 -0400

    Remove attach batch from container runtime (#20603)
    I've added separate batch for attach messages some time ago as a partial work around to a problem of too large batches (problem with hitting 1Mb socket / Kafka limits).
    Since then, we have enabled op compression that (along with chunking) ensures that we never run into 1Mb problem.
    
    Impact on latency:
    
    There is a case to be made that having separate batch and sending ops sooner (that would happen if total size of all attach ops exceeds certain size) might be beneficial as we are reducing latencies - remote clients can start processing these ops sooner, before the rest of the batch is formed. While there is some truth to that, as a general rule we want to move toward faster attachment flow (see #19957 for direction), where impact of such direction would be minimal. Even today, we do not measure this impact.
    
    Impact on AzureClient 1.3 clients:
    
    There should be no impact on legacy clients, as AzureClient 1.3 along with AzureClient 2.0 in compat mode should continue to use old configs, including immediate flush. In such mode every op is sent separately, and right away, so there is no benefit from having separate batch for attach ops.
    Please see #20283 for more details on compatibility discussion.
    
    Unit tests are switched to testing ID Compressor batching instead, that should behave same way and is not covered in UTs today.

--- packages/runtime/container-runtime/src/containerRuntime.ts
+++ packages/runtime/container-runtime/src/containerRuntime.ts
@@ -2812,9 +2812,9 @@ export class ContainerRuntime
 		let checkpoint: IBatchCheckpoint | undefined;
 		let result: T;
 		if (this.mc.config.getBoolean("Fluid.ContainerRuntime.EnableRollback")) {
-			// Note: we are not touching this.pendingAttachBatch here, for two reasons:
-			// 1. It would not help, as we flush attach ops as they become available.
-			// 2. There is no way to undo process of data store creation.
+			// Note: we are not touching any batches other than mainBatch here, for two reasons:
+			// 1. It would not help, as other batches are flushed independently from main batch.
+			// 2. There is no way to undo process of data store creation, blob creation, ID compressor ops, or other things tracked by other batches.
 			checkpoint = this.outbox.checkpoint().mainBatch;
 		}
 		try {
@@ -3936,33 +3936,7 @@ export class ContainerRuntime
 					});
 				}
 
-				// If this is attach message for new data store, and we are in a batch, send this op out of order
-				// Is it safe:
-				//    Yes, this should be safe reordering. Newly created data stores are not visible through API surface.
-				//    They become visible only when aliased, or handle to some sub-element of newly created datastore
-				//    is stored in some DDS, i.e. only after some other op.
-				// Why:
-				//    Attach ops are large, and expensive to process. Plus there are scenarios where a lot of new data
-				//    stores are created, causing issues like relay service throttling (too many ops) and catastrophic
-				//    failure (batch is too large). Pushing them earlier and outside of main batch should alleviate
-				//    these issues.
-				// Cons:
-				//    1. With large batches, relay service may throttle clients. Clients may disconnect while throttled.
-				//    This change creates new possibility of a lot of newly created data stores never being referenced
-				//    because client died before it had a change to submit the rest of the ops. This will create more
-				//    garbage that needs to be collected leveraging GC (Garbage Collection) feature.
-				//    2. Sending ops out of order means they are excluded from rollback functionality. This is not an issue
-				//    today as rollback can't undo creation of data store. To some extent not sending them is a bigger
-				//    issue than sending.
-				// Please note that this does not change file format, so it can be disabled in the future if this
-				// optimization no longer makes sense (for example, batch compression may make it less appealing).
-				if (
-					this.currentlyBatching() &&
-					type === ContainerMessageType.Attach &&
-					this.disableAttachReorder !== true
-				) {
-					this.outbox.submitAttach(message);
-				} else if (type === ContainerMessageType.BlobAttach) {
+				if (type === ContainerMessageType.BlobAttach) {
 					// BlobAttach ops must have their metadata visible and cannot be grouped (see opGroupingManager.ts)
 					this.outbox.submitBlobAttach(message);
 				} else {
