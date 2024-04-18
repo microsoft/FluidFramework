92dbecb23aca141aae64d69381a8093d3da5b6d1
Wayne Ferrao <wayneferrao@microsoft.com>
2024-04-18 15:20:12 -0700

    fix(container-runtime): only send OpRoundtripTime for ops, not noops (#20701)
    [AB#7583](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/7583)
    
    ## Description
    
    This PR investigates and fixes a bug in which some OpRoundtripTime
    events are sent without any performance data
    (`durationInboundToProcessing`, `durationOutbound` and
    `durationNetwork`). This happened because the events were being sent on
    noop messages as well as regular ops.
    
    This resulted in gaps in the Op Latency view which causes gaps in the
    graph.
    
    This fix also removes the need to check for ops with the performance
    data.

--- packages/runtime/container-runtime/src/connectionTelemetry.ts
+++ packages/runtime/container-runtime/src/connectionTelemetry.ts
@@ -361,17 +361,20 @@ class OpPerfTelemetry {
 			// The threshold could be adjusted, but ideally it stays  workload-agnostic, as service
 			// performance impacts all workloads relying on service.
 			const category = duration > latencyThreshold ? "error" : "performance";
-			this.opLatencyLogger.sendPerformanceEvent({
-				eventName: "OpRoundtripTime",
-				sequenceNumber,
-				referenceSequenceNumber: message.referenceSequenceNumber,
-				duration,
-				category,
-				pingLatency: this.pingLatency,
-				msnDistance:
-					this.deltaManager.lastSequenceNumber - this.deltaManager.minimumSequenceNumber,
-				...latencyData.opPerfData,
-			});
+			if (message.type !== MessageType.NoOp) {
+				this.opLatencyLogger.sendPerformanceEvent({
+					eventName: "OpRoundtripTime",
+					sequenceNumber,
+					referenceSequenceNumber: message.referenceSequenceNumber,
+					duration,
+					category,
+					pingLatency: this.pingLatency,
+					msnDistance:
+						this.deltaManager.lastSequenceNumber -
+						this.deltaManager.minimumSequenceNumber,
+					...latencyData.opPerfData,
+				});
+			}
 			this.clientSequenceNumberForLatencyStatistics = undefined;
 			this.latencyStatistics.delete(message.clientSequenceNumber);
 		}
