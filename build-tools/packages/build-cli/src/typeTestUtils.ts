34bc9b04bf23c1c5543b3586e4578eaf597e1448
Tyler Butler <tyler@tylerbutler.com>
2024-04-23 16:58:36 -0700

    Update build-tools/packages/build-cli/src/typeTestUtils.ts

--- build-tools/packages/build-cli/src/typeTestUtils.ts
+++ build-tools/packages/build-cli/src/typeTestUtils.ts
@@ -76,6 +76,8 @@ export function initializeProjectsAndLoadFiles(
 	previousBasePath: string,
 	log: Logger,
 ): { currentFile: SourceFile; previousFile: SourceFile } {
+	// For the current package, we'll load using the tsconfig file and get the root index.ts 
+	// file as the source file to read exports from.
 	const currentFile = new Project({
 		skipFileDependencyResolution: true,
 		tsConfigFilePath: "tsconfig.json",
