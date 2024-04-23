954a1cef5555c02283314ae6ce32c91e083f2ce0
Tony Murphy <anthony.murphy@microsoft.com>
2024-04-23 12:27:32 -0700

    Update packages/test/test-service-load/src/loadTestDataStore.ts

--- packages/test/test-service-load/src/loadTestDataStore.ts
+++ packages/test/test-service-load/src/loadTestDataStore.ts
@@ -94,6 +94,8 @@ export class LoadTestDataStoreModel {
 
 			runtime.deltaManager.on("op", resolveIfDisposedOrCaughtUp);
 			runtime.once("dispose", resolveIfDisposedOrCaughtUp);
+			resolveIfDisposedOrCaughtUp();
+
 		});
 	}
 
