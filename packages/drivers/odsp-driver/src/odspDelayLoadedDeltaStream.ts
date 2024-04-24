75d69a2d0e9abfb7fd4b45d8f009427bfe15e76a
Pragya Garg <praggarg@microsoft.com>
2024-04-24 15:43:03 -0700

    undefined check

--- packages/drivers/odsp-driver/src/odspDelayLoadedDeltaStream.ts
+++ packages/drivers/odsp-driver/src/odspDelayLoadedDeltaStream.ts
@@ -220,10 +220,16 @@ export class OdspDelayLoadedDeltaStream {
 				this.currentConnection = connection;
 				return connection;
 			} catch (error) {
-				// Remove join session information from cache only if it is an error related to connect_document and not a socket related error.
+				// Remove join session information from cache only if it is an error is from socket event connect_document_error.
 				// Otherwise keep it in cache so that this session can be re-used after disconnection.
-				// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-				if ((error as any).errorFrom === "connect_document_error") {
+				// Also keeping an undefined check here to account for any unknown code path that is unable to stamp the value as in that case also
+				// it is safer to clear join session cache and start over.
+				if (
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+					(error as any).errorFrom === "connect_document_error" ||
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+					(error as any).errorFrom === undefined
+				) {
 					this.clearJoinSessionTimer();
 					this.cache.sessionJoinCache.remove(this.joinSessionKey);
 				}
