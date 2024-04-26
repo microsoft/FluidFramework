1eafff3d2ec6867a71965feb6dd7c4cffc35e8f4
Pragya Garg <praggarg@microsoft.com>
2024-04-26 16:29:58 -0700

    lint fix

--- packages/drivers/odsp-driver/src/odspDocumentDeltaConnection.ts
+++ packages/drivers/odsp-driver/src/odspDocumentDeltaConnection.ts
@@ -361,9 +361,9 @@ export class OdspDocumentDeltaConnection extends DocumentDeltaConnection {
 	/**
 	 * Error raising for socket.io issues
 	 */
-	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 	protected createErrorObject(
 		socketEvent: string,
+		// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 		error?: any,
 		canRetry = true,
 	): IAnyDriverError {
