54540d20082f5c77883cedf7982079f7d90d9647
Sonali Deshpande <48232592+sonalideshpandemsft@users.noreply.github.com>
2024-04-30 16:50:58 -0700

    Rename userName to name and userId to id (#20796)
    This PR introduces the following enhancements:
    
    1. Renamed `AzureMember.userName` to `AzureMember.name` to establish
    uniform naming across odsp-client and azure-client modules.
    2. Updated `IMember.userId` to `IMember.id` to align with the properties
    received from AFR.
    3. Replaced all occurrences of `userId` with `id` and `userName` with
    `name` for consistency.

--- azure/packages/test/scenario-runner/src/DocLoaderRunner.ts
+++ azure/packages/test/scenario-runner/src/DocLoaderRunner.ts
@@ -62,8 +62,8 @@ export class DocLoaderRunner extends ScenarioRunner<
 		const ac =
 			runConfig.client ??
 			(await createAzureClient({
-				userId: `testUserId_${runConfig.childId}`,
-				userName: `testUserName_${runConfig.childId}`,
+				id: `testUserId_${runConfig.childId}`,
+				name: `testUserName_${runConfig.childId}`,
 				logger,
 			}));
 
--- packages/service-clients/azure-client/src/test/types/validateAzureClientPrevious.generated.ts
+++ packages/service-clients/azure-client/src/test/types/validateAzureClientPrevious.generated.ts
@@ -286,6 +286,7 @@ declare function get_old_InterfaceDeclaration_AzureMember():
 declare function use_current_InterfaceDeclaration_AzureMember(
     use: TypeOnly<current.AzureMember>): void;
 use_current_InterfaceDeclaration_AzureMember(
+    // @ts-expect-error compatibility expected to be broken
     get_old_InterfaceDeclaration_AzureMember());
 
 /*
@@ -300,6 +301,7 @@ declare function get_current_InterfaceDeclaration_AzureMember():
 declare function use_old_InterfaceDeclaration_AzureMember(
     use: TypeOnly<old.AzureMember>): void;
 use_old_InterfaceDeclaration_AzureMember(
+    // @ts-expect-error compatibility expected to be broken
     get_current_InterfaceDeclaration_AzureMember());
 
 /*
