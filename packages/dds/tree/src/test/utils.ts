97a88ff76f4552d8245870a6b8705941da3a0dc3
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2024-04-30 19:10:11 -0700

    tree: Make validateSnapshotConsistency compare handles by absolute path (#20923)
    ## Description
    
    Make validateSnapshotConsistency compare handles by absolute path.
    
    Also ensures empty field lists and empty values compare the same
    regardless if they were explicitly or implicitly missing.

--- packages/dds/tree/src/test/utils.ts
+++ packages/dds/tree/src/test/utils.ts
@@ -136,6 +136,7 @@ import {
 	disposeSymbol,
 	nestedMapFromFlatList,
 } from "../util/index.js";
+import { isFluidHandle } from "@fluidframework/runtime-utils/internal";
 
 // Testing utilities
 
@@ -596,22 +597,59 @@ export function validateSnapshotConsistency(
 	idDifferentiator: string | undefined = undefined,
 ): void {
 	assert.deepEqual(
-		treeA.tree,
-		treeB.tree,
+		prepareTreeForCompare(treeA.tree),
+		prepareTreeForCompare(treeB.tree),
 		`Inconsistent document tree json representation: ${idDifferentiator}`,
 	);
-	// Note: removed trees are not currently GCed, which allows us to expect that all clients should share the same
+
+	// Note: removed trees are not currently garbage collected, which allows us to expect that all clients should share the same
 	// exact set of them. In the future, we will need to relax this expectation and only enforce that whenever two
 	// clients both have data for the same removed tree (as identified by the first two tuple entries), then they
 	// should be consistent about the content being stored (the third tuple entry).
-	const mapA = nestedMapFromFlatList(treeA.removed);
-	const mapB = nestedMapFromFlatList(treeB.removed);
+	const mapA = nestedMapFromFlatList(
+		treeA.removed.map(([key, num, children]) => [
+			key,
+			num,
+			prepareTreeForCompare([children])[0],
+		]),
+	);
+	const mapB = nestedMapFromFlatList(
+		treeB.removed.map(([key, num, children]) => [
+			key,
+			num,
+			prepareTreeForCompare([children])[0],
+		]),
+	);
 	assert.deepEqual(
 		mapA,
 		mapB,
 		`Inconsistent removed trees json representation: ${idDifferentiator}`,
 	);
 	expectSchemaEqual(treeA.schema, treeB.schema, idDifferentiator);
+}
+
+/**
+ * Make a copy of a {@link JsonableTree} array adjusted for compatibility with `assert.deepEqual`.
+ * @remarks
+ * This replaces handles replaced with `{ Handle: absolutePath }`, and normalizes optional fields to be omitted.
+ */
+export function prepareTreeForCompare(tree: JsonableTree[]): object[] {
+	return tree.map((node): object => {
+		const fields: Record<string, object> = {};
+		for (const [key, children] of Object.entries(node.fields ?? {})) {
+			fields[key] = prepareTreeForCompare(children);
+		}
+		const inputValue = node.value;
+		const value = isFluidHandle(inputValue) ? { Handle: inputValue.absolutePath } : inputValue;
+
+		const output: Record<string, any> = { ...node, value, fields };
+
+		// Normalize optional values to be omitted for cleaner diffs:
+		if (output.value === undefined) delete output.value;
+		if (Reflect.ownKeys(output.fields).length === 0) delete output.fields;
+
+		return output as object;
+	});
 }
 
 export function checkoutWithContent(
