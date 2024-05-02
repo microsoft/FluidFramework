2e15b6e6fab9a9fbe1905d040229047c121da900
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2024-05-02 21:45:46 +0000

    skip(test-e2e): Skip "sweep with multiple successful GC runs and one+ GC op(s) for a single successful summarization" and "sweep with multiple successful GC runs and multiple GC op(s) for a single successful summarization"test for being flaky on FRS

--- packages/test/test-end-to-end-tests/src/test/gc/gcSweepAttachmentBlobs.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/gc/gcSweepAttachmentBlobs.spec.ts
@@ -1252,7 +1252,14 @@ describeCompat("GC attachment blob sweep tests", "NoCompat", (getTestObjectProvi
 						clientType: "interactive",
 					},
 				],
-				async () => {
+				async function () {
+					// This test is flaky on FRS. See ADO:7922 and ADO:7923
+					if (
+						provider.driver.type === "routerlicious" &&
+						provider.driver.endpointName === "frs"
+					) {
+						this.skip();
+					}
 					const { dataStore: mainDataStore, summarizer } =
 						await createDataStoreAndSummarizer();
 
