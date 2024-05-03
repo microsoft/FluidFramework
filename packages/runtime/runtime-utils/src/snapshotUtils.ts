0afa7fb151549e455eacfdd8b8435801bf939d46
Jatin Garg <jatingarg@Jatins-MacBook-Pro-2.local>
2024-05-02 20:28:03 -0700

    add comment

--- packages/runtime/runtime-utils/src/snapshotUtils.ts
+++ packages/runtime/runtime-utils/src/snapshotUtils.ts
@@ -29,7 +29,11 @@ function evaluateSnapshotTreeForMissingBlobs(
 		}
 	}
 	for (const [_, childTree] of Object.entries(snapshotTree.trees)) {
-		// Only evaluate childTree if it does not have a loading groupId.
+		// Only evaluate childTree if it does not have a loading groupId because if the childTree has a loading
+		// groupId then it will be evaluated whether we want to fetch blobs for that childTree or not when
+		// that particular childTree is getting realized. Now we just want to check for blobs which belongs to
+		// tree with current loading groupId. Note: Child with no loading groupId, will fall under parent with
+		// a loading groupId as it does not have its own loading groupId.
 		if (childTree.groupId === undefined) {
 			const value = evaluateSnapshotTreeForMissingBlobs(childTree, blobContents);
 			if (value) {
