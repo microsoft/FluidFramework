bf47463413dd0d7e552dc003884b908530e4a0ae
Tyler Butler <tylerbu@microsoft.com>
2024-05-03 07:48:46 -0700

    build-cli-cjs

--- build-tools/packages/build-cli-cjs/bin/run.js
+++ build-tools/packages/build-cli-cjs/bin/run.js
@@ -2,6 +2,6 @@
 
 // eslint-disable-next-line unicorn/prefer-top-level-await
 (async () => {
-  const oclif = await import('@oclif/core')
-  await oclif.execute({dir: __dirname})
-})()
+	const oclif = await import("@oclif/core");
+	await oclif.execute({ dir: __dirname });
+})();
