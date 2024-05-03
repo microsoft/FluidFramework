c956045abee8dbe4e4f006f30288daca8770cf83
Tyler Butler <tylerbu@microsoft.com>
2024-05-02 19:43:55 -0700

    feat(build-cli): Allow commands to select all packages by default (#16544)
    Today, when using commands that enumerate and filter packages, users
    must provide the `--all` flag, or some other selection flag in order to
    select any packages. For some commands this is undesirable behavior. For
    example, the `flub info` command, which today doesn't use the common
    package selection/filtering flags, runs on all packages by default.
    
    In order to support running all by default, I added a new
    `defaultSelection` abstract property to the PackageCommand base class,
    which can be set by subclasses to change the default behavior. Supports
    selecting all packages, the current directory, or none (current
    default).

--- build-tools/packages/build-cli/src/commands/generate/typetests.ts
+++ build-tools/packages/build-cli/src/commands/generate/typetests.ts
@@ -24,6 +24,7 @@ import { mkdirSync, readJson, rmSync, writeFileSync } from "fs-extra";
 import * as resolve from "resolve.exports";
 import { ModuleKind, Project, type SourceFile } from "ts-morph";
 import { PackageCommand } from "../../BasePackageCommand";
+import type { PackageSelectionDefault } from "../../flags";
 import { ApiLevel, ensureDevDependencyExists, knownApiLevels } from "../../library";
 import { unscopedPackageNameString } from "./entrypoints";
 
@@ -53,6 +54,8 @@ export default class GenerateTypetestsCommand extends PackageCommand<
 		}),
 		...PackageCommand.flags,
 	} as const;
+
+	protected defaultSelection = "dir" as PackageSelectionDefault;
 
 	protected async processPackage(pkg: Package): Promise<void> {
 		const { level, outDir, outFile } = this.flags;
