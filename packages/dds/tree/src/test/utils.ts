f92c77f27990b140749592eb12d43030febcfd20
Kian Thompson <102998837+kian-thompson@users.noreply.github.com>
2024-05-02 23:10:40 -0700

    Align telemetryContextPrefix for SharedTree (#20418)
    https://github.com/microsoft/FluidFramework/pull/10294 added the
    capability to collect some summarize telemetry from DDSes. Whenever this
    `SharedTreeCore` object was created it used a different convention than
    the other DDSes.
    
    Ideally the `telemetryContextPrefix` wouldn't need to be present in the
    ctor, but that's a breaking change for these classes.
    
    Note that `"fluid_sharedTree_"` is currently used in
    [`experimental/.../SharedTree.ts`](https://github.com/microsoft/FluidFramework/blob/main/experimental/dds/tree/src/SharedTree.ts#L544).
    Querying on `Data_runtimeVersion` will be needed as it will now be using
    `"fluid_legacySharedTree_"`.

--- packages/dds/tree/src/test/utils.ts
+++ packages/dds/tree/src/test/utils.ts
@@ -1179,7 +1179,7 @@ export function treeTestFactory(
 			}),
 		options.attributes ?? new SharedTreeFactory().attributes,
 		options.options ?? { jsonValidator: typeboxValidator },
-		options.telemetryContextPrefix ?? "SharedTree",
+		options.telemetryContextPrefix,
 	);
 }
 
