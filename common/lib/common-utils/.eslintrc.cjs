955e1724c9daa5d1767434262c187aeee4e7f223
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-05-07 22:49:47 +0000

    tools: Disable more rules

--- common/lib/common-utils/.eslintrc.cjs
+++ common/lib/common-utils/.eslintrc.cjs
@@ -26,11 +26,15 @@ module.exports = {
 
 		// This package has been deprecated. The following rules have a significant number of violations
 		// that will not be fixed here.
-		"unicorn/prefer-node-protocol": "off",
 		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/no-unsafe-argument": "off",
 		"@typescript-eslint/no-unsafe-member-access": "off",
 		"@typescript-eslint/no-unsafe-call": "off",
+		"@typescript-eslint/no-unsafe-assignment": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"unicorn/text-encoding-identifier-case": "off",
+		"unicorn/prefer-node-protocol": "off",
+		"unicorn/prefer-code-point": "off",
 	},
 	overrides: [
 		{
