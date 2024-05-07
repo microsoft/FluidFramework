95f44861361cc196ab410333e344c9095adc3349
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-05-07 15:44:47 -0700

    Update eslint-config-fluid version for tools

--- tools/changelog-generator-wrapper/src/index.js
+++ tools/changelog-generator-wrapper/src/index.js
@@ -2,9 +2,8 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-
-const getReleaseLine = require("./getReleaseLine");
 const getDependencyReleaseLine = require("./getDependencyReleaseLine");
+const getReleaseLine = require("./getReleaseLine");
 const changelogFunctions = {
 	getReleaseLine: getReleaseLine.getReleaseLine,
 	getDependencyReleaseLine: getDependencyReleaseLine.getDependencyReleaseLine,
