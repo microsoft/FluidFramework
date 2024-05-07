6b05b49ef23363a7de69f1b9a816402197bb13fc
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-05-07 13:26:09 -0700

    Moved events_pkg from dev to dependencies and reverted unnecessary rules

--- server/routerlicious/packages/services-ordering-kafkanode/.eslintrc.cjs
+++ server/routerlicious/packages/services-ordering-kafkanode/.eslintrc.cjs
@@ -12,19 +12,10 @@ module.exports = {
 		"@typescript-eslint/strict-boolean-expressions": "off",
 		// This package uses node's events APIs.
 		// This should probably be reconsidered, but until then we will leave an exception for it here.
-		"import/no-nodejs-modules": ["error"],
+		"import/no-nodejs-modules": "off",
 		"promise/catch-or-return": ["error", { allowFinally: true }],
 
 		// TODO: enable strict null checks in tsconfig and remove this override
 		"@typescript-eslint/prefer-nullish-coalescing": "off",
 	},
-	overrides: [
-		{
-			// Override the rule to allow import of "util"
-			files: ["src/kafkaNodeConsumer.ts", "src/kafkaNodeProducer.ts"],
-			rules: {
-				"import/no-nodejs-modules": "off",
-			},
-		},
-	],
 };
--- server/routerlicious/packages/services-ordering-rdkafka/.eslintrc.cjs
+++ server/routerlicious/packages/services-ordering-rdkafka/.eslintrc.cjs
@@ -17,16 +17,5 @@ module.exports = {
 
 		// TODO: remove usages of deprecated APIs and remove this override
 		"import/no-deprecated": "warn",
-		// This package uses node's events APIs.
-		// This should probably be reconsidered, but until then we will leave an exception for it here.
-		"import/no-nodejs-modules": ["error"],
 	},
-	overrides: [
-		{
-			files: ["src/rdkafkaBase.ts"],
-			rules: {
-				"import/no-nodejs-modules": "off",
-			},
-		},
-	],
 };
