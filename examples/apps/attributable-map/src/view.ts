a59a18209e8dac9296d6d7d750b4de374adcfac9
Josmithr <josmithr@microsoft.com>
2024-05-08 15:46:49 -0700

    refactor: Fix linter violations

--- examples/apps/attributable-map/src/view.ts
+++ examples/apps/attributable-map/src/view.ts
@@ -6,6 +6,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import { IRuntimeAttributor } from "@fluid-experimental/attributor";
+import type { AttributionKey } from "@fluidframework/runtime-definitions/internal";
 
 import { IHitCounter, ITinyliciousUser, greenKey, redKey } from "./dataObject.js";
 
@@ -16,8 +17,10 @@ export function renderHitCounter(
 ): void {
 	const redCounterContainerDiv = document.querySelector("#red-counter-container-div")!;
 	const greenCounterContainerDiv = document.querySelector("#green-counter-container-div")!;
-	const redAttributionContainerDiv = document.querySelector("#red-attribution-container-div");
-	const greenAttributionContainerDiv = document.querySelector("#green-attribution-container-div");
+	const redAttributionContainerDiv = document.querySelector("#red-attribution-container-div")!;
+	const greenAttributionContainerDiv = document.querySelector(
+		"#green-attribution-container-div",
+	)!;
 
 	const redButton = document.querySelector("#red-button")!;
 	redButton.addEventListener("click", () => {
@@ -29,37 +32,45 @@ export function renderHitCounter(
 		hitCounter.hit("green");
 	});
 
+	// update the attribution text
+	const updateAttributionDisplay = (
+		attributionKey: AttributionKey | undefined,
+		containerDiv: Element,
+	): void => {
+		/**
+		 * A production application would manage detached and local attribution key types as well.
+		 * The current example and attributor will concentrate solely on handling the attribution of the Op-stream type.
+		 */
+		if (attributionKey !== undefined && attributionKey.type === "op") {
+			const attribution = runtimeAttributor?.get(attributionKey);
+			const userString = JSON.stringify(attribution?.user);
+			const user = JSON.parse(userString) as ITinyliciousUser;
+			// eslint-disable-next-line unicorn/no-null
+			const timestamp = new Date(attribution?.timestamp ?? JSON.stringify(null));
+			containerDiv.textContent = `Last updated by ${
+				user.name
+			}\non ${timestamp.toLocaleString()}`;
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+			(containerDiv as any).style.display = "block";
+		} else {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+			(containerDiv as any).style.display = "none";
+		}
+	};
+
 	// Function to update counter value and attribution text
-	const updateView = () => {
-		const greenValue = hitCounter.map?.get(greenKey);
-		const redValue = hitCounter.map?.get(redKey);
+	const updateView = (): void => {
+		const greenValue: number | undefined = hitCounter.map?.get(greenKey);
+		const redValue: number | undefined = hitCounter.map?.get(redKey);
 
 		// update the counter value
-		redCounterContainerDiv.textContent = redValue.toString();
-		greenCounterContainerDiv.textContent = greenValue.toString();
+		// eslint-disable-next-line unicorn/no-null
+		redCounterContainerDiv.textContent = redValue?.toString() ?? null;
+		// eslint-disable-next-line unicorn/no-null
+		greenCounterContainerDiv.textContent = greenValue?.toString() ?? null;
 
 		const greenAttributionKey = hitCounter.map?.getAttribution(greenKey);
 		const redAttributionKey = hitCounter.map?.getAttribution(redKey);
-
-		// update the attribution text
-		const updateAttributionDisplay = (attributionKey, containerDiv) => {
-			/**
-			 * A production application would manage detached and local attribution key types as well.
-			 * The current example and attributor will concentrate solely on handling the attribution of the Op-stream type.
-			 */
-			if (attributionKey !== undefined && attributionKey.type === "op") {
-				const attribution = runtimeAttributor?.get(attributionKey);
-				const userString = JSON.stringify(attribution?.user);
-				const user = JSON.parse(userString) as ITinyliciousUser;
-				const timestamp = new Date(attribution?.timestamp ?? JSON.stringify(null));
-				containerDiv.textContent = `Last updated by ${
-					user.name
-				}\non ${timestamp.toLocaleString()}`;
-				containerDiv.style.display = "block";
-			} else {
-				containerDiv.style.display = "none";
-			}
-		};
 
 		updateAttributionDisplay(redAttributionKey, redAttributionContainerDiv);
 		updateAttributionDisplay(greenAttributionKey, greenAttributionContainerDiv);
