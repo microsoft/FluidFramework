6f89dd4afedaf65e45db4bf52dd85cf0e7f2a136
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-05-15 23:57:04 +0000

    fix: Type constraint

--- packages/dds/tree/src/codec/discriminatedUnions.ts
+++ packages/dds/tree/src/codec/discriminatedUnions.ts
@@ -80,7 +80,8 @@ export const unionOptions: ObjectOptions = {
  * ```
  * where only a single property of `EncodedOperation` is populated for a given encoded value.
  */
-export class DiscriminatedUnionDispatcher<TUnion extends object, TArgs extends unknown[], TResult> {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export class DiscriminatedUnionDispatcher<TUnion extends object, TArgs extends any[], TResult> {
 	private readonly library: ReadonlyMap<
 		keyof TUnion,
 		(value: unknown, ...args: TArgs) => TResult
