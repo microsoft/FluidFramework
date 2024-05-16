0671aaeab2bbad3f8c763a7f07a80e11b00658f9
Yann Achard <achardy@microsoft.com>
2024-05-16 15:43:40 -0700

    Remove duplicated assert code

--- packages/dds/tree/src/feature-libraries/sequence-field/sequenceFieldCodecV2.ts
+++ packages/dds/tree/src/feature-libraries/sequence-field/sequenceFieldCodecV2.ts
@@ -135,10 +135,7 @@ export function makeV2Codec(
 		context: ChangeEncodingContext,
 	): RevisionTag {
 		if (encodedRevision === undefined) {
-			assert(
-				context.revision !== undefined,
-				0x965 /* Implicit revision should be provided */,
-			);
+			assert(context.revision !== undefined, "Implicit revision should be provided");
 			return context.revision;
 		}
 
