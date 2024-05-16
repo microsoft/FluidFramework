3d847f56de3609872e6e87401af7e92dd3114491
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2024-05-16 10:20:24 -0700

    More docs

--- experimental/framework/tree-react-api/src/simpleIdentifier.ts
+++ experimental/framework/tree-react-api/src/simpleIdentifier.ts
@@ -10,7 +10,14 @@ const idMap = new WeakMap<object, number>();
 /**
  * Associates a unique number with an object.
  * @remarks
- * This can be handy for generating ids for React from TreeNodes.
+ * The id number is tied to the object identity, not the objects contents: modifying the object will not cause it to get a different id.
+ *
+ * This can be handy for generating [keys for React lists](https://react.dev/learn/rendering-lists#where-to-get-your-key) from TreeNodes.
+ *
+ * Most cases which could use this functions should just use the objects themselves instead of getting ids from them, since the objects will have the same equality as the ids.
+ * For example if storing data associated with the objects in a map, using the object as the key is more efficient than getting an id from it and using that.
+ * This functions exists to deal with the edge case where you would like to use object identity, but you can't.
+ * React keys are an examples of such a case, since React does not allow objects as keys.
  * @public
  */
 export function objectIdNumber(object: object): number {
