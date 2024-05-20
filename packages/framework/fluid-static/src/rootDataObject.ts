198d050b6a8edb84631e9455039aa14eb3c526ab
Matt Rakow <marakow@microsoft.com>
2024-05-20 12:08:13 -0700

    Separate file

--- packages/framework/fluid-static/src/rootDataObject.ts
+++ packages/framework/fluid-static/src/rootDataObject.ts
@@ -9,11 +9,7 @@ import {
 	DataObjectFactory,
 } from "@fluidframework/aqueduct/internal";
 import { type IRuntimeFactory } from "@fluidframework/container-definitions/internal";
-import {
-	CompressionAlgorithms,
-	type ContainerRuntime,
-	type IContainerRuntimeOptions,
-} from "@fluidframework/container-runtime/internal";
+import type { ContainerRuntime } from "@fluidframework/container-runtime/internal";
 import { type IContainerRuntime } from "@fluidframework/container-runtime-definitions/internal";
 import {
 	type FluidObject,
@@ -22,11 +18,11 @@ import {
 	type IResponse,
 } from "@fluidframework/core-interfaces";
 import { type IDirectory } from "@fluidframework/map/internal";
-import { FlushMode } from "@fluidframework/runtime-definitions/internal";
 import { RequestParser } from "@fluidframework/runtime-utils/internal";
 import type { SharedObjectKind } from "@fluidframework/shared-object-base";
 import type { ISharedObjectKind } from "@fluidframework/shared-object-base/internal";
 
+import { compatibilityModeRuntimeOptions } from "./compatibilityConfiguration.js";
 import type {
 	CompatibilityMode,
 	ContainerSchema,
@@ -178,31 +174,6 @@ export function createDOProviderContainerRuntimeFactory(props: {
 }): IRuntimeFactory {
 	return new DOProviderContainerRuntimeFactory(props.schema, props.compatibilityMode);
 }
-
-const compatibilityModeRuntimeOptions: Record<CompatibilityMode, IContainerRuntimeOptions> = {
-	"1": {
-		// 1.x clients are compatible with TurnBased flushing, but here we elect to remain on Immediate flush mode
-		// as a work-around for inability to send batches larger than 1Mb. Immediate flushing keeps batches smaller as
-		// fewer messages will be included per flush.
-		flushMode: FlushMode.Immediate,
-		// Op compression is on by default but introduces a new type of op which is not compatible with 1.x clients.
-		compressionOptions: {
-			minimumBatchSizeInBytes: Number.POSITIVE_INFINITY, // disabled
-			compressionAlgorithm: CompressionAlgorithms.lz4,
-		},
-		// Grouped batching is on by default but introduces a new type of op which is not compatible with 1.x clients.
-		enableGroupedBatching: false,
-		// TODO: Include explicit disables for things that are currently off-by-default?
-	},
-	"2": {
-		// Explicit schema control explicitly makes the container incompatible with 1.x clients, to force their
-		// ejection from collaboration and prevent container corruption.  It is off by default and must be explicitly enabled.
-		explicitSchemaControl: true,
-		// The runtime ID compressor is a prerequisite to use SharedTree but is off by default and must be explicitly enabled.
-		// It introduces a new type of op which is not compatible with 1.x clients.
-		enableRuntimeIdCompressor: "on",
-	},
-};
 
 /**
  * Container code that provides a single {@link IRootDataObject}.
