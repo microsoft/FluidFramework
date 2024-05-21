054cbf1abeae008c2649077b5fd7064b813dfe77
Kian Thompson <102998837+kian-thompson@users.noreply.github.com>
2024-05-21 12:46:49 -0700

    [bump] server: 4.0.1 => 4.0.2 (patch) (#21179)
    Bumped server from 4.0.1 to 4.0.2.

--- server/routerlicious/packages/services-shared/src/packageVersion.ts
+++ server/routerlicious/packages/services-shared/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/server-services-shared";
-export const pkgVersion = "4.0.1";
+export const pkgVersion = "4.0.2";
--- server/routerlicious/packages/services-utils/src/packageVersion.ts
+++ server/routerlicious/packages/services-utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/server-services-utils";
-export const pkgVersion = "4.0.1";
+export const pkgVersion = "4.0.2";
