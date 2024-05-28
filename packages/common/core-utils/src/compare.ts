f7319d9d4e6d10e5b3397741a4b8d4ab7a3573a5
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2024-05-28 22:21:43 +0000

    test Add noUncheckedIndexedAccess and exactOptionalPropertyTypes to tsconfig

--- packages/common/core-utils/src/compare.ts
+++ packages/common/core-utils/src/compare.ts
@@ -3,6 +3,8 @@
  * Licensed under the MIT License.
  */
 
+import { assert } from "./assert.js";
+
 /**
  * Compare two arrays.  Returns true if their elements are equivalent and in the same order.
  *
@@ -25,9 +27,21 @@ export const compareArrays = <T>(
 	//       '===' and 'Object.is()' tied.
 	//       Trivial acceptance adds no measurable overhead.
 	//       30% penalty vs. baseline for exported function [node 14 x64].
-	return (
-		left === right || // Trivial acceptance: 'left' and 'right' are the same instance
-		(left.length === right.length && // Trivial rejection: 'left' and 'right' are different lengths
-			left.every((leftItem, index) => comparator(leftItem, right[index], index)))
-	);
+	if (left === right) { // Trivial acceptance: 'left' and 'right' are the same instance
+		return true;
+	}
+
+	if(left.length !== right.length) { // Trivial rejection: 'left' and 'right' are different lengths
+		return false;
+	}
+
+	for (const [index, leftItem] of left.entries()) {
+        const rightItem = right[index];
+		assert(rightItem !== undefined, "rightItem is undefined in compareArrays");
+        if (!comparator(leftItem, rightItem, index)) {
+            return false;
+        }
+    }
+
+    return true;
 };
