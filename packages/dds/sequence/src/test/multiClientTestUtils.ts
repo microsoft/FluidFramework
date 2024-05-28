5c45994252e4223579768cd065f4e6bb991e0064
Timothy Trindle <titrindl@microsoft.com>
2024-05-28 11:42:17 -0700

    constructClients pass factory to constructClient

--- packages/dds/sequence/src/test/multiClientTestUtils.ts
+++ packages/dds/sequence/src/test/multiClientTestUtils.ts
@@ -78,6 +78,7 @@ export function constructClients(
 			containerRuntimeFactory,
 			String.fromCharCode(index + 65),
 			dataStoreRuntimeOptions,
+			factory,
 		);
 
 		sharedString.connect(services);
