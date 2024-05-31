9bf98f07dbe0f3d043abc0d2873507e98483360e
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-05-31 15:00:33 -0700

    test(devtools-view): Remove dead screenshot tests (#21248)
    These tests have been dead for quite a while, and their existence
    creates more problems than potential value. We will come back and
    reconsider options for visual regression testing in the near future.
    
    
    [AB#8120](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/8120)

--- packages/tools/devtools/devtools-view/src/test/Waiting.test.tsx
+++ packages/tools/devtools/devtools-view/src/test/Waiting.test.tsx
@@ -6,7 +6,7 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import { Waiting, defaultWaitingLabel } from "../../components/index.js";
+import { Waiting, defaultWaitingLabel } from "../components/index.js";
 
 describe("Waiting component tests", () => {
 	it("Displays default label when a label is not specified", async (): Promise<void> => {
