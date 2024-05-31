95d86fabf2113ae8fc74c0b40f2f0f556dbbc67e
Abram Sanderson <absander@microsoft.com>
2024-05-31 11:49:05 -0700

    Add spaces after copyright header on policy check

--- examples/data-objects/inventory-app/src/schema.ts
+++ examples/data-objects/inventory-app/src/schema.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 // eslint-disable-next-line import/no-deprecated
 import { SchemaFactory, TreeConfiguration } from "@fluidframework/tree";
 
