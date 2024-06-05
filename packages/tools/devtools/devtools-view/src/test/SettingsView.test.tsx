cec96e810aedcd9e58b297e5801610f58f4bac63
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-06-05 13:29:39 -0700

    navigation test progress

--- packages/tools/devtools/devtools-view/src/test/SettingsView.test.tsx
+++ packages/tools/devtools/devtools-view/src/test/SettingsView.test.tsx
@@ -7,6 +7,7 @@
 // eslint-disable-next-line import/no-unassigned-import
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import * as axe from "axe-core";
 import React from "react";
 
@@ -20,5 +21,38 @@ describe("SettingsView Accessibility Check", () => {
 			console.log("Accessibility violations:", results.violations);
 		}
 		expect(results.violations.length).toBe(0);
+	});
+
+	it("Can tab/arrow navigate through the SettingsView", async () => {
+		render(<SettingsView />);
+		const user = userEvent.setup();
+
+		// Focus on the first interactive element
+		// userEvent.tab();
+		// expect(screen.getByText("Theme").closest("div")).toHaveFocus();
+
+		// Tab to the Dropdown
+		await user.tab();
+		// expect(screen.getByRole("combobox")).toHaveFocus();
+
+		// // Open the dropdown and use arrow keys to navigate options
+		// userEvent.keyboard("{ArrowDown}");
+		// expect(screen.getByText("Light")).toHaveFocus();
+		// userEvent.keyboard("{ArrowDown}");
+		// expect(screen.getByText("Dark")).toHaveFocus();
+		// userEvent.keyboard("{ArrowDown}");
+		// expect(screen.getByText("High Contrast")).toHaveFocus();
+
+		// // Tab to the next section header
+		// userEvent.tab();
+		// expect(screen.getByText("Usage telemetry").closest("div")).toHaveFocus();
+
+		// // Tab to the link
+		// userEvent.tab();
+		// expect(screen.getByRole("link", { name: "Microsoft Privacy Statement" })).toHaveFocus();
+
+		// // Tab to the switch
+		// userEvent.tab();
+		// expect(screen.getByRole("checkbox")).toHaveFocus();
 	});
 });
