da8215b5835b58d5b78f62d21c0a43c74d9aacdc
Tyler Butler <tyler@tylerbutler.com>
2024-06-06 11:13:25 -0700

    feedback

--- build-tools/packages/build-cli/test/commands/generate/buildVersion.test.ts
+++ build-tools/packages/build-cli/test/commands/generate/buildVersion.test.ts
@@ -3,6 +3,7 @@
  * Licensed under the MIT License.
  */
 
+import { EOL } from "node:os";
 import { expect } from "@oclif/test";
 
 import { initializeCommandTestFunction } from "../../init.js";
@@ -16,22 +17,45 @@ const test_tags = [
 	"client_v1.2.4",
 	"client_v1.2.3",
 	"client_v2.0.0-rc.1.0.0",
-	"client_v2.0.0-rc.1.0.1",
-	"client_v2.0.0-rc.2.0.0",
 	"client_v2.0.0",
+	"client_v2.0.0-rc.1.0.1",
 	"client_v2.0.1",
-	"client_v2.1.0",
+	"client_v2.0.0-rc.2.0.0",
 	"client_v2.1.1",
+	"client_v2.0.0-rc.5.0.0",
 	"client_v2.0.0-rc.3.0.0",
 	"client_v2.0.0-rc.4.0.0",
-	"client_v2.0.0-rc.5.0.0",
-	"client_v2.0.0-rc.6.0.0",
+	"client_v2.1.0",
 	"build-tools_v0.5.2002",
 	"build-tools_v0.4.2001",
-	"build-tools_v0.4.2000",
+	"client_v2.0.0-rc.6.0.0",
 	"build-tools_v0.4.1000",
 	"build-tools_v0.3.2000",
+	"build-tools_v0.4.2000",
 ];
+
+/**
+ * Convenience function to check if a particular line of stdout output equals the expected value.
+ *
+ * @param stdout - the complete stdout string.
+ * @param lineIndex - the index of the line to check.
+ * @param testValue - the value to test against
+ * @returns An assertion that will fail if the line doesn't match the value and pass if it does.
+ */
+function stdoutLineEquals(
+	stdout: string,
+	lineIndex: number,
+	testValue: string,
+): Chai.Assertion {
+	const lines = stdout.split(EOL);
+	if (lineIndex > lines.length) {
+		console.error(lines);
+		throw new Error(
+			`stdout only split into ${lines.length} lines, but lineIndex is ${lineIndex}.`,
+		);
+	}
+	return expect(lines[lineIndex]).equals(testValue);
+}
 
 describe("generate:buildVersion", () => {
 	test
@@ -51,7 +75,7 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("outputs prerelease build number", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.0-12345");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.0-12345");
 		});
 
 	test
@@ -72,7 +96,7 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("reads build number from env variable", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.0-88802");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.0-88802");
 		});
 
 	test
@@ -93,7 +117,7 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("calculates patch version number correctly", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.12345");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.12345");
 		});
 
 	test
@@ -115,7 +139,7 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("reads patch setting from env variable", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.12345");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.12345");
 		});
 
 	test
@@ -134,8 +158,8 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("outputs isLatest=true when release is release", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.5.2002");
-			expect(ctx.stdout).to.contain("isLatest=true");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.5.2002");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=true");
 		});
 
 	test
@@ -154,8 +178,8 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("outputs isLatest=false when release is prerelease", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.5.2002-12345");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.5.2002-12345");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=false");
 		});
 
 	test
@@ -175,8 +199,8 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("reads release setting from env variable", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.5.2002");
-			expect(ctx.stdout).to.contain("isLatest=true");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.5.2002");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=true");
 		});
 
 	test
@@ -196,8 +220,8 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("reads tag name from env variable", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.5.2002");
-			expect(ctx.stdout).to.contain("isLatest=true");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.5.2002");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=true");
 		});
 
 	test
@@ -218,8 +242,8 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("isLatest=false when including internal versions when determining what's latest", (ctx) => {
-			expect(ctx.stdout).to.contain("version=1.2.4");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 1, "version=1.2.4-12345");
+			stdoutLineEquals(ctx.stdout, 4, "isLatest=false");
 		});
 
 	test
@@ -241,8 +265,8 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("reads internal versions setting from env variable", (ctx) => {
-			expect(ctx.stdout).to.contain("version=1.2.4");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 1, "version=1.2.4-12345");
+			stdoutLineEquals(ctx.stdout, 4, "isLatest=false");
 		});
 
 	test
@@ -263,8 +287,8 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("calculates test build numbers correctly", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.0.0-12345-test");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 3, "version=0.0.0-12345-test");
+			stdoutLineEquals(ctx.stdout, 6, "isLatest=false");
 		});
 
 	test
@@ -286,8 +310,8 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("reads test build setting from env variable", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.0.0-12345-test");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 3, "version=0.0.0-12345-test");
+			stdoutLineEquals(ctx.stdout, 6, "isLatest=false");
 		});
 
 	test
@@ -323,8 +347,8 @@ describe("generate:buildVersion", () => {
 			"tinylicious_v0.4.86381",
 		])
 		.it("tinylicious test case from 2022-08-26", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.88879");
-			expect(ctx.stdout).to.contain("isLatest=true");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.88879");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=true");
 		});
 
 	test
@@ -370,8 +394,8 @@ describe("generate:buildVersion", () => {
 			"client_v2.0.0-internal.1.4.2",
 		])
 		.it("lts test case from 2022-10-13", (ctx) => {
-			expect(ctx.stdout).to.contain("version=1.3.0-100339");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 1, "version=1.3.0-100339");
+			stdoutLineEquals(ctx.stdout, 4, "isLatest=false");
 		});
 
 	test
@@ -393,8 +417,8 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("RC version, prerelease", (ctx) => {
-			expect(ctx.stdout).to.contain("version=2.0.0-dev-rc.3.0.0.212045");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 1, "version=2.0.0-dev-rc.3.0.0.212045");
+			stdoutLineEquals(ctx.stdout, 4, "isLatest=false");
 		});
 
 	test
@@ -416,8 +440,8 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("RC version, test", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.0.0-212045-test");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 3, "version=0.0.0-212045-test");
+			stdoutLineEquals(ctx.stdout, 6, "isLatest=false");
 		});
 
 	test
@@ -437,8 +461,8 @@ describe("generate:buildVersion", () => {
 			...test_tags,
 		])
 		.it("RC version, release", (ctx) => {
-			expect(ctx.stdout).to.contain("version=2.0.0-rc.7.0.0");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 0, "version=2.0.0-rc.7.0.0");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=false");
 		});
 
 	test
@@ -452,8 +476,8 @@ describe("generate:buildVersion", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "2.0.0", "--tags", ...test_tags])
 		.it("2.0 version, release", (ctx) => {
-			expect(ctx.stdout).to.contain("version=2.0.0");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 0, "version=2.0.0");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=false");
 		});
 
 	test
@@ -467,8 +491,8 @@ describe("generate:buildVersion", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "2.0.0", "--tags", ...test_tags])
 		.it("2.0 version, prerelease", (ctx) => {
-			expect(ctx.stdout).to.contain("version=2.0.0-212045");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 0, "version=2.0.0-212045");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=false");
 		});
 
 	test
@@ -482,8 +506,8 @@ describe("generate:buildVersion", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "2.0.0", "--tags", ...test_tags])
 		.it("2.0 version, test", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.0.0-212045-test");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 2, "version=0.0.0-212045-test");
+			stdoutLineEquals(ctx.stdout, 5, "isLatest=false");
 		});
 
 	test
@@ -497,8 +521,23 @@ describe("generate:buildVersion", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "2.1.2", "--tags", ...test_tags])
 		.it("next unpublished patch version on latest minor (2.1.2) returns isLatest true", (ctx) => {
-			expect(ctx.stdout).to.contain("version=2.1.2");
-			expect(ctx.stdout).to.contain("isLatest=true");
+			stdoutLineEquals(ctx.stdout, 0, "version=2.1.2");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=true");
+		});
+
+	test
+		.env({
+			VERSION_BUILDNUMBER: "212045",
+			VERSION_TAGNAME: "client",
+			TEST_BUILD: "false",
+			VERSION_RELEASE: "release",
+			VERSION_INCLUDE_INTERNAL_VERSIONS: "True",
+		})
+		.stdout()
+		.command(["generate:buildVersion", "--fileVersion", "2.0.2", "--tags", ...test_tags])
+		.it("next unpublished patch version on non-latest minor (2.0.2) returns isLatest false", (ctx) => {
+			stdoutLineEquals(ctx.stdout, 0, "version=2.0.2");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=false");
 		});
 
 	test
@@ -512,8 +551,8 @@ describe("generate:buildVersion", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "2.2.0", "--tags", ...test_tags])
 		.it("2.2.0 version returns isLatest true", (ctx) => {
-			expect(ctx.stdout).to.contain("version=2.2.0");
-			expect(ctx.stdout).to.contain("isLatest=true");
+			stdoutLineEquals(ctx.stdout, 0, "version=2.2.0");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=true");
 		});
 });
 
@@ -531,8 +570,8 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: prerelease, types: alpha", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.0-88879-alpha-types");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.0-88879-alpha-types");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=false");
 		});
 
 	test
@@ -548,8 +587,8 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: prerelease, types: beta", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.0-88879-beta-types");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.0-88879-beta-types");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=false");
 		});
 
 	test
@@ -565,8 +604,8 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: prerelease, types: none", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.0-88879");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.0-88879");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=false");
 		});
 
 	test
@@ -582,8 +621,8 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: prerelease, types: public", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.0-88879");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.0-88879");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=false");
 		});
 
 	test
@@ -599,8 +638,8 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: prerelease, types: untrimmed", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.0-88879");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.0-88879");
+			stdoutLineEquals(ctx.stdout, 3, "isLatest=false");
 		});
 
 	test
@@ -616,7 +655,7 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: release, types: untrimmed", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.0");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.0");
 		});
 
 	test
@@ -632,7 +671,7 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: release, types: public", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.4.0");
+			stdoutLineEquals(ctx.stdout, 0, "version=0.4.0");
 		});
 
 	test
@@ -648,8 +687,8 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: prerelease, test-build: true, types: alpha", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.0.0-88879-test-alpha-types");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 2, "version=0.0.0-88879-test-alpha-types");
+			stdoutLineEquals(ctx.stdout, 5, "isLatest=false");
 		});
 
 	test
@@ -665,8 +704,8 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: prerelease, test-build: true, types: beta", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.0.0-88879-test-beta-types");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 2, "version=0.0.0-88879-test-beta-types");
+			stdoutLineEquals(ctx.stdout, 5, "isLatest=false");
 		});
 
 	test
@@ -682,8 +721,8 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: prerelease, test-build: true, types: none", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.0.0-88879-test");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 2, "version=0.0.0-88879-test");
+			stdoutLineEquals(ctx.stdout, 5, "isLatest=false");
 		});
 
 	test
@@ -699,8 +738,8 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: prerelease, test-build: true, types: public", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.0.0-88879-test");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 2, "version=0.0.0-88879-test");
+			stdoutLineEquals(ctx.stdout, 5, "isLatest=false");
 		});
 
 	test
@@ -716,8 +755,8 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "0.4.0"])
 		.it("tagName: client, release: prerelease, test-build: true, types: untrimmed", (ctx) => {
-			expect(ctx.stdout).to.contain("version=0.0.0-88879-test");
-			expect(ctx.stdout).to.contain("isLatest=false");
+			stdoutLineEquals(ctx.stdout, 2, "version=0.0.0-88879-test");
+			stdoutLineEquals(ctx.stdout, 5, "isLatest=false");
 		});
 
 	test
@@ -733,7 +772,7 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "2.0.0-dev.7.1.0"])
 		.it("tagName: client, release: prerelease, test-build: false, types: alpha", (ctx) => {
-			expect(ctx.stdout).to.contain("version=2.0.0-dev.7.1.0.88879-alpha-types");
+			stdoutLineEquals(ctx.stdout, 0, "version=2.0.0-dev.7.1.0.88879-alpha-types");
 		});
 
 	test
@@ -749,7 +788,7 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "2.0.0-dev.7.1.0"])
 		.it("tagName: client, release: prerelease, test-build: false, types: beta", (ctx) => {
-			expect(ctx.stdout).to.contain("version=2.0.0-dev.7.1.0.88879-beta-types");
+			stdoutLineEquals(ctx.stdout, 0, "version=2.0.0-dev.7.1.0.88879-beta-types");
 		});
 
 	test
@@ -765,7 +804,7 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "2.0.0-dev.7.1.0"])
 		.it("tagName: client, release: prerelease, test-build: false, types: public", (ctx) => {
-			expect(ctx.stdout).to.contain("version=2.0.0-dev.7.1.0.88879");
+			stdoutLineEquals(ctx.stdout, 0, "version=2.0.0-dev.7.1.0.88879");
 		});
 
 	test
@@ -781,6 +820,6 @@ describe("generate:buildVersion for alpha/beta", () => {
 		.stdout()
 		.command(["generate:buildVersion", "--fileVersion", "2.0.0-dev.7.1.0"])
 		.it("tagName: client, release: prerelease, test-build: false, types: untrimmed", (ctx) => {
-			expect(ctx.stdout).to.contain("version=2.0.0-dev.7.1.0.88879");
+			stdoutLineEquals(ctx.stdout, 0, "version=2.0.0-dev.7.1.0.88879");
 		});
 });
