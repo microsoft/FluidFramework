f2afe853fda00cc6b8936e9580353d262dff1ff7
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2024-06-07 02:06:58 +0000

    typecast isnt needed yet

--- packages/dds/merge-tree/src/client.ts
+++ packages/dds/merge-tree/src/client.ts
@@ -700,7 +700,7 @@ export class Client extends TypedEventEmitter<IClientEvents> {
 		return this.clientNameToIds.get(longClientId)!.data;
 	}
 
-	getLongClientId(shortClientId: number): string {
+	getLongClientId(shortClientId: number) {
 		return shortClientId >= 0 ? this.shortClientIdMap[shortClientId] : "original";
 	}
 
