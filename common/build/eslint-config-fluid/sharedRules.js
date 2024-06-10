6edcb53db387136b0d90873ddd44228393ab546a
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-06-10 20:16:17 +0000

    docs: Add copyright notice

--- common/build/eslint-config-fluid/sharedRules.js
+++ common/build/eslint-config-fluid/sharedRules.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 // This module contains rule details to be shared between the config variants exposed by this package.
 module.exports = {
 	/**
