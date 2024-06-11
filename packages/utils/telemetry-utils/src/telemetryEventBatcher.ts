ce794d87800e658264391da5dd68dc2618168976
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-06-11 21:54:08 +0000

    policy-check

--- packages/utils/telemetry-utils/src/telemetryEventBatcher.ts
+++ packages/utils/telemetry-utils/src/telemetryEventBatcher.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import { truncateToDecimalPlaces } from "./mathTools.js";
 import type {
 	ITelemetryGenericEventExt,
