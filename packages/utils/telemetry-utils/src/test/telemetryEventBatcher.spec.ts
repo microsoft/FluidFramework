951c4c45731b168bdb9754b7067fc2845f0cfaf5
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-06-13 01:05:58 +0000

    comment out test case

--- packages/utils/telemetry-utils/src/test/telemetryEventBatcher.spec.ts
+++ packages/utils/telemetry-utils/src/test/telemetryEventBatcher.spec.ts
@@ -105,7 +105,7 @@ describe("TelemetryEventBatcher", () => {
 		assert.strictEqual(logger.events.length, 2);
 	});
 
-	it("returns correct results for each property", () => {
+	it.only("returns correct results for each property", () => {
 		const threshold = 10;
 		const eventBatcher = new TelemetryEventBatcher<keyof TestTelemetryProperties>(
 			{ eventName: "testEvent" },
@@ -122,7 +122,7 @@ describe("TelemetryEventBatcher", () => {
 				},
 			}));
 		}
-		const result = eventBatcher.measure(() => ({
+		const resultOne = eventBatcher.measure(() => ({
 			telemetryProperties: {
 				propertyOne: 1,
 				propertyTwo: 2,
@@ -131,8 +131,31 @@ describe("TelemetryEventBatcher", () => {
 		}));
 
 		assert.strictEqual(logger.events.length, 1);
-		assert.strictEqual(result.telemetryProperties.propertyOne, 1);
-		assert.strictEqual(result.telemetryProperties.propertyTwo, 2);
-		assert.strictEqual(result.telemetryProperties.propertyThree, 3);
+		assert.strictEqual(resultOne.telemetryProperties.propertyOne, 1);
+		assert.strictEqual(resultOne.telemetryProperties.propertyTwo, 2);
+		assert.strictEqual(resultOne.telemetryProperties.propertyThree, 3);
+
+		// for (let i = 1; i < threshold; i++) {
+		// 	eventBatcher.measure(() => ({
+		// 		telemetryProperties: {
+		// 			propertyOne: i,
+		// 			propertyTwo: i,
+		// 			propertyThree: i,
+		// 		},
+		// 	}));
+		// }
+
+		// const resultTwo = eventBatcher.measure(() => ({
+		// 	telemetryProperties: {
+		// 		propertyOne: threshold,
+		// 		propertyTwo: threshold,
+		// 		propertyThree: threshold,
+		// 	},
+		// }));
+
+		// assert.strictEqual(logger.events.length, 2);
+		// assert.strictEqual(resultTwo.telemetryProperties.propertyOne, 5.5);
+		// assert.strictEqual(resultTwo.telemetryProperties.propertyTwo, 5.5);
+		// assert.strictEqual(resultTwo.telemetryProperties.propertyThree, 5.5);
 	});
 });
