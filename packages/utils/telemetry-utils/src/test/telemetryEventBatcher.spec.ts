9b941352354a62ab655d3a4f520f630e9cd82f26
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-06-13 17:45:37 +0000

    fix tests

--- packages/utils/telemetry-utils/src/test/telemetryEventBatcher.spec.ts
+++ packages/utils/telemetry-utils/src/test/telemetryEventBatcher.spec.ts
@@ -85,7 +85,6 @@ describe("TelemetryEventBatcher", () => {
 
 		for (let i = 0; i < threshold - 1; i++) {
 			eventBatcher.measure(() => ({
-				eventName: "testCall",
 				telemetryProperties: {
 					propertyOne: 1,
 					propertyTwo: 2,
@@ -96,7 +95,6 @@ describe("TelemetryEventBatcher", () => {
 		assert.strictEqual(logger.events.length, 1);
 
 		eventBatcher.measure(() => ({
-			eventName: "testCall",
 			telemetryProperties: {
 				propertyOne: 1,
 				propertyTwo: 2,
@@ -114,6 +112,34 @@ describe("TelemetryEventBatcher", () => {
 			threshold,
 		);
 
+		for (let i = 0; i < threshold; i++) {
+			eventBatcher.measure(() => ({
+				telemetryProperties: {
+					propertyOne: 1,
+					propertyTwo: 2,
+					propertyThree: 3,
+				},
+			}));
+		}
+
+		assert.strictEqual(logger.events.length, 1);
+
+		assert.strictEqual(logger.events[0].avgpropertyOne, 1);
+		assert.strictEqual(logger.events[0].avgpropertyTwo, 2);
+		assert.strictEqual(logger.events[0].avgpropertyThree, 3);
+		assert.strictEqual(logger.events[0].maxpropertyOne, 1);
+		assert.strictEqual(logger.events[0].maxpropertyTwo, 2);
+		assert.strictEqual(logger.events[0].maxpropertyThree, 3);
+	});
+
+	it("contains correct telemetryProperties returned from the logger", () => {
+		const threshold = 10;
+		const eventBatcher = new TelemetryEventBatcher<keyof TestTelemetryProperties>(
+			{ eventName: "testEvent" },
+			logger,
+			threshold,
+		);
+
 		for (let i = 0; i < threshold - 1; i++) {
 			eventBatcher.measure(() => ({
 				telemetryProperties: {
@@ -123,7 +149,7 @@ describe("TelemetryEventBatcher", () => {
 				},
 			}));
 		}
-		const resultOne = eventBatcher.measure(() => ({
+		const result = eventBatcher.measure(() => ({
 			telemetryProperties: {
 				propertyOne: 1,
 				propertyTwo: 2,
@@ -133,21 +159,18 @@ describe("TelemetryEventBatcher", () => {
 
 		assert.strictEqual(logger.events.length, 1);
 
-		assert.strictEqual(resultOne.telemetryProperties.propertyOne, 1);
-		assert.strictEqual(resultOne.telemetryProperties.propertyTwo, 2);
-		assert.strictEqual(resultOne.telemetryProperties.propertyThree, 3);
+		assert.strictEqual(result.telemetryProperties.propertyOne, 1);
+		assert.strictEqual(result.telemetryProperties.propertyTwo, 2);
+		assert.strictEqual(result.telemetryProperties.propertyThree, 3);
 	});
 
-	it("correctly calculates average and max values", () => {
+	it("correctly calculates average and max values for multiple events", () => {
 		const threshold = 10;
 		const eventBatcher = new TelemetryEventBatcher<keyof TestTelemetryProperties>(
 			{ eventName: "testEvent" },
 			logger,
 			threshold,
 		);
-
-		const spy = sinon.spy(logger, "sendPerformanceEvent");
-		let loggedEvent: ITelemetryPerformanceEventExt;
 
 		for (let i = 1; i <= threshold; i++) {
 			eventBatcher.measure(() => ({
@@ -160,14 +183,13 @@ describe("TelemetryEventBatcher", () => {
 		}
 
 		assert.strictEqual(logger.events.length, 1);
-		loggedEvent = logger.events[0];
 
-		assert.strictEqual(loggedEvent.avgpropertyOne, 5.5);
-		assert.strictEqual(loggedEvent.avgpropertyTwo, 15.5);
-		assert.strictEqual(loggedEvent.avgpropertyThree, 105.5);
-		assert.strictEqual(loggedEvent.maxpropertyOne, 10);
-		assert.strictEqual(loggedEvent.maxpropertyTwo, 20);
-		assert.strictEqual(loggedEvent.maxpropertyThree, 110);
+		assert.strictEqual(logger.events[0].avgpropertyOne, 5.5);
+		assert.strictEqual(logger.events[0].avgpropertyTwo, 15.5);
+		assert.strictEqual(logger.events[0].avgpropertyThree, 105.5);
+		assert.strictEqual(logger.events[0].maxpropertyOne, 10);
+		assert.strictEqual(logger.events[0].maxpropertyTwo, 20);
+		assert.strictEqual(logger.events[0].maxpropertyThree, 110);
 
 		// More calls to validate that the average and max are "fresh", i.e. the previous data was cleared when the first telemetry event was generated
 		for (let i = 101; i <= threshold + 100; i++) {
@@ -181,16 +203,13 @@ describe("TelemetryEventBatcher", () => {
 		}
 
 		assert.strictEqual(logger.events.length, 2);
-		loggedEvent = logger.events[1];
 
-		assert.strictEqual(loggedEvent.avgpropertyOne, 105.5);
-		assert.strictEqual(loggedEvent.avgpropertyTwo, 115.5);
-		assert.strictEqual(loggedEvent.avgpropertyThree, 205.5);
-		assert.strictEqual(loggedEvent.maxpropertyOne, 110);
-		assert.strictEqual(loggedEvent.maxpropertyTwo, 120);
-		assert.strictEqual(loggedEvent.maxpropertyThree, 210);
-
-		spy.restore();
+		assert.strictEqual(logger.events[1].avgpropertyOne, 105.5);
+		assert.strictEqual(logger.events[1].avgpropertyTwo, 115.5);
+		assert.strictEqual(logger.events[1].avgpropertyThree, 205.5);
+		assert.strictEqual(logger.events[1].maxpropertyOne, 110);
+		assert.strictEqual(logger.events[1].maxpropertyTwo, 120);
+		assert.strictEqual(logger.events[1].maxpropertyThree, 210);
 	});
 
 	it("separately emits event in different instances", () => {
@@ -212,7 +231,6 @@ describe("TelemetryEventBatcher", () => {
 
 		for (let i = 0; i < thresholdOne - 1; i++) {
 			batcherOne.measure(() => ({
-				eventName: "testCall",
 				telemetryProperties: {
 					propertyOne: 1,
 					propertyTwo: 2,
@@ -223,7 +241,6 @@ describe("TelemetryEventBatcher", () => {
 
 		for (let i = 0; i < thresholdTwo - 1; i++) {
 			batcherTwo.measure(() => ({
-				eventName: "testCall",
 				telemetryProperties: {
 					propertyOne: 4,
 					propertyTwo: 5,
@@ -236,7 +253,6 @@ describe("TelemetryEventBatcher", () => {
 		assert.strictEqual(loggerTwo.events.length, 0);
 
 		const resultOne = batcherOne.measure(() => ({
-			eventName: "testCall",
 			telemetryProperties: {
 				propertyOne: 1,
 				propertyTwo: 2,
@@ -245,7 +261,6 @@ describe("TelemetryEventBatcher", () => {
 		}));
 
 		const resultTwo = batcherTwo.measure(() => ({
-			eventName: "testCall",
 			telemetryProperties: {
 				propertyOne: 4,
 				propertyTwo: 5,
@@ -265,69 +280,7 @@ describe("TelemetryEventBatcher", () => {
 		assert.strictEqual(resultTwo.telemetryProperties.propertyThree, 6);
 	});
 
-	it("correctly calculates data in different instances", () => {
-		const thresholdOne = 10;
-		const loggerOne = new TestLogger();
-		const batcherOne = new TelemetryEventBatcher<keyof TestTelemetryProperties>(
-			{ eventName: "testEventOne" },
-			loggerOne,
-			thresholdOne,
-		);
-		const spyOne = sinon.spy(loggerOne, "sendPerformanceEvent");
-
-		const thresholdTwo = 20;
-		const loggerTwo = new TestLogger();
-		const batcherTwo = new TelemetryEventBatcher<keyof TestTelemetryProperties>(
-			{ eventName: "testEventTwo" },
-			loggerTwo,
-			thresholdTwo,
-		);
-		const spyTwo = sinon.spy(loggerTwo, "sendPerformanceEvent");
-
-		for (let i = 1; i <= thresholdOne; i++) {
-			batcherOne.measure(() => ({
-				eventName: "testCall",
-				telemetryProperties: {
-					propertyOne: i + 10,
-					propertyTwo: i * 10,
-					propertyThree: i ** 2,
-				},
-			}));
-		}
-
-		for (let i = 1; i <= thresholdTwo; i++) {
-			batcherTwo.measure(() => ({
-				eventName: "testCall",
-				telemetryProperties: {
-					propertyOne: i + 100,
-					propertyTwo: i * 100,
-					propertyThree: i ** 3,
-				},
-			}));
-		}
-
-		const loggedEventOne = loggerOne.events[0];
-		const loggedEventTwo = loggerTwo.events[0];
-
-		assert.strictEqual(loggedEventOne.avgpropertyOne, 15.5);
-		assert.strictEqual(loggedEventOne.avgpropertyTwo, 55);
-		assert.strictEqual(loggedEventOne.avgpropertyThree, 38.5);
-		assert.strictEqual(loggedEventOne.maxpropertyOne, 20);
-		assert.strictEqual(loggedEventOne.maxpropertyTwo, 100);
-		assert.strictEqual(loggedEventOne.maxpropertyThree, 100);
-
-		assert.strictEqual(loggedEventTwo.avgpropertyOne, 110.5);
-		assert.strictEqual(loggedEventTwo.avgpropertyTwo, 1050);
-		assert.strictEqual(loggedEventTwo.avgpropertyThree, 2205);
-		assert.strictEqual(loggedEventTwo.maxpropertyOne, 120);
-		assert.strictEqual(loggedEventTwo.maxpropertyTwo, 2000);
-		assert.strictEqual(loggedEventTwo.maxpropertyThree, 8000);
-
-		spyOne.restore();
-		spyTwo.restore();
-	});
-
-	it("correctly calculates average, max values, and duration", () => {
+	it("correctly calculates duration", () => {
 		const threshold = 10;
 		const eventBatcher = new TelemetryEventBatcher<keyof TestTelemetryProperties>(
 			{ eventName: "testEvent" },
@@ -336,35 +289,25 @@ describe("TelemetryEventBatcher", () => {
 		);
 
 		const clock = sinon.useFakeTimers();
-		const spy = sinon.spy(logger, "sendPerformanceEvent");
+		const startingPoint = 50; // Arbitrary starting point.
+		let totalTime = 0; // Sum of all durations.
 
 		for (let i = 1; i <= threshold; i++) {
-			const startTime = i * 100;
-			const endTime = startTime + 50;
-			clock.tick(endTime - startTime);
 			eventBatcher.measure(() => {
-				clock.tick(50);
+				clock.tick(startingPoint + i);
+				totalTime += startingPoint + i;
+
 				return {
 					telemetryProperties: {
-						propertyOne: i,
-						propertyTwo: i + 10,
-						propertyThree: i + 100,
+						propertyOne: 1,
+						propertyTwo: 2,
+						propertyThree: 3,
 					},
 				};
 			});
 		}
 
 		assert.strictEqual(logger.events.length, 1);
-		const loggedEvent = logger.events[0];
-
-		assert.strictEqual(loggedEvent.avgpropertyOne, 5.5);
-		assert.strictEqual(loggedEvent.avgpropertyTwo, 15.5);
-		assert.strictEqual(loggedEvent.avgpropertyThree, 105.5);
-		assert.strictEqual(loggedEvent.maxpropertyOne, 10);
-		assert.strictEqual(loggedEvent.maxpropertyTwo, 20);
-		assert.strictEqual(loggedEvent.maxpropertyThree, 110);
-		assert.strictEqual(loggedEvent.duration, 50);
-
-		spy.restore();
+		assert.strictEqual(logger.events[0].duration, totalTime / threshold);
 	});
 });
