1fd61218f376e7b57d430d646ea191911d3b86be
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-06-13 23:14:45 +0000

    fix documentation

--- packages/dds/shared-object-base/src/TelemetryProperties.ts
+++ packages/dds/shared-object-base/src/TelemetryProperties.ts
@@ -4,7 +4,8 @@
  */
 
 /**
- * TODO
+ * Telemetry properties to include for the {@link TelemetryEventBatcher} logger.
+ * The set of properties must be the same for all calls to the `measure` function.
  */
 export interface ITelemetryProperties {
 	sequenceDifference: number;
