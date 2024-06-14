42de648e7e0da87f243a2ac667e627dfe15a1cd1
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-06-14 19:52:04 +0000

    Transfer TelemetryEventBatcher logic to SampledTelemetryHelper

--- packages/utils/telemetry-utils/src/sampledTelemetryHelper.ts
+++ packages/utils/telemetry-utils/src/sampledTelemetryHelper.ts
@@ -6,6 +6,7 @@
 import { performance } from "@fluid-internal/client-utils";
 import type { IDisposable, ITelemetryBaseProperties } from "@fluidframework/core-interfaces";
 
+import { roundToDecimalPlaces } from "./mathTools.js";
 import {
 	type ITelemetryGenericEventExt,
 	ITelemetryLoggerExt,
@@ -47,6 +48,14 @@ interface Measurements {
 }
 
 /**
+ * TODO
+ */
+interface LoggerData<TCustomDataKey extends string> {
+	measurements: Measurements;
+	customData?: { readonly [key in TCustomDataKey]: number };
+}
+
+/**
  * Helper class that executes a specified code block and writes an
  * {@link @fluidframework/core-interfaces#ITelemetryPerformanceEvent} to a specified logger every time a specified
  * number of executions is reached (or when the class is disposed).
@@ -57,10 +66,12 @@ interface Measurements {
  *
  * @internal
  */
-export class SampledTelemetryHelper implements IDisposable {
+export class SampledTelemetryHelper<TCustomMetrics extends string> implements IDisposable {
 	disposed: boolean = false;
 
-	private readonly measurementsMap = new Map<string, Measurements>();
+	private readonly measurementsMap = new Map<string, LoggerData<TCustomMetrics>>();
+	private readonly dataSums: { [key in TCustomMetrics]?: number } = {};
+	private readonly dataMaxes: { [key in TCustomMetrics]?: number } = {};
 
 	/**
 	 * @param eventBase -
@@ -98,16 +109,27 @@ export class SampledTelemetryHelper implements IDisposable {
 	 * If no such distinction needs to be made, do not provide a value.
 	 * @returns Whatever the passed-in code block returns.
 	 */
-	public measure<T>(codeToMeasure: () => T, bucket: string = ""): T {
+	public measure<T extends LoggerData<TCustomMetrics>>(
+		codeToMeasure: () => T,
+		bucket: string = "",
+	): T {
 		const start = performance.now();
 		const returnValue = codeToMeasure();
 		const duration = performance.now() - start;
 
-		let m = this.measurementsMap.get(bucket);
-		if (m === undefined) {
-			m = { count: 0, duration: -1 };
-			this.measurementsMap.set(bucket, m);
+		let loggerData = this.measurementsMap.get(bucket);
+		if (loggerData === undefined) {
+			loggerData = {
+				measurements: { count: 0, duration: -1 },
+			};
+			this.measurementsMap.set(bucket, loggerData);
 		}
+
+		if (returnValue.customData) {
+			this.accumulateCustomData(returnValue.customData);
+		}
+
+		const m = loggerData.measurements;
 		m.count++;
 		m.duration = duration;
 
@@ -124,19 +146,72 @@ export class SampledTelemetryHelper implements IDisposable {
 		return returnValue;
 	}
 
+	/**
+	 * Accumulates the custom data and sends it to the logger every {@link SampledTelemetryHelper.sampleThreshold} calls.
+	 *
+	 * @param customData -
+	 * A record storing the custom data to be accumulated and eventually logged.
+	 */
+	private accumulateCustomData(customData: Record<TCustomMetrics, number>): void {
+		for (const key of Object.keys(customData) as TCustomMetrics[]) {
+			this.dataSums[key] = (this.dataSums[key] ?? 0) + customData[key];
+			this.dataMaxes[key] = Math.max(
+				this.dataMaxes[key] ?? Number.NEGATIVE_INFINITY,
+				customData[key],
+			);
+		}
+	}
+
+	/**
+	 * Computes average and sets new names for keys.
+	 *
+	 * @param customData - A record storing the custom data that has been accumulated over {@link SampledTelemetryHelper.sampleThreshold} times.
+	 * @param counts - The number of times the {@link SampledTelemetryHelper.measure} has been called.
+	 * @returns A record with the average and maximum values of the custom data.
+	 */
+	private processCustomData(
+		customData: Record<TCustomMetrics, number>,
+		counts: number,
+	): Record<string, number> {
+		const processedCustomData: Record<string, number> = {};
+
+		for (const key of Object.keys(customData) as TCustomMetrics[]) {
+			if (this.dataSums[key] !== undefined) {
+				processedCustomData[`avg${key}`] = roundToDecimalPlaces(
+					this.dataSums[key]! / counts,
+					6,
+				);
+			}
+			if (this.dataMaxes[key] !== undefined) {
+				processedCustomData[`max${key}`] = this.dataMaxes[key] ?? 0; // Added a default value to prevent undefined. But trying to verify why this can be undefined.
+			}
+		}
+
+		return processedCustomData;
+	}
+
 	private flushBucket(bucket: string): void {
-		const measurements = this.measurementsMap.get(bucket);
-		if (measurements === undefined) {
+		const loggerData = this.measurementsMap.get(bucket);
+		if (loggerData === undefined) {
 			return;
 		}
 
+		const { measurements, customData } = loggerData;
+
+		// TODO: I believe the check on `customData` is unnecessary.
 		if (measurements.count !== 0) {
 			const bucketProperties = this.perBucketProperties.get(bucket);
+
+			let processedCustomData: Record<string, number> = {};
+			if (customData !== undefined) {
+				processedCustomData = this.processCustomData(customData, measurements.count);
+			}
 
 			const telemetryEvent: ITelemetryPerformanceEventExt = {
 				...this.eventBase,
 				...bucketProperties, // If the bucket doesn't exist and this is undefined, things work as expected
 				...measurements,
+				...processedCustomData, // If the customData doesn't exist and this is undefined, things work as expected
 			};
 
 			this.logger.sendPerformanceEvent(telemetryEvent);
