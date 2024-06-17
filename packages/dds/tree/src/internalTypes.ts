13c9cc137b639d23973e7edd89997d9f728230ed
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2024-06-17 11:04:33 -0700

    Centralize internalTypes in tree into one module

---
+++ packages/dds/tree/src/internalTypes.ts
@@ -0,0 +1,10 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+// Used by public types, but not part of the desired API surface
+
+export type { _InlineTrick, FlattenKeys } from "./util/index.js";
+export type { ApplyKind } from "./simple-tree/index.js";
+export type { FlexListToUnion, ExtractItemType } from "./feature-libraries/index.js";
