7ee3875ecef1d0d2a6acd3789f3929beeedc1fd2
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2024-06-17 11:06:36 -0700

    Add comment about filtering

--- packages/dds/tree/src/internalTypes.ts
+++ packages/dds/tree/src/internalTypes.ts
@@ -3,7 +3,8 @@
  * Licensed under the MIT License.
  */
 
-// Used by public types, but not part of the desired API surface
+// Used by public types, but not part of the desired API surface.
+// Note that this should only contain types which are `@public` since this is reexported as a namespace and our rollup generator does not filter that.
 
 export type { _InlineTrick, FlattenKeys } from "./util/index.js";
 export type { ApplyKind } from "./simple-tree/index.js";
