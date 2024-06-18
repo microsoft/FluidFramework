9aa33ac44ef3fb6b6eb6049682c4ea5e07cb9625
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-06-18 21:05:27 +0000

    refactor: Restore MockLogger to alpha, but mark as deprecated

--- packages/utils/telemetry-utils/src/mockLogger.ts
+++ packages/utils/telemetry-utils/src/mockLogger.ts
@@ -23,7 +23,15 @@ import type {
  * Records events sent to it, and then can walk back over those events, searching for a set of expected events to
  * match against the logged events.
  *
- * @internal
+ * @deprecated
+ *
+ * This class is not intended for use outside of the `fluid-framework` repo, and will be removed from
+ * package exports in the near future.
+ *
+ * Please migrate usages by either creating your own mock {@link @fluidframework/core-interfaces#ITelemetryBaseLogger}
+ * implementation, or by copying this code as-is into your own repo.
+ *
+ * @alpha
  */
 export class MockLogger implements ITelemetryBaseLogger {
 	/**
