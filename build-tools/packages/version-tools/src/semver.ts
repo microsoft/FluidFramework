79dba29ad0ff0810a702fef121a780ca9e752274
Tyler Butler <tylerbu@microsoft.com>
2024-06-26 15:34:59 -0700

    [main>rc2] Fix comparison of 2.0 releases to RC builds (#21653)
    Ports #21641 to RC2.
    
    With the 2.0.0 release, we now have semver releases that are compared
    with RC and internal build numbers. I have fixed the logic in
    version-tools to correctly treat RC -> 2.0 as a major bump.
    
    I also updated the generate:buildVersion command to use the list of tags
    passed in for all logic. There was some code that checked for existing
    tags and it always used the in-repo tag list. Now, if a list of tags is
    passed in, it is used for all logic. The repo tags are only consulted
    when none are passed in.

--- build-tools/packages/version-tools/src/semver.ts
+++ build-tools/packages/version-tools/src/semver.ts
@@ -6,6 +6,7 @@ import * as semver from "semver";
 
 import { ReleaseVersion, VersionBumpType, VersionBumpTypeExtended } from "./bumpTypes";
 import {
+	DEFAULT_PRERELEASE_IDENTIFIER,
 	bumpInternalVersion,
 	detectInternalVersionConstraintType,
 	fromInternalScheme,
@@ -115,15 +116,21 @@ export function detectBumpType(
 		v2PrereleaseId = prereleaseId;
 	}
 
-	if (v1PrereleaseId === "internal" && v2PrereleaseId === "rc") {
+	const v2Scheme = detectVersionScheme(v2Parsed);
+	if (
 		// This is a special case for RC and internal builds. RC builds are always a
 		// major bump compared to an internal build.
+		(v1PrereleaseId === DEFAULT_PRERELEASE_IDENTIFIER && v2PrereleaseId === "rc") ||
+		// This is a special case for RC and public semver builds. Semver releases with major >= 2
+		// are always major releases compared to RC builds.
+		(v1PrereleaseId === "rc" && v2Scheme === "semver" && v2Parsed.major >= 2)
+	) {
 		return "major";
 	}
 
 	if (v1PrereleaseId !== v2PrereleaseId) {
 		throw new Error(
-			`v1 prerelease ID: ${v1PrereleaseId} cannot be compared to v2 prerelease ID: ${v2PrereleaseId}`,
+			`v1 prerelease ID: '${v1PrereleaseId}' cannot be compared to v2 prerelease ID: '${v2PrereleaseId}'`,
 		);
 	}
 
