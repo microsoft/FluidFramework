1648795efa5e509c273f098530f767f32707cdc1
Jatin Garg <jatingarg@Jatins-MacBook-Pro-2.local>
2024-06-28 17:04:41 -0700

    add test

--- packages/drivers/odsp-driver/src/odspFluidFileLink.ts
+++ packages/drivers/odsp-driver/src/odspFluidFileLink.ts
@@ -90,7 +90,7 @@ function decodeOdspFluidDataStoreLocator(
 
 	let siteUrl: URL | undefined;
 	try {
-		siteUrl = new URL(decodeURIComponent(sitePath), siteOriginUrl);
+		siteUrl = new URL(sitePath, siteOriginUrl);
 	} catch {
 		// Ignore failure to parse url as input might be malformed
 	}
@@ -101,15 +101,13 @@ function decodeOdspFluidDataStoreLocator(
 
 	return {
 		siteUrl: siteUrl.href,
-		driveId: decodeURIComponent(driveId),
-		itemId: decodeURIComponent(itemId),
-		dataStorePath: decodeURIComponent(dataStorePath),
-		appName: appName ? decodeURIComponent(appName) : appName,
-		containerPackageName: containerPackageName
-			? decodeURIComponent(containerPackageName)
-			: containerPackageName,
-		fileVersion: fileVersion ? decodeURIComponent(fileVersion) : fileVersion,
-		context: context ? decodeURIComponent(context) : context,
+		driveId,
+		itemId,
+		dataStorePath,
+		appName,
+		containerPackageName,
+		fileVersion,
+		context,
 	};
 }
 
