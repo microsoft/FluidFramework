fe8bf6fd5823642ac0b8d6ffaa14f1ab5f09cd08
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-07-01 23:26:28 +0000

    [WIP] transformations

--- packages/dds/tree/src/shared-tree/jsonSchema.ts
+++ packages/dds/tree/src/shared-tree/jsonSchema.ts
@@ -2,8 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-
-import type { SimpleLeafSchemaKind, SimpleNodeSchemaKind } from "./simpleSchema.js";
+import type { SimpleArrayNodeSchema, SimpleLeafNodeSchema, SimpleLeafSchemaKind, SimpleMapNodeSchema, SimpleNodeSchema, SimpleNodeSchemaKind, SimpleObjectNodeSchema, SimpleTreeSchema } from "./simpleSchema.js";
 
 // TODOs:
 // "ajv" library for at least testing, maybe type defs as well.
@@ -35,7 +34,7 @@ export interface NodeJsonSchemaBase<TNodeKind extends SimpleNodeSchemaKind, TJso
 export interface ObjectNodeJsonSchema extends NodeJsonSchemaBase<"object", "object"> {
 	// json schema
 	// Always refs to "definitions"
-	readonly properties: Record<string, JsonSchemaId>;
+	readonly properties: Record<string, JsonDefinitionRef>;
 	// json schema
 	readonly required: string[];
 	// json schema
@@ -49,7 +48,7 @@ export interface ObjectNodeJsonSchema extends NodeJsonSchemaBase<"object", "obje
 export interface ArrayNodeJsonSchema extends NodeJsonSchemaBase<"array", "array"> {
 	// json schema
 	// Always refs to "definitions"
-	readonly items: JsonSchemaId[];
+	readonly items: JsonDefinitionRef[];
 	// json schema
 	readonly additionalProperties: false;
 }
@@ -61,7 +60,7 @@ export interface ArrayNodeJsonSchema extends NodeJsonSchemaBase<"array", "array"
 export interface MapNodeJsonSchema extends NodeJsonSchemaBase<"map", "object"> {
 	// json schema
 	// Always refs to "definitions"
-	readonly properties: Record<string, JsonSchemaId>;
+	readonly properties: Record<string, JsonDefinitionRef>;
 	// json schema
 	readonly additionalProperties: false;
 }
@@ -94,7 +93,62 @@ export type NodeJsonSchema =
  * TODO
  * @internal
  */
-export interface TreeJsonSchema {
-	readonly rootSchema: NodeJsonSchema;
+export type TreeJsonSchema = ObjectNodeJsonSchema & {
 	readonly definitions: Record<JsonSchemaId, NodeJsonSchema>;
+}
+
+export function simpleTreeSchemaToJsonSchema(tree: SimpleTreeSchema): TreeJsonSchema {
+	const definitions = convertDefinitions(tree.definitions);
+	const allowedTypes: JsonDefinitionRef[] = tree.rootFieldSchema.allowedTypes.map(type => ({ $ref: `#/definitions/${type}`}));
+	return {
+		definitions,
+		properties: {
+
+		}
+	}
+}
+
+function convertDefinitions(definitions: ReadonlyMap<string, SimpleNodeSchema>): Record<string, JSONSchemaType<unknown>> {
+	const result: Record<string, JSONSchemaType<unknown>> = {};
+	for (const [key, value] of definitions) {
+		result[key] = convertNodeSchema(value);
+	}
+	return result;
+}
+
+function convertNodeSchema(schema: SimpleNodeSchema): JSONSchemaType<unknown> {
+	switch (schema.kind) {
+		case "array":
+			return convertArrayNodeSchema(schema);
+		case "leaf":
+			return convertLeafNodeSchema(schema);
+		case "map":
+			return convertMapNodeSchema(schema);
+		case "object":
+			return convertObjectNodeSchema(schema);
+		default:
+			throw new TypeError(`Unknown node schema kind: ${(schema as SimpleNodeSchema).kind}`);
+	}
+}
+
+function convertArrayNodeSchema(schema: SimpleArrayNodeSchema): JSONSchemaType<any> {
+	const itemTypes = schema.allowedTypes.map(type => `#/definitions/${type}`);
+	return {
+		type: ["array"],
+        items: {
+          type: itemTypes
+        },
+	}
+}
+
+function convertLeafNodeSchema(schema: SimpleLeafNodeSchema): JSONSchemaType<unknown> {
+
+}
+
+function convertObjectNodeSchema(schema: SimpleObjectNodeSchema): JSONSchemaType<unknown> {
+
+}
+
+function convertMapNodeSchema(schema: SimpleMapNodeSchema): JSONSchemaType<unknown> {
+
 }
