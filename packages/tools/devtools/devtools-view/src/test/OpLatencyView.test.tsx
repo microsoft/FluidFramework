94a1c4d2e7aab4b884899b3d903ceec5b59b4398
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-07-08 09:50:13 -0700

    Fixed policy check error

--- packages/tools/devtools/devtools-view/src/test/OpLatencyView.test.tsx
+++ packages/tools/devtools/devtools-view/src/test/OpLatencyView.test.tsx
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import { DevtoolsFeatures } from "@fluidframework/devtools-core/internal";
 import "@testing-library/jest-dom";
 import { render, screen, within } from "@testing-library/react";
