836a7df759b13d4ac57ab246490583d37b28838a
Tyler Butler <tyler@tylerbutler.com>
2024-07-09 15:41:29 -0700

    wip

--- build-tools/packages/build-tools/src/fluidBuild/tasks/leaf/biomeTasks.ts
+++ build-tools/packages/build-tools/src/fluidBuild/tasks/leaf/biomeTasks.ts
@@ -3,13 +3,34 @@
  * Licensed under the MIT License.
  */
 
-import path from "path";
+import { readFile } from "fs/promises";
+import ignore from "ignore";
+import * as JSON5 from "json5";
+import { merge } from "ts-deepmerge";
 import { getResolvedFluidRoot } from "../../../common/fluidUtils";
 import { GitRepo } from "../../../common/gitRepo";
 import { LeafWithFileStatDoneFileTask } from "./leafTask";
 
 // switch to regular import once building ESM
 const findUp = import("find-up");
+
+/**
+ * A type defining the Biome config file. Only the fields we care about are included.
+ *
+ * @privateRemarks
+ *
+ * Biome does have a JSON schema for the config format, so in the future if we need more comprehensive config file
+ * handling, this should be updated to use the schema instead.
+ */
+interface BiomeConfig {
+	extends?: string[];
+	files?: {
+		ignore?: string[];
+	};
+	formatter?: {
+		ignore?: string[];
+	};
+}
 
 export class BiomeTask extends LeafWithFileStatDoneFileTask {
 	// performance note: having individual tasks each acquire repo root and GitRepo
@@ -26,19 +47,19 @@ export class BiomeTask extends LeafWithFileStatDoneFileTask {
 	}
 
 	/**
-	 * Includes all files in the task's package directory and any biome config files in the directory tree. Files ignored
-	 * by git are excluded.
+	 * Includes all files in the the task's package directory that Biome would format and any Biome config files in the
+	 * directory tree.
+	 *
+	 * TODO: currently only includes the main config file. Need to follow extends and get paths for all of them.
 	 */
 	protected async getInputFiles(): Promise<string[]> {
-		const repoRoot = await this.repoRoot;
-		const gitRepo = await this.gitRepo;
+		// Files that would be formatted by biome. Paths are relative to the package directory
+		const files = await this.getBiomeFormattedFiles(this.node.pkg.directory);
+		const configFile = await this.getBiomeConfigPath(this.node.pkg.directory);
 
-		const configFiles = await this.getBiomeConfigPaths(this.node.pkg.directory);
-		const files = (await gitRepo.getFiles(this.node.pkg.directory)).map((file) =>
-			path.join(repoRoot, file),
-		);
-
-		return [...new Set([...configFiles, ...files])];
+		return configFile === undefined
+			? [...new Set(files)]
+			: [...new Set([configFile, ...files])];
 	}
 
 	protected async getOutputFiles(): Promise<string[]> {
@@ -47,20 +68,89 @@ export class BiomeTask extends LeafWithFileStatDoneFileTask {
 	}
 
 	/**
-	 * Returns an array of all the biome config files found from the current working directory up to the root of the repo.
-	 *
-	 * Rather than parse and read the config files, this implementation naively searches for all config files from the
-	 * task's package directory up to the root of the repo and assumes they're all used. In the future we might want to
-	 * parse the config files anyway to extract ignore paths, at which point this implementation can change.
+	 * Returns the closest biome config file found from the current working directory up to the root of the repo.
 	 */
-	private async getBiomeConfigPaths(cwd: string): Promise<string[]> {
+	private async getBiomeConfigPath(cwd: string): Promise<string | undefined> {
 		return (await findUp)
-			.findUpMultiple(["biome.json", "biome.jsonc"], { cwd, stopAt: await this.repoRoot })
-			.then((configs) => {
-				if (configs.length === 0) {
+			.findUp(["biome.json", "biome.jsonc"], { cwd, stopAt: await this.repoRoot })
+			.then((config) => {
+				if (config === undefined) {
 					this.traceError(`Can't find biome config file`);
 				}
-				return configs;
+				return config;
 			});
 	}
+
+	/**
+	 * Return an array of paths to files that Biome would format under the provided path. The returned paths are relative
+	 * to the root of the repo.
+	 */
+	private async getBiomeFormattedFiles(cwd: string): Promise<string[]> {
+		// const repoRoot = await this.repoRoot;
+		const gitRepo = await this.gitRepo;
+
+		/**
+		 * All files that could possibly be formatted before ignore entries are applied. Paths are relative to the root of
+		 * the repo.
+		 */
+		const allPossibleFiles = await gitRepo.getFiles(cwd);
+
+		const configFile = await this.getBiomeConfigPath(cwd);
+		if (configFile === undefined) {
+			// No config, so all files are formatted
+			return allPossibleFiles;
+		}
+
+		const config = await loadBiomeConfig(configFile);
+		const ignoreEntries = await getFormatterIgnoresFromConfig(config);
+
+		const ignoreObject = ignore().add([...ignoreEntries]);
+		const filtered = ignoreObject.filter(allPossibleFiles);
+
+		console.warn(
+			`Found ${allPossibleFiles.length} files to format, reduced to ${filtered.length} files.`,
+		);
+
+		return filtered;
+	}
+}
+
+/**
+ * Loads a Biome configuration file. If the config extends others, then those are loaded recursively and the results are
+ * merged. Array-type values are not merged, in accordance with how Biome applies configs.
+ */
+async function loadBiomeConfig(configPath: string): Promise<BiomeConfig> {
+	const contents = await readFile(configPath, "utf8");
+	const config: BiomeConfig = JSON5.parse(contents);
+
+	if (config.extends !== undefined && config.extends.length > 0) {
+		// Iterate through each extended config, load it, then merge them
+		const configsToMerge = await Promise.all(
+			config.extends.map((configPath) => loadBiomeConfig(configPath)),
+		);
+		// Add the current config as the last one to be applied when they're merged
+		configsToMerge.push(config);
+
+		const mergedConfig = merge.withOptions(
+			{
+				// Biome does not merge arrays
+				mergeArrays: false,
+			},
+			...configsToMerge,
+		);
+
+		return mergedConfig;
+	}
+
+	// extends is undefined, so return the config as-is
+	return config;
+}
+
+/**
+ * Given a Biome config object, returns the combined files.ignore and formatter.ignore values.
+ */
+async function getFormatterIgnoresFromConfig(config: BiomeConfig) {
+	const filesIgnore = config.files?.ignore ?? [];
+	const formatterIgnores = config.formatter?.ignore ?? [];
+	return new Set([...filesIgnore, ...formatterIgnores]);
 }
