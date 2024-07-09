5133d56dd15e4d49c2dcc874e6b08ee9e8ed77ae
Tyler Butler <tyler@tylerbutler.com>
2024-07-08 17:35:06 -0700

    npormalize line endings

--- build-tools/packages/build-cli/src/library/writeFileIfContentsDiffers.ts
+++ build-tools/packages/build-cli/src/library/writeFileIfContentsDiffers.ts
@@ -8,7 +8,8 @@ import { type PathLike } from "node:fs";
 
 /**
  * Writes to a file, but first reads the contents to check if it matches the desired content. If it does, the operation
- * is skipped.
+ * is skipped. Newlines are _always_ normalized to be line-feeds only. This pplies to both the input and the written
+ * contents. This means this function _cannot_ be used to output carriage-return/line-feed line endings.
  *
  * @param filePath - The path to the file to write.
  * @param contents - The contents to write to the file.
@@ -19,8 +20,11 @@ export async function writeFileIfContentsDiffers(
 	contents: string,
 ): Promise<boolean> {
 	const fileContents = await readFile(filePath, { encoding: "utf8" });
-	if (fileContents !== contents) {
-		await writeFile(filePath, contents);
+	const normalizedFileContents = fileContents.replace(/\r\n/g, "\n");
+	const normalizedNewContents = contents.replace(/\r\n/g, "\n");
+
+	if (normalizedFileContents !== normalizedNewContents) {
+		await writeFile(filePath, normalizedNewContents);
 		return true;
 	}
 	return false;
