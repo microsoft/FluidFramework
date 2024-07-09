6cdf698f54685bb2d21beb83a784a629d36435ca
Tyler Butler <tyler@tylerbutler.com>
2024-07-08 17:52:55 -0700

    write utf8

--- build-tools/packages/build-cli/src/library/writeFileIfContentsDiffers.ts
+++ build-tools/packages/build-cli/src/library/writeFileIfContentsDiffers.ts
@@ -24,7 +24,7 @@ export async function writeFileIfContentsDiffers(
 	const normalizedNewContents = contents.replace(/\r\n/g, "\n");
 
 	if (normalizedFileContents !== normalizedNewContents) {
-		await writeFile(filePath, normalizedNewContents);
+		await writeFile(filePath, normalizedNewContents, "utf8");
 		return true;
 	}
 	return false;
