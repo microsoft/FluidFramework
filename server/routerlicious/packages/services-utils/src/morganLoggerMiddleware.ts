c3d91171ca4844cc7822655b63c91dec0e7c8476
Lin Lin <linlin1@microsoft.com>
2024-07-10 14:22:55 -0700

    address comment

--- server/routerlicious/packages/services-utils/src/morganLoggerMiddleware.ts
+++ server/routerlicious/packages/services-utils/src/morganLoggerMiddleware.ts
@@ -120,7 +120,7 @@ export function jsonMorganLoggerMiddleware(
 				[BaseTelemetryProperties.correlationId]: getCorrelationIdWithHttpFallback(req, res),
 				[CommonProperties.serviceName]: serviceName,
 				[CommonProperties.telemetryGroupName]: "http_requests",
-				[HttpProperties.retryCount]: req.query.retry ?? 0,
+				[HttpProperties.retryCount]: Number.parseInt(req.query.retry ?? "0", 10),
 				...additionalProperties,
 				...getTelemetryContextPropertiesWithHttpInfo(req, res),
 			};
