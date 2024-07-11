1ac1f0cb5b02398480fe76f9aaf0decc84c007ff
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2024-07-11 13:45:17 -0700

    Script to run tests in a loop

--- packages/test/test-end-to-end-tests/run-tests.sh
+++ packages/test/test-end-to-end-tests/run-tests.sh
@@ -1,5 +1,7 @@
 mkdir -p ./testRuns
 
+FLUID_TEST_LOGGER_PKG_PATH='/home/alex/code/FluidFramework/packages/test/test-end-to-end-tests/node_modules/@ff-internal/aria-logger'
+
 STATUS=0
 COUNTER=0
 while [ $STATUS -eq 0 ]
@@ -11,9 +13,12 @@ do
 	COUNTER=$((COUNTER+1))
 	export TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 	OUTPUT_FILE=$TIMESTAMP.log
-	echo "Running tests ($COUNTER)...output file: ./testRuns/$OUTPUT_FILE"
+	echo "Running tests ($COUNTER)...output file: ./testRuns/$OUTPUT_FILE";
 	# npm run test:realsvc:tinylicious > ./testRuns/$OUTPUT_FILE 2>&1
-	npm run test:realsvc:local > ./testRuns/$OUTPUT_FILE 2>&1
+	export FLUID_TEST_LOGGER_PKG_PATH='/home/alex/code/FluidFramework/packages/test/test-end-to-end-tests/node_modules/@ff-internal/aria-logger'; \
+	export logger__colorize="false"; \
+	export logger__morganFormat=tiny; \
+	npm run test:realsvc:local:report:full > ./testRuns/$OUTPUT_FILE 2>&1
 	STATUS=$?
 done
 
