f0e59aeb091e81d76646aa1e3cfb04a459569dcd
Tyler Butler <tyler@tylerbutler.com>
2024-07-11 10:43:50 -0700

    Squished branch: test/tylerbu/publish-packages

--- scripts/pack-packages.sh
+++ scripts/pack-packages.sh
@@ -2,28 +2,28 @@
 set -eux -o pipefail
 
 # This script is used in the "npm pack" step in our CI build pipelines.
-# It runs (p)npm pack for all packages and sorts them into scoped/unscoped folders.
+# It runs (p)npm pack for all packages and outputs them to a folder.
 # It also outputs a packagePublishOrder.txt file that contains the order that the packages should be published in.
 
 echo PACKAGE_MANAGER=$PACKAGE_MANAGER
 echo RELEASE_GROUP=$RELEASE_GROUP
 echo STAGING_PATH=$STAGING_PATH
 
-mkdir $STAGING_PATH/pack/
-mkdir $STAGING_PATH/pack/scoped/
+mkdir -p $STAGING_PATH/pack/tarballs/
 mkdir $STAGING_PATH/test-files/
 
 if [ -f ".releaseGroup" ]; then
   flub exec --no-private --concurrency=1 --releaseGroup $RELEASE_GROUP -- "$PACKAGE_MANAGER pack" && \
-  flub exec --no-private --concurrency=1 --releaseGroup $RELEASE_GROUP -- "mv -t $STAGING_PATH/pack/scoped/ ./*.tgz" && \
+  flub exec --no-private --concurrency=1 --releaseGroup $RELEASE_GROUP -- "mv -t $STAGING_PATH/pack/tarballs/ ./*.tgz" && \
   flub exec --no-private --releaseGroup $RELEASE_GROUP -- "[ ! -f ./*test-files.tar ] || (echo 'test files found' && mv -t $STAGING_PATH/test-files/ ./*test-files.tar)"
 
 else
-  $PACKAGE_MANAGER pack && mv -t $STAGING_PATH/pack/scoped/ ./*.tgz
+  $PACKAGE_MANAGER pack && mv -t $STAGING_PATH/pack/tarballs/ ./*.tgz
 fi
 
 # This saves a list of the packages in the working directory in topological order to a temporary file.
 # Each package name is modified to match the packed tar files.
+# TODO: The --tarball flag can be removed once the flub publish command is tested and no longer in dry-run mode.
 flub list $RELEASE_GROUP --no-private --tarball --feed public > $STAGING_PATH/pack/packagePublishOrder-public.txt
 flub list $RELEASE_GROUP --no-private --tarball --feed internal-build > $STAGING_PATH/pack/packagePublishOrder-internal-build.txt
 flub list $RELEASE_GROUP --no-private --tarball --feed internal-dev > $STAGING_PATH/pack/packagePublishOrder-internal-dev.txt
