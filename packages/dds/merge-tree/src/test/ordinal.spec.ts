32fc4e649b344c1e5fda6ee1884fdca7c24b1937
jzaffiro <110866475+jzaffiro@users.noreply.github.com>
2024-07-12 12:17:38 -0700

    chore(merge-tree): Cleanup related to lint fixes  (#21870)
    Minor improvements to #21684, including disabling
    [prefer-code-point](https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-code-point.md)
    for ordinals, reverting function scope changes, and updating
    documentation.
    
    ---------
    
    Co-authored-by: Abram Sanderson <Abram.sanderson@gmail.com>

--- packages/dds/merge-tree/src/test/ordinal.spec.ts
+++ packages/dds/merge-tree/src/test/ordinal.spec.ts
@@ -11,8 +11,12 @@ import { doOverRange } from "./mergeTreeOperationRunner.js";
 
 function computeNumericOrdinal(index: number): string {
 	const prefixLen = Math.floor(index / 0xffff);
-	const prefix = String.fromCodePoint(0xffff).repeat(prefixLen);
-	return `${prefix}${String.fromCodePoint(index - prefixLen * 0xffff)}`;
+	// Ordinals exist purely for lexicographical sort order and use a small set of valid bytes for each string character.
+	// The extra handling fromCodePoint has for things like surrogate pairs is therefore unnecessary.
+	// eslint-disable-next-line unicorn/prefer-code-point
+	const prefix = String.fromCharCode(0xffff).repeat(prefixLen);
+	// eslint-disable-next-line unicorn/prefer-code-point
+	return `${prefix}${String.fromCharCode(index - prefixLen * 0xffff)}`;
 }
 
 describe("MergeTree.ordinals", () => {
@@ -20,7 +24,8 @@ describe("MergeTree.ordinals", () => {
 		{ min: 1, max: 16 },
 		(i) => i + 1,
 		(max) => {
-			let parentOrdinal = String.fromCodePoint(0);
+			// eslint-disable-next-line unicorn/prefer-code-point
+			let parentOrdinal = String.fromCharCode(0);
 			it(`Max ${max}`, () => {
 				doOverRange(
 					{ min: 1, max },
@@ -44,7 +49,8 @@ describe("MergeTree.ordinals", () => {
 	);
 
 	it("numericOrdinal", () => {
-		let previous = String.fromCodePoint(0);
+		// eslint-disable-next-line unicorn/prefer-code-point
+		let previous = String.fromCharCode(0);
 		for (let i = 2; i < Number.MAX_SAFE_INTEGER; i = Math.pow(i, 2)) {
 			const current = computeNumericOrdinal(i);
 			assert(
