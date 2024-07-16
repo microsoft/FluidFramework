8dc7a05a5a984f10eeff9c688293c2420169cff7
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-07-16 17:10:50 +0000

    Revert "build: Add demo app"
    This reverts commit 57f2f6bb07dd94c1ea4e5c2c13f1b543fe07b0c6.

--- packages/dds/tree/src/simple-tree/simpleSchemaToJsonSchema.ts
+++ packages/dds/tree/src/simple-tree/simpleSchemaToJsonSchema.ts
@@ -2,11 +2,6 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
 import type {
 	ArrayNodeJsonSchema,
 	FieldJsonSchema,
