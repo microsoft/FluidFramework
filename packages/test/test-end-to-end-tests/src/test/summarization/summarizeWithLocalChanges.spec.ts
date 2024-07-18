74e68220e330b5ebcb032c6c01c98b1bf8e8bacc
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2024-07-18 14:07:14 -0700

    More test hits

--- packages/test/test-end-to-end-tests/src/test/summarization/summarizeWithLocalChanges.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/summarization/summarizeWithLocalChanges.spec.ts
@@ -414,7 +414,7 @@ describeCompat("Summarizer with local changes", "NoCompat", (getTestObjectProvid
 		},
 	);
 
-	// 276481, 277550, 277863, 276970, 277331, 277338, 276676, 276574, 280283, 280277, 280597, 280730
+	// 276481, 277550, 277863, 276970, 277331, 277338, 276676, 276574, 280283, 280277, 280597, 280730, 280908
 	itExpects.only(
 		"Heuristic based summaries should pass on retry when NodeDidNotRunGC is hit",
 		[
