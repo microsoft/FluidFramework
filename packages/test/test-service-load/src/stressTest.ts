ce0be2f33a0f8cfb2d3ec83ce02284eb27e2a277
Matt Rakow <marakow@microsoft.com>
2024-07-19 17:01:01 -0700

    Split main from stressTest

--- packages/test/test-service-load/src/stressTest.ts
+++ packages/test/test-service-load/src/stressTest.ts
@@ -5,89 +5,22 @@
 
 import child_process from "child_process";
 
-import {
-	DriverEndpoint,
-	ITestDriver,
-	TestDriverTypes,
-} from "@fluid-internal/test-driver-definitions";
+import { ITestDriver } from "@fluid-internal/test-driver-definitions";
 import { ITelemetryLoggerExt } from "@fluidframework/telemetry-utils/internal";
-import commander from "commander";
 import ps from "ps-node";
 
 import { FileLogger } from "./FileLogger.js";
-import { getProfile } from "./getProfile.js";
-import { getTestUsers, TestUsers } from "./getTestUsers.js";
-import { smokeTest } from "./stressSmoke.js";
+import { TestUsers } from "./getTestUsers.js";
 import { TestConfiguration } from "./testConfigFile.js";
-import { createTestDriver, initialize, safeExit } from "./utils.js";
+import { initialize } from "./utils.js";
 
 const createLoginEnv = (userName: string, password: string) =>
 	`{"${userName}": "${password}"}`;
 
-function readRunOptions() {
-	commander
-		.version("0.0.1")
-		.requiredOption("-d, --driver <driver>", "Which test driver info to use", "odsp")
-		.requiredOption(
-			"-p, --profile <profile>",
-			"Which test profile to use from testConfig.json",
-			"ci",
-		)
-		.option("-e, --driverEndpoint <endpoint>", "Which endpoint should the driver target?")
-		.option("-id, --testId <testId>", "Load an existing data store rather than creating new")
-		.option("-c, --credFile <filePath>", "Filename containing user credentials for test")
-		.option("-s, --seed <number>", "Seed for this run")
-		.option("-dbg, --debug", "Debug child processes via --inspect-brk")
-		.option(
-			"-l, --log <filter>",
-			"Filter debug logging. If not provided, uses DEBUG env variable.",
-		)
-		.option("-v, --verbose", "Enables verbose logging")
-		.option(
-			"-b, --browserAuth",
-			"Enables browser auth which may require a user to open a url in a browser.",
-		)
-		.option("-m, --enableMetrics", "Enable capturing client & ops metrics")
-		.option(
-			"--createTestId",
-			"Flag indicating whether to create a document corresponding \
-        to the testId passed",
-		)
-		.parse(process.argv);
-
-	const driver: TestDriverTypes = commander.driver;
-	const endpoint: DriverEndpoint | undefined = commander.driverEndpoint;
-	const profileName: string = commander.profile;
-	const testId: string | undefined = commander.testId;
-	const debug: boolean = commander.debug ?? false;
-	const log: string | undefined = commander.log;
-	const verbose: boolean = commander.verbose ?? false;
-	const seed: number = commander.seed ?? Date.now();
-	const supportsBrowserAuth: boolean = commander.browserAuth ?? false;
-	const credFilePath: string | undefined = commander.credFile;
-	const enableMetrics: boolean = commander.enableMetrics ?? false;
-	const createTestId: boolean = commander.createTestId ?? false;
-
-	return {
-		driver,
-		endpoint,
-		profileName,
-		testId,
-		debug,
-		log,
-		verbose,
-		seed,
-		supportsBrowserAuth,
-		credFilePath,
-		enableMetrics,
-		createTestId,
-	};
-}
-
 /**
  * Implementation of the orchestrator process. Returns the return code to exit the process with.
  */
-async function orchestratorProcess(
+export async function stressTest(
 	testDriver: ITestDriver,
 	profile: TestConfiguration,
 	args: {
@@ -299,59 +232,3 @@ function setupTelemetry(
 		stdErrLine++;
 	});
 }
-
-const main = async () => {
-	const {
-		driver,
-		endpoint,
-		profileName,
-		testId,
-		debug,
-		log,
-		verbose,
-		seed,
-		supportsBrowserAuth,
-		credFilePath,
-		enableMetrics,
-		createTestId,
-	} = readRunOptions();
-
-	if (log !== undefined) {
-		process.env.DEBUG = log;
-	}
-
-	const profile = getProfile(profileName);
-
-	const testUsers = credFilePath !== undefined ? getTestUsers(credFilePath) : undefined;
-
-	const testDriver = await createTestDriver(
-		driver,
-		endpoint,
-		seed,
-		undefined,
-		supportsBrowserAuth,
-	);
-
-	console.log("Starting smoke test...");
-	await smokeTest(testDriver, profileName);
-	console.log("Smoke test complete!");
-
-	console.log("Starting stress test...");
-	const url = await orchestratorProcess(testDriver, profile, {
-		testId,
-		debug,
-		verbose,
-		seed,
-		enableMetrics,
-		createTestId,
-		testUsers,
-		profileName,
-	});
-	console.log("Stress test complete!");
-	await safeExit(0, url);
-};
-
-main().catch((error) => {
-	console.error(error);
-	process.exit(-1);
-});
