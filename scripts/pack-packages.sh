88449e0b972e5dca798763e17cc9ce06178d4b2a
Tyler Butler <tylerbu@microsoft.com>
2024-07-23 16:52:56 -0700

    cleanup

--- scripts/pack-packages.sh
+++ scripts/pack-packages.sh
@@ -3,7 +3,7 @@ set -eux -o pipefail
 
 # This script is used in the "npm pack" step in our CI build pipelines.
 # It runs (p)npm pack for all packages and outputs them to a folder.
-# It also outputs a packagePublishOrder.txt file that contains the order that the packages should be published in.
+# It also outputs a packagePublishOrder file per feed that contains the order in which the packages should be published.
 
 echo PACKAGE_MANAGER=$PACKAGE_MANAGER
 echo RELEASE_GROUP=$RELEASE_GROUP
@@ -12,6 +12,8 @@ echo STAGING_PATH=$STAGING_PATH
 mkdir -p $STAGING_PATH/pack/tarballs/
 mkdir $STAGING_PATH/test-files/
 
+# Runs pack on all packages in the release group and moves the tarballs to the staging folder.
+# If test files are found, they are moved to the test-files folder.
 if [ -f ".releaseGroup" ]; then
   flub exec --no-private --concurrency=1 --releaseGroup $RELEASE_GROUP -- "$PACKAGE_MANAGER pack" && \
   flub exec --no-private --concurrency=1 --releaseGroup $RELEASE_GROUP -- "mv -t $STAGING_PATH/pack/tarballs/ ./*.tgz" && \
@@ -22,9 +24,9 @@ else
 fi
 
 # This saves a list of the packages in the working directory in topological order to a temporary file.
-# Each package name is modified to match the packed tar files.
-# TODO: The --tarball flag can be removed once the flub publish command is tested and no longer in dry-run mode.
-flub list $RELEASE_GROUP --no-private --tarball --feed public > $STAGING_PATH/pack/packagePublishOrder-public.txt
-flub list $RELEASE_GROUP --no-private --tarball --feed internal-build > $STAGING_PATH/pack/packagePublishOrder-internal-build.txt
-flub list $RELEASE_GROUP --no-private --tarball --feed internal-dev > $STAGING_PATH/pack/packagePublishOrder-internal-dev.txt
-flub list $RELEASE_GROUP --no-private --tarball --feed internal-test > $STAGING_PATH/pack/packagePublishOrder-internal-test.txt
+# Packages will be published in this order to avoid dependency issues.
+# See tools/pipelines/templates/include-publish-npm-package-steps.yml for details about how this file is used.
+flub list $RELEASE_GROUP --no-private --feed public > $STAGING_PATH/pack/packagePublishOrder-public.txt
+flub list $RELEASE_GROUP --no-private --feed internal-build > $STAGING_PATH/pack/packagePublishOrder-internal-build.txt
+flub list $RELEASE_GROUP --no-private --feed internal-dev > $STAGING_PATH/pack/packagePublishOrder-internal-dev.txt
+flub list $RELEASE_GROUP --no-private --feed internal-test > $STAGING_PATH/pack/packagePublishOrder-internal-test.txt
