7caa83f1069f598352586af31031f7e68d84e125
Abram Sanderson <absander@microsoft.com>
2024-07-25 16:09:30 -0700

    PR feedback

--- packages/test/test-end-to-end-tests/src/test/handleValidation.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/handleValidation.spec.ts
@@ -8,7 +8,8 @@ import assert from "assert";
 import { generatePairwiseOptions } from "@fluid-private/test-pairwise-generator";
 import { describeCompat } from "@fluid-private/test-version-utils";
 import { ISharedCell } from "@fluidframework/cell/internal";
-import { IContainer } from "@fluidframework/container-definitions/internal";
+// eslint-disable-next-line import/no-internal-modules
+import { waitContainerToCatchUp } from "@fluidframework/container-loader/legacy";
 import {
 	IFluidHandle,
 	IFluidLoadable,
@@ -423,31 +424,6 @@ describeCompat("handle validation", "NoCompat", (getTestObjectProvider, apis) =>
 		};
 	}
 
-	/**
-	 * Loads a container and waits for its sequence number to catch up to some existing container (at the time the function begins).
-	 */
-	async function loadAndCatchUpContainer(
-		provider: ITestObjectProvider,
-		existingContainer: IContainer,
-	): Promise<IContainer> {
-		const seq = existingContainer.deltaManager.lastSequenceNumber;
-		const newContainer = await provider.loadTestContainer(testContainerConfig);
-		if (newContainer.deltaManager.lastSequenceNumber < seq) {
-			await new Promise<void>((resolve, reject) => {
-				const func = () => {
-					if (newContainer.deltaManager.lastSequenceNumber >= seq) {
-						newContainer.deltaManager.off("op", func);
-						newContainer.off("closed", reject);
-						resolve();
-					}
-				};
-				newContainer.deltaManager.on("op", func);
-				newContainer.once("closed", reject);
-			});
-		}
-		return newContainer;
-	}
-
 	for (const handleStorageFactory of handleStorageFactories) {
 		describe(handleStorageFactory.id, () => {
 			/**
@@ -482,7 +458,8 @@ describeCompat("handle validation", "NoCompat", (getTestObjectProvider, apis) =>
 
 				await provider.ensureSynchronized();
 
-				const container2 = await loadAndCatchUpContainer(provider, container1);
+				const container2 = await provider.loadTestContainer(testContainerConfig);
+				await waitContainerToCatchUp(container2);
 				container1.dispose();
 
 				// Validate that the created objects were attached and have correct data in the new container.
@@ -527,7 +504,8 @@ describeCompat("handle validation", "NoCompat", (getTestObjectProvider, apis) =>
 
 				await provider.ensureSynchronized();
 
-				const container2 = await loadAndCatchUpContainer(provider, container1);
+				const container2 = await provider.loadTestContainer(testContainerConfig);
+				await waitContainerToCatchUp(container2);
 				container1.dispose();
 
 				// Validate that the created objects were attached and have correct data in the new container.
@@ -615,21 +593,7 @@ describeCompat("handle validation", "NoCompat", (getTestObjectProvider, apis) =>
 			container1.dispose();
 
 			const container2 = await provider.loadTestContainer(testContainerConfig);
-			if (container2.deltaManager.lastSequenceNumber < seq) {
-				await new Promise<void>((resolve, reject) => {
-					const func = (op) => {
-						if (container2.deltaManager.lastSequenceNumber >= seq) {
-							container2.deltaManager.off("op", func);
-							container2.off("closed", reject);
-							resolve();
-						}
-						console.log(op);
-					};
-					container2.deltaManager.on("op", func);
-					container2.once("closed", reject);
-				});
-			}
-			await provider.ensureSynchronized(container2);
+			await waitContainerToCatchUp(container2);
 
 			const default2 = (await container2.getEntryPoint()) as ITestFluidObject;
 			const attached2 = await attachedDdsUtils.getDDS(default2);
@@ -655,91 +619,81 @@ describeCompat("handle validation", "NoCompat", (getTestObjectProvider, apis) =>
 		});
 	}
 
-	for (const detachedDds1Utils of handleStorageFactories) {
-		for (const detachedDds2Utils of handleStorageFactories) {
-			for (const attachedDdsUtils of handleStorageFactories) {
-				it(`stores ${detachedDds1Utils.id} handle in ${detachedDds2Utils.id} and attaches by storing in ${attachedDdsUtils.id} with new data store`, async () => {
-					/**
-					 * setup required for all portions of the test
-					 */
-					const { container1, provider } = await setup();
+	for (const {
+		detachedDds1Utils,
+		attachedDdsUtils,
+		detachedDds2Utils,
+	} of generatePairwiseOptions({
+		detachedDds1Utils: handleStorageFactories,
+		detachedDds2Utils: handleStorageFactories,
+		attachedDdsUtils: handleStorageFactories,
+	})) {
+		it(`stores ${detachedDds1Utils.id} handle in ${detachedDds2Utils.id} and attaches by storing in ${attachedDdsUtils.id} with new data store`, async () => {
+			/**
+			 * setup required for all portions of the test
+			 */
+			const { container1, provider } = await setup();
 
-					const attachedDataStore = (await container1.getEntryPoint()) as ITestFluidObject;
+			const attachedDataStore = (await container1.getEntryPoint()) as ITestFluidObject;
 
-					const dataStoreB = await attachedDataStore.context.containerRuntime.createDataStore([
-						"default",
-					]);
-					const dataObjectB = (await dataStoreB.entryPoint.get()) as ITestFluidObject;
-					await provider.ensureSynchronized();
+			const dataStoreB = await attachedDataStore.context.containerRuntime.createDataStore([
+				"default",
+			]);
+			const dataObjectB = (await dataStoreB.entryPoint.get()) as ITestFluidObject;
+			await provider.ensureSynchronized();
 
-					/**
-					 * create the first detached dds
-					 */
-					const createdDds1 = detachedDds1Utils.createDDS(dataObjectB.runtime);
+			/**
+			 * create the first detached dds
+			 */
+			const createdDds1 = detachedDds1Utils.createDDS(dataObjectB.runtime);
 
-					/**
-					 * create the second detached dds and store a handle to the first dds in it
-					 */
-					const createdDds2 = detachedDds2Utils.createDDS(dataObjectB.runtime);
-					await createdDds2.storeHandle(createdDds1.handle);
+			/**
+			 * create the second detached dds and store a handle to the first dds in it
+			 */
+			const createdDds2 = detachedDds2Utils.createDDS(dataObjectB.runtime);
+			await createdDds2.storeHandle(createdDds1.handle);
 
-					/**
-					 * get the attached dds
-					 */
-					const attachedDds = await attachedDdsUtils.getDDS(attachedDataStore);
+			/**
+			 * get the attached dds
+			 */
+			const attachedDds = await attachedDdsUtils.getDDS(attachedDataStore);
 
-					/**
-					 * store handle to dds2 in attached dds (which will attach ddss 1 and 2)
-					 */
-					await attachedDds.storeHandle(createdDds2.handle);
+			/**
+			 * store handle to dds2 in attached dds (which will attach ddss 1 and 2)
+			 */
+			await attachedDds.storeHandle(createdDds2.handle);
 
-					/**
-					 * close container, get sequence number and sync
-					 */
-					await provider.ensureSynchronized(container1);
-					const seq = container1.deltaManager.lastSequenceNumber;
-					container1.dispose();
+			/**
+			 * close container, get sequence number and sync
+			 */
+			await provider.ensureSynchronized(container1);
+			const seq = container1.deltaManager.lastSequenceNumber;
+			container1.dispose();
 
-					const container2 = await provider.loadTestContainer(testContainerConfig);
-					if (container2.deltaManager.lastSequenceNumber < seq) {
-						await new Promise<void>((resolve, reject) => {
-							const func = (op) => {
-								if (container2.deltaManager.lastSequenceNumber >= seq) {
-									container2.deltaManager.off("op", func);
-									container2.off("closed", reject);
-									resolve();
-								}
-								console.log(op);
-							};
-							container2.deltaManager.on("op", func);
-							container2.once("closed", reject);
-						});
-					}
-					await provider.ensureSynchronized(container2);
+			const container2 = await provider.loadTestContainer(testContainerConfig);
+			await waitContainerToCatchUp(container2);
 
-					const default2 = (await container2.getEntryPoint()) as ITestFluidObject;
-					const attached2 = await attachedDdsUtils.getDDS(default2);
-					/**
-					 * validation
-					 */
-					const handleFromAttached = await attached2.readHandle();
-					assert(isFluidHandle(handleFromAttached), `not a handle: ${handleFromAttached}`);
+			const default2 = (await container2.getEntryPoint()) as ITestFluidObject;
+			const attached2 = await attachedDdsUtils.getDDS(default2);
+			/**
+			 * validation
+			 */
+			const handleFromAttached = await attached2.readHandle();
+			assert(isFluidHandle(handleFromAttached), `not a handle: ${handleFromAttached}`);
 
-					const refToDetached2 = await getReferencedDDS(handleFromAttached);
-					assert(
-						refToDetached2.id === createdDds2.id,
-						`ids do not match: ${refToDetached2.id}, ${createdDds2.id}`,
-					);
-					const handleFromDetached2 = await refToDetached2.readHandle();
-					assert(isFluidHandle(handleFromDetached2), `not a handle: ${handleFromDetached2}`);
+			const refToDetached2 = await getReferencedDDS(handleFromAttached);
+			assert(
+				refToDetached2.id === createdDds2.id,
+				`ids do not match: ${refToDetached2.id}, ${createdDds2.id}`,
+			);
+			const handleFromDetached2 = await refToDetached2.readHandle();
+			assert(isFluidHandle(handleFromDetached2), `not a handle: ${handleFromDetached2}`);
 
-					const refToDetached1 = await getReferencedDDS(handleFromDetached2);
-					assert(
-						refToDetached1.id === createdDds1.id,
-						`ids do not match: ${refToDetached1.id}, ${createdDds1.id}`,
-					);
-				});
-			}
-		}
+			const refToDetached1 = await getReferencedDDS(handleFromDetached2);
+			assert(
+				refToDetached1.id === createdDds1.id,
+				`ids do not match: ${refToDetached1.id}, ${createdDds1.id}`,
+			);
+		});
 	}
 });
