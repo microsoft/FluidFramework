669a16401a07d45fe492d3b1ea1519f850a17eda
Willie Habimana <whabimana@microsoft.com>
2024-07-26 01:04:44 -0700

    waitForSignal

--- packages/test/test-end-to-end-tests/src/test/signalPerfTelemetry.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/signalPerfTelemetry.spec.ts
@@ -11,19 +11,32 @@ import {
 	ITestFluidObject,
 	ITestObjectProvider,
 	getContainerEntryPointBackCompat,
+	timeoutPromise,
 } from "@fluidframework/test-utils/internal";
 
 const testContainerConfig: ITestContainerConfig = {
 	fluidDataObjectType: DataObjectFactoryType.Test,
 };
 
+const waitForSignal = async (...signallers: { once(e: "signal", l: () => void): void }[]) =>
+	Promise.all(
+		signallers.map(async (signaller, index) =>
+			timeoutPromise((resolve) => signaller.once("signal", () => resolve()), {
+				durationMs: 2000,
+				errorMsg: `Signaller[${index}] Timeout`,
+			}),
+		),
+	);
+
 describeCompat("Signal performance telemetry", "NoCompat", (getTestObjectProvider, apis) => {
 	let provider: ITestObjectProvider;
 	let dataObject: ITestFluidObject;
+	let containerRuntime;
 	beforeEach("getTestObjectProvider", async () => {
 		provider = getTestObjectProvider();
 		const container = await provider.makeTestContainer();
 		dataObject = await getContainerEntryPointBackCompat<ITestFluidObject>(container);
+		containerRuntime = dataObject.context.containerRuntime;
 
 		// need to be connected to send signals
 		if (container.connectionState !== ConnectionState.Connected) {
@@ -40,13 +53,10 @@ describeCompat("Signal performance telemetry", "NoCompat", (getTestObjectProvide
 			},
 		],
 		async () => {
-			// Create container with read-connection
-
-			for (let i = 0; i < 130; i++) {
-				dataObject.context.containerRuntime.submitSignal("signal", "test");
+			for (let i = 0; i < 101; i++) {
+				containerRuntime.submitSignal("signal", "test");
+				await waitForSignal(containerRuntime);
 			}
-
-			await provider.ensureSynchronized();
 		},
 	);
 });
