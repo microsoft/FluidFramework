29636b6fd6c84ef43ea559d57c9d5b0c142f6ba8
Pragya Garg <pragyagarg@Pragyas-MacBook-Pro.local>
2024-07-26 13:20:28 -0700

    review comments

--- packages/runtime/container-runtime/src/opLifecycle/remoteMessageProcessor.ts
+++ packages/runtime/container-runtime/src/opLifecycle/remoteMessageProcessor.ts
@@ -193,29 +193,29 @@ export class RemoteMessageProcessor {
 /**
  * Takes an incoming message and if the contents is a string, JSON.parse's it in place
  * @param mutableMessage - op message received
- * @param hasModernRuntimeMessageEnvelop - false if the message does not contain the modern op envelop where message.type is MessageType.Operation
+ * @param hasModernRuntimeMessageEnvelope - false if the message does not contain the modern op envelop where message.type is MessageType.Operation
  * @param logLegacyCase - callback to log when legacy op is encountered
  */
 export function ensureContentsDeserialized(
 	mutableMessage: ISequencedDocumentMessage,
-	hasModernRuntimeMessageEnvelop: boolean,
+	hasModernRuntimeMessageEnvelope: boolean,
 	logLegacyCase: (codePath: string) => void,
 ): void {
 	// back-compat: ADO #1385: eventually should become unconditional, but only for runtime messages!
 	// System message may have no contents, or in some cases (mostly for back-compat) they may have actual objects.
 	// Old ops may contain empty string (I assume noops).
-	let hasParsedJsonContents: boolean;
+	let didParseJsonContents: boolean;
 	if (typeof mutableMessage.contents === "string" && mutableMessage.contents !== "") {
 		mutableMessage.contents = JSON.parse(mutableMessage.contents);
-		hasParsedJsonContents = true;
+		didParseJsonContents = true;
 	} else {
-		hasParsedJsonContents = false;
+		didParseJsonContents = false;
 	}
 
 	// We expect Modern Runtime Messages to have JSON serialized contents,
 	// and all other messages not to (system messages and legacy runtime messages without outer "op" type envelope)
 	// Let's observe if we are wrong about this to learn about these cases.
-	if (hasModernRuntimeMessageEnvelop !== hasParsedJsonContents) {
+	if (hasModernRuntimeMessageEnvelope !== didParseJsonContents) {
 		logLegacyCase("ensureContentsDeserialized_unexpectedContentsType");
 	}
 }
