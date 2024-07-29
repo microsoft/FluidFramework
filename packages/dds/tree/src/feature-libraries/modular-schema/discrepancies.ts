61915fa2ef9e46bc03625af618fa09c3eb7e5563
Clarence Li <clarenceli@microsoft.com>
2024-07-29 11:16:14 -0400

    Update doc of isRepoSuperset function

--- packages/dds/tree/src/feature-libraries/modular-schema/discrepancies.ts
+++ packages/dds/tree/src/feature-libraries/modular-schema/discrepancies.ts
@@ -390,6 +390,25 @@ function trackObjectNodeDiscrepancies(
 	return differences;
 }
 
+/**
+ * This function uses incompatibilities to determine if changes to a document schema are backward-compatible.
+ * According to the policy of schema evolution, `isRepoSuperset` supports three types of changes:
+ * 1. Adding an optional field to an object node.
+ * 2. Expanding the set of allowed types for a field.
+ * 3. Relaxing a field kind to a more general field kind.
+ *
+ * Notes: We expect isRepoSuperset to return consistent results with allowsRepoSuperset. However, currently there are some inconsistencies:
+ *
+ * - Different Node Kinds: If a and b have different node kinds (e.g., a is an objectNodeSchema and b is a mapNodeSchema),
+ * `isRepoSuperset` will determine that a can never be the superset of b. In contrast, `allowsRepoSuperset` will continue
+ * validating internal fields.
+ *
+ * - Sequence Field Kind: `isRepoSuperset` does not support comparing fields with the sequence field kind.
+ *
+ * - Empty Fields: If a has an empty field and b has a field with the same identifier but includes a non-optional field
+ * with values, `allowsRepoSuperset` will consider a as a superset of b due to its more flexible field. However,
+ * `isRepoSuperset` will produce different results, as the incompatibilities indicate that b has more content than a.
+ */
 export function isRepoSuperset(view: TreeStoredSchema, stored: TreeStoredSchema): boolean {
 	const incompatibilities = getAllowedContentIncompatibilities(view, stored);
 
