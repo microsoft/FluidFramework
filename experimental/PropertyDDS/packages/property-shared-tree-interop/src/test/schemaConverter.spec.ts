54646c02b35c646fad731a74d00793ddf4fcdbea
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2024-08-02 12:06:23 -0700

    Add dummy test

---
+++ experimental/PropertyDDS/packages/property-shared-tree-interop/src/test/schemaConverter.spec.ts
@@ -0,0 +1,9 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+describe("schema converter", () => {
+	// To avoid error from No tests found, leave placeholder test for schema converter.
+	it.skip(`TODO: create and test schema converter`, () => {});
+});
