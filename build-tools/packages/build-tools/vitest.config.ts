8e51f3fe3f7c03b35b4873aba67e73def3e5cdcc
Tyler Butler <tylerbu@microsoft.com>
2024-08-01 17:29:47 -0700

    build-tools

---
+++ build-tools/packages/build-tools/vitest.config.ts
@@ -0,0 +1,23 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		reporters: ["junit", "default"],
+		outputFile: {
+			junit: "nyc/junit-report.xml",
+		},
+		coverage: {
+			provider: "v8",
+			all: true,
+			include: ["src/**/*.*ts", "lib/**/*.*js"],
+			exclude: ["src/test/**/*.*ts", "lib/test/**/*.*js"],
+			reporter: ["cobertura", "html", "text"],
+			reportsDirectory: "./nyc/report",
+		},
+	},
+});
