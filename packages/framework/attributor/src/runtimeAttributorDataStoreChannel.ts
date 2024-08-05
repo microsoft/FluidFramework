28d4ec3cf8a0629595a54aa37358b0f167fda9cc
Jatin Garg <jatingarg@Jatins-MacBook-Pro-2.local>
2024-08-05 10:16:38 -0700

    Convert attributor to IFluidDataStoreChannel

---
+++ packages/framework/attributor/src/runtimeAttributorDataStoreChannel.ts
@@ -0,0 +1,349 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { bufferToString, TypedEventEmitter } from "@fluid-internal/client-utils";
+import { AttachState, IDeltaManager } from "@fluidframework/container-definitions/internal";
+import {
+	FluidObject,
+	IFluidHandle,
+	IRequest,
+	IResponse,
+} from "@fluidframework/core-interfaces";
+import type { IFluidHandleInternal } from "@fluidframework/core-interfaces/internal";
+import { assert, Deferred, unreachableCase } from "@fluidframework/core-utils/internal";
+import { FluidObjectHandle } from "@fluidframework/datastore/internal";
+import { IFluidDataStoreRuntimeEvents } from "@fluidframework/datastore-definitions/internal";
+import {
+	IDocumentMessage,
+	type ISnapshotTree,
+	ISequencedDocumentMessage,
+	IQuorumClients,
+} from "@fluidframework/driver-definitions/internal";
+import {
+	IGarbageCollectionData,
+	IFluidDataStoreChannel,
+	IFluidDataStoreContext,
+	IInboundSignalMessage,
+	VisibilityState,
+	type ISummaryTreeWithStats,
+	type ITelemetryContext,
+	type AttributionInfo,
+	type AttributionKey,
+} from "@fluidframework/runtime-definitions/internal";
+import { SummaryTreeBuilder } from "@fluidframework/runtime-utils/internal";
+import {
+	ITelemetryLoggerExt,
+	MonitoringContext,
+	raiseConnectedEvent,
+	createChildMonitoringContext,
+} from "@fluidframework/telemetry-utils/internal";
+
+import { OpStreamAttributor, type IAttributor } from "./attributor.js";
+import { opBlobName, type IRuntimeAttributor } from "./attributorContracts.js";
+import { AttributorSerializer, chain, deltaEncoder, type Encoder } from "./encoders.js";
+import { makeLZ4Encoder } from "./lz4Encoder.js";
+
+/**
+ * Data store channel for the runtime attributor. This channel is responsible for storing and managing the
+ */
+export class RuntimeAttributorDataStoreChannel
+	extends TypedEventEmitter<IFluidDataStoreRuntimeEvents>
+	implements IFluidDataStoreChannel, IRuntimeAttributor
+{
+	public constructor(
+		public readonly dataStoreContext: IFluidDataStoreContext,
+		existing: boolean,
+	) {
+		super();
+		this.mc = createChildMonitoringContext({
+			logger: dataStoreContext.baseLogger,
+			namespace: "Attributor",
+		});
+		this._attachState = dataStoreContext.attachState;
+		if (existing) {
+			this.visibilityState =
+				dataStoreContext.attachState === AttachState.Detached
+					? VisibilityState.LocallyVisible
+					: VisibilityState.GloballyVisible;
+		} else {
+			this.visibilityState = VisibilityState.NotVisible;
+		}
+		// If it's existing we know it has been attached.
+		if (existing) {
+			this.deferredAttached.resolve();
+		}
+		this.entryPoint = new FluidObjectHandle<FluidObject>(
+			this,
+			"",
+			dataStoreContext.IFluidHandleContext,
+		);
+	}
+
+	public get IFluidDataStoreChannel(): IFluidDataStoreChannel {
+		return this;
+	}
+
+	public get IRuntimeAttributor(): IRuntimeAttributor {
+		return this;
+	}
+
+	public get(key: AttributionKey): AttributionInfo {
+		assert(
+			this.opAttributor !== undefined,
+			0x509 /* RuntimeAttributor must be initialized before getAttributionInfo can be called */,
+		);
+
+		if (key.type === "detached") {
+			throw new Error("Attribution of detached keys is not yet supported.");
+		}
+
+		if (key.type === "local") {
+			// Note: we can *almost* orchestrate this correctly with internal-only changes by looking up the current
+			// client id in the audience. However, for read->write client transition, the container might have not yet
+			// received a client id. This is left as a TODO as it might be more easily solved once the detached case
+			// is settled (e.g. if it's reasonable for the host to know the current user information at container
+			// creation time, we could just use that here as well).
+			throw new Error("Attribution of local keys is not yet supported.");
+		}
+
+		return this.opAttributor.getAttributionInfo(key.seq);
+	}
+
+	public has(key: AttributionKey): boolean {
+		if (key.type === "detached") {
+			return false;
+		}
+
+		if (key.type === "local") {
+			return false;
+		}
+
+		return this.opAttributor?.tryGetAttributionInfo(key.seq) !== undefined;
+	}
+
+	private encoder: Encoder<IAttributor, string> = {
+		encode: unreachableCase,
+		decode: unreachableCase,
+	};
+
+	private _disposed = false;
+	public get disposed(): boolean {
+		return this._disposed;
+	}
+
+	public dispose(): void {
+		this._disposed = true;
+	}
+
+	private opAttributor: IAttributor | undefined;
+	public isEnabled = true;
+	public _attachState: AttachState;
+	public visibilityState: VisibilityState;
+	private readonly deferredAttached = new Deferred<void>();
+	private readonly mc: MonitoringContext;
+	public get logger(): ITelemetryLoggerExt {
+		return this.mc.logger;
+	}
+
+	public async initialize(
+		deltaManager: IDeltaManager<ISequencedDocumentMessage, IDocumentMessage>,
+		quorum: IQuorumClients,
+		baseSnapshotForAttributorTree: ISnapshotTree | undefined,
+		readBlob: (id: string) => Promise<ArrayBufferLike>,
+	): Promise<void> {
+		this.encoder = chain(
+			new AttributorSerializer(
+				(entries) => new OpStreamAttributor(deltaManager, quorum, entries),
+				deltaEncoder,
+			),
+			makeLZ4Encoder(),
+		);
+
+		if (baseSnapshotForAttributorTree === undefined) {
+			this.opAttributor = new OpStreamAttributor(deltaManager, quorum);
+		} else {
+			const id = baseSnapshotForAttributorTree.blobs[opBlobName];
+			assert(
+				id !== undefined,
+				0x50a /* Attributor tree should have op attributor summary blob. */,
+			);
+			const blobContents = await readBlob(id);
+			const attributorSnapshot = bufferToString(blobContents, "utf8");
+			this.opAttributor = this.encoder.decode(attributorSnapshot);
+		}
+	}
+
+	public attachGraph(): void {
+		throw new Error("attachGraph should not be called on the attributor");
+	}
+
+	public bind(handle: IFluidHandle): void {
+		throw new Error("bind should not be called on the attributor");
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.makeVisibleAndAttachGraph}
+	 */
+	public makeVisibleAndAttachGraph(): void {
+		if (this.visibilityState !== VisibilityState.NotVisible) {
+			return;
+		}
+		this.visibilityState = VisibilityState.LocallyVisible;
+
+		this.dataStoreContext.makeLocallyVisible();
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.getAttachSummary}
+	 */
+	public getAttachSummary(telemetryContext?: ITelemetryContext): ISummaryTreeWithStats {
+		assert(
+			this.opAttributor !== undefined,
+			0x50b /* RuntimeAttributor should be initialized before summarization */,
+		);
+		const builder = new SummaryTreeBuilder();
+		builder.addBlob(opBlobName, this.encoder.encode(this.opAttributor));
+		return builder.getSummaryTree();
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.getAttachGCData}
+	 */
+	public getAttachGCData(telemetryContext?: ITelemetryContext): IGarbageCollectionData {
+		return { gcNodes: {} };
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.process}
+	 */
+	public process(
+		message: ISequencedDocumentMessage,
+		local: boolean,
+		localOpMetadata: unknown,
+	): void {
+		throw new Error("Attributor should not receive messages");
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.processSignal}
+	 */
+	public processSignal(message: IInboundSignalMessage, local: boolean): void {
+		throw new Error("Attributor should not receive signals");
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.summarize}
+	 */
+	public async summarize(
+		fullTree?: boolean,
+		trackState?: boolean,
+		telemetryContext?: ITelemetryContext,
+	): Promise<ISummaryTreeWithStats> {
+		assert(
+			this.opAttributor !== undefined,
+			0x50b /* RuntimeAttributor should be initialized before summarization */,
+		);
+		const builder = new SummaryTreeBuilder();
+		builder.addBlob(opBlobName, this.encoder.encode(this.opAttributor));
+		return builder.getSummaryTree();
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.getGCData}
+	 */
+	public async getGCData(fullGC?: boolean): Promise<IGarbageCollectionData> {
+		const garbageCollectionData: IGarbageCollectionData = { gcNodes: {} };
+		return garbageCollectionData;
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.updateUsedRoutes}
+	 */
+	public updateUsedRoutes(usedRoutes: string[]): void {
+		return;
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.setConnectionState}
+	 */
+	public setConnectionState(connected: boolean, clientId?: string): void {
+		raiseConnectedEvent(this.logger, this, connected, clientId);
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.reSubmit}
+	 */
+	public reSubmit(type: string, content: unknown, localOpMetadata: unknown): void {
+		throw new Error("Should not resubmit anything from the attributor");
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.applyStashedOp}
+	 */
+	public async applyStashedOp(content: unknown): Promise<unknown> {
+		throw new Error("Should not apply stashed ops to the attributor");
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.rollback}
+	 */
+	public rollback?(type: string, content: unknown, localOpMetadata: unknown): void {
+		throw new Error("Should not rollback anything from the attributor");
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.entryPoint}
+	 */
+	public readonly entryPoint: IFluidHandleInternal<FluidObject>;
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.request}
+	 */
+	public async request(request: IRequest): Promise<IResponse> {
+		throw new Error("Should not request anything from the attributor");
+	}
+
+	public async waitAttached(): Promise<void> {
+		return this.deferredAttached.promise;
+	}
+
+	/**
+	 * {@inheritdoc IFluidDataStoreChannel.setAttachState}
+	 */
+	public setAttachState(attachState: AttachState.Attaching | AttachState.Attached): void {
+		switch (attachState) {
+			case AttachState.Attaching: {
+				// this.attachGraph();
+
+				this._attachState = AttachState.Attaching;
+
+				assert(
+					this.visibilityState === VisibilityState.LocallyVisible,
+					0x2d1 /* "Data store should be locally visible before it can become globally visible." */,
+				);
+
+				// Mark the data store globally visible and make its child channels visible as well.
+				this.visibilityState = VisibilityState.GloballyVisible;
+
+				// This promise resolution will be moved to attached event once we fix the scheduler.
+				this.deferredAttached.resolve();
+				this.emit("attaching");
+				break;
+			}
+			case AttachState.Attached: {
+				assert(
+					this.visibilityState === VisibilityState.GloballyVisible,
+					0x2d2 /* "Data store should be globally visible when its attached." */,
+				);
+				this._attachState = AttachState.Attached;
+				this.emit("attached");
+				break;
+			}
+			default: {
+				unreachableCase(attachState, "unreached");
+			}
+		}
+	}
+}
--- packages/test/test-end-to-end-tests/src/test/attributionEndToEnd.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/attributionEndToEnd.spec.ts
@@ -6,8 +6,8 @@
 import { strict as assert } from "assert";
 
 import {
+	getRuntimeAttributor,
 	IRuntimeAttributor,
-	createRuntimeAttributor,
 	enableOnNewFileKey,
 } from "@fluid-experimental/attributor";
 import {
@@ -15,6 +15,7 @@ import {
 	itSkipsFailureOnSpecificDrivers,
 } from "@fluid-private/test-version-utils";
 import { IContainer, IFluidCodeDetails } from "@fluidframework/container-definitions/internal";
+import { ContainerRuntime } from "@fluidframework/container-runtime/internal";
 import { ConfigTypes, IConfigProviderBase } from "@fluidframework/core-interfaces";
 import { createInsertOnlyAttributionPolicy } from "@fluidframework/merge-tree/internal";
 import { AttributionInfo } from "@fluidframework/runtime-definitions/internal";
@@ -111,24 +112,31 @@ describeCompat("Attributor", "NoCompat", (getTestObjectProvider, apis) => {
 		return dataObject.getSharedObject<SharedString>(stringId);
 	};
 
-	const getTestConfig = (runtimeAttributor?: IRuntimeAttributor): ITestContainerConfig => ({
+	const getTestConfig = (enable: boolean = false): ITestContainerConfig => ({
 		...testContainerConfig,
-		enableAttribution: runtimeAttributor !== undefined,
+		enableAttribution: enable,
 		loaderProps: {
-			scope: { IRuntimeAttributor: runtimeAttributor },
 			configProvider: configProvider({
-				[enableOnNewFileKey]: runtimeAttributor !== undefined,
+				[enableOnNewFileKey]: enable,
 			}),
 			// TODO this option shouldn't live here - this options object is global to the container
 			// and not specific to the individual dataStoreRuntime.
 			options: {
 				attribution: {
-					track: runtimeAttributor !== undefined,
+					track: enable,
 					policyFactory: createInsertOnlyAttributionPolicy,
 				},
 			} as any,
 		},
 	});
+
+	const getAttributorFromContainer = async (container: IContainer) => {
+		const dataStore = (await container.getEntryPoint()) as ITestFluidObject;
+		const containerRuntime = dataStore.context.containerRuntime as ContainerRuntime;
+		const attributor = await getRuntimeAttributor(containerRuntime);
+		assert(attributor !== undefined, "Attributor should be defined");
+		return attributor;
+	};
 
 	/**
 	 * Tracked by AB#4997, if no error event is detected within one sprint, we will remove
@@ -138,29 +146,31 @@ describeCompat("Attributor", "NoCompat", (getTestObjectProvider, apis) => {
 		"Can attribute content from multiple collaborators",
 		["tinylicious", "t9s"],
 		async () => {
-			const attributor = createRuntimeAttributor();
-			const container1 = await provider.makeTestContainer(getTestConfig(attributor));
+			const container1 = await provider.makeTestContainer(getTestConfig(true));
 			const sharedString1 = await sharedStringFromContainer(container1);
-			const attributor2 = createRuntimeAttributor();
-			const container2 = await provider.loadTestContainer(getTestConfig(attributor2));
+			const container2 = await provider.loadTestContainer(getTestConfig(true));
 			const sharedString2 = await sharedStringFromContainer(container2);
+
+			const attributor1 = await getAttributorFromContainer(container1);
 
 			const text = "client 1";
 			sharedString1.insertText(0, text);
-			assertAttributionMatches(sharedString1, 3, attributor, "local");
+			assertAttributionMatches(sharedString1, 3, attributor1, "local");
 			await provider.ensureSynchronized();
 			sharedString2.insertText(0, "client 2, ");
 			await provider.ensureSynchronized();
 			assert.equal(sharedString1.getText(), "client 2, client 1");
 
+			const attributor2 = await getAttributorFromContainer(container2);
+
 			assert(
 				container1.clientId !== undefined && container2.clientId !== undefined,
 				"Both containers should have client ids.",
 			);
-			assertAttributionMatches(sharedString1, 3, attributor, {
+			assertAttributionMatches(sharedString1, 3, attributor1, {
 				user: container1.audience.getMember(container2.clientId)?.user,
 			});
-			assertAttributionMatches(sharedString1, 13, attributor, {
+			assertAttributionMatches(sharedString1, 13, attributor1, {
 				user: container1.audience.getMember(container1.clientId)?.user,
 			});
 			assertAttributionMatches(sharedString2, 3, attributor2, {
@@ -173,8 +183,7 @@ describeCompat("Attributor", "NoCompat", (getTestObjectProvider, apis) => {
 	);
 
 	it("attributes content created in a detached state", async () => {
-		const attributor = createRuntimeAttributor();
-		const loader = provider.makeTestLoader(getTestConfig(attributor));
+		const loader = provider.makeTestLoader(getTestConfig(true));
 		const defaultCodeDetails: IFluidCodeDetails = {
 			package: "defaultTestPackage",
 			config: {},
@@ -202,9 +211,16 @@ describeCompat("Attributor", "NoCompat", (getTestObjectProvider, apis) => {
 			container1.clientId !== undefined && container2.clientId !== undefined,
 			"Both containers should have client ids.",
 		);
+		const attributor = await getAttributorFromContainer(container1);
 		assertAttributionMatches(sharedString1, 3, attributor, {
 			user: container1.audience.getMember(container2.clientId)?.user,
 		});
 		assertAttributionMatches(sharedString1, 13, attributor, "detached");
+
+		const attributor2 = await getAttributorFromContainer(container2);
+		assertAttributionMatches(sharedString2, 3, attributor2, {
+			user: container1.audience.getMember(container2.clientId)?.user,
+		});
+		assertAttributionMatches(sharedString2, 13, attributor2, "detached");
 	});
 });
