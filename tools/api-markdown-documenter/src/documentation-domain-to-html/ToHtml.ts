c14592e15f750f03a1030623d9a8ee84fe98628a
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-08-06 22:57:12 +0000

    improvement: Quote type in error

--- tools/api-markdown-documenter/src/documentation-domain-to-html/ToHtml.ts
+++ tools/api-markdown-documenter/src/documentation-domain-to-html/ToHtml.ts
@@ -58,7 +58,7 @@ export function documentationNodeToHtml(
 ): HastNodes {
 	if (context.transformations[node.type] === undefined) {
 		throw new Error(
-			`Encountered a DocumentationNode with neither a user-provided nor system-default renderer. Type: ${node.type}. Please provide a transformation for this type.`,
+			`Encountered a DocumentationNode with neither a user-provided nor system-default renderer. Type: "${node.type}". Please provide a transformation for this type.`,
 		);
 	}
 	return context.transformations[node.type](node, context);
