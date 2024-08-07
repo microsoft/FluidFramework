04eb4f657804db6364f6e2e16a397a9ccde68424
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2024-08-07 22:40:49 +0000

    policy check

--- common/build/eslint-plugin-fluid/src/rules/no-unchecked-record-access.js
+++ common/build/eslint-plugin-fluid/src/rules/no-unchecked-record-access.js
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 const ts = require("typescript");
 
 const hasIndexSignature = (type) => type.getStringIndexType() || type.getNumberIndexType();
