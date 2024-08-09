cbb20a9a2486a56cb9a844cf0ccacb7fb8e70fa7
Willie Habimana <whabimana@microsoft.com>
2024-08-09 12:02:40 -0700

    typing and waitForSignal change

--- packages/test/test-end-to-end-tests/src/test/signalPerfTelemetry.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/signalPerfTelemetry.spec.ts
@@ -4,8 +4,9 @@
  */
 
 import { describeCompat, itExpects } from "@fluid-private/test-version-utils";
-import { IContainer } from "@fluidframework/container-definitions/internal";
+import { IContainer, IRuntime } from "@fluidframework/container-definitions/internal";
 import { ConnectionState } from "@fluidframework/container-loader";
+import type { IContainerRuntimeBase } from "@fluidframework/runtime-definitions/internal";
 import { MockLogger } from "@fluidframework/telemetry-utils/internal";
 import {
 	DataObjectFactoryType,
@@ -16,13 +17,32 @@ import {
 	timeoutPromise,
 } from "@fluidframework/test-utils/internal";
 
-const waitForSignal = async (...signallers: { once(e: "signal", l: () => void): void }[]) =>
+type ContainerRuntime = IContainerRuntimeBase &
+	IRuntime & {
+		clientId?: string | undefined;
+	};
+
+const waitForSignals = async (
+	numSignals: number,
+	...signallers: { on(e: "signal", l: () => void): void }[]
+) =>
 	Promise.all(
 		signallers.map(async (signaller, index) =>
-			timeoutPromise((resolve) => signaller.once("signal", () => resolve()), {
-				durationMs: 2000,
-				errorMsg: `Signaller[${index}] Timeout`,
-			}),
+			timeoutPromise(
+				(resolve) => {
+					let count = 0;
+					signaller.on("signal", () => {
+						count++;
+						if (count === numSignals) {
+							resolve();
+						}
+					});
+				},
+				{
+					durationMs: 2000,
+					errorMsg: `Signaller[${index}] Timeout`,
+				},
+			),
 		),
 	);
 
@@ -30,7 +50,7 @@ describeCompat("Signal performance telemetry", "NoCompat", (getTestObjectProvide
 	let provider: ITestObjectProvider;
 	let dataObject: ITestFluidObject;
 	let container: IContainer;
-	let containerRuntime;
+	let containerRuntime: ContainerRuntime;
 	let logger: MockLogger;
 
 	beforeEach("getTestObjectProvider", async () => {
@@ -46,7 +66,7 @@ describeCompat("Signal performance telemetry", "NoCompat", (getTestObjectProvide
 		};
 		container = await provider.makeTestContainer(testContainerConfig);
 		dataObject = await getContainerEntryPointBackCompat<ITestFluidObject>(container);
-		containerRuntime = dataObject.context.containerRuntime;
+		containerRuntime = dataObject.context.containerRuntime as ContainerRuntime;
 
 		// need to be connected to send signals
 		if (container.connectionState !== ConnectionState.Connected) {
@@ -67,8 +87,8 @@ describeCompat("Signal performance telemetry", "NoCompat", (getTestObjectProvide
 				// Send 100+ broadcast signals to trigger latency telemetry event
 				for (let i = 0; i < 101; i++) {
 					containerRuntime.submitSignal("signal", "test");
-					await waitForSignal(containerRuntime);
 				}
+				await waitForSignals(101, containerRuntime);
 			},
 		);
 
@@ -76,99 +96,6 @@ describeCompat("Signal performance telemetry", "NoCompat", (getTestObjectProvide
 			"SignalLost error event after missing signal is detected",
 			[
 				{
-					eventName: "fluid:telemetry:ContainerRuntime:SignalLatency",
-					clientType: "interactive",
-				},
-				{
-					eventName: "fluid:telemetry:ContainerRuntime:SignalLost",
-					clientType: "interactive",
-				},
-			],
-			async () => {
-				for (let i = 0; i < 101; i++) {
-					containerRuntime.submitSignal("signal", "test");
-					await waitForSignal(containerRuntime);
-				}
-
-				// Process a signal with a sequence number that is higher than expected to simulate missing signals and trigger a SignalLost error event.
-				containerRuntime.processSignal(
-					{
-						clientId: containerRuntime.clientId,
-						content: {
-							clientSignalSequenceNumber: 150,
-							contents: {
-								type: "signal",
-								content: "test",
-							},
-						},
-					},
-					true,
-				);
-			},
-		);
-
-		itExpects(
-			"SignalOutOfOrder error event after a missing signal is received non-sequentially",
-			[
-				{
-					eventName: "fluid:telemetry:ContainerRuntime:SignalLatency",
-					clientType: "interactive",
-				},
-				{
-					eventName: "fluid:telemetry:ContainerRuntime:SignalLost",
-					clientType: "interactive",
-				},
-				{
-					eventName: "fluid:telemetry:ContainerRuntime:SignalOutOfOrder",
-					clientType: "interactive",
-				},
-			],
-			async () => {
-				for (let i = 0; i < 101; i++) {
-					containerRuntime.submitSignal("signal", "test");
-					await waitForSignal(containerRuntime);
-				}
-
-				// Create gap in signal sequence number. Should trigger SignalLost error event.
-				containerRuntime.processSignal(
-					{
-						clientId: containerRuntime.clientId,
-						content: {
-							clientSignalSequenceNumber: 150,
-							contents: {
-								type: "signal",
-								content: "test",
-							},
-						},
-					},
-					true,
-				);
-
-				// Simulate an out-of-order signal by processing a signal in the missing sequence gap range. Should trigger SignalOutOfOrder error event.
-				containerRuntime.processSignal(
-					{
-						clientId: containerRuntime.clientId,
-						content: {
-							clientSignalSequenceNumber: 148,
-							contents: {
-								type: "signal",
-								content: "test",
-							},
-						},
-					},
-					true,
-				);
-			},
-		);
-
-		itExpects(
-			"Multiple SignalLost error events after multiple missing signals are detected",
-			[
-				{
-					eventName: "fluid:telemetry:ContainerRuntime:SignalLost",
-					clientType: "interactive",
-				},
-				{
 					eventName: "fluid:telemetry:ContainerRuntime:SignalLost",
 					clientType: "interactive",
 				},
@@ -176,14 +103,14 @@ describeCompat("Signal performance telemetry", "NoCompat", (getTestObjectProvide
 			async () => {
 				// Initial signal
 				containerRuntime.submitSignal("signal", "test");
-				await waitForSignal(containerRuntime);
+				await waitForSignals(1, containerRuntime);
 
-				// Process a signal with a sequence number that is higher than expected to simulate missing signals and trigger a SignalLost error event.
+				// Simulate receiving next in-sequence signal
 				containerRuntime.processSignal(
 					{
 						clientId: containerRuntime.clientId,
 						content: {
-							clientSignalSequenceNumber: 10,
+							clientSignalSequenceNumber: 2,
 							contents: {
 								type: "signal",
 								content: "test",
@@ -198,7 +125,7 @@ describeCompat("Signal performance telemetry", "NoCompat", (getTestObjectProvide
 					{
 						clientId: containerRuntime.clientId,
 						content: {
-							clientSignalSequenceNumber: 15,
+							clientSignalSequenceNumber: 4,
 							contents: {
 								type: "signal",
 								content: "test",
@@ -210,158 +137,272 @@ describeCompat("Signal performance telemetry", "NoCompat", (getTestObjectProvide
 			},
 		);
 
-		itExpects(
-			"Multiple SignalOutOfOrder error events after multiple missing signals are received non-sequentially",
-			[
-				{
-					eventName: "fluid:telemetry:ContainerRuntime:SignalLost",
-					clientType: "interactive",
-				},
-				{
-					eventName: "fluid:telemetry:ContainerRuntime:SignalOutOfOrder",
-					clientType: "interactive",
-				},
-				{
-					eventName: "fluid:telemetry:ContainerRuntime:SignalOutOfOrder",
-					clientType: "interactive",
-				},
-			],
-			async () => {
-				// Initial signal
-				containerRuntime.submitSignal("signal", "test");
-				await waitForSignal(containerRuntime);
-
-				// Create gap in signal sequence number. Should trigger SignalLost error event.
-				containerRuntime.processSignal(
-					{
-						clientId: containerRuntime.clientId,
-						content: {
-							clientSignalSequenceNumber: 10,
-							contents: {
-								type: "signal",
-								content: "test",
-							},
-						},
-					},
-					true,
-				);
-
-				// Simulate an out-of-order signal by processing a signal in the missing sequence gap range. Should trigger SignalOutOfOrder error event.
-				containerRuntime.processSignal(
-					{
-						clientId: containerRuntime.clientId,
-						content: {
-							clientSignalSequenceNumber: 7,
-							contents: {
-								type: "signal",
-								content: "test",
-							},
-						},
-					},
-					true,
-				);
-
-				// Simulate an out-of-order signal by processing a signal in the missing sequence gap range. Should trigger SignalOutOfOrder error event.
-				containerRuntime.processSignal(
-					{
-						clientId: containerRuntime.clientId,
-						content: {
-							clientSignalSequenceNumber: 8,
-							contents: {
-								type: "signal",
-								content: "test",
-							},
-						},
-					},
-					true,
-				);
-			},
-		);
-	});
-
-	describe("for local driver", () => {
-		before(function () {
-			if (provider.driver.type !== "local") {
-				// Skip the tests in non-local driver
-				this.skip();
-			}
-		});
-
-		it("should not emit SignalLost error event after rapid broadcasts", async () => {
-			// Send 100 rapid broadcast signals
-			for (let i = 0; i < 100; i++) {
-				containerRuntime.submitSignal("signal", "test");
-				await waitForSignal(containerRuntime);
-			}
-
-			logger.assertMatchNone(
+			itExpects(
+				"SignalOutOfOrder error event after a missing signal is received non-sequentially",
 				[
 					{
 						eventName: "fluid:telemetry:ContainerRuntime:SignalLost",
 						clientType: "interactive",
 					},
-				],
-				"SignalLost event should not be emitted after rapid broadcasts",
-			);
-		});
-
-		it("should not emit SignalOutOfOrder error event after rapid broadcasts", async () => {
-			// Send 100 rapid broadcast signals
-			for (let i = 0; i < 100; i++) {
-				containerRuntime.submitSignal("signal", "test");
-				await waitForSignal(containerRuntime);
-			}
-
-			logger.assertMatchNone(
-				[
 					{
 						eventName: "fluid:telemetry:ContainerRuntime:SignalOutOfOrder",
 						clientType: "interactive",
 					},
 				],
-				"SignalOutOfOrder event should not be emitted after rapid broadcasts",
-			);
-		});
+				async () => {
+					// Initial signal
+					containerRuntime.submitSignal("signal", "test");
+					await waitForSignals(1, containerRuntime);
 
-		it("should not emit SignalOutOfOrder after disconnect", async () => {
-			// Initial signal
-			for (let i = 0; i < 50; i++) {
-				containerRuntime.submitSignal("signal", "test");
-				await waitForSignal(containerRuntime);
-			}
-
-			container.disconnect();
-			container.connect();
-
-			// need to be connected to send signals
-			if (container.connectionState !== ConnectionState.Connected) {
-				await new Promise((resolve) => container.once("connected", resolve));
-			}
-
-			// Out of order signal should not trigger error event.
-			containerRuntime.processSignal(
-				{
-					clientId: containerRuntime.clientId,
-					content: {
-						clientSignalSequenceNumber: 10,
-						contents: {
-							type: "signal",
-							content: "test",
+					// Simulate receiving next in-sequence signal
+					containerRuntime.processSignal(
+						{
+							clientId: containerRuntime.clientId,
+							content: {
+								clientSignalSequenceNumber: 2,
+								contents: {
+									type: "signal",
+									content: "test",
+								},
+							},
 						},
-					},
+						true,
+					);
+
+					// Simulate a lost signal by processing a signal with a sequence number that is higher than expected
+					containerRuntime.processSignal(
+						{
+							clientId: containerRuntime.clientId,
+							content: {
+								clientSignalSequenceNumber: 4,
+								contents: {
+									type: "signal",
+									content: "test",
+								},
+							},
+						},
+						true,
+					);
+					// Simulate an out of order signal
+					containerRuntime.processSignal(
+						{
+							clientId: containerRuntime.clientId,
+							content: {
+								clientSignalSequenceNumber: 3,
+								contents: {
+									type: "signal",
+									content: "test",
+								},
+							},
+						},
+						true,
+					);
 				},
-				true,
 			);
 
-			logger.assertMatchNone(
+			itExpects(
+				"Multiple SignalLost error events after multiple missing signals are detected",
 				[
+					{
+						eventName: "fluid:telemetry:ContainerRuntime:SignalLost",
+						clientType: "interactive",
+					},
+					{
+						eventName: "fluid:telemetry:ContainerRuntime:SignalLost",
+						clientType: "interactive",
+					},
+				],
+				async () => {
+					// Initial signal
+					containerRuntime.submitSignal("signal", "test");
+					await waitForSignals(1, containerRuntime);
+
+					// Simulate receiving next in-sequence signal
+					containerRuntime.processSignal(
+						{
+							clientId: containerRuntime.clientId,
+							content: {
+								clientSignalSequenceNumber: 2,
+								contents: {
+									type: "signal",
+									content: "test",
+								},
+							},
+						},
+						true,
+					);
+
+					// Simulate a lost signal by processing a signal with a sequence number that is higher than expected
+					containerRuntime.processSignal(
+						{
+							clientId: containerRuntime.clientId,
+							content: {
+								clientSignalSequenceNumber: 4,
+								contents: {
+									type: "signal",
+									content: "test",
+								},
+							},
+						},
+						true,
+					);
+
+					// Simulate another lost signal by processing a signal with a sequence number that is higher than expected
+					containerRuntime.processSignal(
+						{
+							clientId: containerRuntime.clientId,
+							content: {
+								clientSignalSequenceNumber: 6,
+								contents: {
+									type: "signal",
+									content: "test",
+								},
+							},
+						},
+						true,
+					);
+				},
+			);
+
+			itExpects(
+				"Multiple SignalOutOfOrder error events after multiple missing signals are received non-sequentially",
+				[
+					{
+						eventName: "fluid:telemetry:ContainerRuntime:SignalLost",
+						clientType: "interactive",
+					},
+					{
+						eventName: "fluid:telemetry:ContainerRuntime:SignalOutOfOrder",
+						clientType: "interactive",
+					},
 					{
 						eventName: "fluid:telemetry:ContainerRuntime:SignalOutOfOrder",
 						clientType: "interactive",
 					},
 				],
-				"SignalOutOfOrder event should not be emitted after disconnect",
+				async () => {
+					// Initial signal
+					containerRuntime.submitSignal("signal", "test");
+					await waitForSignals(1, containerRuntime);
+
+
+					// Simulate receiving next in-sequence signal
+					containerRuntime.processSignal(
+						{
+							clientId: containerRuntime.clientId,
+							content: {
+								clientSignalSequenceNumber: 2,
+								contents: {
+									type: "signal",
+									content: "test",
+								},
+							},
+						},
+						true,
+					);
+
+					// Simulate a lost signal by processing a signal with a sequence number that is higher than expected
+					containerRuntime.processSignal(
+						{
+							clientId: containerRuntime.clientId,
+							content: {
+								clientSignalSequenceNumber: 5,
+								contents: {
+									type: "signal",
+									content: "test",
+								},
+							},
+						},
+						true,
+					);
+
+					// Simulate out of order signal
+					containerRuntime.processSignal(
+						{
+							clientId: containerRuntime.clientId,
+							content: {
+								clientSignalSequenceNumber: 3,
+								contents: {
+									type: "signal",
+									content: "test",
+								},
+							},
+						},
+						true,
+					);
+
+					// Simulate another out of order signal
+					containerRuntime.processSignal(
+						{
+							clientId: containerRuntime.clientId,
+							content: {
+								clientSignalSequenceNumber: 4,
+								contents: {
+									type: "signal",
+									content: "test",
+								},
+							},
+						},
+						true,
+					);
+				},
 			);
 		});
+
+		describe("for local driver", () => {
+			before(function () {
+				if (provider.driver.type !== "local") {
+					// Skip the tests in non-local driver
+					this.skip();
+				}
+			});
+
+			it("should not emit SignalLost or SignalOutOfOrder error event after rapid broadcasts", async () => {
+				// Send 100 rapid broadcast signals
+				for (let i = 0; i < 100; i++) {
+					containerRuntime.submitSignal("signal", "test");
+				}
+
+				await waitForSignals(100, containerRuntime);
+
+				logger.assertMatchNone(
+					[
+						{
+							eventName: "fluid:telemetry:ContainerRuntime:SignalLost",
+							clientType: "interactive",
+						},
+						{
+							eventName: "fluid:telemetry:ContainerRuntime:SignalOutOfOrder",
+							clientType: "interactive",
+						},
+
+					],
+					"SignalLost event should not be emitted after rapid broadcasts",
+				);
+			});
+
+			it("should not emit SignalOutOfOrder after disconnect", async () => {
+				for (let i = 0; i < 100; i++) {
+					containerRuntime.submitSignal("signal", "test");
+				}
+
+				container.disconnect();
+				container.connect();
+
+				// need to be connected to send signals
+				if (container.connectionState !== ConnectionState.Connected) {
+					await new Promise((resolve) => container.once("connected", resolve));
+				}
+
+				logger.assertMatchNone(
+					[
+						{
+							eventName: "fluid:telemetry:ContainerRuntime:SignalOutOfOrder",
+							clientType: "interactive",
+						},
+					],
+					"SignalOutOfOrder event should not be emitted after disconnect",
+				);
+			});
 	});
 });
