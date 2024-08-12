c4712aac7cfa3afefcbf4c55a3d5c64f521247db
Tyler Butler <tylerbu@microsoft.com>
2024-08-12 14:27:54 -0700

    fluidBuild config

--- fluidBuild.config.cjs
+++ fluidBuild.config.cjs
@@ -10,6 +10,11 @@
 const tscDependsOn = ["^tsc", "^api", "build:genver", "ts2esm"];
 
 /**
+ * The dependencies for the compile task -- excluding building the test files.
+ */
+const compileDependsOn = ["commonjs", "build:esnext", "api", "build:copy"];
+
+/**
  * The settings in this file configure the Fluid build tools, such as fluid-build and flub. Some settings apply to the
  * whole repo, while others apply only to the client release group.
  *
@@ -47,11 +52,11 @@ module.exports = {
 			script: false,
 		},
 		"compile": {
-			dependsOn: ["commonjs", "build:esnext", "api", "build:test", "build:copy"],
+			dependsOn: [...compileDependsOn, "build:test"],
 			script: false,
 		},
 		"commonjs": {
-			dependsOn: ["tsc", "build:test"],
+			dependsOn: ["tsc"],
 			script: false,
 		},
 		"lint": {
@@ -75,7 +80,11 @@ module.exports = {
 		},
 		"build:copy": [],
 		"build:genver": [],
-		"typetests:gen": ["^tsc", "build:genver"], // we may reexport type from dependent packages, needs to build them first.
+		"typetests:gen": [
+			// We may reexport types from dependent packages so we need to build them first.
+			...compileDependsOn,
+			"^typetests:gen",
+		],
 		"ts2esm": [],
 		"tsc": tscDependsOn,
 		"build:esnext": [...tscDependsOn, "^build:esnext"],
@@ -86,8 +95,7 @@ module.exports = {
 		"build:test:cjs": ["typetests:gen", "tsc", "api-extractor:commonjs"],
 		"build:test:esm": ["typetests:gen", "build:esnext", "api-extractor:esnext"],
 		"api": {
-			dependsOn: ["api-extractor:commonjs", "api-extractor:esnext", "typetests:gen"],
-			// dependsOn: ["api-extractor:commonjs", "api-extractor:esnext"],
+			dependsOn: ["api-extractor:commonjs", "api-extractor:esnext"],
 			script: false,
 		},
 		"api-extractor:commonjs": ["tsc"],
@@ -234,9 +242,20 @@ module.exports = {
 				"^packages/tools/fluid-runner/package.json",
 			],
 			"fluid-build-tasks-tsc": [
-				// This can be removed once the client release group is using build-tools 0.39.0+.
-				// See https://github.com/microsoft/FluidFramework/pull/21238
-				"^packages/test/test-end-to-end-tests/package.json",
+				// The packages below are excluded because the policy expects the tsc task to depend on "typetests:gen",
+				// but that is not the case any longer, and the policy is out of date.
+				"^common/lib/protocol-definitions/package.json",
+				"^packages/common/driver-definitions/package.json",
+				"^packages/drivers/debugger/package.json",
+				"^packages/drivers/file-driver/package.json",
+				"^packages/drivers/replay-driver/package.json",
+				"^packages/framework/client-logger/app-insights-logger/package.json",
+				"^packages/framework/client-logger/fluid-telemetry/package.json",
+				"^packages/framework/fluid-framework/package.json",
+				"^packages/framework/oldest-client-observer/package.json",
+				"^packages/test/test-pairwise-generator/package.json",
+				"^packages/tools/devtools/devtools-core/package.json",
+				"^packages/tools/devtools/devtools/package.json",
 			],
 			"html-copyright-file-header": [
 				// Tests generate HTML "snapshot" artifacts
