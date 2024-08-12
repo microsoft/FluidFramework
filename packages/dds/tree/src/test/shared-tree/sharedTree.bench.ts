be8a63314a45d51ecfbf6e1e01c24bfba7c3b5a9
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-08-12 17:58:43 +0000

    revert change in sharedtree & remove it in summary

--- packages/dds/tree/src/test/shared-tree/sharedTree.bench.ts
+++ packages/dds/tree/src/test/shared-tree/sharedTree.bench.ts
@@ -5,145 +5,426 @@
 
 import { strict as assert } from "assert";
 
-import { IsoBuffer } from "@fluid-internal/client-utils";
-import { BenchmarkType, benchmarkCustom, benchmark } from "@fluid-tools/benchmark";
-import type { IChannelServices } from "@fluidframework/datastore-definitions/internal";
-import type { ITree } from "@fluidframework/driver-definitions/internal";
-import type { ISummaryTree } from "@fluidframework/driver-definitions";
-import { convertSummaryTreeToITree } from "@fluidframework/runtime-utils/internal";
 import {
-	MockDeltaConnection,
-	MockFluidDataStoreRuntime,
-	MockStorage,
-} from "@fluidframework/test-runtime-utils/internal";
+	type BenchmarkTimer,
+	BenchmarkType,
+	benchmark,
+	isInPerformanceTestingMode,
+} from "@fluid-tools/benchmark";
 
-import { AllowedUpdateType } from "../../core/index.js";
-import { SharedTreeFactory, type TreeContent } from "../../shared-tree/index.js";
-import { makeDeepContent, makeWideContentWithEndValue } from "../scalableTestTrees.js";
-import { TestTreeProviderLite, schematizeFlexTree, testIdCompressor } from "../utils.js";
-
-// TODO: these tests currently only cover tree content.
-// It might make sense to extend them to cover complex collaboration windows.
+import { rootFieldKey } from "../../core/index.js";
+import { singleJsonCursor, typedJsonCursor } from "../../domains/index.js";
+// eslint-disable-next-line import/no-internal-modules
+import { typeboxValidator } from "../../external-utilities/typeboxValidator.js";
+import {
+	TreeCompressionStrategy,
+	jsonableTreeFromCursor,
+} from "../../feature-libraries/index.js";
+import { type FlexTreeView, SharedTreeFactory } from "../../shared-tree/index.js";
+import {
+	type JSDeepTree,
+	type JSWideTree,
+	deepPath,
+	type deepSchema,
+	localFieldKey,
+	makeDeepContent,
+	makeJsDeepTree,
+	makeJsWideTreeWithEndValue,
+	makeWideContentWithEndValue,
+	readDeepCursorTree,
+	readDeepFlexTree,
+	readDeepTreeAsJSObject,
+	readWideCursorTree,
+	readWideFlexTree,
+	readWideTreeAsJSObject,
+	wideSchema,
+} from "../scalableTestTrees.js";
+import {
+	TestTreeProviderLite,
+	checkoutWithContent,
+	flexTreeViewWithContent,
+	insert,
+	toJsonableTree,
+} from "../utils.js";
 
 // number of nodes in test for wide trees
-const nodesCountWide: [numberOfNodes: number, minLength: number, maxLength: number][] = [
-	[1, 1000, 7000],
-	[10, 1000, 10000],
-	[100, 1000, 500000],
+const nodesCountWide = [
+	[1, BenchmarkType.Measurement],
+	[100, BenchmarkType.Perspective],
+	[500, BenchmarkType.Measurement],
 ];
 // number of nodes in test for deep trees
-const nodesCountDeep: [numberOfNodes: number, minLength: number, maxLength: number][] = [
-	[10, 1000, 25000],
-	[100, 1000, 1000000],
-	[200, 1000, 5000000],
+const nodesCountDeep = [
+	[1, BenchmarkType.Measurement],
+	[10, BenchmarkType.Perspective],
+	[100, BenchmarkType.Measurement],
 ];
 
-describe("Summary benchmarks", () => {
-	// TODO: report these sizes as benchmark output which can be tracked over time.
-	describe("size of", () => {
-		it("an empty tree.", async () => {
-			const provider = new TestTreeProviderLite();
-			const tree = provider.trees[0];
-			const { summary } = tree.getAttachSummary(true);
-			const summaryString = JSON.stringify(summary);
-			const summarySize = IsoBuffer.from(summaryString).byteLength;
-			assert(summarySize < 700);
-		});
-		for (const [numberOfNodes, minLength, maxLength] of nodesCountWide) {
-			benchmarkCustom({
-				only: false,
-				type: BenchmarkType.Measurement,
-				title: `a wide tree with ${numberOfNodes} nodes.`,
-				run: async (reporter) => {
-					const summaryTree = getSummaryTree(makeWideContentWithEndValue(numberOfNodes, 1));
-					const summaryString = JSON.stringify(summaryTree);
-					const summarySize = IsoBuffer.from(summaryString).byteLength;
-					reporter.addMeasurement("summarySize", summarySize);
-					assert(summarySize > minLength);
-					assert(summarySize < maxLength);
+// TODO: ADO#7111 Schema should be fixed to enable schema based encoding.
+const factory = new SharedTreeFactory({
+	jsonValidator: typeboxValidator,
+	treeEncodeType: TreeCompressionStrategy.Uncompressed,
+});
+
+// TODO: Once the "BatchTooLarge" error is no longer an issue, extend tests for larger trees.
+describe("SharedTree benchmarks", () => {
+	describe("Direct JS Object", () => {
+		for (const [numberOfNodes, benchmarkType] of nodesCountDeep) {
+			let tree: JSDeepTree;
+			benchmark({
+				type: benchmarkType,
+				title: `Deep Tree as JS Object: reads with ${numberOfNodes} nodes`,
+				before: () => {
+					tree = makeJsDeepTree(numberOfNodes, 1) as JSDeepTree;
+				},
+				benchmarkFn: () => {
+					const { depth, value } = readDeepTreeAsJSObject(tree);
+					assert.equal(depth, numberOfNodes);
+					assert.equal(value, 1);
 				},
 			});
 		}
-		for (const [numberOfNodes, minLength, maxLength] of nodesCountDeep) {
-			benchmarkCustom({
-				only: false,
-				type: BenchmarkType.Measurement,
-				title: `a deep tree with ${numberOfNodes} nodes.`,
-				run: async (reporter) => {
-					const summaryTree = getSummaryTree(makeDeepContent(numberOfNodes));
-					const summaryString = JSON.stringify(summaryTree);
-					const summarySize = IsoBuffer.from(summaryString).byteLength;
-					reporter.addMeasurement("summarySize", summarySize);
-					assert(summarySize > minLength);
-					assert(summarySize < maxLength);
+		for (const [numberOfNodes, benchmarkType] of nodesCountWide) {
+			let tree: JSWideTree;
+			let expected = 0;
+			benchmark({
+				type: benchmarkType,
+				title: `Wide Tree as JS Object: reads with ${numberOfNodes} nodes`,
+				before: () => {
+					tree = makeJsWideTreeWithEndValue(numberOfNodes, numberOfNodes - 1);
+					for (let i = 0; i < numberOfNodes; i++) {
+						expected += i;
+					}
+				},
+				benchmarkFn: () => {
+					const { nodesCount, sum } = readWideTreeAsJSObject(tree);
+					assert.equal(nodesCount, numberOfNodes);
+					assert.equal(sum, expected);
+				},
+			});
+		}
+		describe(`Edit JS Object`, () => {
+			for (const [numberOfNodes, benchmarkType] of nodesCountDeep) {
+				let tree: JSDeepTree;
+				let currentNode: JSDeepTree;
+				benchmark({
+					type: benchmarkType,
+					title: `Update value at leaf of ${numberOfNodes} deep tree`,
+					before: () => {
+						tree = makeJsDeepTree(numberOfNodes, 1) as JSDeepTree;
+						currentNode = tree;
+						while (typeof currentNode !== "number") {
+							if (typeof currentNode.foo === "number") {
+								break;
+							}
+							currentNode = currentNode.foo;
+						}
+					},
+					benchmarkFn: () => {
+						currentNode.foo = -1;
+					},
+					after: () => {
+						const expected = makeJsDeepTree(numberOfNodes, -1);
+						assert.deepEqual(tree, expected);
+					},
+				});
+			}
+			for (const [numberOfNodes, benchmarkType] of nodesCountWide) {
+				let tree: JSWideTree;
+				benchmark({
+					type: benchmarkType,
+					title: `Update value at leaf of ${numberOfNodes} Wide tree`,
+					before: () => {
+						tree = makeJsWideTreeWithEndValue(numberOfNodes, numberOfNodes - 1);
+					},
+					benchmarkFn: () => {
+						tree.foo[numberOfNodes - 1] = -1;
+					},
+					after: () => {
+						const expected = makeJsWideTreeWithEndValue(numberOfNodes, -1);
+						assert.deepEqual(tree, expected);
+					},
+				});
+			}
+		});
+	});
+	describe("Cursors", () => {
+		for (const [numberOfNodes, benchmarkType] of nodesCountDeep) {
+			let tree: FlexTreeView<typeof deepSchema.rootFieldSchema>;
+			benchmark({
+				type: benchmarkType,
+				title: `Deep Tree with cursor: reads with ${numberOfNodes} nodes`,
+				before: () => {
+					tree = flexTreeViewWithContent(makeDeepContent(numberOfNodes));
+				},
+				benchmarkFn: () => {
+					const { depth, value } = readDeepCursorTree(tree);
+					assert.equal(value, 1);
+					assert.equal(depth, numberOfNodes);
+				},
+			});
+		}
+		for (const [numberOfNodes, benchmarkType] of nodesCountWide) {
+			let tree: FlexTreeView<typeof wideSchema.rootFieldSchema>;
+			let expected = 0;
+			benchmark({
+				type: benchmarkType,
+				title: `Wide Tree with cursor: reads with ${numberOfNodes} nodes`,
+				before: () => {
+					const numbers = [];
+					for (let index = 0; index < numberOfNodes; index++) {
+						numbers.push(index);
+						expected += index;
+					}
+					tree = flexTreeViewWithContent(
+						makeWideContentWithEndValue(numberOfNodes, numberOfNodes - 1),
+					);
+				},
+				benchmarkFn: () => {
+					const { nodesCount, sum } = readWideCursorTree(tree);
+					assert.equal(sum, expected);
+					assert.equal(nodesCount, numberOfNodes);
 				},
 			});
 		}
 	});
-
-	describe("load speed of", () => {
-		function runSummaryBenchmark(title: string, content: TreeContent, type: BenchmarkType) {
-			let summaryTree: ITree;
-			const factory = new SharedTreeFactory();
+	describe("FlexTree bench", () => {
+		for (const [numberOfNodes, benchmarkType] of nodesCountDeep) {
+			let tree: FlexTreeView<typeof deepSchema.rootFieldSchema>;
 			benchmark({
-				title,
-				type,
+				type: benchmarkType,
+				title: `Deep Tree with Flex Tree: reads with ${numberOfNodes} nodes`,
 				before: () => {
-					summaryTree = convertSummaryTreeToITree(getSummaryTree(content));
+					tree = flexTreeViewWithContent(makeDeepContent(numberOfNodes));
 				},
-				benchmarkFnAsync: async () => {
-					const services: IChannelServices = {
-						deltaConnection: new MockDeltaConnection(
-							() => 0,
-							() => {},
-						),
-						objectStorage: new MockStorage(summaryTree),
-					};
-					const datastoreRuntime = new MockFluidDataStoreRuntime({
-						idCompressor: testIdCompressor,
-					});
-					await factory.load(datastoreRuntime, "test", services, factory.attributes);
+				benchmarkFn: () => {
+					const { depth, value } = readDeepFlexTree(tree);
+					assert.equal(depth, numberOfNodes);
+					assert.equal(value, 1);
 				},
 			});
 		}
-
-		for (const [nodeCount, type] of [
-			[1, BenchmarkType.Perspective],
-			[10, BenchmarkType.Perspective],
-			[100, BenchmarkType.Measurement],
-		]) {
-			runSummaryBenchmark(
-				`a deep tree with ${nodeCount} nodes}`,
-				makeDeepContent(nodeCount),
-				type,
-			);
+		for (const [numberOfNodes, benchmarkType] of nodesCountWide) {
+			let tree: FlexTreeView<typeof wideSchema.rootFieldSchema>;
+			let expected: number = 0;
+			benchmark({
+				type: benchmarkType,
+				title: `Wide Tree with Flex Tree: reads with ${numberOfNodes} nodes`,
+				before: () => {
+					expected = ((numberOfNodes - 1) * numberOfNodes) / 2; // Arithmetic sum of [0, numberOfNodes)
+					tree = flexTreeViewWithContent({
+						initialTree: typedJsonCursor(makeJsWideTreeWithEndValue(numberOfNodes)),
+						schema: wideSchema,
+					});
+				},
+				benchmarkFn: () => {
+					const { nodesCount, sum } = readWideFlexTree(tree);
+					assert.equal(sum, expected);
+					assert.equal(nodesCount, numberOfNodes);
+					readWideCursorTree(tree);
+				},
+			});
 		}
+	});
+	describe("Edit with editor", () => {
+		const setCount = 100;
+		for (const [numberOfNodes, benchmarkType] of nodesCountDeep) {
+			benchmark({
+				type: benchmarkType,
+				title: `Update value at leaf of ${numberOfNodes} deep tree ${setCount} times`,
+				benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
+					let duration: number;
+					do {
+						// Since this setup one collects data from one iteration, assert that this is what is expected.
+						assert.equal(state.iterationsPerBatch, 1);
 
-		for (const [nodeCount, type] of [
-			[10, BenchmarkType.Perspective],
-			[100, BenchmarkType.Measurement],
-		]) {
-			runSummaryBenchmark(
-				`a wide tree with ${nodeCount} nodes}`,
-				makeWideContentWithEndValue(nodeCount, 1),
-				type,
-			);
+						// Setup
+						const tree = checkoutWithContent(makeDeepContent(numberOfNodes));
+						const path = deepPath(numberOfNodes);
+
+						// Measure
+						const before = state.timer.now();
+						for (let value = 1; value <= setCount; value++) {
+							tree.editor
+								.valueField({ parent: path, field: localFieldKey })
+								.set(singleJsonCursor(value));
+						}
+						const after = state.timer.now();
+						duration = state.timer.toSeconds(before, after);
+
+						// Cleanup + validation
+						const expected = jsonableTreeFromCursor(
+							typedJsonCursor(makeJsDeepTree(numberOfNodes, setCount)),
+						);
+						const actual = toJsonableTree(tree);
+						assert.deepEqual(actual, [expected]);
+
+						// Collect data
+					} while (state.recordBatch(duration));
+				},
+				// Force batch size of 1
+				minBatchDurationSeconds: 0,
+			});
+		}
+		for (const [numberOfNodes, benchmarkType] of nodesCountWide) {
+			benchmark({
+				type: benchmarkType,
+				title: `Update value at leaf of ${numberOfNodes} wide tree ${setCount} times`,
+				benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
+					let duration: number;
+					do {
+						// Since this setup one collects data from one iteration, assert that this is what is expected.
+						assert.equal(state.iterationsPerBatch, 1);
+
+						// Setup
+						const tree = checkoutWithContent({
+							initialTree: typedJsonCursor(makeJsWideTreeWithEndValue(numberOfNodes)),
+							schema: wideSchema,
+						});
+
+						const rootPath = {
+							parent: undefined,
+							parentField: rootFieldKey,
+							parentIndex: 0,
+						};
+						const nodeIndex = numberOfNodes - 1;
+						const editor = tree.editor.sequenceField({
+							parent: rootPath,
+							field: localFieldKey,
+						});
+
+						// Measure
+						const before = state.timer.now();
+						for (let value = 1; value <= setCount; value++) {
+							editor.remove(nodeIndex, 1);
+							editor.insert(nodeIndex, singleJsonCursor(value));
+						}
+						const after = state.timer.now();
+						duration = state.timer.toSeconds(before, after);
+
+						// Cleanup + validation
+						const expected = jsonableTreeFromCursor(
+							typedJsonCursor(makeJsWideTreeWithEndValue(numberOfNodes, setCount)),
+						);
+						const actual = toJsonableTree(tree);
+						assert.deepEqual(actual, [expected]);
+
+						// Collect data
+					} while (state.recordBatch(duration));
+				},
+				// Force batch size of 1
+				minBatchDurationSeconds: 0,
+			});
+		}
+	});
+
+	describe("acking local commits", () => {
+		const localCommitSize = [1, 25, 100, 500, 1000];
+		for (const size of localCommitSize) {
+			benchmark({
+				type: BenchmarkType.Measurement,
+				title: `for ${size} local commit${size === 1 ? "" : "s"}`,
+				benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
+					let duration: number;
+					do {
+						// Since this setup one collects data from one iteration, assert that this is what is expected.
+						assert.equal(state.iterationsPerBatch, 1);
+
+						// Setup
+						const provider = new TestTreeProviderLite(1, factory);
+						// TODO: specify a schema for these trees.
+						const [tree] = provider.trees;
+						for (let i = 0; i < size; i++) {
+							insert(tree.checkout, i, "test");
+						}
+
+						// Measure
+						const before = state.timer.now();
+						provider.processMessages();
+						const after = state.timer.now();
+						duration = state.timer.toSeconds(before, after);
+						// Collect data
+					} while (state.recordBatch(duration));
+				},
+				// Force batch size of 1
+				minBatchDurationSeconds: 0,
+			});
+		}
+	});
+
+	// Note that this runs the computation for several peers.
+	// In practice, this computation is distributed across peers, so the actual time reported is
+	// divided by the number of peers.
+	describe("rebasing commits", () => {
+		// Each commit generates 2 ops (one for the changeset and one for the UUID minting
+		const opsPerCommit = 2;
+		const sampleSize = 10;
+		// Number of peers that are generating commits.
+		const peerCounts = [2, 4];
+		// Number of commits that are generated in the amount of time it takes of a single commit to round-trip.
+		// E.g., 10 is equivalent to all of the following (and more):
+		// - generating 5 edits per second with a 2000ms round-trip time
+		// - generating 10 edits per second with a 1000ms round-trip time
+		// - generating 100 edits per second with a 100ms round-trip time
+		const commitCounts = [1, 5, 10];
+		for (const peerCount of peerCounts) {
+			for (const commitCount of commitCounts) {
+				const test = benchmark({
+					type: BenchmarkType.Measurement,
+					title: `for ${commitCount} commits per peer for ${peerCount} peers`,
+					benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
+						let duration: number;
+						do {
+							// Since this setup one collects data from one iteration, assert that this is what is expected.
+							assert.equal(state.iterationsPerBatch, 1);
+							const provider = new TestTreeProviderLite(peerCount, factory);
+
+							// This is the start of the stream of commits.
+							// Earlier commits are less out of date and therefore not representative.
+							for (let iCommit = 0; iCommit < commitCount; iCommit++) {
+								for (let iPeer = 0; iPeer < peerCount; iPeer++) {
+									const peer = provider.trees[iPeer];
+									insert(peer.checkout, 0, `p${iPeer}c${iCommit}`);
+								}
+							}
+
+							// This block generates commits that are all out of date to the same degree
+							for (let iCommit = 0; iCommit < commitCount; iCommit++) {
+								for (let iPeer = 0; iPeer < peerCount; iPeer++) {
+									provider.processMessages(opsPerCommit);
+									const peer = provider.trees[iPeer];
+									insert(peer.checkout, 0, `p${iPeer}c${iCommit}`);
+								}
+							}
+
+							// This block measures commits that are all out of date to the same degree.
+							// We could theoretically measure the time it takes for a single commit to be processed,
+							// but averaging over multiple commits gives a more stable result.
+							let timeSum = 0;
+							for (let iCommit = 0; iCommit < sampleSize; iCommit++) {
+								for (let iPeer = 0; iPeer < peerCount; iPeer++) {
+									const before = state.timer.now();
+									provider.processMessages(opsPerCommit);
+									const after = state.timer.now();
+									timeSum += state.timer.toSeconds(before, after);
+									// We still generate commits because it affects local branch rebasing
+									const peer = provider.trees[iPeer];
+									insert(peer.checkout, 0, `p${iPeer}c${iCommit}`);
+								}
+							}
+
+							// We want the average time it would take one peer to process one incoming edit
+							duration = timeSum / (peerCount * peerCount * sampleSize);
+						} while (state.recordBatch(duration));
+					},
+					// Force batch size of 1
+					minBatchDurationSeconds: 0,
+				});
+
+				if (!isInPerformanceTestingMode) {
+					test.timeout(5000);
+				}
+			}
 		}
 	});
 });
-
-/**
- * @param content - content to full the tree with
- * @returns the tree's summary
- */
-function getSummaryTree(content: TreeContent): ISummaryTree {
-	const provider = new TestTreeProviderLite();
-	const tree = provider.trees[0];
-	schematizeFlexTree(tree, {
-		...content,
-		allowedSchemaModifications: AllowedUpdateType.Initialize,
-	});
-	provider.processMessages();
-	const { summary } = tree.getAttachSummary(true);
-	return summary;
-}
--- packages/dds/tree/src/test/shared-tree/summary.bench.ts
+++ packages/dds/tree/src/test/shared-tree/summary.bench.ts
@@ -50,37 +50,33 @@ describe("Summary benchmarks", () => {
 			assert(summarySize < 700);
 		});
 		for (const [numberOfNodes, minLength, maxLength] of nodesCountWide) {
-			it(`a wide tree with ${numberOfNodes} nodes.`, async () => {
-				benchmarkCustom({
-					only: false,
-					type: BenchmarkType.Measurement,
-					title: `a wide tree with ${numberOfNodes} nodes.`,
-					run: async (reporter) => {
-						const summaryTree = getSummaryTree(makeWideContentWithEndValue(numberOfNodes, 1));
-						const summaryString = JSON.stringify(summaryTree);
-						const summarySize = IsoBuffer.from(summaryString).byteLength;
-						reporter.addMeasurement("summarySize", summarySize);
-						assert(summarySize > minLength);
-						assert(summarySize < maxLength);
-					},
-				});
+			benchmarkCustom({
+				only: false,
+				type: BenchmarkType.Measurement,
+				title: `a wide tree with ${numberOfNodes} nodes.`,
+				run: async (reporter) => {
+					const summaryTree = getSummaryTree(makeWideContentWithEndValue(numberOfNodes, 1));
+					const summaryString = JSON.stringify(summaryTree);
+					const summarySize = IsoBuffer.from(summaryString).byteLength;
+					reporter.addMeasurement("summarySize", summarySize);
+					assert(summarySize > minLength);
+					assert(summarySize < maxLength);
+				},
 			});
 		}
 		for (const [numberOfNodes, minLength, maxLength] of nodesCountDeep) {
-			it(`a deep tree with ${numberOfNodes} nodes.`, async () => {
-				benchmarkCustom({
-					only: false,
-					type: BenchmarkType.Measurement,
-					title: `a deep tree with ${numberOfNodes} nodes.`,
-					run: async (reporter) => {
-						const summaryTree = getSummaryTree(makeDeepContent(numberOfNodes));
-						const summaryString = JSON.stringify(summaryTree);
-						const summarySize = IsoBuffer.from(summaryString).byteLength;
-						reporter.addMeasurement("summarySize", summarySize);
-						assert(summarySize > minLength);
-						assert(summarySize < maxLength);
-					},
-				});
+			benchmarkCustom({
+				only: false,
+				type: BenchmarkType.Measurement,
+				title: `a deep tree with ${numberOfNodes} nodes.`,
+				run: async (reporter) => {
+					const summaryTree = getSummaryTree(makeDeepContent(numberOfNodes));
+					const summaryString = JSON.stringify(summaryTree);
+					const summarySize = IsoBuffer.from(summaryString).byteLength;
+					reporter.addMeasurement("summarySize", summarySize);
+					assert(summarySize > minLength);
+					assert(summarySize < maxLength);
+				},
 			});
 		}
 	});
