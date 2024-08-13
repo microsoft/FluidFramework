c808166a08331330a105e81dfe9cd80ba4643d50
Jatin Garg <jatingarg@Jatins-MacBook-Pro-2.local>
2024-08-13 15:49:37 -0700

    Use TextEncoder byte length to calcaulte body size in odsp driver

--- packages/drivers/odsp-driver/src/createNewUtils.ts
+++ packages/drivers/odsp-driver/src/createNewUtils.ts
@@ -253,7 +253,8 @@ export async function createNewFluidContainerCore<T>(args: {
 					`\r\n--${formBoundary}--`;
 
 				let postBody = snapshotBody;
-				// We use the byte length of the post body to determine if we should use the multipart/form-data or not.
+				// We use the byte length of the post body to determine if we should use the multipart/form-data or not. This helps
+				// in cases where the body contains data with different language where 1 char could be multiple code points.
 				if (
 					new TextEncoder().encode(postBodyWithAuth).length <= maxUmpPostBodySize &&
 					authHeader?.startsWith("Bearer")
