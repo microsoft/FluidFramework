6d1aeb267eb66d22460f85d3bd14277b61eeaa8b
seanimam <105244057+seanimam@users.noreply.github.com>
2024-08-13 16:44:35 +0000

    adds new fluid-llm package

---
+++ experimental/framework/fluid-llm/src/index.ts
@@ -0,0 +1,11 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * Replace this
+ */
+export function replaceThis(): boolean {
+	return true;
+}
---
+++ experimental/framework/fluid-llm/src/test/reactSharedTreeView.spec.ts
@@ -0,0 +1,13 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "node:assert";
+
+
+describe("reactSharedTreeView", () => {
+	it("treeDataObject", async () => {
+		assert(true)
+	});
+});
