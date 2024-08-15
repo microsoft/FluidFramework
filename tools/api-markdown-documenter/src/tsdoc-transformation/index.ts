91ad3d6a82ea85b6b45bb76134ba3d375182b11d
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-08-15 22:07:22 +0000

    remove: TSDoc transformation domain

--- tools/api-markdown-documenter/src/tsdoc-transformation/index.ts
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
-export type { TransformationContext } from "./Configuration.js";
-export { transformTsdocNode } from "./Transformation.js";
