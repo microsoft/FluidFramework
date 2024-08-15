227d1a8eefa251ab229a64fa2576633f100dc1eb
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-08-15 00:29:46 +0000

    docs: Fix comment

--- tools/api-markdown-documenter/src/LintApiModel.ts
+++ tools/api-markdown-documenter/src/LintApiModel.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import { fail, strict as assert } from "node:assert";
 import {
 	ApiDocumentedItem,
