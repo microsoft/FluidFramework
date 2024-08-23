1753ffe60b38f76172db636b60881e6a2dab6ae1
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-08-23 21:54:34 +0000

    fix: policy-check

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
--- docusaurus/src/components/HomepageFeatures/index.tsx
+++ docusaurus/src/components/HomepageFeatures/index.tsx
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
