04aec39abfe4469f7f8ecba8ab4b927f953877a6
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-08-27 21:19:46 +0000

    feat: Support heading IDs

--- docusaurus/src/components/shortLinks.tsx
+++ docusaurus/src/components/shortLinks.tsx
@@ -11,13 +11,15 @@
 export interface PackageLinkProps {
 	children?: React.ReactNode;
 	packageName: string;
+	headingId?: string;
 }
 
 /**
  * A convenient mechanism for linking to a package's API documentation.
  */
-export function PackageLink({ packageName, children }: PackageLinkProps): JSX.Element {
-	return <a href={`/docs/api/${packageName}`}>{children}</a>;
+export function PackageLink({ headingId, packageName, children }: PackageLinkProps): JSX.Element {
+	const headingPostfix = headingId ? `#${headingId}` : "";
+	return <a href={`/docs/api/${packageName}${headingPostfix}`}>{children}</a>;
 }
 
 /**
@@ -29,14 +31,16 @@ export interface ApiLinkProps {
 	apiName: string;
 	// TODO: import directly from `api-extractor-model`
 	apiType: "class" | "enum" | "function" | "interface" | "namespace" | "type" | "variable";
+	headingId?: string;
 }
 
 /**
  * A convenient mechanism for linking to the API documentation for a specified API item.
  */
-export function ApiLink({ packageName, apiName, apiType, children }: ApiLinkProps): React.ReactElement {
+export function ApiLink({ apiName, apiType, packageName, headingId, children }: ApiLinkProps): React.ReactElement {
+	const headingPostfix = headingId ? `#${headingId}` : "";
 	// TODO: how to deal with namespaces?
-	const path = `/docs/api/${packageName}/${apiName}-${apiType}`;
+	const path = `/docs/api/${packageName}/${apiName}-${apiType}${headingPostfix}`;
 	return <a href={path}>{children}</a>;
 }
 
