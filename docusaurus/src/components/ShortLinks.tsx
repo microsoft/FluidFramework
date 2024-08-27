d8c3471e2fe7e002414906b180aba05df3a2790a
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-08-27 20:11:30 +0000

    feat: Add "shortlink" for glossary terms

--- docusaurus/src/components/ShortLinks.tsx
+++ docusaurus/src/components/ShortLinks.tsx
@@ -14,7 +14,7 @@ export interface PackageLinkProps {
 }
 
 /**
- * A convenient mechanism for linking to a package's documentation.
+ * A convenient mechanism for linking to a package's API documentation.
  */
 export function PackageLink({ packageName, children }: PackageLinkProps): JSX.Element {
 	return <a href={`/docs/api/${packageName}`}>{children}</a>;
@@ -31,8 +31,30 @@ export interface ApiLinkProps {
 	apiType: "class" | "enum" | "function" | "interface" | "namespace" | "type" | "variable";
 }
 
+/**
+ * A convenient mechanism for linking to the API documentation for a specified API item.
+ */
 export function ApiLink({ packageName, apiName, apiType, children }: ApiLinkProps): React.ReactElement {
 	// TODO: how to deal with namespaces?
 	const path = `/docs/api/${packageName}/${apiName}-${apiType}`;
 	return <a href={path}>{children}</a>;
+}
+
+
+/**
+ * {@link GlossaryLink} input props.
+ */
+export interface GlossaryLinkProps {
+	/**
+	 * The glossary term to link to.
+	 */
+	term: string;
+}
+
+/**
+ * A convenient mechanism for linking to a defined glossary term.
+ * @remarks Assumes that a heading exists for the specified term in `docs/glossary.md`.
+ */
+export function GlossaryLink({ term }: GlossaryLinkProps): JSX.Element {
+	return <a href={`/docs/glossary#${term}`}>{term}</a>;
 }
