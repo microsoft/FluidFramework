4862dc64c056ed628cde7939f619dc5c5eaff726
Tyler Butler <tylerbu@microsoft.com>
2024-08-27 16:35:34 -0700

    builds

--- build-tools/packages/build-cli/src/typeValidator/changeToNamedExports.ts
+++ build-tools/packages/build-cli/src/typeValidator/changeToNamedExports.ts
@@ -1,11 +1,15 @@
 /*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
+* Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+* Licensed under the MIT License.
+*/
+
+// AB#13931: Remove these lint disables
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable unicorn/no-array-for-each */
 
 import { IndentationText, Project } from "ts-morph";
 
-function case_insensitive_comp(strA, strB) {
+function case_insensitive_comp(strA: string, strB: string) {
 	return strA.localeCompare(strB, "en", { sensitivity: "base" });
 }
 
@@ -92,5 +96,7 @@ async function run(): Promise<boolean> {
 }
 
 run()
+	// eslint-disable-next-line n/no-process-exit, unicorn/no-process-exit
 	.then((success) => process.exit(success ? 0 : 1))
+	// eslint-disable-next-line n/no-process-exit, unicorn/no-process-exit, unicorn/prefer-top-level-await
 	.catch(() => process.exit(2));
