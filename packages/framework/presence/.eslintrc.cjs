76d63359ecdddc8c0539b67992ddbede4a7f204c
Jason Hartman <jasonha@microsoft.com>
2024-09-03 10:45:11 -0700

    test(client): remove unused lint exceptions

--- packages/framework/presence/.eslintrc.cjs
+++ packages/framework/presence/.eslintrc.cjs
@@ -8,22 +8,12 @@ module.exports = {
 	parserOptions: {
 		project: ["./tsconfig.json", "./src/test/tsconfig.json"],
 	},
-	rules: {
-		// The clarity of explicit index signatures is helpful in many places with this package.
-		"@typescript-eslint/consistent-indexed-object-style": "off",
-
-		// This library is used in the browser, so we don't want dependencies on most node libraries.
-		"import/no-nodejs-modules": ["error", { allow: ["events"] }],
-	},
 	overrides: [
 		{
 			// Rules only for test files
 			files: ["*.spec.ts", "src/test/**"],
 			rules: {
 				"@typescript-eslint/no-explicit-any": "error",
-
-				// Test files are run in node only so additional node libraries can be used.
-				"import/no-nodejs-modules": ["error", { allow: ["assert", "events"] }],
 			},
 		},
 	],
