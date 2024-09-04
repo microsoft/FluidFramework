cd9e3672ef51aba37ebd8357f0934cc9dacfd4a5
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-09-04 18:46:18 +0000

    remove

--- tools/benchmark/src/test/CustomOptionBenchmark.test.ts
+++ tools/benchmark/src/test/CustomOptionBenchmark.test.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import { benchmarkCustom, type BenchmarkError } from "..";
+import { benchmarkCustom } from "..";
 import { BenchmarkType } from "../Configuration";
 
 describe("`benchmarkCustom` function", () => {
@@ -15,40 +15,5 @@ describe("`benchmarkCustom` function", () => {
 			},
 			type: BenchmarkType.OwnCorrectness,
 		});
-	});
-});
-
-/**
- * TODO: This test attempts to verify that the `benchmarkCustom()` is able to throw an error payload as expected.
- * However, this test is not working as expected due to complication around mocha runtime. The test is skipped for now.
- */
-describe.skip("BenchmarkCustom error handling", () => {
-	const expectedErrorMessage = "INTENTIONAL error to test error handling";
-	let benchmarkEndPayloadIsCorrect: boolean = false;
-
-	const testObject = benchmarkCustom({
-		title: `test`,
-		type: BenchmarkType.Measurement,
-		run: async () => {
-			throw new Error(expectedErrorMessage);
-		},
-	});
-
-	testObject.on("benchmark end", (benchmarkError: BenchmarkError) => {
-		console.log(benchmarkError.error); // Check what is actually being passed here
-		if (benchmarkError.error === expectedErrorMessage) {
-			benchmarkEndPayloadIsCorrect = true;
-		}
-	});
-
-	// For the purpose of this test, we want to:
-	// 1) Report an event (i.e, `benchmark end`) to the reporter
-	// 2) Skip the test if the `bechmarkCustom()` throws an error payload as expected.
-	afterEach(async function () {
-		if (benchmarkEndPayloadIsCorrect) {
-			this.skip();
-		} else {
-			throw new Error("The benchmark end payload was not correct.");
-		}
 	});
 });
