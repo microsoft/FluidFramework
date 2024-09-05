e253704d77f3ec6b0bb4c28676f139bd84d488f7
Tyler Butler <tylerbu@microsoft.com>
2024-09-05 16:45:40 -0700

    policy

--- build-tools/packages/build-tools/src/fluidBuild/fileHashCache.ts
+++ build-tools/packages/build-tools/src/fluidBuild/fileHashCache.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import { readFile } from "fs/promises";
 import { sha256 } from "./hash";
 
