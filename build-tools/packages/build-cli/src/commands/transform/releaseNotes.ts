88ac56341bd521f0347034af24db558c7440f0e7
Tyler Butler <tyler@tylerbutler.com>
2024-09-11 16:11:28 -0700

    Update releaseNotes.ts
    Co-authored-by: jzaffiro <110866475+jzaffiro@users.noreply.github.com>

--- build-tools/packages/build-cli/src/commands/transform/releaseNotes.ts
+++ build-tools/packages/build-cli/src/commands/transform/releaseNotes.ts
@@ -75,7 +75,7 @@ export default class TransformReleaseNotesCommand extends BaseCommand<
 			.use(admonitions, {
 				titleTextMap: (title) => ({
 					// By default the `[!` prefix and `]` suffix are removed; we don't want that, so we override the default and
-					// return he title as-is.
+					// return the title as-is.
 					displayTitle: title,
 					checkedTitle: title,
 				}),
