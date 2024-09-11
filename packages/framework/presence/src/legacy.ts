1aba716f814bdeab86f1968fe3ab6736bf6acd6f
Jason Hartman <jasonha@microsoft.com>
2024-09-11 15:15:15 -0700

    feat(client): Legacy experimental Presence support with example use

---
+++ packages/framework/presence/src/legacy.ts
@@ -0,0 +1,9 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+// eslint-disable-next-line no-restricted-syntax
+export * from "./index.js";
+
+export { ExperimentalLegacyPresenceManager } from "./datastorePresenceManagerFactory.js";
