cde5eea56667921871c7a47426f2df1cd8b55eb8
Jatin Garg <jatingarg@Jatins-MacBook-Pro-2.local>
2024-09-10 23:09:09 -0700

    remove opackage

--- build-tools/packages/code-coverage-tools/src/ADO/index.ts
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
-export { getAzureDevopsApi } from "./getAzureDevopsApi";
-export { IADOCodeCoverageConstants } from "./constants";
-export { getBaselineBuildMetrics } from "./getBaselineBuildMetrics";
--- build-tools/packages/code-coverage-tools/src/index.ts
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
-export { codeCoverageCli, CodeCoverageSummary } from "./codeCoverageCli";
-export { IADOCodeCoverageConstants } from "./ADO/index";
