e024b86dcc2f0b6315288fb9552bb2fa8d18297c
Mark Fields <markfields@users.noreply.github.com>
2024-09-13 16:06:38 -0700

    ContaineRuntime: Refactor batch processing code to support either op-by-op or batch-all-at-once semantics (#22501)
    No behavior change here, just refactoring to be able to express the
    result of RemoteMessageProcessor in terms of ops or batches.
    
    Follow-up change to adjust the logic will be much more scoped then.

--- packages/runtime/container-runtime/src/test/containerRuntime.spec.ts
+++ packages/runtime/container-runtime/src/test/containerRuntime.spec.ts
@@ -69,11 +69,12 @@ import {
 import {
 	ContainerMessageType,
 	type ContainerRuntimeGCMessage,
+	type InboundSequencedContainerRuntimeMessage,
 	type OutboundContainerRuntimeMessage,
 	type RecentlyAddedContainerRuntimeMessageDetails,
 	type UnknownContainerRuntimeMessage,
 } from "../messageTypes.js";
-import type { BatchMessage, InboundBatch } from "../opLifecycle/index.js";
+import type { BatchMessage, InboundMessageResult } from "../opLifecycle/index.js";
 import {
 	IPendingLocalState,
 	IPendingMessage,
@@ -823,11 +824,12 @@ describe("Runtime", () => {
 				return {
 					replayPendingStates: () => {},
 					hasPendingMessages: (): boolean => pendingMessages > 0,
-					processMessage: (_message: ISequencedDocumentMessage, _local: boolean) => {
-						return { localAck: false, localOpMetadata: undefined };
-					},
-					processInboundBatch: (batch: InboundBatch, _local: boolean) => {
-						return batch.messages.map((message) => ({
+					processInboundMessages: (inbound: InboundMessageResult, _local: boolean) => {
+						const messages = inbound.messages;
+						return messages.map<{
+							message: InboundSequencedContainerRuntimeMessage;
+							localOpMetadata?: unknown;
+						}>((message) => ({
 							message,
 							localOpMetadata: undefined,
 						}));
@@ -837,7 +839,7 @@ describe("Runtime", () => {
 					},
 					onFlushBatch: (batch: BatchMessage[], _csn?: number) =>
 						(pendingMessages += batch.length),
-				} as unknown as PendingStateManager;
+				} satisfies Partial<PendingStateManager> as any as PendingStateManager;
 			};
 			const getMockChannelCollection = (): ChannelCollection => {
 				// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
