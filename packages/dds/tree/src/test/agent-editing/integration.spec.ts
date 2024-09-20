f2fee7f94684ebce67861d19f10dac74f4a63d4c
Noah Encke <78610362+noencke@users.noreply.github.com>
2024-09-20 15:27:10 +0000

    Use nick schema

--- packages/dds/tree/src/test/agent-editing/integration.spec.ts
+++ packages/dds/tree/src/test/agent-editing/integration.spec.ts
@@ -13,30 +13,44 @@ import {
 	KLUDGE,
 } from "../../agent-editing/index.js";
 
-const sf = new SchemaFactory("agentSchema");
+const sf = new SchemaFactory("Planner");
 
-class Vector extends sf.object("Vector", {
+export class Session extends sf.object("Session", {
 	id: sf.identifier,
-	x: sf.required(sf.number, { metadata: { description: "The x-coordinate of the vector." } }),
-	y: sf.required(sf.number, { metadata: { description: "The y-coordinate of the vector." } }),
-	z: sf.optional(sf.number, {
+	title: sf.string,
+	abstract: sf.string,
+	sessionType: sf.required(sf.string, {
 		metadata: {
 			description:
-				"The optional z-coordinate of the vector. If absent, this is a 2D vector. If present, it is a 3D vector.",
+				"This is one of four possible strings: 'Session', 'Workshop', 'Panel', or 'Keynote'",
 		},
 	}),
-	timeCreated: sf.required(sf.string, {
-		metadata: { llmDefault: () => "Taylor's underpants are inside out" },
+	created: sf.required(sf.number, { metadata: { llmDefault: () => Date.now() } }),
+	lastChanged: sf.required(sf.number, { metadata: { llmDefault: () => Date.now() } }),
+}) {}
+
+const SessionType = {
+	session: "Session",
+	workshop: "Workshop",
+	panel: "Panel",
+	keynote: "Keynote",
+};
+
+export class Sessions extends sf.array("Sessions", Session) {}
+
+export class Days extends sf.array("Days", Sessions) {}
+
+export class Conference extends sf.object("Conference", {
+	name: sf.string,
+	sessions: sf.required(Sessions, {
+		metadata: {
+			description:
+				"These sessions are not scheduled yet. The user (or AI agent) can move them to a specific day.",
+		},
 	}),
+	days: Days,
+	sessionsPerDay: sf.number,
 }) {}
-
-class RootObject extends sf.object("RootObject", {
-	str: sf.string,
-	vectors: sf.array(Vector),
-	bools: sf.array(sf.boolean),
-}) {}
-
-const prompt = "Please add a handle of vectors that are fairly close to each other.";
 
 describe("Agent Editing Integration", () => {
 	it("Test", async () => {
@@ -44,11 +58,48 @@ describe("Agent Editing Integration", () => {
 		process.env.AZURE_OPENAI_ENDPOINT = "TODO";
 		process.env.AZURE_OPENAI_DEPLOYMENT = "gpt-4o";
 
-		const view = getView(new TreeViewConfiguration({ schema: RootObject }));
+		const view = getView(new TreeViewConfiguration({ schema: Conference }));
 		view.initialize({
-			str: "TEST",
-			vectors: [],
-			bools: [],
+			name: "Roblox Creator x Investor Conference",
+			sessions: [],
+			days: [
+				[
+					{
+						title: "Can Roblox achieve 120 hz?",
+						abstract:
+							"With the latest advancements in the G transformation, we may achieve up to 120 hz and still have time for lunch.",
+						sessionType: SessionType.session,
+						created: Date.now(),
+						lastChanged: Date.now(),
+					},
+					{
+						title: "What about fun?",
+						abstract: "Can profit and the delightful smiles of the children coexist?",
+						sessionType: SessionType.workshop,
+						created: Date.now(),
+						lastChanged: Date.now(),
+					},
+				],
+				[
+					{
+						title: "Monetizing Children",
+						abstract: "Maximize those Robux.",
+						sessionType: SessionType.session,
+						created: Date.now(),
+						lastChanged: Date.now(),
+					},
+					{
+						title:
+							"The Gentrification of Roblox City's Downtown (and why that's a good thing)",
+						abstract:
+							"Real estate prices in Robloxia are skyrocketing, moving cash into the hands of those who can use it most wisely.",
+						sessionType: SessionType.workshop,
+						created: Date.now(),
+						lastChanged: Date.now(),
+					},
+				],
+			],
+			sessionsPerDay: 2,
 		});
 		const context = initializeOpenAIClient(view);
 		try {
@@ -62,3 +113,5 @@ describe("Agent Editing Integration", () => {
 		console.log(stringified);
 	});
 });
+
+const prompt = "Please add a new session to the first day of the conference.";
