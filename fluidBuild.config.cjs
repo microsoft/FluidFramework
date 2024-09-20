d069bdeb7b92b06a964aa69b2b5b18ee010e7b9e
Sonali Deshpande <48232592+sonalideshpandemsft@users.noreply.github.com>
2024-09-20 18:45:01 +0000

    fix exlusions

--- fluidBuild.config.cjs
+++ fluidBuild.config.cjs
@@ -404,19 +404,23 @@ module.exports = {
 				"^packages/tools/webpack-fluid-loader/",
 			],
 			"npm-check-unused-dependencies": [
-		        // Ommitting some sub-directories. Following will eventually be removed from the exclude list as
-		        // we progressively fix the unsed dependency errors.
-		        "^build-tools/",
-		        "^common/",
-		        "^docs/",
-		        "^examples/",
-		        "^experimental/",
-		        "^packages/",
-		        "^server/charts/",
-		        "^server/gitrest/",
-		        "^server/gitssh/",
-		        "^server/historian/",
-		      ],
+				// Ommitting some sub-directories. Following will eventually be removed from the exclude list as
+				// we progressively fix the unsed dependency errors.
+				"^azure",
+				"^build-tools/",
+				"^common/",
+				"^docs/",
+				"^examples/",
+				"^experimental/",
+				"package.json",
+				"^packages/",
+				"^server/charts/",
+				"^server/gitrest/",
+				"^server/gitssh/",
+				"^server/historian/",
+				"^server/routerlicious/",
+				"^tools/",
+			],
 		},
 		packageNames: {
 			// The allowed package scopes for the repo.
