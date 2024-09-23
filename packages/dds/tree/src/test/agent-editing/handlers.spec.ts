42bd639d94e8652afa1812d6bc35e28651e02bfe
Noah Encke <78610362+noencke@users.noreply.github.com>
2024-09-23 19:28:40 +0000

    Fix copyright headers

--- packages/dds/tree/src/test/agent-editing/handlers.spec.ts
+++ packages/dds/tree/src/test/agent-editing/handlers.spec.ts
@@ -1,7 +1,7 @@
-// /*!
-//  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
-//  * Licensed under the MIT License.
-//  */
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
 
 // import { strict as assert } from "assert";
 // import { SchemaFactory } from "../../simple-tree/index.js";
