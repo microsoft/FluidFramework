58be33cc5c856413f677ce02cb3442aa16a7092c
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2024-09-23 18:03:00 -0500

    Improve stageTimingRetriever handler

--- tools/telemetry-generator/src/handlers/stageTimingRetriever.ts
+++ tools/telemetry-generator/src/handlers/stageTimingRetriever.ts
@@ -10,36 +10,53 @@ interface ParsedJob {
 	stageName: string;
 	startTime: number;
 	finishTime: number;
-	totalTime: number;
+	totalSeconds: number;
 	state: string;
 	result: string;
 }
 
+/**
+ * This handler is used to extract timing and result data for a stage in a pipeline from a JSON file that
+ * contains the output from the ADO REST API that gives us information about a pipeline run.
+ * It then sends telemetry events to Kusto with the extracted data.
+ *
+ * It assumes the specified stage has already completed, and will throw otherwise.
+ *
+ * @param fileData - A JSON object obtained by calling JSON.parse() on the output of the ADO REST API that gives us
+ * information about a pipeline run, i.e.
+ * https://dev.azure.com/fluidframework/internal/_apis/build/builds/<buildId>/timeline?api-version=6.0-preview.1
+ * @param logger - The `ITelemetryLogger` to use to output the extracted data.
+ */
 module.exports = function handler(fileData, logger): void {
-	// - fileData is a JSON object obtained by calling JSON.parse() on the contents of a file.
-	// In this particular handler, we are using the timeline REST API to retrieve the status of the pipeline:
-	// Ex. https://dev.azure.com/fluidframework/internal/_apis/build/builds/<buildId>/timeline?api-version=6.0-preview.1
-	// - logger is an ITelemetryBufferedLogger. Call its send() method to write the output telemetry
-	//   events.
 	if (fileData.records?.length === undefined || fileData.records?.length === 0) {
-		console.log(`could not locate records info`);
+		throw new Error ("No records found in the input data.");
 	}
 	if (process.env.BUILD_ID === undefined) {
-		console.log("BUILD_ID not defined.");
-	} else {
-		console.log("BUILD_ID", process.env.BUILD_ID);
+		throw new Error ("BUILD_ID environment variable is not set.");
 	}
+	if (process.env.PIPELINE === undefined) {
+		throw new Error ("PIPELINE environment variable is not set.");
+	}
+	if (process.env.STAGE_ID === undefined) {
+		throw new Error ("STAGE_ID environment variable is not set.");
+	}
+
+	console.log("BUILD_ID", process.env.BUILD_ID);
 
 	const parsedJobs: ParsedJob[] = fileData.records
 		// Note: type === "Task" or type === "Job" would include task-level (or job-level, respectively) telemetry.
 		// It might be interesting in the future - for now we will only collect stage-level telemetry.
-		.filter((job) => job.type === "Stage" && (process.env.STAGE_ID === undefined || job.identifier === process.env.STAGE_ID))
+		.filter((job) => job.type === "Stage" && job.identifier === process.env.STAGE_ID)
 		.map((job) => {
-			const startTime = Date.parse(job.startTime?.toString()) ?? undefined;
-			const finishTime = Date.parse(job.finishTime?.toString()) ?? undefined;
-			const dateDiff =
-				finishTime && startTime ? Math.abs(finishTime - startTime) / 1000 : undefined; // diff in seconds
-			console.log(`Name=${job.name}`);
+			const startTime = Date.parse(job.startTime.toString());
+			const finishTime = Date.parse(job.finishTime.toString());
+			if (Number.isNaN(startTime) || Number.isNaN(finishTime)) {
+				// eslint-disable-next-line unicorn/prefer-type-error -- TypeError feels weird to me here; doesn't really matter, we just want to terminate the process
+				throw new Error("Failed to parse start or finish time. The specified pipeline stage might not have finished yet.");
+			}
+
+			const dateDiff = finishTime && startTime ? Math.abs(finishTime - startTime) / 1000 : undefined; // Seconds
+			console.log(`Processed stage - name='${job.name}' identifier='${job.identifier}' state='${job.state}' result='${job.result}'`);
 			return {
 				// Using the 'identifier' property because that's the one available in the API response for test results,
 				// and we want the values to be consistent so we can correlate them later.
@@ -53,18 +70,13 @@ module.exports = function handler(fileData, logger): void {
 		});
 
 	for (const job of parsedJobs) {
-		// Hardcoding the last stage name for now as it will need to be bypassed (still in Progress).
-		if (job.stageName === "runAfterAll") {
-			continue;
-		}
-
 		logger.send({
 			namespace: "FFEngineering", // Transfer the telemetry associated with pipeline status to namespace "FFEngineering".
 			category: "performance",
 			eventName: "StageTiming",
 			benchmarkType: "PipelineInfo",
 			stageName: job.stageName,
-			duration: job.totalTime,
+			duration: job.totalSeconds,
 			state: job.state,
 			result: job.result,
 			buildId: process.env.BUILD_ID ?? "",
