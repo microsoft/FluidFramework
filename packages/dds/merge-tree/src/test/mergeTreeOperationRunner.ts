7478d22e2e86e452e4c967018462a3737bac40eb
jzaffiro <110866475+jzaffiro@users.noreply.github.com>
2024-09-24 22:56:14 +0000

    Remove comments

--- packages/dds/merge-tree/src/test/mergeTreeOperationRunner.ts
+++ packages/dds/merge-tree/src/test/mergeTreeOperationRunner.ts
@@ -42,8 +42,6 @@ export const obliterateRange: TestOperation = (
 ) => {
 	let startSide: Side;
 	let endSide: Side;
-	// let startPos: number | undefined;
-	// let endPos: number | undefined;
 	if (opEnd - opStart <= 1) {
 		startSide = Side.Before;
 		endSide = Side.After;
