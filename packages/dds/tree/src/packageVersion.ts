18ef26c9f57b99c5f448a66d015f8788ccff1adc
Sonali Deshpande <48232592+sonalideshpandemsft@users.noreply.github.com>
2024-09-25 10:22:52 -0700

    [bump] client: 2.0.8 => 2.0.9 (patch) (#22606)
    Bumped client from 2.0.8 to 2.0.9.

--- packages/dds/tree/src/packageVersion.ts
+++ packages/dds/tree/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/tree";
-export const pkgVersion = "2.0.8";
+export const pkgVersion = "2.0.9";
--- packages/drivers/odsp-driver/src/packageVersion.ts
+++ packages/drivers/odsp-driver/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/odsp-driver";
-export const pkgVersion = "2.0.8";
+export const pkgVersion = "2.0.9";
