microsoft/FluidFramework

commit 990d3f8392786b315e26094ca18ed67dba25cd03

feat(sequence): re-export some merge-tree types from sequence (#18081)
Conservative re-export of merge-tree types from sequence. This is the
subset of types used by a partner codebase. It avoids re-exporting types
or functions that contain explicit references to merge-tree, except in
the case of `MergeTreeDeltaType`.

examples/data-objects/prosemirror/src/prosemirror.tsx
@@ -14,19 +14,11 @@ import {
 } from "@fluidframework/datastore";
 import { ISharedMap, SharedMap } from "@fluidframework/map";
 import {
-	IMergeTreeInsertMsg,
-	ReferenceType,
-	reservedRangeLabelsKey,
-	MergeTreeDeltaType,
-	// eslint-disable-next-line import/no-deprecated
-	createMap,
-} from "@fluidframework/merge-tree";
-import {
 	IFluidDataStoreContext,
 	IFluidDataStoreFactory,
 } from "@fluidframework/runtime-definitions";
 import { IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions";
-import { SharedString } from "@fluidframework/sequence";
+import { SharedString, ReferenceType, reservedRangeLabelsKey } from "@fluidframework/sequence";
 import { EditorView } from "prosemirror-view";
 import { create404Response } from "@fluidframework/runtime-utils";
 
@@ -35,34 +27,23 @@ import React, { useEffect, useRef } from "react";
 import { nodeTypeKey } from "./fluidBridge";
 import { FluidCollabManager, IProvideRichTextEditor } from "./fluidCollabManager";
 
-function createTreeMarkerOps(
+function insertMarkers(
+	text: SharedString,
 	treeRangeLabel: string,
 	beginMarkerPos: number,
 	endMarkerPos: number,
 	nodeType: string,
-): IMergeTreeInsertMsg[] {
-	// eslint-disable-next-line import/no-deprecated
-	const endMarkerProps = createMap<any>();
+) {
+	const endMarkerProps = {};
 	endMarkerProps[reservedRangeLabelsKey] = [treeRangeLabel];
 	endMarkerProps[nodeTypeKey] = nodeType;
 
-	// eslint-disable-next-line import/no-deprecated
-	const beginMarkerProps = createMap<any>();
+	const beginMarkerProps = {};
 	beginMarkerProps[reservedRangeLabelsKey] = [treeRangeLabel];
 	beginMarkerProps[nodeTypeKey] = nodeType;
 
-	return [
-		{
-			seg: { marker: { refType: ReferenceType.Simple }, props: beginMarkerProps },
-			pos1: beginMarkerPos,
-			type: MergeTreeDeltaType.INSERT,
-		},
-		{
-			seg: { marker: { refType: ReferenceType.Simple }, props: endMarkerProps },
-			pos1: endMarkerPos,
-			type: MergeTreeDeltaType.INSERT,
-		},
-	];
+	text.insertMarker(endMarkerPos, ReferenceType.Simple, endMarkerProps);
+	text.insertMarker(beginMarkerPos, ReferenceType.Simple, beginMarkerProps);
 }
 
 /**
@@ -112,8 +93,7 @@ export class ProseMirror extends EventEmitter implements IFluidLoadable, IProvid
 			this.root = SharedMap.create(this.runtime, "root");
 			const text = SharedString.create(this.runtime);
 
-			const ops = createTreeMarkerOps("prosemirror", 0, 1, "paragraph");
-			text.groupOperation({ ops, type: MergeTreeDeltaType.GROUP });
+			insertMarkers(text, "prosemirror", 0, 1, "paragraph");
 			text.insertText(1, "Hello, world!");
 
 			this.root.set("text", text.handle);

examples/data-objects/codemirror/src/codeMirror.ts
@@ -11,13 +11,12 @@ import {
 	mixinRequestHandler,
 } from "@fluidframework/datastore";
 import { ISharedMap, SharedMap } from "@fluidframework/map";
-import { ReferenceType, reservedTileLabelsKey } from "@fluidframework/merge-tree";
 import {
 	IFluidDataStoreContext,
 	IFluidDataStoreFactory,
 } from "@fluidframework/runtime-definitions";
 import { IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions";
-import { SharedString } from "@fluidframework/sequence";
+import { SharedString, ReferenceType, reservedTileLabelsKey } from "@fluidframework/sequence";
 import { create404Response } from "@fluidframework/runtime-utils";
 
 import { PresenceManager } from "./presence";

examples/data-objects/codemirror/src/codeMirrorView.tsx
@@ -4,13 +4,15 @@
  */
 
 import {
+	getTextAndMarkers,
+	SharedString,
+	SequenceDeltaEvent,
 	MergeTreeDeltaType,
 	TextSegment,
 	ReferenceType,
 	reservedTileLabelsKey,
 	Marker,
-} from "@fluidframework/merge-tree";
-import { getTextAndMarkers, SharedString, SequenceDeltaEvent } from "@fluidframework/sequence";
+} from "@fluidframework/sequence";
 import CodeMirror from "codemirror";
 import React, { useEffect, useRef } from "react";
 

examples/data-objects/monaco/src/view.tsx
@@ -4,8 +4,12 @@
  */
 
 // inspiration for this example taken from https://github.com/agentcooper/typescript-play
-import { MergeTreeDeltaType, TextSegment } from "@fluidframework/merge-tree";
-import { SequenceDeltaEvent, SharedString } from "@fluidframework/sequence";
+import {
+	SequenceDeltaEvent,
+	SharedString,
+	MergeTreeDeltaType,
+	TextSegment,
+} from "@fluidframework/sequence";
 import * as monaco from "monaco-editor";
 import React, { useEffect, useRef } from "react";
 

examples/data-objects/prosemirror/src/fluidBridge.ts
@@ -6,21 +6,17 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import { assert } from "@fluidframework/core-utils";
+import { createInsertSegmentOp, IMergeTreeDeltaOp } from "@fluidframework/merge-tree";
 import {
-	createInsertSegmentOp,
+	SharedString,
+	SequenceDeltaEvent,
+	ISequenceDeltaRange,
 	Marker,
 	MergeTreeDeltaType,
 	ReferenceType,
 	reservedRangeLabelsKey,
 	TextSegment,
 	ISegment,
-	IMergeTreeDeltaOp,
-} from "@fluidframework/merge-tree";
-import {
-	SharedString,
-	// ISequenceDeltaRange,
-	SequenceDeltaEvent,
-	ISequenceDeltaRange,
 } from "@fluidframework/sequence";
 import {
 	Schema,

examples/data-objects/prosemirror/src/fluidCollabManager.ts
@@ -3,19 +3,12 @@
  * Licensed under the MIT License.
  */
 
-/* eslint-disable import/no-deprecated */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import { EventEmitter } from "events";
-import {
-	createGroupOp,
-	createRemoveRangeOp,
-	Marker,
-	ReferenceType,
-	TextSegment,
-	IMergeTreeDeltaOp,
-} from "@fluidframework/merge-tree";
-import { SharedString } from "@fluidframework/sequence";
+// eslint-disable-next-line import/no-deprecated
+import { createGroupOp, createRemoveRangeOp, IMergeTreeDeltaOp } from "@fluidframework/merge-tree";
+import { SharedString, Marker, ReferenceType, TextSegment } from "@fluidframework/sequence";
 import { exampleSetup } from "prosemirror-example-setup";
 import { DOMSerializer, Schema, Slice } from "prosemirror-model";
 import { addListNodes } from "prosemirror-schema-list";
@@ -263,6 +256,7 @@ export class FluidCollabManager extends EventEmitter implements IRichTextEditor 
 						operations = operations.concat(sliceOperations);
 					}
 
+					// eslint-disable-next-line import/no-deprecated
 					const groupOp = createGroupOp(...operations);
 					this.text.groupOperation(groupOp);
 
@@ -327,6 +321,7 @@ export class FluidCollabManager extends EventEmitter implements IRichTextEditor 
 						operations = operations.concat(sliceOperations);
 					}
 
+					// eslint-disable-next-line import/no-deprecated
 					const groupOp = createGroupOp(...operations);
 					this.text.groupOperation(groupOp);
 

examples/data-objects/smde/src/smde.ts
@@ -8,13 +8,12 @@ import { assert } from "@fluidframework/core-utils";
 import { IFluidLoadable, IFluidHandle } from "@fluidframework/core-interfaces";
 import { FluidDataStoreRuntime, FluidObjectHandle } from "@fluidframework/datastore";
 import { ISharedMap, SharedMap } from "@fluidframework/map";
-import { ReferenceType, reservedTileLabelsKey } from "@fluidframework/merge-tree";
 import {
 	IFluidDataStoreContext,
 	IFluidDataStoreFactory,
 } from "@fluidframework/runtime-definitions";
 import { IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions";
-import { SharedString } from "@fluidframework/sequence";
+import { SharedString, ReferenceType, reservedTileLabelsKey } from "@fluidframework/sequence";
 
 // eslint-disable-next-line import/no-internal-modules, import/no-unassigned-import
 import "simplemde/dist/simplemde.min.css";

examples/data-objects/smde/src/smdeView.tsx
@@ -4,13 +4,13 @@
  */
 
 import {
+	getTextAndMarkers,
 	MergeTreeDeltaType,
 	TextSegment,
 	ReferenceType,
 	reservedTileLabelsKey,
 	Marker,
-} from "@fluidframework/merge-tree";
-import { getTextAndMarkers } from "@fluidframework/sequence";
+} from "@fluidframework/sequence";
 
 import React, { useEffect, useRef } from "react";
 import SimpleMDE from "simplemde";

commit 1faf667a9b9072987918c73d24bcb4e149e234fa

Fix order of marker creation in prosemirror (#22667)
The ProseMirror example doesn't work. This fixes the first error hit by
first inserting the begin marker at position 0 before inserting the end
marker at position 1 (broken in #18081).

There are still other bugs in this example that should be fixed later.

examples/data-objects/prosemirror/src/prosemirror.tsx
@@ -38,8 +38,7 @@ import { FluidCollabManager, IProvideRichTextEditor } from "./fluidCollabManager
 function insertMarkers(
 	text: SharedString,
 	treeRangeLabel: string,
-	beginMarkerPos: number,
-	endMarkerPos: number,
+	position: number,
 	nodeType: string,
 ) {
 	const endMarkerProps = {};
@@ -50,8 +49,8 @@ function insertMarkers(
 	beginMarkerProps[reservedRangeLabelsKey] = [treeRangeLabel];
 	beginMarkerProps[nodeTypeKey] = nodeType;
 
-	text.insertMarker(endMarkerPos, ReferenceType.Simple, endMarkerProps);
-	text.insertMarker(beginMarkerPos, ReferenceType.Simple, beginMarkerProps);
+	text.insertMarker(position, ReferenceType.Simple, beginMarkerProps);
+	text.insertMarker(position + 1, ReferenceType.Simple, endMarkerProps);
 }
 
 /**
@@ -105,7 +104,7 @@ export class ProseMirror
 			this.root = SharedMap.create(this.runtime, "root");
 			const text = SharedString.create(this.runtime);
 
-			insertMarkers(text, "prosemirror", 0, 1, "paragraph");
+			insertMarkers(text, "prosemirror", 0, "paragraph");
 			text.insertText(1, "Hello, world!");
 
 			this.root.set("text", text.handle);