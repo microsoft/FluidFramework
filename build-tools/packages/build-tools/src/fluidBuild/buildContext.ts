e75fba6934a9d7c471c8ee6cf6d1c557dede736f
Tyler Butler <tylerbu@microsoft.com>
2024-09-27 12:25:32 -0700

    docs

--- build-tools/packages/build-tools/src/fluidBuild/buildContext.ts
+++ build-tools/packages/build-tools/src/fluidBuild/buildContext.ts
@@ -6,8 +6,23 @@
 import type { GitRepo } from "../common/gitRepo";
 import type { IFluidBuildConfig } from "./fluidBuildConfig";
 
+/**
+ * A context object that is passed to fluid-build tasks. It is used to provide easy access to commonly-needed metadata
+ * or tools.
+ */
 export interface BuildContext {
+	/**
+	 * The fluid-build configuration for the repo.
+	 */
 	fluidBuildConfig: IFluidBuildConfig | undefined;
+
+	/**
+	 * The absolute path to the root of the repo.
+	 */
 	repoRoot: string;
+
+	/**
+	 * A GitRepo object that can be used to call git operations.
+	 */
 	gitRepo: GitRepo;
 }
