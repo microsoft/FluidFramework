5c34ed68cefde070cb3441bb470ed851cbefd9e1
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-09-29 00:46:32 +0000

    fix: Correct application of formatting

--- tools/api-markdown-documenter/src/documentation-domain-to-html/default-transformations/PlainTextToHtml.ts
+++ tools/api-markdown-documenter/src/documentation-domain-to-html/default-transformations/PlainTextToHtml.ts
@@ -9,6 +9,7 @@ import { removePosition } from "unist-util-remove-position";
 import type { Nodes as HastNodes } from "hast";
 import type { PlainTextNode } from "../../documentation-domain/index.js";
 import type { TransformationContext } from "../TransformationContext.js";
+import { applyFormatting } from "./Utilities.js";
 
 /**
  * Transform a {@link PlainTextNode} to HTML.
@@ -17,8 +18,9 @@ import type { TransformationContext } from "../TransformationContext.js";
  * @param context - See {@link TransformationContext}.
  */
 export function plainTextToHtml(node: PlainTextNode, context: TransformationContext): HastNodes {
+	let transformed: HastNodes;
 	if (node.escaped) {
-		const parsed = fromHtml(node.text, { fragment: true, verbose: false });
+		transformed = fromHtml(node.text, { fragment: true, verbose: false });
 
 		// `fromHtml` currently includes position data in its output, despite the `verbose: false` option, which is supposed to disable this.
 		// See <https://github.com/syntax-tree/hast-util-from-html/issues/7>
@@ -30,12 +32,12 @@ export function plainTextToHtml(node: PlainTextNode, context: TransformationCont
 
 		// `fromHtml` also adds a `data` property to the root node, which we don't need.
 		delete parsed.data;
-
-		return parsed;
 	} else {
-		return {
+		transformed = {
 			type: "text",
 			value: node.text,
 		};
 	}
+
+	return applyFormatting(transformed, context);
 }
