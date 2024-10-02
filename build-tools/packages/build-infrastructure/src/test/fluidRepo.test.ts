7f19dc3c54d976aac2de3a822ae21e1f9b8c636f
Tyler Butler <tylerbu@microsoft.com>
2024-10-01 18:22:39 -0700

    test repo loading on the main repo too

--- build-tools/packages/build-infrastructure/src/test/fluidRepo.test.ts
+++ build-tools/packages/build-infrastructure/src/test/fluidRepo.test.ts
@@ -12,36 +12,76 @@ import { loadFluidRepo } from "../fluidRepo.js";
 import type { WorkspaceName } from "../types.js";
 // import { findGitRoot } from "../utils.js";
 import { testDataPath } from "./init.js";
+import { findGitRoot } from "../utils.js";
 
 describe("loadFluidRepo", () => {
-	it("loads correctly", () => {
-		const repo = loadFluidRepo(path.join(testDataPath, "./testRepo"));
-		assert.strictEqual(
-			repo.workspaces.size,
-			2,
-			`Expected 2 workspaces, found ${repo.workspaces.size}`,
-		);
+	describe("testRepo", () => {
+		it("loads correctly", () => {
+			const repo = loadFluidRepo(path.join(testDataPath, "./testRepo"));
+			assert.strictEqual(
+				repo.workspaces.size,
+				2,
+				`Expected 2 workspaces, found ${repo.workspaces.size}`,
+			);
 
-		const client = repo.workspaces.get("main" as WorkspaceName);
-		expect(client).to.not.be.undefined;
-		expect(client?.packages.length).to.equal(
-			4,
-			"main workspace has the wrong number of packages",
-		);
-		expect(client?.releaseGroups.size).to.equal(
-			1,
-			"main workspace has the wrong number of release groups",
-		);
+			const client = repo.workspaces.get("main" as WorkspaceName);
+			expect(client).to.not.be.undefined;
+			expect(client?.packages.length).to.equal(
+				4,
+				"main workspace has the wrong number of packages",
+			);
+			expect(client?.releaseGroups.size).to.equal(
+				1,
+				"main workspace has the wrong number of release groups",
+			);
 
-		const second = repo.workspaces.get("second" as WorkspaceName);
-		expect(second).to.not.be.undefined;
-		expect(second?.packages.length).to.equal(
-			3,
-			"second workspace has the wrong number of packages",
-		);
-		expect(second?.releaseGroups.size).to.equal(
-			1,
-			"second workspace has the wrong number of release groups",
-		);
+			const second = repo.workspaces.get("second" as WorkspaceName);
+			expect(second).to.not.be.undefined;
+			expect(second?.packages.length).to.equal(
+				3,
+				"second workspace has the wrong number of packages",
+			);
+			expect(second?.releaseGroups.size).to.equal(
+				1,
+				"second workspace has the wrong number of release groups",
+			);
+		});
+	});
+
+	describe("FluidFramework repo", () => {
+	describe("loadFluidRepo", () => {
+		it("loads correctly", () => {
+			// Load the root config
+			const repo = loadFluidRepo(findGitRoot());
+			assert.strictEqual(
+				repo.workspaces.size,
+				14,
+				`Expected 14 workspaces, found ${repo.workspaces.size}`,
+			);
+
+			const client = repo.workspaces.get("client" as WorkspaceName);
+			expect(client).to.not.be.undefined;
+			expect(client?.packages.length).to.equal(
+				153,
+				"client workspace has the wrong number of packages",
+			);
+			expect(client?.releaseGroups.size).to.equal(
+				1,
+				"client workspace has the wrong number of release groups",
+			);
+
+			const buildTools = repo.workspaces.get("build-tools" as WorkspaceName);
+			expect(buildTools).to.not.be.undefined;
+			expect(buildTools?.packages.length).to.equal(
+				6,
+				"build-tools workspace has the wrong number of packages",
+			);
+			expect(buildTools?.releaseGroups.size).to.equal(
+				1,
+				"build-tools workspace has the wrong number of release groups",
+			);
+		});
+	});
+
 	});
 });
