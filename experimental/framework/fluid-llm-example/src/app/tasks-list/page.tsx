a092eb767d00a745bfce9a790c0007680c4e14c8
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2024-10-03 15:44:52 +0000

    Move SPE code to separate file

--- experimental/framework/fluid-llm-example/src/app/tasks-list/page.tsx
+++ experimental/framework/fluid-llm-example/src/app/tasks-list/page.tsx
@@ -7,8 +7,6 @@
 
 import {
 	SharedTreeAppState,
-	INITIAL_APP_STATE,
-	CONTAINER_SCHEMA,
 	TREE_CONFIGURATION,
 	type SharedTreeTaskGroup,
 } from "@/types/sharedTreeAppSchema";
@@ -27,35 +25,7 @@ import { TaskGroup } from "@/components/TaskGroup";
 import { type TreeView } from "@fluidframework/tree";
 import { useSharedTreeRerender } from "@/useSharedTreeRerender";
 import { useFluidContainerNextJs } from "@/useFluidContainerNextjs";
-import { start } from "@/infra/authHelper";
-import type { IFluidContainer } from "@fluidframework/fluid-static";
-
-const { client, getShareLink, containerId } = await start();
-
-async function loadContainer(
-	id: string,
-): Promise<IFluidContainer<typeof CONTAINER_SCHEMA>> {
-	console.log(`Loading container with id '${id}'`);
-	const res = await client.getContainer(id, CONTAINER_SCHEMA);
-	return res.container;
-}
-
-export async function createAndInitializeContainer(): Promise<IFluidContainer<typeof CONTAINER_SCHEMA>> {
-	console.log("Creating a new container");
-
-	const { container } = await client.createContainer(CONTAINER_SCHEMA);
-	const treeView = container.initialObjects.appState.viewWith(TREE_CONFIGURATION);
-	treeView.initialize(new SharedTreeAppState(INITIAL_APP_STATE));
-	treeView.dispose(); // After initializing, dispose the tree view so later loading of the data can work correctly
-	return container;
-}
-
-async function postAttach(containerId: string, container: IFluidContainer<typeof CONTAINER_SCHEMA>) {
-	// Create a sharing id to the container and set it in the URL hash.
-	// This allows the user to collaborate on the same Fluid container with other users just by sharing the link.
-	const shareId = await getShareLink(containerId);
-	history.replaceState(undefined, "", "#" + shareId);
-}
+import { createAndInitializeContainer, loadContainer, postAttach, containerIdFromUrl } from "./spe";
 
 export default function TasksListPage() {
 	const [selectedTaskGroup, setSelectedTaskGroup] = useState<SharedTreeTaskGroup>();
@@ -63,7 +33,7 @@ export default function TasksListPage() {
 		useState<TreeView<typeof SharedTreeAppState>>();
 
 	const { container, isFluidInitialized, data } = useFluidContainerNextJs(
-		containerId,
+		containerIdFromUrl(),
 		createAndInitializeContainer,
 		postAttach,
 		loadContainer,
