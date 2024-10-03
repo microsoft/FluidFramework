d74bed44e2b934448aec46b943294c0333bc8360
Tyler Butler <tylerbu@microsoft.com>
2024-10-02 17:23:00 -0700

    test repo policy updates

--- build-tools/packages/build-infrastructure/src/errors.ts
+++ build-tools/packages/build-infrastructure/src/errors.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 export class NotInGitRepository extends Error {
 	constructor(public readonly path: string) {
 		super(`Path is not in a Git repository: ${path}`);
