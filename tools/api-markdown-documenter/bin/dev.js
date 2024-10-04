7fd6247363715264ceb06a3b67e7307f32f00802
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-10-04 00:11:36 +0000

    fix

--- tools/api-markdown-documenter/bin/dev.js
+++ tools/api-markdown-documenter/bin/dev.js
@@ -1,9 +1,8 @@
+#!/usr/bin/env -S node --loader ts-node/esm --disable-warning=ExperimentalWarning
 /*!
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-
-#!/usr/bin/env -S node --loader ts-node/esm --disable-warning=ExperimentalWarning
 
 // eslint-disable-next-line n/shebang
 import { execute } from "@oclif/core";
--- tools/api-markdown-documenter/bin/run.js
+++ tools/api-markdown-documenter/bin/run.js
@@ -1,9 +1,8 @@
+#!/usr/bin/env node
 /*!
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-
-#!/usr/bin/env node
 
 import { execute } from "@oclif/core";
 
