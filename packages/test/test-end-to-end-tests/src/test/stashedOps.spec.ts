37c0f2d1c903f12ba6a83e26de2a664f025794aa
Mark Fields <markfields@users.noreply.github.com>
2024-10-10 18:55:39 -0700

    Flakey Test: Fix possible race condition (#22776)
    The test `Parallel Forks: Closes (ForkedContainerError and
    DuplicateBatchError) when hydrating twice and submitting in parallel
    (via Counter DDS)` has had some recent failures when running against
    t9s.
    
    Reading through the test again, the only idea I have so far is that
    maybe one of the containers manages to submit the stashed ops before it
    can pause the outbound queue after creation.
    
    This change aims to close that gap.

--- packages/test/test-end-to-end-tests/src/test/stashedOps.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/stashedOps.spec.ts
@@ -12,6 +12,7 @@ import {
 	IContainer,
 	IHostLoader,
 	LoaderHeader,
+	type ILoaderHeader,
 } from "@fluidframework/container-definitions/internal";
 import { ConnectionState } from "@fluidframework/container-loader";
 import { IContainerExperimental } from "@fluidframework/container-loader/internal";
@@ -2211,8 +2212,17 @@ describeCompat(
 				async function rehydrateConnectAndPause(loggingId: string) {
 					// Rehydrate and immediately pause outbound to ensure we don't send the ops yet
 					// Container won't be connected yet, so no race here.
-					const container = await loader.resolve({ url }, pendingLocalState);
+					const container = await loader.resolve(
+						{
+							url,
+							headers: {
+								[LoaderHeader.loadMode]: { deltaConnection: "none" },
+							} satisfies Partial<ILoaderHeader>,
+						},
+						pendingLocalState,
+					);
 					await container.deltaManager.outbound.pause();
+					container.connect();
 
 					// Wait for the container to connect, and then pause the inbound queue
 					// This order matters - we need to process our inbound join op to finish connecting!
