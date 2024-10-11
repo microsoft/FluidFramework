6e7e4e0396c121a5c31a5fa1eadd103c341eaabd
Josmithr <josmithr@microsoft.com>
2024-10-11 15:11:45 -0700

    style: Space

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -52,6 +52,7 @@ const config: Config = {
 
 	// Set the production url of your site here
 	url: "https://fluidframework.com/",
+
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: "/",
