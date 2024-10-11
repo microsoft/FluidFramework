befc9872538212f23cc40695e5eba317b03860a3
Mark Fields <markfields@users.noreply.github.com>
2024-10-11 08:05:36 +0000

    Add simple op submit and roundtrip benchmarks

---
+++ packages/test/test-end-to-end-tests/src/test/benchmark/opCriticalPath.time.spec.ts
@@ -0,0 +1,167 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "assert";
+
+import { ITestDataObject, describeCompat } from "@fluid-private/test-version-utils";
+import { benchmark } from "@fluid-tools/benchmark";
+import { IContainer } from "@fluidframework/container-definitions/internal";
+import {
+	ContainerRuntime,
+	DefaultSummaryConfiguration,
+} from "@fluidframework/container-runtime/internal";
+import {
+	ITestContainerConfig,
+	ITestObjectProvider,
+	timeoutPromise,
+} from "@fluidframework/test-utils/internal";
+
+const testContainerConfig: ITestContainerConfig = {
+	runtimeOptions: {
+		enableGroupedBatching: true,
+		summaryOptions: {
+			initialSummarizerDelayMs: 0, // back-compat - Old runtime takes 5 seconds to start summarizer without thi
+			summaryConfigOverrides: {
+				...DefaultSummaryConfiguration,
+				...{ maxOps: 10, initialSummarizerDelayMs: 0, minIdleTime: 10, maxIdleTime: 10 },
+			},
+		},
+	},
+};
+
+type Patch<T, U> = Omit<T, keyof U> & U;
+
+type ContainerRuntime_WithPrivates = Patch<ContainerRuntime, { flush: () => void }>;
+
+describeCompat.only(
+	"Op Critical Paths - runtime benchmarks",
+	"NoCompat",
+	(getTestObjectProvider) => {
+		let provider: ITestObjectProvider;
+		let mainContainer: IContainer;
+		let defaultDataStore: ITestDataObject;
+		let containerRuntime: ContainerRuntime_WithPrivates;
+
+		//* no worky
+		// before(async () => {
+		// 	provider = getTestObjectProvider();
+		// 	const loader = provider.makeTestLoader(testContainerConfig);
+		// 	mainContainer = await loader.createDetachedContainer(provider.defaultCodeDetails);
+
+		// 	await mainContainer.attach(provider.driver.createCreateNewRequest());
+		// 	defaultDataStore = (await mainContainer.getEntryPoint()) as ITestDataObject;
+		// 	containerRuntime = defaultDataStore._context
+		// 		.containerRuntime as ContainerRuntime_WithPrivates;
+
+		// 	defaultDataStore._root.set("force", "write connection");
+		// 	await provider.ensureSynchronized();
+		// });
+
+		const before = async () => {
+			provider = getTestObjectProvider();
+			const loader = provider.makeTestLoader(testContainerConfig);
+			mainContainer = await loader.createDetachedContainer(provider.defaultCodeDetails);
+
+			await mainContainer.attach(provider.driver.createCreateNewRequest());
+			defaultDataStore = (await mainContainer.getEntryPoint()) as ITestDataObject;
+			containerRuntime = defaultDataStore._context
+				.containerRuntime as ContainerRuntime_WithPrivates;
+
+			defaultDataStore._root.set("force", "write connection");
+			await provider.ensureSynchronized();
+		};
+		function sendOps(label: string) {
+			Array.from({ length: 100 }).forEach((_, i) => {
+				defaultDataStore._root.set(`key-${i}`, `value-${label}`);
+			});
+
+			//* Waste time to see how the numbers work out
+			const x: number[] = [];
+			Array.from({ length: 0 }).forEach((_, a) => {
+				x[a] = a;
+			});
+
+			containerRuntime.flush();
+		}
+
+		benchmark({
+			title: "Submit+Flush",
+			before, // Set up container for a write connection
+			benchmarkFnAsync: async () => {
+				sendOps("A");
+				const opsSent = await timeoutPromise<number>(
+					(resolve) => {
+						containerRuntime.deltaManager.outbound.once("idle", resolve);
+					},
+					{ errorMsg: "container2 outbound queue never reached idle state" },
+				);
+				assert(opsSent > 0, "Expecting op(s) to be sent.");
+			},
+		});
+
+		//* TIL: beforeEach doesn't happen before each iteration
+		// describe("Process", () => {
+		// 	beforeEach(async () => {
+		// 		await mainContainer.deltaManager.outbound.pause();
+		// 		sendOps();
+		// 	});
+		// 	benchmark({
+		// 		title: "Process",
+		// 		benchmarkFnAsync: async () => {
+		// 			mainContainer.deltaManager.outbound.resume();
+		// 			await provider.ensureSynchronized();
+		// 		},
+		// 	});
+		// });
+	},
+);
+
+describeCompat.only(
+	"Op Critical Paths - runtime benchmarks",
+	"NoCompat",
+	(getTestObjectProvider) => {
+		let provider: ITestObjectProvider;
+		let mainContainer: IContainer;
+		let defaultDataStore: ITestDataObject;
+		let containerRuntime: ContainerRuntime_WithPrivates;
+
+		const before = async () => {
+			provider = getTestObjectProvider();
+			const loader = provider.makeTestLoader(testContainerConfig);
+			mainContainer = await loader.createDetachedContainer(provider.defaultCodeDetails);
+
+			await mainContainer.attach(provider.driver.createCreateNewRequest());
+			defaultDataStore = (await mainContainer.getEntryPoint()) as ITestDataObject;
+			containerRuntime = defaultDataStore._context
+				.containerRuntime as ContainerRuntime_WithPrivates;
+
+			defaultDataStore._root.set("force", "write connection");
+			await provider.ensureSynchronized();
+		};
+
+		function sendOps(label: string) {
+			Array.from({ length: 100 }).forEach((_, i) => {
+				defaultDataStore._root.set(`key-${i}`, `value-${label}`);
+			});
+
+			//* Waste time to see how the numbers work out
+			const x: number[] = [];
+			Array.from({ length: 0 }).forEach((_, a) => {
+				x[a] = a;
+			});
+
+			containerRuntime.flush();
+		}
+
+		benchmark({
+			title: "Roundtrip",
+			before, // Set up container for a write connection
+			benchmarkFnAsync: async () => {
+				sendOps("B");
+				await provider.ensureSynchronized();
+			},
+		});
+	},
+);
