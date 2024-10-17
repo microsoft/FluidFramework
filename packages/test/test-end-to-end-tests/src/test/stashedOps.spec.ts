a6e1efe9daf9c148b7cee1c4efb81266d07424c9
jzaffiro <110866475+jzaffiro@users.noreply.github.com>
2024-10-17 13:33:07 -0700

    Disable parallel forks flaky test  (#22837)
    This test is known to be flaky, and causing noise for OCE. Disabling for
    now, until a fix can be published.
    
    
    [AB#14900](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/14900),
    [AB#20297](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/20297)

--- packages/test/test-end-to-end-tests/src/test/stashedOps.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/stashedOps.spec.ts
@@ -2182,6 +2182,11 @@ describeCompat(
 				},
 			],
 			async function () {
+				// AB#14900, 20297: this test is extremely flaky on Tinylicious and causing noise.
+				// Skip it for now until above items are resolved.
+				if (provider.driver.type === "tinylicious" || provider.driver.type === "t9s") {
+					this.skip();
+				}
 				const incrementValue = 3;
 				const pendingLocalState = await getPendingOps(
 					testContainerConfig_noSummarizer,
