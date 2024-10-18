5e2da02cc65850d48872b4892456de8b62506379
Josmithr <josmithr@microsoft.com>
2024-10-18 11:03:36 -0700

    WIP: How it works

--- docs/src/components/howItWorksSection.tsx
+++ docs/src/components/howItWorksSection.tsx
@@ -3,14 +3,55 @@
  * Licensed under the MIT License.
  */
 
+import React from "react";
+
 import { HomePageSection } from '@site/src/components/homePageSection';
 
 import "@site/src/css/howItWorksSection.css";
 
-export function HowItWorksSection(): JSX.Element {
+export function HowItWorksSection(): React.ReactElement {
 	return <HomePageSection title="See how it works" subtitle="Open Source">
-		<div>
-			TODO
+		<div className="howItWorksSectionBody">
+			<div className="howItWorksSectionCodeBody">
+				<div className="howItWorksCodeColumn">
+					<div className="howItWorksCodeColumnLabel">
+						Sample Code
+					</div>
+					<div className="howItWorksCodeCard">
+						<div className="howItWorksCodeCardFrame">
+							<div className="howItWorksCodeCardBody">
+								<p className="howItWorksCodeCardText">
+									Code
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="howItWorksCodeColumn">
+					<div className="howItWorksCodeColumnLabel">
+						Sample Output
+					</div>
+					<div className="howItWorksCodeCard">
+						<div className="howItWorksCodeCardFrame">
+							<div className="howItWorksCodeCardBody">
+								Foo
+							</div>
+						</div>
+					</div>
+					<div className="howItWorksCodeCard">
+						<div className="howItWorksCodeCardFrame">
+							<div className="howItWorksCodeCardBody">
+								Bar
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="howItWorksTryOtherSamplesButton">
+				<div className="howItWorksTryOtherSamplesButtonFrame">
+					<label className="howItWorksTryOtherSamplesButtonLabel">Try the other samples</label>
+				</div>
+			</div>
 		</div>
 	</HomePageSection>;
 }
