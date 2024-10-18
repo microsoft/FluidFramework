ed8a3790aa358f59f430a70dd45a56227c4cde89
Josmithr <josmithr@microsoft.com>
2024-10-18 16:31:44 -0700

    feat: Add services diagram

--- docs/src/components/serviceSection.tsx
+++ docs/src/components/serviceSection.tsx
@@ -5,10 +5,15 @@
 
 import { HomePageSection } from '@site/src/components/homePageSection';
 
+import Diagram from '@site/static/assets/services-diagram.svg';
+
 import "@site/src/css/serviceSection.css";
 
 export function ServiceSection(): JSX.Element {
 	return <HomePageSection title="Fluid Framework in the Cloud">
+		<div>
+			<Diagram />
+		</div>
 		<div>
 			TODO
 		</div>
