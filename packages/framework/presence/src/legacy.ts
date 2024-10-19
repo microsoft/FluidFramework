06834a9314c4288bc290c7cbaac60c1e560ec617
Jason Hartman <jasonha@microsoft.com>
2024-10-18 18:21:28 -0700

    build(client-presence): add legacy API reports and checks

--- packages/framework/presence/src/legacy.ts
+++ packages/framework/presence/src/legacy.ts
@@ -3,6 +3,14 @@
  * Licensed under the MIT License.
  */
 
+/**
+ * Experimental package for client presence within a connected session.
+ *
+ * See {@link https://github.com/microsoft/FluidFramework/tree/main/packages/framework/presence#readme | README.md } for an overview of the package.
+ *
+ * @packageDocumentation
+ */
+
 // eslint-disable-next-line no-restricted-syntax
 export * from "./index.js";
 
