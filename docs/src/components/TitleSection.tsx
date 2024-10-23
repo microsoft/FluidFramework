a82686e42fb83defa7336813dcf6b85ca86463a5
Josmithr <josmithr@microsoft.com>
2024-10-23 13:43:04 -0700

    refactor: Simplify title section styling

--- docs/src/components/TitleSection.tsx
+++ docs/src/components/TitleSection.tsx
@@ -9,22 +9,20 @@ import "@site/src/css/titleSection.css";
 
 export function TitleSection(): React.ReactElement {
   return (
-    <div className="rootContainer">
-      <div className={"titleSectionContainer"}>
-        <div className="overlay"/>
-        <div className="contentContainer">
-            <div className="titleBox">
-              <h3 className="title">Fluid Framework</h3>
-              <span className="description">Empower collaborative innovation with Fluid Framework's seamless, high-performance tech stack for real-time applications.
-              </span>
-            </div>
-            <div className="videoContainer">
-              <div className="roundedVideo">
-                <iframe width="100%" height="100%"  src="https://www.youtube.com/embed/uL2nMYk6WTQ" title="Fluid Framework 2.0 Beta - Build collaborative apps fast!"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-              </div>
+    <div className={"titleSectionContainer"}>
+      <div className="overlay"/>
+      <div className="contentContainer">
+          <div className="titleBox">
+            <h3 className="title">Fluid Framework</h3>
+            <span className="description">Empower collaborative innovation with Fluid Framework's seamless, high-performance tech stack for real-time applications.
+            </span>
+          </div>
+          <div className="videoContainer">
+            <div className="roundedVideo">
+              <iframe width="100%" height="100%"  src="https://www.youtube.com/embed/uL2nMYk6WTQ" title="Fluid Framework 2.0 Beta - Build collaborative apps fast!"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             </div>
           </div>
-      </div>
+        </div>
     </div>
   )
 }
