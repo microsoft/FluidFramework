777bd8c628f194d79ebfb1b62a1915b7552ed7b0
Dhruv Verma <dhruvverma@microsoft.com>
2024-10-23 14:31:06 -0700

    Added copyright

--- server/routerlicious/packages/routerlicious-base/src/alfred/services/fluidAccessTokenGenerator.ts
+++ server/routerlicious/packages/routerlicious-base/src/alfred/services/fluidAccessTokenGenerator.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { NetworkError } from "@fluidframework/server-services-client";
 import type {
 	IFluidAccessTokenGenerator,
