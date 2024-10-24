80a98b12c8b37558144c3d0acf0adb1bb8c63cb1
Josmithr <josmithr@microsoft.com>
2024-10-24 16:13:08 -0700

    feat: Update dice roller sample in v2 docs

---
+++ docs/src/components/card.tsx
@@ -0,0 +1,18 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import React from "react";
+
+import "@site/src/css/card.css";
+
+export type CardWithBlurProps = React.PropsWithChildren<{
+	// TODO: custom props as needed
+}>;
+
+export function CardWithBlur({children}: CardWithBlurProps): React.ReactElement {
+	return <div className="ffcom-card-with-blur">
+		{children}
+	</div>
+}
