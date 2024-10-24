32156bd28b6edf4b8e4580ec62760ecb380d5787
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2024-10-24 14:28:01 -0700

    update todo

--- packages/dds/tree/src/simple-tree/api/tree.ts
+++ packages/dds/tree/src/simple-tree/api/tree.ts
@@ -69,9 +69,11 @@ export interface ViewableTree {
 	): TreeView<TRoot>;
 
 	// TODO:
-	// Add stored key versions of Tree.exportVerbose, Tree.exportConcise and Tree.exportCompressed here.
-	// Add exportSimpleSchema and exportJsonSchema which constraints the concise format.
-	// Ensure schema exporting APIs here aline and reference APis for exporting view schema to the same formats (which should include stored vs property key choice)
+	// Add stored key versions of Tree.exportVerbose, Tree.exportConcise and Tree.exportCompressed here so tree content can be accessed without a view schema.
+	// Add exportSimpleSchema and exportJsonSchema methods (which should exactly match the concise format, and match the free functions for exporting view schema).
+	// Maybe rename "exportJsonSchema" to align on "concise" terminology.
+	// Ensure schema exporting APIs here aline and reference APIs for exporting view schema to the same formats (which should include stored vs property key choice).
+	// Make sure users of independentView can use these export APIs (maybe provide a reference back to the ViewableTree from the TreeView to accomplish that).
 }
 
 /**
