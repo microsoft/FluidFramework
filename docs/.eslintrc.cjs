d757496f60f3e22db5f015bdf1e019270c6e5974
Josmithr <josmithr@microsoft.com>
2024-10-25 15:20:38 -0700

    tools: Add eslint dependencies and update deps structure

---
+++ docs/.eslintrc.cjs
@@ -0,0 +1,23 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = {
+	extends: [
+		require.resolve("@fluidframework/eslint-config-fluid/minimal-deprecated"),
+		"prettier",
+	],
+	parserOptions: {
+		project: ["./tsconfig.json"],
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+	rules: {
+		// Required by Docusaurus for certain component exports.
+		"import/no-default-export": "off",
+	}
+};
