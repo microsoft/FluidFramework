5bbcabe1c5e4f6e9b9a55e989ddb2f67d3dd52d4
Josmithr <josmithr@microsoft.com>
2024-10-25 16:20:26 -0700

    tools: Update eslint and tsconfig settings

--- docs/.eslintrc.cjs
+++ docs/.eslintrc.cjs
@@ -19,5 +19,22 @@ module.exports = {
 	rules: {
 		// Required by Docusaurus for certain component exports.
 		"import/no-default-export": "off",
+
+		"import/no-unassigned-import": ["error", {
+			// Allow unassigned imports of css files.
+			allow: ["**/*.css"],
+		}],
+
+		"import/no-internal-modules": ["error", {
+			allow: [
+				"@docusaurus/**",
+				"@site/**",
+				"@theme/**",
+			],
+		}],
+
+		"import/no-unresolved": ["error", {
+			ignore: ["^@docusaurus/", "^@theme/", "^@theme-original/"],
+		}]
 	}
 };
