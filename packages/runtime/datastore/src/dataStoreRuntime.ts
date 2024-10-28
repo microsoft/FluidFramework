c5b3581d4e1b0755dec8350b985dc19f4065f90f
Tony Murphy <anthonm@microsoft.com>
2024-10-28 15:55:52 -0700

    more comments

--- packages/runtime/datastore/src/dataStoreRuntime.ts
+++ packages/runtime/datastore/src/dataStoreRuntime.ts
@@ -358,10 +358,15 @@ export class FluidDataStoreRuntime
 	public async request(request: IRequest): Promise<IResponse> {
 		try {
 			const parser = RequestParser.create(request);
-			// if there are not path parts, and the request is via a handle
+			// If there are not path parts, and the request is via a handle
 			// then we should return the entrypoint object for this runtime.
-			// this allows the entrypoint handle to be resolved without the need
+			// This allows the entrypoint handle to be resolved without the need
 			// for the entrypoint object to know anything about requests or handles.
+			//
+			// This works because the entrypoint handle is a fluid object handle,
+			// which always has a real reference to the object itself.
+			// Those get serialized and then deserialized into a plain handle, which really just has a path,
+			// resolution walks to the runtime, which calls this, and get the true object off the internal fluid object handle
 			if (parser.pathParts.length === 0 && request.headers?.viaHandle === true) {
 				return { mimeType: "fluid/object", status: 200, value: await this.entryPoint.get() };
 			}
