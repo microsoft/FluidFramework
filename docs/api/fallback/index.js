e4cd849f0c8f94396fc2e1c64b2b2fb88ca6307e
Josmithr <josmithr@microsoft.com>
2024-10-29 17:21:03 -0700

    refactor: Simplify fallback infra while testing

--- docs/api/fallback/index.js
+++ docs/api/fallback/index.js
@@ -3,7 +3,33 @@
  * Licensed under the MIT License.
  */
 
-import { routes } from "./routes.js";
+// const routes = require("./routes.js");
+
+const routes = [
+	// We previously served the API docs out of `docs/apis`.
+	// Forward to current version of the API docs (`docs/api`).
+	{ from: "/docs/apis", to: "/docs/api" },
+
+	// We previously only versioned our API documentation, where now we version everything.
+	// Forward versioned API paths to the new hierarchy.
+	{ from: "/docs/api/v1", to: "/docs/v1/api" },
+	{ from: "/docs/api/v2", to: "/docs/api" },
+
+	// We previously supported a special path alias for accessing the "current" API docs.
+	// Docusaurus handles this automatically for us, but we still need to support the old pattern.
+	// Forward to current version of the API docs (`docs/api`).
+	{ from: "/docs/api/current", to: "/docs/api" },
+
+	// We previously supported a special path alias for accessing the "lts" version API docs.
+	// Forward to the v1 API docs (`docs/api/v1`).
+	{ from: "/docs/api/lts", to: "/docs/v1/api" },
+
+	// Docusaurus serves the "current" version of the docs from the root path.
+	// If the user explicitly navigates to "v2", we should support that.
+	{ from: "/docs/v2", to: "/docs" },
+
+	// TODO: anything else?
+];
 
 /**
  * Handles incoming HTTP requests and redirects them to the appropriate URL based on the current and LTS versions.
@@ -14,8 +40,6 @@ module.exports = async (context, { headers }) => {
 	const { pathname, search } = new URL(headers["x-ms-original-url"]);
 	const route = routes.find(({ from }) => pathname.startsWith(from));
 
-	console.log(`Incoming request: ${pathname}`);
-
 	if (route === undefined) {
 		context.res = {
 			status: 404,
@@ -23,7 +47,6 @@ module.exports = async (context, { headers }) => {
 		};
 	} else {
 		const redirectLocation = `${pathname.replace(route.from, route.to)}${search}`;
-		console.log(`Redirecting from ${pathname} to ${redirectLocation}!`);
 		context.res = {
 			status: 302,
 			headers: { location: redirectLocation },
