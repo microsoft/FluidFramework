e13c9f2c5e5be49a69a8a367ed5faf2e7e0815a1
Sonali Deshpande <48232592+sonalideshpandemsft@users.noreply.github.com>
2024-11-04 04:24:20 +0000

    add test-version-utils deps

--- packages/service-clients/end-to-end-tests/odsp-client/src/test/.mocharc.cjs
+++ packages/service-clients/end-to-end-tests/odsp-client/src/test/.mocharc.cjs
@@ -8,28 +8,5 @@
 const packageDir = __dirname;
 const getFluidTestMochaConfig = require("@fluid-private/test-version-utils/mocharc-common");
 const config = getFluidTestMochaConfig(packageDir);
-
-// const getFluidTestMochaConfig = require("@fluid-internal/mocha-test-setup/mocharc-common");
-
-// const args = process.argv.slice(2);
-
-// function getFluidTestVariant() {
-// 	const driverIndex = args.indexOf("--driver");
-// 	const endpointIndex = args.indexOf("--odspEndpointName");
-
-// 	const testDriver = driverIndex !== -1 ? args[driverIndex + 1] : "";
-// 	const endpointName = endpointIndex !== -1 ? args[endpointIndex + 1] : "";
-
-// 	return `${testDriver}-${endpointName}`;
-// }
-
-// function getFluidTestMocha(packageDir, additionalRequiredModules = []) {
-// 	const testVariant = getFluidTestVariant();
-// 	process.env.FLUID_TEST_VARIANT = testVariant;
-
-// 	return getFluidTestMochaConfig(packageDir, additionalRequiredModules, testVariant);
-// }
-
-// module.exports = getFluidTestMocha(packageDir);
-
 module.exports = config;
+
