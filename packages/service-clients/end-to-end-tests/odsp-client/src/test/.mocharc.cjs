aefaf8d4c221dd042cc785357068e56645e79321
Sonali Deshpande <48232592+sonalideshpandemsft@users.noreply.github.com>
2024-11-05 18:32:09 +0000

    Fix odsp client script

--- packages/service-clients/end-to-end-tests/odsp-client/src/test/.mocharc.cjs
+++ packages/service-clients/end-to-end-tests/odsp-client/src/test/.mocharc.cjs
@@ -8,4 +8,13 @@
 const packageDir = __dirname;
 const getFluidTestMochaConfig = require("@fluid-private/test-version-utils/mocharc-common");
 const config = getFluidTestMochaConfig(packageDir);
+
+const args = process.argv.slice(2);
+
+const driverIndex = args.indexOf("--driver");
+const endpointIndex = args.indexOf("--odspEndpointName");
+
+// Data_driverEndpointName
+process.env.FLUID_ENDPOINTNAME = endpointIndex;
+
 module.exports = config;
