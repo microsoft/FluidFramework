5f5aa4b6cce474ae33565fc74f95ddecac6b60a6
Tyler Butler <tylerbu@microsoft.com>
2024-11-06 17:50:42 -0800

    cleanup

--- examples/apps/presence-tracker/src/presence.ts
+++ examples/apps/presence-tracker/src/presence.ts
@@ -17,27 +17,7 @@ export interface IFocusState {
 const initialPosition: IMousePosition = { x: 0, y: 0 };
 const initialFocus: IFocusState = { focused: false };
 
-/**
- * This example schema shows two ways to share a simple state (in this case
- * the values of two dice as last rolled by clients). No practical application
- * would need both of these states.
- *
- * The first state, lastRoll, is using the simpler {@link @fluid-experimental/presence#Latest | Latest}
- * pattern (-\> {@link @fluid-experimental/presence#LatestValueManager | LatestValueManager}) where
- * all dice values are updated as a whole ({@link DiceValues} structure which has optional values).
- * If any part of the data is updated, then the entire data structure is shared. This means
- * keeping a local copy of the data structure or recomposing it each time making an update.
- *
- * The second state, lastDiceRolls, is using the {@link @fluid-experimental/presence#LatestMap | LatestMap}
- * pattern (-\> {@link @fluid-experimental/presence#LatestMapManager | LatestMapManager}) where
- * each die is updated independently. This allows for more granular updates, but also requires
- * more verbose setting/reading logic and use of boxed values (e.g. `{ value: DieValue}`). This
- * pattern more directly lends itself to handling arbitrary numbers of dice.
- *
- * Throughout the code, the `lastRoll` state is focus of use.
- */
 const statesSchema = {
-	// mouse: LatestMap<{ position: IMousePosition }, `die${number}`>(),
 	mouse: Latest(initialPosition),
 	focus: Latest(initialFocus),
 } satisfies PresenceStatesSchema;
