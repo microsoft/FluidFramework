744cef78d786a22201e15562c1e72e94a4c9bf98
Sonali Deshpande <48232592+sonalideshpandemsft@users.noreply.github.com>
2024-11-12 19:44:22 +0000

    fix eslint errors

--- packages/common/core-utils/src/test/events/events.spec.ts
+++ packages/common/core-utils/src/test/events/events.spec.ts
@@ -3,16 +3,16 @@
  * Licensed under the MIT License.
  */
 
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { strict as assert } from "node:assert";
 
 // import { validateAssertionError } from "@fluidframework/test-runtime-utils/internal";
 
 import {
-	// EventEmitter,
+	EventEmitter,
 	createEmitter,
-	// type Listenable
-} from "../../events/index.js";
+	// eslint-disable-next-line import/no-internal-modules
+} from "../../events/emitter.js";
+import type { Listenable } from "../../events/index.js";
 
 interface TestEvents {
 	open: () => void;
@@ -31,8 +31,8 @@ describe("EventEmitter", () => {
 
 	it("emits events and collects their results", () => {
 		const emitter = createEmitter<TestEvents>();
-		const listener1 = (arg: string) => arg.toUpperCase();
-		const listener2 = (arg: string) => arg.toLowerCase();
+		const listener1 = (arg: string): string => arg.toUpperCase();
+		const listener2 = (arg: string): string => arg.toLowerCase();
 		emitter.on("compute", listener1);
 		emitter.on("compute", listener2);
 		const results = emitter.emitAndCollect("compute", "hello");
@@ -85,7 +85,7 @@ describe("EventEmitter", () => {
 	it("deregisters events via off", () => {
 		const emitter = createEmitter<TestEvents>();
 		let error = false;
-		const listener = (e: boolean) => (error = e);
+		const listener = (e: boolean): boolean => (error = e);
 		emitter.on("close", listener);
 		emitter.off("close", listener);
 		emitter.emit("close", true);
@@ -112,8 +112,8 @@ describe("EventEmitter", () => {
 		const emitter = createEmitter<TestEvents>();
 		let opened = false;
 		let closed = false;
-		const listenerOpen = () => (opened = true);
-		const listenerClosed = () => (closed = true);
+		const listenerOpen = (): boolean => (opened = true);
+		const listenerClosed = (): boolean => (closed = true);
 		emitter.on("open", listenerOpen);
 		emitter.on("close", listenerClosed);
 		emitter.off("open", listenerOpen);
@@ -129,7 +129,7 @@ describe("EventEmitter", () => {
 	it("correctly handles multiple registrations for the same event", () => {
 		const emitter = createEmitter<TestEvents>();
 		let count: number;
-		const listener = () => (count += 1);
+		const listener = (): number => (count += 1);
 		const off1 = emitter.on("open", listener);
 		const off2 = emitter.on("open", () => listener());
 
@@ -151,10 +151,11 @@ describe("EventEmitter", () => {
 	it("errors on multiple registrations of the same listener", () => {
 		const emitter = createEmitter<TestEvents>();
 		let count = 0;
-		const listener = () => (count += 1);
+		const listener = (): number => (count += 1);
 		emitter.on("open", listener);
 		assert.throws(
 			() => emitter.on("open", listener),
+			// (e: Error) => validateAssertionError(e, /register.*twice.*open/),
 			(e: Error) => {
 				throw new Error(`${e} - /register.*twice.*open/`);
 			},
@@ -168,7 +169,7 @@ describe("EventEmitter", () => {
 		// This test ensures that symbol types are registered, error on double registration, and include the description of the symbol in the error message.
 		const eventSymbol = Symbol("TestEvent");
 		const emitter = createEmitter<{ [eventSymbol]: () => void }>();
-		const listener = () => {};
+		const listener = (): void => {};
 		emitter.on(eventSymbol, listener);
 		emitter.emit(eventSymbol);
 		assert.throws(
@@ -182,7 +183,7 @@ describe("EventEmitter", () => {
 	it("allows repeat deregistrations", () => {
 		const emitter = createEmitter<TestEvents>();
 		const deregister = emitter.on("open", () => {});
-		const listenerB = () => {};
+		const listenerB = (): void => {};
 		emitter.on("open", listenerB);
 		deregister();
 		deregister();
@@ -292,42 +293,42 @@ describe("EventEmitter", () => {
 
 // The below classes correspond to the examples given in the doc comment of `EventEmitter` to ensure that they compile
 
-// interface MyEvents {
-// 	loaded: () => void;
-// 	computed: () => number;
-// }
+interface MyEvents {
+	loaded: () => void;
+	computed: () => number;
+}
 
-// class MyInheritanceClass extends EventEmitter<MyEvents> {
-// 	private load() {
-// 		this.emit("loaded");
-// 		const results: number[] = this.emitAndCollect("computed");
-// 	}
-// }
+class MyInheritanceClass extends EventEmitter<MyEvents> {
+	private load(): void {
+		this.emit("loaded");
+		const results: number[] = this.emitAndCollect("computed");
+	}
+}
 
-// class MyCompositionClass implements Listenable<MyEvents> {
-// 	private readonly events = createEmitter<MyEvents>();
+class MyCompositionClass implements Listenable<MyEvents> {
+	private readonly events = createEmitter<MyEvents>();
 
-// 	private load() {
-// 		this.events.emit("loaded");
-// 		const results: number[] = this.events.emitAndCollect("computed");
-// 	}
+	private load(): void {
+		this.events.emit("loaded");
+		const results: number[] = this.events.emitAndCollect("computed");
+	}
 
-// 	public on<K extends keyof MyEvents>(eventName: K, listener: MyEvents[K]): () => void {
-// 		return this.events.on(eventName, listener);
-// 	}
+	public on<K extends keyof MyEvents>(eventName: K, listener: MyEvents[K]): () => void {
+		return this.events.on(eventName, listener);
+	}
 
-// 	public off<K extends keyof MyEvents>(eventName: K, listener: MyEvents[K]): void {
-// 		return this.events.off(eventName, listener);
-// 	}
-// }
+	public off<K extends keyof MyEvents>(eventName: K, listener: MyEvents[K]): void {
+		return this.events.off(eventName, listener);
+	}
+}
 
-// class MyExposingClass {
-// 	private readonly _events = createEmitter<MyEvents>();
+class MyExposingClass {
+	private readonly _events = createEmitter<MyEvents>();
 
-// 	public readonly events: Listenable<MyEvents> = this._events;
+	public readonly events: Listenable<MyEvents> = this._events;
 
-// 	private load() {
-// 		this._events.emit("loaded");
-// 		const results: number[] = this._events.emitAndCollect("computed");
-// 	}
-// }
+	private load(): void {
+		this._events.emit("loaded");
+		const results: number[] = this._events.emitAndCollect("computed");
+	}
+}
