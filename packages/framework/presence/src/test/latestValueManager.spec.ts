eb731f651e66a53c4e72fc90075cd5fce8bd862f
Tyler Butler <tylerbu@microsoft.com>
2024-11-12 18:02:42 -0800

    test cases for signals in latestValueManager

--- packages/framework/presence/src/test/latestValueManager.spec.ts
+++ packages/framework/presence/src/test/latestValueManager.spec.ts
@@ -3,12 +3,206 @@
  * Licensed under the MIT License.
  */
 
-import type { LatestValueClientData } from "../index.js";
-import { Latest } from "../index.js";
+import { EventAndErrorTrackingLogger } from "@fluidframework/test-utils/internal";
+import { type SinonFakeTimers, useFakeTimers } from "sinon";
+
+import { Latest } from "../latestValueManager.js";
+import type { LatestValueClientData } from "../latestValueTypes.js";
 import type { IPresence } from "../presence.js";
+import type { createPresenceManager } from "../presenceManager.js";
+
+import { MockEphemeralRuntime } from "./mockEphemeralRuntime.js";
+import {
+	prepareConnectedPresence,
+	assertFinalExpectations,
+	generateBasicClientJoin,
+} from "./testUtils.js";
 
 describe("Presence", () => {
 	describe("LatestValueManager", () => {
+		// Note: this test setup mimics the setup in src/test/presenceManager.spec.ts
+		let runtime: MockEphemeralRuntime;
+		let logger: EventAndErrorTrackingLogger;
+		const initialTime = 1000;
+		let clock: SinonFakeTimers;
+		let presence: ReturnType<typeof createPresenceManager>;
+
+		before(async () => {
+			clock = useFakeTimers();
+		});
+
+		beforeEach(() => {
+			logger = new EventAndErrorTrackingLogger();
+			runtime = new MockEphemeralRuntime(logger);
+
+			// We are configuring the runtime to be in a connected state, so ensure it looks connected
+			runtime.connected = true;
+
+			clock.setSystemTime(initialTime);
+
+			// Set up the presence connection
+			presence = prepareConnectedPresence(runtime, "sessionId-2", "client2", clock, logger);
+		});
+
+		afterEach(function (done: Mocha.Done) {
+			clock.reset();
+
+			// If the test passed so far, check final expectations.
+			if (this.currentTest?.state === "passed") {
+				assertFinalExpectations(runtime, logger);
+			}
+			done();
+		});
+
+		after(() => {
+			clock.restore();
+		});
+
+		it("sends update signal with updated data", async () => {
+			// Setup
+			runtime.signalsExpected.push(
+				[
+					"Pres:DatastoreUpdate",
+					{
+						"sendTimestamp": 1010,
+						"avgLatency": 10,
+						"data": {
+							"system:presence": {
+								"clientToSessionId": {
+									"client2": { "rev": 0, "timestamp": 1000, "value": "sessionId-2" },
+								},
+							},
+							"s:name:testStateWorkspace": {
+								"data": {
+									"sessionId-2": { "rev": 0, "timestamp": 1010, "value": { "num": 0 } },
+								},
+							},
+						},
+					},
+				],
+				[
+					"Pres:DatastoreUpdate",
+					{
+						"sendTimestamp": 1010,
+						"avgLatency": 10,
+						"isComplete": true,
+						"data": {
+							"system:presence": {
+								"clientToSessionId": {
+									"client2": { "rev": 0, "timestamp": 1000, "value": "sessionId-2" },
+								},
+							},
+							"s:name:testStateWorkspace": {
+								"data": {
+									"sessionId-2": { "rev": 0, "timestamp": 1010, "value": { "num": 0 } },
+								},
+							},
+						},
+					},
+				],
+				[
+					"Pres:DatastoreUpdate",
+					{
+						"sendTimestamp": 1010,
+						"avgLatency": 10,
+						"isComplete": true,
+						"data": {
+							"system:presence": {
+								"clientToSessionId": {
+									"client2": { "rev": 0, "timestamp": 1000, "value": "sessionId-2" },
+								},
+							},
+							"s:name:testStateWorkspace": {
+								"data": {
+									"sessionId-2": { "rev": 0, "timestamp": 1010, "value": { "num": 0 } },
+								},
+							},
+						},
+					},
+				],
+				[
+					"Pres:DatastoreUpdate",
+					{
+						"sendTimestamp": 1020,
+						"avgLatency": 10,
+						"data": {
+							"system:presence": {
+								"clientToSessionId": {
+									"client2": { "rev": 0, "timestamp": 1000, "value": "sessionId-2" },
+								},
+							},
+							"s:name:testStateWorkspace": {
+								"data": {
+									"sessionId-2": { "rev": 1, "timestamp": 1020, "value": { "num": 42 } },
+								},
+							},
+						},
+					},
+				],
+				[
+					"Pres:DatastoreUpdate",
+					{
+						"sendTimestamp": 1030,
+						"avgLatency": 10,
+						"data": {
+							"system:presence": {
+								"clientToSessionId": {
+									"client2": { "rev": 0, "timestamp": 1000, "value": "sessionId-2" },
+								},
+							},
+							"s:name:testStateWorkspace": {
+								"data": {
+									"sessionId-2": { "rev": 2, "timestamp": 1030, "value": { "num": 65 } },
+								},
+							},
+						},
+					},
+				],
+			);
+
+			// Configure a state workspace
+			const stateWorkspace = presence.getStates("name:testStateWorkspace", {
+				data: Latest({ num: 0 }),
+			});
+
+			const { data } = stateWorkspace.props;
+
+			// Process client join
+			presence.processSignal(
+				"",
+				{
+					type: "Pres:ClientJoin",
+					content: {
+						sendTimestamp: clock.now - 50,
+						avgLatency: 50,
+						data: {},
+						updateProviders: ["client2"],
+					},
+					clientId: "client4",
+				},
+				false,
+			);
+
+			const joinSignal = generateBasicClientJoin(clock.now - 50, {
+				averageLatency: 50,
+				clientSessionId: "sessionId-3",
+				clientConnectionId: "client3",
+				updateProviders: ["client2"],
+			});
+
+			presence.processSignal("", joinSignal, false);
+
+			clock.tick(10);
+			// This will trigger the third signal
+			data.local = { num: 42 };
+
+			clock.tick(10);
+			// This will trigger the fourth signal
+			data.local = { num: 65 };
+
+			assertFinalExpectations(runtime, logger);
+		});
+
 		/**
 		 * See {@link checkCompiles} below
 		 */
