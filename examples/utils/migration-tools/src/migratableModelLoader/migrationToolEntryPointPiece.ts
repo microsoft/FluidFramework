4f93c7c8385d85f5a9354fa387f122a4cc388188
Matt Rakow <marakow@microsoft.com>
2024-11-12 18:16:38 -0800

    Add migrationToolEntryPointPiece

---
+++ examples/utils/migration-tools/src/migratableModelLoader/migrationToolEntryPointPiece.ts
@@ -0,0 +1,52 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import type { IContainerRuntime } from "@fluidframework/container-runtime-definitions/internal";
+import type { IFluidHandle } from "@fluidframework/core-interfaces";
+
+import type { IMigrationTool } from "../interfaces/index.js";
+import { MigrationToolFactory } from "../migrationTool.js";
+
+import type { IEntryPointPiece } from "./loadCompositeRuntime.js";
+
+const migrationToolEntryPointPieceName = "migrationTool";
+
+const migrationToolRegistryKey = "migration-tool";
+const migrationToolFactory = new MigrationToolFactory();
+
+const migrationToolId = "migration-tool";
+
+async function getDataStoreEntryPoint<T>(
+	containerRuntime: IContainerRuntime,
+	alias: string,
+): Promise<T> {
+	const entryPointHandle = (await containerRuntime.getAliasedDataStoreEntryPoint(alias)) as
+		| IFluidHandle<T>
+		| undefined;
+
+	if (entryPointHandle === undefined) {
+		throw new Error(`Default dataStore [${alias}] must exist`);
+	}
+
+	return entryPointHandle.get();
+}
+
+export const migrationToolEntryPointPiece: IEntryPointPiece = {
+	name: migrationToolEntryPointPieceName,
+	registryEntries: [[migrationToolRegistryKey, Promise.resolve(migrationToolFactory)]],
+	onCreate: async (runtime: IContainerRuntime): Promise<void> => {
+		const migrationTool = await runtime.createDataStore(migrationToolRegistryKey);
+		await migrationTool.trySetAlias(migrationToolId);
+	},
+	onLoad: async (runtime: IContainerRuntime): Promise<void> => {
+		// Force the MigrationTool to instantiate in all cases.  The PactMap it uses must be loaded and running in
+		// order to respond with accept ops, and without this call the MigrationTool won't be instantiated on the
+		// summarizer client.
+		await getDataStoreEntryPoint(runtime, migrationToolId);
+	},
+	createPiece: async (runtime: IContainerRuntime): Promise<IMigrationTool> => {
+		return getDataStoreEntryPoint(runtime, migrationToolId);
+	},
+};
