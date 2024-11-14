4c00b47c057e0d6aedbb00beff177eebe77d6e49
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-11-14 20:33:36 +0000

    test: Fix and expand unit tests

--- tools/api-markdown-documenter/src/renderers/markdown-renderer/test/RenderHeading.test.ts
+++ tools/api-markdown-documenter/src/renderers/markdown-renderer/test/RenderHeading.test.ts
@@ -30,20 +30,41 @@ describe("Heading Markdown rendering tests", () => {
 			const expected = ["", "# Hello World {#\\_heading-id\\_}", "", ""].join("\n");
 			expect(result).to.equal(expected);
 		});
+
+		it("Without ID (beyond max heading level)", () => {
+			const input = HeadingNode.createFromPlainText("Hello World");
+			const result = testRender(input, { headingLevel: 7 });
+			const expected = ["", "**Hello World**", "", ""].join("\n");
+			expect(result).to.equal(expected);
+		});
+
+		it("With ID (beyond max heading level)", () => {
+			const input = HeadingNode.createFromPlainText("Hello World", /* id: */ "heading-id");
+			const result = testRender(input, { headingLevel: 7 });
+			const expected = ["", '<a id="heading-id"></a>', "**Hello World**", "", ""].join("\n");
+			expect(result).to.equal(expected);
+		});
 	});
 
 	describe("Table context", () => {
 		it("Without ID", () => {
 			const input = HeadingNode.createFromPlainText("Hello World");
-			const result = testRender(input);
-			const expected = ["", "# Hello World", "", ""].join("\n");
+			const result = testRender(input, { insideTable: true });
+			const expected = ["<h1>Hello World</h1>"].join("\n");
 			expect(result).to.equal(expected);
 		});
 
 		it("With ID", () => {
 			const input = HeadingNode.createFromPlainText("Hello World", /* id: */ "heading-id");
-			const result = testRender(input);
-			const expected = ["", "# Hello World {#heading-id}", "", ""].join("\n");
+			const result = testRender(input, { insideTable: true });
+			const expected = ['<h1 id="heading-id">Hello World</h1>'].join("\n");
+			expect(result).to.equal(expected);
+		});
+
+		it("With ID - includes content that would be escaped in Markdown", () => {
+			const input = HeadingNode.createFromPlainText("Hello World", /* id: */ "_heading-id_");
+			const result = testRender(input, { insideTable: true });
+			const expected = ['<h1 id="_heading-id_">Hello World</h1>'].join("\n");
 			expect(result).to.equal(expected);
 		});
 	});
