98136bcaae024110574fa4591f15b49775645258
Josmithr <josmithr@microsoft.com>
2024-11-15 12:04:23 -0800

    docs: Add missing copyright notice

--- docs/playwright.config.ts
+++ docs/playwright.config.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { defineConfig, devices } from "@playwright/test";
 
 const frontendPort = 3000;
