microsoft/FluidFramework

commit e3c4816e5610f5442323ef24d2941509d7043110

feat(client-presence): broadcast control defaults (#23120)
- `BroadcastControls` replace `LatestValueControls`
- `BroadcastControls` maybe specified on `PresenceStates` thru new
`controls` property as defaults for all value managers.
- `PresenceNotifications` redeclared apart from `PresenceStates` to
avoid `controls` exposure. Also yields better API documentation.
- `allowableUpdateLatencyMs` was renamed from `allowableUpdateLatency`
to clarify units are milliseconds. Specifying this value currently has
no effect.
- Unsupported `forcedRefreshInterval` has been removed until
implementation is closer.

packages/framework/presence/src/latestMapValueManager.ts
@@ -3,9 +3,9 @@
  * Licensed under the MIT License.
  */
 
+import type { BroadcastControls, BroadcastControlSettings } from "./broadcastControls.js";
+import { OptionalBroadcastControl } from "./broadcastControls.js";
 import type { ValueManager } from "./internalTypes.js";
-import type { LatestValueControls } from "./latestValueControls.js";
-import { LatestValueControl } from "./latestValueControls.js";
 import type {
 	LatestValueClientData,
 	LatestValueData,
@@ -189,10 +189,7 @@ class ValueMapImpl<T, K extends string | number> implements ValueMap<K, T> {
 	private countDefined: number;
 	public constructor(
 		private readonly value: InternalTypes.MapValueState<T>,
-		private readonly localUpdate: (
-			updates: InternalTypes.MapValueState<T>,
-			forceUpdate: boolean,
-		) => void,
+		private readonly localUpdate: (updates: InternalTypes.MapValueState<T>) => void,
 	) {
 		// All initial items are expected to be defined.
 		// TODO assert all defined and/or update type.
@@ -210,7 +207,7 @@ class ValueMapImpl<T, K extends string | number> implements ValueMap<K, T> {
 			item.value = value;
 		}
 		const update = { rev: this.value.rev, items: { [key]: item } };
-		this.localUpdate(update, /* forceUpdate */ false);
+		this.localUpdate(update);
 	}
 
 	public clear(): void {
@@ -288,7 +285,7 @@ export interface LatestMapValueManager<T, Keys extends string | number = string 
 	/**
 	 * Controls for management of sending updates.
 	 */
-	readonly controls: LatestValueControls;
+	readonly controls: BroadcastControls;
 
 	/**
 	 * Current value map for this client.
@@ -317,7 +314,7 @@ class LatestMapValueManagerImpl<
 		Required<ValueManager<T, InternalTypes.MapValueState<T>>>
 {
 	public readonly events = createEmitter<LatestMapValueManagerEvents<T, Keys>>();
-	public readonly controls: LatestValueControl;
+	public readonly controls: OptionalBroadcastControl;
 
 	public constructor(
 		private readonly key: RegistrationKey,
@@ -326,14 +323,16 @@ class LatestMapValueManagerImpl<
 			InternalTypes.MapValueState<T>
 		>,
 		public readonly value: InternalTypes.MapValueState<T>,
-		controlSettings: LatestValueControls,
+		controlSettings: BroadcastControlSettings | undefined,
 	) {
-		this.controls = new LatestValueControl(controlSettings);
+		this.controls = new OptionalBroadcastControl(controlSettings);
 
 		this.local = new ValueMapImpl<T, Keys>(
 			value,
-			(updates: InternalTypes.MapValueState<T>, forceUpdate: boolean) => {
-				datastore.localUpdate(key, updates, forceUpdate);
+			(updates: InternalTypes.MapValueState<T>) => {
+				datastore.localUpdate(key, updates, {
+					allowableUpdateLatencyMs: this.controls.allowableUpdateLatencyMs,
+				});
 			},
 		);
 	}
@@ -449,7 +448,7 @@ export function LatestMap<
 	initialValues?: {
 		[K in Keys]: JsonSerializable<T> & JsonDeserialized<T>;
 	},
-	controls?: LatestValueControls,
+	controls?: BroadcastControlSettings,
 ): InternalTypes.ManagerFactory<
 	RegistrationKey,
 	InternalTypes.MapValueState<T>,
@@ -463,12 +462,6 @@ export function LatestMap<
 			value.items[key] = { rev: 0, timestamp, value: initialValues[key as Keys] };
 		}
 	}
-	const controlSettings = controls
-		? { ...controls }
-		: {
-				allowableUpdateLatency: 60,
-				forcedRefreshInterval: 0,
-			};
 	const factory = (
 		key: RegistrationKey,
 		datastoreHandle: InternalTypes.StateDatastoreHandle<
@@ -489,7 +482,7 @@ export function LatestMap<
 				key,
 				datastoreFromHandle(datastoreHandle),
 				value,
-				controlSettings,
+				controls,
 			),
 		),
 	});

packages/framework/presence/src/latestValueManager.ts
@@ -3,10 +3,10 @@
  * Licensed under the MIT License.
  */
 
+import type { BroadcastControls, BroadcastControlSettings } from "./broadcastControls.js";
+import { OptionalBroadcastControl } from "./broadcastControls.js";
 import type { ValueManager } from "./internalTypes.js";
 import { brandedObjectEntries } from "./internalTypes.js";
-import type { LatestValueControls } from "./latestValueControls.js";
-import { LatestValueControl } from "./latestValueControls.js";
 import type { LatestValueClientData, LatestValueData } from "./latestValueTypes.js";
 import type { ISessionClient } from "./presence.js";
 import { datastoreFromHandle, type StateDatastore } from "./stateDatastore.js";
@@ -52,7 +52,7 @@ export interface LatestValueManager<T> {
 	/**
 	 * Controls for management of sending updates.
 	 */
-	readonly controls: LatestValueControls;
+	readonly controls: BroadcastControls;
 
 	/**
 	 * Current state for this client.
@@ -83,15 +83,15 @@ class LatestValueManagerImpl<T, Key extends string>
 		Required<ValueManager<T, InternalTypes.ValueRequiredState<T>>>
 {
 	public readonly events = createEmitter<LatestValueManagerEvents<T>>();
-	public readonly controls: LatestValueControl;
+	public readonly controls: OptionalBroadcastControl;
 
 	public constructor(
 		private readonly key: Key,
 		private readonly datastore: StateDatastore<Key, InternalTypes.ValueRequiredState<T>>,
 		public readonly value: InternalTypes.ValueRequiredState<T>,
-		controlSettings: LatestValueControls,
+		controlSettings: BroadcastControlSettings | undefined,
 	) {
-		this.controls = new LatestValueControl(controlSettings);
+		this.controls = new OptionalBroadcastControl(controlSettings);
 	}
 
 	public get local(): InternalUtilityTypes.FullyReadonly<JsonDeserialized<T>> {
@@ -102,7 +102,9 @@ class LatestValueManagerImpl<T, Key extends string>
 		this.value.rev += 1;
 		this.value.timestamp = Date.now();
 		this.value.value = value;
-		this.datastore.localUpdate(this.key, this.value, /* forceUpdate */ false);
+		this.datastore.localUpdate(this.key, this.value, {
+			allowableUpdateLatencyMs: this.controls.allowableUpdateLatencyMs,
+		});
 	}
 
 	public *clientValues(): IterableIterator<LatestValueClientData<T>> {
@@ -164,7 +166,7 @@ class LatestValueManagerImpl<T, Key extends string>
  */
 export function Latest<T extends object, Key extends string = string>(
 	initialValue: JsonSerializable<T> & JsonDeserialized<T> & object,
-	controls?: LatestValueControls,
+	controls?: BroadcastControlSettings,
 ): InternalTypes.ManagerFactory<
 	Key,
 	InternalTypes.ValueRequiredState<T>,
@@ -177,12 +179,6 @@ export function Latest<T extends object, Key extends string = string>(
 		timestamp: Date.now(),
 		value: { ...initialValue },
 	};
-	const controlSettings = controls
-		? { ...controls }
-		: {
-				allowableUpdateLatency: 60,
-				forcedRefreshInterval: 0,
-			};
 	const factory = (
 		key: Key,
 		datastoreHandle: InternalTypes.StateDatastoreHandle<
@@ -195,12 +191,7 @@ export function Latest<T extends object, Key extends string = string>(
 	} => ({
 		value,
 		manager: brandIVM<LatestValueManagerImpl<T, Key>, T, InternalTypes.ValueRequiredState<T>>(
-			new LatestValueManagerImpl(
-				key,
-				datastoreFromHandle(datastoreHandle),
-				value,
-				controlSettings,
-			),
+			new LatestValueManagerImpl(key, datastoreFromHandle(datastoreHandle), value, controls),
 		),
 	});
 	return Object.assign(factory, { instanceBase: LatestValueManagerImpl });

packages/framework/presence/src/broadcastControls.ts
@@ -1,0 +1,132 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * Common controls for Value Managers.
+ *
+ * @sealed
+ * @alpha
+ */
+export interface BroadcastControls {
+	/**
+	 * Maximum time in milliseconds that a local value update is allowed
+	 * to remain pending before it must be broadcast.
+	 *
+	 * @remarks
+	 * There is no guarantee of broadcast within time allowed
+	 * as other conditions such as disconnect or service throttling may
+	 * cause a delay.
+	 *
+	 * Setting to `undefined` will restore to a system default.
+	 */
+	allowableUpdateLatencyMs: number | undefined;
+
+	/**
+	 * Target time in milliseconds between oldest changed local state
+	 * has been broadcast and forced rebroadcast of all local values.
+	 * A value of less than 10 disables forced refresh.
+	 *
+	 * @privateRemarks
+	 * Any time less than 10 milliseconds is likely to generate too
+	 * many signals. Ideally this feature becomes obsolete as
+	 * we understand the system better and account for holes.
+	 */
+	// forcedRefreshIntervalMs is removed until it is supported.
+	// forcedRefreshIntervalMs: number | undefined;
+}
+
+/**
+ * Value set to configure {@link BroadcastControls}.
+ *
+ * @alpha
+ */
+export interface BroadcastControlSettings {
+	/**
+	 * {@inheritdoc BroadcastControls.allowableUpdateLatencyMs}
+	 *
+	 * @defaultValue 60 [milliseconds]
+	 */
+	readonly allowableUpdateLatencyMs?: number;
+
+	/**
+	 * {@inheritdoc BroadcastControls.forcedRefreshIntervalMs}
+	 *
+	 * @defaultValue 0 (disabled)
+	 */
+	// forcedRefreshIntervalMs is removed until it is supported.
+	// readonly forcedRefreshIntervalMs?: number;
+}
+
+class ForcedRefreshControl
+	implements
+		Pick<
+			BroadcastControls & { forcedRefreshIntervalMs: number | undefined },
+			"forcedRefreshIntervalMs"
+		>
+{
+	private _forcedRefreshInterval: number | undefined;
+
+	public constructor(settings?: BroadcastControlSettings) {
+		// this._forcedRefreshInterval = settings?.forcedRefreshIntervalMs;
+	}
+
+	public get forcedRefreshIntervalMs(): number | undefined {
+		return this._forcedRefreshInterval;
+	}
+	public set forcedRefreshIntervalMs(value: number | undefined) {
+		if (value === undefined) {
+			this._forcedRefreshInterval = undefined;
+		} else {
+			this._forcedRefreshInterval = value >= 10 ? value : undefined;
+			if (value >= 10) {
+				// TODO: enable periodic forced refresh
+				throw new Error("Forced Refresh feature is not implemented");
+			}
+		}
+	}
+}
+
+/**
+ * @internal
+ */
+export class OptionalBroadcastControl
+	extends ForcedRefreshControl
+	implements BroadcastControls
+{
+	public allowableUpdateLatencyMs: number | undefined;
+
+	public constructor(settings?: BroadcastControlSettings) {
+		super(settings);
+		this.allowableUpdateLatencyMs = settings?.allowableUpdateLatencyMs;
+	}
+}
+
+/**
+ * Implements {@link BroadcastControls} but always provides defined value for
+ * {@link BroadcastControls.allowableUpdateLatencyMs | allowableUpdateLatencyMs}.
+ *
+ * If {@link BroadcastControls.allowableUpdateLatencyMs | allowableUpdateLatencyMs}
+ * is set to `undefined`, the default will be restored.
+ *
+ * @internal
+ */
+export class RequiredBroadcastControl
+	extends ForcedRefreshControl
+	implements BroadcastControls
+{
+	private _allowableUpdateLatencyMs: number;
+
+	public constructor(private readonly defaultAllowableUpdateLatencyMs: number) {
+		super();
+		this._allowableUpdateLatencyMs = defaultAllowableUpdateLatencyMs;
+	}
+
+	public get allowableUpdateLatencyMs(): number {
+		return this._allowableUpdateLatencyMs;
+	}
+	public set allowableUpdateLatencyMs(value: number | undefined) {
+		this._allowableUpdateLatencyMs = value ?? this.defaultAllowableUpdateLatencyMs;
+	}
+}

packages/framework/presence/src/exposedInternalTypes.ts
@@ -112,7 +112,7 @@ export namespace InternalTypes {
 		key: TKey,
 		datastoreHandle: StateDatastoreHandle<TKey, TValue>,
 	) => {
-		value?: TValue;
+		initialData?: { value: TValue; allowableUpdateLatencyMs: number | undefined };
 		manager: StateValue<TManager>;
 	});
 

packages/framework/presence/src/index.ts
@@ -32,9 +32,9 @@ export type {
 	PresenceNotificationsSchema,
 	PresenceStates,
 	PresenceStatesEntries,
-	PresenceStatesEntry,
 	PresenceStatesSchema,
 	PresenceWorkspaceAddress,
+	PresenceWorkspaceEntry,
 } from "./types.js";
 
 export {
@@ -45,6 +45,11 @@ export {
 	SessionClientStatus,
 } from "./presence.js";
 
+export type {
+	BroadcastControls,
+	BroadcastControlSettings,
+} from "./broadcastControls.js";
+
 export { acquirePresence } from "./experimentalAccess.js";
 
 export {
@@ -53,7 +58,6 @@ export {
 	ExperimentalPresenceManager,
 } from "./datastorePresenceManagerFactory.js";
 
-export type { LatestValueControls } from "./latestValueControls.js";
 export {
 	LatestMap,
 	type LatestMapItemRemovedClientData,

packages/framework/presence/src/internalTypes.ts
@@ -14,13 +14,11 @@ import type { IRuntimeInternal } from "@fluidframework/presence/internal/contain
 /**
  * @internal
  */
-export interface ClientRecord<
-	TValue extends InternalTypes.ValueDirectoryOrState<unknown> | undefined,
-> {
+export interface ClientRecord<TValue extends InternalTypes.ValueDirectoryOrState<unknown>> {
 	// Caution: any particular item may or may not exist
 	// Typescript does not support absent keys without forcing type to also be undefined.
 	// See https://github.com/microsoft/TypeScript/issues/42810.
-	[ClientSessionId: ClientSessionId]: Exclude<TValue, undefined>;
+	[ClientSessionId: ClientSessionId]: TValue;
 }
 
 /**

packages/framework/presence/src/latestValueControls.ts
@@ -1,59 +1,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
-/**
- * Common controls for the Latest* Value Managers.
- *
- * @alpha
- */
-export interface LatestValueControls {
-	/**
-	 * Maximum time in milliseconds that a local value update is allowed
-	 * to remain pending before it must be broadcast.
-	 *
-	 * @remarks There is no guarantee of broadcast within time allowed
-	 * as other conditions such as disconnect or service throttling may
-	 * cause a delay.
-	 */
-	allowableUpdateLatency: number;
-
-	/**
-	 * Target time in milliseconds between oldest changed local state
-	 * has been broadcast and forced rebroadcast of all local values.
-	 * A value of less than 10 disables forced refresh.
-	 *
-	 * @defaultValue 0
-	 *
-	 * @privateRemarks
-	 * Any time less than 10 milliseconds is likely to generate too
-	 * many signals. Ideally this feature becomes obsolete as
-	 * we understand the system better and account for holes.
-	 */
-	forcedRefreshInterval: number;
-}
-
-/**
- * @internal
- */
-export class LatestValueControl implements LatestValueControls {
-	public allowableUpdateLatency: number;
-	private _forcedRefreshInterval: number;
-
-	public constructor(settings: LatestValueControls) {
-		this.allowableUpdateLatency = settings.allowableUpdateLatency;
-		this._forcedRefreshInterval = settings.forcedRefreshInterval;
-	}
-
-	public get forcedRefreshInterval(): number {
-		return this._forcedRefreshInterval;
-	}
-	public set forcedRefreshInterval(value: number) {
-		this._forcedRefreshInterval = value < 10 ? 0 : value;
-		if (this._forcedRefreshInterval >= 10) {
-			// TODO: enable periodic forced refresh
-			throw new Error("Forced Refresh feature is not implemented");
-		}
-	}
-}

packages/framework/presence/src/notificationsManager.ts
@@ -8,6 +8,7 @@ import type { ISessionClient } from "./presence.js";
 import { datastoreFromHandle, type StateDatastore } from "./stateDatastore.js";
 import { brandIVM } from "./valueManager.js";
 
+import type { JsonTypeWith } from "@fluidframework/presence/internal/core-interfaces";
 import type { Events, ISubscribable } from "@fluidframework/presence/internal/events";
 import { createEmitter } from "@fluidframework/presence/internal/events";
 import type { InternalTypes } from "@fluidframework/presence/internal/exposedInternalTypes";
@@ -147,17 +148,27 @@ class NotificationsManagerImpl<
 		broadcast: (name, ...args) => {
 			this.datastore.localUpdate(
 				this.key,
-				// @ts-expect-error TODO
-				{ rev: 0, timestamp: 0, value: { name, args: [...args] }, ignoreUnmonitored: true },
-				true,
+				{
+					rev: 0,
+					timestamp: 0,
+					value: { name, args: [...(args as JsonTypeWith<never>[])] },
+					ignoreUnmonitored: true,
+				},
+				// This is a notification, so we want to send it immediately.
+				{ allowableUpdateLatencyMs: 0 },
 			);
 		},
 		unicast: (name, targetClient, ...args) => {
 			this.datastore.localUpdate(
 				this.key,
-				// @ts-expect-error TODO
-				{ rev: 0, timestamp: 0, value: { name, args: [...args] }, ignoreUnmonitored: true },
-				targetClient,
+				{
+					rev: 0,
+					timestamp: 0,
+					value: { name, args: [...(args as JsonTypeWith<never>[])] },
+					ignoreUnmonitored: true,
+				},
+				// This is a notification, so we want to send it immediately.
+				{ allowableUpdateLatencyMs: 0, targetClientId: targetClient.getConnectionId() },
 			);
 		},
 	};

commit c6559be1a636c2fc874b2fad6dd78a8306333701

fix(client-presence): restore initial state values (#23127)
Corrects regression from #23120

packages/framework/presence/src/latestMapValueManager.ts
@@ -469,10 +469,10 @@ export function LatestMap<
 			InternalTypes.MapValueState<T>
 		>,
 	): {
-		value: typeof value;
+		initialData: { value: typeof value; allowableUpdateLatencyMs: number | undefined };
 		manager: InternalTypes.StateValue<LatestMapValueManager<T, Keys>>;
 	} => ({
-		value,
+		initialData: { value, allowableUpdateLatencyMs: controls?.allowableUpdateLatencyMs },
 		manager: brandIVM<
 			LatestMapValueManagerImpl<T, RegistrationKey, Keys>,
 			T,

packages/framework/presence/src/latestValueManager.ts
@@ -186,10 +186,10 @@ export function Latest<T extends object, Key extends string = string>(
 			InternalTypes.ValueRequiredState<T>
 		>,
 	): {
-		value: typeof value;
+		initialData: { value: typeof value; allowableUpdateLatencyMs: number | undefined };
 		manager: InternalTypes.StateValue<LatestValueManager<T>>;
 	} => ({
-		value,
+		initialData: { value, allowableUpdateLatencyMs: controls?.allowableUpdateLatencyMs },
 		manager: brandIVM<LatestValueManagerImpl<T, Key>, T, InternalTypes.ValueRequiredState<T>>(
 			new LatestValueManagerImpl(key, datastoreFromHandle(datastoreHandle), value, controls),
 		),