e8c1aa8338459253fdd651ee0ae07bdeffb5141c
Willie Habimana <whabimana@microsoft.com>
2024-11-19 14:57:27 -0800

    attendeeDisconnected tests

--- packages/test/test-end-to-end-tests/src/test/presence.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/presence.spec.ts
@@ -100,70 +100,100 @@ describeCompat("Presence", "NoCompat", (getTestObjectProvider, apis) => {
 		provider = getTestObjectProvider();
 	});
 
-	describe("with single client", () => {
-		let presence: IPresence;
+	describe("States", () => {
+		const testStatesSchema = {
+			latest: Latest({}),
+		};
+		describe("with single client", () => {
+			let presence: IPresence;
 
-		beforeEach("createPresence", async function () {
-			const container = await createContainer();
-			presence = await getPresence(container);
-		});
+			beforeEach("create container and get presence", async function () {
+				const container = await createContainer();
+				presence = await getPresence(container);
 
-		it("can set and get states", () => {
-			const testStatesSchema = {
-				latest: Latest({}),
-			};
-			const testStates = presence.getStates("name:test-states", testStatesSchema);
-			testStates.props.latest.local = { test: 1 };
+				// need to be connected to send signals
+				if (container.connectionState !== ConnectionState.Connected) {
+					await new Promise((resolve) => container.once("connected", resolve));
+				}
+			});
 
-			assert.deepEqual(testStates.props.latest.local, { test: 1 });
+			it("can be created", () => {
+				const testStates = presence.getStates("name:test-states", testStatesSchema);
+				testStates.props.latest.local = { test: 1 };
+
+				assert.deepEqual(testStates.props.latest.local, { test: 1 });
+			});
 		});
 	});
+	describe("attendee support", () => {
+		describe("with single client", () => {
+			let container: IContainer;
+			let presence: IPresence;
 
-	describe("with multiple clients", () => {
-		let container1: IContainer;
-		let container2: IContainer;
-		let container3: IContainer;
-		let presence1: IPresence;
-		let presence2: IPresence;
-		let presence3: IPresence;
+			beforeEach("create container and get presence", async function () {
+				container = await createContainer();
+				presence = await getPresence(container);
 
-		beforeEach("create containers and presences", async function () {
-			container1 = await createContainer();
-			container2 = await loadContainer();
-			container3 = await loadContainer();
+				// need to be connected to send signals
+				if (container.connectionState !== ConnectionState.Connected) {
+					await new Promise((resolve) => container.once("connected", resolve));
+				}
+			});
 
-			presence1 = await getPresence(container1);
-			presence2 = await getPresence(container2);
-			presence3 = await getPresence(container3);
+			it("updates session client status when disconnected", async function () {
+				// ACT
+				container.disconnect();
 
-			// need to be connected to send signals
-			if (container1.connectionState !== ConnectionState.Connected) {
-				await new Promise((resolve) => container1.once("connected", resolve));
-			}
-			if (container2.connectionState !== ConnectionState.Connected) {
-				await new Promise((resolve) => container2.once("connected", resolve));
-			}
-			if (container3.connectionState !== ConnectionState.Connected) {
-				await new Promise((resolve) => container2.once("connected", resolve));
-			}
+				// VERIFY
+				assert.equal(presence.getMyself().getConnectionStatus(), "Disconnected");
+			});
 		});
+		describe("with multiple clients", () => {
+			let container1: IContainer;
+			let container2: IContainer;
+			let container3: IContainer;
+			let presence1: IPresence;
+			let presence2: IPresence;
+			let presence3: IPresence;
 
-		it("announces 'attendeeDisconnected' when remote client disconnects", async () => {
-			// SETUP
-			const disconnectedAttendee = presence3.getMyself();
+			beforeEach("create containers and presences", async function () {
+				container1 = await createContainer();
+				container2 = await loadContainer();
+				container3 = await loadContainer();
 
-			// ACT - disconnect client 3
-			container3.disconnect();
+				presence1 = await getPresence(container1);
+				presence2 = await getPresence(container2);
+				presence3 = await getPresence(container3);
 
-			// VERIFY - client 1 and 2 receive 'attendeeDisconnected' event with correct attendee
-			const disconnectedAttendees = await waitForAttendeeEvent(
-				"attendeeDisconnected",
-				presence1,
-				presence2,
-			);
-			assert.equal(disconnectedAttendees.length, 2);
-			verifyAttendee(disconnectedAttendees[0], disconnectedAttendee);
-			verifyAttendee(disconnectedAttendees[1], disconnectedAttendee);
+				// need to be connected to send signals
+				if (container1.connectionState !== ConnectionState.Connected) {
+					await new Promise((resolve) => container1.once("connected", resolve));
+				}
+				if (container2.connectionState !== ConnectionState.Connected) {
+					await new Promise((resolve) => container2.once("connected", resolve));
+				}
+				if (container3.connectionState !== ConnectionState.Connected) {
+					await new Promise((resolve) => container2.once("connected", resolve));
+				}
+			});
+
+			it("announces 'attendeeDisconnected' when remote client disconnects", async () => {
+				// SETUP
+				const disconnectedAttendee = presence3.getMyself();
+
+				// ACT - disconnect client 3
+				container3.disconnect();
+
+				// VERIFY - client 1 and 2 receive 'attendeeDisconnected' event with correct attendee
+				const disconnectedAttendees = await waitForAttendeeEvent(
+					"attendeeDisconnected",
+					presence1,
+					presence2,
+				);
+				assert.equal(disconnectedAttendees.length, 2);
+				verifyAttendee(disconnectedAttendees[0], disconnectedAttendee);
+				verifyAttendee(disconnectedAttendees[1], disconnectedAttendee);
+			});
 		});
 	});
 });
