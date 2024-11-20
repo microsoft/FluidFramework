eaf7821ec2e0dd0b1baca8e00d44b8226b1b2d53
Tyler Butler <tylerbu@microsoft.com>
2024-11-19 17:03:45 -0800

    tests

--- packages/framework/presence/src/test/batching.vitest.ts
+++ packages/framework/presence/src/test/batching.vitest.ts
@@ -14,51 +14,47 @@ import { MockRuntimeSignalSnapshotter } from "./snapshotEphemeralRuntime.js";
 import { prepareConnectedPresence } from "./testUtils.js";
 
 describe("Presence", () => {
-	describe("LatestValueManager", () => {
-		describe("batching", () => {
-			// IMPORTANT: All tests in this suite have an extra signal for each workspace that is initialized in the test.
-			// This is a bug. See AB#24392. This means that when looking at snapshots from these tests, the "real" snapshots
-			// start at 2, not 1, for most tests. Some tests may have additional incorrect leading signals. Such examples
-			// are noted inline.
-			let runtime: MockRuntimeSignalSnapshotter;
-			let logger: EventAndErrorTrackingLogger;
-			const initialTime = 1000;
-			let clock: SinonFakeTimers;
-			let presence: ReturnType<typeof createPresenceManager>;
+	describe("batching", () => {
+		let runtime: MockRuntimeSignalSnapshotter;
+		let logger: EventAndErrorTrackingLogger;
+		const initialTime = 1000;
+		let clock: SinonFakeTimers;
+		let presence: ReturnType<typeof createPresenceManager>;
 
-			beforeAll(async () => {
-				clock = useFakeTimers();
-			});
+		beforeAll(async () => {
+			clock = useFakeTimers();
+		});
 
-			beforeEach(() => {
-				logger = new EventAndErrorTrackingLogger();
-				runtime = new MockRuntimeSignalSnapshotter(logger);
-				runtime.snapshotSignals = true;
+		beforeEach(() => {
+			logger = new EventAndErrorTrackingLogger();
+			runtime = new MockRuntimeSignalSnapshotter(logger);
+			runtime.snapshotSignals = true;
 
-				// Note that while the initialTime is set to 1000, the prepareConnectedPresence call advances
-				// it to 1010 so all tests start at that time.
-				clock.setSystemTime(initialTime);
+			// Note that while the initialTime is set to 1000, the prepareConnectedPresence call advances
+			// it to 1010 so all tests start at that time.
+			clock.setSystemTime(initialTime);
 
-				// Disable submitting signals with a dummy function. This ensures we don't capture signals from
-				// test setup, like the prepareConnectedPresence call.
-				const submitSignalOriginal = runtime.submitSignal;
-				runtime.submitSignal = () => {};
+			// Disable submitting signals with a dummy function. This ensures we don't capture signals from
+			// test setup, like the prepareConnectedPresence call.
+			const submitSignalOriginal = runtime.submitSignal;
+			runtime.submitSignal = () => {};
 
-				// Set up the presence connection
-				presence = prepareConnectedPresence(runtime, "sessionId-2", "client2", clock, logger);
+			// Set up the presence connection
+			presence = prepareConnectedPresence(runtime, "sessionId-2", "client2", clock, logger);
 
-				// Restore the submiSignal function
-				runtime.submitSignal = submitSignalOriginal;
-			});
+			// Restore the submiSignal function
+			runtime.submitSignal = submitSignalOriginal;
+		});
 
-			afterEach(() => {
-				clock.reset();
-			});
+		afterEach(() => {
+			clock.reset();
+		});
 
-			afterAll(() => {
-				clock.restore();
-			});
+		afterAll(() => {
+			clock.restore();
+		});
 
+		describe("LatestValueManager", () => {
 			it("sends signal immediately when allowable latency is 0", async () => {
 				// Configure a state workspace
 				const stateWorkspace = presence.getStates("name:testStateWorkspace", {
@@ -97,17 +93,17 @@ describe("Presence", () => {
 				// Configure a state workspace
 				const stateWorkspace = presence.getStates("name:testStateWorkspace", {
 					count: Latest({ num: 0 } /* default allowableUpdateLatencyMs = 60 */),
-				}); // will be queued; deadline is now 1070 -- TODO: The deadline is not getting set - why?
+				}); // will be queued; deadline is now 1070
 
 				const { count } = stateWorkspace.props;
 
-				// clock.tick(10); // Time is now 1020
+				clock.tick(10); // Time is now 1020
 				count.local = { num: 12 }; // will be queued; deadline remains 1070
 
 				clock.tick(10); // Time is now 1030
 				count.local = { num: 34 }; // will be queued; deadline remains 1070
 
-				clock.tick(40); // Time is now 1070
+				clock.tick(30); // Time is now 1060
 				count.local = { num: 22 }; // will be queued; deadline remains 1070
 
 				// SIGNAL #1
@@ -115,7 +111,7 @@ describe("Presence", () => {
 				// signal with the value from the last signal (num=22).
 
 				// It's necessary to tick the timer beyond the deadline so the timer will fire.
-				clock.tick(30); // Time is now 1080
+				clock.tick(20); // Time is now 1080
 
 				clock.tick(10); // Time is now 1090
 				count.local = { num: 56 }; // will be queued; deadline is set to 1150
@@ -132,13 +128,15 @@ describe("Presence", () => {
 
 				// It's necessary to tick the timer beyond the deadline so the timer will fire.
 				clock.tick(30); // Time is now 1180
+
+				expect(runtime.submittedSignals).toHaveLength(2);
 			});
 
 			it("batches signals sent within a specified allowableUpdateLatency", async () => {
 				// Configure a state workspace
 				const stateWorkspace = presence.getStates("name:testStateWorkspace", {
 					count: Latest({ num: 0 }, { allowableUpdateLatencyMs: 100 }),
-				}); // where does broadcast of initial state go?
+				});
 
 				const { count } = stateWorkspace.props;
 
@@ -170,6 +168,8 @@ describe("Presence", () => {
 
 				// It's necessary to tick the timer beyond the deadline so the timer will fire.
 				clock.tick(30); // Time is now 1250
+
+				expect(runtime.submittedSignals).toHaveLength(2);
 			});
 
 			it("queued signal is sent immediately with immediate update message", async () => {
@@ -260,7 +260,9 @@ describe("Presence", () => {
 
 				expect(runtime.submittedSignals).toHaveLength(1);
 			});
+		});
 
+		describe("NotificationsManager", () => {
 			// IMPORTANT: RESULTS NOT VALID! See TODOs inline.
 			it("notification signals are sent immediately", async () => {
 				runtime.snapshotSignals = false;
@@ -298,51 +300,32 @@ describe("Presence", () => {
 				// SIGNAL #1
 				testEvents.emit.broadcast("newId", 77);
 
+				expect(
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+					(runtime.submittedSignals.at(-1)?.[1] as any).data[
+						"n:name:testNotificationWorkspace"
+					].testEvents["sessionId-2"].value.args,
+				).toEqual([77]);
+
 				clock.tick(10); // Time is now 1060
 				// SIGNAL #2
-				// TODO: This value is not in the snapshot - it seems that the old value (77) is broadcast
-				// again. Why?
 				testEvents.emit.broadcast("newId", 88);
 
 				expect(runtime.submittedSignals).toHaveLength(2);
 
-				const signal1 = runtime.submittedSignals[0];
 				expect(
 					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-					(signal1?.[1] as any).data["n:name:testNotificationWorkspace"].testEvents[
-						"sessionId-2"
-					].value,
-				).toMatchInlineSnapshot(`
-					{
-					  "args": [
-					    77,
-					  ],
-					  "name": "newId",
-					}
-				`);
-
-				expect(
-					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-					(signal1?.[1] as any).data["n:name:testNotificationWorkspace"].testEvents[
-						"sessionId-2"
-					].value.args,
-				).toEqual([77]);
-
-				const signal2 = runtime.submittedSignals[1];
-				expect(
-					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-					(signal2?.[1] as any).data["n:name:testNotificationWorkspace"].testEvents[
-						"sessionId-2"
-					].value.args,
+					(runtime.submittedSignals.at(-1)?.[1] as any).data[
+						"n:name:testNotificationWorkspace"
+					].testEvents["sessionId-2"].value.args,
 				).toEqual([88]);
 			});
 
-			// IMPORTANT: RESULTS NOT VALID! See TODOs inline.
 			it("notification signals cause queued messages to be sent immediately", async () => {
 				// Configure a state workspaces
 				const stateWorkspace = presence.getStates("name:testStateWorkspace", {
 					count: Latest({ num: 0 }, { allowableUpdateLatencyMs: 100 }),
-				}); // where does broadcast of initial state go?
+				}); // will be queued, deadline is 1110
 
 				// eslint-disable-next-line @typescript-eslint/ban-types
 				const notificationsWorkspace: PresenceNotifications<{}> = presence.getNotifications(
@@ -372,27 +355,25 @@ describe("Presence", () => {
 				});
 
 				clock.tick(10); // Time is now 1020
-				count.local = { num: 12 }; // will be queued, deadline is set to 1120
+				count.local = { num: 12 }; // will be queued, deadline remains 1110
 
-				clock.tick(30); // Time is now 1050
+				clock.tick(10); // Time is now 1030
+				count.local = { num: 34 }; // will be queued, deadline remains 1110
+
+				clock.tick(10); // Time is now 1040
+				count.local = { num: 56 }; // will be queued, deadline remains 1110
+
+				clock.tick(20); // Time is now 1060
 				testEvents.emit.broadcast("newId", 99);
 				// SIGNAL #1
 				// The notification will cause an immediate broadcast of the queued signal
 				// along with the notification signal.
 
-				clock.tick(30); // Time is now 1080
+				clock.tick(30); // Time is now 1090
 				// SIGNAL #2
 				testEvents.emit.broadcast("newId", 111);
 
 				expect(runtime.submittedSignals).toHaveLength(2);
-
-				const signal = runtime.submittedSignals[1];
-				expect(
-					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-					(signal?.[1] as any).data["n:name:testNotificationWorkspace"].testEvents[
-						"sessionId-2"
-					].value.args,
-				).toEqual([111]);
 			});
 		});
 	});
