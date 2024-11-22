d170c9546004c131e5c6a696411811e02c9c9e03
Mark Fields <markfields@users.noreply.github.com>
2024-11-22 23:09:08 +0000

    PR Feedback

--- packages/drivers/odsp-driver/src/odspUrlHelper.ts
+++ packages/drivers/odsp-driver/src/odspUrlHelper.ts
@@ -129,10 +129,11 @@ export async function getOdspUrlParts(url: URL): Promise<IOdspUrlParts | undefin
 
 /**
  * Inspect the ODSP siteUrl to guess if this document is in SPDF or MSIT, to aid livesite investigations
+ * @param urlOnSite - The URL of the site or a resource on the site
  * @returns undefined if the URL doesn't match known SPDF/MSIT patterns, "SPDF" if it's SPDF, "MSIT" if it's MSIT
  */
-export function checkForInternalFarmType(siteUrl: string): "SPDF" | "MSIT" | undefined {
-	const domain = new URL(siteUrl).hostname.toLowerCase();
+export function checkForInternalFarmType(urlOnSite: string): "SPDF" | "MSIT" | undefined {
+	const domain = new URL(urlOnSite).hostname.toLowerCase();
 	if (domain.endsWith(".sharepoint-df.com")) {
 		return "SPDF";
 	} else if (
