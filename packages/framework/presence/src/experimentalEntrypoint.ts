d99449ddc542d9d913504fc30a3058d3ddcd8e20
Tyler Butler <tylerbu@microsoft.com>
2024-11-26 15:41:39 -0800

    Demonstrate a custom entrypoint for a package

---
+++ packages/framework/presence/src/experimentalEntrypoint.ts
@@ -0,0 +1,17 @@
+export type {
+	PresenceNotifications,
+	PresenceNotificationsSchema,
+	PresenceStates,
+	PresenceStatesEntries,
+	PresenceStatesSchema,
+	PresenceWorkspaceAddress,
+	PresenceWorkspaceEntry,
+} from "./types.js";
+
+export {
+	type ClientSessionId,
+	type IPresence,
+	type ISessionClient,
+	type PresenceEvents,
+	SessionClientStatus,
+} from "./presence.js";
