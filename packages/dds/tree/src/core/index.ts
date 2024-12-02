9e61721c36eb1665c5add8a1e858c7983d08b542
daesunp <138815173+daesunp@users.noreply.github.com>
2024-12-02 14:36:53 -0800

    move chunk.ts file to core/tree (#23173)
    ## Description
    
    Moves the `chunk.ts` to `core/tree` to separate some chunk interfaces
    and types from the `ChunkedForest`.

--- packages/dds/tree/src/core/index.ts
+++ packages/dds/tree/src/core/index.ts
@@ -99,6 +99,11 @@ export {
 	type DeltaFieldChanges,
 	type ExclusiveMapTree,
 	deepCopyMapTree,
+	type TreeChunk,
+	dummyRoot,
+	cursorChunk,
+	tryGetChunk,
+	type ChunkedCursor,
 } from "./tree/index.js";
 
 export {
