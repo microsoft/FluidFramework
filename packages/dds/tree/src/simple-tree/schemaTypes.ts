8106c0b3a5ee3d30cb064ec4d4776e59b207ac50
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-12-02 19:31:36 +0000

    docs: Expand notes

--- packages/dds/tree/src/simple-tree/schemaTypes.ts
+++ packages/dds/tree/src/simple-tree/schemaTypes.ts
@@ -190,7 +190,10 @@ export interface FieldProps<TCustomMetadata = unknown> {
 
 	/**
 	 * Optional metadata to associate with the field.
-	 * @remarks Note: this metadata is not persisted in the document.
+	 *
+	 * @remarks
+	 * Note: this metadata is not persisted in the document; it only exists locally at runtime.
+	 * So making changes to these values will not affect collaborators.
 	 */
 	readonly metadata?: FieldSchemaMetadata<TCustomMetadata>;
 }
@@ -836,7 +839,10 @@ export type TreeLeafValue = number | string | boolean | IFluidHandle | null;
 export interface NodeSchemaOptions<out TMetadata = NodeSchemaMetadata> {
 	/**
 	 * Optional metadata to associate with the Node Schema.
-	 * @remarks Note: this metadata is not persisted in the document.
+	 *
+	 * @remarks
+	 * Note: this metadata is not persisted in the document; it only exists locally at runtime.
+	 * So making changes to these values will not affect collaborators.
 	 */
 	readonly metadata?: TMetadata;
 }
