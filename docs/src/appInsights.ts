48227eb033f52fd70fbfbb28ad3f6af9b607c3f0
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-12-04 11:04:30 -0800

    Added AppInsights code

---
+++ docs/src/appInsights.ts
@@ -0,0 +1,21 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
+const reactPlugin = new ReactPlugin();
+
+const appInsights = new ApplicationInsights({
+    config: {
+		connectionString:'InstrumentationKey=8bce9107-e3f7-445c-be87-153f7ecbbe47;IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/;ApplicationId=11a3a32b-28c1-4f28-97fb-4b113a61c1e9',
+    enableAutoRouteTracking: true,
+    enableDebug: true,
+		extensions: [reactPlugin],
+    },
+});
+appInsights.loadAppInsights();
+
+
+export default appInsights;
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -5,6 +5,26 @@
 
 import Layout from "@theme/Layout";
 import React from "react";
+import appInsights from "../appInsights";
+import {v4 as uuidv4} from "uuid";
+
+if (typeof window !== 'undefined') {
+	const userId = localStorage.getItem("userId") || uuidv4();
+	localStorage.setItem("userId", userId);
+	appInsights.context.user.id = userId;
+
+	const currentUrl = window.location.pathname;
+
+	let pageRoute = currentUrl;
+	if(currentUrl === "/" || currentUrl === "") {
+		pageRoute = window.location.href;
+	}else {
+		pageRoute = currentUrl;
+	}
+	appInsights.trackPageView({name: pageRoute});
+}
+
+appInsights.trackEvent({name: "UserActivity", properties: {action: "Viewed Page"}});
 
 import { Homepage } from "@site/src/components/home";
 
