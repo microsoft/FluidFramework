d1d894ea077db6c0c53bc539a46cee17421159a3
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2024-12-10 09:32:46 -0600

    Standardize one jest config

--- examples/service-clients/azure-client/external-controller/jest.config.cjs
+++ examples/service-clients/azure-client/external-controller/jest.config.cjs
@@ -6,8 +6,4 @@
 const getBaseJestConfig = require("../../../../getBaseJestConfig.cjs");
 const { name } = require("./package.json");
 const config = getBaseJestConfig(name);
-config.moduleNameMapper = {
-	// Remove explicit .js from local paths to allow jest to find the .ts* files
-	"^(\\.{1,2}/.*)\\.js$": "$1",
-};
 module.exports = config;
