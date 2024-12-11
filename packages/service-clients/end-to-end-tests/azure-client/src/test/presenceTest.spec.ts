56fa01af6fc925b674cff5ce4cf3f7182239259f
Willie Habimana <whabimana@microsoft.com>
2024-12-11 15:01:16 -0800

    policy check

--- packages/service-clients/end-to-end-tests/azure-client/src/test/presenceTest.spec.ts
+++ packages/service-clients/end-to-end-tests/azure-client/src/test/presenceTest.spec.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import { strict as assert } from "node:assert";
 import { fork, ChildProcess } from "node:child_process";
 
