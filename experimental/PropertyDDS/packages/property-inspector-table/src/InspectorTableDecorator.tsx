4629b9ecf36c2b8d919f06d70cf31e16e506ac3b
Abram Sanderson <absander@microsoft.com>
2024-12-10 18:20:37 -0800

    Remove other propertyDDS packages removed in main

--- experimental/PropertyDDS/packages/property-inspector-table/src/InspectorTableDecorator.tsx
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-// eslint-disable-next-line import/no-unassigned-import
-import "../assets/icons/SVGStoreIcons";
-
-export const InspectorTableDecorator = (storyFn) => storyFn();
