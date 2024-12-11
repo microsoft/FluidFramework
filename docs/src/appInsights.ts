280e23974b8ce97bc3826bcfb645561e987cb8e7
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-12-11 15:23:38 -0800

    Removed cookie banner and wcp-consent

--- docs/src/appInsights.ts
+++ docs/src/appInsights.ts
@@ -3,19 +3,19 @@
  * Licensed under the MIT License.
  */
 
-import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 const reactPlugin = new ReactPlugin();
 
 const appInsights = new ApplicationInsights({
-    config: {
-		connectionString:'InstrumentationKey=8bce9107-e3f7-445c-be87-153f7ecbbe47;IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/;ApplicationId=11a3a32b-28c1-4f28-97fb-4b113a61c1e9',
-    enableAutoRouteTracking: true,
-    enableDebug: true,
+	config: {
+		connectionString:
+			"InstrumentationKey=8bce9107-e3f7-445c-be87-153f7ecbbe47;IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/;ApplicationId=11a3a32b-28c1-4f28-97fb-4b113a61c1e9",
+		enableAutoRouteTracking: true,
+		enableDebug: true,
 		extensions: [reactPlugin],
-    },
+	},
 });
 appInsights.loadAppInsights();
-
 
 export default appInsights;
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -3,31 +3,25 @@
  * Licensed under the MIT License.
  */
 
- ///<reference types="@wcp/wcp-consent" />
 import Layout from "@theme/Layout";
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 import appInsights from "../appInsights";
-const CookieBanner = React.lazy(() => import("../components/CookieBanner"));
 
-import {v4 as uuidv4} from "uuid";
-
-if (typeof window !== 'undefined') {
-	const userId = localStorage.getItem("userId") || uuidv4();
+if (typeof window !== "undefined") {
+	const userId = localStorage.getItem("userId") ?? uuidv4();
 	localStorage.setItem("userId", userId);
 	appInsights.context.user.id = userId;
 
 	const currentUrl = window.location.pathname;
 
 	let pageRoute = currentUrl;
-	if(currentUrl === "/" || currentUrl === "") {
-		pageRoute = window.location.href;
-	}else {
-		pageRoute = currentUrl;
-	}
-	appInsights.trackPageView({name: pageRoute});
+	pageRoute = currentUrl ?? window.location.href;
+	appInsights.trackPageView({ name: pageRoute });
 }
 
-appInsights.trackEvent({name: "UserActivity", properties: {action: "Viewed Page"}});
+appInsights.trackEvent({ name: "UserActivity", properties: { action: "Viewed Page" } });
 
 import { Homepage } from "@site/src/components/home";
 
@@ -37,9 +31,7 @@ import { Homepage } from "@site/src/components/home";
 export default function Home(): React.ReactElement {
 	return (
 		<Layout>
-			<Suspense fallback={<div/>}>
-			<CookieBanner/>
-			</Suspense>
+			<Suspense fallback={<div />}></Suspense>
 			<Homepage />
 		</Layout>
 	);
