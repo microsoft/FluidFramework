f6c7d7fdb8d2f3ea12a147f61a9161c1d092f98c
Mark Fields <markfields@users.noreply.github.com>
2024-12-13 21:27:47 +0000

    Excuse the deprecation on internal usages

--- examples/apps/attributable-map/src/modelContainerRuntimeFactoryWithAttribution.ts
+++ examples/apps/attributable-map/src/modelContainerRuntimeFactoryWithAttribution.ts
@@ -12,12 +12,14 @@ import {
 	IRuntimeFactory,
 } from "@fluidframework/container-definitions/internal";
 import {
+	// eslint-disable-next-line import/no-deprecated -- ContainerRuntime class to be moved to internal scope
 	ContainerRuntime,
 	IContainerRuntimeOptions,
 } from "@fluidframework/container-runtime/internal";
 import { IContainerRuntime } from "@fluidframework/container-runtime-definitions/internal";
 import { NamedFluidDataStoreRegistryEntries } from "@fluidframework/runtime-definitions/internal";
 
+// eslint-disable-next-line import/no-deprecated
 const containerRuntimeWithAttribution = mixinAttributor(ContainerRuntime);
 
 /**
