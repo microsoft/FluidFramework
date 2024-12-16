2dbe145a15cd6bed0e5c0d4ec1cfce3626603cf0
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-12-16 12:06:53 -0800

    Remove script

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -2,12 +2,11 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-require('dotenv').config();
+
 import type { VersionOptions } from "@docusaurus/plugin-content-docs";
 import * as Preset from "@docusaurus/preset-classic";
 import type { Config } from "@docusaurus/types";
 import { themes as prismThemes } from "prism-react-renderer";
-
 
 import DocsVersions from "./config/docs-versions.mjs";
 
