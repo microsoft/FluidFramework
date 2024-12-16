f80e4a638da9c7892f8541d32a645a7ebe0db6d5
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-12-16 23:08:25 +0000

    chore: Add `DeepRequired` utility type

---
+++ tools/api-markdown-documenter/src/utilities/TypeUtilities.ts
@@ -0,0 +1,11 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * Recursive variant of the `Required<T>` utility type.
+ */
+export type DeepRequired<T> = {
+	[K in keyof T]: DeepRequired<T[K]>
+} & Required<T>;
