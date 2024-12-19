724d43ae976693e2c261b001037e96f6af173a8f
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-12-19 23:18:53 +0000

    remove: Another unneeded directory export

--- tools/api-markdown-documenter/src/api-item-transforms/configuration/index.ts
+++ tools/api-markdown-documenter/src/api-item-transforms/configuration/index.ts
@@ -9,13 +9,12 @@ export {
 	type ApiItemTransformationOptions,
 	getApiItemTransformationConfigurationWithDefaults,
 } from "./Configuration.js";
-export {
+export type {
 	// Consumers should not use this, it exists externally for documentation purposes only.
-	type DefaultDocumentationSuiteOptions,
-	type DocumentBoundaries,
-	type DocumentationSuiteOptions,
-	getDocumentationSuiteOptionsWithDefaults,
-	type HierarchyBoundaries,
+	DefaultDocumentationSuiteOptions,
+	DocumentBoundaries,
+	DocumentationSuiteOptions,
+	HierarchyBoundaries,
 } from "./DocumentationSuiteOptions.js";
 export type {
 	ApiItemTransformations,
