51848f993849f4ffa1fc910758dd40f38e918793
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2024-12-20 05:56:55 +0000

    change name

--- examples/apps/ai-collab/src/components/TaskGroup.tsx
+++ examples/apps/ai-collab/src/components/TaskGroup.tsx
@@ -135,7 +135,7 @@ export function TaskGroup(props: {
 		 *
 		 * @returns An event listener cleanup function
 		 */
-		const handleCommitApplied = props.treeView.events.on(
+		const treeEventHandler = props.treeView.events.on(
 			"commitApplied",
 			(commit: CommitMetadata, getRevertible?: RevertibleFactory) => {
 				if (getRevertible !== undefined) {
@@ -152,7 +152,7 @@ export function TaskGroup(props: {
 		);
 
 		return () => {
-			handleCommitApplied();
+			treeEventHandler();
 		};
 	}, [props.treeView.events, undoStack, redoStack]);
 
