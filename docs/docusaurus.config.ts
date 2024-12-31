4de7252f856c66facffc6ea885542b2b9540a3be
Wayne Ferrao <wayne.ferrao93@gmail.com>
2024-12-31 13:26:07 -0500

    spacing fix

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import dotenv from "dotenv";
 import type { VersionOptions } from "@docusaurus/plugin-content-docs";
 import * as Preset from "@docusaurus/preset-classic";
