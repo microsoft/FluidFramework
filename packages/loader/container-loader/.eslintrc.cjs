84a7b598dd2824ecdcb8b7732c823cffdd2c6f8f
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2025-01-02 20:06:02 +0000

    eslint(container-loader): Prefix container-loader before enabling no-unchecked-record-access

--- packages/loader/container-loader/.eslintrc.cjs
+++ packages/loader/container-loader/.eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
 		project: ["./tsconfig.json", "./src/test/tsconfig.json"],
 	},
 	rules: {
-		"@fluid-internal/fluid/no-unchecked-record-access": "warn",
+		"@fluid-internal/fluid/no-unchecked-record-access": "error",
 	},
 	overrides: [
 		{
