a41ec443f82cbf41e7ca71359fd73ac5baac6a00
Abram Sanderson <Abram.sanderson@gmail.com>
2025-01-06 08:36:21 -0800

    Update server/historian/packages/historian-base/src/services/simplifiedCustomDataRetriever.ts

--- server/historian/packages/historian-base/src/services/simplifiedCustomDataRetriever.ts
+++ server/historian/packages/historian-base/src/services/simplifiedCustomDataRetriever.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+ 
 import type { ITenantCustomData } from "@fluidframework/server-services-core";
 
 import type { ISimplifiedCustomDataRetriever } from "./definitions";
