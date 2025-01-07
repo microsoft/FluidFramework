8119e7f015d47f9711d07bd78bf3e8c3db1fdf02
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-01-07 19:12:33 +0000

    docs: Add missing copyright notice

--- tools/api-markdown-documenter/src/api-item-transforms/configuration/test/Hierarchy.test.ts
+++ tools/api-markdown-documenter/src/api-item-transforms/configuration/test/Hierarchy.test.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { ApiItemKind } from "@microsoft/api-extractor-model";
 import { expect } from "chai";
 
