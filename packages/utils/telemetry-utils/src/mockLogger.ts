237f04204d2ebd1cf7d80a5ec97fffcd5d8a55a4
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2025-01-06 18:04:43 -0600

    refactor(telemetry-utlis): Make MockLogger internal (#23473)
    ## Description
    
    Removes the `MockLogger` class from the `@alpha+@legacy` API surface. It
    has been deprecated for external use since 2024-06-18.
    
    Note: originally merged in
    https://github.com/microsoft/FluidFramework/pull/23010 and reverted in
    https://github.com/microsoft/FluidFramework/pull/23035 because it hadn't
    been announced previously as part of the breaking changes in 2.10.
    
    ## Breaking Changes
    
    - `MockLogger` is now removed from the `@alpha+@legacy` API surface,
    becoming only `@internal`.

--- packages/utils/telemetry-utils/src/mockLogger.ts
+++ packages/utils/telemetry-utils/src/mockLogger.ts
@@ -25,28 +25,7 @@ import type {
  * Records events sent to it, and then can walk back over those events, searching for a set of expected events to
  * match against the logged events.
  *
- * @deprecated
- *
- * This class is not intended for use outside of the `fluid-framework` repo, and will be removed from
- * package exports in the near future.
- *
- * Please migrate usages by either creating your own mock {@link @fluidframework/core-interfaces#ITelemetryBaseLogger}
- * implementation, or by copying this code as-is into your own repo.
- *
- * @privateRemarks TODO: When we are ready, this type should be made `internal`, and the deprecation notice should be removed.
- *
- * @deprecated
- *
- * This class is not intended for use outside of the `fluid-framework` repo, and will be removed from
- * package exports in the near future.
- *
- * Please migrate usages by either creating your own mock {@link @fluidframework/core-interfaces#ITelemetryBaseLogger}
- * implementation, or by copying this code as-is into your own repo.
- *
- * @privateRemarks TODO: When we are ready, this type should be made `internal`, and the deprecation notice should be removed.
- *
- * @legacy
- * @alpha
+ * @internal
  */
 export class MockLogger implements ITelemetryBaseLogger {
 	/**
