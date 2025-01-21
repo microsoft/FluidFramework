39954b2b94234f7508dbd7cdaf06d1b9859ae2ba
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-01-21 15:22:21 -0800

    feat(generate:assertTags): Enable per package assert tagging configuration (#23452)
    ## Description
    
    Refactor assert tagger so its relies much less on function side effects
    and global state.
    
    Make assert functions configuration per package instead of global.
    
    Remove exclusion of test packages: exclude them as needed using config.
    
    Configure tree to tag its "fail" function. This will enable a small
    bundle size improvement for tree.

---
+++ packages/test/assertTagging.config.mjs
@@ -0,0 +1,12 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * @type {import("@fluid-tools/build-cli").AssertTaggingConfig}
+ */
+export default {
+	// Disables assert tagging by listing an empty set of functions that should be tagged.
+	assertionFunctions: {},
+};
