99f594ced1a3961d346d137c06a727d632a4cb5b
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-01-21 10:58:45 -0800

    Note internalness

--- packages/common/core-utils/src/assert.ts
+++ packages/common/core-utils/src/assert.ts
@@ -21,6 +21,8 @@
  *
  * In cases where the assert is very unlikely to have an impact on production code but is still useful as documentation and for debugging, consider using `debugAssert` instead
  * to optimize bundle size.
+ *
+ * This API is not intended for use outside of the Fluid Framework client codebase: it will most likely be made internal in the future.
  * @privateRemarks
  * This should be deprecated (as a non internal API) then moved to purely internal.
  * When done the `debugAssert` reference above should be turned into a link.
