89bca77ad7953c65c4ccda330f2bd7f01c0577de
MarioJGMsoft <marioja@microsoft.com>
2025-01-22 18:36:09 +0000

    docs: updated comment in opDecompressor.ts

--- packages/runtime/container-runtime/src/opLifecycle/opDecompressor.ts
+++ packages/runtime/container-runtime/src/opLifecycle/opDecompressor.ts
@@ -30,6 +30,9 @@ interface IPackedContentsContents {
  * 2. Messages in the middle of a compressed batch will have neither batch metadata nor the compression property set
  * 3. The final message of a batch will have batch metadata set to false
  * 4. An individually compressed op will have undefined batch metadata and compression set to true
+ *
+ * Compressed batches are always a single message but this class is able to receive a compressed batch with multiple messages
+ * because we need that functionality for back compat
  */
 export class OpDecompressor {
 	private activeBatch = false;
