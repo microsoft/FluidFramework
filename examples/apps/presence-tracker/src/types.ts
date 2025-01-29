e4bc301e118629297c566c6de61d44ecfd1ab219
Tyler Butler <tylerbu@microsoft.com>
2025-01-29 13:13:43 -0800

    add validator and try to simulate some failures

---
+++ examples/apps/presence-tracker/src/types.ts
@@ -0,0 +1,31 @@
+import type { ValueTypeSchemaValidator } from "@fluidframework/presence/alpha";
+import { type Static, Type } from "@sinclair/typebox";
+// eslint-disable-next-line import/no-internal-modules
+import { Value } from "@sinclair/typebox/value";
+
+/**
+ * IMousePosition is the data that individual session clients share via presence.
+ */
+export type IMousePosition = Static<typeof IMousePosition>;
+
+export const IMousePosition = Type.Object({
+	x: Type.Readonly(Type.Number()),
+	y: Type.Readonly(Type.Number()),
+});
+
+export const IMousePositionValidator: ValueTypeSchemaValidator<IMousePosition> = (
+	maybeValid,
+) => {
+	window.alert("Validator called");
+	console.debug(`Validator called with`, maybeValid);
+	// const isValid = Value.Parse(IMousePosition, maybeValid);
+
+	// Will throw if the value is invalid.
+	Value.Assert(IMousePosition, maybeValid);
+
+	// Will throw if the value is invalid.
+	// const parsed = Value.Parse(IMousePosition, maybeValid);
+
+	// console.debug(parsed);
+	return maybeValid;
+};
