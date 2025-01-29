51dc7097b41e00e67fa6dee603889b5f1efc07b4
jzaffiro <110866475+jzaffiro@users.noreply.github.com>
2025-01-29 22:45:33 +0000

    Use existing op functions

--- packages/dds/merge-tree/src/test/obliterateOperations.ts
+++ packages/dds/merge-tree/src/test/obliterateOperations.ts
@@ -5,18 +5,11 @@
 
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
-import assert from "node:assert";
-
 import type { IRandom } from "@fluid-private/stochastic-test-utils";
 
-import { walkAllChildSegments } from "../mergeTreeNodeWalk.js";
-import type { ISegmentPrivate } from "../mergeTreeNodes.js";
-import { ReferenceType } from "../ops.js";
-import { toRemovalInfo, toMoveInfo } from "../segmentInfos.js";
 import { InteriorSequencePlace, Side } from "../sequencePlace.js";
-import { TextSegment } from "../textSegment.js";
 
-import type { TestOperation } from "./mergeTreeOperationRunner.js";
+import { annotateRange, type TestOperation } from "./mergeTreeOperationRunner.js";
 import type { TestClient } from "./testClient.js";
 
 export const posInField = (
@@ -124,49 +117,12 @@ export const insertAvoidField: TestOperation = (
 	opEnd: number,
 	random: IRandom,
 ) => {
-	const segs: ISegmentPrivate[] = [];
-	// gather all the segments at the pos, including removed segments
-	walkAllChildSegments(client.mergeTree.root, (seg) => {
-		const pos = client.getPosition(seg);
-		if (pos >= opStart) {
-			if (pos <= opStart) {
-				segs.push(seg);
-				return true;
-			}
-			return false;
-		}
-		return true;
-	});
-	if (segs.length > 0) {
-		const text = client.longClientId!.repeat(random.integer(1, 3));
-		const seg = random.pick(segs);
-		const movedOrRemoved = toRemovalInfo(seg) ?? toMoveInfo(seg);
-		const randomOffset = random.integer(0, seg.cachedLength - 1);
-		const initialPos = client.getPosition(seg) + randomOffset;
-		const endpoints = posInField(client, initialPos);
-
-		let offset = randomOffset;
-		if (endpoints !== undefined) {
-			const startSeg = client.getContainingSegment(endpoints.startPos);
-			assert(startSeg.offset !== undefined, "offset should be defined");
-			offset = startSeg.offset;
-		}
-
-		const lref = client.createLocalReferencePosition(
-			seg,
-			movedOrRemoved ? 0 : offset,
-			movedOrRemoved
-				? ReferenceType.SlideOnRemove
-				: random.pick([
-						ReferenceType.Simple,
-						ReferenceType.SlideOnRemove,
-						ReferenceType.Transient,
-					]),
-			undefined,
-		);
-
-		return client.insertAtReferencePositionLocal(lref, TextSegment.make(text));
+	let start = opStart;
+	const endpoints = posInField(client, opStart);
+	if (endpoints !== undefined) {
+		start = endpoints.startPos;
 	}
+	return client.insertTextLocal(start, client.longClientId!.repeat(random.integer(1, 3)));
 };
 
 export const removeWithField: TestOperation = (
@@ -174,13 +130,14 @@ export const removeWithField: TestOperation = (
 	opStart: number,
 	opEnd: number,
 ) => {
+	let start = opStart;
+	let end = opEnd;
 	const fieldEndpoints = getFieldEndpoints(client, opStart, opEnd);
-	if (fieldEndpoints === undefined) {
-		return client.removeRangeLocal(opStart, opEnd);
-	} else {
-		const { startPos, endPos } = fieldEndpoints;
-		return client.removeRangeLocal(startPos, endPos + 1);
+	if (fieldEndpoints !== undefined) {
+		start = fieldEndpoints.startPos;
+		end = fieldEndpoints.endPos + 1;
 	}
+	return client.removeRangeLocal(start, end);
 };
 
 export const annotateWithField: TestOperation = (
@@ -189,27 +146,12 @@ export const annotateWithField: TestOperation = (
 	opEnd: number,
 	random: IRandom,
 ) => {
-	let start: number | undefined;
-	let end: number | undefined;
+	let start = opStart;
+	let end = opEnd;
 	const fieldEndpoints = getFieldEndpoints(client, opStart, opEnd);
 	if (fieldEndpoints !== undefined) {
 		start = fieldEndpoints.startPos;
 		end = fieldEndpoints.endPos + 1;
 	}
-
-	if (random.bool()) {
-		return client.annotateRangeLocal(start ?? opStart, end ?? opEnd, {
-			[random.integer(1, 5)]: client.longClientId,
-		});
-	} else {
-		const max = random.pick([undefined, random.integer(-10, 100)]);
-		const min = random.pick([undefined, random.integer(-100, 10)]);
-		return client.annotateAdjustRangeLocal(start ?? opStart, end ?? opEnd, {
-			[random.integer(0, 2).toString()]: {
-				delta: random.integer(-5, 5),
-				min: (min ?? max ?? 0) > (max ?? 0) ? undefined : min,
-				max,
-			},
-		});
-	}
+	return annotateRange(client, start, end, random);
 };
