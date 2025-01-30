8f27f286728fd0d070b305f5aba26184e16b8dc5
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-01-30 20:07:18 +0000

    tools: Set temporarily disabled rules to warnings

--- packages/runtime/container-runtime/.eslintrc.cjs
+++ packages/runtime/container-runtime/.eslintrc.cjs
@@ -10,11 +10,11 @@ module.exports = {
 	},
 	rules: {
 		"@typescript-eslint/strict-boolean-expressions": "off",
-		"@fluid-internal/fluid/no-unchecked-record-access": "warn",
 
 		// TODO: fix violations and remove overrides
+		"@fluid-internal/fluid/no-unchecked-record-access": "warn",
 		"require-atomic-updates": "warn",
-		"unicorn/no-array-reduce": "off",
+		"unicorn/no-array-reduce": "warn",
 
 		// False positives on non-array `push` methods.
 		// TODO:AB#28686: remove this override once this rule has been disabled in the root config.
@@ -26,7 +26,7 @@ module.exports = {
 			files: ["*.spec.ts", "src/test/**"],
 			rules: {
 				// TODO: remove these overrides and fix violations
-				"@typescript-eslint/explicit-function-return-type": "off",
+				"@typescript-eslint/explicit-function-return-type": "warn",
 				"unicorn/error-message": "warn",
 
 				// Test files are run in node only so additional node libraries can be used.
