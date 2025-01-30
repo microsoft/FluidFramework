2aca98a663bd1327a4fe8b13839151d5b8dc2308
Tommy Brosman <tbrosman@microsoft.com>
2025-01-29 16:58:56 -0800

    Minor tweak to a comment.

--- packages/common/client-utils/src/performanceIsomorphic.ts
+++ packages/common/client-utils/src/performanceIsomorphic.ts
@@ -4,8 +4,11 @@
  */
 
 /**
- * Exposes `Performance.now()` in both Node and browser environments. The performance API is available as an
- * attribute on the `WindowOrWorkerGlobalScope` object, which `globalThis` points to.
+ * Exposes `Performance.now()` in both Node and browser environments.
+ *
+ * @remarks
+ *
+ * The performance API is available as an attribute on the `WindowOrWorkerGlobalScope` object which `globalThis` points to.
  * - The [global `performance` attribute](https://w3c.github.io/hr-time/#the-performance-attribute)
  * - [`globalThis`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis)
  *
