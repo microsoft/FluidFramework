786b6f364122ddd6cac6286ee0ca6f741c173fe6
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-01-30 01:16:27 +0000

    style: Prettier

--- docs/infra/api-markdown-documenter/render-api-documentation.mjs
+++ docs/infra/api-markdown-documenter/render-api-documentation.mjs
@@ -149,7 +149,11 @@ export async function renderApiDocumentation(inputDir, outputDir, uriRootDir, ap
 
 				// Skip packages that are published, but are not intended for direct public consumption.
 				// TODO: Also skip `@fluid-internal` packages once we no longer have public, user-facing APIs that reference their contents.
-				if (["@fluid-example", "@fluid-experimental", "@fluid-private"].includes(packageScope)) {
+				if (
+					["@fluid-example", "@fluid-experimental", "@fluid-private"].includes(
+						packageScope,
+					)
+				) {
 					return true;
 				}
 			}
