015f8b5d39d6ddad5fcd14032ddb41f3e9e00e57
Willie Habimana <whabimana@microsoft.com>
2025-01-30 06:05:41 -0800

    policy check

--- packages/framework/presence/src/test/latestValueManager.spec.ts
+++ packages/framework/presence/src/test/latestValueManager.spec.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import { strict as assert } from "node:assert";
 
 import { createPresenceManager } from "../presenceManager.js";
