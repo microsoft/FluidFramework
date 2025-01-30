2e61586270e4f63b0c44622b6341f319dd7fb91e
Jason Hartman <jasonha@microsoft.com>
2025-01-30 10:23:44 -0800

    improvement(client): remove try-catch and refactor for style and naming preferences

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,11 +1,19 @@
-// .pnpmfile.cjs is a pnpm hook file that allows modification of package.json content
-// (in memory) of the packages that are being installed.
-// https://pnpm.io/pnpmfile
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
 
-// This implementation is based on https://gist.github.com/dvins/33b8fb52480149d37cdeb98890244c5b.
-// Changes:
-//  - Added support for complete dependency removal (specify null for newVersion)
-//  - Specify remapPeerDependencies for local needs
+/*
+ * .pnpmfile.cjs is a pnpm hook file that allows modification of package.json content
+ * (in memory) of the packages that are being installed.
+ * https://pnpm.io/pnpmfile
+ *
+ * This implementation is based on https://gist.github.com/dvins/33b8fb52480149d37cdeb98890244c5b.
+ * Changes:
+ *  - Added support for complete dependency removal (specify null for newVersion)
+ *  - Apply stylistic and naming preferences and comment cleanup
+ *  - Specify remapPeerDependencies for local needs
+ */
 
 //@ts-check
 
@@ -22,40 +30,47 @@ const remapPeerDependencies = [
 	// It should have been an optional peer dependency. We just remove it.
 	{
 		package: "@fluidframework/azure-client",
-		packageVersion: "1.",
+		packageVersionPrefix: "1.",
 		peerDependency: "fluid-framework",
 		newVersion: null,
 	},
 ];
 
 function overridesPeerDependencies(pkg) {
-	if (pkg.peerDependencies) {
-		remapPeerDependencies.map((dep) => {
-			if (pkg.name === dep.package && pkg.version.startsWith(dep.packageVersion)) {
-				console.log(`  - Checking ${pkg.name}@${pkg.version}`); // , pkg.peerDependencies);
+	if (!pkg.peerDependencies) {
+		return;
+	}
 
-				if (dep.peerDependency in pkg.peerDependencies) {
-					try {
-						console.log(
-							`    - Overriding ${pkg.name}@${pkg.version} peerDependency ${dep.peerDependency}@${pkg.peerDependencies[dep.peerDependency]}`,
-						);
+	const applicableRemapPeerDependencies = remapPeerDependencies.filter(
+		(remap) =>
+			remap.package === pkg.name && pkg.version.startsWith(remap.packageVersionPrefix),
+	);
 
-						// First add a new dependency to the package and then remove the peer dependency, if defined.
-						if (dep.newVersion) {
-							// This approach has the added advantage that scoped overrides should now work, too.
-							pkg.dependencies[dep.peerDependency] = dep.newVersion;
-						}
-						delete pkg.peerDependencies[dep.peerDependency];
+	if (applicableRemapPeerDependencies.length === 0) {
+		return;
+	}
 
-						console.log(
-							`      - Overrode ${pkg.name}@${pkg.version} peerDependency ${dep.peerDependency}@${pkg.dependencies[dep.peerDependency]}`,
-						);
-					} catch (err) {
-						console.error(err);
-					}
-				}
+	console.log(`  - Checking ${pkg.name}@${pkg.version}`);
+	for (const dep of applicableRemapPeerDependencies) {
+		if (dep.peerDependency in pkg.peerDependencies) {
+			console.log(
+				`    - Overriding ${pkg.name}@${pkg.version} peerDependency ${dep.peerDependency}@${pkg.peerDependencies[dep.peerDependency]}`,
+			);
+
+			// First add a new dependency to the package, if defined, and then remove the peer dependency.
+			if (dep.newVersion) {
+				// This approach has the added advantage that scoped overrides should now work, too.
+				pkg.dependencies[dep.peerDependency] = dep.newVersion;
 			}
-		});
+			delete pkg.peerDependencies[dep.peerDependency];
+
+			const newDep = pkg.dependencies[dep.peerDependency];
+			console.log(
+				newDep
+					? `      - Overrode ${pkg.name}@${pkg.version} peerDependency to ${dep.peerDependency}@${newDep} (as full dependency)`
+					: `      - Removed ${pkg.name}@${pkg.version} peerDependency`,
+			);
+		}
 	}
 }
 
