0ee37184dcaa3cb238efca99e88fb1a87b32cddc
Rishhi Balakrishnan <107130183+RishhiB@users.noreply.github.com>
2025-01-31 16:55:03 +0000

    remove map packages

--- packages/dds/map/.eslintrc.cjs
+++ packages/dds/map/.eslintrc.cjs
@@ -13,7 +13,8 @@ module.exports = {
 		"@typescript-eslint/strict-boolean-expressions": "off",
 
 		// TODO: consider re-enabling once we have addressed how this rule conflicts with our error codes.
-		"unicorn/numeric-separators-style": "off"
+		"unicorn/numeric-separators-style": "off",
+		"@fluid-internal/fluid/no-unchecked-record-access": "warn",
 	},
 	overrides: [
 		{
