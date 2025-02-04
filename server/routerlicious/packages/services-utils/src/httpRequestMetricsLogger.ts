09e995b45c78ff623753b44450449a84b25b51a0
Dhruv Verma <dhruvverma@microsoft.com>
2025-02-04 13:35:06 +0530

    Fixed policy check

--- server/routerlicious/packages/services-utils/src/httpRequestMetricsLogger.ts
+++ server/routerlicious/packages/services-utils/src/httpRequestMetricsLogger.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import type { IBasicRestWrapperMetricProps } from "@fluidframework/server-services-client";
 import {
 	CommonProperties,
