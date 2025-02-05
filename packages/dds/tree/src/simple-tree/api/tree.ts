e1ae2c86f772fd0505e796c9e149720b754bc7d7
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-02-05 12:20:00 -0800

    Better comments

--- packages/dds/tree/src/simple-tree/api/tree.ts
+++ packages/dds/tree/src/simple-tree/api/tree.ts
@@ -105,12 +105,14 @@ export interface ITree extends ViewableTree, IFluidLoadable {}
 /**
  * {@link ITree} extended with some alpha APIs.
  * @privateRemarks
- * Promote this to alpha.
+ * TODO: Promote this to alpha.
  * @internal
  */
 export interface ITreeAlpha extends ITree {
 	/**
 	 * Exports root in the same format as {@link TreeAlpha.(exportVerbose:1)} using stored keys.
+	 * @remarks
+	 * This is `undefined` iff the root field is empty (this can only happen if the root field is optional).
 	 */
 	exportVerbose(): VerboseTree | undefined;
 
