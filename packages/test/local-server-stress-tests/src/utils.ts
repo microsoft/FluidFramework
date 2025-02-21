1aef1461019ab9c2947767341cf231932efbb963
Tony Murphy <anthony.murphy@microsoft.com>
2025-02-21 09:34:33 -0800

    Update packages/test/local-server-stress-tests/src/utils.ts

--- packages/test/local-server-stress-tests/src/utils.ts
+++ packages/test/local-server-stress-tests/src/utils.ts
@@ -7,9 +7,7 @@ export function makeUnreachableCodePathProxy<T extends object>(name: string): T 
 	// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 	return new Proxy({} as T, {
 		get: (): never => {
-			throw new Error(
-				`Unexpected read of '${name}:' this indicates a bug in the DDS eventual consistency harness.`,
-			);
+			throw new Error(`Unexpected read of '${name}:' this indicates a bug in the harness.`);
 		},
 	});
 }
