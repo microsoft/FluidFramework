7ab58bfe5a63d5efc2f9c62f07e9a1dc0e9f3a85
Abram Sanderson <Abram.Sanderson@microsoft.com>
2025-02-28 11:17:43 -0800

    [bump] client: 2.22.1 => 2.22.2 (patch) (#23945)
    ## Description
    
    Bump client to version 2.22.2 following the release of 2.22.1. Changes
    stem from:
    
    ```
    flub release -g client
    ```
    
    Co-authored-by: Abram Sanderson <absander@microsoft.com>

--- packages/dds/matrix/src/packageVersion.ts
+++ packages/dds/matrix/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/matrix";
-export const pkgVersion = "2.22.1";
+export const pkgVersion = "2.22.2";
--- packages/tools/devtools/devtools-core/src/packageVersion.ts
+++ packages/tools/devtools/devtools-core/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/devtools-core";
-export const pkgVersion = "2.22.1";
+export const pkgVersion = "2.22.2";
