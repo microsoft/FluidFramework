4e3ec21e95b493b8106385bb5af9c17651517e22
Tony Murphy <anthony.murphy@microsoft.com>
2025-03-03 15:03:23 -0800

    skip failing seeds

--- packages/test/local-server-stress-tests/src/test/localServerStress.spec.ts
+++ packages/test/local-server-stress-tests/src/test/localServerStress.spec.ts
@@ -116,7 +116,7 @@ describe("Local Server Stress", () => {
 		defaultTestCount: 100,
 		// skipMinimization: true,
 		// Uncomment to replay a particular seed.
-		replay: [0, 13, 28, 30, 36, 37, 44, 45, 54, 55, 56, 76, 90, 96, 99],
+		skip: [0, 13, 28, 30, 36, 37, 44, 45, 54, 55, 56, 76, 90, 96, 99],
 		// only: [28],
 		saveFailures,
 		// saveSuccesses,
