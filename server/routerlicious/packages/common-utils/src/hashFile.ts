3fbb74b03f732dca0a9f27f6b377d801329ac241
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2025-03-06 18:33:03 +0000

    Remove commented code

--- server/routerlicious/packages/common-utils/src/hashFile.ts
+++ server/routerlicious/packages/common-utils/src/hashFile.ts
@@ -5,44 +5,8 @@
 
 // eslint-disable-next-line import/no-internal-modules
 import sha1 from "sha.js/sha1";
-// // eslint-disable-next-line import/no-internal-modules
-// import sha256 from "sha.js/sha256";
 
 import { IsoBuffer } from "./buffer";
-
-// /**
-//  * Hash a file. Consistent within a session, but should not be persisted and
-//  * is not consistent with git.
-//  * If called under an insecure context for a browser, this will fallback to
-//  * using the node implementation.
-//  *
-//  * @param file - The contents of the file in a buffer.
-//  * @param algorithm - The hash algorithm to use, artificially constrained by what is used internally.
-//  * @param hashEncoding - The encoding of the returned hash, also artificially constrained.
-//  * @returns The hash of the content of the buffer.
-//  *
-//  * @deprecated Moved to the `@fluidframework-internal/client-utils` package.
-//  * @internal
-//  */
-// export async function hashFile(
-// 	file: IsoBuffer,
-// 	algorithm: "SHA-1" | "SHA-256" = "SHA-1",
-// 	hashEncoding: "hex" | "base64" = "hex",
-// ): Promise<string> {
-// 	let engine;
-// 	// eslint-disable-next-line default-case
-// 	switch (algorithm) {
-// 		case "SHA-1": {
-// 			engine = new sha1();
-// 			break;
-// 		}
-// 		case "SHA-256": {
-// 			engine = new sha256();
-// 			break;
-// 		}
-// 	}
-// 	return engine.update(file).digest(hashEncoding) as string;
-// }
 
 /**
  * Create a github hash (Github hashes the string with blob and size)
