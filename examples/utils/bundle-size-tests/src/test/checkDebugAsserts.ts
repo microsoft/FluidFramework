614ddd596d19a176e9b952397302fb508c7091f7
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-03-10 11:34:32 -0700

    Setup and use mocha for bundle testing

--- examples/utils/bundle-size-tests/src/test/checkDebugAsserts.ts
+++ examples/utils/bundle-size-tests/src/test/checkDebugAsserts.ts
@@ -3,18 +3,16 @@
  * Licensed under the MIT License.
  */
 
-// This file must be run after webpack.
-
-// eslint-disable-next-line spaced-comment
-/// <reference types="node" />
-
 import { strict as assert } from "node:assert";
 import { readFileSync } from "node:fs";
 
-const bundle = readFileSync("./dist/debugAssert.js", "utf-8");
+describe("checkDebugAsserts", () => {
+	it("examples", () => {
+		// This file must be run after webpack.
+		const bundle = readFileSync("./dist/debugAssert.js", "utf-8");
 
-assert.match(bundle, /kept 1/);
-assert.match(bundle, /kept 2/);
-assert.doesNotMatch(bundle, /removed in production/);
-
-console.log("Validated expectation for DebugAsserts");
+		assert.match(bundle, /kept 1/);
+		assert.match(bundle, /kept 2/);
+		assert.doesNotMatch(bundle, /removed in production/);
+	});
+});
