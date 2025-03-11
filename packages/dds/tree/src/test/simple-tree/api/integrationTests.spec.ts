c3db051e6ffaac07a797ed87e986beacfa451190
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-03-11 11:51:05 -0700

    Add simple-tree API integration tests, with one failing test

---
+++ packages/dds/tree/src/test/simple-tree/api/integrationTests.spec.ts
@@ -0,0 +1,37 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "node:assert";
+
+import { SchemaFactory } from "../../../simple-tree/index.js";
+import type {
+	ValidateRecursiveSchema,
+	// eslint-disable-next-line import/no-internal-modules
+} from "../../../simple-tree/api/schemaFactoryRecursive.js";
+import { validateUsageError } from "../../utils.js";
+
+const sf = new SchemaFactory("integration");
+
+describe("simple-tree API integration tests", () => {
+	// TODO:
+	// This case should produce a usage error (and thus allow this test to be un-skipped).
+	// Currently this tests hangs forever, not even being terminated by the mocha timeout.
+	// Depending on where the error is detected, tests for recursive maps, arrays and co-recursive cases may be needed.
+	it.skip("making a recursive unhydrated object node errors", () => {
+		class O extends sf.objectRecursive("O", {
+			recursive: sf.optionalRecursive([() => O]),
+		}) {}
+		{
+			type _check = ValidateRecursiveSchema<typeof O>;
+		}
+		const obj = new O({ recursive: undefined });
+		assert.throws(
+			() => {
+				obj.recursive = obj;
+			},
+			validateUsageError(/recursive/),
+		);
+	});
+});
