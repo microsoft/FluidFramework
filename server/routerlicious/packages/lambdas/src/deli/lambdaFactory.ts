cd61686dff1091037de490a6d60f69e14c04eb50
Dhruv Verma <dhruvverma@microsoft.com>
2025-03-13 14:42:24 -0400

    Added support for skipping deleteSummary call from Deli incase EC is beyond TTL

--- server/routerlicious/packages/lambdas/src/deli/lambdaFactory.ts
+++ server/routerlicious/packages/lambdas/src/deli/lambdaFactory.ts
@@ -68,6 +68,7 @@ export class DeliLambdaFactory
 		private readonly reverseProducer: IProducer,
 		private readonly serviceConfiguration: IServiceConfiguration,
 		private readonly clusterDrainingChecker?: IClusterDrainingChecker | undefined,
+		private readonly ephemeralDocumentTTLSec?: number,
 	) {
 		super();
 	}
@@ -227,14 +228,27 @@ export class DeliLambdaFactory
 				) {
 					if (document?.isEphemeralContainer) {
 						if (this.serviceConfiguration.deli.enableEphemeralContainerSummaryCleanup) {
-							// Call to historian to delete summaries
-							await requestWithRetry(
-								async () => gitManager.deleteSummary(false),
-								"deliLambda_onClose" /* callName */,
-								baseLumberjackProperties /* telemetryProperties */,
-								undefined /* shouldRetry */, // The default retry function will ensure NetworkErrors are retried only if canRetry is true and isFatal is false
-								3 /* maxRetries */,
-							);
+							if (this.isEphemeralDocumentWithinTtl(document)) {
+								// Call to historian to delete summaries
+								await requestWithRetry(
+									async () => gitManager.deleteSummary(false),
+									"deliLambda_onClose" /* callName */,
+									baseLumberjackProperties /* telemetryProperties */,
+									undefined /* shouldRetry */, // The default retry function will ensure NetworkErrors are retried only if canRetry is true and isFatal is false
+									3 /* maxRetries */,
+								);
+							} else {
+								Lumberjack.info(
+									"Ephemeral container TTL has expired, not calling deleteSummary",
+									{
+										...baseLumberjackProperties,
+										documentCreationTime: document.createTime,
+										documentExpirationTime:
+											document.createTime +
+											(this.ephemeralDocumentTTLSec ?? 0) * 1000,
+									},
+								);
+							}
 						}
 
 						// Delete the document metadata, soft or hard depending on the configuration
@@ -386,6 +400,19 @@ export class DeliLambdaFactory
 			signalProducerClosedP,
 			reverseProducerClosedP,
 		]);
+	}
+
+	private isEphemeralDocumentWithinTtl(document: IDocument): boolean {
+		if (this.ephemeralDocumentTTLSec === undefined) {
+			// If we do not have a TTL value available, then we assume that the document is within TTL
+			// to keep the behavior consistent with the existing implementation.
+			return true;
+		}
+
+		const currentTime = Date.now();
+		const documentExpirationTime = document.createTime + this.ephemeralDocumentTTLSec * 1000;
+
+		return currentTime <= documentExpirationTime;
 	}
 
 	// Fetches last durable deli state from summary. Returns undefined if not present.
