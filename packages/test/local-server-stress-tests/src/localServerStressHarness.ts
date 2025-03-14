a062624179d669a8d88d5f1d8d134364bd48827b
Tony Murphy <anthony.murphy@microsoft.com>
2025-03-14 15:10:18 -0700

    Local Server Stress: Fix race with dispose/close during synchronize

--- packages/test/local-server-stress-tests/src/localServerStressHarness.ts
+++ packages/test/local-server-stress-tests/src/localServerStressHarness.ts
@@ -774,13 +774,19 @@ async function loadClient(
 }
 
 async function synchronizeClients(connectedClients: Client[]) {
-	const rejects = new Map<Client, ((reason?: any) => void)[]>(
-		connectedClients.map((c) => [c, []]),
-	);
+	const closeOrDispose = new Map<
+		Client,
+		{ error?: Error; rejects: ((reason?: any) => void)[] }
+	>();
 
 	const cleanUps: (() => void)[] = [];
 	for (const c of connectedClients) {
-		const rejector = (err) => rejects.get(c)?.forEach((r) => r(err));
+		const rejector = (error) => {
+			const entry = closeOrDispose.get(c) ?? { rejects: [] };
+			entry.error ??= error;
+			closeOrDispose.set(c, entry);
+			entry.rejects.forEach((r) => r(entry.error));
+		};
 		c.container.once("closed", rejector);
 		c.container.once("disposed", rejector);
 		cleanUps.push(() => {
@@ -793,9 +799,13 @@ async function synchronizeClients(connectedClients: Client[]) {
 			connectedClients.map(async (c) =>
 				timeoutPromise(
 					(resolve, reject) => {
-						if (c.container.connectionState !== ConnectionState.Connected) {
+						const entry = closeOrDispose.get(c) ?? { rejects: [] };
+						closeOrDispose.set(c, entry);
+						if (entry.error) {
+							reject(entry.error);
+						} else if (c.container.connectionState !== ConnectionState.Connected) {
 							c.container.once("connected", () => resolve());
-							rejects.get(c)?.push(reject);
+							entry.rejects.push(reject);
 						} else {
 							resolve();
 						}
@@ -811,9 +821,13 @@ async function synchronizeClients(connectedClients: Client[]) {
 			connectedClients.map(async (c) =>
 				timeoutPromise(
 					(resolve, reject) => {
-						if (c.container.isDirty) {
+						const entry = closeOrDispose.get(c) ?? { rejects: [] };
+						closeOrDispose.set(c, entry);
+						if (entry.error) {
+							reject(entry.error);
+						} else if (c.container.isDirty) {
 							c.container.once("saved", () => resolve());
-							rejects.get(c)?.push(reject);
+							entry.rejects.push(reject);
 						} else {
 							resolve();
 						}
@@ -828,8 +842,12 @@ async function synchronizeClients(connectedClients: Client[]) {
 			...connectedClients.map((c) => c.container.deltaManager.lastKnownSeqNumber),
 		);
 
-		const makeOpHandler = (c: Client, resolve: () => void, reject: () => void) => {
-			if (c.container.deltaManager.lastKnownSeqNumber < maxSeq) {
+		const makeOpHandler = (c: Client, resolve: () => void, reject: (error) => void) => {
+			const entry = closeOrDispose.get(c) ?? { rejects: [] };
+			closeOrDispose.set(c, entry);
+			if (entry.error) {
+				reject(entry.error);
+			} else if (c.container.deltaManager.lastKnownSeqNumber < maxSeq) {
 				const handler = (msg) => {
 					if (msg.sequenceNumber >= maxSeq) {
 						c.container.off("op", handler);
@@ -837,7 +855,7 @@ async function synchronizeClients(connectedClients: Client[]) {
 					}
 				};
 				c.container.on("op", handler);
-				rejects.get(c)?.push(reject);
+				entry.rejects.push(reject);
 			} else {
 				resolve();
 			}
