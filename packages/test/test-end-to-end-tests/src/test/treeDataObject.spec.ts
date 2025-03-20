1d6a9cee6c2f115779249c195b9527d022f5bb53
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-03-20 00:01:21 +0000

    test: Add initialization tests

---
+++ packages/test/test-end-to-end-tests/src/test/treeDataObject.spec.ts
@@ -0,0 +1,91 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "assert";
+
+import { describeCompat } from "@fluid-private/test-version-utils";
+import {
+	ContainerRuntimeFactoryWithDefaultDataStore,
+	PureDataObjectFactory,
+	TreeDataObject,
+} from "@fluidframework/aqueduct/internal";
+import type { IContainer } from "@fluidframework/container-definitions/internal";
+import type { IContainerRuntimeOptions } from "@fluidframework/container-runtime/internal";
+import {
+	createContainerRuntimeFactoryWithDefaultDataStore,
+	getContainerEntryPointBackCompat,
+	type ITestObjectProvider,
+} from "@fluidframework/test-utils/internal";
+import {
+	SchemaFactory,
+	SharedTree,
+	TreeViewConfiguration,
+} from "@fluidframework/tree/internal";
+
+const schemaFactory = new SchemaFactory("test");
+class TestSchema extends schemaFactory.object("TestSchema", {
+	foo: [schemaFactory.string, schemaFactory.number],
+}) {}
+
+const treeViewConfig = new TreeViewConfiguration({ schema: TestSchema });
+
+class TestTreeDataObject extends TreeDataObject<typeof TestSchema> {
+	public readonly config = treeViewConfig;
+
+	public override async initializingFirstTime(): Promise<void> {
+		assert(this.tree.compatibility.canInitialize);
+		this.tree.initialize({ foo: "Hello world" });
+	}
+
+	public static readonly type = "TestTreeDataObject";
+
+	public static getFactory(): PureDataObjectFactory<TestTreeDataObject> {
+		return TestTreeDataObject.factory;
+	}
+
+	private static readonly factory = new PureDataObjectFactory(
+		TestTreeDataObject.type,
+		TestTreeDataObject,
+		[SharedTree.getFactory()],
+		{},
+	);
+}
+
+describeCompat.only("TreeDataObject", "NoCompat", (getTestObjectProvider) => {
+	// Runtime ID compression is required to use SharedTree.
+	const runtimeOptions: IContainerRuntimeOptions = {
+		enableRuntimeIdCompressor: "on",
+	};
+
+	const runtimeFactory = createContainerRuntimeFactoryWithDefaultDataStore(
+		ContainerRuntimeFactoryWithDefaultDataStore,
+		{
+			defaultFactory: TestTreeDataObject.getFactory(),
+			registryEntries: [[TestTreeDataObject.type, TestTreeDataObject.getFactory()]],
+			runtimeOptions,
+		},
+	);
+
+	let provider: ITestObjectProvider;
+	let container: IContainer;
+	beforeEach(async () => {
+		provider = getTestObjectProvider();
+		container = await provider.createContainer(runtimeFactory);
+	});
+
+	it("First time initialization", async () => {
+		const dataObject = await getContainerEntryPointBackCompat<TestTreeDataObject>(container);
+		assert.deepEqual(dataObject.tree.root.foo, "Hello world");
+	});
+
+	it("Load existing", async () => {
+		// Load second container and check data.
+		const container2 = await provider.loadContainer(runtimeFactory);
+
+		const container2DataObject =
+			await getContainerEntryPointBackCompat<TestTreeDataObject>(container2);
+		assert.deepEqual(container2DataObject.tree.root.foo, "Hello world");
+	});
+});
