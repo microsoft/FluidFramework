1c16d7c0ad11dbae1178e13f2e91f225e0cb2479
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-03-24 12:23:30 -0700

    Fix nondeterminstic api report and comments in new file

--- packages/dds/tree/src/sharedTreeAttributes.ts
+++ packages/dds/tree/src/sharedTreeAttributes.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import type { IChannelAttributes } from "@fluidframework/datastore-definitions/internal";
 import { pkgVersion } from "./packageVersion.js";
 
@@ -6,14 +11,13 @@ import { pkgVersion } from "./packageVersion.js";
  * @alpha
  * @legacy
  */
-
 export const SharedTreeFactoryType = "https://graph.microsoft.com/types/tree";
+
 /**
  * {@inheritDoc @fluidframework/shared-object-base#ISharedObjectFactory.attributes}
  * @alpha
  * @legacy
  */
-
 export const SharedTreeAttributes: IChannelAttributes = {
 	type: SharedTreeFactoryType,
 	snapshotFormatVersion: "0.0.0",
