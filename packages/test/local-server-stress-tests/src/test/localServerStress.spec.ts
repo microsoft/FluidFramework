abb66b5217d39981ca8adcad0aef1ce40345e4a5
Tony Murphy <anthony.murphy@microsoft.com>
2025-03-24 09:08:41 -0700

    revert and remove skips

--- packages/test/local-server-stress-tests/src/test/localServerStress.spec.ts
+++ packages/test/local-server-stress-tests/src/test/localServerStress.spec.ts
@@ -12,26 +12,22 @@ import {
 	LocalServerStressModel,
 } from "../localServerStressHarness";
 
-for (let i = 0; i < 200; i++) {
-	describe.only("Local Server Stress", () => {
-		const model: LocalServerStressModel<StressOperations> = {
-			workloadName: `default_${i}`,
-			generatorFactory: () => takeAsync(100, makeGenerator()),
-			reducer,
-			validateConsistency: validateConsistencyOfAllDDS,
-		};
+describe("Local Server Stress", () => {
+	const model: LocalServerStressModel<StressOperations> = {
+		workloadName: "default",
+		generatorFactory: () => takeAsync(100, makeGenerator()),
+		reducer,
+		validateConsistency: validateConsistencyOfAllDDS,
+	};
 
-		createLocalServerStressSuite(model, {
-			defaultTestCount: 100,
-			skipMinimization: true,
-			// Uncomment to replay a particular seed.
-			// replay: 93,
-			// only: [28],
-			saveFailures,
-			// saveSuccesses,
-			// TODO (AB#33713): we've seen seeds 43 and 44 fail in the pipeline with errors that might
-			// represent bugs in the underlying DDSes. Skipping for now.
-			skip: [28],
-		});
+	createLocalServerStressSuite(model, {
+		defaultTestCount: 100,
+		// skipMinimization: true,
+		// Uncomment to replay a particular seed.
+		// replay: 93,
+		// only: [28],
+		saveFailures,
+		// saveSuccesses,
+		skip: [28],
 	});
-}
+});
