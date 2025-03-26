ed85f89ccfe757c136bd6e7575079b3d89d2d56c
Jason Hartman <jasonha@microsoft.com>
2025-03-26 14:48:31 -0700

    build(client-bundle-size-tests): clarify build Make it clear that package is ESM by not using dist folder for webpack. Instead use a `build` output directory. Additionally clean up the build scripts: - explicitly add `:esm` versions such that `:esm` qualified commands like `build-and-test:mocha:esm` will pick these up. - correct and simplify dependencies (webpack does not use the output of `build:esnext`, only required that dependencies have built ESM.)

--- examples/utils/bundle-size-tests/webpack.config.cjs
+++ examples/utils/bundle-size-tests/webpack.config.cjs
@@ -92,7 +92,7 @@ module.exports = {
 		extensions: [".tsx", ".ts", ".js"],
 	},
 	output: {
-		path: path.resolve(__dirname, "dist"),
+		path: path.resolve(__dirname, "build"),
 		library: "bundle",
 	},
 	node: false,
