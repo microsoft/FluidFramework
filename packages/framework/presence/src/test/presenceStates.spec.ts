4a56f6a791a5733af3eb0e265133fdfdc1248458
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-03-28 19:22:31 +0000

    style: Actually fix linter violation

--- packages/framework/presence/src/test/presenceStates.spec.ts
+++ packages/framework/presence/src/test/presenceStates.spec.ts
@@ -8,10 +8,10 @@ import type {
 	JsonSerializable,
 } from "@fluidframework/core-interfaces/internal/exposedUtilityTypes";
 
-import { addControlsTests } from "./broadcastControlsTests.js";
-
 import type { InternalTypes } from "../exposedInternalTypes.js";
 import type { IPresence } from "../presence.js";
+
+import { addControlsTests } from "./broadcastControlsTests.js";
 
 describe("Presence", () => {
 	describe("PresenceStates", () => {
