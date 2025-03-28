74ef64464a5f5be2ec6bf4f764baaf6ee38fd1cb
MarioJGMsoft <marioja@microsoft.com>
2025-03-28 17:17:33 +0000

    docs: Disable no deprecated

--- packages/drivers/odsp-driver/src/createOdspCreateContainerRequest.ts
+++ packages/drivers/odsp-driver/src/createOdspCreateContainerRequest.ts
@@ -13,6 +13,25 @@ import { ISharingLinkKind } from "@fluidframework/odsp-driver-definitions/intern
 import { buildOdspShareLinkReqParams, getContainerPackageName } from "./odspUtils.js";
 
 /**
+ * Create the request object with url and headers for creating a new file on OneDrive Sharepoint
+ * @param siteUrl - Base url for OneDrive
+ * @param driveId - drive identifier
+ * @param filePath - path where file needs to be created
+ * @param fileName - name of the new file to be created
+ * @param createShareLinkType - type of sharing link you would like to create for this file. ShareLinkTypes
+ * will be deprecated soon, so for any new implementation please provide createShareLinkType of type ShareLink
+ * @legacy
+ * @alpha
+ */
+export function createOdspCreateContainerRequest(
+	siteUrl: string,
+	driveId: string,
+	filePath: string,
+	fileName: string,
+	createShareLinkType?: ISharingLinkKind,
+): IRequest;
+
+/**
  * Overloaded version of createOdspCreateContainerRequest that takes in containerPackageInfo
  * @param siteUrl - Base url for OneDrive
  * @param driveId - drive identifier
@@ -35,25 +54,6 @@ export function createOdspCreateContainerRequest(
 	fileName: string,
 	createShareLinkType: ISharingLinkKind | undefined,
 	containerPackageInfo?: IContainerPackageInfo | undefined,
-): IRequest;
-
-/**
- * Create the request object with url and headers for creating a new file on OneDrive Sharepoint
- * @param siteUrl - Base url for OneDrive
- * @param driveId - drive identifier
- * @param filePath - path where file needs to be created
- * @param fileName - name of the new file to be created
- * @param createShareLinkType - type of sharing link you would like to create for this file. ShareLinkTypes
- * will be deprecated soon, so for any new implementation please provide createShareLinkType of type ShareLink
- * @legacy
- * @alpha
- */
-export function createOdspCreateContainerRequest(
-	siteUrl: string,
-	driveId: string,
-	filePath: string,
-	fileName: string,
-	createShareLinkType?: ISharingLinkKind,
 ): IRequest;
 
 /**
