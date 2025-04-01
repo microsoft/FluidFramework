570ab44110362ead2db6f5fc11fc4984576fe88e
Jason Hartman <jasonha@microsoft.com>
2025-03-31 17:10:18 -0700

    ci(client): reduce archive size and fix typos

--- scripts/pack-build-output.sh
+++ scripts/pack-build-output.sh
@@ -10,6 +10,9 @@ mkdir -p build_output_archive
 # Find all build output, excluding those in node_modules and .git.
 # Alter the .gitignore files to only include node_modules and this
 # temp build_output_archive folder, then leverage git to locate changes.
+# Note: api-extractor incremental build logs are excluded from the
+#       archive as they are very large. (maybe can be reduced)
+#
 # .gitignore.files.txt contains the list of all .gitignore files that
 #   were modified.
 # build_output_to_publish.txt contains the list of all complete
@@ -20,14 +23,21 @@ find $WORKING_DIRECTORY -type f -name ".gitignore" ! \( -path "*/node_modules/*"
 (
 	# Disable command echoing for this block
 	set +x
-	echo reducing .gitignores to only node_modules # and .npmrc
+	echo reducing .gitignores to only node_modules
 	for gitignore_filepath in $(cat build_output_archive/.gitignore.files.txt)
 	do
 		echo "node_modules" > $gitignore_filepath
-		# Also ignore out temporary folder (only needed at root)
-		echo "build_output_archive" >> $gitignore_filepath
 	done
 )
+# Additional ignores for root
+#   ignore our temporary folder
+echo "build_output_archive" >> .gitignore
+#   ignore api-extractor incremental build logs as they account for about 115MB of compressed archive
+echo "**/api-extractor-*.done.build.log" >> .gitignore
+#   ignore root api-extractor collection about 4MB (not much) but will probably be regenerated without
+#   the package .done.build.log files.
+echo "_api-extractor-temp/**" >> .gitignore
+
 # Build the content list of unstaged, new files
 git status --porcelain | grep -E '^\?\?' | awk '{print $2}' > build_output_archive/build_output_to_publish.txt
 
