565c046afd4d3ae95773d87506e462ab3091d2e2
Tyler Butler <tylerbu@microsoft.com>
2025-04-01 14:43:07 -0700

    tag asserts

--- packages/runtime/test-runtime-utils/src/assertionShortCodesMap.ts
+++ packages/runtime/test-runtime-utils/src/assertionShortCodesMap.ts
@@ -832,7 +832,6 @@ export const shortCodeMap = {
 	"0x5dd": "cannot group ops with metadata",
 	"0x5df": "Summarize should not be called when not tracking the summary",
 	"0x5e0": "Used route should always be an absolute route",
-	"0x5e1": "interval ID should not be undefined",
 	"0x5e2": "interval ID should not be undefined",
 	"0x5e6": "Requested coded for unsupported format.",
 	"0x5ea": "Tried to encode unsupported fieldKind",
