07c11d6c14a8a28a1b5bc6d9bb6dc68b94470734
Tyler Butler <tylerbu@microsoft.com>
2025-04-01 17:40:26 -0700

    updates

--- build-tools/packages/build-cli/src/library/git.ts
+++ build-tools/packages/build-cli/src/library/git.ts
@@ -165,9 +165,11 @@ export class Repository implements GitContext {
 	public async getShaForBranch(branch: string, remote?: string): Promise<string> {
 		const refspec =
 			remote === undefined ? `refs/heads/${branch}` : `refs/remotes/${remote}/${branch}`;
+		// result is a string of the form '64adcdba56deb16e0641c91ca825401a9f7a01f9 refs/heads/release/client/2.23'
 		const result = await this.git.raw(`show-ref`, refspec);
 
-		return result;
+		const [sha] = result.split(" ");
+		return sha;
 	}
 
 	/**
