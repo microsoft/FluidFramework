5bb8c22a84b63d896e5bed645b03c0642f7dc9d5
Dhruv Verma <dhruvverma@microsoft.com>
2025-04-03 17:17:32 -0400

    Added logging of upgrade header as well

--- server/routerlicious/packages/services-shared/src/webServer.ts
+++ server/routerlicious/packages/services-shared/src/webServer.ts
@@ -93,6 +93,7 @@ const createAndConfigureHttpServer = (
 			const requestProperties = {
 				url: req.url,
 				method: req.method,
+				isWebSocketUpgrade: req.headers.upgrade?.toLowerCase() === 'websocket',
 			};
 			Lumberjack.info("Socket.io request received", requestProperties);
 
