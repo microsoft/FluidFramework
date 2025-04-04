bdc92eee6adf726949683483a3e2daab3fb97a69
Mark Fields <markfields@users.noreply.github.com>
2025-04-04 18:11:58 +0000

    Restate SummarySerializer as GCHandleVisitor throughout SharedObject

--- packages/dds/shared-object-base/src/summarySerializer.ts
+++ packages/dds/shared-object-base/src/summarySerializer.ts
@@ -4,25 +4,27 @@
  */
 
 import { type IFluidHandleInternal } from "@fluidframework/core-interfaces/internal";
-import type { ISerializedHandle } from "@fluidframework/runtime-utils/internal";
+import { type ISerializedHandle } from "@fluidframework/runtime-utils/internal";
 
 import { FluidSerializer } from "./serializer.js";
 
 /**
- * Serializer implementation for serializing handles during summary.
- * @internal
+ * Implementation of IFluidSerializer used by GC to visit all the handles in the DDS to collect its outbound routes
+ *
+ * @remarks - This is given to DDS code that typically produces a serialization of the data, which is then ignored.
+ * All that is needed is getSerializedRoutes() to get the routes. This strategy could be optimized if needed.
  */
-export class SummarySerializer extends FluidSerializer {
-	private readonly serializedRoutes: Set<string> = new Set();
-	public getSerializedRoutes(): string[] {
-		return [...this.serializedRoutes];
+export class GCHandleVisitor extends FluidSerializer {
+	private readonly visitedHandles: Set<string> = new Set();
+	public getVisitedHandles(): string[] {
+		return [...this.visitedHandles];
 	}
 
 	protected bindAndEncodeHandle(
 		handle: IFluidHandleInternal,
 		bind: IFluidHandleInternal,
 	): ISerializedHandle {
-		this.serializedRoutes.add(handle.absolutePath);
+		this.visitedHandles.add(handle.absolutePath);
 		return super.bindAndEncodeHandle(handle, bind);
 	}
 }
