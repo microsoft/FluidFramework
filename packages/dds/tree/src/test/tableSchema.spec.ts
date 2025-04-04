58d3fe067675939d3e30c068c73ebc22eb2cd43a
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-04-04 00:25:34 +0000

    remove: Unused import

--- packages/dds/tree/src/test/tableSchema.spec.ts
+++ packages/dds/tree/src/test/tableSchema.spec.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import { strict as assert, fail } from "node:assert";
+import { strict as assert } from "node:assert";
 
 import { createIdCompressor } from "@fluidframework/id-compressor/internal";
 
