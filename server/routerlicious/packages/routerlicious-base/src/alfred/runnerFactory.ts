c9bbed4386399217ccf678904c6133f91607cde7
dhr-verma <137836091+dhr-verma@users.noreply.github.com>
2025-04-10 12:38:43 -0400

    Added a document denyList in Alfred and Nexus (#24298)
    ## Description
    
    This PR adds a document denyList for Alfred and Nexus. This is similar
    to what we have in Historian. This is added to the all Alfred APIs. The
    list has two features:
    
    1) A list of tenants to block: This list can allow blocking of all
    requests from a given tenant. Useful for situations when we get DDoS-ed
    by 1 tenant.
    2) A list of documents to block: Blocks documents that are large in size
    and can cause OOM issues. Once change here from the previous
    implementation is that now we no longer use the tenantId -> documentId
    map. This is to make OCE life easier. Once GUID is easier to
    type/copy-paste than two GUIDs. Since DocIds are GUIDS chances of
    collision are negligible.
    
    For some `/documents` APIs, we can skip document level checks as these
    ops are either `GET` for document data and not ops or are `DELETE` calls
    to delete the document.
    
    This PR also has TODOs to remove the implementation in Historian and use
    the implementation in routerlicious instead.

--- server/routerlicious/packages/routerlicious-base/src/alfred/runnerFactory.ts
+++ server/routerlicious/packages/routerlicious-base/src/alfred/runnerFactory.ts
@@ -55,6 +55,7 @@ export class AlfredResources implements core.IResources {
 		public readinessCheck?: IReadinessCheck,
 		public fluidAccessTokenGenerator?: core.IFluidAccessTokenGenerator,
 		public redisCacheForGetSession?: core.ICache,
+		public denyList?: core.IDenyList,
 	) {
 		const httpServerConfig: services.IHttpServerConfig = config.get("system:httpServer");
 		const nodeClusterConfig: Partial<services.INodeClusterConfig> | undefined = config.get(
@@ -414,6 +415,12 @@ export class AlfredResourcesFactory implements core.IResourcesFactory<AlfredReso
 			});
 		}
 		const startupCheck = new StartupCheck();
+		const documentsDenyListConfig = config.get("documentDenyList");
+		const tenantsDenyListConfig = config.get("tenantsDenyList");
+		const denyList: core.IDenyList = new utils.DenyList(
+			tenantsDenyListConfig,
+			documentsDenyListConfig,
+		);
 
 		return new AlfredResources(
 			config,
@@ -441,6 +448,7 @@ export class AlfredResourcesFactory implements core.IResourcesFactory<AlfredReso
 			customizations?.readinessCheck,
 			customizations?.fluidAccessTokenGenerator,
 			redisGetSessionCache,
+			denyList,
 		);
 	}
 }
@@ -473,6 +481,7 @@ export class AlfredRunnerFactory implements core.IRunnerFactory<AlfredResources>
 			resources.readinessCheck,
 			resources.fluidAccessTokenGenerator,
 			resources.redisCacheForGetSession,
+			resources.denyList,
 		);
 	}
 }
