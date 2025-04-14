ad06564c8e8b3a19cb2b73e9152664afd57fdc71
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-04-14 11:10:45 -0700

    Update packages/dds/tree/src/feature-libraries/flex-tree/flexTreeTypes.ts
    Co-authored-by: Alex Villarreal <716334+alexvy86@users.noreply.github.com>

--- packages/dds/tree/src/feature-libraries/flex-tree/flexTreeTypes.ts
+++ packages/dds/tree/src/feature-libraries/flex-tree/flexTreeTypes.ts
@@ -96,7 +96,7 @@ export enum TreeStatus {
 	 * Is removed and cannot be added back to the original document tree.
 	 * @remarks
 	 * Nodes can enter this state for multiple reasons:
-	 * - The node was removed and nothing (ex: undo/redo history) kept it from being cleaned up.
+	 * - The node was removed and nothing (e.g. undo/redo history) kept it from being cleaned up.
 	 * - The {@link TreeView} was disposed or had a schema change which made the tree incompatible.
 	 * @privateRemarks
 	 * There was planned work (AB#17948) to make the first reason a node could become "Deleted" impossible,
