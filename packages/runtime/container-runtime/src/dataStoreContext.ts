1f12146e134f8a5caa9591317fe3129add58cc27
Tony Murphy <anthony.murphy@microsoft.com>
2025-04-22 14:36:58 -0700

    Update packages/runtime/container-runtime/src/dataStoreContext.ts
    Co-authored-by: Kian Thompson <102998837+kian-thompson@users.noreply.github.com>

--- packages/runtime/container-runtime/src/dataStoreContext.ts
+++ packages/runtime/container-runtime/src/dataStoreContext.ts
@@ -199,7 +199,7 @@ export interface IFluidDataStoreContextEvents extends IEvent {
  * Eventually we should remove the delta manger from being exposed to Datastore runtimes via the context. However to remove that exposure we need to add new
  * features, and those features themselves need forward and back compat. This proxy is here to enable that back compat. Each feature this proxy is used to
  * support should be listed below, and as layer compat support goes away for those feature, we should also remove them from this proxy, with the eventual goal
- * of completely remove this proxy.
+ * of completely removing this proxy.
  *
  * - Everything regarding readonly is to support older datastore runtimes which do not have the setReadonly function, so they must get their readonly state via the delta manager.
  *
