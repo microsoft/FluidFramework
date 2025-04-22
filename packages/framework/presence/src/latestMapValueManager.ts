f8f0f812672c5a8087014e47f226215ccd073883
Jason Hartman <jasonha@microsoft.com>
2025-04-21 17:27:07 -0700

    feat(client-presence): Latest support beyond `object` - `Latest` now supports `null` initially and as a union type element - `LatestMap` now supports serializable primitives as well

--- packages/framework/presence/src/latestMapValueManager.ts
+++ packages/framework/presence/src/latestMapValueManager.ts
@@ -495,7 +495,7 @@ class LatestMapValueManagerImpl<
  * @alpha
  */
 export function latestMap<
-	T extends object,
+	T,
 	Keys extends string | number = string | number,
 	RegistrationKey extends string = string,
 >(
