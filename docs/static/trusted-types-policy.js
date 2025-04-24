023372bd1679efc9498fdb378e403377d76922c1
Daniel Madrid <danielcar@microsoft.com>
2025-04-24 15:21:28 -0700

    remove console logs

--- docs/static/trusted-types-policy.js
+++ docs/static/trusted-types-policy.js
@@ -8,27 +8,14 @@ if (
   window.trustedTypes &&
   typeof window.trustedTypes.createPolicy === "function"
 ) {
-	console.log("Trusted Types policy already exists");
   if (
     typeof window.trustedTypes.getPolicy !== "function" ||
     !window.trustedTypes.getPolicy("default")
   ) {
-	console.log("Creating default Trusted Types policy");
     window.trustedTypes.createPolicy("default", {
       createHTML: (input) => input,
       createScript: (input) => input,
       createScriptURL: (input) => input,
     });
   }
-} else {
-	console.log("Trusted Types not supported");
-  // Fallback for browsers that do not support Trusted Types
-  window.trustedTypes = {
-	createPolicy: () => ({
-	  createHTML: (input) => input,
-	  createScript: (input) => input,
-	  createScriptURL: (input) => input,
-	}),
-	getPolicy: () => null,
-  };
 }
