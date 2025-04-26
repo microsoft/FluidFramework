5591ecf4b943d6c1bbd113f8ecacd7f430ecd8a3
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-04-26 00:10:46 +0000

    test: Update test

--- examples/utils/import-testing/src/test/importer.spec.ts
+++ examples/utils/import-testing/src/test/importer.spec.ts
@@ -39,7 +39,7 @@ describe("import tests", () => {
 		class ColumnProps extends schemaFactory.object("table-column-props", {
 			label: schemaFactory.optional(schemaFactory.string),
 		}) {}
-		class Column extends TableSchema.createColumn(schemaFactory, ColumnProps) {}
+		class Column extends TableSchema.createColumn(schemaFactory, schemaFactory.optional(ColumnProps)) {}
 
 		class RowProps extends schemaFactory.object("table-row-props", {
 			label: schemaFactory.optional(schemaFactory.string),
