3f2173cff41619c199b684d67fc2e9ba0fcc412c
Tong Chen <114451900+chentong7@users.noreply.github.com>
2025-04-29 11:27:59 -0700

    Update packages/framework/ai-collab/src/aiCollabApi.ts
    Co-authored-by: Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>

--- packages/framework/ai-collab/src/aiCollabApi.ts
+++ packages/framework/ai-collab/src/aiCollabApi.ts
@@ -174,7 +174,7 @@ export interface AiCollabSuccessResponse {
 	 * A list of diffs that represent the changes made by the AI collaboration.
 	 * @remarks This is intended to be used to update your applications UI view with the changes made by the AI collaboration.
 	 */
-	readonly uiDiffs: Diff[];
+	readonly uiDiffs: readonly Diff[];
 }
 
 /**
