f0a71ccce7a055e99ce9f4ba15e1cede529bbc9c
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-04-30 17:11:40 -0700

    Improve and test handling of large schema (#24484)
    ## Description
    
    Large schema can be problematic due to hitting limits on TypeScript's
    recursion depth.
    
    Details on how schema typing is implementing have large impacts on this.
    
    To detect regressions and measure improvements, some test schema needed.
    
    This change adds some large test schema, and makes an improvement that
    was confirmed using these tests.
    
    I have measured clean build times for the tree package with and without
    this change. The largest impact was in the last few steps:
    
    **Unchanged:**
    [21/23] ✓ @fluidframework/tree: tsc --project ./src/test/tsconfig.json -
    7.117s
    [22/23] ✓ @fluidframework/tree: fluid-tsc commonjs --project
    ./src/test/tsconfig.cjs.json - 6.162s
    [23/23] ✓ @fluidframework/tree: eslint --format stylish src - 26.585s
    Build succeeded
    Total time: 46.762s
    
    **With change, but not new large tests**
    [21/23] ✓ @fluidframework/tree: tsc --project ./src/test/tsconfig.json -
    7.243s
    [22/23] ✓ @fluidframework/tree: fluid-tsc commonjs --project
    ./src/test/tsconfig.cjs.json - 5.835s
    [23/23] ✓ @fluidframework/tree: eslint --format stylish src - 24.612s
    Build succeeded
    Total time: 44.456s
    
    **With everything in this PR**
    [21/23] ✓ @fluidframework/tree: tsc --project ./src/test/tsconfig.json -
    8.049s
    [22/23] ✓ @fluidframework/tree: fluid-tsc commonjs --project
    ./src/test/tsconfig.cjs.json - 7.530s
    [23/23] ✓ @fluidframework/tree: eslint --format stylish src - 27.566s
    Build succeeded
    Total time: 49.636s
    
    
    Overall, it looks like this change sped up the build a bit from the
    optimization, but the new large test suite adds extra build and lint
    time which is larger than the improvement. I think this is an acceptable
    tradeoff to have some test coverage of large schema.

--- examples/utils/import-testing/src/test/importer.spec.ts
+++ examples/utils/import-testing/src/test/importer.spec.ts
@@ -5,8 +5,14 @@
 
 import { strict as assert } from "node:assert";
 
-// eslint-disable-next-line import/no-internal-modules
-import { JsonAsTree, SchemaFactoryAlpha, TableSchema } from "@fluidframework/tree/internal";
+import {
+	JsonAsTree,
+	SchemaFactory,
+	SchemaFactoryAlpha,
+	TableSchema,
+	TreeViewConfiguration,
+	// eslint-disable-next-line import/no-internal-modules
+} from "@fluidframework/tree/internal";
 import type {
 	areSafelyAssignable,
 	requireTrue,
@@ -14,6 +20,7 @@ import type {
 	// eslint-disable-next-line import/no-internal-modules
 } from "@fluidframework/tree/internal";
 
+import { Empty001, Empty020, largeUnion } from "../largeExport.js";
 import { BadArraySelf, GoodArraySelf, RecursiveMap } from "../testExports.js";
 
 describe("import tests", () => {
@@ -132,5 +139,21 @@ describe("import tests", () => {
 		type _check1 = requireAssignableTo<undefined, Inner2>;
 		// @ts-expect-error This fails, like it should, due to working schema aware types
 		type _check2 = requireAssignableTo<number, Inner2>;
+	});
+
+	it("LargeImport", () => {
+		const schema = new SchemaFactory("com.example");
+
+		class LargeUnionObjectNode extends schema.object("ObjectNode", { x: largeUnion }) {}
+
+		const config = new TreeViewConfiguration({
+			schema: LargeUnionObjectNode,
+			enableSchemaValidation: true,
+		});
+
+		const node = new LargeUnionObjectNode({
+			// eslint-disable-next-line unicorn/no-null
+			x: new Empty020({ x: new Empty001({ x: null }) }),
+		});
 	});
 });
