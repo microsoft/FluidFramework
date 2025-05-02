bd315e1705d4d9605f7e345a82cabccf005d7a9b
Tony Murphy <anthony.murphy@microsoft.com>
2025-05-02 09:07:38 -0700

    clean up test

--- packages/test/test-end-to-end-tests/src/test/stagingMode.spec.ts
+++ packages/test/test-end-to-end-tests/src/test/stagingMode.spec.ts
@@ -6,6 +6,7 @@
 import { strict as assert } from "assert";
 
 import { ITestDataObject, describeCompat } from "@fluid-private/test-version-utils";
+import type { IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions/internal";
 import type {
 	IContainerRuntimeBaseExperimental,
 	IFluidDataStoreChannel,
@@ -13,152 +14,157 @@ import type {
 import { getContainerEntryPointBackCompat } from "@fluidframework/test-utils/internal";
 import * as semver from "semver";
 
-describeCompat("StagingMode", "FullCompat", (getTestObjectProvider, apis) => {
-	beforeEach(function () {
-		if (semver.compare(apis.containerRuntime.version, "2.40.0") === -1) {
-			return this.skip();
-		}
-	});
+describeCompat(
+	"StagingMode: readonlyInStagingMode",
+	"FullCompat",
+	function (getTestObjectProvider, apis) {
+		const createContainer = async ({
+			test,
+			readonlyInStagingMode,
+		}: {
+			test: Mocha.Context;
+			readonlyInStagingMode: boolean;
+		}) => {
+			// the readonlyInStagingMode support was added to the data store context
+			// in the containerRuntime package in 2.40.0, so ensure it is at least
+			// that version.
+			if (semver.compare(apis.containerRuntime.version, "2.40.0") === -1) {
+				test.skip();
+			}
+			const provider = getTestObjectProvider();
 
-	it("should set runtime to readonly when readonlyInStagingMode is true", async function () {
-		const provider = getTestObjectProvider();
+			const container = await provider.makeTestContainer({
+				policies: { readonlyInStagingMode },
+			});
+			const { _context, _runtime } =
+				await getContainerEntryPointBackCompat<ITestDataObject>(container);
 
-		const container = await provider.makeTestContainer({
-			policies: { readonlyInStagingMode: true },
+			const containerRuntime = _context.containerRuntime as IContainerRuntimeBaseExperimental;
+			return {
+				container,
+				containerRuntime,
+				dsRuntime: _runtime as unknown as IFluidDataStoreChannel & IFluidDataStoreRuntime,
+			};
+		};
+
+		it("should set runtime to readonly when readonlyInStagingMode is true", async function () {
+			const { containerRuntime, dsRuntime } = await createContainer({
+				test: this,
+				readonlyInStagingMode: true,
+			});
+
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				false,
+				"Runtime should not be readonly before entering staging mode.",
+			);
+
+			const controls = containerRuntime.enterStagingMode?.();
+
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				true,
+				"Runtime should be readonly after entering staging mode.",
+			);
+
+			controls?.commitChanges();
+
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				false,
+				"Runtime should not be readonly after committing changes.",
+			);
 		});
-		const { _context, _runtime } =
-			await getContainerEntryPointBackCompat<ITestDataObject>(container);
 
-		const expRuntime = _context.containerRuntime as IContainerRuntimeBaseExperimental;
+		it("should preserve readonly state when set before entering staging mode", async function () {
+			const { container, containerRuntime, dsRuntime } = await createContainer({
+				test: this,
+				readonlyInStagingMode: true,
+			});
 
-		assert.equal(
-			_runtime.isReadOnly(),
-			false,
-			"Runtime should not be readonly before entering staging mode.",
-		);
+			container.forceReadonly?.(true);
 
-		const controls = expRuntime.enterStagingMode?.();
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				true,
+				"Runtime should preserve readonly state set before entering staging mode.",
+			);
 
-		assert.equal(
-			_runtime.isReadOnly(),
-			true,
-			"Runtime should be readonly after entering staging mode.",
-		);
+			const controls = containerRuntime.enterStagingMode?.();
 
-		controls?.commitChanges();
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				true,
+				"Runtime should be readonly after entering staging mode.",
+			);
 
-		assert.equal(
-			_runtime.isReadOnly(),
-			false,
-			"Runtime should not be readonly after committing changes.",
-		);
-	});
+			controls?.commitChanges();
 
-	it("should preserve readonly state when set before entering staging mode", async function () {
-		const provider = getTestObjectProvider();
-
-		const container = await provider.makeTestContainer({
-			policies: { readonlyInStagingMode: true },
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				true,
+				"Runtime should preserve readonly state set before entering staging mode.",
+			);
 		});
-		const { _context, _runtime } =
-			await getContainerEntryPointBackCompat<ITestDataObject>(container);
 
-		const expRuntime = _context.containerRuntime as IContainerRuntimeBaseExperimental;
+		it("should preserve readonly state when set during staging mode", async function () {
+			const { container, containerRuntime, dsRuntime } = await createContainer({
+				test: this,
+				readonlyInStagingMode: true,
+			});
 
-		container.forceReadonly?.(true);
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				false,
+				"Runtime should not be readonly before entering staging mode.",
+			);
 
-		assert.equal(
-			_runtime.isReadOnly(),
-			true,
-			"Runtime should preserve readonly state set before entering staging mode.",
-		);
+			const controls = containerRuntime.enterStagingMode?.();
 
-		const controls = expRuntime.enterStagingMode?.();
+			container.forceReadonly?.(true);
 
-		assert.equal(
-			_runtime.isReadOnly(),
-			true,
-			"Runtime should be readonly after entering staging mode.",
-		);
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				true,
+				"Runtime should preserve readonly state set during staging mode.",
+			);
 
-		controls?.commitChanges();
+			controls?.commitChanges();
 
-		assert.equal(
-			_runtime.isReadOnly(),
-			true,
-			"Runtime should preserve readonly state set before entering staging mode.",
-		);
-	});
-
-	it("should preserve readonly state when set during staging mode", async function () {
-		const provider = getTestObjectProvider();
-
-		const container = await provider.makeTestContainer({
-			policies: { readonlyInStagingMode: true },
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				true,
+				"Runtime should preserve readonly state set during staging mode.",
+			);
 		});
-		const { _context, _runtime } =
-			await getContainerEntryPointBackCompat<ITestDataObject>(container);
 
-		const expRuntime = _context.containerRuntime as IContainerRuntimeBaseExperimental;
+		it("should not set runtime to readonly when readonlyInStagingMode is false", async function () {
+			const { containerRuntime, dsRuntime } = await createContainer({
+				test: this,
+				readonlyInStagingMode: false,
+			});
 
-		assert.equal(
-			_runtime.isReadOnly(),
-			false,
-			"Runtime should not be readonly before entering staging mode.",
-		);
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				false,
+				"Runtime should not be readonly before entering staging mode.",
+			);
 
-		const controls = expRuntime.enterStagingMode?.();
+			const controls = containerRuntime.enterStagingMode?.();
 
-		container.forceReadonly?.(true);
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				dsRuntime.policies === undefined,
+				"Runtime should not be readonly when readonlyInStagingMode is false.",
+			);
 
-		assert.equal(
-			_runtime.isReadOnly(),
-			true,
-			"Runtime should preserve readonly state set during staging mode.",
-		);
+			controls?.commitChanges();
 
-		controls?.commitChanges();
-
-		assert.equal(
-			_runtime.isReadOnly(),
-			true,
-			"Runtime should preserve readonly state set during staging mode.",
-		);
-	});
-
-	it("should not set runtime to readonly when readonlyInStagingMode is false", async function () {
-		const provider = getTestObjectProvider();
-
-		const container = await provider.makeTestContainer({
-			policies: { readonlyInStagingMode: false },
+			assert.equal(
+				dsRuntime.isReadOnly(),
+				false,
+				"Runtime should not be readonly after committing changes.",
+			);
 		});
-		const { _context, _runtime } =
-			await getContainerEntryPointBackCompat<ITestDataObject>(container);
-
-		const runtimeAsChannel = _runtime as unknown as IFluidDataStoreChannel;
-
-		const expRuntime = _context.containerRuntime as IContainerRuntimeBaseExperimental;
-
-		assert.equal(
-			_runtime.isReadOnly(),
-			false,
-			"Runtime should not be readonly before entering staging mode.",
-		);
-
-		const controls = expRuntime.enterStagingMode?.();
-
-		assert.equal(
-			_runtime.isReadOnly(),
-			runtimeAsChannel.policies === undefined,
-			"Runtime should not be readonly when readonlyInStagingMode is false.",
-		);
-
-		controls?.commitChanges();
-
-		assert.equal(
-			_runtime.isReadOnly(),
-			false,
-			"Runtime should not be readonly after committing changes.",
-		);
-	});
-});
+	},
+);
