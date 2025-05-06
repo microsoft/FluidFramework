f07d96c6885912d8a916013676a7a10a2104b778
daesunp <138815173+daesunp@users.noreply.github.com>
2025-05-06 00:10:02 +0000

    add unit test

---
+++ examples/data-objects/table-tree/tests/table.test.ts
@@ -0,0 +1,22 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { globals } from "../jest.config.cjs";
+
+describe("table", () => {
+	beforeAll(async () => {
+		await page.goto(globals.PATH, { waitUntil: "load", timeout: 0 });
+		await page.waitForFunction(() => window["fluidStarted"]);
+	}, 45000);
+
+	beforeEach(async () => {
+		await page.goto(globals.PATH, { waitUntil: "load" });
+		await page.waitForFunction(() => window["fluidStarted"]);
+	});
+
+	it("The page loads and there's a textbox with placeholder Row ID", async () => {
+		await expect(page).toClick("button", { text: "Add Row" });
+	});
+});
