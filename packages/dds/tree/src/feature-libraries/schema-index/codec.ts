698df8c43424247033135fddcd9bb9f3fce90c79
Tommy Brosman <tbrosman@microsoft.com>
2025-05-09 13:12:20 -0700

    Updated a comment.

--- packages/dds/tree/src/feature-libraries/schema-index/codec.ts
+++ packages/dds/tree/src/feature-libraries/schema-index/codec.ts
@@ -27,7 +27,7 @@ import { brand, type JsonCompatible } from "../../util/index.js";
 import { Format as FormatV1 } from "./formatV1.js";
 
 /**
- * Versions for the codec that encodes an in-memory representation of a stored schema into a persisted format (or decodes it in the opposite direction).
+ * Versions for the codec that encodes an in-memory representation of a stored schema {@link TreeStoredSchema} into a persisted format (or decodes it in the opposite direction).
  */
 export enum SchemaCodecVersion {
 	v1 = 1,
