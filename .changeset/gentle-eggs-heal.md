9dfe5c65dd4569e1a7b64430b3df55f074b5ca69
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-05-13 15:58:49 -0700

    Apply suggestions from code review
    Co-authored-by: Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>

--- .changeset/gentle-eggs-heal.md
+++ .changeset/gentle-eggs-heal.md
@@ -2,6 +2,7 @@
 "@fluid-experimental/tree-react-api": minor
 "__section": feature
 ---
-New experimental objectIdNumber API
+New experimental objectIdNumber API has been added
 
-A new `objectIdNumber` has been added which is handy when an identifier is needed which corresponds to an object identity like when specifying a React "key" that corresponds to a `TreeNode`.
+A new `objectIdNumber` has been added, which is handy when an identifier is needed which corresponds to an object identity.
+For example: when specifying a React "key" that corresponds to a `TreeNode`.
--- experimental/framework/tree-react-api/src/simpleIdentifier.ts
+++ experimental/framework/tree-react-api/src/simpleIdentifier.ts
@@ -10,12 +10,12 @@ const idMap = new WeakMap<object, number>();
 /**
  * Associates a unique number with an object.
  * @remarks
- * The id number is tied to the object identity, not the objects contents: modifying the object will not cause it to get a different id.
+ * The ID number is tied to the object identity, not the object's contents; modifying the object will not cause it to get a different ID.
  *
- * This can be handy for generating [keys for React lists](https://react.dev/learn/rendering-lists#where-to-get-your-key) from TreeNodes.
+ * This can be handy for generating {@link https://react.dev/learn/rendering-lists#where-to-get-your-key | keys for React lists} from TreeNodes.
  *
- * Most cases which could use this functions should just use the objects themselves instead of getting ids from them, since the objects will have the same equality as the ids.
- * For example if storing data associated with the objects in a map, using the object as the key is more efficient than getting an id from it and using that.
+ * Most cases which could use this function should just use the objects themselves instead of getting IDs from them, since the objects will have the same equality as the IDs.
+ * For example, if storing data associated with the objects in a map, using the object as the key is more efficient than getting an ID from it and using that.
  * This functions exists to deal with the edge case where you would like to use object identity, but you can't.
  * React keys are an examples of such a case, since React does not allow objects as keys.
  * @public
