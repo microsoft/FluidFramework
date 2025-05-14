b68d74718f20e071d4e4c3557e1739ddbc8332bf
Tyler Butler <tylerbu@microsoft.com>
2025-05-14 11:35:51 -0700

    multiclient wip

---
+++ packages/framework/presence/src/test/TinyliciousClientFactory.ts
@@ -0,0 +1,237 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+// import {
+// 	AzureClient,
+// 	AzureLocalConnectionConfig,
+// 	AzureRemoteConnectionConfig,
+// 	ITelemetryBaseLogger,
+// } from "@fluidframework/azure-client";
+// import {
+// 	AzureClient as AzureClientLegacy,
+// 	AzureLocalConnectionConfig as AzureLocalConnectionConfigLegacy,
+// 	AzureRemoteConnectionConfig as AzureRemoteConnectionConfigLegacy,
+// 	ITelemetryBaseLogger as ITelemetryBaseLoggerLegacy,
+// } from "@fluidframework/azure-client-legacy";
+import { ScopeType } from "@fluidframework/driver-definitions/internal";
+import {
+	MockLogger,
+	createChildLogger,
+	createMultiSinkLogger,
+} from "@fluidframework/telemetry-utils/internal";
+import { InsecureTokenProvider } from "@fluidframework/test-runtime-utils/internal";
+import {
+	TinyliciousClient,
+	type TinyliciousConnectionConfig,
+} from "@fluidframework/tinylicious-client";
+import { default as Axios, AxiosResponse, type AxiosRequestConfig } from "axios";
+import { v4 as uuid } from "uuid";
+
+import { createAzureTokenProvider } from "./AzureTokenFactory.js";
+import type { ITelemetryBaseLogger } from "@fluidframework/core-interfaces";
+
+// eslint-disable-next-line unicorn/prefer-export-from
+export { ScopeType };
+
+const driver = undefined;
+const r11sEndpointName = undefined;
+
+/**
+ * This function will determine if local or remote mode is required (based on FLUID_CLIENT), and return a new
+ * {@link TinyliciousClient} instance based on the mode by setting the Connection config accordingly.
+ */
+export function createTinyliciousClient(
+	id?: string,
+	name?: string,
+	logger?: MockLogger,
+	scopes?: ScopeType[],
+): TinyliciousClient {
+	const tenantId = "frs-client-tenant";
+	const user = {
+		id: id ?? uuid(),
+		name: name ?? uuid(),
+	};
+	// const endPoint = process.env.azure__fluid__relay__service__endpoint as string;
+	// if (useAzure && endPoint === undefined) {
+	// 	throw new Error("Azure Fluid Relay service endpoint is missing");
+	// }
+
+	// use AzureClient remote mode will run against live Azure Fluid Relay.
+	// Default to running Tinylicious for PR validation
+	// and local testing so it's not hindered by service availability
+	const connectionProps: TinyliciousConnectionConfig = {
+		tokenProvider: new InsecureTokenProvider("fooBar", user, scopes),
+		// endpoint: "http://localhost:7071",
+		// type: "local",
+	};
+	const getLogger = (): ITelemetryBaseLogger | undefined => {
+		const testLogger = getTestLogger?.();
+		if (!logger && !testLogger) {
+			return undefined;
+		}
+		if (logger && testLogger) {
+			return createMultiSinkLogger({ loggers: [logger, testLogger] });
+		}
+		return logger ?? testLogger;
+	};
+
+	const createLogger = createChildLogger({
+		logger: getLogger(),
+		properties: {
+			all: {
+				driverType: driver,
+				driverEndpointName: driver,
+			},
+		},
+	});
+	return new TinyliciousClient({
+		connection: connectionProps,
+		logger: createLogger,
+	});
+}
+
+/**
+ * Copy of {@link createTinyliciousClient} with legacy (LTS) AzureClient APIs.
+ */
+export function createAzureClientLegacy(
+	userID?: string,
+	userName?: string,
+	logger?: MockLogger,
+): AzureClientLegacy {
+	const useAzure = process.env.FLUID_CLIENT === "azure";
+	const tenantId = useAzure
+		? (process.env.azure__fluid__relay__service__tenantId as string)
+		: "frs-client-tenant";
+	const user = {
+		id: userID ?? uuid(),
+		name: userName ?? uuid(),
+	};
+	const endPoint = process.env.azure__fluid__relay__service__endpoint as string;
+	if (useAzure && endPoint === undefined) {
+		throw new Error("Azure Azure Fluid Relay service endpoint is missing");
+	}
+
+	// use AzureClient remote mode will run against live Azure Fluid Relay.
+	// Default to running Tinylicious for PR validation
+	// and local testing so it's not hindered by service availability
+	const connectionProps: AzureRemoteConnectionConfigLegacy | AzureLocalConnectionConfigLegacy =
+		useAzure
+			? {
+					tenantId,
+					tokenProvider: createAzureTokenProvider(userID ?? "foo", userName ?? "bar"),
+					endpoint: endPoint,
+					type: "remote",
+				}
+			: {
+					tokenProvider: new InsecureTokenProvider("fooBar", user),
+					endpoint: "http://localhost:7071",
+					type: "local",
+				};
+	const getLogger = (): ITelemetryBaseLoggerLegacy | undefined => {
+		const testLogger = getTestLogger?.();
+		if (!logger && !testLogger) {
+			return undefined;
+		}
+		if (logger && testLogger) {
+			return createMultiSinkLogger({ loggers: [logger, testLogger] });
+		}
+		return logger ?? testLogger;
+	};
+	return new AzureClientLegacy({
+		connection: connectionProps,
+		logger: getLogger(),
+	});
+}
+
+/**
+ * This function is used to create a container using any summary payload.
+ * It is primary intended as a workaround to using the AzureClient, and
+ * is only being used at the moment for ephemeral container E2E tests
+ * since AzureClient does not currently support ephemeral containers.
+ *
+ * Usage of this function for anything other than ephemeral E2E tests is
+ * not recommended.
+ *
+ * @param requestPayload - The summary payload used to create the container,
+ * currently these are mainly fetched from ephemeralSummaryTrees.ts
+ * @param userID - ID for the user creating the container
+ * @param userName - Name for the user creating the container
+ * @returns - An AxiosResponse containing the container ID(response.data.id)
+ */
+export async function createContainerFromPayload(
+	requestPayload: object,
+	userID?: string,
+	userName?: string,
+): Promise<AxiosResponse> {
+	const useAzure = process.env.FLUID_CLIENT === "azure";
+	const tenantId = useAzure
+		? (process.env.azure__fluid__relay__service__tenantId as string)
+		: "local";
+	const user = {
+		id: userID ?? uuid(),
+		name: userName ?? uuid(),
+	};
+	const endPoint = useAzure
+		? (process.env.azure__fluid__relay__service__endpoint as string)
+		: "http://localhost:7071";
+	if (useAzure && endPoint === undefined) {
+		throw new Error("Azure Fluid Relay service endpoint is missing");
+	}
+
+	const tokenProvider = useAzure
+		? createAzureTokenProvider(userID ?? "foo", userName ?? "bar")
+		: new InsecureTokenProvider("fooBar", user);
+	const ordererToken = await tokenProvider.fetchOrdererToken(tenantId, undefined, false);
+
+	const headers = {
+		"Authorization": `Basic ${ordererToken.jwt}`,
+		"Content-Type": "application/json",
+	};
+
+	const url = `/documents/${tenantId}`;
+
+	const options: AxiosRequestConfig = {
+		baseURL: endPoint,
+		data: requestPayload,
+		headers,
+		maxBodyLength: 1048576000,
+		maxContentLength: 1048576000,
+		method: "POST",
+		url,
+	};
+
+	try {
+		const response: AxiosResponse = await Axios(options);
+
+		if (response.status === 201) {
+			console.log("Container created successfully");
+		} else {
+			throw new Error(`Error creating container. Status code: ${response.status}`);
+		}
+
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+		if (response?.data === undefined || (useAzure && response?.data?.id === undefined)) {
+			throw new Error(`ID of the created container is undefined`);
+		}
+
+		return response;
+	} catch (error) {
+		throw new Error(`An error occurred: ${error}`);
+	}
+}
+
+/**
+ * This function takes an AxiosResponse returned by the createContainerFromPayload and returns the containerId.
+ * A separate function is used for this, since the data path to the containerID is not always the same.
+ * (Tinylicious has the ID stored at a different path than other services)
+ *
+ * @param response - A container creation response returned by createContainerFromPayload
+ * @returns - The ID of the container that was created by createContainerFromPayload
+ */
+export function getContainerIdFromPayloadResponse(response: AxiosResponse): string {
+	const useAzure = process.env.FLUID_CLIENT === "azure";
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+	return (useAzure ? response.data.id : response.data) as string;
+}
--- packages/framework/presence/src/test/multiclient.spec.ts
+++ packages/framework/presence/src/test/multiclient.spec.ts
@@ -5,32 +5,40 @@
 
 import { strict as assert } from "node:assert";
 
-import { AzureClient, type AzureContainerServices } from "@fluidframework/azure-client";
-import { type AzureUser, ScopeType } from "@fluidframework/azure-client/internal";
 import { AttachState } from "@fluidframework/container-definitions";
+// import { AzureClient, type AzureContainerServices } from "@fluidframework/azure-client";
 import { ConnectionState } from "@fluidframework/container-loader";
 import { ContainerSchema, type IFluidContainer } from "@fluidframework/fluid-static";
-import {
-	acquirePresenceViaDataObject,
-	ExperimentalPresenceManager,
-	type ExperimentalPresenceDO,
-	type IPresence,
-	type ISessionClient,
-	// eslint-disable-next-line import/no-internal-modules
-} from "@fluidframework/presence/alpha";
+import { ScopeType } from "@fluidframework/driver-definitions/internal";
 import { timeoutPromise } from "@fluidframework/test-utils/internal";
+import {
+	TinyliciousClient,
+	type TinyliciousContainerServices,
+	type TinyliciousUser,
+} from "@fluidframework/tinylicious-client";
+import type { ConfigTypes, IConfigProviderBase } from "@fluidframework/core-interfaces";
 
+import {
+	ExperimentalPresenceManager,
+	getPresenceViaDataObject,
+	type Attendee,
+	type ExperimentalPresenceDO,
+	type Presence,
+} from "../index.js";
 import { createAzureClient } from "./AzureClientFactory.js";
-import { configProvider } from "./utils.js";
+
+const configProvider = (settings: Record<string, ConfigTypes>): IConfigProviderBase => ({
+	getRawConfig: (name: string): ConfigTypes => settings[name],
+});
 
 async function waitForAttendeeEvent(
-	event: "attendeeDisconnected" | "attendeeJoined",
-	...presences: IPresence[]
-): Promise<ISessionClient[]> {
+	event: "attendeeDisconnected" | "attendeeConnected",
+	...presences: Presence[]
+): Promise<Attendee[]> {
 	return Promise.all(
 		presences.map(async (presence, index) =>
-			timeoutPromise<ISessionClient>(
-				(resolve) => presence.events.on(event, (attendee) => resolve(attendee)),
+			timeoutPromise<Attendee>(
+				(resolve) => presence.attendees.events.on(event, (attendee) => resolve(attendee)),
 				{
 					durationMs: 2000,
 					errorMsg: `Attendee[${index}] Timeout`,
@@ -40,18 +48,18 @@ async function waitForAttendeeEvent(
 	);
 }
 
-describe(`Presence with AzureClient`, () => {
+describe(`Presence with TinyliciousClient`, () => {
 	const connectedContainers: IFluidContainer[] = [];
 	const connectTimeoutMs = 10_000;
-	const user1: AzureUser = {
+	const user1: TinyliciousUser = {
 		id: "test-user-id-1",
 		name: "test-user-name-1",
 	};
-	const user2: AzureUser = {
+	const user2: TinyliciousUser = {
 		id: "test-user-id-2",
 		name: "test-user-name-2",
 	};
-	const user3: AzureUser = {
+	const user3: TinyliciousUser = {
 		id: "test-user-id-3",
 		name: "test-user-name-3",
 	};
@@ -66,14 +74,14 @@ describe(`Presence with AzureClient`, () => {
 
 	const getOrCreatePresenceContainer = async (
 		id: string | undefined,
-		user: AzureUser,
+		user: TinyliciousUser,
 		config?: ReturnType<typeof configProvider>,
 		scopes?: ScopeType[],
 	): Promise<{
 		container: IFluidContainer;
-		presence: IPresence;
-		services: AzureContainerServices;
-		client: AzureClient;
+		presence: Presence;
+		services: TinyliciousContainerServices;
+		client: TinyliciousClient;
 		containerId: string;
 	}> => {
 		const client = createAzureClient(user.id, user.name, undefined, config, scopes);
@@ -83,7 +91,7 @@ describe(`Presence with AzureClient`, () => {
 			},
 		};
 		let container: IFluidContainer;
-		let services: AzureContainerServices;
+		let services: TinyliciousContainerServices;
 		let containerId: string;
 		if (id === undefined) {
 			({ container, services } = await client.createContainer(schema, "2"));
@@ -108,7 +116,7 @@ describe(`Presence with AzureClient`, () => {
 			"Container is not attached after attach is called",
 		);
 
-		const presence = acquirePresenceViaDataObject(
+		const presence = getPresenceViaDataObject(
 			container.initialObjects.presence as ExperimentalPresenceDO,
 		);
 		return {
@@ -129,12 +137,12 @@ describe(`Presence with AzureClient`, () => {
 		} = await getOrCreatePresenceContainer(undefined, user1);
 		const { presence: presence2 } = await getOrCreatePresenceContainer(containerId, user2);
 		// Wait for attendees to join
-		await waitForAttendeeEvent("attendeeJoined", presence1, presence2);
+		await waitForAttendeeEvent("attendeeConnected", presence1, presence2);
 		const { presence: presence3 } = await getOrCreatePresenceContainer(containerId, user3);
 		// Wait for attendees to join
-		await waitForAttendeeEvent("attendeeJoined", presence1, presence2, presence3);
+		await waitForAttendeeEvent("attendeeConnected", presence1, presence2, presence3);
 		// Get attendee we will disconnect
-		const disconnectedAttendee = presence1.getMyself();
+		const disconnectedAttendee = presence1.attendees.getMyself();
 
 		// ACT - Disconnect first attendee
 		container1.disconnect();
@@ -148,7 +156,11 @@ describe(`Presence with AzureClient`, () => {
 		assert.strictEqual(returnedAttendees.length, 2);
 
 		for (const attendee of returnedAttendees) {
-			assert.equal(attendee.sessionId, disconnectedAttendee.sessionId, "Session ID mismatch");
+			assert.equal(
+				attendee.attendeeId,
+				disconnectedAttendee.attendeeId,
+				"Session ID mismatch",
+			);
 			assert.equal(
 				attendee.getConnectionId(),
 				disconnectedAttendee.getConnectionId(),
