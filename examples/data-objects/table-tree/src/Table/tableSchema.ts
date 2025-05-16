1142d192d202d1ae9047e879f98e294e75391b16
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-05-16 18:50:59 +0000

    remove: Unused schema type

--- examples/data-objects/table-tree/src/Table/tableSchema.ts
+++ examples/data-objects/table-tree/src/Table/tableSchema.ts
@@ -46,47 +46,6 @@ export class DateTime extends schemaFactory.object("DateTime", {
 	}
 }
 
-/**
- * A SharedTree object that allows users to vote
- */
-export class Vote extends schemaFactory.object("Vote", {
-	votes: schemaFactory.map("votes", schemaFactory.string), // Map of votes
-}) {
-	addVote(vote: string): void {
-		if (this.votes.has(vote) === true) {
-			return;
-		}
-		this.votes.set(vote, "");
-	}
-
-	removeVote(vote: string): void {
-		if (this.votes.has(vote) === false) {
-			return;
-		}
-		this.votes.delete(vote);
-	}
-
-	/**
-	 * Toggle a vote in the map of votes
-	 */
-	toggleVote(vote: string): void {
-		if (this.votes.has(vote) === true) {
-			this.removeVote(vote);
-		} else {
-			this.addVote(vote);
-		}
-	}
-
-	get numberOfVotes(): number {
-		return this.votes.size;
-	}
-
-	// TODO: not sure if userId is necessary for this example app
-	hasVoted(userId: string): boolean {
-		return this.votes.has(userId);
-	}
-}
-
 export const Cell = [schemaFactory.string, schemaFactory.boolean, DateTime] as const;
 
 export class Column extends TableSchema.column({
