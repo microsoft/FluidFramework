fb1d10bee5d649a2e7bf19efdfc6b55f765edcb1
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-05-16 17:42:57 -0700

    refactor(table-tree): Misc. schema simplifications (#24649)
    Made a couple of minor schema simplifications in prep for future
    changes:
    - Removed unused `Vote` schema
    - Remove cell value wrapper (cells are primitives or DateTime, rather
    than a node wrapping one of those).
    - Inlined column/row props schema, since they aren't referenced
    elsewhere.

--- examples/data-objects/table-tree/src/Table/dataObject.ts
+++ examples/data-objects/table-tree/src/Table/dataObject.ts
@@ -55,7 +55,7 @@ export class TableDataObject extends TreeDataObject<TreeView<typeof Table>> {
 					{
 						id: "row-1",
 						cells: {
-							"column-1": { value: "Hello world!" },
+							"column-1": "Hello world!",
 						},
 						props: {},
 					},
