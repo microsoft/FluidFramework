69598967773f5e5ffa8e0902bc39b32c6bdf0a92
Rishhi <rishhib@microsoft.com>
2025-05-21 13:51:20 -0400

    create error obj

--- packages/drivers/driver-base/src/documentDeltaConnection.ts
+++ packages/drivers/driver-base/src/documentDeltaConnection.ts
@@ -401,7 +401,7 @@ export class DocumentDeltaConnection
 	 * However the OdspDocumentDeltaConnection differ in dispose as in there we don't close the socket. There is no
 	 * multiplexing here, so we need to close the socket here.
 	 */
-	public dispose() {
+	public dispose(error?: Error) {
 		this.logger.sendTelemetryEvent({
 			eventName: "ClientClosingDeltaConnection",
 			driverVersion,
@@ -409,7 +409,8 @@ export class DocumentDeltaConnection
 				...this.getConnectionDetailsProps(),
 			}),
 		});
-		this.disconnect();
+		const disconnectError = error ? this.createErrorObject("dispose", error, false) : undefined;
+		this.disconnect(disconnectError);
 	}
 
 	protected disconnect(err?: IAnyDriverError) {
