ad122a0ce893e92acc356c14d460c283f00d06c4
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-05-22 22:23:31 +0000

    refactor: Export props interface

--- examples/benchmarks/bubblebench/common/src/index.ts
+++ examples/benchmarks/bubblebench/common/src/index.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-export { AppView } from "./view/index.js";
+export { AppView, type IAppProps } from "./view/index.js";
 export { IAppState, IBubble, IClient, makeBubble, makeClient, SimpleClient } from "./types.js";
 export { normal, randomColor, rnd } from "./rnd.js";
 export { Stats } from "./stats.js";
