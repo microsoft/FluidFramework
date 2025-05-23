097afdaf0fb67742c86b95d2674d75d3bc897e81
Tyler Butler <tylerbu@microsoft.com>
2025-05-22 17:55:21 -0700

    config

--- packages/dds/task-manager/.eslintrc.cjs
+++ packages/dds/task-manager/.eslintrc.cjs
@@ -4,7 +4,7 @@
  */
 
 module.exports = {
-	extends: [require.resolve("@fluidframework/eslint-config-fluid/recommended"), "prettier"],
+	extends: [require.resolve("@fluidframework/eslint-config-fluid/strict"), "prettier"],
 
 	parserOptions: {
 		project: ["./tsconfig.json", "./src/test/tsconfig.json"],
