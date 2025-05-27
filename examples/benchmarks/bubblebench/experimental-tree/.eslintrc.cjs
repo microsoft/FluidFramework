ef71946471cf2fe39ce84faeab9953e266552217
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-05-27 15:57:42 -0700

    refactor(bubblebench/experimental-tree): Promote eslint config and fix violations (#24699)
    The `minimal-deprecated` config is deprecated and scheduled for
    deletion.
    
    
    [AB#7993](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/7993)

--- examples/benchmarks/bubblebench/experimental-tree/.eslintrc.cjs
+++ examples/benchmarks/bubblebench/experimental-tree/.eslintrc.cjs
@@ -5,7 +5,7 @@
 
 module.exports = {
 	extends: [
-		require.resolve("@fluidframework/eslint-config-fluid/minimal-deprecated"),
+		require.resolve("@fluidframework/eslint-config-fluid"),
 		"prettier",
 		"../../../.eslintrc.cjs",
 	],
@@ -13,6 +13,12 @@ module.exports = {
 		// TODO: AB#18875 - Re-enable react/no-deprecated once we replace uses of the deprecated ReactDOM.render()
 		// with the new React 18 createRoot().
 		"react/no-deprecated": "off",
+
+		// Causes eslint to stack-overflow. Given that this is testing a DDS we are no longer actively developing,
+		// we can probably just ignore this until this package is eventually removed.
+		"@typescript-eslint/no-unsafe-argument": "off",
+
+		// TODO: fix violations and remove this override
 		"@fluid-internal/fluid/no-unchecked-record-access": "warn",
 	},
 };
