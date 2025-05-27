a9493550d143a34fc7ded306c488bfe8c9a98cab
Tyler Butler <tylerbu@microsoft.com>
2025-05-27 15:28:34 -0700

    bump

--- packages/loader/driver-utils/src/packageVersion.ts
+++ packages/loader/driver-utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/driver-utils";
-export const pkgVersion = "2.41.0";
+export const pkgVersion = "2.41.1";
--- packages/utils/odsp-doclib-utils/src/packageVersion.ts
+++ packages/utils/odsp-doclib-utils/src/packageVersion.ts
@@ -6,4 +6,4 @@
  */
 
 export const pkgName = "@fluidframework/odsp-doclib-utils";
-export const pkgVersion = "2.41.0";
+export const pkgVersion = "2.41.1";
