3f0aa29d6d90d55dba501c79e19fd2e7cddb49b5
Jason Hartman <jasonha@microsoft.com>
2025-05-28 13:08:48 -0700

    docs(client-presence): missing API comments adds documentation to beta APIs that are not `@system` nor `attendees` or `states` properties of `Presence`
    Follow-up for #24710

--- packages/framework/presence/src/latestMapValueManager.ts
+++ packages/framework/presence/src/latestMapValueManager.ts
@@ -23,7 +23,7 @@ import { datastoreFromHandle, type StateDatastore } from "./stateDatastore.js";
 import { brandIVM } from "./valueManager.js";
 
 /**
- * Collection of latest known values for a specific client.
+ * Collection of latest known values for a specific {@link Attendee}.
  *
  * @sealed
  * @beta
@@ -34,11 +34,13 @@ export interface LatestMapClientData<
 	SpecificAttendeeId extends AttendeeId = AttendeeId,
 > {
 	/**
-	 * Associated attendee.
+	 * Associated {@link Attendee}.
 	 */
 	attendee: Attendee<SpecificAttendeeId>;
 
 	/**
+	 * Map of items for the state.
+	 *
 	 * @privateRemarks This could be regular map currently as no Map is
 	 * stored internally and a new instance is created for every request.
 	 */
@@ -53,6 +55,9 @@ export interface LatestMapClientData<
  */
 export interface LatestMapItemUpdatedClientData<T, K extends string | number>
 	extends LatestClientData<T> {
+	/**
+	 * Key of the updated item.
+	 */
 	key: K;
 }
 
@@ -63,12 +68,23 @@ export interface LatestMapItemUpdatedClientData<T, K extends string | number>
  * @beta
  */
 export interface LatestMapItemRemovedClientData<K extends string | number> {
+	/**
+	 * Associated {@link Attendee}.
+	 */
 	attendee: Attendee;
+	/**
+	 * Key of the removed item.
+	 */
 	key: K;
+	/**
+	 * Metadata associated with the removal of the item.
+	 */
 	metadata: LatestMetadata;
 }
 
 /**
+ * Events from {@link LatestMapRaw}.
+ *
  * @sealed
  * @beta
  */
@@ -135,6 +151,8 @@ export interface StateMap<K extends string | number, V> {
 	clear(): void;
 
 	/**
+	 * Removes the element with the specified key from the StateMap, if it exists.
+	 *
 	 * @returns true if an element in the StateMap existed and has been removed, or false if
 	 * the element does not exist.
 	 * @remarks No entry is fully removed. Instead an undefined placeholder is locally and
@@ -158,12 +176,14 @@ export interface StateMap<K extends string | number, V> {
 	): void;
 
 	/**
-	 * Returns a specified element from the StateMap object.
+	 * Returns the element with the specified key from the StateMap.
+	 *
 	 * @returns Returns the element associated with the specified key. If no element is associated with the specified key, undefined is returned.
 	 */
 	get(key: K): DeepReadonly<JsonDeserialized<V>> | undefined;
 
 	/**
+	 * Checks if an element with the specified key exists in the StateMap.
 	 * @returns boolean indicating whether an element with the specified key exists or not.
 	 */
 	has(key: K): boolean;
@@ -179,6 +199,8 @@ export interface StateMap<K extends string | number, V> {
 	set(key: K, value: JsonSerializable<V> & JsonDeserialized<V>): this;
 
 	/**
+	 * The number of elements in the StateMap.
+	 *
 	 * @returns the number of elements in the StateMap.
 	 */
 	readonly size: number;
