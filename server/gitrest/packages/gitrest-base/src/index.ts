bde4530a82726677ff3bea82a76b0fe7ecf14415
Xin Zhang <zhangxin@microsoft.com>
2025-06-03 15:55:28 -0400

    export HybridFsManagerFactory to root

--- server/gitrest/packages/gitrest-base/src/index.ts
+++ server/gitrest/packages/gitrest-base/src/index.ts
@@ -23,6 +23,7 @@ export {
 	getSoftDeletedMarkerPath,
 	GitObjectType,
 	GitWholeSummaryManager,
+	HybridFsManagerFactory,
 	IExternalWriterConfig,
 	IFileSystemManager,
 	IFileSystemManagerFactories,
