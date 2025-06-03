f112cb07550e9431b7b78ea75eb80ddc1081cc01
Mark Fields <markfields@users.noreply.github.com>
2025-06-03 09:51:57 -0700

    fix(staging-mode): Add some more preconditions to enterStagingMode (#24738)
    Adding these preconditions to `enterStagingMode`:
    
    1. Must not be detached.
    - Changes made while detached are local-only already, and typically
    don't result in ops anyway (included in attach summary instead), so this
    is not a useful mode. So codify this assumption to simplify our own
    reasoning about Staging Mode
    2. Must be using TurnBased flush mode
    - TurnBased is the only option we provide, with one caveat which is 2.x
    Declarative Mode clients that want to support collab with 1.x clients.
    We don't anticipate offering Staging Mode in that configuration.

--- packages/runtime/container-runtime/src/containerRuntime.ts
+++ packages/runtime/container-runtime/src/containerRuntime.ts
@@ -3419,7 +3419,10 @@ export class ContainerRuntime
 	// eslint-disable-next-line import/no-deprecated
 	public enterStagingMode = (): StageControlsExperimental => {
 		if (this.stageControls !== undefined) {
-			throw new Error("already in staging mode");
+			throw new UsageError("already in staging mode");
+		}
+		if (this.attachState === AttachState.Detached) {
+			throw new UsageError("cannot enter staging mode while detached");
 		}
 
 		// Make sure all BatchManagers are empty before entering staging mode,
