e2f265cfbcea476066406586c2c8abc980d44498
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-06-08 20:19:10 +0000

    refactor: Cleanup

--- tools/api-markdown-documenter/src/documentation-domain-to-markdown/default-transformations/HeadingToMarkdown.ts
+++ tools/api-markdown-documenter/src/documentation-domain-to-markdown/default-transformations/HeadingToMarkdown.ts
@@ -35,48 +35,65 @@ export function headingToMarkdown(
 	headingNode: HeadingNode,
 	context: TransformationContext,
 ): MdastBlockContent[] {
-	const { headingLevel, transformations } = context;
+	// Markdown only supports heading levels up to 6. If our level is beyond that, we will transform the input to simple
+	// bold text, with an accompanying HTML anchor to ensure we can still link to the text.
+	return isInHeadingRange(context.headingLevel)
+		? transformAsHeading(headingNode, context, context.headingLevel)
+		: transformAsBoldText(headingNode, context);
+}
+
+function transformAsHeading(
+	headingNode: HeadingNode,
+	context: TransformationContext,
+	headingLevel: 1 | 2 | 3 | 4 | 5 | 6,
+): MdastBlockContent[] {
+	const { transformations } = context;
 
 	const transformedTitle = transformations[headingNode.title.type](headingNode.title, context);
 
-	// HTML only supports heading levels up to 6. If our level is beyond that, we will transform the input to simple
-	// bold text, with an accompanying anchor to ensure we can still link to the text.
-	if (isInHeadingRange(headingLevel)) {
-		if (headingNode.id !== undefined) {
-			transformedTitle.push({ type: "text", value: ` {#${headingNode.id}}` });
-		}
-
-		const heading: MdastHeading = {
-			type: "heading",
-			depth: headingLevel,
-			children: transformedTitle,
-		};
-		return [heading];
-	} else {
-		const boldTitle: MdastParagraph = {
-			type: "paragraph",
-			children: [
-				{
-					type: "strong",
-					children: transformedTitle,
-				},
-			],
-		};
-
-		if (headingNode.id === undefined) {
-			return [boldTitle];
-		}
-
-		const anchorHtml: MdastHtml = {
-			type: "html",
-			value: `<a id="${headingNode.id ?? ""}"></a>`,
-		};
-		const lineBreak: Break = { type: "break" };
-		return [
-			{
-				type: "paragraph",
-				children: [anchorHtml, lineBreak, ...boldTitle.children],
-			},
-		];
+	if (headingNode.id !== undefined) {
+		transformedTitle.push({ type: "text", value: ` {#${headingNode.id}}` });
 	}
+
+	const heading: MdastHeading = {
+		type: "heading",
+		depth: headingLevel,
+		children: transformedTitle,
+	};
+	return [heading];
+}
+
+function transformAsBoldText(
+	headingNode: HeadingNode,
+	context: TransformationContext,
+): MdastBlockContent[] {
+	const { transformations } = context;
+
+	const transformedTitle = transformations[headingNode.title.type](headingNode.title, context);
+
+	const boldTitle: MdastParagraph = {
+		type: "paragraph",
+		children: [
+			{
+				type: "strong",
+				children: transformedTitle,
+			},
+		],
+	};
+
+	if (headingNode.id === undefined) {
+		return [boldTitle];
+	}
+
+	const anchorHtml: MdastHtml = {
+		type: "html",
+		value: `<a id="${headingNode.id ?? ""}"></a>`,
+	};
+	const lineBreak: Break = { type: "break" };
+	return [
+		{
+			type: "paragraph",
+			children: [anchorHtml, lineBreak, ...boldTitle.children],
+		},
+	];
 }
