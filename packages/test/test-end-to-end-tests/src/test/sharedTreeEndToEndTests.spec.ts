d6712cab492ea020d4081ab48efc3e78354262e5
Navin Agarwal <naagar@microsoft.com>
2025-06-13 16:18:29 -0700

    Part 3: Tests

---
+++ packages/test/test-end-to-end-tests/src/test/sharedTreeEndToEndTests.spec.ts
@@ -0,0 +1,366 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "assert";
+
+import { describeCompat } from "@fluid-private/test-version-utils";
+import { LoaderHeader, type IContainer } from "@fluidframework/container-definitions/internal";
+import { IFluidHandle } from "@fluidframework/core-interfaces";
+import { SummaryType, type ISummaryTree } from "@fluidframework/driver-definitions";
+import {
+	createSummarizerFromFactory,
+	ITestObjectProvider,
+	summarizeNow,
+} from "@fluidframework/test-utils/internal";
+import {
+	ITree,
+	SchemaFactory,
+	TreeViewConfiguration,
+	type TreeView,
+} from "@fluidframework/tree";
+import {
+	configuredSharedTree,
+	ForestTypeOptimized,
+	Tree,
+	TreeAlpha,
+} from "@fluidframework/tree/internal";
+
+const schemaFactory = new SchemaFactory("sharedTreeE2ETests");
+class NoteMetadata extends schemaFactory.object("subNote", {
+	metadataId: schemaFactory.number,
+	metaText: schemaFactory.optional(schemaFactory.string),
+}) {}
+class Note extends schemaFactory.object("note", {
+	noteId: schemaFactory.number,
+	text: schemaFactory.string,
+	color: schemaFactory.string,
+	metadata: schemaFactory.optional(NoteMetadata),
+}) {}
+class NoteList extends schemaFactory.object("noteList", {
+	listId: schemaFactory.identifier,
+	notes: schemaFactory.array(Note),
+}) {}
+
+class Board extends schemaFactory.object("board", {
+	boardId: schemaFactory.string,
+	lists: schemaFactory.array(NoteList),
+}) {}
+
+function createInitialBoard() {
+	const note1 = new Note({
+		noteId: 1,
+		text: "Note 1",
+		color: "red",
+		metadata: {
+			metadataId: 1,
+			metaText: "Meta 1",
+		},
+	});
+	const note2 = new Note({
+		noteId: 2,
+		text: "Note 2",
+		color: "yellow",
+		metadata: {
+			metadataId: 2,
+		},
+	});
+	const noteList1 = new NoteList({
+		listId: "l1",
+		notes: [note1, note2],
+	});
+
+	const note3 = new Note({
+		noteId: 3,
+		text: "Note 3",
+		color: "blue",
+		metadata: {
+			metadataId: 3,
+			metaText: "Meta 3",
+		},
+	});
+	const note4 = new Note({
+		noteId: 4,
+		text: "Note 4",
+		color: "green",
+	});
+	const noteList2 = new NoteList({
+		listId: "l2",
+		notes: [note3, note4],
+	});
+
+	const note5 = new Note({
+		noteId: 5,
+		text: "Note 5",
+		color: "purple",
+		metadata: {
+			metadataId: 5,
+			metaText: "Meta 5",
+		},
+	});
+	const noteList3 = new NoteList({
+		listId: "l3",
+		notes: [note5],
+	});
+	return new Board({
+		boardId: "b1",
+		lists: [noteList1, noteList2, noteList3],
+	});
+}
+
+function updateNote(view: TreeView<typeof Board>, listIndex: number, noteIndex: number) {
+	const list = view.root.lists.at(listIndex);
+	assert(list !== undefined);
+	const note = list.notes.at(noteIndex);
+	assert(note !== undefined);
+	note.text = `Note ${noteIndex + 1} updated`;
+}
+
+function updateMetadata(view: TreeView<typeof Board>, listIndex: number, noteIndex: number) {
+	const list = view.root.lists.at(listIndex);
+	assert(list !== undefined);
+	const note = list.notes.at(noteIndex);
+	assert(note !== undefined);
+	const metadata = note.metadata;
+	assert(metadata !== undefined, "No metadata to update");
+	metadata.metadataId = 100;
+}
+
+function updateMetadataText(
+	view: TreeView<typeof Board>,
+	listIndex: number,
+	noteIndex: number,
+) {
+	const list = view.root.lists.at(listIndex);
+	assert(list !== undefined);
+	const note = list.notes.at(noteIndex);
+	assert(note !== undefined);
+	const metadata = note.metadata;
+	assert(metadata !== undefined, "No metadata to update");
+	metadata.metaText = `Metadata for ${noteIndex + 1} updated`;
+}
+
+function validateTreesEqual(
+	actualView: TreeView<typeof Board>,
+	expectedView: TreeView<typeof Board>,
+): void {
+	const actualRoot = actualView.root;
+	const expectedRoot = expectedView.root;
+	if (actualRoot === undefined || expectedRoot === undefined) {
+		assert.equal(actualRoot === undefined, expectedRoot === undefined);
+		return;
+	}
+
+	// Validate the same schema objects are used.
+	assert.equal(Tree.schema(actualRoot), Tree.schema(expectedRoot));
+
+	// This should catch all cases, assuming exportVerbose works correctly.
+	assert.deepEqual(TreeAlpha.exportVerbose(actualRoot), TreeAlpha.exportVerbose(expectedRoot));
+
+	// Since this uses some of the tools to compare trees that this is testing for, perform the comparison in a few ways to reduce risk of a bug making this pass when it shouldn't:
+	// This case could have false negatives (two trees with ambiguous schema could export the same concise tree),
+	// but should have no false positives since equal trees always have the same concise tree.
+	assert.deepEqual(TreeAlpha.exportConcise(actualRoot), TreeAlpha.exportConcise(expectedRoot));
+}
+
+function validateHandlePathExists(pathPaths: string[], summaryTree: ISummaryTree) {
+	const currentPath = pathPaths[0];
+	let found = false;
+	for (const [key, summaryObject] of Object.entries(summaryTree.tree)) {
+		if (key === currentPath) {
+			found = true;
+			if (pathPaths.length > 1) {
+				assert(
+					summaryObject.type === SummaryType.Tree || summaryObject.type === SummaryType.Handle,
+					`Handle path ${currentPath} should be for a subtree or a handle`,
+				);
+				if (summaryObject.type === SummaryType.Tree) {
+					validateHandlePathExists(pathPaths.slice(1), summaryObject);
+				}
+			}
+			break;
+		}
+	}
+	assert(found, `Handle path ${currentPath} not found in summary tree`);
+}
+
+function validateSummaryTree(currentSummary: ISummaryTree, lastSummary: ISummaryTree) {
+	for (const [key, summaryObject] of Object.entries(currentSummary.tree)) {
+		if (summaryObject.type === SummaryType.Handle) {
+			// Validate that the id (summary path) exists in lastSummary
+			validateHandlePathExists(summaryObject.handle.split("/").slice(1), lastSummary);
+			console.log(`Validated handle path: ${summaryObject.handle}`);
+		} else if (summaryObject.type === SummaryType.Tree) {
+			// Recursively validate nested trees
+			validateSummaryTree(summaryObject, lastSummary);
+		}
+	}
+}
+
+describeCompat("SharedTree", "NoCompat", (getTestObjectProvider, apis) => {
+	const { DataObject, DataObjectFactory } = apis.dataRuntime;
+	const { ContainerRuntimeFactoryWithDefaultDataStore } = apis.containerRuntime;
+	const SharedTree = configuredSharedTree({ forest: ForestTypeOptimized });
+
+	// An extension of Aqueduct's DataObject that creates a SharedTree during initialization and exposes it.
+	class DataObjectWithTree extends DataObject {
+		private readonly treeKey = "tree";
+
+		private _tree: ITree | undefined;
+		public get tree(): ITree {
+			assert(this._tree !== undefined, "Tree not initialized");
+			return this._tree;
+		}
+
+		protected async initializingFirstTime() {
+			const tree = SharedTree.create(this.runtime);
+			this.root.set(this.treeKey, tree.handle);
+		}
+
+		protected async hasInitialized() {
+			const treeHandle = this.root.get<IFluidHandle<ITree>>(this.treeKey);
+			assert(treeHandle, "Tree handle not found");
+			this._tree = await treeHandle.get();
+		}
+	}
+	// A data object factory that creates DataObjectWithTree instances.
+	const dataObjectFactoryWithTree = new DataObjectFactory(
+		"DataObjectWithTree",
+		DataObjectWithTree,
+		[SharedTree.getFactory()],
+		{},
+	);
+
+	// Runtime factory that can create containers with DataObjectWithTree instances via the above data object factory.
+	const runtimeFactory = new ContainerRuntimeFactoryWithDefaultDataStore({
+		defaultFactory: dataObjectFactoryWithTree,
+		registryEntries: [
+			[dataObjectFactoryWithTree.type, Promise.resolve(dataObjectFactoryWithTree)],
+		],
+		runtimeOptions: {
+			enableRuntimeIdCompressor: "on",
+			summaryOptions: { summaryConfigOverrides: { state: "disabled" } },
+		}, // Needed to create a shared tree
+	});
+
+	let provider: ITestObjectProvider;
+	let container1: IContainer;
+	let dataObject1: DataObjectWithTree;
+	let tree1: ITree;
+
+	const loadSummarizer = async (container: IContainer, summaryVersion?: string) => {
+		return createSummarizerFromFactory(
+			provider,
+			container,
+			dataObjectFactoryWithTree,
+			summaryVersion,
+		);
+	};
+
+	async function loadContainerAndTree(summaryVersion?: string) {
+		const loader = provider.createLoader([[provider.defaultCodeDetails, runtimeFactory]]);
+		const loadUrl = await container1.getAbsoluteUrl("");
+		assert(loadUrl !== undefined, "Container's absolute URL is undefined");
+		const container = await loader.resolve({
+			url: loadUrl,
+			headers: { [LoaderHeader.version]: summaryVersion },
+		});
+		const dataObject = (await container.getEntryPoint()) as DataObjectWithTree;
+		return { container, tree: dataObject.tree };
+	}
+
+	beforeEach("setup", async function () {
+		provider = getTestObjectProvider({ syncSummarizer: true });
+
+		// These tests are not service specific. They test internals of the SharedTree and different services
+		// won't make a difference. So, only run them for local server to reduce the number of test combinations
+		// it runs in.
+		if (provider.driver.type !== "local") {
+			this.skip();
+		}
+
+		// Create a loader and a detached container.
+		const loader1 = provider.createLoader([[provider.defaultCodeDetails, runtimeFactory]]);
+		container1 = await loader1.createDetachedContainer(provider.defaultCodeDetails);
+		// Get the create new request to attach the container with.
+		const request = provider.driver.createCreateNewRequest(provider.documentId);
+		dataObject1 = (await container1.getEntryPoint()) as DataObjectWithTree;
+		tree1 = dataObject1.tree;
+		await container1.attach(request);
+	});
+
+	describe("Incremental tree summary", () => {
+		const treeViewConfig = new TreeViewConfiguration({ schema: Board });
+		let treeViewClient1: TreeView<typeof Board>;
+
+		beforeEach(async () => {
+			treeViewClient1 = tree1.viewWith(treeViewConfig);
+			treeViewClient1.initialize(createInitialBoard());
+			await provider.ensureSynchronized();
+		});
+
+		it("3 levels of incrementality", async () => {
+			const { summarizer } = await loadSummarizer(container1);
+			await provider.ensureSynchronized();
+			const summary1 = await summarizeNow(summarizer);
+
+			const { container: container2, tree: treeClient2 } = await loadContainerAndTree(
+				summary1.summaryVersion,
+			);
+			assert(treeClient2 !== undefined, "Tree treeClient2 is undefined");
+			const treeViewClient2 = treeClient2.viewWith(
+				new TreeViewConfiguration({
+					schema: Board,
+				}),
+			);
+			validateTreesEqual(treeViewClient2, treeViewClient1);
+			container2.close();
+
+			updateNote(treeViewClient1, 0, 0);
+			updateMetadata(treeViewClient1, 1, 0);
+			updateMetadataText(treeViewClient1, 2, 0);
+
+			// Second summary at sequence number 10 - previous was at sequence number 0.
+			await provider.ensureSynchronized();
+			const summary2 = await summarizeNow(summarizer);
+			assert(summary2 !== undefined);
+			validateSummaryTree(summary2.summaryTree, summary1.summaryTree);
+
+			const { container: container3, tree: treeClient3 } = await loadContainerAndTree(
+				summary2.summaryVersion,
+			);
+			assert(treeClient3 !== undefined, "Tree treeClient3 is undefined");
+			const treeViewClient3 = treeClient3.viewWith(
+				new TreeViewConfiguration({
+					schema: Board,
+				}),
+			);
+			validateTreesEqual(treeViewClient3, treeViewClient1);
+			container3.close();
+
+			updateNote(treeViewClient1, 0, 0);
+			updateMetadata(treeViewClient1, 0, 0);
+			updateMetadata(treeViewClient1, 1, 0);
+
+			// Third summary at sequence number 20 - previous was at sequence number 10.
+			await provider.ensureSynchronized();
+			const summary3 = await summarizeNow(summarizer);
+			assert(summary3 !== undefined);
+			validateSummaryTree(summary3.summaryTree, summary2.summaryTree);
+
+			const { container: container4, tree: treeClient4 } = await loadContainerAndTree(
+				summary3.summaryVersion,
+			);
+			assert(treeClient4 !== undefined, "Tree treeClient4 is undefined");
+			const treeViewClient4 = treeClient4.viewWith(
+				new TreeViewConfiguration({
+					schema: Board,
+				}),
+			);
+			validateTreesEqual(treeViewClient4, treeViewClient1);
+			container4.close();
+			validateSummaryTree(summary3.summaryTree, summary2.summaryTree);
+		});
+	});
+});
