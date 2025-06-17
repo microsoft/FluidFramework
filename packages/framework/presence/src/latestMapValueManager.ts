1e1980a84f6e65c8923a2de6c039042aa3c531d4
Tyler Butler <tylerbu@microsoft.com>
2025-06-17 15:38:10 -0700

    lint

--- packages/framework/presence/src/latestMapValueManager.ts
+++ packages/framework/presence/src/latestMapValueManager.ts
@@ -670,9 +670,6 @@ export function latestMap<
 
 // #endregion
 
-/**
- * @beta
- */
 // eslint-disable-next-line jsdoc/require-jsdoc -- no tsdoc since the overloads are documented
 export function latestMap<
 	T,
