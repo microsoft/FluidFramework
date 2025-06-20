c885a4970bf2592503e32b277905a4a9ddc24b38
chentong7 <chentong@microsoft.com>
2025-06-20 15:18:48 -0700

    add undo/redo

--- packages/dds/tree/src/test/testHelper.ts
+++ packages/dds/tree/src/test/testHelper.ts
@@ -5,9 +5,15 @@
 
 // eslint-disable-next-line import/no-internal-modules
 import { createIdCompressor } from "@fluidframework/id-compressor/legacy";
-import { independentView } from "../shared-tree/index.js";
 import { SchemaFactoryAlpha, TreeViewConfiguration } from "../simple-tree/index.js";
 import { TableSchema } from "../tableSchema.js";
+import { DefaultTestSharedTreeKind } from "./utils.js";
+import { AttachState } from "@fluidframework/container-definitions";
+import {
+	MockContainerRuntimeFactory,
+	MockFluidDataStoreRuntime,
+} from "@fluidframework/test-runtime-utils/internal";
+import { CommitKind, type Revertible } from "../core/index.js";
 
 /**
  * Define a return type for table tree creation.
@@ -38,6 +44,18 @@ export interface TableTreeDefinition {
 	 */
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	table: any;
+	/**
+	 * The undo stack for the table tree.
+	 */
+	undoStack: Revertible[];
+	/**
+	 * The redo stack for the table tree.
+	 * */
+	redoStack: Revertible[];
+	/**
+	 * Unsubscribe from the table tree events and dispose of the undo/redo stacks.
+	 */
+	unsubscribe: () => void;
 }
 
 /**
@@ -66,15 +84,61 @@ export function createTableTree(tableSize: number, cellValue: string): TableTree
 		row: Row,
 	}) {}
 
-	const treeView = independentView(
+	const sharedTreeFactory = DefaultTestSharedTreeKind.getFactory();
+	const runtime = new MockFluidDataStoreRuntime({
+		idCompressor: createIdCompressor(),
+		attachState: AttachState.Detached,
+	});
+	const tree = sharedTreeFactory.create(runtime, "tree");
+	const runtimeFactory = new MockContainerRuntimeFactory();
+	runtimeFactory.createContainerRuntime(runtime);
+
+	const treeView = tree.viewWith(
 		new TreeViewConfiguration({
 			schema: Table,
 			enableSchemaValidation: true,
 		}),
-		{ idCompressor: createIdCompressor() },
 	);
 
 	treeView.initialize(Table.empty());
+	const undoStack: Revertible[] = [];
+	const redoStack: Revertible[] = [];
+
+	function onDispose(disposed: Revertible): void {
+		const redoIndex = redoStack.indexOf(disposed);
+		if (redoIndex !== -1) {
+			redoStack.splice(redoIndex, 1);
+		} else {
+			const undoIndex = undoStack.indexOf(disposed);
+			if (undoIndex !== -1) {
+				undoStack.splice(undoIndex, 1);
+			}
+		}
+	}
+
+	const unsubscribeFromCommitAppliedEvent = treeView.events.on(
+		"commitApplied",
+		(commit, getRevertible) => {
+			if (getRevertible !== undefined) {
+				const revertible = getRevertible(onDispose);
+				if (commit.kind === CommitKind.Undo) {
+					redoStack.push(revertible);
+				} else {
+					undoStack.push(revertible);
+				}
+			}
+		},
+	);
+	const unsubscribe = (): void => {
+		unsubscribeFromCommitAppliedEvent();
+		for (const revertible of undoStack) {
+			revertible.dispose();
+		}
+		for (const revertible of redoStack) {
+			revertible.dispose();
+		}
+	};
+
 	const table = treeView.root;
 	for (let i = 0; i < tableSize; i++) {
 		const column = new Column({ id: `column-${i}` });
@@ -101,6 +165,9 @@ export function createTableTree(tableSize: number, cellValue: string): TableTree
 		Row,
 		Table,
 		table,
+		undoStack,
+		redoStack,
+		unsubscribe,
 	};
 	return result;
 }
--- packages/dds/tree/src/test/time/tableTree.spec.ts
+++ packages/dds/tree/src/test/time/tableTree.spec.ts
@@ -29,7 +29,6 @@ function runBenchmark({
 	title: string;
 	tableSize: number;
 	cellValue: string;
-
 	operation: (tree: TableTreeDefinition) => void;
 	minBatchDurationSeconds?: number;
 	maxBenchmarkDurationSeconds: number;
@@ -59,61 +58,62 @@ function runBenchmark({
 	});
 }
 
-// /**
-//  * This function runs a benchmark for undo/redo operations on a SharedTree.
-//  */
-// // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-// function runUndoRedoBenchmark({
-// 	title,
-// 	tableSize,
-// 	cellValue,
-// 	setupOperation,
-// 	stackOperation,
-// 	minBatchDurationSeconds = 0,
-// 	maxBenchmarkDurationSeconds,
-// }: {
-// 	title: string;
-// 	tableSize: number;
-// 	cellValue: string;
-// 	/**
-// 	 * A function that sets up the operation to be performed on the tree and stack.
-// 	 */
-// 	setupOperation: (tree: TableTreeDefinition, stack: UndoRedoStackManager) => void;
-// 	/**
-// 	 * The operation to perform on the stack. This should be a function that takes an UndoRedoStackManager
-// 	 * and performs the desired operation.
-// 	 */
-// 	stackOperation: (stack: UndoRedoStackManager) => void;
-// 	minBatchDurationSeconds?: number;
-// 	maxBenchmarkDurationSeconds: number;
-// }) {
-// 	benchmark({
-// 		type: BenchmarkType.Measurement,
-// 		title,
-// 		benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
-// 			let duration: number;
-// 			do {
-// 				assert.equal(state.iterationsPerBatch, 1, "Expected exactly one iteration per batch");
+/**
+ * This function runs a benchmark for undo/redo operations on a SharedTree.
+ */
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function runUndoRedoBenchmark({
+	title,
+	tableSize,
+	cellValue,
+	setupOperation,
+	stackOperation,
+	minBatchDurationSeconds = 0,
+	maxBenchmarkDurationSeconds,
+}: {
+	title: string;
+	tableSize: number;
+	cellValue: string;
+	/**
+	 * A function that sets up the operation to be performed on the tree.
+	 */
+	setupOperation: (tree: TableTreeDefinition) => void;
+	/**
+	 * The operation to perform on the stack. This should be a function that takes an UndoRedoStackManager
+	 * and performs the desired operation.
+	 */
+	stackOperation: (tree: TableTreeDefinition) => void;
+	minBatchDurationSeconds?: number;
+	maxBenchmarkDurationSeconds: number;
+}) {
+	benchmark({
+		type: BenchmarkType.Measurement,
+		title,
+		benchmarkFnCustom: async <T>(state: BenchmarkTimer<T>) => {
+			let duration: number;
+			do {
+				assert.equal(state.iterationsPerBatch, 1, "Expected exactly one iteration per batch");
 
-// 				// Setup
-// 				const localTree: TableTreeDefinition = createTableTree(tableSize, cellValue);
-// 				const stack = new UndoRedoStackManager();
-// 				localTree.openUndo(stack);
-// 				setupOperation(localTree, stack);
+				// Setup
+				const localTree: TableTreeDefinition = createTableTree(tableSize, cellValue);
+				setupOperation(localTree);
 
-// 				// Operation
-// 				const before = state.timer.now();
-// 				stackOperation(stack);
-// 				const after = state.timer.now();
+				// Operation
+				const before = state.timer.now();
+				stackOperation(localTree);
+				const after = state.timer.now();
 
-// 				// Measure
-// 				duration = state.timer.toSeconds(before, after);
-// 			} while (state.recordBatch(duration));
-// 		},
-// 		minBatchDurationSeconds,
-// 		maxBenchmarkDurationSeconds,
-// 	});
-// }
+				// Measure
+				duration = state.timer.toSeconds(before, after);
+
+				// Cleanup
+				localTree.unsubscribe();
+			} while (state.recordBatch(duration));
+		},
+		minBatchDurationSeconds,
+		maxBenchmarkDurationSeconds,
+	});
+}
 
 describe("SharedTree execution time", () => {
 	// The value to be set in the cells of the tree.
@@ -148,10 +148,57 @@ describe("SharedTree execution time", () => {
 					tableSize: treeSize,
 					cellValue,
 					operation: (tree: TableTreeDefinition) => {
+						const { Column, table } = tree;
 						for (let i = 0; i < count; i++) {
-							const { Column, table } = tree;
-							const column = new Column({ id: uuid() });
+							const column = new Column({ id: `column-${uuid()}` });
 							table.insertColumn({ index: Math.floor(table.columns.length / 2), column });
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of undoing insert a column in the middle for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Undo insert the middle column ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { Column, table } = tree;
+						for (let i = 0; i < count; i++) {
+							const column = new Column({ id: `column-${uuid()}` });
+							table.insertColumn({ index: Math.floor(table.columns.length / 2), column });
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						// assert.equal(stack.undoStackLength, count);
+						const { undoStack } = tree;
+						for (let i = 0; i < count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of the SharedTree for redoing an insert column in the middle for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Redo insert the middle column ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { Column, table, undoStack } = tree;
+						for (let i = 0; i < count; i++) {
+							const column = new Column({ id: `column-${uuid()}` });
+							table.insertColumn({ index: Math.floor(table.columns.length / 2), column });
+						}
+						for (let i = 0; i < count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						// assert.equal(stack.undoStackLength, count);
+						const { redoStack } = tree;
+						for (let i = 0; i < count; i++) {
+							redoStack.pop()?.revert();
 						}
 					},
 					maxBenchmarkDurationSeconds,
@@ -165,10 +212,57 @@ describe("SharedTree execution time", () => {
 					tableSize: treeSize,
 					cellValue,
 					operation: (tree: TableTreeDefinition) => {
+						const { Row, table } = tree;
 						for (let i = 0; i < count; i++) {
-							const { Row, table } = tree;
-							const row = new Row({ id: uuid(), cells: {} });
+							const row = new Row({ id: `row-${uuid()}`, cells: {} });
 							table.insertRow({ index: Math.floor(table.rows.length / 2), row });
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of undoing insert a row in the middle for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Undo insert the middle row ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { Row, table } = tree;
+						for (let i = 0; i < count; i++) {
+							const row = new Row({ id: `row-${uuid()}`, cells: {} });
+							table.insertRow({ index: Math.floor(table.rows.length / 2), row });
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						const { undoStack } = tree;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of the SharedTree for redoing an insert row in the middle for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Redo insert the middle row ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { Row, table, undoStack } = tree;
+						for (let i = 0; i < count; i++) {
+							const row = new Row({ id: `row-${uuid()}`, cells: {} });
+							table.insertRow({ index: Math.floor(table.rows.length / 2), row });
+						}
+						for (let i = 0; i < count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						const { redoStack } = tree;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < count; i++) {
+							redoStack.pop()?.revert();
 						}
 					},
 					maxBenchmarkDurationSeconds,
@@ -182,12 +276,63 @@ describe("SharedTree execution time", () => {
 					tableSize: treeSize,
 					cellValue,
 					operation: (tree: TableTreeDefinition) => {
+						const { Column, Row, table } = tree;
 						for (let i = 0; i < count; i++) {
-							const { Column, Row, table } = tree;
-							const column = new Column({ id: uuid() });
-							const row = new Row({ id: uuid(), cells: {} });
+							const column = new Column({ id: `column-${uuid()}` });
+							const row = new Row({ id: `row-${uuid()}`, cells: {} });
 							table.insertColumn({ index: Math.floor(table.columns.length / 2), column });
 							table.insertRow({ index: Math.floor(table.rows.length / 2), row });
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of undoing insert a row and a column in the middle for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Undo insert the middle column and row ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { Column, Row, table } = tree;
+						for (let i = 0; i < count; i++) {
+							const column = new Column({ id: `column-${uuid()}` });
+							const row = new Row({ id: `row-${uuid()}`, cells: {} });
+							table.insertColumn({ index: Math.floor(table.columns.length / 2), column });
+							table.insertRow({ index: Math.floor(table.rows.length / 2), row });
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						const { undoStack } = tree;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < 2 * count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of the SharedTree for redoing an insert row and a column in the middle for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Redo insert the middle column and row ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { Column, Row, table, undoStack } = tree;
+						for (let i = 0; i < count; i++) {
+							const column = new Column({ id: `column-${uuid()}` });
+							const row = new Row({ id: `row-${uuid()}`, cells: {} });
+							table.insertColumn({ index: Math.floor(table.columns.length / 2), column });
+							table.insertRow({ index: Math.floor(table.rows.length / 2), row });
+						}
+						for (let i = 0; i < 2 * count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						const { redoStack } = tree;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < 2 * count; i++) {
+							redoStack.pop()?.revert();
 						}
 					},
 					maxBenchmarkDurationSeconds,
@@ -211,6 +356,51 @@ describe("SharedTree execution time", () => {
 					},
 					maxBenchmarkDurationSeconds,
 				});
+
+				// Test the execute time of undoing remove a column in the middle for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Undo remove the middle column ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { table } = tree;
+						for (let i = 0; i < count; i++) {
+							table.removeColumn(`column-${i}`);
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						const { undoStack } = tree;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of the SharedTree for redoing a remove column in the middle for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Redo remove the middle column ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { table, undoStack } = tree;
+						for (let i = 0; i < count; i++) {
+							table.removeColumn(`column-${i}`);
+						}
+						for (let i = 0; i < count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						const { redoStack } = tree;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < count; i++) {
+							redoStack.pop()?.revert();
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
 			});
 
 			describe(`Row Removal`, () => {
@@ -223,6 +413,51 @@ describe("SharedTree execution time", () => {
 						const { table } = tree;
 						for (let i = 0; i < count; i++) {
 							table.removeRow(`row-${i}`);
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of undoing remove a row in the middle for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Undo remove the middle row ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { table } = tree;
+						for (let i = 0; i < count; i++) {
+							table.removeRow(`row-${i}`);
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						const { undoStack } = tree;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of the SharedTree for redoing a remove row in the middle for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Redo remove the middle row ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { table, undoStack } = tree;
+						for (let i = 0; i < count; i++) {
+							table.removeRow(`row-${i}`);
+						}
+						for (let i = 0; i < count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						const { redoStack } = tree;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < count; i++) {
+							redoStack.pop()?.revert();
 						}
 					},
 					maxBenchmarkDurationSeconds,
@@ -253,14 +488,69 @@ describe("SharedTree execution time", () => {
 					tableSize: treeSize,
 					cellValue,
 					operation: (tree: TableTreeDefinition) => {
+						const { Column, Row, table } = tree;
 						for (let i = 0; i < count; i++) {
-							const { Column, Row, table } = tree;
-							const column = new Column({ id: uuid() });
-							const row = new Row({ id: uuid(), cells: {} });
+							const column = new Column({ id: `column-${uuid()}` });
+							const row = new Row({ id: `column-${uuid()}`, cells: {} });
 							table.insertColumn({ index: Math.floor(table.columns.length / 2), column });
 							table.insertRow({ index: Math.floor(table.rows.length / 2), row });
 							table.removeColumn(column);
 							table.removeRow(row);
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of undoing insert a row and a column and removing them right away for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Undo insert a column and a row and remove them right away ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { Column, Row, table } = tree;
+						for (let i = 0; i < count; i++) {
+							const column = new Column({ id: `column-${uuid()}` });
+							const row = new Row({ id: `row-${uuid()}`, cells: {} });
+							table.insertColumn({ index: Math.floor(table.columns.length / 2), column });
+							table.insertRow({ index: Math.floor(table.rows.length / 2), row });
+							table.removeColumn(column);
+							table.removeRow(row);
+						}
+					},
+					stackOperation: (stack) => {
+						const { undoStack } = stack;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < 4 * count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of the SharedTree for redoing an insert row and a column and removing them right away for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Redo insert a column and a row and remove them right away ${count} times`,
+					tableSize: treeSize,
+					cellValue,
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { Column, Row, table, undoStack } = tree;
+						for (let i = 0; i < count; i++) {
+							const column = new Column({ id: `column-${uuid()}` });
+							const row = new Row({ id: `row-${uuid()}`, cells: {} });
+							table.insertColumn({ index: Math.floor(table.columns.length / 2), column });
+							table.insertRow({ index: Math.floor(table.rows.length / 2), row });
+							table.removeColumn(column);
+							table.removeRow(row);
+						}
+						for (let i = 0; i < 4 * count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						const { redoStack } = tree;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < 4 * count; i++) {
+							redoStack.pop()?.revert();
 						}
 					},
 					maxBenchmarkDurationSeconds,
@@ -285,6 +575,67 @@ describe("SharedTree execution time", () => {
 								},
 								cell: { cellValue },
 							});
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of undoing set a cell value for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Undo set a cell value ${count} times`,
+					tableSize: treeSize,
+					cellValue: "abc",
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { table } = tree;
+						for (let i = 0; i < count; i++) {
+							const rowIndex = Math.floor(table.rows.length / 2);
+							const colIndex = Math.floor(table.columns.length / 2);
+							table.setCell({
+								key: {
+									row: `row-${rowIndex}`,
+									column: `column-${colIndex}`,
+								},
+								cell: { cellValue },
+							});
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						const { undoStack } = tree;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					maxBenchmarkDurationSeconds,
+				});
+
+				// Test the execute time of the SharedTree for redoing a set cell value for a given number of times.
+				runUndoRedoBenchmark({
+					title: `Redo set a cell value ${count} times`,
+					tableSize: treeSize,
+					cellValue: "abc",
+					setupOperation: (tree: TableTreeDefinition) => {
+						const { table, undoStack } = tree;
+						for (let i = 0; i < count; i++) {
+							const rowIndex = Math.floor(table.rows.length / 2);
+							const colIndex = Math.floor(table.columns.length / 2);
+							table.setCell({
+								key: {
+									row: `row-${rowIndex}`,
+									column: `column-${colIndex}`,
+								},
+								cell: { cellValue },
+							});
+						}
+						for (let i = 0; i < count; i++) {
+							undoStack.pop()?.revert();
+						}
+					},
+					stackOperation: (tree: TableTreeDefinition) => {
+						const { redoStack } = tree;
+						// assert.equal(stack.undoStackLength, count);
+						for (let i = 0; i < count; i++) {
+							redoStack.pop()?.revert();
 						}
 					},
 					maxBenchmarkDurationSeconds,
