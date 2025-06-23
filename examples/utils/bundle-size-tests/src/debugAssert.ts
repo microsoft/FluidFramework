ea2b641206a9d6610bda95e1ed907b0d12b92895
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-06-23 10:24:46 -0700

    add a debug bundling test case

--- examples/utils/bundle-size-tests/src/debugAssert.ts
+++ examples/utils/bundle-size-tests/src/debugAssert.ts
@@ -9,7 +9,8 @@ import { debugAssert, assert } from "@fluidframework/core-utils/internal";
 // For now it can be manually verified in ../build/debugAssert.js,
 // and any regression breaking it will show up as a bundle size regression.
 debugAssert(() => "This should be removed in production 1");
-assert(true, "This should be kept 1", () => "This should be removed in production 2");
+assert(true, "This should be kept 1");
+assert(true, "This should be kept 2", () => "This should be removed in production 2");
 // To ensure the bundle being inspected actually contains the correct content,
 // use a different string constant in a way that will not be removed so it can be checked for in the bundle.
-throw new Error("This should be kept 2");
+throw new Error("This should be kept 3");
