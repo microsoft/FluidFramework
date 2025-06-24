ec442c0d79369ec1e06f41b8559408404fffc267
Tyler Butler <tylerbu@microsoft.com>
2025-06-23 18:03:00 -0700

    copyright

--- build-tools/packages/build-cli/src/library/changelogs.ts
+++ build-tools/packages/build-cli/src/library/changelogs.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { readFile, writeFile } from "node:fs/promises";
 import type { IPackage } from "@fluid-tools/build-infrastructure";
 import {
