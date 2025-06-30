8de9813d003ef12f71a00ea7665265ae5e5d6600
Rishhi <rishhib@microsoft.com>
2025-06-30 12:45:59 -0400

    change from this to IRuntimeFactory

--- packages/runtime/runtime-utils/src/runtimeFactoryHelper.ts
+++ packages/runtime/runtime-utils/src/runtimeFactoryHelper.ts
@@ -15,7 +15,7 @@ import { IContainerRuntime } from "@fluidframework/container-runtime-definitions
  * @alpha
  */
 export abstract class RuntimeFactoryHelper<T = IContainerRuntime> implements IRuntimeFactory {
-	public get IRuntimeFactory(): this {
+	public get IRuntimeFactory(): IRuntimeFactory {
 		return this;
 	}
 
