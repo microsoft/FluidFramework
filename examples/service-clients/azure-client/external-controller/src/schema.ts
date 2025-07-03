4cb0ae8c389690294c8db8173a26313b00e5a932
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-07-03 23:45:08 +0000

    docs: Docs

--- examples/service-clients/azure-client/external-controller/src/schema.ts
+++ examples/service-clients/azure-client/external-controller/src/schema.ts
@@ -7,13 +7,20 @@ import { SchemaFactory } from "fluid-framework";
 
 const schemaFactory = new SchemaFactory("fluid-example-external-controller");
 
+/**
+ * Represents a die with a value between 1 and 6.
+ */
 export class Dice extends schemaFactory.object("Dice", {
 	/**
-	 * The value of the die, which should be an integer on [1, 6].
+	 * The value of the die
+	 * @remarks Should be on [1, 6].
 	 */
 	value: schemaFactory.number,
 }) {}
 
+/**
+ * Represents the application state containing two dice.
+ */
 export class App extends schemaFactory.object("AppSchema", {
 	/**
 	 * The first die in the app.
