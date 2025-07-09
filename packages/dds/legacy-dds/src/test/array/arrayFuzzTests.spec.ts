564760dbe0fa8779fc6c1b1a9b799ce8517dc1ee
Daniel Madrid <danielcar@microsoft.com>
2025-07-09 13:03:52 -0700

    nit

--- packages/dds/legacy-dds/src/test/array/arrayFuzzTests.spec.ts
+++ packages/dds/legacy-dds/src/test/array/arrayFuzzTests.spec.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import * as path from "node:path";
 
 import { createDDSFuzzSuite } from "@fluid-private/test-dds-utils";
