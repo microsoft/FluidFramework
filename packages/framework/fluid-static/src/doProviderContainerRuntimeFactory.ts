2a3649bd3bd2d42a47f62b8474e70e0b7dcabd65
Steffen Lösch <5918596+steffenloesch@users.noreply.github.com>
2025-07-09 15:30:55 +0000

    Partial progress, still some compile errors

--- packages/framework/fluid-static/src/doProviderContainerRuntimeFactory.ts
+++ packages/framework/fluid-static/src/doProviderContainerRuntimeFactory.ts
@@ -5,10 +5,14 @@ import type {
 } from "@fluidframework/container-runtime/internal";
 import { FluidDataStoreRegistry } from "@fluidframework/container-runtime/internal";
 import type { IFluidDataStoreRegistry } from "@fluidframework/runtime-definitions/internal";
+import type { SharedObjectKind } from "@fluidframework/shared-object-base";
+import type { ITree } from "@fluidframework/tree/internal";
+
 
 import { DOProviderContainerRuntimeFactory, RootDataObjectFactory } from "./rootDataObject.js";
-import type { CompatibilityMode, ContainerSchema } from "./types.js";
-import { parseDataObjectsFromSharedObjects } from "./utils.js";
+import { TreeDOProviderContainerRuntimeFactory } from "./treeRootDataObject.js";
+import type { CompatibilityMode, ContainerSchema, LoadableObjectKind } from "./types.js";
+import { isSharedObjectKind, parseDataObjectsFromSharedObjects } from "./utils.js";
 
 /**
  * Creates an {@link @fluidframework/aqueduct#BaseContainerRuntimeFactory} which constructs containers
@@ -54,9 +58,21 @@ export function createDOProviderContainerRuntimeFactory(props: {
 	const registry = props.rootDataStoreRegistry ?? new FluidDataStoreRegistry(registryEntries);
 
 	if (props.useTreeBasedDataObject) {
-		throw new Error(
-			"Tree-based data objects are not yet supported in DOProviderContainerRuntimeFactory.",
-		);
+		const containerKinds = Object.values(props.schema.initialObjects);
+		if (containerKinds.length !== 1 || !containerKinds[0] || !(containerKinds[0] as SharedObjectKind<ITree>)) {
+			throw new Error(
+				"Tree based data object currently only support container schema with a single tree.",
+			);
+		}
+		const treeKind = containerKinds[0] as unknown as LoadableObjectKind;
+		if (isSharedObjectKind(treeKind)){
+			return new TreeDOProviderContainerRuntimeFactory(
+				props.schema,
+				props.compatibilityMode,
+				treeKind,
+				registryEntries);
+		}
+
 	}
 
 	return new DOProviderContainerRuntimeFactory(
--- packages/framework/fluid-static/src/treeRootDataObject.ts
+++ packages/framework/fluid-static/src/treeRootDataObject.ts
@@ -3,18 +3,22 @@
  * Licensed under the MIT License.
  */
 
-import { TreeDataObject } from "@fluidframework/aqueduct/internal";
-import type { IFluidHandle } from "@fluidframework/core-interfaces";
-import type { SharedObjectKind } from "@fluidframework/shared-object-base";
+import { BaseContainerRuntimeFactory, PureDataObjectFactory, TreeDataObject } from "@fluidframework/aqueduct/internal";
+import type { IContainerRuntime, IContainerRuntimeInternal } from "@fluidframework/container-runtime-definitions/internal";
+import type { FluidObject, FluidObjectKeys, IFluidHandle, IFluidLoadable } from "@fluidframework/core-interfaces";
+import { assert } from "@fluidframework/core-utils/internal";
+import type { NamedFluidDataStoreRegistryEntries } from "@fluidframework/runtime-definitions/internal";
+import type { ISharedObjectKind, SharedObjectKind } from "@fluidframework/shared-object-base/internal";
 import type { ITree } from "@fluidframework/tree/internal";
 
-import type { IRootDataObject, LoadableObjectRecord } from "./types.js";
+import type { CompatibilityMode, ContainerSchema, IRootDataObject, IStaticEntryPoint, LoadableObjectRecord } from "./types.js";
 
 /**
  * The entry-point/root collaborative object of the {@link IFluidContainer | Fluid Container}.
  * Abstracts the dynamic code required to build a Fluid Container into a static representation for end customers.
  */
 export class TreeRootDataObject extends TreeDataObject<ITree> implements IRootDataObject {
+
 	protected generateView(tree: ITree): ITree {
 		// Return the tree directly as the view
 		// This provides direct access to the tree for the consumer
@@ -43,7 +47,61 @@ export class TreeRootDataObject extends TreeDataObject<ITree> implements IRootDa
 	}
 
 	public async uploadBlob(blob: ArrayBufferLike): Promise<IFluidHandle<ArrayBufferLike>> {
-		// TODO: Implement blob upload functionality
-		throw new Error("Method not implemented.");
+		return this.runtime.uploadBlob(blob);
 	}
+}
+
+const treeRootDataStoreId = "treeRootDOId";
+
+function makeFluidObject<T extends object, K extends FluidObjectKeys<T> = FluidObjectKeys<T>>(
+	object: Omit<T, K>,
+	providerKey: K,
+): T {
+	return Object.defineProperty(object, providerKey, { value: object }) as T;
+}
+
+async function provideEntryPoint(
+	containerRuntime: IContainerRuntime,
+): Promise<IStaticEntryPoint> {
+	const entryPoint = await containerRuntime.getAliasedDataStoreEntryPoint(treeRootDataStoreId);
+	if (entryPoint === undefined) {
+		throw new Error(`default dataStore [${treeRootDataStoreId}] must exist`);
+	}
+	const treeRootDataObject = ((await entryPoint.get()) as FluidObject<TreeRootDataObject>)
+		.TreeRootDataObject;
+	assert(treeRootDataObject !== undefined, 0xb9f /* entryPoint must be of type RootDataObject */);
+	return makeFluidObject<IStaticEntryPoint>(
+		{
+			rootDataObject: treeRootDataObject,
+			extensionStore: containerRuntime as IContainerRuntimeInternal,
+		},
+		"IStaticEntryPoint",
+	);
+}
+
+/**
+ * Factory for Container Runtime instances that provide a {@link IStaticEntryPoint}
+ * (containing single {@link IRootDataObject}) as their entry point.
+ */
+export class TreeDOProviderContainerRuntimeFactory extends BaseContainerRuntimeFactory {
+	readonly #treeRootDataObjectFactory: PureDataObjectFactory<TreeRootDataObject>;
+
+	public constructor(
+		schema: ContainerSchema,
+		compatibilityMode: CompatibilityMode,
+		treeKind: ISharedObjectKind<IFluidLoadable>,
+		registryEntries: NamedFluidDataStoreRegistryEntries
+	) {
+		super({
+			registryEntries,
+			provideEntryPoint
+		});
+		this.#treeRootDataObjectFactory = new PureDataObjectFactory<TreeRootDataObject>(
+			`TreeRootDataObject`,
+			TreeRootDataObject,
+			[treeKind.getFactory()],
+			{},
+		);
+	}
+
 }
