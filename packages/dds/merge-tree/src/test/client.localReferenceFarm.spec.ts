059998287102b658c27222463492ab472cbaae44
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-07-18 21:33:37 +0000

    revert: Unrelated changes

--- packages/dds/merge-tree/src/test/client.localReferenceFarm.spec.ts
+++ packages/dds/merge-tree/src/test/client.localReferenceFarm.spec.ts
@@ -3,6 +3,8 @@
  * Licensed under the MIT License.
  */
 
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 import { strict as assert } from "node:assert";
 
 import { makeRandom } from "@fluid-private/stochastic-test-utils";
