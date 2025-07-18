86ae3fcd7d4b5d1602be66c30689770eca141699
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-07-18 21:30:34 +0000

    fix: Remaining linter violations

--- packages/tools/fluid-runner/src/utils.ts
+++ packages/tools/fluid-runner/src/utils.ts
@@ -5,7 +5,7 @@
 
 import * as fs from "fs";
 
-import { IFluidFileConverter } from "./codeLoaderBundle.js";
+import type { IFluidFileConverter } from "./codeLoaderBundle.js";
 
 /**
  * Is the given snapshot in JSON format
