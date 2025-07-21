9eb71ea0093bf770f493be532b0a0ca1d375ec46
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-07-21 10:40:30 -0700

    refactor(odsp-driver): Enable type-only import/export eslint rules and fix violations (#25046)
    In preparation for these rules becoming default in the next release of
    our eslint configuration.
    
    Type-only imports offer a number of benefits and are considered a best
    practice. More details can be found here:
    https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html

--- packages/drivers/odsp-driver/src/test/socketTests/socketTests.spec.ts
+++ packages/drivers/odsp-driver/src/test/socketTests/socketTests.spec.ts
@@ -5,25 +5,25 @@
 
 import { strict as assert } from "node:assert";
 
-import { IClient } from "@fluidframework/driver-definitions";
-import { IAnyDriverError } from "@fluidframework/driver-definitions/internal";
+import type { IClient } from "@fluidframework/driver-definitions";
+import type { IAnyDriverError } from "@fluidframework/driver-definitions/internal";
 import { createOdspNetworkError } from "@fluidframework/odsp-doclib-utils/internal";
 import {
-	IOdspResolvedUrl,
+	type IOdspResolvedUrl,
 	OdspErrorTypes,
 } from "@fluidframework/odsp-driver-definitions/internal";
 import {
-	ITelemetryLoggerExt,
+	type ITelemetryLoggerExt,
 	MockLogger,
 	isFluidError,
 } from "@fluidframework/telemetry-utils/internal";
 import { stub } from "sinon";
-import { Socket } from "socket.io-client";
+import type { Socket } from "socket.io-client";
 import { v4 as uuid } from "uuid";
 
 import { EpochTracker } from "../../epochTracker.js";
 import { mockify } from "../../mockify.js";
-import { LocalPersistentCache } from "../../odspCache.js";
+import type { LocalPersistentCache } from "../../odspCache.js";
 import { OdspDocumentDeltaConnection } from "../../odspDocumentDeltaConnection.js";
 import { getHashedDocumentId } from "../../odspPublicUtils.js";
 import { SocketIOClientStatic } from "../../socketModule.js";
