88e529a8d9064f921e38d5755568d2b759a7c2b6
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-07-23 16:20:26 +0000

    fix: Line spacing

--- tools/api-markdown-documenter/src/documentation-domain-to-markdown/test/CustomNodeTypeToMarkdown.test.ts
+++ tools/api-markdown-documenter/src/documentation-domain-to-markdown/test/CustomNodeTypeToMarkdown.test.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import { expect } from "chai";
 import type { Text as MdastText } from "mdast";
 
