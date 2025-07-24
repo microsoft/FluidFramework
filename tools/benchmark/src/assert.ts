1700e8fb76154afafb98df39d1cd44acc06f8f3a
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-07-24 14:00:14 -0700

    Update tools/benchmark/src/assert.ts
    Co-authored-by: Alex Villarreal <716334+alexvy86@users.noreply.github.com>

--- tools/benchmark/src/assert.ts
+++ tools/benchmark/src/assert.ts
@@ -9,9 +9,6 @@
  * @param condition - The condition that should be true, if the condition is false an error will be thrown.
  * Only use this API when `false` indicates a logic error in the problem and thus a bug that should be fixed.
  * @param message - The message to include in the error when the condition does not hold.
- * A number should not be specified manually: use a string.
- * Before a release, policy-check should be run, which will convert any asserts still using strings to
- * use numbered error codes instead.
  * @remarks
  * Use this instead of the node 'assert' package, which requires polyfills to run in browser environments.
  */
