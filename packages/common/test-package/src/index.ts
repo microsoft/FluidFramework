b236615be1a5f3369530ffb76cf197830393b37f
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-07-31 17:36:10 +0000

    test: Add test package

---
+++ packages/common/test-package/src/index.ts
@@ -0,0 +1,41 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/* eslint-disable @typescript-eslint/no-empty-interface */
+
+/**
+ * @public
+ */
+export interface A {}
+
+/**
+ * @beta
+ */
+export interface B {}
+
+/**
+ * @alpha
+ */
+export interface C {}
+
+/**
+ * @legacy @public
+ */
+export interface D {}
+
+/**
+ * @legacy @beta
+ */
+export interface E {}
+
+/**
+ * @legacy @alpha
+ */
+export interface F {}
+
+/**
+ * @internal
+ */
+export interface G {}
