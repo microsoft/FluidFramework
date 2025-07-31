2a4bea744deb4f0bde5d17e95ab0689f01a99ecc
Matt Rakow <ChumpChief@users.noreply.github.com>
2025-07-31 15:13:29 -0700

    Remove attachAndGetPendingBlobs (#25125)

--- packages/runtime/container-runtime/src/containerRuntime.ts
+++ packages/runtime/container-runtime/src/containerRuntime.ts
@@ -1898,7 +1898,7 @@ export class ContainerRuntime
 			routeContext: this.handleContext,
 			blobManagerLoadInfo,
 			storage: this.storage,
-			sendBlobAttachOp: (localId: string, blobId?: string) => {
+			sendBlobAttachOp: (localId: string, blobId: string) => {
 				if (!this.disposed) {
 					this.submit(
 						{ type: ContainerMessageType.BlobAttach, contents: undefined },
