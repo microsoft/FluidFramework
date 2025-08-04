72f3f3c2dadf5374420b2a3fed47911a7da4dcaa
Scarlett Lee <90647596+scarlettjlee@users.noreply.github.com>
2025-08-04 15:16:52 -0700

    Revert "Test re-introducing serialization on all add flows"
    This reverts commit 026e0dea65138b761400ee4429d234a6c2ae489c.

--- packages/dds/ordered-collection/src/consensusOrderedCollection.ts
+++ packages/dds/ordered-collection/src/consensusOrderedCollection.ts
@@ -138,13 +138,11 @@ export class ConsensusOrderedCollection<T = any>
 	 * Add a value to the consensus collection.
 	 */
 	public async add(value: T): Promise<void> {
-		const valueSer = this.serializeValue(value, this.serializer);
-
 		if (!this.isAttached()) {
 			// For the case where this is not attached yet, explicitly JSON
 			// clone the value to match the behavior of going thru the wire.
 			const addValue = this.deserializeValue(
-				valueSer,
+				this.serializeValue(value, this.serializer),
 				this.serializer,
 			) as T;
 			this.addCore(addValue);
