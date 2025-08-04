83200d90942f63449ba4cfa15a26fe5d7d4edf10
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-08-04 18:18:50 +0000

    style: Line break

--- tools/api-markdown-documenter/src/documentation-domain/DocumentationNode.ts
+++ tools/api-markdown-documenter/src/documentation-domain/DocumentationNode.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import type { BlockContent, PhrasingContent } from "mdast";
 
 import type { HeadingNode } from "./HeadingNode.js";
