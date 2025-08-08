07148f35fea2f66399a33047a327cc694b1bcbc1
Joel Zhu <66701969+tianzhu007@users.noreply.github.com>
2025-08-08 12:55:51 -0700

    Add the Network Check to Alfred and Nexus  (#24860)
    This PR introduces a network check feature to Alfred and Nexus. This
    enhancement enables the service to validate whether traffic is coming
    from a private endpoint or public network access by using the tenant
    information and the client IP address.
    
    Non-breaking-Changes
    
    ---------
    
    Co-authored-by: Joel Zhu <tianzhu@microsoft.com>

---
+++ server/routerlicious/packages/lambdas/src/nexus/networkHelper.ts
@@ -0,0 +1,62 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { getNetworkInformationFromIP } from "@fluidframework/server-services-client";
+import type * as core from "@fluidframework/server-services-core";
+
+/**
+ * Check the network information to determine if the socket should connect.
+ *
+ * @param tenantManager - The tenant manager to use to get the tenant information.
+ * @param socket - The socket to check the network information for.
+ * @returns A promise that resolves to an object containing a message and a boolean indicating if the socket should connect.
+ */
+export async function checkNetworkInformation(
+	tenantManager: core.ITenantManager,
+	socket: core.IWebSocket,
+): Promise<{ message: string; shouldConnect: boolean }> {
+	const tenantId = socket?.handshake?.query?.tenantId as string | undefined;
+	const tenantInfo = await tenantManager.getTenantfromRiddler(tenantId);
+	const privateLinkEnable = tenantInfo?.customData?.privateEndpoints?.accountLinkId
+		? true
+		: false;
+	const xForwardedFor: string | undefined = socket.handshake.headers["x-forwarded-for"] as
+		| string
+		| undefined;
+	const clientIPAddress = xForwardedFor?.split(",")[0];
+	if (privateLinkEnable && !clientIPAddress) {
+		return {
+			message: "Client IP address is required for private link in x-forwarded-for",
+			shouldConnect: false,
+		};
+	}
+	const networkInfo = getNetworkInformationFromIP(clientIPAddress);
+	if (networkInfo.isPrivateLink) {
+		if (privateLinkEnable) {
+			const accountLinkId = tenantInfo?.customData?.privateEndpoints?.accountLinkId;
+			return networkInfo.privateLinkId === accountLinkId
+				? { message: "This is a private link socket connection", shouldConnect: true }
+				: {
+						message:
+							"This private link should not be connected since the link id does not match",
+						shouldConnect: false,
+				  };
+		} else {
+			return {
+				message:
+					"This private link should not be connected since the tenant is not private link enabled",
+				shouldConnect: false,
+			};
+		}
+	} else {
+		return privateLinkEnable
+			? {
+					message:
+						"This is a failed private link tenant socket connection from public network",
+					shouldConnect: false,
+			  }
+			: { message: "This public network should be connected", shouldConnect: true };
+	}
+}
