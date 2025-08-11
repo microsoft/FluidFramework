1a04b70ffc07476cdfe873b8c2365dcaaee9d75d
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-08-11 10:24:28 -0700

    refactor(api-markdown-documenter): Remove Markdown and HTML AST transformation layers (#25174)
    The API transformation layer now outputs standard `mdast` Markdown trees
    directly ("normalized" to not include our special `Section` and
    `SectionHeading` types).
    
    These `mdast` trees can be used as input to the existing Markdown
    rendering layer.
    
    This PR also removes the library's built-in HTML rendering support.
    
    Users that want to operate on `hast` trees can convert their contents to
    a `hast` representation, using
    [mdast-util-to-hast](https://github.com/syntax-tree/mdast-util-to-hast)
    or another comparable library.
    
    Users that want to render HTML content can further leverage
    [hast-util-to-html](https://github.com/syntax-tree/hast-util-to-html) or
    another comparable library.

--- tools/api-markdown-documenter/src/api-item-transforms/helpers/Helpers.ts
+++ tools/api-markdown-documenter/src/api-item-transforms/helpers/Helpers.ts
@@ -40,7 +40,7 @@ import type {
 } from "mdast";
 
 import type { Logger } from "../../Logging.js";
-import type { Section, SectionHeading } from "../../mdast/index.js";
+import type { Section, SectionContent, SectionHeading } from "../../mdast/index.js";
 import {
 	type ApiFunctionLike,
 	injectSeparator,
@@ -88,7 +88,7 @@ export function createSignatureSection(
 	if (apiItem instanceof ApiDeclaredItem) {
 		const signatureExcerpt = apiItem.getExcerptWithModifiers();
 		if (signatureExcerpt !== "") {
-			const contents: BlockContent[] = [];
+			const contents: SectionContent[] = [];
 
 			contents.push({
 				type: "code",
@@ -130,10 +130,10 @@ export function createSignatureSection(
 function createHeritageTypesContent(
 	apiItem: ApiItem,
 	config: ApiItemTransformationConfiguration,
-): BlockContent[] | undefined {
+): SectionContent[] | undefined {
 	const { logger } = config;
 
-	const contents: BlockContent[] = [];
+	const contents: SectionContent[] = [];
 
 	if (apiItem instanceof ApiClass) {
 		// Render `extends` type if there is one.
@@ -1102,7 +1102,7 @@ export interface ChildSectionProperties {
 export function createChildDetailsSection(
 	childItems: readonly ChildSectionProperties[],
 	config: ApiItemTransformationConfiguration,
-	createChildContent: (apiItem) => BlockContent[],
+	createChildContent: (apiItem) => SectionContent[],
 ): Section[] | undefined {
 	const sections: Section[] = [];
 
@@ -1114,7 +1114,7 @@ export function createChildDetailsSection(
 			!doesItemKindRequireOwnDocument(childItem.itemKind, config.hierarchy) &&
 			childItem.items.length > 0
 		) {
-			const childContents: BlockContent[] = [];
+			const childContents: SectionContent[] = [];
 			for (const item of childItem.items) {
 				childContents.push(...createChildContent(item));
 			}
