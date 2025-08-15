d950f4f7d87d1c965fb04e66e2c429afa147b0e9
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-08-15 23:50:35 +0000

    docs: TODOs

--- examples/data-objects/table-tree/src/react/tableView.tsx
+++ examples/data-objects/table-tree/src/react/tableView.tsx
@@ -54,6 +54,7 @@ export const TableView: React.FC<{ tableModel: TableDataObject }> = ({ tableMode
 
 	const handleRemoveRow = (index: number): void => {
 		if (index >= 0 && index < rows.length) {
+			// TODO: use index-based removal API once that has been added.
 			table.removeRows([table.rows[index]]);
 		}
 	};
@@ -66,6 +67,7 @@ export const TableView: React.FC<{ tableModel: TableDataObject }> = ({ tableMode
 
 	const handleRemoveColumn = (index: number): void => {
 		if (index >= 0 && index < columns.length) {
+			// TODO: use index-based removal API once that has been added.
 			table.removeColumns([table.columns[index]]);
 		}
 	};
