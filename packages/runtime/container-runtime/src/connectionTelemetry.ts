4a0560b44f3ef6a39b13609bb6925643aa1960eb
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-08-20 19:02:45 +0000

    style: Formatting

--- packages/runtime/container-runtime/src/connectionTelemetry.ts
+++ packages/runtime/container-runtime/src/connectionTelemetry.ts
@@ -6,10 +6,7 @@
 import { performanceNow } from "@fluid-internal/client-utils";
 import type { IDeltaManagerFull } from "@fluidframework/container-definitions/internal";
 import type { IContainerRuntimeEvents } from "@fluidframework/container-runtime-definitions/internal";
-import type {
-	ITelemetryBaseLogger,
-	IEventProvider,
-} from "@fluidframework/core-interfaces";
+import type { ITelemetryBaseLogger, IEventProvider } from "@fluidframework/core-interfaces";
 import { assert } from "@fluidframework/core-utils/internal";
 import {
 	type IDocumentMessage,
