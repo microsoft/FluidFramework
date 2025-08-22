6a3c986fd50900c8a015dda8747e143dce8482bd
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-08-22 00:00:38 +0000

    style: Formatting

--- packages/dds/matrix/src/test/time/sharedMatrix.spec.ts
+++ packages/dds/matrix/src/test/time/sharedMatrix.spec.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import { runBenchmarkTestSuite } from "../matrixBenchmarks.js";
 
 runBenchmarkTestSuite("execution-time");
