d1604429f179bff83b96d19a995707ecc3fa875c
Jason Hartman <jasonha@microsoft.com>
2025-08-29 11:25:33 -0700

    test(client): fix js-ts-copyright-file-header spacing requirement

--- packages/service-clients/end-to-end-tests/azure-client/.eslintrc.cjs
+++ packages/service-clients/end-to-end-tests/azure-client/.eslintrc.cjs
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 const importInternalModulesAllowedForTest = [
 	// Allow import of Fluid Framework external API exports.
 	"@fluidframework/*/{beta,alpha,legacy}",
