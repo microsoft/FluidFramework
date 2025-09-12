4d58a23fef7f310e3c600a0efadcd7a0fcfe6e31
Scott Norton <scottnorton@microsoft.com>
2025-09-12 17:42:40 +0000

    remove explicitSchemaControl: false in stress test options matrix

--- packages/test/test-service-load/src/optionsMatrix.ts
+++ packages/test/test-service-load/src/optionsMatrix.ts
@@ -117,7 +117,7 @@ export function generateRuntimeOptions(
 		enableRuntimeIdCompressor: ["on", undefined, "delayed"],
 		enableGroupedBatching: [true, false],
 		createBlobPayloadPending: [true, undefined],
-		explicitSchemaControl: [true, false],
+		explicitSchemaControl: [true],
 	};
 
 	const pairwiseOptions = generatePairwiseOptions<IContainerRuntimeOptionsInternal>(
