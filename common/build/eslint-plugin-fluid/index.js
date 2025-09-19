129b2f78116ed61b65a630483476831dc9aca347
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-09-19 17:59:40 +0000

    feat: Actually export the rule

--- common/build/eslint-plugin-fluid/index.js
+++ common/build/eslint-plugin-fluid/index.js
@@ -3,13 +3,22 @@
  * Licensed under the MIT License.
  */
 
+/**
+ * ESLint plugin for Fluid projects.
+ *
+ * @remarks
+ * Refer to each rule by the unprefixed plugin name in the consumed package.
+ * {@link https://eslint.org/docs/latest/extend/plugins#rules-in-plugins}
+ */
 module.exports = {
 	rules: {
 		/**
+		 * Disallow Markdown link syntax in JSDoc/TSDoc comments.
+		 * Full name: "@fluid-internal/fluid/no-markdown-links-in-jsdoc"
+		 */
+		"no-markdown-links-in-jsdoc": require("./src/rules/no-markdown-links-in-jsdoc"),
+		/**
 		 * Full name: "@fluid-internal/fluid/no-member-release-tags"
-		 *
-		 * Refer to the rule by the unprefixed plugin name in the consumed package.
-		 * {@link https://eslint.org/docs/latest/extend/plugins#rules-in-plugins}
 		 */
 		"no-member-release-tags": require("./src/rules/no-member-release-tags"),
 		"no-restricted-tags-imports": require("./src/rules/no-restricted-tags-imports"),
