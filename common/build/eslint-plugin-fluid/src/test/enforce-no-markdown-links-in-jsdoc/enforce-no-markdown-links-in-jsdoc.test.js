0a765d4cc0f46e14e8c5288049859d9391959c89
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-09-19 19:42:27 +0000

    test: Make unit test less brittle

--- common/build/eslint-plugin-fluid/src/test/enforce-no-markdown-links-in-jsdoc/enforce-no-markdown-links-in-jsdoc.test.js
+++ common/build/eslint-plugin-fluid/src/test/enforce-no-markdown-links-in-jsdoc/enforce-no-markdown-links-in-jsdoc.test.js
@@ -24,12 +24,13 @@ describe("Do not allow Markdown links in JSDoc/TSDoc comments", function () {
 		});
 		const fileToLint = path.join(__dirname, "../example/no-markdown-links-in-jsdoc", file);
 		const results = await eslint.lintFiles([fileToLint]);
+		assert.equal(results.length, 1, "Expected a single result for linting a single file.");
 		return results[0];
 	}
 
 	it("Should report errors for Markdown links in block comments", async function () {
 		const result = await lintFile("test.ts");
-		assert.strictEqual(result.errorCount, 1, "Should have 1 error");
+		assert.strictEqual(result.errorCount, 1);
 		assert.strictEqual(
 			result.messages[0].message,
 			"Markdown link syntax (`[text](url)`) is not allowed in JSDoc/TSDoc comments. Use `{@link url|text}` syntax instead.",
@@ -38,9 +39,6 @@ describe("Do not allow Markdown links in JSDoc/TSDoc comments", function () {
 
 		// Test auto-fix
 		assert.notEqual(result.messages[0].fix, undefined);
-		assert.deepEqual(result.messages[0].fix, {
-			range: [257, 281],
-			text: "{@link https://bing.com | bing}",
-		});
+		assert.deepEqual(result.messages[0].fix.text, "{@link https://bing.com | bing}");
 	});
 });
