e0f90c6079d439b335ab2d89b8d9b3236c834c10
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-09-23 15:37:48 -0700

    Fix build

--- packages/dds/tree/src/test/shared-tree/sharedTree.spec.ts
+++ packages/dds/tree/src/test/shared-tree/sharedTree.spec.ts
@@ -2479,7 +2479,7 @@ describe("SharedTree", () => {
 		const provider = new TestTreeProviderLite(
 			2,
 			configuredSharedTree({
-				jsonValidator: typeboxValidator,
+				jsonValidator: FormatValidatorBasic,
 				formatVersion: SharedTreeFormatVersion.vSharedBranches,
 			}).getFactory(),
 		);
