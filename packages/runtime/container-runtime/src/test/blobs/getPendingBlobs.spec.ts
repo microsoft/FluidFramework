4c7193bb58242c5fd3785fc337c5a4a974688745
Matt Rakow <marakow@microsoft.com>
2025-10-09 12:09:37 -0700

    Add getPendingBlobs while attaching test

--- packages/runtime/container-runtime/src/test/blobs/getPendingBlobs.spec.ts
+++ packages/runtime/container-runtime/src/test/blobs/getPendingBlobs.spec.ts
@@ -11,6 +11,7 @@ import {
 	attachHandle,
 	blobToText,
 	createTestMaterial,
+	ensureBlobsShared,
 	getSummaryContentsWithFormatValidation,
 	textToBlob,
 	unpackHandle,
@@ -78,40 +79,85 @@ for (const createBlobPayloadPending of [false, true]) {
 			assert.strictEqual(redirectTable2?.length, 1);
 		});
 
-		// it("get blobs and wait for blob attach while waiting for op", async () => {
-		// 	await runtime.attach();
-		// 	await runtime.connect();
-		// 	const blob = textToBlob("blob");
-		// 	const handleP = runtime.createBlob(blob);
-		// 	await runtime.processBlobs(true);
-		// 	const pendingStateP = runtime.getPendingLocalState();
-		// 	await runtime.processHandles();
-		// 	await assert.doesNotReject(handleP);
-		// 	const pendingState = await pendingStateP;
-		// 	const pendingBlobs = (pendingState[1] ?? {}) as IPendingBlobs;
-		// 	assert.strictEqual(Object.keys(pendingBlobs).length, 1);
-		// 	assert.strictEqual(Object.values(pendingBlobs)[0].acked, false);
-		// 	assert.ok(Object.values(pendingBlobs)[0].uploadTime);
+		it("getPendingBlobs while attaching", async () => {
+			const {
+				mockBlobStorage,
+				mockOrderingService,
+				blobManager: blobManager1,
+			} = createTestMaterial({ createBlobPayloadPending });
+			mockOrderingService.pause();
+			const handleP = blobManager1.createBlob(textToBlob("hello"));
+			if (!createBlobPayloadPending) {
+				assert.strictEqual(blobManager1.getPendingBlobs(), undefined);
+				return;
+			}
 
-		// 	const summaryData = getSummaryContentsWithFormatValidation(runtime.blobManager);
-		// 	assert.strictEqual(summaryData.ids?.length, 0);
-		// 	assert.strictEqual(summaryData.redirectTable, undefined);
+			const handle = await handleP;
+			const { localId } = unpackHandle(handle);
+			attachHandle(handle);
+			const pendingBlobs = blobManager1.getPendingBlobs();
+			assert.deepStrictEqual(pendingBlobs, {
+				[localId]: { state: "localOnly", blob: bufferToString(textToBlob("hello"), "base64") },
+			});
 
-		// 	const runtime2 = new MockRuntime(
-		// 		mc,
-		// 		false, // createBlobPayloadPending
-		// 		summaryData,
-		// 		false,
-		// 		pendingState,
-		// 	);
-		// 	await runtime2.attach();
-		// 	await runtime2.connect();
-		// 	await runtime2.processAll();
+			const { ids: ids1, redirectTable: redirectTable1 } =
+				getSummaryContentsWithFormatValidation(blobManager1);
+			assert.strictEqual(ids1, undefined);
+			assert.strictEqual(redirectTable1, undefined);
 
-		// 	const summaryData2 = getSummaryContentsWithFormatValidation(runtime2.blobManager);
-		// 	assert.strictEqual(summaryData2.ids?.length, 1);
-		// 	assert.strictEqual(summaryData2.redirectTable?.length, 1);
-		// });
+			const { blobManager: blobManager2 } = createTestMaterial({
+				mockBlobStorage,
+				mockOrderingService,
+				pendingBlobs,
+				createBlobPayloadPending,
+			});
+			const blobBeforeSharing = await blobManager2.getBlob(localId, createBlobPayloadPending);
+			assert.strictEqual(blobToText(blobBeforeSharing), "hello");
+
+			const sharePendingBlobsP = blobManager2.sharePendingBlobs();
+
+			// Drop the original attach op, so we can be sure that the created blob came from the load with pending blobs.
+			// Note that the original blob manager will resubmit it, but it will go to the back of the queue after the
+			// one from the blob manager created from pending blobs.
+			await mockOrderingService.waitDropOne();
+			// Sequence the attach op from the load with pending blobs.
+			await mockOrderingService.waitSequenceOne();
+
+			await sharePendingBlobsP;
+			const blobAfterSharing = await blobManager2.getBlob(localId, createBlobPayloadPending);
+			assert.strictEqual(blobToText(blobAfterSharing), "hello");
+
+			const { ids: ids2, redirectTable: redirectTable2 } =
+				getSummaryContentsWithFormatValidation(blobManager2);
+			assert.strictEqual(ids2?.length, 1);
+			assert.strictEqual(redirectTable2?.length, 1);
+
+			// The attach from the blob manager created from pending blobs will have completed the attach on the original
+			// blob manager too, even though it has an outstanding attach op.
+			await ensureBlobsShared([handle]);
+
+			// In normal cases we don't expect both the original blob manager and the one created from pending blobs to
+			// coexist, but we'll make sure that nothing bad happens if they both get their attach op sequenced.
+			await mockOrderingService.waitSequenceOne();
+			const blobAfterOriginalAttach1 = await blobManager1.getBlob(
+				localId,
+				createBlobPayloadPending,
+			);
+			assert.strictEqual(blobToText(blobAfterOriginalAttach1), "hello");
+			const blobAfterOriginalAttach2 = await blobManager2.getBlob(
+				localId,
+				createBlobPayloadPending,
+			);
+			assert.strictEqual(blobToText(blobAfterOriginalAttach2), "hello");
+			const { ids: ids3, redirectTable: redirectTable3 } =
+				getSummaryContentsWithFormatValidation(blobManager1);
+			assert.strictEqual(ids3?.length, 1);
+			assert.strictEqual(redirectTable3?.length, 1);
+			const { ids: ids4, redirectTable: redirectTable4 } =
+				getSummaryContentsWithFormatValidation(blobManager2);
+			assert.strictEqual(ids4?.length, 1);
+			assert.strictEqual(redirectTable4?.length, 1);
+		});
 
 		// it("shutdown multiple blobs", async () => {
 		// 	await runtime.attach();
