7b13c178118959eb2606ffe6ad021715e6e22129
Matt Rakow <marakow@microsoft.com>
2025-10-10 13:09:02 -0700

    Permit localOnly blobs to be handle attached in getPendingBlobs, add tests for post-attach, multi-state, and round-trip

--- packages/runtime/container-runtime/src/test/blobs/getPendingBlobs.spec.ts
+++ packages/runtime/container-runtime/src/test/blobs/getPendingBlobs.spec.ts
@@ -12,6 +12,7 @@ import type { SerializableLocalBlobRecord } from "../../blobManager/index.js";
 import {
 	attachHandle,
 	createTestMaterial,
+	ensureBlobsShared,
 	getSummaryContentsWithFormatValidation,
 	MIN_TTL,
 	textToBlob,
@@ -104,10 +105,118 @@ for (const createBlobPayloadPending of [false, true]) {
 			assert.strictEqual(redirectTable, undefined);
 		});
 
-		// TODO: Ensure we have both an uploaded with/without expired TTL
-		// TODO: Ensure one of the tests has multiple blobs
-		// TODO: Add test for repeat calls on single blob manager
-		// TODO: Add test for round-tripping through multiple blob managers
-		// TODO: Add comments
+		it("getPendingBlobs after attach completes", async () => {
+			const { blobManager } = createTestMaterial({ createBlobPayloadPending });
+			const handle = await blobManager.createBlob(textToBlob("hello"));
+			await ensureBlobsShared([handle]);
+			const pendingState = blobManager.getPendingBlobs();
+			assert.strictEqual(pendingState, undefined);
+
+			const { ids, redirectTable } = getSummaryContentsWithFormatValidation(blobManager);
+			assert.strictEqual(ids?.length, 1);
+			assert.strictEqual(redirectTable?.length, 1);
+		});
+
+		it("Multiple blobs in multiple states", async () => {
+			const { mockBlobStorage, mockOrderingService, blobManager } = createTestMaterial({
+				createBlobPayloadPending,
+			});
+
+			// First blob fully uploaded/attached
+			const handle1 = await blobManager.createBlob(textToBlob("hello"));
+			await ensureBlobsShared([handle1]);
+
+			// Pause storage and sequencing, so we can advance each blob to a different state
+			mockBlobStorage.pause();
+			mockOrderingService.pause();
+
+			// Second blob attaching
+			const handleP2 = blobManager.createBlob(textToBlob("world"));
+			if (createBlobPayloadPending) {
+				const handle2 = await handleP2;
+				attachHandle(handle2);
+			}
+			await mockBlobStorage.waitCreateOne();
+			await mockOrderingService.waitOpAvailable();
+
+			// Third blob uploading
+			const handleP3 = blobManager.createBlob(textToBlob("fizz"));
+			if (createBlobPayloadPending) {
+				const handle3 = await handleP3;
+				attachHandle(handle3);
+			}
+			await mockBlobStorage.waitBlobAvailable();
+
+			if (createBlobPayloadPending) {
+				// Fourth blob just created but handle not attached, so upload not started
+				await blobManager.createBlob(textToBlob("buzz"));
+				const pendingBlobs = blobManager.getPendingBlobs();
+				const { localId: localId2 } = unpackHandle(await handleP2);
+				const { localId: localId3 } = unpackHandle(await handleP3);
+				assert(
+					pendingBlobs !== undefined && Object.entries(pendingBlobs).length === 2,
+					"Expect two pending blobs",
+				);
+				const pendingBlob2: SerializableLocalBlobRecord | undefined = pendingBlobs[localId2];
+				assert(pendingBlob2 !== undefined, "Expect pending blob with localId2");
+				assert.strictEqual(pendingBlob2.state, "uploaded");
+				assert.strictEqual(pendingBlob2.blob, bufferToString(textToBlob("world"), "base64"));
+				assert.strictEqual(pendingBlob2.storageId, "04fea06420ca60892f73becee3614f6d023a4b7f");
+				assert.strictEqual(typeof pendingBlob2.uploadTime, "number");
+				assert.strictEqual(pendingBlob2.minTTLInSeconds, MIN_TTL);
+
+				const pendingBlob3: SerializableLocalBlobRecord | undefined = pendingBlobs[localId3];
+				assert(pendingBlob3 !== undefined, "Expect pending blob with localId3");
+				assert.strictEqual(pendingBlob3.state, "localOnly");
+				assert.strictEqual(pendingBlob3.blob, bufferToString(textToBlob("fizz"), "base64"));
+			} else {
+				const pendingBlobs = blobManager.getPendingBlobs();
+				assert.strictEqual(pendingBlobs, undefined);
+			}
+
+			// Only the first blob should be in the summary
+			const { ids, redirectTable } = getSummaryContentsWithFormatValidation(blobManager);
+			assert.strictEqual(ids?.length, 1);
+			assert.strictEqual(redirectTable?.length, 1);
+		});
+
+		it("Round-trips pending blobs when they are not shared", async () => {
+			const pendingBlobs = {
+				"a11323e2-27c1-417a-a5c3-e202cf515188": {
+					state: "uploaded",
+					blob: "aGVsbG8=", // "hello"
+					storageId: "b6fc4c620b67d95f953a5c1c1230aaab5db5a1b0",
+					// Even though this is expired, it should still round-trip as-is since this test
+					// doesn't try to start the share process for it.
+					uploadTime: 0,
+					minTTLInSeconds: 86400,
+				},
+				"0f2e8b7e-bec9-4694-9aee-6d635713e5e6": {
+					state: "localOnly",
+					blob: "d29ybGQ=", // "world"
+				},
+				"97268155-d656-4bd2-8271-8ed9a14fc179": {
+					state: "uploaded",
+					blob: "Zml6eg==", // "fizz"
+					storageId: "9b00ce20c29ae905c86d7ae0e832d95d63a468ca",
+					uploadTime: Date.now(),
+					minTTLInSeconds: 86400,
+				},
+			} as const;
+
+			const { blobManager } = createTestMaterial({
+				pendingBlobs,
+				createBlobPayloadPending,
+			});
+
+			const roundTrippedPendingBlobs = blobManager.getPendingBlobs();
+
+			assert.deepStrictEqual(roundTrippedPendingBlobs, pendingBlobs);
+
+			// Nothing should have made it into the summary
+			const { ids, redirectTable } = getSummaryContentsWithFormatValidation(blobManager);
+			assert.strictEqual(ids, undefined);
+			assert.strictEqual(redirectTable, undefined);
+		});
 	});
 }
