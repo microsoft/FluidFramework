36ef5f1524e175f6be07146eb59dbf60e00309ea
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-10-31 13:05:54 -0700

    Add tests

--- packages/dds/tree/src/test/shared-tree/independentView.spec.ts
+++ packages/dds/tree/src/test/shared-tree/independentView.spec.ts
@@ -7,11 +7,15 @@ import { strict as assert } from "node:assert";
 
 import {
 	independentInitializedView,
+	independentTreeAlpha,
+	independentTreeBeta,
 	// eslint-disable-next-line import/no-internal-modules
 } from "../../shared-tree/independentView.js";
 import {
 	extractPersistedSchema,
+	FieldKind,
 	SchemaFactory,
+	SchemaFactoryAlpha,
 	TreeViewConfigurationAlpha,
 } from "../../simple-tree/index.js";
 import { ForestTypeExpensiveDebug, TreeAlpha } from "../../shared-tree/index.js";
@@ -40,5 +44,166 @@ describe("independentView", () => {
 			);
 			assert.equal(view.root, 1);
 		});
+	});
+
+	describe("independentTreeAlpha", () => {
+		it("uninitialized: minimal", () => {
+			const tree = independentTreeAlpha({
+				forest: ForestTypeExpensiveDebug,
+				idCompressor: testIdCompressor,
+			});
+			const emptySchema = tree.exportSimpleSchema();
+			assert.deepEqual(emptySchema.definitions, new Map());
+			assert.equal(emptySchema.root.kind, FieldKind.Optional);
+			assert.deepEqual(emptySchema.root.simpleAllowedTypes, new Map());
+			assert.equal(tree.exportVerbose(), undefined);
+
+			const config = new TreeViewConfigurationAlpha({ schema: SchemaFactory.number });
+
+			const view = tree.viewWith(config);
+			assert(view.compatibility.canInitialize);
+			view.initialize(1);
+			assert.equal(view.root, 1);
+
+			assert.equal(tree.exportVerbose(), 1);
+			const filledSchema = tree.exportSimpleSchema();
+			assert.equal(filledSchema.root.kind, FieldKind.Required);
+			assert.deepEqual(
+				filledSchema.root.simpleAllowedTypes,
+				new Map([[SchemaFactory.number.identifier, { isStaged: undefined }]]),
+			);
+
+			view.dispose();
+		});
+
+		it("uninitialized: staged schema upgrade", () => {
+			const tree = independentTreeAlpha({
+				forest: ForestTypeExpensiveDebug,
+				idCompressor: testIdCompressor,
+			});
+
+			const beforeConfig = new TreeViewConfigurationAlpha({ schema: SchemaFactory.number });
+			const stagedConfig = new TreeViewConfigurationAlpha({
+				schema: SchemaFactoryAlpha.types([
+					SchemaFactory.number,
+					SchemaFactoryAlpha.staged(SchemaFactory.string),
+				]),
+			});
+			const afterConfig = new TreeViewConfigurationAlpha({
+				schema: [SchemaFactory.number, SchemaFactory.string],
+			});
+
+			// Initialize tree
+			{
+				const view = tree.viewWith(beforeConfig);
+				assert(view.compatibility.canInitialize);
+				view.initialize(1);
+				assert.equal(tree.exportVerbose(), 1);
+				const filledSchema = tree.exportSimpleSchema();
+				assert.equal(filledSchema.root.kind, FieldKind.Required);
+				assert.deepEqual(
+					filledSchema.root.simpleAllowedTypes,
+					new Map([[SchemaFactory.number.identifier, { isStaged: undefined }]]),
+				);
+
+				view.dispose();
+			}
+
+			// Open with schema that has change staged
+			{
+				const view = tree.viewWith(stagedConfig);
+				assert.equal(view.root, 1);
+				assert.throws(() => {
+					view.root = "x";
+				}, /does not conform to schema/);
+				view.dispose();
+
+				const filledSchema = tree.exportSimpleSchema();
+				assert.deepEqual(
+					filledSchema.root.simpleAllowedTypes,
+					// This is stored schema, so it does not include the staged type.
+					new Map([[SchemaFactory.number.identifier, { isStaged: undefined }]]),
+				);
+			}
+
+			// Do schema upgrade
+			{
+				const view = tree.viewWith(afterConfig);
+				assert.equal(view.compatibility.canView, false);
+				assert(view.compatibility.canUpgrade);
+				view.upgradeSchema();
+				view.root = "y";
+
+				assert.equal(tree.exportVerbose(), "y");
+				const filledSchema = tree.exportSimpleSchema();
+				assert.deepEqual(
+					filledSchema.root.simpleAllowedTypes,
+					new Map([
+						[SchemaFactory.number.identifier, { isStaged: undefined }],
+						[SchemaFactory.string.identifier, { isStaged: undefined }],
+					]),
+				);
+
+				view.dispose();
+			}
+
+			// Confirm tree is still readable with version that had staged schema.
+			{
+				const view = tree.viewWith(stagedConfig);
+				assert.equal(view.root, "y");
+				view.dispose();
+			}
+
+			// Tree cannot be read with original version.
+			{
+				const view = tree.viewWith(beforeConfig);
+				assert.equal(view.compatibility.canView, false);
+				assert.equal(view.compatibility.canUpgrade, false);
+				view.dispose();
+			}
+		});
+
+		it("initialized", () => {
+			const config = new TreeViewConfigurationAlpha({ schema: SchemaFactory.number });
+			const tree = independentTreeAlpha({
+				forest: ForestTypeExpensiveDebug,
+				jsonValidator: ajvValidator,
+				content: {
+					schema: extractPersistedSchema(config.schema, FluidClientVersion.v2_0, () => true),
+					tree: TreeAlpha.exportCompressed(1, {
+						minVersionForCollab: FluidClientVersion.v2_0,
+					}),
+					idCompressor: testIdCompressor,
+				},
+			});
+
+			const view = tree.viewWith(config);
+			assert.equal(view.root, 1);
+		});
+
+		it("oddly allowed jsonValidator", () => {
+			const config = new TreeViewConfigurationAlpha({ schema: SchemaFactory.number });
+			const tree = independentTreeAlpha({
+				forest: ForestTypeExpensiveDebug,
+				// This does nothing, and is not part of the allowed input type.
+				// Usually this would fail with "Object literal may only specify known properties"
+				// but in this case there is no error.
+				// This is fine as it works, and the jsonValidator is simply unnecessary and unused when not providing content, but seems odd.
+				jsonValidator: ajvValidator,
+			});
+
+			const view = tree.viewWith(config);
+			assert.equal(view.compatibility.canInitialize, true);
+		});
+	});
+
+	it("independentTreeBeta", () => {
+		const tree = independentTreeBeta();
+		const view = tree.viewWith(
+			new TreeViewConfigurationAlpha({ schema: SchemaFactory.number }),
+		);
+		view.initialize(42);
+		assert.equal(view.root, 42);
+		view.dispose();
 	});
 });
