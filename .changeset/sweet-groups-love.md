6a7cb3834a29b5881f568ff84055902eb7ea7750
Navin Agarwal <naagar@microsoft.com>
2025-10-31 09:56:42 -0700

    (compat) Add new error type for layer incompatibility error

---
+++ .changeset/sweet-groups-love.md
@@ -0,0 +1,8 @@
+---
+"@fluidframework/core-interfaces": minor
+"__section": legacy
+---
+Added a new Fluid error type layerIncompatibilityError
+
+A new Fluid error type `layerIncompatibilityError` is added to `FluidErrorTypesAlpha` as @legacy @alpha.
+This will be moved to `FluidErrorTypes` as @legacy @beta in a future legacy breaking release.
--- packages/common/core-interfaces/src/error.ts
+++ packages/common/core-interfaces/src/error.ts
@@ -42,6 +42,25 @@ export const FluidErrorTypes = {
 export type FluidErrorTypes = (typeof FluidErrorTypes)[keyof typeof FluidErrorTypes];
 
 /**
+ * New error types that are still in alpha stage. Once stabilized, they will be moved to FluidErrorTypes.
+ * @legacy @alpha
+ */
+export const FluidErrorTypesAlpha = {
+	...FluidErrorTypes,
+	/**
+	 * Error indicating that two Fluid layers are incompatible. For instance, if the Loader layer is
+	 * not compatible with the Runtime layer, this error type will be used.
+	 */
+	layerIncompatibilityError: "layerIncompatibilityError",
+} as const;
+
+/**
+ * @legacy @alpha
+ */
+export type FluidErrorTypesAlpha =
+	(typeof FluidErrorTypesAlpha)[keyof typeof FluidErrorTypesAlpha];
+
+/**
  * Base interface for all errors and warnings emitted the container.
  *
  * @remarks
@@ -130,14 +149,6 @@ export interface IThrottlingWarning extends IErrorBase {
 }
 
 /**
- * Symbol used to identify an error of type {@link ILayerIncompatibilityError}.
- * @legacy @alpha
- */
-export const layerIncompatibilityErrorSymbol: unique symbol = Symbol(
-	"LayerIncompatibilityError",
-);
-
-/**
  * Usage error indicating that two Fluid layers are incompatible. For instance, if the Loader layer is
  * not compatible with the Runtime layer, the container will be disposed with this error.
  * @legacy @alpha
@@ -146,12 +157,7 @@ export interface ILayerIncompatibilityError extends IErrorBase {
 	/**
 	 * {@inheritDoc IErrorBase.errorType}
 	 */
-	readonly errorType: typeof FluidErrorTypes.usageError;
-
-	/**
-	 * Symbol used to identify this error as a layer incompatibility error.
-	 */
-	readonly [layerIncompatibilityErrorSymbol]: true;
+	readonly errorType: typeof FluidErrorTypesAlpha.layerIncompatibilityError;
 	/**
 	 * The layer that is reporting the incompatibility.
 	 */
