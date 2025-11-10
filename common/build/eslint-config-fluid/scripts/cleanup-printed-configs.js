7c7aa68358ec42c55d2ed8f8eec5039c0c31c226
Tyler Butler <tylerbu@microsoft.com>
2025-11-10 14:52:01 -0800

    build(eslint-config-fluid): Only write printed configs when changed

--- common/build/eslint-config-fluid/scripts/cleanup-printed-configs.js
+++ common/build/eslint-config-fluid/scripts/cleanup-printed-configs.js
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 const sortJson = require("sort-json");
 
@@ -11,6 +11,7 @@ const sortJson = require("sort-json");
 	const args = process.argv.slice(2);
 	const sourcePath = args[0];
 	const files = await fs.promises.readdir(sourcePath);
+	const writePromises = [];
 
 	for (const file of files) {
 		const filePath = path.join(sourcePath, file);
@@ -18,22 +19,26 @@ const sortJson = require("sort-json");
 		if (stat.isDirectory()) {
 			continue;
 		}
-		const content = fs.readFileSync(filePath);
-		const json = JSON.parse(content);
+		const originalContent = await fs.readFile(filePath, "utf8");
+		const json = JSON.parse(originalContent);
 
 		// Remove the parser property because it's an absolute path and will vary based on the local environment.
 		delete json.parser;
 
-		// Write out the file.
-		fs.writeFileSync(filePath, JSON.stringify(json, undefined, 4));
-
-		// Sort the JSON in-place.
+		// Generate the new content with sorting applied
 		// Sorting at all is desirable as otherwise changes in the order of common config references may cause large diffs
 		// with little semantic meaning.
 		// On the other hand, fully sorting the json can be misleading:
 		// some eslint settings depend on object key order ("import-x/resolver" being a known one, see
 		// https://github.com/un-ts/eslint-plugin-import-x/blob/master/src/utils/resolve.ts).
 		// Using depth 2 is a nice compromise.
-		sortJson.overwrite(filePath, { indentSize: 4, depth: 2 });
+		const newContent = sortJson.format(json, { indentSize: 4, depth: 2 });
+
+		// Only write the file if the content has changed
+		if (newContent !== originalContent) {
+			writePromises.push(fs.writeFile(filePath, newContent));
+		}
 	}
+
+	await Promise.all(writePromises);
 })();
