58c96640665305a7d79aea5b400746d8bc2f431d
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2025-11-25 23:32:50 +0000

    fix(eslint-plugin-fluid): no-markdown-links-in-jsdoc

--- common/build/eslint-plugin-fluid/src/rules/no-markdown-links-in-jsdoc.js
+++ common/build/eslint-plugin-fluid/src/rules/no-markdown-links-in-jsdoc.js
@@ -3,8 +3,70 @@
  * Licensed under the MIT License.
  */
 
-// eslint-plugin-no-markdown-comments/lib/rules/no-markdown-links-in-comments.js
-module.exports = {
+//@ts-check
+/**
+ * @typedef {import("eslint").Rule.RuleModule} RuleModule
+ * @typedef {import('@microsoft/tsdoc').DocNode} DocNode
+ * @typedef {import('@microsoft/tsdoc').DocPlainText} DocPlainText
+ *
+ * @typedef {{
+ * 	linkText: string;
+ * 	linkTarget: string;
+ * 	startIndex: number;
+ * 	endIndex: number;
+ * }} MarkdownLinkInfo
+ */
+
+const { fail } = require("node:assert");
+const { DocNodeKind, TSDocParser } = require("@microsoft/tsdoc");
+
+const parser = new TSDocParser();
+
+/**
+ * Checks if a comment text starts with a hyphen.
+ * @param {DocPlainText} plainTextNode - The plain text node to check.
+ * @returns {MarkdownLinkInfo[]}
+ */
+function findMarkdownLinksInPlainText(plainTextNode) {
+	const textRange =
+		plainTextNode.textExcerpt?.getContainingTextRange() ??
+		fail("Expected textExcerpt to be defined.");
+	const matches = plainTextNode.text.matchAll(/\[([^\]]+)\]\(([^)]+)\)/g);
+	return Array.from(matches, (match) => ({
+		linkText: match[1],
+		linkTarget: match[2],
+		startIndex: textRange.pos + match.index,
+		endIndex: textRange.pos + match.index + match[0].length,
+	}));
+}
+
+/**
+ * Checks if a comment body starts with a hyphen.
+ * @param { DocNode } commentBodyNode - The doc node representing the body of the comment.
+ * @returns {MarkdownLinkInfo[]}
+ */
+function findMarkdownLinks(commentBodyNode) {
+	// Walk down first node of the tree until we find a leaf.
+	// If it's plain text, and starts with a hyphen, return true.
+	// Otherwise, return false.
+	if (commentBodyNode.kind === DocNodeKind.PlainText) {
+		return findMarkdownLinksInPlainText(/** @type {DocPlainText} */ (commentBodyNode));
+	}
+
+	const childNodes = commentBodyNode.getChildNodes();
+
+	const links = [];
+	for (const childNode of childNodes) {
+		links.push(...findMarkdownLinks(childNode));
+	}
+	return links;
+}
+
+/**
+ * JSDoc/TSDoc tags do not require a hyphen after them.
+ * @type {RuleModule}
+ */
+const rule = {
 	meta: {
 		type: "problem",
 		docs: {
@@ -32,33 +94,70 @@ module.exports = {
 					.filter((comment) => comment.type === "Block" && comment.value.startsWith("*"));
 
 				for (const comment of comments) {
-					// +2 for the leading "/*", which is omitted by `comment.value`, but included in `comment.range`.
-					const commentStartIndex = comment.range[0] + 2;
+					if (comment.range === undefined) {
+						continue;
+					}
 
-					const matches = comment.value.matchAll(/\[([^\]]+)\]\(([^)]+)\)/g);
-					for (const match of matches) {
-						const [fullMatch, text, url] = match;
+					const commentStartIndex = comment.range[0];
 
-						const startIndex = commentStartIndex + match.index;
-						const endIndex = startIndex + fullMatch.length;
+					// TSDoc parser requires the surrounding "/**" and "*/", but eslint strips those off in `comment.value`.
+					const parserContext = parser.parseString(`/**${comment.value}*/`);
+					const parsedComment = parserContext.docComment;
 
-						context.report({
-							loc: {
-								start: sourceCode.getLocFromIndex(startIndex),
-								end: sourceCode.getLocFromIndex(endIndex),
-							},
-							messageId: "markdownLink",
-							fix(fixer) {
-								const trimmedText = text?.trim();
+					const blocksToCheck = [
+						...parsedComment.customBlocks,
+						...parsedComment.seeBlocks,
+					];
+					if (parsedComment.remarksBlock) {
+						blocksToCheck.push(parsedComment.remarksBlock);
+					}
+					if (parsedComment.privateRemarks) {
+						blocksToCheck.push(parsedComment.privateRemarks);
+					}
+					if (parsedComment.deprecatedBlock) {
+						blocksToCheck.push(parsedComment.deprecatedBlock);
+					}
+					if (parsedComment.returnsBlock) {
+						blocksToCheck.push(parsedComment.returnsBlock);
+					}
+
+					/**
+					 * @param {DocNode} node
+					 * @returns {void}
+					 */
+					function checkCommentBlock(node) {
+						const links = findMarkdownLinks(node);
+						for (const link of links) {
+							const startIndex = commentStartIndex + link.startIndex - 1;
+							const endIndex = commentStartIndex + link.endIndex - 1;
+
+							context.report({
+								loc: {
+									start: sourceCode.getLocFromIndex(startIndex),
+									end: sourceCode.getLocFromIndex(endIndex),
+								},
+								messageId: "markdownLink",
+								fix(fixer) {
+								const trimmedText = link.linkText.trim();
 								const tsdocLink = trimmedText
-									? `{@link ${url} | ${trimmedText}}`
-									: `{@link ${url}}`;
+									? `{@link ${link.linkTarget} | ${trimmedText}}`
+									: `{@link ${link.linkTarget}}`;
 								return fixer.replaceTextRange([startIndex, endIndex], tsdocLink);
 							},
-						});
+							});
+						}
+					}
+
+					// Note: the TSDoc format makes it difficult to extract the range information for the block content specifically.
+					// Instead, we just report the range for the tag itself.
+					checkCommentBlock(parsedComment.summarySection);
+					for (const block of blocksToCheck) {
+						checkCommentBlock(block.content);
 					}
 				}
 			},
 		};
 	},
 };
+
+module.exports = rule;
