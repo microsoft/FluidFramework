1aae5a5e4249ef8afa03813dcd68b81647232181
Tyler Butler <tylerbu@microsoft.com>
2025-11-26 12:32:19 -0800

    chore(eslint9): fix flat config generation script for ESM (__dirname)

--- scripts/generate-flat-eslint-configs.ts
+++ scripts/generate-flat-eslint-configs.ts
@@ -17,6 +17,9 @@
 import { promises as fs } from "fs";
 import * as path from "path";
 
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const repoRoot = path.resolve(__dirname, "..");
 
 interface PackageTarget {
