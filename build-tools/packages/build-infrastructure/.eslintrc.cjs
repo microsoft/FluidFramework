ad0970854b5dd381256547f96dc209e4d2f77be2
Tyler Butler <tylerbu@microsoft.com>
2025-12-01 13:58:45 -0800

    feedback

--- build-tools/packages/build-infrastructure/.eslintrc.cjs
+++ build-tools/packages/build-infrastructure/.eslintrc.cjs
@@ -20,10 +20,6 @@ module.exports = {
 		// This package is exclusively used in a Node.js context
 		"import-x/no-nodejs-modules": "off",
 
-		"import-x/no-default-export": "off",
-		"import-x/no-deprecated": "off",
-		"import-x/no-named-as-default-member": "off",
-
 		"tsdoc/syntax": ["warn"],
 	},
 	overrides: [
