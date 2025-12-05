607da53bf7f1d4c614a238262636d1208e24e85b
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2025-12-05 13:33:06 -0800

    Make field kind upgrades monotonic (#25985)
    ## Description
    
    At the flex-tree abstraction level, the handling of field kind changes
    has issues. This change clarifies the two kinds of upgrades we could
    allow (the current monotonic approach, and a possible future more
    flexible one). This also fixes a bug with never field handling which
    allowed some possible upgrade cycles which are supposed to be disallowed
    (but in practice are not useful schema and won't be used by the public
    API, so should not happen)

--- packages/dds/tree/src/test/feature-libraries/modular-schema/basicRebasers.ts
+++ packages/dds/tree/src/test/feature-libraries/modular-schema/basicRebasers.ts
@@ -113,5 +113,5 @@ export const valueHandler = {
 export const valueField = new FlexFieldKind(
 	brandConst("Value")<FieldKindIdentifier>(),
 	Multiplicity.Single,
-	{ changeHandler: valueHandler, allowedMonotonicUpgrade: (a, b) => false },
+	{ changeHandler: valueHandler, allowMonotonicUpgradeFrom: new Set() },
 );
