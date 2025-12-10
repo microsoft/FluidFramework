748fbfd4ff1305d5789a89e60fa6bf139bdd69e7
Mark Fields <mfields@microsoft.com>
2025-12-09 18:46:28 -0800

    Separate checks

--- packages/runtime/container-runtime/src/channelCollection.ts
+++ packages/runtime/container-runtime/src/channelCollection.ts
@@ -493,25 +493,28 @@ export class ChannelCollection
 			}
 
 			// If a non-local operation then go and create the object, otherwise mark it as officially attached.
-			if (this.alreadyProcessed(attachMessage.id)) {
+			// Check for collision with unbound (not-yet-attached) data store first
+			if (this.contexts.getUnbound(attachMessage.id) !== undefined) {
 				// TODO: dataStoreId may require a different tag from PackageData #7488
-				const existingContext = this.contexts.get(attachMessage.id);
-				const existsInAliasMap = this.aliasMap.get(attachMessage.id) !== undefined;
-				// If it's in the alias map or the context is attaching/attached, it's considered attached
-				const isAttachingOrAttached =
-					existsInAliasMap ||
-					existingContext?.attachState === AttachState.Attaching ||
-					existingContext?.attachState === AttachState.Attached;
 				const error = new DataCorruptionError(
 					// pre-0.58 error message: duplicateDataStoreCreatedWithExistingId
-					isAttachingOrAttached
-						? "Duplicate DataStore created with existing id for attaching/attached DataStore"
-						: "Duplicate DataStore created with existing id for not-yet-attached DataStore",
+					"Duplicate DataStore created with existing id for not-yet-attached DataStore",
 					{
 						...extractSafePropertiesFromMessage(envelope),
 						...tagCodeArtifacts({ dataStoreId: attachMessage.id }),
-						existingAttachState: existingContext?.attachState,
-						existsInAliasMap,
+					},
+				);
+				throw error;
+			}
+			// Check for collision with already processed (attaching/attached or aliased) data store
+			if (this.alreadyProcessed(attachMessage.id)) {
+				// TODO: dataStoreId may require a different tag from PackageData #7488
+				const error = new DataCorruptionError(
+					// pre-0.58 error message: duplicateDataStoreCreatedWithExistingId
+					"Duplicate DataStore created with existing id for attaching/attached DataStore",
+					{
+						...extractSafePropertiesFromMessage(envelope),
+						...tagCodeArtifacts({ dataStoreId: attachMessage.id }),
 					},
 				);
 				throw error;
