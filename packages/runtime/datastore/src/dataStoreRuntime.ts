facd4da7e558de4f310c3abdae0a273f71687695
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2025-12-11 08:05:45 -0800

    Update packages/runtime/datastore/src/dataStoreRuntime.ts
    Co-authored-by: Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>

--- packages/runtime/datastore/src/dataStoreRuntime.ts
+++ packages/runtime/datastore/src/dataStoreRuntime.ts
@@ -308,7 +308,7 @@ export class FluidDataStoreRuntime
 	private readonly submitMessagesWithoutEncodingHandles: boolean;
 
 	/**
-	 * Minimum version of the FF runtime that is required to collaborate on new documents. DDSes may read this value to
+	 * Minimum version of the Fluid Framework runtime that is required to collaborate on new documents. DDSes may read this value to
 	 * determine which feature flags should be enabled. This property is consumed by SharedObjectFactory (which are
 	 * implementations of {@link @fluidframework/datastore-definitions#IChannelFactory}).
 	 * See {@link @fluidframework/container-runtime#LoadContainerRuntimeParams.minVersionForCollab} for more details.
