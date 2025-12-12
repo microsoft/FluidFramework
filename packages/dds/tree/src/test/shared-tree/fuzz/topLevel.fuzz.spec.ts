67319af363c7982038e01bae84c1765520049166
yann-achard-MS <97201204+yann-achard-MS@users.noreply.github.com>
2025-12-12 21:38:03 +0000

    remove seed replay

--- packages/dds/tree/src/test/shared-tree/fuzz/topLevel.fuzz.spec.ts
+++ packages/dds/tree/src/test/shared-tree/fuzz/topLevel.fuzz.spec.ts
@@ -61,7 +61,6 @@ describe("Fuzz - Top-Level", () => {
 				attachingBeforeRehydrateDisable: true,
 			},
 			reconnectProbability: 0.1,
-			replay: 0,
 			idCompressorFactory: deterministicIdCompressorFactory(0xdeadbeef),
 			skip: [
 				...[30], //  0x92a
