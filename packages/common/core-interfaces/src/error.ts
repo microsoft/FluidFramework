microsoft/FluidFramework

commit 01d568b4bcbff38a0ee4b614bad1dfb0c400a360

(compat) Added a new error type for layer incompatibility error (#25784)
Follow up to #25706.

Added a new error type `layerIncompatibilityError` which will be used
when a layer incompatibility is detected between Fluid layers. This will
help applications detect when an error is because of layer
incompatibility and take appropriate steps.

The new error type is added as an legacy / alpha API. Since the
`FluidErrorTypes` type is legacy / beta, added a `FluidErrorTypesAlpha`
type where new error types will be added to stage them. Once the
breaking change window has passed, error types will be moved to
`FluidErrorTypes`.


[AB#51811](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/51811)

packages/common/core-interfaces/src/error.ts
@@ -42,6 +42,29 @@ export const FluidErrorTypes = {
 export type FluidErrorTypes = (typeof FluidErrorTypes)[keyof typeof FluidErrorTypes];
 
 /**
+ * New error types that are still in alpha stage. Once stabilized, they will be moved to FluidErrorTypes.
+ * @legacy @alpha
+ */
+export const FluidErrorTypesAlpha = {
+	...FluidErrorTypes,
+	/**
+	 * Error indicating that two Fluid layers are incompatible. For instance, if the Loader layer is
+	 * not compatible with the Runtime layer, the container create / load will fail with an error of this type.
+	 * In most cases, the layer compatibility validation happens during container load / create causing it to
+	 * fail with this error type.
+	 * In some cases such as for the Runtime and DataStore layer compatibility, the incompatibility may be detected
+	 * during data store loads. In such cases, the data store load will fail with this error type.
+	 */
+	layerIncompatibilityError: "layerIncompatibilityError",
+} as const;
+
+/**
+ * @legacy @alpha
+ */
+export type FluidErrorTypesAlpha =
+	(typeof FluidErrorTypesAlpha)[keyof typeof FluidErrorTypesAlpha];
+
+/**
  * Base interface for all errors and warnings emitted the container.
  *
  * @remarks
@@ -130,15 +153,7 @@ export interface IThrottlingWarning extends IErrorBase {
 }
 
 /**
- * Symbol used to identify an error of type {@link ILayerIncompatibilityError}.
- * @legacy @alpha
- */
-export const layerIncompatibilityErrorSymbol: unique symbol = Symbol(
-	"LayerIncompatibilityError",
-);
-
-/**
- * Usage error indicating that two Fluid layers are incompatible. For instance, if the Loader layer is
+ * Layer incompatibility error indicating that two Fluid layers are incompatible. For instance, if the Loader layer is
  * not compatible with the Runtime layer, the container will be disposed with this error.
  * @legacy @alpha
  */
@@ -146,12 +161,7 @@ export interface ILayerIncompatibilityError extends IErrorBase {
 	/**
 	 * {@inheritDoc IErrorBase.errorType}
 	 */
-	readonly errorType: typeof FluidErrorTypes.usageError;
-
-	/**
-	 * Symbol used to identify this error as a layer incompatibility error.
-	 */
-	readonly [layerIncompatibilityErrorSymbol]: true;
+	readonly errorType: typeof FluidErrorTypesAlpha.layerIncompatibilityError;
 	/**
 	 * The layer that is reporting the incompatibility.
 	 */

packages/utils/telemetry-utils/src/error.ts
@@ -6,7 +6,7 @@
 import type { IErrorBase, ITelemetryBaseProperties } from "@fluidframework/core-interfaces";
 import {
 	FluidErrorTypes,
-	layerIncompatibilityErrorSymbol,
+	FluidErrorTypesAlpha,
 	type IGenericError,
 	type ILayerIncompatibilityError,
 	type IUsageError,
@@ -222,10 +222,10 @@ export class DataProcessingError extends LoggingError implements IErrorBase, IFl
  * @internal
  */
 export class LayerIncompatibilityError
-	extends UsageError
+	extends LoggingError
 	implements ILayerIncompatibilityError
 {
-	public [layerIncompatibilityErrorSymbol] = true as const;
+	public readonly errorType = FluidErrorTypesAlpha.layerIncompatibilityError;
 	public readonly layer: string;
 	public readonly layerVersion: string;
 	public readonly incompatibleLayer: string;

packages/utils/telemetry-utils/src/fluidErrorBase.ts
@@ -5,8 +5,7 @@
 
 import type { ITelemetryBaseProperties } from "@fluidframework/core-interfaces";
 import {
-	FluidErrorTypes,
-	layerIncompatibilityErrorSymbol,
+	FluidErrorTypesAlpha,
 	type ILayerIncompatibilityError,
 } from "@fluidframework/core-interfaces/internal";
 
@@ -125,8 +124,6 @@ export function isLayerIncompatibilityError(
 	error: unknown,
 ): error is ILayerIncompatibilityError {
 	return (
-		isFluidError(error) &&
-		error.errorType === FluidErrorTypes.usageError &&
-		layerIncompatibilityErrorSymbol in error
+		isFluidError(error) && error.errorType === FluidErrorTypesAlpha.layerIncompatibilityError
 	);
 }

packages/test/test-end-to-end-tests/src/test/layerCompat.spec.ts
@@ -30,7 +30,7 @@ import {
 	runtimeCoreCompatDetails,
 } from "@fluidframework/container-runtime/internal";
 import {
-	FluidErrorTypes,
+	FluidErrorTypesAlpha,
 	type ITelemetryBaseProperties,
 } from "@fluidframework/core-interfaces/internal";
 import {
@@ -225,7 +225,7 @@ function getExpectedErrorEvents(
 	const expectedErrorEvents: ExpectedEvents = [
 		{
 			eventName: "fluid:telemetry:Container:ContainerDispose",
-			errorType: FluidErrorTypes.usageError,
+			errorType: FluidErrorTypesAlpha.layerIncompatibilityError,
 		},
 	];
 
@@ -242,7 +242,7 @@ function getExpectedErrorEvents(
 		// during data store realization, so we expect this error event to be logged.
 		expectedErrorEvents.unshift({
 			eventName: "fluid:telemetry:FluidDataStoreContext:RealizeError",
-			errorType: FluidErrorTypes.usageError,
+			errorType: FluidErrorTypesAlpha.layerIncompatibilityError,
 		});
 	} else if (layer2 === "dataStore" && flow === "create") {
 		// In create flows, the layer compat validation in the Runtime layer happens during data store runtime
@@ -251,7 +251,7 @@ function getExpectedErrorEvents(
 		// data store runtime attach flow, so we do not expect this error event to be logged.
 		expectedErrorEvents.unshift({
 			eventName: "fluid:telemetry:FluidDataStoreContext:AttachRuntimeError",
-			errorType: FluidErrorTypes.usageError,
+			errorType: FluidErrorTypesAlpha.layerIncompatibilityError,
 		});
 	}
 

packages/common/core-interfaces/src/index.ts
@@ -14,8 +14,10 @@ export type {
 	IThrottlingWarning,
 	ILayerIncompatibilityError,
 } from "./error.js";
-export { layerIncompatibilityErrorSymbol } from "./error.js";
-export { FluidErrorTypes } from "./error.js";
+export {
+	FluidErrorTypes,
+	FluidErrorTypesAlpha,
+} from "./error.js";
 
 export type {
 	ExtendEventProvider,

commit a8532bdd903626524f17d2ec650d8904046e5308

Upgraded 'layerIncompatibilityError' and 'ILayerIncompatibilityError' to @legacy @beta (#26068)
This PR upgrades the `layerIncompatibilityError` error type and
`ILayerIncompatibilityError` interface from legacy alpha to legacy beta
status. The error type was originally introduced by
https://github.com/microsoft/FluidFramework/pull/25784 in release 2.72.0
and is now being promoted to a more stable API level.
- Moved `layerIncompatibilityError` from `FluidErrorTypesAlpha` to
`FluidErrorTypes`.
- Updated the `ILayerIncompatibilityError` interface to reference the
beta error type.
- Updated all usages across the codebase to use the new beta references.


[AB#54417](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/54417)

packages/common/core-interfaces/src/error.ts
@@ -34,6 +34,18 @@ export const FluidErrorTypes = {
 	 * Error indicating an API is being used improperly resulting in an invalid operation.
 	 */
 	usageError: "usageError",
+
+	/**
+	 * Error indicating that two Fluid layers are incompatible.
+	 * @remarks
+	 * For instance, if the Loader layer is not compatible with the Runtime layer, the container create / load
+	 * will fail with an error of this type.
+	 * In most cases, the layer compatibility validation happens during container load / create causing it to
+	 * fail with this error type.
+	 * In some cases such as for the Runtime and DataStore layer compatibility, the incompatibility may be detected
+	 * during data store loads. In such cases, the data store load will fail with this error type.
+	 */
+	layerIncompatibilityError: "layerIncompatibilityError",
 } as const;
 
 /**
@@ -47,15 +59,6 @@ export type FluidErrorTypes = (typeof FluidErrorTypes)[keyof typeof FluidErrorTy
  */
 export const FluidErrorTypesAlpha = {
 	...FluidErrorTypes,
-	/**
-	 * Error indicating that two Fluid layers are incompatible. For instance, if the Loader layer is
-	 * not compatible with the Runtime layer, the container create / load will fail with an error of this type.
-	 * In most cases, the layer compatibility validation happens during container load / create causing it to
-	 * fail with this error type.
-	 * In some cases such as for the Runtime and DataStore layer compatibility, the incompatibility may be detected
-	 * during data store loads. In such cases, the data store load will fail with this error type.
-	 */
-	layerIncompatibilityError: "layerIncompatibilityError",
 } as const;
 
 /**
@@ -155,13 +158,13 @@ export interface IThrottlingWarning extends IErrorBase {
 /**
  * Layer incompatibility error indicating that two Fluid layers are incompatible. For instance, if the Loader layer is
  * not compatible with the Runtime layer, the container will be disposed with this error.
- * @legacy @alpha
+ * @legacy @beta
  */
 export interface ILayerIncompatibilityError extends IErrorBase {
 	/**
 	 * {@inheritDoc IErrorBase.errorType}
 	 */
-	readonly errorType: typeof FluidErrorTypesAlpha.layerIncompatibilityError;
+	readonly errorType: typeof FluidErrorTypes.layerIncompatibilityError;
 	/**
 	 * The layer that is reporting the incompatibility.
 	 */

packages/utils/telemetry-utils/src/error.ts
@@ -6,7 +6,6 @@
 import type { IErrorBase, ITelemetryBaseProperties } from "@fluidframework/core-interfaces";
 import {
 	FluidErrorTypes,
-	FluidErrorTypesAlpha,
 	type IGenericError,
 	type ILayerIncompatibilityError,
 	type IUsageError,
@@ -235,7 +234,7 @@ export class LayerIncompatibilityError
 	extends LoggingError
 	implements ILayerIncompatibilityError
 {
-	public readonly errorType = FluidErrorTypesAlpha.layerIncompatibilityError;
+	public readonly errorType = FluidErrorTypes.layerIncompatibilityError;
 	public readonly layer: string;
 	public readonly layerVersion: string;
 	public readonly incompatibleLayer: string;

packages/utils/telemetry-utils/src/fluidErrorBase.ts
@@ -5,7 +5,7 @@
 
 import type { ITelemetryBaseProperties } from "@fluidframework/core-interfaces";
 import {
-	FluidErrorTypesAlpha,
+	FluidErrorTypes,
 	type ILayerIncompatibilityError,
 } from "@fluidframework/core-interfaces/internal";
 
@@ -123,7 +123,5 @@ export function isFluidError(error: unknown): error is IFluidErrorBase {
 export function isLayerIncompatibilityError(
 	error: unknown,
 ): error is ILayerIncompatibilityError {
-	return (
-		isFluidError(error) && error.errorType === FluidErrorTypesAlpha.layerIncompatibilityError
-	);
+	return isFluidError(error) && error.errorType === FluidErrorTypes.layerIncompatibilityError;
 }

packages/common/container-definitions/src/test/types/validateContainerDefinitionsPrevious.generated.ts
@@ -781,6 +781,7 @@ declare type old_as_current_for_TypeAlias_ContainerErrorTypes = requireAssignabl
  * typeValidation.broken:
  * "TypeAlias_ContainerErrorTypes": {"backCompat": false}
  */
+// @ts-expect-error compatibility expected to be broken
 declare type current_as_old_for_TypeAlias_ContainerErrorTypes = requireAssignableTo<TypeOnly<current.ContainerErrorTypes>, TypeOnly<old.ContainerErrorTypes>>
 
 /*

packages/common/core-interfaces/src/test/types/validateCoreInterfacesPrevious.generated.ts
@@ -556,6 +556,7 @@ declare type old_as_current_for_TypeAlias_FluidErrorTypes = requireAssignableTo<
  * typeValidation.broken:
  * "TypeAlias_FluidErrorTypes": {"backCompat": false}
  */
+// @ts-expect-error compatibility expected to be broken
 declare type current_as_old_for_TypeAlias_FluidErrorTypes = requireAssignableTo<TypeOnly<current.FluidErrorTypes>, TypeOnly<old.FluidErrorTypes>>
 
 /*

packages/drivers/odsp-driver-definitions/src/test/types/validateOdspDriverDefinitionsPrevious.generated.ts
@@ -160,6 +160,7 @@ declare type old_as_current_for_Interface_IOdspError = requireAssignableTo<TypeO
  * typeValidation.broken:
  * "Interface_IOdspError": {"backCompat": false}
  */
+// @ts-expect-error compatibility expected to be broken
 declare type current_as_old_for_Interface_IOdspError = requireAssignableTo<TypeOnly<current.IOdspError>, TypeOnly<old.IOdspError>>
 
 /*
@@ -520,6 +521,7 @@ declare type old_as_current_for_TypeAlias_OdspError = requireAssignableTo<TypeOn
  * typeValidation.broken:
  * "TypeAlias_OdspError": {"backCompat": false}
  */
+// @ts-expect-error compatibility expected to be broken
 declare type current_as_old_for_TypeAlias_OdspError = requireAssignableTo<TypeOnly<current.OdspError>, TypeOnly<old.OdspError>>
 
 /*
@@ -538,6 +540,7 @@ declare type old_as_current_for_TypeAlias_OdspErrorTypes = requireAssignableTo<T
  * typeValidation.broken:
  * "TypeAlias_OdspErrorTypes": {"backCompat": false}
  */
+// @ts-expect-error compatibility expected to be broken
 declare type current_as_old_for_TypeAlias_OdspErrorTypes = requireAssignableTo<TypeOnly<current.OdspErrorTypes>, TypeOnly<old.OdspErrorTypes>>
 
 /*

packages/test/test-end-to-end-tests/src/test/layerCompat.spec.ts
@@ -30,7 +30,7 @@ import {
 	runtimeCoreCompatDetails,
 } from "@fluidframework/container-runtime/internal";
 import {
-	FluidErrorTypesAlpha,
+	FluidErrorTypes,
 	type ITelemetryBaseProperties,
 } from "@fluidframework/core-interfaces/internal";
 import {
@@ -225,7 +225,7 @@ function getExpectedErrorEvents(
 	const expectedErrorEvents: ExpectedEvents = [
 		{
 			eventName: "fluid:telemetry:Container:ContainerDispose",
-			errorType: FluidErrorTypesAlpha.layerIncompatibilityError,
+			errorType: FluidErrorTypes.layerIncompatibilityError,
 		},
 	];
 
@@ -242,7 +242,7 @@ function getExpectedErrorEvents(
 		// during data store realization, so we expect this error event to be logged.
 		expectedErrorEvents.unshift({
 			eventName: "fluid:telemetry:FluidDataStoreContext:RealizeError",
-			errorType: FluidErrorTypesAlpha.layerIncompatibilityError,
+			errorType: FluidErrorTypes.layerIncompatibilityError,
 		});
 	} else if (layer2 === "dataStore" && flow === "create") {
 		// In create flows, the layer compat validation in the Runtime layer happens during data store runtime
@@ -251,7 +251,7 @@ function getExpectedErrorEvents(
 		// data store runtime attach flow, so we do not expect this error event to be logged.
 		expectedErrorEvents.unshift({
 			eventName: "fluid:telemetry:FluidDataStoreContext:AttachRuntimeError",
-			errorType: FluidErrorTypesAlpha.layerIncompatibilityError,
+			errorType: FluidErrorTypes.layerIncompatibilityError,
 		});
 	}
 