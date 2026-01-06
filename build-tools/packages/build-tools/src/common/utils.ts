c75f713d067a036bd7edffa70df55b853c0d2855
Tyler Butler <tylerbu@microsoft.com>
2026-01-06 12:27:07 -0800

    format

--- build-tools/packages/build-tools/src/common/utils.ts
+++ build-tools/packages/build-tools/src/common/utils.ts
@@ -5,8 +5,8 @@
 
 import * as child_process from "child_process";
 import * as fs from "fs";
-import * as path from "path";
 import { fileURLToPath } from "node:url";
+import * as path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
