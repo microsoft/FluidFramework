ef0bc28747d71f816345f79314e1ef60902c6495
Tyler Butler <tylerbu@microsoft.com>
2026-01-12 17:19:49 -0800

    ignore files

---
+++ server/routerlicious/packages/tinylicious/.eslintrc.cjs
@@ -0,0 +1,27 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = {
+	extends: [
+		require.resolve("@fluidframework/eslint-config-fluid/minimal-deprecated"),
+		"prettier",
+	],
+	parserOptions: {
+		project: ["./tsconfig.json", "./src/test/tsconfig.json"],
+	},
+	rules: {
+		"@typescript-eslint/no-use-before-define": "off",
+		"@typescript-eslint/promise-function-async": "off",
+		"@typescript-eslint/strict-boolean-expressions": "off",
+		"import-x/no-internal-modules": "off",
+		"import-x/no-nodejs-modules": "off",
+
+		// TODO: enable strict null checks in tsconfig and remove this override
+		"@typescript-eslint/prefer-nullish-coalescing": "off",
+
+		// TODO: remove usages of deprecated APIs and remove this override
+		"import-x/no-deprecated": "warn",
+	},
+};
