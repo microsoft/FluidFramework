6ce53ec38d8cc9c99c930b150f1ce7786a903952
Jason Hartman <jasonha@microsoft.com>
2026-01-16 10:48:44 -0800

    improvement(client-presence): misc cleanup - remove unused `export`s - remove extraneous `@system` tag for internal type - cleanup `IEphemeralRuntime`   - make `logger` required in even under test (clean up nullable logger usage)   - correct `submitSignal` docs and use protocol union - address `@typescript-eslint/prefer-nullish-coalescing` suppression (the suppression comment was wrong) - suppress `unicorn/prefer-structured-clone` in test where action is filter and not exact clone

--- packages/framework/presence/src/presenceManager.ts
+++ packages/framework/presence/src/presenceManager.ts
@@ -86,23 +86,20 @@ class PresenceManager implements Presence, PresenceExtensionInterface {
 			this.datastoreManager.getWorkspace(`n:${workspaceAddress}`, requestedContent),
 	};
 
-	private readonly mc: MonitoringContext | undefined = undefined;
+	private readonly mc: MonitoringContext;
 
 	public constructor(
 		private readonly runtime: IEphemeralRuntime,
 		attendeeId: AttendeeId,
 	) {
-		const logger = runtime.logger;
-		if (logger) {
-			this.mc = createChildMonitoringContext({ logger, namespace: "Presence" });
-			this.mc.logger.sendTelemetryEvent({ eventName: "PresenceInstantiated" });
-		}
+		this.mc = createChildMonitoringContext({ logger: runtime.logger, namespace: "Presence" });
+		this.mc.logger.sendTelemetryEvent({ eventName: "PresenceInstantiated" });
 
 		[this.datastoreManager, this.systemWorkspace] = setupSubComponents(
 			attendeeId,
 			runtime,
 			this.events,
-			this.mc?.logger,
+			this.mc.logger,
 			this,
 		);
 		this.attendees = this.systemWorkspace;
@@ -222,7 +219,7 @@ function setupSubComponents(
 	runtime: IEphemeralRuntime,
 	events: Listenable<PresenceEvents & AttendeesEvents> &
 		IEmitter<PresenceEvents & AttendeesEvents>,
-	logger: ITelemetryLoggerExt | undefined,
+	logger: ITelemetryLoggerExt,
 	presence: Presence,
 ): [PresenceDatastoreManager, SystemWorkspace] {
 	const systemWorkspaceDatastore: SystemWorkspaceDatastore = {
