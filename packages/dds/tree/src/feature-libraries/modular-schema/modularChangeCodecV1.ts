50b7a8fa7440f2dadf82f7d620ad5567aa794bc0
Tyler Butler <tylerbu@microsoft.com>
2026-01-29 10:59:00 -0800

    fix(tree): improve eslint-disable comment for prefer-optional-chain
    Update the comment to explain why optional chaining cannot be used:
    it would change behavior since `revisions[0]?.rollbackOf === undefined`
    is true when revisions[0] is undefined. Also restore dropped blank line.

--- packages/dds/tree/src/feature-libraries/modular-schema/modularChangeCodecV1.ts
+++ packages/dds/tree/src/feature-libraries/modular-schema/modularChangeCodecV1.ts
@@ -396,13 +396,14 @@ export function encodeRevisionInfos(
 ): EncodedRevisionInfo[] | undefined {
 	if (context.revision !== undefined) {
 		assert(
-			// eslint-disable-next-line @typescript-eslint/prefer-optional-chain -- TODO: ADO#58522 Code owners should verify if this code change is safe and make it if so or update this comment otherwise
+			// eslint-disable-next-line @typescript-eslint/prefer-optional-chain -- Using optional chaining here would change behavior: `revisions[0]?.rollbackOf === undefined` is true when revisions[0] is undefined, but this check requires revisions[0] to be defined. As currently written, such a change would be safe because context.revision is included in the check and from a couple lines above is confirmed not undefined. But this more verbose form is clearer.
 			revisions.length === 1 &&
 				revisions[0] !== undefined &&
 				revisions[0].revision === context.revision &&
 				revisions[0].rollbackOf === undefined,
 			0x964 /* A tagged change should only contain the tagged revision */,
 		);
+
 		return undefined;
 	}
 
