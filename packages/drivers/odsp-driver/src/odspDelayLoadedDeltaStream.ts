7f81b13b276d819c84122c32e582c55754a20d1b
Steffen Loesch <5918596+steffenloesch@users.noreply.github.com>
2026-01-29 10:33:16 +0100

    Improve assert and re-generate assertion short codes

--- packages/drivers/odsp-driver/src/odspDelayLoadedDeltaStream.ts
+++ packages/drivers/odsp-driver/src/odspDelayLoadedDeltaStream.ts
@@ -513,7 +513,10 @@ export class OdspDelayLoadedDeltaStream {
 		sensitivityLabelsInfo: ISensitivityLabelsInfo,
 	): void {
 		const createdTimestamp = sensitivityLabelsInfo.timestamp;
-		assert(createdTimestamp > 0, 0x8e0 /* time should be positive */);
+		assert(
+			typeof createdTimestamp === "number" && createdTimestamp > 0,
+			0x8e0 /* time should be a positive number */,
+		);
 		if (createdTimestamp > this.labelUpdateTimestamp) {
 			this.labelUpdateTimestamp = createdTimestamp;
 			this.metadataUpdateHandler({
