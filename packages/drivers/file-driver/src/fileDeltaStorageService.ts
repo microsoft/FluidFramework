a12729c3c817a77fc296c93e6254691275d930ff
Tyler Butler <tylerbu@microsoft.com>
2026-02-04 11:23:30 -0800

    chore(lint): remove obsolete eslint-disable comments (drivers, loader, runtime, test)
    Remove obsolete eslint-disable comments from drivers, framework (misc), loader,
    runtime, service-clients, test packages, and tools (misc).
    
    These comments suppressed rules that no longer apply or were removed from the
    active ruleset.

--- packages/drivers/file-driver/src/fileDeltaStorageService.ts
+++ packages/drivers/file-driver/src/fileDeltaStorageService.ts
@@ -24,7 +24,7 @@ export class FileDeltaStorageService implements IDocumentDeltaStorageService {
 	constructor(private readonly path: string) {
 		this.messages = [];
 		let counter = 0;
-		// eslint-disable-next-line no-constant-condition
+
 		while (true) {
 			const filename = `${this.path}//messages${counter === 0 ? "" : counter}.json`;
 			if (!fs.existsSync(filename)) {
