759c8486b390358704311d8e6a5f23d400f2d8d8
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2026-02-10 22:45:36 +0000

    Add --if-present

--- build-tools/packages/build-cli/src/library/package.ts
+++ build-tools/packages/build-cli/src/library/package.ts
@@ -533,7 +533,9 @@ export async function setVersion(
 				],
 				options,
 			],
-			["pnpm", ["-r", "run", "build:genver"], options],
+			// If no packages use `packageVersion.ts` then none will have the build:genver script,
+			// so use --if-present to avoid errors in that case.
+			["pnpm", ["-r", "--if-present", "run", "build:genver"], options],
 		);
 	} else {
 		options = {
