583a96c3d44fa47095a0858d7da981afc574f64e
Tyler Butler <tylerbu@microsoft.com>
2026-02-19 16:33:04 -0800

    deprecate(dds): mark DDS implementation classes as deprecated (#26501)
    ## Summary
    
    Deprecate DDS implementation classes on the legacy API surface as part
    of the custom DDS removal effort:
    
    - `ConsensusRegisterCollectionClass` in
    `@fluidframework/register-collection`
    - `ConsensusOrderedCollection` in `@fluidframework/ordered-collection`
    - `ConsensusQueueClass` in `@fluidframework/ordered-collection`
    
    These classes expose internal implementation details that consumers
    should not depend on. Consumers should use the corresponding interfaces
    (`IConsensusRegisterCollection`, `IConsensusOrderedCollection`) for
    typing and factory/singleton patterns for instance creation.
    
    ## What changed
    
    - Added `@deprecated` TSDoc tags to all three implementation classes
    - Added `eslint-disable` comments for `import-x/no-deprecated` on
    internal usages (factories and subclasses)
    - Regenerated API reports reflecting the `@deprecated` annotations
    - Added changeset

--- packages/dds/ordered-collection/src/consensusQueue.ts
+++ packages/dds/ordered-collection/src/consensusQueue.ts
@@ -8,6 +8,7 @@ import type {
 	IFluidDataStoreRuntime,
 } from "@fluidframework/datastore-definitions/internal";
 
+// eslint-disable-next-line import-x/no-deprecated -- Subclass of deprecated class
 import { ConsensusOrderedCollection } from "./consensusOrderedCollection.js";
 import type { IOrderedCollection } from "./interfaces.js";
 import { SnapshotableArray } from "./snapshotableArray.js";
@@ -32,10 +33,11 @@ class SnapshotableQueue<T> extends SnapshotableArray<T> implements IOrderedColle
  * Implementation of a consensus stack
  *
  * An derived type of ConsensusOrderedCollection with a queue as the backing data and order.
+ * @deprecated Use the `ConsensusQueue` singleton and {@link IConsensusOrderedCollection} for typing. This implementation class will be removed in a future release.
  * @legacy @beta
  */
 // TODO: #22835 Use undefined instead of any (breaking change)
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, import-x/no-deprecated
 export class ConsensusQueueClass<T = any> extends ConsensusOrderedCollection<T> {
 	/**
 	 * Constructs a new consensus queue. If the object is non-local an id and service interfaces will
