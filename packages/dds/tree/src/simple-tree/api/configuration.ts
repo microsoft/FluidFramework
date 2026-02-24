ac4ee0a47e58f6e52ae1fdc82048e652f5f6484c
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2026-02-24 20:51:08 +0000

    Recommend snapshotSchemaCompatibility in more places, and related docs tweaks

--- packages/dds/tree/src/simple-tree/api/configuration.ts
+++ packages/dds/tree/src/simple-tree/api/configuration.ts
@@ -147,6 +147,10 @@ export interface ITreeViewConfiguration<
 > extends ITreeConfigurationOptions {
 	/**
 	 * The schema which the application wants to view the tree with.
+	 * @remarks
+	 * Use {@link SchemaFactory} to construct this the schema.
+	 * Changes to this schema between different versions have important compatibility implications.
+	 * See the {@link https://fluidframework.com/docs/data-structures/tree/schema-evolution | documentation on schema evolution} for more details.
 	 */
 	readonly schema: TSchema;
 }
