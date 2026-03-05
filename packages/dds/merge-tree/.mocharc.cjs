197bc7c699d028fb70978743440de68b44fa08fb
Ji Kim <111468570+jikim-msft@users.noreply.github.com>
2026-03-05 09:26:42 -0800

    Revert "ci: parallelize merge-tree mocha tests" (#26653)
    Reverts microsoft/FluidFramework#26624
    
    [Revert
    Reason](https://github.com/microsoft/FluidFramework/pull/26624#issuecomment-4001332988)
    
    [Investigatation](https://github.com/microsoft/FluidFramework/pull/26624#issuecomment-4001371468)

--- packages/dds/merge-tree/.mocharc.cjs
+++ packages/dds/merge-tree/.mocharc.cjs
@@ -8,12 +8,4 @@
 const getFluidTestMochaConfig = require("@fluid-internal/mocha-test-setup/mocharc-common");
 
 const config = getFluidTestMochaConfig(__dirname);
-// This test suite is the slowest on CI if not run in parallel, so this parallelization speeds up CI significantly.
-config.parallel = true;
-// CI assumes that each package will use one thread when computing how many package's tests to run in parallel.
-// To avoid making this computation too inaccurate and slowing things down with excessive memory use
-// (and other overhead) from too many threads, we keep the number of jobs limited,
-// and only enable this parallelization for packages where doing so actually produces an improvement in CI time.
-// 4 jobs was measured to give most of the speed up, even when just running this suite, while not slowing down CI.
-config.jobs = 4;
 module.exports = config;
