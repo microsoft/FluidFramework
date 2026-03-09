5a544d2fbc973075a70fa8ae78676369de00a5f5
Tyler Butler <tylerbu@microsoft.com>
2026-03-09 15:16:51 -0700

    playwright

--- scripts/codespace-setup/ai-setup.sh
+++ scripts/codespace-setup/ai-setup.sh
@@ -1,4 +1,9 @@
+# TODO: This needs to be somewhere it'll be invoked on every shell start, not just when the codespace is initially
+# created.
 source agent-aliases.sh
+
+sh ./playwright-setup.sh
 
 echo "Installing Agency, which will require you to authenticate, so look for a popup window..."
 curl -sSfL https://aka.ms/InstallTool.sh | sh -s agency && exec $SHELL -l
+echo "Agency installed and authenticated."
