ef0e58946a72889cefd7f23aac3be7f60e478fb3
Willie Habimana <whabimana@microsoft.com>
2026-03-16 13:05:15 -0700

    Clarify internal.ts comment scope

--- packages/dds/tree/src/entrypoints/internal.ts
+++ packages/dds/tree/src/entrypoints/internal.ts
@@ -4,7 +4,7 @@
  */
 
 // This file provides one module of indirection from index.ts to control
-// TypeScript import spec generation for expression declarations. Importantly,
+// TypeScript import spec generation in consumer packages' .d.ts output. Importantly,
 // if alpha / beta / public entrypoints need to search to import spec and those
 // reference index.js, then package exports should not also reference index.js,
 // or that path will be chosen. (import("@fluidframework/tree/internal") in this
