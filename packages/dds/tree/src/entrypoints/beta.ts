f5782c24d1bb1245b2746f47750cf9242ea801e0
daesunp <138815173+daesunp@users.noreply.github.com>
2026-03-19 17:21:00 +0000

    update entrypoint

--- packages/dds/tree/src/entrypoints/beta.ts
+++ packages/dds/tree/src/entrypoints/beta.ts
@@ -102,6 +102,7 @@ export {
 	AnnotatedAllowedTypeUnsafe, 
 	AnnotatedAllowedTypes, 
 	AnnotatedAllowedTypesUnsafe, 
+	ArrayNodeDeltaOp, 
 	CodecWriteOptionsBeta, 
 	ConciseTree, 
 	ExtensibleUnionNode, 
