6e8eaa417ce5abe2ca96a7a1c1fbdd82ad75bb13
daesunp <138815173+daesunp@users.noreply.github.com>
2026-03-25 22:13:48 +0000

    lint fix

--- packages/dds/tree/src/core/tree/anchorSet.ts
+++ packages/dds/tree/src/core/tree/anchorSet.ts
@@ -972,10 +972,7 @@ export class AnchorSet implements AnchorLocator {
 				this.parentField = undefined;
 			},
 			fieldMarks(marks: readonly Delta.Mark[]): void {
-				assert(
-					this.parentField !== undefined,
-					"fieldMarks called outside of a field",
-				);
+				assert(this.parentField !== undefined, "fieldMarks called outside of a field");
 				if (this.parent !== undefined) {
 					const interned = this.anchorSet.internalizePath(this.parent);
 					if (interned instanceof PathNode) {
