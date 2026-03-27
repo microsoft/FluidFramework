d12caeb4cbf53edd6452482dcf6d041d5abcde93
Justin Myhres <6990048+jumyhre@users.noreply.github.com>
2026-03-27 09:32:42 -0700

    Update packages/dds/tree/src/core/rebase/changeRebaser.ts
    Co-authored-by: alex-pardes <105307164+alex-pardes@users.noreply.github.com>

--- packages/dds/tree/src/core/rebase/changeRebaser.ts
+++ packages/dds/tree/src/core/rebase/changeRebaser.ts
@@ -84,7 +84,7 @@ export interface ChangeRebaser<TChangeset> {
 	 * - `rebase(compose([]), a)` is equal to `compose([])`.
 	 *
 	 * @param ignoreNoChangeViolation - When true, no-change constraint violations are suppressed during rebase.
-	 * This is a kludge to support undo/redo flows where an inverse is rebased over newer commits that have no net
+	 * This is a kludge to support undo/redo flows where an undo is rebased over newer commits that have no net
 	 * effect on the document state (e.g. an edit plus its undo). We want the rebaser to treat no-change constraints
 	 * as violated only if the sequence of changes rebased over leave the document in some meaningfully different state.
 	 * Because implementing that rebasing logic in full is more complex, this flag is a hack to manually get
