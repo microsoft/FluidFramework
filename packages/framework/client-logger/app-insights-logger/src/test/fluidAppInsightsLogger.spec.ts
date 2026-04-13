72aae30330bdcfb06d1c53872e5fd48db42254e3
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2026-04-13 14:40:44 -0500

    Fix formatting

--- packages/framework/client-logger/app-insights-logger/src/test/fluidAppInsightsLogger.spec.ts
+++ packages/framework/client-logger/app-insights-logger/src/test/fluidAppInsightsLogger.spec.ts
@@ -30,7 +30,6 @@ describe("FluidAppInsightsLogger", () => {
 		trackEventSpy = spy(appInsightsClient, "trackEvent");
 	});
 
-
 	// AB#69025 - fix the flakiness for this test, if we don't sunset the feature/package before that
 	it.skip("send() routes telemetry events to ApplicationInsights.trackEvent", () => {
 		const logger = createLogger(appInsightsClient);
