776e301e5b260868838cf0398e29ebb82fb3e72d
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2026-04-16 22:02:00 +0000

    Improve restore of MergeTree.options.incrementalUpdate

--- packages/dds/merge-tree/src/test/PartialLengths.perf.spec.ts
+++ packages/dds/merge-tree/src/test/PartialLengths.perf.spec.ts
@@ -12,72 +12,75 @@ import type { OperationStamp } from "../stamps.js";
 import { TextSegment } from "../textSegment.js";
 
 describe("MergeTree partial lengths", () => {
-	const originalIncrementalUpdate: boolean = MergeTree.options.incrementalUpdate;
-
 	for (const incremental of [true, false]) {
 		benchmarkIt({
 			type: BenchmarkType.Measurement,
 			title: `incremental updates = ${incremental}`,
 			category: "partial lengths",
 			run: async () => {
+				const originalIncrementalUpdate: boolean = MergeTree.options.incrementalUpdate;
 				MergeTree.options.incrementalUpdate = incremental;
-				const result = await collectDurationData({
-					benchmarkFn: () => {
-						const mergeTree = new MergeTree();
+				try {
+					return await collectDurationData({
+						benchmarkFn: () => {
+							const mergeTree = new MergeTree();
 
-						const clientId = 0;
-						let i = 1;
-						for (; i < 1001; i++) {
-							const stamp: OperationStamp = {
-								seq: i,
-								clientId,
-							};
-							mergeTree.insertSegments(
-								0,
-								[TextSegment.make("a")],
-								new PriorPerspective(i, clientId),
-								stamp,
-								{
-									op: { type: MergeTreeDeltaType.INSERT },
-								},
-							);
-						}
+							const clientId = 0;
+							let i = 1;
+							for (; i < 1001; i++) {
+								const stamp: OperationStamp = {
+									seq: i,
+									clientId,
+								};
+								mergeTree.insertSegments(
+									0,
+									[TextSegment.make("a")],
+									new PriorPerspective(i, clientId),
+									stamp,
+									{
+										op: { type: MergeTreeDeltaType.INSERT },
+									},
+								);
+							}
 
-						for (; i < 2001; i++) {
-							const stamp: OperationStamp = {
-								seq: i,
-								clientId,
-							};
-							mergeTree.markRangeRemoved(
-								i - 1001,
-								i - 1000,
-								new PriorPerspective(i, clientId),
-								stamp,
-								{
-									op: { type: MergeTreeDeltaType.REMOVE },
-								},
-							);
-						}
+							for (; i < 2001; i++) {
+								const stamp: OperationStamp = {
+									seq: i,
+									clientId,
+								};
+								mergeTree.markRangeRemoved(
+									i - 1001,
+									i - 1000,
+									new PriorPerspective(i, clientId),
+									stamp,
+									{
+										op: { type: MergeTreeDeltaType.REMOVE },
+									},
+								);
+							}
 
-						for (; i < 3001; i++) {
-							const stamp: OperationStamp = {
-								seq: i,
-								clientId,
-							};
-							mergeTree.insertSegments(
-								0,
-								[TextSegment.make("a")],
-								new PriorPerspective(i, clientId),
-								stamp,
-								{
-									op: { type: MergeTreeDeltaType.INSERT },
-								},
-							);
-						}
-					},
-				});
-				MergeTree.options.incrementalUpdate = originalIncrementalUpdate;
-				return result;
+							for (; i < 3001; i++) {
+								const stamp: OperationStamp = {
+									seq: i,
+									clientId,
+								};
+								mergeTree.insertSegments(
+									0,
+									[TextSegment.make("a")],
+									new PriorPerspective(i, clientId),
+									stamp,
+									{
+										op: { type: MergeTreeDeltaType.INSERT },
+									},
+								);
+							}
+						},
+					});
+				} finally {
+					// It is unclear why this lint rule is getting triggered here, but this logic seems correct and necessary.
+					// eslint-disable-next-line require-atomic-updates
+					MergeTree.options.incrementalUpdate = originalIncrementalUpdate;
+				}
 			},
 		});
 	}
