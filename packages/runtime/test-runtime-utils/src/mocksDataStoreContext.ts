5fc4f73272ab12151fd24c360c099dbfd2d79ce4
Sonali Deshpande <sdeshpande@microsoft.com>
2026-04-24 12:30:37 -0500

    fix policy check

--- packages/runtime/test-runtime-utils/src/mocksDataStoreContext.ts
+++ packages/runtime/test-runtime-utils/src/mocksDataStoreContext.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import { EventEmitter } from "@fluid-internal/client-utils";
 import { AttachState, IAudience } from "@fluidframework/container-definitions";
 import { IDeltaManager } from "@fluidframework/container-definitions/internal";
