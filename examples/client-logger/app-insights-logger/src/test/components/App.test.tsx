54f836c805250f8a0364c46b9aa898bb0fd153a2
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2026-04-29 21:22:57 +0000

    fix(test): Cleanup

--- examples/client-logger/app-insights-logger/src/test/components/App.test.tsx
+++ examples/client-logger/app-insights-logger/src/test/components/App.test.tsx
@@ -3,11 +3,17 @@
  * Licensed under the MIT License.
  */
 
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 
 import { App } from "../../components/index.js";
 
+
+
 describe("App Insights Example App UI test", () => {
+	afterEach(() => {
+	cleanup();
+});
+
 	it("App renders", async (): Promise<void> => {
 		render(<App />);
 		await screen.findByText("Loading Shared container...");
