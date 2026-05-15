f4e5b3804502633665add6c90d1441b7a21b13f5
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2026-05-15 00:04:35 +0000

    test: Combine test suites

--- packages/dds/tree/src/test/shared-tree/tableTree.bench.ts
+++ packages/dds/tree/src/test/shared-tree/tableTree.bench.ts
@@ -8,4 +8,5 @@ import { runBenchmarkTestSuite } from "../tableTreeBenchmarks.js";
 // TODO: AB#71782: Investigate why these tests are so slow / possibly contain cross-test contamination and address those issue, then re-enable these tests.
 describe.skip("TableSchema Benchmarks", () => {
 	runBenchmarkTestSuite("execution-time");
+	runBenchmarkTestSuite("memory");
 });
