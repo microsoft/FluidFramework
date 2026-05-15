380ffdbbcfdbe9384a55ccf8eebdd05c366298f9
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2026-05-15 00:02:18 +0000

    docs: Restore TODOs

--- packages/dds/tree/src/test/memory/tableTree.spec.ts
+++ packages/dds/tree/src/test/memory/tableTree.spec.ts
@@ -5,6 +5,7 @@
 
 import { runBenchmarkTestSuite } from "../tableTreeBenchmarks.js";
 
+// TODO: AB#71782: Investigate why these tests are so slow / possibly contain cross-test contamination and address those issue, then re-enable these tests.
 describe.skip("TableSchema Benchmarks", () => {
 	runBenchmarkTestSuite("memory");
 });
