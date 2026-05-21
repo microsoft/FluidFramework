db4b5a5a4b3e2494703cc6dc0fa32b0385acd9e3
Tyler Butler <tylerbu@microsoft.com>
2026-05-21 08:06:58 -0700

    policy

--- build-tools/packages/build-tools/bin/fluid-build.mjs
+++ build-tools/packages/build-tools/bin/fluid-build.mjs
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import "../dist/fluidBuild/fluidBuild.js";
