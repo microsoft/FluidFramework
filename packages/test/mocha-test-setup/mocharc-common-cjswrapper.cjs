26a3c2dca2d26d451eed5389764a1ada9191a123
Jason Hartman <jasonha@microsoft.com>
2026-05-27 10:32:51 -0700

    test(client-mocha-test-setup): fix attw verification Exclude Node10 check since mocharc-common.cjs was renamed to prevent confusion that it is always the import path.

--- packages/test/mocha-test-setup/mocharc-common-cjswrapper.cjs
+++ packages/test/mocha-test-setup/mocharc-common-cjswrapper.cjs
@@ -3,6 +3,10 @@
  * Licensed under the MIT License.
  */
 
+// This file exists to preserve CommonJS default export compatibility for the mocharc-common entrypoint.
+// Once all consumers are using named imports, this file can be removed and all conditions can point
+// directly to the ESM file. attw mocharc-common exclusion can also be removed at that time.
+
 "use strict";
 
 const { getFluidTestMochaConfig } = require("./lib/mocharcCommon.js");
