c9d0075536726fbd546101109b98a6b5ee77fd62
alex-pardes <105307164+alex-pardes@users.noreply.github.com>
2026-05-28 22:22:27 +0000

    Update comment

--- packages/dds/tree/src/feature-libraries/optional-field/optionalFieldChangeTypes.ts
+++ packages/dds/tree/src/feature-libraries/optional-field/optionalFieldChangeTypes.ts
@@ -48,18 +48,12 @@ export interface Replace {
 	/**
 	 * The ID for the node to attach in this field, or undefined if no node should be attached.
 	 *
-	 * Note that this ID may refer to the node detached by this changeset:
-	 * The node in the field is detached and associated with ID `dst`,
-	 * then that node is renamed from `dst` to `src` (at the ModularChangeset level),
-	 * then that node is attached back into the field.
+	 * Note that this ID may refer to the node detached by this OptionalChangeset.
 	 * When this is the case, we say that the node is being pinned in the field.
 	 * While a pin has no effect on the document state if applied as-is, its merge semantics are different from an empty changeset.
 	 * This is because rebasing a pin over a change that detaches the node from the field will result in a changeset that re-attaches the node to the field.
 	 * In other words, a pin guarantees that the node will be in the field after the changeset is rebased and applied, whereas an empty changeset does not.
-	 *
-	 * `src` must be structurally different from `dst` even when the changeset is a pin.
-	 * An optional field changeset that aims to attach a node must be able to affect two nodes,
-	 * so unless we want to mint new IDs during rebasing, such a changeset must have two different IDs.
+	 * The detach portion of an optional field pin should always be represented using `nodeDetach`, rather than `valueReplace.dst`.
 	 */
 	readonly src?: ChangeAtomId;
 }
