b0baa18718d6deb63aa8ad0c9ec3417f16fabce7
brrichards <brrichards@microsoft.com>
2026-06-30 16:56:10 +0000

    test(text-editor): pass devtoolsProps to App in render test

--- examples/data-objects/text-editor/src/test/app.test.tsx
+++ examples/data-objects/text-editor/src/test/app.test.tsx
@@ -46,6 +46,7 @@ describe("app", () => {
 			<App
 				containerId="test"
 				devtoolsLogger={createDevtoolsLogger()}
+				devtoolsProps={{ logger: createDevtoolsLogger(), initialContainers: [] }}
 				initialUsers={[createUserView(1, "Text A"), createUserView(2, "Text B")]}
 			/>
 		);
