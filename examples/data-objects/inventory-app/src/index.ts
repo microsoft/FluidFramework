6eb95835cf8b3a55b37bbe5e27577bf26ba47117
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2026-08-05 16:36:18 +0000

    Factor out example helpers

--- examples/data-objects/inventory-app/src/index.ts
+++ examples/data-objects/inventory-app/src/index.ts
@@ -4,89 +4,13 @@
  */
 
 import { toPropTreeNode } from "@fluidframework/react/alpha";
-import { createTinyliciousServiceClient } from "@fluidframework/tinylicious-driver/alpha";
-import { startEphemeralService } from "@fluidframework/local-driver/alpha";
-import type {
-	DataStoreKind,
-	FluidContainer,
-	ServiceClient,
-	ServiceOptions,
-} from "fluid-framework/alpha";
+import { loadExampleDataStore, renderRoot } from "@fluid-example/example-utils";
 import { createElement } from "react";
-// eslint-disable-next-line import-x/no-internal-modules
-import { createRoot } from "react-dom/client";
 
 import { inventoryDataStoreKind } from "./inventoryList.js";
 import type { Inventory } from "./schema.js";
 import { MainView } from "./view/index.js";
 
-const serviceOptions = {
-	minVersionForCollaboration: "2.100.0",
-} as const;
-
-/**
- * Configures a service client based on the `fluidClient` URL query parameter
- * and our defaults used by examples.
- */
-function getExampleServiceClient(options: ServiceOptions): ServiceClient {
-	const fluidClient = new URLSearchParams(location.search).get("fluidClient") ?? "ephemeral";
-	switch (fluidClient) {
-		case "tinylicious": {
-			return createTinyliciousServiceClient(options);
-		}
-		default: {
-			console.warn(
-				`Unknown fluidClient value: ${JSON.stringify(fluidClient)}, falling back to ephemeral service.`,
-			);
-		}
-		case "ephemeral": {
-			return startEphemeralService().newClient(options);
-		}
-	}
-}
-
-/**
- * Create or load a container based on the current location hash.
- * @privateRemarks
- * This could return `FluidContainerAttached`, but it doesn't have to as only this code needs the container id.
- * Choosing not to expose the `FluidContainerAttached` here keeps the types simpler,
- * and also makes it easier in the future to make this example setup support detached containers and delayed attach scenarios.
- *
- * For simplicity this only supports the `DataStoreKind` overload, and no full `DataStoreRegistry`:
- * this could easily be changed if needed.
- * TODO: We should adjust the container API surface (or add helpers) to make it easier to support both patterns in wrappers.
- */
-async function loadExampleContainer<T>(
-	client: ServiceClient,
-	rootStore: DataStoreKind<T>,
-): Promise<FluidContainer<T>> {
-	const id = location.hash.slice(1);
-	if (id.length > 0) {
-		return client.loadContainer(id, rootStore);
-	} else {
-		const container = await client.createContainer(rootStore);
-		const attachedInner = await container.attach();
-		// eslint-disable-next-line require-atomic-updates -- this example setup assumes this function controls the location hash.
-		location.hash = attachedInner.id;
-		return attachedInner;
-	}
-}
-
-/**
- * A simple opinionated default for loading an example data store as the root of a container configured by the location hash and
- * the `fluidClient` URL query parameter.
- */
-async function loadExampleDataStore<T>(rootStore: DataStoreKind<T>): Promise<T> {
-	const service = getExampleServiceClient(serviceOptions);
-	const container = await loadExampleContainer(service, rootStore);
-	return container.data;
-}
-
 const view = await loadExampleDataStore(inventoryDataStoreKind);
 const root: Inventory = view.root;
-
-const rootEl = document.querySelector("#content");
-if (rootEl === null) {
-	throw new Error("No #content element found");
-}
-createRoot(rootEl).render(createElement(MainView, { root: toPropTreeNode(root) }));
+renderRoot(createElement(MainView, { root: toPropTreeNode(root) }));
