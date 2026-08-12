microsoft/FluidFramework

commit 85691a066df19be69000cc2844b27446c316ca48

MergeTree: Revertibles and Removed Segment Tracking via Local Reference (#11899)
## Description

This change introduces Revertibles to the Merge-Tree package. This
co-locates the core revert logic, which can be used to build undo redo
in the merge tree package. This improves testability, and reduces
concept leakage from the package, as would be necessary to implement
elsewhere.

The primary change here is in how we track and revert removed segments.
Previously it was done via tracking the deleted segments, however this
only worked due to bugs in the handling of removed segments and had
negative impacts on the tree itself in terms of fragmentation. For now,
the previous way of tracking still works to give people time to move to
the new way. Additionally, the new way requires bug fixes currently
under an option (mergeTreeUseNewLengthCalculations), which also breaks
the old way, so staging the release of this will need to be done
carefully.

The primary change in this review is contained in the revertibles file.
For insert and remove the code aligns with the existing undo redo
implementation. For remove it is substantially different. The primary
functions for remove are appendLocalRemoveToRevertibles and
revertLocalRemove. All other changes are in support of the remove
function, or tests for revertibles in general, we were severely lacking.
The most thorough comes from revertibles.farm.spec.ts. There are simple
smoke tests for add, remove, and annotate is both merge tree, and
sequence, along with regression test for issues found with
revertibles.farm.spec.ts.

## Remove Revertible and Local References

As before, the append functions are event based. The primary difference
in the new append for remove is rather than creating a tracking group on
the removed segment itself, it adds a sliding local reference to the
segment. The local reference contains two properties, 1) the serialized

packages/dds/merge-tree/src/localReference.ts
@@ -5,7 +5,7 @@
 
 import { assert } from "@fluidframework/common-utils";
 import { UsageError } from "@fluidframework/container-utils";
-import { List, ListNode } from "./collections";
+import { List, ListNode, walkList } from "./collections";
 import {
     ISegment,
 } from "./mergeTreeNodes";
@@ -22,7 +22,7 @@ import {
 /**
  * @internal
  */
-export function _validateReferenceType(refType: ReferenceType) {
+function _validateReferenceType(refType: ReferenceType) {
     let exclusiveCount = 0;
     if (refTypeIncludesFlag(refType, ReferenceType.Transient)) {
         ++exclusiveCount;
@@ -42,7 +42,7 @@ export function _validateReferenceType(refType: ReferenceType) {
  * @sealed
  */
 export interface LocalReferencePosition extends ReferencePosition {
-    callbacks?: Partial<Record<"beforeSlide" | "afterSlide", () => void>>;
+    callbacks?: Partial<Record<"beforeSlide" | "afterSlide", (ref: LocalReferencePosition) => void>>;
     readonly trackingCollection: TrackingGroupCollection;
 }
 
@@ -57,7 +57,7 @@ class LocalReference implements LocalReferencePosition {
     private offset: number = 0;
     private listNode: ListNode<LocalReference> | undefined;
 
-    public callbacks?: Partial<Record<"beforeSlide" | "afterSlide", () => void>> | undefined;
+    public callbacks?: Partial<Record<"beforeSlide" | "afterSlide", (ref: LocalReferencePosition) => void>> | undefined;
     private _trackingCollection?: TrackingGroupCollection;
     public get trackingCollection(): TrackingGroupCollection {
         return (this._trackingCollection ??= new TrackingGroupCollection(this));
@@ -273,7 +273,6 @@ export class LocalReferenceCollection {
     public addLocalRef(lref: LocalReferencePosition, offset: number) {
         assertLocalReferences(lref);
         assert(offset < this.segment.cachedLength, 0x348 /* offset cannot be beyond segment length */);
-
         if (refTypeIncludesFlag(lref, ReferenceType.Transient)) {
             lref.link(this.segment, offset, undefined);
         } else {
@@ -305,7 +304,7 @@ export class LocalReferenceCollection {
             node?.list?.remove(node);
 
             lref.link(
-                undefined,
+                lref.getSegment(),
                 lref.getOffset(),
                 undefined);
             if (refHasRangeLabels(lref) || refHasTileLabels(lref)) {
@@ -416,6 +415,10 @@ export class LocalReferenceCollection {
         }
     }
 
+    /**
+    * @remarks This method should only be called by mergeTree.
+    * @internal
+    */
     public addBeforeTombstones(...refs: Iterable<LocalReferencePosition>[]) {
         const beforeRefs = this.refsByOffset[0]?.before ?? new List();
 
@@ -424,25 +427,33 @@ export class LocalReferenceCollection {
             refsAtOffset.before ??= beforeRefs;
         }
 
+        let precedingRef: ListNode<LocalReference> | undefined;
         for (const iterable of refs) {
             for (const lref of iterable) {
                 assertLocalReferences(lref);
                 if (refTypeIncludesFlag(lref, ReferenceType.SlideOnRemove)) {
-                    lref.callbacks?.beforeSlide?.();
-                    beforeRefs.unshift(lref);
-                    lref.link(this.segment, 0, beforeRefs.first);
+                    lref.callbacks?.beforeSlide?.(lref);
+                    if (precedingRef === undefined) {
+                        precedingRef = beforeRefs.unshift(lref)?.first;
+                    } else {
+                        precedingRef = beforeRefs.insertAfter(precedingRef, lref)?.first;
+                    }
+                    lref.link(this.segment, 0, precedingRef);
                     if (refHasRangeLabels(lref) || refHasTileLabels(lref)) {
                         this.hierRefCount++;
                     }
                     this.refCount++;
-                    lref.callbacks?.afterSlide?.();
+                    lref.callbacks?.afterSlide?.(lref);
                 } else {
                     lref.link(undefined, 0, undefined);
                 }
             }
         }
     }
-
+    /**
+    * @remarks This method should only be called by mergeTree.
+    * @internal
+    */
     public addAfterTombstones(...refs: Iterable<LocalReferencePosition>[]) {
         const lastOffset = this.segment.cachedLength - 1;
         const afterRefs = this.refsByOffset[lastOffset]?.after ?? new List();
@@ -456,18 +467,99 @@ export class LocalReferenceCollection {
             for (const lref of iterable) {
                 assertLocalReferences(lref);
                 if (refTypeIncludesFlag(lref, ReferenceType.SlideOnRemove)) {
-                    lref.callbacks?.beforeSlide?.();
+                    lref.callbacks?.beforeSlide?.(lref);
                     afterRefs.push(lref);
                     lref.link(this.segment, lastOffset, afterRefs.last);
                     if (refHasRangeLabels(lref) || refHasTileLabels(lref)) {
                         this.hierRefCount++;
                     }
                     this.refCount++;
-                    lref.callbacks?.afterSlide?.();
+                    lref.callbacks?.afterSlide?.(lref);
                 } else {
                     lref.link(undefined, 0, undefined);
                 }
             }
         }
+    }
+
+    /**
+    * @remarks This method should only be called by mergeTree.
+    * @internal
+    */
+     public isAfterTombstone(lref: LocalReferencePosition) {
+        const after = this.refsByOffset[lref.getOffset()]?.after;
+        if (after) {
+            assertLocalReferences(lref);
+            return after.includes(lref.getListNode());
+        }
+        return false;
+    }
+
+    /**
+    * @remarks This method should only be called by mergeTree.
+    * @internal
+    */
+    public walkReferences(
+        visitor: (lref: LocalReferencePosition) => boolean | void | undefined,
+        start?: LocalReferencePosition,
+        forward: boolean = true) {
+        if (start !== undefined) {
+            if (!this.has(start)) {
+                throw new UsageError("start must be in collection");
+            }
+            assertLocalReferences(start);
+        }
+        let offset = start?.getOffset() ?? (forward
+            ? 0
+            : this.segment.cachedLength - 1);
+
+        const offsetPositions: List<IRefsAtOffset[keyof IRefsAtOffset]> = new List();
+            offsetPositions.push(
+                this.refsByOffset[offset]?.before,
+                this.refsByOffset[offset]?.at,
+                this.refsByOffset[offset]?.after);
+
+        const startNode = start?.getListNode();
+        const startList = startNode?.list;
+
+        if (startList !== undefined) {
+            if (forward) {
+                while (!offsetPositions.empty && offsetPositions.first !== startNode) {
+                    offsetPositions.shift();
+                }
+            } else {
+                while (!offsetPositions.empty && offsetPositions.last !== startNode) {
+                    offsetPositions.pop();
+                }
+            }
+        }
+
+        const listWalker = (pos: List<LocalReference>) => {
+            return walkList(
+                pos,
+                (node) => visitor(node.data),
+                startList === pos ? startNode : undefined,
+                forward,
+            );
+        };
+        const increment = forward ? 1 : -1;
+        while (offset >= 0 && offset < this.refsByOffset.length) {
+            while (offsetPositions.length > 0) {
+                const offsetPos = forward
+                    ? offsetPositions.shift()
+                    : offsetPositions.pop();
+                if (offsetPos?.data !== undefined) {
+                    if (listWalker(offsetPos.data) === false) {
+                        return false;
+                    }
+                }
+           }
+            offset += increment;
+            offsetPositions.push(
+                this.refsByOffset[offset]?.before,
+                this.refsByOffset[offset]?.at,
+                this.refsByOffset[offset]?.after);
+        }
+        return true;
     }
 }

packages/dds/merge-tree/src/collections/list.ts
@@ -51,7 +51,7 @@ class DataNode<T> extends HeadNode<T> implements ListNode<T> {
     }
 }
 
-function insertAfter<T>(node: DataNode<T> | HeadNode<T>, ... items: T[]): ListNodeRange<T> | undefined {
+function insertAfter<T>(node: DataNode<T> | HeadNode<T>, items: T[]): ListNodeRange<T> {
     let previousNode = node;
     const oldNext = previousNode._next;
     let newRange: ListNodeRange<T> | undefined;
@@ -68,6 +68,14 @@ function insertAfter<T>(node: DataNode<T> | HeadNode<T>, ... items: T[]): ListNo
     });
     oldNext._prev = previousNode;
     previousNode._next = oldNext;
+    // explicitly prevent newRange from being undefined without casting,
+    // and without additional conditionals, as this is used in some perf critical areas.
+    // i could have just asserted, but that throws a non-user friendly error,
+    // so i went with a more user-friendly error, which describes the
+    // only condition that could lead to this being undefined in the current code.
+    if (newRange === undefined) {
+        throw new UsageError("items must not be empty");
+    }
     return newRange;
 }
 
@@ -75,25 +83,50 @@ export class List<T> implements
     Iterable<ListNode<T>>,
     Partial<ListNodeRange<T>>,
     // try to match array signature and semantics where possible
-    Pick<ListNode<T>[], "pop" | "shift" | "length" | "includes"> {
+    Pick<ListNode<T>[], "pop" | "shift" | "length" | "includes" > {
+    map<U>(callbackfn: (value: ListNode<T>) => U): Iterable<U> {
+        let node = this.first;
+        const iterator: IterableIterator<U> = {
+            next(): IteratorResult<U> {
+                if (node === undefined) {
+                    return { done: true, value: undefined };
+                }
+                const rtn = { value: callbackfn(node), done: false };
+                node = node.next;
+                return rtn;
+            },
+            [Symbol.iterator]() {
+                return this;
+            },
+        };
+        return iterator;
+    }
+
+    insertAfter(preceding: ListNode<T>, ...items: T[]): ListNodeRange<T> {
+        if (!this._includes(preceding)) {
+            throw new Error("preceding not in list");
+        }
+        this._len += items.length;
+        return insertAfter(preceding, items);
+    }
+
     pop(): ListNode<T> | undefined {
         return this.remove(this.last);
     }
 
-    push(item: T): ListNodeRange<T>;
-    push(...items: T[]): ListNodeRange<T> | undefined {
+    push(...items: T[]): ListNodeRange<T> {
         this._len += items.length;
         const start = this.headNode._prev;
-        return insertAfter(start, ... items);
+        return insertAfter(start, items);
     }
 
     shift(): ListNode<T> | undefined {
         return this.remove(this.first);
     }
 
-    unshift(...items: T[]): ListNodeRange<T> | undefined {
+    unshift(...items: T[]): ListNodeRange<T> {
         this._len += items.length;
-        return insertAfter(this.headNode, ... items);
+        return insertAfter(this.headNode, items);
     }
 
     public includes(node: ListNode<T> | undefined): node is ListNode<T> {

packages/dds/merge-tree/src/endOfTreeSegment.ts
@@ -1,0 +1,127 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { assert } from "@fluidframework/common-utils";
+import { UsageError } from "@fluidframework/container-utils";
+import { LocalClientId } from "./constants";
+import { LocalReferenceCollection } from "./localReference";
+import { ISegment, IRemovalInfo, IMergeNode, IMergeBlock } from "./mergeTreeNodes";
+import { depthFirstNodeWalk, NodeAction } from "./mergeTreeNodeWalk";
+
+/**
+ * This is a special segment that is not bound or known by the merge tree itself, but the segment itself pretends
+ * to be a removed segment at the end of the tree. It is removed so it appears as a undefined/0 length segment.
+ * This segment adds the capability to hold local references that have been detached from the real merge tree,
+ * and give the appearance that they exist at the end of the tree. In general, local references only become detached
+ * when the tree becomes empty and the EndOfTreeSegment allows us to gracefully handle that case by giving those
+ * references a place to live. The EndOfTreeSegment is not in itself a generalized solution to the problem of
+ * detached references, as it requires special and careful understanding of its usage. Specifically, it is local only
+ * and would not lead to eventually consistent results if serialization of its contained references was attempted,
+ * as on deserialization a naive re-insertion would not put the back into the EndOfTreeSegment
+ * leading to inconsistent results with existing clients.
+ */
+export class EndOfTreeSegment implements ISegment, IRemovalInfo {
+    type: string = "EndOfTreeSegment";
+    private readonly root: IMergeBlock;
+    constructor(segmentOrNode: IMergeNode) {
+        let maybeRoot: IMergeBlock | undefined = segmentOrNode.isLeaf()
+            ? segmentOrNode.parent
+            : segmentOrNode;
+        while (maybeRoot?.parent !== undefined) {
+            maybeRoot = maybeRoot.parent;
+        }
+        if (maybeRoot === undefined) {
+            throw new UsageError("segmentOrNode must be in rooted tree");
+        }
+        this.root = maybeRoot;
+    }
+    /*
+     * segments must be of at least length one, but
+     * removed segments will have a calculated length
+     * of undefined/0. we leverage this to create
+     * a 0 length segment for the end of the tree
+     */
+    removedSeq: number = 0;
+    removedClientIds: number[] = [LocalClientId];
+    seq = 0;
+    clientId = LocalClientId;
+    cachedLength = 1;
+    isLeaf(): this is ISegment {
+        return true;
+    }
+
+    /**
+     * the current supported usage uses the local refs to
+     * store detached references
+     */
+    localRefs?: LocalReferenceCollection;
+
+    /**
+     * this segment pretends to be a sibling of the last real segment.
+     * so compute the necessary properties to pretend to be that segment.
+     */
+    private getEndSegProps() {
+        let lastSegment: ISegment | undefined;
+        let depth = 1;
+        depthFirstNodeWalk(
+            this.root,
+            this.root.children[this.root.childCount - 1],
+            (node) => {
+                depth++;
+                if (node?.isLeaf()) {
+                    lastSegment = node;
+                    return NodeAction.Exit;
+                }
+             },
+            undefined,
+            undefined,
+            false,
+        );
+        const parent = lastSegment?.parent ?? this.root;
+        const index = parent.childCount;
+        return {
+            parent,
+            index,
+            depth,
+        };
+    }
+
+    get parent() {
+        return this.getEndSegProps().parent;
+    }
+
+    get index() {
+        return this.getEndSegProps().index;
+    }
+    get ordinal() {
+        // just compute an arbitrarily big ordinal
+        // we base it on the depth of the tree
+        // to ensure it is bigger than all ordinals in
+        // the tree, as each layer appends to the previous
+        return String.fromCharCode(0xFFFF).repeat(
+            this.getEndSegProps().depth);
+    }
+
+    /*
+     * since this segment isn't real, throw on any segment
+     * operation that isn't expected
+     */
+    get segmentGroups() {
+        return notSupported();
+    }
+    get trackingCollection() {
+        return notSupported();
+    }
+    addProperties = notSupported;
+    clone = notSupported;
+    canAppend = notSupported;
+    append = notSupported;
+    splitAt = notSupported;
+    toJSONObject = notSupported;
+    ack = notSupported;
+}
+const notSupported = () => {
+    assert(false, "operation not supported");
+};

packages/dds/merge-tree/src/index.ts
@@ -39,3 +39,10 @@ export * from "./segmentPropertiesManager";
 export * from "./sortedSegmentSet";
 export * from "./textSegment";
 export * from "./referencePositions";
+export {
+    MergeTreeDeltaRevertible,
+    MergeTreeRevertibleDriver,
+    appendToMergeTreeDeltaRevertibles,
+    discardMergeTreeDeltaRevertible,
+    revertMergeTreeDeltaRevertibles,
+} from "./revertibles";

packages/dds/merge-tree/src/mergeTree.ts
@@ -906,9 +906,9 @@ export class MergeTree {
             }
         } else {
             for (const ref of refsToSlide) {
-                ref.callbacks?.beforeSlide?.();
+                ref.callbacks?.beforeSlide?.(ref);
                 segment.localRefs?.removeLocalRef(ref);
-                ref.callbacks?.afterSlide?.();
+                ref.callbacks?.afterSlide?.(ref);
             }
         }
         // TODO is it required to update the path lengths?

packages/dds/merge-tree/src/mergeTreeNodeWalk.ts
@@ -168,7 +168,7 @@ export function backwardExcursion(
  */
 export function walkAllChildSegments(
     startBlock: IMergeBlock,
-    leafAction: (segment: ISegment) => boolean | undefined,
+    leafAction: (segment: ISegment) => boolean | undefined | void,
 ): boolean {
     if (startBlock.childCount === 0) {
         return true;

packages/dds/merge-tree/src/mergeTreeTracking.ts
@@ -45,10 +45,12 @@ export class TrackingGroup {
         }
     }
 
-    public unlink(segment: Trackable) {
-        if (this.trackedSet.remove(segment)) {
-            segment.trackingCollection.unlink(this);
+    public unlink(trackable: Trackable) {
+        if (this.trackedSet.remove(trackable)) {
+            trackable.trackingCollection.unlink(this);
+            return true;
         }
+        return false;
     }
 }
 
@@ -71,11 +73,16 @@ export class TrackingGroupCollection {
         }
     }
 
-    public unlink(trackingGroup: TrackingGroup): void {
-        if (trackingGroup.has(this.trackable)) {
-            trackingGroup.unlink(this.trackable);
+    public unlink(trackingGroup: TrackingGroup): boolean {
+        if (this.trackingGroups.has(trackingGroup)) {
+            if (trackingGroup.has(this.trackable)) {
+                trackingGroup.unlink(this.trackable);
+            }
+            this.trackingGroups.delete(trackingGroup);
+            return true;
         }
-        this.trackingGroups.delete(trackingGroup);
+
+        return false;
     }
 
     public copyTo(trackable: Trackable) {

commit 8c4b990e5f02510c289fde488761e2ac1c4fa66b

fix(merge-tree): visit the starting reference in walkReferences
LocalReferenceCollection.walkReferences compared the wrong objects when
locating the before/at/after bucket containing the supplied `start`. It
compared list nodes of the outer bucket list against the list node of
`start` itself, which are never equal, so the walk discarded every bucket
at `start`'s offset and began at the next offset instead. As a result a
walk with an explicit `start` never visited `start` or any other reference
at its offset, and the `startList === pos ? startNode : undefined`
argument passed to walkList was unreachable.

Compare bucket to bucket instead, so the loop stops at the bucket holding
`start` and walkList resumes mid-bucket from `startNode`.

The typo has been present since walkReferences was introduced in #11899.
It went unnoticed because the optional before/at/after fields make the two
node types overlap through `undefined`, which suppresses TypeScript's
TS2367 no-overlap check, and because every in-repo caller passes
`undefined` for `start`.

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

packages/dds/merge-tree/src/localReference.ts
@@ -590,12 +590,14 @@ export class LocalReferenceCollection {
 		const startList = startNode?.list;
 
 		if (startList !== undefined) {
+			// Discard the buckets (before/at/after) which precede the bucket containing `start`,
+			// so the walk begins at that bucket. `listWalker` then resumes mid-bucket at `startNode`.
 			if (forward) {
-				while (!offsetPositions.empty && offsetPositions.first !== startNode) {
+				while (!offsetPositions.empty && offsetPositions.first?.data !== startList) {
 					offsetPositions.shift();
 				}
 			} else {
-				while (!offsetPositions.empty && offsetPositions.last !== startNode) {
+				while (!offsetPositions.empty && offsetPositions.last?.data !== startList) {
 					offsetPositions.pop();
 				}
 			}

packages/dds/merge-tree/src/test/localReference.spec.ts
@@ -1,0 +1,116 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { strict as assert } from "node:assert";
+
+import { LocalReferenceCollection, type LocalReferencePosition } from "../localReference.js";
+import type { ISegmentInternal } from "../mergeTreeNodes.js";
+import { ReferenceType } from "../ops.js";
+import { TextSegment } from "../textSegment.js";
+
+interface TestSetup {
+	collection: LocalReferenceCollection;
+	/**
+	 * All refs in collection order (offset ascending, and insertion order within an offset).
+	 */
+	refs: LocalReferencePosition[];
+}
+
+/**
+ * Creates a collection over a segment of length `text.length` with `refsPerOffset`
+ * references at every offset. Each reference is labeled via its `id` property so
+ * walk order can be asserted.
+ */
+function setup(text: string, refsPerOffset: number): TestSetup {
+	const segment = TextSegment.make(text) as ISegmentInternal;
+	const collection = LocalReferenceCollection.setOrGet(segment);
+	const refs: LocalReferencePosition[] = [];
+	for (let offset = 0; offset < text.length; offset++) {
+		for (let i = 0; i < refsPerOffset; i++) {
+			refs.push(
+				collection.createLocalRef(offset, ReferenceType.Simple, { id: `${offset}-${i}` }),
+			);
+		}
+	}
+	return { collection, refs };
+}
+
+function walk(
+	collection: LocalReferenceCollection,
+	start?: LocalReferencePosition,
+	forward: boolean = true,
+): string[] {
+	const visited: string[] = [];
+	collection.walkReferences(
+		(lref) => {
+			visited.push(lref.properties?.id as string);
+		},
+		start,
+		forward,
+	);
+	return visited;
+}
+
+describe("LocalReferenceCollection", () => {
+	describe("walkReferences", () => {
+		it("walks all references when no start is provided", () => {
+			const { collection } = setup("abc", 2);
+			assert.deepEqual(walk(collection), ["0-0", "0-1", "1-0", "1-1", "2-0", "2-1"]);
+		});
+
+		it("walks all references backward when no start is provided", () => {
+			const { collection } = setup("abc", 2);
+			assert.deepEqual(walk(collection, undefined, false), [
+				"2-1",
+				"2-0",
+				"1-1",
+				"1-0",
+				"0-1",
+				"0-0",
+			]);
+		});
+
+		it("includes the start reference when walking forward", () => {
+			const { collection, refs } = setup("abc", 2);
+			// refs[2] is the first reference at offset 1
+			assert.deepEqual(walk(collection, refs[2]), ["1-0", "1-1", "2-0", "2-1"]);
+		});
+
+		it("includes the start reference when walking backward", () => {
+			const { collection, refs } = setup("abc", 2);
+			assert.deepEqual(walk(collection, refs[2], false), ["1-0", "0-1", "0-0"]);
+		});
+
+		it("resumes mid-list when start is not the first reference at its offset", () => {
+			const { collection, refs } = setup("abc", 3);
+			// refs[4] is the second reference at offset 1
+			assert.deepEqual(walk(collection, refs[4]), ["1-1", "1-2", "2-0", "2-1", "2-2"]);
+			assert.deepEqual(walk(collection, refs[4], false), ["1-1", "1-0", "0-2", "0-1", "0-0"]);
+		});
+
+		it("includes the start reference at the first offset", () => {
+			const { collection, refs } = setup("abc", 1);
+			assert.deepEqual(walk(collection, refs[0]), ["0-0", "1-0", "2-0"]);
+			assert.deepEqual(walk(collection, refs[0], false), ["0-0"]);
+		});
+
+		it("includes the start reference at the last offset", () => {
+			const { collection, refs } = setup("abc", 1);
+			assert.deepEqual(walk(collection, refs[2]), ["2-0"]);
+			assert.deepEqual(walk(collection, refs[2], false), ["2-0", "1-0", "0-0"]);
+		});
+
+		it("stops early when the visitor returns false", () => {
+			const { collection, refs } = setup("abc", 2);
+			const visited: string[] = [];
+			const completed = collection.walkReferences((lref) => {
+				visited.push(lref.properties?.id as string);
+				return lref.properties?.id !== "1-1";
+			}, refs[2]);
+			assert.equal(completed, false);
+			assert.deepEqual(visited, ["1-0", "1-1"]);
+		});
+	});
+});