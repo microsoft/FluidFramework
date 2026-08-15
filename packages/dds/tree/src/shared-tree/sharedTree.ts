482d71cfde2badb339720bef6f421545468e8b77
yann-achard-MS <97201204+yann-achard-MS@users.noreply.github.com>
2026-08-15 01:21:36 +0000

    docs

--- packages/dds/tree/src/shared-tree/sharedTree.ts
+++ packages/dds/tree/src/shared-tree/sharedTree.ts
@@ -667,18 +667,25 @@ export interface SharedTreeOptions
 	/**
 	 * @defaultValue `false`
 	 *
-	 * When `true`, validates that the commits being submitted can be applied to a view (assuming no concurrent changes).
+	 * When `true`, validates that commits being submitted for the first time can be applied without errors to a view.
 	 * In the event that a commit cannot be applied, SharedTree will throw an error and will enter a "broken" state, preventing the offending commit (and any further commits) from being submitted.
-	 * This can be enabled (at the cost of performance) to improve safety against document corruption in the event of a bug in the SharedTree code.
+	 *
+	 * This can be enabled (at the cost of performance) to improve safety against document corruption in the event of a bug in the SharedTree code:
+	 * when the additional validation is enabled, a client will error instead of potentially corrupting the document.
+	 *
+	 * @remarks
+	 * This validation is more expensive than {@link SharedTreeOptions.validateRebasedCommitsBeforeResubmission} because it is likely to be performed more often.
 	 */
 	readonly validateCommitsOnFirstSubmission?: boolean;
 
 	/**
 	 * @defaultValue `false`
 	 *
-	 * When `true`, validates that the commits being resubmitted can be applied to a view (assuming no concurrent changes).
+	 * When `true`, validates that the commits being resubmitted can be applied without errors to a view.
 	 * In the event that a commit cannot be applied, SharedTree will throw an error and will enter a "broken" state, preventing the offending commit (and any further commits) from being submitted.
-	 * This can be enabled (at the cost of performance) to improve safety against document corruption in the event of a bug in the SharedTree code.
+	 *
+	 * This can be enabled (at the cost of performance) to improve safety against document corruption in the event of a bug in the SharedTree code:
+	 * when the additional validation is enabled, a client will error instead of potentially corrupting the document.
 	 */
 	readonly validateRebasedCommitsBeforeResubmission?: boolean;
 }
