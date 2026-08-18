3a506299ab4de250ed25e0d3bd40550719ea5a6b
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2026-08-18 14:14:45 -0500

    build(eslint-config-fluid): consolidate custom rules into config package (#27943)
    ## Description
    
    Moves the custom Fluid ESLint rules into
    `@fluidframework/eslint-config-fluid`, removing the separate
    `@fluid-internal/eslint-plugin-fluid` package and release pipeline.
    Existing `@fluid-internal/fluid/*` rule names are preserved, so
    consumers of the shared config do not need to migrate rule overrides or
    suppression comments.
    
    The config package is updated to 14.1.0 and now owns the rule
    dependencies, documentation, and ESLint 8/9 test coverage. Repository
    package metadata and pipeline-trigger guidance no longer reference the
    removed plugin.
    
    Implements
    [AB#79903](https://dev.azure.com/fluidframework/235294da-091d-4c29-84fc-cdfc3d90890b/_workitems/edit/79903).
    
    ## Reviewer Guidance
    
    The review process is outlined on [this wiki
    page](https://github.com/microsoft/FluidFramework/wiki/PR-Guidelines#guidelines).
    
    The npm deprecation of `@fluid-internal/eslint-plugin-fluid` should
    happen after `@fluidframework/eslint-config-fluid` 14.1.0 is published.
    
    ---------
    
    Copilot-Session: dd9688d7-f485-461a-9a13-1c3ef9f2322a

--- fluidBuild.config.cjs
+++ fluidBuild.config.cjs
@@ -416,7 +416,6 @@ module.exports = {
 		// Independent packages
 		"build-common": "common/build/build-common",
 		"eslint-config-fluid": "common/build/eslint-config-fluid",
-		"eslint-plugin-fluid": "common/build/eslint-plugin-fluid",
 		"common-utils": "common/lib/common-utils",
 		"protocol-def": "common/lib/protocol-definitions",
 
@@ -507,9 +506,8 @@ module.exports = {
 				// Could be renamed, but there is tooling that uses this name and it's not worth it.
 				"common/build/build-common/gen_version.js",
 
-				// ESLint shared config and plugin
+				// ESLint shared config
 				"common/build/eslint-config-fluid/.*",
-				"common/build/eslint-plugin-fluid/.*",
 
 				"common/lib/common-utils/jest-puppeteer.config.js",
 				"common/lib/common-utils/jest.config.js",
@@ -706,7 +704,6 @@ module.exports = {
 			"@fluidframework/build-common",
 			"@fluidframework/common-utils",
 			"@fluidframework/eslint-config-fluid",
-			"@fluid-internal/eslint-plugin-fluid",
 			"@fluidframework/protocol-definitions",
 			"@fluidframework/test-tools",
 			"fluidframework-docs",
