5b996fee946fed3212ac2564e01a694ef3ae5555
Daniel Madrid <105010181+dannimad@users.noreply.github.com>
2026-08-19 20:40:55 +0000

    type test fix

--- packages/framework/aqueduct/src/test/types/validateAqueductPrevious.generated.ts
+++ packages/framework/aqueduct/src/test/types/validateAqueductPrevious.generated.ts
@@ -258,6 +258,7 @@ declare type old_as_current_for_Interface_ContainerRuntimeFactoryWithDefaultData
  * typeValidation.broken:
  * "Interface_ContainerRuntimeFactoryWithDefaultDataStoreProps": {"backCompat": false}
  */
+// @ts-expect-error compatibility expected to be broken
 declare type current_as_old_for_Interface_ContainerRuntimeFactoryWithDefaultDataStoreProps = requireAssignableTo<TypeOnly<current.ContainerRuntimeFactoryWithDefaultDataStoreProps>, TypeOnly<old.ContainerRuntimeFactoryWithDefaultDataStoreProps>>
 
 /*
