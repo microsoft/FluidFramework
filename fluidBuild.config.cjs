a5be956f0c66d12fa323ce00d711f95ffa3d68d1
Jason Hartman <jasonha@microsoft.com>
2026-08-24 23:19:25 +0000

    build(client): fix api-extractor report generation Reports may vary if the metadata file is not present and discovered by `tsdocMetadata` in package.json and without a `exports["."].types` entry api-extractor looks to `types`, `typings`, and `main`, and finally the package root.
    Ensure that metadata file is always generated. And for the current issue with fluid-framework exporting merge-tree type via sequence, add `tsdocMetadata` to merge-tree.

--- fluidBuild.config.cjs
+++ fluidBuild.config.cjs
@@ -210,19 +210,53 @@ module.exports = {
 		// Packages with /legacy exports generate reports from legacy and "current" entrypoints using child tasks.
 		// The "current" entrypoint should be the broadest of "public.d.ts",
 		// "beta.d.ts", and "alpha.d.ts".
-		"build:api-reports": ["build:package:esm", "api-extractor:esnext", "build:esnext"],
-		"build:api-reports:current": ["build:package:esm", "api-extractor:esnext", "build:esnext"],
-		"build:api-reports:legacy": ["build:package:esm", "api-extractor:esnext", "build:esnext"],
-		"ci:build:api-reports": ["build:package:esm", "api-extractor:esnext", "build:esnext"],
+		"build:api-reports": [
+			"build:package:esm",
+			"api-extractor:esnext",
+			"build:esnext",
+			// kludge: api-extractor requires tsdoc-metadata.json to be present in the package type folder
+			// to know that it can use the tsdocs in source package.
+			"^build:api-reports",
+		],
+		"build:api-reports:current": [
+			"build:package:esm",
+			"api-extractor:esnext",
+			"build:esnext",
+			// kludge: api-extractor requires tsdoc-metadata.json to be present in the package type folder
+			// to know that it can use the tsdocs in source package.
+			"^build:api-reports",
+		],
+		"build:api-reports:legacy": [
+			"build:package:esm",
+			"api-extractor:esnext",
+			"build:esnext",
+			// kludge: api-extractor requires tsdoc-metadata.json to be present in the package type folder
+			// to know that it can use the tsdocs in source package.
+			"^build:api-reports",
+		],
+		"ci:build:api-reports": [
+			"build:package:esm",
+			"api-extractor:esnext",
+			"build:esnext",
+			// kludge: api-extractor requires tsdoc-metadata.json to be present in the package type folder
+			// to know that it can use the tsdocs in source package. Under CI, these checks do the generation.
+			"^ci:build:api-reports",
+		],
 		"ci:build:api-reports:current": [
 			"build:package:esm",
 			"api-extractor:esnext",
 			"build:esnext",
+			// kludge: api-extractor requires tsdoc-metadata.json to be present in the package type folder
+			// to know that it can use the tsdocs in source package. Under CI, these checks do the generation.
+			"^ci:build:api-reports",
 		],
 		"ci:build:api-reports:legacy": [
 			"build:package:esm",
 			"api-extractor:esnext",
 			"build:esnext",
+			// kludge: api-extractor requires tsdoc-metadata.json to be present in the package type folder
+			// to know that it can use the tsdocs in source package. Under CI, these checks do the generation.
+			"^ci:build:api-reports",
 		],
 		// With most packages in client building ESM first, there is ideally just "build:esnext" and "build:package:esm" (or "api-extractor:esnext") dependencies.
 		// The package's local 'api-extractor.json' may use the entrypoint from either CJS or ESM,
@@ -245,6 +279,9 @@ module.exports = {
 		// This is done because the "check:exports:*" tasks have variable
 		// names depending on the package configuration and fluid-build does
 		// not support wildcards in task dependencies.
+		// note: these are technically missing dependency on a task that generates tsdoc-metadata.json.
+		// However different tasks are used to do that depending on run under CI or not. The metadata
+		// file is small and changes rarely so it can be checked-in in future to avoid complexity.
 		"check:exports": ["build:package", "api"],
 		"check:exports:bundle-release-tags": ["build:esm", "build:esnext"],
 		// The package's local 'api-extractor-lint.json' may use the entrypoint from either CJS or ESM,
