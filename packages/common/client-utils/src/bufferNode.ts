83d3ea99326f9bbf741e757f04c0b752e60fa59f
Jason Hartman <jasonha@microsoft.com>
2026-08-24 17:51:39 +0000

    docs(client-client-utils): mimic Node Buffer docs

--- packages/common/client-utils/src/bufferNode.ts
+++ packages/common/client-utils/src/bufferNode.ts
@@ -33,52 +33,59 @@ export declare class Buffer<TArrayBuffer extends ArrayBufferLike = ArrayBufferLi
 	implements IsoBuffer<TArrayBuffer>
 {
 	/**
-	 * Static constructor
+	 * Creates a new `IsoBuffer` containing `string`. The `encoding` parameter identifies
+	 * the character encoding to be used when converting `string` into bytes.
 	 *
-	 * @param value - `string`.
-	 * @param encoding - `IsoBufferEncoding`.
+	 * @param string - A string to encode.
+	 * @param encoding - The encoding of `string`. **Default:** `'utf8'`.
 	 *
 	 * @privateRemarks
 	 * Node buffer.buffer.d.ts declaration:
 	 * `from(string: WithImplicitCoercion<string>, encoding?: IsoBufferEncoding): Buffer<ArrayBuffer>;`
 	 */
-	public static from(value: string, encoding?: IsoBufferEncoding): IsoBuffer<ArrayBuffer>;
+	public static from(string: string, encoding?: IsoBufferEncoding): IsoBuffer<ArrayBuffer>;
 	/**
-	 * Static constructor
+	 * Allocates a new `IsoBuffer` using an `array` of bytes in the range `0` – `255`.
+	 * Array entries outside that range will be truncated to fit into it.
 	 *
 	 * @param value - `Uint8Array`.
 	 *
 	 * @remarks
-	 * Is handled as a special case of `ArrayLike<number>` that is compatible with Browser implementation.
+	 * This is limited to `Uint8Array` input to match Node implementation that
+	 * takes `ArrayLike<number>` and returns `IsoBuffer<ArrayBuffer>`.
 	 *
 	 * @privateRemarks
 	 * Node buffer.buffer.d.ts declaration:
 	 * `from(array: WithImplicitCoercion<ArrayLike<number>>): IsoBuffer<ArrayBuffer>;`
-	 * This is required for class to compile. Otherwise static property of from is incompatible.
 	 */
-	public static from(value: Uint8Array): IsoBuffer<ArrayBuffer>;
+	public static from(array: Uint8Array): IsoBuffer<ArrayBuffer>;
 	/**
-	 * Static constructor
+	 * This creates a view of the `ArrayBuffer` without copying the underlying
+	 * memory. For example, when passed a reference to the `.buffer` property of a
+	 * `TypedArray` instance, the newly created `Buffer` will share the same
+	 * allocated memory as the `TypedArray`'s underlying `ArrayBuffer`.
 	 *
-	 * @param value - `ArrayBufferLike`.
-	 * @param byteOffset - `number`.
-	 * @param length - `number`.
+	 * @param arrayBuffer - An `ArrayBuffer`, `SharedArrayBuffer`, for example the
+	 * `.buffer` property of a `TypedArray`.
+	 * @param byteOffset - Index of first byte to expose. **Default:** `0`.
+	 * @param length - Number of bytes to expose. **Default:**
+	 * `arrayBuffer.byteLength - byteOffset`.
 	 *
 	 * @privateRemarks
 	 * Node buffer.buffer.d.ts declaration:
 	 * ```
-	 * public static from<TFArrayBuffer extends WithImplicitCoercion<ArrayBufferLike>>(
-	 * 		arrayBuffer: TFArrayBuffer,
+	 * public static from<TArrayBuffer extends WithImplicitCoercion<ArrayBufferLike>>(
+	 * 		arrayBuffer: TArrayBuffer,
 	 * 		byteOffset?: number,
 	 * 		length?: number,
-	 * ): Buffer<ImplicitArrayBuffer<TFArrayBuffer>>;
+	 * ): Buffer<ImplicitArrayBuffer<TArrayBuffer>>;
 	 * ```
 	 */
-	public static from<TFArrayBuffer extends ArrayBufferLike>(
-		value: TFArrayBuffer,
+	public static from<TArrayBuffer extends ArrayBufferLike>(
+		arrayBuffer: TArrayBuffer,
 		byteOffset?: number,
 		length?: number,
-	): IsoBuffer<TFArrayBuffer>;
+	): IsoBuffer<TArrayBuffer>;
 
 	public static isBuffer(obj: unknown): obj is Buffer;
 }
