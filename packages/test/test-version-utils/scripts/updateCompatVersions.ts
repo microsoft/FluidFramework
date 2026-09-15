a34793ff934925112120e3f4a12b433aa0f2a16e
Jason Hartman <jasonha@microsoft.com>
2026-09-14 20:35:46 -0700

    docs: Apply suggestions from code review
    Co-authored-by: Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
    Co-authored-by: Jason Hartman <jasonha@microsoft.com>

--- packages/test/test-version-utils/scripts/updateCompatVersions.ts
+++ packages/test/test-version-utils/scripts/updateCompatVersions.ts
@@ -13,10 +13,10 @@
  *   regenerates the `CompatibilityCheckpoints.md` table from that file
  *
  * Before running this script, make sure the repo is sufficiently built by executing:
- *   pnpm exec fluid-build build:compile:esm:packages --task test-version-utils
+ *   `pnpm exec fluid-build build:compile:esm:packages --task test-version-utils`
  *
  * Running the update script:
- *   pnpm -r --filter @fluid-private/test-version-utils run update-compat-versions
+ *   `pnpm -r --filter @fluid-private/test-version-utils run update-compat-versions`
  *
  * The script:
  *   1. Reads the current package version from `src/packageVersion.ts`.
