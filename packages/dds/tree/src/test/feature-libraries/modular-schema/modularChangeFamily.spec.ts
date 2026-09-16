70ee48f88d790c52cc114e32d53a6ca43a10e405
alex-pardes <105307164+alex-pardes@users.noreply.github.com>
2026-09-16 17:41:10 +0000

    Fix test

--- packages/dds/tree/src/test/feature-libraries/modular-schema/modularChangeFamily.spec.ts
+++ packages/dds/tree/src/test/feature-libraries/modular-schema/modularChangeFamily.spec.ts
@@ -1338,8 +1338,8 @@ describe("ModularChangeFamily", () => {
 
 	describe("Encoding", () => {
 		function assertEquivalent(change1: ModularChangeset, change2: ModularChangeset) {
-			const normalized1 = normalizeChangeset(change1);
-			const normalized2 = normalizeChangeset(change2);
+			const normalized1 = normalizeChangeset(change1, fieldKinds);
+			const normalized2 = normalizeChangeset(change2, fieldKinds);
 			assertEqual(normalized1, normalized2);
 		}
 
