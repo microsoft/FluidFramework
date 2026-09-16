7a0186dbd1cde8ce8d1e90e63948c6573d01a3b8
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2026-09-16 20:51:53 +0000

    fix(build-tools): satisfy policy for webpack fixtures

--- build-tools/packages/build-tools/src/test/data/webpack/webpack.config.cjs
+++ build-tools/packages/build-tools/src/test/data/webpack/webpack.config.cjs
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 module.exports = (env) => ({
 	extension: "cjs",
 	mode: env.production ? "production" : "development",
